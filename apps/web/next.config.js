6e31607a28f4ea886b3e37efd546e5c016811367
Eirikur Nilsson <eirikur@nilsson.is>
2020-04-15 22:01:26 +0000

    initial commit

---
+++ apps/web/next.config.js
@@ -0,0 +1,6 @@
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
+  // Set this to true if you use CSS modules.
+  // See: https://github.com/css-modules/css-modules
+  cssModules: false
+});
