4b1b4345924454798d74977e09149ce79a2eed5a
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-05-08 13:26:54 +0000

    Cast to proptypes

--- apps/gjafakort/web/pages/en/index.tsx
+++ apps/gjafakort/web/pages/en/index.tsx
@@ -1,4 +1,5 @@
 import { withLocale } from '../../i18n'
 import HomePage from '../../screens/HomePage'
+import { Screen } from '../../types'
 
-export default withLocale('en')(HomePage)
+export default withLocale('en')(HomePage as Screen)
