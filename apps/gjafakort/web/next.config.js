e6928a77b105ba110efda08dbe7f6cdfab0e76ac
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-05-13 14:39:05 +0000

    Start using treat

--- apps/gjafakort/web/next.config.js
+++ apps/gjafakort/web/next.config.js
@@ -1,24 +1,17 @@
-const withSass = require('@zeit/next-sass') // eslint-disable-line
-const withCss = require('@zeit/next-css') // eslint-disable-line
+const withTreat = require('next-treat')()
 
 const {
   API_URL = 'http://localhost:4444/api',
   WEB_PUBLIC_URL = 'http://localhost:4200',
 } = process.env
 
-module.exports = withSass(
-  withCss({
-    cssModules: true,
-    sassLoaderOptions: {
-      includePaths: ['./apps/gjafakort/web/styles'],
-    },
-    serverRuntimeConfig: {
-      // Will only be available on the server side
-      apiUrl: API_URL,
-    },
-    publicRuntimeConfig: {
-      // Will be available on both server and client
-      apiUrl: `${WEB_PUBLIC_URL}/api`,
-    },
-  }),
-)
+module.exports = withTreat({
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiUrl: API_URL,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiUrl: `${WEB_PUBLIC_URL}/api`,
+  },
+})
