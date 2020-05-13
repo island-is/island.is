13aa3fa5b86bf08dbc66ba84715d84bb906eedd5
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-05-13 10:26:07 +0000

    Only run tests from test dir

--- apps/application/jest.config.js
+++ apps/application/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'application',
+  testMatch: ['**/test/**/+(*.)+(spec|test).+(ts|js)?(x)'],
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/apps/application',
   setupFilesAfterEnv: ['./test/setup.ts'],
