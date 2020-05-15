04e1609d721b06acef34779ed8dd17be967cc30c
Brian Johannessen <barabrian89@gmail.com>
2020-05-15 19:30:38 +0000

    select cmp and icon

--- libs/gjafakort-ui/src/index.ts
+++ libs/gjafakort-ui/src/index.ts
@@ -1,3 +1,5 @@
+export * from './lib/icon/Icon'
+export * from './lib/select/Select'
 export * from './lib/grid-container/GridContainer'
 export * from './lib/page/Page'
 export * from './lib/header/Header'
