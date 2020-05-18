a2af3e768f3a5804c1c57536b14855ddebcf54b4
Brian Johannessen <barabrian89@gmail.com>
2020-05-18 12:13:13 +0000

    allow scss little longer

--- apps/gjafakort/web/next.config.js
+++ apps/gjafakort/web/next.config.js
@@ -5,13 +5,21 @@ const {
   WEB_PUBLIC_URL = 'http://localhost:4200',
 } = process.env
 
-module.exports = withTreat({
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    apiUrl: API_URL,
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    apiUrl: `${WEB_PUBLIC_URL}/api`,
-  },
-})
+module.exports = withSass(
+  withCss(
+    withTreat({
+      cssModules: true,
+      sassLoaderOptions: {
+        includePaths: ['./apps/gjafakort/web/styles'],
+      },
+      serverRuntimeConfig: {
+        // Will only be available on the server side
+        apiUrl: API_URL,
+      },
+      publicRuntimeConfig: {
+        // Will be available on both server and client
+        apiUrl: `${WEB_PUBLIC_URL}/api`,
+      },
+    }),
+  ),
+)
