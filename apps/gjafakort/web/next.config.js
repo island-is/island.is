c58cbe7aa65c28b61b21863a2693bb48c034f25a
Brian Johannessen <barabrian89@gmail.com>
2020-05-19 11:09:32 +0000

    remove remaining sass setup

--- apps/gjafakort/web/next.config.js
+++ apps/gjafakort/web/next.config.js
@@ -1,5 +1,3 @@
-const withSass = require('@zeit/next-sass') // eslint-disable-line
-const withCss = require('@zeit/next-css') // eslint-disable-line
 const withTreat = require('next-treat')() // eslint-disable-line
 
 const {
@@ -7,21 +5,13 @@ const {
   WEB_PUBLIC_URL = 'http://localhost:4200',
 } = process.env
 
-module.exports = withSass(
-  withCss(
-    withTreat({
-      cssModules: true,
-      sassLoaderOptions: {
-        includePaths: ['./apps/gjafakort/web/styles'],
-      },
-      serverRuntimeConfig: {
-        // Will only be available on the server side
-        apiUrl: API_URL,
-      },
-      publicRuntimeConfig: {
-        // Will be available on both server and client
-        apiUrl: `${WEB_PUBLIC_URL}/api`,
-      },
-    }),
-  ),
-)
+module.exports = withTreat({
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiUrl: API_URL,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiUrl: `${WEB_PUBLIC_URL}/api`,
+  },
+})
