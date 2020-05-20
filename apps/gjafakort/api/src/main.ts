2dc605fa6c9a568f8e6e6896d288dad6aabd6f6e
Davíð Guðni Halldórsson <davidgudni@gmail.com>
2020-05-20 11:34:27 +0000

    [gjafakort] Adding a liveness probe

--- apps/gjafakort/api/src/main.ts
+++ apps/gjafakort/api/src/main.ts
@@ -3,6 +3,10 @@ import createGraphqlServer from './graphql'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello, I am a liveness probe' })
+})
+
 createGraphqlServer(app)
 
 const port = process.env.port || 3333
