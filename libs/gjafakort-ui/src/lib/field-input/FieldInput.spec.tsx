788d9101ea56211c39e1ad3723d766e5a55ad1dd
Brian Johannessen <barabrian89@gmail.com>
2020-05-20 10:44:56 +0000

    fix tests

--- libs/gjafakort-ui/src/lib/field-input/FieldInput.spec.tsx
+++ libs/gjafakort-ui/src/lib/field-input/FieldInput.spec.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Formik, Field } from 'formik'
 import { render } from '@testing-library/react'
 
 import FieldInput from './FieldInput'
 
 describe(' FieldInput', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<FieldInput />)
+    const { baseElement } = render(
+      <Formik
+        initialValues={{}}
+        onSubmit={() => {
+          // submit
+        }}
+      >
+        <Field component={FieldInput} />
+      </Formik>,
+    )
     expect(baseElement).toBeTruthy()
   })
 })
