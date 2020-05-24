15655863bf1cc7f67ee60091c08b64f14996606d
Petar Shomov <petar@andes.is>
2020-05-24 14:51:42 +0000

    now adding the build of the docker image

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/_common.sh
 
-exec docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=linter --build-arg APP=${APP} -t ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} . 
+exec docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=linter --build-arg APP=${APP} . 
 # exec docker build -f ${DIR}/Dockerfile --target=linter --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg APP=${APP} . 
