c25f34365f7fa6fa8e0aaf6c67550f34a0a1717f
Petar Shomov <petar@andes.is>
2020-05-24 01:38:05 +0000

    now going for linting...

--- scripts/prepare_deps.sh
+++ scripts/prepare_deps.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/_common.sh
 
 # docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
-docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache --cache-to=type=local,dest=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=deps --load --build-arg APP=${APP} -t ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} . 
+docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache --cache-to=type=local,dest=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=deps --build-arg APP=${APP} -t ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} . 
 # $PUBLISH || echo "Not publishing ${DEPS_TAG}"
 # $PUBLISH && docker push ${DOCKER_REGISTRY}${APP}:${DEPS_TAG}
