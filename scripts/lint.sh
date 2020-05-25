779bb85af59d1a1f6217c3d37ad9e466c5094850
Petar Shomov <petar@andes.is>
2020-05-25 09:25:01 +0000

    getting rid of changes that are now on a different branch

--- scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-source $DIR/_common.sh
-
-exec docker build -f ${DIR}/Dockerfile --target=linter --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg APP=${APP} . 
