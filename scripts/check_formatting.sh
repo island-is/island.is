232e203fcee38657cf7ca79d73fead7f0d7744c8
Petar Shomov <petar@andes.is>
2020-05-25 22:28:41 +0000

    going back to caching in docker registry

--- scripts/check_formatting.sh
+++ scripts/check_formatting.sh
@@ -5,7 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/_common.sh
 
-# docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
-docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=formatting --build-arg APP=${APP} . 
+docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
+# docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=formatting --build-arg APP=${APP} . 
+exec docker build -f ${DIR}/Dockerfile --target=formatting --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg APP=${APP} . 
 # $PUBLISH || echo "Not publishing ${DEPS_TAG}"
 # $PUBLISH && docker push ${DOCKER_REGISTRY}${APP}:${DEPS_TAG}
