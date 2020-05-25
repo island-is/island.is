2b70864d2da1b6231e882e85ad2bbb3f225da473
Stefán Harald <stefanharald@gmail.com>
2020-05-25 14:27:39 +0000

    Added base for cms graphql

--- apps/api/src/graphql/domains.ts
+++ apps/api/src/graphql/domains.ts
@@ -1,3 +1,4 @@
 import * as helloWorld from '@island.is/api/domains/hello-world'
+import * as cms from '@island.is/api/domains/cms'
 
-export default [helloWorld]
+export default [helloWorld, cms]
---
+++ libs/api/domains/cms/src/lib/services.ts
@@ -0,0 +1,5 @@
+export const getArticle = (id: string) => ({
+  id,
+  title: 'Forsíða',
+  body: 'Text and some text',
+})
