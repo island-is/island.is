e779e2388f7cd489e0ca6e4138125e4095a03148
Davíð Guðni Halldórsson <davidgudni@gmail.com>
2020-05-25 09:43:09 +0000

    renaming application to gjafakort-application

--- apps/application/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'application',
-  testMatch: ['**/test/**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/application',
-  setupFilesAfterEnv: ['./test/setup.ts'],
-}
