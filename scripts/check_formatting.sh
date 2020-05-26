9e774e1e7618861a5eca8cdcb6a810cbd295a308
Petar Shomov <petar@andes.is>
2020-05-26 17:03:31 +0000

    adding new build arg to enable cache I hope

--- scripts/check_formatting.sh
+++ scripts/check_formatting.sh
@@ -7,7 +7,7 @@ source $DIR/_common.sh
 
 docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
 # docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=formatting --build-arg APP=${APP} . 
-docker build -f ${DIR}/Dockerfile --target=formatting --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg APP=${APP} . 
+docker build -f ${DIR}/Dockerfile --target=formatting --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg BUILDKIT_INLINE_CACHE=1  --build-arg APP=${APP} . 
 # $PUBLISH || echo "Not publishing ${DEPS_TAG}"
 # $PUBLISH && docker push ${DOCKER_REGISTRY}${APP}:${DEPS_TAG}
 docker rmi ${DOCKER_REGISTRY}${APP}:${DEPS_TAG}
--- scripts/prepare_deps.sh
+++ scripts/prepare_deps.sh
@@ -10,7 +10,7 @@ source $DIR/_common.sh
 # docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache --cache-to=type=local,dest=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=deps --build-arg APP=${APP} -t ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} . 
 
 docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
-docker build -f ${DIR}/Dockerfile --target=deps --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg APP=${APP} -t ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} . 
+docker build -f ${DIR}/Dockerfile --target=deps --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg APP=${APP} -t ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} . 
 
 $PUBLISH || echo "Not publishing ${DEPS_TAG}"
 $PUBLISH && docker push ${DOCKER_REGISTRY}${APP}:${DEPS_TAG}
