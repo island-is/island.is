3e832ea9fe9189daf0e7cbc4cba6c47b1939fb84
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-05-27 13:05:05 +0000

    Fix typescript error causing test to fail

--- libs/island-ui/core/src/lib/Column/Column.tsx
+++ libs/island-ui/core/src/lib/Column/Column.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { ReactNode, useContext } from 'react'
 import { Box } from '../Box/Box'
 import { ColumnsContext } from '../Columns/Columns'
