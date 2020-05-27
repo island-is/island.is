8f42fd9404c4582ef226f634408fd101e30aad9d
Petar Shomov <petar@andes.is>
2020-05-27 14:45:27 +0000

    fixing the scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/_common.sh
 
 
-docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=linter --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg APP=${APP} . 
+docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=linter --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg APP=${APP} $PROJECT_ROOT
 # docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
 # docker build -f ${DIR}/Dockerfile --target=linter --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg BUILDKIT_INLINE_CACHE=1  --build-arg APP=${APP} . 
 
