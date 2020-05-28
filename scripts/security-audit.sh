acaf9adca9e123d9fbabd3014712e98d00f61590
Petar Shomov <petar@andes.is>
2020-05-28 20:27:30 +0000

    adding testing and security audit

---
+++ scripts/security-audit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/_common.sh
+
+
+exec docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=security-audit --build-arg BUILDKIT_INLINE_CACHE=1 $PROJECT_ROOT
+# docker pull ${DOCKER_REGISTRY}${APP}:${DEPS_TAG} || true
+# docker build -f ${DIR}/Dockerfile --target=linter --cache-from=${DOCKER_REGISTRY}${APP}:${DEPS_TAG} --build-arg BUILDKIT_INLINE_CACHE=1  --build-arg APP=${APP} . 
+
+# docker rmi ${DOCKER_REGISTRY}${APP}:${DEPS_TAG}
+# docker system prune -a -f
---
+++ scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/_common.sh
+
+
+RUNNER=test-runner
+docker buildx build --platform=linux/amd64 --cache-from=type=local,src=$PROJECT_ROOT/cache -f ${DIR}/Dockerfile --target=test --load --build-arg BUILDKIT_INLINE_CACHE=1 -t ${DOCKER_REGISTRY}${RUNNER}:${DOCKER_TAG} $PROJECT_ROOT
+exec docker run --rm --net=host -e APP=$APP ${DOCKER_REGISTRY}${RUNNER}:${DOCKER_TAG}
