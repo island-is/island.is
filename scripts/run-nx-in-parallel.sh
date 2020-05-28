5031af3fbd8a848c4281b160361a6439c1a424e7
Petar Shomov <petar@andes.is>
2020-05-28 16:06:51 +0000

    not quite sure why this is failing on CI machine and not on mine

--- scripts/run-nx-in-parallel.sh
+++ scripts/run-nx-in-parallel.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/_common.sh
-MAX_PARALELLISM=2
+MAX_JOBS=${MAX_JOBS:-2}
 
 # This is a helper script to find affected projects for a specific target
-exec parallel -k --lb -j $MAX_PARALELLISM APP={} $DIR/$1.sh ::: `$DIR/nx-affected-targets.sh $1 | tr '\n,' ' '`
+exec parallel -k --lb -j $MAX_JOBS APP={} $DIR/$1.sh ::: `$DIR/nx-affected-targets.sh $1 | tr '\n,' ' '`
