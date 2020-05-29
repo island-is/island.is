f0cb9a6f18632139ca500731ea6921eeec05f4de
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-05-29 15:23:55 +0000

    Remove gql from user

--- apps/gjafakort/web/screens/User/User.tsx
+++ apps/gjafakort/web/screens/User/User.tsx
@@ -1,31 +1,7 @@
 import React from 'react'
-import gql from 'graphql-tag'
-import { useLazyQuery } from 'react-apollo'
-
-import { Application } from '../../graphql/schema'
-
-const ApplicationQuery = gql`
-  query ApplicationQuery($ssn: String!) {
-    application(ssn: $ssn) {
-      id
-    }
-  }
-`
 
 function User() {
-  const [getApplication, { data, loading }] = useLazyQuery(ApplicationQuery, {
-    variables: { ssn: '1' },
-  })
-
-  const application = (data || {}).application as Application
-  return (
-    <div>
-      User page {application && application.id}
-      <button onClick={() => getApplication()}>
-        Click me!
-      </button>
-    </div>
-  )
+  return <div>User page</div>
 }
 
 export default User
