7ccd704b0895625ea92802bfa4a6a7b737b23328
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-06-03 10:38:37 +0000

    Abstract redis cache for easy replace if needed

---
+++ libs/cache/src/lib/cache.ts
@@ -0,0 +1,33 @@
+import { RedisCache } from 'apollo-server-cache-redis'
+
+import { logger } from '@island.is/logging'
+
+type Options = {
+  host: string
+  port: number
+  password: string
+  name: string
+}
+
+export const createCache = (options: Options) =>
+  new RedisCache({
+    ...options,
+    connectTimeout: 5000,
+    socket_keepalive: false,
+    reconnectOnError: (err) => {
+      logger.error(`Reconnect on error: ${err}`)
+      var targetError = 'READONLY'
+      if (err.message.slice(0, targetError.length) === targetError) {
+        // Only reconnect when the error starts with "READONLY"
+        return true
+      }
+    },
+    retryStrategy: (times) => {
+      logger.info(`Redis Retry: ${times}`)
+      if (times >= 3) {
+        return undefined
+      }
+      var delay = Math.min(times * 50, 2000)
+      return delay
+    },
+  })
