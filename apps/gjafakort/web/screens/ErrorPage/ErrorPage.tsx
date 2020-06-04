5cb79107071c4b0e67a630a43dcc9890b03dca0b
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-06-04 14:42:28 +0000

    Custom error page for redirection

---
+++ apps/gjafakort/web/screens/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+
+import {
+  Box,
+  Button,
+  ContentBlock,
+  Typography,
+} from '@island.is/island-ui/core'
+
+function ErrorPage() {
+  return (
+    <Box marginTop={12}>
+      <ContentBlock width="large">
+        <Box marginBottom={3}>
+          <Typography variant="h1" as="h1">
+            Villa kom upp
+          </Typography>
+        </Box>
+        <Box marginBottom={9}>
+          <Typography variant="intro">
+            Eitthvað hefur farið úrskeiðis, vinsamlega reyndu aftur
+          </Typography>
+        </Box>
+        <Link href="/">
+          <span>
+            <Button>Aftur heim</Button>
+          </span>
+        </Link>
+      </ContentBlock>
+    </Box>
+  )
+}
+
+export default ErrorPage
