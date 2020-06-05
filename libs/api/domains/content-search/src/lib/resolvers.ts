2532ba49614964232307905f5523b541391d8c4b
Andreas Sundqvist <andreas@stefna.is>
2020-06-05 12:37:33 +0200

    Hack to solve compile issue

--- libs/api/domains/content-search/src/lib/resolvers.ts
+++ libs/api/domains/content-search/src/lib/resolvers.ts
@@ -2,6 +2,7 @@ import { Resolvers } from '@island.is/api/schema'
 
 export const resolvers: Resolvers = {
   Query: {
+    // @ts-ignore
     content(_, { query }, context) {
       return context.searcher.find(query)
     },
