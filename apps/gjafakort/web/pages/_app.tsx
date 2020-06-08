56a6c0f4630926ac9125bd75b6c0f7ebae1fa5d9
Darri Steinn Konráðsson <darri@viceversa.is>
2020-06-08 12:50:31 +0000

    Move font to separate folder and let OFL.txt follow (#98)

--- apps/gjafakort/web/pages/_app.tsx
+++ apps/gjafakort/web/pages/_app.tsx
@@ -64,8 +64,10 @@ const Layout: React.FC = ({ children }) => {
           font-weight: 300;
           font-display: swap;
           src: local('IBM Plex Sans Light'), local('IBMPlexSans-Light'),
-            url('/fonts/ibm-plex-sans-v7-latin-300.woff2') format('woff2'),
-            url('/fonts/ibm-plex-sans-v7-latin-300.woff') format('woff');
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-300.woff2')
+              format('woff2'),
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-300.woff')
+              format('woff');
         }
         @font-face {
           font-family: 'IBM Plex Sans';
@@ -73,8 +75,10 @@ const Layout: React.FC = ({ children }) => {
           font-weight: 400;
           font-display: swap;
           src: local('IBM Plex Sans'), local('IBMPlexSans'),
-            url('/fonts/ibm-plex-sans-v7-latin-regular.woff2') format('woff2'),
-            url('/fonts/ibm-plex-sans-v7-latin-regular.woff') format('woff');
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-regular.woff2')
+              format('woff2'),
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-regular.woff')
+              format('woff');
         }
         @font-face {
           font-family: 'IBM Plex Sans';
@@ -82,8 +86,10 @@ const Layout: React.FC = ({ children }) => {
           font-weight: 400;
           font-display: swap;
           src: local('IBM Plex Sans Italic'), local('IBMPlexSans-Italic'),
-            url('/fonts/ibm-plex-sans-v7-latin-italic.woff2') format('woff2'),
-            url('/fonts/ibm-plex-sans-v7-latin-italic.woff') format('woff');
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-italic.woff2')
+              format('woff2'),
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-italic.woff')
+              format('woff');
         }
         @font-face {
           font-family: 'IBM Plex Sans';
@@ -91,8 +97,10 @@ const Layout: React.FC = ({ children }) => {
           font-weight: 500;
           font-display: swap;
           src: local('IBM Plex Sans Medium'), local('IBMPlexSans-Medium'),
-            url('/fonts/ibm-plex-sans-v7-latin-500.woff2') format('woff2'),
-            url('/fonts/ibm-plex-sans-v7-latin-500.woff') format('woff');
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-500.woff2')
+              format('woff2'),
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-500.woff')
+              format('woff');
         }
         @font-face {
           font-family: 'IBM Plex Sans';
@@ -100,8 +108,10 @@ const Layout: React.FC = ({ children }) => {
           font-weight: 600;
           font-display: swap;
           src: local('IBM Plex Sans SemiBold'), local('IBMPlexSans-SemiBold'),
-            url('/fonts/ibm-plex-sans-v7-latin-600.woff2') format('woff2'),
-            url('/fonts/ibm-plex-sans-v7-latin-600.woff') format('woff');
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-600.woff2')
+              format('woff2'),
+            url('/fonts/ibm-plex/ibm-plex-sans-v7-latin-600.woff')
+              format('woff');
         }
       `}</style>
     </Page>
