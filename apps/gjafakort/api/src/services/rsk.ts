e539d213d183915e644e89faf9d7f8ea9bab728a
Davíð Guðni Halldórsson <davidgudni@gmail.com>
2020-06-08 15:57:26 +0000

    Not exposing error from rsk to user

--- apps/gjafakort/api/src/services/rsk.ts
+++ apps/gjafakort/api/src/services/rsk.ts
@@ -1,4 +1,7 @@
 import { Base64 } from 'js-base64'
+
+import { logger } from '@island.is/logging'
+
 import { environment } from '../environments'
 
 const { rsk } = environment
@@ -15,27 +18,34 @@ interface CompanyRegistryMember {
 export const getCompanyRegistryMembers = async (
   userSSN: string,
 ): Promise<CompanyRegistryMember[]> => {
-  const res = await fetch(
-    `${rsk.url}/companyregistry/members/${userSSN}/companies`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Basic ${Base64.encode(
-          `${rsk.username}:${rsk.password}`,
-        )}`,
-        'Content-Type': 'application/json',
+  try {
+    const res = await fetch(
+      `${rsk.url}/companyregistry/members/${userSSN}/companies`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Basic ${Base64.encode(
+            `${rsk.username}:${rsk.password}`,
+          )}`,
+          'Content-Type': 'application/json',
+        },
       },
-    },
-  )
-  if (!res.ok) {
-    const text = await res.text()
-    throw new Error(text)
+    )
+    if (!res.ok) {
+      const text = await res.text()
+      throw new Error(text)
+    }
+    const { MemberCompanies } = await res.json()
+    if (!MemberCompanies) {
+      return []
+    }
+    return MemberCompanies
+  } catch (err) {
+    logger.error(err)
+    throw new Error(
+      'Error occurred while requesting members from company registry',
+    )
   }
-  const { MemberCompanies } = await res.json()
-  if (!MemberCompanies) {
-    return []
-  }
-  return MemberCompanies
 }
 
 export const getCompanyBySSN = async (
