26567addf033b0c3ac57ee1550f4ed37923c21f7
Andreas Sundqvist <andreas@stefna.is>
2020-06-10 10:48:04 +0200

    Remove generic indexing endpoint

--- apps/services/search-indexer/src/app/indexing.controller.ts
+++ apps/services/search-indexer/src/app/indexing.controller.ts
@@ -1,20 +1,12 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common'
+import {Controller, Get, Param} from '@nestjs/common'
 
-import { IndexingService } from './indexing.service'
-import { Document, SearchIndexes } from '@island.is/api/content-search'
+import {IndexingService} from './indexing.service'
+import {SearchIndexes} from '@island.is/api/content-search'
 
 @Controller('index')
 export class IndexingController {
   constructor(private readonly appService: IndexingService) {}
 
-  @Post(':type')
-  async index(@Param('type') type, @Body() document: Document) {
-    const indexType: SearchIndexes = SearchIndexes[type]
-    await this.appService.indexDocument(indexType, document)
-    return {
-      status: 'OK',
-    }
-  }
   @Get('sync')
   async sync() {
     const syncToken = await this.appService.getLastSyncToken(SearchIndexes.test)
