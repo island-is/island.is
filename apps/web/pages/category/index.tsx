f78c72a717de01a1b0d34e65e6920b09e7e1c5df
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-06-10 18:34:43 +0000

    Remove dummy page

--- apps/web/pages/category/index.tsx
+++ apps/web/pages/category/index.tsx
@@ -2,4 +2,4 @@ import { withLocale } from '../../i18n'
 import categoryScreen from '../../screens/Category/Category'
 // import { Screen } from '../../types'
 
-export default categoryScreen
+export default withLocale('is')(categoryScreen)
