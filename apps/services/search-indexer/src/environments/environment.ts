cf322faa71b2573f2ad54be99b99d8a71c234956
Andreas Sundqvist <andreas@stefna.is>
2020-06-11 18:41:55 +0200

    Use env variable to configure contentful

--- apps/services/search-indexer/src/environments/environment.ts
+++ apps/services/search-indexer/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   contentful: {
     space: '8k0h54kbe6bj',
-    accessToken: 'HH-fqdlwYMMkKtlcMcpJj5Jw4sGae5g3YXKq59hBcdQ',
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     environment: 'sandbox',
     host: 'preview.contentful.com',
   },
