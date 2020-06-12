888c15624c83a3d37aadc7cf2d4b86b0a3b17350
Andreas Sundqvist <andreas@stefna.is>
2020-06-12 14:08:07 +0200

    Make the controller test work in ci

--- apps/services/search-indexer/src/environments/environment.ts
+++ apps/services/search-indexer/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   contentful: {
     space: '8k0h54kbe6bj',
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || 'test',
     environment: 'sandbox',
     host: 'preview.contentful.com',
   },
