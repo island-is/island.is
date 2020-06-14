6a47c9f0d2e93eb7abf159256d4a9a27af22f060
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-06-14 13:41:46 +0000

    Hook up Sentry to web

--- apps/gjafakort/web/pages/_error.tsx
+++ apps/gjafakort/web/pages/_error.tsx
@@ -1,5 +1,66 @@
+import React from 'react'
+import * as Sentry from '@sentry/node'
+import { NextPageContext } from 'next'
+import Error, { ErrorProps as NextErrorProps } from 'next/error'
+
 import { withLocale } from '../i18n'
 import { NotFound } from '../screens'
 import { Screen } from '../types'
 
-export default withLocale('is')(NotFound as Screen)
+type PropTypes = {
+  err: Error
+  statusCode: number
+  isSSRReadyToRender: boolean
+  children?: React.ReactElement
+}
+
+type ErrorProps = {
+  isSSRReadyToRender: boolean
+} & NextErrorProps
+
+const NextError = (props: PropTypes): JSX.Element => {
+  const { statusCode, isSSRReadyToRender, err, children = null } = props
+
+  if (!isSSRReadyToRender && err) {
+    Sentry.captureException(err)
+  }
+
+  return <NotFound />
+}
+
+NextError.getInitialProps = async (
+  props: NextPageContext,
+): Promise<ErrorProps> => {
+  const { res, err, asPath } = props
+  // @ts-ignore
+  const errorInitialProps: ErrorProps = await Error.getInitialProps({
+    res,
+    err,
+  })
+  errorInitialProps.isSSRReadyToRender = true
+
+  if (res) {
+    if (res.statusCode === 404) {
+      return { statusCode: 404, isSSRReadyToRender: true }
+    }
+
+    if (err) {
+      Sentry.captureException(err)
+      return errorInitialProps
+    }
+  } else {
+    if (err) {
+      Sentry.captureException(err)
+      return errorInitialProps
+    }
+  }
+
+  Sentry.captureException(
+    // @ts-ignore
+    new Error(`_error.js getInitialProps missing data at path: ${asPath}`),
+  )
+
+  return errorInitialProps
+}
+
+export default withLocale('is')(NextError)
