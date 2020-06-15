a5b66867e20386c24eda72bce350838396fe0032
Davíð Guðni Halldórsson <david@viceversa.is>
2020-06-15 15:53:58 +0000

    Send application id to yay so that gjafakort will be unique (#140)

--- apps/gjafakort/queue-listener/src/consumers/index.ts
+++ apps/gjafakort/queue-listener/src/consumers/index.ts
@@ -9,7 +9,6 @@ const { production } = environment
 export const startConsumers = async () => {
   const channel = MsgQueue.connect(production)
 
-  // TODO yay user application queue needs only a single try
   return Promise.all(
     handlers.map(async (handler) => {
       const exchangeId = await channel.declareExchange({
