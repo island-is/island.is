03c1cbbe44fb4f2ad8033b9d21086b8fe390d4ea
Petar Shomov <petar@andes.is>
2020-06-19 14:54:30 +0000

    formatting

--- .github/actions/detection.ts
+++ .github/actions/detection.ts
@@ -1,53 +1,53 @@
-import { ActionsListWorkflowRunsForRepoResponseData } from "@octokit/types";
+import { ActionsListWorkflowRunsForRepoResponseData } from '@octokit/types'
 
 const getSuccessWorkflowsForBranch = (
-  response: ActionsListWorkflowRunsForRepoResponseData
+  response: ActionsListWorkflowRunsForRepoResponseData,
 ) => {
   return response.workflow_runs
     .map((wr) => ({ run_number: wr.run_number, sha: wr.head_sha }))
-    .sort((a, b) => b.run_number - a.run_number);
-};
+    .sort((a, b) => b.run_number - a.run_number)
+}
 
 const pickFirstMatchingSuccess = (
   shas: string[],
-  runsBranch: { run_number: number; sha: string }[]
+  runsBranch: { run_number: number; sha: string }[],
 ) => {
   for (const sha of shas) {
-    const lastGoodRun = runsBranch.filter((runs) => runs.sha === sha);
+    const lastGoodRun = runsBranch.filter((runs) => runs.sha === sha)
     if (lastGoodRun.length > 0) {
-      return lastGoodRun[0];
+      return lastGoodRun[0]
     }
   }
-};
+}
 
 export interface WorkflowQueries {
-  getData(branch: string): Promise<ActionsListWorkflowRunsForRepoResponseData>;
+  getData(branch: string): Promise<ActionsListWorkflowRunsForRepoResponseData>
 }
 
 export const findLastGoodBuild = async (
   shas: string[],
   branch,
   base,
-  workflowQueries: WorkflowQueries
+  workflowQueries: WorkflowQueries,
 ) => {
   // First we try to find the last successful workflow build on our branch
-  const successWorkflows = await workflowQueries.getData(branch);
-  let successOnBranch = getSuccessWorkflowsForBranch(successWorkflows);
+  const successWorkflows = await workflowQueries.getData(branch)
+  let successOnBranch = getSuccessWorkflowsForBranch(successWorkflows)
 
-  const matchOnBranch = pickFirstMatchingSuccess(shas, successOnBranch);
+  const matchOnBranch = pickFirstMatchingSuccess(shas, successOnBranch)
   if (matchOnBranch) {
-    return matchOnBranch;
+    return matchOnBranch
   }
 
   // Fallback we try to find the last successful workflow build on the base branch
-  const successWorkflowsBase = await workflowQueries.getData(base);
-  let successOnBase = getSuccessWorkflowsForBranch(successWorkflowsBase);
+  const successWorkflowsBase = await workflowQueries.getData(base)
+  let successOnBase = getSuccessWorkflowsForBranch(successWorkflowsBase)
 
-  const matchOnBase = pickFirstMatchingSuccess(shas, successOnBase);
+  const matchOnBase = pickFirstMatchingSuccess(shas, successOnBase)
   if (matchOnBase) {
-    return matchOnBase;
+    return matchOnBase
   }
 
   // Unlikely but still
-  return {};
-};
+  return {}
+}
