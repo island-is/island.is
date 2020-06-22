7362c041aeb3033e766b52a177197756a3f49a1e
Brian Johannessen <barabrian89@gmail.com>
2020-06-22 10:51:57 +0000

    radio buttons

---
+++ libs/island-ui/core/src/lib/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { RadioButton } from './RadioButton'
+import { Box } from '../Box'
+import { ContentBlock } from '../ContentBlock'
+import { Stack } from '../Stack/Stack'
+
+export default {
+  title: 'Core/RadioButton',
+  component: RadioButton,
+}
+
+export const Default = () => {
+  const [radioButton, setRadioButton] = useState('')
+  return (
+    <ContentBlock>
+      <Box padding={[1, 2, 3]}>
+        <Stack space={4}>
+          <RadioButton
+            name="RadioButton1"
+            id="1"
+            label="RadioButton label"
+            value="1"
+            onChange={({ target }) => {
+              setRadioButton(target.value)
+            }}
+            checked={radioButton === '1'}
+          />
+          <RadioButton
+            name="RadioButton1"
+            id="2"
+            label="RadioButton label"
+            value="2"
+            onChange={({ target }) => {
+              setRadioButton(target.value)
+            }}
+            checked={radioButton === '2'}
+          />
+        </Stack>
+      </Box>
+    </ContentBlock>
+  )
+}
+
+export const Checked = () => {
+  return (
+    <ContentBlock>
+      <Box padding={[1, 2, 3]}>
+        <RadioButton
+          name="RadioButton2"
+          label="This one is checked"
+          onChange={() => {
+            console.log('nope')
+          }}
+          checked
+        />
+      </Box>
+    </ContentBlock>
+  )
+}
