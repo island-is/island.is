4866e72ffba3eedca0a09b78881957dd57623bb0
Brian Johannessen <barabrian89@gmail.com>
2020-06-24 09:16:04 +0000

    add link to user page

--- apps/gjafakort/web/screens/PrivacyPolicy/PrivacyPolicy.tsx
+++ apps/gjafakort/web/screens/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import HtmlParser from 'react-html-parser'
 
-import { Stack, Typography, Box } from '@island.is/island-ui/core'
+import { Stack, Typography, Box, Button } from '@island.is/island-ui/core'
 import { useI18n } from '@island.is/gjafakort-web/i18n'
 import { Layout, AppsSidebar } from '@island.is/gjafakort-web/components'
+import Link from 'next/link'
 
 function User() {
   const {
-    t: { privacyPolicy: t },
+    t: { routes, ...t },
   } = useI18n()
   return (
     <Layout
       left={
         <Box marginBottom={5}>
           <Stack space={4}>
-            <Typography variant="h1">{t.title}</Typography>
-            {t.sections.map((section, index) => (
+            <Typography variant="h1">{t.privacyPolicy.title}</Typography>
+            {t.privacyPolicy.sections.map((section, index) => (
               <Typography variant="p" links key={index}>
                 {HtmlParser(section)}
               </Typography>
@@ -23,7 +24,19 @@ function User() {
           </Stack>
         </Box>
       }
-      right={<AppsSidebar />}
+      right={
+        <Stack space={3}>
+          <Box background="purple100" padding={4} borderRadius="standard">
+            <Box marginBottom={2}>
+              <Typography variant="h4">{t.home.cta.users.label}</Typography>
+            </Box>
+            <Link href={routes.users.home}>
+              <Button width="fluid">{t.home.cta.users.content}</Button>
+            </Link>
+          </Box>
+          <AppsSidebar />
+        </Stack>
+      }
     />
   )
 }
