8b8dcefe29deed8d1a7c3fd484431df7551b4b11
Davíð Guðni Halldórsson <davidgudni@gmail.com>
2020-06-24 12:11:18 +0000

    Moving ferdalag api key

--- apps/gjafakort/api/src/extensions/sentry.ts
+++ apps/gjafakort/api/src/extensions/sentry.ts
@@ -6,26 +6,11 @@ import { environment } from '../environments'
 
 const {
   sentry: { dsn },
-  ferdalag,
 } = environment
 
 Sentry.init({
   dsn,
   environment: 'api',
-  beforeBreadcrumb: (breadCrumb) => {
-    // Strip ferdalag apikey from breadcrumb
-    if (breadCrumb.data.url.startsWith(ferdalag.url)) {
-      const [strippedUrl] = breadCrumb.data.url.split('?')
-      return {
-        ...breadCrumb,
-        data: {
-          ...breadCrumb.data,
-          url: strippedUrl,
-        },
-      }
-    }
-    return breadCrumb
-  },
 })
 
 export const setupRequestHandler = (app: Application) => {
