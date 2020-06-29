d808988e5beba69cf5cfb5ace6f814e79fd13cd2
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-06-29 10:36:23 +0000

    Not using the requestsService

--- apps/gjafakort/web/graphql/httpLink.ts
+++ apps/gjafakort/web/graphql/httpLink.ts
@@ -1,8 +1,6 @@
 import { createHttpLink } from 'apollo-link-http'
 import fetch from 'isomorphic-unfetch'
 
-import { RequestsService } from '../services'
-
 const isBrowser: boolean = process.browser
 
 // Polyfill fetch() on the server (used by apollo-client)
@@ -14,10 +12,5 @@ if (!isBrowser) {
 export default createHttpLink({
   uri: '/api/graphql',
   credentials: 'include',
-  fetch: async (uri: string, options: object) => {
-    const key = RequestsService.start()
-    const response = await fetch(uri, options)
-    RequestsService.stop(key)
-    return response
-  },
+  fetch,
 })
