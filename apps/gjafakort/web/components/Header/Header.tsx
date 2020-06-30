b87b26a4d23cc7ba1b6266ae5ca87031c6c3f0cc
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-06-30 15:33:11 +0000

    Add locale switch button

--- apps/gjafakort/web/components/Header/Header.tsx
+++ apps/gjafakort/web/components/Header/Header.tsx
@@ -25,6 +25,8 @@ function Header() {
   const { setUser, isAuthenticated } = useContext(UserContext)
   const {
     t: { header: t, routes },
+    activeLocale,
+    locale,
   } = useI18n()
   const { data } = useQuery(UserQuery)
 
@@ -32,6 +34,25 @@ function Header() {
   useEffect(() => {
     setUser(user)
   }, [user, setUser])
+
+  const switchLanguage = () => {
+    // TODO: parse the routes to push to the correct one (tricky)
+    switch (activeLocale) {
+      case 'is':
+        return locale('en')
+      case 'en':
+        return locale('is')
+      default:
+        return
+    }
+  }
+
+  const language =
+    activeLocale === 'is'
+      ? 'English'
+      : activeLocale === 'en'
+      ? 'Íslenska'
+      : undefined
 
   return (
     <IslandUIHeader
@@ -42,6 +63,8 @@ function Header() {
       )}
       logoutText={t.logout}
       userLogo={user?.role === 'developer' ? '👑' : undefined}
+      language={language}
+      switchLanguage={switchLanguage}
       userName={user?.name ?? ''}
       authenticated={isAuthenticated}
       onLogout={() => {
