cd5f1c3b0e279055e10eada1634c0620bcda8ea0
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-07-01 14:42:37 +0000

    Minor fix

--- libs/island-ui/core/src/lib/Blockquote/Blockquote.treat.ts
+++ libs/island-ui/core/src/lib/Blockquote/Blockquote.treat.ts
@@ -1,5 +1,5 @@
 import { style } from 'treat'
-import { theme } from '../../theme'
+import { theme } from '@island.is/island-ui/theme'
 
 export const blockquote = style({
   boxShadow: `inset 2px 0 0 ${theme.color.purple400}`,
