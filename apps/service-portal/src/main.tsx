1021a017199422acf61686008b7e891fdc24e441
Olafur <obmagnusson@gmail.com>
2020-07-06 12:36:28 +0000

    Generated service-portal app

---
+++ apps/service-portal/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './app/App'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
