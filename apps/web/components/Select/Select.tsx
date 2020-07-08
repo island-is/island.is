f618f437cb87f4d4be5199b97b2620749a4b1f79
Magnus Skulason <maggisk@gmail.com>
2020-07-08 14:29:45 +0000

    remove unused import

--- apps/web/components/Select/Select.tsx
+++ apps/web/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { FC, forwardRef } from 'react'
+import React, { forwardRef } from 'react'
 import { Icon } from '@island.is/island-ui/core'
 import * as styles from './Select.treat'
 
