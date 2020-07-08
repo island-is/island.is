c345e557be2fadf4486fc6bb653ea8c03f89ef18
Darri Steinn Konráðsson <darri@viceversa.is>
2020-07-08 10:28:23 +0000

    Allow filtering the table (#282)

--- apps/gjafakort/web/screens/Admin/Summary/Summary.tsx
+++ apps/gjafakort/web/screens/Admin/Summary/Summary.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 import { useQuery } from 'react-apollo'
@@ -11,6 +11,10 @@ import {
   Typography,
   Icon,
   Button,
+  Select,
+  Column,
+  Columns,
+  Option,
 } from '@island.is/island-ui/core'
 
 import { ContentLoader } from '@island.is/gjafakort-web/components'
@@ -40,8 +44,30 @@ const CompanyApplicationsQuery = gql`
   }
 `
 
+const Options = {
+  operationsTrouble: 'operationsTrouble',
+  publicHelp: 'publicHelp',
+  operationsTroubleAndOrPublicHelp: 'operationsTroubleAndOrPublicHelp',
+}
+
+const filterOptions: Option[] = [
+  {
+    label: 'Í resktrarerfiðleikum',
+    value: Options.operationsTrouble,
+  },
+  {
+    label: 'Fengið opinberabera aðstoð',
+    value: Options.publicHelp,
+  },
+  {
+    label: 'Í rekstrarerfiðleikum og/eða fengu opinbera hjálp',
+    value: Options.operationsTroubleAndOrPublicHelp,
+  },
+]
+
 function Summary() {
   const { data, loading, error } = useQuery(CompanyApplicationsQuery)
+  const [filter, setFilter] = useState(null)
   const { companyApplications } = data || {}
   if (error || (loading && !data)) {
     return <ContentLoader />
@@ -58,6 +84,28 @@ function Summary() {
   const rejectedApplications = companyApplications.filter(
     (a) => a.state === ApplicationStates.REJECTED,
   )
+
+  const filterApplications = () => {
+    if (!filter) {
+      return companyApplications
+    } else if (filter.value === Options.operationsTrouble) {
+      return companyApplications.filter(
+        (application) => application.operationsTrouble,
+      )
+    } else if (filter.value === Options.publicHelp) {
+      return companyApplications.filter((application) =>
+        Boolean(application.publicHelpAmount),
+      )
+    } else if (filter.value === Options.operationsTroubleAndOrPublicHelp) {
+      return companyApplications.filter(
+        (application) =>
+          application.operationsTrouble ||
+          Boolean(application.publicHelpAmount),
+      )
+    } else {
+      return []
+    }
+  }
 
   return (
     <Box paddingX="gutter">
@@ -86,25 +134,56 @@ function Summary() {
             />
           </Box>
         </Box>
-        <Box marginBottom={4}>
-          <Box display="flex" alignItems="center" marginBottom={1}>
-            <Icon type="alert" width={20} color="yellow600" />
-            <Box marginLeft={2}>
-              <Typography variant="p">
-                merkir fyrirtæki sem voru í rekstrarerfiðleikum 31. desember
-                2019 í skilningi hópundanþágureglugerðar
-              </Typography>
-            </Box>
-          </Box>
-          <Box display="flex" alignItems="center">
-            <Icon type="info" width={20} color="blue600" />
-            <Box marginLeft={2}>
-              <Typography variant="p">
-                merkir fyrirtæki sem hafa á síðastliðnum tveimur reikningsárum
-                eða á yfirstandandi reikningsári fengið opinbera aðstoð
-              </Typography>
-            </Box>
-          </Box>
+        <Box marginBottom={6}>
+          <Columns space="gutter" collapseBelow="lg">
+            <Column width="2/3">
+              <Box>
+                <Box display="flex" alignItems="center" marginBottom={1}>
+                  <Icon type="alert" width={20} color="yellow600" />
+                  <Box marginLeft={2}>
+                    <Typography variant="p">
+                      merkir fyrirtæki sem voru í rekstrarerfiðleikum 31.
+                      desember 2019 í skilningi hópundanþágureglugerðar
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <Icon type="info" width={20} color="blue600" />
+                  <Box marginLeft={2}>
+                    <Typography variant="p">
+                      merkir fyrirtæki sem hafa á síðastliðnum tveimur
+                      reikningsárum eða á yfirstandandi reikningsári fengið
+                      opinbera aðstoð
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Column>
+            <Column width="1/3">
+              <Box position="relative">
+                <Select
+                  name="filter"
+                  label="Sía eftir"
+                  placeholder="Veldu síu"
+                  value={filter}
+                  onChange={(option) => {
+                    setFilter(option)
+                  }}
+                  options={filterOptions}
+                />
+                <Box position="absolute" right={0}>
+                  <Button
+                    variant="text"
+                    onClick={() => {
+                      setFilter(null)
+                    }}
+                  >
+                    endursetja síu
+                  </Button>
+                </Box>
+              </Box>
+            </Column>
+          </Columns>
         </Box>
         <Box marginBottom={6} overflow="scroll">
           <table className={styles.table}>
@@ -121,7 +200,7 @@ function Summary() {
               </tr>
             </thead>
             <tbody>
-              {companyApplications.map((application, index) => (
+              {filterApplications().map((application, index) => (
                 <tr className={styles.tr} key={index}>
                   <td>
                     <Box display="flex" flexDirection="column">
