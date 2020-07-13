e975276f8b589eedae184017a8cc213546718255
Birgir Haraldsson <biggi@stefna.is>
2020-07-13 15:15:22 +0200

    Switch to master in contentful

--- apps/services/search-indexer/src/environments/environment.ts
+++ apps/services/search-indexer/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   contentful: {
     space: '8k0h54kbe6bj',
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || 'test',
-    environment: 'sandbox',
+    environment: 'master',
     host: 'preview.contentful.com',
   },
 }
