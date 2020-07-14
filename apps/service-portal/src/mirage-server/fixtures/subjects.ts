c4780d47efbc2e234715e8d174337595efad5838
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-07-14 09:20:28 +0000

    schemas and some scopes added, not final.

--- apps/service-portal/src/mirage-server/fixtures/subjects.ts
+++ apps/service-portal/src/mirage-server/fixtures/subjects.ts
@@ -1,38 +1,43 @@
-import { Actor } from '../models/actor';
-import { Subject } from '../models/subject';
+import { Subject } from '../models/subject'
 
 export default [
-	{
-		id: 1,
-		name: 'Ólafur Björn Magnússon',
-		nationalId: '2606862759',
-		scope: [
-			'@Island.is/finance',
-			'@Island.is/finance/personal.edit',
-			'@Island.is/finance/vaccines.view'
-		],
-		subjectType: 'person'
-	},
-	{
-		id: 2,
-		name: 'Sendiráðið',
-		nationalId: '5401482231',
-		scope: [
-			'@Island.is/finance',
-			'@Island.is/finance/accounts',
-			'@Island.is/finance/vaccines.view'
-		],
-		subjectType: 'company'
-	},
-	{
-		id: 3,
-		name: 'Þjóðskrá',
-		nationalId: '7401482231',
-		scope: [
-			'@Island.is/finance',
-			'@Island.is/finance/perscriptions.edit',
-			'@Island.is/finance/vaccines.view'
-		],
-		subjectType: 'instituion'
-	}
+  {
+    id: 1,
+    name: 'Ólafur Björn Magnússon',
+    nationalId: '2606862759',
+    scope: [
+      '@Island.is/finance',
+      '@Island.is/finance/personal.edit',
+      '@Island.is/finance/vaccines.view',
+      '@Fjarmalastofa/finance/rsk.view',
+      '@Fjarmalastofa/procure/companystatus.view',
+      '@Sendiradid/staff/health.view',
+    ],
+    subjectType: 'person',
+  },
+  {
+    id: 2,
+    name: 'Sendiráðið',
+    nationalId: '5401482231',
+    scope: [
+      '@Island.is/finance',
+      '@Island.is/finance/accounts',
+      '@Island.is/finance/vaccines.view',
+      '@Fjarmalastofa/finance/rsk.edit',
+      '@Fjarmalastofa/procure/companystatus.edit',
+      '@Sendiradid/staff/health.edit',
+    ],
+    subjectType: 'company',
+  },
+  {
+    id: 3,
+    name: 'Þjóðskrá',
+    nationalId: '7401482231',
+    scope: [
+      '@Island.is/finance',
+      '@Island.is/finance/perscriptions.edit',
+      '@Island.is/finance/vaccines.view',
+    ],
+    subjectType: 'instituion',
+  },
 ] as Subject[]
