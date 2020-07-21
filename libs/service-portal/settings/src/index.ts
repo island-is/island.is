948b5252f03e26f4804bb8a98f8d36168d0dc42f
Atli Guðlaugsson <atli@sendiradid.is>
2020-07-21 10:20:17 +0000

    add settings module library to service-portal

---
+++ libs/service-portal/settings/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/service-portal-settings'
---
+++ libs/service-portal/settings/src/lib/service-portal-settings.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './service-portal-settings.scss'
+
+/* eslint-disable-next-line */
+export interface ServicePortalSettingsProps {}
+
+export const ServicePortalSettings = (props: ServicePortalSettingsProps) => {
+  return (
+    <div>
+      <h1>Welcome to service-portal-settings component!</h1>
+    </div>
+  )
+}
+
+export default ServicePortalSettings
