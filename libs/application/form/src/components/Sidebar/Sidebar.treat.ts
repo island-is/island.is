304becdf07f7b9d76dde652e4a4ea43a8245e05c
Noam <noam@aranja.com>
2020-08-07 20:20:07 +0000

    REmvoe commented out code

--- libs/application/form/src/components/Sidebar/Sidebar.treat.ts
+++ libs/application/form/src/components/Sidebar/Sidebar.treat.ts
@@ -2,7 +2,6 @@ import { style } from 'treat'
 import { theme } from '@island.is/island-ui/theme'
 
 export const root = style({
-  // borderTopWidth: 1,
   borderRightWidth: 1,
   borderStyle: theme.border.style.solid,
   borderColor: theme.color.blue200,
