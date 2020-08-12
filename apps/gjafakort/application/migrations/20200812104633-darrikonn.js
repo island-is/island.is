2183251c2ddc8981a281bd268d27df3b3723a712
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-08-12 10:55:56 +0000

    Set new number for user

---
+++ apps/gjafakort/application/migrations/20200812104633-darrikonn.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(`
+      BEGIN;
+
+        UPDATE application SET data = jsonb_set(data, '{mobileNumber}', '6952527')
+        WHERE type = 'gjafakort-user' AND issuer_ssn = '3110833849';
+
+      COMMIT;
+    `)
+  },
+
+  down: (queryInterface, Sequelize) => {},
+}
