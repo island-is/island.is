265aad5646c90c321648ec79576d40364ff7d2d4
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-08-14 16:06:56 +0000

    Show/hide fixed nav

--- apps/web/components/FixedNav/FixedNav.treat.ts
+++ apps/web/components/FixedNav/FixedNav.treat.ts
@@ -2,5 +2,18 @@ import { style } from 'treat'
 import { theme } from '@island.is/island-ui/theme'
 
 export const container = style({
+  position: 'fixed',
+  left: 0,
+  right: 0,
+  top: 0,
+  zIndex: 1000,
   backgroundColor: theme.color.blue400,
+  transform: `translateY(-100%)`,
+  opacity: 0,
+  transition: 'opacity 150ms ease, transform 150ms ease',
+})
+
+export const show = style({
+  transform: `translateY(0%)`,
+  opacity: 1,
 })
