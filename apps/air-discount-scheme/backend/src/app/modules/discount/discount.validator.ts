31dc82465e1e0927520bfccd9b80005ec275ed5e
Davíð Guðni Halldórsson <david@viceversa.is>
2020-08-17 09:18:20 +0000

    Adding users to discount path (#377)

--- apps/air-discount-scheme/backend/src/app/modules/discount/discount.validator.ts
+++ apps/air-discount-scheme/backend/src/app/modules/discount/discount.validator.ts
@@ -3,14 +3,12 @@ import { ApiProperty } from '@nestjs/swagger'
 
 export class GetDiscountByCodeParams {
   @IsString()
-  @Length(8, 8)
-  @ApiProperty()
-  readonly discountCode: string
-}
-
-export class GetDiscountByCodeQuery {
-  @IsString()
   @Length(10, 10)
   @ApiProperty()
   readonly nationalId: string
+
+  @IsString()
+  @Length(8, 8)
+  @ApiProperty()
+  readonly discountCode: string
 }
--- apps/air-discount-scheme/backend/src/app/modules/discount/e2e/discount.spec.ts
+++ apps/air-discount-scheme/backend/src/app/modules/discount/e2e/discount.spec.ts
@@ -9,29 +9,29 @@ beforeAll(async () => {
 })
 
 describe('Public Discount', () => {
-  it(`GET /public/discount/:discountCode?nationalId should return data`, async () => {
+  it(`GET /public/users/:nationalId/discounts/:discountCode should return data`, async () => {
     const response = await request(app.getHttpServer())
-      .get('/public/discount/12345678?nationalId=2101932009')
+      .get('/public/users/2101932009/discounts/12345678')
       .expect(200)
 
     expect(response.body.discountCode).toBeTruthy()
   })
 
-  it('GET /public/discount/:discountCode with wrong format for discountCode should return bad request', async () => {
+  it('GET /public/users/:nationalId/discounts/:discountCode with wrong format for discountCode should return bad request', async () => {
     await request(app.getHttpServer())
-      .get('/public/discount/1234567?nationalId=1234567890')
+      .get('/public/users/1234567890/discounts/1234567')
       .expect(400)
   })
 
-  it('GET /public/discount/:discountCode without query should return bad request', async () => {
+  it('GET /public/users/:nationalId/discounts/:discountCode without query should return bad request', async () => {
     await request(app.getHttpServer())
-      .get('/public/discount/12345678?nationalId=123456789')
+      .get('/public/users/123456789/discounts/12345678')
       .expect(400)
   })
 
-  it('GET /public/discount/:discountCode with wrong format on nationalId should return bad request', async () => {
-    const response = await request(app.getHttpServer())
-      .get('/public/discount/12345678?nationalId=123456789')
+  it('GET /public/users/:nationalId/discount/:discountCode with wrong format on nationalId should return bad request', async () => {
+    await request(app.getHttpServer())
+      .get('/public/users/123456789/discounts/12345678')
       .expect(400)
   })
 })
