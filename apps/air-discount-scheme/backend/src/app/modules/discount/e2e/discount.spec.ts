ade75ac0f48d0c58e0fb9ff315429be8cf1ff905
Davíð Guðni Halldórsson <davidgudni@gmail.com>
2020-08-19 15:29:38 +0000

    Delete flight should mark flight as invalid

--- apps/air-discount-scheme/backend/src/app/modules/discount/e2e/discount.spec.ts
+++ apps/air-discount-scheme/backend/src/app/modules/discount/e2e/discount.spec.ts
@@ -16,21 +16,19 @@ beforeAll(async () => {
 describe('Get Discount By DiscountCode', () => {
   it(`GET /public/users/:nationalId/discounts/:discountCode should return data`, async () => {
     const response = await request(app.getHttpServer())
-      .get('/public/users/2101932009/discounts/12345678')
+      .get('/public/users/1326487905/discounts/12345678')
       .expect(200)
 
-    expect(response.body.discountCode).toBeTruthy()
+    expect(response.body).toEqual({
+      discountCode: '12345678',
+      flightLegsLeft: 4,
+      nationalId: '1326487905',
+    })
   })
 
   it('GET /public/users/:nationalId/discounts/:discountCode with wrong format for discountCode should return bad request', async () => {
     await request(app.getHttpServer())
-      .get('/public/users/1234567890/discounts/1234567')
-      .expect(400)
-  })
-
-  it('GET /public/users/:nationalId/discounts/:discountCode without query should return bad request', async () => {
-    await request(app.getHttpServer())
-      .get('/public/users/123456789/discounts/12345678')
+      .get('/public/users/1326487905/discounts/1234567')
       .expect(400)
   })
 
@@ -45,7 +43,7 @@ describe('Get Discount By DiscountCode', () => {
       .spyOn(flightService, 'countFlightLegsByNationalId')
       .mockImplementation(() => Promise.resolve(4))
     await request(app.getHttpServer())
-      .get('/public/users/1234567890/discounts/12345678')
+      .get('/public/users/1326487905/discounts/12345678')
       .expect(403)
     spy.mockRestore()
   })
@@ -54,10 +52,14 @@ describe('Get Discount By DiscountCode', () => {
 describe('Create DiscountCode', () => {
   it(`POST /private/users/:nationalId/discounts should return data`, async () => {
     const response = await request(app.getHttpServer())
-      .post('/private/users/2101932009/discounts')
+      .post('/private/users/1326487905/discounts')
       .expect(201)
 
-    expect(response.body.discountCode).toBeTruthy()
+    expect(response.body).toEqual({
+      discountCode: expect.any(String),
+      flightLegsLeft: 4,
+      nationalId: '1326487905',
+    })
   })
 
   it(`POST /private/users/:nationalId/discounts with no flightlegs left should return forbidden`, async () => {
@@ -65,7 +67,7 @@ describe('Create DiscountCode', () => {
       .spyOn(flightService, 'countFlightLegsByNationalId')
       .mockImplementation(() => Promise.resolve(4))
     await request(app.getHttpServer())
-      .post('/private/users/2101932009/discounts')
+      .post('/private/users/1326487905/discounts')
       .expect(403)
     spy.mockRestore()
   })
