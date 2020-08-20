d7817476857a0112df499c7383eb70c845336d30
Olafur <obmagnusson@gmail.com>
2020-08-20 15:24:37 +0000

    changed mock mirage for accounts

--- apps/service-portal/src/hooks/useSubjects/useSubjects.ts
+++ apps/service-portal/src/hooks/useSubjects/useSubjects.ts
@@ -5,14 +5,15 @@ import { API_USER_ACCOUNTS } from '@island.is/service-portal/constants'
 import { ActionType } from '../../store/actions'
 
 const useSubjects = () => {
-  const [{ subjectList, subjectListState }, dispatch] = useStore()
+  const [{ subjectList, subjectListState, userInfo }, dispatch] = useStore()
 
   useEffect(() => {
     async function fetchSubjectList() {
       dispatch({ type: ActionType.FetchSubjectListPending })
 
       try {
-        const res = await fetchWithAuth(API_USER_ACCOUNTS)
+        console.log(userInfo)
+        const res = await fetchWithAuth(`${API_USER_ACCOUNTS}/${userInfo.profile.natreg}`, userInfo)
         const data = await res.json()
         dispatch({
           type: ActionType.FetchSubjectListFulfilled,
