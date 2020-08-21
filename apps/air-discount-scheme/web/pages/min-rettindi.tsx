2d331175e5d555d66a7353b414bb0e4e8392f724
Brian Johannessen <barabrian89@gmail.com>
2020-08-21 20:13:14 +0000

    redirect to Subsidy

--- apps/air-discount-scheme/web/pages/min-rettindi.tsx
+++ apps/air-discount-scheme/web/pages/min-rettindi.tsx
@@ -1,6 +1,6 @@
 import { withLocale } from '../i18n'
-import { User } from '../screens'
+import { Subsidy } from '../screens'
 import { Screen } from '../types'
 import { withAuth } from '../auth'
 
-export default withLocale('is')(withAuth(User as Screen))
+export default withLocale('is')(withAuth(Subsidy as Screen))
