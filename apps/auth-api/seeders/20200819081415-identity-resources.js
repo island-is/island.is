929db3cb72db0fb372b5249004b58498adbe3412
Valur Einarsson <valure@kvikkerfi.is>
2020-08-22 08:57:45 +0100

    Added seed data for profile and email.

--- apps/auth-api/seeders/20200819081415-identity-resources.js
+++ apps/auth-api/seeders/20200819081415-identity-resources.js
@@ -8,6 +8,7 @@ module.exports = {
       name: 'openid',
       display_name: 'Your user identifier',
       required: true,
+      emphasize: false,
       enabled: true,
       show_in_discovery_document: true
     };
@@ -18,18 +19,61 @@ module.exports = {
       {identity_resource_id: openId.id, claim_name: 'natreg'}
     ];
 
-    var identities = queryInterface.bulkInsert('identity_resource', [openId], {});
+    var profile = {
+      id: 'fde089c6-142c-471e-a3f7-c4f76d0360d1',//uuidv4(),
+      name: 'profile',
+      display_name: 'User profile',
+      description: 'Your user profile information (first name, last name, etc.)',
+      required: false,
+      emphasize: true,
+      enabled: true,
+      show_in_discovery_document: true
+    };
 
-    var userClaims = queryInterface.bulkInsert('identity_resource_user_claim', openIdClaims, {});
+    var profileClaims = [
+      {identity_resource_id: profile.id, claim_name: 'name'},
+      {identity_resource_id: profile.id, claim_name: 'family_name'},
+      {identity_resource_id: profile.id, claim_name: 'given_name'},
+      {identity_resource_id: profile.id, claim_name: 'middle_name'},
+      {identity_resource_id: profile.id, claim_name: 'nickname'},
+      {identity_resource_id: profile.id, claim_name: 'preferred_username'},
+      {identity_resource_id: profile.id, claim_name: 'profile'},
+      {identity_resource_id: profile.id, claim_name: 'picture'},
+      {identity_resource_id: profile.id, claim_name: 'website'},
+      {identity_resource_id: profile.id, claim_name: 'gender'},
+      {identity_resource_id: profile.id, claim_name: 'birthdate'},
+      {identity_resource_id: profile.id, claim_name: 'zoneinfo'},
+      {identity_resource_id: profile.id, claim_name: 'locale'},
+      {identity_resource_id: profile.id, claim_name: 'updated_at'}
+    ];
 
-    return Promise.all([identities, userClaims])
+    var email = {
+      id: 'ac5300ca-bce5-49db-8d98-17f73c233b8c',//uuidv4(),
+      name: 'email',
+      display_name: 'Your email address',
+      required: false,
+      emphasize: true,
+      enabled: true,
+      show_in_discovery_document: true
+    };
+
+    var emailClaims = [
+      {identity_resource_id: email.id, claim_name: 'email'},
+      {identity_resource_id: email.id, claim_name: 'email_verified'},
+    ];
+
+    var identityResources = queryInterface.bulkInsert('identity_resource', [openId, profile, email], {});
+
+    var userClaims = queryInterface.bulkInsert('identity_resource_user_claim', openIdClaims.concat(profileClaims, emailClaims), {});
+
+    return Promise.all([identityResources, userClaims])
   },
 
   down: (queryInterface, Sequelize) => {
 
     var userClaims =  queryInterface.bulkDelete('identity_resource_user_claim', null, {});
-    var identities =  queryInterface.bulkDelete('identity_resource', null, {});
+    var identityResources =  queryInterface.bulkDelete('identity_resource', null, {});
 
-    return Promise.all([identities, userClaims])
+    return Promise.all([identityResources, userClaims])
   }
 };
