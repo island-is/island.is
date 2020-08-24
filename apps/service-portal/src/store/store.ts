a934e259c9a2e34eb9a63d738c753abc33a7a6e8
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-08-24 14:15:49 +0000

    addressing pr comments

--- apps/service-portal/src/store/store.ts
+++ apps/service-portal/src/store/store.ts
@@ -1,11 +1,10 @@
 import {
   ServicePortalModule,
   UserWithMeta,
-  Subject,
-  Actor,
 } from '@island.is/service-portal/core'
 import { SubjectListDto } from '../mirage-server/models/subject'
 import { modules } from './modules'
+import { mockSubjects, mockActors } from './mockData'
 import {
   Action,
   ActionType,
@@ -32,75 +31,6 @@ export const initialState: StoreState = {
   subjectListState: 'passive',
   notificationSidebarState: 'closed',
 }
-
-const mockSubjects: Subject[] = [
-  {
-    id: 1,
-    name: 'Ólafur Björn Magnússon',
-    nationalId: '2606862759',
-    scope: [
-      '@island.is/finance',
-      '@island.is/finance/personal.edit',
-      '@island.is/finance/vaccines.view',
-      '@fjarmalastofa/finance/rsk.view',
-      '@fjarmalastofa/procure/companystatus.view',
-      '@sendiradid/staff/health.view',
-    ],
-    subjectType: 'person',
-  },
-  {
-    id: 2,
-    name: 'Sendiráðið',
-    nationalId: '5401482231',
-    scope: [
-      '@island.is/finance',
-      '@island.is/finance/accounts',
-      '@island.is/finance/vaccines.view',
-      '@fjarmalastofa/finance/rsk.edit',
-      '@fjarmalastofa/procure/companystatus.edit',
-      '@sendiradid/staff/health.edit',
-    ],
-    subjectType: 'company',
-  },
-  {
-    id: 3,
-    name: 'Þjóðskrá',
-    nationalId: '7401482231',
-    scope: [
-      '@island.is/finance',
-      '@island.is/finance/perscriptions.edit',
-      '@island.is/finance/vaccines.view',
-    ],
-    subjectType: 'institution',
-  },
-]
-
-const mockActors: Actor[] = [
-  {
-    id: 1,
-    name: 'Ólafur Björn Magnússon',
-    nationalId: '2606862759',
-    subjectIds: [1, 2],
-  },
-  {
-    id: 2,
-    name: 'Baltasar Kormákur',
-    nationalId: '0123456789',
-    subjectIds: [2, 1],
-  },
-  {
-    id: 3,
-    name: 'Atli Guðlaugsson',
-    nationalId: '1507922319',
-    subjectIds: [1, 2],
-  },
-  {
-    id: 4,
-    name: 'Þorvarður Örn Einarsson',
-    nationalId: '3001902359',
-    subjectIds: [1, 2],
-  },
-]
 
 export const reducer = (state: StoreState, action: Action): StoreState => {
   switch (action.type) {
--- apps/service-portal/src/utils/http.ts
+++ apps/service-portal/src/utils/http.ts
@@ -1,25 +1,4 @@
-import { User } from 'oidc-client'
-
-interface Subject {
-  id: number
-  name: string
-  nationalId: string
-  scope: string[]
-  subjectType: 'person' | 'company' | 'institution'
-}
-
-interface Actor {
-  id: number
-  name: string
-  nationalId: string
-  subjectIds: number[]
-}
-
-interface UserWithMeta {
-  user: User
-  mockActors: Actor[]
-  mockSubjects: Subject[]
-}
+import { UserWithMeta } from '@island.is/service-portal/core'
 
 export const fetchWithAuth = (url: string, userInfo: UserWithMeta) =>
   fetch(url, {
