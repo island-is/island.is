f8dfd182a684f247f53cef7161634c5370a6e2be
Brian Johannessen <barabrian89@gmail.com>
2020-08-24 10:07:45 +0000

    adding subsidy code

--- apps/air-discount-scheme/web/screens/Subsidy/Subsidy.tsx
+++ apps/air-discount-scheme/web/screens/Subsidy/Subsidy.tsx
@@ -7,8 +7,7 @@ import {
   GenericPage,
   QueryGetGenericPageArgs,
 } from '@island.is/api/schema'
-import { Box, Breadcrumbs, Stack, Typography } from '@island.is/island-ui/core'
-import Link from 'next/link'
+import { Box, Stack, Typography, Icon, Button } from '@island.is/island-ui/core'
 import {
   Content,
   IntroText,
@@ -16,6 +15,41 @@ import {
 
 export interface SubsidyProps {
   page?: GenericPage
+}
+
+const codes: {
+  name: string
+  kid: boolean
+  code: string
+  remainingCodes: number
+  totalCodes: number
+}[] = [
+  {
+    name: 'Jón Jónsson',
+    kid: false,
+    code: 'ASFEWFA',
+    remainingCodes: 3,
+    totalCodes: 6,
+  },
+  {
+    name: 'Jón Jónsson',
+    kid: true,
+    code: 'ASssWFA',
+    remainingCodes: 6,
+    totalCodes: 6,
+  },
+]
+
+const copyToClipboard = (str) => {
+  const el = document.createElement('textarea')
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.opacity = '0'
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
 }
 
 const Subsidy: Screen<SubsidyProps> = ({
@@ -47,7 +81,78 @@ const Subsidy: Screen<SubsidyProps> = ({
               document={mainContent}
               wrapper={(children) => <Stack space={3}>{children}</Stack>}
             />
+            <Box
+              marginBottom={4}
+              background="blue100"
+              borderRadius="standard"
+              display="flex"
+              alignItems="center"
+              padding={3}
+            >
+              <Icon type="alert" color="blue400" />
+              <Box marginLeft={1} marginRight={2}>
+                <Typography variant="p">
+                  <strong>{attention}</strong>
+                </Typography>
+              </Box>
+              <Typography variant="p">{codeDisclaimer}</Typography>
+            </Box>
+            <Typography variant="h3">{myRights}</Typography>
+            {codes.map(({ name, kid, code, remainingCodes, totalCodes }) => {
+              const remainingPlaceholders = {
+                remaining: remainingCodes,
+                total: totalCodes,
+              }
+              return (
+                <Box
+                  key={code}
+                  padding={2}
+                  marginBottom={2}
+                  border="standard"
+                  borderRadius="standard"
+                  display={['block', 'flex']}
+                  justifyContent="spaceBetween"
+                  alignItems={['flexStart', 'center']}
+                  background="blue100"
+                  flexDirection={['column', 'row']}
+                >
+                  <Box marginBottom={[3, 0]}>
+                    <Typography variant="h3">
+                      {name} {kid && kidsRights}
+                    </Typography>
+                    <Typography variant="p">
+                      {remaining.replace(
+                        /\{{(.*?)\}}/g,
+                        (m, sub) => remainingPlaceholders[sub],
+                      )}
+                    </Typography>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent={['spaceBetween', 'flexStart']}
+                  >
+                    <Box marginRight={[2, 4]}>
+                      <Typography variant="h3" color="roseTinted400">
+                        {code}
+                      </Typography>
+                    </Box>
+                    <Button
+                      noWrap
+                      onClick={() => {
+                        copyToClipboard(code)
+                      }}
+                    >
+                      {copyCode}
+                    </Button>
+                  </Box>
+                </Box>
+              )
+            })}
           </Stack>
+          <Box textAlign="right" marginBottom={4}>
+            <Typography variant="pSmall">{codeDescription}</Typography>
+          </Box>
         </Box>
       }
       right={
