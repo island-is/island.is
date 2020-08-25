600ca52e7d877d664ec8b93d1d2694c0a00eb488
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2020-08-25 14:39:19 +0000

    Reintroduce mock token mocking fuglar token (#478)
    * Reintroduce mock token mocking fuglar token
    
    * auth_time, expires_at mocker values improved
    
    * smaller values, we dont need correct values for the mock

--- apps/service-portal/src/mirage-server/models/jwt-model.ts
+++ apps/service-portal/src/mirage-server/models/jwt-model.ts
@@ -1,37 +1,45 @@
-import { Actor, ActorDto } from './actor'
-import { Subject, SubjectDto } from './subject'
-import { addHours, format } from 'date-fns'
-import jwtDecode from 'jwt-decode'
-
+/* eslint-disable @typescript-eslint/camelcase */
 export class JwtToken {
-  actor: ActorDto
-  sub: SubjectDto
-  exp: string
-  iat: string
-
-  constructor(actor: Actor, subject: Subject) {
-    const sub: SubjectDto = {
-      name: subject.name,
-      nationalId: subject.nationalId,
-      scope: subject.scope,
-      subjectType: subject.subjectType,
-    }
-
-    const act: ActorDto = {
-      name: actor.name,
-      nationalId: actor.nationalId,
-    }
-
-    this.actor = act
-    this.sub = sub
-    const issueDate: Date = new Date()
-    this.iat = format(issueDate, 'T')
-    this.exp = format(addHours(issueDate, 2), 'T')
+  id_token: string
+  session_state: string
+  access_token: string
+  expires_in: number
+  token_type: string
+  refresh_token: string
+  scope: string
+  profile: {
+    s_hash: string
+    sid: string
+    sub: string
+    auth_time: number
+    idp: string
+    amr: string[]
+    name: string
+    natreg: string
+    nat: string
   }
-}
+  expires_at: number
 
-export class JwtUtils {
-  static async parseJwt(token: string): Promise<JwtToken> {
-    return await jwtDecode(token)
+  constructor() {
+    const dateNow = new Date().getTime()
+    this.id_token = 'TestiIdToken'
+    this.session_state = 'TestiSessionState'
+    this.access_token = 'TestiAccessToken'
+    this.expires_in = 3600
+    this.token_type = 'Bearer'
+    this.refresh_token = 'TestiRefreshToken'
+    this.scope = 'openid profile offline_access'
+    this.profile = {
+      s_hash: '29YzPWdEDLq3afOSxHI4zQ',
+      sid: '61109947A02FC45319A71F63D5C2A519',
+      sub: 'F404BD33DFCA505E41EC35B9014EE3B313DE71D66E4A2597DC17359EEA107C77',
+      auth_time: dateNow,
+      idp: 'islykill',
+      amr: ['external'],
+      name: 'Tester Testerson',
+      natreg: '1234561234',
+      nat: 'IS',
+    }
+    this.expires_at = dateNow + 2 * 60 * 60 * 1000
   }
 }
