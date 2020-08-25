729022a8b148e1f131200f34f9a9fc463de4ac39
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-08-25 13:44:47 +0000

    Remove unused pages

--- apps/air-discount-scheme/web/pages/en/subsidy.tsx
+++
@@ -1,6 +0,0 @@
-import { withLocale } from '../../i18n'
-import { Subsidy } from '../../screens'
-import { Screen } from '../../types'
-import { withAuth } from '../../auth'
-
-export default withLocale('en')(withAuth(Subsidy as Screen))
