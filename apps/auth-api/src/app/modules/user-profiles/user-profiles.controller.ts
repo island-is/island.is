cbacd31a877b3e8b058d78927e066b7e9fffc29e
stullifugl <67910583+stullifugl@users.noreply.github.com>
2020-08-25 13:53:21 +0000

    Feature/swagger is4 connection (#474)
    * Fixed error in sequelize.query
    
    * Reverting debug change
    
    * Added authentication option to swagger
    
    * Commented out restriction

--- apps/auth-api/src/app/modules/user-profiles/user-profiles.controller.ts
+++ apps/auth-api/src/app/modules/user-profiles/user-profiles.controller.ts
@@ -1,31 +1,30 @@
 import {
-    Body,
-    Controller,
-    Get,
-    NotFoundException,
-    Param,
-    Post,
-  } from '@nestjs/common'
-  import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+} from '@nestjs/common'
+import { ApiOkResponse, ApiTags, ApiOAuth2 } from '@nestjs/swagger'
 import { UserProfile } from './user-profile.model'
 import { UserProfilesService } from './user-profiles.service'
-  
-  @ApiTags('user-profiles')
-  @Controller('user-profiles')
-  export class UserProfilesController {
-    constructor(private readonly userProfilesService: UserProfilesService) {}
-  
-    @Get(':subjectId')
-    @ApiOkResponse({ type: UserProfile })
-    async findOne(@Param('subjectId') subjectId: string): Promise<UserProfile> {
-      const userProfile = await this.userProfilesService.findBySubjectId(subjectId)
-  
-      if (!userProfile) {
-        throw new NotFoundException("This user profile doesn't exist")
-      }
-  
-      return userProfile
+
+// @ApiOAuth2(['openid:profile']) // add OAuth restriction to this controller
+@ApiTags('user-profiles')
+@Controller('user-profiles')
+export class UserProfilesController {
+  constructor(private readonly userProfilesService: UserProfilesService) {}
+
+  @Get(':subjectId')
+  @ApiOkResponse({ type: UserProfile })
+  async findOne(@Param('subjectId') subjectId: string): Promise<UserProfile> {
+    const userProfile = await this.userProfilesService.findBySubjectId(
+      subjectId,
+    )
+
+    if (!userProfile) {
+      throw new NotFoundException("This user profile doesn't exist")
     }
-  
+
+    return userProfile
   }
-  
+}
