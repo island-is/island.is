d3abfc2d28678ed4c9c5bdae9cd0c3fe60e8463b
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-08-25 08:15:48 +0000

    Adds a test for the case module

---
+++ apps/judicial-system/api/src/app/modules/case/e2e/case.spec.ts
@@ -0,0 +1,24 @@
+import { setup } from '../../../../../test/setup'
+import * as request from 'supertest'
+import { INestApplication } from '@nestjs/common'
+
+let app: INestApplication
+
+beforeAll(async () => {
+  app = await setup()
+})
+
+describe('Application', () => {
+  it(`POST /case should create a case`, async () => {
+    // Act
+    const response = await request(app.getHttpServer())
+      .post('/case')
+      .send({
+        description: 'Test Case',
+      })
+      .expect(201)
+
+    // Assert
+    expect(response.body.id).toBeTruthy()
+  })
+})
