b91b658d411203006933d9aa52674a2dac33bc54
unnarb <unnarb@users.noreply.github.com>
2020-08-26 14:48:08 +0000

    Feature/client defaults (#497)
    * Client defaults and Type enums
    
    * Postman profile fixed like it was in config
    
    * Fixed Dummy Clients build
    
    Co-authored-by: Unnar Bjarnarson <unnar@fuglar.com>

--- apps/auth-api/src/app/modules/clients/clients.service.ts
+++ apps/auth-api/src/app/modules/clients/clients.service.ts
@@ -29,196 +29,175 @@ export class ClientsService {
       throw new NotFoundException("This client doesn't exist")
     }
 
+    this.logger.debug(client);
+
     return client;
   }
 
   buildDummyClients() {
     this.clients = [];
-    // swagger client using code flow + pkce
-    this.clients.push( {
-      ClientId : "swagger",
-      // ClientSecrets : [{ Value : new Secret("swagger_secret".Sha256()) } as Secret }],
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: [
-          "https://localhost:5001/signin-adfs",
-          "https://localhost:44378/oauth2-redirect.html",
-          "https://localhost:5002/oauth2-redirect.html",
-          "https://siidentityserverweb20200731081059.azurewebsites.net/signin-adfs"
-      ],
-      ClientSecrets: null,
 
-      PostLogoutRedirectUris: ["https://localhost:44378/oauth2-redirect.html"],
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "openid", "profile", "swagger_api.read" ],
-      FrontChannelLogoutUri: null
-    } as Client );
+    const swagger = new Client();
+    swagger.ClientId = "swagger";
+    swagger.AllowedGrantTypes = [ GrantType.AuthorizationCode ];
+    swagger.AllowAccessTokensViaBrowser = true;
+    swagger.RedirectUris = [
+      "https://localhost:5001/signin-adfs",
+      "https://localhost:44378/oauth2-redirect.html",
+      "https://localhost:5002/oauth2-redirect.html",
+      "https://siidentityserverweb20200731081059.azurewebsites.net/signin-adfs"
+    ];
+    // ClientSecrets : [{ Value : new Secret("swagger_secret".Sha256()) } as Secret }],
+    swagger.AllowedScopes = [ "openid", "profile", "swagger_api.read" ],
+    swagger.PostLogoutRedirectUris = ["https://localhost:44378/oauth2-redirect.html"],
+    swagger.AllowOfflineAccess = true;
+    swagger.RequirePkce = true;
+    this.clients.push(swagger);
 
-    // interactive client using code flow + pkce
-    this.clients.push( {
-      ClientId : "interactive",
-     //  ClientSecrets : { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: [
-        "https://localhost:5001/signin-adfs",
-        "https://localhost:5002/signin-oidc",
-        "https://localhost:5001/signin-google",
-        "https://localhost:5002/oauth2-redirect.html"
-      ],
-      FrontChannelLogoutUri: "https://localhost:5002/signout-oidc",
-      ClientSecrets: null,
+    const interactive = new Client();
+    interactive.ClientId = "interactive",
+    interactive.AllowedGrantTypes = [ GrantType.AuthorizationCode ],
+    interactive.ClientUri = null,
+    interactive.EnableLocalLogin = true,
+    interactive.AllowAccessTokensViaBrowser = true,
+    interactive.RequirePkce = true,
+    interactive.RequireClientSecret = false,
+    interactive.RedirectUris = [
+      "https://localhost:5001/signin-adfs",
+      "https://localhost:5002/signin-oidc",
+      "https://localhost:5001/signin-google",
+      "https://localhost:5002/oauth2-redirect.html"
+    ],
+    interactive.FrontChannelLogoutUri = "https://localhost:5002/signout-oidc",
+    interactive.ClientSecrets = null,
+    interactive.PostLogoutRedirectUris = ["https://localhost:5002/signout-callback-oidc", "https://localhost:5001/signin-google", "https://localhost:5002/oauth2-redirect.html" ],
+    interactive.AllowOfflineAccess = true,
+    interactive.AllowedScopes = [ "openid", "profile" ],
+    this.clients.push(interactive);
 
-      PostLogoutRedirectUris: ["https://localhost:5002/signout-callback-oidc", "https://localhost:5001/signin-google", "https://localhost:5002/oauth2-redirect.html" ],
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "openid", "profile" ],
-    } as Client);
+    const postman = new Client();
+    postman.ClientId = "postman",
+    postman.RequireClientSecret = false,
+    postman.AllowedGrantTypes = [ GrantType.AuthorizationCode ],
+    postman.RequirePkce = false,
+    postman.RedirectUris = [
+      "https://localhost:5001/signin-adfs", "https://postman", "https://oauth.pstmn.io/v1/callback"
+    ],
+    postman.AllowedScopes = ["openid", "profile", "email", "saml", "postman_resource.scope"  ]
+    this.clients.push(postman);
 
-
-     // interactive client using code flow + pkce
-    this.clients.push( {
-      ClientId : "mvc.code",
-     //  ClientSecrets = { new Secret("secret".Sha256()) },
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: [
-        "https://localhost:5001/signin-adfs",
-        "https://localhost:44302/signin-oidc",
-      ],
-      FrontChannelLogoutUri: "https://localhost:44302/signout-oidc",
-      ClientSecrets: null,
-      PostLogoutRedirectUris: ["https://localhost:44302/signout-callback-oidc" ],
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "openid", "profile" ],
-    } as Client );
-
-
-     this.clients.push( {
-      ClientId : "postman",
-     //  ClientSecrets = { new Secret("secret".Sha256()) },
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: [
-        "https://localhost:5001/signin-adfs", "https://postman", "https://oauth.pstmn.io/v1/callback"
-      ],
-      FrontChannelLogoutUri: null,
-      ClientSecrets: null,
-      PostLogoutRedirectUris: null,
-      AllowOfflineAccess: false,
-      AllowedScopes: ["openid", "profile", "email", "saml", "postman_resource.scope"  ],
-    } as Client );
-
-     // interactive client using clientCredentials
-    this.clients.push( {
-      ClientId : "postman.client.credentials",
-     //  ClientSecrets = { new Secret("secret".Sha256()) },
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.ClientCredentials ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: null,
-      FrontChannelLogoutUri: null,
-      ClientSecrets: null,
-      PostLogoutRedirectUris: null,
-      AllowOfflineAccess: false,
-      AllowedScopes: ["api"],
-    } as Client );
-
-    this.clients.push( {
-      ClientId : "www.certification.openid.net",
-     // ClientSecrets = { new Secret("0F8A81999CA44816A42F7F9055462A3C".Sha256()) },
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: false,
-      RequireClientSecret: false,
-      RedirectUris: ["https://www.certification.openid.net/"],
-      FrontChannelLogoutUri: null,
-      ClientSecrets: null,
-      PostLogoutRedirectUris: null,
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "openid", "profile", "offline_access" ],
-    } as Client );
-
-    this.clients.push( {
-      ClientId : "www.certification.openid.net-2",
+    const islandis1 = new Client();
+    islandis1.ClientId = "island-is-1",
      // ClientSecrets = { new Secret("833CAFE79B8441F3B08CB48BBA804D85".Sha256()) }
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: false,
-      RequireClientSecret: false,
-      RedirectUris: ["https://www.certification.openid.net/"],
-      FrontChannelLogoutUri: null,
-      ClientSecrets: null,
-      PostLogoutRedirectUris: null,
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "openid", "profile", "offline_access" ],
-    } as Client );
+    islandis1.Enabled = true,
+    islandis1.AllowedGrantTypes = [ GrantType.AuthorizationCode ],
+    islandis1.ClientUri = null,
+    islandis1.EnableLocalLogin = true,
+    islandis1.AllowAccessTokensViaBrowser = true,
+    islandis1.RequirePkce = true,
+    islandis1.RequireClientSecret = false,
+    islandis1.RedirectUris = ["http://localhost:4200/signin-oidc", "https://localhost:4200/signin-oidc"],
+    islandis1.FrontChannelLogoutUri = null,
+    islandis1.ClientSecrets = null,
+    islandis1.PostLogoutRedirectUris = null,
+    islandis1.AllowOfflineAccess = true,
+    islandis1.AllowedScopes = [ "openid", "profile", "offline_access" ],
+    this.clients.push(islandis1);
 
-    this.clients.push( {
-      ClientId : "island-is-1",
-     // ClientSecrets = { new Secret("833CAFE79B8441F3B08CB48BBA804D85".Sha256()) }
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.AuthorizationCode ],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: ["http://localhost:4200/signin-oidc", "https://localhost:4200/signin-oidc"],
-      FrontChannelLogoutUri: null,
-      ClientSecrets: null,
-      PostLogoutRedirectUris: null,
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "openid", "profile", "offline_access" ],
-    } as Client );
+    const islandisclientcred1 = new Client();
 
-    this.clients.push( {
-      ClientId : "island-is-client-cred-1",
+    islandisclientcred1.ClientId = "island-is-client-cred-1";
      // ClientSecrets = { new Secret("secret".Sha256()) },
-      Enabled: true,
-      AllowedGrantTypes: [ GrantType.ClientCredentials],
-      ClientUri: null,
-      EnableLocalLogin: true,
-      AllowAccessTokensViaBrowser: true,
-      RequirePkce: true,
-      RequireClientSecret: false,
-      RedirectUris: ["http://localhost:4200/signin-oidc", "https://localhost:4200/signin-oidc"],
-      FrontChannelLogoutUri: null,
-      ClientSecrets: null,
-      PostLogoutRedirectUris: null,
-      AllowOfflineAccess: true,
-      AllowedScopes: [ "api" ],
-    } as Client );
+    islandisclientcred1.Enabled = true;
+    islandisclientcred1.AllowedGrantTypes = [ GrantType.ClientCredentials];
+    islandisclientcred1.ClientUri = null;
+    islandisclientcred1.EnableLocalLogin = true;
+    islandisclientcred1.AllowAccessTokensViaBrowser = true;
+    islandisclientcred1.RequirePkce = true;
+    islandisclientcred1.RequireClientSecret = false;
+    islandisclientcred1.RedirectUris = ["http://localhost:4200/signin-oidc", "https://localhost:4200/signin-oidc"];
+    islandisclientcred1.FrontChannelLogoutUri = null;
+    islandisclientcred1.ClientSecrets = null;
+    islandisclientcred1.PostLogoutRedirectUris = null;
+    islandisclientcred1.AllowOfflineAccess = true;
+    islandisclientcred1.AllowedScopes = [ "api" ];
+    this.clients.push(islandisclientcred1);
 
+         // interactive client using code flow + pkce
+    // this.clients.push( {
+    //   ClientId : "mvc.code",
+    //  //  ClientSecrets = { new Secret("secret".Sha256()) },
+    //   Enabled: true,
+    //   AllowedGrantTypes: [ GrantType.AuthorizationCode ],
+    //   ClientUri: null,
+    //   EnableLocalLogin: true,
+    //   AllowAccessTokensViaBrowser: true,
+    //   RequirePkce: true,
+    //   RequireClientSecret: false,
+    //   RedirectUris: [
+    //     "https://localhost:5001/signin-adfs",
+    //     "https://localhost:44302/signin-oidc",
+    //   ],
+    //   FrontChannelLogoutUri: "https://localhost:44302/signout-oidc",
+    //   ClientSecrets: null,
+    //   PostLogoutRedirectUris: ["https://localhost:44302/signout-callback-oidc" ],
+    //   AllowOfflineAccess: true,
+    //   AllowedScopes: [ "openid", "profile" ],
+    // } as Client );
+
+    // this.clients.push( {
+    //   ClientId : "postman.client.credentials",
+    //  //  ClientSecrets = { new Secret("secret".Sha256()) },
+    //   Enabled: true,
+    //   AllowedGrantTypes: [ GrantType.ClientCredentials ],
+    //   ClientUri: null,
+    //   EnableLocalLogin: true,
+    //   AllowAccessTokensViaBrowser: true,
+    //   RequirePkce: true,
+    //   RequireClientSecret: false,
+    //   RedirectUris: null,
+    //   FrontChannelLogoutUri: null,
+    //   ClientSecrets: null,
+    //   PostLogoutRedirectUris: null,
+    //   AllowOfflineAccess: false,
+    //   AllowedScopes: ["api"],
+    // } as Client );
+
+    // this.clients.push( {
+    //   ClientId : "www.certification.openid.net",
+    //  // ClientSecrets = { new Secret("0F8A81999CA44816A42F7F9055462A3C".Sha256()) },
+    //   Enabled: true,
+    //   AllowedGrantTypes: [ GrantType.AuthorizationCode ],
+    //   ClientUri: null,
+    //   EnableLocalLogin: true,
+    //   AllowAccessTokensViaBrowser: true,
+    //   RequirePkce: false,
+    //   RequireClientSecret: false,
+    //   RedirectUris: ["https://www.certification.openid.net/"],
+    //   FrontChannelLogoutUri: null,
+    //   ClientSecrets: null,
+    //   PostLogoutRedirectUris: null,
+    //   AllowOfflineAccess: true,
+    //   AllowedScopes: [ "openid", "profile", "offline_access" ],
+    // } as Client );
+
+    // this.clients.push( {
+    //   ClientId : "www.certification.openid.net-2",
+    //  // ClientSecrets = { new Secret("833CAFE79B8441F3B08CB48BBA804D85".Sha256()) }
+    //   Enabled: true,
+    //   AllowedGrantTypes: [ GrantType.AuthorizationCode ],
+    //   ClientUri: null,
+    //   EnableLocalLogin: true,
+    //   AllowAccessTokensViaBrowser: true,
+    //   RequirePkce: false,
+    //   RequireClientSecret: false,
+    //   RedirectUris: ["https://www.certification.openid.net/"],
+    //   FrontChannelLogoutUri: null,
+    //   ClientSecrets: null,
+    //   PostLogoutRedirectUris: null,
+    //   AllowOfflineAccess: true,
+    //   AllowedScopes: [ "openid", "profile", "offline_access" ],
+    // } as Client );
 
   }
 
