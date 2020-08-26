2aba29c02791cc16a0bb78c12d2c96a7612b0925
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-08-26 09:42:08 +0000

    Resolve user for thjodskrauser

--- apps/air-discount-scheme/api/src/app/modules/discount/discount.resolver.ts
+++ apps/air-discount-scheme/api/src/app/modules/discount/discount.resolver.ts
@@ -6,7 +6,10 @@ import {
   Resolver,
 } from '@nestjs/graphql'
 
-import { Discount as TDiscount } from '@island.is/air-discount-scheme/types'
+import {
+  Discount as TDiscount,
+  ThjodskraUser,
+} from '@island.is/air-discount-scheme/types'
 import { Authorize, CurrentUser, AuthService, AuthUser } from '../auth'
 import { Discount, FlightLegFund } from './models'
 import { User } from '../user'
@@ -21,7 +24,9 @@ export class DiscountResolver {
     @CurrentUser() user: AuthUser,
     @Context('dataSources') { backendApi },
   ): Promise<Discount> {
-    const relations = await backendApi.getUserRelations(user.nationalId)
+    const relations: ThjodskraUser[] = await backendApi.getUserRelations(
+      user.nationalId,
+    )
     const discounts = await relations.reduce(async (acc, relation) => {
       let discount: TDiscount = await backendApi.getDiscount(
         relation.nationalId,
@@ -38,6 +43,18 @@ export class DiscountResolver {
         (relation) => relation.nationalId === discount.nationalId,
       ),
     }))
+  }
+
+  @ResolveField('user')
+  resolveUser(@Parent() discount: Discount): User {
+    const { user } = discount
+    return {
+      ...user,
+      name: `${user.firstName} ${user.middleName} ${user.lastName}`.replace(
+        /\s\s+/g,
+        ' ',
+      ),
+    }
   }
 
   @ResolveField('flightLegFund')
