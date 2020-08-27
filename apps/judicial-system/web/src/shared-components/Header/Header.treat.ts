052ade94f9f51e4dfcdab6066ee26b089523b180
Ívar Oddsson <ivaroddsson@gmail.com>
2020-08-27 09:43:58 +0000

    Minor padding change

--- apps/judicial-system/web/src/shared-components/Header/Header.treat.ts
+++ apps/judicial-system/web/src/shared-components/Header/Header.treat.ts
@@ -7,10 +7,11 @@ export const header = style({
   justifyContent: 'space-between',
   height: 80,
   padding: '0 16px',
+  boxShadow: '0px 4px 26px rgba(0, 0, 0, 0.05)',
 
   '@media': {
     [`screen and (min-width: ${theme.breakpoints.lg}px)`]: {
-      padding: '0 32px',
+      padding: '0 48px',
     },
   },
 })
