789d598f290c0ad00ca7d59723f0412fa5b34cdd
Ívar Oddsson <ivaroddsson@gmail.com>
2020-08-27 10:26:52 +0000

    Judicial system - Login screen (#506)
    * Created a folder structure and a login screen
    
    * Minor padding change
    
    * Remove unused test

--- apps/judicial-system/web/src/routes/Login/App.spec.tsx
+++ apps/judicial-system/web/src/routes/Login/App.spec.tsx
@@ -3,7 +3,7 @@ import { render, waitFor } from '@testing-library/react'
 import fetchMock from 'fetch-mock'
 import { BrowserRouter } from 'react-router-dom'
 
-import App from './App'
+import App from './Login'
 
 describe('App', () => {
   fetchMock.get('/api/cases', [])
@@ -16,15 +16,5 @@ describe('App', () => {
     )
 
     expect(baseElement).toBeTruthy()
-  })
-
-  it('should have a count as the title', async () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-    )
-    await waitFor(() => getByText('0 cases'))
-    expect(getByText('0 cases')).toBeTruthy()
   })
 })
