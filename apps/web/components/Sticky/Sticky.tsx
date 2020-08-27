43a82f211b29ce3c57d672b726ad722312542e49
Davíð Bachmann <dabbi@dabbi.is>
2020-08-27 10:27:33 +0000

    Treat grid system + Layout overhaul (#486)
    * Fix unclickable SearchInput links
    
    * First version of a flexbox-based Treat grid system
    
    * Layout overhaul (#499)
    
    * Figuring this out
    
    * Re-layout the Home page
    
    * Cleanup
    
    * Bring back link styles
    
    * Clean up CategoryLayout
    
    * Change top to paddingTop and bottom to paddingBottom when dealing with component spacing, to avoid confusion
    
    * Introduce GridContainer. Rename Grid to GridRow, GridItem to GridColumn. Change xl breakpoint to the global max-width of 1440px
    
    * Change the rest of layouts to use Grid
    
    * Cleanup unused
    
    * Have Grid stories up to date
    
    * Make Box component handle spacing
    
    * Format treat file
    
    * Bring back typo since other projects are depending on it
    
    * Fix Footer. Make GridColumn's have relative positioning.
    
    * Fix missing comma
    
    * Correct Footer link grid
    
    * Give Sticky a default top value based on navigation height. Cleanup before merge.

--- apps/web/components/Sticky/Sticky.tsx
+++ apps/web/components/Sticky/Sticky.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from 'react'
-
+import { STICKY_NAV_HEIGHT } from '@island.is/web/constants'
 import * as styles from './Sticky.treat'
+import { theme } from '@island.is/island-ui/theme'
 
 interface Props {
   top?: number
 }
 
-export const Sticky: FC<Props> = ({ children, top = 0 }) => {
+export const Sticky: FC<Props> = ({
+  children,
+  top = STICKY_NAV_HEIGHT + theme.spacing[1],
+}) => {
   return (
     <div className={styles.sticky} style={{ top }}>
       {children}
