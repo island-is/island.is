d6c1ebf378b7da0ecf6a33aa6dadeb8a7a30f209
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-08-27 13:47:20 +0000

    Use typed route keys that locales implement

--- apps/air-discount-scheme/web/pages/min-rettindi.tsx
+++ apps/air-discount-scheme/web/pages/min-rettindi.tsx
@@ -2,4 +2,4 @@ import { withLocale } from '../i18n'
 import { Subsidy } from '../screens'
 import { withAuth } from '../auth'
 
-export default withLocale('is', '/en/my-benefits')(withAuth(Subsidy))
+export default withLocale('is', 'myBenefits')(withAuth(Subsidy))
