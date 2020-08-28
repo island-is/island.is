d3073d949b89b1ca173d25a590fed694f6b812ef
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-08-28 14:53:29 +0000

    Extends the case model

--- apps/judicial-system/api/src/app/modules/case/dto/createCase.dto.ts
+++ apps/judicial-system/api/src/app/modules/case/dto/createCase.dto.ts
@@ -1,9 +1,30 @@
-import { IsNotEmpty, IsString } from 'class-validator'
-import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, IsEnum } from 'class-validator'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { CaseState } from '../case.model'
 
 export class CreateCaseDto {
   @IsNotEmpty()
   @IsString()
   @ApiProperty()
   readonly description: string
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  readonly policeCaseNumber: string
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  readonly suspectNationalId: string
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  readonly suspectName: string
+
+  @IsNotEmpty()
+  @IsEnum(CaseState)
+  @ApiPropertyOptional({ enum: CaseState })
+  readonly state: string
 }
