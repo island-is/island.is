8fe313247606968e1621fb9b6058d387b4ca504c
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-08-28 11:43:01 +0000

    Update swagger docs

--- apps/air-discount-scheme/backend/src/main.ts
+++ apps/air-discount-scheme/backend/src/main.ts
@@ -35,24 +35,41 @@ bootstrap({
             Any valid nationalId will work. Notice the warning above
             about this endpoint.
 
-        2a. Next you can fetch the user that is registered to this
-            discountCode by calling
-            GET /api/public/discounts/{discountCode}/user.
-            This endpoint will return user specific information that
-            should be populated on your booking page and made readonly
-            so the user can not change it.
 
-        2b. You can create a flight. A flight contains all the
-            relevant information about the booking from the customer.
-            Use POST /api/public/discounts/{discountCode}/flights.
-            When this endpoint is used the discountCode is
-            invalidated, so the user can not use it anymore and needs
-            to generate a new discountCode (step 1).
+        2.  Use the discount to book a flight on your end:
+
+          2a. First you need to fetch the user that is registered to this
+              discountCode by calling
+              GET /api/public/discounts/{discountCode}/user.
+              This endpoint will return user specific information that
+              should be populated on your booking page and made readonly
+              so the user can not change it.
+
+          2b. Then you can create a flight. A flight contains all the
+              relevant information about the booking from the customer.
+              Use POST /api/public/discounts/{discountCode}/flights.
+              When this endpoint is used the discountCode is
+              invalidated, so the user can not use it and needs
+              to generate a new discountCode (step 1).
+
 
         3.  When you have created a flight you have the possibility
             to delete it. For example if a payment failure has
-            occurred, you can use the ID that you received when
-            creating the flight to delete it.
+            occurred. You can use the ID that you received when
+            creating the flight to delete it. You have two possibilities:
+
+          3a. You can either delete all related flightLegs that were
+              booked by calling
+              DELETE /api/public/flights/{flightId}
+
+          3b. Or delete specific flightLegs within the booking by:
+
+            3ba. First fetching the related flightLegs by calling
+                 GET /api/public/flights/{flightId}
+
+            3bb. Then use the flightLeg IDs from "3ba." result
+                 to delete the desired flightLegs by calling
+                 DELETE /api/public/flights/{flightId}/flightLegs/{flightLegId}
     `,
     )
     .setVersion('1.0')
