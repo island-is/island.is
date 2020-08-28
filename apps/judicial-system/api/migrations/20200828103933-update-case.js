0d44ea211e16fb08e003e1d2cd256277c26888f2
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-08-28 15:01:58 +0000

    Fixes a formatting error in db migration file

--- apps/judicial-system/api/migrations/20200828103933-update-case.js
+++ apps/judicial-system/api/migrations/20200828103933-update-case.js
@@ -1,50 +1,34 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.addColumn(
-        'case',
-        'police_case_number',
-        {
-          type: Sequelize.STRING,
-          allowNull: false,
-          defaultValue: 'MISSING',
-        }
-      ),
-      queryInterface.addColumn(
-        'case',
-        'suspect_national_id',
-        {
-          type: Sequelize.STRING,
-          allowNull: false,
-          defaultValue: 'MISSING',
-        }
-      ),
-      queryInterface.addColumn(
-        'case',
-        'suspect_name',
-        {
-          type: Sequelize.STRING,
-          allowNull: false,
-          defaultValue: 'MISSING',
-        }
-      ),
-      queryInterface.addColumn(
-        'case',
-        'state',
-        {
-          type: Sequelize.ENUM(
-            'UNKNOWN',
-            'DRAFT',
-            'SUBMITTED',
-            'ACTIVE',
-            'COMPLETED',
-          ),
-          allowNull: false,
-          defaultValue: 'DRAFT',
-        },
-      )
+      queryInterface.addColumn('case', 'police_case_number', {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'MISSING',
+      }),
+      queryInterface.addColumn('case', 'suspect_national_id', {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'MISSING',
+      }),
+      queryInterface.addColumn('case', 'suspect_name', {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'MISSING',
+      }),
+      queryInterface.addColumn('case', 'state', {
+        type: Sequelize.ENUM(
+          'UNKNOWN',
+          'DRAFT',
+          'SUBMITTED',
+          'ACTIVE',
+          'COMPLETED',
+        ),
+        allowNull: false,
+        defaultValue: 'DRAFT',
+      }),
     ])
   },
 
@@ -54,6 +38,6 @@ module.exports = {
       queryInterface.removeColumn('case', 'suspect_national_id'),
       queryInterface.removeColumn('case', 'suspect_name'),
       queryInterface.removeColumn('case', 'state'),
-    ]);
-  }
-};
+    ])
+  },
+}
