1a7a3a5ef6188fd93432372df1839b3e624a92b9
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-08-31 14:56:22 +0000

    Fix token

--- scripts/spin.sh
+++ scripts/spin.sh
@@ -11,11 +11,13 @@ tokens[islandis]=$SPINNAKER_WEBHOOK_ISLANDIS_TOKEN
 tokens[gjafakort]=$SPINNAKER_WEBHOOK_GJAFAKORT_TOKEN
 tokens[air-discount-scheme]=$SPINNAKER_WEBHOOK_AIR_DISCOUNT_SCHEME_TOKEN
 
+TOKEN=${tokens[$SERVICE]}
+
 HELM_SHA1=$(echo $HELM_VERSION | cut -d"_" -f3)
 
 curl -v https://spinnaker-gate.shared.devland.is/webhooks/webhook/$SERVICE -X POST -H "content-type: application/json" --data-binary @- <<BODY
 {
-"token": "${tokens[SERVICE]}",
+"token": "$TOKEN",
 "branch": "$GIT_BRANCH",
 "parameters": { "docker_tag": "$DOCKER_TAG" },
 "artifacts": [
