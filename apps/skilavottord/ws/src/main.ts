54751458e931019a7ef3cba4cea137cfc7af320e
Jonsson <tjonsson@deloitte.is>
2020-08-31 14:41:10 +0000

    using the standard nest library

--- apps/skilavottord/ws/src/main.ts
+++ apps/skilavottord/ws/src/main.ts
@@ -3,19 +3,10 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-
+import { bootstrap } from '@island.is/infra-nest-server'
 import { AppModule } from './app/app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  //const globalPrefix = 'api'
-  //app.setGlobalPrefix(globalPrefix)
-  const port = process.env.PORT || 3333
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix)
-  })
-}
-
-bootstrap()
+bootstrap({
+  appModule: AppModule,
+  name: 'skilavottord-ws',
+})
