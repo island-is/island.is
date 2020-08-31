544a7e5a755ba7d55ecc0d8392ef4d2a198ec17c
Ívar Oddsson <ivaroddsson@gmail.com>
2020-08-31 21:47:06 +0000

    Added an Alert component

---
+++ libs/island-ui/core/src/lib/Alert/Alert.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ContentBlock } from '../..'
+import { Alert } from './Alert'
+import { GridContainer } from '../Grid/GridContainer'
+import { GridRow, GridColumn } from '../Grid'
+export default {
+  title: 'Components/Alert',
+  component: Alert,
+}
+
+export const Info = () => (
+  <GridContainer>
+    <ContentBlock>
+      <GridRow>
+        <GridColumn span={5}>
+          <Alert
+            type="info"
+            title="Vissir þú að þú getur gert eitthvað sniðugt og þetta eru lengri skilaboð?"
+            message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus pellentesque amet, id tortor urna faucibus augue sit. Fames dignissim condimentum nibh ut in."
+          />
+        </GridColumn>
+      </GridRow>
+    </ContentBlock>
+  </GridContainer>
+)
+
+export const Error = () => (
+  <ContentBlock>
+    <Alert
+      type="error"
+      title="Vissir þú að þú getur gert eitthvað sniðugt og þetta eru lengri skilaboð?"
+      message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus pellentesque amet, id tortor urna faucibus augue sit. Fames dignissim condimentum nibh ut in."
+    />
+  </ContentBlock>
+)
