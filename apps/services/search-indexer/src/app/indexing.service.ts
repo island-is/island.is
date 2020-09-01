27e5265b28ab2fd6bf0d4d600f8947d0052edb27
Stefán Harald <stefanharald@gmail.com>
2020-09-01 17:38:47 +0000

    Updated field

--- apps/services/search-indexer/src/app/indexing.service.ts
+++ apps/services/search-indexer/src/app/indexing.service.ts
@@ -124,7 +124,7 @@ export class IndexingService {
         if (doc.data && doc.data.target) {
           if (doc.data.target.sys.contentType.sys.id === 'processEntry') {
             response += (doc.data.target.fields.processTitle ?? '') + '\n'
-            response += (doc.data.target.fields.processDescription ?? '') + '\n'
+            response += (doc.data.target.fields.processInfo ?? '') + '\n'
           } else {
             //todo implement more types
           }
