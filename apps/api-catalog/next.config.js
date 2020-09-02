a5af0dd12eaff4054b66e85abf72c83f9d71fa3d
Sævar Ingi Sigurðsson <saevar.ingi.sigurdsson@advania.is>
2020-09-02 14:03:27 +0000

    changed next.config.js to be able to use ui components

--- apps/api-catalog/next.config.js
+++ apps/api-catalog/next.config.js
@@ -1,6 +1,47 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  // Set this to true if you use CSS modules.
-  // See: https://github.com/css-modules/css-modules
-  cssModules: false,
-})
+const withTreat = require('next-treat')()
+const withSourceMaps = require('@zeit/next-source-maps')
+// const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+
+const {
+  //API_URL = 'http://localhost:4242',
+  WEB_PUBLIC_URL = 'http://localhost:4200',
+  SENTRY_DSN,
+  // SENTRY_AUTH_TOKEN,
+  //NODE_ENV,
+} = process.env
+//const apiPath = '/api'
+//const graphqlPath = '/api/graphql'
+
+module.exports = withSourceMaps(
+  withTreat({
+    webpack: (config, options) => {
+      if (!options.isServer) {
+        config.resolve.alias['@sentry/node'] = '@sentry/browser'
+      }
+
+      // if (SENTRY_DSN && SENTRY_AUTH_TOKEN) {
+      //   config.plugins.push(
+      //     new SentryWebpackPlugin({
+      //       include: '.next',
+      //       ignore: ['node_modules'],
+      //       urlPrefix: '~/_next',
+      //       release: options.buildId,
+      //     }),
+      //   )
+      // }
+
+      return config
+    },
+    serverRuntimeConfig: {
+      // Will only be available on the server side
+      //apiUrl: `${API_URL}${apiPath}`,
+      //graphqlEndpoint: `${API_URL}${graphqlPath}`,
+    },
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      apiUrl: `${WEB_PUBLIC_URL}/api`,
+      SENTRY_DSN,
+      //graphqlEndpoint: graphqlPath,
+    },
+  }),
+)
