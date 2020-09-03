9a123bb2695bcfc42ea128a196366bf61bbda2a1
Atli Guðlaugsson <atli@sendiradid.is>
2020-09-03 14:06:18 +0000

    strict typing for field: [checkbox, input, nunber input, polar question, radio button, select] and footer

--- libs/island-ui/core/src/lib/Footer/Footer.tsx
+++ libs/island-ui/core/src/lib/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import cn from 'classnames'
 import chunk from 'lodash/chunk'
 import { Box } from '../Box/Box'
 import { Logo } from '../Logo/Logo'
