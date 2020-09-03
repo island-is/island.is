8f6a6c6cb304e1282387f48d892def7f76737cad
Guðjón Hólm Sigurðsson <gudjon.holm.sigurdsson@advania.is>
2020-09-03 17:45:04 +0000

    hardcoding styles and testing

--- apps/api-catalog/components/ServiceCard/service-card.tsx
+++ apps/api-catalog/components/ServiceCard/service-card.tsx
@@ -1,4 +1,79 @@
 import React from 'react'
+import { Box } from '@island.is/island-ui/core'
+import { getQueryParser } from 'next/dist/next-server/server/api-utils';
+
+const sharedStyles = {
+  item:{
+    borderRadius:15,
+    paddingTop:5,
+    paddingBottom:5,
+    paddingLeft:15,
+    paddingRight:15,
+    marginRight:10,
+    fontSize:14
+  }
+};
+
+const styles = {
+    card:{
+        paddingLeft:40,
+        paddingTop:40,
+        paddingBottom:40
+    },
+    owner:{
+        fontSize:32,
+        marginBottom:20
+    },
+    name:{
+        fontSize:16,
+        marginBottom:20
+
+    },
+    category:{
+        display:'flex',
+        justifyContent:'spaceBetween'
+    },
+    priceItem:{
+        background: 'lightgray',
+        borderRadius:sharedStyles.item.borderRadius,
+        paddingTop :sharedStyles.item.paddingTop,
+        paddingBottom:sharedStyles.item.paddingBottom,
+        paddingLeft :sharedStyles.item.paddingLeft,
+        paddingRight:sharedStyles.item.paddingRight,
+        marginRight:sharedStyles.item.marginRight,
+        fontSize:sharedStyles.item.fontSize
+    },
+    categories:{
+        background: '#dedea5',
+        borderRadius:sharedStyles.item.borderRadius,
+        paddingTop :sharedStyles.item.paddingTop,
+        paddingBottom:sharedStyles.item.paddingBottom,
+        paddingLeft :sharedStyles.item.paddingLeft,
+        paddingRight:sharedStyles.item.paddingRight,
+        marginRight:sharedStyles.item.marginRight,
+        fontSize:sharedStyles.item.fontSize
+    },
+    type: {
+        background:'green',
+        borderRadius:sharedStyles.item.borderRadius,
+        paddingTop :sharedStyles.item.paddingTop,
+        paddingBottom:sharedStyles.item.paddingBottom,
+        paddingLeft :sharedStyles.item.paddingLeft,
+        paddingRight:sharedStyles.item.paddingRight,
+        marginRight:sharedStyles.item.marginRight,
+        fontSize:sharedStyles.item.fontSize
+    },
+    access: {
+        background:'yellow',
+        borderRadius:sharedStyles.item.borderRadius,
+        paddingTop :sharedStyles.item.paddingTop,
+        paddingBottom:sharedStyles.item.paddingBottom,
+        paddingLeft :sharedStyles.item.paddingLeft,
+        paddingRight:sharedStyles.item.paddingRight,
+        marginRight:sharedStyles.item.marginRight,
+        fontSize:sharedStyles.item.fontSize
+    }
+};
 
 //import './service-card.scss'
 
@@ -8,6 +83,7 @@ export interface ServiceCardInformation {
     pricing:Array<string>;
     categories:Array<string>
     type:Array<string>;
+    access:Array<string>;
 }
 
 export interface ServiceCardProps {
@@ -16,18 +92,40 @@ export interface ServiceCardProps {
 
 export const ServiceCard = (props: ServiceCardProps) => {
   return (
-    <div>
-    <hr />
-    <p className="service-name">Name: {props.service.name}</p>
-    <p>owner: {props.service.owner}</p>
-      <ul className="prices">
-        {	props.service.pricing?.map((item, index) => (
-              <li className="card-item" key={index}>{item + ''} </li>
-          ))
-        }
-    </ul>
-    <hr />
-</div>
+    <Box
+      boxShadow="large"
+      borderRadius="large"
+      style={styles.card}
+    >
+    <p style={styles.owner}>{props.service.owner}</p>
+    <p style={styles.name}>{props.service.name}</p>
+    
+    
+        <div className="prices" style={styles.category} >
+          {	props.service.pricing?.map((item, index) => (
+                <div style={styles.priceItem} className="card-item" key={index}>{item + ''} </div>
+            ))
+          }
+        </div>
+        <div className="categories" style={styles.category} >
+          {	props.service.categories?.map((item, index) => (
+                <div style={styles.categories} className="card-item" key={index}>{item + ''} </div>
+            ))
+          }
+        </div>
+        <div className="type" style={styles.category} >
+          {	props.service.type?.map((item, index) => (
+                <div style={styles.type} className="card-item" key={index}>{item + ''} </div>
+            ))
+          }
+        </div>
+        <div className="access" style={styles.category} >
+          {	props.service.access?.map((item, index) => (
+                <div style={styles.access} className="card-item" key={index}>{item + ''} </div>
+            ))
+          }
+        </div>
+</Box>
   )
 }
 
--- apps/api-catalog/screens/ServiceList/ServiceList.tsx
+++ apps/api-catalog/screens/ServiceList/ServiceList.tsx
@@ -11,9 +11,10 @@ import {
 } from '@island.is/island-ui/core'
 
 const services = [
-  { name:"serviceName1", owner:"serviceOwnerName1", pricing:["free", "custom"], categories:null, type:null},
-  { name:"serviceName2", owner:"serviceOwnerName2", pricing:null, categories:["personal", "public"], type:["react"]},
-  { name:"serviceName3", owner:"serviceOwnerName3", pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"]}
+  { name:"Þjóðskrá",     owner:"Fasteignaskrá",     pricing:null, categories:null, type:["REST"], access:["API GW"]},
+  { name:"serviceName1", owner:"serviceOwnerName1", pricing:["free", "custom"], categories:null, type:null, access:["API GW"]},
+  { name:"serviceName2", owner:"serviceOwnerName2", pricing:null, categories:["personal", "public"], type:["react"], access:["API GW"]},
+  { name:"serviceName3", owner:"serviceOwnerName3", pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"], access:["API GW"]}
 ];
 
 function ServiceList() {
