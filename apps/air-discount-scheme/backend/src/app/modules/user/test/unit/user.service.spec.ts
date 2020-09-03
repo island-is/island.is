aab1eb4e7df9002d832b3b5bd148eb88942ab1bd
Davíð Guðni Halldórsson <davidgudni@gmail.com>
2020-09-03 09:19:35 +0000

    Unit testing the user module

---
+++ apps/air-discount-scheme/backend/src/app/modules/user/test/unit/user.service.spec.ts
@@ -0,0 +1,139 @@
+import { Test } from '@nestjs/testing'
+
+import { User } from '../../user.model'
+import { UserService } from '../../user.service'
+import { FlightService } from '../../../flight'
+import { NationalRegistryService } from '../../../nationalRegistry'
+
+const user: User = {
+  nationalId: '1326487905',
+  firstName: 'Jón',
+  gender: 'kk',
+  lastName: 'Jónsson',
+  middleName: 'Gunnar',
+  address: 'Bessastaðir 1',
+  postalcode: 225,
+  city: 'Álftanes',
+  fund: {
+    nationalId: '1326487905',
+    credit: 2,
+    used: 2,
+    total: 2,
+  },
+}
+
+describe('UserService', () => {
+  let userService: UserService
+  let flightService: FlightService
+  let nationalRegistryService: NationalRegistryService
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: FlightService,
+          useClass: jest.fn(() => ({
+            countFlightLegsByNationalId: () => ({}),
+            isADSPostalCode: () => ({}),
+          })),
+        },
+        {
+          provide: NationalRegistryService,
+          useClass: jest.fn(() => ({
+            getUser: () => ({}),
+          })),
+        },
+      ],
+    }).compile()
+
+    userService = moduleRef.get<UserService>(UserService)
+    flightService = moduleRef.get<FlightService>(FlightService)
+    nationalRegistryService = moduleRef.get<NationalRegistryService>(
+      NationalRegistryService,
+    )
+  })
+
+  describe('getRelations', () => {
+    it('should return the same nationalId as provided in a list', async () => {
+      const result = await userService.getRelations(user.nationalId)
+
+      expect(result).toEqual([user.nationalId])
+    })
+  })
+
+  describe('getUserInfoByNationalId', () => {
+    it('should return user with correct postal code with credit', async () => {
+      const flightLegs = {
+        unused: user.fund.credit,
+        used: user.fund.used,
+        total: user.fund.total,
+      }
+      const isValidPostalCode = true
+
+      const getUserSpy = jest
+        .spyOn(nationalRegistryService, 'getUser')
+        .mockImplementation(() => Promise.resolve(user))
+      const countFlightLegsByNationalIdSpy = jest
+        .spyOn(flightService, 'countFlightLegsByNationalId')
+        .mockImplementation(() => Promise.resolve(flightLegs))
+      const isADSPostalCodeSpy = jest
+        .spyOn(flightService, 'isADSPostalCode')
+        .mockImplementation(() => isValidPostalCode)
+
+      const result = await userService.getUserInfoByNationalId(user.nationalId)
+
+      expect(getUserSpy).toHaveBeenCalledWith(user.nationalId)
+      expect(countFlightLegsByNationalIdSpy).toHaveBeenCalledWith(
+        user.nationalId,
+      )
+      expect(isADSPostalCodeSpy).toHaveBeenCalledWith(user.postalcode)
+      expect(result).toEqual(user)
+    })
+
+    it('should return user with incorrect postal code with no credit', async () => {
+      const flightLegs = {
+        unused: user.fund.credit,
+        used: user.fund.used,
+        total: user.fund.total,
+      }
+      const isValidPostalCode = false
+
+      const getUserSpy = jest
+        .spyOn(nationalRegistryService, 'getUser')
+        .mockImplementation(() => Promise.resolve(user))
+      const countFlightLegsByNationalIdSpy = jest
+        .spyOn(flightService, 'countFlightLegsByNationalId')
+        .mockImplementation(() => Promise.resolve(flightLegs))
+      const isADSPostalCodeSpy = jest
+        .spyOn(flightService, 'isADSPostalCode')
+        .mockImplementation(() => isValidPostalCode)
+
+      const result = await userService.getUserInfoByNationalId(user.nationalId)
+
+      expect(getUserSpy).toHaveBeenCalledWith(user.nationalId)
+      expect(countFlightLegsByNationalIdSpy).toHaveBeenCalledWith(
+        user.nationalId,
+      )
+      expect(isADSPostalCodeSpy).toHaveBeenCalledWith(user.postalcode)
+      expect(result).toEqual({
+        ...user,
+        fund: {
+          ...user.fund,
+          credit: 0,
+        },
+      })
+    })
+
+    it('should return null if nationalRegistryService does not return user', async () => {
+      const getUserSpy = jest
+        .spyOn(nationalRegistryService, 'getUser')
+        .mockImplementation(() => Promise.resolve(null))
+
+      const result = await userService.getUserInfoByNationalId(user.nationalId)
+
+      expect(getUserSpy).toHaveBeenCalledWith(user.nationalId)
+      expect(result).toBe(null)
+    })
+  })
+})
