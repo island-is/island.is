c7a903fb9263bfec3ef734263eb59bc94026af81
Davíð Bachmann <dabbi@dabbi.is>
2020-09-03 10:52:45 +0000

    Fix isLinkInternal to handle Nextjs routes (#630)
    * Fix isLinkInternal to handle Nextjs routes
    
    * Forward 'as' prop to Nextjs Link

--- libs/island-ui/core/src/lib/Link/Link.tsx
+++ libs/island-ui/core/src/lib/Link/Link.tsx
@@ -5,10 +5,9 @@ import cn from 'classnames'
 import * as styles from './Link.treat'
 
 const isLinkInternal = (href) => {
-  // If it's a relative url such as '/path', 'path' and does not contain a protocol we can assume it is internal.
-  if (href.indexOf('://') === -1) return true
+  if (typeof href === 'string' && href.indexOf('://') !== -1) return false
 
-  return false
+  return true
 }
 
 interface Props extends LinkProps {
@@ -46,6 +45,7 @@ const Link: React.FC<Props> = ({
         scroll={scroll}
         passHref={passHref}
         prefetch={prefetch}
+        as={as}
       >
         <a className={classNames} {...linkProps}>
           {children}
