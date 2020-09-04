b3f6dc08ec9b17979223189fb980b80be5efdbdd
Guðjón Hólm Sigurðsson <gudjon.holm.sigurdsson@advania.is>
2020-09-04 09:08:27 +0000

    ServiceList.getInitialProps added to ServiceList

--- apps/api-catalog/screens/ServiceList/ServiceList.tsx
+++ apps/api-catalog/screens/ServiceList/ServiceList.tsx
@@ -10,14 +10,7 @@ import {
   Footer
 } from '@island.is/island-ui/core'
 
-const services = [
-  { name:"Þjóðskrá",     owner:"Fasteignaskrá",     pricing:null, categories:null, type:["REST"], access:["API GW"]},
-  { name:"serviceName1", owner:"serviceOwnerName1", pricing:["free", "custom"], categories:null, type:null, access:["API GW"]},
-  { name:"serviceName2", owner:"serviceOwnerName2", pricing:null, categories:["personal", "public"], type:["react"], access:["API GW"]},
-  { name:"serviceName3", owner:"serviceOwnerName3", pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"], access:["API GW"]}
-];
-
-function ServiceList() {
+export default function ServiceList({services}) {
   return (
     <Page>
       <Head>
@@ -61,4 +54,13 @@ function ServiceList() {
   )
 }
 
-export default ServiceList
+ServiceList.getInitialProps = () => {
+  const services = [
+    { name:"Þjóðskrá",     owner:"Fasteignaskrá",     pricing:null,                          categories:null,                   type:["REST"],  access:["API GW"]},
+    { name:"serviceName1", owner:"serviceOwnerName1", pricing:["free", "custom"],            categories:null,                   type:null,      access:["X-Road"]},
+    { name:"serviceName2", owner:"serviceOwnerName2", pricing:null,                          categories:["personal", "public"], type:["react"], access:["API GW"]},
+    { name:"serviceName3", owner:"serviceOwnerName3", pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"]}
+  ];
+  return {services:services};
+
+}
