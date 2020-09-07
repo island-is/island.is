c911afd2dccf1e8253845e5b23bc5bb616e0f9cf
atligudl <atli@sendiradid.is>
2020-09-07 14:14:31 +0000

    Service portal/documents graphql api (#678)
    * data fetched from external api to grapql
    
    * get document categories
    
    * Document category api
    
    * paging for document list
    
    * connection to documents api, basic paging, action card loader
    
    * fetch document categories from api
    
    * dynamic paging
    
    * documents widget
    
    * wrapper component for document details
    
    * content block set around header
    
    * run formatter
    
    * remove console logs, removing call to document details for now
    
    Co-authored-by: Olafur <obmagnusson@gmail.com>

--- libs/api/domains/documents/src/lib/document.resolver.ts
+++ libs/api/domains/documents/src/lib/document.resolver.ts
@@ -1,14 +1,37 @@
-import { Args, Query, Resolver, Mutation } from '@nestjs/graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 import { DocumentService } from './document.service'
-import { Document } from './document.model'
+import { Document } from './models/document.model'
 import { GetDocumentInput } from './dto/getDocumentInput'
+import { ListDocumentsInput } from './dto/listDocumentsInput'
+import { DocumentDetails } from './models/documentDetails.model'
+import { DocumentCategory } from './models/documentCategory.model'
 
 @Resolver()
 export class DocumentResolver {
   constructor(private documentService: DocumentService) {}
 
-  @Query(() => Document, { nullable: true })
-  getDocument(@Args('input') input: GetDocumentInput): Document {
-    return this.documentService.findByDocumentId(input.id)
+  @Query(() => DocumentDetails, { nullable: true })
+  async getDocument(
+    @Args('input') input: GetDocumentInput,
+  ): Promise<DocumentDetails> {
+    const kennitala = '2606862759' // Pending proper authentication
+    const result = await this.documentService.findByDocumentId(
+      kennitala,
+      input.id,
+    )
+
+    return result
+  }
+
+  @Query(() => [Document], { nullable: true })
+  listDocuments(@Args('input') input: ListDocumentsInput): Promise<Document[]> {
+    input.natReg = '2606862759' // Pending proper authentication
+    return this.documentService.listDocuments(input)
+  }
+
+  @Query(() => [DocumentCategory], { nullable: true })
+  getDocumentCategories(): Promise<DocumentCategory[]> {
+    const natReg = '2606862759' // Pending proper authentication
+    return this.documentService.getCategories(natReg)
   }
 }
