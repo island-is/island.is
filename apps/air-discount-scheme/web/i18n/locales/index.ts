7c1e6d350361a0322b7d18c4fffdb91f114b86e5
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-09-07 14:29:25 +0000

    useTranslations hook for dynamic translations

---
+++ apps/air-discount-scheme/web/i18n/locales/index.ts
@@ -0,0 +1,6 @@
+import * as icelandicTranslations from './is.json'
+import * as englishTranslations from './en.json'
+import { Translation as ITranslation } from './translation'
+
+export type Translation = ITranslation
+export { icelandicTranslations, englishTranslations }
