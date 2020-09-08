d3dc735c5825babae227247cefd91dfc7b794a35
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-09-08 08:00:08 +0000

    Fixes formatting

--- apps/judicial-system/api/src/app/modules/user/user.service.spec.ts
+++ apps/judicial-system/api/src/app/modules/user/user.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { UserService } from './user.service'
 
 describe('UserService', () => {
-  let service: UserService;
+  let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService],
-    }).compile();
+    }).compile()
 
-    service = module.get<UserService>(UserService);
-  });
+    service = module.get<UserService>(UserService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
