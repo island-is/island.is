a4437490ccdf0c0b2b4cab98c4c04421a0fc44a1
Guðjón Hólm Sigurðsson <gudjon.holm.sigurdsson@advania.is>
2020-09-08 20:49:39 +0000

    working on filtering

--- apps/api-catalog/components/ServiceRepository/service-repository.ts
+++ apps/api-catalog/components/ServiceRepository/service-repository.ts
@@ -1,25 +1,63 @@
 import {ServiceStatusValue } from  '..'
+const MAX_LIMIT = 5;
+const OrgServices =[
+    { id:0, owner:"Þjóðskrá",         name:"0 Fasteignaskrá",       pricing:null,                          categories:null,                   type:["REST"],  access:["API GW"], status:ServiceStatusValue.OK},
+    { id:1, owner:"Þjóðskrá",         name:"01 Einstaklingsskrá",    pricing:["free", "custom"],            categories:null,                   type:null,      access:["X-Road"], status:ServiceStatusValue.WARNING},
+    { id:2, owner:"Þjóðskrá",         name:"02 Staðfangaskrá",       pricing:null,                          categories:["personal", "public"], type:["react"], access:["API GW"], status:ServiceStatusValue.ERROR},
+    { id:3, owner:"Skatturinn",       name:"03 Virðisaukaskattur",   pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"], status:ServiceStatusValue.WARNING},
+    { id:4, owner:"Skatturinn",       name:"4 Staðgreiðsla",        pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"], status:ServiceStatusValue.OK},
+    { id:5, owner:"Vinnumálastofnun", name:"5 Fæðingarorlofssjóður",pricing:null,                          categories:["personal", "public"], type:["react"], access:["API GW"], status:ServiceStatusValue.ERROR},
+    { id:6, owner:"Samgöngujstofa",   name:"06 Ökutækjaskrá",        pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"], status:ServiceStatusValue.UNKNOWN},
+  ];
 
-const services = {
-  result:[
-    { id:0, owner:"Þjóðskrá",         name:"Fasteignaskrá",       pricing:null,                          categories:null,                   type:["REST"],  access:["API GW"], status:ServiceStatusValue.OK},
-    { id:1, owner:"Þjóðskrá",         name:"Einstaklingsskrá",    pricing:["free", "custom"],            categories:null,                   type:null,      access:["X-Road"], status:ServiceStatusValue.WARNING},
-    { id:2, owner:"Þjóðskrá",         name:"Staðfangaskrá",       pricing:null,                          categories:["personal", "public"], type:["react"], access:["API GW"], status:ServiceStatusValue.ERROR},
-    { id:3, owner:"Skatturinn",       name:"Virðisaukaskattur",   pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"], status:ServiceStatusValue.WARNING},
-    { id:4, owner:"Skatturinn",       name:"Staðgreiðsla",        pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"], status:ServiceStatusValue.OK},
-    { id:5, owner:"Vinnumálastofnun", name:"Fæðingarorlofssjóður",pricing:null,                          categories:["personal", "public"], type:["react"], access:["API GW"], status:ServiceStatusValue.ERROR},
-    { id:6, owner:"Samgöngujstofa",   name:"Ökutækjaskrá",        pricing:["daily","monthly", "yearly"], categories:["personal", "public"], type:["SOAP"],  access:["API GW"], status:ServiceStatusValue.UNKNOWN},
-  ]
-};
 
-  export function getAllServices() {
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    return services;
-
+export async function getServices(cursor:number, limit:number, ownerFilter:string, nameFilter:string) {
+  
+  let filtered = OrgServices;
+  if (nameFilter !==undefined && nameFilter !== null) {
+    filtered = filtered.filter(e => e.name.includes(nameFilter));
+  }
+  if (ownerFilter !==undefined && ownerFilter !== null) {
+    filtered = filtered.filter(e => e.owner.includes(ownerFilter));
   }
 
-  export function getServicePage(cursor:number, limit:number) {
+  return await limitServices(filtered, cursor, limit);
+}
 
-    return services;
+async function limitServices(services, cursor:number, limit:number) {
+  await timeout(2000);
 
+  const len = services.length;
+  const searchCursor = cursor === null? 0 : cursor;
+  let lastIndex = (limit === null || limit > MAX_LIMIT) ? MAX_LIMIT : limit;
+  if (len < 1) {
+    return { result: null, nextCursor:null }
   }
+
+  
+  const startIndex = ( cursor === null || cursor < 0 )? 0 : services.map(function(x) {return x.id; }).indexOf(searchCursor);
+  if (startIndex < 0) {
+    return { result: null, nextCursor:null };
+  }
+
+  lastIndex+=startIndex;
+  
+  let nextCursor = null;
+
+  if (lastIndex >= len) {
+    lastIndex = len;
+  } else {
+    nextCursor = services[lastIndex].id;
+  }
+
+  const ret ={
+    result: services.slice(startIndex, lastIndex),
+    nextCursor:nextCursor
+  }
+
+  return ret;
+}
