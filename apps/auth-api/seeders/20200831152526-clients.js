0fb0a3f1c08e6cf93ad038d40ecd337d49d8d055
Unnar Bjarnarson <unnar@fuglar.com>
2020-09-09 11:26:31 +0000

    Seed for Clients

--- apps/auth-api/seeders/20200831152526-clients.js
+++ apps/auth-api/seeders/20200831152526-clients.js
@@ -9,7 +9,7 @@ module.exports = {
         require_pkce: false,
 
         //defaults
-        allow_offline_access: 'N',
+        allow_offline_access: false,
         identity_token_lifetime: 300,
         access_token_lifetime: 3600,
         authorization_code_lifetime: 300,
@@ -27,14 +27,79 @@ module.exports = {
         back_channel_logout_session_required: true,
         enabled: true,
         require_consent: false,
-        require_pkce: true,
         allow_plain_text_pkce: false,
         allow_access_token_via_browser: true,
         front_channel_logout_session_required: true,
         allow_remember_consent: true,
         client_claims_prefix: 'client_',
         protocol_type: 'oidc',
-        require_client_secret: true,
+        consent_lifetime: 3600
+      },
+      {
+        client_id: 'island-is-1',
+        require_client_secret: false,
+        require_pkce: false,
+        enable_local_login: true,
+        require_pkce: true,
+        allow_offline_access: true,
+
+
+        //defaults
+        identity_token_lifetime: 300,
+        access_token_lifetime: 3600,
+        authorization_code_lifetime: 300,
+        absolute_refresh_token_lifetime: 2592000,
+        sliding_refresh_token_lifetime: 1296000,
+        refresh_token_usage: 1,
+        update_access_token_claims_on_refresh: true,
+        refresh_token_expiration: 0,
+        access_token_type: 0,
+        include_jwt_id: true,
+        always_send_client_claims: false,
+        device_code_lifetime: 300,
+        always_include_user_claims_in_id_token: false,
+        back_channel_logout_session_required: true,
+        enabled: true,
+        require_consent: false,
+        allow_plain_text_pkce: false,
+        allow_access_token_via_browser: true,
+        front_channel_logout_session_required: true,
+        allow_remember_consent: true,
+        client_claims_prefix: 'client_',
+        protocol_type: 'oidc',
+        consent_lifetime: 3600
+      },
+      {
+        client_id: 'island-is-client-cred-1',
+        require_client_secret: false,
+        require_pkce: false,
+        enable_local_login: true,
+        require_pkce: true,
+        allow_offline_access: true,
+
+        //defaults
+        identity_token_lifetime: 300,
+        access_token_lifetime: 3600,
+        authorization_code_lifetime: 300,
+        absolute_refresh_token_lifetime: 2592000,
+        sliding_refresh_token_lifetime: 1296000,
+        refresh_token_usage: 1,
+        update_access_token_claims_on_refresh: true,
+        refresh_token_expiration: 0,
+        access_token_type: 0,
+        include_jwt_id: true,
+        always_send_client_claims: false,
+        device_code_lifetime: 300,
+        always_include_user_claims_in_id_token: false,
+        back_channel_logout_session_required: true,
+        enabled: true,
+        require_consent: false,
+        allow_plain_text_pkce: false,
+        allow_access_token_via_browser: true,
+        front_channel_logout_session_required: true,
+        allow_remember_consent: true,
+        client_claims_prefix: 'client_',
+        protocol_type: 'oidc'
       }
     ];
 
@@ -59,31 +124,38 @@ module.exports = {
         client_id: 'postman',
         scope_name: 'postman_resource.scope'
       },
-    ];
-
-    var cors = [
       {
-        client_id: 'postman',
-        origin: 'localhost'
-      }
-    ];
-
-    var clientIdbRestrictions = [
-      {
-        name: "islykill",
-        client_id: "postman",
+        client_id: 'island-is-1',
+        scope_name: 'openid'
       },
       {
-        name: "dockobit",
-        client_id: "postman",
+        client_id: 'island-is-1',
+        scope_name: 'profile'
+      },
+      {
+        client_id: 'island-is-1',
+        scope_name: 'offline_access'
+      },
+      {
+        client_id: 'island-is-client-cred-1',
+        scope_name: 'api'
       }
     ];
+
+    var cors = [];
+
+    // var clientIdbRestrictions = [
+    //   {
+    //     name: "islykill",
+    //     client_id: "postman",
+    //   },
+    //   {
+    //     name: "dockobit",
+    //     client_id: "postman",
+    //   }
+    // ];
 
     var redirectUri = [
-      {
-        client_id: 'postman',
-        redirect_uri: 'https://localhost:5001/signin-adfs'
-      },
       {
         client_id: 'postman',
         redirect_uri: 'https://postman'
@@ -92,45 +164,52 @@ module.exports = {
         client_id: 'postman',
         redirect_uri: 'https://oauth.pstmn.io/v1/callback'
       },
+      {
+        client_id: 'island-is-1',
+        redirect_uri: 'http://localhost:4200/signin-oidc'
+      },
+      {
+        client_id: 'island-is-1',
+        redirect_uri: 'https://localhost:4200/signin-oidc'
+      }
     ];
 
     var secrets = [{
-      client_id: 'postman',
-      value: 'postman-secret',
-      description: "secret for postman",
+      client_id: 'island-is-client-cred-1',
+      value: 'secret',
+      description: "secret for island-is-client-cred-1",
       expiration: new Date(),
-      type: "Mobile app"
-    },
-    {
-      client_id: 'postman',
-      value: 'postman-secret-2',
-      description: "second secret for postman",
-      expiration: new Date(),
-      type: "Native app"
-    },
+      type: "General"
+    }
     ];
 
-    var postRedirects = [{
-      client_id: "postman",
-      redirect_uri: "localhost:8080"
+    var postRedirects = null;
 
-    }];
-
-    var clientGrantTypes = [{
+    var clientGrantTypes = [
+    {
       client_id: 'postman',
       grant_type: 'authorization_code',
-    }];
+    },
+    {
+      client_id: 'island-is-1',
+      grant_type: 'authorization_code',
+    },
+    {
+      client_id: 'island-is-client-cred-1',
+      grant_type: 'client_credentials'
+    }
+  ];
 
 
     return new Promise((resolve, reject) => {
       queryInterface.bulkInsert('client', clients, {}).then(result => {
         Promise.all([
           queryInterface.bulkInsert('client_allowed_scope', scopes, {}),
-          queryInterface.bulkInsert('client_allowed_cors_origin', cors, {}),
-          queryInterface.bulkInsert('client_idp_restrictions', clientIdbRestrictions, {}),
+        //  queryInterface.bulkInsert('client_allowed_cors_origin', cors, {}),
+        //  queryInterface.bulkInsert('client_idp_restrictions', clientIdbRestrictions, {}),
           queryInterface.bulkInsert('client_redirect_uri', redirectUri, {}),
           queryInterface.bulkInsert('client_secret', secrets, {}),
-          queryInterface.bulkInsert('client_post_logout_redirect_uri', postRedirects, {}),
+        //  queryInterface.bulkInsert('client_post_logout_redirect_uri', postRedirects, {}),
           queryInterface.bulkInsert('client_grant_type', clientGrantTypes, {})
         ]).then(result => {
           resolve("done");
