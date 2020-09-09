282838cd00211d33c0fd5ee8e7f04a221b9532f1
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2020-09-09 11:58:01 +0000

    rename to referance-next-app

--- apps/referance-next-app/jest.config.js
+++ apps/referance-next-app/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'test-next',
+  name: 'referance-next-app',
   preset: '../../jest.config.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/test-next',
+  coverageDirectory: '../../coverage/apps/referance-next-app',
 }
