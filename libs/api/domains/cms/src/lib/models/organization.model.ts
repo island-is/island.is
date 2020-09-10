25ebeed9d6b5647cef4b2d619c2a689a7b20739b
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-09-10 18:16:35 +0000

    Update organization model

--- libs/api/domains/cms/src/lib/models/organization.model.ts
+++ libs/api/domains/cms/src/lib/models/organization.model.ts
@@ -1,17 +1,30 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, ObjectType, ID } from '@nestjs/graphql'
 
 import { IOrganization } from '../generated/contentfulTypes'
 
+import { OrganizationTag, mapOrganizationTag } from './organizationTag.model'
+
 @ObjectType()
 export class Organization {
+  @Field(() => ID)
+  id: string
+
   @Field()
   title: string
 
   @Field()
   slug: string
+
+  @Field(() => [OrganizationTag])
+  tag?: Array<OrganizationTag>
 }
 
-export const mapOrganization = ({ fields }: IOrganization): Organization => ({
+export const mapOrganization = ({
+  fields,
+  sys,
+}: IOrganization): Organization => ({
+  id: sys.id,
   title: fields.title,
   slug: fields.slug,
+  tag: fields.tag && fields.tag.map(mapOrganizationTag),
 })
