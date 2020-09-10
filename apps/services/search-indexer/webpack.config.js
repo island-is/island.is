27997c858faaab7d7a15c626cf94f3f797941341
Stefán Harald <stefanhar@users.noreply.github.com>
2020-09-10 15:43:22 +0000

    Feature/migrate tweeks (#741)
    * added migrate tweaks
    
    * Breaking upp migrate function
    
    * seperated migrate into libraries
    
    * Create packages working
    
    * Made migrate work without aws
    
    * Added comments
    
    * added logs
    
    * Added get all packages
    
    * elastic wip
    
    * made delete packages check work
    
    * Updated logger case
    
    * Elastic first version
    
    * Elastic working
    
    * Updated variable name
    
    * Added elastic cleanup
    
    * Added AWS cleanup
    
    * Added aws cleanup
    
    * Fixed format
    
    * Removed comments
    
    * fixed language
    
    * Added migrate as a command
    
    * Added readme
    
    * Fixed commetns
    
    * Fixed recommented
    
    * added error handler back

--- apps/services/search-indexer/webpack.config.js
+++ apps/services/search-indexer/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = (config, context) => {
-  config.entry.migrate = config.entry.main[0].replace('main.ts', 'migrate.ts')
+  config.entry.migrate = config.entry.main[0].replace('main.ts', 'migrate')
   config.output.filename = '[name].js'
   return {
     ...config,
