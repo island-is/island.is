07b6c070ec4c334c3b42ee835d6e8133928ecc24
Petar Shomov <petar@andes.is>
2020-09-10 19:45:55 +0000

    avoiding busting the docker layer cache in distributed env

--- scripts/prepare_deps.sh
+++ scripts/prepare_deps.sh
@@ -5,14 +5,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/_common.sh
 
-# Build the node_modules Docker layer and store it in the shared cache so it can be reused later
+# Build the node_modules as well as the base image for the final outputs and store it in the shared cache so it can be reused later
 
 mkdir -p $PROJECT_ROOT/cache
-docker buildx create --driver docker-container --use
+docker buildx create --driver docker-container --use || true
 exec docker buildx build \
   --platform=linux/amd64 \
   --cache-from=type=local,src=$PROJECT_ROOT/cache \
   --cache-to=type=local,dest=$PROJECT_ROOT/cache \
   -f ${DIR}/Dockerfile \
-  --target=deps \
+  --target=output-base \
   $PROJECT_ROOT
