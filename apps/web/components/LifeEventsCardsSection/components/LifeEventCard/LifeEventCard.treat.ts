febb2acd16ba744c63e0e52d0913eabc7410168f
David Bachmann <dabbi@dabbi.is>
2020-09-11 14:25:11 +0000

    Use a shadow on LifeEventsCards on mobile

--- apps/web/components/LifeEventsCardsSection/components/LifeEventCard/LifeEventCard.treat.ts
+++ apps/web/components/LifeEventsCardsSection/components/LifeEventCard/LifeEventCard.treat.ts
@@ -18,21 +18,23 @@ export const thumbnail = style({
 
 export const card = style({
   position: 'relative',
+  borderRadius: theme.border.radius.large,
+  boxShadow: `0px 4px 30px ${theme.color.blue100}`,
 
   '@media': {
     [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
-      borderRadius: theme.border.radius.large,
+      boxShadow: 'none',
     },
   },
 })
 
 export const image = style({
-  backgroundSize: 'contain',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
-  width: '100%',
+  backgroundSize: 'contain',
   flex: 'none',
   paddingBottom: '100%',
+  width: '100%',
 
   '@media': {
     [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
--- apps/web/components/LifeEventsCardsSection/components/LifeEventCard/LifeEventCard.tsx
+++ apps/web/components/LifeEventsCardsSection/components/LifeEventCard/LifeEventCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import * as styles from './LifeEventCard.treat'
 import { Box, Typography, Link, FocusableBox } from '@island.is/island-ui/core'
+import * as styles from './LifeEventCard.treat'
 
 interface Props {
   title: string
@@ -17,6 +17,7 @@ const LifeEventCard: React.FC<Props> = ({ title, intro, image, url }) => {
       borderColor="blue200"
       borderWidth="standard"
       height="full"
+      borderRadius="large"
     >
       <Box
         className={styles.card}
@@ -25,7 +26,7 @@ const LifeEventCard: React.FC<Props> = ({ title, intro, image, url }) => {
         display="flex"
         flexDirection={['column', 'column', 'row']}
         height="full"
-        paddingX={[2, 2, 4]}
+        paddingX={[3, 3, 4]}
         paddingY={[1, 1, 3]}
         overflow="hidden"
       >
@@ -38,6 +39,7 @@ const LifeEventCard: React.FC<Props> = ({ title, intro, image, url }) => {
           flexDirection="column"
           className={styles.content}
           marginRight={[2, 2, 2, 2, 1]}
+          marginTop={[3, 3, 0]}
         >
           <Typography variant="h3" as="h3" paddingBottom={1} color="blue400">
             {title}
