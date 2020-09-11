144b99146ecabf40c173851dd88152489bcd9490
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-09-11 15:44:18 +0000

    english path

--- apps/web/pages/stofnanir/[slug]/index.ts
+++
@@ -1,4 +0,0 @@
-import { withLocale } from '@island.is/web/i18n'
-import organizationScreen from '@island.is/web/screens/Organization/Organization'
-
-export default withLocale('is')(organizationScreen)
