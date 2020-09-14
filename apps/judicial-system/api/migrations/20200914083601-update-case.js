8c817863d50e1a0e310be4480999ffa29b714418
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-09-14 18:16:28 +0000

    Fixes formatting

--- apps/judicial-system/api/migrations/20200914083601-update-case.js
+++ apps/judicial-system/api/migrations/20200914083601-update-case.js
@@ -12,8 +12,8 @@ module.exports = {
         allowNull: true,
       }),
       queryInterface.sequelize.query(
-        'CREATE TYPE "enum_case_custody_provisions" AS ENUM (\'_95_1_A\', \'_95_1_B\', \'_95_1_C\', \'_95_1_D\', \'_95_2\', \'_99_1_B\');\
-        ALTER TABLE "case" ADD COLUMN "custody_provisions" "enum_case_custody_provisions"[];',
+        "CREATE TYPE \"enum_case_custody_provisions\" AS ENUM ('_95_1_A', '_95_1_B', '_95_1_C', '_95_1_D', '_95_2', '_99_1_B');\
+        ALTER TABLE \"case\" ADD COLUMN \"custody_provisions\" \"enum_case_custody_provisions\"[];",
       ),
       queryInterface.sequelize.query(
         'CREATE TYPE "enum_case_custody_restrictions" AS ENUM (\'ISOLATION\', \'VISITAION\', \'COMMUNICATION\', \'MEDIA\');\
@@ -46,16 +46,20 @@ module.exports = {
     return Promise.all([
       queryInterface.removeColumn('case', 'requested_custody_end_date'),
       queryInterface.removeColumn('case', 'laws_broken'),
-      queryInterface.removeColumn('case', 'custody_provisions').then(() =>
-        queryInterface.sequelize.query(
-          'DROP TYPE IF EXISTS "enum_case_custody_provisions";',
+      queryInterface
+        .removeColumn('case', 'custody_provisions')
+        .then(() =>
+          queryInterface.sequelize.query(
+            'DROP TYPE IF EXISTS "enum_case_custody_provisions";',
+          ),
         ),
-      ),
-      queryInterface.removeColumn('case', 'custody_restrictions').then(() =>
-        queryInterface.sequelize.query(
-          'DROP TYPE IF EXISTS "enum_case_custody_restrictions";',
+      queryInterface
+        .removeColumn('case', 'custody_restrictions')
+        .then(() =>
+          queryInterface.sequelize.query(
+            'DROP TYPE IF EXISTS "enum_case_custody_restrictions";',
+          ),
         ),
-      ),
       queryInterface.removeColumn('case', 'case_facts'),
       queryInterface.removeColumn('case', 'witness_accounts'),
       queryInterface.removeColumn('case', 'investigation_progress'),
