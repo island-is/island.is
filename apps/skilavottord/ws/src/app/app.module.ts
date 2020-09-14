a5ccd66dc870456ebe2a4dfb0ec6ce69c199f422
Sigurgeir Guðmundsson <sigurgeirg@localhost.localdomain>
2020-09-14 14:56:36 +0000

    data structure created and added hardcoded data

--- apps/skilavottord/ws/src/app/app.module.ts
+++ apps/skilavottord/ws/src/app/app.module.ts
@@ -1,33 +1,6 @@
-/*import { Module } from '@nestjs/common'
-
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { GraphQLModule } from '@nestjs/graphql'
-//import { join } from 'path'
-import { ItemModule } from '../item/item.module'
-
-@Module({
-  imports: [
-    GraphQLModule.forRoot({
-      definitions: {
-        path: 'apps/skilavottord/ws/src/graphql.schema.d.ts',
-        outputAs: 'class',
-      },*/
-//      typePaths: ['../**/*.graphql'],
-/*      resolverValidationOptions: {
-        requireResolversForResolveType: false,
-      },
-    }),
-    ItemModule,
-  ],
-})*/
-
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-//import { CmsModule } from '@island.is/api/domains/cms'
 
-//import { AuthModule, UserModule, DiscountModule, FlightModule } from './modules'
-//import { BackendAPI } from '../services'
 import { UserModule, CarModule } from './modules'
 
 const debug = process.env.NODE_ENV === 'development'
@@ -39,8 +12,6 @@ const playground = debug || process.env.GQL_PLAYGROUND_ENABLED === 'true'
       debug,
       playground,
       autoSchemaFile: 'apps/skilavottord/ws/src/app/api.graphql',
-      //     path: '/api/graphql',
-      //     context: ({ req }) => ({ req })
     }),
     UserModule,
     CarModule,
--- apps/skilavottord/ws/src/app/modules/user/models/user.service.ts
+++ apps/skilavottord/ws/src/app/modules/user/models/user.service.ts
@@ -1,14 +1,36 @@
 import { User } from '.'
+import { Car } from '../../car'
 
 export class UserService {
   userList: User[]
 
+  carList: Car[]
+
   constructor() {
     this.userList = []
-    this.userList.push(new User('1111111111', 'Ringo', '1111111'))
-    this.userList.push(new User('2222222222', 'Lennon', '2222222'))
-    this.userList.push(new User('3333333333', 'McCartney', '3333333'))
-    this.userList.push(new User('4444444444', 'Harrison', '4444444'))
+    this.carList = []
+    this.carList.push(new Car('Jkd-912', 'Nissan', 'Arya', 'white'))
+    this.userList.push(
+      new User('1111111111', 'Óðinn Jónsson', '1111111', this.carList),
+    )
+    this.carList = []
+    this.carList.push(new Car('Xll-732', 'Nissan', 'Leaf', 'blue'))
+    this.carList.push(new Car('Dtx-429', 'Tesla', 'model-2', 'red'))
+    this.userList.push(
+      new User('2222222222', 'Þór Gunnarsson', '2222222', this.carList),
+    )
+    this.carList = []
+    this.carList.push(new Car('Dty-432', 'Volo', 'v-60', 'white'))
+    this.carList.push(new Car('Hjm-549', 'Suzuki', 'jimny', 'blue'))
+    this.userList.push(
+      new User('3333333333', 'Freyja Jónsdóttir', '3333333', this.carList),
+    )
+    this.carList = []
+    this.carList.push(new Car('Ujn-881', 'Bmw', 'i-8', 'black'))
+    this.carList.push(new Car('Ukb-141', 'Benz', 'G-63', 'black'))
+    this.userList.push(
+      new User('4444444444', 'Loki Sigurðdsson', '4444444', this.carList),
+    )
   }
 
   getUserList(): User[] {
