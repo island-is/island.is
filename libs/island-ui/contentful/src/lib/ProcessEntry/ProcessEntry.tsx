f58783234510d0563fe9ceeebf10235b3f5de128
Magnus Skulason <maggisk@gmail.com>
2020-09-15 22:39:08 +0000

    simpler updated design for process entry component

--- libs/island-ui/contentful/src/lib/ProcessEntry/ProcessEntry.tsx
+++ libs/island-ui/contentful/src/lib/ProcessEntry/ProcessEntry.tsx
@@ -1,118 +1,68 @@
-import React, { FC, ReactNode } from 'react'
-import slugify from '@sindresorhus/slugify'
-import BorderedContent from '../BorderedContent/BorderedContent'
+import React, { FC } from 'react'
 import {
   Typography,
   Button,
   Box,
-  Stack,
   GridRow,
   GridColumn,
+  Stack,
 } from '@island.is/island-ui/core'
-import { Html } from '@island.is/api/schema'
-import { renderHtml } from '../richTextRendering'
+import * as styles from './ProcessEntry.treat'
 
-const Content: React.FC = ({ children }) => (
-  <GridRow>
-    <GridColumn span={['8/8', '8/8', '6/8']} offset={['0', '0', '1/8']}>
-      {children}
-    </GridColumn>
-  </GridRow>
-)
-
-export const ProcessTypes = {
-  Digital: {
-    icon: 'external',
-    title: 'Stafræn umsókn',
-  },
-  'Digital w/login': {
-    icon: 'external',
-    title: 'Aðgangsstýrð stafræn umsókn',
-  },
-  'Not digital': {
-    icon: 'info',
-    title: 'Handvirk umsókn',
-  },
-  'Not digital w/login': {
-    icon: 'external',
-    title: 'Handvirk umsókn með innskráningu',
-  },
-  'No type': {
-    icon: 'external',
-    title: '',
-  },
+export const Titles: { [k: string]: string } = {
+  // TODO: translating these requires some effort
+  Digital: 'Stafræn umsókn',
+  'Digital w/login': 'Aðgangsstýrð stafræn umsókn',
+  'Not digital': 'Handvirk umsókn',
+  'Not digital w/login': 'Handvirk umsókn með innskráningu',
+  'No type': '',
 }
 
 interface ProcessEntryProps {
   processTitle: string
-  processInfo?: Html
   processLink: string
-  buttonText: string
   type: string
-  title: string
-  subtitle?: string
-  details?: Html
+  buttonText: string
 }
 
 export const ProcessEntry: FC<ProcessEntryProps> = ({
   processTitle,
-  processInfo,
   processLink,
-  buttonText,
   type,
-  title,
-  subtitle,
-  details,
+  buttonText,
 }) => {
+  const eyebrow = Titles[type]
+
   return (
-    <BorderedContent
-      topContent={
-        // top part should not be visible unless there's text content (details) to show
-        Boolean(details) && (
-          <Content>
-            <Stack space={[2, 2]}>
-              {title && (
-                <Typography variant="h2" as="h3">
-                  {title}
+    <Box background="blue100" className={styles.container}>
+      <GridRow>
+        <GridColumn span={['9/9', '9/9', '7/9']} offset={['0', '0', '1/9']}>
+          <Box display="flex" flexDirection={['column', 'column', 'row']}>
+            <Box flexGrow={1}>
+              <Stack space={1}>
+                {Boolean(eyebrow) && (
+                  <Typography variant="eyebrow" as="p" color="blue400">
+                    {eyebrow}
+                  </Typography>
+                )}
+                <Typography variant="h3" as="h3">
+                  {processTitle}
                 </Typography>
-              )}
-              {subtitle && (
-                <Typography variant="intro" as="p">
-                  {subtitle}
-                </Typography>
-              )}
-              {renderHtml(details.document)}
-            </Stack>
-          </Content>
-        )
-      }
-      bottomContent={
-        <Content>
-          <Stack space={[2, 2]}>
-            {type !== 'No type' && (
-              <Typography variant="eyebrow" as="h4" color="blue400">
-                {ProcessTypes[type].title}
-              </Typography>
-            )}
-            {processTitle && (
-              <Typography variant="h3" as="h3">
-                {processTitle}
-              </Typography>
-            )}
-            {processInfo && renderHtml(processInfo.document)}
-            <Box paddingTop={[1, 1, 2]}>
-              <Button
-                width="fixed"
-                href={processLink}
-                icon={ProcessTypes[type].icon}
-              >
+              </Stack>
+            </Box>
+            <Box
+              flexShrink={0}
+              paddingTop={[3, 3, 0]}
+              paddingLeft={[0, 0, 4, 8]}
+            >
+              <Button noWrap href={processLink} icon="external">
                 {buttonText}
               </Button>
             </Box>
-          </Stack>
-        </Content>
-      }
-    />
+          </Box>
+        </GridColumn>
+      </GridRow>
+    </Box>
   )
 }
 
