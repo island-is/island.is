60b91cf863a5f4d103cabf9dc587d8b7f1cf8e89
Petar Shomov <petar@andes.is>
2020-09-15 10:03:32 +0000

    changing cache building order a bit

--- scripts/prepare_deps.sh
+++ scripts/prepare_deps.sh
@@ -9,16 +9,18 @@ source $DIR/_common.sh
 
 mkdir -p $PROJECT_ROOT/cache
 docker buildx create --driver docker-container --use || true
+
 docker buildx build \
   --platform=linux/amd64 \
-  --cache-to=type=local,dest=$PROJECT_ROOT/cache \
-  -f ${DIR}/Dockerfile \
-  --target=deps \
-  $PROJECT_ROOT
-docker buildx build \
-  --platform=linux/amd64 \
-  --cache-from=type=local,src=$PROJECT_ROOT/cache \
   --cache-to=type=local,dest=$PROJECT_ROOT/cache_output \
   -f ${DIR}/Dockerfile \
   --target=output-base \
+  $PROJECT_ROOT
+
+docker buildx build \
+  --platform=linux/amd64 \
+  --cache-from=type=local,src=$PROJECT_ROOT/cache_output \
+  --cache-to=type=local,dest=$PROJECT_ROOT/cache \
+  -f ${DIR}/Dockerfile \
+  --target=deps \
   $PROJECT_ROOT
