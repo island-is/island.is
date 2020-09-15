0eb2ffa06973edc9ec5328f94f739da762f023d3
Magnus Skulason <maggisk@gmail.com>
2020-09-15 13:02:59 +0000

    plausible integration

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { defaultLanguage } from '../i18n/I18n'
 
+const getDomain = (host: string): string => {
+  if (typeof host === 'string') {
+    host = host.split(':')[0]
+    if (host.match(/\.?island\.is$/)) {
+      return host.replace(/^www\./, '')
+    }
+  }
+
+  return ''
+}
+
 interface Props {
   lang: string
+  domain: string
 }
 
 class MyDocument extends Document<Props> {
@@ -13,6 +25,7 @@ class MyDocument extends Document<Props> {
     // TODO: Find a more natural way of retrieving the locale here
     let lang = defaultLanguage
 
+    const domain = getDomain(ctx.req.get('host'))
     const locale = ctx.req.url.substring(1, 3)
     const suffix = ctx.req.url.substring(3, 4)
 
@@ -20,15 +33,24 @@ class MyDocument extends Document<Props> {
       lang = locale || lang
     }
 
-    return { ...initialProps, lang }
+    return { ...initialProps, lang, domain }
   }
 
   render() {
-    const { lang } = this.props
+    const { lang, domain } = this.props
 
     return (
       <Html lang={lang}>
-        <Head />
+        <Head>
+          {Boolean(domain) && (
+            <script
+              async
+              defer
+              data-domain={domain}
+              src="https://plausible.io/js/plausible.js"
+            ></script>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
