764170a8f686edf50d6b8ab433d91757de520580
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-09-16 16:55:14 +0000

    Run retag in parallel

--- scripts/retag-unaffected.sh
+++ scripts/retag-unaffected.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # This script re-tags all unaffected Docker images with the newest tag.
 export TOKEN=$(aws ecr get-authorization-token --region eu-west-1 --output text --query 'authorizationData[].authorizationToken')
-echo $1 | tr ' ' '\n' | xargs -I {} bash -c "IMAGE={} $DIR/_retag.sh || exit 255" # exit code 255 makes xargs fail fast
+echo $1 | tr ' ' '\n' | xargs -P 8 -n 1 -I {} bash -c "IMAGE={} $DIR/_retag.sh || exit 255" # exit code 255 makes xargs fail fast
