cdb5c3e921f370a8514ef4ee4d3b4265a088e58a
Noam <noam@aranja.com>
2020-09-16 14:05:24 +0000

    Refactor column layout

--- libs/application/form/src/components/FormMultiField.tsx
+++ libs/application/form/src/components/FormMultiField.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react'
 import FormField from './FormField'
 import { MultiFieldScreen } from '../types'
-import { Box } from '@island.is/island-ui/core'
+import { Box, GridColumn, GridRow } from '@island.is/island-ui/core'
 import { FormValue } from '@island.is/application/template'
 import ConditionHandler from './ConditionHandler'
+import { theme } from '@island.is/island-ui/theme'
 
 const FormMultiField: FC<{
   applicationId: string
@@ -13,36 +14,30 @@ const FormMultiField: FC<{
   answerQuestions(Answers): void
 }> = ({ applicationId, answerQuestions, errors, formValue, multiField }) => {
   return (
-    <Box display="flex" flexWrap="wrap">
+    <GridRow>
       <ConditionHandler
         answerQuestions={answerQuestions}
         formValue={formValue}
         screen={multiField}
       />
-      {multiField.children.map((field, index) => (
-        <Box
-          key={field.id}
-          id={`${field.id}-${field.width}`}
-          paddingTop={2}
-          style={{
-            // TODO improve this
-            width: field.width === 'half' ? '45%' : '100%',
-            alignContent: 'space-between',
-            justifyContent: 'space-between',
-          }}
-        >
-          <FormField
-            applicationId={applicationId}
-            showFieldName
-            field={field}
-            key={field.id}
-            autoFocus={index === 0}
-            errors={errors}
-            formValue={formValue}
-          />
-        </Box>
-      ))}
-    </Box>
+      {multiField.children.map((field, index) => {
+        const isHalfColumn = field.width && field.width === 'half'
+        const span = isHalfColumn ? '1/2' : '1/1'
+        return (
+          <GridColumn key={field.id} span={['1/1', '1/1', span]}>
+            <FormField
+              applicationId={applicationId}
+              showFieldName
+              field={field}
+              key={field.id}
+              autoFocus={index === 0}
+              errors={errors}
+              formValue={formValue}
+            />
+          </GridColumn>
+        )
+      })}
+    </GridRow>
   )
 }
 
