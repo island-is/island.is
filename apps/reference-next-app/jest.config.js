4e5082d11d2322996a661cf15b313d739be8469a
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2020-09-16 14:35:08 +0000

    fix typo in directory's name

--- apps/reference-next-app/jest.config.js
+++ apps/reference-next-app/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'referance-next-app',
+  name: 'reference-next-app',
   preset: '../../jest.config.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/referance-next-app',
+  coverageDirectory: '../../coverage/apps/reference-next-app',
 }
