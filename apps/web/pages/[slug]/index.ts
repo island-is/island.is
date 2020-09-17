e532f1559aaebc628b61477b2c4f87a4a91689e4
Stefán Harald <stefanharald@gmail.com>
2020-09-17 18:59:40 +0000

    added files

---
+++ apps/web/pages/[slug]/index.ts
@@ -0,0 +1,13 @@
+import getConfig from 'next/config'
+import withApollo from '@island.is/web/graphql/withApollo'
+import { withLocale } from '@island.is/web/i18n'
+import articleScreen from '@island.is/web/screens/Article'
+import { withHealthchecks } from '@island.is/web/units/Healthchecks/withHealthchecks'
+
+const { serverRuntimeConfig } = getConfig()
+const { graphqlUrl } = serverRuntimeConfig	
+const externalEndpointDependencies = [graphqlUrl]	
+
+export default withHealthchecks(externalEndpointDependencies)(	
+  withApollo(withLocale('is')(articleScreen)),	
+)
