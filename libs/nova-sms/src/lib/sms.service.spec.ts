e02a6ebcb77ba426cbf14cc45d337244803cbb70
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-09-17 00:10:14 +0000

    Tests nova sms

--- libs/nova-sms/src/lib/sms.service.spec.ts
+++ libs/nova-sms/src/lib/sms.service.spec.ts
@@ -2,14 +2,49 @@ import { mock } from 'jest-mock-extended'
 
 import { Test, TestingModule } from '@nestjs/testing'
 
-import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { Logger, LOGGER_PROVIDER, logger } from '@island.is/logging'
 
 import { SmsService } from './sms.service'
 
+const testLogin = 'Login'
+const testToken = 'Test Token'
+const testSendSms = 'SendSms'
+const testCode = '0'
+const postMock = jest.fn(function(
+  path: string,
+  boddy: Body,
+  init: RequestInit,
+) {
+  switch (path) {
+    case testLogin:
+      return { Token: testToken }
+    case testSendSms:
+      return { Code: testCode }
+  }
+})
+jest.mock('apollo-datasource-rest', function() {
+  return {
+    RESTDataSource: function() {
+      this.post = postMock
+      return this
+    },
+  }
+})
+
+const testOptions = {
+  url: 'Test Url',
+  username: 'Test User',
+  password: 'Test Password',
+}
+const testNumber = '1111111'
+const testMessage = 'Test Message'
+
 describe('SmsService', () => {
-  let service: SmsService
+  let smsService: SmsService
 
   beforeEach(async () => {
+    postMock.mockClear()
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
@@ -18,16 +53,45 @@ describe('SmsService', () => {
         },
         {
           provide: 'SMS_OPTIONS',
-          useValue: {},
+          useValue: testOptions,
         },
         SmsService,
       ],
     }).compile()
 
-    service = module.get<SmsService>(SmsService)
+    smsService = module.get<SmsService>(SmsService)
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  it('should send sms', async () => {
+    const res = await smsService.sendSms(testNumber, testMessage)
+
+    // Verify response
+    expect(res.Code).toBe('0')
+
+    // Verify login
+    expect(postMock).toHaveBeenCalledWith(testLogin, undefined, {
+      headers: {
+        username: testOptions.username,
+        password: testOptions.password,
+      },
+    })
+
+    // Verfy send sms
+    expect(postMock).toHaveBeenCalledWith(
+      testSendSms,
+      {
+        request: {
+          Recipients: [testNumber],
+          SenderName: 'Island.is',
+          SmsText: testMessage,
+          IsFlash: false,
+        },
+      },
+      {
+        headers: {
+          token: testToken,
+        },
+      },
+    )
   })
 })
