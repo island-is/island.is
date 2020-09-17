0147a1d9e5ebeb15748cd5aeaa18526327f9b2b3
Noam <noam@aranja.com>
2020-09-17 15:24:51 +0000

    Update DividerFormField.tsx

--- libs/application/form/src/components/fields/DividerFormField.tsx
+++ libs/application/form/src/components/fields/DividerFormField.tsx
@@ -1,16 +1,16 @@
 import React, { FC } from 'react'
 import { DividerField } from '@island.is/application/template'
-import { Divider, Typography } from '@island.is/island-ui/core'
+import { Box, Divider, Typography } from '@island.is/island-ui/core'
 
 const DividerFormField: FC<{
   field: DividerField
 }> = ({ field }) => {
   return (
-    <>
-      <Divider />
-      {field.name}
-      <Divider />
-    </>
+    <Box marginTop={5} marginBottom={1}>
+      <Typography variant="h3" color="blue400">
+        {field.name}
+      </Typography>
+    </Box>
   )
 }
 
