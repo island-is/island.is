e37b4d2f8d720c32393e50b096280950e8924b2e
unnarb <unnarb@gmail.com>
2020-09-17 10:53:59 +0000

    removed commented out code

--- apps/auth-admin-api/src/main.ts
+++ apps/auth-admin-api/src/main.ts
@@ -1,26 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-// import { Logger } from '@nestjs/common'
-// import { NestFactory } from '@nestjs/core'
-
-// import { AppModule } from './app/app.module'
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule)
-//   const globalPrefix = 'api'
-//   app.setGlobalPrefix(globalPrefix)
-//   const port = process.env.PORT || 3333
-//   await app.listen(port, () => {
-//     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix)
-//   })
-// }
-
-// bootstrap()
-
-
 import '@island.is/infra-tracing'
 import { DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app/app.module'
