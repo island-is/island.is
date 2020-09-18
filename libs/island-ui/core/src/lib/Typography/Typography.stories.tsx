073b1c86d93a9c732312b2f549ce2ffe0dbd7edc
David Bachmann <dabbi@dabbi.is>
2020-09-18 15:18:03 +0000

    Include a 'TextLayout' story to drive home some points

--- libs/island-ui/core/src/lib/Typography/Typography.stories.tsx
+++ libs/island-ui/core/src/lib/Typography/Typography.stories.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Typography } from './Typography'
+import { Box } from '../Box'
 import { select } from '@storybook/addon-knobs'
 import { Colors } from '@island.is/island-ui/theme'
 import { VariantTypes } from './Typography.treat'
+import { GridColumn, GridContainer, GridRow } from '../Grid'
+import { ContentBlock } from '../ContentBlock'
 
 export default {
   title: 'Components/Typography',
@@ -107,3 +110,38 @@ export const Spacing = () =>
   makeSpacingComponent(
     'Typography is a layout component and can control vertical spacing',
   )
+
+export const TextLayout = () => (
+  <Box paddingY={2}>
+    <GridContainer>
+      <GridRow>
+        <GridColumn span="10/12">
+          <Typography as="h1" variant="h1" paddingBottom={2}>
+            Typical text layout
+          </Typography>
+          <Typography as="p" variant="intro">
+            Control vertical rhythm using Typography's <em>paddingY</em>,{' '}
+            <em>paddingTop</em>, <em>paddingBottom</em>.
+          </Typography>
+          <Typography as="p" variant="intro" paddingBottom={6}>
+            Don't rely on the <strong>Stack</strong> component for vertical
+            spacing.
+          </Typography>
+          <Typography as="h2" variant="h2" paddingBottom={2}>
+            Lorem ipsum time
+          </Typography>
+          <Typography as="p" variant="p" paddingBottom={2}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            minus doloremque quis minima adipisci sunt fugit unde officiis? Qui
+            et animi asperiores excepturi ut, amet consectetur dignissimos rerum
+            in fuga eum doloribus. Magnam quibusdam nisi earum culpa quo quidem
+            mollitia, quasi illum provident, molestias voluptate corrupti
+            consectetur autem, numquam sint labore. Delectus eius cupiditate
+            dolores tempora harum suscipit, animi explicabo. Corrupti maiores
+            officia architecto ratione animi eum sequi quos.
+          </Typography>
+        </GridColumn>
+      </GridRow>
+    </GridContainer>
+  </Box>
+)
