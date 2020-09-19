c88e92f590cce8a7471ded698e6087d9b5d14a44
Stefán Harald <stefanhar@users.noreply.github.com>
2020-09-19 17:40:43 +0000

    Removed empty life events (#926)
    * Removed empty life events
    
    * Fixed format

--- apps/web/components/LifeEventsCardsSection/LifeEventsCardsSection.tsx
+++ apps/web/components/LifeEventsCardsSection/LifeEventsCardsSection.tsx
@@ -48,15 +48,17 @@ export const LifeEventsCardsSection: React.FC<LifeEventsSectionProps> = ({
         </GridColumn>
       </GridRow>
       <GridRow>
-        {lifeEvents.map((lifeEvent) => (
-          <GridColumn
-            span={['12/12', '12/12', '6/12', '6/12', '4/12']}
-            paddingBottom={3}
-            key={lifeEvent.title}
-          >
-            {renderLifeEventCard(lifeEvent)}
-          </GridColumn>
-        ))}
+        {lifeEvents
+          .filter((lifeEvent) => lifeEvent.title && lifeEvent.slug) // life event can be empty in some locales
+          .map((lifeEvent) => (
+            <GridColumn
+              span={['12/12', '12/12', '6/12', '6/12', '4/12']}
+              paddingBottom={3}
+              key={lifeEvent.title}
+            >
+              {renderLifeEventCard(lifeEvent)}
+            </GridColumn>
+          ))}
       </GridRow>
     </GridContainer>
   )
