94ad6e5f4570759ea4bed9a2e2456bb3307f2435
Jonsson <tjonsson@deloitte.is>
2020-09-21 12:30:53 +0000

    environment copy from air-disc

--- apps/skilavottord/ws/src/environments/environment.ts
+++ apps/skilavottord/ws/src/environments/environment.ts
@@ -1,3 +1,12 @@
-export const environment = {
+export default {
   production: false,
+  sentry: {
+    dsn: process.env.SENTRY_DSN,
+  },
+  auth: {
+    samlEntryPoint: 'https://innskraning.island.is/?id=ads.local',
+    audience: 'localhost:4200',
+    jwtSecret: 'securesecret',
+  },
+  backendUrl: 'http://localhost:4248',
 }
