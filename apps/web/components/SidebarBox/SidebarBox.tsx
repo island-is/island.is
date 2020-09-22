6c4183bfe7d3ae7a2b81017e0a7475121201dc76
Magnus Skulason <maggisk@gmail.com>
2020-09-22 16:27:50 +0000

    remove unused imports

--- apps/web/components/SidebarBox/SidebarBox.tsx
+++ apps/web/components/SidebarBox/SidebarBox.tsx
@@ -1,11 +1,5 @@
 import React, { FC } from 'react'
-import {
-  Box,
-  BoxProps,
-  Stack,
-  Typography,
-  Divider,
-} from '@island.is/island-ui/core'
+import { Box, BoxProps } from '@island.is/island-ui/core'
 
 export const SidebarBox: FC<BoxProps> = ({ children, ...props }) => (
   <Box background="purple100" padding={4} borderRadius="large" {...props}>
