9f0ea504c493c6270a77fa2ae0d9149621d285c0
Kristján Albert <kal@kosmosogkaos.is>
2020-09-22 09:02:36 +0000

    More web fixes and tweaks (#973)
    * LinkCard (category links) now have hover color
    
    * Work on card image changes

--- apps/web/components/Card/Card.treat.ts
+++ apps/web/components/Card/Card.treat.ts
@@ -1,5 +1,5 @@
 import { style } from 'treat'
-import { theme } from '@island.is/island-ui/theme'
+import { theme, themeUtils } from '@island.is/island-ui/theme'
 
 export const card = style({
   display: 'flex',
@@ -22,41 +22,22 @@ export const cardContent = style({
 })
 
 export const cardContentNarrower = style({
-  width: '66%',
+  width: '60%',
+})
+
+export const image = style({
+  minWidth: '100%',
 })
 
 export const imageContainer = style({
-  position: 'absolute',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexDirection: 'column',
-  overflow: 'hidden',
-  width: '33%',
-  top: 0,
-  bottom: 0,
-  right: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center right',
-  backgroundRepeat: 'no-repeat',
-  ':after': {
-    content: '""',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    background: `linear-gradient(to right, rgba(255,255,255,1) 0%, rgba(255,255,255,0.9) 10%, rgba(255,255,255,0) 100%)`,
-  },
+  position: 'relative',
+  width: '40%',
+  flexGrow: 1,
 })
 
 export const imageContainerStacked = style({
   width: '100%',
-  height: '50%',
-  top: 'initial',
-  opacity: 0.25,
-  left: 0,
-  ':after': {
-    background: `radial-gradient(at bottom right, rgba(255,255,255,0) 30%, rgba(255,255,255,0.5) 60%, rgba(255,255,255,1) 100%)`,
-  },
+  height: 80,
+  marginTop: 16,
+  flexGrow: 1,
 })
