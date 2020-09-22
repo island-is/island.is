8f814749f3d57604132cca83953ac2888856037d
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-09-22 11:54:45 +0000

    Fix Swiper for iOS

--- libs/island-ui/core/src/lib/Swiper/Swiper.tsx
+++ libs/island-ui/core/src/lib/Swiper/Swiper.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import cn from 'classnames'
 import * as styles from './Swiper.treat'
-import { useMeasure } from 'react-use'
 
-const Swiper = ({ children }) => {
-  const [ref, { width }] = useMeasure()
+const Swiper: FC = ({ children }) => {
+  const [width, setWidth] = useState<number>(0)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (ref?.current) {
+      const w = ref?.current?.offsetWidth as number
+      setWidth(~~(w * 0.77))
+    }
+  }, [ref.current])
 
   return (
     <div className={styles.root}>
       <div className={cn(styles.container)} ref={ref}>
         <div className={styles.slides}>
           {React.Children.map(children, (child) => (
-            <div className={styles.slide} style={{ width: ~~(width * 0.77) }}>
+            <div className={styles.slide} style={{ width }}>
               {child}
             </div>
           ))}
