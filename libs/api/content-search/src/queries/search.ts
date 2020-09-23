ccbd11902ac962bc2522297fe26b35cae1e6c26b
Jérémy Barbet <jeremgraph@gmail.com>
2020-09-23 11:35:23 +0000

    chore: fix import + generate contentfulTypes

--- libs/api/content-search/src/queries/search.ts
+++ libs/api/content-search/src/queries/search.ts
@@ -1,4 +1,5 @@
-import { Tag } from '@island.is/api/schema'
+import { Tag } from '@island.is/api/domains/content-search'
+
 import { tagQuery } from './documentByMetaData'
 
 interface SearchInput {
