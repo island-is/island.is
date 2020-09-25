287919a6dbca49f95cdbac3ae8058a2a805cdfc9
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-09-25 09:54:19 +0000

    Changes some postgres column types from string to text (#1067)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/api/migrations/20200907181858-update-case.js
+++ apps/judicial-system/api/migrations/20200907181858-update-case.js
@@ -2,32 +2,60 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.addColumn('case', 'suspect_address', {
-        type: Sequelize.STRING,
-        allowNull: true,
-      }),
-      queryInterface.addColumn('case', 'court', {
-        type: Sequelize.STRING,
-        allowNull: true,
-      }),
-      queryInterface.addColumn('case', 'arrest_date', {
-        type: Sequelize.DATE,
-        allowNull: true,
-      }),
-      queryInterface.addColumn('case', 'requested_court_date', {
-        type: Sequelize.DATE,
-        allowNull: true,
-      }),
-    ])
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn(
+          'case',
+          'suspect_address',
+          {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+        queryInterface.addColumn(
+          'case',
+          'court',
+          {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+        queryInterface.addColumn(
+          'case',
+          'arrest_date',
+          {
+            type: Sequelize.DATE,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+        queryInterface.addColumn(
+          'case',
+          'requested_court_date',
+          {
+            type: Sequelize.DATE,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+      ])
+    })
   },
 
   down: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.removeColumn('case', 'suspect_address'),
-      queryInterface.removeColumn('case', 'court'),
-      queryInterface.removeColumn('case', 'arrest_date'),
-      queryInterface.removeColumn('case', 'requested_court_date'),
-    ])
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn('case', 'suspect_address', {
+          transaction: t,
+        }),
+        queryInterface.removeColumn('case', 'court', { transaction: t }),
+        queryInterface.removeColumn('case', 'arrest_date', { transaction: t }),
+        queryInterface.removeColumn('case', 'requested_court_date', {
+          transaction: t,
+        }),
+      ])
+    })
   },
 }
--- apps/judicial-system/api/migrations/20200922125905-update-case.js
+++ apps/judicial-system/api/migrations/20200922125905-update-case.js
@@ -2,13 +2,24 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('case', 'court_case_number', {
-      type: Sequelize.STRING,
-      allowNull: true,
+    return queryInterface.sequelize.transaction((t) => {
+      return queryInterface.addColumn(
+        'case',
+        'court_case_number',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction: t },
+      )
     })
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('case', 'court_case_number')
+    return queryInterface.sequelize.transaction((t) => {
+      return queryInterface.removeColumn('case', 'court_case_number', {
+        transaction: t,
+      })
+    })
   },
 }
