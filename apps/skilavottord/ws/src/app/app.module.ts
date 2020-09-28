d14b5d1f8330314eb46b25469675c9ca7672081c
dd-sdang <64913954+dd-sdang@users.noreply.github.com>
2020-09-28 18:21:22 +0200

    Added path to /api/graphql (#1094)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/skilavottord/ws/src/app/app.module.ts
+++ apps/skilavottord/ws/src/app/app.module.ts
@@ -13,6 +13,7 @@ const playground = debug || process.env.GQL_PLAYGROUND_ENABLED === 'true'
       debug,
       playground,
       autoSchemaFile: 'apps/skilavottord/ws/src/app/api.graphql',
+      path: '/api/graphql',
     }),
     CarownerModule,
   ],
