7cf84aa2a1dbdce50bf2933cc58cc2472d7000b7
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-09-28 17:49:24 +0000

    Fixes roles handling in client context

--- apps/judicial-system/web/src/utils/userContext.ts
+++ apps/judicial-system/web/src/utils/userContext.ts
@@ -2,7 +2,7 @@ import React from 'react'
 import { User } from '../types'
 
 const userContext = React.createContext<{ user: User }>({
-  user: { nationalId: '', role: [] },
+  user: { nationalId: '', role: '' },
 })
 
 export { userContext }
