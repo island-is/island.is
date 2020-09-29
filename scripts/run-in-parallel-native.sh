0e60c71848ffe5b0e503b22ee08974412674bc75
Petar Shomov <petar@andes.is>
2020-09-29 10:52:49 +0000

    trying to fix the parallelism issue with lint

---
+++ scripts/run-in-parallel-native.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/_common.sh
+MAX_JOBS=${MAX_JOBS:-2}
+
+AFFECTED_PROJECTS=`echo ${AFFECTED_PROJECTS} | tr -d '\n[:space:]'`
+echo "Running '$AFFECTED_PROJECTS' in parallel of ${MAX_JOBS} for target $1"
+
+if [ ! -z "$AFFECTED_PROJECTS" ]
+then
+  echo "Affected projects for target $1 are '$AFFECTED_PROJECTS'"
+  nx run-many --target=$1 --projects="$AFFECTED_PROJECTS" --parallel --maxParallel=$MAX_JOBS
+else
+  echo "No affected projects for target $1"
+fi
