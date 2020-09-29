b8d4e014d6997b9529fb69deefe10c1148b40985
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-09-29 22:24:03 +0000

    Removes unused import

--- apps/judicial-system/api/src/app/modules/case/case.controller.ts
+++ apps/judicial-system/api/src/app/modules/case/case.controller.ts
@@ -11,7 +11,6 @@ import {
   Req,
   ForbiddenException,
   Query,
-  Res,
   ConflictException,
 } from '@nestjs/common'
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
