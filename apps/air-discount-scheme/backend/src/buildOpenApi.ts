d8e1c9fefb6c0c1cb577f625968fcba59389abb6
Jérémy Barbet <jeremgraph@gmail.com>
2020-09-29 11:37:09 +0000

    fix: remove old TODOs

--- apps/air-discount-scheme/backend/src/buildOpenApi.ts
+++ apps/air-discount-scheme/backend/src/buildOpenApi.ts
@@ -3,7 +3,6 @@ import { buildOpenApi } from '@island.is/infra-nest-server'
 import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
 
-// TODO - redis
 buildOpenApi({
   path: 'apps/air-discount-scheme/backend/src/openapi.yaml',
   appModule: AppModule,
--- apps/judicial-system/api/src/buildOpenApi.ts
+++ apps/judicial-system/api/src/buildOpenApi.ts
@@ -3,7 +3,6 @@ import { buildOpenApi } from '@island.is/infra-nest-server'
 import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
 
-// TODO - redis
 buildOpenApi({
   path: 'apps/judicial-system/api/src/openapi.yaml',
   appModule: AppModule,
