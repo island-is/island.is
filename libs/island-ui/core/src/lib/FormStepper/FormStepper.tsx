bb8f7c3ff22bc93d8d60b0f04f9608743f96e840
David Bachmann <dabbi@dabbi.is>
2020-09-30 13:08:00 +0000

    Format

--- libs/island-ui/core/src/lib/FormStepper/FormStepper.tsx
+++ libs/island-ui/core/src/lib/FormStepper/FormStepper.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Typography from '../Typography/Typography'
 
 const FormStepper = () => {
-  return <Typography>Todo: Component that shows current and remaining steps in a form.</Typography>
+  return (
+    <Typography>
+      Todo: Component that shows current and remaining steps in a form.
+    </Typography>
+  )
 }
 
 export default FormStepper
