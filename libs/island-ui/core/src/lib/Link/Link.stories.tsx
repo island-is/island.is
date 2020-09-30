4602a503ccdbc6cf7807cc29fe2a1a92b1db4dec
David Bachmann <dabbi@dabbi.is>
2020-09-30 11:58:38 +0000

    Delete ExternalLink component

--- libs/island-ui/core/src/lib/Link/Link.stories.tsx
+++ libs/island-ui/core/src/lib/Link/Link.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from './Link'
 import ArrowLinkComponent from './ArrowLink/ArrowLink'
-import ExternalLinkComponent from './ExternalLink/ExternalLink'
 import { Box } from '../Box'
 
 export default {
@@ -34,21 +33,5 @@ export const DefaultBlue = () => (
 export const ArrowLink = () => (
   <Box padding={4}>
     <ArrowLinkComponent href="/something">Arrow link</ArrowLinkComponent>
-  </Box>
-)
-
-export const ExternalLink = () => (
-  <Box background="blue400" padding={4}>
-    <ExternalLinkComponent href="https://external.is">
-      External link
-    </ExternalLinkComponent>
-  </Box>
-)
-
-export const ExternalLinkBlue = () => (
-  <Box padding={4}>
-    <ExternalLinkComponent href="https://external.is" color="blue400">
-      External link
-    </ExternalLinkComponent>
   </Box>
 )
