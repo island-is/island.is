82ddf90bc61ddd89c28bf2b77969786dce7326ac
Dang, Sandra <sandang@deloittedigital.se>
2020-09-30 09:55:31 +0200

    Added responsiveness to completed page

--- apps/skilavottord/web/screens/Completed/Completed.tsx
+++ apps/skilavottord/web/screens/Completed/Completed.tsx
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react'
 import {
   Stack,
   Typography,
-  Button,
   GridContainer,
   GridRow,
   GridColumn,
   Link,
   Box,
+  Button
 } from '@island.is/island-ui/core'
 import { ProcessPageLayout } from '@island.is/skilavottord-web/components/Layouts'
 import { useRouter } from 'next/router'
@@ -53,22 +53,22 @@ const Completed = (props) => {
                 <Stack space={4}>
                   <Stack space={2}>
                     <GridRow>
-                      <GridColumn span={'6/9'}>
+                      <GridColumn span={['9/9', '6/9', '6/9', '6/9']}>
                         <Typography variant="p">
                           {`${t.confirmedBy.user} Albert Flores`}
                         </Typography>
                       </GridColumn>
-                      <GridColumn span={'3/9'}>
+                      <GridColumn span={['9/9', '3/9', '3/9', '3/9']}>
                         <Typography variant="h5">2019-06-12 00:55</Typography>
                       </GridColumn>
                     </GridRow>
                     <GridRow>
-                      <GridColumn span={'6/9'}>
+                      <GridColumn span={['9/9', '6/9', '6/9', '6/9']}>
                         <Typography variant="p">
                           {`${t.confirmedBy.user} Albert Flores`}
                         </Typography>
                       </GridColumn>
-                      <GridColumn span={'3/9'}>
+                      <GridColumn span={['9/9', '3/9', '3/9', '3/9']}>
                         <Typography variant="h5">2019-06-12 00:55</Typography>
                       </GridColumn>
                     </GridRow>
@@ -76,32 +76,32 @@ const Completed = (props) => {
                   <Box borderTopWidth="standard" borderColor="blue200"></Box>
                   <Stack space={1}>
                     <GridRow>
-                      <GridColumn span={'6/9'}>
+                      <GridColumn span={['9/9', '6/9', '6/9', '6/9']}>
                         <Typography variant="p">
                           {`${t.confirmedBy.company} VAKA`}
                         </Typography>
                       </GridColumn>
-                      <GridColumn span={'3/9'}>
+                      <GridColumn span={['9/9', '3/9', '3/9', '3/9']}>
                         <Typography variant="h5">2019-06-12 00:55</Typography>
                       </GridColumn>
                     </GridRow>
                     <GridRow>
-                      <GridColumn span={'6/9'}>
+                      <GridColumn span={['9/9', '6/9', '6/9', '6/9']}>
                         <Typography variant="p">
                           {`${t.confirmedBy.authority}`}
                         </Typography>
                       </GridColumn>
-                      <GridColumn span={'3/9'}>
+                      <GridColumn span={['9/9', '3/9', '3/9', '3/9']}>
                         <Typography variant="h5">2019-06-12 00:55</Typography>
                       </GridColumn>
                     </GridRow>
                     <GridRow>
-                      <GridColumn span={'6/9'}>
+                      <GridColumn span={['9/9', '6/9', '6/9', '6/9']}>
                         <Typography variant="p">
                           {`${t.confirmedBy.fund}`}
                         </Typography>
                       </GridColumn>
-                      <GridColumn span={'3/9'}>
+                      <GridColumn span={['9/9', '3/9', '3/9', '3/9']}>
                         <Typography variant="h5">2019-06-12 00:55</Typography>
                       </GridColumn>
                     </GridRow>
