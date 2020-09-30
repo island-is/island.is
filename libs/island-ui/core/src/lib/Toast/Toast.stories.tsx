93883bba5677d2cc6e2914cfc9d76fa0287f4eea
David Bachmann <dabbi@dabbi.is>
2020-09-30 15:50:02 +0000

    Add react-toastify to the project

--- libs/island-ui/core/src/lib/Toast/Toast.stories.tsx
+++ libs/island-ui/core/src/lib/Toast/Toast.stories.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import Toast from './Toast'
+import Toast, { toast } from './Toast'
+import { Button } from '../Button/Button'
 
 export default {
   title: 'Alerts/Toast',
   component: Toast,
 }
 
-export const Default = () => <Toast />
+export const Default = () => (
+  <div>
+    <Button onClick={() => toast.success('Success')} />
+    <Toast
+
+    />
+  </div>
+)
---
+++ libs/island-ui/core/src/lib/Toast/Toast.treat.ts
@@ -0,0 +1,307 @@
+import { style, styleMap, globalStyle } from 'treat'
+import { theme } from '@island.is/island-ui/theme'
+
+export const root = style({
+  position: 'relative',
+})
+
+globalStyle(`${root} .Toastify__toast-container`, {
+  zIndex: 9999,
+  transform: 'translate3d(0, 0, 9999px)',
+  position: 'fixed',
+  padding: '4px',
+  width: '320px',
+  boxSizing: 'border-box',
+  color: `${theme.color.white}`,
+  '@media': {
+    '(max-width: 480px)': {
+      width: '100vw',
+      padding: 0,
+      left: 0,
+      margin: 0,
+    },
+  },
+})
+
+/** Used to define the position of the ToastContainer **/
+globalStyle(`${root} .Toastify__toast-container--top-left`, {
+  top: '1em',
+  left: '1em',
+  '@media': {
+    '(max-width: 480px)': {
+      top: 0,
+      transform: 'translateX(0)',
+    },
+  },
+})
+globalStyle(`${root} .Toastify__toast-container--top-center`, {
+  top: '1em',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  '@media': {
+    '(max-width: 480px)': {
+      top: 0,
+      transform: 'translateX(0)',
+    },
+  },
+})
+globalStyle(`${root} .Toastify__toast-container--top-right`, {
+  top: '1em',
+  right: '1em',
+  '@media': {
+    '(max-width: 480px)': {
+      top: 0,
+      transform: 'translateX(0)',
+    },
+  },
+})
+globalStyle(`${root} .Toastify__toast-container--bottom-left`, {
+  bottom: '1em',
+  left: '1em',
+  '@media': {
+    '(max-width: 480px)': {
+      bottom: 0,
+      transform: 'translateX(0)',
+    },
+  },
+})
+globalStyle(`${root} .Toastify__toast-container--bottom-center`, {
+  bottom: '1em',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  '@media': {
+    '(max-width: 480px)': {
+      bottom: 0,
+      transform: 'translateX(0)',
+    },
+  },
+})
+globalStyle(`${root} .Toastify__toast-container--bottom-right`, {
+  bottom: '1em',
+  right: '1em',
+  '@media': {
+    '(max-width: 480px)': {
+      bottom: 0,
+      transform: 'translateX(0)',
+    },
+  },
+})
+
+/** Classes for the displayed toast **/
+globalStyle(`${root} .Toastify__toast`, {
+  position: 'relative',
+  minHeight: '64px',
+  boxSizing: 'border-box',
+  marginBottom: '1rem',
+  padding: '8px',
+  borderRadius: '1px',
+  boxShadow:
+    '0 1px 10px 0 rgba(0, 0, 0, 0.1), 0 2px 15px 0 rgba(0, 0, 0, 0.05)',
+  display: 'flex',
+  justifyContent: 'space-between',
+  maxHeight: '800px',
+  overflow: 'hidden',
+  fontFamily: 'sans-serif',
+  cursor: 'pointer',
+  direction: 'ltr',
+  '@media': {
+    '(max-width: 480px)': {
+      marginBottom: 0,
+    },
+  },
+})
+
+globalStyle(`${root} .Toastify__toast--dark`, {
+  background: '#121212',
+  color: '#fff',
+})
+
+globalStyle(`${root} .Toastify__toast--default`, {
+  background: `${theme.color.white}`,
+  color: '#aaa',
+})
+
+globalStyle(`${root} .Toastify__toast--info`, {
+  background: '#3498db',
+})
+
+globalStyle(`${root} .Toastify__toast--success`, {
+  background: '#07bc0c',
+})
+globalStyle(`${root} .Toastify__toast--warning`, {
+  background: '#f1c40f',
+})
+globalStyle(`${root} .Toastify__toast--error`, {
+  background: '#e74c3c',
+})
+
+globalStyle(`${root} .Toastify__toast-body`, {
+  flex: '1 1 auto',
+  margin: 'auto 0',
+})
+
+globalStyle(`${root} .Toastify__close-button`, {
+  color: '#fff',
+  background: 'transparent',
+  outline: 'none',
+  border: 'none',
+  padding: '0',
+  cursor: 'pointer',
+  opacity: 0.7,
+  transition: '0.3s ease',
+  alignSelf: 'flex-start',
+})
+
+globalStyle(`${root} .Toastify__close-button--default`, {
+  color: '#000',
+  opacity: 0.5,
+})
+
+globalStyle(`${root} .Toastify__close-button > svg`, {
+  fill: 'currentColor',
+  height: 16,
+  width: 14,
+})
+
+globalStyle(
+  `${root} .Toastify__close-button:hover, .Toastify__close-button:focus`,
+  {
+    opacity: 1,
+  },
+)
+
+/** Classes for the progress bar **/
+globalStyle(`${root} .Toastify__progress-bar`, {
+  position: 'absolute',
+  bottom: '0',
+  left: '0',
+  width: '100%',
+  height: '5px',
+  zIndex: 9999,
+  opacity: 0.7,
+  backgroundColor: 'rgba(255, 255, 255, 0.7)',
+  transformOrigin: 'left',
+})
+
+globalStyle(`${root} .Toastify__progress-bar--animated`, {
+  animation: 'Toastify__trackProgress linear 1 forwards',
+})
+
+globalStyle(`${root} .Toastify__progress-bar--controlled`, {
+  transition: 'transform 0.2s',
+})
+
+globalStyle(`${root} .Toastify__progress-bar--default`, {
+  background:
+    'linear-gradient(to right, #4cd964, #5ac8fa, #007aff, #34aadc, #5856d6, #ff2d55)',
+})
+
+globalStyle(`${root} .Toastify__progress-bar--dark`, {
+  background: '#bb86fc',
+})
+
+/** Bounce animation */
+globalStyle(
+  `${root} .Toastify__bounce-enter--top-left, .Toastify__bounce-enter--bottom-left`,
+  {
+    animationName: 'Toastify__bounceInLeft',
+  },
+)
+
+globalStyle(
+  `${root} .Toastify__bounce-enter--top-right, .Toastify__bounce-enter--bottom-right`,
+  {
+    animationName: 'Toastify__bounceInRight',
+  },
+)
+globalStyle(`${root} .Toastify__bounce-enter--top-center`, {
+  animationName: 'Toastify__bounceInDown',
+})
+globalStyle(`${root} .Toastify__bounce-enter--bottom-center`, {
+  animationName: 'Toastify__bounceInUp',
+})
+globalStyle(
+  `${root} .Toastify__bounce-exit--top-left, .Toastify__bounce-exit--bottom-left`,
+  {
+    animationName: 'Toastify__bounceOutLeft',
+  },
+)
+
+globalStyle(
+  `${root} .Toastify__bounce-exit--top-right, .Toastify__bounce-exit--bottom-right`,
+  {
+    animationName: 'Toastify__bounceOutRight',
+  },
+)
+
+globalStyle(`${root} .Toastify__bounce-exit--top-center`, {
+  animationName: 'Toastify__bounceOutUp',
+})
+
+globalStyle(`${root} .Toastify__bounce-exit--bottom-center`, {
+  animationName: 'Toastify__bounceOutDown',
+})
+
+/** Zoom animation */
+
+globalStyle(`${root} .Toastify__zoom-enter`, {
+  animationName: 'Toastify__zoomIn',
+})
+
+globalStyle(`${root} .Toastify__zoom-exit`, {
+  animationName: 'Toastify__zoomOut',
+})
+
+/** Flip animation */
+globalStyle(`${root} .Toastify__flip-enter`, {
+  animationName: 'Toastify__flipIn',
+})
+
+globalStyle(`${root} .Toastify__flip-exit`, {
+  animationName: 'Toastify__flipOut',
+})
+
+/** Slide animation */
+globalStyle(
+  `${root} .Toastify__slide-enter--top-left, .Toastify__slide-enter--bottom-left`,
+  {
+    animationName: 'Toastify__slideInLeft',
+  },
+)
+
+globalStyle(
+  `${root} .Toastify__slide-enter--top-right, .Toastify__slide-enter--bottom-right`,
+  {
+    animationName: 'Toastify__slideInRight',
+  },
+)
+
+globalStyle(`${root} .Toastify__slide-enter--top-center`, {
+  animationName: 'Toastify__slideInDown',
+})
+
+globalStyle(`${root} .Toastify__slide-enter--bottom-center`, {
+  animationName: 'Toastify__slideInUp',
+})
+
+globalStyle(
+  `${root} .Toastify__slide-exit--top-left, .Toastify__slide-exit--bottom-left`,
+  {
+    animationName: 'Toastify__slideOutLeft',
+  },
+)
+
+globalStyle(
+  `${root} .Toastify__slide-exit--top-right, .Toastify__slide-exit--bottom-right`,
+  {
+    animationName: 'Toastify__slideOutRight',
+  },
+)
+
+globalStyle(`${root} .Toastify__slide-exit--top-center`, {
+  animationName: 'Toastify__slideOutUp',
+})
+
+globalStyle(`${root} .Toastify__slide-exit--bottom-center`, {
+  animationName: 'Toastify__slideOutDown',
+})
