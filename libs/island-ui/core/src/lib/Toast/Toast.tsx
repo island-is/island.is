46653c6b5a194ff5a74a4814171a9896c9615802
David Bachmann <dabbi@dabbi.is>
2020-09-30 11:58:13 +0000

    Prepare Toast component

---
+++ libs/island-ui/core/src/lib/Toast/Toast.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Typography from '../Typography/Typography'
+
+const Toast = () => {
+    return (
+        <Typography>Todo</Typography>
+    )
+}
+
+export default Toast
