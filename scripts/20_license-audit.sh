83fcf51590ac9672e5be031218d9a2a185a15cbc
Petar Shomov <petar@andes.is>
2020-10-01 16:54:51 +0000

    fixing permissions

--- scripts/20_license-audit.sh
+++ scripts/20_license-audit.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-source $DIR/_common.sh
-
-cd $PROJECT_ROOT
 yarn run license-audit
-cd -
--- scripts/20_lint.sh
+++ scripts/20_lint.sh
@@ -7,12 +7,3 @@ source $DIR/_common.sh
 
 exec yarn run \
   lint ${APP}
-
-# Docker based run, not used at the moment due to severe speed penalty
-# exec docker buildx build \
-#   --platform=linux/amd64 \
-#   --cache-from=type=local,src=$PROJECT_ROOT/cache \
-#   -f ${DIR}/Dockerfile \
-#   --target=linter \
-#   --build-arg APP=${APP} \
-#   $PROJECT_ROOT
