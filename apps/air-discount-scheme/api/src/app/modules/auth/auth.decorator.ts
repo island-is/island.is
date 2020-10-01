1cce7bdd6d5228f33d240a24e3872612902c7b45
Darri Steinn Konráðsson <darri@viceversa.is>
2020-10-01 12:39:09 +0000

    Fix new createParameter api after nest update (#1147)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/air-discount-scheme/api/src/app/modules/auth/auth.decorator.ts
+++ apps/air-discount-scheme/api/src/app/modules/auth/auth.decorator.ts
@@ -3,5 +3,5 @@ import { createParamDecorator } from '@nestjs/common'
 import { AuthUser } from './auth.types'
 
 export const CurrentUser = createParamDecorator(
-  (data, [_1, _2, { req }]): AuthUser => req.user,
+  (data, { args: [_1, _2, { req }] }): AuthUser => req.user,
 )
