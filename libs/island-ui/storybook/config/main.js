0c0ae65d73433c73ae530422b65d421e64dc1e2c
David Bachmann <dabbi@dabbi.is>
2020-10-01 14:11:07 +0000

    Formatting

--- libs/island-ui/storybook/config/main.js
+++ libs/island-ui/storybook/config/main.js
@@ -1,7 +1,5 @@
 module.exports = {
-  stories: [
-    '../../core/src/**/*.stories.@(tsx|mdx)',
-  ],
+  stories: ['../../core/src/**/*.stories.@(tsx|mdx)'],
   addons: [
     '@storybook/addon-knobs/register',
     '@storybook/addon-a11y/register',
