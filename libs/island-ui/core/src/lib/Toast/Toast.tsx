f3aaf970f82162c8152ac861fa02f6dfc9d4d31c
Davíð Bachmann <dabbi@dabbi.is>
2020-10-01 22:30:38 +0000

    island-ui/ Toast (#1149)
    * Add react-toastify to the project
    
    * Rendering custom messages in toasts. Style toasts in our theme.
    
    * Finalize
    
    * Change mediaqueries to use our theme
    
    * Remove all animations except Slide
    
    * Pin toastify version
    
    * I guess yarn.lock is significant
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/island-ui/core/src/lib/Toast/Toast.tsx
+++ libs/island-ui/core/src/lib/Toast/Toast.tsx
@@ -1,8 +1,89 @@
 import React from 'react'
+import {
+  ToastContainer as ToastifyContainer,
+  toast as toastify,
+  Slide,
+} from 'react-toastify'
+import { Box } from '../Box'
+import Icon from '../Icon/Icon'
 import Typography from '../Typography/Typography'
+import * as toastStyles from './Toast.treat'
+import { toastKeyframes } from './toastKeyframes'
 
-const Toast = () => {
-  return <Typography>Todo</Typography>
+declare module 'react' {
+  // Make React recognize `jsx` prop on the style element.
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean
+  }
 }
 
-export default Toast
+interface ToastProps {
+  hideProgressBar?: boolean
+  timeout?: number
+  closeButton?: boolean
+}
+
+const RenderMessage = ({
+  message,
+  type,
+}: {
+  message: string
+  type: 'error' | 'success' | 'warning' | 'info'
+}) => {
+  const colors = {
+    error: 'red400' as const,
+    success: 'mint400' as const,
+    warning: 'yellow600' as const,
+    info: 'blue400' as const,
+  }
+  const icons = {
+    error: 'toasterError' as const,
+    success: 'toasterSuccess' as const,
+    warning: 'toasterWarning' as const,
+    info: 'toasterInfo' as const,
+  }
+  return (
+    <Box display="flex" padding={1} alignItems="flexStart">
+      <Box flexShrink={0}>
+        <Icon type={icons[type]} color={colors[type]} />
+      </Box>
+      <Box paddingLeft={2}>
+        <Typography variant="h5">{message}</Typography>
+      </Box>
+    </Box>
+  )
+}
+
+const ToastContainer: React.FC<ToastProps> = ({
+  hideProgressBar = false,
+  timeout = 5000,
+}) => {
+  return (
+    <div className={toastStyles.root}>
+      <ToastifyContainer
+        position="bottom-right"
+        autoClose={timeout}
+        hideProgressBar={hideProgressBar}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Slide}
+      />
+      <style jsx>{toastKeyframes}</style>
+    </div>
+  )
+}
+
+export const toast = {
+  success: (message: string) =>
+    toastify.success(<RenderMessage type="success" message={message} />),
+  error: (message: string) =>
+    toastify.error(<RenderMessage type="error" message={message} />),
+  info: (message: string) =>
+    toastify.info(<RenderMessage type="info" message={message} />),
+  warning: (message: string) =>
+    toastify.warning(<RenderMessage type="warning" message={message} />),
+}
+
+export default ToastContainer
