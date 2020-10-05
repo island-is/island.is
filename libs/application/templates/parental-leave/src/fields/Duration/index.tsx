291af65c2f846a7ddf4d2fea3f674cbe06081e0d
Noam <noam@aranja.com>
2020-10-05 14:05:53 +0000

    Bring in previous calculation slider for the durations screen

---
+++ libs/application/templates/parental-leave/src/fields/Duration/index.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useState, useEffect } from 'react'
+import { Controller, useFormContext } from 'react-hook-form'
+
+import { FieldBaseProps } from '@island.is/application/core'
+import { Box } from '@island.is/island-ui/core'
+import Slider from '../components/Slider'
+
+import { theme } from '@island.is/island-ui/theme'
+
+const ParentalLeaveCalculations: FC<FieldBaseProps> = ({
+  field,
+  formValue,
+}) => {
+  const { id } = field
+  const { clearErrors } = useFormContext()
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  const monthsToUse = (formValue.usage as number) || 1
+  const [monthsToSpread, setMonthsToSpread] = useState<number | undefined>(
+    undefined,
+  )
+  const maxMonths = 24
+
+  useEffect(() => {
+    // TODO: Calculate the percentage they can receive based on the duration spread
+  }, [monthsToSpread, monthsToUse])
+
+  return (
+    <Box marginTop={8}>
+      <Controller
+        defaultValue={monthsToUse}
+        name={id}
+        render={({ onChange, value }) => (
+          <Slider
+            min={monthsToUse}
+            max={maxMonths}
+            trackStyle={{ gridTemplateRows: 8 }}
+            calculateCellStyle={(index: number) => {
+              const isActive =
+                (value && index < value) ||
+                (monthsToSpread && index < monthsToSpread) ||
+                index < monthsToUse
+              return {
+                background: isActive
+                  ? theme.color.mint400
+                  : theme.color.dark200,
+              }
+            }}
+            showMinMaxLabels
+            showToolTip
+            label={{ singular: 'mánuður', plural: 'mánuðir' }}
+            currentIndex={monthsToSpread || monthsToUse}
+            onChange={(selectedMonthsToSpread: number) => {
+              clearErrors(id)
+              onChange(selectedMonthsToSpread)
+              setMonthsToSpread(selectedMonthsToSpread)
+            }}
+          />
+        )}
+      />
+    </Box>
+  )
+}
+
+export default ParentalLeaveCalculations
