658053f0796eccc70e61a97d4c3cda1c961216c6
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-10-08 13:24:18 +0000

    Generate schemas in node

---
+++ scripts/generate-schemas.js
@@ -0,0 +1,33 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const { promisify } = require('util')
+
+SCHEMA_PATH = 'libs/api/schema/src/lib/schema.d.ts'
+
+YARN_COMMANDS = [
+  'yarn nx run application-system-api:build-schema',
+  'yarn nx run api-domains-application:codegen',
+  'yarn nx run api:build-schema',
+  'yarn affected:schemas',
+]
+
+const main = async () => {
+  if (!(await promisify(fs.exists)(SCHEMA_PATH))) {
+    await promisify(fs.writeFile)(SCHEMA_PATH, 'export default () => {}')
+  }
+  for (cmd of YARN_COMMANDS) {
+    console.log(`Running ${cmd}`)
+    try {
+      const { stdout, stderr } = await promisify(exec)(cmd)
+      console.log(`stdout: ${stdout}`)
+      if (stderr) {
+        console.error(`stderr: ${stderr}`)
+      }
+    } catch (err) {
+      console.error(err)
+      process.exit(err.code)
+    }
+  }
+}
+
+main()
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,12 +4,10 @@ if (process.env.CI) {
 } else {
   exec('yarn schemas', (err, stdout, stderr) => {
     if (err) {
-      // node couldn't execute the command
       console.error(err)
       process.exit(err.code)
     }
 
-    // the *entire* stdout and stderr (buffered)
     console.log(`stdout: ${stdout}`)
     if (stderr) {
       console.error(`stderr: ${stderr}`)
