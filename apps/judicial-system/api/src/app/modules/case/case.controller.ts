4ac90e6abdc3c8b386673fc3d298697dc244ddf8
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-10-08 10:56:34 +0000

    Implements email sending

--- apps/judicial-system/api/src/app/modules/case/case.controller.ts
+++ apps/judicial-system/api/src/app/modules/case/case.controller.ts
@@ -18,6 +18,7 @@ import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
 
 import { LOGGER_PROVIDER, Logger } from '@island.is/logging'
 import { SigningServiceResponse } from '@island.is/dokobit-signing'
+import { EmailService } from '@island.is/email-service'
 import { CaseState, CaseTransition } from '@island.is/judicial-system/types'
 
 import { JwtAuthGuard } from '../auth'
@@ -28,6 +29,7 @@ import { CaseService } from './case.service'
 import { CaseValidationPipe } from './case.pipe'
 import { transitionCase as transitionUpdate } from './case.state'
 import { generateRequestPdf } from './case.pdf'
+import { environment } from '../../../environments'
 
 @UseGuards(JwtAuthGuard)
 @Controller('api')
@@ -37,9 +39,43 @@ export class CaseController {
     private readonly caseService: CaseService,
     @Inject(UserService)
     private readonly userService: UserService,
+    @Inject(EmailService)
+    private readonly emailService: EmailService,
     @Inject(LOGGER_PROVIDER)
     private readonly logger: Logger,
   ) {}
+
+  private async sendRequestAsPdf(
+    existingCase: Case,
+    pdf: string,
+  ): Promise<void> {
+    await this.emailService.sendEmail({
+      from: {
+        name: environment.email.fromName,
+        address: environment.email.fromEmail,
+      },
+      replyTo: {
+        name: environment.email.replyToName,
+        address: environment.email.replyToEmail,
+      },
+      to: [
+        {
+          name: existingCase.prosecutor?.name,
+          address: existingCase.prosecutor?.email,
+        },
+      ],
+      subject: `Krafa í máli ${existingCase.policeCaseNumber}`,
+      text: 'Sjá viðhengi',
+      html: 'Sjá viðhengi',
+      attachments: [
+        {
+          filename: `${existingCase.policeCaseNumber}.pdf`,
+          content: pdf,
+          encoding: 'binary',
+        },
+      ],
+    })
+  }
 
   @Post('case')
   @ApiCreatedResponse({ type: Case, description: 'Creates a new case' })
@@ -62,7 +98,7 @@ export class CaseController {
     )
 
     if (numberOfAffectedRows === 0) {
-      throw new NotFoundException(`A case with the id ${id} does not exist`)
+      throw new NotFoundException(`Case ${id} does not exist`)
     }
 
     return updatedCase
@@ -78,6 +114,8 @@ export class CaseController {
     @Body() transition: TransitionCaseDto,
     @Req() req,
   ): Promise<Case> {
+    // Use existingCase.modified when client is ready to send last modified timestamp with all updates
+
     const existingCase = await this.findCaseById(id)
 
     const user = await this.userService.findByNationalId(req.user.nationalId)
@@ -86,7 +124,6 @@ export class CaseController {
 
     const { numberOfAffectedRows, updatedCase } = await this.caseService.update(
       id,
-      // existingCase.modified, Uncomment when client is ready to send last modified timestamp with all updates
       update,
     )
 
@@ -96,10 +133,13 @@ export class CaseController {
       )
     }
 
-    // Find a better place when we start sending email
+    // Find a better place for this
     if (transition.transition === CaseTransition.SUBMIT) {
       const dbCase = await this.findCaseById(id)
-      generateRequestPdf(dbCase)
+
+      const pdf = await generateRequestPdf(dbCase)
+
+      await this.sendRequestAsPdf(dbCase, pdf)
     }
 
     return updatedCase
@@ -200,18 +240,18 @@ export class CaseController {
   ): Promise<Case> {
     const existingCase = await this.findCaseById(id)
 
-    if (existingCase.judge?.role !== UserRole.JUDGE) {
-      throw new UnauthorizedException(
-        `A ruling signature cannot be verified by a user with role ${existingCase.judge?.role}`,
-      )
-    }
-
     if (
       existingCase.state !== CaseState.ACCEPTED &&
       existingCase.state !== CaseState.REJECTED
     ) {
       throw new ForbiddenException(
         `Cannot confirm a ruling signature for a case in state ${existingCase.state}`,
+      )
+    }
+
+    if (existingCase.judge?.role !== UserRole.JUDGE) {
+      throw new UnauthorizedException(
+        `A ruling signature cannot be verified by a user with role ${existingCase.judge?.role}`,
       )
     }
 
@@ -222,7 +262,7 @@ export class CaseController {
     const existingCase = await this.caseService.findById(id)
 
     if (!existingCase) {
-      throw new NotFoundException(`A case with the id ${id} does not exist`)
+      throw new NotFoundException(`Case ${id} does not exist`)
     }
 
     return existingCase
