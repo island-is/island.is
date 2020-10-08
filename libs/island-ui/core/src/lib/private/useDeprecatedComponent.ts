050b97c5fe60b732a371ff85b8ca64349fe33b15
Brian Johannessen <barabrian89@gmail.com>
2020-10-08 13:43:56 +0000

    remove string type

--- libs/island-ui/core/src/lib/private/useDeprecatedComponent.ts
+++ libs/island-ui/core/src/lib/private/useDeprecatedComponent.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 const useDeprecatedComponent = (
   componentName: string,
-  customMsg: string = `${componentName} is deprecated`,
+  customMsg = `${componentName} is deprecated`,
 ) =>
   useEffect(() => {
     if (process.env.NODE_ENV === 'development') {
