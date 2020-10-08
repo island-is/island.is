3645eed87dc687bda0c2d4865a18612089a6f9d3
arnifreyrs <arni@aranja.com>
2020-10-08 12:58:30 +0000

    Repeaters have custom components

--- libs/application/templates/reference-template/src/fields/CustomRepeater.tsx
+++ libs/application/templates/reference-template/src/fields/CustomRepeater.tsx
@@ -1,15 +1,15 @@
 import React, { FC } from 'react'
 import { RepeaterProps } from '@island.is/application/core'
-import { Button, Typography } from '@island.is/island-ui/core'
+import { Button, Text } from '@island.is/island-ui/core'
 
 const CustomRepeater: FC<RepeaterProps> = ({ expandRepeater }) => {
   return (
     <>
-      <Typography as={'p'}>
+      <Text as={'p'}>
         Repeaters are presented as custom components. They primarily focus on
         easily implementing repetitive flows. The `expandRepeater` prop is
         important as it initiates that subflow to generate more nested items.
-      </Typography>
+      </Text>
 
       <Button onClick={expandRepeater}>Expand repeater</Button>
     </>
