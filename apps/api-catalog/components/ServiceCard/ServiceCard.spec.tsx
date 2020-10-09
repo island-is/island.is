a2ae46d1b3fd33f0e30d35378d588a4c23d4e5fb
guttih <gudjonholm@gmail.com>
2020-10-09 11:15:38 +0000

    api-catalog fixing test index.spec.tsx

--- apps/api-catalog/components/ServiceCard/ServiceCard.spec.tsx
+++ apps/api-catalog/components/ServiceCard/ServiceCard.spec.tsx
@@ -1,80 +1,101 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render } from '@testing-library/react'
 
-import {ServiceCard} from '.';
-import { 
+import { ServiceCard } from '.'
+
+
+import { SERVICE_STATUS } from '..';
+import {
   ApiService,
+  AccessCategory,
   PricingCategory,
   DataCategory,
   TypeCategory,
-  AccessCategory
- } from '@island.is/api/schema';
-import ContentfulApi from '../../services/contentful';
+  XroadInfo,
+} from '@island.is/api/schema';
 
-describe(' ServiceCard ', async () => {
-  // const service:ServiceCardInformation = { 
-  //   id:'0', 
-  //   owner:"Þjóðskrá", 
-  //   name:"Fasteignaskrá", 
-  //   status:SERVICE_STATUS.OK,      
-  //   pricing:[PRICING_CATEGORY.FREE, PRICING_CATEGORY.PAID],
-  //   data:   [DATA_CATEGORY.PUBLIC],
-  //   type:   [TYPE_CATEGORY.REACT,  TYPE_CATEGORY.SOAP], 
-  //   access: [ACCESS_CATEGORY.X_ROAD],
-  // };
-  
+//libs\api\schema\src\lib\schema.d.ts
+
+describe(' ServiceCard ', () => {
+  const xroadID: XroadInfo = {
+    instance: "instance",
+    memberClass: "memberclass",
+    memberCode: "membercode",
+    serviceCode: "serviceCode",
+    subsystemCode: "subsystem"
+  }
   const service: ApiService = {
     id: '0',
-    owner: 'Þjóðskrá',
-    name: 'Fasteignaskrá',
-    description: 'OK',
+    owner: "Þjóðskrá",
+    name: "Fasteignaskrá",
+    /*status:SERVICE_STATUS.OK,      */
     pricing: [PricingCategory.Free, PricingCategory.Paid],
     data: [DataCategory.Public],
-    type: [TypeCategory.Rest],
+    type: [TypeCategory.Rest, TypeCategory.Soap],
     access: [AccessCategory.Xroad],
-    xroadIdentifier: [{
-      instance: 'TestInstance',
-      memberClass: 'TestMemberClass',
-      memberCode: 'TestMemberCode',
-      subsystemCode: 'TestSubsystemCode',
-      serviceCode: 'TestServiceCode'
-    }]
+    description: "some description",
+    xroadIdentifier: [
+      {
+        instance: "instance",
+        memberClass: "memberclass",
+        memberCode: "membercode",
+        serviceCode: "serviceCode",
+        subsystemCode: "subsystem"
+      }]
   };
 
-  const client = new ContentfulApi();
-  const filterStrings = await client.fetchPageBySlug('service-filter', 'is-IS');
+  const strings = [
+    { id: "catalog-filter-access", text: "Aðgengi" },
+    { id: "catalog-filter-data", text: "Gögn" },
+    { id: "catalog-filter-data-financial", text: "Fjármál" },
+    { id: "catalog-filter-data-health", text: "Heilsa" },
+    { id: "catalog-filter-data-official", text: "Opinber" },
+    { id: "catalog-filter-data-personal", text: "Persónugreinanleg" },
+    { id: "catalog-filter-data-public", text: "Almenn" },
+    { id: "catalog-filter-pricing", text: "Verð" },
+    { id: "catalog-filter-pricing-free", text: "Gjaldfrjáls" },
+    { id: "catalog-filter-pricing-paid", text: "Gjaldskyld" },
+    { id: "catalog-filter-type", text: "Tegund" },
+    { id: "catalog-filter-type-graphql", text: "GraphQL" },
+    { id: "catalog-filter-type-react", text: "REST" },
+    { id: "catalog-filter-type-soap", text: "SOAP" },
+    { id: "catalog-filter-access-xroad", text: "X-Road" },
+    { id: "catalog-filter-access-apigw", text: "API GW" },
+    { id: "catalog-filter-search", text: "Leita" }
+  ]
 
   it('should render successfully', () => {
-    const { baseElement } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+    const { baseElement } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
     expect(baseElement).toBeTruthy()
   })
   describe('Card values should contain', () => {
 
     it('should contain service name', () => {
-      const { getByText } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+      const { getByText } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
       expect(getByText(service.name)).toBeTruthy()
     })
-  
+
     it('should contain owner name', () => {
-      const { getByText } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+      const { getByText } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
       expect(getByText(service.owner)).toBeTruthy()
     })
-  
+
     it('should contain all pricing values', () => {
-      const { getByText } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+      const { getByText } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
       expect(getByText(service.pricing[0])).toBeTruthy()
       expect(getByText(service.pricing[1])).toBeTruthy()
     })
     it('should contain all categories values', () => {
-      const { getByText } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+      const { getByText } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
       expect(getByText(service.data[0])).toBeTruthy()
     })
     it('should contain all types values', () => {
-      const { getByText } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+      const { getByText } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
       expect(getByText(service.type[0])).toBeTruthy()
+      expect(getByText(service.type[1])).toBeTruthy()
     })
     it('should contain all Access values', () => {
-      const { getByText } = render(<ServiceCard service={service} strings={filterStrings.strings} />)
+      const { getByText } = render(<ServiceCard cardWidth={250} service={service} strings={strings} />)
       expect(getByText(service.access[0])).toBeTruthy()
     })
   })
--- apps/api-catalog/specs/index.spec.tsx
+++ apps/api-catalog/specs/index.spec.tsx
@@ -2,14 +2,33 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import Home from '../pages/index'
+import { Page } from '../services/contentful.types'
 
 describe('Home', () => {
 
-  const pageContent = {
-    title:"The title",
-    introText: "the intro text",
-    buttons: [
-      {label:"Button1", linkUrl:"http://button1"}
+  const pageContent:Page = {
+    id: 'home',
+    strings: [
+      { id: 'home-title', text: 'Viskuausan' },
+      {
+        id: 'home-intro',
+        text: 'Hér getur þú skoðað og leitað í fjölda vefþjónusta og gagnaskilgreininga hjá hinu opinbera'
+      },
+      { id: 'home-catalog-button', text: 'API Vörulisti' },
+      { id: 'home-dm-button', text: 'Gagnamódel' },
+      { id: 'home-dg-button', text: 'Þróunarhandbók' },
+      {
+        id: 'home-cata-btn-txt',
+        text: 'Skoða og leita í fjölda vefþjónusta hjá hinu opinbera.'
+      },
+      {
+        id: 'home-dg-btn-txt',
+        text: 'Skoða þróunarhandbók Stafræns Íslands. Bókin er leiðarvísir um bestu vinnubrögð við þróun vefþjónusta.'
+      },
+      {
+        id: 'home-dm-btn-txt',
+        text: 'Skoða og leita í fjölda gagnaskilgreininga hjá hinu opinbera.'
+      }
     ]
   }
   
@@ -20,7 +39,7 @@ describe('Home', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<Home pageContent={pageContent}/>)
-    expect(getByText(pageContent.title)).toBeTruthy() 
+    expect(getByText(pageContent.strings[0].text)).toBeTruthy() 
     
   })
 })
