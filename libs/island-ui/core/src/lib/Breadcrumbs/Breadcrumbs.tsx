6f3d6a94ce2910c8bf5cb2c6e4c1877ea75e5192
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-10-09 13:46:29 +0000

    Use Typography in Breadcrumbs for now

--- libs/island-ui/core/src/lib/Breadcrumbs/Breadcrumbs.tsx
+++ libs/island-ui/core/src/lib/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import React, { FC, Children } from 'react'
-import { Text } from '../Text/Text'
+import { Typography } from '../Typography/Typography'
 import { Icon } from '../Icon/Icon'
 import { Colors } from '@island.is/island-ui/theme'
 import * as styles from './Breadcrumbs.treat'
@@ -22,9 +22,9 @@ export const Breadcrumbs: FC<BreadcrumbsProps> = ({
     <div aria-label={label}>
       {crumbs.map((child, index) => (
         <span key={index}>
-          <Text variant="eyebrow" as="span" color={color}>
+          <Typography variant="eyebrow" as="span" color={color}>
             {child}
-          </Text>
+          </Typography>
           {crumbs.length - 1 > index && (
             <span className={styles.divider}>
               <Icon type="bullet" width="4" color={separatorColor} />
