8a94e98b88a00d32e4ed8157ddec2fc616de282f
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-10-12 13:58:50 +0000

    Move ci specific scripts to separate folder

--- scripts/ci/run-affected-in-parallel.sh
+++ scripts/ci/run-affected-in-parallel.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/_common.sh
 MAX_JOBS=${MAX_JOBS:-2}
 
-AFFECTED_PROJECTS=`$DIR/nx-affected-targets.sh $1 | tr '\n,' ' '`
+AFFECTED_PROJECTS=`$DIR/_nx-affected-targets.sh $1 | tr '\n,' ' '`
 echo "Affected projects for target $1 are '$AFFECTED_PROJECTS'"
 
 # This is a helper script to run in parallel affected projects for a specific target
