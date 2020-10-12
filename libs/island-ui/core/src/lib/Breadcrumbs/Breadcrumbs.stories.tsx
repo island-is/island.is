ac4fa7c4b5f8632c761209d330c7e7018cfb1c4c
David Bachmann <dabbi@dabbi.is>
2020-10-12 09:48:16 +0000

    Add 'design' addon to Storybook to show live Figma design inside stories

--- libs/island-ui/core/src/lib/Breadcrumbs/Breadcrumbs.stories.tsx
+++ libs/island-ui/core/src/lib/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Breadcrumbs } from './Breadcrumbs'
+import { withDesign } from 'storybook-addon-designs'
+
+const figmaLink =
+  'https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=93%3A685'
 
 export default {
   title: 'Navigation/Breadcrumbs',
   component: Breadcrumbs,
+  decorators: [withDesign],
   parameters: {
     docs: {
       description: {
-        component:
-          '[View in Figma](https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=93%3A685)',
+        component: `[View in Figma](${figmaLink})`,
       },
+    },
+    design: {
+      type: 'figma',
+      url: figmaLink,
     },
   },
 }
