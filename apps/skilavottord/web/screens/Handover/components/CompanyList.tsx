2ee2448de99b3a0431818bb9c9a5c9ee00d920ee
dd-sdang <64913954+dd-sdang@users.noreply.github.com>
2020-10-12 15:55:48 +0200

    Skilavottord/add auth (#1317)
    * Added auth to pages and auth on confirm recycling
    
    * Fix routing with authentication and translation
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/skilavottord/web/screens/Handover/components/CompanyList.tsx
+++ apps/skilavottord/web/screens/Handover/components/CompanyList.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import CompanyListItem from './CompanyListItem'
 
-const CompanyList = ({ companies }) => {
+const CompanyList = () => {
+  const companies = [
+    {
+      name: 'Company 1',
+      address: 'Address',
+      phone: '01234',
+      website: 'http://www.some-company.is',
+    },
+    {
+      name: 'Company 2',
+      address: 'Address',
+      phone: '01234',
+      website: 'http://www.some-company.is',
+    },
+    {
+      name: 'Company 3',
+      address: 'Address',
+      phone: '01234',
+      website: 'http://www.some-company.is',
+    },
+  ]
+
   return (
     <>
       {companies.map((company, index) => (
