4fdf5aa19ea68a81ef56d656e18c1f05191a0c50
Ívar Oddsson <ivaroddsson@gmail.com>
2020-10-13 11:33:12 +0000

    Fix time issue in localstorage

---
+++ apps/judicial-system/web/src/routes/Prosecutor/CreateDetentionRequest/StepTwo/StepTwo.spec.tsx
@@ -0,0 +1,36 @@
+import { createMemoryHistory } from 'history'
+import React from 'react'
+import { render, act, waitFor, getByText } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import StepTwo from './StepTwo'
+import { Router } from 'react-router-dom'
+import fetchMock from 'fetch-mock'
+import * as Constants from '../../../../utils/constants'
+
+describe('Create detention request, step two', () => {
+  test("should display the correct arrestTime and requestedCourtDate if it's in localstorage", () => {
+    // Arrange
+    const history = createMemoryHistory()
+
+    Storage.prototype.getItem = jest.fn(() => {
+      return JSON.stringify({
+        id: 'test_id',
+        requestedCustodyEndTime: '2020-11-02T12:03:00Z',
+        custodyProvisions: [],
+        requestedCustodyRestrictions: [],
+      })
+    })
+
+    // Act
+    const { getByTestId } = render(
+      <Router history={history}>
+        <StepTwo />
+      </Router>,
+    )
+
+    // Assert
+    expect(
+      (getByTestId('requestedCustodyEndTime') as HTMLInputElement).value,
+    ).toEqual('13:37')
+  })
+})
