eaba942dc8eeea1fcdebfb72a2658afa6dd67474
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-10-13 17:22:58 +0000

    mock sign in

--- apps/service-portal/src/store/store.ts
+++ apps/service-portal/src/store/store.ts
@@ -19,8 +19,14 @@ export interface StoreState {
   routes: ServicePortalRoute[]
 }
 
+const MOCK_SIGN_IN = false
+
+const userObject = JSON.parse(
+  '{"id_token":"a.e.b","session_state":"a.b","access_token":"a.b.c","refresh_token":"a","token_type":"Bearer","scope":"openid profile offline_access","profile":{"s_hash":"sss","sid":"98","sub":"FA","auth_time":1602141813,"idp":"islykill","amr":["external"],"name":"Tester Testerson","natreg":"1231231234","nat":"IS"},"expires_at":1602152675,"state":"/minar-upplysingar"}',
+)
+
 export const initialState: StoreState = {
-  userInfo: null,
+  userInfo: MOCK_SIGN_IN ? userObject : null,
   userInfoState: 'passive',
   modules: modules,
   navigationState: 'passive',
