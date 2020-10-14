ba8bb49ccaacadc28a51cf35804bcde6a1c2b95f
Eirikur Nilsson <eirikur@nilsson.is>
2020-10-14 23:05:27 +0000

    Mock search resolvers

--- libs/api/mocks/src/graphql/resolvers.ts
+++ libs/api/mocks/src/graphql/resolvers.ts
@@ -1,7 +1,7 @@
+import merge from 'lodash/merge'
 import { Resolvers } from './types'
 import createFieldResolver from './createFieldResolver'
 import { resolvers as cmsResolvers } from '../domains/cms'
+import { resolvers as searchResolvers } from '../domains/search'
 
-export const resolvers = createFieldResolver<Resolvers>({
-  ...cmsResolvers,
-})
+export const resolvers = createFieldResolver<Resolvers>(merge({}, cmsResolvers, searchResolvers))
