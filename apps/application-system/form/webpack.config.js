a8955565401bdc14a25b218670b3e4ce6b18a33b
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2020-10-15 10:31:30 +0000

    silence webpack noise

--- apps/application-system/form/webpack.config.js
+++ apps/application-system/form/webpack.config.js
@@ -5,6 +5,11 @@ const nrwlConfig = require('@nrwl/react/plugins/webpack.js')
 
 module.exports = (config) => {
   nrwlConfig(config) // first call it so that it @nrwl/react plugin adds its configs,
+  config.stats.chunks = false
+  config.stats.modules = false
+  config.devServer.stats.chunks = false
+  config.devServer.stats.modules = false
+  config.devServer.noInfo = true
   return {
     ...config,
     plugins: [...config.plugins, new TreatPlugin()],
