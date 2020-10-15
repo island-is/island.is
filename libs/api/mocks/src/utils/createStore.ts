d48f4cda9958e34801b4f3d0c975a34d6f858f2b
Eirikur Nilsson <eirikur@nilsson.is>
2020-10-15 01:14:24 +0000

    Make createStore util

---
+++ libs/api/mocks/src/utils/createStore.ts
@@ -0,0 +1,45 @@
+type Internal<T> = {
+  $current: T | null
+  $reset: () => void
+}
+
+type Store<T> = T & Internal<T>
+
+type Initializer<T> = () => T
+
+type PropertyHandlerName =
+  | 'get'
+  | 'set'
+  | 'has'
+  | 'defineProperty'
+  | 'deleteProperty'
+
+export const createStore = <T extends {}>(initializerFn: Initializer<T>): Store<T> => {
+  const internal: Internal<T> = {
+    $current: null,
+    $reset() {
+      internal.$current = null
+    },
+  }
+
+  const propertyHandler = <Handler extends PropertyHandlerName>(handler: Handler) => (
+    target: Internal<T>, property: string, ...args: any[]
+  ) => {
+    if (String(property).startsWith('$')) {
+      return (Reflect[handler] as any).call(Reflect, target, property, ...args)
+    } else {
+      if (!internal.$current) {
+        internal.$current = initializerFn()
+      }
+      return (Reflect[handler] as any).call(Reflect, internal.$current, property, ...args)
+    }
+  }
+
+  return new Proxy(internal, {
+    get: propertyHandler('get'),
+    set: propertyHandler('set'),
+    has: propertyHandler('has'),
+    deleteProperty: propertyHandler('deleteProperty'),
+    defineProperty: propertyHandler('defineProperty'),
+  }) as Store<T>
+}
