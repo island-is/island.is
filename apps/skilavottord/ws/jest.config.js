c74a0db607ba737cab564a955febeeb35b8d25de
Sigurgeir Guðmundsson <sigurgeirg@localhost.localdomain>
2020-10-15 20:51:39 +0000

    testfiles added

--- apps/skilavottord/ws/jest.config.js
+++ apps/skilavottord/ws/jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  name: 'skilavottord-ws',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/skilavottord/ws',
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  name: 'reference-backend',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/reference-backend',
+  setupFiles: ['./test/environment.jest.ts'],
+  setupFilesAfterEnv: ['./test/setup.ts'],
+  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
 }
