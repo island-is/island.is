12e4e93746d74cd8f95501ae92e0c561383c816c
Jérémy Barbet <jeremgraph@gmail.com>
2020-10-16 14:50:18 +0000

    refactor: add mobile design and move the logic into utils functions (#1380)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/island-ui/core/src/lib/AsyncSelect/AsyncSelect.stories.tsx
+++ libs/island-ui/core/src/lib/AsyncSelect/AsyncSelect.stories.tsx
@@ -1,27 +1,19 @@
 import React from 'react'
 import { withDesign } from 'storybook-addon-designs'
+
+import { withFigma } from '../../utils/withFigma'
 import { AsyncSelect } from './AsyncSelect'
 import { Box } from '../Box/Box'
 import { ContentBlock } from '../ContentBlock/ContentBlock'
-
-const figmaLink =
-  'https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=50%3A101'
 
 export default {
   title: 'Form/AsyncSelect',
   component: AsyncSelect,
   decorators: [withDesign],
-  parameters: {
-    docs: {
-      description: {
-        component: `[View in Figma](${figmaLink})`,
-      },
-    },
-    design: {
-      type: 'figma',
-      url: figmaLink,
-    },
-  },
+  parameters: withFigma({
+    desktop:
+      'https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=50%3A101',
+  }),
 }
 
 export const Default = () => (
