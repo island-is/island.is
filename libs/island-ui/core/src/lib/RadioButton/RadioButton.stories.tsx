76b25bc48f797c25287edc861f6868ea6d5997e1
Jérémy Barbet <jeremgraph@gmail.com>
2020-10-16 14:50:18 +0000

    refactor: add mobile design and move the logic into utils functions (#1380)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/island-ui/core/src/lib/RadioButton/RadioButton.stories.tsx
+++ libs/island-ui/core/src/lib/RadioButton/RadioButton.stories.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import { withDesign } from 'storybook-addon-designs'
+
+import { withFigma } from '../../utils/withFigma'
 import { RadioButton } from './RadioButton'
 import { Box } from '../Box/Box'
-
-const figmaLink =
-  'https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=49%3A135'
 
 export default {
   title: 'Form/RadioButton',
   component: RadioButton,
   decorators: [withDesign],
-  parameters: {
-    docs: {
-      description: {
-        component: `[View in Figma](${figmaLink})`,
-      },
-    },
-    design: {
-      type: 'figma',
-      url: figmaLink,
-    },
-  },
+  parameters: withFigma({
+    desktop:
+      'https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=49%3A135',
+    mobile:
+      'https://www.figma.com/file/rU3mPM1cLfHa3u7TWuutPQ/UI-Library-%E2%80%93-%F0%9F%93%B1Mobile?node-id=30%3A2',
+  }),
 }
 
 const Template = (args) => <RadioButton {...args} />
