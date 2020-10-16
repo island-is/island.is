49eb776bb97cd78f4d20c6cc7dbc26f1fcf619db
Atli Guðlaugsson <atli@sendiradid.is>
2020-10-16 09:23:47 +0000

    remove dev state from store

--- apps/service-portal/src/store/store.ts
+++ apps/service-portal/src/store/store.ts
@@ -19,12 +19,8 @@ export interface StoreState {
   routes: ServicePortalRoute[]
 }
 
-const userObject = JSON.parse(
-  '{"id_token":"a.e.b","session_state":"a.b","access_token":"a.b.c","refresh_token":"a","token_type":"Bearer","scope":"openid profile offline_access","profile":{"s_hash":"sss","sid":"98","sub":"FA","auth_time":1602141813,"idp":"islykill","amr":["external"],"name":"Tester Testerson","natreg":"1231231234","nat":"IS"},"expires_at":1602152675,"state":"/minar-upplysingar"}',
-)
-
 export const initialState: StoreState = {
-  userInfo: userObject,
+  userInfo: null,
   userInfoState: 'passive',
   modules: modules,
   navigationState: 'passive',
