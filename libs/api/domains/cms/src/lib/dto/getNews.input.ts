30271a8db6bff96fcb50a8e9fdb6704a366747a4
Stefán Harald <stefanhar@users.noreply.github.com>
2020-10-16 11:54:53 +0000

    Feature/api web news tweeks (#1352)
    * Working elastic news and cleanup
    
    * Added aggregation for news dates
    
    * Fixed format
    
    * Added date aggregation
    
    * Added news list
    
    * Added months to news list
    
    * Fixed format
    
    * Made months int
    
    * Added order
    
    * Made types more consistent
    
    * Removed news list dto
    
    * Added comment
    
    * Moved defaults to dto
    
    * Fixed lint error
    
    * Fix for broken build
    
    * Removed changes fiel
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/cms/src/lib/dto/getNews.input.ts
+++ libs/api/domains/cms/src/lib/dto/getNews.input.ts
@@ -1,11 +1,42 @@
 import { Field, InputType, Int } from '@nestjs/graphql'
-import { IsInt, IsOptional, IsString } from 'class-validator'
+import {
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator'
 
 @InputType()
 export class GetNewsInput {
-  @Field()
+  @Field({ nullable: true })
   @IsString()
-  lang: string
+  @IsOptional()
+  lang?: string = 'is'
+
+  @Field(() => Int, { nullable: true })
+  @IsInt()
+  @IsOptional()
+  year?: number
+
+  @Field(() => Int, { nullable: true })
+  @IsInt()
+  @Min(1)
+  @Max(12)
+  @IsOptional()
+  month?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+
+  @Field({ nullable: true })
+  @IsEnum(['asc', 'desc'])
+  @IsOptional()
+  order?: 'asc' | 'desc' = 'desc'
+
+  @Field(() => Int, { nullable: true })
+  @IsInt()
+  @IsOptional()
+  page?: number = 1
 
   @Field(() => Int, { nullable: true })
   @IsInt()
