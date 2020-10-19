3bf89aeeccb0782f888c07d8d69974ec1ad1c649
Jérémy Barbet <jeremgraph@gmail.com>
2020-10-19 15:11:41 +0000

    chore: sentry setup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,6 @@
 const withTreat = require('next-treat')()
 const withHealthcheckConfig = require('./next-modules/withHealthcheckConfig')
 
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 // These modules need to be transpiled for IE11 support. This is not ideal,
 // we should aim to drop IE11 support, or only use dependencies that have
 // ES5 code (optionally plus ES6 module syntax).
@@ -14,12 +12,20 @@ const transpileModules = [
 ]
 const withTM = require('next-transpile-modules')(transpileModules)
 
-const { API_URL = 'http://localhost:4444' } = process.env
+const { API_URL = 'http://localhost:4444', SENTRY_DSN } = process.env
 const graphqlPath = '/api/graphql'
 
 module.exports = withTreat(
   withTM(
     withHealthcheckConfig({
+      webpack: (config, options) => {
+        if (!options.isServer) {
+          config.resolve.alias['@sentry/node'] = '@sentry/browser'
+        }
+
+        return config
+      },
+
       cssModules: false,
       serverRuntimeConfig: {
         // Will only be available on the server side
@@ -31,6 +37,7 @@ module.exports = withTreat(
         // Will be available on both server and client
         graphqlUrl: '',
         graphqlEndpoint: graphqlPath,
+        SENTRY_DSN,
       },
     }),
   ),
