a2fdec1afd439d9590a13b4e76b382a7b788e010
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2020-10-19 18:31:59 +0000

    Custom modal for process link content

---
+++ libs/island-ui/contentful/src/lib/IframeModal/IframeModal.tsx
@@ -0,0 +1,107 @@
+import React, {
+  FC,
+  forwardRef,
+  ReactElement,
+  Ref,
+  useEffect,
+  useState,
+  useLayoutEffect,
+  useRef,
+} from 'react'
+import { Box, Icon, Text } from '@island.is/island-ui/core'
+import {
+  useDialogState,
+  Dialog,
+  DialogDisclosure,
+  DialogBackdrop,
+  DialogProps,
+} from 'reakit/Dialog'
+
+import * as styles from './IframeModal.treat'
+
+interface IframeModalProps {
+  title?: string
+  label?: string
+  closeWindowLabel?: string
+  baseId: string
+  src: string
+  animated?: boolean
+  disclosure?: ReactElement
+}
+
+export const IframeModalDiv = forwardRef(
+  (props: DialogProps, ref: Ref<HTMLDivElement>) => {
+    const [mounted, setMounted] = useState(false)
+
+    useLayoutEffect(function () {
+      setMounted(true)
+    }, [])
+
+    return mounted && <div className={styles.backdrop} {...props} ref={ref} />
+  },
+)
+
+export const IframeModal: FC<IframeModalProps> = ({
+  title,
+  label,
+  closeWindowLabel = 'Loka glugga',
+  animated = true,
+  baseId,
+  src,
+  disclosure,
+}) => {
+  const initialFocusedRef = useRef(null)
+
+  const dialog = useDialogState({
+    animated,
+    baseId,
+  } as DialogProps)
+
+  useEffect(() => {
+    if (dialog.visible && initialFocusedRef.current) {
+      initialFocusedRef.current.focus()
+    }
+  }, [dialog.visible, initialFocusedRef])
+
+  return (
+    <>
+      {!!disclosure && (
+        <DialogDisclosure {...dialog} {...disclosure.props}>
+          {(disclosureProps) => React.cloneElement(disclosure, disclosureProps)}
+        </DialogDisclosure>
+      )}
+      <DialogBackdrop {...dialog} as={IframeModalDiv}>
+        <Dialog
+          {...dialog}
+          role="dialog"
+          aria-label={label}
+          className={styles.dialog}
+        >
+          <Box
+            display="flex"
+            marginLeft={2}
+            width="full"
+            alignItems="center"
+            className={styles.head}
+          >
+            <div className={styles.heading} title={title}>
+              <Text truncate variant="h3" as="h1">
+                {title}
+              </Text>
+            </div>
+            <button
+              onClick={dialog.hide}
+              className={styles.close}
+              aria-label={closeWindowLabel}
+            >
+              <Icon size="large" color="blue400" icon="closeCircle" />
+            </button>
+          </Box>
+          <iframe ref={initialFocusedRef} className={styles.iframe} src={src} />
+        </Dialog>
+      </DialogBackdrop>
+    </>
+  )
+}
+
+export default IframeModal
--- libs/island-ui/contentful/src/lib/ProcessEntry/ProcessEntry.tsx
+++ libs/island-ui/contentful/src/lib/ProcessEntry/ProcessEntry.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from 'react'
 import {
   Text,
-  ButtonDeprecated as Button,
+  Button,
   Box,
   GridRow,
   GridColumn,
   Stack,
+  Link,
+  ButtonProps,
 } from '@island.is/island-ui/core'
+import IframeModal from '../IframeModal/IframeModal'
+
 import * as styles from './ProcessEntry.treat'
 
 export const Titles: { [k: string]: string } = {
@@ -21,60 +25,94 @@ export const Titles: { [k: string]: string } = {
 export interface ProcessEntryProps {
   processTitle: string
   processLink: string
+  openLinkInModal: boolean
   type: string
   buttonText: string
+}
+
+export const ProcessEntryLinkButton: FC<
+  Omit<ProcessEntryProps, 'type'> & ButtonProps
+> = ({
+  processTitle,
+  buttonText,
+  processLink,
+  openLinkInModal,
+  ...buttonProps
+}) => {
+  const button = (
+    <Button icon="open" iconType="outline" {...buttonProps}>
+      {buttonText}
+    </Button>
+  )
+
+  return openLinkInModal ? (
+    <IframeModal
+      title={processTitle}
+      baseId="process-entry-modal-iframe"
+      disclosure={button}
+      src={processLink}
+    />
+  ) : (
+    <Link href={processLink}>{button}</Link>
+  )
 }
 
 export const ProcessEntry: FC<ProcessEntryProps> = ({
   processTitle,
   processLink,
+  openLinkInModal,
   type,
   buttonText,
 }) => {
   const label = Titles[type]
 
   return (
-    <Box width="full" background="blue100" borderRadius="large">
-      <GridRow className={styles.row}>
-        <GridColumn
-          className={styles.column}
-          span={['9/9', '9/9', '9/9', '9/9', '7/9']}
-          offset={['0', '0', '0', '0', '1/9']}
-        >
-          <Box
-            paddingY={4}
-            paddingX={[3, 3, 3, 3, 0]}
-            display="flex"
-            flexGrow={1}
-            flexDirection={['column', 'column', 'row', 'row', 'row']}
+    <>
+      <Box width="full" background="blue100" borderRadius="large">
+        <GridRow className={styles.row}>
+          <GridColumn
+            className={styles.column}
+            span={['9/9', '9/9', '9/9', '9/9', '7/9']}
+            offset={['0', '0', '0', '0', '1/9']}
           >
-            <Box flexGrow={1}>
-              <Stack space={1}>
-                {Boolean(label) && (
-                  <Text variant="eyebrow" color="blue400">
-                    {label}
-                  </Text>
-                )}
-                <Text variant="h3" as="h3">
-                  {processTitle}
-                </Text>
-              </Stack>
-            </Box>
             <Box
-              flexShrink={0}
-              paddingTop={[3, 3, 3, 3, 0]}
-              paddingLeft={[0, 0, 0, 0, 8]}
-              alignItems="flexStart"
-              justifyContent="flexStart"
+              paddingY={4}
+              paddingX={[3, 3, 3, 3, 0]}
+              display="flex"
+              flexGrow={1}
+              flexDirection={['column', 'column', 'row', 'row', 'row']}
             >
-              <Button noWrap href={processLink} icon="external">
-                {buttonText}
-              </Button>
+              <Box flexGrow={1}>
+                <Stack space={1}>
+                  {Boolean(label) && (
+                    <Text variant="eyebrow" color="blue400">
+                      {label}
+                    </Text>
+                  )}
+                  <Text variant="h3" as="h3">
+                    {processTitle}
+                  </Text>
+                </Stack>
+              </Box>
+              <Box
+                flexShrink={0}
+                paddingTop={[3, 3, 3, 3, 0]}
+                paddingLeft={[0, 0, 0, 0, 8]}
+                alignItems="flexStart"
+                justifyContent="flexStart"
+              >
+                <ProcessEntryLinkButton
+                  processTitle={processTitle}
+                  processLink={processLink}
+                  openLinkInModal={openLinkInModal}
+                  buttonText={buttonText}
+                />
+              </Box>
             </Box>
-          </Box>
-        </GridColumn>
-      </GridRow>
-    </Box>
+          </GridColumn>
+        </GridRow>
+      </Box>
+    </>
   )
 }
 
