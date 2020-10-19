d5b7d81f249ea7015e7f8f6d5c7c10cd6bdfa7c9
Ívar Oddsson <ivaroddsson@gmail.com>
2020-10-19 11:02:04 +0000

    Judicial system/required pt5 (#1404)
    * Added favicon
    
    * Refactor required fields for ruling step one route
    
    * Added tests
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/routes/Judge/Ruling/Ruling.spec.tsx
+++ apps/judicial-system/web/src/routes/Judge/Ruling/Ruling.spec.tsx
@@ -1,12 +1,50 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
-import { RulingStepTwo } from './'
+import { act, render, waitFor } from '@testing-library/react'
+import { RulingStepOne, RulingStepTwo } from './'
 import * as Constants from '../../../utils/constants'
 import { CaseAppealDecision } from '@island.is/judicial-system/types'
 import userEvent from '@testing-library/user-event'
 import fetchMock from 'fetch-mock'
 
 describe('Ruling routes', () => {
+  describe(Constants.RULING_STEP_ONE_ROUTE, () => {
+    test('should now allow users to continue unless every required field has been filled out', async () => {
+      // Arrange
+
+      // Have custodyEndDate in localstorage because it's hard to use the datepicker with useEvents
+      Storage.prototype.getItem = jest.fn(() => {
+        return JSON.stringify({
+          custodyEndDate: '2020-10-24',
+        })
+      })
+
+      Storage.prototype.setItem = jest.fn()
+
+      // Act and Assert
+      const { getByTestId } = render(<RulingStepOne />)
+
+      await act(async () => {
+        await userEvent.type(
+          getByTestId('ruling') as HTMLInputElement,
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non igitur bene. Idem fecisset Epicurus, si sententiam hanc, quae nunc Hieronymi est, coniunxisset cum Aristippi vetere sententia. Respondent extrema primis, media utrisque, omnia omnibus. Nam prius a se poterit quisque discedere quam appetitum earum rerum, quae sibi conducant, amittere. Duo Reges: constructio interrete. Sed quae tandem ista ratio est?',
+        )
+        userEvent.tab()
+        expect(
+          (getByTestId('continueButton') as HTMLButtonElement).disabled,
+        ).toBe(true)
+
+        await userEvent.type(
+          getByTestId('custodyEndTime') as HTMLInputElement,
+          '12:31',
+        )
+        userEvent.tab()
+        expect(
+          (getByTestId('continueButton') as HTMLButtonElement).disabled,
+        ).toBe(false)
+      })
+    })
+  })
+
   describe(Constants.RULING_STEP_TWO_ROUTE, () => {
     test('should not have a selected radio button by default', () => {
       // Arrange
