f00b0ef456fa8f82f160471890c2e8530f2b1248
valurefugl <65780958+valurefugl@users.noreply.github.com>
2020-10-20 14:26:42 +0000

    Removed domain from db and models. Fixed active bug in dto. (#1436)
    * Removed domain from db and models. Fixed active bug in dto.
    
    * Formatted.

--- libs/auth-api-lib/src/lib/entities/models/api-resource-scope.model.ts
+++ libs/auth-api-lib/src/lib/entities/models/api-resource-scope.model.ts
@@ -26,17 +26,6 @@ export class ApiResourceScope extends Model<ApiResourceScope> {
     allowNull: false,
   })
   @ForeignKey(() => ApiResource)
-  @ApiProperty({
-    example: 'domain',
-  })
-  domain: string
-
-  @PrimaryKey
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
-  @ForeignKey(() => ApiResource)
   @ApiProperty()
   apiResourceName: string
 
