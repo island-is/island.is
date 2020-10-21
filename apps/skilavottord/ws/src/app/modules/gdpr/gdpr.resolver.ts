08597aae66757b6eecf2c86879adcbcd136e0781
Sigurgeir Gudmundsson <16959841+essgje@users.noreply.github.com>
2020-10-21 13:12:07 +0000

    Feature dbseqgraps (#1464)
    * Logger update
    
    * format issue
    
    Co-authored-by: Sigurgeir Guðmundsson <sigurgeirg@localhost.localdomain>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/skilavottord/ws/src/app/modules/gdpr/gdpr.resolver.ts
+++ apps/skilavottord/ws/src/app/modules/gdpr/gdpr.resolver.ts
@@ -2,15 +2,20 @@ import { Inject } from '@nestjs/common'
 import { Query, Resolver, Float, Args } from '@nestjs/graphql'
 import { GdprService } from './gdpr.service'
 import { GdprModel } from './model/gdpr.model'
-// import { GdprService } from './models'
+import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
 
 @Resolver(() => GdprModel)
 export class GdprResolver {
-  constructor(@Inject(GdprService) private gdprService: GdprService) {}
+  constructor(
+    @Inject(GdprService) private gdprService: GdprService,
+    @Inject(LOGGER_PROVIDER) private logger: Logger,
+  ) {}
 
   @Query(() => [GdprModel])
-  async getAllgdprs(): Promise<GdprModel[]> {
-    return await this.gdprService.findAll()
+  async getAllGdprs(): Promise<GdprModel[]> {
+    const res = this.gdprService.findAll()
+    this.logger.debug('getAllGdrps responce:' + JSON.stringify(res, null, 2))
+    return res
   }
 
   // @Query(() => Gdpr)
