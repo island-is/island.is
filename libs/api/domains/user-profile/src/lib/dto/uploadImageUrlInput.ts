54397d8ec82f66bb248bf5b9819f39a88f1e7824
Olafur <obmagnusson@gmail.com>
2020-10-21 13:44:42 +0000

    rename

--- libs/api/domains/user-profile/src/lib/dto/uploadImageUrlInput.ts
+++ libs/api/domains/user-profile/src/lib/dto/uploadImageUrlInput.ts
@@ -2,7 +2,7 @@ import { Field, InputType } from '@nestjs/graphql'
 import { IsString } from 'class-validator'
 
 @InputType()
-export class UploadImageUrl {
+export class UploadImageUrlInput {
   @Field(() => String)
   @IsString()
   uploadUrl!: string
