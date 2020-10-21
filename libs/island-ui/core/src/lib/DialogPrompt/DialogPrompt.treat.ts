7420391717b05eb21dc773aa27cceac3928f1cfc
David Bachmann <dabbi@dabbi.is>
2020-10-21 15:21:35 +0000

    Create a hexToRgba util exported from island-ui/utils

--- libs/island-ui/core/src/lib/DialogPrompt/DialogPrompt.treat.ts
+++ libs/island-ui/core/src/lib/DialogPrompt/DialogPrompt.treat.ts
@@ -1,15 +1,6 @@
 import { style } from 'treat'
 import { theme } from '@island.is/island-ui/theme'
-
-const hexToRgb = (hex: string) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-
-  return result
-    ? `${parseInt(result[1], 16)}, ` +
-        `${parseInt(result[2], 16)}, ` +
-        `${parseInt(result[3], 16)}`
-    : null
-}
+import { hexToRgba } from '../../utils/hexToRgba'
 
 const spacing = theme.spacing[3]
 
@@ -21,7 +12,7 @@ export const backdrop = style({
   height: '100%',
   overflowX: 'hidden',
   overflowY: 'auto',
-  backgroundColor: `rgba(${hexToRgb(theme.color.blue100)}, 0.7)`,
+  backgroundColor: hexToRgba(theme.color.blue100, 0.7),
   transition: `opacity 250ms ease-in-out`,
   opacity: 0,
   zIndex: 10000,
