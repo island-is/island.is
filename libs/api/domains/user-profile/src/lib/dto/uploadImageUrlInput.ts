eb77854407a1044ab63b9f4079b804482d7a4dfc
Olafur <obmagnusson@gmail.com>
2020-10-21 14:14:48 +0000

    remove input and touch dto in service

--- libs/api/domains/user-profile/src/lib/dto/uploadImageUrlInput.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql'
-import { IsString } from 'class-validator'
-
-@InputType()
-export class UploadImageUrlInput {
-  @Field(() => String)
-  @IsString()
-  uploadUrl!: string
-}
