97880b29409df6fc36c69d4fdd0e8725a71c4a39
David Bachmann <dabbi@dabbi.is>
2020-10-22 14:19:38 +0000

    Make buttons optional

--- libs/island-ui/core/src/lib/DialogPrompt/DialogPrompt.stories.tsx
+++ libs/island-ui/core/src/lib/DialogPrompt/DialogPrompt.stories.tsx
@@ -24,6 +24,34 @@ export const Default = () => {
       disclosureElement={<Button variant="primary">Open dialog</Button>}
       onConfirm={() => console.log('Confirmed')}
       onCancel={() => console.log('Cancelled')}
+      buttonTextConfirm="Confirm"
+      buttonTextCancel="Cancel"
+    />
+  )
+}
+
+export const NoButtons = () => {
+  return (
+    <DialogPrompt
+      baseId="demo_dialog"
+      title="With no buttons this acts as an alert"
+      description="It's probably a good idea to include at least one button."
+      ariaLabel="Use aria-label to explain what this is doing"
+      disclosureElement={<Button variant="primary">Open dialog</Button>}
+    />
+  )
+}
+
+export const SingleButton = () => {
+  return (
+    <DialogPrompt
+      baseId="demo_dialog"
+      title="Single button"
+      description="It's good practice to include at least a single button for the user to acknowledge the message."
+      ariaLabel="Use aria-label to explain what this is doing"
+      disclosureElement={<Button variant="primary">Open dialog</Button>}
+      buttonTextConfirm="Okay"
+      onConfirm={() => console.log('Okay')}
     />
   )
 }
--- libs/island-ui/core/src/lib/DialogPrompt/DialogPrompt.tsx
+++ libs/island-ui/core/src/lib/DialogPrompt/DialogPrompt.tsx
@@ -40,79 +40,89 @@ export const DialogPrompt = ({
   disclosureElement,
   onConfirm,
   onCancel,
-  buttonTextCancel = 'Cancel',
-  buttonTextConfirm = 'Confirm',
-}: DialogPromptProps) => (
-  <ModalBase
-    disclosure={disclosureElement}
-    baseId={baseId}
-    aria-label={ariaLabel}
-    className={styles.dialog}
-  >
-    {({ closeModal }) => {
-      const handleClose = () => {
-        onCancel && onCancel()
-        closeModal()
-      }
-      const handleConfirm = () => {
-        onConfirm && onConfirm()
-        closeModal()
-      }
-      return (
-        <Box
-          position="relative"
-          width="full"
-          marginX={3}
-          paddingX={[3, 4, 4, 8]}
-          paddingY={[6, 6, 6, 12]}
-          borderRadius="large"
-          background="white"
-          className={styles.content}
-        >
-          <GridContainer position="none">
-            <FocusableBox
-              component="button"
-              onClick={handleClose}
-              className={styles.close}
-            >
-              <Icon icon="close" color="blue400" size="medium" />
-            </FocusableBox>
-            <Text variant="h2" as="h3" paddingBottom={2}>
-              {title}
-            </Text>
-            {description && (
-              <Text variant="intro" paddingBottom={2}>
-                {description}
+  buttonTextCancel,
+  buttonTextConfirm,
+}: DialogPromptProps) => {
+  const hasButtons = !!buttonTextCancel || !!buttonTextConfirm
+
+  return (
+    <ModalBase
+      disclosure={disclosureElement}
+      baseId={baseId}
+      aria-label={ariaLabel}
+      className={styles.dialog}
+    >
+      {({ closeModal }) => {
+        const handleClose = () => {
+          onCancel?.()
+          closeModal()
+        }
+        const handleConfirm = () => {
+          onConfirm?.()
+          closeModal()
+        }
+        return (
+          <Box
+            position="relative"
+            width="full"
+            marginX={3}
+            paddingX={[3, 4, 4, 8]}
+            paddingY={[6, 6, 6, 12]}
+            borderRadius="large"
+            background="white"
+            className={styles.content}
+          >
+            <GridContainer position="none">
+              <FocusableBox
+                component="button"
+                onClick={handleClose}
+                className={styles.close}
+              >
+                <Icon icon="close" color="blue400" size="medium" />
+              </FocusableBox>
+              <Text variant="h2" as="h3" paddingBottom={2}>
+                {title}
               </Text>
-            )}
-            <GridRow align="spaceBetween">
-              <GridColumn
-                span={['12/12', '12/12', '1/3']}
-                paddingTop={[2, 2, 7]}
-              >
-                <Button
-                  size="default"
-                  variant="ghost"
-                  onClick={handleClose}
-                  fluid
-                >
-                  {buttonTextCancel}
-                </Button>
-              </GridColumn>
-              <GridColumn
-                span={['12/12', '12/12', '1/3']}
-                paddingTop={[2, 2, 7]}
-              >
-                <Button size="default" onClick={handleConfirm} fluid>
-                  {buttonTextConfirm}
-                </Button>
-              </GridColumn>
-            </GridRow>
-          </GridContainer>
-        </Box>
-      )
-    }}
-  </ModalBase>
-)
+              {description && (
+                <Text variant="intro" paddingBottom={2}>
+                  {description}
+                </Text>
+              )}
+              {hasButtons && (
+                <GridRow align="spaceBetween">
+                  {buttonTextCancel && (
+                    <GridColumn
+                      span={['12/12', '12/12', '1/3']}
+                      paddingTop={[2, 2, 7]}
+                    >
+                      <Button
+                        size="default"
+                        variant="ghost"
+                        onClick={handleClose}
+                        fluid
+                      >
+                        {buttonTextCancel}
+                      </Button>
+                    </GridColumn>
+                  )}
+                  {buttonTextConfirm && (
+                    <GridColumn
+                      span={['12/12', '12/12', '1/3']}
+                      paddingTop={[2, 2, 7]}
+                    >
+                      <Button size="default" onClick={handleConfirm} fluid>
+                        {buttonTextConfirm}
+                      </Button>
+                    </GridColumn>
+                  )}
+                </GridRow>
+              )}
+            </GridContainer>
+          </Box>
+        )
+      }}
+    </ModalBase>
+  )
+}
 
 export default DialogPrompt
