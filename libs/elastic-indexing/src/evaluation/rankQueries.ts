8a53ab0073f2528af6e969e0ec8297bba5036036
Haukur Hlöðversson <haukur@haukurh.is>
2020-10-22 16:41:24 +0000

    Added more rank evaluations test cases

--- libs/elastic-indexing/src/evaluation/rankQueries.ts
+++ libs/elastic-indexing/src/evaluation/rankQueries.ts
@@ -68,8 +68,58 @@ export const rankQueries: Request[] = [
     ratings: [
       { _index: '_all', _id: '6rtr288qVOjyddl2LVroNi', rating: 10 }, // title: Skattur af vöxtum og arði
       { _index: '_all', _id: '6e3SWIyt0ayXwSuqB4HiiE', rating: 9 }, // title: Persónuafsláttur og skattur af launum
-      { _index: '_all', _id: '3vTJup1Nk3lwB6AtzhX1ps', rating: 7 }, // title: Að stofna fyrirtæki
-      { _index: '_all', _id: '1TTXfamiqT1ouLc46OwRLq', rating: 6 }, // title: Bifreiðatryggingar og -gjöld
+      { _index: '_all', _id: '3vTJup1Nk3lwB6AtzhX1ps', rating: 8 }, // title: Að stofna fyrirtæki
+      { _index: '_all', _id: '723lCFxDq2HT0Ev3T6PpC1', rating: 7 }, // title: Beiðni um vottorð frá Skattinum
+      { _index: '_all', _id: '5xEWjCVcXPyiiB3TnIKNYV', rating: 6 }, // title: Um skatta, afslætti og frádrátt fyrir fatlað fólk
+      { _index: '_all', _id: '7sXI0Ni8XNCeiSsqHKVEl2', rating: 5 }, // title: Skattalegt heimilisfesti námsmanna erlendis
+      { _index: '_all', _id: '3vvl6CJg3e1xaWY2PMQITi', rating: 4 }, // title: Lækkun tekjuskattsstofns
+    ],
+  },
+  {
+    id: 'highly-compounded-word',
+    request: 'krókaaflahlutdeild',
+    ratings: [
+      { _index: '_all', _id: '2h5ORZuiMEVtyD4cZ9y0lm', rating: 10 }, // title: Flutningur krókaaflahlutdeildar
+      { _index: '_all', _id: '1ylpI2uwQqe9DGOmN9rGrm', rating: 9 }, // title: Skipti krókaaflamarks og aflamarks
+      { _index: '_all', _id: '4rVlMI2YRoNhrpdXHjHQuR', rating: 8 }, // title: Flutningur aflahlutdeildar
+      { _index: '_all', _id: '5HSAXc9bvZ5SAQieDaHdDn', rating: 7 }, // title: Flutningur krókaaflamarks
+      { _index: '_all', _id: '3GvUzzXXN8pH0hemVZeQ9x', rating: 6 }, // title: Umsókn um rafræna afladagbók fyrir skip
+      { _index: '_all', _id: 'RxshrDyWeuAVSsDZmrnP9', rating: 5}, // title: Aflamark til frístundaskipa
+      { _index: '_all', _id: '4dH6N62YJguMRZysu31xqo', rating: 4}, // title: Flutningur aflamarks
+      { _index: '_all', _id: '1rGrVudguQJXzMzdbr9b66', rating: 3}, // title: Afli uppsjávarveiðiskips
+      { _index: '_all', _id: '6Z3CrVVjcWDPswYM2RkQr9', rating: 2}, // title: Áætlaður úthafskarfaafli
+    ],
+  },
+  {
+    id: 'pool-popular-words',
+    request: 'stofnun fyrirtækis upplýsingar',
+    ratings: [
+      { _index: '_all', _id: 'VM5OiLpdHHInwHoSJLb53', rating: 10 }, // title: Stofnun fyrirtækis – almennar upplýsingar
+      { _index: '_all', _id: '3vTJup1Nk3lwB6AtzhX1ps', rating: 10 }, // title: Að stofna fyrirtæki
+      { _index: '_all', _id: '33ZXB46KnROb8nyYwRvJ3U', rating: 10 }, // title: Að stofna fyrirtæki
+    ],
+  },
+  {
+    id: 'specific-word-with-popular-word',
+    request: 'svæðisáætlun sveitarfélag',
+    ratings: [
+      { _index: '_all', _id: '1TtMsHpkgKjTh8lqVKOIu3', rating: 10 }, // title: Svæðisáætlun sveitarfélaga
+    ],
+  },
+  {
+    id: 'specific-word-with-popular-word',
+    request: 'svæðisáætlun sveitarfélag',
+    ratings: [
+      { _index: '_all', _id: '1TtMsHpkgKjTh8lqVKOIu3', rating: 10 }, // title: Svæðisáætlun sveitarfélaga
+    ],
+  },
+  {
+    id: 'semi-irrelevant-content-popular-words',
+    request: 'félag eldri borgara',
+    ratings: [
+      { _index: '_all', _id: '5IACQSPuDm0tOazEpVRgfY', rating: 10 }, // title: Félagsstarf og þjónustumiðstöðvar fyrir eldri borgara
+      { _index: '_all', _id: '6uvcCPQ7VIrMPH6qBlTf0r', rating: 1 }, // title: Styrkir, bætur og kjör
+      { _index: '_all', _id: '5SMAerVNcWJZ5dsVBLKkAL', rating: 1 }, // title: Að fara á eftirlaun
     ],
   },
 ]
