078aeea897125031e8f90b8f9a95ce0c51a6212f
Sigga Hrafns <sigridur@kosmosogkaos.is>
2020-10-22 15:19:06 +0000

    Tell me your story component, UI and form functionality only

---
+++ apps/web/screens/AboutPage/TellMeYourStory.treat.ts
@@ -0,0 +1,19 @@
+import { style } from 'treat'
+
+export const topImage = style({
+  marginBottom: '-150px',
+  width: '100%',
+})
+
+export const contentImage = style({
+  maxHeight: '318px',
+  maxWidth: '318px',
+})
+
+export const alignSelfCenter = style({
+  alignSelf: 'center',
+})
+
+export const justifyContentFlexEnd = style({
+  justifyContent: 'flex-end',
+})
