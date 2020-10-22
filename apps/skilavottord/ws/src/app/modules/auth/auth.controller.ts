74fb92371390ff433be2ee1e857d2da2c3dd2677
Tómas Árni Jónsson <69652832+tommigitgaur@users.noreply.github.com>
2020-10-22 17:08:53 +0000

    Feature debugdev (#1502)
    * debug on dev environment
    
    * degug on dev env
    
    * debug
    
    * Athentication for companies
    
    * Auth and debug
    
    * auth and debug
    
    * auth and debug
    
    * auth and debug2
    
    Co-authored-by: Jonsson <tjonsson@deloitte.is>

--- apps/skilavottord/ws/src/app/modules/auth/auth.controller.ts
+++ apps/skilavottord/ws/src/app/modules/auth/auth.controller.ts
@@ -20,9 +20,20 @@ import {
   SSN_IS_NOT_A_PERSON,
 } from '@island.is/skilavottord/consts'
 import { environment } from '../../../environments'
-import { Cookie, CookieOptions, Credentials, VerifyResult } from './auth.types'
 
-const { samlEntryPoint, audience: audienceUrl, jwtSecret } = environment.auth
+//import { UserResolver } from './../user/user.resolver'
+
+import { Cookie, CookieOptions, Credentials, VerifyResult } from './auth.types'
+import { Role, AuthUser } from './auth.types'
+import { AuthService } from './auth.service'
+
+const {
+  samlEntryPoint,
+  samlEntryPoint2,
+  audience: audienceUrl,
+  jwtSecret,
+} = environment.auth
+//const { samlEntryPoint2, audience: audienceUrl2, jwtSecret2 } = environment.auth
 
 const JWT_EXPIRES_IN_SECONDS = 3600
 const ONE_HOUR = 60 * 60 * 1000
@@ -59,19 +70,18 @@ const loginIS = new IslandisLogin({
   audienceUrl,
 })
 
-@Controller('/api/auth/citizen')
+@Controller('/api/auth')
 export class AuthController {
   constructor(@Inject(LOGGER_PROVIDER) private logger: Logger) {}
 
-  @Post('/callback')
-  async callback(@Body('token') token, @Res() res, @Req() req) {
-    //console.log("Duddi ralli rai")
+  @Post('/citizen/callback')
+  async callback1(@Body('token') token, @Res() res, @Req() req) {
+    this.logger.info('--- /citizen/callback starting ---')
     let verifyResult: VerifyResult
     try {
       verifyResult = await loginIS.verify(token)
     } catch (err) {
       this.logger.error(err)
-      //console.log("Duddi ralli rai - error")
       return res.redirect('/error')
     }
 
@@ -79,7 +89,6 @@ export class AuthController {
     const { user } = verifyResult
     if (!user) {
       this.logger.error('Could not verify user authenticity')
-      //console.log("Duddi ralli rai - error 2")
       return res.redirect('/error')
     }
 
@@ -108,32 +117,20 @@ export class AuthController {
     }
 
     const maxAge = JWT_EXPIRES_IN_SECONDS * 1000
-    /*    this.logger.error('---user---')
-    this.logger.error(user)
-    this.logger.error('---csrfToken---')
-    this.logger.error(csrfToken)
-    this.logger.error('---jwtToken---')
-    this.logger.error(jwtToken)
-    this.logger.error('---CSRF_COOKIE---')
-    this.logger.error(CSRF_COOKIE)
-    this.logger.error('---ACCESS_TOKEN_COOKIE---')
-    this.logger.error(ACCESS_TOKEN_COOKIE)
-    this.logger.error('---returnUrl---')
-    this.logger.error(returnUrl) */
-    console.log('---user---')
-    console.log(user)
-    console.log('---csrfToken---')
-    console.log(csrfToken)
-    console.log('---jwtToken---')
-    console.log(jwtToken)
-    console.log('---CSRF_COOKIE---')
-    console.log(CSRF_COOKIE)
-    console.log('---ACCESS_TOKEN_COOKIE---')
-    console.log(ACCESS_TOKEN_COOKIE)
-    console.log('---returnUrl---')
-    console.log(returnUrl)
-    //    console.log("---res---")
-    //    console.log(res)
+
+    this.logger.info(
+      `  - kennitala = ${user.kennitala}  nafn = ${user.fullname}   simi = ${user.mobile}`,
+    )
+    this.logger.info(`  - csrfToken = ${csrfToken}`)
+    this.logger.info(`  - CSRF_COOKIE = ${CSRF_COOKIE.name}`)
+    this.logger.info(`  - ACCESS_TOKEN_COOKIE = ${ACCESS_TOKEN_COOKIE.name}`)
+    this.logger.info(`  - returnUrl = ${returnUrl}`)
+    this.logger.info(`  - CSRF_COOKIE = ${CSRF_COOKIE.name}`)
+    //const authService = new AuthService()
+
+    //const RoleForUser: string = 'Citizen'
+    this.logger.info(`  - Role for ${user.fullname} is Citizen`)
+    this.logger.info(`--- /citizen/callback ending ---`)
     return res
       .cookie(CSRF_COOKIE.name, csrfToken, {
         ...CSRF_COOKIE.options,
@@ -143,25 +140,124 @@ export class AuthController {
         ...ACCESS_TOKEN_COOKIE.options,
         maxAge,
       })
+
       .redirect(returnUrl ?? '/_')
   }
 
-  @Get('/login')
-  login(@Res() res, @Query() query) {
-    console.log('---/login starting---')
+  @Post('/company/callback')
+  // async callback2(@Body('token') token, @Res() res, @Req() req) {
+  async callback2(@Body('token') token, @Res() res) {
+    this.logger.info('--- /company/callback starting ---')
+    let verifyResult: VerifyResult
+    try {
+      verifyResult = await loginIS.verify(token)
+    } catch (err) {
+      this.logger.error(err)
+      return res.redirect('/error')
+    }
+
+    // const { returnUrl } = req.cookies[REDIRECT_COOKIE_NAME] || {}
+    const { user } = verifyResult
+    if (!user) {
+      this.logger.error('Could not verify user authenticity')
+      return res.redirect('/error')
+    }
+
+    if (!kennitala.isPerson(user.kennitala)) {
+      this.logger.warn('User used company kennitala to log in')
+      return res.redirect(`/error?errorType=${SSN_IS_NOT_A_PERSON}`)
+    }
+
+    const csrfToken = new Entropy({ bits: 128 }).string()
+    const jwtToken = jwt.sign(
+      {
+        user: {
+          nationalId: user.kennitala,
+          name: user.fullname,
+          mobile: user.mobile,
+        },
+        csrfToken,
+      } as Credentials,
+      jwtSecret,
+      { expiresIn: JWT_EXPIRES_IN_SECONDS },
+    )
+
+    const tokenParts = jwtToken.split('.')
+    if (tokenParts.length !== 3) {
+      return res.redirect('/error')
+    }
+
+    const maxAge = JWT_EXPIRES_IN_SECONDS * 1000
+
+    this.logger.info(
+      `  - kennitala = ${user.kennitala}  nafn = ${user.fullname}   simi = ${user.mobile}`,
+    )
+    this.logger.info(`  - csrfToken = ${csrfToken}`)
+    this.logger.info(`  - CSRF_COOKIE = ${CSRF_COOKIE.name}`)
+    this.logger.info(`  - ACCESS_TOKEN_COOKIE = ${ACCESS_TOKEN_COOKIE.name}`)
+    this.logger.info(`  - CSRF_COOKIE = ${CSRF_COOKIE.name}`)
+    const authService = new AuthService()
+
+    const RoleUser: AuthUser = {
+      nationalId: user.kennitala,
+      mobile: user.mobile,
+      name: user.fullname,
+    }
+    //  RoleUser = { nationalId: user.kennitala, mobile: user.mobile, name: user.fullname}
+    let RoleForUser: Role = 'user'
+    RoleForUser = authService.getRole(RoleUser)
+
+    this.logger.info(`  - Role for ${user.fullname} is ${RoleForUser}`)
+    let returnUrlComp: string
+    if (RoleForUser.includes('RecyclingCompany')) {
+      returnUrlComp = '/deregister-vehicle'
+    } else if (RoleForUser.includes('RecyclingFund')) {
+      returnUrlComp = '/list-vehicles'
+    } else {
+      return '/error'
+    }
+    this.logger.info(`  - redirecting to ${returnUrlComp}`)
+    this.logger.info(`--- /company/callback ending ---`)
+    return res
+      .cookie(CSRF_COOKIE.name, csrfToken, {
+        ...CSRF_COOKIE.options,
+        maxAge,
+      })
+      .cookie(ACCESS_TOKEN_COOKIE.name, jwtToken, {
+        ...ACCESS_TOKEN_COOKIE.options,
+        maxAge,
+      })
+
+      .redirect(returnUrlComp ?? '/_')
+  }
+
+  @Get('/citizen/login')
+  login1(@Res() res, @Query() query) {
+    this.logger.info('--- /citizen/login starting ---')
     const { returnUrl } = query
     const { name, options } = REDIRECT_COOKIE
     res.clearCookie(name, options)
-    console.log('---returnUrl---')
-    console.log(returnUrl)
-    console.log('---samlEntryPoint---')
-    console.log(samlEntryPoint)
+    this.logger.info(`  - returnUrl = ${returnUrl}`)
+    this.logger.info(`  - samlEntryPoint = ${samlEntryPoint}`)
     return res
       .cookie(name, { returnUrl }, { ...options, maxAge: ONE_HOUR })
       .redirect(samlEntryPoint)
   }
 
-  @Get('/logout')
+  @Get('/company/login')
+  login2(@Res() res, @Query() query) {
+    this.logger.info('--- /company/login starting ---')
+    const { returnUrl } = query
+    const { name, options } = REDIRECT_COOKIE
+    res.clearCookie(name, options)
+    this.logger.info(`  - returnUrl = ${returnUrl}`)
+    this.logger.info(`  - samlEntryPoint = ${samlEntryPoint2}`)
+    return res
+      .cookie(name, { returnUrl }, { ...options, maxAge: ONE_HOUR })
+      .redirect(samlEntryPoint2)
+  }
+
+  @Get('/citizen/logout')
   logout(@Res() res) {
     res.clearCookie(ACCESS_TOKEN_COOKIE.name, ACCESS_TOKEN_COOKIE.options)
     res.clearCookie(CSRF_COOKIE.name, CSRF_COOKIE.options)
