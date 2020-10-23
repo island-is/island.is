e420d31ef7ca27383ce1b302eb9416da0a8b8c8b
Jérémy Barbet <jeremgraph@gmail.com>
2020-10-23 09:20:18 +0000

    fix: shorten util

--- libs/island-ui/utils/src/lib/escapeGrid.ts
+++ libs/island-ui/utils/src/lib/escapeGrid.ts
@@ -1,16 +1,6 @@
 import { theme } from '@island.is/island-ui/theme'
 
-export const escapeGrid = (gutter?: number) => {
-  if (gutter) {
-    return {
-      marginLeft: -gutter * 2,
-      marginRight: -gutter * 2,
-    }
-  }
-
-  // If gutter is not defined, we fallback to escape mobile gutter
-  return {
-    marginLeft: -theme.grid.gutter.mobile * 2,
-    marginRight: -theme.grid.gutter.mobile * 2,
-  }
-}
+export const escapeGrid = (gutter: number = theme.grid.gutter.mobile) => ({
+  marginLeft: -gutter * 2,
+  marginRight: -gutter * 2,
+})
