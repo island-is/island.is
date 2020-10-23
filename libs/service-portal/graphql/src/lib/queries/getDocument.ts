44cdf68e82fc206969d9270b8bf931865cd0550d
Andri Andresson <andriandresson@gmail.com>
2020-10-23 09:04:55 +0000

    chore: updated Get_Document gql query

--- libs/service-portal/graphql/src/lib/queries/getDocument.ts
+++ libs/service-portal/graphql/src/lib/queries/getDocument.ts
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client'
 export const GET_DOCUMENT = gql`
   query GetDocument($input: GetDocumentInput!) {
     getDocument(input: $input) {
-      url
+      content
     }
   }
 `
