b8824ef6f869d8e4c3e3cd9f0bd1d033b5e7b593
Tómas Árni Jónsson <69652832+tommigitgaur@users.noreply.github.com>
2020-10-24 15:04:15 +0000

    Feature debugdev (#1520)
    * debug on dev environment
    
    * degug on dev env
    
    * debug
    
    * Athentication for companies
    
    * Auth and debug
    
    * auth and debug
    
    * auth and debug
    
    * auth and debug2
    
    * debug and company auth, samEntryPoint set to fixed
    
    * fix
    
    * fix
    
    * fix
    
    Co-authored-by: Jonsson <tjonsson@deloitte.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/skilavottord/ws/src/app/modules/auth/auth.controller.ts
+++ apps/skilavottord/ws/src/app/modules/auth/auth.controller.ts
@@ -27,13 +27,7 @@ import { Cookie, CookieOptions, Credentials, VerifyResult } from './auth.types'
 import { Role, AuthUser } from './auth.types'
 import { AuthService } from './auth.service'
 
-const {
-  samlEntryPoint,
-  samlEntryPoint2,
-  audience: audienceUrl,
-  jwtSecret,
-} = environment.auth
-//const { samlEntryPoint2, audience: audienceUrl2, jwtSecret2 } = environment.auth
+const { samlEntryPoint, audience: audienceUrl, jwtSecret } = environment.auth
 
 const JWT_EXPIRES_IN_SECONDS = 3600
 const ONE_HOUR = 60 * 60 * 1000
@@ -247,6 +241,7 @@ export class AuthController {
   @Get('/company/login')
   login2(@Res() res, @Query() query) {
     this.logger.info('--- /company/login starting ---')
+    const samlEntryPoint2 = 'https://innskraning.island.is/?id=sv_company.local'
     const { returnUrl } = query
     const { name, options } = REDIRECT_COOKIE
     res.clearCookie(name, options)
