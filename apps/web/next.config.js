00a604469d3273fc80d5d6275db273c8c00b5ddc
Jérémy Barbet <jeremgraph@gmail.com>
2020-10-26 13:23:48 +0000

    chore: add analyze script

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
 const withTreat = require('next-treat')()
 const withHealthcheckConfig = require('./next-modules/withHealthcheckConfig')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 // These modules need to be transpiled for IE11 support. This is not ideal,
 // we should aim to drop IE11 support, or only use dependencies that have
@@ -11,34 +12,46 @@ const transpileModules = [
   'escape-string-regexp', // Used by slugify.
 ]
 const withTM = require('next-transpile-modules')(transpileModules)
-
 const { API_URL = 'http://localhost:4444', SENTRY_DSN } = process.env
 const graphqlPath = '/api/graphql'
 
 module.exports = withTreat(
   withTM(
     withHealthcheckConfig({
-      webpack: (config, options) => {
-        if (!options.isServer) {
+      webpack: (config, { isServer }) => {
+        if (!isServer) {
           config.resolve.alias['@sentry/node'] = '@sentry/browser'
+        }
+
+        if (process.env.ANALYZE === 'true') {
+          config.plugins.push(
+            new BundleAnalyzerPlugin({
+              analyzerMode: 'server',
+              analyzerPort: isServer ? 8888 : 8889,
+              openAnalyzer: true,
+            }),
+          )
         }
 
         return config
       },
 
       cssModules: false,
+
       serverRuntimeConfig: {
         // Will only be available on the server side
         // Requests made by the server are internal request made directly to the api hostname
         graphqlUrl: API_URL,
         graphqlEndpoint: graphqlPath,
       },
+
       publicRuntimeConfig: {
         // Will be available on both server and client
         graphqlUrl: '',
         graphqlEndpoint: graphqlPath,
         SENTRY_DSN,
       },
+
       env: {
         API_MOCKS: process.env.API_MOCKS,
       },
