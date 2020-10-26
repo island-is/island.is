2d934f913c586240eca564dd656822d26332f304
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2020-10-26 12:12:23 +0000

    handle fallbacks in translations service instead of in the translation contentful extension

--- libs/api/domains/translations/src/lib/translations.service.ts
+++ libs/api/domains/translations/src/lib/translations.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { logger } from '@island.is/logging'
 import { ApolloError } from 'apollo-server-express'
-import { ContentfulRepository } from '@island.is/api/domains/cms'
+import { ContentfulRepository, localeMap } from '@island.is/api/domains/cms'
 import { isEmpty, mergeWith } from 'lodash'
+
+// Declare fallbacks for locales here since they are not set in Contentful for various reasons,
+// this can be replaced by fetching contentful locales if fallback is set in the future, same format.
+const locales = [
+  { code: 'is-IS', fallbackCode: null },
+  { code: 'en', fallbackCode: 'is-IS' },
+]
 
 export interface TranslationsDict {
   [key: string]: string
@@ -23,19 +30,25 @@ export class TranslationsService {
     namespaces?: string[],
     lang?: string,
   ): Promise<TranslationsDict | null> => {
+    const locale = locales.find((l) => l.code === localeMap[lang])
+
     const result = await this.contentfulRepository
-      .getLocalizedEntries<any>(lang, {
+      .getLocalizedEntries<any>('*', {
         ['content_type']: 'namespace',
         select: 'fields.strings,fields.fallback',
         'fields.namespace[in]': namespaces.join(','),
       })
       .catch(errorHandler('getNamespace'))
 
-    const withFallbacks = result?.items?.map(({ fields }) =>
-      mergeWith({}, fields.fallback, fields.strings, (o, s) =>
-        isEmpty(s) ? o : s,
-      ),
-    )
+    const withFallbacks = result?.items?.map(({ fields }) => {
+      console.log('fields', fields)
+      return mergeWith(
+        {},
+        locale.fallbackCode ? fields.strings[locale.fallbackCode] : {},
+        fields.strings[locale.code],
+        (o, s) => (isEmpty(s) ? o : s),
+      )
+    })
 
     return withFallbacks.reduce((obj, cur) => Object.assign(obj, cur), {})
   }
