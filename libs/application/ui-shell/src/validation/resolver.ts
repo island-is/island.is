8550518f86ec39fe5410c913d4ce55be9c84877a
Árni Freyr Snorrason <arni@aranja.com>
2020-10-27 12:16:21 +0000

    Fix schema validation once and for all, so repeaters work properly. (#1558)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/ui-shell/src/validation/resolver.ts
+++ libs/application/ui-shell/src/validation/resolver.ts
@@ -1,19 +1,6 @@
 import { FormValue, validateAnswers } from '@island.is/application/core'
-import merge from 'lodash/merge'
 import { Resolver } from 'react-hook-form'
 import { ResolverContext } from '../types'
-
-interface Error {
-  [key: string]: string | Error
-}
-
-function constructError(path: (number | string)[], message: string): Error {
-  if (path.length === 1) {
-    return { [path[0]]: message }
-  }
-  const [first, ...rest] = path
-  return { [first]: constructError(rest, message) }
-}
 
 // TODO type this properly
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
@@ -29,19 +16,9 @@ export const resolver: Resolver<FormValue, ResolverContext> = (
     }
   }
   const { dataSchema } = context
-  const validationError = validateAnswers(formValue, true, dataSchema)
+  const validationError = validateAnswers(dataSchema, formValue, false)
   if (validationError) {
-    const { errors } = validationError
-    const errorMap: Record<string, Error> = {}
-    errors.forEach(({ path, message }) => {
-      const [first, ...rest] = path
-
-      errorMap[first] = merge(
-        rest?.length ? constructError(rest, message) : message,
-        errorMap[first],
-      ) as Error
-    })
-    return { values: {}, errors: errorMap }
+    return { values: {}, errors: validationError }
   }
 
   return {
