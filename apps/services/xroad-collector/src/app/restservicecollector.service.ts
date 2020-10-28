8d3ba9501b281ca599a1e1b831201be85c6b3ea1
Sævar Már Atlason <54210288+saevarma@users.noreply.github.com>
2020-10-28 10:29:38 +0000

    Feature/xroad collector (#1538)
    * Adding xroad-collector into apps/services.
    
    * Updating readme for xroad-collector and api-catalogue-services
    
    * Change error handling after codereview.
    Adding static function to Provider for nicer logging.
    
    * Fixing broken lint rule.
    
    * Adding @islandi-is/advania to codeowners for services/xroad-collector
    
    Co-authored-by: Sævar Már Atlason <saevar.mar.atlason@advania.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/services/xroad-collector/src/app/restservicecollector.service.ts
@@ -0,0 +1,51 @@
+import { ElasticService } from '@island.is/api-catalogue/elastic'
+import { Injectable } from '@nestjs/common'
+import { ProviderService } from '@island.is/api-catalogue/services'
+import { ServiceCollector } from './servicecollector.interface'
+import { logger } from '@island.is/logging'
+import { RestMetadataService } from '@island.is/api-catalogue/services'
+import { Provider, providerToString } from '@island.is/api-catalogue/types'
+
+@Injectable()
+export class RestServiceCollector implements ServiceCollector {
+  constructor(
+    private readonly providerService: ProviderService,
+    private readonly restMetadataService: RestMetadataService,
+    private readonly elasticService: ElasticService,
+  ) {}
+
+  async indexServices(): Promise<void> {
+    logger.info('Start indexing of REST services')
+
+    const protectedProviders = await this.providerService.getProtectedProviders()
+    const publicProviders = await this.providerService.getPublicProviders()
+
+    await this.indexProviders(protectedProviders.concat(publicProviders))
+
+    logger.info('Finished indexing of REST services')
+  }
+
+  private async indexProviders(providers: Array<Provider>): Promise<void> {
+    // Remove the index so we can recreate it
+    // with the latest state in X-Road
+    await this.elasticService.deleteIndex()
+
+    for (const provider of providers) {
+      try {
+        // For each provider get list af all REST services
+        // currently supporting those who were registered using OpenAPI
+        const services = await this.restMetadataService.getServices(provider)
+
+        // Insert into Elastic
+        await this.elasticService.bulk(services)
+      } catch (err) {
+        logger.error(
+          `Failed to index service metadata for provider ${providerToString(
+            provider,
+          )}`,
+          err,
+        )
+      }
+    }
+  }
+}
