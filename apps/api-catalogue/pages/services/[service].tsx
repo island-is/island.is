f10c18a6d3b696c5784ce5887a0db82a5c6d67ca
Arnar Páll Skúlason <Addips@users.noreply.github.com>
2020-10-28 14:47:37 +0000

    updating details page to use query to prevent contacting wrong endpoint (#1592)
    Co-authored-by: Arnar Páll Skúlason <arnar.pall.skulason@advania.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/api-catalogue/pages/services/[service].tsx
+++ apps/api-catalogue/pages/services/[service].tsx
@@ -1,11 +1,6 @@
-import { Query, QueryGetApiServiceByIdArgs } from '@island.is/api/schema'
 import { GetServerSideProps } from 'next'
 import { ServiceDetail, ServiceDetailProps } from '../../screens'
 import ContentfulApi from '../../services/contentful'
-import { GET_API_SERVICE_QUERY } from '../../screens/Queries'
-import initApollo from '../../graphql/client'
-
-const apolloClient = initApollo({})
 
 export const getServerSideProps: GetServerSideProps<ServiceDetailProps> = async (
   ctx,
@@ -16,25 +11,10 @@ export const getServerSideProps: GetServerSideProps<ServiceDetailProps> = async 
   const filterStrings = await client.fetchPageBySlug('service-filter', locale)
   const id = ctx.params.service as string
 
-  const [
-    {
-      data: { getApiServiceById },
-    },
-  ] = await Promise.all([
-    apolloClient.query<Query, QueryGetApiServiceByIdArgs>({
-      query: GET_API_SERVICE_QUERY,
-      variables: {
-        input: {
-          id: id,
-        },
-      },
-    }),
-  ])
-
   return {
     props: {
       filterStrings: filterStrings,
-      service: getApiServiceById,
+      serviceId: id,
     },
   }
 }
