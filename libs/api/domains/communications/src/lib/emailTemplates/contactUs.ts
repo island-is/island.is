17258d243f2f2a2d4d1d844107856090efa31a55
Stefán Harald <stefanhar@users.noreply.github.com>
2020-10-28 14:22:11 +0000

    Moved emails to env variables (#1582)
    * Moved emails to env variables
    
    * Fixed typo
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/communications/src/lib/emailTemplates/contactUs.ts
+++ libs/api/domains/communications/src/lib/emailTemplates/contactUs.ts
@@ -1,6 +1,7 @@
 import { dedent } from 'ts-dedent'
 import { SendMailOptions } from 'nodemailer'
 import { ContactUsInput } from '../dto/contactUs.input'
+import { environment } from '../environments/environment'
 
 export const getTemplate = (input: ContactUsInput): SendMailOptions => ({
   from: {
@@ -14,7 +15,7 @@ export const getTemplate = (input: ContactUsInput): SendMailOptions => ({
   to: [
     {
       name: 'Island.is þjónustuborð',
-      address: 'island@island.is',
+      address: environment.emailOptions.contactUsDestination,
     },
   ],
   subject: `Contact us: ${input.name}`,
