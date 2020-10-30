3578623333e199a9c7f9116e824b66ab12ace0fc
Stefán Harald <stefanharald@gmail.com>
2020-10-30 17:45:09 +0000

    Updated checks

--- libs/api/domains/cms/src/lib/search/importers/article.service.ts
+++ libs/api/domains/cms/src/lib/search/importers/article.service.ts
@@ -1,6 +1,7 @@
 import { MappedData } from '@island.is/api/content-search'
 import { logger } from '@island.is/logging'
 import { Injectable } from '@nestjs/common'
+import { Entry } from 'contentful'
 
 import { IArticle } from '../../generated/contentfulTypes'
 import { mapArticle, Article } from '../../models/article.model'
@@ -9,79 +10,85 @@ import { createTerms, extractStringsFromObject } from './utils'
 
 @Injectable()
 export class ArticleSyncService {
-  processSyncData(items) {
-    return items.filter((item) => item.sys.contentType.sys.id === 'article')
-  }
+  processSyncData(entries: Entry<any>[]): IArticle[] {
+    // only process articles that we consider not to be empty and dont have circular structures
+    return entries.reduce<IArticle[]>((processedEntries: IArticle[], entry) => {
+      // only process articles that we consider not to be empty
+      if (entry.sys.contentType.sys.id === 'article' && !!entry.fields.title) {
+        // remove potential circular reference in relatred articles
+        if (entry.fields?.relatedArticles?.length) {
+          const relatedArticles = entry.fields.relatedArticles.map(
+            ({
+              sys,
+              fields: { relatedArticles, ...prunedRelatedArticlesFields },
+            }) => ({
+              sys,
+              fields: prunedRelatedArticlesFields,
+            }),
+          )
 
-  doMapping(entries: IArticle[]): MappedData[] {
-    logger.info('Mapping articles', { count: entries.length })
-
-    return entries
-      .map<MappedData | boolean>((entry) => {
-        let mapped: Article
-
-        try {
-          entry.fields.relatedArticles = entry.fields?.relatedArticles?.[0]
-            ?.fields
-            ? (entry.fields.relatedArticles.map(
-                ({
-                  sys,
-                  fields: { relatedArticles, ...prunedRelatedArticlesFields },
-                }) => ({
-                  sys,
-                  fields: prunedRelatedArticlesFields,
-                }),
-              ) as IArticle[])
-            : []
-
-          mapped = mapArticle(entry)
-
-          // we consider article that dont have a title to be empty
-          if (!mapped.title) {
-            throw new Error('Trying to import empty article entry')
-          }
-
-          const type = 'webArticle'
-          return {
-            _id: mapped.id,
-            title: mapped.title,
-            content: extractStringsFromObject(mapped.body),
-            type,
-            termPool: createTerms([
-              mapped.title,
-              mapped.category?.title,
-              mapped.group?.title,
-            ]),
-            response: JSON.stringify({ ...mapped, __typename: type }),
-            tags: [
-              {
-                key: entry.fields?.group?.fields?.slug,
-                value: entry.fields?.group?.fields?.title,
-                type: 'group',
-              },
-              {
-                key: entry.fields?.category?.fields?.slug,
-                value: entry.fields?.category?.fields?.title,
-                type: 'category',
-              },
-              ...mapped.otherCategories.map((x) => ({
-                key: x.slug,
-                value: x.title,
-                type: 'category',
-              })),
-              {
-                key: entry.fields?.slug,
-                type: 'slug',
-              },
-            ],
-            dateCreated: entry.sys.createdAt,
-            dateUpdated: new Date().getTime().toString(),
-          }
-        } catch (error) {
-          logger.warn('Failed to import article', { error: error.message })
-          return false
+          return { ...entry, relatedArticles }
         }
-      })
-      .filter((value): value is MappedData => Boolean(value))
+
+
+      }
+    }
+        processedEntries.push(entry as IArticle)
+      }
+      return processedEntries
+    }, [])
   }
+
+doMapping(entries: IArticle[]): MappedData[] {
+  logger.info('Mapping articles', { count: entries.length })
+
+  return entries
+    .map<MappedData | boolean>((entry) => {
+      let mapped: Article
+
+      try {
+        mapped = mapArticle(entry)
+        const type = 'webArticle'
+        return {
+          _id: mapped.id,
+          title: mapped.title,
+          content: extractStringsFromObject(mapped.body),
+          type,
+          termPool: createTerms([
+            mapped.title,
+            mapped.category?.title,
+            mapped.group?.title,
+          ]),
+          response: JSON.stringify({ ...mapped, __typename: type }),
+          tags: [
+            {
+              key: entry.fields?.group?.fields?.slug,
+              value: entry.fields?.group?.fields?.title,
+              type: 'group',
+            },
+            {
+              key: entry.fields?.category?.fields?.slug,
+              value: entry.fields?.category?.fields?.title,
+              type: 'category',
+            },
+            ...mapped.otherCategories.map((x) => ({
+              key: x.slug,
+              value: x.title,
+              type: 'category',
+            })),
+            {
+              key: entry.fields?.slug,
+              type: 'slug',
+            },
+          ],
+          dateCreated: entry.sys.createdAt,
+          dateUpdated: new Date().getTime().toString(),
+        }
+      } catch (error) {
+        logger.warn('Failed to import article', { error: error.message })
+        return false
+      }
+    })
+    .filter((value): value is MappedData => Boolean(value))
+}
 }
