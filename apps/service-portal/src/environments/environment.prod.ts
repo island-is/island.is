7fefe5ca43c9e0ce755ab8ae3851c0992b6d5793
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-10-30 10:53:52 +0000

    update env

--- apps/service-portal/src/environments/environment.prod.ts
+++ apps/service-portal/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export default {
   production: true,
   identityServer: {
-    IDENTITY_SERVER_BASE_URL: process.env.IDENTITY_SERVER_BASE_URL,
+    IDENTITY_SERVER_BASE_URL:
+      'https://siidentityserverweb20200805020732.azurewebsites.net/',
   },
 }
