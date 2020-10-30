a271df44ee03c7ee99fb47bb78042376ad97374a
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-10-30 10:56:24 +0000

    remove dash

--- apps/service-portal/src/environments/environment.prod.ts
+++ apps/service-portal/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export default {
   production: true,
   identityServer: {
     IDENTITY_SERVER_BASE_URL:
-      'https://siidentityserverweb20200805020732.azurewebsites.net/',
+      'https://siidentityserverweb20200805020732.azurewebsites.net',
   },
 }
