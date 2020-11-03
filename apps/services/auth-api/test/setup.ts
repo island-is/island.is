ae053c8975068537a14f8f5bd859b827c0aad11c
unnarb <unnarb@gmail.com>
2020-11-03 13:48:48 +0000

    Omitig test v2

--- apps/services/auth-api/test/setup.ts
+++ apps/services/auth-api/test/setup.ts
@@ -1,55 +1,56 @@
-// import { testServer, TestServerOptions } from '@island.is/infra-nest-server'
-// import { INestApplication } from '@nestjs/common'
-// import { Sequelize } from 'sequelize-typescript'
-// import { AppModule } from '../src/app/app.module'
-// import { execSync } from 'child_process'
+import { testServer, TestServerOptions } from '@island.is/infra-nest-server'
+import { INestApplication } from '@nestjs/common'
+import { Sequelize } from 'sequelize-typescript'
+import { AppModule } from '../src/app/app.module'
 
-// export let app: INestApplication
-// let sequelize: Sequelize
+export let app: INestApplication
+let sequelize: Sequelize
 
-// export const truncate = () => {
-//   if (!sequelize) {
-//     return
-//   }
+export const truncate = () => {
+  if (!sequelize) {
+    return
+  }
 
-//   Promise.all(
-//     Object.values(sequelize.models).map((model) => {
-//       if (model.tableName.toLowerCase() === 'sequelize') {
-//         return null
-//       }
+  Promise.all(
+    Object.values(sequelize.models).map((model) => {
+      if (model.tableName.toLowerCase() === 'sequelize') {
+        return null
+      }
 
-//       return model.destroy({
-//         where: {},
-//         cascade: true,
-//         truncate: true,
-//         force: true,
-//       })
-//     }),
-//   )
-// }
+      return model.destroy({
+        where: {},
+        cascade: true,
+        truncate: true,
+        force: true,
+      })
+    }),
+  )
+}
 
-// export const setup = async (options?: Partial<TestServerOptions>) => {
-//   app = await testServer({
-//     appModule: AppModule,
-//     ...options,
-//   })
-//   //TODO: find out why this setup does not work for this project
-//   // sequelize = await app.resolve(getConnectionToken() as Type<Sequelize>)
-//   // await sequelize.sync()
+export const setup = async (options?: Partial<TestServerOptions>) => {
+  app = await testServer({
+    appModule: AppModule,
+    ...options,
+  })
+  //TODO: find out why this setup does not work for this project
+  // sequelize = await app.resolve(getConnectionToken() as Type<Sequelize>)
+  // await sequelize.sync()
 
-//   // Populate the database models
-//   execSync('yarn nx run services-auth-api:migrate')
-//   // Seed the database
-//   execSync('yarn nx run services-auth-api:seed')
+  // Populate the database models
+  // TODO: Add after figuring out auth
+  // execSync('yarn nx run services-auth-api:migrate')
+  // Seed the database
+  // TODO: add after figuring out auth
+  // execSync('yarn nx run services-auth-api:seed')
 
-//   return app
-// }
+  return app
+}
 
-// beforeEach(() => truncate())
+beforeEach(() => truncate())
 
-// afterAll(async () => {
-//   if (app && sequelize) {
-//     await app.close()
-//     await sequelize.close()
-//   }
-// })
+afterAll(async () => {
+  if (app && sequelize) {
+    await app.close()
+    await sequelize.close()
+  }
+})
