d51909d0a27901686da6d343ef92f9c52a01631e
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-11-03 15:34:24 +0000

    sentry added

--- apps/service-portal/src/Main.tsx
+++ apps/service-portal/src/Main.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+import { environment } from './environments'
 
 import App from './app/App'
+
+Sentry.init({
+  dsn: environment.sentry.dsn,
+  integrations: [new Integrations.BrowserTracing()],
+  environment: 'frontend',
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
