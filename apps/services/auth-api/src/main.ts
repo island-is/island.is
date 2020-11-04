8e3ef55cf761f01078c0f36a45d92ee1dd1c7a6e
Jérémy Barbet <jeremgraph@gmail.com>
2020-11-04 10:59:32 +0000

    chore: change the way to call the postinstall scripts (#1225)
    * wip
    
    * fix: more work
    
    * fix: missing commands name
    
    * fix: test caching
    
    * fix: format
    
    * chore: more
    
    * more stuff
    
    * more stuff
    
    * fix: link
    
    * chore: update with new commands
    
    * chore: improvements
    
    * fix: disable parallel
    
    * fix: contentfultypes
    
    * chore: minor changes
    
    * Revert "fix: contentfultypes"
    
    This reverts commit 185ba5c998d134bdad7c2ff505094088d4bb696a.
    
    * chore: re-run
    
    * fix: remove use of AdgerdirNews type
    
    * docs: update
    
    * fix: skip-cache on github workflow
    
    * fix: hash keys
    
    * chore: codeowner and doc
    
    * chore: remove branch build
    
    * fix: after merge
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/auth-api/src/main.ts
+++ apps/services/auth-api/src/main.ts
@@ -1,15 +1,12 @@
 import '@island.is/infra-tracing'
-import { DocumentBuilder } from '@nestjs/swagger'
-import { AppModule } from './app/app.module'
+
 import { bootstrap } from '@island.is/infra-nest-server'
+
+import { AppModule } from './app/app.module'
+import { openApi } from './openApi'
 
 bootstrap({
   appModule: AppModule,
   name: 'auth-api',
-  openApi: new DocumentBuilder()
-    .setTitle('IdentityServer Api')
-    .setDescription('Api for IdentityServer.')
-    .setVersion('1.0')
-    .addTag('auth-api')
-    .build(),
+  openApi,
 })
