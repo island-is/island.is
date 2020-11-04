eebf35c17d5f30ca220ba64d386a17487204bb5e
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2020-11-04 10:36:10 +0000

    Sentry added to service portal and user profile service (#1689)
    * sentry added
    
    * removed comment
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/user-profile/src/main.ts
+++ apps/services/user-profile/src/main.ts
@@ -1,10 +1,20 @@
 import '@island.is/infra-tracing'
 import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
+import * as Sentry from '@sentry/node'
+import { environment } from './environments'
+import { SentryInterceptor } from '@island.is/infra-monitoring'
+
 import { bootstrap } from '@island.is/infra-nest-server'
+
+Sentry.init({
+  dsn: environment.sentry.dsn,
+  environment: 'backend',
+})
 
 bootstrap({
   appModule: AppModule,
   name: 'services-user-profile',
+  interceptors: [new SentryInterceptor()],
   openApi,
 })
