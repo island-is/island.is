d628c4dc469d9485578270d3a54ec0da8812fdd4
dd-sdang <64913954+dd-sdang@users.noreply.github.com>
2020-11-05 12:22:12 +0100

    Skilavottord/Completed page with error handling (#1722)
    * Added error message box
    
    * Refactor endpoint names
    
    * Added error handling for the Completed page
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/skilavottord/web/components/GDPR/withGDPR.tsx
+++ apps/skilavottord/web/components/GDPR/withGDPR.tsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react'
 import { UserContext } from '@island.is/skilavottord-web/context'
 import { useQuery } from '@apollo/client'
-import { GET_GDPR_INFO } from '@island.is/skilavottord-web/graphql/queries'
+import { GDPR_INFO_BY_NATIONAL_ID } from '@island.is/skilavottord-web/graphql/queries'
 import { GDPR } from './GDPR'
 
 export const withGDPR = (WrappedComponent) => () => {
   const { user } = useContext(UserContext)
 
   const nationalId = user?.nationalId ?? ''
-  const { data, loading } = useQuery(GET_GDPR_INFO, {
+  const { data, loading } = useQuery(GDPR_INFO_BY_NATIONAL_ID, {
     variables: { nationalId },
   })
 
---
+++ apps/skilavottord/web/components/OutlinedError/OutlinedError.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from 'react'
+import { Box, Button, Icon, Stack, Text } from '@island.is/island-ui/core'
+
+interface OutlinedErrorProps {
+  title: string
+  message: string
+  primaryButton: Button
+  secondaryButton?: Button
+}
+
+interface Button {
+  text: string
+  action: () => void
+}
+
+export const OutlinedError: FC<OutlinedErrorProps> = ({
+  title,
+  message,
+  primaryButton,
+  secondaryButton,
+}: OutlinedErrorProps) => {
+  return (
+    <Box>
+      <Stack space={4}>
+        <Stack space={[3, 3, 2, 2]}>
+          <Box
+            display="flex"
+            background="red100"
+            borderColor="red200"
+            border="standard"
+            paddingY={2}
+            paddingLeft={2}
+            paddingRight={[3, 3, 3, 2]}
+            borderRadius="large"
+          >
+            <Box flexShrink={0} marginRight={[1, 1, 1, 2]}>
+              <Icon icon="warning" color="red400" size="large" />
+            </Box>
+            <Box>
+              <Text variant="h5">{title}</Text>
+              <Text>{message}</Text>
+            </Box>
+          </Box>
+        </Stack>
+        <Box display="flex" justifyContent="spaceBetween">
+          {secondaryButton && (
+            <Button variant="ghost" onClick={secondaryButton.action}>
+              {secondaryButton.text}
+            </Button>
+          )}
+          <Button
+            variant={secondaryButton ? 'primary' : 'ghost'}
+            onClick={primaryButton.action}
+          >
+            {primaryButton.text}
+          </Button>
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
+export default OutlinedError
