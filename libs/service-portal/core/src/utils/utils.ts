71c9a3ea02a6f0dd296b9e3d2cd82a846e9f9dbf
atligudl <atli@sendiradid.is>
2020-11-05 15:53:31 +0000

    Service Portal Family Relation (#1732)
    * sorting and familyrelation enum
    
    * filter out yourself for family
    
    * fix resolver
    
    * display family relation in family member card, layout and style updates to family member list
    
    * format usenatreg family info hook
    
    Co-authored-by: Olafur <obmagnusson@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/service-portal/core/src/utils/utils.ts
+++ libs/service-portal/core/src/utils/utils.ts
@@ -13,3 +13,11 @@ export const getNameAbbreviation = (name: string) => {
 
   return initials
 }
+
+export const formatNationalId = (nationalId: string): string => {
+  if (nationalId?.length === 10) {
+    return `${nationalId.slice(0, 6)}-${nationalId.slice(6)}`
+  } else {
+    return nationalId
+  }
+}
