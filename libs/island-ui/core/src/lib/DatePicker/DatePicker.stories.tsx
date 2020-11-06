512b51ce632897594810019c887bde848a28e74b
Brian Johannessen <barabrian89@gmail.com>
2020-11-06 08:37:30 +0000

    figma with mobile link

--- libs/island-ui/core/src/lib/DatePicker/DatePicker.stories.tsx
+++ libs/island-ui/core/src/lib/DatePicker/DatePicker.stories.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { withDesign } from 'storybook-addon-designs'
 
 import { withFigma } from '../../utils/withFigma'
-import { Box } from '../Box/Box'
-import { ContentBlock } from '../ContentBlock/ContentBlock'
 import { DatePicker } from './DatePicker'
 
 export default {
   title: 'Form/DatePicker',
   component: DatePicker,
+  decorators: [withDesign],
+  parameters: withFigma({
+    desktop:
+      'https://www.figma.com/file/pDczqgdlWxgn3YugWZfe1v/UI-Library-%E2%80%93-%F0%9F%96%A5%EF%B8%8F-Desktop?node-id=50%3A155',
+    mobile:
+      'https://www.figma.com/file/rU3mPM1cLfHa3u7TWuutPQ/UI-Library-%E2%80%93-%F0%9F%93%B1Mobile?node-id=53%3A1536',
+  }),
 }
 const Template = (args) => <DatePicker {...args} />
 
