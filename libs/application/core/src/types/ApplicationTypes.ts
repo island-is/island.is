e3df6461ad8216a0f89df08a22de9ab19c4a50a9
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-11-06 14:58:11 +0000

    Create driving lessons application

--- libs/application/core/src/types/ApplicationTypes.ts
+++ libs/application/core/src/types/ApplicationTypes.ts
@@ -1,6 +1,7 @@
 export enum ApplicationTypes {
   EXAMPLE = 'ExampleForm',
   DRIVING_LESSONS = 'DrivingLessons',
+  DRIVING_LICENSE = 'DrivingLicense',
   PARENTAL_LEAVE = 'ParentalLeave',
   META_APPLICATION = 'MetaApplication',
   DOCUMENT_PROVIDER_ONBOARDING = 'DocumentProviderOnboarding',
---
+++ libs/application/templates/driving-license/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'application-templates-driving-license',
+  preset: '../../../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../coverage/libs/application/templates/driving-license',
+}
