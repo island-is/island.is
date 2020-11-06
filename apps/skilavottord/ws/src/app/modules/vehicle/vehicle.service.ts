2b9d9a9d5ed0fb8cdb9fb5fb9d74b98ce5767db1
Sigurgeir Gudmundsson <16959841+essgje@users.noreply.github.com>
2020-11-06 14:18:07 +0000

    Feature endp allderegve (#1752)
    * endpoint added, null problem workaround
    
    * lint fixing
    
    * removing test
    
    * rm not null constr.
    
    Co-authored-by: Sigurgeir Guðmundsson <sigurgeirg@localhost.localdomain>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/skilavottord/ws/src/app/modules/vehicle/vehicle.service.ts
+++ apps/skilavottord/ws/src/app/modules/vehicle/vehicle.service.ts
@@ -1,26 +1,42 @@
 import { Inject, Injectable } from '@nestjs/common'
-import { InjectModel } from '@nestjs/sequelize'
-import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { logger, Logger, LOGGER_PROVIDER } from '@island.is/logging'
 import { VehicleModel } from './model/vehicle.model'
 import { RecyclingRequestModel } from '../recycling.request/model/recycling.request.model'
+import { RecyclingPartnerModel } from '../recycling.partner/model/recycling.partner.model'
 
 @Injectable()
 export class VehicleService {
   constructor(
-    @InjectModel(VehicleModel)
-    private vehicleModel: typeof VehicleModel,
     @Inject(LOGGER_PROVIDER)
     private logger: Logger,
-    @InjectModel(RecyclingRequestModel)
-    private recyclingRequestModel: typeof RecyclingRequestModel,
   ) {}
 
   async findAll(): Promise<VehicleModel[]> {
     this.logger.info('Getting all vehicles...')
-    return await this.vehicleModel.findAll({
+    return await VehicleModel.findAll({
       include: [
         {
-          model: this.recyclingRequestModel,
+          model: RecyclingRequestModel,
+        },
+      ],
+    })
+  }
+
+  async findAllDeregistered(): Promise<VehicleModel[]> {
+    return await VehicleModel.findAll({
+      include: [
+        {
+          model: RecyclingRequestModel,
+          where: {
+            //requestType: 'test',
+            requestType: 'handOver',
+            // recyclingPartnerId: NotNull,
+          },
+          include: [
+            {
+              model: RecyclingPartnerModel,
+            },
+          ],
         },
       ],
     })
@@ -28,7 +44,7 @@ export class VehicleService {
 
   async findByVehicleId(vehicleId: string): Promise<VehicleModel> {
     this.logger.info(`Finding vehicle for vehicleId - "${vehicleId}"`)
-    return this.vehicleModel.findOne({
+    return VehicleModel.findOne({
       where: { vehicleId },
     })
   }
