f9aac601d11844b5b67f4c8d4b4d599ade21a2cd
qdong <qdong@deloitte.is>
2020-11-06 13:50:11 +0000

    Docker-build-create new vehicle

--- apps/skilavottord/ws/src/app/modules/vehicle/vehicle.resolver.ts
+++ apps/skilavottord/ws/src/app/modules/vehicle/vehicle.resolver.ts
@@ -1,5 +1,5 @@
 import { Inject } from '@nestjs/common'
-import { Query, Resolver } from '@nestjs/graphql'
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql'
 import { VehicleModel } from './model/vehicle.model'
 import { VehicleService } from './vehicle.service'
 import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
@@ -18,10 +18,23 @@ export class VehicleResolver {
     return res
   }
 
-  // @Query(() => Gdpr)
-  // async getVehiclesForNationalId(
-  //   @Args('nationalId') nid: string,
-  // ): Promise<Gdpr> {
-  //   return await this.gdprService.findByNationalId(nid)
-  // }
+  @Mutation(() => Boolean)
+  async createSkilavottordVehicle(
+    @Args('permno') permno: string,
+    @Args('nationalId') nid: string,
+    @Args('type') type: string,
+    @Args('color') color: string,
+    @Args('newRegDate') newReg: Date,
+    @Args('vinNumber') vin: string,
+  ) {
+    const newVehicle = new VehicleModel()
+    newVehicle.vinNumber = vin
+    newVehicle.newregDate = newReg
+    newVehicle.vehicleColor = color
+    newVehicle.vehicleType = type
+    newVehicle.ownerNationalId = nid
+    newVehicle.vehicleId = permno
+    await this.vehicleService.create(newVehicle)
+    return true
+  }
 }
