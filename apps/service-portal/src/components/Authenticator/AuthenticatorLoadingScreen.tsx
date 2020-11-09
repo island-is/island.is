6020d64b04483a2582ca32a771e9d9eb28dbffe0
atligudl <atli@sendiradid.is>
2020-11-09 11:09:37 +0000

    remove the text from authenticator loading screen (#1779)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/service-portal/src/components/Authenticator/AuthenticatorLoadingScreen.tsx
+++ apps/service-portal/src/components/Authenticator/AuthenticatorLoadingScreen.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Box, Text, Stack, LoadingIcon } from '@island.is/island-ui/core'
+import { Box, LoadingIcon } from '@island.is/island-ui/core'
 import * as styles from './Authenticator.treat'
 
 const AuthenticatorLoadingScreen: FC<{}> = () => {
@@ -10,12 +10,7 @@ const AuthenticatorLoadingScreen: FC<{}> = () => {
       alignItems="center"
       className={styles.wrapper}
     >
-      <Stack space={3} align="center">
-        <LoadingIcon animate size={40} />
-        <Text variant="h4" color="blue600">
-          Unnið úr auðkenningu
-        </Text>
-      </Stack>
+      <LoadingIcon animate size={40} />
     </Box>
   )
 }
