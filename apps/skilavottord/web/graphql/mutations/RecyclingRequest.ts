0666cbe3351d5347c5b59603c29d5123a260ea53
Dang, Sandra <sandang@deloittedigital.se>
2020-11-09 19:35:31 +0100

    Fix citizen request

--- apps/skilavottord/web/graphql/mutations/RecyclingRequest.ts
+++ apps/skilavottord/web/graphql/mutations/RecyclingRequest.ts
@@ -2,13 +2,11 @@ import gql from 'graphql-tag'
 
 export const CREATE_RECYCLING_REQUEST_CITIZEN = gql`
   mutation createSkilavottordRecyclingRequest(
-    $partnerId: String
     $nameOfRequestor: String
     $permno: String!
     $requestType: String!
   ) {
     createSkilavottordRecyclingRequest(
-      partnerId: $partnerId
       nameOfRequestor: $nameOfRequestor
       permno: $permno
       requestType: $requestType
