a02d7f784a10b43de5ec433073b29f1c0f981342
Sigga Hrafns <sigridur@kosmosogkaos.is>
2020-11-09 09:21:18 +0000

    Replacing modal for ui core dialog

--- apps/web/components/LanguageToggler/LanguageToggler.tsx
+++ apps/web/components/LanguageToggler/LanguageToggler.tsx
@@ -2,7 +2,12 @@ import React, { FC, useContext } from 'react'
 import { useRouter } from 'next/router'
 import findKey from 'lodash/findKey'
 import { useApolloClient } from '@apollo/client/react'
-import { Button, ButtonTypes, Hidden, Text } from '@island.is/island-ui/core'
+import {
+  Button,
+  ButtonTypes,
+  Hidden,
+  DialogPrompt,
+} from '@island.is/island-ui/core'
 import { useI18n } from '@island.is/web/i18n'
 import routeNames, { PathTypes, routes } from '@island.is/web/i18n/routeNames'
 import { GET_CONTENT_SLUG } from '@island.is/web/screens/queries/Article'
@@ -88,18 +93,16 @@ export const LanguageToggler: FC<{
   const LanguageButton = (
     <>
       {otherLanguage === 'en' ? (
-        <Modal
+        <DialogPrompt
           baseId={LanguageButtonId}
           title={gn('switchToEnglishModalTitle')}
-          label="Confirm switching to english"
-          disclosure={Disclosure}
+          description={gn('switchToEnglishModalText')}
+          ariaLabel="Confirm switching to english"
+          disclosureElement={Disclosure}
           onConfirm={onClick}
-          lang="en"
-        >
-          <Text variant="intro" as="p">
-            {gn('switchToEnglishModalText')}
-          </Text>
-        </Modal>
+          buttonTextConfirm="Confirm"
+          buttonTextCancel="Cancel"
+        />
       ) : (
         Disclosure
       )}
