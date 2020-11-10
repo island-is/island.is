bcdc6f75c438e12db32641a1dcb020004cc24ce6
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-11-10 09:55:48 +0000

    ternary expanded to 3 conditions for stage, prod and dev

--- apps/service-portal/src/environments/environment.prod.ts
+++ apps/service-portal/src/environments/environment.prod.ts
@@ -2,7 +2,9 @@ export default {
   production: true,
   identityServer: {
     IDENTITY_SERVER_ISSUER_URL:
-      window.location.origin === 'https://beta.minarsidur.island.is'
+      window.location.origin === 'https://service-portal.staging01.devland.is/'
+        ? 'https://identity-server.staging01.devland.is'
+        : window.location.origin === 'https://beta.minarsidur.island.is'
         ? 'https://innskra.island.is'
         : 'https://identity-server.dev01.devland.is',
   },
