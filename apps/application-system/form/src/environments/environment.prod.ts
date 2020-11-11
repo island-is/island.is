98f6908513107c8737150bfea8a08ba0dbe00026
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2020-11-11 10:55:29 +0000

    fix after merge

--- apps/application-system/form/src/environments/environment.prod.ts
+++ apps/application-system/form/src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export default {
   production: true,
-  identityServer: {
-    baseUrl: 'https://identity-server.dev01.devland.is',
-  },
   baseApiUrl: process.env.API_URL,
   identityServer: {
     authority: process.env.IDENTITY_SERVER_ISSUER_URL,
