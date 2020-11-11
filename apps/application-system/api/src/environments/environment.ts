6fbd9ab2fd8187e8e53491f0a135dd4216d042d6
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2020-11-11 11:51:37 +0000

    add audience

--- apps/application-system/api/src/environments/environment.ts
+++ apps/application-system/api/src/environments/environment.ts
@@ -2,7 +2,7 @@ export default {
   production: false,
   identityServer: {
     issuer: 'https://identity-server.dev01.devland.is',
-    audience: '',
+    audience: 'api_resource.scope',
     jwksUri:
       'https://identity-server.dev01.devland.is/.well-known/openid-configuration/jwks',
   },
