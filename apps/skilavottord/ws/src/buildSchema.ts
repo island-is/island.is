a55ff2b14ec2f1ce07f1843e35d8bed62afb4cdb
Jérémy Barbet <jeremgraph@gmail.com>
2020-11-12 12:05:39 +0000

    fix: dto input and non-necessary resolver

--- apps/skilavottord/ws/src/buildSchema.ts
+++ apps/skilavottord/ws/src/buildSchema.ts
@@ -1,7 +1,6 @@
 import { buildSchema } from '@island.is/infra-nest-server'
 import { FjarsyslaResolver } from './app/modules/fjarsysla/fjarsysla.resolver'
 import { GdprResolver } from './app/modules/gdpr/gdpr.resolver'
-import { RecyclingPartnerResolver } from './app/modules/recycling.partner/recycling.partner.resolver'
 import { RecyclingRequestResolver } from './app/modules/recycling.request/recycling.request.resolver'
 import { SamgongustofaResolver } from './app/modules/samgongustofa/samgongustofa.resolver'
 import { UserResolver } from './app/modules/user/user.resolver'
@@ -15,7 +14,6 @@ buildSchema({
     SamgongustofaResolver,
     FjarsyslaResolver,
     GdprResolver,
-    RecyclingPartnerResolver,
     VehicleResolver,
     RecyclingRequestResolver,
     VehicleOwnerResolver,
