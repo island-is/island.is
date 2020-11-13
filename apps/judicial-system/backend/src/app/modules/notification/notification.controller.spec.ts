d5007256803a4f33553ca6da7e895d62fa180ae5
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-11-13 12:01:44 +0000

    Implements prison ruling notification

---
+++ apps/judicial-system/backend/src/app/modules/notification/notification.controller.spec.ts
@@ -0,0 +1,22 @@
+// import { Test, TestingModule } from '@nestjs/testing'
+// import { NotificationController } from './notification.controller'
+
+describe('NotificationController', () => {
+  it('', () => {
+    expect(true).toBe(true)
+  })
+
+  // let controller: NotificationController
+
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     controllers: [NotificationController],
+  //   }).compile();
+
+  //   controller = module.get<NotificationController>(NotificationController);
+  // });
+
+  // it('should be defined', () => {
+  //   expect(controller).toBeDefined();
+  // });
+})
