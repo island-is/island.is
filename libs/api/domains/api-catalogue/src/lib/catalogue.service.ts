5d88e830c31dd2c48fd64deda0a3997319776198
Arnar Páll Skúlason <arnarpask@gmail.com>
2020-11-13 09:40:10 +0000

    setting libs for api-catalogue to strict

--- libs/api/domains/api-catalogue/src/lib/catalogue.service.ts
+++ libs/api/domains/api-catalogue/src/lib/catalogue.service.ts
@@ -15,7 +15,7 @@ export class ApiCatalogueService {
   ) {}
 
   async getCatalogue(input: GetApiCatalogueInput): Promise<ApiCatalogue> {
-    const res: ApiCatalogue = {
+    let res: ApiCatalogue = {
       services: [],
       pageInfo: {
         nextCursor: null,
@@ -24,11 +24,11 @@ export class ApiCatalogueService {
 
     try {
       //Set the search after parameter as an empty array since it will be ignored in elastic
-      let searchAfter = []
-      const { limit, cursor, query, pricing, data, type, access } = input
+      let searchAfter: string[] = []
+      const { limit = 25, cursor, query, pricing, data, type, access } = input
 
       if (typeof cursor !== 'undefined' && cursor !== null) {
-        const temp = Buffer.from(input.cursor, 'base64').toString()
+        const temp = Buffer.from(input.cursor as string, 'base64').toString()
         searchAfter = temp.split(',')
       }
 
@@ -48,12 +48,15 @@ export class ApiCatalogueService {
         body?.hits?.hits.pop()
         //get the sort parameters of the last item to use as the cursor for next search
         searchAfter = body?.hits?.hits[body?.hits?.hits.length - 1].sort
-        res.pageInfo.nextCursor = Buffer.from(searchAfter.toString()).toString(
+        /*res.pageInfo!.nextCursor = Buffer.from(searchAfter.toString()).toString(
           'base64',
-        )
+        )*/
+        res = {...res,  pageInfo: { nextCursor: Buffer.from(searchAfter.toString()).toString(
+          'base64',
+        )}}
       }
 
-      body?.hits?.hits.forEach((x) => res.services.push(x._source))
+      body?.hits?.hits.forEach((x: { _source: ApiService }) => res.services.push(x._source))
 
       return res
     } catch (exception) {
@@ -62,12 +65,13 @@ export class ApiCatalogueService {
     }
   }
 
-  async getApiServiceById(id: string): Promise<ApiService> {
+  async getApiServiceById(id: string): Promise<ApiService | null> {
     try {
       const { body } = await this.elastic.fetchById(id)
       if (body?.hits?.total.value > 0) {
         return body?.hits?.hits[0]._source
       }
+      return null
     } catch (exception) {
       logger.error(exception)
       return null
