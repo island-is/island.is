b0a71079ffa6391a38d1b557ea8bf427c53730a3
hinriksteinar <hsv7@hi.is>
2020-11-17 11:00:45 +0000

    Fixed filestructure, added accordion expanded

--- libs/application/templates/document-provider-onboarding/src/fields/DocumentProvicerApplication/Review/index.tsx
+++ libs/application/templates/document-provider-onboarding/src/fields/DocumentProvicerApplication/Review/index.tsx
@@ -1,15 +1,24 @@
 import React, { FC, useState } from 'react'
 import { useFormContext } from 'react-hook-form'
-import { FieldBaseProps, formatText } from '@island.is/application/core'
 import {
+  FieldBaseProps,
+  formatText,
+  getValueViaPath,
+} from '@island.is/application/core'
+import {
+  Accordion,
+  AccordionItem,
   Box,
   GridColumn,
   GridRow,
   Input,
   Text,
-  Divider,
 } from '@island.is/island-ui/core'
-import { FieldDescription } from '@island.is/shared/form-fields'
+import {
+  FieldDescription,
+  RadioController,
+  SelectController,
+} from '@island.is/shared/form-fields'
 import { useLocale } from '@island.is/localization'
 
 const Review: FC<FieldBaseProps> = ({ field, application }) => {
@@ -26,170 +35,172 @@ const Review: FC<FieldBaseProps> = ({ field, application }) => {
       )}
 
       <Box marginTop={8} marginBottom={6}>
-        {/* Applicant */}
-        <Box marginBottom={3}>
-          <Text variant="h3">Upplýsingar umsækjanda</Text>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="6/12">
-              <Input
-                id={'applicant.nationalId'}
-                name={'applicant.nationalId'}
-                label={'Kennitala'}
-                ref={register}
-              />
-            </GridColumn>
-            <GridColumn span="6/12">
-              <Input
-                id={'applicant.name'}
-                name={'applicant.name'}
-                label={'Nafn'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="6/12">
-              <Input
-                id={'applicant.email'}
-                name={'applicant.email'}
-                label={'Netfang'}
-                ref={register}
-              />
-            </GridColumn>
-            <GridColumn span="6/12">
-              <Input
-                id={'applicant.phoneNumber'}
-                name={'applicant.phoneNumber'}
-                label={'Símanúmer'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="6/12">
-              <Input
-                id={'applicant.address'}
-                name={'applicant.address'}
-                label={'Heimilisfang'}
-                ref={register}
-              />
-            </GridColumn>
-            <GridColumn span="6/12">
-              <Input
-                id={'applicant.zipCode'}
-                name={'applicant.zipCode'}
-                label={'Póstnúmer og staður'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-        </Box>
-        {/* administrativeContact */}
-        <Box marginBottom={3}>
-          <Text variant="h3">Ábyrgðarmaður</Text>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="12/12">
-              <Input
-                id={'administrativeContact.name'}
-                name={'administrativeContact.name'}
-                label={'nafn'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="6/12">
-              <Input
-                id={'administrativeContact.email'}
-                name={'administrativeContact.email'}
-                label={'Netfang'}
-                ref={register}
-              />
-            </GridColumn>
-            <GridColumn span="6/12">
-              <Input
-                id={'administrativeContact.phoneNumber'}
-                name={'administrativeContact.phoneNumber'}
-                label={'Símanúmer'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-        </Box>
-        {/* technicalContact */}
-        <Box marginBottom={3}>
-          <Text variant="h3">Tæknilegur tengiliður</Text>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="12/12">
-              <Input
-                id={'technicalContact.name'}
-                name={'technicalContact.name'}
-                label={'nafn'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="6/12">
-              <Input
-                id={'technicalContact.email'}
-                name={'technicalContact.email'}
-                label={'Netfang'}
-                ref={register}
-              />
-            </GridColumn>
-            <GridColumn span="6/12">
-              <Input
-                id={'technicalContact.phoneNumber'}
-                name={'technicalContact.phoneNumber'}
-                label={'Símanúmer'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-        </Box>
-        {/* helpdeskContact */}
-        <Box marginBottom={3}>
-          <Text variant="h3">Notendaaðstoð</Text>
-          <Box marginTop={3} />
-          <GridRow>
-            <GridColumn span="6/12">
-              <Input
-                id={'helpDesk.email'}
-                name={'helpDesk.email'}
-                label={'Netfang'}
-                ref={register}
-              />
-            </GridColumn>
-            <GridColumn span="6/12">
-              <Input
-                id={'helpDesk.phoneNumber'}
-                name={'helpDesk.phoneNumber'}
-                label={'Símanúmer'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-          <Box marginTop={3} />
-          <GridRow>
-            {/* Maybe add condition here, because this can be empty ? opinions */}
-            <GridColumn span="6/12">
-              <Input
-                id={'helpDesk.chatbot'}
-                name={'helpDesk.chatbot'}
-                label={'Snjallmenni'}
-                ref={register}
-              />
-            </GridColumn>
-          </GridRow>
-        </Box>
+        <Accordion singleExpand={false}>
+          {/* Applicant */}
+          <AccordionItem id="id_1" label="Upplýsingar umsækjanda" startExpanded>
+            <Box paddingY={4}>
+              <GridRow>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'applicant.nationalId'}
+                    name={'applicant.nationalId'}
+                    label={'Kennitala'}
+                    ref={register}
+                  />
+                </GridColumn>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'applicant.name'}
+                    name={'applicant.name'}
+                    label={'Nafn'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+              <Box marginTop={3} />
+              <GridRow>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'applicant.email'}
+                    name={'applicant.email'}
+                    label={'Netfang'}
+                    ref={register}
+                  />
+                </GridColumn>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'applicant.phoneNumber'}
+                    name={'applicant.phoneNumber'}
+                    label={'Símanúmer'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+              <Box marginTop={3} />
+              <GridRow>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'applicant.address'}
+                    name={'applicant.address'}
+                    label={'Heimilisfang'}
+                    ref={register}
+                  />
+                </GridColumn>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'applicant.zipCode'}
+                    name={'applicant.zipCode'}
+                    label={'Póstnúmer og staður'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+            </Box>
+          </AccordionItem>
+          {/* administrativeContact */}
+          <AccordionItem id="id_1" label="Ábyrgðarmaður" startExpanded>
+            <Box paddingY={4}>
+              <GridRow>
+                <GridColumn span="12/12">
+                  <Input
+                    id={'administrativeContact.name'}
+                    name={'administrativeContact.name'}
+                    label={'nafn'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+              <Box marginTop={3} />
+              <GridRow>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'administrativeContact.email'}
+                    name={'administrativeContact.email'}
+                    label={'Netfang'}
+                    ref={register}
+                  />
+                </GridColumn>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'administrativeContact.phoneNumber'}
+                    name={'administrativeContact.phoneNumber'}
+                    label={'Símanúmer'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+            </Box>
+          </AccordionItem>
+          {/* technicalContact */}
+          <AccordionItem id="id_1" label="Tæknilegur tengiliður" startExpanded>
+            <Box paddingY={4}>
+              <GridRow>
+                <GridColumn span="12/12">
+                  <Input
+                    id={'technicalContact.name'}
+                    name={'technicalContact.name'}
+                    label={'nafn'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+              <Box marginTop={3} />
+              <GridRow>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'technicalContact.email'}
+                    name={'technicalContact.email'}
+                    label={'Netfang'}
+                    ref={register}
+                  />
+                </GridColumn>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'technicalContact.phoneNumber'}
+                    name={'technicalContact.phoneNumber'}
+                    label={'Símanúmer'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+            </Box>
+          </AccordionItem>
+          {/* helpdeskContact */}
+          <AccordionItem id="id_1" label="Notendaaðstoð" startExpanded>
+            <Box paddingY={4}>
+              <GridRow>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'helpDesk.email'}
+                    name={'helpDesk.email'}
+                    label={'Netfang'}
+                    ref={register}
+                  />
+                </GridColumn>
+                <GridColumn span="6/12">
+                  <Input
+                    id={'helpDesk.phoneNumber'}
+                    name={'helpDesk.phoneNumber'}
+                    label={'Símanúmer'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+              <Box marginTop={3} />
+              <GridRow>
+                {/* Maybe add condition here, because this can be empty ? opinions */}
+                <GridColumn span="6/12">
+                  <Input
+                    id={'helpDesk.chatbot'}
+                    name={'helpDesk.chatbot'}
+                    label={'Snjallmenni'}
+                    ref={register}
+                  />
+                </GridColumn>
+              </GridRow>
+            </Box>
+          </AccordionItem>
+        </Accordion>
       </Box>
     </div>
   )
