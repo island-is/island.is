119a7a3f6902e2ff12bf62c131dfa0860fb365bf
Sigurður Sturla Bjarnason <sigurdur@fuglar.com>
2020-11-17 13:03:00 +0000

    Added promise to the functions

--- libs/auth-api-lib/seeders/20201115110448-update-secret.js
+++ libs/auth-api-lib/seeders/20201115110448-update-secret.js
@@ -14,16 +14,22 @@ module.exports = {
       },
     ]
 
-    queryInterface.bulkDelete('client_secret', {
-      client_id: 'island-is-client-cred-1',
+    return new Promise((resolve) => {
+      queryInterface.bulkDelete('client_secret', {
+        client_id: 'island-is-client-cred-1'
+      }).then(() => {
+        Promise.all([
+          queryInterface.bulkInsert('client_secret', secrets, {})
+        ]).then(() => {
+          resolve('done')
+        })
+      })
     })
-
-    return queryInterface.bulkInsert('client_secret', secrets, {})
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('client_secret', {
-      value: 'island-is-client-cred-1',
+      value: 'K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols=',
     })
   },
 }
