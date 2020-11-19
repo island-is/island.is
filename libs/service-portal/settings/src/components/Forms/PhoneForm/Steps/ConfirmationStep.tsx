0c10fa7346d1ba9016aac36cc1fa50b31d3ff227
atligudl <atli@sendiradid.is>
2020-11-19 10:56:52 +0000

    Service Portal Modal Refactor (#1933)
    * add initial visibility param to modal base, set disclosure param as optional
    
    * refactor service portal modal to use modal base core component
    
    * add toggle close flag to modal base, update user onboarding modal to handle side effects correctly
    
    * remove unused code
    
    * update modal close button
    
    * responsive styles
    
    * code comments and remove dev state
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/service-portal/settings/src/components/Forms/PhoneForm/Steps/ConfirmationStep.tsx
+++ libs/service-portal/settings/src/components/Forms/PhoneForm/Steps/ConfirmationStep.tsx
@@ -68,28 +68,37 @@ export const ConfirmationStep: FC<Props> = ({
           )}
         />
       </Box>
-      <Box display="flex" justifyContent="spaceBetween" marginTop={4}>
-        <Button variant="ghost" onClick={onBack}>
-          {formatMessage({
-            id: 'service.portal:go-back',
-            defaultMessage: 'Til baka',
-          })}
-        </Button>
-        <Button
-          type="submit"
-          variant="primary"
-          icon="arrowForward"
-          disabled={loading}
-        >
-          {formatMessage(
-            submitButtonText
-              ? submitButtonText
-              : {
-                  id: 'sp.settings:save-changes',
-                  defaultMessage: 'Vista breytingar',
-                },
-          )}
-        </Button>
+      <Box
+        display="flex"
+        justifyContent="spaceBetween"
+        flexWrap="wrap"
+        marginTop={4}
+      >
+        <Box marginBottom={[1, 0]}>
+          <Button variant="ghost" onClick={onBack}>
+            {formatMessage({
+              id: 'service.portal:go-back',
+              defaultMessage: 'Til baka',
+            })}
+          </Button>
+        </Box>
+        <Box marginBottom={[1, 0]}>
+          <Button
+            type="submit"
+            variant="primary"
+            icon="arrowForward"
+            disabled={loading}
+          >
+            {formatMessage(
+              submitButtonText
+                ? submitButtonText
+                : {
+                    id: 'sp.settings:save-changes',
+                    defaultMessage: 'Vista breytingar',
+                  },
+            )}
+          </Button>
+        </Box>
       </Box>
     </form>
   )
