13b7114f96629d14acd7f4831d4422526ec07f3e
Sigurgeir Guðmundsson <sigurgeirg@pop-os.localdomain>
2020-11-19 11:53:10 +0000

    remove backend

--- apps/skilavottord/backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'skilavottord-backend',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/skilavottord/backend',
-  setupFiles: ['./test/environment.jest.ts'],
-  setupFilesAfterEnv: ['./test/setup.ts'],
-  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
-}
