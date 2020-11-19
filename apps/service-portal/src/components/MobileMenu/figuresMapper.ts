6a7fc3e725a799c734e1a327f613d4cd20501296
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-11-19 14:33:52 +0000

    image refactor

--- apps/service-portal/src/components/MobileMenu/figuresMapper.ts
+++ apps/service-portal/src/components/MobileMenu/figuresMapper.ts
@@ -2,12 +2,14 @@ import { ServicePortalPath } from '@island.is/service-portal/core'
 
 export const getMobileMenuFigure = (path: ServicePortalPath | undefined) => {
   if (path === ServicePortalPath.FjarmalRoot)
-    return '/assets/images/payment.jpg'
-  if (path === ServicePortalPath.MinGognRoot) return '/assets/images/jobs.jpg'
-  if (path === ServicePortalPath.FamilyRoot) return '/assets/images/family.jpg'
-  if (path === ServicePortalPath.HeilsaRoot) return '/assets/images/baby.jpg'
+    return '/assets/images/payment.svg'
+  if (path === ServicePortalPath.MinGognRoot)
+    return '/assets/images/jobsGrid.svg'
+  if (path === ServicePortalPath.FamilyRoot)
+    return '/assets/images/familyGrid.svg'
+  if (path === ServicePortalPath.HeilsaRoot) return '/assets/images/health.svg'
   if (path === ServicePortalPath.MenntunRoot)
-    return '/assets/images/education.jpg'
-  if (path === ServicePortalPath.EignirRoot) return '/assets/images/school.jpg'
-  return '/assets/images/school.jpg'
+    return '/assets/images/education.svg'
+  if (path === ServicePortalPath.EignirRoot) return '/assets/images/school.svg'
+  return '/assets/images/school.svg'
 }
