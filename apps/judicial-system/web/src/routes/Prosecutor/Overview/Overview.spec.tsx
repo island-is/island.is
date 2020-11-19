d30723ce48bc53b3221b3c3ac0f64de5761f8fee
Ívar Oddsson <ivaroddsson@gmail.com>
2020-11-19 09:08:31 +0000

    Judicial system/Now using strict TS (#1923)
    * Started working on refactoring the user context
    
    * Refactor userContext
    
    * Refactor usercontest
    
    * Fix tests

--- apps/judicial-system/web/src/routes/Prosecutor/Overview/Overview.spec.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Overview/Overview.spec.tsx
@@ -2,28 +2,28 @@ import React from 'react'
 import { render, waitFor, screen } from '@testing-library/react'
 import Overview from './Overview'
 import * as Constants from '../../../utils/constants'
-import { userContext } from '@island.is/judicial-system-web/src/utils/userContext'
 import { MemoryRouter, Route } from 'react-router-dom'
-import {
-  mockCaseQueries,
-  mockProsecutorUserContext,
-} from '../../../utils/mocks'
+import { mockCaseQueries, mockProsecutorQuery } from '../../../utils/mocks'
 import { MockedProvider } from '@apollo/client/testing'
+import { UserProvider } from '@island.is/judicial-system-web/src/shared-components/UserProvider/UserProvider'
 
 describe('/stofna-krofu/yfirlit', () => {
   test('should display the approprieate custody provisions', async () => {
     // Arrange
     render(
-      <MockedProvider mocks={mockCaseQueries} addTypename={false}>
-        <userContext.Provider value={mockProsecutorUserContext}>
-          <MemoryRouter
-            initialEntries={[`${Constants.STEP_THREE_ROUTE}/test_id`]}
-          >
+      <MockedProvider
+        mocks={[...mockCaseQueries, ...mockProsecutorQuery]}
+        addTypename={false}
+      >
+        <MemoryRouter
+          initialEntries={[`${Constants.STEP_THREE_ROUTE}/test_id`]}
+        >
+          <UserProvider>
             <Route path={`${Constants.STEP_THREE_ROUTE}/:id`}>
               <Overview />
             </Route>
-          </MemoryRouter>
-        </userContext.Provider>
+          </UserProvider>
+        </MemoryRouter>
       </MockedProvider>,
     )
 
