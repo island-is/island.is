51c2dd115bb9c053c45b606750b7e118c335d3d6
unnarb <unnarb@gmail.com>
2020-11-23 09:41:33 +0000

    Fix formatting

--- libs/auth-api-lib/migrations/20201120202032-modify_client_add_new_fields.js
+++ libs/auth-api-lib/migrations/20201120202032-modify_client_add_new_fields.js
@@ -3,22 +3,22 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-    queryInterface.addColumn(
-      'client', // table name
-      'national_id', // new field name
-      {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-    ),
-    queryInterface.addColumn(
-      'client', // table name
-      'client_type', // new field name
-      {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-    ),])
+      queryInterface.addColumn(
+        'client', // table name
+        'national_id', // new field name
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      ),
+      queryInterface.addColumn(
+        'client', // table name
+        'client_type', // new field name
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      ),])
   },
 
   down: (queryInterface, Sequelize) => {
--- libs/auth-api-lib/seeders/20201120204558-update-clients-with-new-fields.js
+++ libs/auth-api-lib/seeders/20201120204558-update-clients-with-new-fields.js
@@ -2,10 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.query("UPDATE client SET national_id = '5501692829', client_type = 'spa' WHERE client_id IN ('island-is-client-cred-1', 'island-is-1', 'postman',  'apex-auth_client')");
+    return queryInterface.sequelize.query(
+      "UPDATE client SET national_id = '5501692829', client_type = 'spa' WHERE client_id IN ('island-is-client-cred-1', 'island-is-1', 'postman',  'apex-auth_client')",
+    )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.query("UPDATE client SET national_id = null, client_type = null WHERE client_id IN ('island-is-client-cred-1', 'island-is-1', 'postman',  'apex-auth_client')");
+    return queryInterface.sequelize.query(
+      "UPDATE client SET national_id = null, client_type = null WHERE client_id IN ('island-is-client-cred-1', 'island-is-1', 'postman',  'apex-auth_client')",
+    )
   },
 }
