894b348c37f72c55ad71f9515b6fde4e0943e4bb
hinriksteinar <hsv7@hi.is>
2020-11-24 12:48:10 +0000

    change to post and add required parameters

--- libs/application/templates/document-provider-onboarding/src/fields/TestPhase/TestEndPoint/index.tsx
+++ libs/application/templates/document-provider-onboarding/src/fields/TestPhase/TestEndPoint/index.tsx
@@ -5,25 +5,41 @@ import { FieldDescription } from '@island.is/shared/form-fields'
 import { useFormContext, Controller } from 'react-hook-form'
 import CopyToClipboardInput from '../../DocumentProvicerApplication/Components/CopyToClipboardInput/Index'
 
-const TestEnvironment: FC<FieldBaseProps> = () => {
+const TestEnvironment: FC<FieldBaseProps> = ({ application }) => {
   interface Variable {
     id: string
     name: string
     value: string
   }
 
-  const { register, errors, trigger } = useFormContext()
+  const { register, errors, trigger, getValues } = useFormContext()
 
+  //Todo: move this
   const fetchAndValidateData = async (isValid: boolean) => {
-    //TODO: If he presses save again ?
     if (isValid) {
-      fetch('/api/endPointVariables')
+      fetch('/api/endPointVariables', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          data: {
+            applicant: application.applicant,
+            endPoint: getValues('endPoint'),
+          },
+        }),
+      })
         .then((response) => response.json())
-        .then((json) => setendPointVariables(json))
+        .then((data) => {
+          setendPointVariables(data)
+        })
+        .catch((error) => {
+          //Todo: show error
+          console.error('Error:', error)
+        })
     }
   }
 
   let [variables, setendPointVariables] = useState<Variable[]>([])
+  console.log(variables)
 
   return (
     <Box>
@@ -54,21 +70,22 @@ const TestEnvironment: FC<FieldBaseProps> = () => {
         <Button
           variant="primary"
           onClick={() => {
-            trigger('endPoint').then((answer) => fetchAndValidateData(answer))
+            trigger(['endPoint']).then((answer) => fetchAndValidateData(answer))
           }}
         >
           Vista endapunkt
         </Button>
       </Box>
 
-      {variables.map((Variable) => (
-        <Box marginBottom={3} key={Variable.id}>
-          <CopyToClipboardInput
-            inputLabel={Variable.name}
-            inputValue={Variable.value}
-          ></CopyToClipboardInput>
-        </Box>
-      ))}
+      {variables &&
+        variables.map((Variable, index) => (
+          <Box marginBottom={3} key={index}>
+            <CopyToClipboardInput
+              inputLabel={Variable.name}
+              inputValue={Variable.value}
+            ></CopyToClipboardInput>
+          </Box>
+        ))}
     </Box>
   )
 }
