0316ed3129c8fdd1137309778fa8c97a5554b4ee
Andri Andresson <andriandresson@gmail.com>
2020-11-25 09:11:24 +0000

    feat: updated localization strings

--- libs/service-portal/settings/src/components/Forms/LanguageForm.tsx
+++ libs/service-portal/settings/src/components/Forms/LanguageForm.tsx
@@ -1,5 +1,5 @@
 import { Box, Select } from '@island.is/island-ui/core'
-import { Locale } from '@island.is/localization'
+import { Locale, useLocale } from '@island.is/localization'
 import React, { FC, useEffect } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 
@@ -26,6 +26,7 @@ export const LanguageForm: FC<Props> = ({
   onSubmit,
 }) => {
   const { handleSubmit, control, reset } = useForm()
+  const { formatMessage } = useLocale()
 
   useEffect(() => {
     if (language)
@@ -46,7 +47,10 @@ export const LanguageForm: FC<Props> = ({
               name={name}
               value={value}
               onChange={onChange}
-              label="Tungumál"
+              label={formatMessage({
+                id: 'service.portal:language',
+                defaultMessage: 'Tungumál',
+              })}
               options={[
                 { label: 'Íslenska', value: 'is' },
                 { label: 'English', value: 'en' },
