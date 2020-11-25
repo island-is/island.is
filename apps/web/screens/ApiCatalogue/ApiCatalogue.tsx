5b2645f313568a3047ca3c607e61c07c24b95e8f
Sævar Ingi <saevar43@gmail.com>
2020-11-25 17:38:43 +0000

    Feature/api catalogue webpages (#2003)
    * Added starter api-catalogue pages and screens to web app
    
    * run yarn format
    
    * Added configuration to web to disable ApiCatalog pages
    
    * run yarn format
    
    * made disableApiCatalog an env variable
    
    * run yarn format
    
    Co-authored-by: Sævar Ingi Sigurðsson <saevar.ingi.sigurdsson@advania.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/web/screens/ApiCatalogue/ApiCatalogue.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Screen } from '@island.is/web/types'
+import { withMainLayout } from '@island.is/web/layouts/main'
+import getConfig from 'next/config'
+import { CustomNextError } from '@island.is/web/units/errors'
+
+const { publicRuntimeConfig } = getConfig()
+
+/* TEMPORARY LAYOUT CREATED TO SCAFFOLD API CATALOGUE INTO THE WEB */
+
+interface ApiCatalogueProps {
+  title: string
+}
+
+const ApiCatalogue: Screen<ApiCatalogueProps> = ({ title }) => {
+  const { disableApiCatalog: disablePage } = publicRuntimeConfig
+
+  if (disablePage === 'true') {
+    throw new CustomNextError(404, 'Not found')
+  }
+
+  return <h1>{title}</h1>
+}
+
+ApiCatalogue.getInitialProps = async ({ apolloClient, locale, query }) => {
+  return {
+    title: 'Vörulisti Vefþjónusta',
+  }
+}
+
+export default withMainLayout(ApiCatalogue)
