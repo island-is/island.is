5977b159af1382af8336295bf008e8d75f82ca2c
hinriksteinar <hsv7@hi.is>
2020-11-26 11:41:40 +0000

    Feature/document provider application test account (#2016)
    * file structure cleanup
    
    * mirage initial setup
    
    * minor fix on mirage initial setup
    
    * minor fix on mirage initial setup
    
    * test-account screen
    
    * endpoint component implementantion
    
    * endpoint component change text
    
    * removed console log
    
    * Automated test screen
    
    * added production environment component
    
    * removed unused fields and text fixes
    
    * copyToClipboard component
    
    * merge main
    
    * added introscreen to automated tests
    
    * copy to clipboard code cleanup
    
    * error handling on endpoint
    
    * cleanup of unused imports
    
    * change to post and add required parameters
    
    * Error handling form Automated tests and loading
    
    * lint fixes
    
    * lint fixes
    
    * lint fixes
    
    * fixing copyToClipdoard
    
    * added url validation
    
    Co-authored-by: ssh16 <saeunnsh@advania.is>

---
+++ libs/application/templates/document-provider-onboarding/src/mirage-server.ts
@@ -0,0 +1,70 @@
+import { Server } from 'miragejs'
+
+export function makeServer({ environment = 'development' } = {}) {
+  new Server({
+    routes() {
+      this.passthrough('http://localhost:4444/api/graphql')
+      this.get('/api/keys', () => [
+        {
+          id: '1',
+          name: 'Client ID',
+          value: '5016d8d5cb6ce0758107b9969ea3c201',
+        },
+        {
+          id: '2',
+          name: 'Secret key',
+          value: '5016d8d5cb6ce0758107b9969ea3c201',
+        },
+      ])
+      this.post('/api/endPointVariables', (schema, request) => {
+        const attrs = JSON.parse(request.requestBody)
+        console.log(attrs)
+        const data = [
+          {
+            id: '1',
+            name: 'Audience',
+            value: '5016d8d5cb6ce0758107b9969ea3c201',
+          },
+          {
+            id: '2',
+            name: 'Scope',
+            value: '5016d8d5cb6ce0758107b9969ea3c201',
+          },
+        ]
+        return data
+      })
+      this.get(
+        '/api/testMyEndpoint',
+        () => [
+          {
+            id: '1',
+            isValid: true,
+            message: 'Skjal fannst fyrir skráða kennitölu',
+          },
+          {
+            id: '2',
+            isValid: false,
+            message: 'Ekki tókst að sækja skjal til skjalaveitu',
+          },
+        ],
+        {
+          timing: 2000,
+        },
+      )
+      this.get('/api/prodKeys', () => [
+        {
+          id: '1',
+          name: 'Client ID',
+          value: '5016d8d5cb6ce0758107b9969ea3c201',
+        },
+        {
+          id: '2',
+          name: 'Secret key',
+          value: '5016d8d5cb6ce0758107b9969ea3c201',
+        },
+      ])
+    },
+  })
+}
+
+export default makeServer
