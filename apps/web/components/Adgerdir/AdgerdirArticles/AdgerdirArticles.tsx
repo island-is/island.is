012d3208444d6e4b8eeb522b0b5c2789d5350b57
Ívar Oddsson <ivaroddsson@gmail.com>
2020-11-26 13:57:17 +0000

    Fix tags in adgerdir project

--- apps/web/components/Adgerdir/AdgerdirArticles/AdgerdirArticles.tsx
+++ apps/web/components/Adgerdir/AdgerdirArticles/AdgerdirArticles.tsx
@@ -313,7 +313,7 @@ export const AdgerdirArticles: FC<AdgerdirArticlesProps> = ({
                       id,
                       title,
                       tagProps: {
-                        outlined: true,
+                        label: true,
                       },
                     }
                   })}
