024a48afc7862911cd5e4fbf089434fedc003049
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-11-27 15:19:34 +0000

    dl/medical-certificate

--- libs/application/templates/driving-license/src/forms/application.ts
+++ libs/application/templates/driving-license/src/forms/application.ts
@@ -133,6 +133,35 @@ export const application: Form = buildForm({
       ],
     }),
     buildSection({
+      id: 'healthDeclaration',
+      name: 'Heilbrigðisyfirlýsing',
+      children: [
+        buildMultiField({
+          id: 'healthDeclaration',
+          name: 'Heilbrigðisyfirlýsing',
+          children: [
+            buildIntroductionField({
+              id: 'intro',
+              name: '',
+              introduction:
+                'Ef sótt er um réttindi í flokkum <b>AM</b>, <b>A1</b>, <b>A2</b>, <b>A</b>, <b>B</b>, <b>BE</b> eða <b>T</b> nægir heilbrigðisyfirlýsing ein og sér, nema sýslumaður telji þörf á læknisvottorði eða ef umsækjandi hefur náð 65 ára aldri eða hann vilji heldur skila læknisvottorði. Með umsókn um aðra flokka ökuréttinda (aukin ökuréttindi) er krafist læknisvottorðs á sérstöku eyðublaði.',
+            }),
+            buildCheckboxField({
+              id: 'useMedicalCertification',
+              name: '',
+              options: [
+                {
+                  value: 'useMedicalCertification',
+                  label:
+                    'Umsækjandi óskar eftir að skila inn læknisvottorði í stað heilbrigðisyfirlýsingu',
+                },
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+    buildSection({
       id: 'confirmation',
       name: 'Staðfesta',
       children: [
