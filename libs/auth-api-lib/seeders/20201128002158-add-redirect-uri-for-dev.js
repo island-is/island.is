425554e1c54f17363e27edcb658dd04a612998a6
Sigurður Sturla Bjarnason <sigurdur@fuglar.com>
2020-11-28 01:21:09 +0000

    Adding the seed files only to be run on dev env

---
+++ libs/auth-api-lib/seeders/20201128002158-add-redirect-uri-for-dev.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface) => {
+    const redirectUri = [
+      {
+        client_id: 'island-is-1',
+        redirect_uri: 'https://localhost:5001/oauth2-redirect.html',
+      },
+    ]
+
+    if (process.env.NODE_ENV === 'development') {
+      return queryInterface.bulkInsert('client_redirect_uri', redirectUri, {})
+    } else {
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('done'), 100)
+      })
+    }
+  },
+
+  down: (queryInterface, Sequelize) => {
+    if (process.env.NODE_ENV === 'development') {
+      return queryInterface.bulkDelete('client_redirect_uri', {
+        client_id: 'island-is-1',
+        redirect_uri: 'https://localhost:5001/oauth2-redirect.html',
+      })
+    } else {
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('done'), 100)
+      })
+    }
+  },
+}
---
+++ libs/auth-api-lib/seeders/20201128002958-add-scope-for-dev.js
@@ -0,0 +1,37 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface) => {
+    const clientAllowedScope = [
+      {
+        client_id: 'island-is-client-cred-1',
+        scope_name: 'api_resource.scope',
+      },
+    ]
+
+    if (process.env.NODE_ENV === 'development') {
+      return queryInterface.bulkInsert(
+        'client_allowed_scope',
+        clientAllowedScope,
+        {},
+      )
+    } else {
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('done'), 100)
+      })
+    }
+  },
+
+  down: (queryInterface, Sequelize) => {
+    if (process.env.NODE_ENV === 'development') {
+      return queryInterface.bulkDelete('client_allowed_scope', {
+        client_id: 'island-is-client-cred-1',
+        scope_name: 'api_resource.scope',
+      })
+    } else {
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve('done'), 100)
+      })
+    }
+  },
+}
