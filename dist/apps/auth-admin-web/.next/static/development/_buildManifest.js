24248e814bea98798ba40b39667b2a746ee898ef
Sigurður Sturla Bjarnason <sigurdur@fuglar.com>
2020-11-30 11:14:40 +0000

    Removing github cache

--- dist/apps/auth-admin-web/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/client":["static\u002Fchunks\u002Fpages\u002Fclient.js"],"/resources":["static\u002Fchunks\u002Fpages\u002Fresources.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
