1547d264524d3a475614b4c3552baf60dc378caa
unnarb <unnarb@gmail.com>
2020-12-01 09:18:55 +0000

    remove dist

--- dist/apps/auth-admin-web/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/404":["static\u002Fchunks\u002Fpages\u002F404.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
