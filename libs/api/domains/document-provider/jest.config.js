4ca4976fbefc82bea0bf147249914e0be729ef00
Guðjón Ingi Magnússon <fjandakornid@gmail.com>
2020-12-01 13:48:06 +0000

    Feature/document provider api (#2077)
    * Added document-provider-api and mocked base functionality
    
    * fixed linting errors, added minor documentation and added to codeowners file
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/api/domains/document-provider/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'api-domains-document-provider',
+  preset: '../../../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/api/domains/document-provider',
+}
