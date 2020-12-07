efe7ee2f5859896ea7c02f18b3e03ac4b71882e8
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2020-12-07 11:37:49 +0000

    Add comment on filter props

--- libs/island-ui/core/src/lib/Filter/Filter.tsx
+++ libs/island-ui/core/src/lib/Filter/Filter.tsx
@@ -4,7 +4,14 @@ import { Button } from '../Button/Button'
 import { Stack } from '../Stack/Stack'
 
 export interface FilterProps {
+  /**
+   * Label for the clear button.
+   * Should be used for localization.
+   */
   labelClear: string
+  /**
+   * Event handler for clear filter event.
+   */
   onFilterClear: () => void
 }
 
