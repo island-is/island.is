f0f0b9705ce78c506ca2d4383ec933ab46a8e787
Sigga Hrafns <sigridur@kosmosogkaos.is>
2020-12-08 08:52:13 +0000

    Remove unnecessary test

--- libs/island-ui/core/src/lib/Navigation/Navigation.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-describe('Navigation', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<div>ok</div>)
-    expect(baseElement).toBeTruthy()
-  })
-})
