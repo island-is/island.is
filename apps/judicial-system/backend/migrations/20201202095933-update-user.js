33d50a28a28b482f0591a6d51c3e54ea20f82863
Guðjón Guðjónsson <gudjon@kolibri.is>
2020-12-08 10:13:58 +0000

    Judicial system/user institution (#2140)
    * Adds a migration for user institution
    
    * Makes user institution optional
    
    * Uses institusion in request pdf
    
    * Fixes unit test

---
+++ apps/judicial-system/backend/migrations/20201202095933-update-user.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.addColumn(
+        'user',
+        'institution',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction: t },
+      ),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.removeColumn('user', 'institution', { transaction: t }),
+    )
+  },
+}
