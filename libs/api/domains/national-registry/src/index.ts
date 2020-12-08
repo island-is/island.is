4ad2e1e3c9b4a5c3dfbda3414fac80455ac20e2d
Darri Steinn Konráðsson <darri@viceversa.is>
2020-12-08 14:00:51 +0000

    nr/speed up resolvers by fetching on demand (#2146)
    * Split up resolvers, since the resolve different resources
    
    * Resolve individual fields on demand to speed up querying
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/national-registry/src/index.ts
+++ libs/api/domains/national-registry/src/index.ts
@@ -1,2 +1,3 @@
-export * from './lib/nationalRegistry.module'
-export * from './lib/nationalRegistry.resolver'
+export { FamilyMemberResolver as NationalRegistryFamilyMemberResolver } from './lib/familyMember.resolver'
+export { NationalRegistryModule } from './lib/nationalRegistry.module'
+export { UserResolver as NationalRegistryUserResolver } from './lib/user.resolver'
--- libs/api/domains/national-registry/src/lib/familyMember.resolver.ts
+++ libs/api/domains/national-registry/src/lib/familyMember.resolver.ts
@@ -12,23 +12,17 @@ import {
 } from '@island.is/auth-nest-tools'
 
 @UseGuards(IdsAuthGuard, ScopesGuard)
-@Resolver()
-export class NationalRegistryResolver {
-  constructor(private nationalRegistryService: NationalRegistryService) {}
-  //Touching this
-  @Query(() => NationalRegistryUser, {
-    name: 'nationalRegistryUser',
-    nullable: true,
-  })
-  getMyInfo(@CurrentUser() user: AuthUser): Promise<User> {
-    return this.nationalRegistryService.GetMyinfo(user.nationalId)
-  }
+@Resolver(() => NationalRegistryFamilyMember)
+export class FamilyMemberResolver {
+  constructor(
+    private readonly nationalRegistryService: NationalRegistryService,
+  ) {}
 
   @Query(() => [NationalRegistryFamilyMember], {
     name: 'nationalRegistryFamily',
     nullable: true,
   })
   getMyFamily(@CurrentUser() user: AuthUser): Promise<FamilyMember[]> {
-    return this.nationalRegistryService.GetMyFamily(user.nationalId)
+    return this.nationalRegistryService.getFamily(user.nationalId)
   }
 }
