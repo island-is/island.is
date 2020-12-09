a7acb8fe6e7e38b650217033730e707286b057fd
guttih <gudjonholm@gmail.com>
2020-12-09 15:52:46 +0000

    Adding servicelist and serviceDetail under the new layout

---
+++ apps/web/components/ServiceListContainer/index.ts
@@ -0,0 +1,1 @@
+export { ServiceListContainer } from './ServiceListContainer'
--- apps/web/screens/ApiCatalogue/ApiCatalogue.tsx
+++ apps/web/screens/ApiCatalogue/ApiCatalogue.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import { Screen } from '@island.is/web/types'
+
+import { GetNamespaceQuery } from '@island.is/web/graphql/schema'
+import {
+  Query,
+  QueryGetApiCatalogueArgs,
+  QueryGetNamespaceArgs,
+} from '@island.is/api/schema'
+
+import { GET_NAMESPACE_QUERY } from '../queries'
+import { GET_CATALOGUE_QUERY } from '../queries/ApiCatalogue'
+import { useNamespace } from '../../hooks'
+
 import { withMainLayout } from '@island.is/web/layouts/main'
 import { SubpageLayout } from '@island.is/web/screens/Layouts/Layouts'
 import SidebarLayout from '@island.is/web/screens/Layouts/SidebarLayout'
@@ -40,8 +52,46 @@ const ApiCatalogue: Screen<ApiCatalogueProps> = ({ subpageHeader }) => {
   /* DISABLE FROM WEB WHILE WIP */
   const { disableApiCatalog: disablePage } = publicRuntimeConfig
 
-  if (disablePage === 'true') {
-    throw new CustomNextError(404, 'Not found')
+const ApiCatalogue: Screen<ApiCatalogueProps> = ({
+  title,
+  data,
+  loading,
+  error,
+  designGuideContent,
+  staticContent,
+  filterContent,
+}) => {
+  // const { disableApiCatalog: disablePage } = publicRuntimeConfig
+
+  // if (disablePage === 'true') {
+  //   throw new CustomNextError(404, 'Not found')
+  // }
+
+  const n = useNamespace(staticContent)
+  const fn = useNamespace(filterContent)
+  const dn = useNamespace(designGuideContent)
+  const TEXT_NOT_FOUND = n('notFound')
+  const HEADING_ERROR = n('errorHeading')
+  const TEXT_ERROR = n('errorText')
+  const TEXT_LOAD_MORE = n('fmButton')
+
+  const translateTags = (): TagDisplayNames => {
+    const names: TagDisplayNames = {
+      APIGW: fn('accessApigw'),
+      XROAD: fn('accessXroad'),
+      FINANCIAL: fn('dataFinancial'),
+      HEALTH: fn('dataHealth'),
+      OFFICIAL: fn('dataOfficial'),
+      PERSONAL: fn('dataPersonal'),
+      PUBLIC: fn('dataPublic'),
+      FREE: fn('pricingFree'),
+      PAID: fn('pricingPaid'),
+      GRAPHQL: fn('typeGraphql'),
+      REST: fn('typeRest'),
+      SOAP: fn('typeSoap'),
+      OPEN: 'OPEN', //tag not currently used
+    }
+    return names
   }
   /* --- */
   const { activeLocale } = useI18n()
