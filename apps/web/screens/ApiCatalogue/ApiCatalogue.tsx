b6cec0361619c26947740cecbbb55243b902790d
Sævar Ingi Sigurðsson <saevar.ingi.sigurdsson@advania.is>
2020-12-11 15:09:18 +0000

    simplify ServiceList and move business logic to ApiCatalogue screen

--- apps/web/screens/ApiCatalogue/ApiCatalogue.tsx
+++ apps/web/screens/ApiCatalogue/ApiCatalogue.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import getConfig from 'next/config'
 
 import { Screen } from '@island.is/web/types'
+import { CustomNextError } from '@island.is/web/units/errors'
 
 import { GetNamespaceQuery } from '@island.is/web/graphql/schema'
 import {
@@ -11,7 +12,7 @@ import {
   GetApiCatalogueInput,
 } from '@island.is/api/schema'
 
-import { GET_NAMESPACE_QUERY, GET_CATALOGUE_QUERY } from '../queries'
+import { GET_NAMESPACE_QUERY, GET_CATALOGUE_QUERY } from '@island.is/web/screens/queries'
 import { useNamespace } from '../../hooks'
 
 import { withMainLayout } from '@island.is/web/layouts/main'
@@ -55,7 +56,7 @@ const ApiCatalogue: Screen<ApiCatalogueProps> = ({ subpageHeader }) => {
   const { disableApiCatalog: disablePage } = publicRuntimeConfig
 
 const ApiCatalogue: Screen<ApiCatalogueProps> = ({
-  title,
+  mainContent,
   staticContent,
   filterContent,
 }) => {
@@ -65,7 +66,9 @@ const ApiCatalogue: Screen<ApiCatalogueProps> = ({
     throw new CustomNextError(404, 'Not found')
   }
 
-  const n = useNamespace(staticContent)
+  
+  const n = useNamespace(mainContent)
+  const sn = useNamespace(staticContent)
   const fn = useNamespace(filterContent)
 
   const translateTags = (): ServiceTagDisplayNames => {
@@ -119,11 +122,11 @@ const ApiCatalogue: Screen<ApiCatalogueProps> = ({
   })
 
   const { data, loading, error, fetchMore, refetch } = useQuery<
-    Query,
-    QueryGetApiCatalogueArgs
-  >(GET_CATALOGUE_QUERY, {
-    variables: {
-      input: parameters,
+      Query,
+      QueryGetApiCatalogueArgs
+    >(GET_CATALOGUE_QUERY, {
+      variables: {
+        input: parameters,
     },
   })
 
