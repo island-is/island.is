bf0f22f3ded3df9fbe2f576cf1ae0967cc8ee012
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-12-11 12:24:47 +0000

    plausible util for events added

---
+++ apps/service-portal/src/utils/plausibleEvent.ts
@@ -0,0 +1,7 @@
+export const plausibleEvent = (eventName: string, parameters: object) => {
+  const plausible = (<any>window).plausible
+  if (plausible) {
+    plausible(eventName, { props: parameters })
+    console.log(eventName, { props: parameters })
+  }
+}
