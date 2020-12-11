c18d2712dc6eb8a13dba52c6c1ed78d6b7b55392
guttih <gudjonholm@gmail.com>
2020-12-11 14:34:23 +0000

    api-catalogue component *ServiceInformation*  added in ServiceDetails in web

---
+++ apps/web/components/ServiceInformation/ServiceInformation.tsx
@@ -0,0 +1,196 @@
+import React from 'react'
+import { useWindowSize, useIsomorphicLayoutEffect } from 'react-use'
+import {
+  Box,
+  Breadcrumbs,
+  GridColumn,
+  GridRow,
+  Inline,
+  Tag,
+  Text,
+} from '@island.is/island-ui/core'
+import { theme } from '@island.is/island-ui/theme'
+import { ApiService } from '@island.is/api/schema'
+import { GetNamespaceQuery } from '@island.is/web/graphql/schema'
+import { useNamespace } from '../../hooks'
+
+export interface ServiceInformationProps {
+  service: ApiService
+  strings: GetNamespaceQuery['getNamespace']
+}
+
+export const ServiceInformation = ({
+  service,
+  strings,
+}: ServiceInformationProps) => {
+  const { width } = useWindowSize()
+  const [isMobile, setIsMobile] = React.useState(false)
+
+  useIsomorphicLayoutEffect(() => {
+    if (width < theme.breakpoints.md) {
+      return setIsMobile(true)
+    }
+    setIsMobile(false)
+  }, [width])
+
+  const n = useNamespace(strings)
+
+  const capitalize = (s: string) => {
+    if (typeof s !== 'string') return ''
+
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
+  }
+
+  const horizontalLine = () => {
+    return (
+      <div
+        style={{
+          borderBottomWidth: 1,
+          borderBottomStyle: 'solid',
+          borderBottomColor: theme.color.blue200,
+        }}
+      />
+    )
+  }
+  const showMobileTags = () => {
+    return (
+      <div>
+        {/* Heading for data tags */}
+        <GridRow>
+          <GridColumn paddingTop="containerGutter">
+            <Text variant="h3">{n('data')}</Text>
+          </GridColumn>
+        </GridRow>
+        {horizontalLine()}
+        {/* Tags for data  */}
+        <GridRow>
+          <GridColumn paddingTop="gutter">
+            <Inline space={1}>
+              {service.data?.map((item) => (
+                <Tag variant="white" outlined>
+                  {n(`data${capitalize(item)}`)}
+                </Tag>
+              ))}
+            </Inline>
+          </GridColumn>
+        </GridRow>
+        {/* Headings for type and access tags*/}
+        <GridRow>
+          <GridColumn span="2/4" paddingTop="containerGutter">
+            <Text variant="h3">{n('type')}</Text>
+          </GridColumn>
+          <GridColumn span="2/4" paddingTop="containerGutter">
+            <Text variant="h3">{n('access')}</Text>
+          </GridColumn>
+        </GridRow>
+        {horizontalLine()}
+        {/* Tags for type and access*/}
+        <GridRow>
+          <GridColumn span="2/4" paddingTop="gutter">
+            <Inline space={1}>
+              {service.type?.map((item) => (
+                <Tag variant="white" outlined>
+                  {n(`type${capitalize(item)}`)}
+                </Tag>
+              ))}
+            </Inline>
+          </GridColumn>
+          <GridColumn span="2/4" paddingTop="gutter">
+            <Inline space={1}>
+              {service.access?.map((item) => (
+                <Tag variant="white" outlined>
+                  {n(`access${capitalize(item)}`)}
+                </Tag>
+              ))}
+            </Inline>
+          </GridColumn>
+        </GridRow>
+      </div>
+    )
+  }
+  const showDesktopTags = () => {
+    return (
+      <div>
+        {/* Headings for tags */}
+        <GridRow>
+          <GridColumn span="2/4" paddingTop="containerGutter">
+            <Text variant="h3">{n('data')}</Text>
+          </GridColumn>
+          <GridColumn span="1/4" paddingTop="containerGutter">
+            <Text variant="h3">{n('type')}</Text>
+          </GridColumn>
+          <GridColumn span="1/4" paddingTop="containerGutter">
+            <Text variant="h3">{n('access')}</Text>
+          </GridColumn>
+        </GridRow>
+        {horizontalLine()}
+        {/* Tags */}
+        <GridRow>
+          <GridColumn span="2/4" paddingTop="gutter">
+            <Inline space={1}>
+              {service.data?.map((item) => (
+                <Tag variant="white" outlined>
+                  {n(`data${capitalize(item)}`)}
+                </Tag>
+              ))}
+            </Inline>
+          </GridColumn>
+          <GridColumn span="1/4" paddingTop="gutter">
+            <Inline space={1}>
+              {service.type?.map((item) => (
+                <Tag variant="white" outlined>
+                  {n(`type${capitalize(item)}`)}
+                </Tag>
+              ))}
+            </Inline>
+          </GridColumn>
+          <GridColumn span="1/4" paddingTop="gutter">
+            <Inline space={1}>
+              {service.access?.map((item) => (
+                <Tag variant="white" outlined>
+                  {n(`access${capitalize(item)}`)}
+                </Tag>
+              ))}
+            </Inline>
+          </GridColumn>
+        </GridRow>
+      </div>
+    )
+  }
+  // Main page
+  return (
+    <Box>
+      <Breadcrumbs>
+        <a href="/throun/vefthjonustur">Vefþjónustur</a>
+        <a href="/throun/vefthjonustur/vorulisti">API Vörulisti</a>
+        <span>{service.name}</span>
+      </Breadcrumbs>
+      <GridRow>
+        <GridColumn>
+          <Text variant="h1">{service.name}</Text>
+        </GridColumn>
+        {service.pricing.length > 0 && (
+          <GridColumn>
+            <Tag variant="white" outlined>
+              {n(`pricing${capitalize(service.pricing[0])}`)}
+            </Tag>
+          </GridColumn>
+        )}
+      </GridRow>
+      <GridRow>
+        <GridColumn paddingTop="gutter">
+          <Text variant="intro">{service.description}</Text>
+        </GridColumn>
+      </GridRow>
+      <GridRow>
+        <GridColumn paddingTop="gutter">
+          <Text variant="h3">Framleiðandi</Text>
+        </GridColumn>
+        <GridColumn paddingTop="gutter">
+          <Text variant="intro">{service.owner}</Text>
+        </GridColumn>
+      </GridRow>
+      {isMobile ? showMobileTags() : showDesktopTags()}
+    </Box>
+  )
+}
--- apps/web/screens/ServiceDetails/ServiceDetails.tsx
+++ apps/web/screens/ServiceDetails/ServiceDetails.tsx
@@ -12,45 +12,60 @@ import {
 } from '@island.is/api/schema'
 import { GET_NAMESPACE_QUERY } from '../queries'
 import { GET_API_SERVICE_QUERY } from '../queries/ApiCatalogue'
-import { ApiService } from '@island.is/api/schema'
 import {
-  ServiceView,
   SubpageDetailsContent,
   SubpageMainContent,
+  ServiceInformation,
 } from '../../components'
-import { ApolloClient, NormalizedCacheObject, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { SubpageLayout } from '../Layouts/Layouts'
 import SidebarLayout from '../Layouts/SidebarLayout'
+import { LoadingIcon } from '@island.is/island-ui/core'
 
 const { publicRuntimeConfig } = getConfig()
 
-
 interface ServiceDetailsProps {
-  service: ApiService
+  //service: ApiService
+  serviceId: string
   strings: GetNamespaceQuery['getNamespace']
 }
 
-const ServiceDetails: Screen<ServiceDetailsProps> = ({ service, strings }) => {
+const ServiceDetails: Screen<ServiceDetailsProps> = ({
+  /*service,*/ serviceId,
+  strings,
+}) => {
   const { disableApiCatalog: disablePage } = publicRuntimeConfig
 
   if (disablePage === 'true') {
     throw new CustomNextError(404, 'Not found')
   }
 
-  // const { data, loading, error } = useQuery<Query, QueryGetApiServiceByIdArgs>(GET_API_SERVICE_QUERY, {
-  //   variables: {
-  //     input: {
-  //       id: serviceId,
-  //     },
-  //   },
-  // })
+  const { data, loading, error } = useQuery<Query, QueryGetApiServiceByIdArgs>(
+    GET_API_SERVICE_QUERY,
+    {
+      variables: {
+        input: {
+          id: serviceId,
+        },
+      },
+    },
+  )
 
   return (
     <SubpageLayout
       main={
         <SidebarLayout sidebarContent={<div>Navigation menu kemur hér</div>}>
           <SubpageMainContent
-            main={<ServiceView strings={strings} service={service} />}
+            main={
+              loading ? (
+                <LoadingIcon animate color="blue400" size={32} />
+              ) : (
+                <ServiceInformation
+                  strings={strings}
+                  service={data.getApiServiceById}
+                />
+              )
+            }
           />
         </SidebarLayout>
       }
@@ -74,20 +89,6 @@ const ServiceDetails: Screen<ServiceDetailsProps> = ({ service, strings }) => {
 
 ServiceDetails.getInitialProps = async ({ apolloClient, locale, query }) => {
   const serviceId = String(query.slug)
-  const { data, loading, error } = await apolloClient.query<
-    Query,
-    QueryGetApiServiceByIdArgs
-  >({
-    query: GET_API_SERVICE_QUERY,
-    variables: {
-      input: {
-        id: serviceId,
-      },
-    },
-  })
-  const service: ApiService = data.getApiServiceById
-    ? data.getApiServiceById
-    : null
 
   const [filterContent] = await Promise.all([
     apolloClient
@@ -104,7 +105,7 @@ ServiceDetails.getInitialProps = async ({ apolloClient, locale, query }) => {
   ])
 
   return {
-    service: service,
+    serviceId: serviceId,
     strings: filterContent,
   }
 }
