9df9464a76ef73262c24f8ea301e2bdac9266871
Ívar Oddsson <ivaroddsson@gmail.com>
2020-12-11 11:08:26 +0000

    Update apps/judicial-system/web/src/shared-components/BlueBox/BlueBox.tsx
    Co-authored-by: Brian <barabrian89@gmail.com>

--- apps/judicial-system/web/src/shared-components/BlueBox/BlueBox.tsx
+++ apps/judicial-system/web/src/shared-components/BlueBox/BlueBox.tsx
@@ -1,9 +1,9 @@
 import { Box } from '@island.is/island-ui/core'
 import React from 'react'
-import * as styles from './BlueBox.treat'
-
-const BlueBox: React.FC<{}> = (props) => {
-  return <Box className={styles.blueBox}>{props.children}</Box>
-}
+const BlueBox: React.FC = ({ children }) => (
+  <Box background="blue100" padding={3} borderRadius="large">
+    {children}
+  </Box>
+)
 
 export default BlueBox
