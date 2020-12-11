c0e0f7f07764594d9957c7aa754c63e3a9098627
Ívar Oddsson <ivaroddsson@gmail.com>
2020-12-11 09:12:43 +0000

    Added delete case animation

--- apps/judicial-system/web/src/routes/Shared/DetentionRequests/DetentionRequests.treat.ts
+++ apps/judicial-system/web/src/routes/Shared/DetentionRequests/DetentionRequests.treat.ts
@@ -1,4 +1,4 @@
-import { style } from 'treat'
+import { style, styleMap } from 'treat'
 import { theme } from '@island.is/island-ui/theme'
 
 export const detentionRequestsContainer = style({
@@ -32,13 +32,66 @@ export const detentionRequestsError = style({
 })
 
 export const detentionRequestsTableRow = style({
+  display: 'flex',
+  flex: 1,
   borderBottom: `1px solid ${theme.color.blue200}`,
   cursor: 'pointer',
+})
+
+export const thead = style({
+  display: 'flex',
+  background: theme.color.blue100,
+  boxShadow: `inset 0px -1px 0px ${theme.color.blue200}`,
+})
+
+export const deleteButtonContainer = style({
+  boxShadow: `inset 0px -1px 0px ${theme.color.blue200}`,
 })
 
 export const thButton = style({
   ':active': {
     color: theme.color.dark400,
+  },
+})
+
+export const tableRowContainer = style({
+  minWidth: '100%',
+  transition: 'all .2s ease-in-out',
+  float: 'right',
+
+  selectors: {
+    ['&.isDeleting']: {
+      minWidth: 'calc(100% + 100px)',
+    },
+  },
+})
+
+export const tr = style({
+  display: 'flex',
+  flex: 1,
+})
+
+export const th = style({
+  display: 'flex',
+  flex: 1,
+  padding: '12px 24px',
+})
+
+export const td = style({
+  display: 'flex',
+  flex: 1,
+  padding: theme.spacing[3],
+})
+
+export const deleteButton = styleMap({
+  closed: {
+    width: 0,
+    overflow: 'hidden',
+    transition: 'all .2s ease-in-out',
+  },
+  open: {
+    width: '100px',
+    transition: 'all .2s ease-in-out',
   },
 })
 
