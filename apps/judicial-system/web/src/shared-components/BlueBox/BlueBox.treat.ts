0dc37c7efc3613100624323473f69c6066bb222f
Ívar Oddsson <ivaroddsson@gmail.com>
2020-12-11 11:09:33 +0000

    Refactor BlueBox

--- apps/judicial-system/web/src/shared-components/BlueBox/BlueBox.treat.ts
+++
@@ -1,8 +0,0 @@
-import { theme } from '@island.is/island-ui/theme'
-import { style } from 'treat'
-
-export const blueBox = style({
-  backgroundColor: theme.color.blue100,
-  borderRadius: theme.border.radius.large,
-  padding: theme.spacing[3],
-})
--- apps/judicial-system/web/src/shared-components/BlueBox/BlueBox.tsx
+++ apps/judicial-system/web/src/shared-components/BlueBox/BlueBox.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@island.is/island-ui/core'
 import React from 'react'
+
 const BlueBox: React.FC = ({ children }) => (
   <Box background="blue100" padding={3} borderRadius="large">
     {children}
