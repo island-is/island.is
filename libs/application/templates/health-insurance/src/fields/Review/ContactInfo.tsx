715f9bbc4acd75ce2ec45883b0172ab1e9355697
dd-sdang <64913954+dd-sdang@users.noreply.github.com>
2020-12-14 19:05:22 +0100

    Sjukratryggingar/confirm section (#2223)
    * Added confirmation page
    
    * Added custom confirmation checkbox
    
    * Added sections to review component and split to smaller components
    
    * Refactor form field ids
    
    * Renamed review file
    
    * Fix linter errors
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/health-insurance/src/fields/Review/ContactInfo.tsx
@@ -0,0 +1,126 @@
+import React, { FC } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { FieldBaseProps, formatText } from '@island.is/application/core'
+import {
+  Box,
+  GridColumn,
+  GridRow,
+  Input,
+  Stack,
+} from '@island.is/island-ui/core'
+import { useLocale } from '@island.is/localization'
+import { FieldDescription } from '@island.is/shared/form-fields'
+import { m } from '../../forms/messages'
+
+const ContactInfo: FC<FieldBaseProps> = ({ application }) => {
+  const { register } = useFormContext()
+  const { formatMessage } = useLocale()
+
+  return (
+    <Box paddingY={2}>
+      <Stack space={5}>
+        <Box>
+          <Stack space={2}>
+            <GridRow>
+              <GridColumn span="6/12">
+                <Input
+                  id={'applicant.name'}
+                  name={'applicant.name'}
+                  label={formatText(m.name, application, formatMessage)}
+                  ref={register}
+                  disabled
+                />
+              </GridColumn>
+              <GridColumn span="6/12">
+                <Input
+                  id={'applicant.nationalId'}
+                  name={'applicant.nationalId'}
+                  label={formatText(m.nationalId, application, formatMessage)}
+                  ref={register}
+                  disabled
+                />
+              </GridColumn>
+            </GridRow>
+            <GridRow>
+              <GridColumn span="6/12">
+                <Input
+                  id={'applicant.address'}
+                  name={'applicant.address'}
+                  label={formatText(m.address, application, formatMessage)}
+                  ref={register}
+                  disabled
+                />
+              </GridColumn>
+              <GridColumn span="6/12">
+                <Input
+                  id={'applicant.postalCode'}
+                  name={'applicant.postalCode'}
+                  label={formatText(m.postalCode, application, formatMessage)}
+                  ref={register}
+                  disabled
+                />
+              </GridColumn>
+            </GridRow>
+            <GridRow>
+              <GridColumn span="6/12">
+                <Input
+                  id={'applicant.city'}
+                  name={'applicant.city'}
+                  label={formatText(m.city, application, formatMessage)}
+                  ref={register}
+                  disabled
+                />
+              </GridColumn>
+              <GridColumn span="6/12">
+                <Input
+                  id={'applicant.nationality'}
+                  name={'applicant.nationality'}
+                  label={formatText(m.nationality, application, formatMessage)}
+                  ref={register}
+                  disabled
+                />
+              </GridColumn>
+            </GridRow>
+          </Stack>
+          <FieldDescription
+            description={formatText(
+              m.editNationalRegistryData,
+              application,
+              formatMessage,
+            )}
+          />
+        </Box>
+
+        <Box>
+          <GridRow>
+            <GridColumn span="6/12">
+              <Input
+                id={'applicant.email'}
+                name={'applicant.email'}
+                label={formatText(m.email, application, formatMessage)}
+                ref={register}
+              />
+            </GridColumn>
+            <GridColumn span="6/12">
+              <Input
+                id={'applicant.phoneNumber'}
+                name={'applicant.phoneNumber'}
+                label={formatText(m.phoneNumber, application, formatMessage)}
+                ref={register}
+              />
+            </GridColumn>
+          </GridRow>
+          <FieldDescription
+            description={formatText(
+              m.editDigitalIslandData,
+              application,
+              formatMessage,
+            )}
+          />
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
+
+export default ContactInfo
