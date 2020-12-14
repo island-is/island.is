7e2876d4d7c532c08bb99a5d662acfe61e28cd96
Árni Freyr Snorrason <arni@aranja.com>
2020-12-14 13:08:01 +0000

    User profile data provider (#2162)
    * Start work on user profile data provider for the application system.
    
    * Propogate access token from graphql layer to REST
    
    * Change accessToken to authorization for current user decorator.
    
    Remove the now unnecessary email and mobile number question from parental leave form.
    
    * Review fixes
    
    * Remove console.log
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/auth-nest-tools/src/lib/jwt.strategy.ts
+++ libs/auth-nest-tools/src/lib/jwt.strategy.ts
@@ -28,7 +28,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return {
       nationalId: payload.nationalId ?? payload.natreg,
       scope: payload.scope,
-      accessToken: '',
+      authorization: '',
     }
   }
 }
