ce97941a7606ea7c702fd2247c7e141dc40c0eb8
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2020-12-15 14:21:54 +0000

    Making backgroundColor for Input responsive.

--- libs/island-ui/core/src/lib/Input/Input.treat.ts
+++ libs/island-ui/core/src/lib/Input/Input.treat.ts
@@ -1,6 +1,8 @@
 import { style, styleMap } from 'treat'
-import { theme, themeUtils } from '@island.is/island-ui/theme'
+import { Theme, theme, themeUtils } from '@island.is/island-ui/theme'
 import * as mixins from './Input.mixins'
+import { mapToStyleProperty } from '../../utils/mapToStyleProperty'
+import { mapValues } from 'lodash'
 
 export const containerDisabled = style({})
 export const noLabel = style({})
@@ -16,14 +18,23 @@ export const container = style({
 
 export const containerSizes = styleMap(mixins.containerSizes)
 
-export const containerBackgrounds = styleMap({
-  white: {
-    backgroundColor: theme.color.white,
-  },
-  blue: {
-    backgroundColor: theme.color.blue100,
-  },
-})
+const backgroundColorRules = {
+  white: theme.color.white,
+  blue: theme.color.blue100
+} as const
+
+const makeContainerBackground = (breakpoint: keyof Theme['breakpoints']) =>
+  styleMap(
+    mapValues(backgroundColorRules, (color) => 
+      themeUtils.responsiveStyle({ [breakpoint]: { backgroundColor: color } })
+    )
+  )
+
+export const containerBackgroundXs = makeContainerBackground('xs')
+export const containerBackgroundSm = makeContainerBackground('sm')
+export const containerBackgroundMd = makeContainerBackground('md')
+export const containerBackgroundLg = makeContainerBackground('lg')
+export const containerBackgroundXl = makeContainerBackground('xl')
 
 export const input = style({
   ...mixins.input,
@@ -39,23 +50,26 @@ export const input = style({
 
 export const inputSize = styleMap(mixins.inputSizes)
 
-// To handle styling auto-fill states
-export const inputBackground = styleMap({
-  white: {
-    selectors: {
-      '&:-webkit-autofill, &:-webkit-autofill:focus, &:-webkit-autofill:hover': {
-        boxShadow: `0 0 0px 1000px ${theme.color.white} inset`,
-      },
-    },
-  },
-  blue: {
-    selectors: {
-      '&:-webkit-autofill, &:-webkit-autofill:focus, &:-webkit-autofill:hover': {
-        boxShadow: `0 0 0px 1000px ${theme.color.blue100} inset`,
-      },
-    },
-  },
-})
+const makeInputBackground = (breakpoint: keyof Theme['breakpoints']) => 
+  styleMap(
+    mapValues(backgroundColorRules, (color) => 
+      themeUtils.responsiveStyle({ 
+        [breakpoint]: { 
+          selectors: {
+            '&:-webkit-autofill, &:-webkit-autofill:focus, &:-webkit-autofill:hover': {
+              boxShadow: `0 0 0px 1000px ${color} inset`,
+            }
+          }
+        }
+      })
+    )
+  )
+
+export const inputBackgroundXs = makeInputBackground('xs')
+export const inputBackgroundSm = makeInputBackground('sm')
+export const inputBackgroundMd = makeInputBackground('md')
+export const inputBackgroundLg = makeInputBackground('lg')
+export const inputBackgroundXl = makeInputBackground('xl')
 
 export const textarea = style({
   ...mixins.textarea,
