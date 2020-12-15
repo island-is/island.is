08f13bd2acc10725635327b258549ccce9114afa
Petar Shomov <petar@andes.is>
2020-12-15 15:51:23 +0000

    Fix deploy revert broken env commit (#2245) (#2247)
    * Revert "use mock data (#2237)"
    
    This reverts commit 72a593c684966bb8c4e8e69ec67bc401658670b6.
    
    * Revert "Feature/connect to dpm service (#2226)"
    
    This reverts commit c29b11814bb741ee5e9d7a5df71c345022230867.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    Co-authored-by: Sindri Guðmundsson <sindrigudmundsson@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/document-provider/src/lib/client/models/clientCredentials.ts
+++
@@ -1,5 +0,0 @@
-export interface ClientCredentials {
-  providerId: string
-  clientId: string
-  clientSecret: string
-}
--- libs/api/domains/document-provider/src/lib/models/audienceAndScope.model.ts
+++ libs/api/domains/document-provider/src/lib/models/audienceAndScope.model.ts
@@ -2,14 +2,9 @@ import { Field, ObjectType } from '@nestjs/graphql'
 
 @ObjectType()
 export class AudienceAndScope {
-  constructor(audience: string, scope: string) {
-    this.audience = audience
-    this.scope = scope
-  }
+  @Field(() => String)
+  audience!: string
 
   @Field(() => String)
-  audience: string
-
-  @Field(() => String)
-  scope: string
+  scope!: string
 }
