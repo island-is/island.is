89f4209035180b75d78330fe7b938cb2736a25de
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-12-15 14:50:46 +0000

    Fix deploy revert broken env commit (#2245)
    * Revert "use mock data (#2237)"
    
    This reverts commit 72a593c684966bb8c4e8e69ec67bc401658670b6.
    
    * Revert "Feature/connect to dpm service (#2226)"
    
    This reverts commit c29b11814bb741ee5e9d7a5df71c345022230867.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/document-provider/src/lib/document-provider.module.ts
+++ libs/api/domains/document-provider/src/lib/document-provider.module.ts
@@ -1,33 +1,9 @@
-import { DynamicModule, HttpModule } from '@nestjs/common'
-
+import { Module } from '@nestjs/common'
 import { DocumentProviderResolver } from './document-provider.resolver'
 import { DocumentProviderService } from './document-provider.service'
-import {
-  DocumentProviderClient,
-  DocumentProviderClientConfig,
-  DOCUMENT_PROVIDER_CLIENT_CONFIG,
-} from './client/documentProviderClient'
-import { DocumentProviderRepository } from './document-provider.repository'
 
-export class DocumentProviderModule {
-  static register(config: DocumentProviderClientConfig): DynamicModule {
-    return {
-      module: DocumentProviderModule,
-      imports: [
-        HttpModule.register({
-          timeout: 10000,
-        }),
-      ],
-      providers: [
-        DocumentProviderResolver,
-        DocumentProviderService,
-        DocumentProviderRepository,
-        DocumentProviderClient,
-        {
-          provide: DOCUMENT_PROVIDER_CLIENT_CONFIG,
-          useValue: config,
-        },
-      ],
-    }
-  }
-}
+@Module({
+  providers: [DocumentProviderResolver, DocumentProviderService],
+  exports: [],
+})
+export class DocumentProviderModule {}
