40df2506d6152e286e01768df7ef608896100f49
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-12-15 14:18:03 +0000

    Revert "use mock data (#2237)"
    This reverts commit 72a593c684966bb8c4e8e69ec67bc401658670b6.

--- libs/api/domains/document-provider/src/lib/document-provider.service.ts
+++ libs/api/domains/document-provider/src/lib/document-provider.service.ts
@@ -23,60 +23,46 @@ export class DocumentProviderService {
     nationalId: string,
     clientName: string,
   ): Promise<ClientCredentials> {
-    logger.info(`Create client: ${nationalId} - ${clientName}`)
-
-    return new ClientCredentials(
-      '5016d8d5cb6ce0758107b9969ea3c301',
-      '7a557951364a960a608735371db61ed8ed320d6bfc59f52fe37fc08e23dbd8d1',
+    const currentProvider = await this.documentProviderRepository.getProvider(
+      nationalId,
     )
 
-    // const currentProvider = await this.documentProviderRepository.getProvider(
-    //   nationalId,
-    // )
+    if (currentProvider !== null) {
+      throw new ApolloError('Provider already exists for this organisation.')
+    }
 
-    // if (currentProvider !== null) {
-    //   throw new ApolloError('Provider already exists for this organisation.')
-    // }
+    const result = await this.documentProviderClient
+      .createClient(nationalId, clientName)
+      .catch(handleError)
 
-    // const result = await this.documentProviderClient
-    //   .createClient(nationalId, clientName)
-    //   .catch(handleError)
+    const { providerId } = result
+    await this.documentProviderRepository.saveProvider(nationalId, providerId)
 
-    // const { providerId } = result
-    // await this.documentProviderRepository.saveProvider(nationalId, providerId)
-
-    // const credentials = new ClientCredentials(
-    //   result.clientId,
-    //   result.clientSecret,
-    // )
-    // return credentials
+    const credentials = new ClientCredentials(
+      result.clientId,
+      result.clientSecret,
+    )
+    return credentials
   }
 
   async registerEndpoint(
     nationalId: string,
     endpoint: string,
   ): Promise<AudienceAndScope> {
-    logger.info(`Register endpoint: ${nationalId} - ${endpoint}`)
-
-    return new AudienceAndScope(
-      'https://test-skjalaveita-island-is.azurewebsites.net',
-      'https://test-skjalaveita-island-is.azurewebsites.net/api/v1/customer/.default',
+    const providerId = await this.documentProviderRepository.getProvider(
+      nationalId,
     )
 
-    // const providerId = await this.documentProviderRepository.getProvider(
-    //   nationalId,
-    // )
+    if (providerId === null) {
+      throw new ApolloError('No provider exists for this organisation.')
+    }
 
-    // if (providerId === null) {
-    //   throw new ApolloError('No provider exists for this organisation.')
-    // }
+    const result = await this.documentProviderClient
+      .updateEndpoint(providerId, endpoint)
+      .catch(handleError)
 
-    // const result = await this.documentProviderClient
-    //   .updateEndpoint(providerId, endpoint)
-    //   .catch(handleError)
-
-    // const audienceAndScope = new AudienceAndScope(result.audience, result.scope)
-    // return audienceAndScope
+    const audienceAndScope = new AudienceAndScope(result.audience, result.scope)
+    return audienceAndScope
   }
 
   async runEndpointTests(
