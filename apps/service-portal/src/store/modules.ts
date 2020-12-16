b22088b3dd77efd55003d3aef33137706882ba16
Darri Steinn Konradsson <darrikonn@gmail.com>
2020-12-16 15:39:19 +0000

    Add driving-license to service portal

--- apps/service-portal/src/store/modules.ts
+++ apps/service-portal/src/store/modules.ts
@@ -8,6 +8,7 @@ import { healthModule } from '@island.is/service-portal/health'
 import { educationModule } from '@island.is/service-portal/education'
 import { assetsModule } from '@island.is/service-portal/assets'
 import { eligibilityModule } from '@island.is/service-portal/eligibility'
+import { drivingLicenseModule } from '@island.is/service-portal/driving-license'
 
 export const modules: ServicePortalModule[] = [
   applicationsModule,
@@ -19,4 +20,5 @@ export const modules: ServicePortalModule[] = [
   educationModule,
   eligibilityModule,
   assetsModule,
+  drivingLicenseModule,
 ]
