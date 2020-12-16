53d6ac0bec177c91046d6cf03925636a78e0cf23
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-12-16 14:17:10 +0000

    better typescript comment on baseevent

--- libs/plausible/src/lib/BaseEvent.d.ts
+++ libs/plausible/src/lib/BaseEvent.d.ts
@@ -1,12 +1,11 @@
-/*  This is the type declaration for all events sent to plausible.
-    When creating a new event in plausible the schema for that event should be defined here
-    with an eventName: The name of the event
-    featureName: the name of the feature the event is being tracked, if the event is in the root of the product
-    or a generic event in the product the product name should be defined as the featureName
-    params: additional meta data that is optionally sent with the event
-    The name of the event in plausible should then be the featureName appended with the eventName
-    This is to make sure events dont clash between products and features for better analization of
-    events.
+/**
+ * This is the type declaration for all events sent to plausible.
+ * When creating a new event in plausible the schema for that event should be defined here
+ * @param eventName The name of the event
+ * @param featureName the name of the feature the event is being tracked. If the event is in the root of the product or a generic event in the product the product name should be defined as the featureName
+ * @param params additional meta data that is optionally sent with the event
+ * The name of the event that is sent to plausible is the featureName appended with the eventName
+ * This is to make sure events dont clash between products and features for better granularity of events.
  */
 export default interface BaseEvent {
   eventName: string
