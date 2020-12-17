7e6cc6c6a1483d93356feed986ae34d7a2d177e3
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2020-12-17 17:10:26 +0000

    Remove outdated util function

--- libs/application/api-template-utils/src/lib/utils/assign.ts
+++ libs/application/api-template-utils/src/lib/utils/assign.ts
@@ -1,6 +1,4 @@
 import jwt from 'jsonwebtoken'
-import { SendMailOptions } from 'nodemailer'
-import { dedent } from 'ts-dedent'
 
 import { Application } from '@island.is/application/core'
 
@@ -14,42 +12,4 @@ export const createAssignToken = (application: Application, secret: string) => {
   )
 
   return token
-}
-
-export const createAssignTemplate = (
-  application: Application,
-  email: string,
-  token: string,
-): SendMailOptions => {
-  const template = {
-    from: {
-      name: 'Fæðingarorlofssjóður',
-      address: 'baering@aranja.com',
-    },
-    replyTo: {
-      name: 'Fæðingarorlofssjóður',
-      address: 'baering@aranja.com',
-    },
-    to: [
-      {
-        name: '',
-        address: email,
-      },
-    ],
-    subject: ``,
-    // TODO: place in translation file
-    // TODO: get client origin from environment to replace http://localhost:4200
-    text: dedent(`
-      Góðan dag.
-      
-      Umsækjandi með kennitölu ${application.applicant} hefur skráð þig sem atvinnuveitanda í umsókn sinni.
-      
-      Ef þú áttir von á þessum tölvupósti þá getur þú haldið áfram hingað til þess að fara yfir umsóknina: http://localhost:4200/tengjast-umsokn?token=${token}
-
-      Með kveðju.
-      Starfsfólk fæðingarorlofssjóðs
-    `),
-  }
-
-  return template
 }
