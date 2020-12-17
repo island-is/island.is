d8c161098079849657ca28c1ef688319d0d5f3af
Unnar Bjarnarson <unnar@fuglar.com>
2020-12-17 13:23:14 +0000

    Comment fix

--- apps/services/auth-admin-api/src/app/modules/clients/client-secret.controller.ts
+++ apps/services/auth-admin-api/src/app/modules/clients/client-secret.controller.ts
@@ -36,7 +36,7 @@ export class ClientSecretController {
   @ApiCreatedResponse()
   async delete(@Body() clientSecret: ClientSecretDTO): Promise<number> {
     if (!clientSecret) {
-      throw new BadRequestException('Client Secret object is required')
+      throw new BadRequestException('The Client Secret object is required')
     }
 
     return await this.clientsService.removeClientSecret(clientSecret)
