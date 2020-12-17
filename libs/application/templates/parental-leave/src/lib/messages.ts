7cbdc52b87332cd9a0c19dc8556c43f9e9bcd6cc
Noam <noam@aranja.com>
2020-12-17 12:31:59 +0000

    Add expand collapse button

--- libs/application/templates/parental-leave/src/lib/messages.ts
+++ libs/application/templates/parental-leave/src/lib/messages.ts
@@ -791,6 +791,16 @@ export const mm: MessageDir = {
     },
   }),
   confirmation: defineMessages({
+    epxandAll: {
+      id: 'pl.application:confirmation.buttons.expandAll',
+      defaultMessage: 'Auka allt',
+      description: 'Expand all',
+    },
+    collapseAll: {
+      id: 'pl.application:confirmation.buttons.collapseAll',
+      defaultMessage: 'Hrynja allt',
+      description: 'Collapse all',
+    },
     section: {
       id: 'pl.application:confirmation.section',
       defaultMessage: 'Samþykkja',
