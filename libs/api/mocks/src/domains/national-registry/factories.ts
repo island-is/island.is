d56123889d44c05982e22cf5497e4888976dce8f
Olafur <obmagnusson@gmail.com>
2020-12-18 10:55:17 +0000

    family members

--- libs/api/mocks/src/domains/national-registry/factories.ts
+++ libs/api/mocks/src/domains/national-registry/factories.ts
@@ -12,6 +12,21 @@ export const banMarking = factory<BanMarking>({
   banMarked: () => faker.random.boolean(),
 })
 
+export const getMinorGender = () => {
+  const genderArr: Gender[] = ['FEMALE_MINOR', 'MALE_MINOR', 'TRANSGENDER_MINOR']
+  return faker.random.arrayElement(genderArr)
+}
+
+export const getGender = () => {
+  const genderArr: Gender[] = ['FEMALE', 'MALE', 'TRANSGENDER']
+  return faker.random.arrayElement(genderArr)
+}
+
+export const getMaritalStatus = () => {
+  const genderArr: MaritalStatus[] = ['DIVORCED', 'SEPARATED', 'MARRIED', 'UNMARRIED']
+  return faker.random.arrayElement(genderArr)
+}
+
 export const address = `${faker.address.streetName().toString()}
   ${faker.random.number(200)},
   ${faker.address.zipCode()}
@@ -20,20 +35,31 @@ export const address = `${faker.address.streetName().toString()}
 export const registryUser = factory<NationalRegistryUser>({
   nationalId: '1231231234',
   fullName: 'Tester Testerson',
-  gender: 'MALE',
+  gender: getGender(),
   legalResidence: () => address,
   birthPlace: () => faker.address.city().toString(),
   citizenship: () => faker.address.countryCode().toString(),
   religion: () => faker.lorem.sentence(3).toString(),
-  maritalStatus: 'MARRIED',
+  maritalStatus: getMaritalStatus(),
   banMarking: banMarking(),
 })
 
-export const familyMember = factory<NationalRegistryFamilyMember>({
+export const spouse = factory<NationalRegistryFamilyMember>({
   nationalId: () => faker.random.alphaNumeric(10),
   fullName: () => faker.name.findName(),
-  gender: 'FEMALE',
+  gender: getGender(),
   maritalStatus: 'MARRIED',
   address: address,
   familyRelation: 'spouse',
 })
+
+export const child = factory<NationalRegistryFamilyMember>({
+  nationalId: () => faker.random.alphaNumeric(10),
+  fullName: () => faker.name.findName(),
+  gender: getMinorGender(),
+  maritalStatus: 'UNMARRIED',
+  address: address,
+  familyRelation: 'child',
+})
+
+
