b80de494b872db7864f6b2d0381c448a179663e7
Valur Einarsson <valure@fuglar.com>
2020-12-23 14:10:06 +0000

    Added provider with session in _app.

--- apps/auth-admin-web/pages/_app.tsx
+++ apps/auth-admin-web/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/App.scss';
+import React from 'react';
+import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
