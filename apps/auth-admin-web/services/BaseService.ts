684e105d4a5f1aab1eff1929f58ceacabf6ec68f
Valur Einarsson <valure@fuglar.com>
2020-12-28 16:21:35 +0000

    Now sends access token with api requests.

--- apps/auth-admin-web/services/BaseService.ts
+++ apps/auth-admin-web/services/BaseService.ts
@@ -1,12 +1,15 @@
 import APIResponse from '../entities/common/APIResponse';
 import api from './api';
 import { ApiStatusStore } from '../store/ApiStatusStore';
+import { getSession } from 'next-auth/client';
 
 export class BaseService {
   protected static async GET(path: string) {
     ApiStatusStore.getInstance().clearStatus();
+    const session = await getSession();
+
     try {
-      const response = await api.get(path);
+      const response = await api.get(path, BaseService.getConfig(session));
       return BaseService.handleResponse(response);
     } catch (error) {
       return BaseService.handleError(error);
@@ -14,10 +17,12 @@ export class BaseService {
   }
 
   protected static async DELETE(path: string, body: any = null) {
+    ApiStatusStore.getInstance().clearStatus();
+    const session = await getSession();
+
     if (!body) {
-      ApiStatusStore.getInstance().clearStatus();
       try {
-        const response = await api.delete(path);
+        const response = await api.delete(path, BaseService.getConfig(session));
         return BaseService.handleResponse(response);
       } catch (error) {
         return BaseService.handleError(error);
@@ -25,7 +30,10 @@ export class BaseService {
     }
 
     try {
-      const response = await api.delete(path, { data: body });
+      const response = await api.delete(
+        path,
+        BaseService.getConfigWithData(session, body)
+      );
       return BaseService.handleResponse(response);
     } catch (error) {
       return BaseService.handleError(error);
@@ -34,9 +42,15 @@ export class BaseService {
 
   protected static async POST(path: string, body: any = null) {
     ApiStatusStore.getInstance().clearStatus();
+    const session = await getSession();
+
     if (!body) {
       try {
-        const response = await api.post(path);
+        const response = await api.post(
+          path,
+          null,
+          BaseService.getConfig(session)
+        );
         return BaseService.handleResponse(response);
       } catch (error) {
         return BaseService.handleError(error);
@@ -44,7 +58,11 @@ export class BaseService {
     }
 
     try {
-      const response = await api.post(path, body);
+      const response = await api.post(
+        path,
+        body,
+        BaseService.getConfig(session)
+      );
       return BaseService.handleResponse(response);
     } catch (error) {
       return BaseService.handleError(error);
@@ -53,8 +71,14 @@ export class BaseService {
 
   protected static async PUT(path: string, body: any) {
     ApiStatusStore.getInstance().clearStatus();
+    const session = await getSession();
+
     try {
-      const response = await api.put(path, body);
+      const response = await api.put(
+        path,
+        body,
+        BaseService.getConfig(session)
+      );
       return BaseService.handleResponse(response);
     } catch (error) {
       return BaseService.handleError(error);
@@ -63,8 +87,14 @@ export class BaseService {
 
   protected static async PATCH(path: string, body: any) {
     ApiStatusStore.getInstance().clearStatus();
+    const session = await getSession();
+
     try {
-      const response = await api.patch(path, body);
+      const response = await api.patch(
+        path,
+        body,
+        BaseService.getConfig(session)
+      );
       return BaseService.handleResponse(response);
     } catch (error) {
       return BaseService.handleError(error);
@@ -102,5 +132,18 @@ export class BaseService {
       });
     }
     return null;
+  }
+
+  private static getConfig(session: any) {
+    return {
+      headers: { Authorization: `Bearer ${session.accessToken}` },
+    };
+  }
+
+  private static getConfigWithData(session: any, body: any) {
+    return {
+      headers: { Authorization: `Bearer ${session.accessToken}` },
+      data: body,
+    };
   }
 }
