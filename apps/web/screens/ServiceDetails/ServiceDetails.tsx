5e67f434c6f8066067be565a11a1d5451357b9fe
Sigga Hrafns <sigridur@kosmosogkaos.is>
2020-12-28 12:34:15 +0000

    Use new breadcrumbs in service detail

--- apps/web/screens/ServiceDetails/ServiceDetails.tsx
+++ apps/web/screens/ServiceDetails/ServiceDetails.tsx
@@ -149,16 +149,17 @@ const ServiceDetails: Screen<ServiceDetailsProps> = ({
                 </Box>
                 <Box marginBottom={2} display={['none', 'none', 'inline']}>
                   {/* Show when NOT a device */}
-                  <Breadcrumbs>
-                    <Link href={n('linkIslandIs')}>
-                      {n('linkIslandIsText')}
-                    </Link>
-                    <a href={n('linkThroun')}>{n('linkThrounText')}</a>
-                    <a href={n('linkServiceList')}>
-                      {n('linkServiceListText')}
-                    </a>
-                    <span>{n('linkDetailsLastText')}</span>
-                  </Breadcrumbs>
+                  <Breadcrumbs
+                    items={[
+                      { title: n('linkIslandIsText'), href: n('linkIslandIs') },
+                      { title: n('linkThrounText'), href: n('linkThroun') },
+                      {
+                        title: n('linkServiceListText'),
+                        href: n('linkServiceList'),
+                      },
+                      { title: n('linkDetailsLastText') },
+                    ]}
+                  />
                 </Box>
                 {!service ? (
                   <Box>
