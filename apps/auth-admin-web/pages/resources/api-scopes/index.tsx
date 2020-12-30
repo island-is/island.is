546be2ec2db6834c0dffa5ef56cb2294be27af13
unnarb <unnarb@gmail.com>
2020-12-30 16:52:05 +0000

    Resource Lists to pages

---
+++ apps/auth-admin-web/pages/resources/api-scopes/index.tsx
@@ -0,0 +1,13 @@
+import ContentWrapper from '../../../components/Layout/ContentWrapper';
+import ApiScopeList from '../../../components/Resource/lists/ApiScopeList';
+import ResourcesTabsNav from '../../../components/Resource/nav/ResourcesTabsNav';
+import React from 'react';
+
+export default function Index() {
+  return (
+    <ContentWrapper>
+        <ResourcesTabsNav />         
+        <ApiScopeList />
+    </ContentWrapper>
+  );
+}
---
+++ apps/auth-admin-web/pages/resources/identity-resources/index.tsx
@@ -0,0 +1,13 @@
+import ContentWrapper from '../../../components/Layout/ContentWrapper';
+import ResourcesTabsNav from '../../../components/Resource/nav/ResourcesTabsNav';
+import React from 'react';
+import IdentityResourcesList from '../../../components/Resource/lists/IdentityResourcesList';
+
+export default function Index() {
+  return (
+    <ContentWrapper>
+        <ResourcesTabsNav />         
+        <IdentityResourcesList />
+    </ContentWrapper>
+  );
+}
