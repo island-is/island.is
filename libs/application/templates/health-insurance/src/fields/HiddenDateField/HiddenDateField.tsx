eef51fbf57f06d3c16596e221763a3db3098bc2a
dd-sdang <64913954+dd-sdang@users.noreply.github.com>
2021-01-05 16:01:55 +0100

    Sjukratryggingar/mock prefilled fields and confirm page (#2410)
    * Added mock values to prefilled fields
    
    * Confirmation page layout and image
    
    * Fix HiddenDateField showing in mobile
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/health-insurance/src/fields/HiddenDateField/HiddenDateField.tsx
+++ libs/application/templates/health-insurance/src/fields/HiddenDateField/HiddenDateField.tsx
@@ -1,15 +1,16 @@
 import React, { FC } from 'react'
 import { FieldBaseProps, getValueViaPath } from '@island.is/application/core'
-import { Input, Hidden } from '@island.is/island-ui/core'
+import { Input, Box } from '@island.is/island-ui/core'
 import { useFormContext } from 'react-hook-form'
 import { MissingInfoType } from '../../types'
 
 const HiddenDateField: FC<FieldBaseProps> = ({ application }) => {
   const { register } = useFormContext()
-  const missingInfo = getValueViaPath(
-    application.answers,
-    'missingInfo',
-  ) as MissingInfoType[]
+  const missingInfo =
+    (getValueViaPath(
+      application.answers,
+      'missingInfo',
+    ) as MissingInfoType[]) || []
 
   let index = 0
 
@@ -18,14 +19,14 @@ const HiddenDateField: FC<FieldBaseProps> = ({ application }) => {
   }
 
   return (
-    <Hidden above="xs">
+    <Box hidden>
       <Input
         id={`missingInfo[${index}].date`}
         name={`missingInfo[${index}].date`}
         defaultValue={`${new Date()}`}
         ref={register}
       />
-    </Hidden>
+    </Box>
   )
 }
 
