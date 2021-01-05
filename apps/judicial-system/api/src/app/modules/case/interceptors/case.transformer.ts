463ccb56cb80c86a43d91b95023503a01e1f26bc
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-01-05 15:03:10 +0000

    Maps alternative travel ban null values to false

--- apps/judicial-system/api/src/app/modules/case/interceptors/case.transformer.ts
+++ apps/judicial-system/api/src/app/modules/case/interceptors/case.transformer.ts
@@ -3,6 +3,8 @@ import { Case } from '../models'
 const fiveMinutes = 5 * 60 * 1000
 
 export function transformCase(theCase: Case) {
+  theCase.alternativeTravelBan = theCase.alternativeTravelBan ?? false
+
   if (theCase.courtDate) {
     theCase.isCourtDateInThePast =
       Date.now() - fiveMinutes > new Date(theCase.courtDate).getTime()
