69d82e9eb6030e2f3fe775c46950556f4f6d9191
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-01-07 13:43:44 +0000

    Conditionally render applications based on license and age

--- libs/service-portal/driving-license/src/screens/DrivingLicense/DrivingLicense.tsx
+++ libs/service-portal/driving-license/src/screens/DrivingLicense/DrivingLicense.tsx
@@ -7,11 +7,7 @@ import { useLocale, useNamespaces } from '@island.is/localization'
 import { Box, GridColumn, GridRow, Stack } from '@island.is/island-ui/core'
 import { Application, Eligibility } from './components'
 
-type Data = {
-  heading: MessageDescriptor | string
-  subtext: MessageDescriptor | string
-  link: string
-}
+const ELIGABLE_FOR_RENEWAL_AGE = 68
 
 const NationalRegistryUserQuery = gql`
   query NationalRegistryUserQuery {
@@ -32,100 +28,35 @@ function DrivingLicense(): JSX.Element {
   return (
     <Box marginBottom={[6, 6, 10]}>
       <Stack space={[6, 6, 10]}>
-        <Box>
-          <GridRow>
-            <GridColumn span={['12/12', '12/12', '6/8', '6/8']}>
-              <Stack space={2}>
-                <Inline space={1}>
-                  <Text variant="h1" as="h1">
-                    {formatMessage({
-                      id: 'sp.driving-license:title',
-                      defaultMessage: 'Ökuréttindi',
-                    })}
-                  </Text>
-                  <Tag variant="blue" outlined>
-                    {formatMessage({
-                      id: 'service.portal:in-progress',
-                      defaultMessage: 'Í vinnslu',
-                    })}
-                  </Tag>
-                </Inline>
-                <Text as="p">
-                  {formatMessage({
-                    id: 'sp.driving-license:overview-subtext',
-                    defaultMessage:
-                      'Hér gefur að líta núverandi ökuréttindi þín og gildistíma þeirra.',
-                  })}
-                </Text>
-              </Stack>
-            </GridColumn>
-          </GridRow>
-        </Box>
-        {data.map((item, index) => (
-          <GridRow>
-            <GridColumn span="12/12" order={[2, 2, 1]}>
-              <Box
-                display="flex"
-                flexDirection="column"
-                height="full"
-                justifyContent="center"
-                marginTop={[3, 3, 0]}
-              >
-                <Box marginBottom={2}>
-                  <Text variant="h2" as="h2">
-                    {formatMessage(item.heading)}
-                  </Text>
-                </Box>
-                <Text marginBottom={[3, 4]}>{formatMessage(item.subtext)}</Text>
-                <Box>
-                  <Link to={item.link}>
-                    <ArrowLink>
-                      {formatMessage({
-                        id: 'service.portal:continue-button',
-                        defaultMessage: 'Halda áfram',
-                      })}
-                    </ArrowLink>
-                  </Link>
-                </Box>
-              </Box>
-            </GridColumn>
-          </GridRow>
-        ))}
+        <Eligibility />
+
+        <Application
+          heading={defineMessage({
+            id: 'service.portal:driving-license-general-license-title',
+            defaultMessage: 'Almenn ökuréttindi',
+          })}
+          subText={defineMessage({
+            id: 'sp.driving-license:general-license-subtext',
+            defaultMessage:
+              'Ökunám á fólksbifreið getur hafist við 16 ára aldur en ökuréttindi eru fyrst veitt við 17 ára aldur. Réttindi til að aka bifhjóli (skellinöðru) er hægt að fá 15 ára og dráttarvél 16 ára. Til að hefja ökunám þarf að hafa samband við löggiltan ökukennara. Hann hefur umsjón með bæði verklegum og bóklegum hluta námsins og vísar á ökuskóla þar sem bóklegt nám fer fram.',
+          })}
+        />
+
+        {(nationalRegistryUser?.age || 0) >= ELIGABLE_FOR_RENEWAL_AGE && (
+          <Application
+            heading={defineMessage({
+              id: 'service.portal:driving-license-renewal-heading',
+              defaultMessage: 'Endurnýjun ökuskírteinis vegna aldurs',
+            })}
+            subText={defineMessage({
+              id: 'service.portal:driving-license-renewal-subtext',
+              defaultMessage:
+                'Almenn ökuréttindi (B réttindi) þarf að endurnýja við 70 ára aldur. Nýja ökuskírteinið gildir í 4 ár. Eftir það þarf að endurnýja það þriðja og annað hvert ár en eftir 80 ára aldur á árs fresti.',
+            })}
+          />
+        )}
       </Stack>
     </Box>
-  )
-  return (
-    <>
-      <Box marginBottom={4}>
-        <Inline space={1}>
-          <Text variant="h1" as="h1">
-            {formatMessage({
-              id: 'sp.driving-license:title',
-              defaultMessage: 'Ökuréttindi',
-            })}
-          </Text>
-          <Tag variant="blue" outlined>
-            {formatMessage({
-              id: 'service.portal:in-progress',
-              defaultMessage: 'Í vinnslu',
-            })}
-          </Tag>
-        </Inline>
-      </Box>
-      <Box marginBottom={5}>
-        <GridRow>
-          <GridColumn span={['1/1', '6/8']}>
-            <Text>
-              {formatMessage({
-                id: 'sp.driving-license:intro',
-                defaultMessage:
-                  'Hér gefur að líta núverandi ökuréttindi þín og gildistíma þeirra.',
-              })}
-            </Text>
-          </GridColumn>
-        </GridRow>
-      </Box>
-    </>
   )
 }
 
