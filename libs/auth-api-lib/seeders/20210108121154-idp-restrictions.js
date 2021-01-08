beefbf0bae960b29d917edbc31a09ddcacabc652
unnarb <unnarb@gmail.com>
2021-01-08 15:50:21 +0000

    Ipd Restriction in db

---
+++ libs/auth-api-lib/seeders/20210108121154-idp-restrictions.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('idp_restrictions', [
+      {
+        name: 'sim',
+        description: 'Sim Card',
+        helptext: 'Allows users to login with sim cards',
+        level: 4,
+        created: now(),
+      },
+      {
+        name: 'card',
+        description: 'Identity Card',
+        helptext: 'Allows users to login with identity cards',
+        level: 4,
+        created: now(),
+      },
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('idp_restrictions', {
+      name: 'sim'
+    },
+    {
+      name: 'card'
+    })
+  }
+};
