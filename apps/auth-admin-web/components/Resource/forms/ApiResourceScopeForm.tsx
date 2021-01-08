0f315a6c392751160758917830e8d6ed2c621b9c
unnarb <unnarb@gmail.com>
2021-01-08 10:27:27 +0000

    Help texts

--- apps/auth-admin-web/components/Resource/forms/ApiResourceScopeForm.tsx
+++ apps/auth-admin-web/components/Resource/forms/ApiResourceScopeForm.tsx
@@ -95,8 +95,7 @@ const ApiResourceScopeForm: React.FC<Props> = (props: Props) => {
           <h1>Allowed scopes</h1>
           <div className="api-resource-scope-form__container__form">
             <div className="api-resource-scope-form__help">
-              Decide which information you would like applications to be able to
-              access on a user's behalf.
+            An API must have at least one scope. Each scope can have different settings.
             </div>
             <form onSubmit={handleSubmit(add)}>
               <div className="api-resource-scope-form__container__fields">
