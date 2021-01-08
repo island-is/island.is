ee73ac1969575d0e27617311e7f98d5c876f89f9
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2021-01-08 14:53:53 +0000

    Adding start-server-and-test pkg. Update 40_e2e.sh script.

--- scripts/ci/40_e2e.sh
+++ scripts/ci/40_e2e.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-yarn run e2e $1 --headless --production --record --group=$1
+HOST="http://localhost:4200"
+
+ yarn start-server-and-test "yarn start $(echo ${1%-*})" $HOST "yarn run e2e $1 --headless --production --record --group=$1 --base-url $HOST --devServerTarget '' "
