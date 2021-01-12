93330044e2943936ec149ed9228f2d81738da1a0
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-01-12 14:43:02 +0000

    Allow controlling space for multi fields

--- libs/application/ui-shell/src/components/Screen.tsx
+++ libs/application/ui-shell/src/components/Screen.tsx
@@ -195,7 +195,7 @@ const Screen: FC<ScreenProps> = ({
           span={['12/12', '12/12', '7/9', '7/9']}
           offset={['0', '0', '1/9']}
         >
-          <Text variant="h2" marginBottom={2}>
+          <Text variant="h2" marginBottom={1}>
             {formatText(screen.title, application, formatMessage)}
           </Text>
           <Box>
