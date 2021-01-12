3bc02a766edf1a8fd1257be952fe313947673cdf
Sigga Hrafns <sigridur@kosmosogkaos.is>
2021-01-12 11:40:24 +0000

    minor tweaks

--- apps/web/screens/ServiceDetails/ServiceDetails.tsx
+++ apps/web/screens/ServiceDetails/ServiceDetails.tsx
@@ -97,6 +97,7 @@ const ServiceDetails: Screen<ServiceDetailsProps> = ({
               title={n('linkThrounText')}
               titleLink={{
                 href: n('linkThroun'),
+                active: false
               }}
             />
           }
