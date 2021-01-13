6f4c6211a7246bf24a7f2a174e89ecb0301495d7
Brian Johannessen <barabrian89@gmail.com>
2021-01-13 14:17:28 +0000

    rich text render refactor

---
+++ libs/island-ui/contentful/src/lib/RichTextRC/RichText.treat.ts
@@ -0,0 +1,83 @@
+import { theme } from 'libs/island-ui/theme/src'
+import { style } from 'treat'
+
+export const orderedList = style({
+  counterReset: 'section',
+  marginTop: 14,
+  marginBottom: 14,
+  selectors: {
+    'ol &': {
+      marginTop: 0,
+      marginBottom: 0,
+    },
+  },
+})
+
+export const unorderedList = style({
+  listStyle: 'none',
+  marginTop: 14,
+  marginBottom: 14,
+  selectors: {
+    'ul &': {
+      marginTop: 0,
+      marginBottom: 0,
+    },
+  },
+})
+
+export const listItem = style({
+  selectors: {
+    'ol &': {
+      listStyle: 'none',
+      position: 'relative',
+    },
+    'ol ol &': {
+      paddingLeft: theme.spacing[3],
+    },
+    'ol &:before': {
+      float: 'left',
+      margin: `2px 12px 0 0`,
+      counterIncrement: 'section',
+      content: 'counters(section, ".") " "',
+      color: theme.color.red400,
+      fontWeight: theme.typography.semiBold,
+    },
+    'ul &': {
+      position: 'relative',
+      paddingLeft: theme.spacing[3],
+    },
+    'ul &:before': {
+      content: '""',
+      position: 'absolute',
+      top: '10px',
+      left: 0,
+      borderRadius: '50%',
+      border: `4px solid red`,
+    },
+    '&:not(:first-child), ul ul &:first-child, ol ol &:first-child': {
+      marginTop: 14,
+    },
+    '&:not(:last-child), ul ul &:last-child, ol ol &:last-child': {
+      marginBottom: 14,
+    },
+  },
+})
+
+export const paragraph = style({
+  selectors: {
+    'p + &': {
+      marginTop: 14,
+    },
+    [`${listItem} &`]: {
+      marginBottom: 0,
+    },
+  },
+})
+
+export const heading = style({
+  selectors: {
+    '&:first-child': {
+      marginTop: 0,
+    },
+  },
+})
