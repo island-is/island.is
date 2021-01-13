0ac9676a2e660d8a262f55672891869ee390dd3e
Brian Johannessen <barabrian89@gmail.com>
2021-01-13 15:42:58 +0000

    mobile margins

--- libs/island-ui/contentful/src/lib/RichTextRC/RichText.treat.ts
+++ libs/island-ui/contentful/src/lib/RichTextRC/RichText.treat.ts
@@ -1,10 +1,16 @@
 import { style } from 'treat'
-import { theme } from '@island.is/island-ui/theme'
+import { theme, themeUtils } from '@island.is/island-ui/theme'
 
 export const orderedList = style({
   counterReset: 'section',
-  marginTop: 14,
-  marginBottom: 14,
+  marginTop: 12,
+  marginBottom: 12,
+  ...themeUtils.responsiveStyle({
+    md: {
+      marginTop: 14,
+      marginBottom: 14,
+    },
+  }),
   selectors: {
     'ol &': {
       marginTop: 0,
@@ -15,8 +21,14 @@ export const orderedList = style({
 
 export const unorderedList = style({
   listStyle: 'none',
-  marginTop: 14,
-  marginBottom: 14,
+  marginTop: 12,
+  marginBottom: 12,
+  ...themeUtils.responsiveStyle({
+    md: {
+      marginTop: 14,
+      marginBottom: 14,
+    },
+  }),
   selectors: {
     'ul &': {
       marginTop: 0,
@@ -26,48 +38,67 @@ export const unorderedList = style({
 })
 
 export const listItem = style({
-  selectors: {
-    'ol &': {
-      listStyle: 'none',
-      position: 'relative',
+  ...themeUtils.responsiveStyle({
+    xs: {
+      selectors: {
+        'ol &': {
+          listStyle: 'none',
+          position: 'relative',
+        },
+        'ol ol &': {
+          paddingLeft: theme.spacing[3],
+        },
+        'ol &:before': {
+          float: 'left',
+          margin: `2px 12px 0 0`,
+          counterIncrement: 'section',
+          content: 'counters(section, ".") " "',
+          color: theme.color.red400,
+          fontWeight: theme.typography.semiBold,
+        },
+        'ul &': {
+          position: 'relative',
+          paddingLeft: theme.spacing[3],
+        },
+        'ul &:before': {
+          content: '""',
+          position: 'absolute',
+          top: '10px',
+          left: 0,
+          borderRadius: '50%',
+          border: `4px solid red`,
+        },
+        '&:not(:first-child), ul ul &:first-child, ol ol &:first-child': {
+          marginTop: 12,
+        },
+        '&:not(:last-child), ul ul &:last-child, ol ol &:last-child': {
+          marginBottom: 12,
+        },
+      },
     },
-    'ol ol &': {
-      paddingLeft: theme.spacing[3],
+    md: {
+      selectors: {
+        '&:not(:first-child), ul ul &:first-child, ol ol &:first-child': {
+          marginTop: 14,
+        },
+        '&:not(:last-child), ul ul &:last-child, ol ol &:last-child': {
+          marginBottom: 14,
+        },
+      },
     },
-    'ol &:before': {
-      float: 'left',
-      margin: `2px 12px 0 0`,
-      counterIncrement: 'section',
-      content: 'counters(section, ".") " "',
-      color: theme.color.red400,
-      fontWeight: theme.typography.semiBold,
-    },
-    'ul &': {
-      position: 'relative',
-      paddingLeft: theme.spacing[3],
-    },
-    'ul &:before': {
-      content: '""',
-      position: 'absolute',
-      top: '10px',
-      left: 0,
-      borderRadius: '50%',
-      border: `4px solid red`,
-    },
-    '&:not(:first-child), ul ul &:first-child, ol ol &:first-child': {
-      marginTop: 14,
-    },
-    '&:not(:last-child), ul ul &:last-child, ol ol &:last-child': {
-      marginBottom: 14,
-    },
-  },
+  }),
 })
 
 export const paragraph = style({
-  selectors: {
-    'p + &': {
+  marginTop: 12,
+  marginBottom: 12,
+  ...themeUtils.responsiveStyle({
+    md: {
       marginTop: 14,
+      marginBottom: 14,
     },
+  }),
+  selectors: {
     [`${listItem} &`]: {
       marginBottom: 0,
     },
