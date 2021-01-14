aff209d52a48baaee3d73c97aa11daf06fd1cb92
Stefán Harald <stefanhar@users.noreply.github.com>
2021-01-14 12:18:18 +0000

    Feature/content search remove index aliases (#2470)
    * removed index aliases
    
    * Fixed type errors
    
    * Removed unused code
    
    * Added camelcase ignore to ES config
    
    * Added correct gitbook format
    
    * Added tests for index manager
    
    * Fixed wording on test case
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/search-indexer/src/migrate/index.ts
+++ apps/services/search-indexer/src/migrate/index.ts
@@ -1,31 +1,19 @@
 import yargs from 'yargs'
-
 import { logger } from '@island.is/logging'
 import { environment } from '../environments/environment'
 import * as aws from './aws'
 import * as dictionary from './dictionary'
 import * as elastic from './elastic'
 import * as kibana from './kibana'
-
-interface PromiseStatus {
-  success: boolean
-  error?: Error
-}
+import {
+  getElasticsearchIndex,
+  getElasticVersion,
+} from '@island.is/content-search-index-manager'
 
 const { locales } = environment
 
 class App {
   async run() {
-    const versions = await Promise.all(
-      locales.map((locale: string) =>
-        elastic.getCurrentVersionFromConfig(locale),
-      ),
-    )
-    if (!versions.every((version) => version === versions[0])) {
-      logger.error('All of the index templates should have the same version')
-      process.exit(1)
-    }
-
     logger.info('Starting migration of dictionaries and ES config', environment)
 
     const hasAwsAccess = await aws.checkAWSAccess()
@@ -36,7 +24,7 @@ class App {
     } else {
       logger.info('No aws access found running in local development mode')
       // get packageId list for local development packages have package ids matching filenames in the dictionary repo
-      esPackages = dictionary.getFakeEsPackages()
+      esPackages = dictionary.getFakeEsPackages() // TODO: Make ES migration get it's own aws packages?
     }
 
     await this.migrateES(esPackages)
@@ -47,15 +35,6 @@ class App {
       logger.error('Failed migrating kibana', e)
     }
 
-    // we remove unused AWS ES packages after ES migrate cause we cant/should not remove packages already in use
-    if (hasAwsAccess) {
-      logger.info('Cleaning up unused packages')
-      // we want to disassociate and delete all packages below old index version, since other packages might be in use
-      const dictionaryVersion = await dictionary.getDictionaryVersion()
-      const oldDictionaryVersion = parseInt(dictionaryVersion) - 1
-      await aws.disassociateUnusedPackagesFromAwsEs(oldDictionaryVersion)
-      await aws.deleteUnusedPackagesFromAwsEs(oldDictionaryVersion)
-    }
     logger.info('Done!')
   }
 
@@ -97,125 +76,64 @@ class App {
     return filteredAwsEsPackages // es config needs the package ids when generating the index template
   }
 
-  private async migrateES(
-    esPackages: aws.AwsEsPackage[],
-  ): Promise<elastic.MigrationInfo> {
+  private async migrateES(esPackages: aws.AwsEsPackage[]): Promise<boolean> {
     logger.info('Starting elasticsearch migration')
-    await elastic.checkAccess() // this throws if there is no connection hence ensuring we dont continue
-    const processedMigrations: elastic.MigrationInfo = {} // to rollback changes on failure
-    const requests = locales.map(
-      async (locale): Promise<PromiseStatus> => {
-        const oldIndexVersion = await elastic.getCurrentVersionFromIndices(
-          locale,
-        )
-        const newIndexVersion = await elastic.getCurrentVersionFromConfig(
-          locale,
-        )
+    await elastic.checkAccess() // this throws if there is no connection hence ensuring we don't continue
 
-        // old index version is used in aws cleanup so we allways set it
-        processedMigrations[locale] = {
-          oldIndexVersion,
-        }
+    await Promise.all(
+      locales.map(
+        async (locale): Promise<boolean> => {
+          const newIndexName = getElasticsearchIndex(locale)
+          const newIndexExists = await elastic.checkIfIndexExists(newIndexName)
 
-        if (oldIndexVersion !== newIndexVersion) {
-          logger.info(
-            'Elasticsearch index version does not match code index version, updating elasticsearch config',
-            {
+          if (!newIndexExists) {
+            logger.info('New index not found, updating elasticsearch config', {
               locale,
-              esIndexVersion: oldIndexVersion,
-              codeIndexVersion: newIndexVersion,
-            },
-          )
-
-          try {
-            processedMigrations[
-              locale
-            ].oldTemplate = await elastic.getEsTemplate(locale)
-            await elastic.updateIndexTemplate(locale, esPackages)
-            await elastic.createNewIndexVersion(locale, newIndexVersion)
-            processedMigrations[locale].newIndexVersion = newIndexVersion
-            await elastic.importContentToIndex(locale, newIndexVersion, 'full')
-            await elastic.moveAliasToNewIndex(
-              locale,
-              newIndexVersion,
-              oldIndexVersion,
-            ) // we assume we dont have to rollback on failure here
-            await elastic.removeIndexesBelowVersion(locale, oldIndexVersion) // we keep the old index version as a backup
-          } catch (error) {
-            logger.error('Failed to migrate to new index', {
-              locale,
-              newIndexVersion,
-              oldIndexVersion,
+              newIndexName,
             })
-            return {
-              success: false, // pass this to promise all where we revert all changes if we have any false values (allSettled is not supported)
-              error,
+
+            try {
+              await elastic.updateIndexTemplate(locale, esPackages)
+              await elastic.importContentToIndex(locale, newIndexName, 'full')
+            } catch (error) {
+              logger.error('Failed to migrate to new index', {
+                locale,
+                newIndexName,
+                error: error.message,
+              })
+              throw error
             }
-          }
-        } else {
-          logger.info(
-            'Elasticsearch index version matches code index version, no need to update index',
-            {
+          } else {
+            logger.info(
+              'Elasticsearch index version matches code index version, skipping index update',
+              {
+                locale,
+                newIndexName,
+              },
+            )
+            logger.info('Initializing elastic data')
+            // index mappers might have new rules so we run a initialize sync to make sure all data is up to date with this new version
+            await elastic.importContentToIndex(
               locale,
-              esIndexVersion: oldIndexVersion,
-              codeIndexVersion: newIndexVersion,
-            },
-          )
-          logger.info('Initializing elastic data')
-          // we send a initialize sync request to ensure all assets are up to date before this container runs
-          await elastic.importContentToIndex(
-            locale,
-            oldIndexVersion,
-            'initialize',
-          )
-        }
-        return { success: true }
-      },
+              newIndexName,
+              'initialize',
+            )
+          }
+          return true
+        },
+      ),
     )
-
-    try {
-      // we wait for all promises to settle then we throw error if needed
-      const results = await Promise.all(requests)
-      results.forEach((result) => {
-        if (!result.success) throw result.error
-      })
-    } catch (error) {
-      logger.error(
-        'Failed to migrate elasticsearch, rolling back to earlier version',
-        error,
-      )
-      const locales = Object.keys(processedMigrations)
-      const reverts = locales.map(async (locale) => {
-        const { oldTemplate, newIndexVersion } = processedMigrations[locale]
-
-        if (oldTemplate) {
-          logger.info('Rolling back to earlier template', { locale })
-          await elastic.updateEsTemplate(locale, oldTemplate)
-        }
-
-        if (newIndexVersion) {
-          logger.info('Removing new index version', { locale })
-          await elastic.removeIndexVersion(locale, newIndexVersion)
-        }
-      })
-      await Promise.all(reverts)
-      logger.info('Done rolling back to earlier version')
-      throw error // we dont want this indexer to run if it could not migrate
-    }
 
     // rank the search results so we can see if they change
-    const currentIcelandicIndexVersion = await elastic.getCurrentVersionFromIndices(
-      'is',
-    )
-    await elastic.rankSearchQueries('is', currentIcelandicIndexVersion)
+    await elastic.rankSearchQueries(getElasticsearchIndex('is'))
 
     logger.info('Elasticsearch migration completed')
-    return processedMigrations
+    return true
   }
 
   private async migrateKibana() {
     logger.info('Starting kibana migration')
-    const version = await elastic.getCurrentVersionFromConfig(locales[0])
+    const version = getElasticVersion()
     await kibana.importObjects(version)
     logger.info('Done')
   }
@@ -236,7 +154,6 @@ async function migrateBootstrap() {
     await app.run()
   }
 }
-// TODO: Add lock to this procedure to ensure only a single initContainer can run this
 
 migrateBootstrap().catch((error) => {
   logger.error('ERROR: ', error)
