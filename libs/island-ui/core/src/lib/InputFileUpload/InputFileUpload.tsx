182aae3625b64ac5799498dab545c41f81733ddf
Noam <noam@aranja.com>
2021-01-15 17:12:41 +0000

    Update InputFileUpload.tsx

--- libs/island-ui/core/src/lib/InputFileUpload/InputFileUpload.tsx
+++ libs/island-ui/core/src/lib/InputFileUpload/InputFileUpload.tsx
@@ -15,7 +15,7 @@ export type UploadFileStatus = 'error' | 'done' | 'uploading'
 export interface UploadFile {
   name: string
   url?: string
-  uploadKey?: string
+  key?: string
   status?: UploadFileStatus
   percent?: number
   originalFileObj?: File | Blob
