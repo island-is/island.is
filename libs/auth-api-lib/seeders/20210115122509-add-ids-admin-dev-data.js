7257d642ed3b4ed603a1537142394686fbae3c85
Sigurður Sturla Bjarnason <sigurdur@fuglar.com>
2021-01-15 14:38:58 +0000

    Adding the modified seed file with staging and guessed at prod url

--- libs/auth-api-lib/seeders/20210115122509-add-ids-admin-dev-data.js
+++ libs/auth-api-lib/seeders/20210115122509-add-ids-admin-dev-data.js
@@ -11,12 +11,30 @@ module.exports = {
             client_id: client_id,
             redirect_uri: 'https://auth-admin-web.dev01.devland.is',
           },
+          {
+            client_id: client_id,
+            redirect_uri: 'https://auth-admin-web.staging01.devland.is',
+          },
+          {
+            client_id: client_id,
+            redirect_uri: 'https://identity-server-admin.devland.is',
+          },
         ]),
         queryInterface.bulkInsert('client_redirect_uri', [
           {
             client_id: client_id,
             redirect_uri:
               'https://auth-admin-web.dev01.devland.is/api/auth/callback/identity-server',
+          },
+          {
+            client_id: client_id,
+            redirect_uri:
+              'https://auth-admin-web.staging01.devland.is/api/auth/callback/identity-server',
+          },
+          {
+            client_id: client_id,
+            redirect_uri:
+              'https://identity-server-admin.devland.is/api/auth/callback/identity-server',
           },
         ]),
       ]).then(() => resolve('done'))
@@ -30,10 +48,30 @@ module.exports = {
           client_id: client_id,
           redirect_uri: 'https://auth-admin-web.dev01.devland.is',
         }),
+        queryInterface.bulkDelete('client_post_logout_redirect_uri', {
+          client_id: client_id,
+          redirect_uri: 'https://auth-admin-web.staging01.devland.is',
+        }),
+        queryInterface.bulkDelete('client_post_logout_redirect_uri', {
+          client_id: client_id,
+          redirect_uri: 'https://identity-server-admin.devland.is',
+        }),
+
         queryInterface.bulkDelete('client_redirect_uri', {
-            client_id: client_id,
-            redirect_uri: 'https://auth-admin-web.dev01.devland.is/api/auth/callback/identity-server',
-        })
+          client_id: client_id,
+          redirect_uri:
+            'https://auth-admin-web.dev01.devland.is/api/auth/callback/identity-server',
+        }),
+        queryInterface.bulkDelete('client_redirect_uri', {
+          client_id: client_id,
+          redirect_uri:
+            'https://auth-admin-web.staging01.devland.is/api/auth/callback/identity-server',
+        }),
+        queryInterface.bulkDelete('client_redirect_uri', {
+          client_id: client_id,
+          redirect_uri:
+            'https://identity-server-admin.devland.is/api/auth/callback/identity-server',
+        }),
       ]).then(() => resolve('done'))
     })
   },
