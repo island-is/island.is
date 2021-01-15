e406a3a485c315ab059d405bfb13ccef399a6947
Sigurður Sturla Bjarnason <sigurdur@fuglar.com>
2021-01-15 14:20:53 +0000

    Adding the seed file

---
+++ libs/auth-api-lib/seeders/20210115122509-add-ids-admin-dev-data.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const client_id = 'ids-admin'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return new Promise((resolve) => {
+      Promise.all([
+        queryInterface.bulkInsert('client_post_logout_redirect_uri', [
+          {
+            client_id: client_id,
+            redirect_uri: 'https://auth-admin-web.dev01.devland.is',
+          },
+        ]),
+        queryInterface.bulkInsert('client_redirect_uri', [
+          {
+            client_id: client_id,
+            redirect_uri:
+              'https://auth-admin-web.dev01.devland.is/api/auth/callback/identity-server',
+          },
+        ]),
+      ]).then(() => resolve('done'))
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return new Promise((resolve) => {
+      Promise.all([
+        queryInterface.bulkDelete('client_post_logout_redirect_uri', {
+          client_id: client_id,
+          redirect_uri: 'https://auth-admin-web.dev01.devland.is',
+        }),
+        queryInterface.bulkDelete('client_redirect_uri', {
+            client_id: client_id,
+            redirect_uri: 'https://auth-admin-web.dev01.devland.is/api/auth/callback/identity-server',
+        })
+      ]).then(() => resolve('done'))
+    })
+  },
+}
