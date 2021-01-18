e0d4efc92819f7aeb88f4da9dffdfbcbe87d33ce
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-01-18 22:48:20 +0000

    Add example reference

---
+++ libs/application/templates/children-domicile-transfer/src/fields/ExampleReference/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+  FieldBaseProps, /* Field */
+} from '@island.is/application/core'
+
+// Only needed if using additional props other than FieldBaseProps
+// interface AdditionalProps {
+//   someProp: string
+// }
+
+// interface FieldBasePropsWithAdditionalProps extends FieldBaseProps {
+//   field: Field & {
+//     props: AdditionalProps
+//   }
+// }
+
+// Usage:
+// buildCustomField(
+// {
+//   id: 'domicileChangeInformation',
+//   title: '',
+//   component: 'DomicileChangeInformation',
+// }),
+
+// With custom props that will be accessible from props.field.props.${propName}
+// buildCustomField(
+// {
+//   id: 'domicileChangeInformation',
+//   title: '',
+//   component: 'DomicileChangeInformation',
+// },
+// {
+//   someProp: 'Hello there string'
+// }),
+
+const ExampleReference = ({ application }: FieldBaseProps) => {
+  return (
+    <div>{JSON.stringify(application)}</div>
+  )
+}
+
+export default ExampleReference
