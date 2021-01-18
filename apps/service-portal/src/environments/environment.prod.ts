8b6757e3c09707e542e06c076d744fb39e3fe874
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2021-01-18 12:58:17 +0000

    changes to auth and reset email url

--- apps/service-portal/src/environments/environment.prod.ts
+++ apps/service-portal/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export default {
   production: true,
   identityServer: {
     IDENTITY_SERVER_ISSUER_URL:
-      window.location.origin === 'https://beta.minarsidur.staging01.devland.is'
-        ? 'https://beta.minarsidur.dev01.devland.is'
+      window.location.origin === 'https://service-portal.staging01.devland.is/'
+        ? 'https://identity-server.staging01.devland.is'
         : window.location.origin === 'https://beta.minarsidur.island.is'
         ? 'https://innskra.island.is'
         : 'https://identity-server.dev01.devland.is',
