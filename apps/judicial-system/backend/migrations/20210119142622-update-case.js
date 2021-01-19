57286cd87605039b2785fb10f8f431a32cf88834
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-01-19 14:59:09 +0000

    Adds a field for other prosecutor demands

---
+++ apps/judicial-system/backend/migrations/20210119142622-update-case.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.addColumn(
+        'case',
+        'other_demands',
+        {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        { transaction: t },
+      ),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.removeColumn('case', 'other_demands', {
+        transaction: t,
+      }),
+    )
+  },
+};
