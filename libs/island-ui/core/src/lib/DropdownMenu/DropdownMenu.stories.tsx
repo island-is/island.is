592157db3b91be1ab3c6c41883b02e3c1c419d66
Brian Johannessen <barabrian89@gmail.com>
2021-01-19 12:12:14 +0000

    remove figma

--- libs/island-ui/core/src/lib/DropdownMenu/DropdownMenu.stories.tsx
+++ libs/island-ui/core/src/lib/DropdownMenu/DropdownMenu.stories.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { withDesign } from 'storybook-addon-designs'
-import { withFigma } from '../../utils/withFigma'
 
 import { DropdownMenu } from './DropdownMenu'
 
 export default {
   title: 'Navigation/DropdownMenu',
   component: DropdownMenu,
-  decorators: [withDesign],
-  parameters: withFigma({
-    desktop:
-      'https://www.figma.com/file/cl7qnkUWOFwgMQKt2FsiZn/H%C3%B6nnun?node-id=6170%3A685',
-    mobile:
-      'https://www.figma.com/file/cl7qnkUWOFwgMQKt2FsiZn/H%C3%B6nnun?node-id=6271%3A1298',
-  }),
 }
 
 const Template = (args) => <DropdownMenu {...args} />
