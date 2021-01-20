45686f39a12d92c1852afc9eb63f3d402fc8a319
Brynjar Ingimarsson <brynjar@stefna.is>
2021-01-20 10:46:45 +0000

    Initialize library api-domains-syslumenn

---
+++ libs/api/domains/syslumenn/src/lib/syslumenn.client.ts
@@ -0,0 +1,30 @@
+import {HttpService, Inject, Injectable} from "@nestjs/common";
+
+export const SYSLUMENN_CLIENT_CONFIG = 'SYSLUMENN_CLIENT_CONFIG'
+
+export interface SyslumennClientConfig {
+  url: string
+  username: string
+  password: string
+}
+
+@Injectable
+export class SyslumennClient {
+  private accessToken: string
+
+  constructor(
+    private httpService: HttpService,
+    @Inject(SYSLUMENN_CLIENT_CONFIG)
+    private clientConfig: SyslumennClientConfig
+  ) {}
+
+  private async getToken() {
+
+    return
+  }
+
+  async getHomestayList(year: number) {
+
+    return
+  }
+}
---
+++ libs/api/domains/syslumenn/src/lib/syslumenn.resolver.ts
@@ -0,0 +1,6 @@
+import {Resolver} from "@nestjs/graphql";
+
+@Resolver()
+export class SyslumennResolver {
+
+}
