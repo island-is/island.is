da13b557577d4f16c7d132b56a83ffb43bbabe4c
Ívar Oddsson <ivaroddsson@gmail.com>
2021-01-21 15:25:43 +0000

    Replace '../' imports with '@island.is' imports (#2608)
    * Replace '../' imports with '@island.is' imports
    
    * Import from index file
    
    * Import from index file
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/utils/mocks.ts
+++ apps/judicial-system/web/src/utils/mocks.ts
@@ -9,8 +9,11 @@ import {
   User,
   UserRole,
 } from '@island.is/judicial-system/types'
-import { CaseQuery, UpdateCaseMutation } from '../graphql'
-import { UserQuery } from '../shared-components/UserProvider/UserProvider'
+import {
+  CaseQuery,
+  UpdateCaseMutation,
+} from '@island.is/judicial-system-web/src/graphql'
+import { UserQuery } from '@island.is/judicial-system-web/src/shared-components/UserProvider/UserProvider'
 
 export const mockProsecutor = {
   role: UserRole.PROSECUTOR,
