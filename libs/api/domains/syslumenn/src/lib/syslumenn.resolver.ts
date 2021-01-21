95b7dfd867bd2c901bdbbf7693b2d262126f348a
Brynjar Ingimarsson <brynjar@stefna.is>
2021-01-21 12:35:08 +0000

    Add cache control directive to GraphQL query

--- libs/api/domains/syslumenn/src/lib/syslumenn.resolver.ts
+++ libs/api/domains/syslumenn/src/lib/syslumenn.resolver.ts
@@ -2,33 +2,21 @@ import {Args, Query, Resolver} from "@nestjs/graphql";
 import {GetHomestaysInput} from "./dto/getHomestays.input";
 import {Homestay} from "./models/homestay"
 import {SyslumennService} from "./syslumenn.service";
+import {environment} from "../../../cms/src/lib/environments";
+
+const { cacheTime } = environment
+
+const cacheControlDirective = (ms = cacheTime) => `@cacheControl(maxAge: ${ms})`
 
 @Resolver()
 export class SyslumennResolver {
   constructor(private syslumennService: SyslumennService) {}
 
+  @Directive(cacheControlDirective())
   @Query(() => [Homestay])
   getHomestays(
     @Args('input') input: GetHomestaysInput
   ): Promise<Homestay[]> {
     return this.syslumennService.getHomestays(input.year)
-    /*return [
-      {
-        registrationNumber: "H222",
-        address: "Hello",
-        name: "Hooo",
-        year: input.year,
-        city: "Akureyri",
-        manager: "ok"
-      },
-      {
-        registrationNumber: "H222",
-        address: "Hello",
-        name: "Hooo",
-        year: input.year,
-        city: "Reykjavík",
-        manager: "ok"
-      }
-    ]*/
   }
 }
