b0e4072fbca07ab6858424b68547e5652b1e0d97
Valur Einarsson <valure@fuglar.com>
2021-01-22 15:25:21 +0000

    Seed adding app provider to idp_restrictions table

---
+++ libs/auth-api-lib/seeders/20210122152137-add-app-provider.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('idp_restrictions', [
+      {
+        name: 'app',
+        description: 'Mobile App',
+        helptext: 'Allows users to login with mobile app',
+        level: 4,
+      },
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'idp_restrictions',
+      {
+        name: 'app',
+    )
+  },
+}
