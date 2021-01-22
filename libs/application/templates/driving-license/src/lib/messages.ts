15c7ef73c58a40d4c51edaff6dd3f655d153cd45
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-01-22 15:05:43 +0000

    Add translations

--- libs/application/templates/driving-license/src/lib/messages.ts
+++ libs/application/templates/driving-license/src/lib/messages.ts
@@ -1,9 +1,319 @@
 import { defineMessages } from 'react-intl'
 
 export const m = defineMessages({
-  testing: {
-    id: 'dl.application:bikeType.A2',
-    defaultMessage: '<span><b>A2</b> Bifhjól</span>',
-    description: '<span><b>A2</b> Bike</span>',
+  yes: {
+    id: 'dl.application:shared.yes',
+    defaultMessage: 'Já',
+    description: 'Yes',
+  },
+  no: {
+    id: 'dl.application:shared.no',
+    defaultMessage: 'Nei',
+    description: 'No',
+  },
+  applicationName: {
+    id: 'dl.application:application.name',
+    defaultMessage: 'Ökuskilríki',
+    description: 'Driving license',
+  },
+  externalDataSection: {
+    id: 'dl.application:externalData.section',
+    defaultMessage: 'Ökuréttindi',
+    description: 'Driving license',
+  },
+  externalDataTitle: {
+    id: 'dl.application:externalData.title',
+    defaultMessage: 'Upplýsingasöfnun og skilyrði',
+    description: 'Information gathering and conditions',
+  },
+  nationalRegistryTitle: {
+    id: 'dl.application:nationalRegistry.title',
+    defaultMessage: 'Persónuuplýsingar úr Þjóðskrá',
+    description: 'Personal information from the National Registry',
+  },
+  nationalRegistrySubTitle: {
+    id: 'dl.application:nationalRegistry.subTitle',
+    defaultMessage:
+      'Til þess að auðvelda fyrir sækjum við persónuuplýsingar úr Þjóðskrá til þess að fylla út í umsóknina',
+    description:
+      'Information from the National Registry will be used to prefill the data in the application',
+  },
+  userProfileInformationTitle: {
+    id: 'pl.application:userprofile.title',
+    defaultMessage: 'Netfang og símanúmer úr þínum stillingum',
+    description: 'Your user profile information',
+  },
+  userProfileInformationSubTitle: {
+    id: 'pl.application:userprofile.subTitle',
+    defaultMessage:
+      'Til þess að auðvelda umsóknarferlið er gott að hafa stillt netfang og símanúmer á mínum síðum',
+    description:
+      'In order to apply for this application we need your email and phone number',
+  },
+  penaltyPointsTitle: {
+    id: 'pl.application:penaltyPoints.title',
+    defaultMessage: 'Punktastaða úr Ökuskírteinaskrá',
+    description: 'Penalty points from Ökuskírteinaskrá',
+  },
+  penaltyPointsSubTitle: {
+    id: 'pl.application:penaltyPoints.subTitle',
+    defaultMessage:
+      'Til þess að tryggja að notandi hafi heimild til þess að sækja um ökuskírteini út frá punktastöðu',
+    description:
+      'In order to ensure the user has permission to apply for driving license',
+  },
+  residenceTitle: {
+    id: 'pl.application:residence.title',
+    defaultMessage: 'Búseta',
+    description: 'Recidence',
+  },
+  residenceSubTitle: {
+    id: 'pl.application:residence.subtitle',
+    defaultMessage:
+      'Ég hef fasta búsetu hér á landi eins og hún er skilgreind í VIII. viðauka reglugerðar um ökuskírteini eða tel mig fullnægja skilyrðum um búsetu hér á landi til að fá gefið út ökuskírteini.',
+    description:
+      "I've lived in Iceland according to VIII and here by confirm that I meet the conditions to apply for a driving license",
+  },
+  typeFieldMultiFieldTitle: {
+    id: 'pl.application:typeField.multiFieldTitle',
+    defaultMessage: 'Ég er að sækja um:',
+    description: "I'm applying for:",
+  },
+  typeFieldCheckbox: {
+    id: 'pl.application:typeField.checkbox',
+    defaultMessage: 'Tegund ökutækja',
+    description: 'Type of vehicle',
+  },
+  typeFieldCar: {
+    id: 'pl.application:typeField.car',
+    defaultMessage: 'Almenn ökuréttindi',
+    description: 'General car license',
+  },
+  typeFieldMotorcycle: {
+    id: 'pl.application:typeField.motorcycle',
+    defaultMessage: 'Bifhjólaréttindi',
+    description: 'Motorcycle license',
+  },
+  typeFieldTrailer: {
+    id: 'pl.application:typeField.trailer',
+    defaultMessage: 'Kerrur og eftirvagnar',
+    description: 'Trailers',
+  },
+  subTypeFieldTitle: {
+    id: 'pl.application:subTypeField.subtitle',
+    defaultMessage: 'Tegund',
+    description: 'Type',
+  },
+  subTypeFieldMultiFieldTitle: {
+    id: 'pl.application:subTypeField.multiFieldTitle',
+    defaultMessage: 'Ég er að sækja um:',
+    description: "I'm applying for:",
+  },
+  subTypeFieldCar: {
+    id: 'pl.application:subTypeField.car',
+    defaultMessage: 'Fólksbílaflokkar:',
+    description: 'Passenger car categories',
+  },
+  subTypeFieldMotorcycle: {
+    id: 'pl.application:subTypeField.motorcycle',
+    defaultMessage: 'Bifhjólaflokkar:',
+    description: 'Motorcycle categories',
+  },
+  informationSectionTitle: {
+    id: 'pl.application:informationSection.title',
+    defaultMessage: 'Upplýsingar:',
+    description: 'Information:',
+  },
+  informationMultiFieldTitle: {
+    id: 'pl.application:informationMultiField.title',
+    defaultMessage: 'Upplýsingar:',
+    description: 'Information:',
+  },
+  informationApplicant: {
+    id: 'pl.application:information.applicant',
+    defaultMessage: 'Umsækjandi',
+    description: 'Applicant',
+  },
+  informationTeacher: {
+    id: 'pl.application:information.teacher',
+    defaultMessage: 'Ökukennari',
+    description: 'Teacher',
+  },
+  healthDeclarationSectionTitle: {
+    id: 'pl.application:healthDeclarationSection.title',
+    defaultMessage: 'Heilbrigðisyfirlýsing',
+    description: 'Health declaration',
+  },
+  healthDeclarationMultiFieldTitle: {
+    id: 'pl.application:healthDeclarationMultiField.title',
+    defaultMessage: 'Heilbrigðisyfirlýsing',
+    description: 'Health declaration',
+  },
+  healthDeclarationMultiFieldSubTitle: {
+    id: 'pl.application:healthDeclarationMultiField.subTitle',
+    defaultMessage: 'Yfirlýsing um líkamlegt og andlegt heilbrigði',
+    description: 'Statement of physical and mental health',
+  },
+  healthDeclaration1: {
+    id: 'pl.application:healthDeclaration.1',
+    defaultMessage: '1. Notar þú gleraugu, snertilinsur eða hefur skerta sjón?',
+    description:
+      '1. Do you wear glasses, contact lenses or have impaired vision?',
+  },
+  healthDeclaration2: {
+    id: 'pl.application:healthDeclaration.2',
+    defaultMessage:
+      '2. Hefur þú verið flogaveik(ur) eða orðið fyrir alvarlegri truflun á meðvitund og stjórn hreyfinga?',
+    description:
+      '2. Have you had epilepsy or a severe disturbance of consciousness and control of movement?',
+  },
+  healthDeclaration3: {
+    id: 'pl.application:healthDeclaration.3',
+    defaultMessage:
+      '3. Hefur þú nú eða hefur þú haft alvarlegan hjartasjúkdóm?',
+    description:
+      '3. Do you now have or have you had a serious heart condition?',
+  },
+  healthDeclaration4: {
+    id: 'pl.application:healthDeclaration.4',
+    defaultMessage: '4. Hefur þú nú eða hefur þú haft alvarlegan geðsjúkdóm?',
+    description: '4. Do you now have or have you had a serious mental illness?',
+  },
+  healthDeclaration5: {
+    id: 'pl.application:healthDeclaration.5',
+    defaultMessage:
+      '5. Notar þú að staðaldri læknislyf eða lyfjablöndur sem geta haft áhrif á meðvitund?',
+    description:
+      '5. Do you always use medicines or combinations of medicines that may affect your consciousness?',
+  },
+  healthDeclaration6: {
+    id: 'pl.application:healthDeclaration.6',
+    defaultMessage:
+      '6. Ert þú háð(ur) áfengi, ávana- og/eða fíkniefnum eða misnotar þú geðræn lyf sem verkað gætu á meðvitund?',
+    description:
+      '6. Are you addicted to alcohol, drugs and / or drugs or are you abusing psychotropic drugs that could affect your consciousness?',
+  },
+  healthDeclaration7: {
+    id: 'pl.application:healthDeclaration.7',
+    defaultMessage: '7. Notar þú insúlín og/eða töflur við sykursýki?',
+    description: '7. Do you use insulin and / or tablets for diabetes?',
+  },
+  healthDeclaration8: {
+    id: 'pl.application:healthDeclaration.8',
+    defaultMessage:
+      '8. Hefur þú nú eða hefur þú haft hömlur í hreyfikerfi líkamans?',
+    description:
+      "8. Do you now have or have you had restrictions on your body's motor system?",
+  },
+  healthDeclaration9: {
+    id: 'pl.application:healthDeclaration.9',
+    defaultMessage:
+      '9. Átt þú við einhvern annan sjúkdóm að stríða sem þú telur að geti haft áhrif á öryggi þitt í akstri í framtíðinni?',
+    description:
+      '9. Do you have any other illnesses that you think may affect your driving safety in the future?',
+  },
+  overviewSectionTitle: {
+    id: 'pl.application:overviewSection.title',
+    defaultMessage: 'Staðfesting',
+    description: 'Confirmation',
+  },
+  overviewMultiFieldTitle: {
+    id: 'pl.application:overviewMultiField.title',
+    defaultMessage: 'Yfirlit',
+    description: 'Overview',
+  },
+  overviewMultiFieldDescription: {
+    id: 'pl.application:overviewMultiField.Description',
+    defaultMessage:
+      'Vinsamlegast farðu yfir gögnin hér að neðan til að staðfesta að réttar upplýsingar hafi verið gefnar upp.',
+    description: "Please review the data below to confirm they're correct.",
+  },
+  overviewSubType: {
+    id: 'pl.application:overview.subType',
+    defaultMessage: 'Tegund ökuréttinda',
+    description: 'Driving license type',
+  },
+  overviewName: {
+    id: 'pl.application:overview.Name',
+    defaultMessage: 'Nafn',
+    description: 'Name',
+  },
+  overviewPhoneNumber: {
+    id: 'pl.application:overview.phoneNumber',
+    defaultMessage: 'Sími',
+    description: 'Phone number',
+  },
+  overviewNationalId: {
+    id: 'pl.application:overview.nationalId',
+    defaultMessage: 'Kennitala',
+    description: 'National Id',
+  },
+  overviewStreetAddress: {
+    id: 'pl.application:overview.streetAddress',
+    defaultMessage: 'Heimili',
+    description: 'Street address',
+  },
+  overviewPostalCode: {
+    id: 'pl.application:overview.postalCode',
+    defaultMessage: 'Póstnúmer',
+    description: 'Postal code',
+  },
+  overviewEmail: {
+    id: 'pl.application:overview.email',
+    defaultMessage: 'Netfang',
+    description: 'Email',
+  },
+  overviewCity: {
+    id: 'pl.application:overview.City',
+    defaultMessage: 'Staður',
+    description: 'City',
+  },
+  overviewTeacher: {
+    id: 'pl.application:overview.teacher',
+    defaultMessage: 'Ökukennari',
+    description: 'Teacher',
+  },
+  overviewBringData: {
+    id: 'pl.application:overview.bringData',
+    defaultMessage: 'Gögn höfð meðferðis til sýslumanns',
+    description: 'Will bring along data',
+  },
+  overviewBringPhoneData: {
+    id: 'pl.application:overview.bringPhoneData',
+    defaultMessage: 'Ég kem með mynd og rithandarsýni til sýslumanns',
+    description: 'I bring a picture and a handwriting sample to the sheriff',
+  },
+  overviewBringCertificateData: {
+    id: 'pl.application:overview.bringCertificateData',
+    defaultMessage: 'Ég kem með vottorð frá lækni meðferðis',
+    description: 'I bring a certificate from a doctor',
+  },
+  overviewSubmit: {
+    id: 'pl.application:overview.submit',
+    defaultMessage: 'Smelltu hér til að senda inn umsókn',
+    description: 'Click here to submit an application',
+  },
+  overviewCongratulations: {
+    id: 'pl.application:overview.congratulations',
+    defaultMessage: 'Til hamingju',
+    description: 'Congratulations',
+  },
+  congratulationsBullet1: {
+    id: 'pl.application:congratulations.bullet1',
+    defaultMessage: 'Umsókn þín um almenn ökuréttindi hefur verið staðfest',
+    description:
+      'Your application for a general driving license has been confirmed',
+  },
+  congratulationsBullet2: {
+    id: 'pl.application:congratulations.bullet2',
+    defaultMessage:
+      'Þú munt fá póst á skráð netfang þegar ökuskírteini er gefið út',
+    description:
+      "You will receive an email at a registered email address when the driver's license is issued",
+  },
+  congratulationsBullet3: {
+    id: 'pl.application:congratulations.bullet3',
+    defaultMessage: 'Hægt er að fylgjast með virkum umsóknum á Mínum síðum',
+    description: 'You can follow active applications on the Service Portal',
   },
 })
