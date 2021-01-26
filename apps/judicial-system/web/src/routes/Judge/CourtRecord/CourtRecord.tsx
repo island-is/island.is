ea176692cce2f83e7798008db39281cbfc095f1f
Ívar Oddsson <ivaroddsson@gmail.com>
2021-01-26 14:37:48 +0000

    Revert prefill change

--- apps/judicial-system/web/src/routes/Judge/CourtRecord/CourtRecord.tsx
+++ apps/judicial-system/web/src/routes/Judge/CourtRecord/CourtRecord.tsx
@@ -109,7 +109,7 @@ export const CourtRecord: React.FC = () => {
     if (!workingCase && data?.case) {
       let theCase = data.case
 
-      if (theCase.courtAttendees !== defaultCourtAttendees(theCase)) {
+      if (!theCase.courtAttendees) {
         theCase = { ...theCase, courtAttendees: defaultCourtAttendees(theCase) }
 
         if (theCase.courtAttendees) {
