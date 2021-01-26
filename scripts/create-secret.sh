997e67acf612430b219f68745d4e42f7e1815d78
Jón Levy <jon.levy@ruv.is>
2021-01-26 14:14:28 +0000

    refactor(scripts): added colors for prompt, success, error

--- scripts/create-secret.sh
+++ scripts/create-secret.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+
+
 # Creates a secret (SecureString) in the parameter store using the path /k8s/[secret-name]
 
+## Colors
+BLUE=$'\e[1;34m'
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+
 ## Prompt user for secret name
-read -p 'Secret name: ' SECRET_NAME
+read -p $BLUE'Secret name: ' SECRET_NAME
 
 # Minimum length
 MIN_LENGTH="{6,16}"
@@ -21,28 +28,28 @@ PATTERN=$ALPHANUMERIC_DASH$MIN_LENGTH$ONE_OR_MORE
 # Blank secret name not allowed
 if [ -z "$SECRET_NAME" ]
 then
-    echo 'Secret name cannot be empty'
+    echo $RED'Secret name cannot be empty'
     exit 0
 fi
 
 # Validate characters
 if [[ ! $SECRET_NAME =~ $ALPHANUMERIC_DASH$ONE_OR_MORE ]]
 then
-    echo "Secret name can only contain letters, numbers and dash"
+    echo $RED"Secret name can only contain letters, numbers and dash"
     exit 0
 fi
 
 # Validate minimum length
 if [[ ! $SECRET_NAME =~ $ALPHANUMERIC_DASH$MIN_LENGTH ]]
 then
-    echo 'Secret name should be 6-16 characters long.'
+    echo $RED'Secret name should be 6-16 characters long.'
     exit 0
 fi
 
 # Validate regex pattern
 if [[ $SECRET_NAME =~ $PATTERN ]]
 then
-    echo "Success!"
+    echo $GREEN"Success!"
     exit 0
 else
     echo "This should not happen!"
