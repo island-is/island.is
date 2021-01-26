a5f3a0d7fb3a43f73036186d7c34d14e12d1e49a
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2021-01-26 21:44:18 +0000

    Change Service model datatype to have environment -> version.

--- libs/api-catalogue/services/src/lib/utils.ts
+++ libs/api-catalogue/services/src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { logger } from '@island.is/logging'
 import { ServiceId } from '../../gen/fetch/xrd-rest'
 
+const SEPERATOR = '-'
+
 export enum SortOrder {
   ASC,
   DESC,
@@ -34,6 +36,58 @@ export const serviceIdSort = (
   else if (codeA > codeB) return sortOrder === SortOrder.ASC ? 1 : -1
 
   return 0
+}
+
+/**
+ * Parses version number from the X-Road serviceCode.
+ *
+ * @param serviceCode Should be in the following formats:
+ *                    - petstore-v1
+ *                    - petstore-v2
+ *                    - amazing-petstore-v1
+ *                    - amazing-petstore-v2
+ * @returns The string that is the last segment when split by '-'
+ *          If '-' is not found then returns the serviceCode unparsed.
+ */
+export const parseVersionNumber = (serviceCode: string) => {
+  if (isValidServiceCodeWithVersion(serviceCode)) {
+    return serviceCode.split(SEPERATOR).splice(-1, 1)[0]
+  }
+  return serviceCode
+}
+
+/**
+ * Parses the serviceCode from the X-Road serviceCode to
+ * strip away the version number.
+ *
+ * @param serviceCode Should be in the following formats:
+ *                    - petstore-v1
+ *                    - petstore-v2
+ *                    - amazing-petstore-v1
+ *                    - amazing-petstore-v2
+ * @returns The string without the last segment after the last '-'
+ */
+export const parseServiceCode = (serviceCode: string) => {
+  if (isValidServiceCodeWithVersion(serviceCode)) {
+    const splits = serviceCode.split(SEPERATOR)
+    splits.splice(-1, 1)
+    return splits.join(SEPERATOR)
+  }
+  return serviceCode
+}
+
+const isValidServiceCodeWithVersion = (serviceCode: string) => {
+  const regExp = new RegExp('^v[0-9]+(.[0-9]+){0,2}$')
+
+  if (serviceCode) {
+    const splits = serviceCode.split(SEPERATOR)
+
+    if (splits && splits.length > 1 && regExp.test(splits[splits.length - 1])) {
+      return true
+    }
+  }
+
+  return false
 }
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
