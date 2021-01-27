1fc8e35d0d00a69bbbbfe4e7157c23ce75763386
Jérémy Barbet <jeremgraph@gmail.com>
2021-01-27 12:34:42 +0000

    fix: format

--- libs/application/core/src/types/ApplicationTemplate.ts
+++ libs/application/core/src/types/ApplicationTemplate.ts
@@ -15,7 +15,7 @@ export interface ApplicationTemplate<
   TContext extends ApplicationContext,
   TStateSchema extends ApplicationStateSchema<TEvents>,
   TEvents extends EventObject
-  > {
+> {
   readonly readyForProduction?: boolean
   readonly type: ApplicationTypes
   readonly name: string
