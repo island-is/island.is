2fd63d642a9630792d961191f5852e5e72039805
Brynjar Ingimarsson <brynjar@stefna.is>
2021-01-28 14:01:26 +0000

    Use theme.spacing for consistency

--- apps/web/components/Organization/Slice/Districts/DistrictsSlice.treat.ts
+++ apps/web/components/Organization/Slice/Districts/DistrictsSlice.treat.ts
@@ -1,17 +1,17 @@
 import { style } from 'treat'
-import { themeUtils } from '@island.is/island-ui/theme'
+import {theme, themeUtils} from '@island.is/island-ui/theme'
 
 export const districtsList = style({
   columnGap: 30,
   ...themeUtils.responsiveStyle({
     xs: {
       columnCount: 1,
-      marginTop: 40,
-      marginBottom: 40,
+      marginTop: theme.spacing[5],
+      marginBottom: theme.spacing[5],
     },
     md: {
       columnCount: 2,
-      marginTop: 75,
+      marginTop: theme.spacing[9],
     },
     lg: { columnCount: 3 },
     xl: { columnCount: 3 },
@@ -19,5 +19,5 @@ export const districtsList = style({
 })
 
 export const districtsListItem = style({
-  marginBottom: 32,
+  marginBottom: theme.spacing[4],
 })
