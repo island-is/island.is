c960134646ce1d06272fc24ba12a41cf3b85eb96
Brynjar Ingimarsson <brynjar@stefna.is>
2021-01-28 12:51:51 +0000

    Remove necessary fragments

--- apps/web/components/Organization/OfficeCard/OfficeCard.tsx
+++ apps/web/components/Organization/OfficeCard/OfficeCard.tsx
@@ -16,26 +16,24 @@ export const OfficeCard: React.FC<OfficeCardProps> = ({
   openingHours,
 }) => {
   return (
-    <>
-      <Box
-        borderRadius="large"
-        overflow="hidden"
-        background="blue100"
-        boxShadow="subtle"
-        border="standard"
-        borderColor="blue200"
-        marginBottom={[1, 1, 1]}
-        marginTop={[1, 1, 1]}
-        paddingX={[3, 3, 3]}
-        paddingY={[3, 3, 3]}
-      >
-        <h1 className={styles.title}>{name}</h1>
-        <p style={{ fontWeight: 300, marginTop: 14 }}>{address}</p>
-        <div style={{ paddingTop: 18, fontWeight: 'bold' }}>Opnunartími</div>
-        {openingHours.split('\n').map((line) => (
-          <p style={{ fontWeight: 300, marginTop: 14 }}>{line}</p>
-        ))}
-      </Box>
-    </>
+    <Box
+      borderRadius="large"
+      overflow="hidden"
+      background="blue100"
+      boxShadow="subtle"
+      border="standard"
+      borderColor="blue200"
+      marginBottom={[1, 1, 1]}
+      marginTop={[1, 1, 1]}
+      paddingX={[3, 3, 3]}
+      paddingY={[3, 3, 3]}
+    >
+      <h1 className={styles.title}>{name}</h1>
+      <p style={{ fontWeight: 300, marginTop: 14 }}>{address}</p>
+      <div style={{ paddingTop: 18, fontWeight: 'bold' }}>Opnunartími</div>
+      {openingHours.split('\n').map((line) => (
+        <p style={{ fontWeight: 300, marginTop: 14 }}>{line}</p>
+      ))}
+    </Box>
   )
 }
--- apps/web/components/Organization/Slice/Districts/DistrictsSlice.tsx
+++ apps/web/components/Organization/Slice/Districts/DistrictsSlice.tsx
@@ -20,39 +20,37 @@ export const DistrictsSlice: React.FC<SliceProps> = ({
   slice,
 }) => {
   return (
-    <>
-      <section key={slice.id} aria-labelledby={'sliceTitle-' + slice.id}>
-        <GridContainer>
-          <Box
-            borderTopWidth="standard"
-            borderColor="standard"
-            paddingTop={[8, 6, 15]}
-            paddingBottom={[4, 5, 10]}
-          >
-            <h2 id={'sliceTitle-' + slice.id} className={styles.districtsTitle}>
-              {slice.title}
-            </h2>
-            <GridRow>
-              <GridColumn span={['12/12', '12/12', '7/12']}>
-                <ul className={styles.districtsList}>
-                  {slice.links.map((link) => (
-                    <li className={styles.districtsListItem}>
-                      <Link href={link.url}>
-                        <a>
-                          <Button variant="text">{link.text}</Button>
-                        </a>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </GridColumn>
-              <GridColumn span={['12/12', '12/12', '5/12']}>
-                <img src={slice.image.url} alt="" />
-              </GridColumn>
-            </GridRow>
-          </Box>
-        </GridContainer>
-      </section>
-    </>
+    <section key={slice.id} aria-labelledby={'sliceTitle-' + slice.id}>
+      <GridContainer>
+        <Box
+          borderTopWidth="standard"
+          borderColor="standard"
+          paddingTop={[8, 6, 15]}
+          paddingBottom={[4, 5, 10]}
+        >
+          <h2 id={'sliceTitle-' + slice.id} className={styles.districtsTitle}>
+            {slice.title}
+          </h2>
+          <GridRow>
+            <GridColumn span={['12/12', '12/12', '7/12']}>
+              <ul className={styles.districtsList}>
+                {slice.links.map((link) => (
+                  <li className={styles.districtsListItem}>
+                    <Link href={link.url}>
+                      <a>
+                        <Button variant="text">{link.text}</Button>
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </GridColumn>
+            <GridColumn span={['12/12', '12/12', '5/12']}>
+              <img src={slice.image.url} alt="" />
+            </GridColumn>
+          </GridRow>
+        </Box>
+      </GridContainer>
+    </section>
   )
 }
