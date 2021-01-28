1b95ba8224efe21a1a01a62d605d063d4ecfdc5f
Brynjar Ingimarsson <brynjar@stefna.is>
2021-01-28 14:10:30 +0000

    Fix formatting

--- apps/web/components/Organization/Slice/Districts/DistrictsSlice.treat.ts
+++ apps/web/components/Organization/Slice/Districts/DistrictsSlice.treat.ts
@@ -1,5 +1,5 @@
 import { style } from 'treat'
-import {theme, themeUtils} from '@island.is/island-ui/theme'
+import { theme, themeUtils } from '@island.is/island-ui/theme'
 
 export const districtsList = style({
   columnGap: 30,
