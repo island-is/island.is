896421ab2a80834f7409ab40cbee84e97c9fd8d0
Brynjar Ingimarsson <brynjar@stefna.is>
2021-02-05 16:17:08 +0000

    Add auction queries to web

---
+++ apps/web/screens/queries/Auction.tsx
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag'
+
+export const GET_AUCTIONS_QUERY = gql`
+  query GetAuctions($input: GetAuctionsInput!) {
+    getAuctions(input: $input) {
+      id
+      title
+      type
+      date
+    }
+  }
+`
+
+export const GET_AUCTION_QUERY = gql`
+  query GetAuction($input: GetAuctionInput!) {
+    getAuction(input: $input) {
+      id
+      title
+      type
+      date
+      content
+      organization {
+        title
+      }
+    }
+  }
+`
