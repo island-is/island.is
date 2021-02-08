47ef0b50b7f7fff8d19b0c935f1ec65f5981e8fd
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-02-08 15:45:05 +0000

    Remove console.log

--- apps/web/screens/Home.tsx
+++ apps/web/screens/Home.tsx
@@ -95,7 +95,6 @@ const Home: Screen<HomeProps> = ({
         </Box>
         <Inline space={2}>
           {page.featuredThings.map(({ title, attention, thing }) => {
-            console.log('thing', thing)
             const cardUrl = linkResolver(thing?.type as LinkType, [thing?.slug])
             return cardUrl?.href && cardUrl?.href.length > 0 ? (
               <Link key={title} {...cardUrl} skipTab>
