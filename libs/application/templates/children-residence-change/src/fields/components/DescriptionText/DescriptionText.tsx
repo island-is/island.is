1ca1f3134328c76ed67600ee6e2b01aced78b801
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-02-09 17:55:40 +0000

    Handle description object

--- libs/application/templates/children-residence-change/src/fields/components/DescriptionText/DescriptionText.tsx
+++ libs/application/templates/children-residence-change/src/fields/components/DescriptionText/DescriptionText.tsx
@@ -2,51 +2,82 @@ import React, { Fragment, ReactElement } from 'react'
 import HtmlParser from 'react-html-parser'
 import { MessageDescriptor } from 'react-intl'
 import { useLocale } from '@island.is/localization'
-import { Text } from '@island.is/island-ui/core'
+import { Text, TextProps } from '@island.is/island-ui/core'
 
 interface Props {
   text: MessageDescriptor
   format?: { [key: string]: string }
 }
 
+const isHeading = (type: string) => {
 const headingTags = ['h1', 'h2', 'h3', 'h4']
+return headingTags.includes(type)
+}
+
+const InnerDescription = ({
+  item,
+  lastItem,
+}: {
+  item: ReactElement
+  lastItem: boolean
+}) => {
+  const heading = isHeading(item.type as string)
+  const marginBottom = heading ? 1 : 2;
+  return (
+    <Text variant={heading ? 'h4' : 'default'} marginBottom={lastItem ? 0 : marginBottom}>
+      {item.props.children.map((element: string | ReactElement, i: number) => {
+        return <Fragment key={i}>{element}</Fragment>
+      })}
+    </Text>
+  )
+}
+
 const DescriptionText = ({ text, format }: Props) => {
-  console.log({ text })
   const { formatMessage } = useLocale()
-  const description: string | ReactElement[] = (formatMessage(text, {
-    h1: (str: string) => <h1>{str}</h1>,
-    h2: (str: string) => <h2>{str}</h2>,
-    h3: (str: string) => <h3>{str}</h3>,
-    h4: (str: string) => <h4>{str}</h4>,
-    p: (str: string) => <p>{str}</p>,
-    strong: (str: string) => <strong>{str}</strong>,
-    em: (str: string) => <em>{str}</em>,
-    ...format,
-  }) as unknown) as string | ReactElement[]
-  const parsedDescription =
-    typeof description === 'string'
+  const description: string | ReactElement | ReactElement[] = (formatMessage(
+    text,
+    {
+      h1: (str: string) => <h1>{str}</h1>,
+      h2: (str: string) => <h2>{str}</h2>,
+      h3: (str: string) => <h3>{str}</h3>,
+      h4: (str: string) => <h4>{str}</h4>,
+      p: (str: string) => <p>{str}</p>,
+      span: (str: string) => <span>{str}</span>,
+      strong: (str: string) => <strong>{str}</strong>,
+      em: (str: string) => <em>{str}</em>,
+      ...format,
+    },
+  ) as unknown) as string | ReactElement | ReactElement[]
+
+  // When formatting fails in 'formatMessage' it returns a string rather than a ReactElement.
+  // Then we use the HtmlParser to parse the outputted string that contains markup
+  const descriptionType = typeof description
+  const parsedDescription: string | ReactElement | ReactElement[] =
+    descriptionType === 'string'
       ? HtmlParser(description as string)
       : description
+  if (Array.isArray(parsedDescription)) {
+    return (
+      <>
+        {(parsedDescription as ReactElement[]).map((item, i) => {
+          return (
+            <InnerDescription
+              key={i}
+              item={item}
+              lastItem={
+                i + 1 === (parsedDescription as ReactElement[]).length
+              }
+            />
+          )
+        })}
+      </>
+    )
+  }
   return (
-    <>
-      {(parsedDescription as ReactElement[]).map((item, i) => {
-        const isHeading = headingTags.includes(item.type as string)
-        const marginBottom = isHeading ? 1 : 2
-        return (
-          <Text
-            key={i}
-            variant={isHeading ? 'h4' : 'default'}
-            marginBottom={i + 1 === description.length ? 0 : marginBottom}
-          >
-            {item.props.children.map(
-              (element: string | ReactElement, index: number) => {
-                return <Fragment key={index}>{element}</Fragment>
-              },
-            )}
-          </Text>
-        )
-      })}
-    </>
+    <InnerDescription
+      item={parsedDescription as ReactElement}
+      lastItem={true}
+    />
   )
 }
 
