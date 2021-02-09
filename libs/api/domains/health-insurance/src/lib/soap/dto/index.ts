50246a84f1c9d116d04c782772dbeb0197be59cf
qdd1 <6514088+qdd1@users.noreply.github.com>
2021-02-09 14:30:34 +0000

    Feature/health insurance upload xml to sjukra (#2826)
    * health-insurance - Initilise vistaskjal call
    
    * Added S3 bucket call
    
    * health-insurance - ready for mock test
    
    Co-authored-by: qdong <qdong@deloitte.is>

--- libs/api/domains/health-insurance/src/lib/soap/dto/index.ts
+++ libs/api/domains/health-insurance/src/lib/soap/dto/index.ts
@@ -1,2 +1,4 @@
 export { GetSjukratryggdurTypeDto } from './getSjukraTryggdurType.dto'
 export { GetFaUmsoknSjukratryggingTypeDto } from './getFaUmsoknSjukratryggingType.dto'
+export { GetVistaSkjalDtoType } from './getVistaSkjalDto.dto'
+export { GetVistaSkjalBody } from './getVistaSkjalBody.dto'
