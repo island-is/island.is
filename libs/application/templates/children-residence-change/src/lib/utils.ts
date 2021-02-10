f66de3fe7e98e164138cae283378260bd4161007
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-02-10 13:15:58 +0000

    Fix answers extraction

--- libs/application/templates/children-residence-change/src/lib/utils.ts
+++ libs/application/templates/children-residence-change/src/lib/utils.ts
@@ -24,13 +24,14 @@ export const extractChildrenFromApplication = (application: Application) => {
 }
 
 export const extractAnswersFromApplication = (application: Application) => {
+  const contactInfo = application.answers.contactInfo as string[]
   return {
     selectedChildren: application.answers.selectChild as string[],
     selectedDuration: application.answers.selectDuration as string[],
     reason: application.answers.residenceChangeReason as string,
     contactInformation: {
-      email: application.answers.email as string,
-      phoneNumber: application.answers.phoneNumber as string,
+      email: contactInfo[0] as string,
+      phoneNumber: contactInfo[1] as string,
     },
   }
 }
