d86891de4920c7560daf8e421312f0f71776c7d0
Sæunn Sif Heiðarsdóttir <saeunnsh@advania.is>
2021-02-11 12:45:52 +0000

    Dpo application/send email (#2856)
    * wip
    
    * test
    
    * testBranch
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * email
    
    * cleanup
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/template-api-modules/src/lib/modules/templates/index.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/index.ts
@@ -1,7 +1,13 @@
 import { ReferenceTemplateModule } from './reference-template/reference-template.module'
 import { ParentalLeaveModule } from './parental-leave/parental-leave.module'
+import { DocumentProviderOnboardingModule } from './document-provider-onboarding/document-provider-onboarding.module'
 
-export const modules = [ReferenceTemplateModule, ParentalLeaveModule]
+export const modules = [
+  ReferenceTemplateModule,
+  ParentalLeaveModule,
+  DocumentProviderOnboardingModule,
+]
 
 export { ReferenceTemplateService } from './reference-template/reference-template.service'
 export { ParentalLeaveService } from './parental-leave/parental-leave.service'
+export { DocumentProviderOnboardingService } from './document-provider-onboarding/document-provider-onboarding.service'
