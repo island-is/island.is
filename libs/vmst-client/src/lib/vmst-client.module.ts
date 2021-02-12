ab7c8a216eff3b0c467e4ece97b77d3b03fa2fac
Bæring Gunnar Steinþórsson <baering@aranja.com>
2021-02-12 10:33:24 +0000

    Temporarily log all network activity in the VMST client (#2872)
    To solve an issue with requests through X-Road failing
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/vmst-client/src/lib/vmst-client.module.ts
+++ libs/vmst-client/src/lib/vmst-client.module.ts
@@ -1,5 +1,8 @@
 import { DynamicModule } from '@nestjs/common'
 import fetch from 'isomorphic-fetch'
+
+import { logger } from '@island.is/logging'
+
 import {
   Configuration,
   ParentalLeaveApi,
@@ -14,6 +17,52 @@ export interface VMSTClientModuleConfig {
   xRoadClient: string
 }
 
+const stringify = (json: unknown) => {
+  try {
+    return JSON.stringify(json, null, '  ')
+  } catch {
+    return json
+  }
+}
+
+const wrappedFetchForLogging = (
+  input: RequestInfo,
+  init?: RequestInit,
+): Promise<Response> => {
+  logger.info('VMST Client request')
+  logger.info(stringify(input))
+  if (init) {
+    logger.info(
+      stringify({
+        ...init,
+        headers: {
+          ...(init['headers'] || {}),
+          'api-key': 'hidden',
+        },
+      }),
+    )
+  }
+  return new Promise((resolve, reject) => {
+    fetch(input, init)
+      .then((response) => {
+        logger.info('VMST Client response')
+        logger.info(
+          stringify({
+            status: response.status,
+            statusText: response.statusText,
+            ok: response.ok,
+          }),
+        )
+        resolve(response)
+      })
+      .catch((error) => {
+        logger.info('VMST Client error')
+        logger.info(stringify(error))
+        reject(error)
+      })
+  })
+}
+
 export class VMSTClientModule {
   static register(config: VMSTClientModuleConfig): DynamicModule {
     const headers = {
@@ -22,7 +71,7 @@ export class VMSTClientModule {
     }
 
     const providerConfiguration = new Configuration({
-      fetchApi: fetch,
+      fetchApi: wrappedFetchForLogging,
       basePath: config.xRoadPath,
       headers,
     })
