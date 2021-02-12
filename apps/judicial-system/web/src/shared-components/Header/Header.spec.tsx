8e44c0f50372945c9ab525f46331214a84b2ea34
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-02-12 11:48:47 +0000

    Fixes unit test

--- apps/judicial-system/web/src/shared-components/Header/Header.spec.tsx
+++ apps/judicial-system/web/src/shared-components/Header/Header.spec.tsx
@@ -1,21 +1,32 @@
 import { createMemoryHistory } from 'history'
 import { Header } from './'
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Router } from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
 import * as Constants from '@island.is/judicial-system-web/src/utils/constants'
+import { MockedProvider } from '@apollo/client/testing'
+import { mockProsecutorQuery } from '../../utils/mocks'
+import { UserProvider } from '..'
 
 describe('Header', () => {
-  test('should direct users to detention requests route when user click the logo', () => {
+  test('should direct users to detention requests route when user click the logo', async () => {
     // Arrange
     const history = createMemoryHistory()
     history.push(Constants.STEP_ONE_ROUTE)
     const { getByTestId } = render(
-      <Router history={history}>
-        <Header pathname={Constants.REQUEST_LIST_ROUTE} />
-      </Router>,
+      <MockedProvider mocks={mockProsecutorQuery} addTypename={false}>
+        <Router history={history}>
+          <UserProvider>
+            <Header pathname={Constants.REQUEST_LIST_ROUTE} />
+          </UserProvider>
+        </Router>
+      </MockedProvider>,
     )
+
+    // wait for login to complete
+    await screen.findByTestId('logout-button')
+
     const logo = getByTestId('link-to-home')
 
     // Act
@@ -23,6 +34,7 @@ describe('Header', () => {
     userEvent.click(logo)
 
     // Assert
+    console.log('!!!!!!!!', history.location.pathname)
     expect(history.location.pathname).toEqual(Constants.REQUEST_LIST_ROUTE)
   })
 })
