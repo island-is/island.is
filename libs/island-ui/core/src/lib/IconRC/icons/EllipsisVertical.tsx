f6a06b917af57fab9127e8c906d6ba30e0eea60e
Brian Johannessen <barabrian89@gmail.com>
2021-02-12 10:50:04 +0000

    icon

--- libs/island-ui/core/src/lib/IconRC/icons/EllipsisVertical.tsx
+++ libs/island-ui/core/src/lib/IconRC/icons/EllipsisVertical.tsx
@@ -5,21 +5,19 @@ const SvgEllipsisVertical = ({
   title,
   titleId,
   ...props
-}: React.SVGProps<SVGSVGElement> & SVGRProps) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="ellipsis-vertical_svg__ionicon"
-      viewBox="0 0 512 512"
-      aria-labelledby={titleId}
-      {...props}
-    >
-      {title ? <title id={titleId}>{title}</title> : null}
-      <circle cx={256} cy={256} r={48} />
-      <circle cx={256} cy={416} r={48} />
-      <circle cx={256} cy={96} r={48} />
-    </svg>
-  )
-}
+}: React.SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="ellipsis-vertical_svg__ionicon"
+    viewBox="0 0 512 512"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <circle cx={256} cy={256} r={48} />
+    <circle cx={256} cy={416} r={48} />
+    <circle cx={256} cy={96} r={48} />
+  </svg>
+)
 
 export default SvgEllipsisVertical
