af30e2e69cc46f5c44760a3a8de2185dde4861d3
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-02-14 12:28:56 +0000

    Fixes return type

--- apps/judicial-system/backend/src/app/modules/case/interceptors/case.filter.ts
+++ apps/judicial-system/backend/src/app/modules/case/interceptors/case.filter.ts
@@ -3,6 +3,6 @@ import { CaseState, UserRole } from '@island.is/judicial-system/types'
 export function isStateVisibleToRole(
   state: CaseState,
   role: UserRole,
-): unknown {
+): boolean {
   return state !== CaseState.NEW || role === UserRole.PROSECUTOR
 }
