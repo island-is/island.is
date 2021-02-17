7f92b78ebc2a99b66010fa686d28fe9e28f7fb04
Brynjar Ingimarsson <binni@outlook.com>
2021-02-17 14:57:51 +0000

    Use rich text instead of markdown for syslumenn pages (#2890)
    * Add rich text fields to backend models
    
    * Change from MarkdownText to richText renderer in frontend
    
    * Change field names
    
    * Fix build error
    
    * Use richText options to style footerItems
    
    * Fix build error
    
    Co-authored-by: Ingólfur Sigfússon <ingolfur@stefna.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/cms/src/lib/models/twoColumnText.model.ts
+++ libs/api/domains/cms/src/lib/models/twoColumnText.model.ts
@@ -4,6 +4,7 @@ import { ITwoColumnText } from '../generated/contentfulTypes'
 
 import { Link, mapLink } from './link.model'
 import { SystemMetadata } from 'api-cms-domain'
+import { mapDocument, SliceUnion } from '../unions/slice.union'
 
 @ObjectType()
 export class TwoColumnText {
@@ -13,8 +14,8 @@ export class TwoColumnText {
   @Field({ nullable: true })
   rightTitle?: string
 
-  @Field({ nullable: true })
-  rightContent?: string
+  @Field(() => [SliceUnion], { nullable: true })
+  rightContent: Array<typeof SliceUnion>
 
   @Field(() => Link, { nullable: true })
   rightLink?: Link
@@ -22,8 +23,8 @@ export class TwoColumnText {
   @Field({ nullable: true })
   leftTitle?: string
 
-  @Field({ nullable: true })
-  leftContent?: string
+  @Field(() => [SliceUnion], { nullable: true })
+  leftContent: Array<typeof SliceUnion>
 
   @Field(() => Link, { nullable: true })
   leftLink?: Link
@@ -36,9 +37,13 @@ export const mapTwoColumnText = ({
   typename: 'TwoColumnText',
   id: sys.id,
   rightTitle: fields.rightTitle ?? '',
-  rightContent: fields.rightContent ?? '',
+  rightContent: fields.rightContent
+    ? mapDocument(fields.rightContent, sys.id + ':right-content')
+    : [],
   rightLink: fields.rightLink ? mapLink(fields.rightLink) : null,
   leftTitle: fields.leftTitle ?? '',
-  leftContent: fields.leftContent ?? '',
+  leftContent: fields.leftContent
+    ? mapDocument(fields.leftContent, sys.id + ':left-content')
+    : [],
   leftLink: fields.leftLink ? mapLink(fields.leftLink) : null,
 })
