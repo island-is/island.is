28ee9fdf390358c8c0a401124311a53d9106522d
qdong <qdong@deloitte.is>
2021-02-17 14:26:37 +0000

    health-insurance - debugging

--- libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { logger } from '@island.is/logging'
 
 import { SharedTemplateApiService } from '../../shared'
 import { TemplateApiModuleActionProps } from '../../../types'
@@ -13,13 +12,13 @@ export class HealthInsuranceService {
     // private healthInsuranceServiceBackend: HealthInsuranceServiceBackend,
   ) {}
   async sendApplication({ application }: TemplateApiModuleActionProps){
+    console.log("---------- Application --------------")
+    console.log(JSON.stringify(application, null, 2))
     const vistaSkjal = transformApplicationToHealthInsuranceDTO(application)
-    logger.debug("---------- Application --------------")
-    logger.debug(JSON.stringify(application, null, 2))
-    logger.debug("-------------- Vistaskjal inputs --------------")
-    logger.debug(JSON.stringify(vistaSkjal, null, 2))
+    console.log("-------------- Vistaskjal inputs --------------")
+    console.log(JSON.stringify(vistaSkjal, null, 2))
     await new Promise((resolve) => setTimeout(resolve, 2000))
     // await this.healthInsuranceServiceBackend.applyInsurance(vistaSkjal)
-    logger.debug("Health-Insurance - Finished sendApplication")
+    console.log("Health-Insurance - Finished sendApplication")
   }
 }
