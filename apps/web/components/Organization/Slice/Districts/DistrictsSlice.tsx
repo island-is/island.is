8206c8666bbb7c41a5274d97a0f4f3b7a4b871a3
Ingólfur Sigfússon <ingolfur@stefna.is>
2021-02-17 09:31:35 +0000

    Render districts svg images as object to enable xlink and hover effects

--- apps/web/components/Organization/Slice/Districts/DistrictsSlice.tsx
+++ apps/web/components/Organization/Slice/Districts/DistrictsSlice.tsx
@@ -58,9 +58,17 @@ export const DistrictsSlice: React.FC<SliceProps> = ({ slice }) => {
                 ))}
               </Box>
             </GridColumn>
-            <GridColumn span={['12/12', '12/12', '4/11']}>
-              <img src={slice.image.url} alt="" />
-            </GridColumn>
+            {!!slice.image && (
+              <GridColumn span={['12/12', '12/12', '4/11']}>
+                {slice.image.url.split('.').pop() === 'svg' ? (
+                  <object data={slice.image.url} type="image/svg+xml">
+                    <img src={slice.image.url} alt="" />
+                  </object>
+                ) : (
+                  <img src={slice.image.url} alt="" />
+                )}
+              </GridColumn>
+            )}
           </GridRow>
         </Box>
       </section>
