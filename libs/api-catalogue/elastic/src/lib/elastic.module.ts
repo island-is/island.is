993843d9fb6abf035a68642598761e5c22a35fe2
guttih <gudjonholm@gmail.com>
2021-02-19 11:50:12 +0000

    ElasticSearch, reading config from a service

--- libs/api-catalogue/elastic/src/lib/elastic.module.ts
+++ libs/api-catalogue/elastic/src/lib/elastic.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ElasticService } from './elastic.service'
+import { ElasticConfigService } from './elasticconfig.service'
 
 @Module({
-  providers: [ElasticService],
+  providers: [ElasticService, ElasticConfigService],
   exports: [ElasticService],
 })
 export class ElasticModule {}
