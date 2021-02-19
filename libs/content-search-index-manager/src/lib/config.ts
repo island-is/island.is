72d5cbf74a6e3e9c10c9dbf3a29c7a5d802572df
Stefán Harald <stefanharald@gmail.com>
2021-02-19 14:48:20 +0000

    Bumped dictionary version

--- libs/content-search-index-manager/src/lib/config.ts
+++ libs/content-search-index-manager/src/lib/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  dictionaryVersion: '7139c83', // this version gets appended to files keep it as 7 letters
+  dictionaryVersion: '2b73253', // this version gets appended to files keep it as 7 letters
 }
