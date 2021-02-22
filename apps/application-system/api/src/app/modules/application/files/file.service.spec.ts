63accaac04b4174394ac2bb66998866cab5f8dc8
RikkiAbb <rikardarnar@gmail.com>
2021-02-22 10:07:09 +0000

    Uses specific application in each test

--- apps/application-system/api/src/app/modules/application/files/file.service.spec.ts
+++ apps/application-system/api/src/app/modules/application/files/file.service.spec.ts
@@ -21,16 +21,8 @@ describe('FileService', () => {
   const applicantSsn = '0113215029'
   const applicationId = '1111-2222-3333-4444'
 
-  const application = ({
-    id: applicationId,
-    state: 'draft',
-    applicant: applicantSsn,
-    assignees: [],
-    typeId: ApplicationTypes.CHILDREN_RESIDENCE_CHANGE,
-    modified: new Date(),
-    created: new Date(),
-    attachments: {},
-    answers: {
+  const createApplication = (
+    answers: object = {
       email: 'email@email.is ',
       phoneNumber: applicantsPhoneNumber,
       parentB: {
@@ -39,26 +31,37 @@ describe('FileService', () => {
       },
       expiry: 'permanent',
     },
-    externalData: {
-      parentNationalRegistry: {
-        data: {
-          id: 'id',
-          name: 'parent b',
-          ssn: '0022993322',
-          postalCode: '101',
-          address: 'Borgartún',
-          city: 'Reykjavík',
+  ) =>
+    (({
+      id: applicationId,
+      state: 'draft',
+      applicant: applicantSsn,
+      assignees: [],
+      typeId: ApplicationTypes.CHILDREN_RESIDENCE_CHANGE,
+      modified: new Date(),
+      created: new Date(),
+      attachments: {},
+      answers,
+      externalData: {
+        parentNationalRegistry: {
+          data: {
+            id: 'id',
+            name: 'parent b',
+            ssn: '0022993322',
+            postalCode: '101',
+            address: 'Borgartún',
+            city: 'Reykjavík',
+          },
+          status: 'success',
+          date: new Date(),
         },
-        status: 'success',
-        date: new Date(),
+        nationalRegistry: {
+          data: { nationalId: applicantSsn, fullName: applicantName },
+          status: 'success',
+          date: new Date(),
+        },
       },
-      nationalRegistry: {
-        data: { nationalId: applicantSsn, fullName: applicantName },
-        status: 'success',
-        date: new Date(),
-      },
-    },
-  } as unknown) as Application
+    } as unknown) as Application)
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -106,12 +109,14 @@ describe('FileService', () => {
   })
 
   it('should generate pdf for children residence change and return a presigned url', async () => {
+    const application = createApplication()
+
     const response = await service.createPdf(
       application,
       PdfTypes.CHILDREN_RESIDENCE_CHANGE,
     )
 
-    const fileName = `children-residence-change/${applicationId}/unsigned.pdf`
+    const fileName = `children-residence-change/${application.id}/unsigned.pdf`
 
     expect(aws.uploadFile).toHaveBeenCalledWith(Buffer.from('buffer'), fileName)
 
@@ -120,13 +125,15 @@ describe('FileService', () => {
     expect(response).toEqual('url')
   })
   it('should request file signature for children residence transfer then return controlCode and documentToken', async () => {
+    const application = createApplication()
+
     const response = await service.requestFileSignature(
       application,
       PdfTypes.CHILDREN_RESIDENCE_CHANGE,
     )
 
     expect(aws.getFile).toHaveBeenCalledWith(
-      `children-residence-change/${applicationId}/unsigned.pdf`,
+      `children-residence-change/${application.id}/unsigned.pdf`,
     )
 
     expect(signingService.requestSignature).toHaveBeenCalledWith(
@@ -147,12 +154,9 @@ describe('FileService', () => {
   })
 
   it('should throw error for request file signature since phone number is missing', async () => {
-    let app = application
-    app.answers = {}
-
     const act = async () =>
       await service.requestFileSignature(
-        application,
+        createApplication({}),
         PdfTypes.CHILDREN_RESIDENCE_CHANGE,
       )
 
@@ -172,7 +176,7 @@ describe('FileService', () => {
 
     const act = async () =>
       await service.requestFileSignature(
-        application,
+        createApplication(),
         PdfTypes.CHILDREN_RESIDENCE_CHANGE,
       )
 
