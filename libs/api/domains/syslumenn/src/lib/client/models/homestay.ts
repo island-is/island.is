e182ac6f5e80f08c56fcd0e2b23e855e2dcba7e5
Brynjar Ingimarsson <brynjar@stefna.is>
2021-02-22 16:55:57 +0000

    Add new fields to getHomestays

--- libs/api/domains/syslumenn/src/lib/client/models/homestay.ts
+++ libs/api/domains/syslumenn/src/lib/client/models/homestay.ts
@@ -5,4 +5,8 @@ export interface IHomestay {
   abyrgdarmadur?: string
   umsoknarAr?: number
   sveitarfelag?: string
+  gestafjoldi?: number
+  fjoldiHerbergja?: number
+  fastanumber?: string
+  ibudanumer?: string
 }
