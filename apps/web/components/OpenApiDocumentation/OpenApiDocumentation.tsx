b199335aca6fdd558acea5d236fdd06c333ded2a
Sævar Ingi Sigurðsson <saevar.ingi.sigurdsson@advania.is>
2021-02-22 17:10:19 +0000

    Switched to using patched redoc react component

--- apps/web/components/OpenApiDocumentation/OpenApiDocumentation.tsx
+++ apps/web/components/OpenApiDocumentation/OpenApiDocumentation.tsx
@@ -1,45 +1,24 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { OpenApi } from '@island.is/api-catalogue/types'
 import { Box } from '@island.is/island-ui/core'
-
-///////////////////////////////////////////////////////////////////////////////
-//
-// To use this component, the parent component or page must add the the redoc
-// script.  It can be done like this:
-//    useScript(
-//      'https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js',
-//      true,
-//      'redoc'
-//    )
-///////////////////////////////////////////////////////////////////////////////
+import RedocStandalone from './RedocStandalone'
 
 export interface OpenApiDocumentationProps {
   spec: OpenApi
 }
 
-interface Window {
-  Redoc: any
-}
-
-declare const window: Window
-
 export const OpenApiDocumentation: FC<OpenApiDocumentationProps> = ({
   spec,
 }: OpenApiDocumentationProps) => {
-  useEffect(() => {
-    window.Redoc.init(
-      spec,
-      {
-        noAutoAuth: true,
-        showExtensions: true,
-      },
-      document.getElementById('redoc-container'),
-    )
-  }, [spec])
-
   return (
-    <Box width="full">
-      <Box id="redoc-container" background="white" />
+    <Box width="full" paddingTop="containerGutter">
+      <RedocStandalone
+        spec={spec}
+        options={{
+          noAutoAuth: true,
+          showExtensions: true,
+        }}
+      />
     </Box>
   )
 }
---
+++ apps/web/components/OpenApiDocumentation/RedocStandalone.tsx
@@ -0,0 +1,9 @@
+import { RedocStandalone, MenuStore } from 'redoc'
+
+const originalActivate = MenuStore.prototype.activate
+
+MenuStore.prototype.activate = function (menuItem) {
+  originalActivate.apply(this, [menuItem, false, false])
+}
+
+export default RedocStandalone
