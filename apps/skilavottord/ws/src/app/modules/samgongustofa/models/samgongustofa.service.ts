ea60e69ad2d00b7720589a07afe0fba1777e608d
Sigurgeir Gudmundsson <essgje@gmail.com>
2021-02-22 17:28:48 +0000

    stolen and lock tjekk added

--- apps/skilavottord/ws/src/app/modules/samgongustofa/models/samgongustofa.service.ts
+++ apps/skilavottord/ws/src/app/modules/samgongustofa/models/samgongustofa.service.ts
@@ -227,45 +227,51 @@ export class SamgongustofaService {
                 )
                 .then(function (basicInfo) {
                   //  If there is any information in updatelocks, stolens, ownerregistrationerrors then we may not deregister it
-                  /*if (
-                    !(
-                      basicInfo['vehicle']['updatelocks'][0] == '' &&
-                      basicInfo['vehicle']['stolens'][0] == '' &&
-                      basicInfo['vehicle']['ownerregistrationerrors'][0] == ''
-                    )
-                  ) {
-                    newVehicleArr[i]['isRecyclable'] = false
-                  }*/
-                  // If there is any information in updatelocks and there is nothing in enddate (lock is acive)then we may not deregister it
                   if (
-                    !(basicInfo['vehicle']['updatelocks'][0] == '') &&
-                    basicInfo['vehicle']['updatelocks']['updatelock'][
-                      'enddate'
-                    ][0] == ''
+                    typeof basicInfo.vehicle.ownerregistrationerrors[0]
+                      .ownerregistrationerror != 'undefined'
                   ) {
-                    this.logger.info(
-                      `--- Car ${carObj['permno']} has updatelocks and no enddate - not recyclable  `,
+                    //Handle registrationerror
+                    loggerReplacement.info(
+                      'vehicle has ownerregistrationerrors',
                     )
-                    newVehicleArr[i]['isRecyclable'] = false
-                    // If there is any information in stolens and there is nothing in enddate (lock is acive)then we may not deregister it
-                  } else if (
-                    !(basicInfo['vehicle']['stolens'][0] == '') &&
-                    basicInfo['vehicle']['stolens']['stolen']['enddate'][0] ==
-                      ''
-                  ) {
-                    this.logger.info(
-                      `--- Car ${carObj['permno']} has been stolen and no enddate - not recyclable  `,
-                    )
-                    newVehicleArr[i]['isRecyclable'] = false
-                    // If there is any information in ownerregistrationerrors and there is nothing in enddate (lock is acive)then we may not deregister it
-                  } else if (
-                    !(basicInfo['vehicle']['ownerregistrationerrors'][0] == '')
-                  ) {
-                    this.logger.info(
-                      `--- Car ${carObj['permno']} has ownerregistrationerror and no enddate - not recyclable  `,
-                    )
-                    newVehicleArr[i]['isRecyclable'] = false
+                    newVehicleArr[i].isRecyclable = false
                   }
+                  if (
+                    //Handle stolen
+                    typeof basicInfo.vehicle.stolens[0].stolen != 'undefined'
+                  ) {
+                    for (let stolenEndDate of basicInfo.vehicle.stolens[0]
+                      .stolen) {
+                      if (!stolenEndDate.enddate[0].trim()) {
+                        loggerReplacement.info('vehicle is stolen')
+                        newVehicleArr[i].isRecyclable = false
+                        break
+                      }
+                    }
+                  }
+                  if (
+                    typeof basicInfo.vehicle.updatelocks[0].updatelock !=
+                    'undefined'
+                  ) {
+                    //Handle lock
+                    for (let lockEndDate of basicInfo.vehicle.updatelocks[0]
+                      .updatelock) {
+                      if (!lockEndDate.enddate[0].trim()) {
+                        loggerReplacement.info('vehicle is locked')
+                        newVehicleArr[i].isRecyclable = false
+                        break
+                      }
+                    }
+                  }
+                  if (newVehicleArr[i].isRecyclable) {
+                    loggerReplacement.info(
+                      'vehicle is clean. stolen, locked, registrationerror tjekked',
+                    )
+                  }
+                  loggerReplacement.info(
+                    'isRecycleble=' + newVehicleArr[i].isRecyclable,
+                  )
                   return newVehicleArr[i]
                 })
                 .catch(function (err) {
