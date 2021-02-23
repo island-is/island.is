15c9ee6fc84fa5bfd4140775e8fb382d0da91b26
Jérémy Barbet <jeremgraph@gmail.com>
2021-02-23 10:18:52 +0000

    fix: add escaped line breaks chars

--- libs/application/templates/children-residence-change/src/lib/messages/duration.ts
+++ libs/application/templates/children-residence-change/src/lib/messages/duration.ts
@@ -16,7 +16,7 @@ export const duration = {
     description: {
       id: 'crc.application:section.arrangement.duration.description#markdown',
       defaultMessage:
-        'Veldu í hversu langan tíma samningurinn á að gilda. Hægt er að gera tímabundna lögheimilsbreytingu til a.m.k. 6 mánaða eða lengur eða velja að samningur gildi til frambúðar.\n\nEkki er hægt að gera nýjan samning innan 6 mánaða frá gildistöku breytingar.\n\nAthugið að samningurinn tekur gildi þann dag sem sýslumaður staðfestir samninginn og getur þar af leiðandi ekki verið afturvirkur.\n\nSamningurinn verður staðfestur innan 14 daga.',
+        'Veldu í hversu langan tíma samningurinn á að gilda. Hægt er að gera tímabundna lögheimilsbreytingu til a.m.k. 6 mánaða eða lengur eða velja að samningur gildi til frambúðar.\\n\\nEkki er hægt að gera nýjan samning innan 6 mánaða frá gildistöku breytingar.\\n\\nAthugið að samningurinn tekur gildi þann dag sem sýslumaður staðfestir samninginn og getur þar af leiðandi ekki verið afturvirkur.\\n\\nSamningurinn verður staðfestur innan 14 daga.',
       description: 'Duration page description',
     },
   }),
