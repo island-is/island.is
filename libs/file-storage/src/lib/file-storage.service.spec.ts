9241da83202f9ed51dce3cf43ac0cbaf89e309e2
Eirikur Nilsson <eirikur@nilsson.is>
2021-02-23 10:04:29 +0000

    Fix FileStorage spec

--- libs/file-storage/src/lib/file-storage.service.spec.ts
+++ libs/file-storage/src/lib/file-storage.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test } from '@nestjs/testing'
 import { FileStorageService } from './file-storage.service'
+import { ConfigModule } from '@nestjs/config'
+import { fileStorageConfiguration } from './file-storage.configuration'
 
 describe('FileStorageService', () => {
   let service: FileStorageService
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports: [ConfigModule.forFeature(fileStorageConfiguration)],
       providers: [FileStorageService],
     }).compile()
 
