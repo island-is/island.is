5905c44fb442aeb452c0e6348f903618a6f3f52e
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-02-24 15:35:57 +0000

    Add requestFileSignature mutation

---
+++ libs/application/graphql/src/lib/mutations/requestFileSignature.ts
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client'
+
+export const REQUEST_FILE_SIGNATURE = gql`
+  mutation RequestFileSignature($input: RequestFileSignatureInput!) {
+    requestFileSignature(input: $input) {
+      attachments
+    }
+  }
+`
