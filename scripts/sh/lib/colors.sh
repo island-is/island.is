1489b46115979e8e0be4998894d9ecefbd85c5b5
Jón Levy <levy@andes.is>
2021-02-25 16:21:34 +0000

    added shellspec for sh script tests

---
+++ scripts/sh/lib/colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+NORMAL="\x1b[0m"
+BOLD="\x1b[1m"
+FAINT="\x1b[2m"
+ITALIC="\x1b[3m"
+UNDERLINE="\x1b[4m"
+BLINK_SLOW="\x1b[5m"
+BLINK_RAPID="\x1b[6m"
+INVERSE="\x1b[7m"
+CONCEAL="\x1b[8m"
+CROSSED_OUT="\x1b[9m"
+# TEXT colors.
+BLACK="\x1b[30m"
+RED="\x1b[31m"
+GREEN="\x1b[32m"
+YELLOW="\x1b[33m"
+BLUE="\x1b[34m"
+MAGENTA="\x1b[35m"
+CYAN="\x1b[36m"
+WHITE="\x1b[37m"
+# BACKGROUND colors.
+BG_BLACK="\x1b[40m"
+BG_RED="\x1b[41m"
+BG_GREEN="\x1b[42m"
+BG_YELLOW="\x1b[43m"
+BG_BLUE="\x1b[44m"
+BG_MAGENTA="\x1b[45m"
+BG_CYAN="\x1b[46m"
+BG_WHITE="\x1b[47m"
+# RESETS
+NOSTYLE="\x1b[0m"
+NOUNDERLINE="\x1b[24m"
+NOINVERSE="\x1b[27m"
+NOCOLOR="\x1b[39m"
---
+++ scripts/spec/secrets_spec.sh
@@ -0,0 +1,80 @@
+#shellcheck shell=sh disable=SC2016
+
+Describe 'create-scripts tests'
+  Include sh/create-secret.sh
+  Describe 'Secrets'
+    It 'validates output from error function'
+      When call __error_exit 'this broke' # this is evaluation
+      # The output should eq "[ERROR]: this broke"
+      The status should be failure
+    End
+    It 'validates string does not end with slash'
+      When call validate_slash '/k8s/some/secret/' # this is evaluation
+      The status should be failure
+    End
+
+  #   It 'calls external command also'
+  #     When call expr 1 + 2
+  #     The output should eq 3
+  #   End
+
+  #   It 'calls the defined function instead of external command that same name'
+  #     expr() { echo "be called"; }
+  #     When call expr 1 + 2
+  #     The output should eq "be called"
+  #   End
+
+  #   It 'must be one call each example'
+  #     When call echo 1
+  #     When call echo 2 # can not be called more than once.
+  #     The output should eq 1
+  #   End
+
+  #   It 'not calling is allowed'
+  #     The value 123 should eq 123
+  #   End
+
+  #   It 'can not be called after expectation'
+  #     The value 123 should eq 123
+  #     When call echo 1 # can not be called after expectation.
+  #   End
+
+  #   It 'calls external command'
+  #     expr() { echo "not called"; }
+  #     When run command expr 1 + 2
+  #     The output should eq 3
+  #   End
+  # End
+
+  # Describe 'run evaluation'
+  #   It 'can trap exit'
+  #     abort() { exit 1; }
+  #     When run abort # if use "call evaluation", shellspec is terminate
+  #     The status should be failure
+  #   End
+
+  #   It 'can not modify variable because it run with in subshell'
+  #     set_value() { SHELLSPEC_VERSION=$1; }
+  #     When run set_value 'no-version'
+  #     The value "$SHELLSPEC_VERSION" should not eq 'no-version'
+  #   End
+
+  #   It 'calls BeforeRun/AfterRun hook'
+  #     before_run() {
+  #       # You can temporary redefine function here
+  #       # redefined function is restored after run evaluation
+  #       # because run evaluation runs with in subshell
+  #       echo before
+  #     }
+  #     after_run() {
+  #       echo after
+  #     }
+  #     BeforeRun before_run
+  #     AfterRun after_run
+  #     When run echo 123
+  #     The line 1 of output should eq 'before'
+  #     The line 2 of output should eq 123
+  #     The line 3 of output should eq 'after'
+  #   End
+  End
+End
