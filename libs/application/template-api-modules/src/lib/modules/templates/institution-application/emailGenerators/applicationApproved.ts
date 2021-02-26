321601f2d3abd1f9534ba68d358ab9dcad977bcf
Olafur <obmagnusson@gmail.com>
2021-02-26 14:46:01 +0000

    attachment list sent with email

--- libs/application/template-api-modules/src/lib/modules/templates/institution-application/emailGenerators/applicationApproved.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/institution-application/emailGenerators/applicationApproved.ts
@@ -2,7 +2,14 @@ import { dedent } from 'ts-dedent'
 import get from 'lodash/get'
 
 import { EmailTemplateGenerator } from '../../../../types'
-import { m } from '@island.is/application/templates/institution-application'
+import { messages } from '@island.is/application/templates/institution-application'
+import mapValues from 'lodash/mapValues'
+
+
+export interface nodemailAttachment {
+  filename: string
+  href: string
+}
 
 export const generateApplicationEmail: EmailTemplateGenerator = (
   props,
@@ -23,13 +30,23 @@ export const generateApplicationEmail: EmailTemplateGenerator = (
   const projectFinance = get(application.answers, 'project.finance')
   const projectBackground = get(application.answers, 'project.background')
 
+  const hasTime = get(application.answers, 'constraints.hasTime') as boolean
+  const hasMoral = get(application.answers, 'constraints.hasMoral') as boolean
+  const hasOther = get(application.answers, 'constraints.hasOther') as boolean
+  const hasShopping = get(application.answers, 'constraints.hasShopping') as boolean
+  const hasFinancial = get(application.answers, 'constraints.hasFinancial') as boolean
+  const hasTechnical = get(application.answers, 'constraints.hasTechnical') as boolean
+
+  const noConstraints = [hasTime, hasMoral, hasOther, hasShopping, hasFinancial, hasTechnical].every((x) => !x)
+
   console.log(application.answers)
-  console.log('applicant Email', applicantEmail)
-  // TODO translate using locale
-  const subject =
-    locale === 'is'
-      ? 'Umsókn samþykkt: ReferenceTemplate'
-      : 'Application approved: Reference Template'
+  const subject = `Umsókn frá ${institutionApplicant}`
+  const attachments = get(application.answers, 'attatchments') as []
+  const mailAttachments = attachments.map(
+    (attachment) => <nodemailAttachment>{
+      filename: get(attachment, 'name'),
+      href: `${get(attachment, 'url')}/${get(attachment, 'key')}`
+    })
 
   const secondaryContact = ``
 
@@ -37,17 +54,49 @@ export const generateApplicationEmail: EmailTemplateGenerator = (
     dedent(`
         Umsókn hefur send verið inn frá ${institutionApplicant}.
 
-        # Nafn ${applicantName}
+        # Nafn  ${applicantName}
         # Tölvupóstfang ${applicantEmail}
-        # Sími ${applicantPhone}
+        # Sími  ${applicantPhone}
 
-        Nafn verkefnis:
+        Verkefni
+
+        ${messages.projectName.defaultMessage}:
         ${projectName}
 
-        Bakgrunnur verkefnis:
+        ${messages.projectBackground.defaultMessage}:
         ${projectBackground}
 
-        Markmið verkefnis ávinningur og markhópur
+        ${messages.projectGoals.defaultMessage}:
+        ${projectGoal}
+
+        ${messages.projectFinance.defaultMessage}:
+        ${projectFinance}
+
+        ${messages.projectScope.defaultMessage}:
+        ${projectScope}
+
+
+        ${noConstraints ? 'Engar skorður skilgreindar.' : 'Skorður:'}
+
+        ${hasTechnical ?
+        `Tæknilegar skorður
+          ${get(application.answers, 'constraints.technical')}` : ''}
+        ${hasFinancial ?
+        `Fjárhagslegar skorður
+          ${get(application.answers, 'constraints.financial')}` : ''}
+        ${hasTime ?
+        `Tímaskorður
+          ${get(application.answers, 'constraints.time')}` : ''}
+        ${hasShopping ?
+        `Innkaupa skorður
+          ${get(application.answers, 'constraints.shopping')}` : ''}
+        ${hasMoral ?
+        `Siðferðilegar skorður
+          ${get(application.answers, 'constraints.moral')}` : ''}
+        ${hasOther ?
+        `Aðrar skorður
+          ${get(application.answers, 'constraints.other')}` : ''}
+
 
       `)
 
@@ -62,6 +111,7 @@ export const generateApplicationEmail: EmailTemplateGenerator = (
         address: applicantEmail as string,
       },
     ],
+    attachments: mailAttachments,
     subject,
     html: `<p>${body
       .split('')
