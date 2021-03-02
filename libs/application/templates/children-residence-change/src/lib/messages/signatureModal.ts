3fafb880044250467f2a357d4429a5b62ca382c9
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-03-02 10:50:43 +0000

    Change translation string ids

--- libs/application/templates/children-residence-change/src/lib/messages/signatureModal.ts
+++ libs/application/templates/children-residence-change/src/lib/messages/signatureModal.ts
@@ -4,25 +4,24 @@ import { defineMessages } from 'react-intl'
 export const signatureModal = {
   general: defineMessages({
     title: {
-      id: 'crc.application:section.contract.signatureModal.title',
+      id: 'crc.application:signatureModal.title',
       defaultMessage: 'Rafræn undirritun',
       description: 'Signature modal title',
     },
     closeButtonLabel: {
-      id: 'crc.application:section.contract.signatureModal.closeButtonLabel',
+      id: 'crc.application:signatureModal.closeButtonLabel',
       defaultMessage: 'Loka',
       description: 'Close modal button label',
     },
   }),
   security: defineMessages({
     numberLabel: {
-      id:
-        'crc.application:section.contract.signatureModal.security.numberLabel',
+      id: 'crc.application:signatureModal.security.numberLabel',
       defaultMessage: 'Öryggistala:',
       description: 'Signature security number label',
     },
     message: {
-      id: 'crc.application:section.contract.signatureModal.security.text',
+      id: 'crc.application:signatureModal.security.text',
       defaultMessage:
         'Þetta er ekki pin-númerið. Staðfestu aðeins innskráningu ef sama öryggistala birtist í símanum þínum.',
       description: 'Signature security text',
@@ -30,13 +29,12 @@ export const signatureModal = {
   }),
   defaultError: defineMessages({
     title: {
-      id: 'crc.application:section.contract.signatureModal.defaultError.title',
+      id: 'crc.application:signatureModal.defaultError.title',
       defaultMessage: 'Villa kom upp við undirritun',
       description: 'Signature default error title',
     },
     message: {
-      id:
-        'crc.application:section.contract.signatureModal.defaultError.message',
+      id: 'crc.application:signatureModal.defaultError.message',
       defaultMessage:
         'Það hefur eitthvað farið úrskeiðis við undirritun, vinsamlegast reynið aftur.',
       description: 'Signature default error message',
