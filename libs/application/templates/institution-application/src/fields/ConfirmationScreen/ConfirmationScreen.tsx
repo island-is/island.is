ed0308aaf7e5896db828ef42dc7d4641e192f67a
Andri Andresson <andri@sendiradid.is>
2021-03-02 12:32:37 +0000

    wip: updated confirmationScreen

--- libs/application/templates/institution-application/src/fields/ConfirmationScreen/ConfirmationScreen.tsx
+++ libs/application/templates/institution-application/src/fields/ConfirmationScreen/ConfirmationScreen.tsx
@@ -1,55 +1,36 @@
 import React, { FC } from 'react'
 import { FieldBaseProps, formatText } from '@island.is/application/core'
-import { Box, Bullet, BulletList, Stack, Text } from '@island.is/island-ui/core'
+import { Box, Bullet, BulletList, Stack, Link } from '@island.is/island-ui/core'
 import { useLocale } from '@island.is/localization'
-import { m } from '../../forms/messages'
-import Markdown from 'markdown-to-jsx'
-// import ManOnBenchIllustration from '../../assets/ManOnBenchIllustration'
+import InstitutionIllustration from '../../assets/InstitutionIllustration'
 
 const ConfirmationScreen: FC<FieldBaseProps> = ({ field, application }) => {
   const { formatMessage } = useLocale()
 
-  const isMissingInfoConfirmation =
-    field.id === 'successfulSubmissionMissingInfo'
-
-  // const pageTitle = isMissingInfoConfirmation
-  //   ? m.successfulSubmissionMissingInfoTitle
-  //   : m.successfulSubmissionTitle
-
   return (
-    <Stack space={2}>
-      <Text variant="h2">
-        {formatText('pageTitle', application, formatMessage)}
-      </Text>
-      <Text>
-        {/* <Markdown>
-          {formatText(
-            () => ({
-              ...m.successfulSubmissionMessage,
-              values: { applicationNumber: application.id },
-            }),
-            application,
-            formatMessage,
-          )}
-        </Markdown> */}
-      </Text>
-      <Box display="flex" justifyContent="center" paddingY={2} size={1}>
-        {/* <ManOnBenchIllustration /> */}
-      </Box>
-      <Box marginBottom={6}>
-        <h1>Bingo Dingo</h1>
-        {/* <BulletList>
-          {!isMissingInfoConfirmation && (
-            <Bullet>
-              {formatText(m.nextStepReviewTime, application, formatMessage)}
-            </Bullet>
-          )}
+    <Box marginTop={3}>
+      <Stack space={6}>
+        <BulletList>
           <Bullet>
-            {formatText(m.nextStepStatusCheck, application, formatMessage)}
+            Við munum nú fara yfir verkefnið og við sendum á þig svör innan
+            tíðar.{' '}
           </Bullet>
-        </BulletList> */}
-      </Box>
-    </Stack>
+          <Bullet>
+            Við verðum í sambandi ef okkur vantar frekari upplýsingar.{' '}
+          </Bullet>
+          <Bullet>
+            Ef þú þarft frekari upplýsingar þá getur þú haft samband í síma 847
+            3759 eða á netfangið{' '}
+            <Link href="mailto:island@island.is" color="blue400">
+              island@island.is
+            </Link>
+          </Bullet>
+        </BulletList>
+        <Box display="flex" justifyContent="center" size={1}>
+          <InstitutionIllustration />
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
