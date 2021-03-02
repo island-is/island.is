b5ba02a8e0af15496932b9613913c0d1b44292c1
qdd1 <6514088+qdd1@users.noreply.github.com>
2021-03-02 12:08:57 +0000

    Health insurance move application under templates (#3079)
    * health-insurance - initialise under libs folder
    
    * health-insurance - lint, test, format
    
    * health-insurance - yarn format
    
    * health-insurance - format
    
    Co-authored-by: qdong <qdong@deloitte.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.module.ts
@@ -1,4 +1,5 @@
 import { DynamicModule } from '@nestjs/common'
+import { HealthInsuranceModule as HealthInsuranceMod } from '@island.is/health-insurance'
 
 // This is a shared module that gives you access to common methods
 import { SharedTemplateAPIModule } from '../../shared'
@@ -10,11 +11,27 @@ import { BaseTemplateAPIModuleConfig } from '../../../types'
 // Here you import your module service
 import { HealthInsuranceService } from './health-insurance.service'
 
+const XROAD_HEALTH_INSURANCE_WSDL_URL = process.env.healthInsurancewsdlUrl ?? ''
+const XROAD_HEALTH_INSURANCE_BASE_URL =
+  process.env.baseUrl ?? 'http://localhost:8080'
+const XROAD_HEALTH_INSURANCE_USERNAME =
+  process.env.healthInsuranceUsername ?? ''
+const XROAD_HEALTH_INSURANCE_PASSWORD =
+  process.env.healthInsurancePassword ?? ''
+
 export class HealthInsuranceModule {
   static register(config: BaseTemplateAPIModuleConfig): DynamicModule {
     return {
       module: HealthInsuranceModule,
-      imports: [SharedTemplateAPIModule.register(config)],
+      imports: [
+        HealthInsuranceMod.register({
+          wsdlUrl: XROAD_HEALTH_INSURANCE_WSDL_URL,
+          baseUrl: XROAD_HEALTH_INSURANCE_BASE_URL,
+          username: XROAD_HEALTH_INSURANCE_USERNAME,
+          password: XROAD_HEALTH_INSURANCE_PASSWORD,
+        }),
+        SharedTemplateAPIModule.register(config),
+      ],
       providers: [HealthInsuranceService],
       exports: [HealthInsuranceService],
     }
