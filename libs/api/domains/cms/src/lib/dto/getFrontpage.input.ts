9f3e9575c5568e5b9438ef0da87cdbf0ad83e1b7
kksteini <steini@kosmosogkaos.is>
2021-03-03 10:13:59 +0000

    Strict applied to the new incoming frontpage.model

--- libs/api/domains/cms/src/lib/dto/getFrontpage.input.ts
+++ libs/api/domains/cms/src/lib/dto/getFrontpage.input.ts
@@ -5,9 +5,9 @@ import { IsString } from 'class-validator'
 export class GetFrontpageInput {
   @Field()
   @IsString()
-  pageIdentifier: string
+  pageIdentifier!: string
 
   @Field()
   @IsString()
-  lang: string
+  lang: string = 'is-IS'
 }
