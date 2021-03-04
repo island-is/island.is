d2846ea736778d2d9e3dc66c3af6fa27dd489c15
Andri Andresson <andriandresson@gmail.com>
2021-03-04 09:30:54 +0000

    feat: fieldDescription rework

--- libs/application/templates/institution-application/src/fields/FieldDescription/FieldDescription.tsx
+++ libs/application/templates/institution-application/src/fields/FieldDescription/FieldDescription.tsx
@@ -1,28 +1,28 @@
 import React, { FC } from 'react'
 import { Stack, Box, Text } from '@island.is/island-ui/core'
-import { FieldBaseProps } from '@island.is/application/core'
+import { FieldBaseProps, formatText } from '@island.is/application/core'
+import { MessageDescriptor } from 'react-intl'
+import { useLocale } from '@island.is/localization'
 
 type FieldDescriptionProps = FieldBaseProps & {
   field: {
     props?: {
-      title?: string
-      subTitle?: string
-      description?: string
+      subTitle?: MessageDescriptor
+      description?: MessageDescriptor
     }
   }
 }
 
-const FieldDescription: FC<FieldDescriptionProps> = ({ field }) => {
+const FieldDescription: FC<FieldDescriptionProps> = ({ field, application }) => {
+  const { formatMessage } = useLocale()
+
   const { props = {} } = field
 
   return (
-    <Box marginBottom={2} marginTop={5}>
-      <Stack space={1}>
-        {props.title && <Text variant="h2">{props.title}</Text>}
-        {props.subTitle && <Text variant="h4">{props.subTitle}</Text>}
-        {props.description && <Text>{props.description}</Text>}
-      </Stack>
-    </Box>
+    <Stack space={1}>
+      {props.subTitle && <Box marginTop={5}><Text variant="h4">{formatText(props.subTitle, application, formatMessage)}</Text></Box>}
+      {props.description && <Box marginBottom={2}><Text>{formatText(props.description, application, formatMessage)}</Text></Box>}
+    </Stack>
   )
 }
 
