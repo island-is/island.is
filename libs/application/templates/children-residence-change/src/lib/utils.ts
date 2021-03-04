bf4cc986de39607fd1063188f9ea0fe1d02995ae
Vilhjálmur Ari Gunnarsson <villi@kolibri.is>
2021-03-04 15:46:01 +0000

    feature(children-residence-change): mock data screen (#3129)
    * Fix default value
    
    * Add a very rough version of the mock data screen
    
    * Extract mockdata if available
    
    * Opt in to use mock data
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/children-residence-change/src/lib/utils.ts
+++ libs/application/templates/children-residence-change/src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Application } from '@island.is/application/core'
+import { Application, FormValue } from '@island.is/application/core'
 import { NationalRegistryUser } from '@island.is/api/schema'
 import {
   PersonResidenceChange,
@@ -11,14 +11,25 @@ export const extractApplicantFromApplication = (application: Application) => {
   }) as NationalRegistryUser
 }
 
+const dataToUse = ({ answers, externalData }: Application, key: string) => {
+  const mockData = ((answers.mockData as FormValue)?.[key] as FormValue)?.data
+  const data = externalData[key]?.data
+  if (answers.useMocks === 'no') {
+    return data
+  }
+  return mockData || data
+}
+
 export const extractParentFromApplication = (application: Application) => {
-  return (application.externalData.parentNationalRegistry?.data as {
+  const data = dataToUse(application, 'parentNationalRegistry')
+  return (data as {
     parent?: object
   }) as ParentResidenceChange
 }
 
 export const extractChildrenFromApplication = (application: Application) => {
-  return (application.externalData.childrenNationalRegistry?.data as {
+  const data = dataToUse(application, 'childrenNationalRegistry')
+  return (data as {
     registeredChildren?: object
   }) as PersonResidenceChange[]
 }
