baab736624adc49211f7b164aa0ebb01a5e3e442
unnarb <unnarb@gmail.com>
2021-03-04 09:51:37 +0000

    Format code

--- libs/auth-api-lib/migrations/20210303154058-api-scope-delegation-fields.js
+++ libs/auth-api-lib/migrations/20210303154058-api-scope-delegation-fields.js
@@ -30,7 +30,10 @@ module.exports = {
     return Promise.all([
       queryInterface.removeColumn('api_scope', 'grant_to_legal_guardians'),
       queryInterface.removeColumn('api_scope', 'grant_to_procuring_holders'),
-      queryInterface.removeColumn('api_scope', 'allow_explicit_delegation_grant'),
+      queryInterface.removeColumn(
+        'api_scope',
+        'allow_explicit_delegation_grant',
+      ),
       queryInterface.removeColumn('api_scope', 'automatic_delegation_grant'),
       queryInterface.removeColumn('api_scope', 'also_for_delegated_user'),
     ])
