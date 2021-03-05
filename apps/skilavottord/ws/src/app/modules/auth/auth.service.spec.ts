85350b5a2dfd58be86b314af94cf8bcd5791ff0b
Sigurgeir Gudmundsson <essgje@gmail.com>
2021-03-05 13:14:04 +0000

    userRole test added

--- apps/skilavottord/ws/src/app/modules/auth/auth.service.spec.ts
+++ apps/skilavottord/ws/src/app/modules/auth/auth.service.spec.ts
@@ -1,61 +1,140 @@
 import { Test } from '@nestjs/testing'
 import { AuthService } from './auth.service'
+import { logger, LOGGER_PROVIDER } from '@island.is/logging'
 
+const userList = [
+  {
+    nationalId: '1111111111',
+    name: 'Gerfimaður-1',
+    role: 'developer',
+    partnerId: '',
+  },
+  {
+    nationalId: '2222222222',
+    name: 'Gerfimaður-2',
+    role: 'developer',
+    partnerId: '',
+  },
+  {
+    nationalId: '3333333333',
+    name: 'Gerfimaður-3',
+    role: 'developer',
+    partnerId: '',
+  },
+  {
+    nationalId: '4444444444',
+    name: 'Gerfimaður-4',
+    role: 'recyclingFund',
+    partnerId: '',
+  },
+  {
+    nationalId: '5555555555',
+    name: 'xxxxxxxxxxxxxxx',
+    role: 'recyclingFund',
+    partnerId: '',
+  },
+  {
+    nationalId: '6666666666',
+    name: 'xxxxxxxxxx',
+    role: 'recyclingCompany',
+    partnerId: '999',
+  },
+]
 describe('skilavottordApiTest', () => {
   it('should work', () => {
     expect(AuthService.test()).toEqual('test')
   })
 
-  // describe('AuthService', () => {
-  //   let authService: AuthService
+  describe('AuthService', () => {
+    let authService: AuthService
 
-  //   const user1 = {
-  //     nationalId: '1111111111',
-  //     name: 'Gervimaður',
-  //     mobile: '',
-  //     role: 'developer',
-  //   }
+    beforeEach(async () => {
+      const moduleRef = await Test.createTestingModule({
+        providers: [
+          AuthService,
+          {
+            provide: LOGGER_PROVIDER,
+            useValue: logger,
+          },
+        ],
+      }).compile()
+      authService = moduleRef.get(AuthService)
+      const httpServiceSpy = jest
+        .spyOn(JSON, 'parse')
+        .mockImplementation(() => userList)
+    })
+    describe('getRole test', () => {
+      it('should return a correct user with role', () => {
+        const userx = {
+          nationalId: '1111111111',
+          name: 'Gervimaður-developer',
+          mobile: '',
+          role: 'developer',
+        }
+        const ur = authService.getUserRole(userx)
+        expect(ur.role).toBe('developer')
+      })
+    })
 
-  //   const user2 = {
-  //     nationalId: '2222222222',
-  //     name: 'Gervimaður',
-  //     mobile: '',
-  //     role: '',
-  //   }
+    describe('getUserRole test', () => {
+      it('should return a correct user with role', () => {
+        const userx = {
+          nationalId: '1111111111',
+          name: 'Gervimaður-developer',
+          mobile: '',
+          role: '',
+        }
+        const ur = authService.getUserRole(userx)
+        expect(ur.role).toBe('developer')
+      })
+      it('should return a correct user with role', () => {
+        const userx = {
+          nationalId: '8888888888',
+          name: 'Gervimaður-citizen',
+          mobile: '',
+          role: '',
+        }
+        const ur = authService.getUserRole(userx)
+        expect(ur.role).toBe('citizen')
+      })
+      // it('undefined should return citizen', () => {
+      //   const userx = undefined
+      //   const ur = authService.getUserRole(userx)
+      //   expect(ur.role).toBe('citizen')
+      // })
+    })
 
-  //   const userList = `[{ "nationalId":"2310765229", "name": "xxxxxxxxxxxxxxxxxx", "role":"developer", "partnerId":""},
-  // { "nationalId":"1111111111", "name": "xxxxxxxxxxxxxxxxxxx", "role":"developer", "partnerId":""},
-  // { "nationalId":"1111111111","name": "xxxxxxxxxxxxxxxxxxxxxxxxxx", "role":"developer", "partnerId":""},
-  // { "nationalId":"1111111111","name": "xxxxxxxxxxxxxxxxxxxxxxxxxx", "role":"recyclingFund", "partnerId":""},
-  // { "nationalId":"1111111111","name": "xxxxxxxxxxxxxxx", "role":"recyclingFund", "partnerId":""},
-  // { "nationalId":"1111111111","name": "xxxxxxxxxx", "role":"recyclingCompany", "partnerId":"999"}
-  // ]`
-  //   beforeEach(async () => {
-  //     const moduleRef = await Test.createTestingModule({
-  //       providers: [AuthService],
-  //     }).compile()
-
-  //     authService = moduleRef.get(AuthService)
-  //   })
-
-  //   describe('getRole test', () => {
-  //     it('should return a correct user with role', () => {
-  //       const user = user1
-  //       const ur = authService.getUserRole(user)
-  //       expect(ur.role).toBe('recyclingCompany')
-  //     })
-  //   })
-
-  //   describe('checkRole test ', () => {
-  //     it('allways return false for citizen', () => {
-  //       // Arrange & Act
-  //       const hasPermission = authService.checkRole(user2, 'citizen')
-  //       expect(hasPermission).toBeFalsy()
-  //     })
-  //     it('allways return true for other than citizen', () => {
-  //       // Arrange & Act
-  //       const hasPermission = authService.checkRole(user1, 'developer')
-  //       expect(hasPermission).toBeTruthy()
-  //     })
-  //   })
+    describe('checkRole test ', () => {
+      it('allways return false for citizen', () => {
+        const userx = {
+          nationalId: '8888888888',
+          name: 'Gervimaður-1',
+          mobile: '',
+          role: 'citizen',
+        }
+        const hasPermission = authService.checkRole(userx, 'citizen')
+        expect(hasPermission).toBeFalsy()
+      })
+      it('nationalId not in userList, return false', () => {
+        const userx = {
+          nationalId: '8888888888',
+          name: 'Gervimaður-1',
+          mobile: '',
+          role: 'developer',
+        }
+        let hasPermission = authService.checkRole(userx, 'citizen')
+        expect(hasPermission).toBeFalsy()
+      })
+      it('allways return true for other than citizen', () => {
+        const userx = {
+          nationalId: '1111111111',
+          name: 'Gervimaður-1',
+          mobile: '',
+          role: 'developer',
+        }
+        const hasPermission = authService.checkRole(userx, 'developer')
+        expect(hasPermission).toBeTruthy()
+      })
+    })
+  })
 })
--- apps/skilavottord/ws/src/app/modules/auth/auth.service.ts
+++ apps/skilavottord/ws/src/app/modules/auth/auth.service.ts
@@ -3,52 +3,79 @@ import { environment } from '../../../environments'
 import { User } from '../user'
 import { Role, AuthUser } from './auth.types'
 
+const { userList } = environment.skilavottord
 @Injectable()
 export class AuthService {
-  roleArr: [User]
-  constructor() {
-    const { userList } = environment.skilavottord
-    this.roleArr = JSON.parse(userList)
-  }
+  constructor() {}
   // TODO
   getRole(user: AuthUser): Role {
-    const picked = this.roleArr.find((o) => o.nationalId === user.nationalId)
-    //TODO if not found
-    return picked.role as Role
+    try {
+      // this.logger.info('getRole start...')
+      const ulist = JSON.parse(userList)
+      const picked = ulist.find((o) => o.nationalId === user.nationalId)
+      //TODO if not found
+      // this.logger.info('getRole return value:' + picked.role)
+      return picked.role as Role
+    } catch (error) {
+      // this.logger.error('getRole exception:' + error.message)
+      console.log('getRole exception:' + error.message)
+    }
   }
-
-  // //TODO
-  // getUserRoleNew(user: AuthUser): User {
-  //   const picked = this.roleArr.find((o) => o.nationalId === user.nationalId)
-  //   let curruser = new User()
-  //   // curruser.nationalId = user.nationalId
-  //   if (!picked) {
-  //     // curruser.role = 'citizen'
-  //   } else {
-  //     // curruser.role = picked.role
-  //     // curruser.partnerId = picked.partnerId
-  //   }
-  //   return null
-  // }
 
   // get user role
   getUserRole(user: AuthUser): User {
-    const picked = this.roleArr.find((o) => o.nationalId === user.nationalId)
-    return picked
+    try {
+      // this.logger.info('getUserRole start...')
+      const ulist = JSON.parse(userList)
+      const picked = ulist.find((o) => o.nationalId === user.nationalId)
+      if (!picked) {
+        let u = user as User
+        u.role = 'citizen'
+        return u
+      } else {
+        return picked
+      }
+    } catch (error) {
+      // this.logger.error('getUserRole exception.' + error.message)
+      console.log('getUserRole exception.' + error.message)
+    }
   }
 
   // check role
+  // checkRole(user: AuthUser, role: Role): boolean {
+  //   const picked = this.roleArr.find((o) => o.nationalId === user.nationalId)
+  //   if (!picked) {
+  //     return false
+  //   } else {
+  //     const r = picked.role as Role
+  //     return (
+  //       r === 'developer' || r === 'recyclingCompany' || r === 'recyclingFund'
+  //     )
+  //   }
+  //   return false
+  // }
+  // check role
   checkRole(user: AuthUser, role: Role): boolean {
-    const picked = this.roleArr.find((o) => o.nationalId === user.nationalId)
-    if (!picked) {
+    // this.logger.info('checkRole for user start...')
+    try {
+      const ulist = JSON.parse(userList)
+      const picked = ulist.find((o) => o.nationalId === user.nationalId)
+      if (!picked) {
+        // this.logger.info('checkRole user not found in userList')
+        return false
+      } else {
+        const r = picked.role as Role
+        // this.logger.info('checkRole user found in userList. user role:' + r)
+        return (
+          r === 'developer' || r === 'recyclingCompany' || r === 'recyclingFund'
+        )
+      }
+      // this.logger.info('checkRole return default false')
       return false
-    } else {
-      const r = picked.role as Role
-      return (
-        r === 'developer' || r === 'recyclingCompany' || r === 'recyclingFund'
-      )
+    } catch (err) {
+      // this.logger.error('checkRole exeption in auth.service:' + err.message)
+      return false
     }
-    return false
   }
 
   // test
