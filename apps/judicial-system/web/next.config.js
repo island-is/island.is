bdc9d0390efc4b964d6336333981db481d407783
Ívar Oddsson <ivaroddsson@gmail.com>
2021-03-05 14:05:09 +0000

    Fix format

--- apps/judicial-system/web/next.config.js
+++ apps/judicial-system/web/next.config.js
@@ -8,32 +8,31 @@ const {
 const graphqlPath = '/api/graphql'
 
 module.exports = withTreat({
-    webpack: (config, options) => {
-      // if (!options.isServer) {
-      //   config.resolve.alias['@sentry/node'] = '@sentry/browser'
-      // }
-      return config
-    },
-    serverRuntimeConfig: {
-      // Will only be available on the server side
-      apiUrl: API_URL,
-      graphqlEndpoint: graphqlPath,
-    },
-    publicRuntimeConfig: {
-      // Will be available on both server and client
-      apiUrl: `${WEB_PUBLIC_URL}${API_PATH}`,
-      graphqlEndpoint: `${API_PATH}${graphqlPath}`,
-    },
-    env: {
-      API_MOCKS: process.env.API_MOCKS || '',
-    },
-    publicRuntimeConfig: {
-      // Will be available on both server and client
-      graphqlUrl: '',
-      graphqlEndpoint: graphqlPath,
-    },
-    devIndicators: {
-      autoPrerender: false,
-    },
+  webpack: (config, options) => {
+    // if (!options.isServer) {
+    //   config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    // }
+    return config
   },
-)
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiUrl: API_URL,
+    graphqlEndpoint: graphqlPath,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiUrl: `${WEB_PUBLIC_URL}${API_PATH}`,
+    graphqlEndpoint: `${API_PATH}${graphqlPath}`,
+  },
+  env: {
+    API_MOCKS: process.env.API_MOCKS || '',
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    graphqlUrl: '',
+    graphqlEndpoint: graphqlPath,
+  },
+  devIndicators: {
+    autoPrerender: false,
+  },
+})
