f0f2b2e9125d02c08c2f1c97d5e2d897e43b6078
essgje <essgje@gmail.com>
2021-03-07 11:43:56 +0000

    env skrá uppfærð

--- apps/skilavottord/ws/src/environments/environment.ts
+++ apps/skilavottord/ws/src/environments/environment.ts
@@ -10,22 +10,33 @@ export default {
     jwtSecret: 'securesecret',
   },
   samgongustofa: {
-    soapUrl: process.env.SAMGONGUSTOFA_SOAP_URL,
-    soapUsername: process.env.SAMGONGUSTOFA_SOAP_USER,
-    soapPassword: process.env.SAMGONGUSTOFA_SOAP_PASS,
-    restAuthUrl: process.env.SAMGONGUSTOFA_REST_AUTH_URL,
-    restDeRegUrl: process.env.SAMGONGUSTOFA_REST_DEREG_URL,
-    restUsername: process.env.SAMGONGUSTOFA_REST_USER,
-    restPassword: process.env.SAMGONGUSTOFA_REST_PASS,
-    restReportingStation: process.env.SAMGONGUSTOFA_REST_REPORTING_STATION,
+    soapUrl:
+      process.env.SAMGONGUSTOFA_SOAP_URL ??
+      'https://test-xml.samgongustofa.is/scripts/WebObjects.dll/XML.woa/1/ws/.USXMLWS',
+    soapUsername: process.env.SAMGONGUSTOFA_SOAP_USER ?? 'DeloitteTest',
+    soapPassword: process.env.SAMGONGUSTOFA_SOAP_PASS ?? 'MajoneS55',
+    restAuthUrl:
+      process.env.SAMGONGUSTOFA_REST_AUTH_URL ??
+      'https://test-api.samgongustofa.is/vehicle/registrations/authenticate',
+    restDeRegUrl:
+      process.env.SAMGONGUSTOFA_REST_DEREG_URL ??
+      'https://test-api.samgongustofa.is/vehicle/registrations/deregistration',
+    restUsername: process.env.SAMGONGUSTOFA_REST_USER ?? 'DELOITTE.AFSKRA',
+    restPassword: process.env.SAMGONGUSTOFA_REST_PASS ?? 'FlugaaFramrudu',
+    restReportingStation:
+      process.env.SAMGONGUSTOFA_REST_REPORTING_STATION ?? '',
   },
   fjarsysla: {
-    restUrl: process.env.FJARSYSLA_REST_URL,
-    restUsername: process.env.FJARSYSLA_REST_USER,
-    restPassword: process.env.FJARSYSLA_REST_PASS,
+    restUrl:
+      process.env.FJARSYSLA_REST_URL ??
+      'https://tbrws-s.hysing.is/restv2/LeggjaASkilagjald',
+    restUsername: process.env.FJARSYSLA_REST_USER ?? 'fjs-samgong-p',
+    restPassword: process.env.FJARSYSLA_REST_PASS ?? 'katla.123',
   },
   skilavottord: {
-    userList: process.env.SKILAVOTTORD_USER_LIST ?? '[]',
+    userList:
+      process.env.SKILAVOTTORD_USER_LIST ??
+      '[{"nationalId":"0301665909","name":"Sigurgeir Guðmundsson","role":"developer","partnerId":""},{"nationalId":"2811638099","name":"Tómas Árni Jónsson","role":"developer","partnerId":""}]',
   },
   backendUrl: 'http://localhost:3333',
-}
+ }
