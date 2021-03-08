0c1fbf401cacd3cc33cbaf3b1246e01b0804b46f
essgje <essgje@gmail.com>
2021-03-08 11:06:55 +0000

    remove sensetive data in env

--- apps/skilavottord/ws/src/environments/environment.ts
+++ apps/skilavottord/ws/src/environments/environment.ts
@@ -12,33 +12,33 @@ export default {
   samgongustofa: {
     soapUrl:
       process.env.SAMGONGUSTOFA_SOAP_URL ??
-      'https://test-xml.samgongustofa.is/scripts/WebObjects.dll/XML.woa/1/ws/.USXMLWS',
-    soapUsername: process.env.SAMGONGUSTOFA_SOAP_USER ?? 'DeloitteTest',
-    soapPassword: process.env.SAMGONGUSTOFA_SOAP_PASS ?? 'MajoneS55',
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    soapUsername: process.env.SAMGONGUSTOFA_SOAP_USER ?? 'xxxxxxxxxxxx',
+    soapPassword: process.env.SAMGONGUSTOFA_SOAP_PASS ?? 'xxxxxxxxx',
     restAuthUrl:
       process.env.SAMGONGUSTOFA_REST_AUTH_URL ??
-      'https://test-api.samgongustofa.is/vehicle/registrations/authenticate',
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     restDeRegUrl:
       process.env.SAMGONGUSTOFA_REST_DEREG_URL ??
-      'https://test-api.samgongustofa.is/vehicle/registrations/deregistration',
-    restUsername: process.env.SAMGONGUSTOFA_REST_USER ?? 'DELOITTE.AFSKRA',
-    restPassword: process.env.SAMGONGUSTOFA_REST_PASS ?? 'FlugaaFramrudu',
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    restUsername: process.env.SAMGONGUSTOFA_REST_USER ?? 'xxxxxxxxxxxxxxx',
+    restPassword: process.env.SAMGONGUSTOFA_REST_PASS ?? 'xxxxxxxxxxxxxx',
     restReportingStation:
       process.env.SAMGONGUSTOFA_REST_REPORTING_STATION ?? '',
   },
   fjarsysla: {
     restUrl:
       process.env.FJARSYSLA_REST_URL ??
-      'https://tbrws-s.hysing.is/restv2/LeggjaASkilagjald',
-    restUsername: process.env.FJARSYSLA_REST_USER ?? 'fjs-samgong-p',
-    restPassword: process.env.FJARSYSLA_REST_PASS ?? 'katla.123',
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    restUsername: process.env.FJARSYSLA_REST_USER ?? 'xxxxxxxxxxxxx',
+    restPassword: process.env.FJARSYSLA_REST_PASS ?? 'xxxxxxxxx',
   },
   skilavottord: {
     userList:
       process.env.SKILAVOTTORD_USER_LIST ??
-      `[{"nationalId":"0301665909","name":"Sigurgeir Guðmundsson","role":"developer","partnerId":""},
-        {"nationalId":"1311615909","name":"Axel Rós","role":"recyclingFund","partnerId":""},
-        {"nationalId":"2811638099","name":"Tómas Árni Jónsson","role":"recyclingCompany","partnerId":""}]`,
+      `[{"nationalId":"1111111111","name":"xxxxxxxxxxxxxxxxxxxxx","role":"developer","partnerId":""},
+        {"nationalId":"2222222222","name":"xxxxxxxx","role":"recyclingFund","partnerId":""},
+        {"nationalId":"3333333333","name":"xxxxxxxxxxxxxxxxxx","role":"recyclingCompany","partnerId":""}]`,
   },
   backendUrl: 'http://localhost:3333',
 }
