86e743520f4c9150913acb3e45e2feb20696a96d
unnarb <unnarb@gmail.com>
2021-03-09 15:06:25 +0000

    Delete console log

--- apps/auth-admin-web/services/GrantTypeService.ts
+++ apps/auth-admin-web/services/GrantTypeService.ts
@@ -26,7 +26,6 @@ export class GrantTypeService extends BaseService {
 
   /** Create a Grant Type  */
   static async create(grantType: GrantTypeDTO): Promise<GrantType | null> {
-    console.log(grantType)
     return BaseService.POST(`grants`, grantType)
   }
 
