a42e67d71d0f565ff7a81e92a87759bbe3afdf4c
qdong <qdong@deloitte.is>
2021-03-09 10:25:31 +0000

    feature(health-insurance) - minor format

--- libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.utils.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.utils.ts
@@ -92,7 +92,7 @@ export const transformApplicationToHealthInsuranceDTO = (
   if (arrFiles.length > 0) {
     try {
       const arrUrl: string[] = Object.values(application.attachments) ?? []
-      for ( let i = 0; i < arrUrl.length; i++){
+      for (let i = 0; i < arrUrl.length; i++) {
         const { region, bucket, key } = AmazonS3URI(arrUrl[i])
         bucketName = bucket
         attachmentNames.push(key)
