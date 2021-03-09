fe1d8bc8f8b255881d3ccf0664e7ef47abd75096
Andri Andresson <andri@sendiradid.is>
2021-03-09 15:52:07 +0000

    cleanup: remove duplicate implementation

--- apps/api/src/app/environments/environment.ts
+++ apps/api/src/app/environments/environment.ts
@@ -72,8 +72,5 @@ export default {
     url: process.env.SYSLUMENN_HOST ?? '',
     username: process.env.SYSLUMENN_USERNAME ?? '',
     password: process.env.SYSLUMENN_PASSWORD ?? '',
-  },
-  fileUploadService: {
-    uploadBucket:  process.env.FILE_STORAGE_UPLOAD_BUCKET ?? 'testing-islandis'
   }
 }
--- libs/file-storage/src/lib/config/fileStorageConfig.ts
+++
@@ -1,5 +0,0 @@
-export const FILE_STORAGE_CONFIG = 'FILE_STORAGE_CONFIG'
-
-export interface FileStorageConfig {
-  uploadBucket: string
-}
