02d7439efd4e9733272704ea00789be5fab14d79
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-03-10 15:23:33 +0000

    Add comment

--- libs/application/templates/children-residence-change/src/fields/components/DescriptionText/DescriptionText.tsx
+++ libs/application/templates/children-residence-change/src/fields/components/DescriptionText/DescriptionText.tsx
@@ -26,6 +26,9 @@ const textOverride = {
 const DescriptionText = ({ text, format }: Props) => {
   const { formatMessage } = useIntl()
   const markdown = formatMessage(text, format)
+  // markdown-to-jsx is able to handle this in most cases but when using 'formatMessage'
+  // it does not work for some reason. That is the reason for this special handling here.
+  // We will take a look at this later with the localization team.
   const formattedMarkdown = markdown.replace(/&#39;/g, '&apos;')
   return (
     <Markdown
