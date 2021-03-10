a9d5d32f096b47643be836f63aa4b458abb49333
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-03-10 11:20:13 +0000

    Remove cy wait

--- apps/web-e2e/src/integration/categories.spec.ts
+++ apps/web-e2e/src/integration/categories.spec.ts
@@ -3,8 +3,6 @@ import { cms } from '@island.is/api/mocks'
 describe('/flokkur', () => {
   beforeEach(() => {
     cy.visit(`/flokkur/${cms.store.articleCategories[0].slug}`)
-    // TODO: Find better way so the e2e CI test doesn't fail without wait
-    cy.wait(2000)
   })
 
   it('should contain h1 title', () => {
