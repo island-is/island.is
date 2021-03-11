3ba6e114ae449bd6bebb2bc1919f7dd038c1b0f4
Brynjar Ingimarsson <brynjar@stefna.is>
2021-03-11 12:53:14 +0000

    Add SI slices to organization slices

---
+++ apps/web/components/Organization/Slice/BulletList/BulletList.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BulletListSlice as BulletListProps } from '@island.is/web/graphql/schema'
+import { Box } from '@island.is/island-ui/core'
+import { BulletList } from '../../../BulletList/BulletList'
+
+interface SliceProps {
+  slice: BulletListProps
+}
+
+export const BulletListSlice: React.FC<SliceProps> = ({ slice }) => {
+  return (
+    <section key={slice.id} aria-labelledby={'sliceTitle-' + slice.id}>
+      <Box paddingBottom={[8, 5, 10]}>
+        <BulletList
+          bullets={slice.bullets.map((bullet) => {
+            switch (bullet.__typename) {
+              case 'IconBullet':
+                return {
+                  ...bullet,
+                  type: 'IconBullet',
+                  icon: bullet.icon.url,
+                }
+              case 'NumberBulletGroup':
+                return { ...bullet, type: 'NumberBulletGroup' }
+              default:
+                return null
+            }
+          })}
+        />
+      </Box>
+    </section>
+  )
+}
