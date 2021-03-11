e49406c8c894e2494a54380d0406c8514da937b4
Brynjar Ingimarsson <brynjar@stefna.is>
2021-03-11 12:53:14 +0000

    Add about slices to organization slices (temporary)

--- apps/web/components/Organization/Slice/OrganizationSlice.tsx
+++ apps/web/components/Organization/Slice/OrganizationSlice.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react'
 import { Slice } from '@island.is/web/graphql/schema'
 import { Namespace } from '@island.is/api/schema'
 import dynamic from 'next/dynamic'
+import { TeamList } from '../../../../../libs/island-ui/contentful/src/lib/TeamList/TeamList'
+import { TellUsAStoryForm, ContactUs } from '@island.is/island-ui/contentful'
 
 const DistrictsSlice = dynamic(() =>
   import('@island.is/web/components').then((mod) => mod.DistrictsSlice),
@@ -31,6 +33,26 @@ const AccordionSlice = dynamic(() =>
   import('@island.is/web/components').then((mod) => mod.AccordionSlice),
 )
 
+const MailingListSignupSlice = dynamic(() =>
+  import('@island.is/web/components').then((mod) => mod.MailingListSignupSlice),
+)
+
+const LogoListSlice = dynamic(() =>
+  import('@island.is/web/components').then((mod) => mod.LogoListSlice),
+)
+
+const TabSectionSlice = dynamic(() =>
+  import('@island.is/web/components').then((mod) => mod.TabSectionSlice),
+)
+
+const BulletListSlice = dynamic(() =>
+  import('@island.is/web/components').then((mod) => mod.BulletListSlice),
+)
+
+const StorySlice = dynamic(() =>
+  import('@island.is/web/components').then((mod) => mod.StorySlice),
+)
+
 interface OrganizationSliceProps {
   slice: Slice
   namespace?: Namespace
@@ -55,7 +77,28 @@ export const OrganizationSlice: FC<OrganizationSliceProps> = ({
       return <OneColumnTextSlice slice={slice} />
     case 'AccordionSlice':
       return <AccordionSlice slice={slice} />
+    case 'MailingListSignupSlice':
+      return <MailingListSignupSlice slice={slice} namespace={namespace} />
+    case 'LogoListSlice':
+      return <LogoListSlice slice={slice} />
+    case 'TabSection':
+      return <TabSectionSlice slice={slice} />
+    case 'BulletListSlice':
+      return <BulletListSlice slice={slice} />
+    case 'StorySlice':
+      return <StorySlice slice={slice} />
+    case 'TeamList':
+      return <TeamList {...slice} />
+    case 'ContactUs':
+      return (
+        <ContactUs
+          {...slice}
+          onSubmit={async (data) => console.warn(data)}
+          state="edit"
+        />
+      )
     default:
+      console.log(slice.__typename)
       return <></>
   }
 }
