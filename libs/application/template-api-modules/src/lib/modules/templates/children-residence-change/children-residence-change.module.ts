62f60a50f0391c5436e5db214d78e321a72f1267
RikkiAbb <rikardarnar@gmail.com>
2021-03-12 14:17:19 +0000

    Sends email confirmations to both parents on application submit

--- libs/application/template-api-modules/src/lib/modules/templates/children-residence-change/children-residence-change.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/children-residence-change/children-residence-change.module.ts
@@ -5,12 +5,16 @@ import {
   PRESIGNED_BUCKET,
 } from './children-residence-change.service'
 import { SyslumennModule } from '@island.is/api/domains/syslumenn'
+import { SharedTemplateAPIModule } from '../../shared'
 
 export class ChildrenResidenceChangeModule {
   static register(config: BaseTemplateAPIModuleConfig): DynamicModule {
     return {
       module: ChildrenResidenceChangeModule,
-      imports: [SyslumennModule.register(config.syslumenn)],
+      imports: [
+        SyslumennModule.register(config.syslumenn),
+        SharedTemplateAPIModule.register(config),
+      ],
       providers: [
         {
           provide: PRESIGNED_BUCKET,
---
+++ libs/application/template-api-modules/src/lib/modules/templates/children-residence-change/emailGenerators/applicationSubmitted.ts
@@ -0,0 +1,52 @@
+import { AttachmentEmailTemplateGenerator } from '../../../../types'
+
+export const generateApplicationSubmittedEmail: AttachmentEmailTemplateGenerator = (
+  props,
+  fileContent,
+  email,
+) => {
+  const {
+    application,
+    options: { clientLocationOrigin },
+  } = props
+  const applicationLink = `${clientLocationOrigin}/umsokn/${application.id}`
+
+  const subject = 'Afrit af samning'
+  const body = `Góðan dag.
+
+        Í viðhengi er afrit af undirrituðum samning um breytt lögheimili barns
+
+        Breyting á lögheimili og þar með á greiðslu meðlags og barnabóta tekur gildi eftir að sýslumaður hefur staðfest samninginn.
+
+        Staðfesting sýslumanns verður send í rafræn skjöl á Ísland.is.
+
+        Þú getur séð stöðu umsóknarinnar á mínum síðum á Ísland.is
+
+        <a href=${applicationLink} target="_blank">Fara á mínar síður</a>.
+      `
+
+  return {
+    from: {
+      name: 'Devland.is',
+      address: 'development@island.is',
+    },
+    to: [
+      {
+        name: '',
+        address: email,
+      },
+    ],
+    subject,
+    html: `<p>${body
+      .split('')
+      .map((c) => (c === '\n' ? `<br />\n` : c))
+      .join('')}</p>`,
+    attachments: [
+      {
+        filename: `${application.id}.pdf`,
+        content: fileContent,
+        encoding: 'binary',
+      },
+    ],
+  }
+}
