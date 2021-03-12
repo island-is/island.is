39543650e8cc48e1cbe3cb11000ad8f9c374abff
atligudl <atli@sendiradid.is>
2021-03-12 09:42:30 +0000

    feat(data-protection-complaint): Add complaint screens (#3225)
    * set of subject of complaint screen
    
    * add complaint overview custom field to overview screen
    
    * format complainee repeater
    
    * add info section to complaint overview
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/data-protection-complaint/src/lib/messages/overview.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl'
+
+export const overview = {
+  general: defineMessages({
+    pageTitle: {
+      id: 'dpac.application:section.overview.pageTitle',
+      defaultMessage: 'Yfirlit kvörtunar',
+      description: 'overview page title',
+    },
+    pageDescription: {
+      id: 'dpac.application:section.overview.pageDescription',
+      defaultMessage: `Farðu vel yfir efnið áður en þú sendir inn kvörtunina. Það flýtir fyrir afgreiðslu málsins hjá Persónuvernd ef kvörtunin er skýr og afmörkuð.`,
+      description: 'overview page description',
+    },
+  }),
+}
