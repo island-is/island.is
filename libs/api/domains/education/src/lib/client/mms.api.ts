39706ab29d646e43d5a76433dbb7a7e3fdbf540f
Davíð Guðni <davidgudni@gmail.com>
2021-03-15 11:22:50 +0000

    fix(education-domain): stop faking data and connect to xroad

--- libs/api/domains/education/src/lib/client/mms.api.ts
+++ libs/api/domains/education/src/lib/client/mms.api.ts
@@ -7,20 +7,21 @@ import { LicenseResponse, LicensePDFResponse } from './mms.type'
 export class MMSApi {
   private readonly xroadApiUrl: string
   private readonly xroadClientId: string
-  private readonly secret: string
+  private readonly xroadLicenseServiceId: string
 
-  constructor(xroadBaseUrl: string, xroadClientId: string, secret: string) {
-    const xroadPath = 'r1/IS-DEV/GOV/10005/XXX' // TODO update this path when we have connected the api to xroad
-    this.xroadApiUrl = `${xroadBaseUrl}/${xroadPath}`
+  constructor(
+    xroadBaseUrl: string,
+    xroadClientId: string,
+    xroadLicenseServiceId: string,
+  ) {
+    this.xroadApiUrl = `${xroadBaseUrl}/${xroadLicenseServiceId}`
     this.xroadClientId = xroadClientId
-    this.secret = secret
   }
 
   async requestApi(url: string) {
     const res = await fetch(`${this.xroadApiUrl}/${url}`, {
       headers: {
         'X-Road-Client': this.xroadClientId,
-        SECRET: this.secret,
         Accept: 'application/json',
       },
     })
@@ -28,29 +29,10 @@ export class MMSApi {
   }
 
   getLicenses(nationalId: User['nationalId']): Promise<LicenseResponse[]> {
-    //return this.requestApi(`api/licenses/${nationalId}`) // TODO fix
-    return Promise.resolve([
-      {
-        id: '987654',
-        school: 'Menntamálaráðuneytið',
-        programme: 'Kennararéttindi',
-        date: '2010-09-01',
-      },
-      {
-        id: '456789',
-        school: 'Viðskiptaráð',
-        programme: 'Próf í verðbréfaviðskiptum',
-        date: '2005-05-25',
-      },
-    ])
+    return this.requestApi(`api/public/users/${nationalId}/licenses`)
   }
 
   getLicensePDF(licenseId: string): Promise<LicensePDFResponse> {
-    //return this.requestApi(`api/licenses/licenseId/export`) // TODO fix
-    return Promise.resolve({
-      id: licenseId,
-      content: 'PDF CONTENT',
-      filename: `nationalID-${licenseId}`,
-    })
+    return this.requestApi(`api/public/licenses/${licenseId}/pdf`)
   }
 }
