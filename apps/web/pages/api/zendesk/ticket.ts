1bde0dafb919211ed7c133d5ccdca92b8dbaabd8
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-03-15 17:43:19 +0000

    WIP

--- apps/web/pages/api/zendesk/ticket.ts
+++ apps/web/pages/api/zendesk/ticket.ts
@@ -2,44 +2,42 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const { subject, body } = req.body
-
-    const data = JSON.stringify({
-      ticket: { subject, comment: { body } },
-    })
-
-    const token = Buffer.from(
-      'stjanilofts@gmail.com/token:csA8760vTZKc6eSmXsAFH7yF5HsE5gQFVNxSW9lN',
-    ).toString('base64')
-
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${token}`,
-    }
-
-    let response
-
-    try {
-      response = await axios.post(
-        'https://stjanilofts.zendesk.com/api/v2/tickets.json',
-        data,
-        {
-          headers,
-        },
-      )
-    } catch (e) {
-      return res
-        .status(200)
-        .json({ success: 'false', message: e.response.data.error })
-    }
-
-    return res.status(200).json({ success: 'true', message: response.data })
+  if (req.method !== 'POST') {
+    return res.status(401).json({ success: 'false' })
   }
 
-  return res
-    .status(200)
-    .json({ success: 'false', message: 'Must be a POST request.' })
+  const { subject, body } = req.body
+
+  const data = JSON.stringify({
+    ticket: { requester_id: '431452825554', subject, comment: { body } },
+  })
+
+  const token = Buffer.from(
+    `${process.env.ZENDESK_EMAIL}/token:${process.env.ZENDESK_TOKEN}`,
+  ).toString('base64')
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Basic ${token}`,
+  }
+
+  let response
+
+  try {
+    response = await axios.post(
+      `https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/tickets.json`,
+      data,
+      {
+        headers,
+      },
+    )
+  } catch (e) {
+    return res
+      .status(200)
+      .json({ success: 'false', message: e.response.statusText })
+  }
+
+  return res.status(200).json({ success: 'true' })
 }
 
 export default handler
