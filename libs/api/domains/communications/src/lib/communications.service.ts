7f283743b5c0aadb83d0918fd44846627641bc15
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-03-15 15:00:24 +0000

    Zendesk instead of email for contact form

--- libs/api/domains/communications/src/lib/communications.service.ts
+++ libs/api/domains/communications/src/lib/communications.service.ts
@@ -1,4 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common'
+import axios from 'axios'
+import toQueryString from 'to-querystring'
 import { EmailService } from '@island.is/email-service'
 import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
 import { SendMailOptions } from 'nodemailer'
@@ -6,6 +8,9 @@ import { ContactUsInput } from './dto/contactUs.input'
 import { TellUsAStoryInput } from './dto/tellUsAStory.input'
 import { getTemplate as getContactUsTemplate } from './emailTemplates/contactUs'
 import { getTemplate as getTellUsAStoryTemplate } from './emailTemplates/tellUsAStory'
+
+import { environment } from './environments/environment'
+const { zendeskOptions } = environment
 
 type SendEmailInput = ContactUsInput | TellUsAStoryInput
 interface EmailTypeTemplateMap {
@@ -43,5 +48,113 @@ export class CommunicationsService {
       // we dont want the client to see these errors since they might contain sensitive data
       throw new Error('Failed to send message')
     }
+  }
+
+  async sendZendeskTicket(input: ContactUsInput): Promise<boolean> {
+    const api = `https://${zendeskOptions.subdomain}.zendesk.com/api/v2`
+
+    const token = Buffer.from(
+      `${zendeskOptions.email}/token:${zendeskOptions.token}`,
+    ).toString('base64')
+
+    const params = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${token}`,
+      },
+    }
+
+    let searchUserResponse
+
+    // First check if a user is found with this email
+    try {
+      searchUserResponse = await axios.get(
+        `${api}/search.json?query=${encodeURIComponent(
+          `type:user "${input.email}"`,
+        )}`,
+        params,
+      )
+    } catch (error) {
+      this.logger.error('Failed to search for user', {
+        message: error.response.data.description,
+      })
+
+      throw new Error(
+        `Failed to search for user: ${error.response.data.description}`,
+      )
+    }
+
+    let user =
+      searchUserResponse.data.results.length > 0 &&
+      searchUserResponse.data.results[0]
+
+    // If we did not find an existing user we will create a new one
+    if (!user) {
+      let identities = []
+
+      if (input.phone) {
+        identities.push({
+          type: 'phone_number',
+          value: input.phone,
+        })
+      }
+
+      const newUser = JSON.stringify({
+        user: {
+          name: input.name,
+          email: input.email,
+          identities,
+        },
+      })
+
+      let createUserResponse
+
+      try {
+        createUserResponse = await axios.post(
+          `${api}/users.json`,
+          newUser,
+          params,
+        )
+      } catch (error) {
+        this.logger.error('Failed to create Zendesk user', {
+          message: error.response.data.description,
+        })
+
+        throw new Error(
+          `Failed to create Zendesk user: ${error.response.data.description}`,
+        )
+      }
+
+      user = createUserResponse.data.user
+    }
+
+    const newTicket = JSON.stringify({
+      ticket: {
+        requester_id: user.id,
+        subject: input.subject ?? '',
+        comment: { body: input.message ?? '' },
+        tags: ['web'],
+      },
+    })
+
+    let createTicketResponse
+
+    try {
+      createTicketResponse = await axios.post(
+        `${api}/tickets.json`,
+        newTicket,
+        params,
+      )
+    } catch (error) {
+      this.logger.error('Failed to submit Zendesk ticket', {
+        message: error.response.data.description,
+      })
+
+      throw new Error(
+        `Failed to submit Zendesk ticket: ${error.response.data.description}`,
+      )
+    }
+
+    return true
   }
 }
