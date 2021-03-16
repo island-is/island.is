8b364c0d06b8469ca143b587c863d9afd5d89c2f
RikkiAbb <rikardarnar@gmail.com>
2021-03-16 14:04:28 +0000

    Fixes after merge conflict

--- apps/application-system/api/src/app/modules/application/application.controller.ts
+++ apps/application-system/api/src/app/modules/application/application.controller.ts
@@ -46,7 +46,7 @@ import { TemplateAPIService } from '@island.is/application/template-api-modules'
 
 import { Application } from './application.model'
 import { ApplicationService } from './application.service'
-import { FileService } from '@island.is/application/file-service'
+import { FileService } from './files/file.service'
 import { CreateApplicationDto } from './dto/createApplication.dto'
 import { UpdateApplicationDto } from './dto/updateApplication.dto'
 import { AddAttachmentDto } from './dto/addAttachment.dto'
