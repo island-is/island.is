c6beea41585e1ee02c13871f0ce913980f1e4a83
mar <mar.nospam@anomy.net>
2021-03-16 13:53:02 +0000

    refactor: Export `NamespaceGetter` for possible use around town…
    ...and flag `useNamespaceStrict`'s sloppy signature as @deprecated

--- apps/web/hooks/useNamespace.ts
+++ apps/web/hooks/useNamespace.ts
@@ -3,28 +3,31 @@ type Nully = undefined | null
 type ContentFulValues = any // FIXME: Figure out what type the actual values could be
 type Messages = Readonly<Record<string, ContentFulValues | undefined>>
 
-// NOTE: Typesafe/clever/strict signature to provide nice auto-complete for cases
-// where `namespace`'s type is fully described
-export function useNamespaceStrict<M extends Messages>(namespace: M = {} as M) {
-  function nFunc<K extends keyof M>(
-    key: K,
-    fallback?: Nully,
-  ): M[K] extends Nully ? K : M[K]
-  function nFunc<K extends keyof M, F extends ContentFulValues>(
+export type NamespaceGetter<M extends Messages> = {
+  <K extends keyof M>(key: K, fallback?: Nully): M[K] extends Nully ? K : M[K]
+  <K extends keyof M, F extends ContentFulValues>(
     key: K,
     fallback: F,
   ): M[K] extends Nully ? F : M[K]
+  /** @deprecated
+   * Sloppy fallback version (based on the assumption that this is somehow desirable/practical)
+   */
+  (key: string, fallback?: any): any
+}
 
-  // Implementation
-  function nFunc(key: string, fallback?: any) {
+// NOTE: Typesafe/clever/strict signature to provide nice auto-complete for cases
+// where `namespace`'s type is fully described
+export function useNamespaceStrict<M extends Messages>(
+  namespace: M = {} as M,
+): NamespaceGetter<M> {
+  return (key: string, fallback?: any) => {
     return namespace[key] ?? fallback ?? key
   }
-
-  return nFunc
 }
 
 // NOTE: Dumb signature that accepts arbitrary `key` values and returns any
 // (The "clever" signature above is entirely opt-in)
+// TODO: mark this as @deprecated
 export function useNamespace(namespace = {}) {
   return (key: string, fallback?: any) => {
     return namespace[key as keyof typeof namespace] ?? fallback ?? key
