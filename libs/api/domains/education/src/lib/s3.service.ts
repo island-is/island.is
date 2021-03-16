432713470f2a88a83b54fb0d85520e492ba8598c
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-03-16 14:11:42 +0000

    s3 service to upload a stream and return a signed url for download
    has a 5 second time frame to download from the url

---
+++ libs/api/domains/education/src/lib/s3.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@nestjs/common'
+import { Inject } from '@nestjs/common'
+import * as S3 from 'aws-sdk/clients/s3'
+import { Response } from 'node-fetch'
+import stream from 'stream'
+
+import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
+
+type S3Location = {
+  fileName: string
+  bucket: string
+}
+
+type FileUpload = {
+  passThrough: stream.PassThrough
+  promise: Promise<S3.ManagedUpload.SendData>
+}
+
+@Injectable()
+export class S3Service {
+  private readonly s3: S3
+
+  constructor(
+    @Inject(LOGGER_PROVIDER)
+    private readonly logger: Logger,
+  ) {
+    this.s3 = new S3()
+  }
+
+  private uploadFromStream(
+    fileResponse: Response,
+    { fileName, bucket }: S3Location,
+  ): FileUpload {
+    const passThrough = new stream.PassThrough()
+    const promise = this.s3
+      .upload({
+        Bucket: bucket,
+        Key: fileName,
+        ContentType: 'application/pdf',
+        Body: passThrough,
+      })
+      .promise()
+    return { passThrough, promise }
+  }
+
+  async uploadFileFromStream(
+    stream: Response,
+    s3Location: S3Location,
+  ): Promise<string | null> {
+    const { passThrough, promise } = this.uploadFromStream(stream, s3Location)
+
+    stream.body.pipe(passThrough)
+
+    return promise
+      .then((result) => {
+        const fiveSeconds = 5
+        return this.s3.getSignedUrl('getObject', {
+          Bucket: s3Location.bucket,
+          Key: result.Key,
+          Expires: fiveSeconds,
+        })
+      })
+      .catch((err) => {
+        this.logger.error(err)
+        return null
+      })
+  }
+}
