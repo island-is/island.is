7d6ef2a914c7dc0454d5dfdf894b49f71a75a5d6
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2021-03-16 11:06:04 +0000

    Continue skipping ReferenceApplicationTemplate test

--- apps/application-system/api/src/app/modules/application/e2e/application.spec.ts
+++ apps/application-system/api/src/app/modules/application/e2e/application.spec.ts
@@ -75,7 +75,7 @@ describe('Application system API', () => {
   })
 
   // This template does not have readyForProduction: false
-  it('should fail when POST-ing an application whose template is not ready for production, on production environment', async () => {
+  it.skip('should fail when POST-ing an application whose template is not ready for production, on production environment', async () => {
     const envBefore = environment.environment
     environment.environment = 'production'
 
