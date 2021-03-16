04f8fef157a7b917b3743d8e1af3d4501a953b6b
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-03-16 14:17:57 +0000

    Fix data mapping and update routes to fit mms api

--- libs/api/domains/education/src/lib/client/mms.api.ts
+++ libs/api/domains/education/src/lib/client/mms.api.ts
@@ -1,38 +1,43 @@
-import fetch from 'node-fetch'
+import fetch, { Response } from 'node-fetch'
 
 import { User } from '@island.is/auth-nest-tools'
 
-import { LicenseResponse, LicensePDFResponse } from './mms.type'
+import { LicenseResponse } from './mms.type'
 
 export class MMSApi {
   private readonly xroadApiUrl: string
   private readonly xroadClientId: string
-  private readonly xroadLicenseServiceId: string
 
   constructor(
     xroadBaseUrl: string,
     xroadClientId: string,
     xroadLicenseServiceId: string,
   ) {
-    this.xroadApiUrl = `${xroadBaseUrl}/${xroadLicenseServiceId}`
+    this.xroadApiUrl = `${xroadBaseUrl}/r1/${xroadLicenseServiceId}`
     this.xroadClientId = xroadClientId
   }
 
-  async requestApi(url: string) {
-    const res = await fetch(`${this.xroadApiUrl}/${url}`, {
+  async requestApi(url: string): Promise<Response> {
+    return fetch(`${this.xroadApiUrl}/${url}`, {
       headers: {
         'X-Road-Client': this.xroadClientId,
         Accept: 'application/json',
       },
     })
-    return res.json()
   }
 
-  getLicenses(nationalId: User['nationalId']): Promise<LicenseResponse[]> {
-    return this.requestApi(`api/public/users/${nationalId}/licenses`)
+  async getLicenses(
+    nationalId: User['nationalId'],
+  ): Promise<LicenseResponse[]> {
+    const response = await this.requestApi(
+      `api/public/users/${nationalId}/licenses`,
+    )
+    return response.json()
   }
 
-  getLicensePDF(licenseId: string): Promise<LicensePDFResponse> {
-    return this.requestApi(`api/public/licenses/${licenseId}/pdf`)
+  downloadLicensePDF(nationalId: string, licenseId: string): Promise<Response> {
+    return this.requestApi(
+      `api/public/users/${nationalId}/licenses/${licenseId}/pdf`,
+    )
   }
 }
--- libs/api/domains/education/src/lib/client/mms.type.ts
+++ libs/api/domains/education/src/lib/client/mms.type.ts
@@ -1,12 +1,12 @@
 export interface LicenseResponse {
   id: string
+  issued: string
+  nationalId: string
+  type: string
+  fullName: string
+  dokobitToken: string | null
   school: string
-  programme: string
-  date: string
-}
-
-export interface LicensePDFResponse {
-  id: string
-  content: string
-  filename: string
+  country: string
+  created: string
+  modified: string
 }
