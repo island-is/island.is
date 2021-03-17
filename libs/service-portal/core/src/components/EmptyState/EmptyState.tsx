8a03a8b12b08edc14484f2dcae7ca317cf3eac08
Brian Johannessen <barabrian89@gmail.com>
2021-03-17 14:01:17 +0000

    added empty state

---
+++ libs/service-portal/core/src/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useLocale } from '@island.is/localization'
+import { MessageDescriptor } from 'react-intl'
+import { Text, Box } from '@island.is/island-ui/core'
+
+interface Props {
+  title: MessageDescriptor
+  description?: MessageDescriptor
+}
+
+export const EmptyState = ({ title, description }: Props) => {
+  const { formatMessage } = useLocale()
+  return (
+    <>
+      <Text marginBottom={1} variant="h3">
+        {formatMessage(title)}
+      </Text>
+      {description && <Text>{formatMessage(description)}</Text>}
+      <Box
+        component="img"
+        marginTop={4}
+        width="full"
+        src="/assets/images/emptyState.svg"
+      />
+    </>
+  )
+}
+
+export default EmptyState
