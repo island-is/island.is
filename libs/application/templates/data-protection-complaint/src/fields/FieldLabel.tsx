5fc12778176836e138111b5c9c40fccfcfa5e45c
atligudl <atli@sendiradid.is>
2021-03-18 13:11:16 +0000

    adjusts spacings in complainee multi field (#3252)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/data-protection-complaint/src/fields/FieldLabel.tsx
+++ libs/application/templates/data-protection-complaint/src/fields/FieldLabel.tsx
@@ -8,7 +8,7 @@ export const FieldLabel: FC<FieldBaseProps> = ({ application, field }) => {
   const { formatMessage } = useLocale()
 
   return (
-    <Text variant="h4" marginTop={1}>
+    <Text variant="h4" marginTop={3}>
       {formatText(title, application, formatMessage)}
     </Text>
   )
