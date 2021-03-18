b33c26920468d5f3a19f3de398f5490be81a6f46
Bjarni Freyr Björnsson <bjarmif@gmail.com>
2021-03-18 15:09:22 +0000

    feat: added temp content on home

--- apps/native/app/src/screens/home/home.tsx
+++ apps/native/app/src/screens/home/home.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { SafeAreaView, StatusBar, Image } from 'react-native'
-import { Button } from '@island.is/island-ui-native'
+import { Button, Heading } from '@island.is/island-ui-native'
 import { WebView } from 'react-native-webview'
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo-island.png';
 
 export const Home = () => {
   return (
@@ -13,10 +13,13 @@ export const Home = () => {
         style={{
           width: '100%',
           height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
+        <Image source={logo} resizeMode="contain" style={{ width: 45, height: 45, marginBottom: 20 }} />
+        <Heading isCenterAligned>Skráðu þig inn í appið með rafrænum skilríkjum</Heading>
         <Button title="Auðkenna" onPress={() => console.log('on press')} />
-        <Image source={logo} resizeMode="contain" style={{ width: 200, height: 200 }} />
         {/* <WebView
           source={{ uri: 'https://island.is/' }}
           style={{ width: 400, height: 200 }}
