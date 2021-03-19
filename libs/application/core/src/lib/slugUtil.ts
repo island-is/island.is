93b77847f55afb2ce16f0f62656c826fc7622923
Jérémy Barbet <jeremgraph@gmail.com>
2021-03-19 14:02:31 +0000

    fix: new paths

--- libs/application/core/src/lib/slugUtil.ts
+++ libs/application/core/src/lib/slugUtil.ts
@@ -7,3 +7,8 @@ export const getTypeFromSlug = (slug: string) =>
   Object.entries(ApplicationConfigurations).find(
     ([_, item]) => item.slug === slug,
   )?.[0] as ApplicationTypes | undefined
+
+export const getSlugFromType = (type: ApplicationTypes) =>
+  Object.entries(ApplicationConfigurations).find(([id]) => id === type)?.[0] as
+    | string
+    | undefined
--- libs/service-portal/applications/src/screens/ApplicationList/ApplicationList.tsx
+++ libs/service-portal/applications/src/screens/ApplicationList/ApplicationList.tsx
@@ -13,7 +13,7 @@ import {
   ActionCard,
 } from '@island.is/island-ui/core'
 import { useApplications } from '@island.is/service-portal/graphql'
-import { Application } from '@island.is/application/core'
+import { Application, getSlugFromType } from '@island.is/application/core'
 import { useLocale, useNamespaces } from '@island.is/localization'
 import * as Sentry from '@sentry/react'
 import { useHistory } from 'react-router-dom'
@@ -25,12 +25,12 @@ const isStaging = window.location.origin.includes('beta.staging01.devland.is')
 const isProduction = window.location.origin.includes('https://island.is')
 
 const baseUrlForm = isDev
-  ? 'https://umsoknir.dev01.devland.is'
+  ? 'https://dev01.devland.is/umsoknir'
   : isStaging
-  ? 'https://umsoknir.staging01.devland.is'
+  ? 'https://staging01.devland.is/umsoknir'
   : isProduction
-  ? 'https://umsoknir.island.is'
-  : 'http://localhost:4242'
+  ? 'https://island.is/umsoknir'
+  : 'http://localhost:4242/umsoknir'
 
 const ApplicationList: ServicePortalModuleComponent = () => {
   useNamespaces('sp.applications')
@@ -72,6 +72,11 @@ const ApplicationList: ServicePortalModuleComponent = () => {
       <Stack space={2}>
         {applications.map((application: Application) => {
           const isComplete = application.progress === 1
+          const slug = getSlugFromType(application.typeId)
+
+          if (!slug) {
+            return null
+          }
 
           return (
             <ActionCard
@@ -93,7 +98,7 @@ const ApplicationList: ServicePortalModuleComponent = () => {
                 size: 'small',
                 icon: undefined,
                 onClick: () =>
-                  history.replace(`${baseUrlForm}/umsokn/${application.id}`),
+                  window.open(`${baseUrlForm}/${slug}/${application.id}`),
               }}
               text={
                 isComplete
