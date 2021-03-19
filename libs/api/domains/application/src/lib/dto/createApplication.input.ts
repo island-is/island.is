7b6d001933f67ccaf4cefbae7871b35c45281b8b
Bæring Gunnar Steinþórsson <baering@aranja.com>
2021-03-19 15:55:34 +0000

    Only accept typeId when creating applications (#3303)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/application/src/lib/dto/createApplication.input.ts
+++ libs/api/domains/application/src/lib/dto/createApplication.input.ts
@@ -1,53 +1,14 @@
 import { Field, InputType, registerEnumType } from '@nestjs/graphql'
-import graphqlTypeJson from 'graphql-type-json'
-import {
-  IsString,
-  IsOptional,
-  IsEnum,
-  IsObject,
-  IsArray,
-} from 'class-validator'
-import {
-  CreateApplicationDtoStatusEnum,
-  CreateApplicationDtoTypeIdEnum,
-} from '../../../gen/fetch'
+import { IsEnum } from 'class-validator'
+import { CreateApplicationDtoTypeIdEnum } from '../../../gen/fetch'
 
 registerEnumType(CreateApplicationDtoTypeIdEnum, {
   name: 'CreateApplicationDtoTypeIdEnum',
 })
 
-registerEnumType(CreateApplicationDtoStatusEnum, {
-  name: 'CreateApplicationDtoStatusEnum',
-})
-
 @InputType()
 export class CreateApplicationInput {
-  @Field(() => String)
-  @IsString()
-  applicant!: string
-
-  @Field(() => [String])
-  @IsArray()
-  assignees!: string[]
-
-  @Field(() => String)
-  @IsString()
-  state!: string
-
-  @Field(() => graphqlTypeJson, { nullable: true })
-  @IsObject()
-  @IsOptional()
-  attachments?: object
-
   @Field(() => CreateApplicationDtoTypeIdEnum)
   @IsEnum(CreateApplicationDtoTypeIdEnum)
   typeId!: CreateApplicationDtoTypeIdEnum
-
-  @Field(() => graphqlTypeJson)
-  @IsObject()
-  answers!: object
-
-  @Field(() => CreateApplicationDtoStatusEnum)
-  @IsEnum(CreateApplicationDtoStatusEnum)
-  status!: CreateApplicationDtoStatusEnum
 }
