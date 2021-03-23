426e0640e00beffbeecc7778b1d8045b386bf430
Sigga Hrafns <sigridur@kosmosogkaos.is>
2021-03-23 10:31:23 +0000

    UI for collect signatures

--- libs/application/templates/party-letter/src/fields/index.ts
+++ libs/application/templates/party-letter/src/fields/index.ts
@@ -1,2 +1,3 @@
 export { default as Recommendations } from './Recommendations'
 export { default as PartyLetter } from './PartyLetter'
+export { default as SignatureDisclaimer } from './SignatureDisclaimer'
--- libs/application/templates/party-letter/src/forms/CollectSignatures.ts
+++ libs/application/templates/party-letter/src/forms/CollectSignatures.ts
@@ -1,37 +1,100 @@
 import {
   buildForm,
-  buildDescriptionField,
+  buildCustomField,
   buildMultiField,
+  buildDescriptionField,
+  buildCheckboxField,
   buildSection,
   buildSubmitField,
+  buildTextField,
+  buildExternalDataProvider,
+  buildDataProviderItem,
   Form,
   FormModes,
+  Application,
 } from '@island.is/application/core'
+import { m } from '../lib/messages'
 
 export const ReviewApplication: Form = buildForm({
   id: 'Collect signatures',
-  title: 'Safna undirskriftum',
+  title: m.collectSignatures.applicationTitle,
   mode: FormModes.REVIEW,
   children: [
+    /* todo: see if external data can be provided for signaturee
+     buildSection({
+      id: 'termsAndConditions',
+      title: m.externalDataSection.title,
+      children: [
+        buildExternalDataProvider({
+          id: 'approveTermsAndConditions',
+          title: m.externalDataSection.title,
+          subTitle: m.externalDataSection.subTitle,
+          dataProviders: [
+            buildDataProviderItem({
+              id: 'userProfile',
+              type: 'UserProfileProvider',
+              title: 'm.userProfileInformationTitle',
+              subTitle: 'm.userProfileInformationSubTitle',
+            }),
+          ],
+        }),
+      ],
+    }),*/
     buildSection({
       id: 'intro',
-      title: 'Safna meðmælum',
+      title: m.collectSignatures.stepTitle,
       children: [
         buildMultiField({
           id: 'about',
-          title: '',
+          title: m.collectSignatures.sectionTitle,
           children: [
-            buildDescriptionField({
-              id: 'final',
-              title: 'Safna meðmælum',
-              description: 'Þú getur nú farið að taka á móti meðmælum',
+            buildCustomField({
+              id: 'disclaimer',
+              title: '',
+              component: 'SignatureDisclaimer',
             }),
+            buildTextField({
+              id: 'signature',
+              title: m.collectSignatures.nameInput,
+              variant: 'text',
+              placeholder: m.collectSignatures.nameInput,
+              backgroundColor: 'blue',
+              width: 'half',
+              // todo: default should be name of signaturee
+            }),
+            buildDescriptionField({
+              id: 'firstDescription',
+              title: '',
+              space: 5,
+              description: m.collectSignatures.descriptionPt1,
+            }),
+            buildDescriptionField({
+              id: 'secondDescription',
+              title: '',
+              space: 4,
+              description: m.collectSignatures.descriptionPt2,
+            }),
+            buildCheckboxField({
+              id: 'terms',
+              title: '',
+              options: [
+                { value: 'agree', label: m.collectSignatures.agreeTermsLabel },
+              ],
+              large: true,
+              backgroundColor: 'blue',
+              defaultValue: '',
+            }),
+
             buildSubmitField({
-              id: 'approvedByReviewer',
+              id: 'sign',
               placement: 'footer',
-              title: 'Senda inn umsókn',
+              title: m.collectSignatures.submitButton,
               actions: [
-                { event: 'APPROVE', name: 'Senda inn umsókn', type: 'primary' },
+                {
+                  event: 'APPROVE',
+                  name: m.collectSignatures.submitButton,
+                  type: 'primary',
+                },
               ],
             }),
           ],
