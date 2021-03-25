363c76632cccded9cfab8d83392c5e014a285317
Stefán Harald <stefanharald@gmail.com>
2021-03-25 09:33:27 +0000

    Fixed format

--- apps/signature-system/src/openApi.ts
+++ apps/signature-system/src/openApi.ts
@@ -6,5 +6,5 @@ export const openApi = new DocumentBuilder()
     'This API manages non-digital signatures collected by systems within island.is.',
   )
   .setVersion('1.0')
-  .addTag('reference')
+  .addTag('user')
   .build()
