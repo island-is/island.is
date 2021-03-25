f852b315b84e12512c8e54224aea339139d5c5f9
Valur Einarsson <valure@fuglar.com>
2021-03-25 15:45:54 +0000

    feat(auth-api): Add company registry client.
    Gitbook linting fix
    
    Remove required constraint in yaml.
    
    Move old and new rsk clients to version folders.
    
    Export using aliases.

---
+++ libs/clients/rsk/v2/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'clients-rsk-v2',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/clients/rsk-v2',
+}
