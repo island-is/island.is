864295f8590c2102420f69debc8225a008830c00
Olafur <obmagnusson@gmail.com>
2021-03-29 11:12:08 +0000

    remove test env file

--- apps/download-service/jest.config.js
+++ apps/download-service/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/apps/download-service',
-  setupFiles: ['./test/environment.jest.ts'],
+  setupFiles: [],
   setupFilesAfterEnv: ['./test/setup.ts'],
   moduleFileExtensions: ['ts', 'js', 'html', 'json'],
   globals: {
