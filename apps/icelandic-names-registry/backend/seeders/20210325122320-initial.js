a02c761507ef30a255428b233401548977ea0e68
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-03-30 16:00:27 +0000

    Remap values

--- apps/icelandic-names-registry/backend/seeders/20210325122320-initial.js
+++ apps/icelandic-names-registry/backend/seeders/20210325122320-initial.js
@@ -1,80 +1,21 @@
 'use strict'
+
 const data = require('../data')
 
-const yesterday = 86400000
+const remapped = data.map((x) => {
+  if (x.visible === 1) {
+    x.visible = true
+  }
 
-const shared = {
-  created: new Date(),
-  modified: new Date(),
-}
-
-console.log('data', data)
+  delete x.id
+  return x
+})
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('icelandic_names', data, {})
+  up: (queryInterface, _) => {
+    return queryInterface.bulkInsert('icelandic_names', remapped, {})
   },
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface, _) => {
     return queryInterface.bulkDelete('icelandic_names', null, {})
   },
-
-  // up: (queryInterface, Sequelize) => {
-  //   return queryInterface.bulkInsert(
-  //     'names',
-  //     [
-  //       {
-  //         name: 'Jón',
-  //         type: 'DR',
-  //         visible: 1,
-  //         ...shared,
-  //       },
-  //       {
-  //         name: 'Magnús',
-  //         type: 'DR',
-  //         visible: 1,
-  //         ...shared,
-  //       },
-  //       {
-  //         name: 'Pétur',
-  //         type: 'DR',
-  //         visible: 1,
-  //         ...shared,
-  //       },
-  //       {
-  //         name: 'Satan',
-  //         type: 'MI',
-  //         status: 'Haf',
-  //         visible: 1,
-  //         verdict_date: new Date(Date.now() - yesterday * 2),
-  //         ...shared,
-  //       },
-  //       {
-  //         name: 'Kristín',
-  //         type: 'ST',
-  //         visible: 1,
-  //         ...shared,
-  //       },
-  //       {
-  //         name: 'Alpine',
-  //         type: 'MI',
-  //         status: 'Haf',
-  //         verdict_date: new Date(Date.now() - yesterday * 8),
-  //         visible: 1,
-  //         ...shared,
-  //       },
-  //       {
-  //         name: 'Alaía',
-  //         type: 'ST',
-  //         status: 'Sam',
-  //         visible: 1,
-  //         verdict_date: new Date(Date.now() - yesterday * 11),
-  //         ...shared,
-  //       },
-  //     ],
-  //     {},
-  //   )
-  // },
-  // down: (queryInterface, Sequelize) => {
-  //   return queryInterface.bulkDelete('names', null, {})
-  // },
 }
