30cb3a236a70fbfaaa187d3e6c42584c8213b0e1
Árni Jónsson <addininja@gmail.com>
2021-03-30 10:02:03 +0000

    Moved the rendering logic over to a separate file

--- apps/judicial-system/web/src/routes/Prosecutor/CreateDetentionRequest/StepFive/StepFive.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/CreateDetentionRequest/StepFive/StepFive.tsx
@@ -1,18 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import {
-  Text,
-  Box,
-  ContentBlock,
-  InputFileUpload,
-  AlertMessage,
-} from '@island.is/island-ui/core'
 import { Case } from '@island.is/judicial-system/types'
-import {
-  FormContentContainer,
-  FormFooter,
-  PageLayout,
-} from '@island.is/judicial-system-web/src/shared-components'
-import * as Constants from '@island.is/judicial-system-web/src/utils/constants'
+import { PageLayout } from '@island.is/judicial-system-web/src/shared-components'
 import { useQuery } from '@apollo/client'
 import { CaseQuery } from '@island.is/judicial-system-web/graphql'
 import {
@@ -20,8 +8,7 @@ import {
   Sections,
 } from '@island.is/judicial-system-web/src/types'
 import { useRouter } from 'next/router'
-import * as styles from './StepFive.treat'
-import { useS3Upload } from '@island.is/judicial-system-web/src/utils/useS3Upload'
+import { StepFiveForm } from './StepFiveForm'
 
 export const StepFive: React.FC = () => {
   const [workingCase, setWorkingCase] = useState<Case>()
@@ -46,8 +33,6 @@ export const StepFive: React.FC = () => {
     }
   }, [id, workingCase, setWorkingCase, resCase])
 
-  const { files, onChange, onRemove } = useS3Upload(workingCase)
-
   return (
     <PageLayout
       activeSection={
@@ -62,76 +47,7 @@ export const StepFive: React.FC = () => {
       parentCaseDecision={workingCase?.parentCase?.decision}
       caseType={workingCase?.type}
     >
-      {workingCase ? (
-        <>
-          <FormContentContainer>
-            <Box marginBottom={10}>
-              <Text as="h1" variant="h1">
-                Rannsóknargögn
-              </Text>
-            </Box>
-            <Box marginBottom={5}>
-              <AlertMessage
-                title="Meðferð gagna"
-                message={
-                  <ul className={styles.ul}>
-                    <li>
-                      <Box marginLeft={1}>
-                        <Text>
-                          Hér er hægt að hlaða upp rannsóknargögnum til að sýna
-                          dómara.
-                        </Text>
-                      </Box>
-                    </li>
-                    <li>
-                      <Box marginLeft={1}>
-                        <Text>
-                          Skjölin eru eingöngu aðgengileg settum dómara í málinu
-                          og aðgengi að þeim lokast þegar dómari hefur
-                          úrskurðað.
-                        </Text>
-                      </Box>
-                    </li>
-                    <li>
-                      <Box marginLeft={1}>
-                        <Text>
-                          Skjölin verða ekki lögð fyrir eða flutt í málakerfi
-                          dómstóls nema annar hvor aðilinn kæri úrskurðinn.
-                        </Text>
-                      </Box>
-                    </li>
-                  </ul>
-                }
-                type="info"
-              />
-            </Box>
-            <Box marginBottom={3}>
-              <Text variant="h3" as="h3">
-                Rannsóknargögn
-              </Text>
-            </Box>
-            <Box marginBottom={[2, 2, 3]}>
-              <ContentBlock>
-                <InputFileUpload
-                  fileList={files}
-                  header="Dragðu skjöl hingað til að hlaða upp"
-                  buttonLabel="Velja skjöl til að hlaða upp"
-                  onChange={onChange}
-                  onRemove={onRemove}
-                  errorMessage=""
-                  showFileSize
-                />
-              </ContentBlock>
-            </Box>
-          </FormContentContainer>
-          <FormContentContainer isFooter>
-            <FormFooter
-              previousUrl={`${Constants.STEP_FOUR_ROUTE}/${workingCase.id}`}
-              nextUrl={`${Constants.STEP_SIX_ROUTE}/${workingCase.id}`}
-            />
-          </FormContentContainer>
-        </>
-      ) : null}
+      {workingCase ? <StepFiveForm case={workingCase} /> : null}
     </PageLayout>
   )
 }
