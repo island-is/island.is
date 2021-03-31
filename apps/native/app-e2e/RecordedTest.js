7d7f5cc22e3ec2508676b95db62a93574097f980
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2021-03-31 13:17:12 +0000

    feat: ci test mobile

--- apps/native/app-e2e/RecordedTest.js
+++
@@ -1,7 +0,0 @@
-describe('Recorded suite', () => {
-	it('My Recorded Test', async () => {
-		await element(by.id("BUTTON_LOGIN")).tap();
-		//vhhgg
-		await device.takeScreenshot("Screenshot 1");
-	})
-});
