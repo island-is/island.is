f5143013e96bebf020f119a59c03a29abf4acc50
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-04-07 13:10:35 +0000

    Fixes case sensitivity issue

--- apps/judicial-system/api/src/app/modules/file/dto/index.ts
+++ apps/judicial-system/api/src/app/modules/file/dto/index.ts
@@ -1,4 +1,4 @@
 export { CreatePresignedPostInput } from './createPresignedPost.input'
 export { DeleteFileInput } from './deleteFile.input'
 export { CreateFileInput } from './createFile.input'
-export { GetSignedUrlInput } from './getsignedUrl.input'
+export { GetSignedUrlInput } from './getSignedUrl.input'
