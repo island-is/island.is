5b72b9739ed13740bcd868a795e05a0c7f457bbc
Brian Johannessen <barabrian89@gmail.com>
2021-04-07 15:36:31 +0000

    remove file replacement code

--- libs/service-portal/education/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import { getStaticEnv } from '@island.is/utils/environment'
-
-export default {
-  production: true,
-  featureFlagSdkKey: getStaticEnv('SI_PUBLIC_CONFIGCAT_SDK_KEY'),
-}
--- libs/service-portal/education/src/environments/environment.ts
+++ libs/service-portal/education/src/environments/environment.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// When building for production, this file is replaced with `environment.prod.ts`.
-
 export default {
-  production: false,
   featureFlagSdkKey: 'YcfYCOwBTUeI04mWOWpPdA/KgCHhUk0_k2BdiKMaNh3qA',
 }
