f1e6069d1ed28fb033b24fc3f7adbb307499b55b
Valur Sverrisson <valur@hugsmidjan.is>
2021-04-09 14:49:13 +0000

    feat: move RegulationStatus to separate component

--- apps/web/screens/Regulations/RegulationDisplay.treat.ts
+++ apps/web/screens/Regulations/RegulationDisplay.treat.ts
@@ -1,19 +1,6 @@
 import { style, globalStyle } from 'treat'
 import { theme } from '@island.is/island-ui/theme'
-const { color, typography, spacing } = theme
-
-export const ball = style({
-  display: 'inline-block',
-  marginRight: spacing[2],
-  backgroundColor: color.mint600,
-  borderRadius: '50%',
-  fontSize: 16 / 18 + 'em',
-  width: '1em',
-  height: '1em',
-})
-export const ballRed = style({
-  backgroundColor: color.red600,
-})
+const { color, typography } = theme
 
 export const diffToggler = style({
   float: 'right',
@@ -21,10 +8,6 @@ export const diffToggler = style({
   ':hover': {
     textDecoration: 'underline',
   },
-})
-
-export const metaDate = style({
-  display: 'inline-block',
 })
 
 // ---------------------------------------------------------------------------
