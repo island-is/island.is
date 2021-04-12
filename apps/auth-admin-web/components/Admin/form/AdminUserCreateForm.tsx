63e49f3bb57adb1445ecff6eb55b433e4f7cdc14
unnarb <unnarb@gmail.com>
2021-04-12 10:12:34 +0000

    AdminUserCreateForm

--- apps/auth-admin-web/components/Admin/form/AdminUserCreateForm.tsx
+++ apps/auth-admin-web/components/Admin/form/AdminUserCreateForm.tsx
@@ -6,6 +6,8 @@ import { AdminAccessDTO } from './../../../entities/dtos/admin-acess.dto'
 import { AdminAccess } from './../../../entities/models/admin-access.model'
 import { AdminAccessService } from './../../../services/AdminAccessService'
 import ValidationUtils from './../../../utils/validation.utils'
+import TranslationUtils from './../../../utils/translation.utils'
+import { FormPage } from './../../../entities/common/Translation'
 interface Props {
   adminAccess: AdminAccessDTO
   handleSaveButtonClicked?: (admin: AdminAccess) => void
@@ -20,6 +22,9 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
   const { register, handleSubmit, errors, formState } = useForm<FormOutput>()
   const { isSubmitting } = formState
   const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [translation] = useState<FormPage>(
+    TranslationUtils.getFormPage('AdminUserCreateForm'),
+  )
   const admin = props.adminAccess
 
   useEffect(() => {
@@ -62,18 +67,22 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
     <div className="admin-user-create-form">
       <div className="admin-user-create-form__wrapper">
         <div className="admin-user-create-form__container">
-          <h1>{isEditing ? 'Edit Admin User' : 'Create a new Admin User'}</h1>
+          <h1>{isEditing ? translation.editTitle : translation.title}</h1>
           <div className="admin-user-create-form__container__form">
             <div className="admin-user-create-form__help">
-              Add user for the Admin UI by filling out the form
+              {translation.help}
             </div>
             <form onSubmit={handleSubmit(save)}>
               <div className="admin-user-create-form__container__fields">
                 <div className="admin-user-create-form__container__field">
-                  <label className="admin-user-create-form__label">
-                    National Id (Kennitala)
+                  <label
+                    className="admin-user-create-form__label"
+                    htmlFor="nationalId"
+                  >
+                    {translation.fields['nationalId'].label}
                   </label>
                   <input
+                    id="nationalId"
                     type="text"
                     name="admin.nationalId"
                     ref={register({
@@ -84,12 +93,14 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
                     })}
                     defaultValue={admin.nationalId}
                     className="admin-user-create-form__input"
-                    placeholder="0123456789"
+                    placeholder={translation.fields['nationalId'].placeholder}
                     maxLength={10}
-                    title="The nationalId (Kennitala) of the Admin User"
+                    title={translation.fields['nationalId'].helpText}
                     readOnly={isEditing}
                   />
-                  <HelpBox helpText="The nationalId (Kennitala) of the Admin User" />
+                  <HelpBox
+                    helpText={translation.fields['nationalId'].helpText}
+                  />
                   <ErrorMessage
                     as="span"
                     errors={errors}
@@ -99,10 +110,14 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
                 </div>
 
                 <div className="admin-user-create-form__container__field">
-                  <label className="admin-user-create-form__label">
-                    User email
+                  <label
+                    className="admin-user-create-form__label"
+                    htmlFor="email"
+                  >
+                    {translation.fields['email'].label}
                   </label>
                   <input
+                    id="email"
                     type="text"
                     ref={register({
                       required: true,
@@ -111,49 +126,64 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
                     name="admin.email"
                     defaultValue={admin.email ?? ''}
                     className="admin-user-create-form__input"
-                    title="The email of the admin user"
-                    placeholder="john@example.com"
+                    title={translation.fields['email'].helpText}
+                    placeholder={translation.fields['email'].placeholder}
                   />
                   <ErrorMessage
                     as="span"
                     errors={errors}
                     name="admin.email"
-                    message="Email is required and needs to be in a right format"
+                    message={translation.fields['email'].errorMessage}
                   />
-                  <HelpBox helpText="The email of the admin user" />
+                  <HelpBox
+                    helpText={translation.fields['email'].errorMessage}
+                  />
                 </div>
 
                 <div className="admin-user-create-form__container__field">
-                  <label className="admin-user-create-form__label">Scope</label>
+                  <label
+                    className="admin-user-create-form__label"
+                    htmlFor="scope"
+                  >
+                    {translation.fields['scope'].label}
+                  </label>
                   <select
+                    id="scope"
                     name="admin.scope"
                     ref={register({ required: true })}
-                    title="Select the appropriate Scope for this Admin User"
+                    title={translation.fields['scope'].helpText}
                   >
                     <option value="auth-admin-api.full_control" selected>
                       Full control
                     </option>
                   </select>
 
-                  <HelpBox helpText="Select the appropriate Scope for this Admin User" />
+                  <HelpBox helpText={translation.fields['scope'].helpText} />
                   <ErrorMessage
                     as="span"
                     errors={errors}
                     name="admin.scope"
-                    message="Select the appropriate Scope for this Admin User"
+                    message={translation.fields['scope'].errorMessage}
                   />
                 </div>
 
                 <div className="admin-user-create-formcontainer__checkbox__field hidden">
-                  <label className="admin-user-create-formlabel">Active</label>
+                  <label
+                    className="admin-user-create-formlabel"
+                    htmlFor="active"
+                  >
+                    {translation.fields['active'].label}
+                  </label>
                   <input
+                    id="active"
                     type="checkbox"
                     name="admin.active"
                     className="admin-user-create-formcheckbox"
                     defaultChecked={true}
                     ref={register}
+                    title={translation.fields['active'].helpText}
                   ></input>
-                  <HelpBox helpText="Sets if user is active or not" />
+                  <HelpBox helpText={translation.fields['active'].helpText} />
                 </div>
               </div>
 
@@ -164,7 +194,7 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
                     type="button"
                     onClick={props.handleCancel}
                   >
-                    Cancel
+                    {translation.cancelButton}
                   </button>
                 </div>
                 <div className="admin-user-create-form__button__container">
@@ -172,7 +202,7 @@ const AdminUserCreateForm: React.FC<Props> = (props: Props) => {
                     type="submit"
                     className="admin-user-create-form__button__save"
                     disabled={isSubmitting}
-                    value="Save"
+                    value={translation.saveButton}
                   />
                 </div>
               </div>
