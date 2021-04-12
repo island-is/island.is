d419a5850fd9d7ee0df10226f992c9c9115f693f
Andri Andresson <andri@sendiradid.is>
2021-04-12 13:38:51 +0000

    feat: institutions application (#3199)
    * feat: initialized institution application
    
    * feat: added applicationTypes & templateLoader
    
    * wip: application form
    
    working on SecondaryContact  & confirmationScreen
    
    * wip: secondaryContact
    
    added styles
    
    * wip: reviewScreen
    
    * api set up
    
    * wip: added constraints
    
    * email template
    
    * wip: attatchments
    
    * attachment list sent with email
    
    * wip: updated confirmationScreen
    
    * feat: added custom field description
    
    * feat: updated secondary contacts
    
    * refactor: project object + updated required fields
    
    * email templates
    
    * refactor: messages
    
    * refactor: messages cleanup
    
    * feat: fieldDescription rework
    
    * feat: added all fields to reviewScreen
    
    * feat: added localization to confirmationScreen
    
    * chore: linting
    
    * refactor: updated email template
    
    * feat: updated email templates + localization messages
    
    * chore: cleanup
    
    * chore: removing svg warnings
    
    * feat: added approved state with template and localization
    
    * chore: updated fallback recipient email
    
    * feat: added uploadBucket env to fileStorageModule
    
    * feat: added email sender- & recipient address env variables
    
    * cleanup: remove duplicate implementation
    
    * Update ApplicationTypes.ts
    
    * Update yarn.lock
    
    * chore: formatting
    
    * chore: formatting
    
    * Update .eslintrc
    
    * Update template.ts
    
    * chore: linting
    
    * chore: gitbook
    
    * chore: linting
    
    * chore: linting
    
    * chore: text update
    
    * chore: updated text
    
    * feat: added optional "required" prop to textFormField & buildTextField
    
    * feat: added extra field, otherRoles
    
    * chore: constraints update
    
    localization constraints variable names change + UI update
    
    * refactor: replaced lodash/get with application/core helper function
    
    * feat: simplify conditions + added max_file_size + added defaultValues
    
    * feat: updated attachment links
    
    * fix: attachments not in initial state - added refetch
    
    * feat: updated mailAttachments
    
    url has been removed from attachment answers
    
    * feat: added extract-strings workspace command
    
    * chore: updating gitbook
    
    * chore: linting
    
    * chore: text update
    
    * chore: fixing typo
    
    * feat: added generateSignedUrl method
    
    * feat: added signedUrl to attachments
    
    * chore: removing logs
    
    * refactor: renaming institution-application to institution-collaboration
    
    * chore: updated readme
    
    * chore: attachment cleanup
    
    * chore: removing unused mutation
    
    * chore: linting
    
    * Update SUMMARY.md
    
    * feat: added application attachments to email
    
    * chore: update readme
    
    Requested in review
    
    * feat: updated institution service config
    
    * feat: updated emails
    
    Added name to recipient & sender
    
    * chore: updated env section
    
    * refactor: updated & added env variables
    
    * fix: error when resolving config file path when running tests
    
    * chore: change link path to relative
    
    * chore: revert recipient mail env variables
    
    * chore: fixing typo
    
    * chore: remove todo comment
    
    * fix: added validation
    
    Making sure only authenticated user who match application.applicant is able to act as an applicant.
    
    Co-authored-by: Olafur <obmagnusson@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/institution-collaboration/src/fields/index.ts
@@ -0,0 +1,6 @@
+export { default as ConfirmationScreen } from './ConfirmationScreen/ConfirmationScreen'
+export { default as SecondaryContact } from './SecondaryContact/SecondaryContact'
+export { default as Constraints } from './Constraints/Constraints'
+
+export { default as ReviewScreen } from './ReviewScreen/ReviewScreen'
+export { default as FieldDescription } from './FieldDescription/FieldDescription'
