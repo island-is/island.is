e7b2d0b3343e8d66e8c8475b456f8e9fdc0a9b05
unnarb <unnarb@users.noreply.github.com>
2021-04-13 13:09:59 +0000

    feat('auth-admin-web'): Localization of Pages (#3481)
    * Adding localization to Pages
    
    * Fix validateDescription
    
    * Fix Client localization
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/auth-admin-web/pages/resources/api-scopes/index.tsx
+++ apps/auth-admin-web/pages/resources/api-scopes/index.tsx
@@ -1,9 +1,16 @@
 import ContentWrapper from '../../../components/Layout/ContentWrapper'
 import ApiScopeList from '../../../components/Resource/lists/ApiScopeList'
 import ResourcesTabsNav from '../../../components/Resource/nav/ResourcesTabsNav'
-import React from 'react'
+import React, { useEffect } from 'react'
+import LocalizationUtils from '../../../utils/localization.utils'
 
 const Index: React.FC = () => {
+  useEffect(() => {
+    document.title = LocalizationUtils.getPageTitle(
+      'resources.api-scopes.index',
+    )
+  }, [])
+
   return (
     <ContentWrapper>
       <ResourcesTabsNav />
