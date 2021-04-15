a673eee07ce508c382f9dd2d4a51c1bc94ca2bfa
unnarb <unnarb@gmail.com>
2021-04-15 13:03:23 +0000

    Show Environment

---
+++ apps/auth-admin-web/components/Layout/ShowEnvironment.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import LocalizationUtils from '../../utils/localization.utils'
+import { Localization } from '../../entities/common/Localization'
+import { Environment } from '../../entities/common/Environment'
+import { EnvironmentUtils } from './../../utils/environment.utils'
+
+const ShowEnvironment: React.FC = () => {
+  const [localization] = useState<Localization>(
+    LocalizationUtils.getLocalization(),
+  )
+  const [activeEnv, setActiveEnv] = useState(Environment.LOCAL)
+  useEffect(() => {
+    setActiveEnv(EnvironmentUtils.getEnvironment(document.URL))
+  }, [])
+
+  return (
+    <div className="environment">
+      <div
+        className={`environment__title ${
+          activeEnv === Environment.PROD ? 'danger' : ''
+        }`}
+      >
+        {localization.environment[activeEnv].title}
+      </div>
+      <div className="environment__description">
+        {localization.environment[activeEnv].description}
+      </div>
+      <div className="environment__help__text">
+        {localization.environment[activeEnv].helpText}
+      </div>
+    </div>
+  )
+}
+
+export default ShowEnvironment
