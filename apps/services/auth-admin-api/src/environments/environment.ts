a1172133ecb614768a8ab4331b7a023fc30ada85
Valur Einarsson <valure@fuglar.com>
2021-04-16 16:11:41 +0100

    Revert env file.

--- apps/services/auth-admin-api/src/environments/environment.ts
+++ apps/services/auth-admin-api/src/environments/environment.ts
@@ -1,12 +1,8 @@
 export const environment = {
-  // production: false,
-  // auth: {
-  //   issuer: 'https://identity-server.dev01.devland.is',
-  //   jwksUri:
-  //     'https://identity-server.dev01.devland.is/.well-known/openid-configuration/jwks',
-  // },
+  production: false,
   auth: {
-    issuer: 'https://localhost:6001',
-    jwksUri: 'http://localhost:6002/.well-known/openid-configuration/jwks',
+    issuer: 'https://identity-server.dev01.devland.is',
+    jwksUri:
+      'https://identity-server.dev01.devland.is/.well-known/openid-configuration/jwks',
   },
 }
