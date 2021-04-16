7d172ce91471f3e9b29d51113d720f3ac9d334e3
Sigga Hrafns <sigridur@kosmosogkaos.is>
2021-04-16 09:59:03 +0000

    Fixing lint

--- libs/application/templates/party-letter/src/assets/Logo.tsx
+++ libs/application/templates/party-letter/src/assets/Logo.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react'
 import { Box, Text, Hyphen } from '@island.is/island-ui/core'
 import { m } from '../lib/messages'
 import { useLocale } from '@island.is/localization'
-import { ReactComponent as CoatOfARms } from './coat_of_arms.svg'
+import CoatOfArms from './CoatOfArms'
 
 const Logo: FC = () => {
   const { formatMessage } = useLocale()
 
   return (
     <Box display="flex" alignItems="center">
-      <CoatOfARms title="smu" />
+      <CoatOfArms />
       <Box marginLeft={2}>
         <Text variant="eyebrow">{formatMessage(m.logo.service)}</Text>
         <Text variant="h5">
