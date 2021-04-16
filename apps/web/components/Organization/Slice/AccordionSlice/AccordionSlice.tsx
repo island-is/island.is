c94130e63c26e9b595a9250034eb30d5737ba93b
Brynjar Ingimarsson <brynjar@stefna.is>
2021-04-16 08:58:40 +0000

    Add missing border and padding on accordion slice

--- apps/web/components/Organization/Slice/AccordionSlice/AccordionSlice.tsx
+++ apps/web/components/Organization/Slice/AccordionSlice/AccordionSlice.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { AccordionCard, Box, Text } from '@island.is/island-ui/core'
+import {
+  AccordionCard,
+  Box,
+  GridContainer,
+  Text,
+} from '@island.is/island-ui/core'
 import { AccordionSlice as AccordionSliceSchema } from '@island.is/web/graphql/schema'
 import * as styles from './AccordionSlice.treat'
 import { richText, SliceType } from '@island.is/island-ui/contentful'
@@ -11,22 +16,29 @@ interface SliceProps {
 export const AccordionSlice: React.FC<SliceProps> = ({ slice }) => {
   return (
     <section key={slice.id} aria-labelledby={'sliceTitle-' + slice.id}>
-      <Text variant="h3" as="h2" marginBottom={2}>
-        {slice.title}
-      </Text>
-      {slice.accordionItems.map((item) => (
-        <Box paddingY={1}>
-          <AccordionCard
-            id={item.id}
-            label={item.title}
-            startExpanded={slice.accordionItems.length === 1}
-          >
-            <Box className={styles.accordionBox}>
-              {richText(item.content as SliceType[])}
-            </Box>
-          </AccordionCard>
-        </Box>
-      ))}
+      <Box
+        borderTopWidth="standard"
+        borderColor="standard"
+        paddingTop={[4, 4, 6]}
+        paddingBottom={[4, 4, 6]}
+      >
+        <Text variant="h3" as="h2" marginBottom={2}>
+          {slice.title}
+        </Text>
+        {slice.accordionItems.map((item) => (
+          <Box paddingY={1}>
+            <AccordionCard
+              id={item.id}
+              label={item.title}
+              startExpanded={slice.accordionItems.length === 1}
+            >
+              <Box className={styles.accordionBox}>
+                {richText(item.content as SliceType[])}
+              </Box>
+            </AccordionCard>
+          </Box>
+        ))}
+      </Box>
     </section>
   )
 }
