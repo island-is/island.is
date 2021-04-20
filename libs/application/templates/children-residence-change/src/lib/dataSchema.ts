7c057053d3aa9c6bb28563c8a98cf914e7543a20
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-04-20 16:18:58 +0000

    Add counterParty to dataschema

--- libs/application/templates/children-residence-change/src/lib/dataSchema.ts
+++ libs/application/templates/children-residence-change/src/lib/dataSchema.ts
@@ -1,6 +1,9 @@
 import { error } from './messages/index'
 import * as z from 'zod'
 
+const emailRegex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
+const isValidEmail = (value: string) => emailRegex.test(value)
+
 enum Duration {
   Permanent = 'permanent',
   Temporary = 'temporary',
@@ -27,7 +30,24 @@ export const dataSchema = z.object({
     .min(1, error.validation.selectChild.defaultMessage),
   residenceChangeReason: z.string().optional(),
   parentA: parentContactInfo,
-  counterParty: parentContactInfo,
+  counterParty: z
+    .object({
+      email: z
+        .string()
+        .refine((v) => (v && isValidEmail(v)) || v === '', {
+          message: error.validation.invalidEmail.defaultMessage,
+        })
+        .optional(),
+      phoneNumber: z
+        .string()
+        .refine((v) => (v && v.length === 7) || v === '', {
+          message: error.validation.invalidPhoneNumber.defaultMessage,
+        })
+        .optional(),
+    })
+    .refine((v) => v.email || v.phoneNumber, {
+      message: 'það verður að fylla inn annaðhvort',
+    }),
   parentB: parentContactInfo,
   approveTerms: terms,
   approveTermsParentB: terms,
