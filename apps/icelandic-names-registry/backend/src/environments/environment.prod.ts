b7d24914a8c2365f8c9a1cf89899eca0dc45c1c1
Kristján Albert <kal@kosmosogkaos.is>
2021-04-23 11:35:40 +0000

    fix(icelandic-name-registry): interface for environment (#3580)
    * interface for environemnt
    
    * refactor

--- apps/icelandic-names-registry/backend/src/environments/environment.prod.ts
+++ apps/icelandic-names-registry/backend/src/environments/environment.prod.ts
@@ -1,9 +1,13 @@
-export default {
+import baseEnvironment from './environment'
+
+const prodEnvironment: typeof baseEnvironment = {
   production: true,
   auth: {
-    issuer: process.env.IDENTITY_SERVER_ISSUER_URL,
-    audience: '@island.is',
-    jwksUri: process.env.IDENTITY_SERVER_JWKS_URI,
+    issuer: process.env.IDENTITY_SERVER_ISSUER_URL ?? '',
+    audience: '',
+    jwksUri: process.env.IDENTITY_SERVER_JWKS_URI ?? '',
   },
   allowedNationalIds: process.env.ALLOWED_NATIONAL_IDS ?? '',
 }
+
+export default prodEnvironment
