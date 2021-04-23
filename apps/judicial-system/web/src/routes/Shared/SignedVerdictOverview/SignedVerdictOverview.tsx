4de8c7db6c8dbe87b014630bee0862832bcd4a34
Ívar Oddsson <ivaroddsson@gmail.com>
2021-04-23 13:46:38 +0000

    Do not allow people to open files if case is appealable

--- apps/judicial-system/web/src/routes/Shared/SignedVerdictOverview/SignedVerdictOverview.tsx
+++ apps/judicial-system/web/src/routes/Shared/SignedVerdictOverview/SignedVerdictOverview.tsx
@@ -212,8 +212,11 @@ export const SignedVerdictOverview: React.FC = () => {
 
   const canCaseFilesBeOpened = () => {
     if (
-      workingCase?.accusedAppealDecision === CaseAppealDecision.APPEAL ||
-      workingCase?.prosecutorAppealDecision === CaseAppealDecision.APPEAL
+      workingCase?.isCaseAppealable === false &&
+      (workingCase?.accusedAppealDecision === CaseAppealDecision.APPEAL ||
+        workingCase?.prosecutorAppealDecision === CaseAppealDecision.APPEAL ||
+        !!workingCase?.accusedPostponedAppealDate ||
+        !!workingCase?.prosecutorPostponedAppealDate)
     ) {
       return true
     } else {
