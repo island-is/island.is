edcb3dd0e97e46f8fd1b32c87f81e649b2601355
Kristján Albert <kal@kosmosogkaos.is>
2021-04-23 12:52:13 +0000

    fix(icelandic-names-registry): default to string for auth values (#3582)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/icelandic-names-registry/backend/src/environments/environment.ts
+++ apps/icelandic-names-registry/backend/src/environments/environment.ts
@@ -1,5 +1,3 @@
-import { AuthConfig } from '@island.is/auth-nest-tools'
-
 export default {
   production: false,
   auth: {
@@ -7,6 +5,6 @@ export default {
     audience: '',
     jwksUri:
       'https://identity-server.dev01.devland.is/.well-known/openid-configuration/jwks',
-  } as AuthConfig,
+  },
   allowedNationalIds: process.env.ALLOWED_NATIONAL_IDS ?? '',
 }
