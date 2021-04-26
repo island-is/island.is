2a30f31940154311a0cc2c6a2aab36f57fe8f5db
Stefán Harald <stefanharald@gmail.com>
2021-04-26 16:50:25 +0000

    Removed comment

--- apps/endorsement-system/src/app/modules/endorsement/dto/bulkEndorsement.dto.ts
+++ apps/endorsement-system/src/app/modules/endorsement/dto/bulkEndorsement.dto.ts
@@ -2,7 +2,6 @@ import { IsString, IsArray } from 'class-validator'
 import { IsNationalId } from '../validators/isNationalId.decorator'
 
 export class BulkEndorsementDto {
-  // TODO: Validate nationalId here
   @IsArray()
   @IsString({ each: true })
   @IsNationalId({ each: true })
