c81e6079a15b126c13b4b9177af9026142c33e0f
Eirikur Nilsson <eirikur@nilsson.is>
2021-04-26 21:26:27 +0000

    Integrate auth/react into application-form app

--- apps/application-system/form/src/main.tsx
+++ apps/application-system/form/src/main.tsx
@@ -3,9 +3,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
-import { environment } from './environments'
+import { configure } from '@island.is/auth/react'
+import { ApplicationIdentityServerScope } from '@island.is/application/core'
 
+import { environment } from './environments'
 import App from './app/App'
+
+configure({
+  baseUrl: `${window.location.origin}/umsoknir`,
+  redirectPath: '/signin-oidc',
+  redirectPathSilent: '/silent/signin-oidc',
+  authority: environment.identityServer.authority,
+  client_id: 'island-is-1',
+  scope: `openid profile api_resource.scope ${ApplicationIdentityServerScope.read} ${ApplicationIdentityServerScope.write}`,
+  post_logout_redirect_uri: `${window.location.origin}`,
+})
 
 Sentry.init({
   dsn: environment.sentry.dsn,
