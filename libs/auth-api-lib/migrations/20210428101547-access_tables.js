b0fb298e26b6b5e1130dd7bcddec72cf354c15b1
unnarb <unnarb@gmail.com>
2021-04-28 11:08:15 +0000

    Delete seed, set export in migration

--- libs/auth-api-lib/migrations/20210428101547-access_tables.js
+++ libs/auth-api-lib/migrations/20210428101547-access_tables.js
@@ -4,7 +4,15 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.query(`
       BEGIN;
-      
+        
+        CREATE TABLE api_scope_user (
+          national_id VARCHAR NOT NULL,
+          email VARCHAR NOT NULL,
+          created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+          updated TIMESTAMP WITH TIME ZONE,
+          PRIMARY KEY (national_id)
+        );
+
         CREATE TABLE api_scope_user_access (
             national_id VARCHAR NOT NULL,
             scope VARCHAR NOT NULL,
@@ -12,16 +20,18 @@ module.exports = {
             modified TIMESTAMP WITH TIME ZONE,
             PRIMARY KEY (national_id, scope),
             CONSTRAINT FK_api_scope_user_access_api_scope FOREIGN KEY (scope)
-                REFERENCES public.api_scope (name)
+                REFERENCES public.api_scope (name),
+            CONSTRAINT FK_api_scope_user_access_api_scope_user FOREIGN KEY (national_id)
+                REFERENCES public.api_scope_user (national_id)
         );
 
-        CREATE TABLE api_scope_user (
-            national_id VARCHAR NOT NULL,
-            email VARCHAR NOT NULL,
-            created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-            updated TIMESTAMP WITH TIME ZONE,
-            PRIMARY KEY (national_id)
-        );
+        INSERT INTO api_scope_user (national_id, email)
+          SELECT national_id, email
+        FROM admin_access;
+
+        INSERT INTO api_scope_user_access (national_id, scope)
+          SELECT national_id, scope
+        FROM admin_access;
 
       COMMIT;
     `)
