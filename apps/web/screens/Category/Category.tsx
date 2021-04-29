716b23afffe3c40f1a58b5ab0cb26ea7d837e999
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-04-29 14:03:52 +0000

    Another link fix on Category screen

--- apps/web/screens/Category/Category.tsx
+++ apps/web/screens/Category/Category.tsx
@@ -345,15 +345,16 @@ const Category: Screen<CategoryProps> = ({
             <LinkContext.Provider
               value={{
                 linkRenderer: (href, children) => (
-                  <Link href={href} pureChildren skipTab>
+                  <Link href={href} skipTab>
                     {children}
                   </Link>
                 ),
               }}
             >
               <Text truncate>
-                <Link {...linkResolver('homepage')} passHref>
+                <a {...linkResolver('homepage')}>
                   <Button
+                    as="span"
                     preTextIcon="arrowBack"
                     preTextIconType="filled"
                     size="small"
@@ -362,7 +363,7 @@ const Category: Screen<CategoryProps> = ({
                   >
                     Ísland.is
                   </Button>
-                </Link>
+                </a>
               </Text>
             </LinkContext.Provider>
           </Box>
