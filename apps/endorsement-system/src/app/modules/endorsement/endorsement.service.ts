cd97775df50a07ab594387bb883095ad78e2a6d4
Stefán Harald <stefanharald@gmail.com>
2021-04-30 16:21:21 +0000

    Fixed text even more

--- apps/endorsement-system/src/app/modules/endorsement/endorsement.service.ts
+++ apps/endorsement-system/src/app/modules/endorsement/endorsement.service.ts
@@ -299,7 +299,7 @@ export class EndorsementService {
     )
 
     return this.endorsementModel.bulkCreate(endorsements, {
-      ignoreDuplicates: true, // this ignores existing endorsements in this list conflicting
+      ignoreDuplicates: true, // this ignores endorsements that would cause unique constraint errors when inserting into this list
       returning: true,
     })
   }
