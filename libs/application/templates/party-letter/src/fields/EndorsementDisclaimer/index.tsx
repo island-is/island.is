6f845caefd56cea568b14490e00cd2e3d0fda1fd
Sigga Hrafns <sigridur@kosmosogkaos.is>
2021-04-30 08:54:06 +0000

    Use stop using hardcoded id

--- libs/application/templates/party-letter/src/fields/EndorsementDisclaimer/index.tsx
+++ libs/application/templates/party-letter/src/fields/EndorsementDisclaimer/index.tsx
@@ -81,7 +81,7 @@ const EndorsementDisclaimer: FC<FieldBaseProps> = ({ application }) => {
     const success = await createEndorsement({
       variables: {
         input: {
-          listId: '9c0b4106-4213-43be-a6b2-ff324f4ba0c1',
+          listId: application.answers.endorsementListId,
         },
       },
     })
