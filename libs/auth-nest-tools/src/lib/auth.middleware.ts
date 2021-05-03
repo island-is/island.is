278e12bfa897d524ea55954cc5bacdf61f5a3293
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2021-05-03 16:00:34 +0000

    feat(core): implement audit module (#3419)
    * Add actor properties to User
    
    Refactor nest decorators.
    Add getCurrentUser helper.
    
    * Move application-system-api to use @CurrentUser
    
    * Remove unused CurrentRestUser
    
    * Fix application api e2e tests
    
    Implement a MockAuthGuard.
    
    * Audit module
    
    * Integrate audit log into application-system-api
    
    * CI fixes
    
    * Fix issues from main
    
    * Revert ApiHeader removal
    
    * Revert judicial system changes
    
    * Fix tests
    
    * Format and lint
    
    * Remove test config
    
    * Create UserMiddleware for OpenAPI requests
    
    Proxy user ip and user-agent to internal services.
    
    * Update auth-nest-tools to support user vs client authentication tokens
    
    * Switch to IdsUserGuard in most places
    
    * Integrate AuthMiddleware in DocumentProvider
    
    * Format and fix test
    
    * Remove auth-api audit integration for now
    
    * Finish audit service integration in application system
    
    * Fix tests
    
    * Address feedback
    
    * Rename access token body parameter in service portal
    
    * Use x-real-ip header
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/auth-nest-tools/src/lib/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { Auth } from './auth'
+
+// These types are copied from our OpenAPI generated api clients.
+type FetchAPI = WindowOrWorkerGlobalScope['fetch']
+
+interface FetchParams {
+  url: string
+  init: RequestInit
+}
+
+interface RequestContext {
+  fetch: FetchAPI
+  url: string
+  init: RequestInit
+}
+
+interface Middleware {
+  pre?(context: RequestContext): Promise<FetchParams | void>
+}
+
+/**
+ * Middleware that adds user authorization and information to OpenAPI Client requests.
+ */
+export class AuthMiddleware implements Middleware {
+  constructor(private auth: Auth) {}
+
+  async pre(context: RequestContext) {
+    context.init.headers = Object.assign({}, context.init.headers, {
+      authorization: this.auth.authorization,
+      'User-Agent': this.auth.userAgent,
+      'X-Real-IP': this.auth.ip,
+    })
+  }
+}
