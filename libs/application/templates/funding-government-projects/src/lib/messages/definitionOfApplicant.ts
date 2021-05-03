7143a6fa951051c8a98bf82bd43fc8db1259902e
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2021-05-03 14:21:36 +0000

    feat: Application funding government projects/screen wireframes (#3696)
    * bootstrapping application for government funding
    
    * codeowners update
    
    * Delete .eslintrc
    
    * Delete yarn.lock
    
    * re add yarnlock and fix eslint
    
    * remove a lot of the bloat
    
    * minimal refactor
    
    * yarn gitbook
    
    * localization
    
    * fix casing
    
    * removing unused schema properties
    
    * missing lifecycle
    
    * including jest file to fix test error
    
    * missing test output in workspace.json
    
    * refactor of application bootstrap
    
    * fixes
    
    * wrong file as preset
    
    * sections and translations set up
    
    * remove unused template
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/funding-government-projects/src/lib/messages/definitionOfApplicant.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl'
+
+export const definitionOfApplicant = {
+  general: defineMessages({
+    pageTitle: {
+      id: `affgp.application:section.definitionOfApplicant.pageTitle`,
+      defaultMessage: 'Yfirlit',
+      description: 'Definition of applicant section title',
+    },
+  }),
+  labels: defineMessages({}),
+}
---
+++ libs/application/templates/funding-government-projects/src/lib/messages/informationAboutInsitituion.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl'
+
+export const informationAboutInstitution = {
+  general: defineMessages({
+    pageTitle: {
+      id: `affgp.application:section.informationAboutInstitution.pageTitle`,
+      defaultMessage: 'Upplýsingar um stofnun',
+      description: 'Information About Institution page title',
+    },
+  }),
+  labels: defineMessages({}),
+}
