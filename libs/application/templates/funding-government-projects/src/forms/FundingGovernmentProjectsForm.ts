f2f96cc8ec5364a6b20c8c0429659a1e19f6b1c4
Ólafur Björn Magnússon <obmagnusson@gmail.com>
2021-05-04 13:57:52 +0000

    feat(funding-government-projects): Bootstrap Api Module template and email submissions (#3708)
    * init template
    
    * add submit
    
    * overview template adjusted
    
    * wording
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/funding-government-projects/src/forms/FundingGovernmentProjectsForm.ts
+++ libs/application/templates/funding-government-projects/src/forms/FundingGovernmentProjectsForm.ts
@@ -5,6 +5,9 @@ import {
   FormModes,
   buildDescriptionField,
   buildCustomField,
+  buildMultiField,
+  DefaultEvents,
+  buildSubmitField,
 } from '@island.is/application/core'
 import {
   section,
@@ -58,10 +61,29 @@ export const FundingGovernmentProjectsForm: Form = buildForm({
       id: 'overview',
       title: section.overview,
       children: [
-        buildDescriptionField({
-          id: 'placeholderId4',
+        buildMultiField({
+          id: 'overviewMultifield',
           title: overview.general.pageTitle,
-          description: 'Umsókn',
+          description: overview.general.pageTitle.description,
+          children: [
+            buildCustomField({
+              id: 'overviewCustomField',
+              title: overview.general.pageTitle,
+              description: overview.general.pageTitle.description,
+              component: 'Overview',
+            }),
+            buildSubmitField({
+              id: 'overview.submitField',
+              title: '',
+              actions: [
+                {
+                  event: DefaultEvents.SUBMIT,
+                  name: overview.labels.submit,
+                  type: 'primary',
+                },
+              ],
+            }),
+          ],
         }),
       ],
     }),
