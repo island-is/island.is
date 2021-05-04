8af29b90c3d8ba0c02c93fe7e0babb0cf405299d
Ívar Oddsson <ivaroddsson@gmail.com>
2021-05-04 20:28:48 +0000

    Added a skip to content anchor

--- apps/judicial-system/web/pages/_document.tsx
+++ apps/judicial-system/web/pages/_document.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { SkipToContent } from '../src/shared-components'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="is">
+        <SkipToContent />
         <Head />
         <body>
-          <Main />
+          <div id="main">
+            <Main />
+          </div>
           {/* Here we will mount our modal portal */}
           <div id="modal" />
           <NextScript />
