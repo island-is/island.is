71cbe6c13945359c319d8f67c87714d8b35c24cd
Andri Andresson <andriandresson@gmail.com>
2021-05-04 14:58:26 +0000

    Feat: added submitted Screen

--- libs/application/templates/funding-government-projects/src/lib/messages/submitted.ts
+++ libs/application/templates/funding-government-projects/src/lib/messages/submitted.ts
@@ -8,5 +8,24 @@ export const submitted = {
       description: 'Submitted page title',
     },
   }),
-  labels: defineMessages({}),
+  labels: defineMessages({
+    descriptionBulletOne: {
+      id: `affgp.application:section.submitted.descriptionBulletOne`,
+      defaultMessage:
+        'Við munum nú fara yfir verkefnið og við sendum á þig svör innan tíðar.',
+      description: 'submitted descriptionBulletOne',
+    },
+    descriptionBulletTwo: {
+      id: `affgp.application:section.submitted.descriptionBulletTwo`,
+      defaultMessage:
+        'Við verðum í sambandi ef okkur vantar frekari upplýsingar.',
+      description: 'submitted descriptionBulletTwo',
+    },
+    descriptionBulletThree: {
+      id: `affgp.application:section.submitted.descriptionBulletThree`,
+      defaultMessage:
+        'Ef þú þarft frekari upplýsingar þá getur þú haft samband í síma {tel} eða á netfangið',
+      description: 'submitted descriptionBulletThree',
+    }
+  }),
 }
