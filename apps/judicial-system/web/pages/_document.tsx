69b8c3578ab4f4c60c43d9c8a374d470acd69a49
Ívar Oddsson <ivaroddsson@gmail.com>
2021-05-04 15:05:33 +0000

    Add lang to html tag

--- apps/judicial-system/web/pages/_document.tsx
+++ apps/judicial-system/web/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="is">
         <Head />
         <body>
           <Main />
