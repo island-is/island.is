1f96cacb16eab6e3f5bc76261cce3e6dfe12e829
unnarb <unnarb@gmail.com>
2021-05-05 15:31:34 +0000

    DelegationScopeController

---
+++ libs/auth-api-lib/src/lib/entities/dto/delegation-scope.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsString } from 'class-validator'
+
+export class DelegationScopeDTO {
+  @IsString()
+  @ApiProperty()
+  delegationId!: string
+  @IsString()
+  @ApiProperty()
+  scopeName!: string
+}
---
+++ libs/auth-api-lib/src/lib/services/delegation-scope.service.ts
@@ -0,0 +1,52 @@
+import { Logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { Inject, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize'
+import { DelegationScopeDTO } from '../entities/dto/delegation-scope.dto'
+import { DelegationScope } from '../entities/models/delegation-scope.model'
+
+@Injectable()
+export class DelegationScopeService {
+  constructor(
+    @InjectModel(DelegationScope)
+    private delegationScopeModel: typeof DelegationScope,
+    @Inject(LOGGER_PROVIDER)
+    private logger: Logger,
+  ) {}
+
+  async create(
+    delegationScope: DelegationScopeDTO,
+  ): Promise<DelegationScope | null> {
+    this.logger.debug('Creating new delegation scope')
+    return await this.delegationScopeModel.create({ ...delegationScope })
+  }
+
+  async findAll(
+    delegationId: string,
+    scopeName: string | null = null,
+  ): Promise<DelegationScope[] | null> {
+    if (scopeName) {
+      const response = await this.delegationScopeModel.findOne({
+        where: { delegationId: delegationId, scopeName: scopeName },
+      })
+      return <DelegationScope[]>[response]
+    }
+    return await this.delegationScopeModel.findAll({
+      where: { delegationId: delegationId },
+    })
+  }
+
+  async delete(
+    delegationId: string,
+    scopeName?: string | null,
+  ): Promise<number> {
+    if (scopeName) {
+      return await this.delegationScopeModel.destroy({
+        where: { delegationId: delegationId, scopeName: scopeName },
+      })
+    }
+
+    return await this.delegationScopeModel.destroy({
+      where: { delegationId: delegationId },
+    })
+  }
+}
