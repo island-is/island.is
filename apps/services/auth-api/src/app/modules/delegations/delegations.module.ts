808f57488429852354b51bef5d8d87a1f6b64332
unnarb <unnarb@gmail.com>
2021-05-06 13:16:24 +0000

    add delegationscope to module

--- apps/services/auth-api/src/app/modules/delegations/delegations.module.ts
+++ apps/services/auth-api/src/app/modules/delegations/delegations.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
-import { DelegationsService, Delegation } from '@island.is/auth-api-lib'
+import {
+  DelegationsService,
+  Delegation,
+  DelegationScope,
+} from '@island.is/auth-api-lib'
 import { DelegationsController } from './delegations.controller'
 import { RskModule } from '@island.is/clients/rsk/v2'
 import { RskConfig } from './rsk.config'
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Delegation]),
+    SequelizeModule.forFeature([Delegation, DelegationScope]),
     RskModule.register(RskConfig.get()),
   ],
   controllers: [DelegationsController],
