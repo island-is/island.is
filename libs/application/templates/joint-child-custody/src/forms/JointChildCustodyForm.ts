6c74c32a74d6182a9f65fcee7d7aa5650e5ce56a
RikkiAbb <rikardarnar@gmail.com>
2021-05-06 15:49:01 +0000

    removes syslumenn logo

--- libs/application/templates/joint-child-custody/src/forms/JointChildCustodyForm.ts
+++ libs/application/templates/joint-child-custody/src/forms/JointChildCustodyForm.ts
@@ -5,13 +5,11 @@ import {
   FormModes,
   buildTextField,
 } from '@island.is/application/core'
-import Logo from '../../assets/Logo'
 import * as m from '../lib/messages'
 
 export const JointChildCustodyForm: Form = buildForm({
   id: 'JointChildCustodyForm',
   title: m.application.name,
-  logo: Logo,
   mode: FormModes.APPLYING,
   children: [
     buildSection({
