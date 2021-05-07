8435bfc6eea4d2f9dde6d2294b518d72673e2c3e
Valur Einarsson <valure@fuglar.com>
2021-05-07 12:48:32 +0100

    Add current actor decorator.

---
+++ libs/auth-nest-tools/src/lib/current-actor.decorator.ts
@@ -0,0 +1,29 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { logger } from '@island.is/logging'
+
+import { User } from './user'
+import { getRequest } from './getRequest'
+
+export const getCurrentActor = (context: ExecutionContext): User => {
+  const request = getRequest(context)
+
+  const user = request.user
+
+  if (!user) {
+    logger.warn(
+      'No user authentication found. Did you forget to add IdsUserGuard?',
+    )
+    throw new UnauthorizedException()
+  }
+  return user.actor ?? user
+}
+
+export const CurrentActor = createParamDecorator(
+  (options: unknown, context: ExecutionContext): User => {
+    return getCurrentActor(context)
+  },
+)
