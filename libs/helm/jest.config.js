8699f484a989771ba7866ae4ab7bea66b07a818d
Petar Shomov <petar@andes.is>
2021-05-10 15:08:06 +0000

    refactoring

--- libs/helm/jest.config.js
+++ libs/helm/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/helm',
+  coverageDirectory: '../../coverage/libs/helm',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
   displayName: 'Config DSL',
 }
