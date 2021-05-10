e38e789e88306a0f8ce7108903861f8c605d0163
Petar Shomov <petar@andes.is>
2021-05-10 14:52:43 +0000

    Moving the service configuration from Helm to the monorepo

---
+++ apps/web/infra/web.ts
@@ -0,0 +1,57 @@
+import { ref, service, ServiceBuilder } from '../../../infra/src/dsl/dsl'
+
+export const serviceSetup = (services: {
+  api: ServiceBuilder<'api'>
+}): ServiceBuilder<'web'> => {
+  const web = service('web')
+  web
+    .namespace('islandis')
+    .env({
+      API_URL: ref((h) => `http://${h.svc(services.api)}`),
+      TRACKING_DOMAIN: {
+        dev: 'beta.dev01.devland.is',
+        staging: 'beta.staging01.devland.is',
+        prod: 'island.is',
+      },
+      DISABLE_API_CATALOGUE: { dev: 'false', staging: 'false', prod: 'false' },
+      DISABLE_SYSLUMENN_PAGE: { dev: 'false', staging: 'false', prod: 'false' },
+      DISABLE_ORGANIZATION_CHATBOT: {
+        dev: 'false',
+        staging: 'false',
+        prod: 'false',
+      },
+      DISABLE_REGULATIONS_PAGE: {
+        dev: 'false',
+        staging: 'false',
+        prod: 'true',
+      },
+    })
+    .secrets({
+      SENTRY_DSN: '/k8s/web/SENTRY_DSN',
+    })
+    .ingress({
+      primary: {
+        host: {
+          dev: ['beta'],
+          staging: ['beta'],
+          prod: ['', 'www.island.is'],
+        },
+        extraAnnotations: {
+          dev: {},
+          staging: {},
+          prod: {
+            'nginx.ingress.kubernetes.io/enable-global-auth': 'false',
+          },
+        },
+        paths: ['/'],
+      },
+    })
+    .liveness('/liveness')
+    .readiness('/readiness')
+    .extraAttributes({
+      dev: {},
+      staging: { basicAuth: '/k8s/web/basic_auth' },
+      prod: {},
+    })
+  return web
+}
