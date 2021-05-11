e9310628464f2107975c8ccf076839e62bd642d2
Brynjar Ingimarsson <brynjar@stefna.is>
2021-05-11 16:21:07 +0000

    Add CTA option to accordion slice

--- apps/web/screens/queries/fragments.ts
+++ apps/web/screens/queries/fragments.ts
@@ -410,10 +410,15 @@ export const slices = gql`
     __typename
     id
     title
+    type
     accordionItems {
       title
       content {
         ...HtmlFields
+      }
+      link {
+        url
+        text
       }
     }
   }
