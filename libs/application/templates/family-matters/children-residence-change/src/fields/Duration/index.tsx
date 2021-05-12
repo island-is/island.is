16246d1f0ca0e741f9a70586aa976c339bbc898f
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-05-12 13:54:48 +0000

    Extract duration field to core library

--- libs/application/templates/family-matters/children-residence-change/src/fields/Duration/index.tsx
+++ libs/application/templates/family-matters/children-residence-change/src/fields/Duration/index.tsx
@@ -1,78 +1,25 @@
-import React, { useState } from 'react'
-import { useIntl } from 'react-intl'
-import addMonths from 'date-fns/addMonths'
-import { useLocale } from '@island.is/localization'
-import { DescriptionText } from '@island.is/application/templates/family-matters-core/components'
+import React from 'react'
+import { Duration } from '@island.is/application/templates/family-matters-core/fields'
 import { duration } from '../../lib/messages'
 import { CRCFieldBaseProps } from '../../types'
-import { Box } from '@island.is/island-ui/core'
-import {
-  DatePickerController,
-  RadioController,
-} from '@island.is/shared/form-fields'
 
 const typeInput = 'selectDuration.type'
 const dateInput = 'selectDuration.date'
 
 export const selectDurationInputs = [typeInput, dateInput]
 
-export type ValidAnswers = 'permanent' | 'temporary' | undefined
-const Duration = ({ application, errors }: CRCFieldBaseProps) => {
-  const { formatMessage } = useIntl()
-  const { lang } = useLocale()
-
+const CRCDuration = ({ application, errors }: CRCFieldBaseProps) => {
   const durationTypeError = errors?.selectDuration?.type
   const durationDateError = errors?.selectDuration?.date
 
-  const [statefulAnswer, setStatefulAnswer] = useState<string | undefined>(
-    application.answers?.selectDuration?.type,
-  )
   return (
-    <>
-      <Box marginTop={3} marginBottom={5}>
-        <DescriptionText text={duration.general.description} />
-      </Box>
-      <Box marginTop={3} marginBottom={2}>
-        <RadioController
-          id={`${typeInput}`}
-          defaultValue={
-            statefulAnswer !== undefined ? [statefulAnswer] : undefined
-          }
-          options={[
-            {
-              value: 'permanent',
-              label: formatMessage(duration.permanentInput.label),
-              subLabel: formatMessage(duration.permanentInput.subLabel),
-              tooltip: formatMessage(duration.permanentInput.tooltip),
-            },
-            {
-              value: 'temporary',
-              label: formatMessage(duration.temporaryInput.label),
-              subLabel: formatMessage(duration.temporaryInput.subLabel),
-              tooltip: formatMessage(duration.temporaryInput.tooltip),
-            },
-          ]}
-          onSelect={(newAnswer) => setStatefulAnswer(newAnswer as ValidAnswers)}
-          largeButtons
-          error={durationTypeError}
-        />
-
-        {statefulAnswer === 'temporary' && (
-          <Box>
-            <DatePickerController
-              id={`${dateInput}`}
-              backgroundColor="blue"
-              locale={lang}
-              label={formatMessage(duration.dateInput.label)}
-              placeholder={formatMessage(duration.dateInput.placeholder)}
-              error={durationDateError}
-              minDate={addMonths(new Date(), 6)}
-            />
-          </Box>
-        )}
-      </Box>
-    </>
+    <Duration
+      typeInput={{ id: typeInput, error: durationTypeError }}
+      dateInput={{ id: dateInput, error: durationDateError }}
+      translations={duration}
+      currentAnswer={application.answers?.selectDuration?.type}
+    />
   )
 }
 
-export default Duration
+export default CRCDuration
