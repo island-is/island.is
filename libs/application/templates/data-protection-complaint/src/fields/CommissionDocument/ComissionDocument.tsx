7e745ee98d53e905ff23a4675adee5e1f980f6ab
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2021-05-17 09:26:32 +0000

    wrap anchor around button

--- libs/application/templates/data-protection-complaint/src/fields/CommissionDocument/ComissionDocument.tsx
+++ libs/application/templates/data-protection-complaint/src/fields/CommissionDocument/ComissionDocument.tsx
@@ -7,7 +7,7 @@ import { info } from '../../lib/messages'
 export const CommissionDocument: FC<FieldBaseProps> = () => {
   const { formatMessage } = useLocale()
   return (
-    <Box>
+    <Box marginBottom={2}>
       <Text>
         {formatMessage(
           info.general.commissionsPageDescriptionOfDocumentTextOne,
@@ -19,17 +19,17 @@ export const CommissionDocument: FC<FieldBaseProps> = () => {
             info.general.commissionsPageDescriptionOfDocumentTextTwo,
           )}
         </Text>
-        <Button variant="ghost" icon="open" iconType="outline">
-          <a
-            href={
-              'https://assets.ctfassets.net/8k0h54kbe6bj/MB2Kv7jA0vGPpsB9EUM8H/3ce4660155c428ca6ff6d34819731ce0/umbod-utfyllanleg.pdf'
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <a
+          href={
+            'https://assets.ctfassets.net/8k0h54kbe6bj/MB2Kv7jA0vGPpsB9EUM8H/3ce4660155c428ca6ff6d34819731ce0/umbod-utfyllanleg.pdf'
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="ghost" icon="open" iconType="outline">
             {formatMessage(info.general.comissionPageButtonName)}
-          </a>
-        </Button>
+          </Button>
+        </a>
       </Inline>
     </Box>
   )
