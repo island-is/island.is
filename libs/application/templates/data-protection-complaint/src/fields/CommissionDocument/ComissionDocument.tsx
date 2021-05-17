dc9c730671529dd595fb7f5cda39324736f89a8c
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2021-05-17 11:41:00 +0000

    feat(AS): added document for power of attorney (#3843)
    * added document for power of attorney
    
    * remove todo
    
    * fix translation
    
    * wrap anchor around button
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/data-protection-complaint/src/fields/CommissionDocument/ComissionDocument.tsx
@@ -0,0 +1,36 @@
+import { FieldBaseProps } from '@island.is/application/core'
+import { Box, Button, Inline, Text } from '@island.is/island-ui/core'
+import { useLocale } from '@island.is/localization'
+import React, { FC } from 'react'
+import { info } from '../../lib/messages'
+
+export const CommissionDocument: FC<FieldBaseProps> = () => {
+  const { formatMessage } = useLocale()
+  return (
+    <Box marginBottom={2}>
+      <Text>
+        {formatMessage(
+          info.general.commissionsPageDescriptionOfDocumentTextOne,
+        )}
+      </Text>
+      <Inline alignY="center" space={3}>
+        <Text>
+          {formatMessage(
+            info.general.commissionsPageDescriptionOfDocumentTextTwo,
+          )}
+        </Text>
+        <a
+          href={
+            'https://assets.ctfassets.net/8k0h54kbe6bj/MB2Kv7jA0vGPpsB9EUM8H/3ce4660155c428ca6ff6d34819731ce0/umbod-utfyllanleg.pdf'
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="ghost" icon="open" iconType="outline">
+            {formatMessage(info.general.comissionPageButtonName)}
+          </Button>
+        </a>
+      </Inline>
+    </Box>
+  )
+}
