47bf0034fd405be0c9cd522c651709ad28ced430
Valur Einarsson <valure@fuglar.com>
2021-05-18 09:22:58 +0100

    Move env vars to environment. Export config as const.

--- apps/services/auth-public-api/src/app/modules/delegations/rsk.config.ts
+++ apps/services/auth-public-api/src/app/modules/delegations/rsk.config.ts
@@ -1,26 +1,17 @@
 import { RskModuleConfig } from '@island.is/clients/rsk/v2'
-import { createXRoadAPIPath, XRoadMemberClass } from '@island.is/utils/api'
+import { createXRoadAPIPath } from '@island.is/utils/api'
 import { Base64 } from 'js-base64'
+import { environment } from '../../../environments'
 
-const XROAD_BASE_PATH = process.env.XROAD_BASE_PATH_WITH_ENV ?? ''
-const XROAD_MEMBER_CLASS = XRoadMemberClass.GovernmentInstitution
-const XROAD_MEMBER_CODE = process.env.XROAD_RSK_MEMBER_CODE ?? ''
-const XROAD_API_PATH = process.env.XROAD_RSK_API_PATH ?? ''
-const XROAD_CLIENT_ID = process.env.XROAD_RSK_CLIENT_ID ?? ''
-const USERNAME = process.env.RSK_USERNAME ?? ''
-const PASSWORD = process.env.RSK_PASSWORD ?? ''
-
-export class RskConfig {
-  static get(): RskModuleConfig {
-    return <RskModuleConfig>{
-      xRoadPath: createXRoadAPIPath(
-        XROAD_BASE_PATH,
-        XROAD_MEMBER_CLASS,
-        XROAD_MEMBER_CODE,
-        XROAD_API_PATH,
-      ),
-      xRoadClient: XROAD_CLIENT_ID,
-      basicAuth: `${Base64.encode(`${USERNAME}:${PASSWORD}`)}`,
-    }
-  }
+export const RskConfig: RskModuleConfig = {
+  xRoadPath: createXRoadAPIPath(
+    environment.rsk.xroad.basePath,
+    environment.rsk.xroad.memberClass,
+    environment.rsk.xroad.memberCode,
+    environment.rsk.xroad.apiPath,
+  ),
+  xRoadClient: environment.rsk.xroad.clientId,
+  basicAuth: `${Base64.encode(
+    `${environment.rsk.username}:${environment.rsk.password}`,
+  )}`,
 }
--- apps/services/auth-public-api/src/environments/environment.ts
+++ apps/services/auth-public-api/src/environments/environment.ts
@@ -1,3 +1,5 @@
+import { XRoadMemberClass } from '@island.is/utils/api'
+
 export default {
   production: false,
   auth: {
@@ -5,4 +7,15 @@ export default {
     jwksUri: 'http://localhost:6002/.well-known/openid-configuration/jwks',
   },
   port: 3333,
+  rsk: {
+    xroad: {
+      basePath: process.env.XROAD_BASE_PATH_WITH_ENV,
+      memberClass: XRoadMemberClass.GovernmentInstitution,
+      memberCode: process.env.XROAD_RSK_MEMBER_CODE,
+      apiPath: process.env.XROAD_RSK_API_PATH,
+      clientId: process.env.XROAD_RSK_CLIENT_ID,
+    },
+    username: process.env.RSK_USERNAME,
+    password: process.env.RSK_PASSWORD,
+  },
 }
