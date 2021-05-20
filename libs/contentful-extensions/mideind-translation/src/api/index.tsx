6b9e1525662e9cf5b5673ee8aa790e7b212eb3fe
kksteini <77672665+kksteini@users.noreply.github.com>
2021-05-20 16:55:52 +0000

    chore(contentful): Added the source code for the already hosted extensions on Contentful (#3716)
    * Added the source code for the already hosted extensions on Contentful
    
    * Simplified extension names. Moved the contentful-tranlsation-extension into the contentful-extensions folder
    
    * Fixed some typos
    
    * Removed unnecessary files
    
    * Updated README-s
    
    * Update README.md
    
    * Update README.md
    
    * Bare minimum introduction of the extensions to the workspace
    
    * Ran the formatter after adding images in the Github editor
    
    * Formatting fix
    
    * Ran 'yarn gitbook'
    
    * Made extensions consistent with previous extension in the MR
    
    * Fixed preset paths
    
    * The contentful sdk is not to be tested outside of its own environment
    
    * Fixed forgotten field type
    
    * Default showcase tests not needed
    
    * Added the Contentful build artifacts to the gitignore
    
    * Intermediate changes for mideind feedback
    
    * Ran the formatter
    
    * Added a Readme hierarchy
    
    * Ran 'yarn gitbook'
    
    * Intermediate cleanup for the mideind translation feedback
    
    * Ran the formatter
    
    * Ran 'yarn gitbook' but this time without 'yarn nx format:write'
    
    * Accidentally had a node_modules folder in the extension space when running gitbook
    
    * Added the contentful-extension directory readme to the nxignore
    
    * Updated CODEOWNERS
    
    * Updated the Miðeind extension to send corrected texts on publish
    
    * Formatter
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/contentful-extensions/mideind-translation/src/api/index.tsx
@@ -0,0 +1,55 @@
+const base = 'https://stafraentisland.greynir.is/translate'
+
+const defaultParams = {
+  sourceLanguageCode: 'is',
+  targetLanguageCode: 'en',
+}
+
+async function translateTexts(texts: string[]) {
+  var translations = []
+  const body: any = {
+    contents: texts,
+    ...defaultParams,
+  }
+
+  const response = await fetch(`${base}/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-Key': process.env.MIDEIND_TOKEN || '',
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.json())
+
+  for (const translation of response.translations) {
+    translations.push(translation.translatedText.trim())
+  }
+
+  return translations
+}
+
+async function sendTexts(
+  iceTexts: string[],
+  enTexts: string[],
+  reference: string,
+) {
+  const body: any = {
+    machineTranslatedText: '', // Required even if empty
+    translationReference: 1 || reference, // Reference to be accepted later by Miðeind
+    originalText: iceTexts.join(' '), // String expected, not array
+    correctedText: enTexts.join(' '), // String expected, not array
+    languagePair: 'is-en',
+    model: 'transformer-base',
+  }
+
+  const response = await fetch(`${base}/corrected`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-Key': process.env.MIDEIND_TOKEN || '',
+    },
+    body: JSON.stringify(body),
+  })
+}
+
+export { translateTexts, sendTexts }
