04d18ef8dccccfb93a65fba7ee6f199a347ba438
Brynjar Ingimarsson <brynjar@stefna.is>
2021-05-20 10:58:00 +0000

    Add new slice 'Overview Links' to backend

---
+++ libs/api/domains/cms/src/lib/models/overviewLinks.model.ts
@@ -0,0 +1,27 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { IOverviewLinks } from '../generated/contentfulTypes'
+import { Link, mapLink } from './link.model'
+import { IntroLinkImage, mapIntroLinkImage } from './introLinkImage.model'
+import { SystemMetadata } from 'api-cms-domain'
+
+@ObjectType()
+export class OverviewLinks {
+  @Field(() => ID)
+  id!: string
+
+  @Field(() => [IntroLinkImage])
+  overviewLinks?: Array<IntroLinkImage>
+
+  @Field(() => Link, { nullable: true })
+  link?: Link | null
+}
+
+export const mapOverviewLinks = ({
+  sys,
+  fields,
+}: IOverviewLinks): SystemMetadata<OverviewLinks> => ({
+  typename: 'OverviewLinks',
+  id: sys.id,
+  overviewLinks: (fields.overviewLinks ?? []).map(mapIntroLinkImage),
+  link: fields.link ? mapLink(fields.link) : null,
+})
