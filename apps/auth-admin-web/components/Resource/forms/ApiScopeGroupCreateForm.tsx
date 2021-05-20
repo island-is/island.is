2d5dfe0dce502938ff92bd3d6676cc75ee9d3623
unnarb <unnarb@gmail.com>
2021-05-20 16:01:39 +0000

    Api groups p2

---
+++ apps/auth-admin-web/components/Resource/forms/ApiScopeGroupCreateForm.tsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
+import HelpBox from '../../common/HelpBox'
+import { ResourcesService } from '../../../services/ResourcesService'
+import LocalizationUtils from '../../../utils/localization.utils'
+import { FormControl } from '../../../entities/common/Localization'
+import ValidationUtils from './../../../utils/validation.utils'
+import { ApiScopeGroupDTO } from './../../../entities/dtos/api-scope-group.dto'
+import { ApiScopeGroup } from './../../../entities/models/api-scope-group.model'
+
+interface Props {
+  apiScopeGroup?: ApiScopeGroup
+  handleNext?: () => void
+  handleBack?: () => void
+  handleChanges?: () => void
+  handleNewGroupAdded: () => void
+}
+
+const ApiScopeGroupCreateForm: React.FC<Props> = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState,
+  } = useForm<ApiScopeGroupDTO>()
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [localization] = useState<FormControl>(
+    LocalizationUtils.getFormControl('ApiScopeGroupCreateForm'),
+  )
+  useEffect(() => {
+    if (props.apiScopeGroup) {
+      setIsEditing(true)
+    }
+  }, [props.apiScopeGroup])
+
+  const save = async (group: ApiScopeGroupDTO): Promise<void> => {
+    const response = isEditing
+      ? await ResourcesService.updateApiScopeGroup(
+          group,
+          props.apiScopeGroup.id,
+        )
+      : await ResourcesService.createApiScopeGroup(group)
+
+    if (response) {
+      props.handleNewGroupAdded()
+    }
+  }
+
+  return (
+    <div className="api-scope-group">
+      <div className="api-scope-group__wrapper">
+        <div className="api-scope-group__container">
+          <h1>{localization.title}</h1>
+          <div className="api-scope-group__container__form">
+            <div className="api-scope-group__help">{localization.help}</div>
+            <form onSubmit={handleSubmit(save)}>
+              <div className="api-scope-group__container__fields">
+                <div className="api-scope-group__container__field">
+                  <label htmlFor="name" className="api-scope-group__label">
+                    {localization.fields['name'].label}
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    ref={register({
+                      required: true,
+                      validate: ValidationUtils.validateIdentifier,
+                    })}
+                    defaultValue={props.apiScopeGroup.name}
+                    className="api-resource-form__input"
+                    placeholder={localization.fields['name'].placeholder}
+                    maxLength={10}
+                    title={localization.fields['name'].helpText}
+                  />
+                  <HelpBox helpText={localization.fields['name'].helpText} />
+                  <ErrorMessage
+                    as="span"
+                    errors={errors}
+                    name="name"
+                    message={localization.fields['name'].errorMessage}
+                  />
+                </div>
+                <div className="api-scope-group__container__field">
+                  <label
+                    htmlFor="description"
+                    className="api-scope-group__label"
+                  >
+                    {localization.fields['description'].label}
+                  </label>
+                  <input
+                    type="text"
+                    name="description"
+                    ref={register({
+                      required: true,
+                      validate: ValidationUtils.validateDescription,
+                    })}
+                    defaultValue={props.apiScopeGroup.description}
+                    className="api-resource-form__input"
+                    placeholder={localization.fields['description'].placeholder}
+                    maxLength={10}
+                    title={localization.fields['description'].helpText}
+                  />
+                  <HelpBox
+                    helpText={localization.fields['description'].helpText}
+                  />
+                  <ErrorMessage
+                    as="span"
+                    errors={errors}
+                    name="name"
+                    message={localization.fields['description'].errorMessage}
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="api-scope-group__buttons__container">
+              <div className="api-scope-group__button__container">
+                <button
+                  type="button"
+                  className="api-scope-group__button__cancel"
+                  onClick={props.handleBack}
+                  title={localization.buttons['cancel'].helpText}
+                >
+                  {localization.buttons['cancel'].text}
+                </button>
+              </div>
+              <div className="api-scope-group__button__container">
+                <button
+                  type="button"
+                  className="api-scope-group__button__save"
+                  onClick={props.handleNext}
+                  title={localization.buttons['save'].helpText}
+                >
+                  {localization.buttons['save'].text}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ApiScopeGroupCreateForm
