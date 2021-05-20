cbb301e044ab469dbbb8f4ab94aa3e886a576e8c
unnarb <unnarb@gmail.com>
2021-05-20 10:53:11 +0000

    groupId nullable

--- libs/auth-api-lib/src/lib/entities/models/api-scope.model.ts
+++ libs/auth-api-lib/src/lib/entities/models/api-scope.model.ts
@@ -57,7 +57,7 @@ export class ApiScope extends Model<ApiScope> {
   })
   @ForeignKey(() => ApiScopeGroup)
   @ApiProperty()
-  groupId!: string
+  groupId?: string
 
   @Column({
     type: DataType.BOOLEAN,
