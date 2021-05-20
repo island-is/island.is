24530de689e6058c319daf1d5bd9e5a614fe46ed
Þórður Hafliðason <tolleinn@gmail.com>
2021-05-20 13:20:21 +0000

    Remove wrong class validation

--- libs/api/domains/finance/src/lib/dto/getOverview.input.ts
+++ libs/api/domains/finance/src/lib/dto/getOverview.input.ts
@@ -1,15 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql'
-import { IsString, Min } from 'class-validator'
+import { IsString } from 'class-validator'
 
 @InputType()
 export class GetFinancialOverviewInput {
   @Field()
   @IsString()
-  @Min(1)
   OrgID!: string
 
   @Field()
   @IsString()
-  @Min(1)
   chargeTypeID!: string
 }
