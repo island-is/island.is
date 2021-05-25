716bff423ac0f3c2956009739c05abbccd524aa5
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-05-25 09:45:20 +0000

    Use graphql context where appropriate

--- libs/auth-nest-tools/src/lib/current-user.decorator.ts
+++ libs/auth-nest-tools/src/lib/current-user.decorator.ts
@@ -3,15 +3,27 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
 import { logger } from '@island.is/logging'
 
 import { User } from './user'
 import { getRequest } from './getRequest'
 
-export const getCurrentUser = (context: ExecutionContext): User => {
-  const request = getRequest(context)
+export const getCurrentUser = (
+  context: ExecutionContext & { contextType?: string },
+): User => {
+  const resolveUser = () => {
+    if (context.contextType === 'graphql') {
+      const ctx = GqlExecutionContext.create(context)
+      const { req } = ctx.getContext()
+      return req.user
+    } else {
+      const request = getRequest(context)
+      return request.user
+    }
+  }
 
-  const user = request.user
+  const user = resolveUser()
   if (!user) {
     logger.warn(
       'No user authentication found. Did you forget to add IdsUserGuard?',
