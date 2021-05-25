1061d49498bb35a8bf16f66b8d5e2810996ff789
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2021-05-25 15:48:20 +0000

    Adding module dependency on auth and audit modules. Update env config.

--- apps/services/documents/src/app/app.module.ts
+++ apps/services/documents/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { SequelizeConfigService } from './sequelizeConfig.service'
 import { DocumentProviderModule } from './modules/document-provider/document-provider.module'
+import { AuditModule } from '@island.is/nest/audit'
+import { AuthModule } from '@island.is/auth-nest-tools'
+import { environment } from '../environments'
 
 @Module({
   imports: [
+    AuditModule.forRoot(environment.audit),
+    AuthModule.register(environment.auth),
     SequelizeModule.forRootAsync({
       useClass: SequelizeConfigService,
     }),
--- apps/services/documents/src/environments/environment.ts
+++ apps/services/documents/src/environments/environment.ts
@@ -1,11 +1,27 @@
 const devConfig = {
   production: false,
   port: 3369,
+  audit: {
+    defaultNamespace: '@island.is/documents',
+  },
+  auth: {
+    issuer: 'https://identity-server.dev01.devland.is',
+    audience: '@island.is',
+  },
 }
 
 const prodConfig = {
   production: true,
   port: 3333,
+  audit: {
+    defaultNamespace: '@island.is/documents',
+    groupName: process.env.AUDIT_GROUP_NAME,
+    serviceName: 'services-documents',
+  },
+  auth: {
+    issuer: process.env.IDENTITY_SERVER_ISSUER_URL,
+    audience: '@island.is',
+  },
 }
 
 export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig
