4014448557b4323cd13aba381a63430a621bdf58
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-05-25 12:15:30 +0000

    Format

--- libs/auth-nest-tools/src/lib/getRequest.ts
+++ libs/auth-nest-tools/src/lib/getRequest.ts
@@ -1,7 +1,9 @@
 import { ExecutionContext } from '@nestjs/common'
 import { GqlExecutionContext } from '@nestjs/graphql'
 
-export const getRequest = (context: ExecutionContext & { contextType?: string }) => {
+export const getRequest = (
+  context: ExecutionContext & { contextType?: string },
+) => {
   if (context.contextType === 'graphql') {
     const ctx = GqlExecutionContext.create(context)
     return ctx.getContext().req
