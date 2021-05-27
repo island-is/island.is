df6873b8a5c7d660330912051afc9c6ec6a7ea5e
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-05-27 09:09:31 +0000

    Updates migration to copy the contents of prosecutor fields to the new court fields

--- apps/judicial-system/backend/migrations/20210520084959-update-case.js
+++ apps/judicial-system/backend/migrations/20210520084959-update-case.js
@@ -4,24 +4,40 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) =>
       Promise.all([
-        queryInterface.addColumn(
-          'case',
-          'court_case_facts',
-          {
-            type: Sequelize.TEXT,
-            allowNull: true,
-          },
-          { transaction: t },
-        ),
-        queryInterface.addColumn(
-          'case',
-          'court_legal_arguments',
-          {
-            type: Sequelize.TEXT,
-            allowNull: true,
-          },
-          { transaction: t },
-        ),
+        queryInterface
+          .addColumn(
+            'case',
+            'court_case_facts',
+            {
+              type: Sequelize.TEXT,
+              allowNull: true,
+            },
+            { transaction: t },
+          )
+          .then(() =>
+            queryInterface.sequelize.query(
+              `UPDATE "case" \
+               SET "court_case_facts" = "case_facts"`,
+              { transaction: t },
+            ),
+          ),
+        queryInterface
+          .addColumn(
+            'case',
+            'court_legal_arguments',
+            {
+              type: Sequelize.TEXT,
+              allowNull: true,
+            },
+            { transaction: t },
+          )
+          .then(() =>
+            queryInterface.sequelize.query(
+              `UPDATE "case" \
+               SET "court_legal_arguments" = "legal_arguments"`,
+              { transaction: t },
+            ),
+          ),
       ]),
     )
   },
