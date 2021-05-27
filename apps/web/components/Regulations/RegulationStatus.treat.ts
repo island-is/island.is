9aa269c61e88960f93d912a2e2b556e48238bdcf
mar <mar.nospam@anomy.net>
2021-05-27 10:11:22 +0000

    feat: Add yellow <Ball/> type for outdated/historic/future view modes

--- apps/web/components/Regulations/RegulationStatus.treat.ts
+++ apps/web/components/Regulations/RegulationStatus.treat.ts
@@ -1,6 +1,4 @@
 import { style } from 'treat'
-import { theme } from '@island.is/island-ui/theme'
-const { color, spacing } = theme
 
 export const printText = style({
   '@media': {
@@ -11,19 +9,6 @@ export const printText = style({
       display: 'block !important',
     },
   },
-})
-
-export const ball = style({
-  display: 'inline-block',
-  marginRight: spacing[2],
-  backgroundColor: color.mint600,
-  borderRadius: '50%',
-  fontSize: 16 / 18 + 'em',
-  width: '1em',
-  height: '1em',
-})
-export const ballRed = style({
-  backgroundColor: color.red600,
 })
 
 export const metaDate = style({
