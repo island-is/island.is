03d4b9d73741fe0a2615f8bc53433690966c3c3d
Eirikur Nilsson <eirikur@nilsson.is>
2021-05-28 16:43:20 +0000

    Get actor information from userInfo

--- apps/service-portal/src/components/UserDelegations/UserDelegations.tsx
+++ apps/service-portal/src/components/UserDelegations/UserDelegations.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react'
-import { getUserManager, useAuth, User } from '@island.is/auth/react'
+import React from 'react'
+import { useAuth } from '@island.is/auth/react'
 import { Stack, Text, TopicCard } from '@island.is/island-ui/core'
 import { useLocale } from '@island.is/localization'
 import {
@@ -7,54 +7,31 @@ import {
   useActorDelegationsQuery,
 } from '@island.is/service-portal/graphql'
 
-// The actor name and nationalId should be added to userinfo endpoint so we
-// can get rid of this mess.
-interface JoseUtil {
-  parseJwt(
-    token: string,
-  ): {
-    payload: {
-      nationalId: string
-      act?: {
-        nationalId: string
-      }
-    }
-  }
-}
-const getActorNationalId = (userInfo: User | null) => {
-  if (!userInfo) {
-    return null
-  }
-  const jose = (getUserManager() as any)._joseUtil as JoseUtil
-  const jwt = jose.parseJwt(userInfo.access_token)
-  return jwt.payload.act?.nationalId ?? jwt.payload.nationalId
-}
-
 export const UserDelegations = () => {
   const { formatMessage } = useLocale()
   const { userInfo, switchUser } = useAuth()
   const { data } = useActorDelegationsQuery()
   const currentNationalId = userInfo?.profile.nationalId as string
-  const actorNationalId = useMemo(() => getActorNationalId(userInfo), [
-    userInfo,
-  ])
-  const hasData = data && data.authActorDelegations.length > 0
-  const isDelegation = currentNationalId !== actorNationalId
+  const actor = userInfo?.profile.act
 
-  if (!hasData && !isDelegation) {
+  // Loading or no delegations.
+  if (!data || data.authActorDelegations.length === 0) {
     return null
   }
 
-  let delegations = [] as ActorDelegationsQuery['authActorDelegations']
-  if (data) {
-    delegations.push(...data.authActorDelegations)
-  }
-  if (isDelegation && actorNationalId !== null) {
+  let delegations = [...data.authActorDelegations]
+
+  if (actor) {
+    // Remove the current delegation from the list.
     delegations = delegations.filter(
       (delegation) => delegation.fromNationalId !== currentNationalId,
     )
-    // Don't have the name yet. This is a temporary label until that is fixed.
-    delegations.unshift({ fromName: 'Ég', fromNationalId: actorNationalId })
+
+    // The server does not return a delegation for the authenticated user, so we add it manually.
+    delegations.unshift({
+      fromName: actor.name,
+      fromNationalId: actor.nationalId,
+    })
   }
 
   return (
