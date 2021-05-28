6a54ac32071d54c288fe170e28659a411afa88a6
arnorhs <arnor@kosmosogkaos.is>
2021-05-28 17:07:46 +0000

    feat(giftcard): Adding a new type of gift card (ferðagjöf 2 - 2021 version) (#3998)
    * Adding a new type of gift card (ferðagjöf 2 - 2021 version)
    
    * translation strings changes
    
    * configcat
    
    * removing configcat and using vanilla env variable
    
    * version time
    
    * revert translations
    
    * some string changes and redirect frontpage to island.is/ferdagjof
    
    * trying to fix e2e test
    
    * routing keys for sns
    
    * formatting
    
    * format

--- apps/gjafakort/web-e2e/src/integration/app.spec.ts
+++ apps/gjafakort/web-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 describe('gjafakort-web', () => {
-  it('should display welcome message', () => {
+  it('should redirect to /ferdagjof', () => {
     cy.visit('/')
-    cy.get('h1').contains('Gjöf til ferðalaga innanlands')
+    cy.url().should('be.equal', 'https://island.is/ferdagjof')
   })
 })
