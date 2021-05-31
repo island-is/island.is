2f0e135d286a3f5e6470b112ce064a38711e5204
unnarb <unnarb@gmail.com>
2021-05-31 15:04:32 +0000

    Add to models

--- apps/auth-admin-web/entities/dtos/api-scope-group.dto.ts
+++ apps/auth-admin-web/entities/dtos/api-scope-group.dto.ts
@@ -2,8 +2,10 @@ export class ApiScopeGroupDTO {
   constructor() {
     // TODO: Remove this when implemented in Auth-Admin-Web
     this.domainName = '@island.is'
+    this.displayName = 'Temporary'
   }
   name!: string
+  displayName!: string
   description!: string
   domainName!: string
 }
