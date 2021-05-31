ad58ea7a484d6f144f847004bec742783801886d
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2021-05-31 13:17:50 +0000

    Remove documents scope export from index as the changes were removed, to be added later.

--- libs/auth/scopes/src/index.ts
+++ libs/auth/scopes/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/application.scope'
 export * from './lib/auth.scope'
 export * from './lib/authAdmin.scope'
-export * from './lib/documents.scope'
 export * from './lib/userProfile.scope'
