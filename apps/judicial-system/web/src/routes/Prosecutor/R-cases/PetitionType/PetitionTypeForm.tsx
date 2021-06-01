e9e1b1330c0af52c6342b1ca99255998bf010fe8
Ívar Oddsson <ivaroddsson@gmail.com>
2021-06-01 14:28:47 +0000

    Add footer to petition type step

--- apps/judicial-system/web/src/routes/Prosecutor/R-cases/PetitionType/PetitionTypeForm.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/R-cases/PetitionType/PetitionTypeForm.tsx
@@ -3,6 +3,7 @@ import { Box, Input, Select, Text } from '@island.is/island-ui/core'
 import {
   BlueBox,
   FormContentContainer,
+  FormFooter,
 } from '@island.is/judicial-system-web/src/shared-components'
 import useCase from '@island.is/judicial-system-web/src/utils/hooks/useCase'
 import { Case } from '@island.is/judicial-system/types'
@@ -11,6 +12,7 @@ import {
   validateAndSendToServer,
 } from '@island.is/judicial-system-web/src/utils/formHelper'
 import { petitionTypes } from './PetitionTypes'
+import * as constants from '@island.is/judicial-system-web/src/utils/constants'
 
 interface Props {
   workingCase: Case
@@ -23,62 +25,75 @@ const PetitionTypeForm: React.FC<Props> = (props) => {
   const { updateCase } = useCase()
 
   return (
-    <FormContentContainer>
-      <Box marginBottom={7}>
-        <Text as="h1" variant="h1">
-          Rannsóknarheimild
-        </Text>
-      </Box>
-      <Box component="section" marginBottom={5}>
-        <Box marginBottom={3}>
-          <Text as="h3" variant="h3">
-            Efni kröfu
+    <>
+      <FormContentContainer>
+        <Box marginBottom={7}>
+          <Text as="h1" variant="h1">
+            Rannsóknarheimild
           </Text>
         </Box>
-        <BlueBox>
-          <Box marginBottom={2}>
-            <Select
-              name="petition-type"
-              options={petitionTypes}
-              label="Tegund kröfu"
-              placeholder="Veldu tegund kröfu"
+        <Box component="section" marginBottom={5}>
+          <Box marginBottom={3}>
+            <Text as="h3" variant="h3">
+              Efni kröfu
+            </Text>
+          </Box>
+          <BlueBox>
+            <Box marginBottom={2}>
+              <Select
+                name="petition-type"
+                options={petitionTypes}
+                label="Tegund kröfu"
+                placeholder="Veldu tegund kröfu"
+                required
+              />
+            </Box>
+            <Input
+              data-testid="petition-description"
+              name="petition-description"
+              label="Efni kröfu"
+              placeholder="Skráðu efni kröfu"
+              defaultValue={''}
+              errorMessage={petitionDescriptionEM}
+              hasError={petitionDescriptionEM !== ''}
+              onChange={(event) =>
+                removeTabsValidateAndSet(
+                  'petitionDescription',
+                  event,
+                  ['empty'],
+                  workingCase,
+                  setWorkingCase,
+                  petitionDescriptionEM,
+                  setPetitionDescriptionEM,
+                )
+              }
+              onBlur={(event) =>
+                validateAndSendToServer(
+                  'petitionDescription',
+                  event.target.value,
+                  ['empty'],
+                  workingCase,
+                  updateCase,
+                  setPetitionDescriptionEM,
+                )
+              }
               required
             />
-          </Box>
-          <Input
-            data-testid="petition-description"
-            name="petition-description"
-            label="Efni kröfu"
-            placeholder="Skráðu efni kröfu"
-            defaultValue={''}
-            errorMessage={petitionDescriptionEM}
-            hasError={petitionDescriptionEM !== ''}
-            onChange={(event) =>
-              removeTabsValidateAndSet(
-                'petitionDescription',
-                event,
-                ['empty'],
-                workingCase,
-                setWorkingCase,
-                petitionDescriptionEM,
-                setPetitionDescriptionEM,
-              )
-            }
-            onBlur={(event) =>
-              validateAndSendToServer(
-                'petitionDescription',
-                event.target.value,
-                ['empty'],
-                workingCase,
-                updateCase,
-                setPetitionDescriptionEM,
-              )
-            }
-            required
-          />
-        </BlueBox>
-      </Box>
-    </FormContentContainer>
+          </BlueBox>
+        </Box>
+      </FormContentContainer>
+      <FormContentContainer isFooter>
+        <FormFooter
+          previousUrl={constants.REQUEST_LIST_ROUTE}
+          onNextButtonClick={() => console.log('hero')}
+          nextIsLoading={false}
+          nextIsDisabled={false}
+          nextButtonText={
+            workingCase.id === '' ? 'Stofna kröfu' : 'Halda áfram'
+          }
+        />
+      </FormContentContainer>
+    </>
   )
 }
 
