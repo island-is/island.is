2ec8fba8f5f18528688629646bfbf2a0c3d45232
arnorhs <arnor@kosmosogkaos.is>
2021-06-01 12:26:31 +0000

    format

--- apps/gjafakort/api/src/services/versionConfiguration.ts
+++ apps/gjafakort/api/src/services/versionConfiguration.ts
@@ -3,7 +3,8 @@ interface Version {
   type: 'gjafakort-user' | 'gjafakort-user-2'
 }
 
-export const getVersionConfiguration = async (): Promise<Version> => Promise.resolve({
-  yearBornLimit: 2003,
-  type: 'gjafakort-user-2',
-})
+export const getVersionConfiguration = async (): Promise<Version> =>
+  Promise.resolve({
+    yearBornLimit: 2003,
+    type: 'gjafakort-user-2',
+  })
