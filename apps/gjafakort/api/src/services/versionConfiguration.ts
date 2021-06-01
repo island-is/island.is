8faa9a7a9a151555a8b1633b56ce8bfa2bc332e8
arnorhs <arnor@kosmosogkaos.is>
2021-06-01 11:08:45 +0000

    chore: removing configcat configuration and environment flag check for the new giftcard, making the new one the default

--- apps/gjafakort/api/src/services/versionConfiguration.ts
+++ apps/gjafakort/api/src/services/versionConfiguration.ts
@@ -1,39 +1,9 @@
-import { createClient, IJSAutoPollOptions, DataGovernance } from 'configcat-js'
-import { environment } from '../environments'
-
 interface Version {
   yearBornLimit: number
   type: 'gjafakort-user' | 'gjafakort-user-2'
 }
 
-const v1: Version = {
-  yearBornLimit: 2002,
-  type: 'gjafakort-user',
-}
-
-const v2: Version = {
+export const getVersionConfiguration = async (): Promise<Version> => Promise.resolve({
   yearBornLimit: 2003,
   type: 'gjafakort-user-2',
-}
-
-const IS_FERDAGJOF_2_ENABLED = 'isferdagjof2enabled'
-
-const sdkKey = environment.configCat.sdkKey
-if (!sdkKey) {
-  throw new Error(
-    'Trying to initialize configcat client without configCat.sdkKey environment variable',
-  )
-}
-
-const configCat = eval('require')('configcat-node').createClient(sdkKey, {
-  dataGovernance: DataGovernance.EuOnly,
 })
-
-export const getVersionConfiguration = async (): Promise<Version> => {
-  const isFerdagjof2Enabled = await configCat.getValueAsync(
-    IS_FERDAGJOF_2_ENABLED,
-    false,
-  )
-
-  return isFerdagjof2Enabled ? v2 : v1
-}
