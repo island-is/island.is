9bee910b3128eb6da6cfc5b638e306c15dbf998a
Ívar Oddsson <ivaroddsson@gmail.com>
2021-06-01 15:10:15 +0000

    Add PageLayout to Defendant screen

--- apps/judicial-system/web/src/routes/Prosecutor/R-cases/Defendant/Defendant.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/R-cases/Defendant/Defendant.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { CaseQuery } from '@island.is/judicial-system-web/graphql'
+import { PageLayout } from '@island.is/judicial-system-web/src/shared-components'
+import {
+  CaseData,
+  ProsecutorSubsections,
+  Sections,
+} from '@island.is/judicial-system-web/src/types'
+import { Case } from '@island.is/judicial-system/types'
+import { useRouter } from 'next/router'
 
 const Defendant = () => {
-  return <p>hreer</p>
+  const router = useRouter()
+  const id = router.query.id
+
+  const [workingCase, setWorkingCase] = useState<Case>()
+  const { data, loading } = useQuery<CaseData>(CaseQuery, {
+    variables: { input: { id: id } },
+    fetchPolicy: 'no-cache',
+    skip: !id,
+  })
+
+  useEffect(() => {
+    document.title = 'Rannsóknarheimild - Réttarvörslugátt'
+  }, [])
+
+  return (
+    <PageLayout
+      activeSection={
+        workingCase?.parentCase ? Sections.EXTENSION : Sections.PROSECUTOR
+      }
+      activeSubSection={ProsecutorSubsections.CUSTODY_PETITION_STEP_ONE}
+      isLoading={loading}
+      notFound={id !== undefined && data?.case === undefined}
+      isExtension={workingCase?.parentCase && true}
+      decision={workingCase?.decision}
+      parentCaseDecision={workingCase?.parentCase?.decision}
+      caseType={workingCase?.type}
+      caseId={workingCase?.id}
+    >
+      <p>hreer</p>
+    </PageLayout>
+  )
 }
 
 export default Defendant
