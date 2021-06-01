9775e390037b7653ecdf5b65b18ba4b3807af34e
unnarb <unnarb@gmail.com>
2021-06-01 09:45:54 +0000

    adding Api-Scope Module to app.module

--- apps/services/auth-public-api/src/app/modules/resources/api-scope.module.ts
+++ apps/services/auth-public-api/src/app/modules/resources/api-scope.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
-import { ApiScope, ResourcesService } from '@island.is/auth-api-lib'
+import {
+  IdentityResource,
+  IdentityResourceUserClaim,
+  ApiScope,
+  ApiScopeUserClaim,
+  ApiResource,
+  ApiResourceUserClaim,
+  ApiResourceScope,
+  ApiResourceSecret,
+  ApiScopeUserAccess,
+  ApiScopeUser,
+  ApiScopeGroup,
+  Domain,
+  ResourcesService,
+} from '@island.is/auth-api-lib'
 import { ApiScopeController } from './api-scope.controller'
 
 @Module({
-  imports: [SequelizeModule.forFeature([ApiScope])],
+  imports: [
+    SequelizeModule.forFeature([
+      IdentityResource,
+      IdentityResourceUserClaim,
+      ApiScope,
+      ApiScopeUserClaim,
+      ApiResource,
+      ApiResourceUserClaim,
+      ApiResourceScope,
+      ApiResourceSecret,
+      ApiScopeUserAccess,
+      ApiScopeUser,
+      ApiScopeGroup,
+      Domain,
+    ]),
+  ],
   controllers: [ApiScopeController],
   providers: [ResourcesService],
 })
