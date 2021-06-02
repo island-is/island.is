78edaa5d6e736959de53f38b4d4a88353e14dba2
Stefán Harald <stefanharald@gmail.com>
2021-06-02 10:36:45 +0000

    Added all metadata fields to export

--- libs/api/domains/endorsement-system/src/lib/models/endorsementMetadata.model.ts
+++ libs/api/domains/endorsement-system/src/lib/models/endorsementMetadata.model.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 import graphqlTypeJson from 'graphql-type-json'
+import { EndorsementMetadataSignedTagsEnum } from '../enums/endorsementMetadataSignedTags.enum'
 
 @ObjectType()
 export class EndorsementMetadata {
@@ -11,4 +12,10 @@ export class EndorsementMetadata {
 
   @Field({ nullable: true })
   invalidated!: boolean | null
+
+  @Field({ nullable: true })
+  bulkEndorsement!: boolean | null
+
+  @Field(() => [EndorsementMetadataSignedTagsEnum], { nullable: true })
+  signedTags!: EndorsementMetadataSignedTagsEnum[] | null
 }
