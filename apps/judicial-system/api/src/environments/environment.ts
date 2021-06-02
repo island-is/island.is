dbd78eeceff15d47f8193a4a30f30c66b68a0a3c
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-06-02 12:35:51 +0000

    Fixes strict typescript issues

--- apps/judicial-system/api/src/environments/environment.ts
+++ apps/judicial-system/api/src/environments/environment.ts
@@ -88,8 +88,8 @@ const prodConfig = {
     samlEntryPoint: process.env.SAML_ENTRY_POINT,
     audience: process.env.AUTH_AUDIENCE,
     allowAuthBypass: process.env.ALLOW_AUTH_BYPASS === 'true',
-    jwtSecret: process.env.AUTH_JWT_SECRET,
-    secretToken: process.env.SECRET_TOKEN,
+    jwtSecret: process.env.AUTH_JWT_SECRET || '',
+    secretToken: process.env.SECRET_TOKEN || '',
   },
   auditTrail: {
     useGenericLogger: process.env.AUDIT_TRAIL_USE_GENERIC_LOGGER === 'true',
@@ -98,15 +98,15 @@ const prodConfig = {
     region: process.env.AUDIT_TRAIL_REGION,
   },
   xRoad: {
-    basePathWithEnv: process.env.XROAD_BASE_PATH_WITH_ENV,
-    clientId: process.env.XROAD_CLIENT_ID,
-    clientCert: process.env.XROAD_CLIENT_CERT,
-    clientKey: process.env.XROAD_CLIENT_KEY,
-    clientCa: process.env.XROAD_CLIENT_PEM,
+    basePathWithEnv: process.env.XROAD_BASE_PATH_WITH_ENV || '',
+    clientId: process.env.XROAD_CLIENT_ID || '',
+    clientCert: process.env.XROAD_CLIENT_CERT || '',
+    clientKey: process.env.XROAD_CLIENT_KEY || '',
+    clientCa: process.env.XROAD_CLIENT_PEM || '',
   },
   courtClientOptions: {
-    apiPath: process.env.XROAD_COURT_API_PATH,
-    memberCode: process.env.XROAD_COURT_MEMBER_CODE,
+    apiPath: process.env.XROAD_COURT_API_PATH || '',
+    memberCode: process.env.XROAD_COURT_MEMBER_CODE || '',
     serviceOptions: JSON.parse(
       process.env.COURTS_CREDENTIALS || '{}',
     ) as CourtClientServiceOptions,
