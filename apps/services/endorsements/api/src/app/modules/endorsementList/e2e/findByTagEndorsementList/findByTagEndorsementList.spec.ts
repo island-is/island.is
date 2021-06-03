3d31d66d77841512e827da467a5067525815f1bc
Stefán Harald <stefanharald@gmail.com>
2021-06-03 08:14:57 +0000

    Fixed tags tests

--- apps/services/endorsements/api/src/app/modules/endorsementList/e2e/findByTagEndorsementList/findByTagEndorsementList.spec.ts
+++ apps/services/endorsements/api/src/app/modules/endorsementList/e2e/findByTagEndorsementList/findByTagEndorsementList.spec.ts
@@ -11,32 +11,34 @@ beforeAll(async () => {
 })
 
 describe('findByTagEndorsementList', () => {
-  it(`GET /endorsement-list?tag should return validation error when called with a non existing tag`, async () => {
+  it(`GET /endorsement-list?tags should return validation error when called with a non existing tag`, async () => {
     const response = await request(app.getHttpServer())
-      .get('/endorsement-list?tag=thisTagIsUsedInE2ETests')
+      .get(
+        `/endorsement-list?tags[]=thisTagIsUsedInE2ETests&tags[]=${EndorsementTag.PARTY_APPLICATION_SUDVESTURKJORDAEMI_2021}`,
+      )
       .send()
       .expect(400)
 
     expect(response.body).toMatchObject({
       ...errorExpectedStructure,
       statusCode: 400,
-      message: ['tag must be a valid enum value'],
+      message: ['each value in tags must be a valid enum value'],
     })
   })
-  it(`GET /endorsement-list?tag should return 200 and empty list when no data exists for given tag`, async () => {
+  it(`GET /endorsement-list?tags should return 200 and empty list when no data exists for given tags`, async () => {
     const response = await request(app.getHttpServer())
       .get(
-        `/endorsement-list?tag=${EndorsementTag.PARTY_APPLICATION_SUDVESTURKJORDAEMI_2021}`,
+        `/endorsement-list?tags=${EndorsementTag.PARTY_APPLICATION_SUDVESTURKJORDAEMI_2021}`,
       )
       .send()
       .expect(200)
 
     expect(response.body).toStrictEqual([])
   })
-  it(`GET /endorsement-list?tag should return 200 and a list`, async () => {
+  it(`GET /endorsement-list?tags should return 200 and a list`, async () => {
     const response = await request(app.getHttpServer())
       .get(
-        `/endorsement-list?tag=${EndorsementTag.PARTY_APPLICATION_SUDURKJORDAEMI_2021}`,
+        `/endorsement-list?tags=${EndorsementTag.PARTY_APPLICATION_SUDURKJORDAEMI_2021}`,
       )
       .send()
       .expect(200)
