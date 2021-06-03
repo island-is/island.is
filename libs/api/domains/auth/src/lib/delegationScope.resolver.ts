abdca4cd78141808e378da95edb9f75494945f15
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-06-03 15:59:44 +0000

    Add delegationScope type with field resolvers

---
+++ libs/api/domains/auth/src/lib/delegationScope.resolver.ts
@@ -0,0 +1,22 @@
+import { Parent, Resolver, ResolveField } from '@nestjs/graphql'
+import { UseGuards } from '@nestjs/common'
+
+import { IdsUserGuard } from '@island.is/auth-nest-tools'
+import {
+  DelegationScopeDTO,
+} from '@island.is/clients/auth-public-api'
+
+import { DelegationScope } from './models'
+
+@Resolver(() => DelegationScope)
+export class DelegationScopeResolver {
+  @ResolveField('id')
+  resolveId(@Parent() delegationScope: DelegationScopeDTO): string {
+    return `${delegationScope.delegationId}_${delegationScope.scopeName}`
+  }
+
+  @ResolveField('name')
+  resolveName(@Parent() delegationScope: DelegationScopeDTO): string {
+    return delegationScope.scopeName
+  }
+}
