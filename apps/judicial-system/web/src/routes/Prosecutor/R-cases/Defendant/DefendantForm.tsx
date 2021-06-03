6faa0cacd948d3ab97e644184f980811bd210b88
Ívar Oddsson <ivaroddsson@gmail.com>
2021-06-03 08:37:14 +0000

    Move petitionType to defendant screen

--- apps/judicial-system/web/src/routes/Prosecutor/R-cases/Defendant/DefendantForm.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/R-cases/Defendant/DefendantForm.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import { Box, Text } from '@island.is/island-ui/core'
-import { FormContentContainer } from '@island.is/judicial-system-web/src/shared-components'
+import React, { useState } from 'react'
+import { Box, Input, Select, Text } from '@island.is/island-ui/core'
+import {
+  BlueBox,
+  FormContentContainer,
+} from '@island.is/judicial-system-web/src/shared-components'
 import { Case } from '@island.is/judicial-system/types'
 import LokeCaseNumber from '../../SharedComponents/LokeCaseNumber/LokeCaseNumber'
 import DefendantInfo from '../../SharedComponents/DefendantInfo/DefendantInfo'
+import { petitionTypes } from '../PetitionType/PetitionTypes'
+import {
+  removeTabsValidateAndSet,
+  validateAndSendToServer,
+} from '@island.is/judicial-system-web/src/utils/formHelper'
+import useCase from '@island.is/judicial-system-web/src/utils/hooks/useCase'
 
 interface Props {
   workingCase: Case
@@ -12,6 +21,8 @@ interface Props {
 
 const DefendantForm: React.FC<Props> = (props) => {
   const { workingCase, setWorkingCase } = props
+  const [petitionDescriptionEM, setPetitionDescriptionEM] = useState<string>('')
+  const { updateCase } = useCase()
 
   return (
     <FormContentContainer>
@@ -31,6 +42,56 @@ const DefendantForm: React.FC<Props> = (props) => {
           setWorkingCase={setWorkingCase}
           policeCaseNumberErrorMessage={'' /**TODO */}
         />
+      </Box>
+      <Box component="section" marginBottom={5}>
+        <Box marginBottom={3}>
+          <Text as="h3" variant="h3">
+            Efni kröfu
+          </Text>
+        </Box>
+        <BlueBox>
+          <Box marginBottom={2}>
+            <Select
+              name="petition-type"
+              options={petitionTypes}
+              label="Tegund kröfu"
+              placeholder="Veldu tegund kröfu"
+              required
+            />
+          </Box>
+          <Input
+            data-testid="petition-description"
+            name="petition-description"
+            label="Efni kröfu"
+            placeholder="Skráðu efni kröfu"
+            defaultValue={''}
+            errorMessage={petitionDescriptionEM}
+            hasError={petitionDescriptionEM !== ''}
+            onChange={(event) => {
+              workingCase.id &&
+                removeTabsValidateAndSet(
+                  'petitionDescription',
+                  event,
+                  ['empty'],
+                  workingCase,
+                  setWorkingCase,
+                  petitionDescriptionEM,
+                  setPetitionDescriptionEM,
+                )
+            }}
+            onBlur={(event) =>
+              validateAndSendToServer(
+                'petitionDescription',
+                event.target.value,
+                ['empty'],
+                workingCase,
+                updateCase,
+                setPetitionDescriptionEM,
+              )
+            }
+            required
+          />
+        </BlueBox>
       </Box>
       <Box component="section" marginBottom={5}>
         <Box marginBottom={3}>
