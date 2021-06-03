7504775641bfa9fba2cd43869904215c7e2b193b
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-06-03 16:05:01 +0000

    Fix nestjs providers

--- libs/api/domains/auth/src/lib/auth.module.ts
+++ libs/api/domains/auth/src/lib/auth.module.ts
@@ -1,16 +1,24 @@
 import { DynamicModule, Module } from '@nestjs/common'
+
 import {
   AuthPublicApiClientModule,
   AuthPublicApiClientModuleConfig,
 } from '@island.is/clients/auth-public-api'
 
-import { AuthResolver } from './auth.resolver'
+import { AuthScopeResolver } from './authScope.resolver'
 import { AuthService } from './auth.service'
+import { DelegationScopeResolver } from './delegationScope.resolver'
+import { MainResolver } from './main.resolver'
 
 export type Config = AuthPublicApiClientModuleConfig
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  providers: [
+    MainResolver,
+    DelegationScopeResolver,
+    AuthScopeResolver,
+    AuthService,
+  ],
 })
 export class AuthModule {
   static register(config: Config): DynamicModule {
