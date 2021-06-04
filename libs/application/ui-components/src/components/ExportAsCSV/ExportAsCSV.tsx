3b20795bd37130c91acb56df6d15dab80b6eecd3
Stefán Harald <stefanharald@gmail.com>
2021-06-04 15:45:17 +0000

    MAde no data message optional

--- libs/application/ui-components/src/components/ExportAsCSV/ExportAsCSV.tsx
+++ libs/application/ui-components/src/components/ExportAsCSV/ExportAsCSV.tsx
@@ -5,18 +5,20 @@ import { CSVLink } from 'react-csv'
 interface ExportCSVProps {
   data: string | object[]
   filename: string
-  title: string
+  title?: string
+  noDataMessage?: string
 }
 
 export const ExportAsCSV = ({
   data,
   filename,
   title = 'Sækja niðurstöður',
+  noDataMessage = 'Engin gögn fundust',
 }: ExportCSVProps) => {
   return (
     <Box display="flex">
       <CSVLink
-        data={data && data.length > 0 ? data : 'Engin gögn fundust'}
+        data={data?.length > 0 ? data : noDataMessage}
         filename={filename}
       >
         <Button variant="text" icon="download" iconType="outline">
