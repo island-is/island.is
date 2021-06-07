471ed1a246dc6d80c01bb0b054d39e4ef5fa3dda
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-06-07 13:24:47 +0000

    WIP

---
+++ apps/web/screens/ServiceWeb/mock/index.ts
@@ -0,0 +1,25 @@
+export const categories = [
+  {
+    title: 'Skírteini',
+    description: 'Vegabréf, ökuskírteini, bílpróf, ökuréttindi o.fl.',
+  },
+  {
+    title: 'Fölskyldumál',
+    description:
+      'Hjónavígslur, forsjá barns, meðlag, sáttameðferð, skilnaður o.fl.',
+  },
+  {
+    title: 'Andlát og dánarbú',
+    description: 'Andlátstilkynning, skipti dánarbús, arfur o.fl.',
+  },
+  {
+    title: 'Gjöld og innheimta',
+    description:
+      'Innheimta sekta, vanrækslugjald, ofgreiddar bætur, endurkrafa skaðabóta o.fl.',
+  },
+  {
+    title: 'Leyfi og löggildingar',
+    description:
+      'Heimagisting, Lögmannsréttindi, Meistarapróf, Löggilding fasteigna o.fl.',
+  },
+]
