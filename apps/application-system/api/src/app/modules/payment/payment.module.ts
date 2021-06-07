7dc6ba41365c70815ceecbfd84b20e340ad7cd2b
Phi <juliusgisla@gmail.com>
2021-06-07 15:52:42 +0000

    payments

--- apps/application-system/api/src/app/modules/payment/payment.module.ts
+++ apps/application-system/api/src/app/modules/payment/payment.module.ts
@@ -1,13 +1,17 @@
 import { DynamicModule, Module } from '@nestjs/common'
 import { BullModule as NestBullModule } from '@nestjs/bull'
 import { SequelizeModule } from '@nestjs/sequelize'
+import { createRedisCluster } from '@island.is/cache'
+import { TemplateAPIModule } from '@island.is/application/template-api-modules'
 import { AuthModule } from '@island.is/auth-nest-tools'
 import { TranslationsModule } from '@island.is/api/domains/translations'
+import { SigningModule } from '@island.is/dokobit-signing'
 import { AuditModule } from '@island.is/nest/audit'
 
 import { Payment } from './payment.model'
 import { PaymentController } from './payment.controller'
 import { PaymentService } from './payment.service'
+import { AwsService } from '../application/files/aws.service'
 import { environment } from '../../../environments'
 import {
   PAYMENT_CONFIG,
@@ -15,11 +19,35 @@ import {
 } from './payment.configuration'
 import { ApplicationAccessService } from '../application/tools/applicationAccess.service'
 
+let BullModule: DynamicModule
+
+if (process.env.INIT_SCHEMA === 'true') {
+  BullModule = NestBullModule.registerQueueAsync()
+} else {
+  const bullModuleName = 'application_system_api_payment_bull_module'
+  BullModule = NestBullModule.registerQueueAsync({
+    name: 'upload',
+    useFactory: () => ({
+      prefix: `{${bullModuleName}}`,
+      createClient: () =>
+        createRedisCluster({
+          name: bullModuleName,
+          ssl: environment.production,
+          nodes: environment.redis.urls,
+          noPrefix: true,
+        }),
+    }),
+  })
+}
+
 @Module({
   imports: [
     AuditModule.forRoot(environment.audit),
     AuthModule.register(environment.auth),
+    TemplateAPIModule.register(environment.templateApi),
     SequelizeModule.forFeature([Payment]),
+    BullModule,
+    SigningModule.register(environment.signingOptions),
     TranslationsModule,
   ],
   controllers: [PaymentController],
@@ -29,6 +57,7 @@ import { ApplicationAccessService } from '../application/tools/applicationAccess
       provide: PAYMENT_CONFIG,
       useValue: environment.application as PaymentConfig,
     },
+    AwsService,
     ApplicationAccessService,
   ],
 })
--- libs/application/core/src/types/PaymentType.ts
+++ libs/application/core/src/types/PaymentType.ts
@@ -1,4 +1,4 @@
-export interface Payment {
+export interface PaymentType {
   id: string,
   applicationId: string,
   fulfilled: boolean,
