64d40c114aa788c57119f578310044b3039b0cb2
Noam <noam@aranja.com>
2021-06-08 17:54:10 +0000

    Remove the form mode from the InReview screen.

--- libs/application/templates/parental-leave/src/forms/InReview.ts
+++ libs/application/templates/parental-leave/src/forms/InReview.ts
@@ -3,7 +3,6 @@ import {
   buildForm,
   buildSection,
   Form,
-  FormModes,
 } from '@island.is/application/core'
 
 import Logo from '../assets/Logo'
@@ -16,7 +15,6 @@ export const InReview: Form = buildForm({
   id: 'ParentalLeaveInReview',
   title: inReviewFormMessages.formTitle,
   logo: Logo,
-  mode: FormModes.REVIEW,
   children: [
     buildSection({
       id: 'review',
