e46b49434ecc8714f24912ec031094a5d17be61c
Ívar Oddsson <ivaroddsson@gmail.com>
2021-06-09 11:21:55 +0000

    Simplify routes

--- apps/judicial-system/web/pages/krafa/rannsoknarheimild/domkrofur-og-lagaakvaedi/[id].tsx
+++ apps/judicial-system/web/pages/krafa/rannsoknarheimild/domkrofur-og-lagaakvaedi/[id].tsx
@@ -1,8 +1,2 @@
-import React from 'react'
 import PoliceDemands from '@island.is/judicial-system-web/src/routes/Prosecutor/R-cases/PoliceDemands/PoliceDemands'
-
-const PoliceDemandsRoute = () => {
-  return <PoliceDemands />
-}
-
-export default PoliceDemandsRoute
+export default PoliceDemands
--- apps/judicial-system/web/pages/krafa/rannsoknarheimild/greinargerd/[id].tsx
+++ apps/judicial-system/web/pages/krafa/rannsoknarheimild/greinargerd/[id].tsx
@@ -1,8 +1,2 @@
-import React from 'react'
 import PoliceReport from '@island.is/judicial-system-web/src/routes/Prosecutor/R-cases/PoliceReport/PoliceReport'
-
-const PoliceReportRoute = () => {
-  return <PoliceReport />
-}
-
-export default PoliceReportRoute
+export default PoliceReport
