584b60be7357df17e1fa740430ea292790af0172
Ívar Oddsson <ivaroddsson@gmail.com>
2021-06-09 11:19:59 +0000

    Simplify new r-case route

--- apps/judicial-system/web/pages/krafa/ny/rannsoknarheimild.tsx
+++ apps/judicial-system/web/pages/krafa/ny/rannsoknarheimild.tsx
@@ -1,8 +1,2 @@
-import React from 'react'
 import Defendant from '@island.is/judicial-system-web/src/routes/Prosecutor/R-cases/Defendant/Defendant'
-
-const NewRCase = () => {
-  return <Defendant />
-}
-
-export default NewRCase
+export default Defendant
