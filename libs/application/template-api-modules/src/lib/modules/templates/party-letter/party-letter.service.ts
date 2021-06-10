5a448866c50a856188b77ed4ae4b4252939bbbd5
Sigga Hrafns <sigridur@kosmosogkaos.is>
2021-06-10 08:38:11 +0000

    Adding open endorsemet mutation on rejected

--- libs/application/template-api-modules/src/lib/modules/templates/party-letter/party-letter.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/party-letter/party-letter.service.ts
@@ -39,7 +39,32 @@ export class PartyLetterService {
     private readonly sharedTemplateAPIService: SharedTemplateApiService,
   ) {}
 
-  async assignMinistryOfJustice({ application }: TemplateApiModuleActionProps) {
+  async assignMinistryOfJustice({
+    application,
+    authorization,
+  }: TemplateApiModuleActionProps) {
+    const CLOSE_ENDORSEMENT = `
+    mutation {
+      endorsementSystemCloseEndorsementList(input: {
+        listId: "${
+          (application.externalData?.createEndorsementList.data as any).id
+        }",
+      }) {
+        id
+        closedDate
+      }
+    }
+  `
+    const endorsementId: EndorsementListResponse = await this.sharedTemplateAPIService
+      .makeGraphqlQuery(authorization, CLOSE_ENDORSEMENT)
+      .then((response) => response.json())
+
+    if ('errors' in endorsementId) {
+      throw new Error('Failed to close endorsement list')
+    }
+
+    console.log("assign: ", endorsementId)
+
     await this.sharedTemplateAPIService.assignApplicationThroughEmail(
       generateAssignMinistryOfJusticeApplicationEmail,
       application,
@@ -50,23 +75,25 @@ export class PartyLetterService {
     application,
     authorization,
   }: TemplateApiModuleActionProps) {
-    const CLOSE_ENDORSEMENT = `
+    const OPEN_ENDORSEMENT = `
       mutation {
-        endorsementSystemCloseEndorsementList(input: {
+        endorsementSystemOpenEndorsementList(input: {
           listId: "${
             (application.externalData?.createEndorsementList.data as any).id
           }",
         }) {
           id
+          closedDate
         }
       }
     `
     const endorsementId: EndorsementListResponse = await this.sharedTemplateAPIService
-      .makeGraphqlQuery(authorization, CLOSE_ENDORSEMENT)
+      .makeGraphqlQuery(authorization, OPEN_ENDORSEMENT)
       .then((response) => response.json())
+    console.log("reject: ",endorsementId)
 
     if ('errors' in endorsementId) {
-      throw new Error('Failed to close endorsement list')
+      throw new Error('Failed to open endorsement list')
     }
 
     await this.sharedTemplateAPIService.sendEmail(
