9d4ba11d58e52fb0e2ac3c6cfd66765d18a3f74e
Árni Jónsson <addininja@gmail.com>
2021-06-10 15:54:31 +0000

    feat(financial-aid): Bootstrap the project (#4022)
    * Docker setup
    
    * Setup of financial aid backend.
    
    * setup basic nest.
    
    * Added a financial aid types library.
    
    * Added a get all function for financial aid applications.
    
    * Added Api layer to financial aid.
    
    * Added a create application to the api and backend.
    
    * Added the base for the osk web.
    
    * Added scaffolding for the application form.
    
    * feat(financial-aid): The whole form process frontEnd and sessionStorage
    
    * Web update.
    
    * Review update.
    
    * web osk update.
    
    * Veita added.
    
    * Municipality added
    
    * adding formatters and formatting homacircumstances
    
    * homeCircumstances formatted in summary and small fix in homeCircumstances form
    
    * Formatting employment status
    
    * adding loading state to summary and fixing types in form and summary
    
    * createapplication
    
    * few veitu setups f.ex. table, nav and reading from database applications
    
    * adding home and employment custom
    
    * auth system.
    
    * Working auth system.
    
    * form frontend fixes
    
    * adding changing to form from mvp meeting
    
    * Finished the user flow.
    
    * Formatting
    
    * Added the robin hood team to the code owners file.
    
    * Added a missing open api file from the project.
    
    * Added the missing code gen file.
    
    * Fixed a typo in docker compose.
    
    * Fixed lint issues.
    
    * Updated .nxignore
    
    * veita front-end profile setup
    
    * adding tax to form and updating form summary/new design
    
    * adding document title to form
    
    * going over frontend on mobile
    
    * Setup backend unit tests
    
    * sveita in progress, pages tree and routes made
    
    * making application/id endpoint and fixing financial aid  prices
    
    * making navigation in veita and connecting applicant endpoint and making profile
    
    * making files format for applicant
    
    * added esbuild support.
    
    * Fixed formatting.
    
    * UserContext added to web-osk
    
    * adding insertat to formatters
    
    * Removed unused veitu page.
    
    * Removed unused test file.
    
    * Adding studentcustom, fixing key errors and adding aidcalc to formatters
    
    * Fixed formatting.
    
    Co-authored-by: MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/financial-aid/api/src/app/modules/application/application.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common'
+
+import { ApplicationResolver } from './application.resolver'
+
+@Module({
+  providers: [ApplicationResolver],
+})
+export class ApplicationModule {}
