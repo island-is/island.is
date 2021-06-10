2711e658953208edfb522ef1a968821bfdd26e87
Darri Steinn Konráðsson <darri@viceversa.is>
2021-06-10 17:04:35 +0000

    feat(access-control): hook up resolvers to ui (#4165)
    * Move main resolver to delegation resolver in order to use field resolvers
    
    * Resolve id for interface
    
    * Resolvers for fetching api scopes
    
    * Fix endpoint
    
    * Resolvers for updating a delegation
    
    * Create resolvers for deleting a delegation
    
    * Fix create delegation resolver
    
    * Create resolvers for fetching multiple custom delegations
    
    * Allow hooking into value change
    
    * Hook up delegations query to ui
    
    * Create delegation and hook up to mutations
    
    * Render api scopes and delegations jointly from queries with multi controls
    
    * Hook up delete and update mutations
    
    * Lint and format
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/auth/src/lib/models/apiScopeGroup.model.ts
+++ libs/api/domains/auth/src/lib/models/apiScopeGroup.model.ts
@@ -1,16 +1,13 @@
 import { Field, ObjectType, ID } from '@nestjs/graphql'
 
-@ObjectType()
-export class AuthScope {
+@ObjectType('AuthApiScopeGroup')
+export class ApiScopeGroup {
   @Field(() => ID)
   name!: string
 
   @Field(() => String)
   displayName!: string
 
-  @Field(() => String)
-  groupName!: string
-
-  @Field(() => String)
-  description!: string
+  @Field(() => String, { nullable: true })
+  description?: string
 }
