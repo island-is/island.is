94a6a398746efdbc44ddf0328afb8af10e06e266
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-06-11 14:47:15 +0000

    Fix tests

--- apps/judicial-system/web/pages/_app.tsx
+++ apps/judicial-system/web/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import App, { AppProps } from 'next/app'
 import getConfig from 'next/config'
-import gql from 'graphql-tag'
 import ApolloClient from 'apollo-client'
 import { NormalizedCacheObject } from 'apollo-cache-inmemory'
 import { ApolloProvider } from '@apollo/client'
