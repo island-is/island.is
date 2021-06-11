eb410e37cbb851305afa23e80964279bde38af78
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-06-11 13:04:24 +0000

    Fix

--- apps/judicial-system/web/graphql/httpLink.ts
+++ apps/judicial-system/web/graphql/httpLink.ts
@@ -3,7 +3,8 @@ import fetch from 'isomorphic-unfetch'
 import environment from '../src/environments/environment'
 
 // Polyfill fetch() on the server (used by apollo-client)
-// eslint-disable-next-line @typescript-eslint/no-explicit-any(global as any).fetch = fetch
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+;(global as any).fetch = fetch
 
 export default createHttpLink({
   uri: `${environment.api.url}/api/graphql`,
