c8443bed6b76de56b48abefe62ae08b2c60aa82a
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-06-11 12:44:42 +0000

    Add absolute path to be able to fetch translations on localhost

--- apps/judicial-system/web/graphql/httpLink.ts
+++ apps/judicial-system/web/graphql/httpLink.ts
@@ -1,12 +1,12 @@
 import { createHttpLink } from '@apollo/client'
 import fetch from 'isomorphic-unfetch'
+import environment from '../src/environments/environment'
 
 // Polyfill fetch() on the server (used by apollo-client)
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-;(global as any).fetch = fetch
+// eslint-disable-next-line @typescript-eslint/no-explicit-any(global as any).fetch = fetch
 
 export default createHttpLink({
-  uri: '/api/graphql',
+  uri: `${environment.api.url}/api/graphql`,
   credentials: 'include',
   fetch,
 })
--- apps/judicial-system/web/src/environments/environment.ts
+++ apps/judicial-system/web/src/environments/environment.ts
@@ -1,9 +1,15 @@
 const devConfig = {
   production: false,
+  api: {
+    url: 'http://localhost:3333',
+  },
 }
 
 const prodConfig = {
   production: true,
+  api: {
+    url: '',
+  },
 }
 
 export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig
