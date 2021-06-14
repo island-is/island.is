b8611cd911ed98e04855fc5a27f4780efb98c00a
Phi <juliusgisla@gmail.com>
2021-06-14 09:00:16 +0000

    bad lint fix

--- apps/application-system/api/src/app/modules/application/application.model.ts
+++ apps/application-system/api/src/app/modules/application/application.model.ts
@@ -87,7 +87,7 @@ export class Application extends Model<Application> {
     allowNull: false,
   })
   @ApiProperty()
-  externalData!: Record<string, unknown>
+  externalData!: object
 
   @Column({
     type: DataType.ENUM,
