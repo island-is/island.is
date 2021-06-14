fd1c76a04ff65199e7d102378ca5672c6b421d46
Valur Einarsson <valure@fuglar.com>
2021-06-14 12:05:20 +0100

    feat(auth-api-lib): Get delegated scopes

--- libs/auth-api-lib/src/lib/services/delegation-scope.service.ts
+++ libs/auth-api-lib/src/lib/services/delegation-scope.service.ts
@@ -8,12 +8,20 @@ import {
   UpdateDelegationScopeDTO,
 } from '../entities/dto/delegation-scope.dto'
 import { DelegationScope } from '../entities/models/delegation-scope.model'
+import { Delegation } from '../entities/models/delegation.model'
+import { Op } from 'sequelize'
+import { ApiScope } from '../entities/models/api-scope.model'
+import { IdentityResource } from '../entities/models/identity-resource.model'
 
 @Injectable()
 export class DelegationScopeService {
   constructor(
     @InjectModel(DelegationScope)
     private delegationScopeModel: typeof DelegationScope,
+    @InjectModel(ApiScope)
+    private apiScopeModel: typeof ApiScope,
+    @InjectModel(IdentityResource)
+    private identityResourceModel: typeof IdentityResource,
     @Inject(LOGGER_PROVIDER)
     private logger: Logger,
   ) {}
@@ -78,5 +86,90 @@ export class DelegationScopeService {
     return this.delegationScopeModel.destroy({
       where: { delegationId: delegationId },
     })
+  }
+
+  async findAllValidCustomScopesTo(
+    toNationalId: string,
+    fromNationalId: string,
+  ): Promise<DelegationScope[]> {
+    const now = new Date()
+
+    return this.delegationScopeModel.findAll({
+      where: {
+        [Op.and]: [
+          { validFrom: { [Op.lt]: now } },
+          { validTo: { [Op.or]: [{ [Op.eq]: null }, { [Op.gt]: now }] } },
+        ],
+      },
+      include: [
+        {
+          model: Delegation,
+          where: {
+            [Op.and]: [
+              { toNationalId: toNationalId, fromNationalId: fromNationalId },
+            ],
+          },
+        },
+        {
+          model: ApiScope,
+          where: {
+            allowExplicitDelegationGrant: true,
+          },
+          required: false,
+        },
+        {
+          model: IdentityResource,
+          where: {
+            allowExplicitDelegationGrant: true,
+          },
+          required: false,
+        },
+      ],
+    })
+  }
+
+  async findAllProcurationScopes(): Promise<string[]> {
+    const apiScopes = await this.apiScopeModel.findAll({
+      where: { grantToProcuringHolders: true },
+    })
+
+    const identityResources = await this.identityResourceModel.findAll({
+      where: { grantToProcuringHolders: true },
+    })
+
+    return [
+      ...apiScopes.map((s) => s.name),
+      ...identityResources.map((s) => s.name),
+    ]
+  }
+
+  async findAllLegalGuardianScopes(): Promise<string[]> {
+    const apiScopes = await this.apiScopeModel.findAll({
+      where: { grantToLegalGuardians: true },
+    })
+
+    const identityResources = await this.identityResourceModel.findAll({
+      where: { grantToLegalGuardians: true },
+    })
+
+    return [
+      ...apiScopes.map((s) => s.name),
+      ...identityResources.map((s) => s.name),
+    ]
+  }
+
+  async findAllAutomaticScopes(): Promise<string[]> {
+    const apiScopes = await this.apiScopeModel.findAll({
+      where: { automaticDelegationGrant: true },
+    })
+
+    const identityResources = await this.identityResourceModel.findAll({
+      where: { automaticDelegationGrant: true },
+    })
+
+    return [
+      ...apiScopes.map((s) => s.name),
+      ...identityResources.map((s) => s.name),
+    ]
   }
 }
