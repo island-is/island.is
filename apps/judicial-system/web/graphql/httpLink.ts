85053846b07c5841439735322c04aa8bc8ebd202
Vilhjalmur Gunnarsson <villi@kolibri.is>
2021-06-15 10:56:41 +0000

    Add horrible fix because of jest failing on getConfig

--- apps/judicial-system/web/graphql/httpLink.ts
+++ apps/judicial-system/web/graphql/httpLink.ts
@@ -2,7 +2,8 @@ import { createHttpLink } from '@apollo/client'
 import fetch from 'isomorphic-unfetch'
 import getConfig from 'next/config'
 
-const { publicRuntimeConfig, serverRuntimeConfig } = getConfig()
+// const { publicRuntimeConfig = {}, serverRuntimeConfig = {} } = getConfig()
+const { publicRuntimeConfig = {}, serverRuntimeConfig = {} } = getConfig() || {}
 
 // Polyfill fetch() on the server (used by apollo-client)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
