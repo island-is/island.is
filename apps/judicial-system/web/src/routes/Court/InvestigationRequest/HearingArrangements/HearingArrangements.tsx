20572ca09425f4a10e257dc95107eef060dbbcf0
Ívar Oddsson <ivaroddsson@gmail.com>
2021-06-15 12:53:01 +0000

    Send headsup notification

--- apps/judicial-system/web/src/routes/Court/InvestigationRequest/HearingArrangements/HearingArrangements.tsx
+++ apps/judicial-system/web/src/routes/Court/InvestigationRequest/HearingArrangements/HearingArrangements.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { PageLayout } from '@island.is/judicial-system-web/src/shared-components'
-import { Case } from '@island.is/judicial-system/types'
+import {
+  Modal,
+  PageLayout,
+} from '@island.is/judicial-system-web/src/shared-components'
+import { Case, NotificationType } from '@island.is/judicial-system/types'
 import {
   CaseData,
   JudgeSubsections,
@@ -12,12 +15,16 @@ import { CaseQuery } from '@island.is/judicial-system-web/graphql'
 import { useRouter } from 'next/router'
 import HearingArrangementsForm from './HearingArrangementsForm'
 import { UsersQuery } from '@island.is/judicial-system-web/src/utils/mutations'
+import * as Constants from '@island.is/judicial-system-web/src/utils/constants'
+import { useCase } from '@island.is/judicial-system-web/src/utils/hooks'
 
 const HearingArrangements = () => {
   const [workingCase, setWorkingCase] = useState<Case>()
+  const [modalVisible, setModalVisible] = useState(false)
 
   const router = useRouter()
   const id = router.query.id
+  const { sendNotification, isSendingNotification } = useCase()
 
   const { data, loading } = useQuery<CaseData>(CaseQuery, {
     variables: { input: { id: id } },
@@ -39,6 +46,23 @@ const HearingArrangements = () => {
     }
   }, [workingCase, setWorkingCase, data])
 
+  const handleNextButtonClick = async () => {
+    if (workingCase) {
+      const notificationSent = await sendNotification(
+        workingCase.id,
+        NotificationType.COURT_DATE,
+      )
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      if (notificationSent && !window.Cypress) {
+        setModalVisible(true)
+      } else {
+        router.push(`${Constants.R_CASE_COURT_RECORD_ROUTE}/${id}`)
+      }
+    }
+  }
+
   return (
     <PageLayout
       activeSection={
@@ -52,12 +76,25 @@ const HearingArrangements = () => {
       caseId={workingCase?.id}
     >
       {workingCase && users && (
-        <HearingArrangementsForm
-          workingCase={workingCase}
-          setWorkingCase={setWorkingCase}
-          isLoading={loading || userLoading}
-          users={users}
-        />
+        <>
+          <HearingArrangementsForm
+            workingCase={workingCase}
+            setWorkingCase={setWorkingCase}
+            isLoading={loading || userLoading || isSendingNotification}
+            users={users}
+            handleNextButtonClick={handleNextButtonClick}
+          />
+          {modalVisible && (
+            <Modal
+              title="Tilkynning um fyrirtökutíma hefur verið send"
+              text="Tilkynning um fyrirtökutíma hefur verið send á ákæranda, fangelsi og verjanda hafi verjandi verið skráður."
+              handlePrimaryButtonClick={() => {
+                router.push(`${Constants.R_CASE_COURT_RECORD_ROUTE}/${id}`)
+              }}
+              primaryButtonText="Loka glugga"
+            />
+          )}
+        </>
       )}
     </PageLayout>
   )
