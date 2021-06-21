5237bae9830153a3e22c63fdbddae14aae602a53
Brynjar Ingimarsson <brynjar@stefna.is>
2021-06-21 12:46:08 +0000

    Make header title a link

--- apps/web/components/Project/Header/DefaultProjectHeader.tsx
+++ apps/web/components/Project/Header/DefaultProjectHeader.tsx
@@ -5,9 +5,11 @@ import {
   GridContainer,
   GridRow,
   GridColumn,
+  Link,
 } from '@island.is/island-ui/core'
 import * as styles from './DefaultProjectHeader.treat'
 import { ProjectPage } from '@island.is/web/graphql/schema'
+import { useLinkResolver } from '@island.is/web/hooks/useLinkResolver'
 
 interface DefaultProjectHeaderProps {
   projectPage: ProjectPage
@@ -15,28 +17,34 @@ interface DefaultProjectHeaderProps {
 
 export const DefaultProjectHeader = ({
   projectPage,
-}: DefaultProjectHeaderProps) => (
-  <Box className={styles.headerBg}>
-    <GridContainer>
-      <GridRow>
-        <GridColumn
-          span={['12/12', '12/12', '12/12', '7/12', '6/12']}
-          offset={['0', '0', '0', '1/12', '1/12']}
-        >
-          <Text variant="eyebrow" color="white" marginTop={5}>
-            Ísland.is
-          </Text>
-          <Text variant="h1" color="white" marginTop={4}>
-            {projectPage.title}
-          </Text>
-          <Text variant="intro" color="white" marginTop={3}>
-            {projectPage.subtitle}
-          </Text>
-          <Text color="white" marginTop={3}>
-            {projectPage.intro}
-          </Text>
-        </GridColumn>
-      </GridRow>
-    </GridContainer>
-  </Box>
-)
+}: DefaultProjectHeaderProps) => {
+  const { linkResolver } = useLinkResolver()
+
+  return (
+    <Box className={styles.headerBg}>
+      <GridContainer>
+        <GridRow>
+          <GridColumn
+            span={['12/12', '12/12', '12/12', '7/12', '6/12']}
+            offset={['0', '0', '0', '1/12', '1/12']}
+          >
+            <Text variant="eyebrow" color="white" marginTop={5}>
+              Ísland.is
+            </Text>
+            <Link href={linkResolver('projectpage', [projectPage.slug]).href}>
+              <Text variant="h1" color="white" marginTop={4}>
+                {projectPage.title}
+              </Text>
+            </Link>
+            <Text variant="intro" color="white" marginTop={3}>
+              {projectPage.subtitle}
+            </Text>
+            <Text color="white" marginTop={3}>
+              {projectPage.intro}
+            </Text>
+          </GridColumn>
+        </GridRow>
+      </GridContainer>
+    </Box>
+  )
+}
