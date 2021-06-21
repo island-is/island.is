1201b71bf90d9be0aee8ba95294f9f887e8b6046
Sævar Már Atlason <54210288+saevarma@users.noreply.github.com>
2021-06-21 09:40:35 +0000

    feat(reference-backend): Add authentication, audit logs and update enviroment config structure (#4242)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/reference-backend/src/environments/environment.ts
+++ apps/reference-backend/src/environments/environment.ts
@@ -1,9 +1,15 @@
-const devConfig = {
-  production: false,
+export default {
+  production: process.env.NODE_ENV === 'production',
+  audit: {
+    defaultNamespace: '@island.is/reference-backend',
+    groupName: process.env.AUDIT_GROUP_NAME,
+    // Same service name as in Nx workspace.json
+    serviceName: 'reference-backend',
+  },
+  auth: {
+    issuer:
+      process.env.IDENTITY_SERVER_ISSUER_URL ??
+      'https://identity-server.dev01.devland.is',
+    audience: '@island.is',
+  },
 }
-
-const prodConfig = {
-  production: true,
-}
-
-export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig
