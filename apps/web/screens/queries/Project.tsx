b33f38172f5879c56b0df828bbc08d54abbcbbb0
Brynjar Ingimarsson <brynjar@stefna.is>
2021-06-21 09:43:31 +0000

    Add project pages to web

---
+++ apps/web/screens/queries/Project.tsx
@@ -0,0 +1,32 @@
+import gql from 'graphql-tag'
+import { slices } from './fragments'
+
+export const GET_PROJECT_PAGE_QUERY = gql`
+  query GetProjectPage($input: GetProjectPageInput!) {
+    getProjectPage(input: $input) {
+      id
+      title
+      slug
+      theme
+      sidebar
+      sidebarLinks {
+        text
+        url
+      }
+      subtitle
+      intro
+      content {
+        ...AllSlices
+      }
+      slices {
+        ...AllSlices
+      }
+      newsTag {
+        id
+        title
+        slug
+      }
+    }
+  }
+  ${slices}
+`
