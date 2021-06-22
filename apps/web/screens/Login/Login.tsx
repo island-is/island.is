f8a300cd1b6385fe89b12e269006cf75a8d05a87
Þórður Hafliðason <tolleinn@gmail.com>
2021-06-22 17:45:48 +0000

    Design has been updated. Final

--- apps/web/screens/Login/Login.tsx
+++ apps/web/screens/Login/Login.tsx
@@ -15,7 +15,7 @@ import {
   Hidden,
 } from '@island.is/island-ui/core'
 import * as styles from './Login.treat'
-import SvgPlant from '../../components/Login/svgPlant'
+import SvgLogin from '../../components/Login/svgLogin'
 import { LoginPageTexts } from '../../components/Login/LoginTexts.types'
 import { Screen } from '@island.is/web/types'
 import { withMainLayout } from '@island.is/web/layouts/main'
@@ -68,7 +68,7 @@ const LoginPage: Screen<LoginProps> = ({ namespace }) => {
               <Text as="h2" variant="h1" marginBottom="p3" marginTop="p1">
                 {n('nyjuSidurTitle', 'Ný útgáfa af mínum síðum á island.is')}
               </Text>
-              <Text as="p" variant="intro" marginBottom="p5">
+              <Text as="p" variant="default" marginBottom="p5">
                 {n(
                   'nyjuSidurText',
                   'Ný útgáfa minna síðna á ísland.is. Hér er um að ræða beta útgáfu að nýjum mínum síðum, ekki eru allir möguleikar sem eru á gömlu mínum síðum í boði hér ennþá.',
@@ -95,11 +95,15 @@ const LoginPage: Screen<LoginProps> = ({ namespace }) => {
             </GridColumn>
             <GridColumn
               span={['12/12', '12/12', '6/12']}
-              paddingBottom={[3, 3, 4]}
+              paddingBottom={[3, 4, 5]}
             >
               <Hidden below="md">
-                <Box display="flex" alignItems="center" justifyContent="center">
-                  <SvgPlant />
+                <Box
+                  display="flex"
+                  justifyContent="flexEnd"
+                  alignItems="flexStart"
+                >
+                  <SvgLogin />
                 </Box>
               </Hidden>
             </GridColumn>
