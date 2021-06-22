8cdc1f23a5ecd89aefc8334a3f4b8920a7daf5e8
Phi <juliusgisla@gmail.com>
2021-06-22 11:33:37 +0000

    ARK secret

--- apps/api/src/app/environments/environment.ts
+++ apps/api/src/app/environments/environment.ts
@@ -113,7 +113,7 @@ const devConfig = {
     password: process.env.PAYMENT_PASSWORD,
     callbackBaseUrl: process.env.PAYMENT_BASE_CALLBACK_URL,
     callbackAdditionUrl: process.env.PAYMENT_ADDITION_CALLBACK_URL,
-    arkBaseUrl: 'https://uat.arkid.is',
+    arkBaseUrl: process.env.ARK_ENDPOINT,
   },
   temporaryVoterRegistry: {
     baseApiUrl: 'http://localhost:4248',
@@ -226,7 +226,7 @@ const prodConfig = {
     password: process.env.PAYMENT_PASSWORD,
     callbackBaseUrl: process.env.PAYMENT_BASE_CALLBACK_URL,
     callbackAdditionUrl: process.env.PAYMENT_ADDITION_CALLBACK_URL,
-    arkBaseUrl: 'https://uat.arkid.is',
+    arkBaseUrl: process.env.ARK_ENDPOINT,
   },
   temporaryVoterRegistry: {
     baseApiUrl: process.env.TEMPORARY_VOTER_REGISTRY_BASE_API_URL,
--- libs/clients/payment/README.md
+++ libs/clients/payment/README.md
@@ -1,7 +1,10 @@
-# Rsk
+# Ark
 
 This library serves as a client for endpoints accessible to payment ARK
 
 ## Running unit tests
 
 Run `nx test payment` to execute the unit tests via [Jest](https://jestjs.io).
+
+## GET SECRETS
+Run `yarn get-secrets application-system-api` - You need a valid AWS login.
