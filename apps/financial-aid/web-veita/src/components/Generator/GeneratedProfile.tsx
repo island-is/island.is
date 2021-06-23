7351154123b82cd95906803bad74710392937be3
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-06-23 14:43:09 +0000

    Veita added to config and two packages added(Identicon & md5)

--- apps/financial-aid/web-veita/src/components/Generator/GeneratedProfile.tsx
+++ apps/financial-aid/web-veita/src/components/Generator/GeneratedProfile.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from 'react'
-// import Identicon from 'identicon.js'
-// import md5 from 'md5'
+import Identicon from 'identicon.js'
+import md5 from 'md5'
 
 interface Props {
   ssn?: string
@@ -8,19 +8,21 @@ interface Props {
 }
 
 const GeneratedProfile: FunctionComponent<Props> = ({ ssn, size }) => {
-  // const data = new Identicon(md5(ssn), {
-  //   size,
-  //   background: [255, 255, 255, 0],
-  //   format: 'svg',
-  //   margin: 0.1,
-  // })
-
-  const data = {
+  
+  
+  const data = new Identicon(md5(ssn), {
     size,
     background: [255, 255, 255, 0],
     format: 'svg',
     margin: 0.1,
-  }
+  })
+
+  // const data = {
+  //   size,
+  //   background: [255, 255, 255, 0],
+  //   format: 'svg',
+  //   margin: 0.1,
+  // }
 
   return (
     <img
