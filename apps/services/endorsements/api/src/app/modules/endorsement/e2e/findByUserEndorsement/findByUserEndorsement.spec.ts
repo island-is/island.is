6b5c96f151ce8e3086bfe86ae7482929eb9bffb1
Stefán Harald <stefanharald@gmail.com>
2021-06-25 11:46:22 +0000

    Remove invalidated from endorsmenet api

--- apps/services/endorsements/api/src/app/modules/endorsement/e2e/findByUserEndorsement/findByUserEndorsement.spec.ts
+++ apps/services/endorsements/api/src/app/modules/endorsement/e2e/findByUserEndorsement/findByUserEndorsement.spec.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import { IdsUserGuard, MockAuthGuard } from '@island.is/auth-nest-tools'
 import { setup } from '../../../../../../test/setup'
 import { errorExpectedStructure } from '../../../../../../test/testHelpers'
-import { Endorsement } from '../../endorsement.model'
+import { Endorsement } from '../../models/endorsement.model'
 import { authNationalId } from './seed'
 
 let app: INestApplication
