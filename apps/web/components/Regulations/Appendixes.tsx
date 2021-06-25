ebda20a941eff73fb7d84375ce81355f62766f20
mar <mar.nospam@anomy.net>
2021-06-25 16:24:53 +0000

    fix: Move react components into separate library module to fix builds

--- apps/web/components/Regulations/Appendixes.tsx
+++ apps/web/components/Regulations/Appendixes.tsx
@@ -4,7 +4,7 @@ import React, { memo } from 'react'
 import { HTMLText, PlainText, useDomid } from '@island.is/regulations'
 import { RegulationMaybeDiff } from '@island.is/regulations/web'
 import { Accordion, AccordionItem, Box, Text } from '@island.is/island-ui/core'
-import { HTMLBox } from '@island.is/regulations'
+import { HTMLBox } from '@island.is/regulations/react'
 
 const hasDiff = (text: string) => /<(del|ins)/.test(text)
 
