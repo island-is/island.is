cabb37c1a9e7af60e64c897c0b9d525119eb8c3b
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2021-06-28 09:40:13 +0000

    feat(ctao): Complaint Information (#4313)
    * Bootstrap Complaints to Althingi Ombudsman application
    
    * Adding prerequisites form
    
    * installing eslint and prettier to my WSL ubuntu code and fixing lint errors
    
    * working in prerequisities
    
    * adding gitbook and fixing lint error
    
    * connecting prerequisites to template
    
    * combining prerequisites and main form
    
    * building user information section
    
    * deleting unused files
    
    * Adding data provider messages
    
    * data schema for conditions
    
    * get information from external data
    
    * Set up complainee information sections
    
    * added messages for conditions box
    
    * cleanup: added utils and seperate complainee messages
    
    * testing error message fixes
    
    * getting old yarn.lock
    
    * yarn lock
    
    * yarn lock again
    
    * add section for complaint information
    
    * zod schema
    
    * some changes to yarn lock
    
    * Section updated as per new design
    
    * add info message when a complaint type is selected
    
    * update section titles
    
    * add complaintType to dataSchema
    
    * place complaint related sections under the same section
    
    * Added logo and updated section title
    
    * fixing error messages in informationsection and adding new complainee section
    
    * refactors and updates as per new design
    
    * validation
    
    * attachment section
    
    * uncomment schema
    
    * changing messages
    
    * don't allow dates older than a year ago
    
    * feat(ctao): section for court action question
    
    * attachment section
    
    * adding condition for complained for information
    
    * cleanup
    
    Co-authored-by: Sigrún Tinna Gissurardóttir <sigrun@sendiradid.is>
    Co-authored-by: Þórarinn <thorarinn@sendiradid.is>
    Co-authored-by: Sigrún Tinna Gissurardóttir <39527334+sigruntg@users.noreply.github.com>
    Co-authored-by: Benedikt Jonsson <bensijons@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/complaints-to-althingi-ombudsman/src/lib/messages/error.ts
+++ libs/application/templates/complaints-to-althingi-ombudsman/src/lib/messages/error.ts
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl'
 // Error messages in the application
 export const error = defineMessages({
   required: {
-    id: 'dpac.application:error.required',
+    id: 'ctao.application:error.required',
     defaultMessage: 'Skylda er að fylla út reitinn',
     description: 'Error message when a required field has not been filled out',
   },
