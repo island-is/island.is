9fb99ce701f64f92b6e10b07bb498bb35b3afb23
Þórður Hafliðason <tolleinn@gmail.com>
2021-06-28 10:08:55 +0000

    Hide the tab service

--- libs/service-portal/finance/src/index.ts
+++ libs/service-portal/finance/src/index.ts
@@ -1,18 +1,18 @@
-import { Query } from '@island.is/api/schema'
+// import { Query } from '@island.is/api/schema'
 import {
   ServicePortalModule,
   ServicePortalPath,
   ServicePortalRoute,
 } from '@island.is/service-portal/core'
-import { GET_TAPS_QUERY } from '@island.is/service-portal/graphql'
-import * as Sentry from '@sentry/react'
+// import { GET_TAPS_QUERY } from '@island.is/service-portal/graphql'
+// import * as Sentry from '@sentry/react'
 import { defineMessage } from 'react-intl'
 import { lazy } from 'react'
 
 export const financeModule: ServicePortalModule = {
   name: 'Fjármál',
   widgets: () => [],
-  routes: async ({ client }) => {
+  routes: async () => {
     const routes: ServicePortalRoute[] = [
       {
         name: 'Fjármál',
@@ -36,40 +36,56 @@ export const financeModule: ServicePortalModule = {
         path: ServicePortalPath.FinanceBills,
         render: () => lazy(() => import('./screens/FinanceBills')),
       },
+      {
+        name: defineMessage({
+          id: 'service.portal:finance-transactions',
+          defaultMessage: 'Hreyfingar',
+        }),
+        path: ServicePortalPath.FinanceTransactions,
+        render: () => lazy(() => import('./screens/FinanceTransactions')),
+      },
+      {
+        name: defineMessage({
+          id: 'service.portal:finance-employee-claims',
+          defaultMessage: 'Laungreiðendakröfur',
+        }),
+        path: ServicePortalPath.FinanceEmployeeClaims,
+        render: () => lazy(() => import('./screens/FinanceEmployeeClaims')),
+      },
     ]
-    try {
-      const res = await client.query<Query>({
-        query: GET_TAPS_QUERY,
-      })
+    // try {
+    //   const res = await client.query<Query>({
+    //     query: GET_TAPS_QUERY,
+    //   })
 
-      const data = res?.data?.getCustomerTapControl
+    //   const data = res?.data?.getCustomerTapControl
 
-      // Show customer records:
-      if (data?.RecordsTap) {
-        routes.push({
-          name: defineMessage({
-            id: 'service.portal:finance-transactions',
-            defaultMessage: 'Hreyfingar',
-          }),
-          path: ServicePortalPath.FinanceTransactions,
-          render: () => lazy(() => import('./screens/FinanceTransactions')),
-        })
-      }
+    //   // Show customer records:
+    //   if (data?.RecordsTap) {
+    //     routes.push({
+    //       name: defineMessage({
+    //         id: 'service.portal:finance-transactions',
+    //         defaultMessage: 'Hreyfingar',
+    //       }),
+    //       path: ServicePortalPath.FinanceTransactions,
+    //       render: () => lazy(() => import('./screens/FinanceTransactions')),
+    //     })
+    //   }
 
-      // Show employee claims:
-      if (data?.employeeClaimsTap) {
-        routes.push({
-          name: defineMessage({
-            id: 'service.portal:finance-employee-claims',
-            defaultMessage: 'Laungreiðendakröfur',
-          }),
-          path: ServicePortalPath.FinanceEmployeeClaims,
-          render: () => lazy(() => import('./screens/FinanceEmployeeClaims')),
-        })
-      }
-    } catch (error) {
-      Sentry.captureException(error)
-    }
+    //   // Show employee claims:
+    //   if (data?.employeeClaimsTap) {
+    //     routes.push({
+    //       name: defineMessage({
+    //         id: 'service.portal:finance-employee-claims',
+    //         defaultMessage: 'Laungreiðendakröfur',
+    //       }),
+    //       path: ServicePortalPath.FinanceEmployeeClaims,
+    //       render: () => lazy(() => import('./screens/FinanceEmployeeClaims')),
+    //     })
+    //   }
+    // } catch (error) {
+    //   Sentry.captureException(error)
+    // }
 
     return routes
   },
