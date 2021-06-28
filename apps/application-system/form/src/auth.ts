89f6432a4f3855c20ac1fd4bb46788ed4a9a297d
Jérémy Barbet <jeremgraph@gmail.com>
2021-06-28 12:39:24 +0000

    chore(core): allow to pass an array of scope instead of string interpolation

--- apps/application-system/form/src/auth.ts
+++ apps/application-system/form/src/auth.ts
@@ -3,10 +3,7 @@ import { environment } from './environments'
 import {
   ApplicationScope,
   NationalRegistryScope,
-  PartyLetterRegistryScope,
-  TemporaryVoterRegistryScope,
   UserProfileScope,
-  EndorsementScope,
 } from '@island.is/auth/scopes'
 
 const userMocked = process.env.API_MOCKS === 'true'
@@ -22,7 +19,15 @@ if (userMocked) {
     redirectPathSilent: '/silent/signin-oidc',
     authority: environment.identityServer.authority,
     client_id: 'island-is-1',
-    scope: `openid profile api_resource.scope ${ApplicationScope.read} ${ApplicationScope.write} ${UserProfileScope.read} ${NationalRegistryScope.individuals}`,
+    scope: [
+      'openid',
+      'profile',
+      'api_resource.scope',
+      ApplicationScope.read,
+      ApplicationScope.write,
+      UserProfileScope.read,
+      NationalRegistryScope.individuals,
+    ],
     post_logout_redirect_uri: `${window.location.origin}`,
   })
 }
