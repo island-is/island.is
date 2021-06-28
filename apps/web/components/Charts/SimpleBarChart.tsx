9f531282f7efefd16b4af16e41fbfec3691ebc57
Alexandra Diljá <alexandrab18@ru.is>
2021-06-28 17:32:39 +0000

    line added

--- apps/web/components/Charts/SimpleBarChart.tsx
+++ apps/web/components/Charts/SimpleBarChart.tsx
@@ -1,67 +1,73 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
+import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Line } from 'recharts';
 
 
 const data = [
   {
-      fund_year: 2021,
-      sott: 72,
-      veitt: 0.0
-  },
-  {
       fund_year: 2010,
       sott: 109,
-      veitt: 23.0
+      veitt: 23.0,
+      amount: 127632000
   },
   {
       fund_year: 2012,
       sott: 188,
-      veitt: 53.0
+      veitt: 53.0,
+      amount: 163385000
   },
   {
       fund_year: 2011,
       sott: 223,
-      veitt: 46.0
+      veitt: 46.0,
+      amount: 157583000
   },
   {
       fund_year: 2014,
       sott: 234,
-      veitt: 56.0
+      veitt: 56.0,
+      amount: 1057143000
   },
   {
       fund_year: 2015,
       sott: 268,
-      veitt: 87.0
+      veitt: 87.0,
+      amount: 2114543000
   },
   {
       fund_year: 2013,
       sott: 326,
-      veitt: 61.0
+      veitt: 61.0,
+      amount: 606991000
   },
   {
       fund_year: 2016,
       sott: 492,
-      veitt: 106.0
+      veitt: 106.0,
+      amount: 2461778000
   },
   {
       fund_year: 2017,
       sott: 508,
-      veitt: 101.0
+      veitt: 101.0,
+      amount: 2406155000
   },
   {
       fund_year: 2018,
       sott: 600,
-      veitt: 84.0
+      veitt: 84.0,
+      amount: 1833837000
   },
   {
       fund_year: 2019,
       sott: 630,
-      veitt: 76.0
+      veitt: 76.0,
+      amount: 1594440000
   },
   {
       fund_year: 2020,
       sott: 882,
-      veitt: 89.0
+      veitt: 89.0,
+      amount: 1152500000
   }
 ]
 
@@ -78,7 +84,7 @@ export const SimpleBarChart = () => {
   const sorted_data = data.sort((a, b) => {return a.fund_year - b.fund_year})
     return (
       <ResponsiveContainer width="100%" height="100%">
-        <BarChart
+        <ComposedChart
           width={500}
           height={300}
           data={sorted_data}
@@ -90,13 +96,15 @@ export const SimpleBarChart = () => {
           }}
         >
           <CartesianGrid strokeDasharray="1" vertical={false} color='#CCDFFF'/>
-          <XAxis dataKey="fund_year"/>
-          <YAxis/>
+          <XAxis dataKey="fund_year" stroke="#00003C"/>
+          <YAxis yAxisId="left" orientation="left" stroke="#00003C" />
+          <YAxis yAxisId="right" hide />
           <Tooltip />
           <Legend iconType='circle' align="right" formatter={renderColorfulLegendText}/>
-          <Bar dataKey="sott" fill="#99C0FF" stackId='a' barSize={16} /> 
-          <Bar dataKey="veitt" fill="#0061FF" radius={[20,20,0,0]} stackId='a' barSize={16}/>
-        </BarChart>
+          <Bar dataKey="sott" fill="#99C0FF" stackId='a' barSize={16} yAxisId="left"/> 
+          <Bar dataKey="veitt" fill="#0061FF" radius={[20,20,0,0]} stackId='a' barSize={16} yAxisId="left"/>
+          <Line dataKey="amount" stroke="#99F4EA" yAxisId="right" strokeWidth={3} dot={{ strokeWidth: 2 }}/>
+        </ComposedChart>
       </ResponsiveContainer>
     )
 }
