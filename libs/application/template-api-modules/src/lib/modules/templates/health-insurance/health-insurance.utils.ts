4c958c212ceda2420acccf77b788ffdf7989c933
Eirikur Nilsson <eirikur@nilsson.is>
2021-06-29 18:19:19 +0000

    Revert "fix(health-insurance): support single digit month and date"
    508b04d2bedf33071b9ee7339d40ae466ba66167

--- libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.utils.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/health-insurance/health-insurance.utils.ts
@@ -1,6 +1,4 @@
 import get from 'lodash/get'
-import parse from 'date-fns/parse'
-import parseISO from 'date-fns/parseISO'
 import { logger } from '@island.is/logging'
 
 import { Application } from '@island.is/application/core'
@@ -23,27 +21,6 @@ const extractAnswerFromJson = (object: unknown, key: string) => {
     return null
   }
   return JSON.parse(value)
-}
-
-export const parseNationalRegistryDate = (dateString: string | null) => {
-  if (typeof dateString !== 'string') {
-    return undefined
-  }
-
-  // Icelandic format
-  let parsedDate = parse(dateString, 'd.M.yyyy HH:mm:ss', new Date())
-  if (!isNaN(parsedDate.getTime())) {
-    return parsedDate
-  }
-
-  // ISO format (in the future hopefully)
-  parsedDate = parseISO(dateString)
-  if (!isNaN(parsedDate.getTime())) {
-    return parsedDate
-  }
-
-  // Others
-  return undefined
 }
 
 export const transformApplicationToHealthInsuranceDTO = (
@@ -80,6 +57,17 @@ export const transformApplicationToHealthInsuranceDTO = (
       throw new Error(
         `Student's application must have confirmation of student document`,
       )
+    }
+
+    // Extract national registry date
+    // It could be null
+    let registryDate: string | null = extractAnswer(
+      application.externalData,
+      'nationalRegistry.data.address.lastUpdated',
+    )
+    if (registryDate) {
+      const pattern = /(\d{2})\.(\d{2})\.(\d{4})/
+      registryDate = registryDate.split(' ')[0].replace(pattern, '$3-$2-$1')
     }
 
     // There is 2 fields to add information in frontend
@@ -121,12 +109,10 @@ export const transformApplicationToHealthInsuranceDTO = (
       phoneNumber:
         extractAnswer(application.answers, 'applicant.phoneNumber') ?? '',
       // Registry date could be empty
-      residenceDateFromNationalRegistry: parseNationalRegistryDate(
-        extractAnswer(
-          application.externalData,
-          'nationalRegistry.data.address.lastUpdated',
-        ),
-      ),
+      residenceDateFromNationalRegistry:
+        registryDate && !isNaN(Date.parse(registryDate))
+          ? new Date(registryDate)
+          : undefined,
       // Could not get this yet, so sent in empty
       residenceDateUserThink: undefined,
       userStatus: userStatus,
