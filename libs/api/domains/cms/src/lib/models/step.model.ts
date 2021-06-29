6984df2e8b8597d4520de9c094f8c1d67ee1238c
Brynjar Ingimarsson <brynjar@stefna.is>
2021-06-29 12:43:29 +0000

    Add stepper type to backend

---
+++ libs/api/domains/cms/src/lib/models/step.model.ts
@@ -0,0 +1,36 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+
+import { IStep } from '../generated/contentfulTypes'
+import { SystemMetadata } from 'api-cms-domain'
+import { mapDocument, SliceUnion } from '../unions/slice.union'
+
+@ObjectType()
+export class Step {
+  @Field(() => ID)
+  id!: string
+
+  @Field()
+  title?: string
+
+  @Field()
+  slug?: string
+
+  @Field(() => [SliceUnion], { nullable: true })
+  text?: Array<typeof SliceUnion>
+
+  @Field()
+  isAnswer?: boolean
+
+  @Field({ nullable: true })
+  options?: string | null
+}
+
+export const mapStep = ({ sys, fields }: IStep): SystemMetadata<Step> => ({
+  typename: 'AccordionSlice',
+  id: sys.id,
+  title: fields.title ?? '',
+  slug: fields.slug ?? '',
+  text: fields.text ? mapDocument(fields.text, sys.id + ':text') : [],
+  isAnswer: fields.isAnswer ?? false,
+  options: fields.options ? JSON.stringify(fields.options) : '[]',
+})
