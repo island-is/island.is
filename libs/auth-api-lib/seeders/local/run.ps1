ae5b34664c8b92a848322e9757d8aafa7eb8b187
Valur Einarsson <valure@fuglar.com>
2021-07-01 21:52:00 +0100

    feat(auth-lib): Local seed data

---
+++ libs/auth-api-lib/seeders/local/run.ps1
@@ -0,0 +1,10 @@
+
+npx sequelize db:seed --seeders-path .\seeders\local\ --seed `
+grant_types.js,`
+identity_resources.js,`
+ids-admin.client.js,`
+island.is.client.js,`
+island.is.client.js,`
+skra.is.client.js,`
+delegations.js
+
