5c7e878c05abf0eca90df2455da656c373f901ca
Ívar Oddsson <ivaroddsson@gmail.com>
2021-07-01 08:39:58 +0000

    Checkpoint 2

---
+++ apps/judicial-system/web/messages/RestrictionCases/Prosecutor/lawsBrokenForm.ts
@@ -0,0 +1,24 @@
+import { defineMessage, defineMessages } from 'react-intl'
+
+// Strings for Laws broken form
+export const lawsBrokenFrom = {
+  sections: {
+    custodyProvisions: defineMessages({
+      title: {
+        id:
+          'judicial.system:forms.restrinction.laws_broken.custody_provision_a-lið1.mgr.95.gr.title',
+        defaultMessage: 'a-lið 1. mgr. 95. gr.',
+        description:
+          'Prosecutor laws broken form. Custody provisions - a-lið 1. mgr. 95. gr.: title',
+      },
+      tooltip: {
+        id:
+          'judicial.system:forms.restrinction.laws_broken.custody_provision_a-lið1.mgr.95.gr.tooltip',
+        defaultMessage:
+          'Að ætla megi að sakborningur muni torvelda rannsókn málsins, svo sem með því að afmá merki eftir brot, skjóta undan munum ellegar hafa áhrif á samseka eða vitni.',
+        description:
+          'Prosecutor laws broken form. Custody provisions - a-lið 1. mgr. 95. gr.: tooltip',
+      },
+    }),
+  },
+}
---
+++ apps/judicial-system/web/messages/SharedComponents/policeCaseNumber.ts
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl'
+
+// Strings for the lokeCaseNumber component
+export const policeCaseNumber = defineMessages({
+  heading: {
+    id: 'judicial.system:component.policeCaseNumber.heading',
+    defaultMessage: 'Málsnúmer lögreglu',
+    description: 'Accused form. Police casenumber: Heading',
+  },
+  label: {
+    id: 'judicial.system:component.policeCaseNumber.label',
+    defaultMessage: 'Slá inn LÖKE málsnúmer',
+    description: 'Accused form. Police casenumber: Label',
+  },
+  placeholder: {
+    id: 'judicial.system:component.policeCaseNumber.placeholder',
+    defaultMessage: '007-2020-X',
+    description: 'Accused form. Police casenumber: Placeholder',
+  },
+})
