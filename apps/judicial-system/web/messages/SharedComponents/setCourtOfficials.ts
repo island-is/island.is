f0646ea9b1566095e7d4c94d5de2785496eb9da3
Ívar Oddsson <ivaroddsson@gmail.com>
2021-07-01 10:14:41 +0000

    Checkpoint

---
+++ apps/judicial-system/web/messages/SharedComponents/setCourtOfficials.ts
@@ -0,0 +1,12 @@
+import { defineMessage, defineMessages } from 'react-intl'
+
+// Strings for court officials
+export const setCourtOfficials = {
+  judge: defineMessages({
+    tooltip: {
+      id: 'judicial.system:component.defender.email.label',
+      defaultMessage: 'Netfang verjanda',
+      description: 'Defender component email field: Label',
+    },
+  }),
+}
