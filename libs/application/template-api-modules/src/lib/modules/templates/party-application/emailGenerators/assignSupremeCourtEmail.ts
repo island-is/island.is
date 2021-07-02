8cd35359a78377376d12b50308e8516ce341df40
Stefán Harald <stefanhar@users.noreply.github.com>
2021-07-02 16:19:26 +0000

    fix: Endorsements system bug fixes (#4472)
    * fix(party-application): Refactor EndorsementsListSubmission (#4402)
    
    * wip
    
    * wip
    
    * Some refactoring
    
    * Some cleanup
    
    * Use hook again
    
    * Remove hardcoded list
    
    * Add listid back in
    
    * Remove hardcoded in utils
    
    * Show warning when no endorsements
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit 5e45ed7c683ccce11ac6c631dfad7e97c34fe332)
    
    * fix(party-application): fixing a few warnings in party letter and application (#4414)
    
    * fixing a few warnings is party letter and application
    
    * fix for empty table in select table
    
    * format
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit e016870684571df79876231ec835ba262c249dca)
    
    * fix(application-system): Fixed party application environment secrets (#4430)
    
    * Fixed environment secrets
    
    * Added charts
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit 3e70c6ae94defdf6a4ab48ed96f24c6adca9d203)
    
    * fix(api): RSK environment variables (#4417)
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit 064311978ef91631aa5037b8f8e4cf484042979e)
    
    * fix(party-application): sort by date in desc order (#4433)
    
    * sort by date in desc order
    
    * orderBy
    
    * one more orderBy
    
    * fix & format
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit 0b28b104e1f3c7f60acef5663f0b94e46e4ec959)
    
    * fix(party-application): fixing the problem with upload skipping the first row (#4441)
    
    * fixing the problem with upload skipping the first row
    
    * the same for party letter
    
    * format
    
    * bulk upload for multiple columns per sheet
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit 01bcd43729d53f3e580936d3e1bdcc5aed16995e)
    
    * feat(application-templates-party-application): Email per constituency (#4453)
    
    * Added email per constituency
    
    * Changed type to interface
    
    * changed type to interface
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    (cherry picked from commit db52c2f3a9ceb48a41edd2b7502b53515c2649ad)
    
    Co-authored-by: Sigga Hrafns <70899104+siggakogk@users.noreply.github.com>
    Co-authored-by: albinagu <47886428+albinagu@users.noreply.github.com>

--- libs/application/template-api-modules/src/lib/modules/templates/party-application/emailGenerators/assignSupremeCourtEmail.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/party-application/emailGenerators/assignSupremeCourtEmail.ts
@@ -1,17 +1,51 @@
 import { dedent } from 'ts-dedent'
 import { AssignmentEmailTemplateGenerator } from '../../../../types'
+import { EndorsementListTagsEnum } from '../gen/fetch'
 
-export const generateAssignSupremeCourtApplicationEmail: AssignmentEmailTemplateGenerator = (
-  props,
-  assignLink,
-) => {
+type Constituency = Extract<
+  EndorsementListTagsEnum,
+  | EndorsementListTagsEnum.partyApplicationNordausturkjordaemi2021
+  | EndorsementListTagsEnum.partyApplicationNordvesturkjordaemi2021
+  | EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiNordur2021
+  | EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiSudur2021
+  | EndorsementListTagsEnum.partyApplicationSudurkjordaemi2021
+  | EndorsementListTagsEnum.partyApplicationSudvesturkjordaemi2021
+>
+export type GenerateAssignSupremeCourtApplicationEmailOptions = Record<
+  | 'partyApplicationRvkSouth'
+  | 'partyApplicationRvkNorth'
+  | 'partyApplicationSouthWest'
+  | 'partyApplicationNorthWest'
+  | 'partyApplicationNorth'
+  | 'partyApplicationSouth',
+  string
+>
+export const generateAssignSupremeCourtApplicationEmail = (
+  adminEmails: GenerateAssignSupremeCourtApplicationEmailOptions,
+): AssignmentEmailTemplateGenerator => (props, assignLink) => {
   const {
     application,
     options: { email },
   } = props
 
+  const constituencyEmailMap = {
+    [EndorsementListTagsEnum.partyApplicationNordausturkjordaemi2021]:
+      adminEmails.partyApplicationNorth,
+    [EndorsementListTagsEnum.partyApplicationNordvesturkjordaemi2021]:
+      adminEmails.partyApplicationNorthWest,
+    [EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiNordur2021]:
+      adminEmails.partyApplicationRvkNorth,
+    [EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiSudur2021]:
+      adminEmails.partyApplicationRvkSouth,
+    [EndorsementListTagsEnum.partyApplicationSudurkjordaemi2021]:
+      adminEmails.partyApplicationSouth,
+    [EndorsementListTagsEnum.partyApplicationSudvesturkjordaemi2021]:
+      adminEmails.partyApplicationSouthWest,
+  }
+
   const supremeCourtEmail =
-    process.env.PARTY_APPLICATION_SUBMISSION_DESTINATION_EMAIL ?? ''
+    constituencyEmailMap[application.answers.constituency as Constituency]
+
   const { partyLetter, partyName } = application.externalData
     .partyLetterRegistry?.data as any
 
