435ae5506135574bb2e994634c456d3b51407cfa
mar <mar.nospam@anomy.net>
2021-07-05 09:33:05 +0000

    feat: Apply `regulationEditingStyling` and `diffStyling` to Editor

--- libs/service-portal/regulations-admin/src/components/MiniDiff.treat.ts
+++ libs/service-portal/regulations-admin/src/components/MiniDiff.treat.ts
@@ -1,18 +1,6 @@
 import { style } from 'treat'
-import { theme } from '@island.is/island-ui/theme'
-const { color } = theme
+import { diffStyling } from '@island.is/regulations/styling'
 
-export const wrapper = style({
-  selectors: {
-    '& ins': {
-      padding: `0 0.125em`,
-      textDecorationColor: 'none',
-      backgroundColor: color.mint200,
-    },
-    '& del': {
-      padding: `0 0.125em`,
-      textDecorationColor: 'rgba(0, 0, 0, 0.5)',
-      backgroundColor: color.red200,
-    },
-  },
-})
+export const wrapper = style({})
+
+diffStyling(wrapper)
