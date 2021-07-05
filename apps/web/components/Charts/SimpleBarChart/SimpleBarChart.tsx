437c0723ea6d18ab85478ce2a61bf80ee828b5aa
Alexandra Diljá <alexandrab18@ru.is>
2021-07-05 13:44:14 +0000

    bar comparison

--- apps/web/components/Charts/SimpleBarChart/SimpleBarChart.tsx
+++ apps/web/components/Charts/SimpleBarChart/SimpleBarChart.tsx
@@ -1,3 +1,4 @@
+import { forEach } from 'lodash'
 import React from 'react'
 import {
   BarChart,
@@ -11,70 +12,104 @@ import {
   Line,
 } from 'recharts'
 
-const veitt = [
-  { fund_year: 2016, "Karlmaður": 75 },
-  { fund_year: 2015, "Karlmaður": 69 },
-  { fund_year: 2020, "Karlmaður": 66 },
-  { fund_year: 2017, "Karlmaður": 64 },
-  { fund_year: 2019, "Karlmaður": 54 },
-  { fund_year: 2018, "Karlmaður": 54 },
-  { fund_year: 2013, "Karlmaður": 41 },
-  { fund_year: 2014, "Karlmaður": 40 },
-  { fund_year: 2012, "Karlmaður": 38 },
-  { fund_year: 2017, "Kona": 37 },
-  { fund_year: 2016, "Kona": 31 },
-  { fund_year: 2011, "Karlmaður": 30 },
-  { fund_year: 2018, "Kona": 30 },
-  { fund_year: 2020, "Kona": 23 },
-  { fund_year: 2019, "Kona": 22 },
-  { fund_year: 2013, "Kona": 19 },
-  { fund_year: 2015, "Kona": 18 },
-  { fund_year: 2014, "Kona": 16 },
-  { fund_year: 2010, "Karlmaður": 16 },
-  { fund_year: 2011, "Kona": 15 },
-  { fund_year: 2012, "Kona": 14 },
-  { fund_year: 2010, "Kona": 6 },
-  { fund_year: 2011, "Óskilgreint": 1 },
-  { fund_year: 2012, "Óskilgreint": 1 },
-  { fund_year: 2010, "Óskilgreint": 1 },
-  { fund_year: 2013, "Óskilgreint": 1 }
+const data = [
+  {
+      "fund_year": 2010,
+      "Karlmaður veittir styrkir": 16,
+      "Kona veittir styrkir": 6,
+      "Óskilgreint veittir styrkir": 1,
+      "Karlmaður umsóknir": 88,
+      "Kona umsóknir": 16,
+      "Óskilgreint umsóknir": 5
+  },
+  {
+      "fund_year": 2011,
+      "Karlmaður veittir styrkir": 30,
+      "Kona veittir styrkir": 15,
+      "Óskilgreint veittir styrkir": 1,
+      "Karlmaður umsóknir": 169,
+      "Kona umsóknir": 48,
+      "Óskilgreint umsóknir": 6
+  },
+  {
+      "fund_year": 2012,
+      "Karlmaður veittir styrkir": 38,
+      "Kona veittir styrkir": 14,
+      "Óskilgreint veittir styrkir": 1,
+      "Karlmaður umsóknir": 144,
+      "Kona umsóknir": 41,
+      "Óskilgreint umsóknir": 3
+  },
+  {
+      "fund_year": 2013,
+      "Karlmaður veittir styrkir": 41,
+      "Kona veittir styrkir": 19,
+      "Óskilgreint veittir styrkir": 1,
+      "Karlmaður umsóknir": 237,
+      "Kona umsóknir": 86,
+      "Óskilgreint umsóknir": 3
+  },
+  {
+      "fund_year": 2014,
+      "Karlmaður veittir styrkir": 40,
+      "Kona veittir styrkir": 16,
+      "Karlmaður umsóknir": 175,
+      "Kona umsóknir": 57,
+      "Óskilgreint umsóknir": 2
+  },
+  {
+      "fund_year": 2015,
+      "Karlmaður veittir styrkir": 69,
+      "Kona veittir styrkir": 18,
+      "Karlmaður umsóknir": 211,
+      "Kona umsóknir": 54,
+      "Óskilgreint umsóknir": 3
+  },
+  {
+      "fund_year": 2016,
+      "Karlmaður veittir styrkir": 75,
+      "Kona veittir styrkir": 31,
+      "Karlmaður umsóknir": 364,
+      "Kona umsóknir": 128
+  },
+  {
+      "fund_year": 2017,
+      "Karlmaður veittir styrkir": 64,
+      "Kona veittir styrkir": 37,
+      "Karlmaður umsóknir": 367,
+      "Kona umsóknir": 141
+  },
+  {
+      "fund_year": 2018,
+      "Karlmaður veittir styrkir": 54,
+      "Kona veittir styrkir": 30,
+      "Karlmaður umsóknir": 410,
+      "Kona umsóknir": 189,
+      "Óskilgreint umsóknir": 1
+  },
+  {
+      "fund_year": 2019,
+      "Karlmaður veittir styrkir": 54,
+      "Kona veittir styrkir": 22,
+      "Karlmaður umsóknir": 433,
+      "Kona umsóknir": 196,
+      "Óskilgreint umsóknir": 1
+  },
+  {
+      "fund_year": 2020,
+      "Karlmaður veittir styrkir": 66,
+      "Kona veittir styrkir": 23,
+      "Karlmaður umsóknir": 602,
+      "Kona umsóknir": 278,
+      "Óskilgreint umsóknir": 2
+  },
+  {
+      "fund_year": 2021,
+      "Karlmaður umsóknir": 48,
+      "Kona umsóknir": 24
+  }
 ]
 
-const umsoknir = [
-  { fund_year: 2020, "Karlmaður": 602 },
-  { fund_year: 2019, "Karlmaður": 433 },
-  { fund_year: 2018, "Karlmaður": 410 },
-  { fund_year: 2017, "Karlmaður": 367 },
-  { fund_year: 2016, "Karlmaður": 364 },
-  { fund_year: 2020, "Kona": 278 },
-  { fund_year: 2013, "Karlmaður": 237 },
-  { fund_year: 2015, "Karlmaður": 211 },
-  { fund_year: 2019, "Kona": 196 },
-  { fund_year: 2018, "Kona": 189 },
-  { fund_year: 2014, "Karlmaður": 175 },
-  { fund_year: 2011, "Karlmaður": 169 },
-  { fund_year: 2012, "Karlmaður": 144 },
-  { fund_year: 2017, "Kona": 141 },
-  { fund_year: 2016, "Kona": 128 },
-  { fund_year: 2010, "Karlmaður": 88 },
-  { fund_year: 2013, "Kona": 86 },
-  { fund_year: 2014, "Kona": 57 },
-  { fund_year: 2015, "Kona": 54 },
-  { fund_year: 2021, "Karlmaður": 48 },
-  { fund_year: 2011, "Kona": 48 },
-  { fund_year: 2012, "Kona": 41 },
-  { fund_year: 2021, "Kona": 24 },
-  { fund_year: 2010, "Kona": 16 },
-  { fund_year: 2011, "Óskilgreint": 6 },
-  { fund_year: 2010, "Óskilgreint": 5 },
-  { fund_year: 2015, "Óskilgreint": 3 },
-  { fund_year: 2012, "Óskilgreint": 3 },
-  { fund_year: 2013, "Óskilgreint": 3 },
-  { fund_year: 2020, "Óskilgreint": 2 },
-  { fund_year: 2014, "Óskilgreint": 2 },
-  { fund_year: 2019, "Óskilgreint": 1 },
-  { fund_year: 2018, "Óskilgreint": 1 }
-]
 
 
 const dataKeysName = {
@@ -133,27 +168,12 @@ interface SimpleBarChartProps {
   dataset?: string,
 }
 export const SimpleBarChart = ({dataset}: SimpleBarChartProps) => {
-  let data = umsoknir
-  switch(dataset) {
-    case "umsokn":
-      data = umsoknir
-      break
-    case "veitt":
-      data = veitt
-      break
-    default:
-      data = umsoknir
-      break 
-  }
-  const sorted_data = data.sort((a, b) => {
-    return a.fund_year - b.fund_year
-  })
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart
         width={250}
         height={150}
-        data={sorted_data}
+        data={data}
         margin={{
           top: 30,
           right: 0,
@@ -173,23 +193,44 @@ export const SimpleBarChart = ({dataset}: SimpleBarChartProps) => {
         <Tooltip />
         <Legend iconType="circle" align="right" content={renderLegend} />
         <Bar
-          dataKey="Karlmaður"
-          fill="#FF99B9"
-          radius={[20, 20, 0, 0]}
+          dataKey="Óskilgreint umsóknir"
+          fill="#B5B6EC"
           barSize={16}
+          stackId="a"
         />
         <Bar
-          dataKey="Kona"
+          dataKey="Karlmaður umsóknir"
+          fill="#FF99B9"
+          barSize={16}
+          stackId="a"
+        />
+        <Bar
+          dataKey="Kona umsóknir"
           fill="#D799C7"
           radius={[20, 20, 0, 0]}
           barSize={16}
+          stackId="a"
         />
         <Bar
-          dataKey="Óskilgreint"
-          fill="#B5B6EC"
+          dataKey="Óskilgreint veittir styrkir"
+          fill="#99F4EA"
+          barSize={16}
+          stackId="b"
+        />
+        <Bar
+          dataKey="Karlmaður veittir styrkir"
+          fill="#C3ABD9"
+          barSize={16}
+          stackId="b"
+        />
+        <Bar
+          dataKey="Kona veittir styrkir"
+          fill="#99C0FF"
           radius={[20, 20, 0, 0]}
           barSize={16}
+          stackId="b"
         />
+        
       </BarChart>
     </ResponsiveContainer>
   )
