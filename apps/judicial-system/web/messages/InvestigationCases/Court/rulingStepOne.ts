3c6c48f844e995991745101e8e939d25cafee66f
Ívar Oddsson <ivaroddsson@gmail.com>
2021-07-05 15:16:41 +0000

    Contentful checkpoint

---
+++ apps/judicial-system/web/messages/InvestigationCases/Court/rulingStepOne.ts
@@ -0,0 +1,26 @@
+import { defineMessages } from 'react-intl'
+
+export const icRulingStepOne = {
+  sections: {
+    courtCaseFacts: defineMessages({
+      tooltip: {
+        id:
+          'judicial.system:investigation_cases.ruling_step_one.court_case_facts.tooltip',
+        defaultMessage:
+          'Greinargerð lögreglu er forbókuð hér fyrir neðan. Hægt er að breyta textanum og mun hann birtast með þeim hætti í úrskurði dómara.',
+        description:
+          'Notaður sem upplýsingatexti í upplýsingasvæði við "greinargerð um málsatvik" titlinn á úrskurðar skrefi í gæsluvarðhalds- og farbannsmálum.',
+      },
+    }),
+    courtLegalArguments: defineMessages({
+      tooltip: {
+        id:
+          'judicial.system:investigation_cases.ruling_step_one.court_legal_arguments.tooltip',
+        defaultMessage:
+          'Greinargerð lögreglu er forbókuð hér fyrir neðan. Hægt er að breyta textanum og mun hann birtast með þeim hætti í úrskurði dómara.',
+        description:
+          'Notaður sem upplýsingatexti í upplýsingasvæði við "greinargerð um lagarök" titlinn á úrskurðar skrefi í gæsluvarðhalds- og farbannsmálum.',
+      },
+    }),
+  },
+}
