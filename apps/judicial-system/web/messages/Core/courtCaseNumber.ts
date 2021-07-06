a2a78568c8ff5bf403cf6bbb08369ac69ff403fb
Ívar Oddsson <ivaroddsson@gmail.com>
2021-07-06 15:00:09 +0000

    Move string to Core

--- apps/judicial-system/web/messages/Core/courtCaseNumber.ts
+++ apps/judicial-system/web/messages/Core/courtCaseNumber.ts
@@ -1,15 +1,14 @@
 import { defineMessages } from 'react-intl'
 
-// Strings for CourtCaseNumber components
 export const courtCaseNumber = defineMessages({
   title: {
-    id: 'judicial.system:component.court_case_number.title',
+    id: 'judicial.system.core:court_case_number.title',
     defaultMessage: 'Málsnúmer héraðsdóms',
     description:
       'Notaður sem titill fyrir "Málsnúmer héraðsdóms" hlutann á yfirlits skrefi dómara í öllum málstegundum',
   },
   explanation: {
-    id: 'judicial.system:component.court_case_number.explanation',
+    id: 'judicial.system.core:court_case_number.explanation',
     defaultMessage:
       'Smelltu á hnappinn til að stofna nýtt mál eða skráðu inn málsnúmer sem er þegar til í Auði. Athugið að gögn verða sjálfkrafa vistuð á það málsnúmer sem slegið er inn.',
     description:
