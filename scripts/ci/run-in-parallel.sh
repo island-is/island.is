0205c643e435d15d73b8bb186e915f47f449767d
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-07-06 14:45:35 +0000

    Fix run in parallel

--- scripts/ci/run-in-parallel.sh
+++ scripts/ci/run-in-parallel.sh
@@ -10,5 +10,7 @@ MAX_JOBS=${MAX_JOBS:-2}
 AFFECTED_PROJECTS=$(echo "${AFFECTED_PROJECTS}" | tr '\n,' ' ')
 echo "Running '$AFFECTED_PROJECTS' in parallel of ${MAX_JOBS} for target $1"
 
+IFS=" " read -ra AFFECTED_PROJECTS <<< "$AFFECTED_PROJECTS"
+
 # This is a helper script to run in parallel a list of provided projects for a specific target
-exec parallel --halt soon,fail=1 --lb -j "$MAX_JOBS" APP={} "$DIR"/"$1".sh ::: "$AFFECTED_PROJECTS"
+exec parallel --halt soon,fail=1 --lb -j "$MAX_JOBS" APP={} "$DIR"/"$1".sh ::: "${AFFECTED_PROJECTS[@]}"
