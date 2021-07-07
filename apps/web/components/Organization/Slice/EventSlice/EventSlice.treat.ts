4b1a91d57f8656ef34a9cc09a658b1d1a3a352fe
Brynjar Ingimarsson <brynjar@stefna.is>
2021-07-07 17:45:58 +0000

    Add event slice component

---
+++ apps/web/components/Organization/Slice/EventSlice/EventSlice.treat.ts
@@ -0,0 +1,9 @@
+import { style } from 'treat'
+
+export const wrapper = style({
+  padding: '50px 40px 30px 40px',
+  background:
+    'url(https://images.ctfassets.net/8k0h54kbe6bj/1LnFsjESmfKO6Cx76O7kbE/2c073e23ee587522276efc1cb7599ce4/drawing__2_.svg), rgb(31, 101, 251)',
+  backgroundSize: 'cover',
+  backgroundPositionY: 'center',
+})
