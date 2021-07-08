7a9a432bef0d0bf9edbdad1baa5bcfc878ff81c8
Brynjar Ingimarsson <brynjar@stefna.is>
2021-07-08 11:09:59 +0000

    Fix styling

--- apps/web/components/Organization/Slice/EventSlice/EventSlice.treat.ts
+++ apps/web/components/Organization/Slice/EventSlice/EventSlice.treat.ts
@@ -2,6 +2,6 @@ import { style } from 'treat'
 
 export const wrapper = style({
   padding: '50px 40px 30px 40px',
-  backgroundSize: 'cover',
-  backgroundPositionY: 'center',
+  backgroundSize: 'cover !important',
+  backgroundPositionY: 'center !important',
 })
