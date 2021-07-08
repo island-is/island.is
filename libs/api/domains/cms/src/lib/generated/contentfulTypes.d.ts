13bf2bbc94c0ae9cc761071781f20087d590da5f
Brynjar Ingimarsson <binni@outlook.com>
2021-07-08 11:33:43 +0000

    fix(web): Add convention slices and OG for entry page (#4518)
    * fix(web): Add OG image and description to project pages (#4516)
    
    * Add field for featured image
    
    * Add opengraph image and description to project page
    
    * feat(web): Add event card and event signup form slices (#4515)
    
    * Add email signup fields
    
    * Add signup form as component
    
    * Add conference form variant to signup slice
    
    * Add event slice to backend
    
    * Add event slice component
    
    * Update GraphQL queries
    
    * Add event slice to organization slices
    
    * Add export for new form
    
    * Fix linting error
    
    * Small fixes
    
    * Fix linting error
    
    * Remove hardcoded string
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    * Fix styling
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/cms/src/lib/generated/contentfulTypes.d.ts
+++ libs/api/domains/cms/src/lib/generated/contentfulTypes.d.ts
@@ -630,6 +630,40 @@ export interface IErrorPage extends Entry<IErrorPageFields> {
   }
 }
 
+export interface IEventSliceFields {
+  /** Title */
+  title?: string | undefined
+
+  /** Subtitle */
+  subtitle?: string | undefined
+
+  /** Date */
+  date?: string | undefined
+
+  /** Link */
+  link?: ILink | undefined
+
+  /** Background Image */
+  backgroundImage?: Asset | undefined
+}
+
+export interface IEventSlice extends Entry<IEventSliceFields> {
+  sys: {
+    id: string
+    type: string
+    createdAt: string
+    updatedAt: string
+    locale: string
+    contentType: {
+      sys: {
+        id: 'eventSlice'
+        linkType: 'ContentType'
+        type: 'Link'
+      }
+    }
+  }
+}
+
 export interface IFaqListFields {
   /** Title */
   title: string
@@ -1433,11 +1467,29 @@ export interface IMailingListSignupFields {
   /** Title */
   title: string
 
+  /** Variant */
+  variant: 'default' | 'conference'
+
   /** Description */
   description?: string | undefined
 
-  /** Input label */
+  /** Email label */
   inputLabel: string
+
+  /** Name label */
+  fullNameLabel: string
+
+  /** QuestionLabel */
+  questionLabel?: string | undefined
+
+  /** Yes Label */
+  yesLabel?: string | undefined
+
+  /** No Label */
+  noLabel?: string | undefined
+
+  /** Disclaimer Label */
+  disclaimerLabel?: string | undefined
 
   /** Submit button text */
   buttonText: string
@@ -1900,6 +1952,7 @@ export interface IOrganizationPageFields {
         | IBigBulletList
         | IDistricts
         | IMailingListSignup
+        | IEventSlice
         | IFeaturedArticles
         | ISectionHeading
         | ILogoListSlice
@@ -1982,6 +2035,8 @@ export interface IOrganizationSubpageFields {
         | IContactUs
         | IDistricts
         | IMailingListSignup
+        | IEventSlice
+        | ILatestNewsSlice
         | IMultipleStatistics
         | IOneColumnText
         | ITabSection
@@ -2390,6 +2445,9 @@ export interface IProjectPageFields {
 
   /** Project Subpages */
   projectSubpages?: IProjectSubpage[] | undefined
+
+  /** Featured Image */
+  featuredImage?: Asset | undefined
 }
 
 export interface IProjectPage extends Entry<IProjectPageFields> {
@@ -3597,6 +3655,7 @@ export type CONTENT_TYPE =
   | 'districts'
   | 'embeddedVideo'
   | 'errorPage'
+  | 'eventSlice'
   | 'faqList'
   | 'featured'
   | 'featuredArticles'
