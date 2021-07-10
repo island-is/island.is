39a7f6c55ae8895e355abc2e0f8b70d419aa869e
Petar Shomov <petar@andes.is>
2021-07-10 22:56:14 +0000

    Revert "propagating the `--skip-nx-options` when going schema generation" Revert "Changing NX cache folder location to avoid being cached with node_modules by default (#4532)"

--- scripts/schemas.js
+++ scripts/schemas.js
@@ -40,12 +40,9 @@ const main = async () => {
 
     try {
       await exec(
-        `nx run-many --target=${target} --all --with-deps --parallel --maxParallel=6 $NX_OPTIONS`,
-        {
-          env: skipCache
-            ? { ...process.env, NX_OPTIONS: '--skip-nx-cache' }
-            : process.env,
-        },
+        `nx run-many --target=${target} --all --with-deps --parallel --maxParallel=6 ${
+          skipCache ? '--skip-nx-cache' : ''
+        }`,
       )
     } catch (err) {
       console.error(`Error running command: ${err.message}`)
