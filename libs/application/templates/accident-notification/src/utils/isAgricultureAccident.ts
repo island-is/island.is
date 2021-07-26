7a2e8c17db9b28fbbc85bc192089d6b3c168bf71
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2021-07-26 16:22:01 +0000

    remove console log

--- libs/application/templates/accident-notification/src/utils/isAgricultureAccident.ts
+++ libs/application/templates/accident-notification/src/utils/isAgricultureAccident.ts
@@ -2,7 +2,6 @@ import { FormValue } from '@island.is/application/core'
 import { WorkAccidentTypeEnum } from '../types'
 
 export const isAgricultureAccident = (formValue: FormValue) => {
-  console.log(formValue)
   const workAccidentType = (formValue as {
     workAccident: { type: WorkAccidentTypeEnum }
   })?.workAccident?.type
