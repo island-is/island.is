ab0f89ba187c1eff3f2396a5f124597301bb90b7
Þórarinn <thorarinn@sendiradid.is>
2021-07-27 14:29:03 +0000

    Add util for getting representative data

---
+++ libs/application/templates/accident-notification/src/utils/getRepresentativeData.ts
@@ -0,0 +1,44 @@
+import { FormValue } from '@island.is/application/core'
+import {
+  companyInfo,
+  fishingCompanyInfo,
+  rescueSquadInfo,
+  schoolInfo,
+  sportsClubInfo,
+} from '../lib/messages'
+
+type CompanyInfoType = {
+  name: string
+  nationalRegistrationId: string
+  email: string
+  phoneNumber: string
+}
+
+export const getRepresentativeData = (answers: FormValue) => {
+  if (isGeneralWorkplaceAccident(answers))
+    return {
+      info: (answers as { companyInfo: CompanyInfoType }).companyInfo,
+      title: companyInfo.labels.descriptionField,
+    }
+  if (isStudiesAccident(answers))
+    return {
+      info: (answers as { schoolInfo: CompanyInfoType }).schoolInfo,
+      title: schoolInfo.labels.descriptionField,
+    }
+  if (isFishermanAccident(answers))
+    return {
+      info: (answers as { fishingCompanyInfo: CompanyInfoType })
+        .fishingCompanyInfo,
+      title: fishingCompanyInfo.labels.descriptionField,
+    }
+  if (isProfessionalAthleteAccident(answers))
+    return {
+      info: (answers as { sportsClubInfo: CompanyInfoType }).sportsClubInfo,
+      title: sportsClubInfo.labels.descriptionField,
+    }
+  if (isRescueWorkAccident(answers))
+    return {
+      info: (answers as { rescueSquadInfo: CompanyInfoType }).rescueSquadInfo,
+      title: rescueSquadInfo.labels.descriptionField,
+    }
+}
