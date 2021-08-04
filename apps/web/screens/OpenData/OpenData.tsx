9c0b36ae282ac2fb63c139ca9c85ec0d957723e0
Alexandra Diljá <alexandrab18@ru.is>
2021-08-04 15:38:56 +0000

    frontpage graph scroll in mobile

--- apps/web/screens/OpenData/OpenData.tsx
+++ apps/web/screens/OpenData/OpenData.tsx
@@ -115,7 +115,11 @@ const OpenDataPage: Screen<OpenDataProps> = ({ page, megaMenuData }) => {
               </Box>
             </GridColumn>
             <GridColumn span={['12/12', '7/12', '7/12']}>
-              <SimpleLineChart graphData={pageHeaderGraph} />
+              <Box style={{ height: '340px', overflowX: 'scroll' }}>
+                <Box style={{ width: '899px', height: '100%' }}>
+                  <SimpleLineChart graphData={pageHeaderGraph} />
+                </Box>
+              </Box>
             </GridColumn>
           </GridRow>
         </GridContainer>
