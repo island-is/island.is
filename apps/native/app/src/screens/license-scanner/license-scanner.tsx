79f36697456639b422b241e5f3697a20aeb54ddd
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2021-08-11 12:06:24 +0000

    feat: barcode scanner results

--- apps/native/app/src/screens/license-scanner/license-scanner.tsx
+++ apps/native/app/src/screens/license-scanner/license-scanner.tsx
@@ -1,25 +1,25 @@
+import { dynamicColor, font } from '@island.is/island-ui-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics'
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { useIntl } from 'react-intl'
+import { StyleSheet, View } from 'react-native'
 import {
   Navigation,
   NavigationFunctionComponent,
 } from 'react-native-navigation'
-import { BarCodeScanner } from 'expo-barcode-scanner'
-import { authStore } from '../../stores/auth-store'
-import {
-  ComponentRegistry,
-  StackRegistry,
-} from '../../utils/component-registry'
 import {
   useNavigationButtonPress,
   useNavigationComponentDidAppear,
   useNavigationComponentDidDisappear,
 } from 'react-native-navigation-hooks/dist'
 import styled from 'styled-components/native'
-import { dynamicColor, font } from '@island.is/island-ui-native'
-import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics';
-import { useIntl } from 'react-intl'
 import { useThemedNavigationOptions } from '../../hooks/use-themed-navigation-options'
+import { authStore } from '../../stores/auth-store'
+import {
+  ComponentRegistry,
+  StackRegistry,
+} from '../../utils/component-registry'
 
 const Bubble = styled.View`
   padding-top: 16px;
@@ -61,19 +61,17 @@ const {
 export const LicenseScannerScreen: NavigationFunctionComponent = ({
   componentId,
 }) => {
-  useNavigationOptions(componentId);
+  useNavigationOptions(componentId)
   const [hasPermission, setHasPermission] = useState<boolean>()
   const [active, setActive] = useState(false)
-  const intl = useIntl();
+  const intl = useIntl()
 
   useEffect(() => {
-    ;(async () => {
-      authStore.setState({ noLockScreenUntilNextAppStateActive: true })
-      const { status } = await BarCodeScanner.requestPermissionsAsync()
-      console.log('status', status)
+    authStore.setState({ noLockScreenUntilNextAppStateActive: true })
+    BarCodeScanner.requestPermissionsAsync().then(({ status }) => {
       setHasPermission(status === 'granted')
       authStore.setState({ noLockScreenUntilNextAppStateActive: false })
-    })()
+    })
   }, [])
 
   useNavigationComponentDidAppear(() => {
@@ -91,7 +89,7 @@ export const LicenseScannerScreen: NavigationFunctionComponent = ({
   })
 
   const handleBarCodeScanned = ({ type, data }: any) => {
-    impactAsync(ImpactFeedbackStyle.Heavy);
+    impactAsync(ImpactFeedbackStyle.Heavy)
     setActive(false)
     Navigation.push(StackRegistry.LicenseScannerStack, {
       component: {
@@ -123,11 +121,10 @@ export const LicenseScannerScreen: NavigationFunctionComponent = ({
         <Bubble>
           <BubbleText>
             {typeof hasPermission === 'undefined'
-              ? intl.formatMessage({ id: 'licenseScanner.awaitingPermission'})
+              ? intl.formatMessage({ id: 'licenseScanner.awaitingPermission' })
               : hasPermission === false
-              ? intl.formatMessage({ id: 'licenseScanner.noCameraAccess'})
-              : intl.formatMessage({ id: 'licenseScanner.helperMessage'})
-            }
+              ? intl.formatMessage({ id: 'licenseScanner.noCameraAccess' })
+              : intl.formatMessage({ id: 'licenseScanner.helperMessage' })}
           </BubbleText>
         </Bubble>
       </View>
@@ -135,4 +132,4 @@ export const LicenseScannerScreen: NavigationFunctionComponent = ({
   )
 }
 
-LicenseScannerScreen.options = getNavigationOptions();
+LicenseScannerScreen.options = getNavigationOptions()
---
+++ apps/native/app/src/screens/license-scanner/scan-result-card.tsx
@@ -0,0 +1,224 @@
+import { font, Skeleton } from '@island.is/island-ui-native'
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import styled from 'styled-components/native'
+import agencyLogo from '../../assets/temp/agency-logo.png'
+
+import danger from '../../../../island-ui/src/assets/card/danger.png'
+import success from '../../../../island-ui/src/assets/card/is-verified.png'
+import background from '../../../../island-ui/src/assets/card/okuskyrteini.png'
+
+const Host = styled.View`
+  border-radius: 16px;
+  margin-bottom: 32px;
+`
+
+const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 24px;
+`
+
+const Subtitle = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+const SubtitleIcon = styled.View`
+  width: 24px;
+  height: 24px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 4px;
+  overflow: hidden;
+  margin-left: -3px;
+`
+
+const SubtitleImage = styled.Image`
+  margin-top: -2px;
+`
+
+const SubtitleText = styled.Text`
+  ${font({
+    fontWeight: '600',
+    fontSize: 13,
+    lineHeight: 15,
+  })}
+`
+
+const Detail = styled.View`
+  flex: 1;
+`
+
+const Title = styled.Text`
+  margin-bottom: 4px;
+
+  ${font({
+    fontWeight: '600',
+  })}
+`
+
+const Logo = styled.Image`
+  width: 62px;
+  height: 62px;
+  margin-top: -8px;
+`
+
+const Content = styled.View`
+  flex-direction: row;
+  padding: 16px 24px;
+  padding-top: 0px;
+`
+
+const Label = styled.Text`
+  ${font({
+    fontSize: 12,
+    color: '#8D6679',
+  })}
+  margin-bottom: 8px;
+`
+
+const Value = styled.Text`
+  ${font({
+    fontWeight: '600',
+    fontSize: 13,
+    lineHeight: 15,
+  })}
+`
+
+const LabelGroup = styled.View`
+  margin-bottom: 16px;
+`
+
+const Photo = styled.Image`
+  width: 79px;
+  height: 109px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 4px;
+`
+
+const Left = styled.View`
+  flex-direction: column;
+  margin-right: 16px;
+  flex: 1;
+`
+
+const Placeholder = styled.View`
+  background-color: white;
+  border-radius: 4px;
+  opacity: 0.2;
+  height: 16px;
+`
+
+const Background = styled.Image`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #e2c4d1;
+  border-radius: 16px;
+`
+
+interface ScanResultCardProps {
+  loading: boolean
+  error?: boolean
+  errorMessage?: string
+  nationalId?: string
+  name?: string
+  licenseNumber?: string
+  photo?: string
+}
+
+export function ScanResultCard(props: ScanResultCardProps) {
+  const {
+    error,
+    errorMessage,
+    loading,
+    nationalId,
+    name,
+    licenseNumber,
+    photo,
+  } = props
+
+  return (
+    <Host>
+      <Background source={background} resizeMode="stretch" />
+      <Header>
+        <Detail>
+          <Title>Ökuskírteini (IS)</Title>
+          <Subtitle>
+            <SubtitleIcon>
+              {loading ? (
+                <ActivityIndicator
+                  color="#000"
+                  animating
+                  size="small"
+                  style={{ transform: [{ scale: 0.8 }] }}
+                />
+              ) : error ? (
+                <SubtitleImage source={danger} />
+              ) : (
+                <SubtitleImage source={success} />
+              )}
+            </SubtitleIcon>
+            <SubtitleText>
+              {loading
+                ? 'Hleð upplýsingum'
+                : error
+                ? 'Villa við skönnun'
+                : 'Í gildi'}
+            </SubtitleText>
+          </Subtitle>
+        </Detail>
+        <Logo source={agencyLogo} />
+      </Header>
+      {error ? (
+        <Content>
+          <Left>
+            <LabelGroup>
+              <Label>Villuskilaboð</Label>
+              <Value>{errorMessage}</Value>
+            </LabelGroup>
+          </Left>
+        </Content>
+      ) : (
+        <Content>
+          <Left>
+            <LabelGroup>
+              <Label>Nafn</Label>
+              {loading ? (
+                <Placeholder style={{ width: 120 }} />
+              ) : (
+                <Value>
+                  {name}
+                </Value>
+              )}
+            </LabelGroup>
+            <LabelGroup>
+              <Label>Kennitala</Label>
+              {loading ? (
+                <Placeholder style={{ width: 120 }} />
+              ) : (
+                <Value>
+                  {nationalId?.substr(0, 6)}-{nationalId?.substr(-4)}
+                </Value>
+              )}
+            </LabelGroup>
+            <LabelGroup>
+              <Label>Númer</Label>
+              {loading ? (
+                <Placeholder style={{ width: 80 }} />
+              ) : (
+                <Value>{licenseNumber ?? 'N/A'}</Value>
+              )}
+            </LabelGroup>
+          </Left>
+          <Photo source={{ uri: photo }} />
+        </Content>
+      )}
+    </Host>
+  )
+}
