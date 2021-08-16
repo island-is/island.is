5d27ca9514cbaeb740cc0205f6c6ca00fa95af14
unnarb <unnarb@gmail.com>
2021-08-16 17:49:47 +0000

    with Treat

--- apps/unemployment/next.config.js
+++ apps/unemployment/next.config.js
@@ -1,5 +1,6 @@
 const withTreat = require('next-treat')()
+const withSourceMaps = require('@zeit/next-source-maps')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx')
 
-module.exports = withNx({})
+module.exports = withSourceMaps(withTreat({}))
