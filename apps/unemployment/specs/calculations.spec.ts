b246016ec30b75397660c08e4ac8d24042decc85
Ketill <ketill@ozio.is>
2021-08-17 20:47:09 +0000

    added file for calculations and tests

---
+++ apps/unemployment/specs/calculations.spec.ts
@@ -0,0 +1,8 @@
+// import React from 'react'
+import { makeCalculations } from '../utils/overview-calculations'
+
+describe('Calculations', () => {
+  it('should calculate correctly', () => {
+    makeCalculations({})
+  })
+})
---
+++ apps/unemployment/utils/overview-calculations.ts
@@ -0,0 +1,12 @@
+export interface CalculationParameters {}
+
+export function makeCalculations(params: CalculationParameters) {
+  let constants = {
+    BaseAmount: 307.43, // GrU
+    MaximumSalaryConnectedConnected: 472.835, // MaxTekjTeng
+    PercentOfSalary: 0.7, // PrTekjTeng
+    PercentOfParentalLeaveOnBaseUnemploymentBenefits: 0.06, //PrBbGr
+    PercentOfParentalLeaveOnPayConnectedBenefits: 0.04, // PrBbTekjuTeng
+    MaximumUnaffectedSalary: 71262, // FriTekjMark
+  }
+}
