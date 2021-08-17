922a8e0334854677c640766f4e7cf3cc5ab7dd0c
Ketill <ketill@ozio.is>
2021-08-17 22:04:32 +0000

    fixed initial step parsing

--- apps/unemployment/pages/application/[step].tsx
+++ apps/unemployment/pages/application/[step].tsx
@@ -7,76 +7,85 @@ import { ApplicationService } from './../../services/application.service'
 import { ApplicationData } from './../../entities/application-data'
 import { InitialInfo } from './../../entities/initial-info'
 import { UserService } from './../../services/user.service'
+import ChildrenUnderCare from '../../components/forms/children-under-care'
 
-        
 const Index: React.FC = () => {
-    const { query } = useRouter()
-    const stepQuery = query.step
-    const [step, setStep] = useState(1)
-    const router = useRouter()
-    const { user } = useContext(UserContext)
-    const [applicationData, setApplicationData] = useState<ApplicationData>()
-  
-    /** Load the client and set the step from query if there is one */
-    useEffect(() => {
-        let application = getApplication()
-        if (!application) {
-            application = ApplicationData.getFromUser(UserService.getUser())
-        }
-        setApplicationData(application)
-        document.title = "Samskipti"
-    }, [stepQuery, user])
-  
-    const getApplication = () => {
-        return ApplicationService.getApplication()
+  const { query } = useRouter()
+  const stepQuery = parseInt(query.step as string)
+  const [step, setStep] = useState(1)
+  const router = useRouter()
+  const { user } = useContext(UserContext)
+  const [applicationData, setApplicationData] = useState<ApplicationData>()
+
+  /** Load the client and set the step from query if there is one */
+  useEffect(() => {
+    let application = getApplication()
+    if (!application) {
+      application = ApplicationData.getFromUser(UserService.getUser())
     }
-  
-    const changesMade = () => {
-      getApplication()
+    setApplicationData(application)
+    document.title = 'Samskipti'
+
+    if (step !== stepQuery) {
+      setStep(stepQuery)
     }
-  
-    const handleNext = () => {
-      setStep(step + 1)
-    }
-  
-    const handleStepChange = (step: UnemploymentStep) => {
-      setStep(step)
-    }
-  
-    const handleBack = () => {
-      setStep(step - 1)
-    }
-  
-    const handleCancel = () => {
-      router.push('/clients')
-    }
-  
-    const handleSaved = (application: any) => {
-      console.log("saved")
-      console.log(application)
-        getApplication()
-        handleNext()      
-    }
-  
-    if (applicationData)
-    {
-      switch (step) {
-        case UnemploymentStep.PersonalInformation:
-          return <PersonalInformation defaultValues={applicationData} onSubmit={handleSaved} ></PersonalInformation>
-        case UnemploymentStep.Income:
-          return <div>2</div>
-        default: {
-          return (
-            <div></div>
-          )
-        }
-      }
-      
-    }
-    return <div></div>
-      
-    
-    
+  }, [stepQuery, user])
+
+  const getApplication = () => {
+    return ApplicationService.getApplication()
   }
-  
-  export default Index
+
+  const changesMade = () => {
+    getApplication()
+  }
+
+  const handleNext = () => {
+    setStep(step + 1)
+  }
+
+  const handleStepChange = (step: UnemploymentStep) => {
+    setStep(step)
+  }
+
+  const handleBack = () => {
+    setStep(step - 1)
+  }
+
+  const handleCancel = () => {
+    router.push('/clients')
+  }
+
+  const handleSaved = (application: any) => {
+    console.log('saved')
+    console.log(application)
+    getApplication()
+    handleNext()
+  }
+
+  if (applicationData) {
+    switch (step) {
+      case UnemploymentStep.PersonalInformation:
+        return (
+          <PersonalInformation
+            defaultValues={applicationData}
+            onSubmit={handleSaved}
+          ></PersonalInformation>
+        )
+      case UnemploymentStep.Income:
+        return <div>2</div>
+      case UnemploymentStep.ChildrenUnderCare:
+        return (
+          <ChildrenUnderCare
+            defaultValues={applicationData}
+            onSubmit={handleSaved}
+          />
+        )
+      default: {
+        return <div></div>
+      }
+    }
+  }
+  return <div></div>
+}
+
+export default Index
