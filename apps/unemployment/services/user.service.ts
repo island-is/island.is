4371cd9c448db3999bf78f0e54d4bb1abba41c69
Unnar Bjarnarson <unnar@fuglar.com>
2021-08-17 12:14:08 +0000

    user service

---
+++ apps/unemployment/services/user.service.ts
@@ -0,0 +1,14 @@
+import { User } from "../entities/user";
+
+export class UserService {
+    // TODO: Remove static and make injectable
+    public static getUser() : User {
+        return { name: "Guðrún Jónsdóttir", 
+        nationalId: "1706941119",
+        address: "Lindargata 3",
+        city: "Reykjavík",
+        postalCode: "101",
+        email: "gj@island.is"
+    } as User
+    }
+}
