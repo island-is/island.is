fb76d8034e47f43d93c70cf32265523acab36408
Ketill <ketill@ozio.is>
2021-08-17 20:33:54 +0000

    add calc constants to labor service

---
+++ apps/unemployment/entities/directorate-of-labour-constants.ts
@@ -0,0 +1,8 @@
+export interface CalculationConstants {
+  BaseAmount: number // GrU
+  MaximumSalaryConnectedConnected: number // MaxTekjTeng
+  PercentOfSalary: number // PrTekjTeng
+  PercentOfParentalLeaveOnBaseUnemploymentBenefits: number //PrBbGr
+  PercentOfParentalLeaveOnPayConnectedBenefits: number // PrBbTekjuTeng
+  MaximumUnaffectedSalary: number // FriTekjMark
+}
