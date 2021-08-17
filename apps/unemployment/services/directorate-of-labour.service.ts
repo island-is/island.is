420cfe6e5dbc4a026ad8a2465809d8ac6d24f161
Ketill <ketill@ozio.is>
2021-08-17 13:13:35 +0000

    checkin before type update

--- apps/unemployment/services/directorate-of-labour.service.ts
+++ apps/unemployment/services/directorate-of-labour.service.ts
@@ -1,3 +1,5 @@
+import { range } from 'lodash'
+import { education } from '../utils/mock-db-education'
 import { jobs } from '../utils/mock-db-jobs'
 import { mockAsync } from '../utils/service-utils'
 
@@ -28,7 +30,41 @@ export class DirectorateOfLabourService {
     ])
   }
 
-  public getRecognizedJobs: () => Promise<string[]> = async () => {
+  // TODO: FIX any
+  public getRecognizedJobs: () => Promise<any[]> = async () => {
     return mockAsync(jobs)
+  }
+
+  // TODO: FIX any
+  public getRecognizedEducation: () => Promise<any[]> = async () => {
+    return mockAsync(education)
+  }
+
+  public getUniversityEducationLevels: () => Promise<string[]> = async () => {
+    return mockAsync([
+      'Phd',
+      'MS',
+      'MPM',
+      'MPA',
+      'ML',
+      'MHRM',
+      'MBA',
+      'MAcc',
+      'MA',
+      'M.ed',
+      'Diploma',
+      'BS',
+      'BA',
+      'B.ed',
+    ])
+  }
+
+  public getSelectableYears: () => Promise<string[]> = async () => {
+    // TODO: Test
+    return mockAsync(range(2021, 1962, -1).map((num) => num.toString()))
+  }
+
+  public getDrivingLicenseTypes: () => Promise<string[]> = async () => {
+    return mockAsync(range(2021, 1962, -1).map((num) => num.toString()))
   }
 }
