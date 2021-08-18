ae73c269af51f9aa7c0f9bfca34ddd078ca5b477
Björn Lárus Arnórsson <bjornlarus@gmail.com>
2021-08-18 01:24:30 +0000

    ui

--- apps/unemployment/components/steps/steps.tsx
+++ apps/unemployment/components/steps/steps.tsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import { UnemploymentStep } from "./../../entities/enums/unemployment-step.enum";
-
+import {
+    FormStepper
+} from '@island.is/island-ui/core'
 interface Props {
     step: UnemploymentStep
 }
 
-const Steps: React.FC<Props> = ({step}) => {
+const Steps: React.FC<Props> = ({ step }) => {
     // TODO: List up steps and set the active step
-    return <div className="step active">Steps</div>
+    console.log(step)
+    return <div className="step active">
+        <FormStepper theme="blue"
+            sections={[{
+                name: 'Samskipti'
+            }, {
+                name: 'Atvinnulok'
+            }, {
+                name: 'Fylgigögn'
+            }, {
+                name: 'Börn'
+            }, {
+                name: 'Uppgjör'
+            },
+            {
+                name: 'Lífeyrir'
+            },
+            {
+                name: 'Persónuafsláttur'
+            },
+            {
+                name: 'Fjármagnstekjur'
+            },
+            {
+                name: 'Launaupplýsingar'
+            },
+            {
+                name: 'Viðbótarupplýsingar'
+            },
+            {
+                name: 'Sundurliðun'
+            }]} />
+    </div>
 }
 
 export default Steps
