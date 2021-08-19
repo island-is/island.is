f94e4450366792f0b5d50d3394b4547d0bb53248
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-08-19 10:26:31 +0000

    dno prettier lul

--- infra/scripts/ci/prettier-check.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-# Run code tests
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT="$DIR/../.."
-
-(cd "$ROOT"; ./node_modules/.bin/prettier --check .)
