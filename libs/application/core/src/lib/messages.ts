fb51f4fed5f0b7c9eabb9e8d80d85bf279bfb3dc
Jérémy Barbet <jeremgraph@gmail.com>
2021-08-19 09:06:24 +0000

    fix: remove unused messages

--- libs/application/core/src/lib/messages.ts
+++ libs/application/core/src/lib/messages.ts
@@ -202,17 +202,6 @@ export const coreErrorMessages = defineMessages({
     defaultMessage: 'Ekki er hægt að tengja umsókn án auðkenningartóka',
     description: 'It is not possible to open an application without a token',
   },
-  expiredTokenWithRetryErrorTitle: {
-    id: 'application.system:core.expired.token.with.retry.error.title',
-    defaultMessage: 'Úps! Tölvupóstur tengill útrunninn',
-    description: 'Oops! Email link expired',
-  },
-  expiredTokenWithRetryErrorDescription: {
-    id: 'application.system:core.expired.token.with.retry.error.description',
-    defaultMessage:
-      'Fyrri hlekkur sem þú fékkst í tölvupósti er útrunninn, við sendum þér nýjan á skráð netfang, skoðaðu pósthólfið þitt til að finna nýjan link',
-    description: `Your previous email link has expired and we're sending a new email to the assigned person, check your email to find the new link`,
-  },
   couldNotAssignApplicationErrorTitle: {
     id: 'application.system:could.not.assign.application.error.title',
     defaultMessage: 'Úps! Ekki tókst að tengjast umsókn',
