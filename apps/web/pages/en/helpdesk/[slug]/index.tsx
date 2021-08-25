ae324daeccf0b6c776a4c0aef4ed64b9c7116302
kksteini <steini@kosmosogkaos.is>
2021-08-25 10:39:20 +0000

    Removed old files

--- apps/web/pages/en/helpdesk/[slug]/index.tsx
+++
@@ -1,5 +0,0 @@
-import withApollo from '@island.is/web/graphql/withApollo'
-import { withLocale } from '@island.is/web/i18n'
-import serviceWebCategoryPage from '@island.is/web/screens/ServiceWeb/Category/Category'
-
-export default withApollo(withLocale('en')(serviceWebCategoryPage))
