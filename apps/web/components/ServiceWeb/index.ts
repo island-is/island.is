d1dc8b7cf24314e73f5af46784eb0d70df86ab6c
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-08-27 12:43:43 +0000

    Remove unused file and package

--- apps/web/components/ServiceWeb/index.ts
+++ apps/web/components/ServiceWeb/index.ts
@@ -1,4 +1,3 @@
 export { ServiceWebSearchSection } from './SearchSection'
 export { ServiceWebHeader } from './Header'
 export { ServiceWebSearchInput } from './SearchInput'
-export { renderHtml } from './utils/render-html'
