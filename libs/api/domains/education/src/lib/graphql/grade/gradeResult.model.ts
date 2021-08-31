d65672691d31751e654acd3fa26fe0eadebeeff3
Brian <barabrian89@gmail.com>
2021-08-31 13:03:42 +0000

    feat(service-portal-education): implementing v2 student assessment api (#4657)
    * Adding elementary grades
    
    * Putting generic grades and courses into arrays
    
    * StudentAssessmentTable based on new data structure
    
    * init and start for service-portal-education
    
    * only map if grade is not undefined
    
    * remove console.log
    
    * remove educationStudentAssessment feature flag
    
    Co-authored-by: Davíð Guðni <davidgudni@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/education/src/lib/graphql/grade/gradeResult.model.ts
+++ libs/api/domains/education/src/lib/graphql/grade/gradeResult.model.ts
@@ -1,20 +1,12 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 
-import { IcelandicGrade } from './icelandicGrade.model'
-import { EnglishGrade } from './englishGrade.model'
-import { MathGrade } from './mathGrade.model'
+import { CourseGrade } from './courseGrade.model'
 
 @ObjectType('EducationGradeResult')
 export class GradeResult {
   @Field(() => String)
   studentYear!: string
 
-  @Field(() => IcelandicGrade, { nullable: true })
-  icelandicGrade?: IcelandicGrade
-
-  @Field(() => MathGrade, { nullable: true })
-  mathGrade?: MathGrade
-
-  @Field(() => EnglishGrade, { nullable: true })
-  englishGrade?: EnglishGrade
+  @Field(() => [CourseGrade])
+  courses: CourseGrade[]
 }
