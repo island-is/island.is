15164e7c901132c39c17c987513d9c40b001e515
Sigurður Fannar Vilhelmsson <sigurdur@stefna.is>
2021-09-01 17:04:54 +0000

    Add Auction to Syslumenn API client

--- libs/api/domains/syslumenn/src/lib/syslumenn.resolver.ts
+++ libs/api/domains/syslumenn/src/lib/syslumenn.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Directive, Query, Resolver } from '@nestjs/graphql'
 import { GetHomestaysInput } from './dto/getHomestays.input'
 import { Homestay } from './models/homestay'
+import { SyslumennAuction } from './models/syslumennAuction'
 import { SyslumennService } from './syslumenn.service'
 
 const cacheTime = process.env.CACHE_TIME || 5
@@ -9,11 +10,18 @@ const cacheControlDirective = (ms = cacheTime) => `@cacheControl(maxAge: ${ms})`
 
 @Resolver()
 export class SyslumennResolver {
-  constructor(private syslumennService: SyslumennService) {}
+  constructor(private syslumennService: SyslumennService) { }
 
   @Directive(cacheControlDirective())
   @Query(() => [Homestay])
   getHomestays(@Args('input') input: GetHomestaysInput): Promise<Homestay[]> {
     return this.syslumennService.getHomestays(input.year)
+  }
+
+  // Note: We need to be careful when caching the Auction data, as it's prone to changes only minutes before the auction takes place.
+  // TODO: Check if the CACHE_TIME in production. If it's only a few seconds, we should consider caching the data.
+  @Query(() => [SyslumennAuction])
+  getSyslumennAuctions(): Promise<SyslumennAuction[]> {
+    return this.syslumennService.getSyslumennAuctions()
   }
 }
--- libs/api/domains/syslumenn/src/lib/syslumenn.service.ts
+++ libs/api/domains/syslumenn/src/lib/syslumenn.service.ts
@@ -1,16 +1,23 @@
 import { SyslumennClient } from './client/syslumenn.client'
 import { Homestay, mapHomestay } from './models/homestay'
+import { SyslumennAuction, mapSyslumennAuction } from './models/syslumennAuction'
 import { Injectable } from '@nestjs/common'
 import { Person, Attachment, DataUploadResponse } from './models/dataUpload'
 
 @Injectable()
 export class SyslumennService {
-  constructor(private syslumennClient: SyslumennClient) {}
+  constructor(private syslumennClient: SyslumennClient) { }
 
   async getHomestays(year?: number): Promise<Homestay[]> {
     const homestays = await this.syslumennClient.getHomestays(year)
 
     return (homestays ?? []).map(mapHomestay)
+  }
+
+  async getSyslumennAuctions(): Promise<SyslumennAuction[]> {
+    const syslumennAuctions = await this.syslumennClient.getSyslumennAuctions()
+
+    return (syslumennAuctions ?? []).map(mapSyslumennAuction)
   }
 
   async uploadData(
