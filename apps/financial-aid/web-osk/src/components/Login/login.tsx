e8f5b7663f7e62c535a36e411ffb2af49f4e7dcb
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2021-09-03 12:23:58 +0000

    feat(fa): Mock current application (#4840)
    * Adds fake current application for fake users
    
    * Moves const
    
    * Fixes build
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/financial-aid/web-osk/src/components/Login/login.tsx
+++ apps/financial-aid/web-osk/src/components/Login/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GridContainer, Button, Text } from '@island.is/island-ui/core'
+import { GridContainer, Button, Text, Box } from '@island.is/island-ui/core'
 
 import { useRouter } from 'next/router'
 
@@ -32,32 +32,45 @@ const Login = ({ headline, about }: Props) => {
       >
         Innskráning
       </Button>
-      <br />
-      <br />
-      <Button
-        onClick={() => {
-          router.push(`${apiLoginRoute}?nationalId=0000000000`)
-        }}
-        data-testid="logout-button"
-        preTextIconType="filled"
-        type="button"
-        variant="primary"
-      >
-        Plat notandi (Árnason)
-      </Button>
-      <br />
-      <br />
-      <Button
-        onClick={() => {
-          router.push(`${apiLoginRoute}?nationalId=0000000001`)
-        }}
-        data-testid="logout-button"
-        preTextIconType="filled"
-        type="button"
-        variant="primary"
-      >
-        Plat notandi (Margrétdóttir)
-      </Button>
+      <Box paddingTop={4}>
+        <Button
+          onClick={() => {
+            router.push(`${apiLoginRoute}?nationalId=0000000000`)
+          }}
+          data-testid="logout-button"
+          preTextIconType="filled"
+          type="button"
+          variant="primary"
+        >
+          Plat notandi (Árnason)
+        </Button>
+      </Box>
+      <Box paddingTop={4}>
+        <Button
+          onClick={() => {
+            router.push(`${apiLoginRoute}?nationalId=0000000001`)
+          }}
+          data-testid="logout-button"
+          preTextIconType="filled"
+          type="button"
+          variant="primary"
+        >
+          Plat notandi (Margrétardóttir)
+        </Button>
+      </Box>
+      <Box paddingTop={4}>
+        <Button
+          onClick={() => {
+            router.push(`${apiLoginRoute}?nationalId=0000000003`)
+          }}
+          data-testid="logout-button"
+          preTextIconType="filled"
+          type="button"
+          variant="primary"
+        >
+          Plat notandi (Frilluson)
+        </Button>
+      </Box>
     </GridContainer>
   )
 }
