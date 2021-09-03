fb599249d76ccc73fc8cbba07578bf6aa35a6928
Sigurður Fannar Vilhelmsson <sigurdur@stefna.is>
2021-09-03 10:05:29 +0000

    feat(web): Syslumenn auctions via Syslumenn API (#4823)
    * Auctions fetched via Syslumenn API and listed as a flat list of lots
    
    * Fix formatting
    
    * Remove obsolete auction details page
    
    * Fix input focusing problem and get link color from theme
    
    * Fix formatting
    
    Co-authored-by: Brynjar Ingimarsson <brynjar@stefna.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/web/pages/s/syslumenn/uppbod/[slug]/index.ts
+++
@@ -1,5 +0,0 @@
-import withApollo from '@island.is/web/graphql/withApollo'
-import { withLocale } from '@island.is/web/i18n'
-import Auction from '@island.is/web/screens/Organization/Syslumenn/Auction'
-
-export default withApollo(withLocale('is')(Auction))
