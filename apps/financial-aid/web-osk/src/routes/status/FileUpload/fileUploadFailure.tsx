008e7a149f4de6ee734cf282bd9768ef901fc56b
RikkiAbb <rikardarnar@gmail.com>
2021-09-08 16:18:06 +0000

    Changes anchor to link

--- apps/financial-aid/web-osk/src/routes/status/FileUpload/fileUploadFailure.tsx
+++ apps/financial-aid/web-osk/src/routes/status/FileUpload/fileUploadFailure.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { FileUploadResult } from '@island.is/financial-aid-web/osk/src/components'
-import { Text } from '@island.is/island-ui/core'
+import { Text, Link } from '@island.is/island-ui/core'
 
 const FileUploadFailure = () => {
   const router = useRouter()
@@ -15,9 +15,14 @@ const FileUploadFailure = () => {
     >
       <Text marginTop={5}>
         Þú getur reynt aftur síðar eða sent gögnin með tölvupósti á{' '}
-        <a href="mailto: felagsthjonusta@hafnarfjordur.is">
+        <Link
+          href="mailto: felagsthjonusta@hafnarfjordur.is"
+          color="blue400"
+          underline="small"
+          underlineVisibility="always"
+        >
           felagsthjonusta@hafnarfjordur.is
-        </a>
+        </Link>
         . Gættu þess að láta kennitölu þína fylgja með gögnunum ef þú sendir þau
         með tölvupósti.
       </Text>
