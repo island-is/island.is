db9a449906df58b48ccecc4b7972af7f9700000e
RikkiAbb <rikardarnar@gmail.com>
2021-09-09 09:43:22 +0000

    Formats

--- apps/financial-aid/web-osk/src/components/FileUploadResult/FileUploadResult.tsx
+++ apps/financial-aid/web-osk/src/components/FileUploadResult/FileUploadResult.tsx
@@ -4,7 +4,7 @@ import {
   StatusLayout,
   Footer,
 } from '@island.is/financial-aid-web/osk/src/components'
-import FileList from '@island.is/financial-aid/shared/components'
+import { FileList } from '@island.is/financial-aid/shared/components'
 import { FormContext } from '@island.is/financial-aid-web/osk/src/components/FormProvider/FormProvider'
 import { Box, Text } from '@island.is/island-ui/core'
 import * as styles from './fileUpload.treat'
