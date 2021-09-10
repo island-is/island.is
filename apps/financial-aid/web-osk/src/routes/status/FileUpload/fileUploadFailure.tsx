2923ca70c91cbc61152428022be66bcdc0edcd7b
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-09-10 16:27:03 +0000

    markgin error

--- apps/financial-aid/web-osk/src/routes/status/FileUpload/fileUploadFailure.tsx
+++ apps/financial-aid/web-osk/src/routes/status/FileUpload/fileUploadFailure.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { FileUploadResult } from '@island.is/financial-aid-web/osk/src/components'
-import { Text, Link, Button, Box } from '@island.is/island-ui/core'
+import { Text, Link, Button } from '@island.is/island-ui/core'
 import { Routes } from '@island.is/financial-aid/shared/lib'
 
 const FileUploadFailure = () => {
@@ -17,8 +17,9 @@ const FileUploadFailure = () => {
       ${Routes.statusFileUpload(router.query.id as string)}`)
       }
     >
-      <Box marginTop={5}>
+      <Text marginTop={5}>
         Þú getur reynt aftur síðar eða sent gögnin með tölvupósti á{' '}
+        {/* Todo verður ekki blátt */}
         <Link
           href="mailto: felagsthjonusta@hafnarfjordur.is"
           color="blue400"
@@ -29,7 +30,7 @@ const FileUploadFailure = () => {
         </Link>
         . Gættu þess að láta kennitölu þína fylgja með gögnunum ef þú sendir þau
         með tölvupósti.
-      </Box>
+      </Text>
     </FileUploadResult>
   )
 }
