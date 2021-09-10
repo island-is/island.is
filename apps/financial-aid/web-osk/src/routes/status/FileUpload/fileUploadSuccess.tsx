297f22074ed034d630c3bc2c3ba506029a0aa423
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-09-10 14:03:14 +0000

    emtying form if successfull

--- apps/financial-aid/web-osk/src/routes/status/FileUpload/fileUploadSuccess.tsx
+++ apps/financial-aid/web-osk/src/routes/status/FileUpload/fileUploadSuccess.tsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useRouter } from 'next/router'
 import { FileUploadResult } from '@island.is/financial-aid-web/osk/src/components'
 import { Routes } from '@island.is/financial-aid/shared/lib'
-import { Box } from '@island.is/island-ui/core'
-
-// import * as styles from '@island.is/financial-aid/shared/components/FileList/FileList.treat'
+import { FormContext } from '@island.is/financial-aid-web/osk/src/components/FormProvider/FormProvider'
 
 const FileUploadSuccess = () => {
   const router = useRouter()
+
+  const { updateForm } = useContext(FormContext)
 
   return (
     <FileUploadResult
       subtitle={'Eftirfarandi gögn hafa verið send inn'}
       subtitleColor={'mint600'}
       nextButtonText={'Til baka á forsíðu'}
-      nextButtonAction={() =>
+      nextButtonAction={() => {
+        updateForm({
+          submitted: false,
+          incomeFiles: [],
+          taxReturnFiles: [],
+          otherFiles: [],
+        })
         router.push(`
       ${Routes.statusPage(router.query.id as string)}`)
-      }
-    >
-      {/* <Box className={styles.filesLink}></Box> */}
-    </FileUploadResult>
+      }}
+    />
   )
 }
 
