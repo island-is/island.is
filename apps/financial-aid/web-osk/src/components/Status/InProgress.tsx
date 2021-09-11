244accfa918b36369070c52df4b0f4685b0699ae
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-09-11 18:29:55 +0000

    adding consinecty to get month

--- apps/financial-aid/web-osk/src/components/Status/InProgress.tsx
+++ apps/financial-aid/web-osk/src/components/Status/InProgress.tsx
@@ -22,14 +22,14 @@ interface Props {
 const InProgress = ({ currentApplication }: Props) => {
   const router = useRouter()
 
-  const nextMonth = parseInt(format(new Date(), 'MM'))
+  const nextMonth = months[new Date().getMonth()].toLowerCase()
   const currentYear = format(new Date(), 'yyyy')
 
   return (
     <>
       <Text as="h2" variant="h3" color="blue400" marginBottom={[4, 4, 5]}>
         Umsókn {getState[currentApplication.state].toLowerCase()} til útgreiðslu
-        í {months[nextMonth].toLowerCase()} {` `} {currentYear}
+        í {nextMonth} {` `} {currentYear}
       </Text>
 
       {currentApplication.state === ApplicationState.DATANEEDED && (
