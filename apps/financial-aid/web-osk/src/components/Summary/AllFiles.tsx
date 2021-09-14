4755711147b5e3fd9672ce296b1e328a4c541b4c
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-09-14 11:06:17 +0000

    removing unused check

--- apps/financial-aid/web-osk/src/components/Summary/AllFiles.tsx
+++ apps/financial-aid/web-osk/src/components/Summary/AllFiles.tsx
@@ -27,36 +27,32 @@ const AllFiles = () => {
       <Box marginRight={3}>
         <Text fontWeight="semiBold">Gögn</Text>
         <Box>
-          {allFiles && (
-            <>
-              {allFiles.map((file, index) => {
-                return (
-                  <>
-                    {file && (
-                      <a
-                        href={file.name}
-                        key={`file-` + index}
-                        className={styles.filesButtons}
-                        target="_blank"
-                        download
-                      >
-                        <Box marginRight={1} display="flex" alignItems="center">
-                          <Icon
-                            color="blue400"
-                            icon="document"
-                            size="small"
-                            type="outline"
-                          />
-                        </Box>
+          {allFiles.map((file, index) => {
+            return (
+              <>
+                {file && (
+                  <a
+                    href={file.name}
+                    key={`file-` + index}
+                    className={styles.filesButtons}
+                    target="_blank"
+                    download
+                  >
+                    <Box marginRight={1} display="flex" alignItems="center">
+                      <Icon
+                        color="blue400"
+                        icon="document"
+                        size="small"
+                        type="outline"
+                      />
+                    </Box>
 
-                        <Text>{file.name}</Text>
-                      </a>
-                    )}
-                  </>
-                )
-              })}
-            </>
-          )}
+                    <Text>{file.name}</Text>
+                  </a>
+                )}
+              </>
+            )
+          })}
         </Box>
       </Box>
 
