caab0e41ed40e89b34f3b63a02fbb190a81f33af
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-09-14 14:01:06 +0000

    WIP

--- apps/web/pages/homestatic/index.tsx
+++ apps/web/pages/homestatic/index.tsx
@@ -1,4 +1,6 @@
 // import withApollo from '@island.is/web/graphql/withApollo'
 // import { withLocale } from '@island.is/web/i18n'
-export { getStaticProps } from '@island.is/web/screens/HomeStatic'
-export { HomeStatic as default } from '@island.is/web/screens/HomeStatic'
+export {
+  HomeStatic as default,
+  getStaticProps,
+} from '@island.is/web/screens/HomeStatic'
