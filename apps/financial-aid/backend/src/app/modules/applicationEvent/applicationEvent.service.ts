7dd0a8fe945c93a414dfd32ee76673d04535a7b6
RikkiAbb <rikardarnar@gmail.com>
2021-09-14 10:29:08 +0000

    Adds logger to application events

--- apps/financial-aid/backend/src/app/modules/applicationEvent/applicationEvent.service.ts
+++ apps/financial-aid/backend/src/app/modules/applicationEvent/applicationEvent.service.ts
@@ -1,22 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Inject } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 
 import { ApplicationEventModel } from './models'
 
 import { CreateApplicationEventDto } from './dto'
 
+import type { Logger } from '@island.is/logging'
+import { LOGGER_PROVIDER } from '@island.is/logging'
+
 @Injectable()
 export class ApplicationEventService {
   constructor(
     @InjectModel(ApplicationEventModel)
     private readonly applicationEventModel: typeof ApplicationEventModel,
+    @Inject(LOGGER_PROVIDER)
+    private readonly logger: Logger,
   ) {}
 
   async getAll(): Promise<ApplicationEventModel[]> {
+    this.logger.debug('Getting all application events')
     return this.applicationEventModel.findAll()
   }
 
   async findById(id: string): Promise<ApplicationEventModel[]> {
+    this.logger.debug('Finding application event by id')
     return this.applicationEventModel.findAll({
       where: {
         applicationId: id,
@@ -28,7 +35,7 @@ export class ApplicationEventService {
   async create(
     applicationEvent: CreateApplicationEventDto,
   ): Promise<ApplicationEventModel> {
-    // this.logger.debug('Creating a new application event')
+    this.logger.debug('Creating a new application event')
     return this.applicationEventModel.create(applicationEvent)
   }
 }
