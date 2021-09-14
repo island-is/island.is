3da1dd7ae4225d104858fed032f4feb2b119e182
Þórður Hafliðason <tolleinn@gmail.com>
2021-09-14 15:47:55 +0000

    fix: use proper translation namespaces for wip section

--- libs/service-portal/wip/src/screens/FinanceWIP/FinanceWIP.tsx
+++ libs/service-portal/wip/src/screens/FinanceWIP/FinanceWIP.tsx
@@ -7,22 +7,22 @@ import { defineMessage } from 'react-intl'
 import { useNamespaces } from '@island.is/localization'
 
 export const SettingsWIP: ServicePortalModuleComponent = () => {
-  useNamespaces('sp.finance')
+  useNamespaces('sp.wip')
 
   return (
     <InfoScreen
       title={defineMessage({
-        id: 'sp.finance:title',
+        id: 'sp.wip:finance-title',
         defaultMessage: 'Fjármál',
       })}
       intro={defineMessage({
-        id: 'sp.finance:wip-intro',
+        id: 'sp.wip:finance-intro',
         defaultMessage: `Hér eru upplýsingar um það sem kemur til með að koma inn undir
         fjármál á næstunni.`,
       })}
       externalHref="https://minarsidur.island.is/minar-sidur/fjarmal/fjarmal-stada-vid-rikissjod-og-stofnanir/"
       externalLinkTitle={defineMessage({
-        id: 'sp.finance:external-link-title',
+        id: 'sp.wip:external-link-title',
         defaultMessage: 'Fara á fjármál',
       })}
       figure="./assets/images/working.jpg"
