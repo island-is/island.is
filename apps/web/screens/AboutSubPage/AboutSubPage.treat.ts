f9f676cb4f7fd3ee8abf0c3c82fc1cb5e585e7c5
kksteini <steini@kosmosogkaos.is>
2021-09-16 10:58:30 +0000

    Reverted accidental treat addition

--- apps/web/screens/AboutSubPage/AboutSubPage.treat.ts
+++ apps/web/screens/AboutSubPage/AboutSubPage.treat.ts
@@ -1,6 +1,5 @@
 import { style } from 'treat'
 import { theme } from '@island.is/island-ui/theme'
-import { responsiveStyleMap } from '@island.is/island-ui/theme'
 
 export const bottomContent = style({
   margin: 'auto',
@@ -8,25 +7,5 @@ export const bottomContent = style({
     [`screen and (min-width: ${theme.breakpoints.xl}px)`]: {
       padding: `0 ${theme.grid.gutter.mobile * 2}px`,
     },
-  },
-})
-
-export const gradient = style({
-  background:
-    'linear-gradient(120.92deg, #0161FD -0.52%, #3F46D2 29.07%, #812EA4 59.85%, #C21578 90.63%, #FD0050 117.86%)',
-})
-
-export const tabSectionImg = responsiveStyleMap({
-  maxWidth: {
-    md: '382px',
-  },
-  maxHeight: '400px',
-  width: '100%',
-  float: 'right',
-  marginLeft: {
-    md: '20px',
-  },
-  marginRight: {
-    lg: '-80px',
   },
 })
