1563d984632849a6c28d1bf9cb16aa46dadc4572
RikkiAbb <rikardarnar@gmail.com>
2021-09-20 13:00:28 +0000

    Adds staff table

---
+++ apps/financial-aid/backend/migrations/20210920105246-create-staff.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.createTable(
+        'staff',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          national_id: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          municipalityId: {
+            type: Sequelize.UUID,
+            allowNull: false,
+          },
+          role: {
+            type: Sequelize.ENUM('employee', 'admin'),
+            allowNull: false,
+          },
+          active: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+          },
+          phone_number: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        },
+        { transaction: t },
+      ),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.dropTable('staff', { transaction: t }),
+    )
+  }
+};
