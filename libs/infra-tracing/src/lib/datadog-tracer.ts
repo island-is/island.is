e48b8cbe16154187209ae3bc2dd00abc69566a9d
Petar Shomov <petar@andes.is>
2021-09-22 15:29:33 +0000

    removing some PII info from URLs when sending to DataDog

--- libs/infra-tracing/src/lib/datadog-tracer.ts
+++ libs/infra-tracing/src/lib/datadog-tracer.ts
@@ -1,8 +1,27 @@
 import tracer from 'dd-trace'
+import { isPerson } from 'kennitala'
 
 if (process.env.NODE_ENV !== 'development') {
   tracer.init({ logInjection: true }) // initialized in a different file to avoid hoisting.
   tracer.use('express', { blacklist: ['/liveness', '/readiness', '/metrics'] })
+  tracer.use('http', {
+    client: {
+      hooks: {
+        request: (span, req, res) => {
+          const url = res?.url
+          if (url) {
+            const urlRewritten = url
+              .split('/')
+              .map((segm) =>
+                isPerson(segm) && segm.length === 10 ? '--MASKED--' : segm,
+              )
+              .join('/')
+            span?.setTag('http.url', urlRewritten)
+          }
+        },
+      },
+    },
+  })
 }
 
 export default tracer
