e8eb1823ba73bcc1c26503b2bb49e8f4e178bc34
Andri Andresson <andriandresson@gmail.com>
2021-09-23 13:18:42 +0000

    chore: fixing typo + undo commented code

--- libs/application/templates/accident-notification/src/forms/AccidentNotificationForm/externalDataSection.ts
+++ libs/application/templates/accident-notification/src/forms/AccidentNotificationForm/externalDataSection.ts
@@ -1,10 +1,14 @@
 import {
   buildCustomField,
+  buildDataProviderItem,
+  buildExternalDataProvider,
   buildMultiField,
   buildSection,
+  buildSubSection,
 } from '@island.is/application/core'
 
 import { externalData } from '../../lib/messages'
+import { DataProviderTypes } from '../../types'
 
 export const externalDataSection = buildSection({
   id: 'ExternalDataSection',
@@ -21,44 +25,44 @@ export const externalDataSection = buildSection({
         }),
       ],
     }),
-    // buildSubSection({
-    //   id: 'AccidentNotificationForm',
-    //   title: externalData.dataProvider.sectionTitle,
-    //   children: [
-    //     buildExternalDataProvider({
-    //       title: externalData.dataProvider.pageTitle,
-    //       id: 'approveExternalData',
-    //       subTitle: externalData.dataProvider.subTitle,
-    //       description: '',
-    //       checkboxLabel: externalData.dataProvider.checkboxLabel,
-    //       dataProviders: [
-    //         buildDataProviderItem({
-    //           id: 'nationalRegistry',
-    //           type: DataProviderTypes.NationalRegistry,
-    //           title: externalData.nationalRegistry.title,
-    //           subTitle: externalData.nationalRegistry.description,
-    //         }),
-    //         buildDataProviderItem({
-    //           id: 'userProfile',
-    //           type: DataProviderTypes.UserProfile,
-    //           title: externalData.userProfile.title,
-    //           subTitle: externalData.userProfile.description,
-    //         }),
-    //         buildDataProviderItem({
-    //           id: 'revAndCustoms',
-    //           type: '',
-    //           title: externalData.revAndCustoms.title,
-    //           subTitle: externalData.revAndCustoms.description,
-    //         }),
-    //         buildDataProviderItem({
-    //           id: 'notifications',
-    //           type: '',
-    //           title: externalData.notifications.title,
-    //           subTitle: externalData.notifications.description,
-    //         }),
-    //       ],
-    //     }),
-    //   ],
-    // }),
+    buildSubSection({
+      id: 'AccidentNotificationForm',
+      title: externalData.dataProvider.sectionTitle,
+      children: [
+        buildExternalDataProvider({
+          title: externalData.dataProvider.pageTitle,
+          id: 'approveExternalData',
+          subTitle: externalData.dataProvider.subTitle,
+          description: '',
+          checkboxLabel: externalData.dataProvider.checkboxLabel,
+          dataProviders: [
+            buildDataProviderItem({
+              id: 'nationalRegistry',
+              type: DataProviderTypes.NationalRegistry,
+              title: externalData.nationalRegistry.title,
+              subTitle: externalData.nationalRegistry.description,
+            }),
+            buildDataProviderItem({
+              id: 'userProfile',
+              type: DataProviderTypes.UserProfile,
+              title: externalData.userProfile.title,
+              subTitle: externalData.userProfile.description,
+            }),
+            buildDataProviderItem({
+              id: 'revAndCustoms',
+              type: '',
+              title: externalData.revAndCustoms.title,
+              subTitle: externalData.revAndCustoms.description,
+            }),
+            buildDataProviderItem({
+              id: 'notifications',
+              type: '',
+              title: externalData.notifications.title,
+              subTitle: externalData.notifications.description,
+            }),
+          ],
+        }),
+      ],
+    }),
   ],
 })
