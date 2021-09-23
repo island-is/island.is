80f48cc54abe4238da34062cab996246916956c0
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-09-23 14:56:20 +0000

    logohref correct

--- apps/financial-aid/web-osk/src/components/Logo/index.tsx
+++ apps/financial-aid/web-osk/src/components/Logo/index.tsx
@@ -7,50 +7,16 @@ interface LogoProps {
   className?: string
 }
 
-// const LogoHfj = dynamic(() =>
-//   import('@island.is/financial-aid/shared/components'),
-// )
-
-// const importIcon = (icon: any) =>
-//   dynamic(() =>
-//     import(`@island.is/financial-aid/shared/components`)
-//       .then((c) => {
-//         console.log(c)
-//         return c
-//       })
-//       .catch((err) => {
-//         console.log(c)
-//         return () => <span />
-//       }),
-//   )
 const Logo = ({ className }: LogoProps) => {
   const { municipality } = useContext(MunicipalityContext)
-  console.log(municipality)
-
-  // const [name, setName] = useState('sis')
-
-  // import { FileList } from '@island.is/financial-aid/shared/components'
-
-  // const [icon, setIcon] = useState(null)
-  // console.log(icon)
-
-  // // // Lazy load all icons!
-  // useEffect(() => {
-  //   async function load() {
-  //     const LoadedIcon = await importIcon('LogoHfj')
-  //     return <LoadedIcon />
-  //   }
-
-  //   load().then((e) => {
-  //     if (!icon) {
-  //       setIcon(e)
-  //     }
-  //   })
-  // }, [icon])
 
   return (
     <a
-      href={'https://www.samband.is/'}
+      href={
+        municipality?.homePage
+          ? municipality.homePage
+          : 'https://www.samband.is/'
+      }
       target="_blank"
       className={cn({ [`${className}`]: true })}
     >
