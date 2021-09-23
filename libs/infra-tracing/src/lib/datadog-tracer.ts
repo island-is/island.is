4e1d4a735b1bf7c817b50275f9173f41d87ffc50
Petar Shomov <petar@andes.is>
2021-09-23 15:40:20 +0000

    adding a bit of logging to find out why this is not working

--- libs/infra-tracing/src/lib/datadog-tracer.ts
+++ libs/infra-tracing/src/lib/datadog-tracer.ts
@@ -1,5 +1,6 @@
 import tracer from 'dd-trace'
 import { isPerson } from 'kennitala'
+import { logger } from '@island.is/logging'
 
 if (process.env.NODE_ENV !== 'development') {
   tracer.init({ logInjection: true }) // initialized in a different file to avoid hoisting.
@@ -8,6 +9,7 @@ if (process.env.NODE_ENV !== 'development') {
     client: {
       hooks: {
         request: (span, req, res) => {
+          logger.info(`HTTP Tracer: Got span with response url: ${res?.url}`)
           const url = res?.url
           if (url) {
             const urlRewritten = url
