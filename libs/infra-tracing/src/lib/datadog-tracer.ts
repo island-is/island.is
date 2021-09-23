297043f5469a79b5a4ae5378b742f63a286e6c35
Petar Shomov <petar@andes.is>
2021-09-23 18:40:11 +0000

    adding fallback plan for the url construction

--- libs/infra-tracing/src/lib/datadog-tracer.ts
+++ libs/infra-tracing/src/lib/datadog-tracer.ts
@@ -1,6 +1,5 @@
 import tracer from 'dd-trace'
 import { isPerson } from 'kennitala'
-import { logger } from '@island.is/logging'
 
 if (process.env.NODE_ENV !== 'development') {
   tracer.init({ logInjection: true }) // initialized in a different file to avoid hoisting.
@@ -9,14 +8,12 @@ if (process.env.NODE_ENV !== 'development') {
     client: {
       hooks: {
         request: (span, req, res) => {
-          logger.info(`HTTP Tracer: Got span with response url: ${res?.url}`)
-          const url = res?.url
+          const url =
+            res?.url || `http(s)://${req?.getHeader('host')}${req?.path}`
           if (url) {
             const urlRewritten = url
               .split('/')
-              .map((segm) =>
-                isPerson(segm) && segm.length === 10 ? '--MASKED--' : segm,
-              )
+              .map((segm) => (isPerson(segm) ? '--MASKED--' : segm))
               .join('/')
             span?.setTag('http.url', urlRewritten)
           }
