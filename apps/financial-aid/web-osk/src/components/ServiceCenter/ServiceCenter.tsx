53076db57a906d346c7398326fe9487d2797c91e
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-09-27 17:35:53 +0000

    feat(financial-aid): Access control depending on postcodes (#5101)
    * adding data to shared
    
    * adding data to project
    
    * adding another fake user
    
    * moving all usercontent to userprovider
    
    * adding check if postacode is not defined let the user through
    
    * formatting
    
    * fixing typo
    
    * adding Routes
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/financial-aid/web-osk/src/components/ServiceCenter/ServiceCenter.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Text } from '@island.is/island-ui/core'
+
+import {
+  ContentContainer,
+  Footer,
+} from '@island.is/financial-aid-web/osk/src/components'
+
+import { ServiceCenter as IServiceCenter } from '@island.is/financial-aid/shared/data'
+import { useLogOut } from '@island.is/financial-aid-web/osk/src/utils/useLogOut'
+
+interface Props {
+  serviceCenter?: IServiceCenter
+}
+
+const ServiceCenter = ({ serviceCenter }: Props) => {
+  const logOut = useLogOut()
+
+  return (
+    <>
+      <ContentContainer>
+        <Text as="h1" variant="h2" marginBottom={[1, 1, 2]}>
+          Fjárhagsaðstoð Hafnarfjarðar
+        </Text>
+
+        <Text marginBottom={[1, 1, 2]}>
+          Athugaðu að á þessum tímapunkti er þessi umsókn um fjárhagsaðstoð
+          eingöngu ætluð íbúum Hafnarfjarðar.
+        </Text>
+
+        {serviceCenter && (
+          <Text>
+            Athugaðu að {serviceCenter?.name} er ekki orðið hluti af lausninni
+            núna
+          </Text>
+        )}
+      </ContentContainer>
+      <Footer
+        onPrevButtonClick={() => logOut()}
+        previousIsDestructive={true}
+        prevButtonText="Hætta við"
+        hideNextButton={true}
+      />
+    </>
+  )
+}
+
+export default ServiceCenter
