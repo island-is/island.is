57ea47f38a49bbd0a2fd04279a7cd9152b737c27
Ívar Oddsson <ivaroddsson@gmail.com>
2021-09-27 14:48:17 +0000

    Case file modal text to CF

---
+++ apps/judicial-system/web/messages/Core/caseFiles.ts
@@ -0,0 +1,21 @@
+import { defineMessages } from 'react-intl'
+
+export const caseFiles = {
+  modal: {
+    fileNotFound: defineMessages({
+      title: {
+        id: 'judicial.system.core:file_not_found.title',
+        defaultMessage: 'Skjalið er ekki lengur aðgengilegt í Réttarvörslugátt',
+        description:
+          'Notaður sem titill fyrir popup glugga sem segir að skjal sé ekki lengur aðgengilegt í RVG þegar reynt er að opna það',
+      },
+      text: {
+        id: 'judicial.system.core:file_not_found.text',
+        defaultMessage:
+          'Rannsóknargögnum er eytt af skráasvæði Réttarvörslugáttar eftir að þau hafa verið vistuð í varðveisluskyni í Auði. Ef þau eru ekki vistuð í Auði er þeim eytt eftir að kærufresti lýkur.',
+        description:
+          'Notaður sem titill fyrir popup glugga sem segir að skjal sé ekki lengur aðgengilegt í RVG þegar reynt er að opna það',
+      },
+    }),
+  },
+}
