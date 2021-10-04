98110b58fe91c136e8ff30e1ea99118124277be3
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-10-04 14:44:51 +0000

    Masks national id and name of investigation cases in case list

---
+++ apps/judicial-system/backend/src/app/modules/case/interceptors/case.transformer.spec.ts
@@ -0,0 +1,57 @@
+import each from 'jest-each'
+
+import { CaseType } from '@island.is/judicial-system/types'
+
+import { Case } from '../models'
+import { transformCase } from './case.transformer'
+
+describe('Cases Interceptor', () => {
+  each`
+    type
+    ${CaseType.CUSTODY}
+    ${CaseType.TRAVEL_BAN}
+  `.it(
+    'should not mask national id and name in restriction cases',
+    ({ type }) => {
+      const theCase = {
+        type,
+        accusedNationalId: '1234567890',
+        accusedName: 'John Doe',
+      } as Case
+
+      const res = transformCase(theCase)
+
+      expect(res.accusedNationalId).toBe('1234567890')
+      expect(res.accusedName).toBe('John Doe')
+    },
+  )
+
+  each`
+    type
+    ${CaseType.SEARCH_WARRANT}
+    ${CaseType.BANKING_SECRECY_WAIVER}
+    ${CaseType.PHONE_TAPPING}
+    ${CaseType.TELECOMMUNICATIONS}
+    ${CaseType.TRACKING_EQUIPMENT}
+    ${CaseType.PSYCHIATRIC_EXAMINATION}
+    ${CaseType.SOUND_RECORDING_EQUIPMENT}
+    ${CaseType.AUTOPSY}
+    ${CaseType.BODY_SEARCH}
+    ${CaseType.INTERNET_USAGE}
+    ${CaseType.OTHER}
+  `.it(
+    'should mask national id and name in investigation cases',
+    ({ type }) => {
+      const theCase = {
+        type,
+        accusedNationalId: '1234567890',
+        accusedName: 'John Doe',
+      } as Case
+
+      const res = transformCase(theCase)
+
+      expect(res.accusedNationalId).toBe('0000000000')
+      expect(res.accusedName).toBe('X')
+    },
+  )
+})
