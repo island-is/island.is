7cb4160acd9bb9319a004f01c31034492684caa8
Sigrún Tinna Gissurardóttir <39527334+sigruntg@users.noreply.github.com>
2021-10-05 10:30:47 +0000

    fix(pdpp): data provider text update (#5185)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/public-debt-payment-plan/src/lib/messages/externalData.ts
+++ libs/application/templates/public-debt-payment-plan/src/lib/messages/externalData.ts
@@ -12,56 +12,61 @@ export const externalData = {
     },
     subTitle: {
       id: `pdpp.application:application.externalData.subTitle`,
-      defaultMessage: 'Eftirfarandi upplýsingar verða sóttar rafrænt:',
+      defaultMessage: 'Eftirfarandi gögn verða sótt rafrænt',
       description: 'External data section sub title',
     },
     checkboxLabel: {
       id: `pdpp.application:application.externalData.checkboxLabel`,
-      defaultMessage:
-        'Ég skil að ofangreindra upplýsinga verður aflað við úrvinnslu á þessari umsókn.',
+      defaultMessage: 'Ég skil að ofangreindra gagna verður aflað',
       description: 'External data checkbox label',
+    },
+    description: {
+      id: `pdpp.application:application.externalData.description`,
+      defaultMessage:
+        'Tilagangur neðangreindrar upplýsingaöflunar er að gera greiðsluáætlun skv. 12. gr. laga um innheimtu opinberra skatta og gjalda, nr. 150/2019.',
+      description: 'External data description',
     },
   }),
   labels: defineMessages({
     paymentPlanTitle: {
       id: `pdpp.application:section.externalData.paymentPlanTitle`,
-      defaultMessage: 'Upplýsingar frá skattinum',
+      defaultMessage: 'Upplýsingar frá Innheimtustofnun sveitarfélaga',
       description: 'External data section payment plan title',
     },
     paymentPlanSubtitle: {
       id: `pdpp.application:section.externalData.paymentPlanSubtitle`,
-      defaultMessage: 'Upplýsingar um ráðstöfunartekjur. ',
+      defaultMessage: 'Upplýsingar um meðlagsgreiðslur.',
       description: 'External data section payment plan subtitle',
     },
     nationalRegistryTitle: {
       id: `pdpp.application:section.externalData.nationRegistryTitle`,
-      defaultMessage: 'Grunnupplýsingar frá Þjóðskrá Íslands',
+      defaultMessage: 'Upplýsingar frá Þjóðskrá',
       description: 'National Registry Title',
     },
     nationalRegistrySubTitle: {
       id: `pdpp.application:section.externalData.nationalRegistrySubTitle`,
-      defaultMessage: 'Nafn, kennitala og lögheimili.',
+      defaultMessage: 'Nafn, kennitala, símanúmer, netfang.',
       description: 'National Registry Subtitle',
     },
     userProfileTitle: {
       id: `pdpp.application:section.externalData.userProfileTitle`,
-      defaultMessage: 'Upplýsingar úr prófílgrunni á island.is',
+      defaultMessage: 'Upplýsingar frá Skattinum og Fjársýslunni',
       description: 'User Profile Title',
     },
     userProfileSubTitle: {
       id: `pdpp.application:section.externalData.userProfileSubTitle`,
       defaultMessage:
-        'Símanúmer, netfang. Upplýsingar um símanúmer eða netfang er hægt að uppfæra á vefsíðu island.is ef þess þarf.',
+        'Upplýsingar um stöðu krafna, launagreiðanda og ráðstöfunartekjur samkvæmt staðgreiðsluskrá. ',
       description: 'User Profile Subtitle',
     },
     paymentEmployerTitle: {
       id: `pdpp.application:section.externalData.paymentEmployerTitle`,
-      defaultMessage: 'Upplýsingar frá skattinum',
+      defaultMessage: 'Samþykki fyrir tilkynningar',
       description: 'External data section payment plan title',
     },
     paymentEmployerSubtitle: {
       id: `pdpp.application:section.externalData.paymentEmployerSubtitle`,
-      defaultMessage: 'Upplýsingar um vinnuveitanda',
+      defaultMessage: 'Skilaboð um greiðslur o.fl. verða send til þín.',
       description: 'External data section payment plan title',
     },
     paymentDebtsTitle: {
