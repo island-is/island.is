75d40ac39018cc244b943e6d8224eb77aa5d1c75
arnorhs <arnor@kosmosogkaos.is>
2021-10-06 10:32:00 +0000

    Revert "using @island.is/logger for logging the provider error"
    This reverts commit 0adaddf6e951dc8e979dc574e323b3684c8ea439.

--- libs/application/data-providers/src/providers/PaymentCatalogProvider.ts
+++ libs/application/data-providers/src/providers/PaymentCatalogProvider.ts
@@ -1,6 +1,5 @@
 import { PaymentCatalogItem } from '@island.is/api/schema'
 import { BasicDataProvider } from '@island.is/application/core'
-import { logger } from '@island.is/logging'
 
 export abstract class PaymentCatalogProvider extends BasicDataProvider {
   async getCatalogForOrganization(
@@ -25,7 +24,7 @@ export abstract class PaymentCatalogProvider extends BasicDataProvider {
       const response = await res.json()
 
       if (response.errors) {
-        logger.error(response)
+        console.error(response)
         throw new Error('Payment catalog provider response')
       }
 
