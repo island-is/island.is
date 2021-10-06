9dfdcc79e695d926a1b9a34ed0847fe10bceb57c
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2021-10-06 00:50:14 +0000

    Stop on first missing answer in prerequisites

--- libs/application/templates/parental-leave/src/forms/Prerequisites.ts
+++ libs/application/templates/parental-leave/src/forms/Prerequisites.ts
@@ -28,6 +28,7 @@ export const PrerequisitesForm: Form = buildForm({
   title: parentalLeaveFormMessages.shared.formTitle,
   logo: Logo,
   mode: FormModes.APPLYING,
+  stopOnFirstMissingAnswer: true,
   children: [
     buildSection({
       id: 'prerequisites',
