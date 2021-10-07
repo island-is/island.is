82126a213592e47b23a97f45cf860ff129c21ecd
RikkiAbb <rikardarnar@gmail.com>
2021-10-07 12:35:48 +0000

    Changes env name

--- apps/financial-aid/backend/src/environments/environment.ts
+++ apps/financial-aid/backend/src/environments/environment.ts
@@ -18,7 +18,7 @@ const prodConfig = {
     getTimeToLiveMinutes: 5,
   },
   identityServerAuth: {
-    issuer: process.env.IDENTITY_SERVER_DOMAIN ?? '',
+    issuer: process.env.IDENTITY_SERVER_ISSUER_URL ?? '',
     audience: '@samband.is',
   },
   emailOptions: {
