e69210cf4ce187713f88add513bf4ded96ecf8d0
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-10-08 11:24:39 +0000

    Change text to Breytilegur (e. varies)

--- libs/service-portal/settings/access-control/src/screens/AccessControl/components/AccessCard/AccessCard.tsx
+++ libs/service-portal/settings/access-control/src/screens/AccessControl/components/AccessCard/AccessCard.tsx
@@ -55,9 +55,8 @@ function AccessCard({
             {validTo
               ? format(new Date(validTo), 'dd.MM.yyyy')
               : formatMessage({
-                  id:
-                    'service.portal.settings.accessControl:home-view-undefined',
-                  defaultMessage: 'Óskilgreint',
+                  id: 'service.portal.settings.accessControl:home-view-varies',
+                  defaultMessage: 'Breytilegur',
                 })}
           </Text>
         </Inline>
