f82c244f9658d0fcfce8ef45e6950155615b852c
Andri Andresson <andriandresson@gmail.com>
2021-10-08 10:27:20 +0000

    feat: updated options

--- libs/application/templates/data-protection-complaint/src/fields/ComplaineeRepeater/ComplaineeRepeaterItem.tsx
+++ libs/application/templates/data-protection-complaint/src/fields/ComplaineeRepeater/ComplaineeRepeaterItem.tsx
@@ -107,7 +107,6 @@ export const ComplaineeRepeaterItem: FC<Props> = ({
             formatMessage,
           )}
           error={errors && getErrorViaPath(errors, nationalIdField)}
-          required
           backgroundColor="blue"
           defaultValue=""
         />
@@ -122,7 +121,7 @@ export const ComplaineeRepeaterItem: FC<Props> = ({
         largeButtons
         options={[
           { value: YES, label: formatMessage(sharedFields.yes) },
-          { value: NO, label: formatMessage(sharedFields.no) },
+          { value: NO, label: formatMessage(sharedFields.noIdontKnow) },
         ]}
         split="1/2"
         onSelect={handleOnSelect}
