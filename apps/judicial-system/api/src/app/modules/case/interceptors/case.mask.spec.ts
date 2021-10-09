47fdfa2ed529c6e6642fc9ba18394e1b6f694131
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-10-09 13:07:55 +0000

    feat(judicial-system): Court Flow Masking (#5219)
    * Masks uncompleted investigation cases for non-assigned judges and registrars
    
    * Hides parts of case for masked cases in the court flow
    
    * Show info panel instead of continue button in investigation cases is case is masked and current user doesn't have access to continue in the case
    
    Co-authored-by: Ívar Oddsson <ivaroddsson@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/api/src/app/modules/case/interceptors/case.mask.spec.ts
+++ apps/judicial-system/api/src/app/modules/case/interceptors/case.mask.spec.ts
@@ -11,6 +11,7 @@ import {
   SessionArrangements,
   UserRole,
 } from '@island.is/judicial-system/types'
+import type { User } from '@island.is/judicial-system/types'
 
 import { Case } from '../models'
 import { maskCase, maskCaseByUser } from './case.mask'
@@ -22,7 +23,7 @@ function createCase(type: CaseType): Case {
     modified: '-',
     type,
     description: '-',
-    state: CaseState.DISMISSED,
+    state: CaseState.SUBMITTED,
     policeCaseNumber: '-',
     accusedNationalId: '-',
     accusedName: '-',
@@ -155,6 +156,46 @@ function createCase(type: CaseType): Case {
   }
 }
 
+function maskedCase(theCase: Case) {
+  return {
+    id: theCase.id,
+    created: theCase.created,
+    modified: theCase.modified,
+    type: theCase.type,
+    state: theCase.state,
+    policeCaseNumber: theCase.policeCaseNumber,
+    accusedNationalId: '0000000000',
+    accusedName: 'X',
+    defenderName: theCase.defenderName,
+    defenderEmail: theCase.defenderEmail,
+    defenderPhoneNumber: theCase.defenderPhoneNumber,
+    defenderIsSpokesperson: theCase.defenderIsSpokesperson,
+    court: theCase.court,
+    requestedCourtDate: theCase.requestedCourtDate,
+    courtCaseNumber: theCase.courtCaseNumber,
+    sessionArrangements: theCase.sessionArrangements,
+    courtDate: theCase.courtDate,
+    courtRoom: theCase.courtRoom,
+    courtEndTime: theCase.courtEndTime,
+    decision: theCase.decision,
+    validToDate: theCase.validToDate,
+    isValidToDateInThePast: theCase.isValidToDateInThePast,
+    prosecutor: theCase.prosecutor,
+    rulingDate: theCase.rulingDate,
+    accusedAppealDecision: theCase.accusedAppealDecision,
+    prosecutorAppealDecision: theCase.prosecutorAppealDecision,
+    accusedPostponedAppealDate: theCase.accusedPostponedAppealDate,
+    prosecutorPostponedAppealDate: theCase.prosecutorPostponedAppealDate,
+    judge: theCase.judge,
+    registrar: theCase.registrar,
+    parentCase: theCase.parentCase && {
+      id: theCase.parentCase.id,
+      decision: theCase.parentCase.decision,
+    },
+    isMasked: true,
+  }
+}
+
 describe('Mask Case', () => {
   each`
     type
@@ -186,35 +227,7 @@ describe('Mask Case', () => {
 
     const res = maskCase(theCase)
 
-    expect(res).toStrictEqual({
-      id: theCase.id,
-      created: theCase.created,
-      modified: theCase.modified,
-      type: theCase.type,
-      state: theCase.state,
-      policeCaseNumber: theCase.policeCaseNumber,
-      accusedNationalId: '0000000000',
-      accusedName: 'X',
-      defenderName: theCase.defenderName,
-      defenderEmail: theCase.defenderEmail,
-      defenderPhoneNumber: theCase.defenderPhoneNumber,
-      defenderIsSpokesperson: theCase.defenderIsSpokesperson,
-      court: theCase.court,
-      courtCaseNumber: theCase.courtCaseNumber,
-      courtEndTime: theCase.courtEndTime,
-      decision: theCase.decision,
-      validToDate: theCase.validToDate,
-      isValidToDateInThePast: theCase.isValidToDateInThePast,
-      prosecutor: theCase.prosecutor,
-      rulingDate: theCase.rulingDate,
-      accusedAppealDecision: theCase.accusedAppealDecision,
-      prosecutorAppealDecision: theCase.prosecutorAppealDecision,
-      accusedPostponedAppealDate: theCase.accusedPostponedAppealDate,
-      prosecutorPostponedAppealDate: theCase.prosecutorPostponedAppealDate,
-      judge: theCase.judge,
-      parentCase: theCase.parentCase && { id: theCase.parentCase.id },
-      isMasked: true,
-    })
+    expect(res).toStrictEqual(maskedCase(theCase))
   })
 })
 
@@ -226,9 +239,9 @@ describe('Mask Case by User', () => {
   `.describe('given a $type case', ({ type }) => {
     each`
       user
-      ${{ role: UserRole.PROSECUTOR }}
-      ${{ role: UserRole.JUDGE }}
-      ${{ role: UserRole.REGISTRAR }}
+      ${{ id: '+', role: UserRole.PROSECUTOR }}
+      ${{ id: '+', role: UserRole.JUDGE }}
+      ${{ id: '+', role: UserRole.REGISTRAR }}
     `.it('should not mask the case for $user', ({ user }) => {
       const theCase = createCase(type)
 
@@ -254,7 +267,7 @@ describe('Mask Case by User', () => {
   `.describe('given a $type case', ({ type }) => {
     each`
       user
-      ${{ role: UserRole.PROSECUTOR }}
+      ${{ id: '+', role: UserRole.PROSECUTOR }}
       ${{ id: '-', role: UserRole.JUDGE }}
       ${{ id: '-', role: UserRole.REGISTRAR }}
     `.it('should not mask the case for $user', ({ user }) => {
@@ -267,42 +280,32 @@ describe('Mask Case by User', () => {
 
     each`
       user
-      ${{ role: UserRole.JUDGE }}
-      ${{ role: UserRole.REGISTRAR }}
+      ${{ id: '+', role: UserRole.JUDGE }}
+      ${{ id: '+', role: UserRole.REGISTRAR }}
     `.it('should mask the case for $user', ({ user }) => {
       const theCase = createCase(type)
 
       const res = maskCaseByUser(theCase, user)
 
-      expect(res).toStrictEqual({
-        id: theCase.id,
-        created: theCase.created,
-        modified: theCase.modified,
-        type: theCase.type,
-        state: theCase.state,
-        policeCaseNumber: theCase.policeCaseNumber,
-        accusedNationalId: '0000000000',
-        accusedName: 'X',
-        defenderName: theCase.defenderName,
-        defenderEmail: theCase.defenderEmail,
-        defenderPhoneNumber: theCase.defenderPhoneNumber,
-        defenderIsSpokesperson: theCase.defenderIsSpokesperson,
-        court: theCase.court,
-        courtCaseNumber: theCase.courtCaseNumber,
-        courtEndTime: theCase.courtEndTime,
-        decision: theCase.decision,
-        validToDate: theCase.validToDate,
-        isValidToDateInThePast: theCase.isValidToDateInThePast,
-        prosecutor: theCase.prosecutor,
-        rulingDate: theCase.rulingDate,
-        accusedAppealDecision: theCase.accusedAppealDecision,
-        prosecutorAppealDecision: theCase.prosecutorAppealDecision,
-        accusedPostponedAppealDate: theCase.accusedPostponedAppealDate,
-        prosecutorPostponedAppealDate: theCase.prosecutorPostponedAppealDate,
-        judge: theCase.judge,
-        parentCase: theCase.parentCase && { id: theCase.parentCase.id },
-        isMasked: true,
-      })
+      expect(res).toStrictEqual(maskedCase(theCase))
+    })
+
+    it('should mask cases without a judge for judges', () => {
+      const theCase = { ...createCase(type), judge: undefined }
+      const judge = { id: '+', role: UserRole.JUDGE } as User
+
+      const res = maskCaseByUser(theCase, judge)
+
+      expect(res).toStrictEqual(maskedCase(theCase))
+    })
+
+    it('should mask cases without a registrar for registrars', () => {
+      const theCase = { ...createCase(type), registrar: undefined }
+      const registrar = { id: '+', role: UserRole.REGISTRAR } as User
+
+      const res = maskCaseByUser(theCase, registrar)
+
+      expect(res).toStrictEqual(maskedCase(theCase))
     })
   })
 })
