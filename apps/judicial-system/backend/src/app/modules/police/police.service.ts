7b3a23483e1b2dec8a8c0f114d5eabb7f10dc8b2
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-10-13 09:52:42 +0000

    feat(judicial-system): Get police case files (#5252)
    * Adds a backend endpoint to get police case files
    
    * Adds police xroad settings to helm
    
    * Adds an api endpoint to get police case files
    
    * Forbids the getting of police case files for completed cases
    
    * Adds feature hiding for police case files
    
    * Update apps/judicial-system/backend/src/app/modules/police/police.controller.ts
    
    * Adds missing import
    
    * Fixes type export
    
    * Fixes type import
    
    Co-authored-by: Ívar Oddsson <ivaroddsson@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/judicial-system/backend/src/app/modules/police/police.service.ts
@@ -0,0 +1,78 @@
+import { Agent } from 'https'
+import fetch from 'isomorphic-fetch'
+
+import {
+  BadGatewayException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+
+import { LOGGER_PROVIDER } from '@island.is/logging'
+import type { Logger } from '@island.is/logging'
+import {
+  createXRoadAPIPath,
+  XRoadMemberClass,
+} from '@island.is/shared/utils/server'
+import type { PoliceCaseFile } from '@island.is/judicial-system/types'
+
+import { environment } from '../../../environments'
+
+@Injectable()
+export class PoliceService {
+  private xRoadPath = createXRoadAPIPath(
+    environment.xRoad.basePathWithEnv,
+    XRoadMemberClass.GovernmentInstitution,
+    environment.policeServiceOptions.memberCode,
+    environment.policeServiceOptions.apiPath,
+  )
+
+  private agent = new Agent({
+    cert: environment.xRoad.clientCert,
+    key: environment.xRoad.clientKey,
+    ca: environment.xRoad.clientCa,
+    rejectUnauthorized: false,
+  })
+
+  constructor(
+    @Inject(LOGGER_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
+
+  async getAllPoliceCaseFiles(caseId: string): Promise<PoliceCaseFile[]> {
+    let res: Response
+    try {
+      res = await fetch(
+        `${this.xRoadPath}/api/Rettarvarsla/GetDocumentListById/${caseId}`,
+        {
+          headers: { 'X-Road-Client': environment.xRoad.clientId },
+          agent: this.agent,
+        } as RequestInit,
+      )
+    } catch (error) {
+      this.logger.error(
+        `Failed to get police case files for case ${caseId}`,
+        error,
+      )
+
+      throw new BadGatewayException(
+        `Failed to get police case files for case ${caseId}`,
+      )
+    }
+
+    if (!res.ok) {
+      throw new NotFoundException(
+        `No police case files found for case ${caseId}`,
+      )
+    }
+
+    const files = await res.json()
+
+    return files.map(
+      (file: { rvMalSkjolMals_ID: string; heitiSkjals: string }) => ({
+        id: file.rvMalSkjolMals_ID,
+        name: file.heitiSkjals,
+      }),
+    )
+  }
+}
