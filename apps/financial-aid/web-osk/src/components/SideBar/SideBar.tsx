c9954774330a4f8d528e5c866609efd2888df740
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-10-14 13:18:05 +0000

    feat(financial-aid): Create my application endpoint and statuspage upgrade (#5250)
    * adding new endpoint that returns application and some events and created application provider
    
    * adding comment
    
    * self review
    
    * refacoring apporved and recjtion
    
    * adding in progress
    
    * chaning margin
    
    * removing endpoint and checking on user
    
    * isstatevisible adding and munc and application provider united
    
    * chaings provider to hooks
    
    * removing user from session
    
    * fixing comments from Rikka
    
    * adding hooks folder
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/financial-aid/web-osk/src/components/SideBar/SideBar.tsx
+++ apps/financial-aid/web-osk/src/components/SideBar/SideBar.tsx
@@ -3,16 +3,15 @@ import { Box, Text } from '@island.is/island-ui/core'
 
 import { Stepper } from '@island.is/financial-aid-web/osk/src/components'
 
-import { UserContext } from '@island.is/financial-aid-web/osk/src/components/UserProvider/UserProvider'
-
 import { useRouter } from 'next/router'
 
 import * as styles from './SideBar.treat'
+import { AppContext } from '@island.is/financial-aid-web/osk/src/components/AppProvider/AppProvider'
 
 const SideBar = () => {
   const router = useRouter()
 
-  const { user } = useContext(UserContext)
+  const { user } = useContext(AppContext)
 
   return (
     <>
