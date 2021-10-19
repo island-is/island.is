dbdcfe3e59ca1621c8f4d560a121ff5fa5187824
RikkiAbb <rikardarnar@gmail.com>
2021-10-19 10:59:29 +0000

    Adds migration

---
+++ apps/financial-aid/backend/migrations/20211019104356-staff-add-column-municipality_homepage.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      Promise.all([
+        queryInterface.addColumn(
+          'staff',
+          'municipality_homepage',
+          {
+            type: Sequelize.TEXT,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+      ]),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      Promise.all([
+        queryInterface.removeColumn('staff', 'municipality_homepage', {
+          transaction: t,
+        }),
+      ]),
+    )
+  }
+};
---
+++ apps/financial-aid/backend/migrations/20211019104412-municipality-add-column-email.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      Promise.all([
+        queryInterface.addColumn(
+          'municipality',
+          'email',
+          {
+            type: Sequelize.TEXT,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+      ]),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      Promise.all([
+        queryInterface.removeColumn('municipality', 'email', {
+          transaction: t,
+        }),
+      ]),
+    )
+  }
+};
