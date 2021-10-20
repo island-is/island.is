a2bfb304cb514ad0b427bd713cb4a3eaaf626a25
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2021-10-20 15:43:44 +0000

    Fixing yarn format.

--- libs/api/domains/driving-license/src/lib/drivingLicense.service.ts
+++ libs/api/domains/driving-license/src/lib/drivingLicense.service.ts
@@ -99,10 +99,11 @@ export class DrivingLicenseService {
     type: DrivingLicenseApplicationType,
   ): Promise<ApplicationEligibility> {
     const assessmentResult = await this.getDrivingAssessmentResult(nationalId)
-    const hasFinishedSchool =
-      await this.drivingLicenseApi.getHasFinishedOkugerdi({
+    const hasFinishedSchool = await this.drivingLicenseApi.getHasFinishedOkugerdi(
+      {
         nationalId,
-      })
+      },
+    )
 
     const canApply = await this.canApplyFor(nationalId, type)
 
@@ -181,15 +182,16 @@ export class DrivingLicenseService {
     nationalId: User['nationalId'],
     input: NewTemporaryDrivingLicenseInput,
   ): Promise<NewDrivingLicenseResult> {
-    const success =
-      await this.drivingLicenseApi.postCreateDrivingLicenseTemporary({
+    const success = await this.drivingLicenseApi.postCreateDrivingLicenseTemporary(
+      {
         willBringHealthCertificate: input.needsToPresentHealthCertificate,
         willBringQualityPhoto: input.needsToPresentQualityPhoto,
         juristictionId: input.juristictionId,
         nationalIdTeacher: input.teacherNationalId,
         nationalIdApplicant: nationalId,
         sendLicenseInMail: false,
-      })
+      },
+    )
 
     return {
       success,
