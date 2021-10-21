459702d6c5cdc961df3ec28364f17791276e045c
RikkiAbb <rikardarnar@gmail.com>
2021-10-21 14:18:17 +0000

    Creates migration

---
+++ apps/financial-aid/backend/migrations/20211021141602-aid-remove-column-with_parents.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      Promise.all([
+        queryInterface.removeColumn(
+          'aid',
+          'with_parents',
+          { transaction: t },
+        ),
+      ]),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      Promise.all([
+        queryInterface.addColumn('aid', 'with_parents', {
+          transaction: t,
+        }),
+      ]),
+    )
+  },
+}
