f4da43895e363de2a811a26a425a29776cad6482
Rafn Árnason <rafnarnason@Rafns-MacBook-Pro.local>
2021-10-22 09:50:36 +0000

    added email attachment setup

--- apps/services/endorsements/api/src/app/modules/endorsementList/endorsementList.service.ts
+++ apps/services/endorsements/api/src/app/modules/endorsementList/endorsementList.service.ts
@@ -206,6 +206,7 @@ export class EndorsementListService {
   }
 
   async sendMail() {
+    
     try {
       return await this.emailService.sendEmail({
         from: {
@@ -223,6 +224,44 @@ export class EndorsementListService {
         Vinsamlegast staðfestu skráninguna með því að smella á hlekkinn hér fyrir neðan:
         <br>Ef hlekkurinn er ekki lengur í gildi biðjum við þig að endurtaka skráninguna á Ísland.is.
         <br><br>Ef þú kannast ekki við að hafa sett inn þetta netfang, vinsamlegast hunsaðu þennan póst.`,
+        attachments: [
+        //   {   // utf-8 string as an attachment
+        //     filename: 'text1.txt',
+        //     content: 'hello world!'
+        // },
+        // {   // binary buffer as an attachment
+        //     filename: 'text2.txt',
+        //     content: new Buffer('hello world!','utf-8')
+        // },
+        // {   // file on disk as an attachment
+        //     filename: 'text3.txt',
+        //     path: '/path/to/file.txt' // stream this file
+        // },
+        // {   // filename and content type is derived from path
+        //     path: '/path/to/file.txt'
+        // },
+        // {   // stream as an attachment
+        //     filename: 'text4.txt',
+        //     content: fs.createReadStream('file.txt')
+        // },
+        // {   // define custom content type for the attachment
+        //     filename: 'text.bin',
+        //     content: 'hello world!',
+        //     contentType: 'text/plain'
+        // },
+        {   // use URL as an attachment
+            filename: 'license.txt',
+            path: 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE'
+        },
+        // {   // encoded string as an attachment
+        //     filename: 'text1.txt',
+        //     content: 'aGVsbG8gd29ybGQh',
+        //     encoding: 'base64'
+        // },
+        // {   // data uri as an attachment
+        //     path: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
+        // }
+    ]
       })
     } catch (exception) {
       this.logger.error(exception)
