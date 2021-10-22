02094e79cfa0f7c5938527ff6f80b51cf15c5fff
Eirikur Nilsson <eirikur@nilsson.is>
2021-10-22 15:16:03 +0000

    Override @nrwl/jest resolver to take back .css.ts modules

--- jest.preset.js
+++ jest.preset.js
@@ -1,11 +1,15 @@
+const path = require('path')
 const nxPreset = require('@nrwl/jest/preset')
+
+const customResolver = path.relative(process.cwd(), path.join(__dirname, 'jest.resolver'))
+
 module.exports = {
   ...nxPreset,
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
   },
-  resolver: '@nrwl/jest/plugins/resolver',
+  resolver: customResolver,
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
 }
