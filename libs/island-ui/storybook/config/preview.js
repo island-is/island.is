fd2f3d9c4156c3944d883fb447f5bba46044fb50
Eirikur Nilsson <eirikur@nilsson.is>
2021-10-24 21:53:34 +0000

    Remove redundant globalStyles import

--- libs/island-ui/storybook/config/preview.js
+++ libs/island-ui/storybook/config/preview.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { IntlProvider } from 'react-intl'
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { globalStyles } from '@island.is/island-ui/core'
 import { FormProvider, useForm } from 'react-hook-form'
 import { ApolloProvider } from '@apollo/client'
-
-globalStyles()
 
 export const parameters = {
   viewMode: 'docs',
