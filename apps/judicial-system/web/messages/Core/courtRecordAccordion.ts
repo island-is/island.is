d5bd920ae587b35114d650263784d0f752f616af
Ívar Oddsson <ivaroddsson@gmail.com>
2021-10-25 23:04:48 +0000

    chore(judicial-system): Layout changes to the confirmation screen (#5389)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/messages/Core/courtRecordAccordion.ts
+++ apps/judicial-system/web/messages/Core/courtRecordAccordion.ts
@@ -59,5 +59,24 @@ export const courtRecordAccordion = {
           'Notaður sem texti í "Réttindi ..." svæði í þingbókarfellilistanum.',
       },
     }),
+    conclusion: defineMessages({
+      disclaimer: {
+        id: 'judicial.system.core:court_record_accordion.conclusion.disclaimer',
+        defaultMessage:
+          'Úrskurðarorðið er lesið í heyranda hljóði fyrir viðstadda.',
+        description:
+          'Notaður sem texti undir "Úrskurðarorð" hlutanum í þingbókarfellilistanum.',
+      },
+    }),
+    appealDecision: defineMessages({
+      disclaimer: {
+        id:
+          'judicial.system.core:court_record_accordion.appeal_decision.disclaimer',
+        defaultMessage:
+          'Dómari leiðbeinir málsaðilum um rétt þeirra til að kæra úrskurð þennan til Landsréttar innan þriggja sólarhringa.',
+        description:
+          'Notaður sem texti í "Ákvörðun um kæru" hlutanum í þingbókarfellilistanum.',
+      },
+    }),
   },
 }
--- apps/judicial-system/web/src/shared-components/AccordionItems/CourtRecordAccordionItem/CourtRecordAccordionItem.tsx
+++ apps/judicial-system/web/src/shared-components/AccordionItems/CourtRecordAccordionItem/CourtRecordAccordionItem.tsx
@@ -5,6 +5,9 @@ import {
   capitalize,
   caseTypes,
   formatAccusedByGender,
+  formatAlternativeTravelBanRestrictions,
+  formatAppeal,
+  formatCustodyRestrictions,
   formatDate,
   NounCases,
   TIME_FORMAT,
@@ -12,7 +15,11 @@ import {
 import {
   AccusedPleaDecision,
   CaseAppealDecision,
+  CaseDecision,
+  CaseType,
+  isInvestigationCase,
   isRestrictionCase,
+  SessionArrangements,
 } from '@island.is/judicial-system/types'
 import type { Case } from '@island.is/judicial-system/types'
 import AccordionListItem from '../../AccordionListItem/AccordionListItem'
@@ -20,13 +27,24 @@ import { closedCourt } from '@island.is/judicial-system-web/messages'
 import { useIntl } from 'react-intl'
 import { courtRecordAccordion as m } from '@island.is/judicial-system-web/messages/Core/courtRecordAccordion'
 import MarkdownWrapper from '../../MarkdownWrapper/MarkdownWrapper'
+import { rcConfirmation } from '@island.is/judicial-system-web/messages'
 
 interface Props {
   workingCase: Case
 }
-
 const CourtRecordAccordionItem: React.FC<Props> = ({ workingCase }: Props) => {
   const { formatMessage } = useIntl()
+
+  const custodyRestrictions = formatCustodyRestrictions(
+    workingCase.accusedGender,
+    workingCase.custodyRestrictions,
+  )
+
+  const alternativeTravelBanRestrictions = formatAlternativeTravelBanRestrictions(
+    workingCase.accusedGender,
+    workingCase.custodyRestrictions,
+    workingCase.otherRestrictions,
+  )
 
   return (
     <AccordionItem
@@ -158,6 +176,119 @@ const CourtRecordAccordionItem: React.FC<Props> = ({ workingCase }: Props) => {
       <AccordionListItem title="Málflutningur" breakSpaces>
         <Text>{workingCase.litigationPresentations}</Text>
       </AccordionListItem>
+      {(isRestrictionCase(workingCase.type) ||
+        workingCase.sessionArrangements !==
+          SessionArrangements.REMOTE_SESSION) && (
+        <Box marginBottom={3}>
+          <Text>{formatMessage(m.sections.conclusion.disclaimer)}</Text>
+        </Box>
+      )}
+      <Box component="section" marginBottom={3}>
+        <AccordionListItem title="Ákvörðun um kæru">
+          <Box marginBottom={2}>
+            <Text>{formatMessage(m.sections.appealDecision.disclaimer)}</Text>
+          </Box>
+          {workingCase.prosecutorAppealDecision !==
+            CaseAppealDecision.NOT_APPLICABLE && (
+            <Box marginBottom={1}>
+              <Text variant="h4">
+                {formatAppeal(workingCase.prosecutorAppealDecision, 'Sækjandi')}
+              </Text>
+            </Box>
+          )}
+          {workingCase.accusedAppealDecision !==
+            CaseAppealDecision.NOT_APPLICABLE && (
+            <Text variant="h4">
+              {formatAppeal(
+                workingCase.accusedAppealDecision,
+                isRestrictionCase(workingCase.type)
+                  ? capitalize(formatAccusedByGender(workingCase.accusedGender))
+                  : 'Varnaraðili',
+                isRestrictionCase(workingCase.type)
+                  ? workingCase.accusedGender
+                  : undefined,
+              )}
+            </Text>
+          )}
+        </AccordionListItem>
+      </Box>
+      {(workingCase.accusedAppealAnnouncement ||
+        workingCase.prosecutorAppealAnnouncement) && (
+        <Box component="section" marginBottom={6}>
+          {workingCase.accusedAppealAnnouncement &&
+            workingCase.accusedAppealDecision === CaseAppealDecision.APPEAL && (
+              <Box marginBottom={2}>
+                <Text variant="eyebrow" color="blue400">
+                  {`Yfirlýsing um kæru ${formatAccusedByGender(
+                    workingCase.accusedGender,
+                    NounCases.GENITIVE,
+                    isInvestigationCase(workingCase.type),
+                  )}`}
+                </Text>
+                <Text>{workingCase.accusedAppealAnnouncement}</Text>
+              </Box>
+            )}
+          {workingCase.prosecutorAppealAnnouncement &&
+            workingCase.prosecutorAppealDecision ===
+              CaseAppealDecision.APPEAL && (
+              <Box marginBottom={2}>
+                <Text variant="eyebrow" color="blue400">
+                  Yfirlýsing um kæru sækjanda
+                </Text>
+                <Text>{workingCase.prosecutorAppealAnnouncement}</Text>
+              </Box>
+            )}
+        </Box>
+      )}
+      {workingCase.type === CaseType.CUSTODY &&
+        workingCase.decision === CaseDecision.ACCEPTING && (
+          <AccordionListItem title="Tilhögun gæsluvarðhalds">
+            {custodyRestrictions && (
+              <Box marginBottom={2}>
+                <Text>{custodyRestrictions}</Text>
+              </Box>
+            )}
+            <Text>
+              {formatMessage(
+                rcConfirmation.sections.custodyRestrictions.disclaimer,
+                {
+                  caseType: 'gæsluvarðhaldsins',
+                },
+              )}
+            </Text>
+          </AccordionListItem>
+        )}
+      {((workingCase.type === CaseType.CUSTODY &&
+        workingCase.decision ===
+          CaseDecision.ACCEPTING_ALTERNATIVE_TRAVEL_BAN) ||
+        (workingCase.type === CaseType.TRAVEL_BAN &&
+          workingCase.decision === CaseDecision.ACCEPTING)) && (
+        <AccordionListItem title="Tilhögun farbanns">
+          {alternativeTravelBanRestrictions && (
+            <Box marginBottom={2}>
+              <Text>
+                {alternativeTravelBanRestrictions
+                  .split('\n')
+                  .map((str, index) => {
+                    return (
+                      <div key={index}>
+                        <Text>{str}</Text>
+                      </div>
+                    )
+                  })}
+              </Text>
+            </Box>
+          )}
+          <Text>
+            {formatMessage(
+              rcConfirmation.sections.custodyRestrictions.disclaimer,
+              {
+                caseType: 'farbannsins',
+              },
+            )}
+          </Text>
+        </AccordionListItem>
+      )}
     </AccordionItem>
   )
 }
