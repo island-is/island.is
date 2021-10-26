28af6b4c4d87fff8ee5aa4dcd037c8bb0159edc6
Alexandra Diljá <alexandrab18@ru.is>
2021-10-26 10:12:19 +0000

    mask cleanup

--- apps/services/endorsements/api/src/app/modules/endorsementList/interceptors/endorsementList.mask.ts
+++ apps/services/endorsements/api/src/app/modules/endorsementList/interceptors/endorsementList.mask.ts
@@ -2,20 +2,6 @@ import { EndorsementList } from '../endorsementList.model'
 
 export function maskEndorsementList(theList: EndorsementList): EndorsementList {
   // Masks owner from EndorsementList
-  return {
-    id: theList.id,
-    counter: theList.counter,
-    title: theList.title,
-    description: theList.description,
-    openedDate: theList.openedDate,
-    closedDate: theList.closedDate,
-    endorsementMetadata: theList.endorsementMetadata,
-    tags: theList.tags,
-    owner: 'xxxxxx-xxxx',
-    validationRules: theList.validationRules,
-    adminLock: theList.adminLock,
-    meta: theList.meta,
-    created: theList.created,
-    modified: theList.modified,
-  } as EndorsementList
+  theList.owner = 'xxxxxx-xxxx'
+  return theList
 }
