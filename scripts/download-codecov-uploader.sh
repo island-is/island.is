fe86e322ecf5c5982228a6f5b304eac78cd85645
Davíð Guðni <davidgudni@gmail.com>
2021-10-27 15:37:56 +0200

    chore(infra): run tests on main to get code coverage

--- scripts/download-codecov-uploader.sh
+++ scripts/download-codecov-uploader.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-BIN="$HOME/.local/bin"
-
+echo "Downloading codecov"
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
 
 curl -O https://uploader.codecov.io/latest/linux/codecov
@@ -11,8 +10,6 @@ curl -O https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
 echo "Running an integrity check"
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
 shasum -a 256 -c codecov.SHA256SUM
+rm codecov.SHA256SUM.sig codecov.SHA256SUM
 
-echo "Installing codecov into $BIN"
 chmod +x codecov
-mkdir -p "$BIN"
-mv codecov "$BIN/codecov"
