f01a4f76d7058c2f42487acb095f14ad2b510475
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-10-27 14:05:25 +0000

    Updates migration

--- apps/judicial-system/backend/migrations/20211026164423-update-case.js
+++ apps/judicial-system/backend/migrations/20211026164423-update-case.js
@@ -3,23 +3,82 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) =>
-      queryInterface.removeColumn('case', 'is_accused_rights_hidden', {
-        transaction: t,
-      }),
+      queryInterface
+        .addColumn(
+          'case',
+          'accused_bookings',
+          { type: Sequelize.TEXT, allowNull: true },
+          { transaction: t },
+        )
+        .then(() =>
+          queryInterface.sequelize.query(
+            `SELECT id, type, session_arrangements, is_accused_rights_hidden, accused_plea_decision, accused_plea_announcement FROM "case" WHERE state in ('ACCEPTED', 'REJECTED', 'DISMISSED')`,
+            { transaction: t },
+          ),
+        )
+        .then((res) =>
+          Promise.all(
+            res[0].map((c) => {
+              let accusedBookings = ''
+              if (c.type === 'CUSTODY' || c.type === 'TRAVEL_BAN') {
+                if (!c.is_accused_rights_hidden) {
+                  accusedBookings +=
+                    'Sakborningi er bent á að honum sé óskylt að svara spurningum er varða brot það sem honum er gefið að sök, sbr. 2. mgr. 113. gr. laga nr. 88/2008. Sakborningur er enn fremur áminntur um sannsögli kjósi hann að tjá sig um sakarefnið, sbr. 1. mgr. 114. gr. sömu laga.\n\nSakborningi er kynnt krafa á dómskjali nr. 1.\n\n'
+                }
+                if (c.accused_plea_decision === 'ACCEPT') {
+                  accusedBookings += 'Sakborningur samþykkir kröfuna. '
+                }
+                if (c.accused_plea_decision === 'REJECT') {
+                  accusedBookings += 'Sakborningur mótmælir kröfunni. '
+                }
+                accusedBookings += c.accused_plea_announcement ?? ''
+                return queryInterface.sequelize.query(
+                  `UPDATE "case" set accused_bookings = '${accusedBookings.replace(
+                    /'/g,
+                    "''",
+                  )}' WHERE id = '${c.id}'`,
+                  {
+                    transaction: t,
+                  },
+                )
+              } else {
+                if (
+                  c.session_arrangements === 'ALL_PRESENT' &&
+                  !c.is_accused_rights_hidden
+                ) {
+                  accusedBookings +=
+                    'Varnaraðila er bent á að honum sé óskylt að svara spurningum er varða brot það sem honum er gefið að sök, sbr. 2. mgr. 113. gr. laga nr. 88/2008. Varnaraðili er enn fremur áminntur um sannsögli kjósi hann að tjá sig um sakarefnið, sbr. 1. mgr. 114. gr. sömu laga.\n\nVarnaraðila er kynnt krafa á dómskjali nr. 1.\n\n'
+                }
+                if (c.accused_plea_decision !== 'NOT_APPLICABLE') {
+                  if (c.accused_plea_decision === 'ACCEPT') {
+                    accusedBookings += 'Varnaraðili samþykkir kröfuna. '
+                  }
+                  if (c.accused_plea_decision === 'REJECT') {
+                    accusedBookings += 'Varnaraðili mótmælir kröfunni. '
+                  }
+                  accusedBookings += c.accused_plea_announcement ?? ''
+                }
+                return queryInterface.sequelize.query(
+                  `UPDATE "case" set accused_bookings = '${accusedBookings.replace(
+                    /'/g,
+                    "''",
+                  )}' WHERE id = '${c.id}'`,
+                  {
+                    transaction: t,
+                  },
+                )
+              }
+            }),
+          ),
+        ),
     )
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) =>
-      queryInterface.addColumn(
-        'case',
-        'is_accused_rights_hidden',
-        {
-          type: Sequelize.BOOLEAN,
-          allowNull: true,
-        },
-        { transaction: t },
-      ),
+      queryInterface.removeColumn('case', 'accused_bookings', {
+        transaction: t,
+      }),
     )
   },
 }
