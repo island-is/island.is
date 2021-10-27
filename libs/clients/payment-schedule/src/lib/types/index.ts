313610dc39a38b318007302c2915e0fefc58ac9a
Toti91 <torarinng@gmail.com>
2021-10-27 14:34:06 +0000

    fixes to API after update

--- libs/clients/payment-schedule/src/lib/types/index.ts
+++ libs/clients/payment-schedule/src/lib/types/index.ts
@@ -1,6 +1,6 @@
 export { ChargeType } from './deptCharge.enum'
 export { ScheduleType } from './schedule.enum'
-export { Conditions } from './conditions.type'
+export { Conditions, ConditionsResponse } from './conditions.type'
 export { DebtSchedules, DebtSchedulesResponse } from './debtSchedules.type'
 export { Employer, WageDeductionResponse } from './employer.type'
 export {
