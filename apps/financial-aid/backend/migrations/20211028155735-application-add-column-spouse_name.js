02aa73cc411d1ab59b9fa104074f912dbf72c276
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-10-28 16:32:57 +0000

    formatting

--- apps/financial-aid/backend/migrations/20211028155735-application-add-column-spouse_name.js
+++ apps/financial-aid/backend/migrations/20211028155735-application-add-column-spouse_name.js
@@ -1,29 +1,29 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) =>
-    Promise.all([
-      queryInterface.addColumn(
-        'applications',
-        'spouse_name',
-        {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-        { transaction: t },
-      ),
-    ]),
-  )
+      Promise.all([
+        queryInterface.addColumn(
+          'applications',
+          'spouse_name',
+          {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+          { transaction: t },
+        ),
+      ]),
+    )
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) =>
-    Promise.all([
-      queryInterface.removeColumn('applications', 'spouse_name', {
-        transaction: t,
-      }),
-    ]),
-  )
-  }
-};
+      Promise.all([
+        queryInterface.removeColumn('applications', 'spouse_name', {
+          transaction: t,
+        }),
+      ]),
+    )
+  },
+}
