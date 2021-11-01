774ca4963beaf0a2175919bccd1c85abbf8c7c6c
Kristján Albert <kal@kosmosogkaos.is>
2021-11-01 09:28:20 +0000

    feat(web/island-ui): language toggler changes (#5438)
    * WIP
    
    * Go directly to english page if slug exists
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/web/components/LanguageToggler/LanguageToggler.tsx
+++ apps/web/components/LanguageToggler/LanguageToggler.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, ReactElement } from 'react'
+import React, { useContext, ReactElement, useState, FC } from 'react'
 import { useRouter } from 'next/router'
 import { useApolloClient } from '@apollo/client/react'
 import {
@@ -6,14 +6,18 @@ import {
   ButtonTypes,
   Hidden,
   DialogPrompt,
+  ButtonProps,
 } from '@island.is/island-ui/core'
 import { useI18n } from '@island.is/web/i18n'
 import { Locale } from '@island.is/shared/types'
 import { GET_CONTENT_SLUG } from '@island.is/web/screens/queries/Article'
 import { GlobalContext } from '@island.is/web/context'
-import { ContentLanguage } from '@island.is/web/graphql/schema'
+import {
+  GetContentSlugQuery,
+  GetContentSlugQueryVariables,
+} from '@island.is/web/graphql/schema'
 import { useNamespace } from '@island.is/web/hooks'
-import { useLinkResolver } from '@island.is/web/hooks/useLinkResolver'
+import { useLinkResolver, LinkType } from '@island.is/web/hooks/useLinkResolver'
 
 type LanguageTogglerProps = {
   dialogId?: string
@@ -29,82 +33,118 @@ export const LanguageToggler = ({
 }: LanguageTogglerProps) => {
   const client = useApolloClient()
   const Router = useRouter()
+  const [showDialog, setShowDialog] = useState<boolean>(false)
   const { contentfulId, globalNamespace } = useContext(GlobalContext)
   const { activeLocale, locale, t } = useI18n()
   const gn = useNamespace(globalNamespace)
   const otherLanguage = (activeLocale === 'en' ? 'is' : 'en') as Locale
   const { linkResolver, typeResolver } = useLinkResolver()
 
-  const onClick = async () => {
-    locale(t.otherLanguageCode)
+  const getOtherLanguagePath = async () => {
+    if (showDialog) {
+      return null
+    }
 
     if (!contentfulId) {
       const { type } = typeResolver(Router.asPath.split('?')[0], true)
       const pagePath = linkResolver(type, [], otherLanguage).href
+
       if (pagePath === '/404') {
-        // if we can't resolve the path go to homepage
-        return Router.push(linkResolver('homepage').href)
+        return setShowDialog(true)
       } else {
-        // go to the resolved path if able
         return Router.push(pagePath)
       }
     } else {
-      return getContentSlug(contentfulId).then((res) => {
+      await getContentSlug(contentfulId).then((res) => {
         const slug = res.data?.getContentSlug?.slug
-        const type = res.data?.getContentSlug?.type
+        const type = res.data?.getContentSlug?.type as LinkType
 
-        if (type && slug) {
-          return Router.push(linkResolver(type, [slug], otherLanguage).href)
+        if (type && slug?.[otherLanguage]) {
+          return goToOtherLanguagePage(
+            linkResolver(type, [slug[otherLanguage]], otherLanguage).href,
+          )
         }
 
-        return Router.push(linkResolver('homepage', [], otherLanguage).href)
+        setShowDialog(true)
       })
     }
   }
 
+  const goToOtherLanguagePage = (path) => {
+    locale(t.otherLanguageCode)
+    Router.push(path)
+  }
+
+  const onClick = async () => {
+    await getOtherLanguagePath()
+  }
+
   const getContentSlug = async (contentfulId: string) => {
-    return client.query({
+    return client.query<GetContentSlugQuery, GetContentSlugQueryVariables>({
       query: GET_CONTENT_SLUG,
       variables: {
         input: {
           id: contentfulId as string,
-          lang: otherLanguage as ContentLanguage,
         },
       },
     })
   }
 
+  const buttonElementProps: ButtonElementProps = {
+    buttonColorScheme,
+    otherLanguage,
+    otherLanguageAria: t.otherLanguageAria,
+    onClick,
+  }
+
   const Disclosure: ReactElement = (
-    <Button
-      colorScheme={buttonColorScheme}
-      variant="utility"
-      onClick={otherLanguage === 'en' ? null : onClick}
-      aria-label={t.otherLanguageAria}
-      lang={otherLanguage === 'en' ? 'en' : 'is'}
-    >
-      {t.otherLanguageName}
-    </Button>
+    <ButtonElement {...buttonElementProps}>{t.otherLanguageName}</ButtonElement>
   )
 
-  const LanguageButton =
-    otherLanguage === 'en' ? (
-      <DialogPrompt
-        baseId={dialogId}
-        title={gn('switchToEnglishModalTitle')}
-        description={gn('switchToEnglishModalText')}
-        ariaLabel="Confirm switching to english"
-        disclosureElement={Disclosure}
-        onConfirm={onClick}
-        buttonTextConfirm="Confirm"
-        buttonTextCancel="Cancel"
-      />
-    ) : (
-      Disclosure
-    )
-
-  return !hideWhenMobile ? (
-    LanguageButton
-  ) : (
-    <Hidden below="md">{LanguageButton}</Hidden>
+  const Dialog = (
+    <DialogPrompt
+      baseId={dialogId}
+      initialVisibility={true}
+      title={gn('switchToEnglishModalTitle')}
+      description={gn('switchToEnglishModalText')}
+      ariaLabel="Confirm switching to english"
+      disclosureElement={Disclosure}
+      onConfirm={() => {
+        goToOtherLanguagePage(linkResolver('homepage', [], otherLanguage).href)
+      }}
+      buttonTextConfirm="Go to home page"
+      buttonTextCancel="Keep viewing"
+    />
   )
+
+  const Content = showDialog ? Dialog : Disclosure
+
+  return !hideWhenMobile ? Content : <Hidden below="md">{Content}</Hidden>
 }
+
+type ButtonElementProps = {
+  buttonColorScheme?: ButtonTypes['colorScheme']
+  otherLanguage: Locale
+  otherLanguageAria: string
+  onClick: () => void
+}
+
+const ButtonElement: FC<ButtonElementProps & ButtonProps> = ({
+  buttonColorScheme = 'default',
+  otherLanguage,
+  otherLanguageAria,
+  onClick,
+  children,
+  ...props
+}) => (
+  <Button
+    colorScheme={buttonColorScheme}
+    variant="utility"
+    onClick={onClick}
+    aria-label={otherLanguageAria}
+    lang={otherLanguage === 'en' ? 'en' : 'is'}
+    {...props}
+  >
+    {children}
+  </Button>
+)
