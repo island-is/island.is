f79f531b440562f70dea3bcf15615b9c0bb97ff6
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-11-02 16:12:19 +0000

    setting up the modal in a way i think it should look

--- apps/financial-aid/web-veita/src/components/ModalTypes/AcceptModal.tsx
+++ apps/financial-aid/web-veita/src/components/ModalTypes/AcceptModal.tsx
@@ -5,7 +5,7 @@ import {
   NumberInput,
 } from '@island.is/financial-aid-web/veita/src/components'
 import cn from 'classnames'
-import { Text } from '@island.is/island-ui/core'
+import { Box, Button, Input, Text } from '@island.is/island-ui/core'
 
 interface Props {
   onCancel: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -20,6 +20,8 @@ const AcceptModal = ({
 }: Props) => {
   const maximumInputLength = 6
   const [amount, setAmount] = useState<number>(0)
+  const [income, setIncome] = useState<number>(0)
+  const [personalTax, setPersonalTax] = useState<number>(0)
   const [hasError, setHasError] = useState(false)
 
   return (
@@ -38,14 +40,86 @@ const AcceptModal = ({
       hasError={hasError}
       errorMessage="Þú þarft að setja inn upphæð"
     >
-      <NumberInput
-        placeholder="Skrifaðu upphæð útborgunar"
-        onUpdate={(input) => {
-          setHasError(false)
-          setAmount(input)
-        }}
-        maximumInputLength={maximumInputLength}
-      />
+      <Box marginBottom={3}>
+        <NumberInput
+          id="aid"
+          label="Grunnupphæð"
+          name="aid"
+          placeholder="Skrifaðu upphæð útborgunar"
+          onUpdate={(input) => {
+            setHasError(false)
+            setAmount(input)
+          }}
+          maximumInputLength={maximumInputLength}
+        />
+      </Box>
+      <Box marginBottom={3}>
+        <NumberInput
+          id="income"
+          label="Tekjur"
+          name="income"
+          placeholder="Skrifaðu upphæð"
+          onUpdate={(input) => {
+            setHasError(false)
+            setIncome(input)
+          }}
+          maximumInputLength={maximumInputLength}
+        />
+      </Box>
+
+      <Box marginBottom={3}>
+        <Button icon="add" variant="text">
+          Bættu við frádráttarlið
+        </Button>
+      </Box>
+
+      <Box marginBottom={3}>
+        <NumberInput
+          id="discount"
+          label="Persónuafsláttur"
+          name="discount"
+          placeholder="Skrifaðu prósentuhlutfall"
+          onUpdate={(input) => {
+            setHasError(false)
+            setPersonalTax(input)
+          }}
+          maximumInputLength={3}
+        />
+      </Box>
+
+      <Box marginBottom={3}>
+        <Button icon="add" variant="text">
+          Bættu við skattkorti
+        </Button>
+      </Box>
+
+      <Box marginBottom={3}>
+        <Input
+          label="Skattur"
+          name="Test1"
+          value={0}
+          placeholder="This is the placeholder"
+        />
+      </Box>
+
+      <Text variant="h3" marginBottom={3}>
+        Útreikningur
+      </Text>
+
+      <Box
+        display="flex"
+        justifyContent="spaceBetween"
+        background="blue100"
+        borderTopWidth="standard"
+        borderBottomWidth="standard"
+        borderColor="blue200"
+        paddingY={2}
+        paddingX={3}
+        marginBottom={4}
+      >
+        <Text variant="small">Upphæð aðstoðar</Text>
+        <Text> 98.142 kr.</Text>
+      </Box>
     </InputModal>
   )
 }
--- apps/financial-aid/web-veita/src/components/NumberInput/NumberInput.tsx
+++ apps/financial-aid/web-veita/src/components/NumberInput/NumberInput.tsx
@@ -1,17 +1,24 @@
+import { Input } from '@island.is/island-ui/core'
 import React, { useContext, useState, useCallback, ChangeEvent } from 'react'
 
 import * as styles from './NumberInput.css'
 
 interface Props {
+  id: string
   maximumInputLength: number
   placeholder: string
   onUpdate(n: number): void
+  label: string
+  name: string
 }
 
 export default function NumberInput({
   maximumInputLength,
   onUpdate,
   placeholder,
+  id,
+  label,
+  name,
 }: Props) {
   const sanitizeNumber = (n: string) => n.replace(/[^\d]/g, '')
   const formatNumber = (n: string) => Number(n).toLocaleString('de-DE')
@@ -19,11 +26,11 @@ export default function NumberInput({
   const [text, setText] = useState<string>('')
 
   const handleChange = useCallback(
-    (ev: ChangeEvent<HTMLInputElement>) => {
+    (ev: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       const value = ev.currentTarget.value
 
       const formattedValue = formatNumber(sanitizeNumber(value))
-
+      console.log(formattedValue)
       if (sanitizeNumber(value).length > maximumInputLength) {
         return
       }
@@ -35,12 +42,15 @@ export default function NumberInput({
   )
 
   return (
-    <input
+    <Input
+      id={id}
+      label={label}
+      name={name}
       value={text}
-      onChange={handleChange}
       type="text"
       placeholder={placeholder}
-      className={`${styles.inputStyle}`}
+      backgroundColor="blue"
+      onChange={handleChange}
     />
   )
 }
