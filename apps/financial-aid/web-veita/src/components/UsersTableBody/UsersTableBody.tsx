7f8ff098a8f93c4ac6c3a4d1fa6949174485b08a
RikkiAbb <rikardarnar@gmail.com>
2021-11-04 10:29:18 +0000

    Orders staff list

--- apps/financial-aid/web-veita/src/components/UsersTableBody/UsersTableBody.tsx
+++ apps/financial-aid/web-veita/src/components/UsersTableBody/UsersTableBody.tsx
@@ -30,7 +30,7 @@ const UsersTableBody = ({ user, index }: PageProps) => {
         })}
       >
         <Box display="flex">
-          <Text variant="h5">
+          <Text variant="h5" color={user.active ? 'dark400' : 'dark300'}>
             {user.name} {isLoggedInUser ? '(Þú)' : ''}
           </Text>
         </Box>
@@ -42,7 +42,9 @@ const UsersTableBody = ({ user, index }: PageProps) => {
         })}
       >
         <Box display="flex">
-          <Text>{formatNationalId(user.nationalId)}</Text>
+          <Text color={user.active ? 'dark400' : 'dark300'}>
+            {formatNationalId(user.nationalId)}
+          </Text>
         </Box>
       </td>
 
@@ -51,7 +53,9 @@ const UsersTableBody = ({ user, index }: PageProps) => {
           [`${styles.tablePadding} `]: true,
         })}
       >
-        <Text> {staffRoleDescription(user.roles)}</Text>
+        <Text color={user.active ? 'dark400' : 'dark300'}>
+          {staffRoleDescription(user.roles)}
+        </Text>
       </td>
 
       {isLoggedInUser === false && (
