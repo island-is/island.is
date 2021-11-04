ba7fbb667a70bbf887ed9b69a169e3f9de3994bc
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2021-11-04 22:01:25 +0000

    feat(accident-notification): An/status of notification (#5478)
    * push for generation
    
    * added client and initial xml parsing
    
    * format
    
    * config changes
    
    * logging and correct headder
    
    * most types gathered and sent
    
    * null errors
    
    * minor fixes error handling
    
    * get status of accident notificatino
    
    * fix funciont call names
    
    * presist external data
    
    * added documentid in overview
    
    * undefined fix
    
    * state refactor
    
    * add documentid to email
    
    * attachment types
    
    * changing env
    
    * fix to envs in api
    
    * WiP: inReview & AssigneeInReview form refactor
    
    * rm consoles adding logger and error handling
    
    * additional files enum
    
    * fisherman info added
    
    * feat: updated inReviewForm
    
    * change health insurance response
    
    * temp commit to pull in main
    
    * add changes to accident status to new location of api
    
    * remove obsolete code
    
    * refactor
    
    * accident notification status reintroduced
    
    * refactor and remove old code
    
    * status of confirmation
    
    * fixed console.log
    
    * typescript error in schemas
    
    * fix enum
    
    Co-authored-by: obmagnusson <obmagnusson@gmail.com>
    Co-authored-by: Andri Andresson <andriandresson@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/accident-notification/src/utils/hasMissingDocuments.ts
+++ libs/application/templates/accident-notification/src/utils/hasMissingDocuments.ts
@@ -1,15 +1,74 @@
-import { FormValue } from '@island.is/application/core'
 import {
-  AttachmentsEnum,
-  FileType,
-  PowerOfAttorneyUploadEnum,
-  WhoIsTheNotificationForEnum,
-} from '..'
+  FormValue,
+  getValueViaPath,
+  MessageFormatter,
+} from '@island.is/application/core'
+import { AttachmentsEnum, FileType, WhoIsTheNotificationForEnum } from '..'
 import { YES } from '../constants'
 import { AccidentNotification } from '../lib/dataSchema'
+import { attachments } from '../lib/messages'
 
 const hasAttachment = (attachment: FileType[] | undefined) =>
   attachment && attachment.length > 0
+
+const includesAttachment = (answers: any, attachmentType: string): boolean => {
+  return answers?.accidentStatus?.receivedAttachments?.[attachmentType]
+}
+
+export const hasReceivedInjuryCertifcate = (answers: FormValue) => {
+  return includesAttachment(answers, 'InjuryCertificate')
+}
+
+export const hasReceivedProxyDocument = (answers: FormValue) => {
+  return includesAttachment(answers, 'ProxyDocument')
+}
+
+export const hasReceivedPoliceReport = (answers: FormValue) => {
+  return includesAttachment(answers, 'PoliceReport')
+}
+
+export const hasReceivedAllDocuments = (answers: FormValue) => {
+  return (
+    hasReceivedPoliceReport(answers) &&
+    hasReceivedProxyDocument(answers) &&
+    hasReceivedInjuryCertifcate(answers)
+  )
+}
+
+export const getErrorMessageForMissingDocuments = (
+  answers: FormValue,
+  formatMessage: MessageFormatter,
+) => {
+  const whoIsTheNotificationFor = getValueViaPath(
+    answers,
+    'whoIsTheNotificationFor.answer',
+  )
+  const wasTheAccidentFatal = answers.wasTheAccidentFatal
+  const missingDocuments = []
+
+  if (!hasReceivedInjuryCertifcate(answers)) {
+    missingDocuments.push(
+      formatMessage(attachments.documentNames.injuryCertificate),
+    )
+  }
+
+  if (
+    whoIsTheNotificationFor === WhoIsTheNotificationForEnum.POWEROFATTORNEY &&
+    !hasReceivedProxyDocument(answers)
+  ) {
+    missingDocuments.push(
+      formatMessage(attachments.documentNames.powerOfAttorneyDocument),
+    )
+  }
+
+  if (wasTheAccidentFatal === YES && !hasReceivedPoliceReport(answers)) {
+    missingDocuments.push(
+      formatMessage(attachments.documentNames.deathCertificate),
+    )
+  }
+
+  return missingDocuments.join(', ')
+}
 
 export const hasMissingInjuryCertificate = (answers: FormValue) => {
   const injuryCertificate = (answers as AccidentNotification).injuryCertificate
