34608a478a5ee46590b6b365865143cb15f78013
Magnus Skulason <maggisk@gmail.com>
2021-11-05 11:38:18 +0000

    update as suggested by review

--- apps/services/notifications/src/main.ts
+++ apps/services/notifications/src/main.ts
@@ -1,21 +1,10 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
+import { bootstrap } from '@island.is/infra-nest-server'
 
 import { AppModule } from './app/app.module'
+import { openApi } from './openApi'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const globalPrefix = 'api'
-  app.setGlobalPrefix(globalPrefix)
-  const port = process.env.PORT || 3333
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix)
-  })
-}
-
-bootstrap()
+bootstrap({
+  appModule: AppModule,
+  name: 'reference-backend',
+  openApi,
+})
