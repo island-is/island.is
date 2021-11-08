6314ac6561b0701e0c0b2b6c5b2d1db79b7c29de
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-11-08 13:45:07 +0000

    undoing unused css code

--- apps/financial-aid/web-osk/src/routes/application/SummaryForm/summaryForm.css.ts
+++ apps/financial-aid/web-osk/src/routes/application/SummaryForm/summaryForm.css.ts
@@ -5,16 +5,3 @@ export const taxReturn = style({
   color: theme.color.red400,
   fontWeight: 'bold',
 })
-
-export const userInfoContainer = style({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(1, 1fr)',
-  alignItems: 'flex-start',
-  columnGap: theme.spacing[3],
-  rowGap: theme.spacing[3],
-  '@media': {
-    [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
-      gridTemplateColumns: 'repeat(7, 1fr)',
-    },
-  },
-})
