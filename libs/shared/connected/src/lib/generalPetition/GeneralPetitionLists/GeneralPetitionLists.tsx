89ea560ba4005eb4495612b08adfa709ba37d3bf
albina <albina@kosmosogkaos.is>
2021-11-08 16:46:44 +0000

    pagination fix, and a few minor changes in web

--- libs/shared/connected/src/lib/generalPetition/GeneralPetitionLists/GeneralPetitionLists.tsx
+++ libs/shared/connected/src/lib/generalPetition/GeneralPetitionLists/GeneralPetitionLists.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
-import { Box, Text, Stack, ActionCard } from '@island.is/island-ui/core'
+import { Box, Text, Stack, ActionCard, Link } from '@island.is/island-ui/core'
 import { useRouter } from 'next/router'
 import { useGetPetitionLists } from './useGetPetitionLists'
+import format from 'date-fns/format'
+
+const formatDate = (date: string) => {
+  try {
+    return format(new Date(date), 'dd.MM.yyyy')
+  } catch {
+    return date
+  }
+}
 
 export const GeneralPetitionLists = () => {
   const petitionLists = useGetPetitionLists()
@@ -15,19 +24,29 @@ export const GeneralPetitionLists = () => {
       <Stack space={4}>
         {petitionLists?.data?.map((petition: any) => {
           return (
-            <ActionCard
-              key={petition.title}
-              backgroundColor="blue"
-              heading={petition.title}
-              text={petition.description}
-              cta={{
-                label: 'Nánar um lista',
-                variant: 'text',
-                icon: 'arrowForward',
-                onClick: () =>
-                  router.push('/undirskriftalistar/' + petition.id),
-              }}
-            />
+            <Link
+              href={''}
+              key={petition.id}
+              onClick={() => router.push('/undirskriftalistar/' + petition.id)}
+            >
+              <ActionCard
+                key={petition.title}
+                backgroundColor="blue"
+                heading={petition.title}
+                text={
+                  'Tímabil lista:' +
+                  ' ' +
+                  formatDate(petition.openedDate) +
+                  ' - ' +
+                  formatDate(petition.closedDate)
+                }
+                cta={{
+                  label: 'Nánar um lista',
+                  variant: 'text',
+                  icon: 'arrowForward',
+                }}
+              />
+            </Link>
           )
         })}
       </Stack>
