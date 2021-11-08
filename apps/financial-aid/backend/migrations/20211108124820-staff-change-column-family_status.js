53ed5c821e218f3057a374a374e864717193f460
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-11-08 13:22:16 +0000

    chaning unknonwn, single og notinformaed to married to add NotCohabitation

--- apps/financial-aid/backend/migrations/20211108124820-staff-change-column-family_status.js
+++ apps/financial-aid/backend/migrations/20211108124820-staff-change-column-family_status.js
@@ -1,51 +1,34 @@
 'use strict'
 
+const replaceEnum = require('sequelize-replace-enum-postgres').default
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) =>
-      Promise.all([
-        queryInterface.changeColumn(
-          'applications',
-          'family_status',
-          {
-            type: Sequelize.ENUM(
-              'NotCohabitation',
-              'Cohabitation',
-              'UnregisteredCohabitation',
-              'Married',
-              'MarriedNotLivingTogether',
-            ),
-            allowNull: false,
-            defaultValue: 'NotCohabitation',
-          },
-          { transaction: t },
-        ),
-      ]),
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction((t) =>
+      queryInterface.sequelize.query(
+        `UPDATE "applications" SET family_status = 'Married' WHERE family_status = 'Unknown' OR family_status = 'Single' OR family_status = 'NotInformed';`,
+        { transaction: t },
+      ),
     )
+    // replaceEnum does not support transactions
+    return replaceEnum({
+      queryInterface,
+      tableName: 'applications',
+      columnName: 'family_status',
+      defaultValue: 'NotCohabitation',
+      newValues: [
+        'NotCohabitation',
+        'Cohabitation',
+        'UnregisteredCohabitation',
+        'Married',
+        'MarriedNotLivingTogether',
+      ],
+      enumName: 'enum_applications_family_status',
+    })
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) =>
-      Promise.all([
-        queryInterface.changeColumn(
-          'applications',
-          'family_status',
-          {
-            type: Sequelize.ENUM(
-              'Unknown',
-              'Single',
-              'Cohabitation',
-              'UnregisteredCohabitation',
-              'Married',
-              'MarriedNotLivingTogether',
-              'NotInformed',
-            ),
-            allowNull: false,
-            defaultValue: 'Unknown',
-          },
-          { transaction: t },
-        ),
-      ]),
-    )
+  down: async (queryInterface, Sequelize) => {
+    // no need to roll back
+    return
   },
 }
