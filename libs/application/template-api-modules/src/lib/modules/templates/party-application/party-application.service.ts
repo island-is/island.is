146825c66cbc069674a0085c7969e775bd013106
arnorhs <arnor@kosmosogkaos.is>
2021-11-09 23:06:43 +0000

    more endorsement related fixes

--- libs/application/template-api-modules/src/lib/modules/templates/party-application/party-application.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/party-application/party-application.service.ts
@@ -32,22 +32,11 @@ const CREATE_ENDORSEMENT_LIST_QUERY = `
 
 export const DEFAULT_CLOSED_DATE = 'default closed date'
 
-interface ErrorResponse {
-  errors: {
-    message: string
+interface EndorsementListData {
+  endorsementSystemCreateEndorsementList: {
+    id: string
   }
 }
-
-type EndorsementListResponse =
-  | {
-      data: {
-        endorsementSystemCreateEndorsementList: {
-          id: string
-        }
-      }
-    }
-  | ErrorResponse
-
 export interface PartyApplicationServiceOptions {
   adminEmails: GenerateAssignSupremeCourtApplicationEmailOptions
 }
@@ -134,62 +123,67 @@ export class PartyApplicationService {
   }: TemplateApiModuleActionProps) {
     const partyLetter = application.externalData.partyLetterRegistry
       ?.data as PartyLetterData
-    const endorsementList: EndorsementListResponse = await this.sharedTemplateAPIService
-      .makeGraphqlQuery(auth.authorization, CREATE_ENDORSEMENT_LIST_QUERY, {
-        input: {
-          title: partyLetter.partyName,
-          description: partyLetter.partyLetter,
-          endorsementMetadata: [
-            { field: EndorsementMetadataDtoFieldEnum.fullName },
-            { field: EndorsementMetadataDtoFieldEnum.signedTags },
-            {
-              field: EndorsementMetadataDtoFieldEnum.address,
-              keepUpToDate: true,
-            },
-            {
-              field: EndorsementMetadataDtoFieldEnum.voterRegion,
-              keepUpToDate: true,
-            },
-          ],
-          tags: [application.answers.constituency as EndorsementListTagsEnum],
-          validationRules: [
-            {
-              type: 'minAgeAtDate',
-              value: {
-                date: '2021-09-25T00:00:00Z',
-                age: 18,
+    const endorsementListResponse = await this.sharedTemplateAPIService
+      .makeGraphqlQuery<EndorsementListData>(
+        auth.authorization,
+        CREATE_ENDORSEMENT_LIST_QUERY,
+        {
+          input: {
+            title: partyLetter.partyName,
+            description: partyLetter.partyLetter,
+            endorsementMetadata: [
+              { field: EndorsementMetadataDtoFieldEnum.fullName },
+              { field: EndorsementMetadataDtoFieldEnum.signedTags },
+              {
+                field: EndorsementMetadataDtoFieldEnum.address,
+                keepUpToDate: true,
               },
-            },
-            {
-              type: 'uniqueWithinTags',
-              value: {
-                tags: [
-                  EndorsementListTagsEnum.partyApplicationNordausturkjordaemi2021,
-                  EndorsementListTagsEnum.partyApplicationNordvesturkjordaemi2021,
-                  EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiNordur2021,
-                  EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiSudur2021,
-                  EndorsementListTagsEnum.partyApplicationSudurkjordaemi2021,
-                  EndorsementListTagsEnum.partyApplicationSudvesturkjordaemi2021,
-                ],
+              {
+                field: EndorsementMetadataDtoFieldEnum.voterRegion,
+                keepUpToDate: true,
               },
+            ],
+            tags: [application.answers.constituency as EndorsementListTagsEnum],
+            validationRules: [
+              {
+                type: 'minAgeAtDate',
+                value: {
+                  date: '2021-09-25T00:00:00Z',
+                  age: 18,
+                },
+              },
+              {
+                type: 'uniqueWithinTags',
+                value: {
+                  tags: [
+                    EndorsementListTagsEnum.partyApplicationNordausturkjordaemi2021,
+                    EndorsementListTagsEnum.partyApplicationNordvesturkjordaemi2021,
+                    EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiNordur2021,
+                    EndorsementListTagsEnum.partyApplicationReykjavikurkjordaemiSudur2021,
+                    EndorsementListTagsEnum.partyApplicationSudurkjordaemi2021,
+                    EndorsementListTagsEnum.partyApplicationSudvesturkjordaemi2021,
+                  ],
+                },
+              },
+            ],
+            meta: {
+              // to be able to link back to this application
+              applicationTypeId: application.typeId,
+              applicationId: application.id,
             },
-          ],
-          meta: {
-            // to be able to link back to this application
-            applicationTypeId: application.typeId,
-            applicationId: application.id,
           },
         },
-      })
+      )
       .then((response) => response.json())
 
-    if ('errors' in endorsementList) {
+    if ('errors' in endorsementListResponse) {
       throw new Error('Failed to create endorsement list')
     }
 
     // This gets written to externalData under the key createEndorsementList
     return {
-      id: endorsementList.data.endorsementSystemCreateEndorsementList.id,
+      id:
+        endorsementListResponse.data?.endorsementSystemCreateEndorsementList.id,
     }
   }
 }
