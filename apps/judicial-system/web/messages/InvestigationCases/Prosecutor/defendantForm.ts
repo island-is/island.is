850cda2ede2a555ece4d0242f870c3ab446375dc
Ívar Oddsson <oddsson@Ivars-iMac.local>
2021-11-15 11:22:02 +0000

    Add defender info to investigation cases

--- apps/judicial-system/web/messages/InvestigationCases/Prosecutor/defendantForm.ts
+++ apps/judicial-system/web/messages/InvestigationCases/Prosecutor/defendantForm.ts
@@ -74,5 +74,80 @@ export const defendant = {
           'Notaður sem titill fyrir "upplýsingar um varnaraðila" hlutann á varnaraðila skrefi í rannsóknarheimildum.',
       }),
     },
+    defenderInfo: {
+      heading: defineMessage({
+        id:
+          'judicial.system.investigation_cases:defendant.defender_info.heading',
+        defaultMessage: 'Verjandi varnaraðila',
+        description:
+          'Notaður sem titill fyrir "upplýsingar um verjanda varnaraðila" hlutann á varnaraðila skrefi í rannsóknarheimildum.',
+      }),
+      name: defineMessages({
+        label: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.name.label',
+          defaultMessage: 'Nafn verjanda',
+          description:
+            'Notaður sem titill í "nafn verjanda" textaboxi á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+        placeholder: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.name.placeholder',
+          defaultMessage: 'Fullt nafn',
+          description:
+            'Notaður sem skýritexti í "nafn verjanda" textaboxi á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+      }),
+      email: defineMessages({
+        label: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.email.label',
+          defaultMessage: 'Netfang verjanda',
+          description:
+            'Notaður sem titill í "netfang verjanda" textaboxi á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+        placeholder: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.email.placeholder',
+          defaultMessage: 'Netfang',
+          description:
+            'Notaður sem skýritexti í "nafn verjanda" textaboxi á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+      }),
+      phoneNumber: defineMessages({
+        label: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.phoneNumber.label',
+          defaultMessage: 'Símanúmer verjanda',
+          description:
+            'Notaður sem titill í "símanúmer verjanda" textaboxi á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+        placeholder: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.phone_number.placeholder',
+          defaultMessage: 'Símanúmer',
+          description:
+            'Notaður sem skýritexti í "símanúmer verjanda" textaboxi á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+      }),
+      sendRequest: defineMessages({
+        label: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.send_request.label',
+          defaultMessage:
+            'Senda kröfu sjálfvirkt í tölvupósti til verjanda við úthlutun fyrirtökutíma',
+          description:
+            'Notaður sem texti í "senda kröfu sjálfvirkt..." gátreit á varnaraðila skrefi í rannsóknarheimildum.',
+        },
+        tooltip: {
+          id:
+            'judicial.system.investigation_cases:defendant.defender_info.send_request.tooltip',
+          defaultMessage:
+            'Ef hakað er hér þá fær verjandi kröfunnar senda þegar fyrirtökutíma hefur verið úthlutað',
+          description:
+            'Notaður sem upplýsingatexti í upplýsingasvæði við "senda kröfu sjálfvirkt..." gátreit á sakbornings skrefi í rannsóknarheimildum.',
+        },
+      }),
+    },
   },
 }
