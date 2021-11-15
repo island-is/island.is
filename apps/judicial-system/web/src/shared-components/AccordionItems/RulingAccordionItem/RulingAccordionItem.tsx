105ab2583d6cd63e751e1635d9722c85a38f2fb4
Ívar Oddsson <oddsson@Ivars-iMac.local>
2021-11-15 15:33:05 +0000

    Revert

--- apps/judicial-system/web/src/shared-components/AccordionItems/RulingAccordionItem/RulingAccordionItem.tsx
+++ apps/judicial-system/web/src/shared-components/AccordionItems/RulingAccordionItem/RulingAccordionItem.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import { useIntl } from 'react-intl'
 import { Text, Box, AccordionItem } from '@island.is/island-ui/core'
-import {
-  Case,
-  isInvestigationCase,
-  isRestrictionCase,
-  SessionArrangements,
-} from '@island.is/judicial-system/types'
+import type { Case } from '@island.is/judicial-system/types'
 import { rulingAccordion as m } from '@island.is/judicial-system-web/messages/Core/rulingAccordion'
 import * as style from './RulingAccordionItem.css'
-import {
-  capitalize,
-  formatAccusedByGender,
-  formatAppeal,
-  NounCases,
-} from '@island.is/judicial-system/formatters'
 
 interface Props {
   workingCase: Case
@@ -26,22 +15,6 @@ const RulingAccordionItem: React.FC<Props> = ({
   startExpanded,
 }: Props) => {
   const { formatMessage } = useIntl()
-  const prosecutorAppeal = formatAppeal(
-    workingCase.prosecutorAppealDecision,
-    'Sækjandi',
-  )
-
-  const accusedAppeal = formatAppeal(
-    workingCase.accusedAppealDecision,
-    capitalize(
-      formatAccusedByGender(
-        workingCase.accusedGender,
-        NounCases.NOMINATIVE,
-        isInvestigationCase(workingCase.type),
-      ),
-    ),
-    workingCase.accusedGender,
-  )
 
   return (
     <AccordionItem
@@ -72,42 +45,12 @@ const RulingAccordionItem: React.FC<Props> = ({
         <Box marginBottom={2}>
           <Text>{workingCase.courtLegalArguments}</Text>
         </Box>
-        <Box marginBottom={1}>
-          <Text variant="eyebrow" color="blue400">
-            {formatMessage(m.sections.ruling.title)}
-          </Text>
-        </Box>
-        <Box marginBottom={2}>
-          <Text>
-            <span className={style.breakSpaces}>{workingCase.ruling}</span>
-          </Text>
-        </Box>
-        <Box marginBottom={1}>
-          <Text variant="eyebrow" color="blue400">
-            {formatMessage(m.sections.appealDecisions.title)}
-          </Text>
-        </Box>
-        <Box marginBottom={2}>
-          {(isRestrictionCase(workingCase.type) ||
-            workingCase.sessionArrangements ===
-              SessionArrangements.ALL_PRESENT) && (
-            <Text>
-              {formatMessage(m.sections.appealDecisions.appealDirections)}
-            </Text>
-          )}
-        </Box>
-        <Box marginBottom={2}>
-          <Text>{`${prosecutorAppeal}${
-            workingCase.prosecutorAppealAnnouncement
-              ? ` ${workingCase.prosecutorAppealAnnouncement}`
-              : ''
-          }`}</Text>
-        </Box>
-        <Text>{`${accusedAppeal}${
-          workingCase.accusedAppealAnnouncement
-            ? ` ${workingCase.accusedAppealAnnouncement}`
-            : ''
-        }`}</Text>
+        <Text variant="eyebrow" color="blue400">
+          {formatMessage(m.sections.ruling.title)}
+        </Text>
+        <Text>
+          <span className={style.breakSpaces}>{workingCase.ruling}</span>
+        </Text>
       </Box>
     </AccordionItem>
   )
