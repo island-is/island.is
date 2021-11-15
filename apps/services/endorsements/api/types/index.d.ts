9bbb84a99cbae9a44df05f2a27272f104cb01c20
albina <albina@kosmosogkaos.is>
2021-11-15 11:20:25 +0000

    cleanup

--- apps/services/endorsements/api/types/index.d.ts
+++ apps/services/endorsements/api/types/index.d.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express'
 import type { EndorsementList } from '../src/app/modules/endorsementList/endorsementList.model'
 
+export {}
 declare global {
   namespace jest {
     interface Expect {
