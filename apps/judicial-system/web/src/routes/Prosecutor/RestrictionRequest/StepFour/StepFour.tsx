f5ec37fc65f8d30492499d512b045c6ae6e2f836
Ívar Oddsson <ivaroddsson@gmail.com>
2021-11-15 14:26:15 +0000

    feat(judicial-system): Allow court users to partially accept custody requests (#5604)
    * Allow court users to partially accept custody request
    
    * Use isAcceptingCaseDecision function
    
    * Ruling autofill fixes
    
    Co-authored-by: Ívar Oddsson <oddsson@Ivars-iMac.local>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/routes/Prosecutor/RestrictionRequest/StepFour/StepFour.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/RestrictionRequest/StepFour/StepFour.tsx
@@ -5,6 +5,7 @@ import {
   CaseCustodyRestrictions,
   CaseDecision,
   CaseType,
+  isAcceptingCaseDecision,
 } from '@island.is/judicial-system/types'
 import type { Case } from '@island.is/judicial-system/types'
 import { isPoliceReportStepValidRC } from '@island.is/judicial-system-web/src/utils/validate'
@@ -67,7 +68,7 @@ export const StepFour: React.FC = () => {
           accusedNationalId: formatNationalId(theCase.accusedNationalId),
           extensionSuffix:
             theCase.parentCase !== undefined &&
-            theCase.parentCase?.decision === CaseDecision.ACCEPTING
+            isAcceptingCaseDecision(theCase.parentCase.decision)
               ? ' áframhaldandi'
               : '',
           caseType:
