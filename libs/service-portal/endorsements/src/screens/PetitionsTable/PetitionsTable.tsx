dc1478f048d3d2166c71bcf8a42cb3528cd937c9
albina <albina@kosmosogkaos.is>
2021-11-17 10:01:41 +0000

    a few tweaks for csv dl

--- libs/service-portal/endorsements/src/screens/PetitionsTable/PetitionsTable.tsx
+++ libs/service-portal/endorsements/src/screens/PetitionsTable/PetitionsTable.tsx
@@ -87,12 +87,14 @@ const PetitionsTable = (data: any) => {
         <Text variant="h3" marginBottom={2}>
           {formatMessage(m.viewPetition.enorsementsTableTitle)}
         </Text>
-        <ExportAsCSV
-          data={mapToCSVFile(listOfPetitions) as object[]}
-          filename="Meðmælalisti"
-          title="Sækja lista"
-          variant="text"
-        />
+        {data.isViewTypeEdit && (
+          <ExportAsCSV
+            data={mapToCSVFile(listOfPetitions) as object[]}
+            filename="Meðmælalisti"
+            title="Sækja lista"
+            variant="text"
+          />
+        )}
       </Box>
       <Stack space={3}>
         <T.Table>
@@ -120,7 +122,7 @@ const PetitionsTable = (data: any) => {
           </T.Body>
         </T.Table>
 
-        {listOfPetitions && !!listOfPetitions.length && (
+        {listOfPetitions && !!listOfPetitions.length ? (
           <>
             <Pagination
               page={page}
@@ -136,7 +138,7 @@ const PetitionsTable = (data: any) => {
               )}
             />
 
-            {data.isSendEmailVisible && (
+            {data.isViewTypeEdit && (
               <Box marginTop={5}>
                 <Text variant="h3" marginBottom={2}>
                   {formatMessage(m.viewPetition.sendListTitle)}
@@ -173,6 +175,8 @@ const PetitionsTable = (data: any) => {
               </Box>
             )}
           </>
+        ) : (
+          <Text>{formatMessage(m.viewPetition.noPetitionsText)}</Text>
         )}
       </Stack>
     </Box>
