77f0ad2107c68a036feaf45b210855324b91a05c
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-11-17 11:27:43 +0000

    Make sure we speak to the correct s3 endpoint

--- apps/github-actions-cache/src/app/external.ts
+++ apps/github-actions-cache/src/app/external.ts
@@ -3,7 +3,7 @@ import environment from '../environments/environment'
 import { createCache } from '@island.is/cache'
 
 const { redis, production } = environment
-export const s3 = new S3()
+export const s3 = new S3({ region: 'eu-west-1' })
 export const cache = createCache({
   name: 'github-actions-cache',
   nodes: redis.urls,
