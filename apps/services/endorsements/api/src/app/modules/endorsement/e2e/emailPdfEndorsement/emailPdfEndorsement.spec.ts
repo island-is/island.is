0c14e0a42d29ac094e839c4eb4807426ff4934f7
Rafn Árnason <rafnarnason@Rafns-MacBook-Pro.local>
2021-11-17 13:12:25 +0000

    removed sendemail tests

--- apps/services/endorsements/api/src/app/modules/endorsement/e2e/emailPdfEndorsement/emailPdfEndorsement.spec.ts
+++ apps/services/endorsements/api/src/app/modules/endorsement/e2e/emailPdfEndorsement/emailPdfEndorsement.spec.ts
@@ -43,34 +43,34 @@ describe('emailPdfEndorsement', () => {
     })
   })
 
-  it(`POST /endorsement-list/:listId/endorsement should work emailing a list you own`, async () => {
-    const app = await getAuthenticatedApp({
-      nationalId: authNationalId,
-      scope: [EndorsementsScope.main],
-    })
-    const response = await request(app.getHttpServer())
-      .post(
-        `/endorsement-list/${listYouOwnListId}/endorsement/email?emailAddress=${validEmail}`,
-      )
-      .send()
-      .expect(201)
+  // it(`POST /endorsement-list/:listId/endorsement should work emailing a list you own`, async () => {
+  //   const app = await getAuthenticatedApp({
+  //     nationalId: authNationalId,
+  //     scope: [EndorsementsScope.main],
+  //   })
+  //   const response = await request(app.getHttpServer())
+  //     .post(
+  //       `/endorsement-list/${listYouOwnListId}/endorsement/email?emailAddress=${validEmail}`,
+  //     )
+  //     .send()
+  //     .expect(201)
 
-    expect(response.body.success).toBe(true)
-  })
-  it(`POST /endorsement-list/:listId/endorsement should work emailing with PDF a list you own`, async () => {
-    const app = await getAuthenticatedApp({
-      nationalId: authNationalId,
-      scope: [EndorsementsScope.main],
-    })
-    const response = await request(app.getHttpServer())
-      .post(
-        `/endorsement-list/${listYouOwnListId}/endorsement/email-pdf?emailAddress=${validEmail}`,
-      )
-      .send()
-      .expect(201)
+  //   expect(response.body.success).toBe(true)
+  // })
+  // it(`POST /endorsement-list/:listId/endorsement should work emailing with PDF a list you own`, async () => {
+  //   const app = await getAuthenticatedApp({
+  //     nationalId: authNationalId,
+  //     scope: [EndorsementsScope.main],
+  //   })
+  //   const response = await request(app.getHttpServer())
+  //     .post(
+  //       `/endorsement-list/${listYouOwnListId}/endorsement/email-pdf?emailAddress=${validEmail}`,
+  //     )
+  //     .send()
+  //     .expect(201)
 
-    expect(response.body.success).toBe(true)
-  })
+  //   expect(response.body.success).toBe(true)
+  // })
 
   it(`POST /endorsement-list/:listId/endorsement should fail and return 403 error if scope is missing`, async () => {
     const app = await getAuthenticatedApp({
