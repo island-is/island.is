e17b506019807a594afc1c9ed31436609c321463
Magnus Skulason <maggisk@gmail.com>
2021-11-18 16:08:14 +0000

    revert accidental whitespace change

--- apps/services/endorsements/api/src/app/modules/endorsementList/dto/validationRule.dto.ts
+++ apps/services/endorsements/api/src/app/modules/endorsementList/dto/validationRule.dto.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IsEnum, IsObject, IsOptional } from 'class-validator'
 import { IsEndorsementValidator } from '../../endorsementValidator/endorsementValidator.decorator'
 import { ValidationRule } from '../../endorsementValidator/endorsementValidator.service'
-
 export class ValidationRuleDto {
   @ApiProperty({ enum: ValidationRule })
   @IsEnum(ValidationRule)
