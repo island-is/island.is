5a400e857d0b6ed4142c8852da8f6cfa5d27b3d0
Disa <disa@hugsmidjan.is>
2021-11-18 14:35:24 +0000

    refactor: make headings smaller in settings

--- libs/service-portal/settings/personal-information/src/screens/EditEmail/EditEmail.tsx
+++ libs/service-portal/settings/personal-information/src/screens/EditEmail/EditEmail.tsx
@@ -69,7 +69,7 @@ export const EditEmail: ServicePortalModuleComponent = () => {
   return (
     <>
       <Box marginBottom={4}>
-        <Text variant="h1" as="h1">
+        <Text variant="h3" as="h1">
           {formatMessage({
             id: 'sp.settings:edit-email',
             defaultMessage: 'Breyta netfangi',
