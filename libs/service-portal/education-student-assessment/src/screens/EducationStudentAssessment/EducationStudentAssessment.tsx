382ce424bf1130c30df01d76405327f35c45f337
Disa <disa@hugsmidjan.is>
2021-11-18 14:37:28 +0000

    refactor: move title for student assessment tables

--- libs/service-portal/education-student-assessment/src/screens/EducationStudentAssessment/EducationStudentAssessment.tsx
+++ libs/service-portal/education-student-assessment/src/screens/EducationStudentAssessment/EducationStudentAssessment.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { defineMessage } from 'react-intl'
 
 import { Box } from '@island.is/island-ui/core'
 import { useNamespaces } from '@island.is/localization'
-import { IntroHeader } from '@island.is/service-portal/core'
 import { StudentAssessmentTable } from './components/StudentAssessmentTable'
 
 function EducationStudentAssessment(): JSX.Element {
@@ -11,18 +9,6 @@ function EducationStudentAssessment(): JSX.Element {
 
   return (
     <Box marginBottom={[6, 6, 10]}>
-      <IntroHeader
-        title={defineMessage({
-          id:
-            'sp.education-student-assessment:education-student-assessment-title',
-          defaultMessage: 'Námsmat',
-        })}
-        intro={defineMessage({
-          id:
-            'sp.education-student-assessment:education-student-assessment-intro',
-          defaultMessage: 'Hér getur þú skoðað námsmat.',
-        })}
-      />
       <StudentAssessmentTable />
     </Box>
   )
