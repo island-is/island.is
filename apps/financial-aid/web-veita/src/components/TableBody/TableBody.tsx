600b2b82f660c04386e3d0470b84690a97dd963e
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-11-19 11:16:20 +0000

    fix(fa): Making underline in tables visable (#5680)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/financial-aid/web-veita/src/components/TableBody/TableBody.tsx
+++ apps/financial-aid/web-veita/src/components/TableBody/TableBody.tsx
@@ -37,7 +37,7 @@ const ActivationButtonTableItem = (
   destructiveColorScheme: boolean,
 ) => {
   return (
-    <Box>
+    <Box className={tableStyles.paddingForButton}>
       <Button
         onClick={(event) => {
           event.stopPropagation()
