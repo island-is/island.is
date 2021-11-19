496e0e64479846e6adec01b4476669c3b2476799
Kristjan Albert Loftsson <kal@kosmosogkaos.is>
2021-11-19 15:38:13 +0000

    Add accordion option for sub links in Navigation

--- libs/island-ui/core/src/lib/Navigation/Navigation.tsx
+++ libs/island-ui/core/src/lib/Navigation/Navigation.tsx
@@ -1,15 +1,27 @@
-import React, { FC, useState, useEffect, Fragment, ReactNode } from 'react'
+import React, { FC, useState, useEffect, ReactNode, createContext } from 'react'
 import cn from 'classnames'
+import AnimateHeight from 'react-animate-height'
 import { useMenuState, Menu, MenuButton, MenuStateReturn } from 'reakit/Menu'
 import { theme, Colors } from '@island.is/island-ui/theme'
 import { Text } from '../Text/Text'
 import { Box } from '../Box/Box'
 import { BoxProps } from '../Box/types'
 import { FocusableBox } from '../FocusableBox/FocusableBox'
-import { Button } from '../Button/Button'
+import { Icon } from '../IconRC/Icon'
 
 import * as styles from './Navigation.css'
-import { Icon } from '../IconRC/Icon'
+
+type NavigationContextProps = {
+  baseId: string
+  activeAccordions: Array<string>
+  toggleAccordion: (id: string) => void
+}
+
+export const NavigationContext = createContext<NavigationContextProps>({
+  baseId: '',
+  activeAccordions: [],
+  toggleAccordion: () => null,
+})
 
 export type NavigationColorAttributes =
   | 'color'
@@ -45,6 +57,7 @@ export interface NavigationItem {
   title: string
   href?: string
   active?: boolean
+  accordion?: boolean
   items?: NavigationItem[]
   typename?: string
   slug?: string[]
@@ -67,13 +80,22 @@ interface NavigationTreeProps {
   renderLink?: (link: ReactNode, item?: NavigationItem) => ReactNode
   menuState: MenuStateReturn
   linkOnClick?: () => void
+  id?: string
+  labelId?: string
 }
 export interface NavigationProps {
   title: string
   label?: string
   activeItemTitle?: string
   colorScheme?: keyof typeof styles.colorScheme
+  /**
+   * Keep all child menu items expanded
+   */
   expand?: boolean
+  /**
+   * Only a single acccordion can be expanded at a time
+   */
+  singleAccordion?: boolean
   isMenuDialog?: boolean
   titleLink?: Pick<NavigationItem, 'href' | 'active'>
   items: NavigationItem[]
@@ -97,6 +119,9 @@ const basePadding = {
 
 const defaultLinkRender: NavigationTreeProps['renderLink'] = (link) => link
 
+const toggleId = (arr: Array<string> = [], id: string, single = false) =>
+  arr.includes(id) ? arr.filter((i) => i !== id) : [...(single ? [] : arr), id]
+
 export const Navigation: FC<NavigationProps> = ({
   title = 'Efnisyfirlit',
   titleLink,
@@ -104,6 +129,7 @@ export const Navigation: FC<NavigationProps> = ({
   label,
   colorScheme = 'blue',
   expand,
+  singleAccordion = false,
   renderLink = defaultLinkRender,
   isMenuDialog = false,
   items,
@@ -111,13 +137,13 @@ export const Navigation: FC<NavigationProps> = ({
   baseId,
 }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [activeAccordions, setActiveAccordions] = useState<Array<string>>([])
 
   const color = colorSchemeColors[colorScheme]['color']
   const activeColor = colorSchemeColors[colorScheme]['activeColor']
   const backgroundColor = colorSchemeColors[colorScheme]['backgroundColor']
   const dividerColor = colorSchemeColors[colorScheme]['dividerColor']
 
-  const someActiveLinks = items.some((x) => x.active)
   const menu = useMenuState({ animated: true, baseId, visible: false })
 
   useEffect(() => {
@@ -129,6 +155,10 @@ export const Navigation: FC<NavigationProps> = ({
         href: titleLink.href,
       }
     : null
+
+  const toggleAccordion = (id: string) => {
+    setActiveAccordions(toggleId(activeAccordions, id, singleAccordion))
+  }
 
   const Title: MobileNavigationDialogProps['Title'] = titleLinkProps ? (
     renderLink(
@@ -174,7 +204,9 @@ export const Navigation: FC<NavigationProps> = ({
   )
 
   return (
-    <>
+    <NavigationContext.Provider
+      value={{ baseId, activeAccordions, toggleAccordion }}
+    >
       {isMenuDialog ? (
         <Box
           background={backgroundColor}
@@ -201,7 +233,7 @@ export const Navigation: FC<NavigationProps> = ({
               transform: 'none',
               width: '100%',
               borderRadius: '8px',
-              zIndex: 10,
+              zIndex: 1000000,
             }}
             className={cn(styles.transition, styles.menuShadow[colorScheme])}
           >
@@ -240,7 +272,7 @@ export const Navigation: FC<NavigationProps> = ({
           />
         </Box>
       )}
-    </>
+    </NavigationContext.Provider>
   )
 }
 
@@ -267,14 +299,19 @@ const MobileNavigationDialog = ({
           marginRight={2}
           style={{ top: '50%', transform: 'translateY(-50%)' }}
         >
-          <Button
-            circle
-            colorScheme="negative"
-            icon={'chevronUp'}
-            size="small"
-            aria-controls={`CloseNavigationDialog`}
+          <FocusableBox
+            component="button"
             onClick={onClick}
-          />
+            aria-controls={`CloseNavigationDialog`}
+            background={colorSchemeColors[colorScheme]['dividerColor']}
+            className={styles.dropdownIcon}
+          >
+            <Icon
+              icon={'chevronUp'}
+              size="small"
+              color={colorSchemeColors[colorScheme]['color']}
+            />
+          </FocusableBox>
         </Box>
       </Box>
       <Box display="flex" alignItems="center" paddingY={2}>
@@ -317,15 +354,23 @@ const MobileButton = ({ title, colorScheme }: MobileButtonProps) => {
       </Text>
 
       <Box
-        component="span"
         position="absolute"
         right={0}
         marginRight={2}
         style={{ top: '50%', transform: 'translateY(-50%)' }}
       >
-        <span className={styles.dropdownIcon}>
-          <Icon icon={'chevronDown'} size="small" color="blue400" />
-        </span>
+        <FocusableBox
+          component="button"
+          aria-controls={`OpenNavigationDialog`}
+          background={colorSchemeColors[colorScheme]['dividerColor']}
+          className={styles.dropdownIcon}
+        >
+          <Icon
+            icon={'chevronDown'}
+            size="small"
+            color={colorSchemeColors[colorScheme]['color']}
+          />
+        </FocusableBox>
       </Box>
     </Box>
   )
@@ -339,65 +384,41 @@ export const NavigationTree: FC<NavigationTreeProps> = ({
   renderLink = defaultLinkRender,
   menuState,
   linkOnClick,
+  id = '',
+  labelId = '',
 }: NavigationTreeProps) => {
   return (
-    <Box
-      component="ul"
-      className={cn(styles.ul, styles.level[level])}
-      style={{
-        borderLeftColor:
-          level > 1
-            ? theme.color[colorSchemeColors[colorScheme]['dividerColor']]
-            : undefined,
-      }}
-    >
-      {items.map((item, index) => {
-        const { title, href, items = [], active } = item
-        const nextLevel: Level = (level + 1) as Level
-        const isChildren = level > 1
-        const showNextLevel =
-          (active || expand) && items.length && nextLevel <= MAX_LEVELS
+    <NavigationContext.Consumer>
+      {({ baseId, activeAccordions, toggleAccordion }) => (
+        <Box
+          component="ul"
+          {...(id && { id })}
+          {...(labelId && { 'aria-labelledby': labelId })}
+          className={cn(styles.ul, styles.level[level])}
+          style={{
+            borderLeftColor:
+              level > 1
+                ? theme.color[colorSchemeColors[colorScheme]['dividerColor']]
+                : undefined,
+          }}
+        >
+          {items.map((item, index) => {
+            const { title, href, items = [], active, accordion } = item
+            const nextLevel: Level = (level + 1) as Level
+            const isChildren = level > 1
+            const showNextLevel =
+              (active || expand) &&
+              items.length &&
+              nextLevel <= MAX_LEVELS &&
+              !accordion
+            const useAccordion =
+              items.length && nextLevel <= MAX_LEVELS && accordion
+            const accordionId = `${level}-${index}`
+            const activeAccordion = activeAccordions.includes(accordionId)
+            const labelId = `${baseId}-title-${accordionId}`
+            const id = `${baseId}-tree-${accordionId}`
 
-        return (
-          <li key={index}>
-            {renderLink(
-              <FocusableBox
-                component="a"
-                href={href}
-                borderRadius="large"
-                paddingLeft={isChildren ? 2 : 3}
-                paddingRight={2}
-                paddingY={isChildren ? 'smallGutter' : 1}
-                className={styles.link}
-                onClick={linkOnClick}
-              >
-                {({
-                  isFocused,
-                  isHovered,
-                }: {
-                  isFocused: boolean
-                  isHovered: boolean
-                }) => {
-                  const textColor =
-                    active || isFocused || isHovered
-                      ? colorSchemeColors[colorScheme]['activeColor']
-                      : colorSchemeColors[colorScheme]['color']
-
-                  return (
-                    <Text
-                      as="span"
-                      color={textColor}
-                      variant={isChildren ? 'small' : 'default'}
-                      fontWeight={active ? 'semiBold' : 'light'}
-                    >
-                      {title}
-                    </Text>
-                  )
-                }}
-              </FocusableBox>,
-              item,
-            )}
-            {showNextLevel ? (
+            const nextLevelTree = (
               <NavigationTree
                 items={items}
                 level={nextLevel}
@@ -407,10 +428,109 @@ export const NavigationTree: FC<NavigationTreeProps> = ({
                 menuState={menuState}
                 linkOnClick={linkOnClick}
               />
-            ) : null}
-          </li>
-        )
-      })}
-    </Box>
+            )
+
+            return (
+              <li key={index} className={styles.listItem}>
+                {renderLink(
+                  <FocusableBox
+                    component="a"
+                    href={href}
+                    borderRadius="large"
+                    paddingLeft={isChildren ? 2 : 3}
+                    paddingRight={2}
+                    paddingY={isChildren ? 'smallGutter' : 1}
+                    className={styles.link}
+                    onClick={linkOnClick}
+                  >
+                    {({
+                      isFocused,
+                      isHovered,
+                    }: {
+                      isFocused: boolean
+                      isHovered: boolean
+                    }) => {
+                      const textColor =
+                        active || isFocused || isHovered
+                          ? colorSchemeColors[colorScheme]['activeColor']
+                          : colorSchemeColors[colorScheme]['color']
+
+                      return (
+                        <span
+                          className={cn(styles.text, {
+                            [styles.textNarrower]: useAccordion,
+                          })}
+                        >
+                          <Text
+                            id={`navigation-title-${accordionId}`}
+                            as="span"
+                            color={textColor}
+                            variant={isChildren ? 'small' : 'default'}
+                            fontWeight={active ? 'semiBold' : 'light'}
+                          >
+                            {title}
+                          </Text>
+                        </span>
+                      )
+                    }}
+                  </FocusableBox>,
+                  item,
+                )}
+                {!!useAccordion && (
+                  <FocusableBox
+                    component="button"
+                    onClick={() => {
+                      toggleAccordion(accordionId)
+                    }}
+                    background={colorSchemeColors[colorScheme]['dividerColor']}
+                    marginRight={2}
+                    aria-expanded={activeAccordion}
+                    aria-controls={id}
+                    className={cn(
+                      styles.accordionIcon,
+                      styles.largerClickableArea,
+                    )}
+                  >
+                    <Icon
+                      icon="add"
+                      color={colorSchemeColors[colorScheme]['color']}
+                      size="small"
+                      className={cn(
+                        styles.icon,
+                        activeAccordion
+                          ? styles.iconAddHidden
+                          : styles.iconAddVisible,
+                      )}
+                    />
+                    <Icon
+                      icon="remove"
+                      color={colorSchemeColors[colorScheme]['color']}
+                      size="small"
+                      className={cn(
+                        styles.icon,
+                        activeAccordion
+                          ? styles.iconRemoveVisible
+                          : styles.iconRemoveHidden,
+                      )}
+                    />
+                  </FocusableBox>
+                )}
+                {!!useAccordion && (
+                  <AnimateHeight
+                    id={id}
+                    duration={300}
+                    height={activeAccordion ? 'auto' : 0}
+                    aria-labelledby={labelId}
+                  >
+                    {nextLevelTree}
+                  </AnimateHeight>
+                )}
+                {!!showNextLevel && nextLevelTree}
+              </li>
+            )
+          })}
+        </Box>
+      )}
+    </NavigationContext.Consumer>
   )
 }
