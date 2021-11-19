f5bff6a66094544048bc85b0024e40b24d4d89b1
kksteini <steini@juni.is>
2021-11-19 12:12:44 +0000

    Typescript fix

--- apps/web/hooks/useLinkResolver/useLinkResolver.ts
+++ apps/web/hooks/useLinkResolver/useLinkResolver.ts
@@ -19,7 +19,7 @@ interface TypeResolverResponse {
   slug?: string[]
 }
 
-export type LinkType = keyof typeof routesTemplate | 'linkurl'
+export type LinkType = keyof typeof routesTemplate | 'linkurl' | 'link'
 
 /*
 The order here matters for type resolution, arrange overlapping types from most specific to least specific for correct type resolution
