82ebb1a00b80fd697b378f541c8219abef99034e
Unnur Sól Ingimarsdóttir <ori.unnuri@ORI0480-5.local>
2021-11-24 13:58:11 +0000

    Contentful config

--- libs/application/templates/criminal-record/src/lib/messages.ts
+++ libs/application/templates/criminal-record/src/lib/messages.ts
@@ -7,12 +7,12 @@ export const m = defineMessages({
     description: 'Some description',
   },
   externalDataTitle: {
-    id: 'cr.application.externalData.title',
+    id: 'cr.application:application.title',
     defaultMessage: 'Umsókn um sakaskrá',
     description: 'Title of the application',
   },
   externalDataSubTitle: {
-    id: 'cr.application.externalData.title',
+    id: 'cr.application:externalData.title',
     defaultMessage: 'Eftirfarandi gögn verða sótt rafrænt',
     description: 'he following data will be retrieved electronically',
   },
@@ -89,7 +89,7 @@ export const m = defineMessages({
     description: `Application's name`,
   },
   institutionName: {
-    id: 'cr.application.institution',
+    id: 'cr.application:institution',
     defaultMessage: 'Sýslumenn',
     description: `Institution's name`,
   },
@@ -104,119 +104,12 @@ export const m = defineMessages({
     description: 'Cost',
   },
   draftDescription: {
-    id: 'example.application:draft.description',
+    id: 'cr.application:draft.description',
     defaultMessage: 'Á eftir að samþykkja gagnaöflun',
     description: 'Description of the state',
   },
-  introSection: {
-    id: 'example.application:intro.section',
-    defaultMessage: 'Upplýsingar',
-    description: 'Some description',
-  },
-  introField: {
-    id: 'example.application:intro.field',
-    defaultMessage: 'Velkomin(n)',
-    description: 'Some description',
-  },
-  introIntroduction: {
-    id: 'example.application:intro.introduction',
-    defaultMessage:
-      '*Hello*, **{name}**! [This is a link to Google!](http://google.com)',
-    description: 'Some description',
-  },
-  about: {
-    id: 'example.application:about',
-    defaultMessage: 'Um þig',
-    description: 'Some description',
-  },
-  personName: {
-    id: 'example.application:person.name',
-    defaultMessage: 'Nafn',
-    description: 'Some description',
-  },
-  nationalId: {
-    id: 'example.application:person.nationalId',
-    defaultMessage: 'Kennitala',
-    description: 'Some description',
-  },
-  age: {
-    id: 'example.application:person.age',
-    defaultMessage: 'Aldur',
-    description: 'Some description',
-  },
-  email: {
-    id: 'example.application:person.email',
-    defaultMessage: 'Netfang',
-    description: 'Some description',
-  },
-  phoneNumber: {
-    id: 'example.application:person.phoneNumber',
-    defaultMessage: 'Símanúmer',
-    description: 'Some description',
-  },
-  career: {
-    id: 'example.application:career',
-    defaultMessage: 'Starfsferill',
-    description: 'Some description',
-  },
-  history: {
-    id: 'example.application:history',
-    defaultMessage: 'Hvar hefur þú unnið áður?',
-    description: 'Some description',
-  },
-  careerHistory: {
-    id: 'example.application:careerHistory',
-    defaultMessage: 'Hefurðu unnið yfir höfuð einhvern tímann áður?',
-    description: 'Some description',
-  },
-  careerHistoryCompanies: {
-    id: 'example.application:careerHistoryCompanies',
-    defaultMessage: 'Hefurðu unnið fyrir eftirfarandi aðila?',
-    description: 'Some description',
-  },
-  future: {
-    id: 'example.application:future',
-    defaultMessage: 'Hvar langar þig að vinna?',
-    description: 'Some description',
-  },
-  dreamJob: {
-    id: 'example.application:dreamJob',
-    defaultMessage: 'Einhver draumavinnustaður?',
-    description: 'Some description',
-  },
-  yesOptionLabel: {
-    id: 'example.application:yes.option.label',
-    defaultMessage: 'Já',
-    description: 'Some description',
-  },
-  noOptionLabel: {
-    id: 'example.application:no.option.label',
-    defaultMessage: 'Nei',
-    description: 'Some description',
-  },
-  governmentOptionLabel: {
-    id: 'example.application:government.option.label',
-    defaultMessage: 'The government',
-    description: 'Some description',
-  },
-  outroMessage: {
-    id: 'example.application:outro.message',
-    defaultMessage:
-      'Your application #{id} is now in review. The ID of the application is returned by the createApplication API action and read from application.externalData',
-    description: 'Some description',
-  },
-  dataSchemePhoneNumber: {
-    id: 'example.application:dataSchema.phoneNumber',
-    defaultMessage: 'Símanúmerið þarf að vera gilt.',
-    description: 'Error message when phone number is invalid.',
-  },
-  dataSchemeNationalId: {
-    id: 'example.application:dataSchema.national.id',
-    defaultMessage: 'Kennitala þarf að vera gild.',
-    description: 'Error message when the kennitala is invalid.',
-  },
   errorDataProvider: {
-    id: 'dl.application:error.dataProvider',
+    id: 'cr.application:error.dataProvider',
     defaultMessage: 'Reyndu aftur síðar',
     description: 'Unhandled error in criminal record data provider',
   },
@@ -278,5 +171,11 @@ export const m = defineMessages({
     id: 'cr.application:successDescription',
     defaultMessage: 'Þú getur nú nálgast umsóknina þína inni á mínum síðum',
     description: '',
+  },
+  outroMessage: {
+    id: 'cr.application:outro.message',
+    defaultMessage:
+      'Your application #{id} is now in review. The ID of the application is returned by the createApplication API action and read from application.externalData',
+    description: 'Some description',
   },
 })
