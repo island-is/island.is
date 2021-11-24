b6f4bc742047327c08267da9e53e9020ac9c27f6
johannaagma <johannaagma@gmail.com>
2021-11-24 15:18:32 +0000

    Cleanup

--- libs/clients/criminal-record/src/lib/criminalRecordApi.service.ts
+++ libs/clients/criminal-record/src/lib/criminalRecordApi.service.ts
@@ -7,8 +7,6 @@ export class CriminalRecordApi {
   constructor(private readonly api: CrimeCertificateApi) {}
 
   public async getCriminalRecord(ssn: string): Promise<CriminalRecord> {
-    console.log('---------------- CriminalRecordApi.getCriminalRecord ' + ssn + ' ----------------');
-
     const blob = await this.api.apiPdfCreatePersonIdGet({ personId: ssn })
     const blobText = await blob.arrayBuffer()
     const blobTextBase64 = Buffer.from(blobText).toString('base64')
@@ -20,9 +18,7 @@ export class CriminalRecordApi {
     return record
   }
 
-  public async checkCriminalRecord(ssn: string): Promise<Boolean> {
-    console.log('---------------- CriminalRecordApi.checkCriminalRecord ----------------');
-    
+  public async checkCriminalRecord(ssn: string): Promise<Boolean> {    
     const record = await this.getCriminalRecord(ssn)
 
     return record.pdfBase64.length > 0
