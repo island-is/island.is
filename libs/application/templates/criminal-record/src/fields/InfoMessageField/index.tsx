1e365dcd1b87084dc2e60b107fca253b259ff9c1
Unnur Sól Ingimarsdóttir <ori.unnuri@ORI0480-5.local>
2021-11-24 15:49:58 +0000

    Display criminal record

--- libs/application/templates/criminal-record/src/fields/InfoMessageField/index.tsx
+++ libs/application/templates/criminal-record/src/fields/InfoMessageField/index.tsx
@@ -11,6 +11,7 @@ import React, { FC } from 'react'
 import { useLocale } from '@island.is/localization'
 import { FieldBaseProps, formatText } from '@island.is/application/core'
 import { Box } from '@island.is/island-ui/core'
+import { ApiActions } from '../../shared'
 
 type InfoMessageFieldProps = {
   field: {
@@ -23,6 +24,15 @@ type InfoMessageFieldProps = {
       marginTop?: ResponsiveSpace
     }
   }
+  application: {
+    externalData: {
+      getCriminalRecord: {
+        data: {
+          pdfBase64: string
+        }
+      }
+    }
+  }
 }
 
 export const InfoMessageField: FC<FieldBaseProps & InfoMessageFieldProps> = ({
@@ -30,47 +40,59 @@ export const InfoMessageField: FC<FieldBaseProps & InfoMessageFieldProps> = ({
   field,
 }) => {
   const { title, description, props } = field
+  const { externalData } = application
   const { formatMessage } = useLocale()
   const { link, marginBottom = 2, marginTop = 0 } = props
+
+  console.log(externalData)
   return (
-    <Box
-      marginBottom={marginBottom}
-      marginTop={marginTop}
-      background="blue100"
-      padding={4}
-      display="flex"
-    >
-      <LinkContext.Provider
-        value={{
-          linkRenderer: (href, children) => (
-            <a
-              style={{
-                color: '#0061ff',
-                textDecoration: 'none',
-                boxShadow: 'inset 0 -1px 0 0 currentColor',
-                paddingBottom: 4,
-              }}
-              href={href}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {children}
-            </a>
-          ),
-        }}
+    <>
+      <Box
+        marginBottom={marginBottom}
+        marginTop={marginTop}
+        background="blue100"
+        padding={4}
+        display="flex"
       >
-        <Text variant="small">
-          {description && formatText(description, application, formatMessage)}{' '}
-          <Link
-            href={link?.url || ''}
-            color="blue400"
-            underline="normal"
-            underlineVisibility="always"
-          >
-            {link?.title}
-          </Link>
-        </Text>
-      </LinkContext.Provider>
-    </Box>
+        <LinkContext.Provider
+          value={{
+            linkRenderer: (href, children) => (
+              <a
+                style={{
+                  color: '#0061ff',
+                  textDecoration: 'none',
+                  boxShadow: 'inset 0 -1px 0 0 currentColor',
+                  paddingBottom: 4,
+                }}
+                href={href}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {children}
+              </a>
+            ),
+          }}
+        >
+          <Text variant="small">
+            {description && formatText(description, application, formatMessage)}{' '}
+            <Link
+              href={link?.url || ''}
+              color="blue400"
+              underline="normal"
+              underlineVisibility="always"
+            >
+              {link?.title}
+            </Link>
+          </Text>
+        </LinkContext.Provider>
+      </Box>
+      <object
+        data={`data:application/pdf;base64,${externalData.getCriminalRecord.data?.pdfBase64}`}
+        // type="application/pdf"
+      ></object>
+      <iframe
+        src={`data:application/pdf;base64,${externalData.getCriminalRecord.data?.pdfBase64}`}
+      ></iframe>
+    </>
   )
 }
