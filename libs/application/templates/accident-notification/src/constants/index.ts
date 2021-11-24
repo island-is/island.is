2643edf836c84743928dcbe4a6f0f92c1965d95b
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2021-11-24 11:15:50 +0000

    add jpg, jpeg and png as allowed file extensions

--- libs/application/templates/accident-notification/src/constants/index.ts
+++ libs/application/templates/accident-notification/src/constants/index.ts
@@ -1,7 +1,7 @@
 export const YES = 'yes'
 export const NO = 'no'
 
-export const UPLOAD_ACCEPT = '.pdf, .doc, .docx, .rtf'
+export const UPLOAD_ACCEPT = '.pdf, .doc, .docx, .rtf, .jpg, .jpeg, .png'
 
 export enum States {
   // Draft flow
