324e1abeca437243846ea195f6230163e7a20473
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-11-30 13:04:13 +0000

    Remove entrypoint

--- apps/external-contracts-tests/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-
- yarn run --silent nx run external-contracts-tests:test --json --silent | tail -n 1 > report.json
- node -r esbuild-register jest-to-dd.ts report.json     
