455fdf999013789c691974be908832ae75ae92ce
kksteini <77672665+kksteini@users.noreply.github.com>
2021-12-01 14:27:59 +0000

    fix(web): Frontpage UI changes and tweaks (#5826)
    * fix(web): fix double footer (#5781)
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    * feat(web): new front page (#5562)
    
    * New components for new front page
    
    * Option for illustration above footer
    
    * New Home screen and model changes
    
    * Remove fragment wrapper
    
    * Footer changes
    
    * Update factories
    
    * Remove unused components
    
    * Remove unused
    
    * Update color scheme settings for CategoryCard
    
    * Set correct colors
    
    * Add mobile image field
    
    * Option for desktop and mobile video with image fallback
    
    * Multiple videos instead of single for both desktop and mobile
    
    * Use existing LinkList and Link models for front page NewLinks
    
    * Option for max rows in grid items
    
    * Revert change and show link list items only if date set
    
    * Fix mobile overflow-x on grid items wrapper
    
    * Add field to link factory
    
    * Add heading variant option for CategoryCard
    
    * Minor change
    
    * Hide show more button for NewLinks
    
    * PR comment fixes
    
    * PR Footer changes
    
    * more PR comment changes
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    * fix(web): search filter sidebar and results (#5812)
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    * fix(web): set fixed height for search section on desktop (#5816)
    
    * Set fixed height for search section on desktop
    
    * Schema generation fix
    
    Co-authored-by: kksteini <steini@juni.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    Co-authored-by: Kristján Albert <kal@kosmosogkaos.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: kksteini <steini@juni.is>

--- libs/cms/src/lib/models/lifeEventPage.model.ts
+++ libs/cms/src/lib/models/lifeEventPage.model.ts
@@ -1,12 +1,8 @@
 import { Field, ObjectType, ID } from '@nestjs/graphql'
-import {
-  ILifeEventPage,
-  ILifeEventPageFields,
-} from '../generated/contentfulTypes'
+import { ILifeEventPage } from '../generated/contentfulTypes'
 import { Image, mapImage } from './image.model'
 import { ArticleCategory, mapArticleCategory } from './articleCategory.model'
 import { mapDocument, SliceUnion } from '../unions/slice.union'
-import { Entry } from 'contentful'
 
 @ObjectType()
 export class LifeEventPage {
@@ -15,6 +11,9 @@ export class LifeEventPage {
 
   @Field()
   title!: string
+
+  @Field(() => String, { nullable: true })
+  shortTitle?: string | null
 
   @Field()
   slug!: string
@@ -27,6 +26,9 @@ export class LifeEventPage {
 
   @Field(() => Image, { nullable: true })
   thumbnail?: Image | null
+
+  @Field(() => Image, { nullable: true })
+  tinyThumbnail?: Image | null
 
   @Field(() => [SliceUnion])
   content!: Array<typeof SliceUnion>
@@ -41,10 +43,12 @@ export const mapLifeEventPage = ({
 }: ILifeEventPage): LifeEventPage => ({
   id: sys.id,
   title: fields.title ?? '',
+  shortTitle: fields.shortTitle ?? '',
   slug: fields.slug ?? '',
   intro: fields.intro ?? '',
   image: fields.image ? mapImage(fields.image) : null,
   thumbnail: fields.thumbnail ? mapImage(fields.thumbnail) : null,
+  tinyThumbnail: fields.tinyThumbnail ? mapImage(fields.tinyThumbnail) : null,
   content: fields?.content
     ? mapDocument(fields.content, sys.id + ':content')
     : [],
