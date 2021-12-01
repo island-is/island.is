1d868c643cd4daa8d69eee523d081fc6b0d46ff5
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2021-12-01 22:43:16 +0000

    Removing unused stuff.

--- apps/services/auth-public-api/test/hooks/useTestDelegations.ts
+++
@@ -1,8 +0,0 @@
-import { TestApp } from '@island.is/testing/nest'
-
-export const useTestDelegations = () => ({
-  override: (builder: TestingModuleBuilder) => {
-    return builder
-  },
-  extend: (app: TestApp) => {},
-})
