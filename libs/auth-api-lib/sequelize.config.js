e8a65eb8691e83c86490d5e73038d8129f69115d
Fiddi <fridrik@programm.is>
2021-12-01 09:57:52 +0000

    Attemt for test fix

--- libs/auth-api-lib/sequelize.config.js
+++ libs/auth-api-lib/sequelize.config.js
@@ -15,6 +15,8 @@ module.exports = {
     database: 'test_db',
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: 5433,
+    seederStorage: 'sequelize',
   },
   production: {
     username: process.env.DB_USER,
