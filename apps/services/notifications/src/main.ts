f55a5bea9ae0aa58342d07540c1ddbb62ce47fd6
Magnus Skulason <maggisk@gmail.com>
2021-12-02 11:03:49 +0000

    move worker entry point to main.ts

--- apps/services/notifications/src/main.ts
+++ apps/services/notifications/src/main.ts
@@ -1,11 +1,25 @@
+import yargs from 'yargs'
 import { bootstrap } from '@island.is/infra-nest-server'
-
+import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
+import { ConsumerService } from './app/modules/notifications/consumer.service'
 
-bootstrap({
-  appModule: AppModule,
-  name: 'services-notifications',
-  openApi,
-  swaggerPath: '',
-})
+const worker = async () => {
+  const app = await NestFactory.createApplicationContext(AppModule)
+  app.enableShutdownHooks()
+  await app.get(ConsumerService).run()
+}
+
+const { argv } = yargs(process.argv.slice(2))
+
+if (argv.job === 'worker') {
+  worker()
+} else {
+  bootstrap({
+    appModule: AppModule,
+    name: 'services-notifications',
+    openApi,
+    swaggerPath: '',
+  })
+}
