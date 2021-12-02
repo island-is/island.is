a80e9dc821062f08d14a7b4f4ea0f444323dc3e9
jGeir <juliusgisla@gmail.com>
2021-12-02 17:16:35 +0000

    rm redundancy

--- apps/air-discount-scheme/api/src/app/modules/auth/auth.module.ts
+++ apps/air-discount-scheme/api/src/app/modules/auth/auth.module.ts
@@ -6,10 +6,8 @@ import { environment } from '../../../environments'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { JwtStrategy } from './jwt.strategy'
-import { AuthPublicApiClientModule, AuthPublicApiClientModuleConfig } from '@island.is/clients/auth-public-api'
 
 const ONE_HOUR = 3600
-const auth_config = {baseApiUrl: process.env.AUTH_PUBLIC_API_URL ?? 'http://localhost:4242',} as AuthPublicApiClientModuleConfig
 @Module({
   imports: [
     PassportModule.register({
@@ -22,9 +20,6 @@ const auth_config = {baseApiUrl: process.env.AUTH_PUBLIC_API_URL ?? 'http://loca
         expiresIn: ONE_HOUR,
       },
     }),
-    AuthPublicApiClientModule.register(
-      auth_config
-    )
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
