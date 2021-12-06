171e3444830dbf234370a94e7b28c93be31b74e1
Rafn Árnason <rafnarnason@Rafns-MacBook-Pro.local>
2021-12-06 14:09:12 +0000

    cleanup format

--- apps/services/user-profile/src/app/app.module.ts
+++ apps/services/user-profile/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { SequelizeConfigService } from './sequelizeConfig.service'
-import { UserProfileModule } from './user-profile/userProfile.module'
 import { UserNotificationsModule } from './user-notifications/user-notifications.module'
+import { UserProfileModule } from './user-profile/userProfile.module'
+
 
 @Module({
   imports: [
