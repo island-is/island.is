4336cef00c9c57afa427c98d65d94c9363a384a7
Rafn Árnason <rafnarnason@Rafns-MacBook-Pro.local>
2021-12-10 13:00:28 +0000

    format

--- apps/services/user-profile/src/app/user-profile/dto/deviceToken.dto.ts
+++ apps/services/user-profile/src/app/user-profile/dto/deviceToken.dto.ts
@@ -6,8 +6,6 @@ import { UserDeviceTokens } from '../userDeviceTokens.model'
 const EXAMPLE_TOKEN =
   'f4XghAZSRs6L-RNWRo9-Mw:APA91bFGgAc-0rhMgeHCDvkMJBH_nU4dApG6qqATliEbPs9xXf5n7EJ7FiAjJ6NNCHMBKdqHMdLrkaFHxuShzTwmZquyCjchuVMwAGmlwdXY8vZWnVqvMVItYn5lfIH-mR7Q9FvnNlhv'
 
-
-  
 export class DeviceTokenDto extends PartialType(UserDeviceTokens) {
   @ApiProperty({
     required: true,
