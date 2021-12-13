492d38c80683e9b79447cbcec778263abb52a11d
Sigrún Tinna Gissurardóttir <sigrun@sendiradid.is>
2021-12-13 10:44:40 +0000

    removing console.logs

--- libs/application/templates/accident-notification/src/utils/isUniqueAssignee.ts
+++ libs/application/templates/accident-notification/src/utils/isUniqueAssignee.ts
@@ -6,9 +6,7 @@ export const isUniqueAssignee = (
 ): boolean => {
   const applicant = getValueViaPath(formValue, 'applicant.nationalId')
   const assignee = getValueViaPath(formValue, 'representative.nationalId')
-  console.log(applicant, assignee)
   const isSamePerson = applicant === assignee
-  console.log(isSamePerson)
 
   return !isSamePerson && isAssignee
 }
