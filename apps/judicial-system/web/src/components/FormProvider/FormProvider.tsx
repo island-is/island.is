7eb0bd724be2f3a6fd83b4b8ef1f8e71447ee51c
Guðjón Guðjónsson <gudjon@kolibri.is>
2021-12-14 17:05:08 +0000

    Rewrites case loading

--- apps/judicial-system/web/src/components/FormProvider/FormProvider.tsx
+++ apps/judicial-system/web/src/components/FormProvider/FormProvider.tsx
@@ -7,6 +7,14 @@ import { CaseQuery } from '@island.is/judicial-system-web/graphql'
 
 import { CaseData } from '../../types'
 
+type ProviderState =
+  | 'fetch'
+  | 'refresh'
+  | 'up-to-date'
+  | 'ready'
+  | 'not-found'
+  | undefined
+
 interface FormProvider {
   workingCase: Case
   setWorkingCase: React.Dispatch<React.SetStateAction<Case>>
@@ -48,54 +56,76 @@ const FormProvider = ({ children }: Props) => {
       // is updated when the case is created.
       CaseType.OTHER
 
+  const [state, setState] = useState<ProviderState>()
+  const [caseId, setCaseId] = useState<string>()
+  const [path, setPath] = useState<string>()
+
   const [workingCase, setWorkingCase] = useState<Case>({
     ...initialState,
     type: caseType,
   })
-  const [isLoadingWorkingCase, setIsLoadingWorkingCase] = useState(false)
-  const [caseNotFound, setCaseNotFound] = useState(false)
-  const [isCaseUpToDate, setIsCaseUpToDate] = useState(false)
+
+  // Used in exported indicators
+  const replacingCase = router.query.id && router.query.id !== caseId
+  const replacingPath = router.pathname !== path
+
+  useEffect(() => {
+    if (!router.query.id) {
+      // Not working on a case
+      setState(undefined)
+    } else if (router.query.id === caseId) {
+      // Working on the same case as the previous page
+      setState('refresh')
+    } else {
+      // Starting work on a different case
+      setState('fetch')
+    }
+
+    setCaseId(router.query.id as string)
+    setPath(router.pathname)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query.id, router.pathname])
 
   const [getCase] = useLazyQuery<CaseData>(CaseQuery, {
     fetchPolicy: 'no-cache',
     onCompleted: (caseData) => {
       if (caseData?.case) {
         setWorkingCase(caseData.case)
-        setIsLoadingWorkingCase(false)
-        setIsCaseUpToDate(true)
+
+        // The case has been loaded from the server
+        setState('up-to-date')
       }
     },
     onError: () => {
-      setCaseNotFound(true)
-      setIsLoadingWorkingCase(false)
+      // The case was not found
+      setState('not-found')
     },
   })
 
   useEffect(() => {
-    if (
-      id &&
-      (router.pathname.startsWith('/krafa') ||
-        router.pathname.startsWith('/domur'))
-    ) {
-      setIsLoadingWorkingCase(true)
+    if (state === 'fetch' || state === 'refresh') {
       getCase({ variables: { input: { id } } })
     }
-  }, [id, router.pathname, getCase])
+  }, [getCase, id, state])
 
   useEffect(() => {
-    if (isCaseUpToDate) {
-      setIsCaseUpToDate(false)
+    if (state === 'up-to-date') {
+      // The case may change on the server so we only stay up to date for a short time
+      // The time needs to be long enough to let hooks take appropriate actions, for instance auto fill
+      setTimeout(() => setState('ready'), 1000)
     }
-  }, [isCaseUpToDate])
+  }, [state])
 
   return (
     <FormContext.Provider
       value={{
         workingCase,
         setWorkingCase,
-        isLoadingWorkingCase,
-        caseNotFound,
-        isCaseUpToDate,
+        // Loading when we have just switched cases or we are still fetching
+        isLoadingWorkingCase: replacingCase || state === 'fetch',
+        // Not found until we navigate to a different page
+        caseNotFound: !replacingPath && state === 'not-found',
+        isCaseUpToDate: state === 'up-to-date',
       }}
     >
       {children}
