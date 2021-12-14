aafdd9583926d4f9dc316c7d5e434d950d654f30
kksteini <steini@kosmosogkaos.is>
2021-12-14 12:59:44 +0000

    Added processEntry aggregations to ES

--- libs/api/domains/content-search/src/lib/contentSearch.service.ts
+++ libs/api/domains/content-search/src/lib/contentSearch.service.ts
@@ -3,6 +3,7 @@ import {
   ElasticService,
   TagAggregationResponse,
   TypeAggregationResponse,
+  ProcessEntryAggregationResponse,
 } from '@island.is/content-search-toolkit'
 import { logger } from '@island.is/logging'
 import { SearchResult } from './models/searchResult.model'
@@ -22,6 +23,19 @@ export class ContentSearchService {
 
   private getIndex(lang: ElasticsearchIndexLocale = 'is') {
     return getElasticsearchIndex(lang)
+  }
+
+  mapProcessEntryAggregations(
+    aggregations: ProcessEntryAggregationResponse,
+  ): number | undefined {
+    if (!aggregations?.processEntryCount) {
+      return
+    }
+    let total = 0
+    for (const bucket of aggregations.processEntryCount.buckets) {
+      total += bucket.doc_count * bucket.key
+    }
+    return total
   }
 
   mapTagAggregations(
@@ -68,6 +82,9 @@ export class ContentSearchService {
       ),
       typesCount: this.mapTypeAggregations(
         body.aggregations as TypeAggregationResponse,
+      ),
+      processEntryCount: this.mapProcessEntryAggregations(
+        body.aggregations as ProcessEntryAggregationResponse,
       ),
     }
   }
