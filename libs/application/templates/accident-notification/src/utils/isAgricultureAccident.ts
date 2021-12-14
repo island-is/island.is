eb099705293c54f0cd46b084eda9df7edb1dbf6c
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2021-12-14 11:59:46 +0000

    chore(accident-notification): Refactor of how we get values from answers (#5690)
    * refactor of how we get values
    
    * typos
    
    * remove unused
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/accident-notification/src/utils/isAgricultureAccident.ts
+++ libs/application/templates/accident-notification/src/utils/isAgricultureAccident.ts
@@ -1,13 +1,14 @@
-import { FormValue } from '@island.is/application/core'
+import { FormValue, getValueViaPath } from '@island.is/application/core'
 import { WorkAccidentTypeEnum } from '../types'
 import { isWorkAccident } from './isWorkAccident'
 
 // As this is a second question the user is asked there is a case where he could go back and select home activities and keep the agriculture type.
 // Therefore we need to check also whether this is a work accident
 export const isAgricultureAccident = (formValue: FormValue) => {
-  const workAccidentType = (formValue as {
-    workAccident: { type: WorkAccidentTypeEnum }
-  })?.workAccident?.type
+  const workAccidentType = getValueViaPath(
+    formValue,
+    'workAccident.type',
+  ) as WorkAccidentTypeEnum
   return (
     workAccidentType === WorkAccidentTypeEnum.AGRICULTURE &&
     isWorkAccident(formValue)
