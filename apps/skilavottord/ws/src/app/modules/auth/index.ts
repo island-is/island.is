a6125dba47e6f49cc73001f3871fe0f13d19d689
Sigurgeir Gudmundsson <16959841+essgje@users.noreply.github.com>
2021-12-15 10:33:27 +0000

    feat(skilavottord): Access Control API (#5955)
    Main has errors, thus using admin privileges to merge. No additional errors come from this PR.

--- apps/skilavottord/ws/src/app/modules/auth/index.ts
+++ apps/skilavottord/ws/src/app/modules/auth/index.ts
@@ -1,6 +1,5 @@
 export { AuthModule } from './auth.module'
-export { AuthService } from './auth.service'
-export type { AuthUser } from './auth.types'
+export type { AuthUser, User } from './auth.types'
 export { Role } from './auth.types'
 export { Authorize } from './auth.guard'
 export { CurrentUser } from './auth.decorator'
