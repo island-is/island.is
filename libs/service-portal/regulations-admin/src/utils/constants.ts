67a4cb88fc9bf214f807b550f6e3c720d18d993a
mar <mar.nospam@anomy.net>
2021-12-15 14:10:24 +0000

    feat: Add typing info for regulationTypes (options) array

--- libs/service-portal/regulations-admin/src/utils/constants.ts
+++ libs/service-portal/regulations-admin/src/utils/constants.ts
@@ -1,4 +1,13 @@
-export const regulationTypes = [
+import { RegulationType } from '@island.is/regulations'
+
+/** List of labelled regulation types.
+ *
+ * Fit for use in Select boxes
+ */
+export const regulationTypes: Array<{
+  label: string
+  value: RegulationType
+}> = [
   { label: 'Stofnreglugerð', value: 'base' },
   { label: 'Breytingareglugerð', value: 'amending' },
 ]
