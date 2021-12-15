00ced39426658ec32a32d9dae415043f0ebff44f
Darri Steinn Konradsson <darrikonn@gmail.com>
2021-12-15 14:20:18 +0000

    Move implicit deps to per project

--- libs/shared/connected/src/index.ts
+++ libs/shared/connected/src/index.ts
@@ -1,3 +1,1 @@
-export { CompanyList } from './lib/skilavottord/CompanyList/CompanyList'
-export { CompanyListConnected } from './lib/skilavottord/CompanyList/CompanyListConnected'
 export { GeneralPetitionLists } from './lib/generalPetition/GeneralPetitionLists/GeneralPetitionLists'
