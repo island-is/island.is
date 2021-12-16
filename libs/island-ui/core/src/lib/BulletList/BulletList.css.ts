6ae3e707514cb0a033bbe1d6ba8d65aaccf93e46
Kristján Albert <kal@kosmosogkaos.is>
2021-12-16 14:32:44 +0000

    fix(island-ui): move global styles from Text (#6020)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/island-ui/core/src/lib/BulletList/BulletList.css.ts
+++ libs/island-ui/core/src/lib/BulletList/BulletList.css.ts
@@ -1,9 +1,11 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 import { theme } from '@island.is/island-ui/theme'
 
 export const container = style({
   counterReset: 'section',
 })
+
+export const content = style({})
 
 export const bullet = style({
   display: 'inline-block',
@@ -36,4 +38,18 @@ export const icon = style({
     backgroundColor: theme.color.red400,
     borderRadius: '50%',
   },
+})
+
+globalStyle(`${content} a`, {
+  cursor: 'pointer',
+  color: theme.color.blue400,
+  textDecoration: 'none',
+  boxShadow: `inset 0 -2px 0 0 currentColor`,
+  transition: 'color .2s, box-shadow .2s',
+  paddingBottom: 4,
+})
+
+globalStyle(`${content} a:hover`, {
+  color: theme.color.blueberry400,
+  boxShadow: `inset 0 -2px 0 0 ${theme.color.blueberry400}`,
 })
