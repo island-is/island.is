0e27e1a098de1ef9f11652461d4bb8a2b8ebea64
Alexandra Diljá <alexandrab18@ru.is>
2021-12-16 09:46:39 +0000

    restoring migartion

--- apps/services/endorsements/api/migrations/20210811145737-endorsement-list-metadata-sync.js
+++ apps/services/endorsements/api/migrations/20210811145737-endorsement-list-metadata-sync.js
@@ -18,7 +18,61 @@ module.exports = {
         {
           field: 'fullName',
         },
+        {
+          field: 'signedTags',
+        },
+        {
+          field: 'address',
+          keepUpToDate: true,
+        },
       ]
+
+      // add correct metadata fields for existing partyLetter lists
+      await queryInterface.bulkUpdate(
+        'endorsement_list',
+        {
+          endorsement_metadata: JSON.stringify(newMetadataFields),
+        },
+        {
+          tags: {
+            [Sequelize.Op.overlap]: queryInterface.sequelize.cast(
+              ['partyLetter2021'],
+              'varchar[]',
+            ),
+          },
+        },
+        { transaction: t },
+      )
+
+      // add correct metadata fields for existing partyApplication lists
+      await queryInterface.bulkUpdate(
+        'endorsement_list',
+        {
+          endorsement_metadata: JSON.stringify([
+            ...newMetadataFields,
+            {
+              field: 'voterRegion',
+              keepUpToDate: true,
+            },
+          ]),
+        },
+        {
+          tags: {
+            [Sequelize.Op.overlap]: queryInterface.sequelize.cast(
+              [
+                'partyApplicationNordausturkjordaemi2021',
+                'partyApplicationNordvesturkjordaemi2021',
+                'partyApplicationReykjavikurkjordaemiNordur2021',
+                'partyApplicationReykjavikurkjordaemiSudur2021',
+                'partyApplicationSudurkjordaemi2021',
+                'partyApplicationSudvesturkjordaemi2021',
+              ],
+              'varchar[]',
+            ),
+          },
+        },
+        { transaction: t },
+      )
     })
   },
 
