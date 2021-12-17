be63629fb7d2b2d951f58bef61e1cfbf5a2afa0b
arnorhs <arnor@kosmosogkaos.is>
2021-12-17 11:11:36 +0000

    Reverting last couple of changes

--- apps/services/user-notification/test/globalSetup.ts
+++ apps/services/user-notification/test/globalSetup.ts
@@ -1,11 +1,13 @@
-import { GenericContainer } from 'testcontainers'
+import { GenericContainer, Wait } from 'testcontainers'
 
 export default async () => {
   const lc = await new GenericContainer(
-    `public.ecr.aws/s4w6t4b6/localstack/localstack:0.11.1`,
+    `${process.env.DOCKER_REGISTRY ?? ''}localstack/localstack:0.13.1`,
   )
     .withEnv('SERVICES', 'sqs')
+    .withEnv('DEBUG', '1')
     .withExposedPorts(4566)
+    .withStartupTimeout(120000)
     .start()
 
   ;(global as any).__localstack__ = lc
