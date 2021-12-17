df015180e58878c03c0e6dcd0e8cb1fee443c877
arnorhs <arnor@kosmosogkaos.is>
2021-12-17 14:35:25 +0000

    adding time

--- libs/message-queue/src/index.ts
+++ libs/message-queue/src/index.ts
@@ -1,3 +1,4 @@
+import * as time from './lib/nest/time'
 export { default as Channel } from './lib/channel'
 export { default } from './lib/message-queue'
 
@@ -7,4 +8,4 @@ export * from './lib/nest/worker.service'
 export * from './lib/nest/types'
 export * from './lib/nest/decorators'
 export * from './lib/nest/utils'
-export * as time from './lib/nest/time'
+export { time }
