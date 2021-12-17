9a4f588613b7ff494b1dbc2ba42f4f6b80b5a3ef
Fiddi <fridrik@programm.is>
2021-12-17 16:38:33 +0000

    Fixing test after merge

--- apps/services/personal-representative-public/src/app/modules/personalRepresentativeRights/e2e/personalRepresentativeRights.spec.ts
+++ apps/services/personal-representative-public/src/app/modules/personalRepresentativeRights/e2e/personalRepresentativeRights.spec.ts
@@ -176,6 +176,7 @@ describe('PersonalRepresentativePermissionController', () => {
       const simpleAccessData: PersonalRepresentativeAccessDTO = {
         nationalIdPersonalRepresentative: '1234567890',
         nationalIdRepresentedPerson: '1234567891',
+        serviceProvider: 'testServiceProvider'
       }
       // Test get personal rep
       const response = await server
