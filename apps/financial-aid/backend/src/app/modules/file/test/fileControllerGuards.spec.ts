33f13a7257867e3c21dfbd133a66fe6ab720fdd2
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2021-12-20 14:30:52 +0000

    boom

--- apps/financial-aid/backend/src/app/modules/file/test/fileControllerGuards.spec.ts
+++ apps/financial-aid/backend/src/app/modules/file/test/fileControllerGuards.spec.ts
@@ -1,5 +1,6 @@
 import { IdsUserGuard } from '@island.is/auth-nest-tools'
 import { CanActivate } from '@nestjs/common'
+import { RolesGuard } from '../../../guards/roles.guard'
 
 import { FileController } from '../file.controller'
 
@@ -24,6 +25,90 @@ describe('FileController - guards', () => {
 
     it('should have IdsUserGuard as quard 1', () => {
       expect(guard).toBeInstanceOf(IdsUserGuard)
+    })
+  })
+})
+
+describe('FileController - Creates a new signed url guards', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let guards: any[]
+
+  beforeEach(() => {
+    guards = Reflect.getMetadata(
+      '__guards__',
+      FileController.prototype.createSignedUrl,
+    )
+  })
+
+  it('should have one guards', () => {
+    expect(guards).toHaveLength(1)
+  })
+
+  describe('RolesGuard', () => {
+    let guard: CanActivate
+
+    beforeEach(() => {
+      guard = new guards[0]()
+    })
+
+    it('should have RolesGuard as quard 1', () => {
+      expect(guard).toBeInstanceOf(RolesGuard)
+    })
+  })
+})
+
+describe('FileController - Creates a new signed url guards', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let guards: any[]
+
+  beforeEach(() => {
+    guards = Reflect.getMetadata(
+      '__guards__',
+      FileController.prototype.createSignedUrlForId,
+    )
+  })
+
+  it('should have one guards', () => {
+    expect(guards).toHaveLength(1)
+  })
+
+  describe('RolesGuard', () => {
+    let guard: CanActivate
+
+    beforeEach(() => {
+      guard = new guards[0]()
+    })
+
+    it('should have RolesGuard as quard 1', () => {
+      expect(guard).toBeInstanceOf(RolesGuard)
+    })
+  })
+})
+
+describe('FileController - Creates a new signed url guards', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let guards: any[]
+
+  beforeEach(() => {
+    guards = Reflect.getMetadata(
+      '__guards__',
+      FileController.prototype.createFiles,
+    )
+  })
+
+  it('should have one guards', () => {
+    expect(guards).toHaveLength(1)
+  })
+
+  describe('RolesGuard', () => {
+    let guard: CanActivate
+
+    beforeEach(() => {
+      guard = new guards[0]()
+    })
+
+    it('should have RolesGuard as quard 1', () => {
+      expect(guard).toBeInstanceOf(RolesGuard)
     })
   })
 })
