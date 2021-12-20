edc52bc38d222d83e3f37ea322a13e287adf410f
Ívar Oddsson <oddsson@Ivars-iMac.local>
2021-12-20 09:13:14 +0000

    Remove REMOTE_SESSION from session_arrangements in DB

---
+++ apps/judicial-system/backend/migrations/20211220090749-update-case.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const replaceEnum = require('sequelize-replace-enum-postgres').default
+
+module.exports = {
+  up: (queryInterface) => {
+    // replaceEnum does not support transactions
+    return replaceEnum({
+      queryInterface,
+      tableName: 'case',
+      columnName: 'session_arrangements',
+      newValues: [
+        'ALL_PRESENT',
+        'ALL_PRESENT_SPOKESPERSON',
+        'PROSECUTOR_PRESENT',
+      ],
+      enumName: 'enum_case_session_arrangements',
+    })
+  },
+
+  down: (queryInterface) => {
+    // replaceEnum does not support transactions
+    return replaceEnum({
+      queryInterface,
+      tableName: 'case',
+      columnName: 'session_arrangements',
+      newValues: [
+        'ALL_PRESENT',
+        'ALL_PRESENT_SPOKESPERSON',
+        'PROSECUTOR_PRESENT',
+        'REMOTE_SESSION',
+      ],
+      enumName: 'enum_case_session_arrangements',
+    })
+  },
+}
