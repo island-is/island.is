0d9b29784f14542897054e0fde67338665b49f86
Ásdís Erna Guðmundsdóttir <disa.erna@gmail.com>
2021-12-20 13:57:39 +0000

    feat: add path header to accessDenied

--- libs/service-portal/core/src/screens/AccessDenied/AccessDenied.tsx
+++ libs/service-portal/core/src/screens/AccessDenied/AccessDenied.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { m, ServicePortalModuleComponent } from '@island.is/service-portal/core'
-import { MessageDescriptor } from 'react-intl'
 import { useLocale } from '@island.is/localization'
 import { ErrorScreen } from '../ErrorScreen/ErrorScreen'
-interface Props {
-  title?: string | MessageDescriptor
-}
+import { useLocation } from 'react-router-dom'
+import { servicePortalMasterNavigation } from '@island.is/service-portal/core'
 
-export const AccessDenied: ServicePortalModuleComponent<Props> = ({
-  title,
-}) => {
+export const AccessDenied: ServicePortalModuleComponent = () => {
   const { formatMessage } = useLocale()
-
+  const { pathname } = useLocation()
+  const navItem = servicePortalMasterNavigation[0].children?.find(
+    (x) => x.path === pathname,
+  )
   return (
     <ErrorScreen
       tag={formatMessage(m.accessDenied)}
       tagVariant="red"
-      title={formatMessage(title || m.accessDenied)}
+      title={
+        (navItem && formatMessage(navItem?.name)) ||
+        formatMessage(m.accessDenied).toString()
+      }
       figure="./assets/images/hourglass.svg"
     >
       {formatMessage(m.accessDeniedText)}
