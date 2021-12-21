8594b1ee9687dfbf7d5fe787ab3ae4da5ad7181a
obmagnusson <obmagnusson@gmail.com>
2021-12-21 12:35:58 +0000

    test boilerplate

--- apps/application-system/api/src/app/modules/application/application-lifecycle.spec.ts
+++ apps/application-system/api/src/app/modules/application/application-lifecycle.spec.ts
@@ -1,16 +1,38 @@
-import { Test } from '@nestjs/testing'
+import { TestApp } from '@island.is/testing/nest'
+import request from 'supertest'
+import { createCurrentUser } from '@island.is/testing/fixtures'
+import { IdsUserGuard, MockAuthGuard } from '@island.is/auth-nest-tools'
+import { ApplicationScope } from '@island.is/auth/scopes'
+import { INestApplication } from '@nestjs/common'
+import { setup } from '../../../../test/setup'
 import { ApplicationLifeCycleService } from './application-lifecycle.service'
 
-describe('PublicFlightController', () => {
-  let lifeCycleService: ApplicationLifeCycleService
+const currentUser = createCurrentUser()
+let app: INestApplication
+const nationalId = '1234564321'
+let server: request.SuperTest<request.Test>
+let lifeCycleService: ApplicationLifeCycleService
 
-  beforeEach(async () => {
-    const moduleRef = await Test.createTestingModule({
-      providers: [ApplicationLifeCycleService],
-    }).compile()
+beforeAll(async () => {
+  app = await setup({
+    override: (builder) =>
+      builder.overrideGuard(IdsUserGuard).useValue(
+        new MockAuthGuard({
+          nationalId,
+          scope: [ApplicationScope.read, ApplicationScope.write],
+        }),
+      ),
+  })
 
-    lifeCycleService = moduleRef.get<ApplicationLifeCycleService>(
-      ApplicationLifeCycleService,
-    )
+  lifeCycleService = app.get<ApplicationLifeCycleService>(
+    ApplicationLifeCycleService,
+  )
+  server = request(app.getHttpServer())
+})
+describe('Example functional test', () => {
+  let app: TestApp
+
+  afterAll(async () => {
+    await app.cleanUp()
   })
 })
