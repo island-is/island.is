4df5d96d6303aab1eeb622ef87c79b10e26b6abe
Ívar Oddsson <oddsson@Ivars-iMac.local>
2021-12-21 08:30:20 +0000

    Remove unused code from migration

--- apps/judicial-system/backend/migrations/20211220090749-update-case.js
+++ apps/judicial-system/backend/migrations/20211220090749-update-case.js
@@ -5,25 +5,23 @@ const replaceEnum = require('sequelize-replace-enum-postgres').default
 module.exports = {
   up: (queryInterface) => {
     // replaceEnum does not support transactions
-    return queryInterface.sequelize.transaction(() =>
-      queryInterface.sequelize
-        .query(
-          `UPDATE "case" set session_arrangements = 'PROSECUTOR_PRESENT' WHERE session_arrangements = 'REMOTE_SESSION'`,
-        )
-        .then(() => {
-          return replaceEnum({
-            queryInterface,
-            tableName: 'case',
-            columnName: 'session_arrangements',
-            newValues: [
-              'ALL_PRESENT',
-              'ALL_PRESENT_SPOKESPERSON',
-              'PROSECUTOR_PRESENT',
-            ],
-            enumName: 'enum_case_session_arrangements',
-          })
-        }),
-    )
+    return queryInterface.sequelize
+      .query(
+        `UPDATE "case" set session_arrangements = 'PROSECUTOR_PRESENT' WHERE session_arrangements = 'REMOTE_SESSION'`,
+      )
+      .then(() => {
+        return replaceEnum({
+          queryInterface,
+          tableName: 'case',
+          columnName: 'session_arrangements',
+          newValues: [
+            'ALL_PRESENT',
+            'ALL_PRESENT_SPOKESPERSON',
+            'PROSECUTOR_PRESENT',
+          ],
+          enumName: 'enum_case_session_arrangements',
+        })
+      })
   },
 
   down: (queryInterface) => {
