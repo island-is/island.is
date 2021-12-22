dbdb7be8a3ef03b375f227d46974b2a9cc72c641
arnorhs <arnorhs@gmail.com>
2021-12-22 11:57:31 +0000

    chore(services-user-notification): updating image name and adding express (#6077)
    * chore(services-user-notification): updating image name
    
    * docker express
    
    * charts
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/user-notification/infra/user-notification.ts
+++ apps/services/user-notification/infra/user-notification.ts
@@ -5,7 +5,7 @@ const DEAD_LETTER_QUEUE_NAME = 'user-notification-failure'
 
 export const userNotificationServiceSetup = (): ServiceBuilder<'user-notification'> =>
   service('user-notification')
-    .image('user-notification')
+    .image('services-user-notification')
     .namespace('user-notification')
     .serviceAccount('user-notification')
     .command('node')
@@ -19,7 +19,7 @@ export const userNotificationServiceSetup = (): ServiceBuilder<'user-notificatio
 
 export const userNotificationWorkerSetup = (): ServiceBuilder<'user-notification-worker'> =>
   service('user-notification-worker')
-    .image('user-notification-worker')
+    .image('services-user-notification')
     .namespace('user-notification')
     .serviceAccount('user-notification-worker')
     .command('node')
