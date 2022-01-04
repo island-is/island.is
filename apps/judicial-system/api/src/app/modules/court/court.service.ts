5aaf42689492f3bb79d50c59607ac86e0c0d4ecc
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-01-04 12:10:13 +0000

    Sets the appropriate court system subtype for "Rannsókn á rafrænum gögnum"

--- apps/judicial-system/api/src/app/modules/court/court.service.ts
+++ apps/judicial-system/api/src/app/modules/court/court.service.ts
@@ -37,7 +37,7 @@ const subTypes = {
   // 'Skýrslutaka fyrir dómi',
   TELECOMMUNICATIONS: 'Upplýsingar um fjarskiptasamskipti',
   INTERNET_USAGE: 'Upplýsingar um vefnotkun',
-  ELECTRONIC_DATA_DISCOVERY_INVESTIGATION: 'Rannsókn á rafrænum gögnum',
+  ELECTRONIC_DATA_DISCOVERY_INVESTIGATION: 'Annað', // This subtype is not in the court system
 }
 
 @Injectable()
