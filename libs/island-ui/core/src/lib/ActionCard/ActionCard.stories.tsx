eeb9c9674789834aa9c7e41b313da4fd69f36bce
Ásdís Erna Guðmundsdóttir <disa.erna@gmail.com>
2022-01-04 13:58:28 +0000

    feat: add avatar to stories

--- libs/island-ui/core/src/lib/ActionCard/ActionCard.stories.tsx
+++ libs/island-ui/core/src/lib/ActionCard/ActionCard.stories.tsx
@@ -111,7 +111,7 @@ export const ApplicationCardCompletedVariant = () => (
     heading="Parental Leave"
     tag={{
       label: 'Completed',
-      variant: 'mint',
+      variant: 'blue',
       outlined: false,
     }}
     text="Your application is in progress. Waiting for VMST approval."
@@ -124,7 +124,20 @@ export const ApplicationCardCompletedVariant = () => (
     progressMeter={{
       active: true,
       progress: 1,
-      variant: 'mint',
+      variant: 'blue',
+    }}
+  />
+)
+
+export const Avatar = () => (
+  <ActionCard
+    avatar
+    heading="Jón Jónsson"
+    text="Kennitala: 010100-0100"
+    cta={{
+      label: 'Skoða upplýsingar',
+      variant: 'text',
+      size: 'small',
     }}
   />
 )
