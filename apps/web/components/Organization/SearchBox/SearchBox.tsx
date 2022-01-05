5500cba179aa1bd7a7ed0b3abf68d7d35820f247
RunarVestmann <runarvestmann@gmail.com>
2022-01-05 16:24:45 +0000

    Show loading icon while typing, enabled english search and improved keyboard navigation (tab, enter and arrow keys)

--- apps/web/components/Organization/SearchBox/SearchBox.tsx
+++ apps/web/components/Organization/SearchBox/SearchBox.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Box, AsyncSearch, Text, Button } from '@island.is/island-ui/core'
+import {
+  Box,
+  AsyncSearch,
+  Text,
+  Button,
+  AsyncSearchOption,
+} from '@island.is/island-ui/core'
 import { useLazyQuery } from '@apollo/client'
 import {
   Query,
@@ -10,6 +16,10 @@ import { GET_ORGANIZATION_SERVICES_QUERY } from '@island.is/web/screens/queries'
 import { LinkType, useLinkResolver } from '@island.is/web/hooks/useLinkResolver'
 import { useRouter } from 'next/router'
 import { useDebounce } from 'react-use'
+
+interface AsyncSearchOptionWithIsArticleField extends AsyncSearchOption {
+  isArticle: boolean
+}
 
 interface SearchBoxProps {
   organizationPage: Query['getOrganizationPage']
@@ -25,13 +35,16 @@ export const SearchBox = ({
   searchAllText,
 }: SearchBoxProps) => {
   const { linkResolver } = useLinkResolver()
-  const Router = useRouter()
+  const router = useRouter()
 
   const [value, setValue] = useState('')
   const [options, setOptions] = useState([])
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [waitingForNextPageToLoad, setWaitingForNextPageToLoad] = useState(
+    false,
+  )
 
-  const [fetch, { data }] = useLazyQuery<Query, QueryGetArticlesArgs>(
+  const [fetch, { data, loading }] = useLazyQuery<Query, QueryGetArticlesArgs>(
     GET_ORGANIZATION_SERVICES_QUERY,
   )
 
@@ -41,16 +54,15 @@ export const SearchBox = ({
         fetch({
           variables: {
             input: {
-              lang: 'is',
+              lang: router.asPath.includes('/en/') ? 'en' : 'is',
               organization: organizationPage.slug,
               size: 500,
               sort: SortField.Popular,
             },
           },
         })
-
-        setIsLoading(false)
       }
+      setIsLoading(false)
     },
     300,
     [value],
@@ -59,16 +71,17 @@ export const SearchBox = ({
   const items = data?.getArticles?.map((item, index) => ({
     label: item.title,
     value: item.slug,
+    isArticle: true,
     component: ({ active }) => {
       return (
         <Box
-          key={index}
+          key={`article-${item.id ?? ''}-${index}`}
           cursor="pointer"
           outline="none"
           paddingX={2}
           paddingY={1}
           role="button"
-          background={active ? 'white' : 'blue100'}
+          background={active ? 'blue100' : 'white'}
           onClick={() => {
             setOptions([])
           }}
@@ -84,7 +97,35 @@ export const SearchBox = ({
     setOptions([])
   }
 
-  useEffect(() => {
+  const handleOptionSelect = (
+    selectedItem: AsyncSearchOptionWithIsArticleField,
+    value: string,
+  ) => {
+    setOptions([])
+    if (!value) return
+    setWaitingForNextPageToLoad(true)
+
+    let pathname: string
+    let searchAll = false
+
+    if (selectedItem && selectedItem?.isArticle) {
+      const newValue = selectedItem.value
+      pathname = linkResolver('Article' as LinkType, [newValue]).href
+      setValue(selectedItem.label)
+    } else {
+      pathname = linkResolver('search').href
+      searchAll = true
+    }
+
+    router
+      .push({
+        pathname,
+        query: searchAll ? { q: value } : {},
+      })
+      .then(() => window.scrollTo(0, 0))
+  }
+
+  const updateOptions = () => {
     const newOpts = items
       ? items
           .filter(
@@ -101,19 +142,27 @@ export const SearchBox = ({
     setOptions(
       newOpts.length
         ? newOpts.concat({
-            label: 'searchAll',
+            label: value,
             value: '',
-            component: () => (
-              <Box padding={2}>
+            isArticle: false,
+            component: ({ active }) => (
+              <Box
+                padding={2}
+                background={active ? 'blue100' : 'white'}
+                cursor="pointer"
+              >
                 <Button
                   type="button"
                   variant="text"
-                  onClick={() =>
-                    Router.push({
-                      pathname: linkResolver('search').href,
-                      query: { q: value },
-                    })
-                  }
+                  onClick={() => {
+                    setWaitingForNextPageToLoad(true)
+                    router
+                      .push({
+                        pathname: linkResolver('search').href,
+                        query: { q: value },
+                      })
+                      .then(() => window.scrollTo(0, 0))
+                  }}
                 >
                   {searchAllText}
                 </Button>
@@ -122,15 +171,40 @@ export const SearchBox = ({
           })
         : [
             {
+              label: value,
+              value: '',
+              isArticle: false,
               component: () => (
                 <Box padding={2} disabled>
                   <Text as="span">{noResultsText}</Text>
+                  <Box cursor="pointer">
+                    <Button
+                      type="button"
+                      variant="text"
+                      onClick={() => {
+                        setWaitingForNextPageToLoad(true)
+                        router
+                          .push({
+                            pathname: linkResolver('search').href,
+                            query: { q: value },
+                          })
+                          .then(() => window.scrollTo(0, 0))
+                      }}
+                    >
+                      {searchAllText}
+                    </Button>
+                  </Box>
                 </Box>
               ),
             },
           ],
     )
-  }, [value])
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(updateOptions, [value, loading, data])
+
+  const busy = loading || isLoading || waitingForNextPageToLoad
 
   return (
     <Box marginTop={3}>
@@ -140,36 +214,25 @@ export const SearchBox = ({
         key="island-organization"
         placeholder={placeholder}
         options={options}
-        loading={isLoading}
+        loading={busy}
         initialInputValue={''}
         inputValue={value}
-        onInputValueChange={(value) => {
-          setIsLoading(true)
-          setValue(value)
+        onInputValueChange={(newValue) => {
+          setIsLoading(newValue !== value)
+          setValue(newValue ?? '')
         }}
         closeMenuOnSubmit
         onSubmit={(value, selectedOption) => {
-          setOptions([])
-
-          value &&
-            Router.push({
-              pathname: selectedOption
-                ? linkResolver('Article' as LinkType, [selectedOption.value])
-                    .href
-                : linkResolver('search').href,
-              query: { q: value },
-            })
+          handleOptionSelect(
+            selectedOption as AsyncSearchOptionWithIsArticleField,
+            value,
+          )
         }}
         onChange={(i, option) => {
-          setOptions([])
-
-          value &&
-            Router.push({
-              pathname: linkResolver('Article' as LinkType, [
-                option.selectedItem.value,
-              ]).href,
-              query: { q: value.toLowerCase() },
-            })
+          handleOptionSelect(
+            option.selectedItem as AsyncSearchOptionWithIsArticleField,
+            value,
+          )
         }}
       />
     </Box>
