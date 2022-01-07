266648bc423c6856a8cf469e144d4deb03ba5333
Petar Shomov <petar@andes.is>
2022-01-07 14:30:57 +0000

    Revert "a bit better docs"
    This reverts commit ebe542e451be5549072f95982788f47765489f84.

--- apps/external-contracts-tests/test-suites/national-registry.spec.ts
+++ apps/external-contracts-tests/test-suites/national-registry.spec.ts
@@ -1,10 +1,6 @@
-// NOTE: To run this locally, you'll need to portforward xroad service port and set
-// the environment variable "XROAD_BASE_PATH_WITH_ENV=http://localhost:8088/r1/IS-TEST"
-// Example:
-//
-// $ kubectl port-forward svc/socat-xroad 8088:80 -n socat &
-// $ XROAD_BASE_PATH_WITH_ENV=http://localhost:8088/r1/IS-DEV XROAD_CLIENT_ID=IS-DEV/GOV/10000/island-is-client XROAD_TJODSKRA_API_PATH=/SKRA-Protected/Einstaklingar-v1 XROAD_TJODSKRA_MEMBER_CODE=10001 yarn nx run external-contracts-tests:external-test
-
+// NOTE: To run this locally, you'll need to portforward soffia and set
+// the environment variable "SOFFIA_SOAP_URL" to https://localhost:8443
+// kubectl port-forward svc/socat-xroad 8088:80 -n socat
 import { Test, TestingModule } from '@nestjs/testing'
 import {
   createXRoadAPIPath,
