cd420fe83f670e378607c9705dc21be1a2065f6b
arnorhs <arnor@kosmosogkaos.is>
2022-01-07 14:11:35 +0000

    adding infra config for the worker and removing worker's health checks

--- apps/services/user-notification/infra/user-notification.ts
+++ apps/services/user-notification/infra/user-notification.ts
@@ -39,6 +39,19 @@ export const userNotificationWorkerSetup = (): ServiceBuilder<'user-notification
     .env({
       MAIN_QUEUE_NAME,
       DEAD_LETTER_QUEUE_NAME,
+      IDENTITY_SERVER_PATH: {
+        dev: 'https://identity-server.dev01.devland.is',
+        staging: 'https://identity-server.staging01.devland.is',
+        prod: 'https://innskra.island.is',
+      },
     })
-    .liveness('/liveness')
-    .readiness('/liveness')
+    .secrets({
+      GOOGLE_APPLICATION_CREDENTIALS:
+        '/k8s/user-notification/GOOGLE_APPLICATION_CREDENTIALS',
+      SERVICE_USER_PROFILE_BASEPATH:
+        '/k8s/user-notification/SERVICE_USER_PROFILE_BASEPATH',
+      USER_NOTIFICATION_CLIENT_ID:
+        '/k8s/user-notification/USER_NOTIFICATION_CLIENT_ID',
+      USER_NOTIFICATION_CLIENT_SECRET:
+        '/k8s/user-notification/USER_NOTIFICATION_CLIENT_SECRET',
+    })
