87eef292adda677e62a6f04133e3efceb75b6f38
Petar Shomov <petar@andes.is>
2022-01-07 10:35:47 +0000

    switching to using the new NationalRegistry client

--- apps/external-contracts-tests/test-suites/national-registry.spec.ts
+++ apps/external-contracts-tests/test-suites/national-registry.spec.ts
@@ -1,24 +1,52 @@
 // NOTE: To run this locally, you'll need to portforward soffia and set
 // the environment variable "SOFFIA_SOAP_URL" to https://localhost:8443
 // kubectl port-forward svc/socat-soffia 8443:443 -n socat
-import { NationalRegistryApi } from '@island.is/clients/national-registry-v1'
+import { Test, TestingModule } from '@nestjs/testing'
+import {
+  NationalRegistryModule,
+  EinstaklingarApi,
+} from '@island.is/clients/national-registry-v2'
+import {
+  createXRoadAPIPath,
+  XRoadMemberClass,
+} from '@island.is/shared/utils/server'
 
-describe('is.island.external.national', () => {
-  let client: NationalRegistryApi
+import { LoggingModule } from '@island.is/logging'
+
+describe('is.island.external.national', async () => {
+  let client: EinstaklingarApi
+  const XROAD_CLIENT = process.env.XROAD_CLIENT_ID!
   beforeAll(async () => {
-    client = await NationalRegistryApi.instantiateClass({
-      baseSoapUrl: process.env.SOFFIA_SOAP_URL!,
-      user: process.env.SOFFIA_USER!,
-      password: process.env.SOFFIA_PASS!,
-      host: process.env.SOFFIA_HOST_URL!,
-    })
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        LoggingModule,
+        NationalRegistryModule.register({
+          xRoadPath: createXRoadAPIPath(
+            process.env.XROAD_BASE_PATH_WITH_ENV!,
+            XRoadMemberClass.GovernmentInstitution,
+            process.env.XROAD_TJODSKRA_MEMBER_CODE!,
+            process.env.XROAD_TJODSKRA_API_PATH!,
+          ),
+          xRoadClient: XROAD_CLIENT,
+        }),
+      ],
+    }).compile()
+    client = await module.get<EinstaklingarApi>(EinstaklingarApi)
   })
   it('should get user correctly', async () => {
-    const user = await client.getUser('0101302989')
-    expect(user.Fulltnafn).toEqual('Gervimaður Ameríka')
+    const user = await client.einstaklingarGetEinstaklingur({
+      id: '0101302989',
+      xRoadClient: XROAD_CLIENT,
+    })
+    expect(user.fulltNafn).toEqual('Gervimaður Ameríka')
   })
   it('throws error if user is not found', async () => {
-    await expect(client.getUser('0123456789')).rejects.toThrow(
+    await expect(
+      client.einstaklingarGetEinstaklingur({
+        id: '0123456789',
+        xRoadClient: XROAD_CLIENT,
+      }),
+    ).rejects.toThrow(
       'user with nationalId 0123456789 not found in national Registry',
     )
   })
