c02943259ce974a4578939ca19bd51255b56888e
obmagnusson <obmagnusson@gmail.com>
2022-01-11 10:44:04 +0000

    api with sbmit

--- libs/application/template-api-modules/src/lib/modules/templates/data-protection-complaint/data-protection-complaint.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/data-protection-complaint/data-protection-complaint.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+
+import { SharedTemplateApiService } from '../../shared'
+import { TemplateApiModuleActionProps } from '../../../types'
+import { applicationToCaseRequest } from './data-protection-utils'
 import {
   CaseApi,
   TokenMiddleware,
 } from '@island.is/clients/data-protection-complaint'
-import { SharedTemplateApiService } from '../../shared'
-import { TemplateApiModuleActionProps } from '../../../types'
+import { LOGGER_PROVIDER } from '@island.is/logging'
+import type { Logger } from '@island.is/logging'
+import { PdfFileProvider } from './attachments/providers/pdfFileProvider'
+import { ApplicationAttachmentProvider } from './attachments/providers/applicationAttachmentProvider'
 
 @Injectable()
 export class DataProtectionComplaintService {
   constructor(
-    private readonly sharedTemplateAPIService: SharedTemplateApiService,
+    @Inject(LOGGER_PROVIDER) private logger: Logger,
     private readonly caseApi: CaseApi,
     private readonly tokenMiddleware: TokenMiddleware,
+    private readonly applicationAttachmentProvider: ApplicationAttachmentProvider,
+    private readonly pdfFileProvider: PdfFileProvider,
   ) {}
 
   get caseApiWithAuth() {
@@ -19,7 +27,27 @@ export class DataProtectionComplaintService {
   }
 
   async sendApplication({ application }: TemplateApiModuleActionProps) {
-    // Pretend to be doing stuff for a short while
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      const attachments = [
+        ...(await this.pdfFileProvider.getFiles(application, 'kvörtun')),
+        ...(await this.applicationAttachmentProvider.getFiles(
+          ['complaint.documents', 'commissions.documents'],
+          application,
+        )),
+      ]
+
+      const caseRequest = await applicationToCaseRequest(
+        application,
+        attachments,
+      )
+
+      const newCase = await this.caseApiWithAuth.createCase({
+        requestData: caseRequest,
+      })
+    } catch (error) {
+      this.logger.error('Error submitting', error)
+
+      throw new Error('Villa kom kom upp við að senda umsókn')
+    }
   }
 }
--- libs/application/templates/data-protection-complaint/src/index.ts
+++ libs/application/templates/data-protection-complaint/src/index.ts
@@ -4,3 +4,13 @@ export const getDataProviders = () => import('./dataProviders/')
 export const getFields = () => import('./fields/')
 
 export default DataProtectionComplaintTemplate
+export {
+  OnBehalf,
+  SubjectOfComplaint,
+  subjectOfComplaintValueLabelMapper,
+  onBehalfValueLabelMapper,
+  YES,
+  NO,
+  yesNoValueLabelMapper,
+} from './shared'
+export { DataProtectionComplaint } from './lib/dataSchema'
