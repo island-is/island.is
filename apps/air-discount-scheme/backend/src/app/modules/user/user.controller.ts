66a8943662ae4a08d5709261395686b7f583d185
jGeir <juliusgisla@gmail.com>
2022-01-13 16:58:37 +0000

    user controller

--- apps/air-discount-scheme/backend/src/app/modules/user/user.controller.ts
+++ apps/air-discount-scheme/backend/src/app/modules/user/user.controller.ts
@@ -17,15 +17,9 @@ import { UserService } from './user.service'
 import { AirlineUser, User } from './user.model'
 import { DiscountService } from '../discount'
 import { FlightService } from '../flight'
-import {
-  CurrentUser,
-  IdsUserGuard,
-  Scopes,
-  ScopesGuard,
-} from '@island.is/auth-nest-tools'
+import { CurrentUser, IdsUserGuard, Scopes, ScopesGuard } from '@island.is/auth-nest-tools'
 import { NationalRegistryXRoadService } from '@island.is/api/domains/national-registry-x-road'
 import { User as AuthUser } from '@island.is/auth-nest-tools'
-import { Person } from '../nationalRegistryV2/person.model'
 
 @ApiTags('Users')
 @Controller('api/public')
@@ -66,7 +60,6 @@ export class PrivateUserController {
   constructor(
     private readonly flightService: FlightService,
     private readonly userService: UserService,
-    private thjodskraXroad: NationalRegistryXRoadService,
   ) {}
 
   @Get('users/:nationalId/relations')
@@ -75,37 +68,14 @@ export class PrivateUserController {
     @Param() params: GetUserRelationsParams,
     @CurrentUser() user: AuthUser,
   ): Promise<User[]> {
-    console.log('parent')
-    console.log(user.nationalId)
-    const person = await this.thjodskraXroad.getNationalRegistryPerson(
-      user,
-      user.nationalId,
-    )
-
-    console.log(person)
-    const relations = await this.thjodskraXroad.getChildrenCustodyInformation(
-      user,
-      user.nationalId,
-    )
-    console.log('children')
-    console.log(relations)
-    console.log('child 0')
-    console.log(relations[0])
-    console.log('')
-    // const relations = await this.userService.getRelations(params.nationalId)
-    // const users = await Promise.all([
-    //   this.userService.getUserInfoByNationalId(params.nationalId),
-    //   ...relations.map((nationalId) =>
-    //     this.userService.getUserInfoByNationalId(nationalId),
-    //   ),
-    // ])
-    // const users = await Promise.all([
-    //   this.thjodskraXroad.getNationalRegistryPerson(user, user.nationalId),
-    //   ...relations.map((nationalId) =>
-    //     this.thjodskraXroad.getNationalRegistryPerson({scope: user.scope, authorization: user.authorization, client: user.client} as AuthUser, nationalId as any)
-    //     )
-    // ])
-    // return users.filter((user) => user) as Person[]
-    return {} as User[]
+    // this is WIP
+    const relations = await this.userService.getRelations(params.nationalId)
+    const users = await Promise.all([
+      this.userService.getUserInfoByNationalId(params.nationalId),
+      ...relations.map((nationalId) =>
+        this.userService.getUserInfoByNationalId(nationalId),
+      ),
+    ])
+    return users.filter((user) => user) as User[]
   }
 }
