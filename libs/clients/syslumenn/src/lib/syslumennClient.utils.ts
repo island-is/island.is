966f62b7c14599ac05b8035492b0753c56ce06eb
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2022-01-13 13:29:23 +0000

    feat(web): Syslumenn auctions, show respondents and petitioners for ships and real estate (#6244)
    * Added another field to query and showed the respondents and petitioners for other auctions than real estate auctions
    
    * Changed how we render the where the auction takes place
    
    * Change how we render repondents
    
    * Only show respondents and petitioners for ships and real estate
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/clients/syslumenn/src/lib/syslumennClient.utils.ts
+++ libs/clients/syslumenn/src/lib/syslumennClient.utils.ts
@@ -70,7 +70,7 @@ export const mapSyslumennAuction = (auction: Uppbod): SyslumennAuction => ({
   office: auction.embaetti ?? '',
   location: auction.starfsstod ?? '',
   auctionType: auction.tegund ?? '',
-  lotType: auction.andlag ?? '',
+  lotType: auction?.andlag?.trim() ?? '',
   lotName: auction.andlagHeiti ?? '',
   lotId: auction.fastanumer ?? '',
   lotItems: auction.lausafjarmunir ?? '',
@@ -78,6 +78,7 @@ export const mapSyslumennAuction = (auction: Uppbod): SyslumennAuction => ({
   auctionTime: auction.klukkan ?? '',
   petitioners: auction.gerdarbeidendur ?? '',
   respondent: auction.gerdartholar ?? '',
+  auctionTakesPlaceAt: auction.uppbodStadur ?? '',
 })
 
 export const mapOperatingLicense = (
