2ae87cc805dbbb4f05b3b284c080559bb9a144fa
jGeir <juliusgisla@gmail.com>
2022-01-15 12:33:18 +0000

    format

--- apps/air-discount-scheme/backend/src/app/modules/common/auth.guard.ts
+++ apps/air-discount-scheme/backend/src/app/modules/common/auth.guard.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-} from '@nestjs/common'
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common'
 
 import { environment } from '../../../environments'
 import { HttpRequest } from '../../app.types'
