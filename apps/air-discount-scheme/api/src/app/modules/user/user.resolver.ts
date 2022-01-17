8e1110cde3ae3ea2e9c3693a5e72a9374a6883d3
jGeir <juliusgisla@gmail.com>
2022-01-17 14:38:18 +0000

    refactor

--- apps/air-discount-scheme/api/src/app/modules/user/user.resolver.ts
+++ apps/air-discount-scheme/api/src/app/modules/user/user.resolver.ts
@@ -23,7 +23,7 @@ export class UserResolver {
   @Query(() => User, { nullable: true })
   async user(@CurrentUser() user: AuthUser): Promise<User | undefined> {
     if (!user) {
-      return null as User
+      return undefined
     }
     user.role = getRole(user)
     return user as User
