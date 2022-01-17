db5ece6b7a269dc0cf41453cfc5cb0a84aef3866
RikkiAbb <rikardarnar@gmail.com>
2022-01-17 15:13:06 +0000

    Removes console log

--- libs/application/templates/financial-aid/src/dataProviders/VeitaProvider.ts
+++ libs/application/templates/financial-aid/src/dataProviders/VeitaProvider.ts
@@ -24,8 +24,6 @@ export class VeitaProvider extends BasicDataProvider {
         const returnObject =
           response.data.hasUserFinancialAidApplicationForCurrentPeriod
 
-        console.log('returnObj', returnObject)
-
         return Promise.resolve({ currentApplicationId: returnObject })
       })
       .catch((error) => {
