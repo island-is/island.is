9d0a84185580b2a57baf6393f88563b0c46e1b51
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-01-18 16:40:38 +0100

    adding test to utils

--- libs/application/templates/financial-aid/src/lib/utils.ts
+++ libs/application/templates/financial-aid/src/lib/utils.ts
@@ -1,4 +1,8 @@
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+
 const emailRegex = /^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$/i
 export const isValidEmail = (value: string) => emailRegex.test(value)
-export const isValidPhone = (value: string) =>
-  value !== undefined && value.length === 7
+export const isValidPhone = (value: string) => {
+  const phone = parsePhoneNumberFromString(value, 'IS')
+  return Boolean(phone && phone.isValid())
+}
--- libs/application/templates/financial-aid/test/utils.spec.ts
+++ libs/application/templates/financial-aid/test/utils.spec.ts
@@ -1,4 +1,4 @@
-import { isValidEmail } from '../src/lib/utils'
+import { isValidEmail, isValidPhone } from '../src/lib/utils'
 
 describe('Utilts test', () => {
   describe('is email valid test', () => {
@@ -6,6 +6,72 @@ describe('Utilts test', () => {
       const notValidEmail = isValidEmail('test')
 
       expect(notValidEmail).toEqual(false)
+    })
+  })
+  describe('is email valid test', () => {
+    test('should return that email is not valid', () => {
+      const notValidEmail = isValidEmail('test@')
+
+      expect(notValidEmail).toEqual(false)
+    })
+  })
+
+  describe('is email valid test', () => {
+    test('should return that email is not valid', () => {
+      const notValidEmail = isValidEmail('test@is')
+
+      expect(notValidEmail).toEqual(false)
+    })
+  })
+  describe('is email valid test', () => {
+    test('should return that email is valid', () => {
+      const notValidEmail = isValidEmail('test@is.is')
+
+      expect(notValidEmail).toEqual(true)
+    })
+  })
+
+  describe('is email valid test', () => {
+    test('should return that email is valid', () => {
+      const notValidEmail = isValidEmail('test@is.is')
+
+      expect(notValidEmail).toEqual(true)
+    })
+  })
+
+  describe('is phone valid test', () => {
+    test('should return that phone is not valid', () => {
+      const notValidEmail = isValidPhone('test')
+
+      expect(notValidEmail).toEqual(false)
+    })
+  })
+
+  describe('is phone valid test', () => {
+    test('should return that phone is not valid', () => {
+      const notValidEmail = isValidPhone('+0001111')
+
+      expect(notValidEmail).toEqual(false)
+    })
+  })
+
+  describe('is phone valid test', () => {
+    test('should return that phone is not valid', () => {
+      const notValidEmail = isValidPhone('+354 000 1111')
+      expect(notValidEmail).toEqual(false)
+    })
+  })
+  describe('is phone valid test', () => {
+    test('should return that phone is valid', () => {
+      const notValidEmail = isValidPhone('0000000')
+      expect(notValidEmail).toEqual(false)
+    })
+  })
+
+  describe('is phone valid test', () => {
+    test('should return that phone is valid', () => {
+      const notValidEmail = isValidPhone('5677977')
+      expect(notValidEmail).toEqual(true)
     })
   })
 })
