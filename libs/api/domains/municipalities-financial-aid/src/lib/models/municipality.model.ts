596f4909464a47a0b30c3233f8a2e7b386699d1a
RikkiAbb <rikardarnar@gmail.com>
2022-01-18 16:04:00 +0000

    Fetches data for municipalities

---
+++ libs/api/domains/municipalities-financial-aid/src/lib/models/municipality.model.ts
@@ -0,0 +1,33 @@
+import { Field, ObjectType, ID } from '@nestjs/graphql'
+
+import { AidModel } from './aid.model'
+
+@ObjectType()
+export class MunicipalityModel {
+  @Field(() => ID)
+  readonly id!: string
+
+  @Field()
+  readonly name!: string
+
+  @Field()
+  readonly active!: boolean
+
+  @Field({ nullable: true })
+  readonly homepage?: string
+
+  @Field()
+  readonly municipalityId!: string
+
+  @Field()
+  readonly individualAid!: AidModel
+
+  @Field()
+  readonly cohabitationAid!: AidModel
+
+  @Field({ nullable: true })
+  readonly email?: string
+
+  @Field({ nullable: true })
+  readonly rulesHomepage?: string
+}
--- libs/api/domains/municipalities-financial-aid/src/lib/municipalitiesFinancialAid.resolver.ts
+++ libs/api/domains/municipalities-financial-aid/src/lib/municipalitiesFinancialAid.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 import type { User } from '@island.is/auth-nest-tools'
 import {
   IdsUserGuard,
@@ -6,8 +6,12 @@ import {
   CurrentUser,
 } from '@island.is/auth-nest-tools'
 import { UseGuards } from '@nestjs/common'
+import type { MunicipalityControllerGetByIdRequest } from '@island.is/clients/municipalities-financial-aid'
+import type { Municipality } from '@island.is/financial-aid/shared/lib'
 
 import { MunicipalitiesFinancialAidService } from './municipalitiesFinancialAid.service'
+import { MunicipalityModel } from './models/municipality.model'
+import { MunicipalityQueryInput } from './models/municipality.input'
 
 @UseGuards(IdsUserGuard, ScopesGuard)
 @Resolver()
@@ -23,6 +27,18 @@ export class MunicipalitiesFinancialAidResolver {
     return await this.municipalitiesFinancialAidService.municipalitiesFinancialAidCurrentApplication(
       user,
       user.nationalId,
+    )
+  }
+
+  @Query(() => MunicipalityModel, { nullable: true })
+  async municipalityInfoForFinancialAid(
+    @Args('input', { type: () => MunicipalityQueryInput })
+    input: MunicipalityQueryInput,
+    @CurrentUser() user: User,
+  ): Promise<MunicipalityModel> {
+    return await this.municipalitiesFinancialAidService.municipalityInfoForFinancialAId(
+      user,
+      input,
     )
   }
 }
