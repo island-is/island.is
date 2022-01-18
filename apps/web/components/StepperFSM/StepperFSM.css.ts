5750e177a8b08daebf4ad20b1707a9c82e36f86b
RunarVestmann <runarvestmann@gmail.com>
2022-01-18 14:51:19 +0000

    Changed how the links look and also made sure that nothing is selected when the url changes

--- apps/web/components/StepperFSM/StepperFSM.css.ts
+++ apps/web/components/StepperFSM/StepperFSM.css.ts
@@ -3,3 +3,9 @@ import { style } from '@vanilla-extract/css'
 export const container = style({
   minHeight: 400,
 })
+
+export const answerRowContainer = style({
+  display: 'grid',
+  gridTemplateColumns: '7fr 2fr 1fr',
+  borderBottom: '1px solid lightgrey',
+})
