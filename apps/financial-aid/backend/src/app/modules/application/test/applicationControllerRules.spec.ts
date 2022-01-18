fda393e986f3b08228c26e9ed4d4d478a4302344
RikkiAbb <rikardarnar@gmail.com>
2022-01-18 15:42:19 +0000

    Removes outdated tests

--- apps/financial-aid/backend/src/app/modules/application/test/applicationControllerRules.spec.ts
+++ apps/financial-aid/backend/src/app/modules/application/test/applicationControllerRules.spec.ts
@@ -1,24 +1,6 @@
 import { RolesRule, StaffRole } from '@island.is/financial-aid/shared/lib'
 import { ApplicationController } from '../application.controller'
 
-describe('ApplicationController - Checks if user has a current application for this period rules', () => {
-  let rules: any[]
-
-  beforeEach(() => {
-    rules = Reflect.getMetadata(
-      'roles-rules',
-      ApplicationController.prototype.getCurrentApplication,
-    )
-  })
-  it('should have one rule', () => {
-    expect(rules).toHaveLength(1)
-  })
-
-  it('should give permission to applicant', () => {
-    expect(rules).toContain(RolesRule.OSK)
-  })
-})
-
 describe('ApplicationController - Searches for application by nationalId rules', () => {
   describe('ApplicationController - findApplication roles rules', () => {
     let rules: any[]
