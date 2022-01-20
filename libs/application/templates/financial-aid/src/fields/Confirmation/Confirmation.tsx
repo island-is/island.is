00d00fd042853326e4845299b44ad7c64af8a59c
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-01-20 16:28:56 +0100

    confirmation page almost ready

--- libs/application/templates/financial-aid/src/fields/Confirmation/Confirmation.tsx
+++ libs/application/templates/financial-aid/src/fields/Confirmation/Confirmation.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
-import { AlertMessage, Box, Text } from '@island.is/island-ui/core'
+import { AlertMessage, Box, Button, Text } from '@island.is/island-ui/core'
 import { FAFieldBaseProps } from '../../lib/types'
 import { useIntl } from 'react-intl'
-import { confirmation } from '../../lib/messages'
-import * as styles from '../Shared.css'
-import cn from 'classnames'
-import { InputController, RadioController } from '@island.is/shared/form-fields'
-import { useFormContext } from 'react-hook-form'
-import DescriptionText from '../DescriptionText/DescriptionText'
+import { confirmation, copyUrl } from '../../lib/messages'
+import { DescriptionText, ConfirmationSectionImage, CopyUrl } from '..'
 
-const Confirmation = ({ errors, application }: FAFieldBaseProps) => {
+const Confirmation = ({}: FAFieldBaseProps) => {
   const { formatMessage } = useIntl()
 
   return (
     <>
       <Box marginTop={[4, 4, 5]}>
-        <AlertMessage
-          type="success"
-          title="Umsókn þín um fjárhagsaðstoð er móttekin"
-        />
+        <Box>
+          <AlertMessage
+            type="success"
+            title={formatMessage(confirmation.alertMessages.success)}
+          />
+        </Box>
+        <Box marginTop={[2, 2, 3]}>
+          {/* //Todo customize alert message depending on applicant */}
+          <AlertMessage
+            type="warning"
+            title={formatMessage(confirmation.alertMessages.dataNeeded)}
+            message={formatMessage(confirmation.alertMessages.dataNeededText)}
+          />
+        </Box>
       </Box>
 
       <Text as="h3" variant="h3" marginTop={[4, 4, 5]}>
@@ -27,6 +33,54 @@ const Confirmation = ({ errors, application }: FAFieldBaseProps) => {
 
       <Box marginTop={2}>
         <DescriptionText text={confirmation.nextSteps.content} />
+      </Box>
+
+      <Text as="h3" variant="h3" marginTop={[4, 4, 5]}>
+        {formatMessage(confirmation.sharedLink.title)}
+      </Text>
+
+      <Box marginTop={2}>
+        <CopyUrl
+          inputLabel={formatMessage(copyUrl.inputLabel)}
+          buttonLabel={formatMessage(copyUrl.buttonLabel)}
+          successMessage={formatMessage(copyUrl.successMessage)}
+        />
+      </Box>
+
+      <Text as="h3" variant="h3" marginTop={[4, 4, 5]}>
+        {formatMessage(confirmation.links.title)}
+      </Text>
+
+      <Box marginTop={2}>
+        <Button
+          icon="open"
+          colorScheme="default"
+          iconType="outline"
+          // onClick={() => router.push(Routes.statusPage(applicationId as string))}
+          preTextIconType="filled"
+          size="small"
+          type="button"
+          variant="text"
+        >
+          {formatMessage(confirmation.links.statusPage)}
+        </Button>
+      </Box>
+      <Box marginTop={2}>
+        <Button
+          icon="open"
+          colorScheme="default"
+          iconType="outline"
+          // onClick={() => router.push(Routes.statusPage(applicationId as string))}
+          preTextIconType="filled"
+          size="small"
+          type="button"
+          variant="text"
+        >
+          {formatMessage(confirmation.links.muncipalityInfoPage)}
+        </Button>
+      </Box>
+      <Box marginTop={[4, 4, 5]}>
+        <ConfirmationSectionImage />
       </Box>
     </>
   )
--- libs/application/templates/financial-aid/src/lib/messages/confirmation.ts
+++ libs/application/templates/financial-aid/src/lib/messages/confirmation.ts
@@ -5,25 +5,89 @@ export const confirmation = {
     sectionTitle: {
       id: 'fa.application:section.confirmation.general.sectionTitle',
       defaultMessage: 'Staðfesting',
-      description: 'Student form section title',
+      description: 'Confirmation section title',
     },
     pageTitle: {
       id: 'fa.application:section.confirmation.general.pageTitle',
       defaultMessage: 'Staðfesting',
-      description: 'Student form page title',
+      description: 'Confirmation page title',
     },
   }),
   nextSteps: defineMessages({
     title: {
       id: 'fa.application:section.confirmation.nextSteps.title',
       defaultMessage: '   Hér eru næstu skref',
-      description: 'Student form next step title',
+      description: 'Confirmation next step title',
     },
     content: {
       id: 'fa.application:section.confirmation.nextSteps.content#markup',
       defaultMessage:
         '* Vinnsluaðili sveitarfélagsins vinnur úr umsókninni. Umsóknin verður afgreidd eins fljótt og auðið er. \n* Ef umsóknin er samþykkt getur þú reiknað með útgreiðslu í byrjun október. \n* Ef þörf er á frekari upplýsingum eða gögnum til að vinna úr umsókninni mun vinnsluaðili sveitarfélagsins hafa samband.',
-      description: 'Student form next steps',
+      description: 'Confirmation next steps',
+    },
+  }),
+  links: defineMessages({
+    title: {
+      id: 'fa.application:section.confirmation.links.title',
+      defaultMessage: 'Frekari aðgerðir í boði',
+      description: 'Confirmation links title',
+    },
+    statusPage: {
+      id: 'fa.application:section.confirmation.links.statusPage',
+      defaultMessage: 'Sjá stöðu umsóknar',
+      description: 'Confirmation link to status page',
+    },
+    muncipalityInfoPage: {
+      id: 'fa.application:section.confirmation.links.muncipalityInfoPage',
+      defaultMessage: 'Upplýsingar um fjárhagsaðstoð hjá sveitarfélaginu',
+      description: 'Confirmation link to status page',
+    },
+  }),
+  alertMessages: defineMessages({
+    success: {
+      id: 'fa.application:section.confirmation.alertMessages.alertMessages',
+      defaultMessage: 'Umsókn þín um fjárhagsaðstoð er móttekin',
+      description: 'Alert message when application has successfully been sent',
+    },
+    dataNeeded: {
+      id: 'fa.application:section.confirmation.alertMessages.dataNeeded',
+      defaultMessage: 'Tekjugögn vantar',
+      description: 'Alert message when files are needed for the application',
+    },
+    dataNeededText: {
+      id: 'fa.application:section.confirmation.alertMessages.dataNeededText',
+      defaultMessage:
+        'Á stöðusíðu umsóknar getur þú hlaðið upp þeim gögnum sem vantar',
+      description: 'Alert message when files are needed for the application',
+    },
+  }),
+  alertMessagesInRelationship: defineMessages({
+    success: {
+      id:
+        'fa.application:section.confirmation.alertMessagesInRelationship.alertMessages',
+      defaultMessage: 'Þinn hluti umsóknar um fjárhagsaðstoð er móttekinn',
+      description:
+        'Alert message when application has successfully sent his side of application',
+    },
+    dataNeeded: {
+      id:
+        'fa.application:section.confirmation.alertMessagesInRelationship.dataNeeded',
+      defaultMessage: 'Gögn maka vantar',
+      description: 'Alert message when files are needed from spouse',
+    },
+    dataNeededText: {
+      id:
+        'fa.application:section.confirmation.alertMessagesInRelationship.dataNeededText',
+      defaultMessage:
+        'Maki fær sendan hlekk á umsókn til að hlaða upp gögnum svo hægt sé að vinna umsóknina',
+      description: 'Alert message when files are needed for the application',
+    },
+  }),
+  sharedLink: defineMessages({
+    title: {
+      id: 'fa.application:section.confirmation.sharedLink.title',
+      defaultMessage: 'Deila hlekk handa maka',
+      description: 'Sharing link with spouse title',
     },
   }),
 }
