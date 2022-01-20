bd0ac41db169f3fa0d75d8bbbb1d08c83ff4edf9
Alexandra Diljá <alexandrab18@ru.is>
2022-01-20 13:41:07 +0000

    quality photo provider in shared is abstract

--- libs/application/templates/driving-license/src/fields/QualityPhoto/hooks/useQualityPhoto.ts
+++ libs/application/templates/driving-license/src/fields/QualityPhoto/hooks/useQualityPhoto.ts
@@ -1,6 +1,7 @@
 import { Application, getValueViaPath } from '@island.is/application/core'
 import { QUALITY_PHOTO } from './queries.graphql'
 import { useQuery, ApolloError } from '@apollo/client'
+import { YES } from '../../../lib/constants'
 
 export interface QualityPhotoType {
   qualityPhoto: string | null
@@ -15,6 +16,28 @@ export type HasQualityPhotoData = {
 }
 
 export const useQualityPhoto = (application: Application): QualityPhotoType => {
+  // If running locally or on dev allow for fake data
+  const useFakeData = getValueViaPath<'yes' | 'no'>(
+    application.answers,
+    'fakeData.useFakeData',
+  )
+  // To use fake data for the quality photo provider take a look at the implementation in libs/application/templates/driving-license/src/forms/application.ts
+  if (useFakeData === YES) {
+    const fakeQualityPhoto = getValueViaPath<'yes' | 'no'>(
+      application.answers,
+      'fakeData.qualityPhoto',
+    )
+    const qualityPhoto: QualityPhotoType = {
+      qualityPhoto:
+        fakeQualityPhoto === YES
+          ? `data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAQEBAQEBAQEBAQGBgUGBggHBwcHCAwJCQkJCQwTDA4MDA4MExEUEA8QFBEeFxUVFx4iHRsdIiolJSo0MjRERFwBBAQEBAQEBAQEBAYGBQYGCAcHBwcIDAkJCQkJDBMMDgwMDgwTERQQDxAUER4XFRUXHiIdGx0iKiUlKjQyNEREXP/CABEIAAIAAgMBIgACEQEDEQH/xAAUAAEAAAAAAAAAAAAAAAAAAAAH/9oACAEBAAAAAHP/xAAUAQEAAAAAAAAAAAAAAAAAAAAH/9oACAECEAAAADv/xAAUAQEAAAAAAAAAAAAAAAAAAAAG/9oACAEDEAAAAHn/xAAZEAABBQAAAAAAAAAAAAAAAAAAAgMTU5H/2gAIAQEAAT8AjbrTh//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z`
+          : null,
+      loading: false,
+      error: undefined,
+    }
+    return qualityPhoto
+  }
+
   const hasQualityPhoto = getValueViaPath<HasQualityPhotoData>(
     application.externalData,
     'qualityPhoto',
