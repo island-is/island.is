22c3c45c75bd5ac48c2f9eed484a1e7e2781d695
mar <mar.nospam@anomy.net>
2022-01-20 11:49:18 +0000

    feat: Change home intro/title section

--- libs/service-portal/regulations-admin/src/screens/Home.tsx
+++ libs/service-portal/regulations-admin/src/screens/Home.tsx
@@ -2,9 +2,8 @@ import * as s from './Home.css'
 
 import React from 'react'
 
-import { Box, Button } from '@island.is/island-ui/core'
+import { Box, Button, Text } from '@island.is/island-ui/core'
 import { useNamespaces } from '@island.is/localization'
-import { IntroHeader } from '@island.is/service-portal/core'
 import { TaskList } from '../components/TaskList'
 import { ShippedRegulations } from '../components/ShippedRegulations'
 import { homeMessages as msg } from '../messages'
@@ -19,7 +18,18 @@ const Home = () => {
 
   return (
     <Box marginBottom={[6, 6, 10]}>
-      <IntroHeader title={msg.title} intro={msg.intro} />
+      <Box marginBottom={[4, 4, 8]}>
+        <Text as="h1" variant="h1">
+          {t(msg.title)}
+        </Text>
+        {msg.intro && (
+          <Text as="p" marginTop={1}>
+            {t(msg.intro)}
+          </Text>
+        )}
+      </Box>
+
+      {/* <IntroHeader title={msg.title} intro={msg.intro} /> */}
 
       <div className={s.newButtonBox}>
         <Button
