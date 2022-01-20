eb0cdd7c1dd213c424ecff79b8a04bb868fa9baf
RunarVestmann <runarvestmann@gmail.com>
2022-01-20 16:01:27 +0000

    Improved UX for the Stepper Helper and added transition and config validation

---
+++ apps/web/components/StepperFSM/StepperFSMHelper.css.ts
@@ -0,0 +1,19 @@
+import { style } from '@vanilla-extract/css'
+
+export const bold = style({
+  fontWeight: 'bold',
+})
+
+export const border = style({
+  border: '1px solid lightgrey',
+  borderRadius: '.5rem',
+  padding: '.2rem',
+  paddingRight: '0',
+})
+
+export const fitBorder = style({
+  border: '1px solid lightgrey',
+  borderRadius: '.5rem',
+  padding: '.2rem',
+  width: 'fit-content',
+})
