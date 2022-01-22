f9b95ddcd004741d6f7c35f808a9a0571d8b839f
jGeir <juliusgisla@gmail.com>
2022-01-22 18:50:46 +0000

    envs

--- apps/air-discount-scheme/web/lib/environment.ts
+++ apps/air-discount-scheme/web/lib/environment.ts
@@ -1,3 +1,4 @@
+import { getStaticEnv } from '@island.is/shared/utils'
 const isProd = process.env.NODE_ENV === 'production'
 
 const devConfig = {
@@ -10,11 +11,20 @@ const devConfig = {
   csrfCookieName: 'next-auth.csrf-token',
 }
 
+if (isProd) {
+  if (process.env.NEXT_PUBLIC_NEXTAUTH_URL === undefined) {
+    throw new Error('Missing Nextauth_Url in web environment.')
+  }
+  if (process.env.NEXT_PUBLIC_IDENTITY_SERVER_DOMAIN === undefined) {
+    throw new Error('Missing IDS domain in web environment.')
+  }
+}
+
 const prodConfig = {
   production: true,
   NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-  identityServerDomain: process.env.NEXT_PUBLIC_IDENTITY_SERVER_DOMAIN,
   identityServerLogoutURL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+  identityServerDomain: process.env.NEXT_PUBLIC_IDENTITY_SERVER_DOMAIN,
   identityServerSecret: process.env.IDENTITY_SERVER_SECRET,
   idsTokenCookieName: '__Secure-next-auth.session-token',
   csrfCookieName: '__Host-next-auth.csrf-token',
