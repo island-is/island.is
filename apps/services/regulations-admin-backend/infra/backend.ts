4e88f52ee68a72d40ea8176b3ce0ee7476bec5e8
Valur Sverrisson <valur@hugsmidjan.is>
2022-01-24 12:41:07 +0000

    fix: remove postgresInfo to avoid over-configuration

--- apps/services/regulations-admin-backend/infra/backend.ts
+++ apps/services/regulations-admin-backend/infra/backend.ts
@@ -1,17 +1,13 @@
 import { service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
 import { settings } from '../../../../infra/src/dsl/settings'
 
-const postgresInfo = {
-  passwordSecret: '/k8s/regulations-admin-backend/DB_PASSWORD',
-}
 export const serviceSetup = (): ServiceBuilder<'regulations-admin-backend'> =>
   service('regulations-admin-backend')
     .image('regulations-admin-backend')
     .namespace('regulations-admin')
-    .postgres(postgresInfo)
+    .postgres()
     .initContainer({
       containers: [{ command: 'npx', args: ['sequelize-cli', 'db:migrate'] }],
-      postgres: postgresInfo,
     })
     .secrets({
       REGULATIONS_API_URL: '/k8s/api/REGULATIONS_API_URL',
