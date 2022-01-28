814f34d502424f312c8d7a959abd2b0c856a5994
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-01-28 10:10:17 +0100

    fixing link!

--- libs/financial-aid/shared/src/lib/formatters.ts
+++ libs/financial-aid/shared/src/lib/formatters.ts
@@ -248,7 +248,7 @@ export const getApplicantEmailDataFromEventType = (
         data: {
           title: 'Fjárhagsaðstoð Umsókn móttekin',
           header: `Umsókn þín fyrir ${getPeriod.month} er móttekin og er nú í vinnslu`,
-          content: `Umsóknin verður afgreidd eins fljótt og hægt er. Þú færð annan tölvupóst þegar vinnsla klárast eða ef okkur vantar einhver gögn frá þér.<br><br>Þú getur fylgst með stöðu umsóknarinnar á <a href=${applicationLink} target="_blank"> <b>stöðusíðu umsóknarinnar</b></a>.`,
+          content: `Umsóknin verður afgreidd eins fljótt og hægt er. Þú færð annan tölvupóst þegar vinnsla klárast eða ef okkur vantar einhver gögn frá þér.<br><br>Þú getur fylgst með stöðu umsóknarinnar á <a href="${applicationLink}" target="_blank"> <b>stöðusíðu umsóknarinnar</b></a>.`,
           applicationChange: 'Umsókn móttekin og í vinnslu',
           applicationMonth: getPeriod.month,
           applicationYear: getPeriod.year,
