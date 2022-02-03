19fd07a012d3ac1994c2f8fe1ac409a5409156cf
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-02-03 15:41:07 +0000

    denied text

--- apps/financial-aid/web-veita/src/components/ModalTypes/RejectModal.tsx
+++ apps/financial-aid/web-veita/src/components/ModalTypes/RejectModal.tsx
@@ -20,7 +20,7 @@ const RejectModal = ({
 
   return (
     <InputModal
-      headline="Umsókn þinni um aðstoð hefur verið synjað"
+      headline="Skrifaðu ástæðu höfnunar"
       onCancel={onCancel}
       onSubmit={() => {
         if (!comment) {
@@ -32,13 +32,13 @@ const RejectModal = ({
       submitButtonText="Synja og senda á umsækjanda"
       isModalVisable={isModalVisable}
       hasError={hasError}
-      errorMessage="Þú þarft að greina frá ástæðu synjunar"
+      errorMessage="Þú þarft að greina frá ástæðu höfnunar"
     >
       <Box marginBottom={2}>
         <Input
-          label="Ástæða synjunar"
+          label="Ástæða höfnunar"
           name="reasonForRejection"
-          placeholder="Umsókn þinni um fjárhagsaðstoð hefur verið synjað …"
+          placeholder="Umsókn þinn um fjárhagsaðstoð hefur verið hafnað…"
           rows={4}
           value={comment}
           textarea
@@ -52,9 +52,10 @@ const RejectModal = ({
       </Box>
 
       <Text variant="small" marginBottom={10}>
-        Þegar umsóknum er synjað er hlekkur á slóð um reglur um fjárhagsaðstoð
-        sveitarfélagsins birtur í tölvupósti sem er sendur á umsækjanda. Í
-        ástæðu synjunar er því óhætt að vísa til reglna um fjárhagsaðstoð.
+        Þegar umsóknum er hafnað er hlekkur á slóð um reglur um fjárhagsaðstoð
+        sveitarfélagsins birtur í tölvupósti sem er sendur á umsækjanda og á
+        stöðusíðu. Í ástæðu höfnunar er því óhætt að vísa til reglna um
+        fjárhagsaðstoð.
       </Text>
     </InputModal>
   )
