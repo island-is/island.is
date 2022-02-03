a700ac4ef46a50fb4e70f67ed87f449b0901773e
Petar Shomov <petar@andes.is>
2022-02-03 17:33:21 +0000

    working on reproducing the correct git commits we should be using

--- .github/actions/v2/change-detection.spec.ts
+++ .github/actions/v2/change-detection.spec.ts
@@ -3,16 +3,18 @@ import { writeFile, mkdtemp } from 'fs/promises'
 import { join } from 'path'
 
 let fileA: string
+let fileB: string
 
 async function makeChange(
   git: SimpleGit,
   path: string,
   content: string,
   message: string,
-) {
+): Promise<string> {
   await writeFile(path, content)
   await git.add(path)
-  await git.commit(message, [path])
+  const commit = await git.commit(message, [path])
+  return commit.commit
 }
 
 describe('Change detection', () => {
@@ -22,13 +24,21 @@ describe('Change detection', () => {
     beforeEach(async () => {
       path = await mkdtemp(`${__dirname}/test-data/repo`)
       fileA = join(path, 'A.txt')
+      fileB = join(path, 'B.txt')
       git = simpleGit(path, { baseDir: path })
       const r = await git.init()
     })
     it('should skip bad commit', async () => {
+      const br = await git.checkoutLocalBranch('main')
       await makeChange(git, fileA, 'test1', 'A-good')
-      await makeChange(git, fileA, 'test2', 'B-bad')
-      await makeChange(git, fileA, 'test3', 'C')
+      const forkSha = await makeChange(git, fileA, 'test2', 'B-bad')
+      await git.checkoutBranch('fix', 'main')
+      const fixSha = await makeChange(git, fileB, 'test3', 'C')
+      await git.checkout('main')
+      const mainSha = await makeChange(git, fileA, 'test3', 'D-good')
+      // const merge = await git.mergeFromTo('fix', 'main')
+      const br2 = await git.raw('merge-base', 'fix', 'main')
+
       const logs = await git.log({ format: {} })
       expect(logs.total).toBe(3)
     })
