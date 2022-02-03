002cc71fdb81935f31afb0f4d8a6e4d24800dc13
Petar Shomov <petar@andes.is>
2022-02-03 17:36:14 +0000

    adding the merging thing

--- .github/actions/v2/change-detection.spec.ts
+++ .github/actions/v2/change-detection.spec.ts
@@ -36,11 +36,11 @@ describe('Change detection', () => {
       const fixSha = await makeChange(git, fileB, 'test3', 'C')
       await git.checkout('main')
       const mainSha = await makeChange(git, fileA, 'test3', 'D-good')
-      // const merge = await git.mergeFromTo('fix', 'main')
+      const merge = await git.mergeFromTo('fix', 'main')
       const br2 = await git.raw('merge-base', 'fix', 'main')
 
       const logs = await git.log({ format: {} })
-      expect(logs.total).toBe(3)
+      expect(logs.total).toBe(5)
     })
   })
 })
