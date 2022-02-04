1ed48b38f67f1910f91ad50e0f4fdb9ed4724c85
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2022-02-04 14:52:57 +0000

    feat: push token sync

--- apps/native/app/src/screens/user/tab-settings.tsx
+++ apps/native/app/src/screens/user/tab-settings.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@apollo/client'
 import {
   Alert,
   TableViewAccessory,
@@ -9,6 +10,7 @@ import * as Sentry from '@sentry/react-native'
 import {
   authenticateAsync
 } from 'expo-local-authentication'
+import gql from 'graphql-tag'
 import React, { useEffect, useRef, useState } from 'react'
 import { useIntl } from 'react-intl'
 import {
@@ -25,6 +27,7 @@ import CodePush, {
   LocalPackage
 } from '../../../../node_modules/react-native-code-push'
 import { PressableHighlight } from '../../components/pressable-highlight/pressable-highlight'
+import { client } from '../../graphql/client'
 import { showPicker } from '../../lib/show-picker'
 import { authStore } from '../../stores/auth-store'
 import {
@@ -89,6 +92,18 @@ export function TabSettings() {
     })
   }
 
+  const userProfile = useQuery(gql`
+    query {
+      getUserProfile {
+        nationalId
+        locale
+        documentNotifications
+      }
+    }
+  `, {
+    client,
+  });
+
   const onLanguagePress = () => {
     showPicker({
       type: 'radio',
@@ -142,7 +157,38 @@ export function TabSettings() {
           title={intl.formatMessage({
             id: 'settings.communication.newDocumentsNotifications',
           })}
-          accessory={<PreferencesSwitch name="notificationsNewDocuments" />}
+          accessory={
+            <Switch
+              onValueChange={(value) => {
+                client.mutate({
+                  mutation: gql`
+                    mutation updateProfile($input: UpdateUserProfileInput!) {
+                      updateProfile(input: $input) {
+                        nationalId
+                        locale
+                        documentNotifications
+                      }
+                    }
+                  `,
+                  variables: {
+                    input: {
+                      documentNotifications: value,
+                    }
+                  }
+                }).catch((err) => {
+                  console.log(JSON.stringify(err));
+                  alert(err.message)
+                })
+              }}
+              disabled={userProfile.loading || !userProfile.data}
+              value={userProfile.data?.getUserProfile?.documentNotifications}
+              thumbColor={Platform.select({ android: theme.color.dark100 })}
+              trackColor={{
+                false: theme.color.dark200,
+                true: theme.color.blue400,
+              }}
+            />
+          }
         />
         <TableViewCell
           title={intl.formatMessage({
