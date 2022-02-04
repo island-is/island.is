148e45aec667502d905dc7be2d043f127053d1e5
Davíð Guðni <davidgudni@gmail.com>
2022-02-04 13:36:20 +0100

    Expand on the metrics collected in enhanced fetch

--- libs/clients/middlewares/src/lib/withMetrics.ts
+++ libs/clients/middlewares/src/lib/withMetrics.ts
@@ -14,8 +14,27 @@ export const withMetrics = ({
   const client = dogStatsD({
     prefix: `${name}.`,
   })
-  return (input, init) => {
-    client.increment('requests')
-    return fetch(input, init)
+  return async (input, init) => {
+    try {
+      const response = await fetch(input, init)
+      client.increment('requests')
+      if (response.status >= 200 && response.status < 300) {
+        client.increment('requests.2xx')
+        client.increment('requests.ok')
+      } else if (response.status >= 300 && response.status < 400) {
+        client.increment('requests.3xx')
+        client.increment('requests.ok')
+      } else if (response.status >= 400 && response.status < 500) {
+        client.increment('requests.4xx')
+        client.increment('requests.error')
+      } else if (response.status >= 500 && response.status < 600) {
+        client.increment('requests.5xx')
+        client.increment('requests.error')
+      }
+      return response
+    } catch (error) {
+      client.increment('requests.client_error')
+      throw error
+    }
   }
 }
