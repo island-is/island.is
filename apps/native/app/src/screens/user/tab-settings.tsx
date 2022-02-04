1ae9b5695b7ecd74b4ee3aa9c6c0aa57c0fd7170
Bjarni Freyr Björnsson <bjarmif@gmail.com>
2022-02-04 10:54:24 +0000

    feat: hide notification settings and removed sentry native crash

--- apps/native/app/src/screens/user/tab-settings.tsx
+++ apps/native/app/src/screens/user/tab-settings.tsx
@@ -144,7 +144,7 @@ export function TabSettings() {
           })}
           accessory={<PreferencesSwitch name="notificationsNewDocuments" />}
         />
-        <TableViewCell
+        {/* <TableViewCell
           title={intl.formatMessage({
             id: 'settings.communication.appUpdatesNotifications',
           })}
@@ -157,7 +157,7 @@ export function TabSettings() {
           accessory={
             <PreferencesSwitch name="notificationsApplicationStatusUpdates" />
           }
-        />
+        /> */}
       </TableViewGroup>
       <TableViewGroup
         header={intl.formatMessage({
@@ -404,9 +404,6 @@ export function TabSettings() {
           onPress={() => {
             console.log(pushToken)
             RNAlert.prompt('token', 'yup', undefined, undefined, pushToken)
-          }}
-          onLongPress={() => {
-            Sentry.nativeCrash()
           }}
         >
           <TableViewCell title="Push Token" subtitle={pushToken} />
