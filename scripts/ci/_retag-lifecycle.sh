93787e103cbc051634c8a50c55c30cb1d0630b3e
Snorri Hjörvar Jóhannsson <snorri@andes.is>
2022-02-07 11:57:23 +0000

    retag unaffected in feature branches

---
+++ scripts/ci/_retag-lifecycle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# This is a script to re-tag a Docker image without pulling the pushing the image but rather directly in the registry.
+
+MANIFEST=$(aws ecr batch-get-image --repository-name "$IMAGE" --image-ids imageTag="$LAST_GOOD_BUILD_DOCKER_TAG" --query 'images[].imageManifest' --output text)
+aws ecr put-image --repository-name "$IMAGE" --image-tag "$POLICY_TAG" --image-manifest "$MANIFEST"
