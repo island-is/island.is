bb8d35e1a0da017279422f4e6ed6c6d5d5284bb5
Snorri Hjörvar Jóhannsson <snorri@andes.is>
2022-02-07 10:46:09 +0000

    Updating build container script and adding policy tag to images

--- infra/scripts/build-docker-container.sh
+++ infra/scripts/build-docker-container.sh
@@ -6,9 +6,10 @@ DOCKER_REGISTRY=${DOCKER_REGISTRY:-821090935708.dkr.ecr.eu-west-1.amazonaws.com/
 PUBLISH=${PUBLISH:-false}
 DOCKER_IMAGE="${DOCKER_REGISTRY}helm-config"
 DOCKER_TAG=$1
+POLICY_TAG=$2
 
-docker build -f "$DIR"/Dockerfile -t "$DOCKER_IMAGE":"${DOCKER_TAG}" "$DIR"/../..
+docker build -f "$DIR"/Dockerfile -t "$DOCKER_IMAGE":"${DOCKER_TAG}" -t "$DOCKER_IMAGE":${POLICY_TAG} "$DIR"/../..
 
 if [[ "true" = "$PUBLISH" ]] ; then
-    docker push "$DOCKER_IMAGE":"${DOCKER_TAG}"
+    docker push "$DOCKER_IMAGE":"${DOCKER_TAG} --all-tags"
 fi
