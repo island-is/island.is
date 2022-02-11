48b167851689aca778c49922ed126c3a128e8d3f
Benedikt Jónsson <bensijons@gmail.com>
2022-02-11 14:07:29 +0000

    feat(fishing-license): oidc token exchange (#6532)
    * create providers
    
    * use auto auth in config
    
    * reconfigure auto auth and use IdsClientConfig
    
    * add client config and update workspace.json
    
    * fix imports
    
    * set additionalProperties = false when fetching document
    
    * fix formatting
    fix formatting# Please enter the commit message for your changes. Lines starting
    
    * formatting
    
    * update gitbook
    
    * addressing comments
    
    * add gitbook ignore to readme
    
    * yarn gitbook
    
    * add missing provider
    
    * inject configs

--- libs/clients/fishing-licence/src/lib/fishing-license-client.module.ts
+++ libs/clients/fishing-licence/src/lib/fishing-license-client.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ApiConfiguration } from './apiConfiguration'
+import { exportedApis } from './apis'
 import {
   FishingLicenceApiClientMock,
   FISHING_LICENSE_CLIENT,
 } from './fishing-license-client.mock'
 
 @Module({
-  imports: [],
-  controllers: [],
+  exports: exportedApis,
   providers: [
     { provide: FISHING_LICENSE_CLIENT, useClass: FishingLicenceApiClientMock },
+    ApiConfiguration,
+    ...exportedApis,
   ],
 })
 export class FishingLicenseClientModule {}
