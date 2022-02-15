55e1be2ebfe6f7211e65385153ad96817a92f632
Benedikt Jónsson <bensijons@gmail.com>
2022-02-15 14:15:26 +0000

    use fiskistofa apis (#6616)

--- libs/api/domains/fishing-license/src/graphql/fishing-license.resolver.ts
+++ libs/api/domains/fishing-license/src/graphql/fishing-license.resolver.ts
@@ -1,19 +1,20 @@
 import { UseGuards } from '@nestjs/common'
 import { Args, Query, Resolver } from '@nestjs/graphql'
 import { Audit } from '@island.is/nest/audit'
-import { ApiScope } from '@island.is/auth/scopes'
+import { ApiScope, FishingLicenseScope } from '@island.is/auth/scopes'
 import {
   CurrentUser,
+  IdsAuthGuard,
   IdsUserGuard,
   Scopes,
   ScopesGuard,
 } from '@island.is/auth-nest-tools'
 import type { User } from '@island.is/auth-nest-tools'
-import { FishingLicenseService } from '../lib/fishing-license.service'
 import { Ship } from './models/ship.model'
 import { FishingLicense } from './models/fishing-license.model'
+import { FishingLicenseService } from '../lib/fishing-license.service'
 
-@UseGuards(IdsUserGuard, ScopesGuard)
+@UseGuards(IdsUserGuard, IdsAuthGuard, ScopesGuard)
 @Resolver()
 export class FishingLicenseResolver {
   constructor(private fishingLicenseService: FishingLicenseService) {}
@@ -24,7 +25,8 @@ export class FishingLicenseResolver {
   })
   @Audit()
   async shipQuery(@CurrentUser() user: User): Promise<Ship[]> {
-    return await this.fishingLicenseService.getShips(user.nationalId)
+    console.log('getting shipss')
+    return await this.fishingLicenseService.getShips(user.nationalId, user)
   }
 
   @Query(() => [FishingLicense], { name: 'fishingLicenses' })
@@ -32,9 +34,12 @@ export class FishingLicenseResolver {
   async fishingLicensesQuery(
     @Args('registrationNumber', { type: () => Number })
     registrationNumber: number,
+    @CurrentUser() user: User,
   ) {
+    console.log('getting fishings licelicieclei')
     return await this.fishingLicenseService.getFishingLicenses(
       registrationNumber,
+      user,
     )
   }
 }
--- libs/clients/fishing-license/src/index.ts
+++ libs/clients/fishing-license/src/index.ts
@@ -6,3 +6,4 @@ export {
 } from './lib/fishing-license-client.mock' //todo remove mock and export from ./gen/fetch
 export * from './lib/types'
 export { FishingLicenseClientConfig } from './lib/FishingLicenseClientConfig'
+export * from './lib/gen/fetch'
