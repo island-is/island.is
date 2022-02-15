00d26db9b31246f6c7f3b386449289121e10e377
Ívar Oddsson <ivaroddsson@gmail.com>
2022-02-15 22:23:11 +0000

    feat(island-ui): Add autoExpand prop to Input component (#6603)
    * Automatically grow textareas when typing
    
    * Add a autoExpand prop on input
    
    * Remove autoExpand from accusedBookings
    
    Co-authored-by: Ívar Oddsson <oddsson@Ivars-iMac.local>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/components/RulingInput/RulingInput.tsx
+++ apps/judicial-system/web/src/components/RulingInput/RulingInput.tsx
@@ -1,13 +1,15 @@
-import { Input } from '@island.is/island-ui/core'
-import { Case, isAcceptingCaseDecision } from '@island.is/judicial-system/types'
 import React, { useEffect, useState } from 'react'
+import { Input } from '@island.is/island-ui/core'
 import { useIntl } from 'react-intl'
+
+import { Case, isAcceptingCaseDecision } from '@island.is/judicial-system/types'
+import { ruling as m } from '@island.is/judicial-system-web/messages'
+
+import { useCase } from '../../utils/hooks'
 import {
   removeTabsValidateAndSet,
   validateAndSendToServer,
 } from '../../utils/formHelper'
-import { useCase } from '../../utils/hooks'
-import { ruling as m } from '@island.is/judicial-system-web/messages'
 
 interface Props {
   workingCase: Case
@@ -55,7 +57,6 @@ const RulingInput: React.FC<Props> = (props) => {
       label={formatMessage(m.label)}
       placeholder={formatMessage(m.placeholder)}
       value={workingCase.ruling || ''}
-      rows={rows ?? 16}
       errorMessage={rulingErrorMessage}
       hasError={rulingErrorMessage !== ''}
       required={isRequired}
@@ -81,6 +82,7 @@ const RulingInput: React.FC<Props> = (props) => {
         )
       }
       textarea
+      rows={rows ?? 16}
     />
   )
 }
