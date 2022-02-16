47f51cb7eac9a055ea0729e9cb980fa38b4a3bc7
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-02-16 13:17:38 +0000

    connecting files to samband

--- libs/api/domains/municipalities-financial-aid/src/lib/municipalitiesFinancialAid.service.ts
+++ libs/api/domains/municipalities-financial-aid/src/lib/municipalitiesFinancialAid.service.ts
@@ -3,15 +3,22 @@ import type { Auth } from '@island.is/auth-nest-tools'
 import { AuthMiddleware } from '@island.is/auth-nest-tools'
 import {
   ApplicationApi,
+  GetSignedUrlDto,
   MunicipalityApi,
+  FilesApi,
+  FileControllerCreateSignedUrlRequest,
+  CreateFilesModelFromJSON,
 } from '@island.is/clients/municipalities-financial-aid'
 import { MunicipalityQueryInput } from './models/municipality.input'
+import { SignedUrlModel } from './models/signedUrl.model'
+import { GetSignedUrlInput } from './dto/getSignedUrl.input'
 
 @Injectable()
 export class MunicipalitiesFinancialAidService {
   constructor(
     private applicationApi: ApplicationApi,
     private municipalityApi: MunicipalityApi,
+    private filesApi: FilesApi,
   ) {}
 
   applicationApiWithAuth(auth: Auth) {
@@ -20,6 +27,10 @@ export class MunicipalitiesFinancialAidService {
 
   municipalityApiWithAuth(auth: Auth) {
     return this.municipalityApi.withMiddleware(new AuthMiddleware(auth))
+  }
+
+  fileApiWithAuth(auth: Auth) {
+    return this.filesApi.withMiddleware(new AuthMiddleware(auth))
   }
 
   async municipalitiesFinancialAidCurrentApplication(auth: Auth) {
@@ -42,6 +53,23 @@ export class MunicipalitiesFinancialAidService {
   ) {
     return await this.municipalityApiWithAuth(auth)
       .municipalityControllerGetById(municipalityCode)
+      .then((res) => {
+        return res
+      })
+      .catch((error) => {
+        if (error.status === 404) {
+          return null
+        }
+        throw error
+      })
+  }
+
+  async municipalitiesFinancialAidCreateSignedUrl(
+    auth: Auth,
+    getSignedUrl: GetSignedUrlInput,
+  ) {
+    return await this.fileApiWithAuth(auth)
+      .fileControllerCreateSignedUrl({ getSignedUrlDto: getSignedUrl })
       .then((res) => {
         return res
       })
