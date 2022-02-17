bed9f0d8fba9df8e95b99a7ce393731027e4f0e7
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-02-17 18:37:01 +0000

    Rewrites barrel files

--- apps/judicial-system/backend/src/app/modules/case/guards/test/caseExistsGuard.spec.ts
+++ apps/judicial-system/backend/src/app/modules/case/guards/test/caseExistsGuard.spec.ts
@@ -10,7 +10,7 @@ import { createTestingCaseModule } from '../../test/createTestingCaseModule'
 import { Defendant } from '../../../defendant'
 import { Institution } from '../../../institution'
 import { User } from '../../../user'
-import { Case } from '../../models'
+import { Case } from '../../models/case.model'
 import { CaseExistsGuard } from '../caseExists.guard'
 
 interface Then {
