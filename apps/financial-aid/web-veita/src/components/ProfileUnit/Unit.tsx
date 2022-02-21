d3bb6af7536a25dec8ad6bb89ba351e2eccf0714
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-02-21 12:10:47 +0000

    Tesing other structure

--- apps/financial-aid/web-veita/src/components/ProfileUnit/Unit.tsx
+++ apps/financial-aid/web-veita/src/components/ProfileUnit/Unit.tsx
@@ -1,40 +1,9 @@
 import React from 'react'
-import { Text, Box, Link } from '@island.is/island-ui/core'
+import { Text, Box } from '@island.is/island-ui/core'
 
 import * as styles from './ProfileUnit.css'
 import cn from 'classnames'
-
-const LinkItem = (link?: string, onclick?: () => void, content?: string) => {
-  return (
-    link && (
-      <Link href={link} color="blue400" onClick={onclick}>
-        {content}
-      </Link>
-    )
-  )
-}
-
-const ButtonItem = (onclick?: () => void, content?: string) => {
-  return (
-    onclick && (
-      <button onClick={onclick} className={styles.button}>
-        {content}
-      </button>
-    )
-  )
-}
-
-const OtherItem = (other?: string) => {
-  return (
-    other && (
-      <Box background="blue100" borderRadius="large" padding={2} marginTop={1}>
-        <Text variant="small">
-          „<em>{other}</em>“
-        </Text>
-      </Box>
-    )
-  )
-}
+import { LinkItem, ButtonItem, OtherItem } from './GetUnitContent'
 
 interface Props {
   info: {
