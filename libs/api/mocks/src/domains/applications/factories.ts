8d3827868243929e716a0e97dd3d18d8c04c7424
Alexandra Diljá <alexandrab18@ru.is>
2022-02-21 12:33:15 +0000

    factory add actors

--- libs/api/mocks/src/domains/applications/factories.ts
+++ libs/api/mocks/src/domains/applications/factories.ts
@@ -12,6 +12,7 @@ export const application = factory<Application>({
   modified: () => faker.date.past().toISOString(),
   applicant: () => faker.random.alphaNumeric(10),
   assignees: [],
+  actors: [],
   state: 'draft',
   typeId: 'ExampleForm',
   answers: [],
