56b467ef50bf39f3ad269c001208ed6642988bfb
Þórður H <tolleinn@gmail.com>
2022-02-22 12:06:46 +0000

    Update pathToAsset.ts

--- apps/services/user-profile/src/app/user-profile/utils/pathToAsset.ts
+++ apps/services/user-profile/src/app/user-profile/utils/pathToAsset.ts
@@ -3,8 +3,8 @@ import { join } from 'path'
 
 export const pathToAsset = (file: string, baseUrl: string) => {
   if (isRunningOnEnvironment('local')) {
-    return join(__dirname, `../../service-portal/assets/images/${file}`)
+    return `${baseUrl}/assets/images/${file}`
   }
 
-  return `${baseUrl}/assets/images/${file}`
+  return join(__dirname, `../../service-portal/assets/images/${file}`)
 }
