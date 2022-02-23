f629856568ae2e7efdb752b87275f918773a3285
Valur Sverrisson <valur@hugsmidjan.is>
2022-02-23 15:38:33 +0000

    fix: tweak review data

--- libs/service-portal/regulations-admin/src/components/EditReviewOverview.tsx
+++ libs/service-portal/regulations-admin/src/components/EditReviewOverview.tsx
@@ -112,7 +112,7 @@ export const EditReviewOverview = (props: EditReviewOverviewProps) => {
     setClicked({ ...clicked, [key]: true })
 
   const formatDate = (date?: Date, format?: string) =>
-    date ? formatDateFns(date, format || 'dd MMMM yyyy') : ''
+    date ? formatDateFns(date, format || 'd. MMMM yyyy') : ''
 
   if (hasWarnings) {
     return null
@@ -165,24 +165,18 @@ export const EditReviewOverview = (props: EditReviewOverviewProps) => {
           <Text variant="h4" as="h4" marginBottom={2}>
             {t(reviewMessagse.impactsTitle)}
           </Text>
-          Foo!
-          {Object.keys(draft.impacts).forEach((key) => {
+
+          {Object.keys(draft.impacts).map((key) => {
             const impactsList = draft.impacts[key]
-            console.log({ key, impactsList })
-
             const label =
-              key === 'self'
-                ? t(impactMsgs.selfAffecting)
-                : `${prettyName(key as RegName)} – `
-
-            return <>{label}</>
+              key === 'self' ? t(impactMsgs.selfAffecting) : `${key}`
 
             return (
               <OverviewItem key={key} label={label} step="impacts">
-                Foo!
                 {impactsList.map((impact) => (
                   <div key={impact.id}>
-                    {formatDate(impact.date.value)}{' '}
+                    {formatDate(impact.date.value)}
+                    {' - '}
                     {t(
                       impact.type === 'amend'
                         ? 'Textabreyting'
