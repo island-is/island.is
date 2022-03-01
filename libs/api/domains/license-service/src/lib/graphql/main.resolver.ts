bfae753b23c89c95622b704bde844fc4bee40cb8
disaerna <disa.erna@gmail.com>
2022-03-01 11:42:56 +0000

    feat: add license scope

--- libs/api/domains/license-service/src/lib/graphql/main.resolver.ts
+++ libs/api/domains/license-service/src/lib/graphql/main.resolver.ts
@@ -74,7 +74,7 @@ export class VerifyPkPassInput {
 }
 
 @UseGuards(IdsUserGuard, ScopesGuard)
-@Scopes(ApiScope.internal)
+@Scopes(ApiScope.licenses)
 @Resolver()
 @Audit({ namespace: '@island.is/api/license-service' })
 export class MainResolver {
