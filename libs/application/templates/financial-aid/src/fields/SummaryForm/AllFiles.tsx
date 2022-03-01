dbc0689ff08a414b733a27b236cf45a9aaf51684
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2022-03-01 16:13:05 +0000

    needs to check because if empty throws error

--- libs/application/templates/financial-aid/src/fields/SummaryForm/AllFiles.tsx
+++ libs/application/templates/financial-aid/src/fields/SummaryForm/AllFiles.tsx
@@ -22,44 +22,46 @@ const AllFiles = ({ taxFiles, incomeFiles, applicationId }: Props) => {
     <>
       {allFiles &&
         allFiles.map((file: UploadFile, index: number) => {
-          return (
-            <a
-              onClick={() => {
-                createSignedUrlMutation({
-                  variables: {
-                    input: {
-                      fileName: encodeFilenames(file.name),
-                      folder: applicationId,
+          if (file) {
+            return (
+              <a
+                onClick={() => {
+                  createSignedUrlMutation({
+                    variables: {
+                      input: {
+                        fileName: encodeFilenames(file.name),
+                        folder: applicationId,
+                      },
                     },
-                  },
-                }).then((response) => {
-                  window.open(response.data?.getSignedUrl.url)
-                })
-              }}
-              key={`file-` + index}
-              target="_blank"
-              download
-              rel="noreferrer noopener"
-              className={styles.filesButtons}
-            >
-              <Box
-                display="flex"
-                alignItems="center"
-                marginBottom="smallGutter"
+                  }).then((response) => {
+                    window.open(response.data?.getSignedUrl.url)
+                  })
+                }}
+                key={`file-` + index}
+                target="_blank"
+                download
+                rel="noreferrer noopener"
+                className={styles.filesButtons}
               >
-                <Box marginRight={1} display="flex" alignItems="center">
-                  <Icon
-                    color="blue400"
-                    icon="document"
-                    size="small"
-                    type="outline"
-                  />
-                </Box>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  marginBottom="smallGutter"
+                >
+                  <Box marginRight={1} display="flex" alignItems="center">
+                    <Icon
+                      color="blue400"
+                      icon="document"
+                      size="small"
+                      type="outline"
+                    />
+                  </Box>
 
-                <Text>{file.name}</Text>
-              </Box>
-            </a>
-          )
+                  <Text>{file.name}</Text>
+                </Box>
+              </a>
+            )
+          }
         })}
     </>
   )
