9788f45e4ca86da0860cc57e8624a61b9909e4a9
johannaagma <johannaagma@gmail.com>
2022-03-02 13:16:35 +0000

    Cleanup constants

--- libs/application/templates/mortgage-certificate/src/lib/constants.ts
+++ libs/application/templates/mortgage-certificate/src/lib/constants.ts
@@ -1,10 +1,8 @@
 import { DefaultEvents } from '@island.is/application/core'
 
 export enum MCEvents {
-  //PENDING = 'PENDING',
   PENDING_REJECTED = 'PENDING_REJECTED',
   PENDING_REJECTED_TRY_AGAIN = 'PENDING_REJECTED_TRY_AGAIN',
-  //ERROR = 'ERROR',
 }
 
 export type Events =
@@ -12,14 +10,11 @@ export type Events =
   | { type: DefaultEvents.SUBMIT }
   | { type: DefaultEvents.PAYMENT }
   | { type: DefaultEvents.REJECT }
-  //| { type: MCEvents.PENDING }
   | { type: MCEvents.PENDING_REJECTED }
   | { type: MCEvents.PENDING_REJECTED_TRY_AGAIN }
-//| { type: MCEvents.ERROR }
 
 export enum States {
   DRAFT = 'draft',
-  PENDING = 'pending',
   PENDING_REJECTED = 'pending_rejected',
   PENDING_REJECTED_TRY_AGAIN = 'pending_rejected_try_again',
   PAYMENT_INFO = 'payment_info',
