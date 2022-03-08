d1a56d48d45eca80387312a5106724c2441f63fa
RunarVestmann <runarvestmann@gmail.com>
2022-03-08 16:10:37 +0000

    Added a background image

---
+++ apps/web/components/ServiceWeb/Background/Variations/Mannaudstorg/Mannaudstorg.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css'
+
+export const bg = style({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  backgroundImage:
+    'url(https://images.ctfassets.net/8k0h54kbe6bj/5mnW5sXXiCT9OeESZk4wD/f814fe00378ff8bacb858bbfacda1cd3/Mynd_bg.jpg)',
+  backgroundRepeat: 'no-repeat',
+  backgroundAttachment: 'fixed',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+})
