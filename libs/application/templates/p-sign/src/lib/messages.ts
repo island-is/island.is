759ddfc6ce9a5b8de53706386f15f83f52fc91d7
Kristofer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-03-11 16:24:00 +0000

    Revert "Rename identifiers in messages"
    This reverts commit aecef1daffcb31a891b5e70e0a87879db435db3a.

--- libs/application/templates/p-sign/src/lib/messages.ts
+++ libs/application/templates/p-sign/src/lib/messages.ts
@@ -124,7 +124,7 @@ export const m = defineMessages({
 
   /* Quality Photo Section */
   qualityPhotoTitle: {
-    id: 'ps.application:qualityPhotoTitle',
+    id: 'ps.application:qualityPhotoSectionTitle',
     defaultMessage: 'Mynd í stæðiskort',
     description: 'Title for quality photo section',
   },
@@ -277,7 +277,7 @@ export const m = defineMessages({
     description: 'Title for overview section',
   },
   overviewSectionTitle: {
-    id: 'ps.application:overviewSectionTitle',
+    id: 'ps.application:overviewTitle',
     defaultMessage: 'Yfirlit',
     description: 'Title for overview section',
   },
