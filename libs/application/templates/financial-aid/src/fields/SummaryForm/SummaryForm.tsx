9969a563de6b5fa6a9df4bf7fc6fcd68af6678b7
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-03-15 16:19:24 +0000

    merge conflict

--- libs/application/templates/financial-aid/src/fields/SummaryForm/SummaryForm.tsx
+++ libs/application/templates/financial-aid/src/fields/SummaryForm/SummaryForm.tsx
@@ -31,13 +31,17 @@ import {
   getMessageHomeCircumstances,
   getMessageFamilyStatus,
 } from '../../lib/formatters'
-import AllFiles from './AllFiles'
 import { findFamilyStatus } from '../../lib/utils'
+import AllFiles from './Files'
 import useApplication from '../../lib/hooks/useApplication'
 
 import cn from 'classnames'
 import * as styles from './../Shared.css'
 import { hasSpouse } from '../../lib/utils'
+import SummaryInfo from './SummaryInfo'
+import ContactInfo from './ContactInfo'
+import UserInfo from './UserInfo'
+import Comment from './Comment'
 
 const SummaryForm = ({
   application,
@@ -88,6 +92,49 @@ const SummaryForm = ({
       })
   }
 
+  const summaryItems = [
+    {
+      route: Routes.INRELATIONSHIP,
+      label: m.inRelationship.general.sectionTitle,
+      info: m.inRelationship.general.sectionTitle,
+    },
+    {
+      route: Routes.HOMECIRCUMSTANCES,
+      label: m.homeCircumstancesForm.general.sectionTitle,
+      info: answers?.homeCircumstances.type === HomeCircumstances.OTHER
+        ? answers?.homeCircumstances?.custom
+        : getMessageHomeCircumstances[answers?.homeCircumstances?.type],
+    },
+    {
+      route: Routes.STUDENT,
+      label: m.studentForm.general.sectionTitle,
+      info: getMessageApproveOptions[answers?.student?.isStudent],
+      comment: answers?.student?.isStudent === ApproveOptions.Yes ? answers?.student?.custom : undefined
+    },
+    {
+      route: Routes.EMPLOYMENT,
+      label: m.employmentForm.general.sectionTitle,
+      info: answers?.employment.type === Employment.OTHER
+        ? answers?.employment.custom
+        : getMessageEmploymentStatus[answers.employment?.type],
+    },
+    {
+      route: Routes.INCOME,
+      label: m.incomeForm.general.sectionTitle,
+      info: getMessageApproveOptionsForIncome[answers?.income],
+    },
+    {
+      route: Routes.PERSONALTAXCREDIT,
+      label: m.summaryForm.formInfo.personalTaxCreditTitle,
+      info: getMessageApproveOptions[answers?.personalTaxCredit],
+    },
+    {
+      route: Routes.BANKINFO,
+      label: m.bankInfoForm.general.sectionTitle,
+      info: formatBankInfo(answers?.bankInfo),
+    },
+  ]
+
   return (
     <>
       <Box
@@ -124,50 +171,6 @@ const SummaryForm = ({
         </Box>
       )}
 
-      <Box marginTop={[4, 4, 5]}>
-        <DescriptionText text={m.summaryForm.general.calculationsOverview} />
-      </Box>
-
-      <Box
-        paddingY={[4, 4, 5]}
-        marginTop={4}
-        borderTopWidth="standard"
-        borderColor="blue300"
-      >
-        <GridRow marginBottom={3}>
-          <GridColumn span={['12/12', '12/12', '12/12', '5/12']}>
-            <Box>
-              <Text fontWeight="semiBold">
-                {formatMessage(m.summaryForm.userInfo.name)}
-              </Text>
-              <Text>
-                {externalData.nationalRegistry?.data?.applicant?.fullName}
-              </Text>
-            </Box>
-          </GridColumn>
-          <GridColumn span={['12/12', '12/12', '12/12', '5/12']}>
-            <Box marginTop={[3, 3, 3, 0]}>
-              <Text fontWeight="semiBold">
-                {formatMessage(m.summaryForm.userInfo.nationalId)}
-              </Text>
-              <Text>
-                {externalData?.nationalRegistry?.data?.applicant?.nationalId}
-              </Text>
-            </Box>
-          </GridColumn>
-          <GridColumn span={['12/12', '12/12', '12/12', '5/12']}>
-            <Box marginTop={3}>
-              <Text fontWeight="semiBold">
-                {formatMessage(m.summaryForm.userInfo.address)}
-              </Text>
-              <Text>
-                {formatAddress(externalData.nationalRegistry?.data?.applicant)}
-              </Text>
-            </Box>
-          </GridColumn>
-        </GridRow>
-      </Box>
-
       <SummaryBlock editAction={() => goToScreen?.(Routes.INRELATIONSHIP)}>
         <Text fontWeight="semiBold">
           {formatMessage(m.inRelationship.general.sectionTitle)}
@@ -179,149 +182,25 @@ const SummaryForm = ({
           )}
         </Text>
       </SummaryBlock>
+      
+      <UserInfo name={externalData?.nationalRegistry?.data?.applicant?.fullName} nationalId={externalData?.nationalRegistry?.data?.applicant?.nationalId} address={formatAddress(externalData.nationalRegistry?.data?.applicant)} />
 
-      <SummaryBlock editAction={() => goToScreen?.(Routes.HOMECIRCUMSTANCES)}>
-        <Text fontWeight="semiBold">
-          {formatMessage(m.homeCircumstancesForm.general.sectionTitle)}
-        </Text>
+      <SummaryInfo items={summaryItems} goToScreen={goToScreen} />
 
-        {answers?.homeCircumstances && (
-          <Text>
-            {answers?.homeCircumstances.type === HomeCircumstances.OTHER
-              ? answers?.homeCircumstances?.custom
-              : formatMessage(
-                  getMessageHomeCircumstances[answers?.homeCircumstances?.type],
-                )}
-          </Text>
-        )}
-      </SummaryBlock>
+      <ContactInfo route={Routes.CONTACTINFO} email={answers?.contactInfo?.email} phone={answers?.contactInfo?.phone} goToScreen={goToScreen} />
 
-      <SummaryBlock editAction={() => goToScreen?.(Routes.STUDENT)}>
-        <Text fontWeight="semiBold">
-          {formatMessage(m.studentForm.general.sectionTitle)}
-        </Text>
+      <AllFiles
+        route={answers.income === ApproveOptions.Yes
+          ? Routes.INCOMEFILES
+          : Routes.TAXRETURNFILES}
+        goToScreen={goToScreen}
+        taxFiles={answers.taxReturnFiles}
+        incomeFiles={answers.incomeFiles}
+        applicationId={id}
+      />
 
-        <Text>
-          {formatMessage(getMessageApproveOptions[answers?.student?.isStudent])}
-        </Text>
+      <Comment commentId={formCommentId} comment={answers?.formComment} />
 
-        {answers?.student?.isStudent === ApproveOptions.Yes && (
-          <Text marginTop={2}>{answers?.student?.custom}</Text>
-        )}
-      </SummaryBlock>
-
-      <SummaryBlock editAction={() => goToScreen?.(Routes.EMPLOYMENT)}>
-        <Text fontWeight="semiBold">
-          {formatMessage(m.employmentForm.general.sectionTitle)}
-        </Text>
-
-        {answers?.employment && (
-          <Text>
-            {answers?.employment.type === Employment.OTHER
-              ? answers?.employment.custom
-              : formatMessage(
-                  getMessageEmploymentStatus[answers.employment?.type],
-                )}
-          </Text>
-        )}
-      </SummaryBlock>
-
-      <SummaryBlock editAction={() => goToScreen?.(Routes.INCOME)}>
-        <Text fontWeight="semiBold">
-          {formatMessage(m.incomeForm.general.sectionTitle)}
-        </Text>
-
-        <Text>
-          {formatMessage(getMessageApproveOptionsForIncome[answers?.income])}
-        </Text>
-      </SummaryBlock>
-
-      <SummaryBlock editAction={() => goToScreen?.(Routes.PERSONALTAXCREDIT)}>
-        <Text fontWeight="semiBold">
-          {formatMessage(m.summaryForm.formInfo.personalTaxCreditTitle)}
-        </Text>
-        {answers?.personalTaxCredit && (
-          <Text>
-            {formatMessage(
-              getMessageApproveOptions[answers?.personalTaxCredit],
-            )}
-          </Text>
-        )}
-      </SummaryBlock>
-
-      <SummaryBlock editAction={() => goToScreen?.(Routes.BANKINFO)}>
-        <Text fontWeight="semiBold">
-          {formatMessage(m.bankInfoForm.general.sectionTitle)}
-        </Text>
-
-        <Text>{formatBankInfo(answers?.bankInfo)}</Text>
-      </SummaryBlock>
-
-      <SummaryBlock editAction={() => goToScreen?.(Routes.CONTACTINFO)}>
-        <GridRow>
-          <GridColumn span={['12/12', '12/12', '12/12', '6/12']}>
-            <Text fontWeight="semiBold">
-              {formatMessage(m.contactInfo.emailInput.label)}{' '}
-            </Text>
-            <Text marginBottom={[3, 3, 3, 0]}>
-              {answers?.contactInfo?.email}
-            </Text>
-          </GridColumn>
-          <GridColumn span={['12/12', '12/12', '12/12', '6/12']}>
-            <Text fontWeight="semiBold">
-              {formatMessage(m.contactInfo.phoneInput.label)}{' '}
-            </Text>
-            <Text>{answers?.contactInfo?.phone}</Text>
-          </GridColumn>
-        </GridRow>
-      </SummaryBlock>
-
-      <SummaryBlock
-        editAction={() =>
-          answers.income === ApproveOptions.Yes
-            ? goToScreen?.(Routes.INCOMEFILES)
-            : goToScreen?.(Routes.TAXRETURNFILES)
-        }
-      >
-        <Text fontWeight="semiBold">Gögn</Text>
-
-        <AllFiles
-          taxFiles={answers.taxReturnFiles}
-          incomeFiles={answers.incomeFiles}
-          applicationId={id}
-        />
-      </SummaryBlock>
-
-      <Text as="h3" variant="h3">
-        {formatMessage(m.summaryForm.formInfo.formCommentLabel)}
-      </Text>
-
-      <Box marginTop={[3, 3, 4]} marginBottom={4}>
-        <Controller
-          name={formCommentId}
-          defaultValue={answers?.formComment}
-          render={({ value, onChange }) => {
-            return (
-              <Input
-                id={formCommentId}
-                name={formCommentId}
-                label={formatMessage(m.summaryForm.formInfo.formCommentTitle)}
-                placeholder={formatMessage(
-                  m.summaryForm.formInfo.formCommentPlaceholder,
-                )}
-                value={value}
-                textarea={true}
-                rows={8}
-                backgroundColor="blue"
-                onChange={(e) => {
-                  onChange(e.target.value)
-                  setValue(formCommentId, e.target.value)
-                }}
-              />
-            )
-          }}
-        />
-      </Box>
       <Box
         className={cn(styles.errorMessage, {
           [`${styles.showErrorMessage}`]: formError,
