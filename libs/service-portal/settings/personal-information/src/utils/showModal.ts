792cc57ab73e3d294bfa35be586dd7b0b39f9513
Þórður Hafliðason <tolleinn@gmail.com>
2022-03-16 09:33:48 +0000

    Update showModal rule

--- libs/service-portal/settings/personal-information/src/utils/showModal.ts
+++ libs/service-portal/settings/personal-information/src/utils/showModal.ts
@@ -5,9 +5,7 @@ import differenceInMonths from 'date-fns/differenceInMonths'
  * MODIFIED_FALLBACK_TIME
  * This represents the date of the deployment of this feature.
  * It's used for displaying the modal at least 6 months from deployment.
- * For some use-cases we can have a user come in with email+tel,
- * but still not any modification date,
- * since the email+tel comes from islyklar service.
+ * For some use-cases we can have a user come in with email+tel, but no modification date.
  * When the user interacts with the modal, a modification date is set.
  */
 const MODIFIED_FALLBACK_TIME = new Date('2022-04-01T00:00:00Z')
@@ -24,9 +22,7 @@ export const showModal = (getUserProfile: UserProfile | undefined | null) => {
   const modifiedProfileDate = getUserProfile.modified ?? MODIFIED_FALLBACK_TIME
   const dateNow = new Date()
   const dateModified = new Date(modifiedProfileDate)
-  const diffInMonths = modifiedProfileDate
-    ? differenceInMonths(dateNow, dateModified)
-    : 0
+  const diffInMonths = differenceInMonths(dateNow, dateModified)
   const diffModifiedOverMaxDate = diffInMonths >= 6
 
   return (
