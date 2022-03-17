fa64f1a0cf046531ef1e232af4606f635d9eeb85
Kristofer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-03-17 13:35:55 +0000

    Remove unnecessary commented imports

--- libs/clients/driving-license-book/src/lib/drivingLicenseBookClient.module.ts
+++ libs/clients/driving-license-book/src/lib/drivingLicenseBookClient.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common'
-// import { ApiConfiguration } from './apiConfiguration'
-// import { exportedApis } from './apis'
 import { DrivingLicenseBookClientService } from './drivingLicenseBookClient.service'
 
 @Module({
