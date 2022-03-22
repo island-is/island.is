54ba5615318cf4b19232c670652b325d124103e9
Arnar Kári <arnar.kari@kolibri.is>
2022-03-22 15:49:55 +0000

    refactor(judicial-system): rename Restriction/Investingation requests

--- apps/judicial-system/web/pages/domur/fyrirtokutimi/[id].ts
+++ apps/judicial-system/web/pages/domur/fyrirtokutimi/[id].ts
@@ -1,3 +1,3 @@
-import { HearingArrangements } from '@island.is/judicial-system-web/src/routes/Court/RestrictionRequest/HearingArrangements/HearingArrangements'
+import { HearingArrangements } from '@island.is/judicial-system-web/src/routes/Court/RestrictionCase/HearingArrangements/HearingArrangements'
 
 export default HearingArrangements
