3671461f0603ae2ebb4c9b2612f489d882b4ed10
Arnar Kári <arnar.kari@kolibri.is>
2022-03-22 16:06:40 +0000

    refactor(judicial-system): remove unused prop

--- apps/judicial-system/web/pages/krafa/ny/gaesluvardhald.tsx
+++ apps/judicial-system/web/pages/krafa/ny/gaesluvardhald.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import StepOne from '@island.is/judicial-system-web/src/routes/Prosecutor/RestrictionCase/StepOne/StepOne'
-import { CaseType } from '@island.is/judicial-system/types'
 
 const NewCustody = () => {
-  return <StepOne type={CaseType.CUSTODY} />
+  return <StepOne />
 }
 
 export default NewCustody
