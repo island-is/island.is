bd9b4cada239f2889c2475133f9dde435f7eb381
Maciej <maciej@viceversa.is>
2022-03-23 14:17:18 +0000

    adjust formatters, fix tests

--- libs/application/ui-components/src/utilities/formatters.spec.tsx
+++ libs/application/ui-components/src/utilities/formatters.spec.tsx
@@ -7,8 +7,9 @@ describe('formatters', () => {
     it('should return the same value if bank info is in correct format', async () => {
       // arrange
       const bankInfo = '0000-11-222222'
+      const expectedBankInfo = '0000-11-222222'
       // assert
-      expect(formatBankInfo(bankInfo)).toBe(bankInfo)
+      expect(formatBankInfo(bankInfo)).toBe(expectedBankInfo)
     })
 
     it('should format bank info if it comes as 14 characters length string', async () => {
@@ -22,7 +23,7 @@ describe('formatters', () => {
     it('should return empty string if bank info is too long', async () => {
       // arrange
       const bankInfo = '0000112222222'
-      const expectedBankInfo = ''
+      const expectedBankInfo = '0000112222222'
       // assert
       expect(formatBankInfo(bankInfo)).toBe(expectedBankInfo)
     })
@@ -30,7 +31,7 @@ describe('formatters', () => {
     it('should return empty string if bank info comes in weird format', async () => {
       // arrange
       const bankInfo = '000#test011222$$222'
-      const expectedBankInfo = ''
+      const expectedBankInfo = '000#test011222$$222'
       // assert
       expect(formatBankInfo(bankInfo)).toBe(expectedBankInfo)
     })
--- libs/application/ui-components/src/utilities/formatters.ts
+++ libs/application/ui-components/src/utilities/formatters.ts
@@ -1,12 +1,8 @@
 export const formatBankInfo = (bankInfo: string) => {
-  if (/^\d{4}-\d{2}-\d{6}$/.test(bankInfo)) {
-    return bankInfo
-  }
-
   const formattedBankInfo = bankInfo.replace(/^(.{4})(.{2})/, '$1-$2-')
   if (formattedBankInfo && formattedBankInfo.length === 14) {
     return formattedBankInfo
   }
 
-  return ''
+  return bankInfo
 }
