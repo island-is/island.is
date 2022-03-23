a70d5474fc2be18de4f465e382018918a0afefdb
Kristofer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-03-23 09:58:28 +0000

    Add types

--- libs/api/domains/driving-license-book/src/drivinLicenceBook.type.ts
+++ libs/api/domains/driving-license-book/src/drivinLicenceBook.type.ts
@@ -19,3 +19,75 @@ export interface DrivingLicenseBookStudent {
   active: boolean
   bookLicenseCategories: string[]
 }
+
+export interface DrivingBookLesson {
+  id: string
+  registerDate: string
+  lessonTime: number
+  teacherNationalId: string
+  teacherName: string
+  comments: string
+}
+
+export interface DrivingSchoolExam {
+  id: string
+  examDate: string
+  schoolNationalId: string
+  schoolName: string
+  schoolEmployeeNationalId: string
+  schoolEmployeeName: string
+  schoolTypeId: number
+  schoolTypeName: string
+  schoolTypeCode: string
+  comments: string
+}
+
+export interface DrivingLicenceTestResult {
+  id: string
+  examDate: string
+  score: number
+  scorePart1: number
+  scorePart2: number
+  hasPassed: boolean
+  testCenterNationalId: string
+  testCenterName: string
+  testExaminerNationalId: string
+  testExaminerName: string
+  testTypeId: number
+  testTypeName: string
+  testTypeCode: string
+  comments: string
+}
+
+export interface DrivingLicenseBook {
+  id: string
+  licenseCategory: string
+  createdOn: string
+  teacherNationalId: string
+  teacherName: string
+  schoolNationalId: string
+  schoolName: string
+  isDigital: boolean
+  totalLessonTime: number
+  totalLessonCount: number
+  teachersAndLessons: DrivingBookLesson[]
+  drivingSchoolExams: DrivingSchoolExam[]
+  testResults: DrivingLicenceTestResult[]
+}
+
+export interface DrivingLicenseBookStudentOverview extends DrivingLicenseBookStudent {
+  book: DrivingLicenseBook
+}
+
+export interface PracticalDrivingLesson {
+  bookId: string
+  id: string
+  studentNationalId: string
+  studentName: string
+  licenseCategory: string
+  teacherNationalId: string
+  teacherName: string
+  minutes: number
+  createdOn: string
+  comments: string
+}
