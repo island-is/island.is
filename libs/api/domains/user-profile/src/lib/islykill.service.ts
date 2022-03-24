60ab9518c624e3fc53336c0d338946205d278e97
Þórður Hafliðason <tolleinn@gmail.com>
2022-03-24 16:20:27 +0000

    Onboarding updates

--- libs/api/domains/user-profile/src/lib/islykill.service.ts
+++ libs/api/domains/user-profile/src/lib/islykill.service.ts
@@ -90,7 +90,6 @@ export class IslykillService {
       ssn: nationalId,
       email,
       mobile,
-      canNudge: true, // When creating an account, the default value of canNudge should always be true.
     }
 
     const errorMsg = 'Unable to create islykill settings for user'
