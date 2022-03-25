bb8d4adc9d57093efcb18caa151ba3464356b1ed
Kristofer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-03-25 10:42:26 +0000

    In-file announcement-of-death-submission -> announcement-of-death

--- libs/application/template-api-modules/src/lib/modules/templates/announcement-of-death/announcement-of-death.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/announcement-of-death/announcement-of-death.module.ts
@@ -1,6 +1,6 @@
 import { DynamicModule } from '@nestjs/common'
 import { SyslumennClientModule } from '@island.is/clients/syslumenn'
-import { AnnouncementOfDeathSubmissionService } from './announcement-of-death-submission.service'
+import { AnnouncementOfDeathSubmissionService } from './announcement-of-death.service'
 import { SharedTemplateAPIModule } from '../../shared'
 import { BaseTemplateAPIModuleConfig } from '../../../types'
 
