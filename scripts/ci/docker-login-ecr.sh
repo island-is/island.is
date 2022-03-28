37d8c23c7d56fd265b245fe27dc87aaa76816b24
Snorri Hjörvar Jóhannsson <snorri@andes.is>
2022-03-28 10:37:42 +0000

    Lets see if this works

--- scripts/ci/docker-login-ecr.sh
+++ scripts/ci/docker-login-ecr.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 
-aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin "${DOCKER_REGISTRY}"
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin "${DOCKER_REGISTRY}"
