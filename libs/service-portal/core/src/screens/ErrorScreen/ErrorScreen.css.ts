cb56f889e92034e04d00d5b0b9be9cf06baff58d
disaerna <disa.erna@gmail.com>
2022-03-29 13:18:02 +0000

    fix: image css

--- libs/service-portal/core/src/screens/ErrorScreen/ErrorScreen.css.ts
+++ libs/service-portal/core/src/screens/ErrorScreen/ErrorScreen.css.ts
@@ -2,12 +2,11 @@ import { themeUtils } from '@island.is/island-ui/theme'
 import { style } from '@vanilla-extract/css'
 
 export const img = style({
-  maxHeight: '25vh',
   minHeight: '20vh',
   ...themeUtils.responsiveStyle({
     md: {
-      maxHeight: 'unset',
-      minHeight: '30vh',
+      maxHeight: '300px',
+      minHeight: '250px',
     },
   }),
 })
