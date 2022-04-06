e838def38b172cb98c045d85943232932343486e
runarvestmann <runarvestmann@gmail.com>
2022-04-06 13:34:02 +0000

    Added new field and also added fail safe for project subpage

--- libs/cms/src/lib/models/projectPage.model.ts
+++ libs/cms/src/lib/models/projectPage.model.ts
@@ -61,27 +61,35 @@ export class ProjectPage {
 
   @Field()
   defaultHeaderBackgroundColor!: string
+
+  @Field()
+  featuredDescription!: string
 }
 
-export const mapProjectPage = ({ sys, fields }: IProjectPage): ProjectPage => ({
-  id: sys.id,
-  title: fields.title ?? '',
-  slug: fields.slug ?? '',
-  theme: fields.theme ?? 'default',
-  sidebar: fields.sidebar ?? false,
-  sidebarLinks: (fields.sidebarLinks ?? []).map(mapLinkGroup),
-  subtitle: fields.subtitle ?? '',
-  intro: fields.intro ?? '',
-  content: fields.content
-    ? mapDocument(fields.content, sys.id + ':content')
-    : [],
-  stepper: fields.stepper ? mapStepper(fields.stepper) : null,
-  slices: (fields.slices ?? []).map(safelyMapSliceUnion),
-  newsTag: fields.newsTag ? mapGenericTag(fields.newsTag) : null,
-  projectSubpages: (fields.projectSubpages ?? []).map(mapProjectSubpage),
-  featuredImage: fields.featuredImage ? mapImage(fields.featuredImage) : null,
-  defaultHeaderImage: fields.defaultHeaderImage
-    ? mapImage(fields.defaultHeaderImage)
-    : null,
-  defaultHeaderBackgroundColor: fields.defaultHeaderBackgroundColor ?? '',
-})
+export const mapProjectPage = ({ sys, fields }: IProjectPage): ProjectPage => {
+  return {
+    id: sys.id,
+    title: fields.title ?? '',
+    slug: fields.slug ?? '',
+    theme: fields.theme ?? 'default',
+    sidebar: fields.sidebar ?? false,
+    sidebarLinks: (fields.sidebarLinks ?? []).map(mapLinkGroup),
+    subtitle: fields.subtitle ?? '',
+    intro: fields.intro ?? '',
+    content: fields.content
+      ? mapDocument(fields.content, sys.id + ':content')
+      : [],
+    stepper: fields.stepper ? mapStepper(fields.stepper) : null,
+    slices: (fields.slices ?? []).map(safelyMapSliceUnion),
+    newsTag: fields.newsTag ? mapGenericTag(fields.newsTag) : null,
+    projectSubpages: (fields.projectSubpages ?? [])
+      .filter((p) => p.fields?.title)
+      .map(mapProjectSubpage),
+    featuredImage: fields.featuredImage ? mapImage(fields.featuredImage) : null,
+    defaultHeaderImage: fields.defaultHeaderImage
+      ? mapImage(fields.defaultHeaderImage)
+      : null,
+    defaultHeaderBackgroundColor: fields.defaultHeaderBackgroundColor ?? '',
+    featuredDescription: fields.featuredDescription ?? '',
+  }
+}
