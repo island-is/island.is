b9b889f542a9bbb0d7290949ef4bd23d670d4cfd
runarvestmann <runarvestmann@gmail.com>
2022-04-06 10:24:58 +0000

    Removed the z index prop

--- libs/island-ui/core/src/lib/DropdownMenu/DropdownMenu.tsx
+++ libs/island-ui/core/src/lib/DropdownMenu/DropdownMenu.tsx
@@ -38,7 +38,6 @@ export interface DropdownMenuProps {
    */
   icon?: ButtonProps['icon']
   disclosure?: ReactElement
-  zIndex?: number
 }
 
 export const DropdownMenu = ({
@@ -47,7 +46,6 @@ export const DropdownMenu = ({
   title,
   icon,
   disclosure,
-  zIndex,
 }: DropdownMenuProps) => {
   const menu = useMenuState({ placement: 'bottom', gutter: 8 })
   const menuBoxStyle = useBoxStyles({
@@ -85,7 +83,6 @@ export const DropdownMenu = ({
         {...menu}
         aria-label={menuLabel}
         className={cn(styles.menu, menuBoxStyle)}
-        style={{ zIndex }}
       >
         {items.map((item, index) => {
           let anchorProps = {}
