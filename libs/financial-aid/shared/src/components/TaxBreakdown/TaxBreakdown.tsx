4f6691b05ee70d21c53e6f1090185b446ff6a9be
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-04-07 15:44:38 +0000

    Tax breakdown fix

--- libs/financial-aid/shared/src/components/TaxBreakdown/TaxBreakdown.tsx
+++ libs/financial-aid/shared/src/components/TaxBreakdown/TaxBreakdown.tsx
@@ -8,6 +8,7 @@ import {
   formatNationalId,
   getMonth,
 } from '@island.is/financial-aid/shared/lib'
+import TaxBreakdownHeadline from './TaxBreakdownHeadline'
 
 export const taxBreakDownHeaders = [
   'Fyrirtæki',
@@ -21,6 +22,38 @@ interface Props {
 }
 
 const TaxBreakdown = ({ items }: Props) => {
+  const date = new Date()
+  const currentMonth = date.getMonth()
+  const currentYear = date.getFullYear()
+  const lastThreeMonths = []
+
+  // Create an array with the year and month of the last three months
+  for (let i = 1; i <= 3; i++) {
+    const month = currentMonth - i
+    const year = month < 0 ? currentYear - 1 : currentYear
+
+    lastThreeMonths.unshift({ year, month: month < 0 ? 12 + month : month })
+  }
+
+  const filterTaxByMonth = (month: number) => {
+    const taxInMonth = items.filter((item) => item.month - 1 === month)
+
+    if (taxInMonth.length > 0) {
+      return taxInMonth.map((item, i) => (
+        <TaxBreakdownItem
+          key={`${i}-${month}-taxBreakdown-${item.payerNationalId}`}
+          items={[
+            formatNationalId(item.payerNationalId),
+            `${item.totalSalary.toLocaleString('de-DE')} kr.`,
+            `${item.personalAllowance.toLocaleString('de-DE')} kr.`,
+            `${item.withheldAtSource.toLocaleString('de-DE')} kr.`,
+          ]}
+        />
+      ))
+    }
+    return <TaxBreakdownItem items={['Engin staðgreiðsla']} />
+  }
+
   return (
     <table className={styles.tableContainer}>
       <thead>
@@ -37,20 +70,15 @@ const TaxBreakdown = ({ items }: Props) => {
         </tr>
       </thead>
       <tbody>
-        {items.map((i, index) => {
-          return (
-            <TaxBreakdownItem
-              headline={`${getMonth(i.month - 1)} ${i.year}`}
-              key={`${index}-taxbreakDown-${i.payerNationalId}-${i.userType}`}
-              items={[
-                formatNationalId(i.payerNationalId),
-                `${i.totalSalary.toLocaleString('de-DE')} kr.`,
-                `${i.personalAllowance.toLocaleString('de-DE')} kr.`,
-                `${i.withheldAtSource.toLocaleString('de-DE')} kr.`,
-              ]}
+        {lastThreeMonths.map((i) => (
+          <>
+            <TaxBreakdownHeadline
+              key={`${i.month}-taxHeadline`}
+              headline={`${getMonth(i.month)} ${i.year}`}
             />
-          )
-        })}
+            {filterTaxByMonth(i.month)}
+          </>
+        ))}
       </tbody>
     </table>
   )
