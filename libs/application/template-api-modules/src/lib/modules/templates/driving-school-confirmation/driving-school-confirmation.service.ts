bc2446a73b57a59f76d3e342e2c1dab2abe2f79a
Alexandra Diljá <alexandrab18@ru.is>
2022-04-07 15:54:20 +0000

    submit

--- libs/application/template-api-modules/src/lib/modules/templates/driving-school-confirmation/driving-school-confirmation.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/driving-school-confirmation/driving-school-confirmation.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { TemplateApiModuleActionProps } from '../../../types'
 import { DrivingLicenseBookService } from '@island.is/api/domains/driving-license-book'
 import { DrivingSchool, DrivingSchoolType } from './types'
+import { getValueViaPath } from '@island.is/application/core'
 
 @Injectable()
 export class DrivingSchoolConfirmationService {
@@ -9,22 +10,52 @@ export class DrivingSchoolConfirmationService {
     private readonly drivingLicenseBookService: DrivingLicenseBookService,
   ) {}
 
-  async submitApplication({ application, auth }: TemplateApiModuleActionProps) {
-    // const schoolType = (application.externalData.schoolTypes
-    //   .data as DrivingSchoolType[]).find(
-    //   (type) => type.schoolTypeCode === application.answers.schoolTypeCode,
-    // )
-    // const payload = {
-    //   bookId: '',
-    //   schoolTypeId: schoolType?.schoolTypeId,
-    //   schoolNationlId:
-    //     (application.externalData.employee?.data as DrivingSchool).nationalId ??
-    //     '',
-    //   schoolEmployeeNationalId: auth.nationalId,
-    //   createdOn: '',
-    //   comments: '',
-    // }
+  async submitApplication({
+    application: { answers, externalData },
+    auth,
+  }: TemplateApiModuleActionProps) {
+    console.log('DRIVING SCHOOL SERVICE')
+    console.log(answers)
+    // TODO: Confirmation email
+    const nationalId = getValueViaPath<string>(answers, 'nationalId')
+    const email = getValueViaPath<string>(answers, 'email')
 
-    return { success: true, id: '123' }
+    const confirmation = getValueViaPath<{ date: string; school: string }>(
+      answers,
+      'confirmation',
+    )
+    if (!confirmation) {
+      throw new Error(`Missing date and school`)
+    }
+    const schoolNationlId = (externalData.employee.data as DrivingSchool)
+      .nationalId
+    console.log({
+      bookId: '398D2BEA-0ABE-49CF-88EE-171E83F058DE',
+      schoolTypeId: parseInt(confirmation?.school),
+      schoolNationlId: schoolNationlId,
+      schoolEmployeeNationalId: auth.nationalId,
+      createdOn: confirmation?.date,
+      comments: '',
+    })
+    try {
+      const result = await this.drivingLicenseBookService.createDrivingSchoolTestResult(
+        {
+          bookId: '398D2BEA-0ABE-49CF-88EE-171E83F058DE',
+          schoolTypeId: parseInt(confirmation?.school),
+          schoolNationlId: schoolNationlId,
+          schoolEmployeeNationalId: auth.nationalId,
+          createdOn: confirmation?.date,
+          comments: '',
+        },
+      )
+
+      if (!result) {
+        throw new Error(`Application submission failed`)
+      }
+
+      return { success: true, id: result.id }
+    } catch (e) {
+        throw new Error(`Application submission failed`)
+    }
   }
 }
