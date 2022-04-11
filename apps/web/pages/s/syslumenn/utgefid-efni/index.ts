2122a3fac3fc92a88f7a593155fe1452c7a00caa
runarvestmann <runarvestmann@gmail.com>
2022-04-11 15:36:15 +0000

    Changed comments

--- apps/web/pages/s/syslumenn/utgefid-efni/index.ts
+++ apps/web/pages/s/syslumenn/utgefid-efni/index.ts
@@ -5,11 +5,7 @@ import { withLocale } from '@island.is/web/i18n'
 import subPage from '@island.is/web/screens/Organization/SubPage'
 
 /**
- * This page was added since syslumenn have already set up their published material
- * by creating an organization subpage filled with accordions and file links.
- *
- * That page has the same slug as the new generic published material page for organizations.
- *
- * This page will be removed in the future if syslumenn want to utilize the new generic published material page instead
+ * This page was added since syslumenn have a custom published material page set up in Contentful.
+ * That page has the same slug as the new generic published material page for organizations, so that's why we explicitly added this route.
  */
 export default withApollo(withLocale('is')(subPage))
