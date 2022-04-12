cc56e9f13c11d2820c9d64645383d7904950dcfa
runarvestmann <runarvestmann@gmail.com>
2022-04-12 08:58:29 +0000

    Removed mock import

--- apps/web/pages/s/syslumenn/utgefid-efni/index.ts
+++ apps/web/pages/s/syslumenn/utgefid-efni/index.ts
@@ -1,5 +1,3 @@
-import '@island.is/api/mocks'
-
 import withApollo from '@island.is/web/graphql/withApollo'
 import { withLocale } from '@island.is/web/i18n'
 import subPage from '@island.is/web/screens/Organization/SubPage'
