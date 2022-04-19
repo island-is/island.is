2bfa4e29cc4cb53a11381fdd8969e4074ba32b4f
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-04-19 12:50:54 +0000

    Remove optional config

--- libs/clients/municipalities-financial-aid/src/municipalitiesFinancialAidClient.module.ts
+++ libs/clients/municipalities-financial-aid/src/municipalitiesFinancialAidClient.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { exportedApis } from './apis'
-import { MunicipalitiesFinancialAidConfig } from './municipalitiesFinancialAid.config'
 
 @Module({
-  imports: [MunicipalitiesFinancialAidConfig.registerOptional()],
   providers: exportedApis,
   exports: exportedApis,
 })
