5af04e0a8b1f4a5fa265b6e3115953c19fc3da23
runarvestmann <runarvestmann@gmail.com>
2022-04-20 12:28:49 +0000

    Added FeatureFlagUser

--- apps/web/utils/featureFlag.ts
+++ apps/web/utils/featureFlag.ts
@@ -1,11 +1,12 @@
-import { createClient } from '@island.is/feature-flags'
+import { createClient, FeatureFlagUser } from '@island.is/feature-flags'
 
 export const getFeatureFlag = async (
   flag: string,
   defaultValue: boolean | string,
+  user?: FeatureFlagUser,
 ) => {
   const client = createClient()
-  const value = await client.getValue(flag, defaultValue)
+  const value = await client.getValue(flag, defaultValue, user)
   client.dispose()
   return value
 }
