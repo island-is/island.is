2d1fc7afdabb27120f18a695385d59198c80cb8a
runarvestmann <runarvestmann@gmail.com>
2022-04-20 15:14:21 +0000

    Added sdk key to client public runtime config

--- apps/web/utils/featureFlag.ts
+++ apps/web/utils/featureFlag.ts
@@ -1,12 +1,23 @@
-import { createClient, FeatureFlagUser } from '@island.is/feature-flags'
+import {
+  createClient,
+  FeatureFlagUser,
+  FeatureFlagClient,
+} from '@island.is/feature-flags'
+import getConfig from 'next/config'
+
+const { publicRuntimeConfig } = getConfig()
+let client: FeatureFlagClient | null
 
 export const getFeatureFlag = async (
   flag: string,
   defaultValue: boolean | string,
   user?: FeatureFlagUser,
 ) => {
-  const client = createClient()
+  if (!client) {
+    client = createClient({
+      sdkKey: publicRuntimeConfig.configCatSdkKey,
+    })
+  }
   const value = await client.getValue(flag, defaultValue, user)
-  client.dispose()
   return value
 }
