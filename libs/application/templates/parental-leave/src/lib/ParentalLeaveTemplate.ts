95b36f2336ef6d1ff6f9f4f37b1b9a42a03e2848
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-22 09:27:39 +0200

    feat: clear personalAllowance and personalAllowanmceFromSpouse if the conditions are met

--- libs/application/templates/parental-leave/src/lib/ParentalLeaveTemplate.ts
+++ libs/application/templates/parental-leave/src/lib/ParentalLeaveTemplate.ts
@@ -100,7 +100,11 @@ const ParentalLeaveTemplate: ApplicationTemplate<
       },
       [States.DRAFT]: {
         entry: 'clearAssignees',
-        exit: 'setOtherParentIdIfSelectedSpouse',
+        exit: [
+          'setOtherParentIdIfSelectedSpouse',
+          'clearPersonalAllowanceIfUsePersonalAllowanceIsNo',
+          'clearSpouseAllowanceIfUseSpouseAllowanceIsNo'
+        ],
         meta: {
           name: States.DRAFT,
           actionCard: {
@@ -678,6 +682,38 @@ const ParentalLeaveTemplate: ApplicationTemplate<
             application.answers,
             'otherParentId',
             getOtherParentId(application),
+          )
+        }
+
+        return context
+      }),
+      clearPersonalAllowanceIfUsePersonalAllowanceIsNo: assign((context) => {
+        const { application } = context
+
+        const answers = getApplicationAnswers(application.answers)
+
+        if (answers.usePersonalAllowance === NO
+          && (answers.personalUsage || answers.personalUseAsMuchAsPossible)) {
+          set(
+            application.answers,
+            'personalAllowance',
+            null,
+          )
+        }
+
+        return context
+      }),
+      clearSpouseAllowanceIfUseSpouseAllowanceIsNo: assign((context) => {
+        const { application } = context
+
+        const answers = getApplicationAnswers(application.answers)
+
+        if (answers.usePersonalAllowanceFromSpouse === NO
+          && (answers.spouseUsage || answers.spouseUseAsMuchAsPossible)) {
+          set(
+            application.answers,
+            'personalAllowanceFromSpouse',
+            null,
           )
         }
 
