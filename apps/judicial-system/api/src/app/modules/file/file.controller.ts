69e93e3167e6263e14aed4cae1ca3de5acef4860
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-04-26 10:38:44 +0000

    Refactors api file controller

--- apps/judicial-system/api/src/app/modules/file/file.controller.ts
+++ apps/judicial-system/api/src/app/modules/file/file.controller.ts
@@ -38,7 +38,7 @@ export class FileController {
 
   private async getPdf(
     id: string,
-    pdfDoc: string,
+    route: string,
     req: Request,
     res: Response,
   ): Promise<Response> {
@@ -48,7 +48,7 @@ export class FileController {
     headers.set('cookie', req.headers.cookie as string)
 
     const result = await fetch(
-      `${environment.backend.url}/api/case/${id}/${pdfDoc}`,
+      `${environment.backend.url}/api/case/${id}/${route}`,
       { headers },
     )
 
@@ -63,6 +63,30 @@ export class FileController {
     return stream.pipe(res)
   }
 
+  private async tryGetPdf(
+    userId: string,
+    auditAction: AuditedAction,
+    id: string,
+    route: string,
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    try {
+      return this.auditTrailService.audit(
+        userId,
+        auditAction,
+        this.getPdf(id, route, req, res),
+        id,
+      )
+    } catch (error) {
+      if (error instanceof FileExeption) {
+        return res.status(error.status).json(error.message)
+      }
+
+      throw error
+    }
+  }
+
   @Get('request')
   @Header('Content-Type', 'application/pdf')
   async getRequestPdf(
@@ -73,25 +97,14 @@ export class FileController {
   ): Promise<Response> {
     this.logger.debug(`Getting the request for case ${id} as a pdf document`)
 
-    try {
-      return this.auditTrailService.audit(
-        user.id,
-        AuditedAction.GET_REQUEST_PDF,
-        this.getPdf(id, 'request', req, res),
-        id,
-      )
-    } catch (error) {
-      this.logger.debug(
-        `Failed to get the request for case ${id} as a pdf document`,
-        error,
-      )
-
-      if (error instanceof FileExeption) {
-        return res.status(error.status).json(error.message)
-      }
-
-      throw error
-    }
+    return this.tryGetPdf(
+      user.id,
+      AuditedAction.GET_REQUEST_PDF,
+      id,
+      'request',
+      req,
+      res,
+    )
   }
 
   @Get('courtRecord')
@@ -106,25 +119,14 @@ export class FileController {
       `Getting the court record for case ${id} as a pdf document`,
     )
 
-    try {
-      return this.auditTrailService.audit(
-        user.id,
-        AuditedAction.GET_COURT_RECORD,
-        this.getPdf(id, 'courtRecord', req, res),
-        id,
-      )
-    } catch (error) {
-      this.logger.debug(
-        `Failed to get the court record for case ${id} as a pdf document`,
-        error,
-      )
-
-      if (error instanceof FileExeption) {
-        return res.status(error.status).json(error.message)
-      }
-
-      throw error
-    }
+    return this.tryGetPdf(
+      user.id,
+      AuditedAction.GET_COURT_RECORD,
+      id,
+      'courtRecord',
+      req,
+      res,
+    )
   }
 
   @Get('ruling')
@@ -137,25 +139,14 @@ export class FileController {
   ): Promise<Response> {
     this.logger.debug(`Getting the ruling for case ${id} as a pdf document`)
 
-    try {
-      return this.auditTrailService.audit(
-        user.id,
-        AuditedAction.GET_RULING_PDF,
-        this.getPdf(id, 'ruling', req, res),
-        id,
-      )
-    } catch (error) {
-      this.logger.debug(
-        `Failed to get the ruling for case ${id} as a pdf document`,
-        error,
-      )
-
-      if (error instanceof FileExeption) {
-        return res.status(error.status).json(error.message)
-      }
-
-      throw error
-    }
+    return this.tryGetPdf(
+      user.id,
+      AuditedAction.GET_RULING_PDF,
+      id,
+      'ruling',
+      req,
+      res,
+    )
   }
 
   @Get('custodyNotice')
@@ -168,24 +159,13 @@ export class FileController {
   ): Promise<Response> {
     this.logger.debug(`Getting the ruling for case ${id} as a pdf document`)
 
-    try {
-      return this.auditTrailService.audit(
-        user.id,
-        AuditedAction.GET_CUSTODY_NOTICE_PDF,
-        this.getPdf(id, 'custodyNotice', req, res),
-        id,
-      )
-    } catch (error) {
-      this.logger.debug(
-        `Failed to get the custody notice for case ${id} as a pdf document`,
-        error,
-      )
-
-      if (error instanceof FileExeption) {
-        return res.status(error.status).json(error.message)
-      }
-
-      throw error
-    }
+    return this.tryGetPdf(
+      user.id,
+      AuditedAction.GET_CUSTODY_NOTICE_PDF,
+      id,
+      'custodyNotice',
+      req,
+      res,
+    )
   }
 }
