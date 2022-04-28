ddf7e5a41be0d8a150320892e7efe5589d341ad9
Eirikur Nilsson <eirikur@nilsson.is>
2022-04-28 21:25:01 +0000

    chore(core): Remove Ferðagjöf (gjafakort) project

--- apps/gjafakort/application/src/domains/index.ts
+++
@@ -1,2 +0,0 @@
-export { routes as applicationRoutes } from './applications'
-export { routes as issuerRoutes } from './issuers'
--- apps/gjafakort/web/context/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as UserContext } from './UserContext'
