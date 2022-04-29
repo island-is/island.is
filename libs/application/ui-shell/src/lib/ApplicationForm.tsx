ec54fa8868969bcb06c9b9a599df3b79939a4785
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2022-04-29 10:28:07 +0000

    add userprofile locale changes from main to release

--- libs/application/ui-shell/src/lib/ApplicationForm.tsx
+++ libs/application/ui-shell/src/lib/ApplicationForm.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useEffect, useState } from 'react'
-import { useLazyQuery, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import * as Sentry from '@sentry/react'
-import { isLocale } from 'class-validator'
 
 import { APPLICATION_APPLICATION } from '@island.is/application/graphql'
 import {
@@ -11,7 +10,6 @@ import {
   Schema,
   coreMessages,
 } from '@island.is/application/core'
-import { Locale } from '@island.is/shared/types'
 import {
   getApplicationTemplateByTypeId,
   getApplicationUIFields,
@@ -24,9 +22,6 @@ import { FieldProvider, useFields } from '../context/FieldContext'
 import { LoadingShell } from '../components/LoadingShell'
 import { FormShell } from './FormShell'
 import { ErrorShell } from '../components/ErrorShell'
-import { Query } from '@island.is/api/schema'
-import { USER_PROFILE } from '@island.is/service-portal/graphql'
-import { useAuth } from '@island.is/auth/react'
 
 const ApplicationLoader: FC<{
   applicationId: string
@@ -78,11 +73,11 @@ const ShellWrapper: FC<{
   const [dataSchema, setDataSchema] = useState<Schema>()
   const [form, setForm] = useState<Form>()
   const [, fieldsDispatch] = useFields()
-  const { formatMessage, changeLanguage, lang } = useLocale()
+  const { formatMessage } = useLocale()
   const featureFlagClient = useFeatureFlagClient()
-  const { userInfo } = useAuth()
 
   useApplicationNamespaces(application.typeId)
+
   useEffect(() => {
     async function populateForm() {
       if (dataSchema === undefined && form === undefined) {
@@ -133,27 +128,6 @@ const ShellWrapper: FC<{
     featureFlagClient,
   ])
 
-  // TODO: Change when IDS has locale. TODO2:
-  // const [
-  //   getUserProfile,
-  //   { data: userProfData, loading: userProfileLoading },
-  // ] = useLazyQuery<Query>(USER_PROFILE)
-  // const userProfile = userProfData?.getUserProfile || null
-
-  // useEffect(() => {
-  //   if (userInfo?.profile.nationalId) getUserProfile()
-  // }, [userInfo, getUserProfile])
-
-  // useEffect(() => {
-  //   if (
-  //     userProfile?.locale &&
-  //     isLocale(userProfile.locale) &&
-  //     userProfile.locale !== lang
-  //   )
-  //     changeLanguage(userProfile.locale as Locale)
-  // }, [userProfile, changeLanguage, lang])
-
-  // if (!form || !dataSchema || userProfileLoading) {
   if (!form || !dataSchema) {
     return <LoadingShell />
   }
