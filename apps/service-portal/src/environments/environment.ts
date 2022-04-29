16139ffb571ccf8c117dcf74827becf3368f441c
Petar Shomov <petar@andes.is>
2022-04-29 23:55:05 +0000

    dummy change

--- apps/service-portal/src/environments/environment.ts
+++ apps/service-portal/src/environments/environment.ts
@@ -20,8 +20,8 @@ const prodConfig = {
   featureFlagSdkKey: getStaticEnv('SI_PUBLIC_CONFIGCAT_SDK_KEY'),
   DD_RUM_CLIENT_TOKEN: getStaticEnv('SI_PUBLIC_DD_RUM_CLIENT_TOKEN'),
   DD_RUM_APPLICATION_ID: getStaticEnv('SI_PUBLIC_DD_RUM_APPLICATION_ID'),
-  APP_VERSION: getStaticEnv('APP_VERSION'),
   ENVIRONMENT: getStaticEnv('SI_PUBLIC_ENVIRONMENT'),
+  APP_VERSION: getStaticEnv('APP_VERSION'),
 }
 
 export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig
