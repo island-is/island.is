7b2cde261c760a4c6624e6564b2498e9d40b9c9f
Petar Shomov <petar@andes.is>
2022-04-30 09:37:46 +0000

    not quite getting what the issue is

--- apps/service-portal/src/Main.tsx
+++ apps/service-portal/src/Main.tsx
@@ -10,16 +10,16 @@ import App from './app/App'
 import { userMonitoring } from '@island.is/user-monitoring'
 
 console.debug(`Checking if we should init DD RUM`)
-if (!isRunningOnEnvironment('local')) {
-  console.debug(`About to init DD RUM`)
-  userMonitoring.initDdRum({
-    service: 'service-portal',
-    applicationId: environment.DD_RUM_APPLICATION_ID,
-    clientToken: environment.DD_RUM_CLIENT_TOKEN,
-    env: environment.ENVIRONMENT,
-    version: environment.APP_VERSION,
-  })
-}
+// if (!isRunningOnEnvironment('local')) {
+console.debug(`About to init DD RUM`)
+// userMonitoring.initDdRum({
+//   service: 'service-portal',
+//   applicationId: environment.DD_RUM_APPLICATION_ID,
+//   clientToken: environment.DD_RUM_CLIENT_TOKEN,
+//   env: environment.ENVIRONMENT,
+//   version: environment.APP_VERSION,
+// })
+// }
 console.debug(`About to render the DOM`)
 
 ReactDOM.render(<App />, document.getElementById('root'))
