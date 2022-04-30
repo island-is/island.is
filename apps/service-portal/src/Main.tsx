7709f6b3c9f7779787026e37eab4c60b58057995
Petar Shomov <petar@andes.is>
2022-04-30 13:13:50 +0000

    troubleshooting

--- apps/service-portal/src/Main.tsx
+++ apps/service-portal/src/Main.tsx
@@ -10,16 +10,19 @@ import App from './app/App'
 import { userMonitoring } from '@island.is/user-monitoring'
 
 console.debug(`Checking if we should init DD RUM`)
-// if (!isRunningOnEnvironment('local')) {
-console.debug(`About to init DD RUM`)
-// userMonitoring.initDdRum({
-//   service: 'service-portal',
-//   applicationId: environment.DD_RUM_APPLICATION_ID,
-//   clientToken: environment.DD_RUM_CLIENT_TOKEN,
-//   env: environment.ENVIRONMENT,
-//   version: environment.APP_VERSION,
-// })
-// }
+if (!isRunningOnEnvironment('local')) {
+  console.debug(`About to init DD RUM`)
+  const params = {
+    service: 'service-portal',
+    applicationId: environment.DD_RUM_APPLICATION_ID,
+    clientToken: environment.DD_RUM_CLIENT_TOKEN,
+    env: environment.ENVIRONMENT,
+    version: environment.APP_VERSION,
+  }
+  // userMonitoring.initDdRum(params)
+  console.log(`params: ${JSON.stringify(params)}`)
+  console.debug('done with the RUM')
+}
 console.debug(`About to render the DOM`)
 
 ReactDOM.render(<App />, document.getElementById('root'))
