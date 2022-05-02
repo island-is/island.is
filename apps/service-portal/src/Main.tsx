f7a437de129c4f6a1166cfd905d7eb91c71435c4
Petar Shomov <petar@andes.is>
2022-05-02 13:31:22 +0000

    now adding air-discount-scheme web

--- apps/service-portal/src/Main.tsx
+++ apps/service-portal/src/Main.tsx
@@ -9,20 +9,14 @@ import './auth'
 import { environment } from './environments'
 import App from './app/App'
 
-console.debug(`Checking if we should init DD RUM`)
 if (!isRunningOnEnvironment('local')) {
-  console.debug(`About to init DD RUM`)
-  const params = {
+  userMonitoring.initDdRum({
     service: 'service-portal',
     applicationId: environment.DD_RUM_APPLICATION_ID,
     clientToken: environment.DD_RUM_CLIENT_TOKEN,
     env: environment.ENVIRONMENT,
     version: environment.APP_VERSION,
-  }
-  userMonitoring.initDdRum(params)
-  console.log(`params: ${JSON.stringify(params)}`)
-  console.debug('done with the RUM')
+  })
 }
-console.debug(`About to render the DOM`)
 
 ReactDOM.render(<App />, document.getElementById('root'))
