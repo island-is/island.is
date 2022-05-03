cefda2b76c6edb2257a2b4209372a8d70b2db075
Snorri Hjörvar Jóhannsson <snojo@Snorris-MacBook-Pro.local>
2022-05-03 13:01:03 +0000

    feat(ci): adding db undo script

---
+++ scripts/sequilize-undo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+CURRENT=$1 # Current state of DB
+TARGET=$2 # Targeted state of DB
+
+DIFF="git diff --name-only $TARGET...$CURRENT -- $(find ../apps -name 'migrations')"
+
+while IFS= read -r line; do
+    echo "Undoing migration of: $line"
+    npx sequelize-cli db:migrate:undo:all --to $line
+done <<< "$DIFF"
+
+echo "Sequilize Undo Complete"
