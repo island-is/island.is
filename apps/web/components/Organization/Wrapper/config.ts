90edb3e02101a36b2cb06e337c34d15bc0cfc848
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2022-05-06 16:42:40 +0000

    feat(web): Chat panel iteration (#7196)
    * Created a reusable ChatBubble
    
    * Added another font
    
    * Added translation to chat bubble and changed color theme
    
    * Added types and changed theme
    
    * Changes to styles and language
    
    * Removed unused styles
    
    * Fixed import error
    
    * Renaming
    
    * Stopped using slugs and started using entry ids instead since slugs might be different between locales
    
    * Added an onLoad function
    
    * Changed config fetch
    
    * Fetched config when needed
    
    * Initialized the client to be undefined
    
    * Changes to chat panel props
    
    * Changed conditional logic for rendering article chat panels
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/web/components/Organization/Wrapper/config.ts
+++ apps/web/components/Organization/Wrapper/config.ts
@@ -1,12 +1,7 @@
-import {
-  LiveChatIncChatPanelProps,
-  WatsonChatPanelProps,
-} from '@island.is/web/components'
-
-export const watsonConfig: Record<string, WatsonChatPanelProps> = {
-  // Test page for Watson chat bot
-  // https://app.contentful.com/spaces/8k0h54kbe6bj/entries/3NFDdRCIe5RVnlSEbHxoTT
-  '3NFDdRCIe5RVnlSEbHxoTT': {
+export const watsonConfig = {
+  // Discrict Commisioners (Sýslumenn) organization
+  // https://app.contentful.com/spaces/8k0h54kbe6bj/entries/kENblMMMvZ3DlyXw1dwxQ
+  kENblMMMvZ3DlyXw1dwxQ: {
     integrationID: 'b1a80e76-da12-4333-8872-936b08246eaa',
     region: 'eu-gb',
     serviceInstanceID: 'bc3d8312-d862-4750-b8bf-529db282050a',
@@ -59,14 +54,5 @@ export const watsonConfig: Record<string, WatsonChatPanelProps> = {
       suggestions_sectionTitleAlternateResponses:
         'Sendu eitthvað af eftirfarandi skilaboðum',
     },
-  },
-}
-
-export const liveChatIncConfig: Record<string, LiveChatIncChatPanelProps> = {
-  // Information for Ukrainian citizens
-  // https://app.contentful.com/spaces/8k0h54kbe6bj/entries/7GtuCCd7MEZhZKe0oXcHdb
-  '7GtuCCd7MEZhZKe0oXcHdb': {
-    license: 13822368,
-    version: '2.0',
   },
 }
