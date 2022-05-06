1632aefe9f431a6aa6e4a3fa83954dc98b8aacdf
Petar Shomov <petar@andes.is>
2022-05-06 17:24:31 +0000

    fixing up feature generation for non-dev env

---
+++ .github/actions/update-comment.js
@@ -0,0 +1,37 @@
+module.exports = async ({ github, context, comment }) => {
+  const owner = context.repo.owner
+  const repo = context.repo.repo
+  const issueNr = context.issue.number
+  const runsIterator = github.paginate.iterator(
+    github.rest.issues.listComments,
+    {
+      owner: owner,
+      repo: repo,
+      issue_number: issueNr,
+    },
+  )
+  let commentId
+  for await (const comments of runsIterator) {
+    for (const comment of comments.data) {
+      if (comment.body?.startsWith('Affected services are: ')) {
+        commentId = comment.id
+      }
+    }
+  }
+  if (commentId) {
+    await github.rest.issues.updateComment({
+      issue_number: issueNr,
+      owner: owner,
+      repo: repo,
+      comment_id: commentId,
+      body: `${comment}`,
+    })
+  } else {
+    await github.rest.issues.createComment({
+      issue_number: issueNr,
+      owner: owner,
+      repo: repo,
+      body: `${comment}`,
+    })
+  }
+}
--- apps/air-discount-scheme/api/src/main.ts
+++ apps/air-discount-scheme/api/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrap } from '@island.is/infra-nest-server'
 
 import { AppModule } from './app/app.module'
 
+// dummy change
 bootstrap({
   appModule: AppModule,
   name: 'ads-api',
