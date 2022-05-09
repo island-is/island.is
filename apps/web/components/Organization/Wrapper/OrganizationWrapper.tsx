188ac7837871598fc6c36e977c0d7b64533e13d3
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2022-05-09 10:51:05 +0000

    feat(web): Hotfix - Sýslumenn Watson Chat Panel (#7346)

--- apps/web/components/Organization/Wrapper/OrganizationWrapper.tsx
+++ apps/web/components/Organization/Wrapper/OrganizationWrapper.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode, useEffect, useState, useMemo } from 'react'
 import { useWindowSize } from 'react-use'
 import { useRouter } from 'next/router'
 import NextLink from 'next/link'
-import getConfig from 'next/config'
 import { theme } from '@island.is/island-ui/theme'
 import { LayoutProps } from '@island.is/web/layouts/main'
 import {
@@ -43,9 +42,11 @@ import {
   UtlendingastofnunFooter,
   UtlendingastofnunHeader,
 } from './Themes/UtlendingastofnunTheme'
-import { endpoints as chatPanelEndpoints } from '../../ChatPanel/config'
+import { boostChatPanelEndpoints } from '@island.is/web/components'
 import MannaudstorgFooter from './Themes/MannaudstorgTheme/MannaudstorgFooter'
-import { useNamespace } from '@island.is/web/hooks'
+import { useFeatureFlag, useNamespace } from '@island.is/web/hooks'
+import { watsonConfig } from './config'
+import { WatsonChatPanel } from '@island.is/web/components'
 import * as styles from './OrganizationWrapper.css'
 
 interface NavigationData {
@@ -205,29 +206,34 @@ export const OrganizationFooter: React.FC<FooterProps> = ({
 }
 
 export const OrganizationChatPanel = ({
-  slugs,
+  organizationIds,
   pushUp = false,
 }: {
-  slugs: string[]
+  organizationIds: string[]
   pushUp?: boolean
 }) => {
-  // remove when organization chat-bot is ready for release
-  const { publicRuntimeConfig } = getConfig()
-  const { disableOrganizationChatbot } = publicRuntimeConfig
-  if (disableOrganizationChatbot === 'true') {
-    return null
+  const { loading, value: isWatsonChatPanelEnabled } = useFeatureFlag(
+    'isWatsonChatPanelEnabled',
+    false,
+  )
+
+  if (loading) return null
+
+  const id = organizationIds.find((id) => {
+    if (!isWatsonChatPanelEnabled) return id in boostChatPanelEndpoints
+    return id in watsonConfig
+  })
+
+  if (!isWatsonChatPanelEnabled) {
+    return id ? (
+      <BoostChatPanel
+        endpoint={id as keyof typeof boostChatPanelEndpoints}
+        pushUp={pushUp}
+      />
+    ) : null
   }
 
-  const chatEnabled = ['syslumenn']
-
-  const slug = slugs.find((x) => chatEnabled.includes(x))
-
-  return slug ? (
-    <BoostChatPanel
-      endpoint={slug as keyof typeof chatPanelEndpoints}
-      pushUp={pushUp}
-    />
-  ) : null
+  return id in watsonConfig ? <WatsonChatPanel {...watsonConfig[id]} /> : null
 }
 
 const SecondaryMenu = ({
@@ -467,7 +473,9 @@ export const OrganizationWrapper: React.FC<WrapperProps> = ({
           force={true}
         />
       )}
-      <OrganizationChatPanel slugs={[organizationPage?.slug]} />
+      <OrganizationChatPanel
+        organizationIds={[organizationPage?.organization?.id]}
+      />
     </>
   )
 }
--- apps/web/screens/Article/Article.tsx
+++ apps/web/screens/Article/Article.tsx
@@ -26,11 +26,9 @@ import {
   InstitutionPanel,
   InstitutionsPanel,
   OrganizationFooter,
-  OrganizationChatPanel,
   Sticky,
   Webreader,
   AppendedArticleComponents,
-  LiveChatIncChatPanel,
 } from '@island.is/web/components'
 import { withMainLayout } from '@island.is/web/layouts/main'
 import { GET_ARTICLE_QUERY, GET_NAMESPACE_QUERY } from '../queries'
@@ -60,7 +58,7 @@ import { useScrollPosition } from '../../hooks/useScrollPosition'
 import { scrollTo } from '../../hooks/useScrollSpy'
 import StepperFSM from '../../components/StepperFSM/StepperFSM'
 import { getStepOptionsFromUIConfiguration } from '../../components/StepperFSM/StepperFSMUtils'
-import { liveChatIncConfig } from './config'
+import { ArticleChatPanel } from './components/ArticleChatPanel'
 import * as styles from './Article.css'
 
 type Article = GetSingleArticleQuery['getSingleArticle']
@@ -608,14 +606,7 @@ const ArticleScreen: Screen<ArticleProps> = ({
             portalRef.current,
           )}
       </SidebarLayout>
-      {article.id in liveChatIncConfig ? (
-        <LiveChatIncChatPanel {...liveChatIncConfig[article.id]} />
-      ) : (
-        <OrganizationChatPanel
-          slugs={article.organization.map((x) => x.slug)}
-          pushUp={isVisible}
-        />
-      )}
+      <ArticleChatPanel article={article} pushUp={isVisible} />
       <OrganizationFooter
         organizations={article.organization as Organization[]}
       />
