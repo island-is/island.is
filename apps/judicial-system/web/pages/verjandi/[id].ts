967e2518e1afa3624d94c6aaca026e416e9ebe43
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-05-10 13:52:18 +0000

    Reverts folder rename

--- apps/judicial-system/web/pages/verjandi/[id].ts
+++ apps/judicial-system/web/pages/verjandi/[id].ts
@@ -1,3 +1,3 @@
-import { SignedVerdictOverview } from '@island.is/judicial-system-web/src/routes/Shared/CaseOverview/SignedVerdictOverview'
+import { SignedVerdictOverview } from '@island.is/judicial-system-web/src/routes/Shared/SignedVerdictOverview/SignedVerdictOverview'
 
 export default SignedVerdictOverview
