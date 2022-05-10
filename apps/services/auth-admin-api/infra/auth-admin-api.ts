8b13df0575ff32bcc0002b8a045576c4a6dd35fd
peturgq <petur@petur.io>
2022-05-10 12:57:01 +0000

    Remove more un-needed imports, add XroadConf for RskProcuring

--- apps/services/auth-admin-api/infra/auth-admin-api.ts
+++ apps/services/auth-admin-api/infra/auth-admin-api.ts
@@ -1,4 +1,4 @@
-import { ref, service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
+import { service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
 
 export const serviceSetup = (): ServiceBuilder<'auth-admin-api'> => {
   return service('auth-admin-api')
