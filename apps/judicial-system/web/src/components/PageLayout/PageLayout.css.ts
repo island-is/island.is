def0f47a767fac649482543df6475649f5390a93
Arnar Kári Ágústsson <arnarkari93@gmail.com>
2022-05-11 10:37:39 +0000

    feat(j-s): mobile stepper (#7376)
    * fix(j-s): make stepper responsive
    
    * feat(j-s): set initial scale to viewport
    
    This was added to make content take full with of mobile devices
    
    * fix(ui): remove padding in form stepper
    
    * fix(j-s): remove extra margin on mobile
    
    * fix(j-s): fix margin for instituion logo
    
    Co-authored-by: Arnar Kári <arnar.kari@kolibri.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/components/PageLayout/PageLayout.css.ts
+++ apps/judicial-system/web/src/components/PageLayout/PageLayout.css.ts
@@ -1,6 +1,10 @@
 import { style } from '@vanilla-extract/css'
 import { theme } from '@island.is/island-ui/theme'
 
+export const container = style({
+  padding: 0,
+})
+
 export const processContainer = style({
   minHeight: 'calc(100vh - 112px)',
 })
