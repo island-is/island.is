50158006fa7e1ff5dd430f6635e2b9069df5fc05
Gudjon <gudjon@programm.is>
2022-05-12 16:06:00 +0000

    Added empty fallbacks for config

--- libs/api/domains/financial-statements-inao/src/lib/client/dataverseClient.config.ts
+++ libs/api/domains/financial-statements-inao/src/lib/client/dataverseClient.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from '@island.is/nest/config'
 export const dataverseClientConfig = defineConfig({
   name: 'DataverseClient',
   load: (env) => ({
-    basePath: env.required('FINANCIAL_STATEMENTS_INAO_BASE_PATH'),
-    issuer: env.required('FINANCIAL_STATEMENTS_INAO_ISSUER'),
-    scope: env.required('FINANCIAL_STATEMENTS_INAO_SCOPE'),
-    tokenEndpoint: env.required('FINANCIAL_STATEMENTS_INAO_TOKEN_ENDPOINT'),
-    clientId: env.required('FINANCIAL_STATEMENTS_INAO_CLIENT_ID'),
-    clientSecret: env.required('FINANCIAL_STATEMENTS_INAO_CLIENT_SECRET'),
+    basePath: env.required('FINANCIAL_STATEMENTS_INAO_BASE_PATH', ''),
+    issuer: env.required('FINANCIAL_STATEMENTS_INAO_ISSUER', ''),
+    scope: env.required('FINANCIAL_STATEMENTS_INAO_SCOPE', ''),
+    tokenEndpoint: env.required('FINANCIAL_STATEMENTS_INAO_TOKEN_ENDPOINT', ''),
+    clientId: env.required('FINANCIAL_STATEMENTS_INAO_CLIENT_ID', ''),
+    clientSecret: env.required('FINANCIAL_STATEMENTS_INAO_CLIENT_SECRET', ''),
   }),
 })
