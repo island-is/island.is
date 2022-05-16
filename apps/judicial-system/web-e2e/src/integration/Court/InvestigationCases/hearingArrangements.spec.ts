eb4ee53aa348366b8a5902b8758cd9d071192f2c
Ívar Oddsson <ivaroddsson@gmail.com>
2022-05-16 10:35:08 +0000

    Add e2e test for defender not found

--- apps/judicial-system/web-e2e/src/integration/Court/InvestigationCases/hearingArrangements.spec.ts
+++ apps/judicial-system/web-e2e/src/integration/Court/InvestigationCases/hearingArrangements.spec.ts
@@ -29,7 +29,7 @@ describe(`${IC_COURT_HEARING_ARRANGEMENTS_ROUTE}/:id`, () => {
     cy.contains(comment)
   })
 
-  it('should ask for defender info depending on selected session arrangement', () => {
+  it.only('should ask for defender info depending on selected session arrangement and warn users if defender is not found in the lawyer registry', () => {
     const caseData = makeInvestigationCase()
     const caseDataAddition: Case = {
       ...caseData,
@@ -44,6 +44,9 @@ describe(`${IC_COURT_HEARING_ARRANGEMENTS_ROUTE}/:id`, () => {
     cy.get('[name="defenderName"]').should('exist')
     cy.get('[name="defenderEmail"]').should('exist')
     cy.get('[name="defenderPhoneNumber"]').should('exist')
+
+    cy.get('[name="defenderName"]').type(faker.name.firstName())
+    cy.get('[name="defenderNotFound"]').should('exist')
 
     cy.get('[name="session-arrangements-all-present_spokesperson"]').click()
     cy.get('[name="defenderName"]').should('exist')
