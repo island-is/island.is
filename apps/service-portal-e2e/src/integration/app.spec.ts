8c98999db17153c73487911731784bca4f696bf4
disaerna <disa.erna@gmail.com>
2022-05-16 11:15:13 +0000

    refactor: change test?

--- apps/service-portal-e2e/src/integration/app.spec.ts
+++ apps/service-portal-e2e/src/integration/app.spec.ts
@@ -1,6 +1,7 @@
 describe('service-portal', () => {
   it('should navigate the homepage', () => {
     cy.visit('/minarsidur')
-    cy.contains('Eldri útgáfa').click()
+
+    cy.contains('Yfirlit').click()
   })
 })
