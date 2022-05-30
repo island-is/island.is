654c80ec2aa23e68cc60fce3ab0a16fac6f5f525
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-05-30 10:15:09 +0000

    fix(fa): Getting name of unregistered spouse (#7531)
    * Read spouse name from auth
    
    * Cleanup
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/financial-aid/src/dataProviders/VeitaProvider.ts
+++ libs/application/templates/financial-aid/src/dataProviders/VeitaProvider.ts
@@ -6,7 +6,7 @@ import {
 import { CurrentApplication, DataProviderTypes } from '../lib/types'
 
 export class VeitaProvider extends BasicDataProvider {
-  readonly type = DataProviderTypes.NationalRegistry
+  readonly type = DataProviderTypes.Veita
   async provide(): Promise<CurrentApplication> {
     // TODO: We probably need application system id and current Veita id here
     const query = `
