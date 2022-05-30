3d36453ad432472a60a0f5ee3085575a5308cce9
Rafn Árnason <rafnarnason@gmail.com>
2022-05-30 15:23:13 +0000

    updated tested readme

--- libs/clients/driving-license-book/src/lib/drivingLicenseBookClient.module.ts
+++ libs/clients/driving-license-book/src/lib/drivingLicenseBookClient.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ApiConfiguration } from './apiConfiguration'
+import { exportedApis } from './apis'
 import { DrivingLicenseBookClientApiFactory } from './drivingLicenseBookClient.service'
+
+// what check bare minimum 
 
 @Module({
   providers: [DrivingLicenseBookClientApiFactory],
   exports: [DrivingLicenseBookClientApiFactory],
 })
+// @Module({
+//   providers: [ApiConfiguration, ...exportedApis,DrivingLicenseBookClientApiFactory],
+//   exports: [...exportedApis,DrivingLicenseBookClientApiFactory],
+// })
 export class DrivingLicenseBookClientModule {}
