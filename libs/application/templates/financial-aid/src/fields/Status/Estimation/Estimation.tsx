147c11c11c5dbd17c2db63f82db3d40e1760666a
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-05-31 16:44:15 +0000

    Remove unnecessary check

--- libs/application/templates/financial-aid/src/fields/Status/Estimation/Estimation.tsx
+++ libs/application/templates/financial-aid/src/fields/Status/Estimation/Estimation.tsx
@@ -33,19 +33,9 @@ export const Estimation = ({
   const { formatMessage } = useIntl()
 
   const getAidType = () => {
-    switch (true) {
-      case nationalRegistry?.data?.applicant?.spouse?.maritalStatus !=
-        undefined:
-        return (
-          martialStatusTypeFromMartialCode(
-            nationalRegistry?.data?.applicant?.spouse?.maritalStatus,
-          ) === MartialStatusType.SINGLE
-        )
-      default:
-        return !showSpouseData[
-          findFamilyStatus(application.answers, application.externalData)
-        ]
-    }
+    return !showSpouseData[
+      findFamilyStatus(application.answers, application.externalData)
+    ]
   }
 
   const aidAmount = useMemo(() => {
