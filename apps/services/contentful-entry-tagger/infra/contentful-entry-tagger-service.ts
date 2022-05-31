e99784edba6beadfcf33ccd5be6a70cb4415e4be
runarvestmann <runarvestmann@gmail.com>
2022-05-31 12:29:53 +0000

    Changed service name

--- apps/services/contentful-entry-tagger/infra/contentful-entry-tagger-service.ts
+++ apps/services/contentful-entry-tagger/infra/contentful-entry-tagger-service.ts
@@ -1,7 +1,7 @@
 import { service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
 
 export const serviceSetup = (): ServiceBuilder<'contentful-entry-tagger-service'> =>
-  service('services-contentful-entry-tagger-service')
+  service('services-contentful-entry-tagger')
     .image('services-contentful-entry-tagger')
     .namespace('contentful-entry-tagger')
     .serviceAccount('contentful-entry-tagger')
