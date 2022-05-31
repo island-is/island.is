07b8a124ec8f842ce1372a5ec3189295e34f83ce
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-05-31 16:26:21 +0000

    Add cohabitation case

--- libs/application/templates/financial-aid/src/lib/utils.ts
+++ libs/application/templates/financial-aid/src/lib/utils.ts
@@ -71,11 +71,12 @@ export function findFamilyStatus(
   answers: FAApplication['answers'],
   externalData: FAApplication['externalData'],
 ) {
+  const maritialStatus = martialStatusTypeFromMartialCode(externalData.nationalRegistry?.data?.applicant?.spouse?.maritalStatus)
   switch (true) {
-    case martialStatusTypeFromMartialCode(
-      externalData.nationalRegistry?.data?.applicant?.spouse?.maritalStatus,
-    ) === MartialStatusType.MARRIED:
+    case maritialStatus === MartialStatusType.MARRIED:
       return FamilyStatus.MARRIED
+    case externalData.nationalRegistry?.data?.applicant?.spouse != null:
+      return FamilyStatus.COHABITATION
     case answers?.relationshipStatus?.unregisteredCohabitation ===
       ApproveOptions.Yes:
       return FamilyStatus.UNREGISTERED_COBAHITATION
