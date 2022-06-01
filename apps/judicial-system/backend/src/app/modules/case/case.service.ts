4dd0c7fbdd252ab0aae97cb6c280a8cc718d0fe4
Ívar Oddsson <ivaroddsson@gmail.com>
2022-06-01 09:32:02 +0000

    feat(j-s): Iteration on the modify ruling page (#7556)
    * feat(j-s): disable input when modify ruling
    
    * Disable fields when modifying a ruling
    
    * Disable fields when modifying a ruling
    
    * Back buttons now say Back not Cancel
    
    * Add AlertMessage to modifyRuling page
    
    * Added a 'forceRegeneration' query parameter to pdf generation
    
    * Refactor backend tests
    
    * Rename forceRegeneration to useSigned
    
    Co-authored-by: Arnar Kári <arnar.kari@kolibri.is>
    Co-authored-by: Ívar Oddsson <oddsson@Ivars-iMac.local>

--- apps/judicial-system/backend/src/app/modules/case/case.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.service.ts
@@ -725,16 +725,18 @@ export class CaseService {
     return getCourtRecordPdfAsBuffer(theCase, user, this.formatMessage)
   }
 
-  async getRulingPdf(theCase: Case): Promise<Buffer> {
-    try {
-      return await this.awsS3Service.getObject(
-        `generated/${theCase.id}/ruling.pdf`,
-      )
-    } catch (error) {
-      this.logger.info(
-        `The ruling for case ${theCase.id} was not found in AWS S3`,
-        { error },
-      )
+  async getRulingPdf(theCase: Case, useSigned = true): Promise<Buffer> {
+    if (useSigned) {
+      try {
+        return await this.awsS3Service.getObject(
+          `generated/${theCase.id}/ruling.pdf`,
+        )
+      } catch (error) {
+        this.logger.info(
+          `The ruling for case ${theCase.id} was not found in AWS S3`,
+          { error },
+        )
+      }
     }
 
     await this.refreshFormatMessage()
