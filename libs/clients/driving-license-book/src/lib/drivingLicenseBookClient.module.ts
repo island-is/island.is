fd2644d44271c31127de97fe742c0401c90b006c
Rafn Árnason <rafnarnason@gmail.com>
2022-06-02 12:49:00 +0000

    removed comment unused deps format

--- libs/clients/driving-license-book/src/lib/drivingLicenseBookClient.module.ts
+++ libs/clients/driving-license-book/src/lib/drivingLicenseBookClient.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common'
-import { ApiConfiguration } from './apiConfiguration'
-import { exportedApis } from './apis'
 import { DrivingLicenseBookClientApiFactory } from './drivingLicenseBookClient.service'
-
-// what check bare minimum
 
 @Module({
   providers: [DrivingLicenseBookClientApiFactory],
   exports: [DrivingLicenseBookClientApiFactory],
 })
-// @Module({
-//   providers: [ApiConfiguration, ...exportedApis,DrivingLicenseBookClientApiFactory],
-//   exports: [...exportedApis,DrivingLicenseBookClientApiFactory],
-// })
 export class DrivingLicenseBookClientModule {}
