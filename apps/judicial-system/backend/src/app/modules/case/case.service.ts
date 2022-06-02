207fe38b506fb47d54e3829318ce7ad40aac8167
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-06-02 11:02:48 +0000

    fix(j-s): Fixes court record upload (#7590)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/backend/src/app/modules/case/case.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.service.ts
@@ -479,37 +479,38 @@ export class CaseService {
     ]
 
     if (theCase.courtId && theCase.courtCaseNumber) {
-      const isModifyingRuling = Boolean(theCase.rulingDate)
-
       uploadPromises.push(
         this.uploadSignedRulingPdfToCourt(theCase, user, signedRulingPdf).then(
           (res) => {
             rulingUploadedToCourt = res
           },
         ),
-        isModifyingRuling
-          ? this.uploadCaseFilesPdfToCourt(theCase, user)
-          : Promise.resolve(),
-        isModifyingRuling
-          ? getCourtRecordPdfAsString(theCase, this.formatMessage)
-              .then((courtRecordPdf) => {
-                return this.uploadCourtRecordPdfToCourt(
-                  theCase,
-                  user,
-                  courtRecordPdf,
-                ).then((res) => {
-                  courtRecordUploadedToCourt = res
-                })
-              })
-              .catch((reason) => {
-                // Log and ignore this error. The court record can be uploaded manually.
-                this.logger.error(
-                  `Failed to generate court record pdf for case ${theCase.id}`,
-                  { reason },
-                )
-              })
-          : Promise.resolve(),
       )
+
+      const isModifyingRuling = Boolean(theCase.rulingDate)
+
+      if (!isModifyingRuling) {
+        uploadPromises.push(
+          this.uploadCaseFilesPdfToCourt(theCase, user),
+          getCourtRecordPdfAsString(theCase, this.formatMessage)
+            .then(async (courtRecordPdf) => {
+              return this.uploadCourtRecordPdfToCourt(
+                theCase,
+                user,
+                courtRecordPdf,
+              ).then((res) => {
+                courtRecordUploadedToCourt = res
+              })
+            })
+            .catch((reason) => {
+              // Log and ignore this error. The court record can be uploaded manually.
+              this.logger.error(
+                `Failed to generate court record pdf for case ${theCase.id}`,
+                { reason },
+              )
+            }),
+        )
+      }
     }
 
     await Promise.all(uploadPromises)
