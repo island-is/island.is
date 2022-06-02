795e0900ef3ce85cc3944ebcf22e76cf0942489b
Jón Levy <levy@andes.is>
2022-06-02 12:58:16 +0000

    fix: rename dockerImage to containerImage, bash error handling

--- scripts/run-test-proxies.sh
+++ scripts/run-test-proxies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+set -euo pipefail
+
 ./scripts/run-es-proxy.sh "$@" & ./scripts/run-soffia-proxy.sh "$@" & ./scripts/run-xroad-proxy.sh "$@" &
--- scripts/stop-test-proxies.sh
+++ scripts/stop-test-proxies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euox pipefail
+set -euo pipefail
 
 function stop_proxy() {
   local proxy builder
