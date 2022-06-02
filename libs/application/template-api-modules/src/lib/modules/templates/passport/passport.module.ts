009c64b326909a64f93fba2d8ec2e740dc5af2e4
Alexandra Diljá <alexandrab18@ru.is>
2022-06-02 11:36:45 +0000

    payment working

--- libs/application/template-api-modules/src/lib/modules/templates/passport/passport.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/passport/passport.module.ts
@@ -1,5 +1,4 @@
 import { DynamicModule } from '@nestjs/common'
-import { SyslumennClientModule } from '@island.is/clients/syslumenn'
 import { PassportService } from './passport.service'
 import { SharedTemplateAPIModule } from '../../shared'
 import { BaseTemplateAPIModuleConfig } from '../../../types'
@@ -9,7 +8,6 @@ export class PassportModule {
     return {
       module: PassportModule,
       imports: [
-        SyslumennClientModule,
         SharedTemplateAPIModule.register(config),
       ],
       providers: [PassportService],
--- libs/application/templates/passport/src/dataProviders/FeeInfoProvider.ts
+++ libs/application/templates/passport/src/dataProviders/FeeInfoProvider.ts
@@ -16,40 +16,10 @@ export class FeeInfoProvider extends PaymentCatalogProvider {
   async provide(): Promise<PaymentCatalogItem[]> {
     const items =
       (await this.getCatalogForOrganization(SYSLUMADUR_NATIONAL_ID)) || []
-    // return items.filter(({ chargeItemCode }) =>
-    //   CHARGE_ITEM_CODES.includes(chargeItemCode),
-    // )
-    const mock = [
-      {
-        performingOrgID: '6509142520',
-        chargeType: 'AY1',
-        chargeItemCode: 'AY105',
-        chargeItemName: 'Vegabréf, almennt gjald',
-        priceAmount: 13000,
-      },
-      {
-        performingOrgID: '6509142520',
-        chargeType: 'AY1',
-        chargeItemCode: 'AY106',
-        chargeItemName: 'Vegabréf, skyndiútgáfa',
-        priceAmount: 26000,
-      },
-      {
-        performingOrgID: '6509142520',
-        chargeType: 'AY1',
-        chargeItemCode: 'AY107',
-        chargeItemName: 'Vegabréf, aðrir almennt gjald',
-        priceAmount: 5600,
-      },
-      {
-        performingOrgID: '6509142520',
-        chargeType: 'AY1',
-        chargeItemCode: 'AY108',
-        chargeItemName: 'Vegabréf, aðrir skyndiútgáfa',
-        priceAmount: 11000,
-      },
-    ]
-    return mock
+    return items.filter(({ chargeItemCode }) =>
+      CHARGE_ITEM_CODES.includes(chargeItemCode),
+    )
+
   }
 
   onProvideError(result: string): FailedDataProviderResult {
