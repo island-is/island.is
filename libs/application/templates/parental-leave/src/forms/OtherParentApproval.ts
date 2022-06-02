e7f9aadafc2dba23477c1280f7f8e1ec01f81a97
Mashoof <mmashoof@deloittedigital.se>
2022-06-02 17:35:33 +0200

    fix: rebasing

--- libs/application/templates/parental-leave/src/forms/OtherParentApproval.ts
+++ libs/application/templates/parental-leave/src/forms/OtherParentApproval.ts
@@ -123,7 +123,7 @@ export const OtherParentApproval: Form = buildForm({
             buildSubmitField({
               id: 'submit',
               title: coreMessages.buttonSubmit,
-              placement: 'footer',
+              placement: 'footer', 
               actions: [
                 {
                   name: coreMessages.buttonReject,
@@ -133,7 +133,7 @@ export const OtherParentApproval: Form = buildForm({
                 {
                   name: coreMessages.buttonApprove,
                   type: 'primary',
-                  event: '',
+                  event: 'APPROVE',
                 },
               ],
             }),
