42df35f7639ec54e98cb2f4fd9024b04e67dcaad
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-06-03 12:53:26 +0000

    chore: some added decoratos

--- libs/api/domains/regulations/src/lib/dto/createPresignedPost.input.ts
+++ libs/api/domains/regulations/src/lib/dto/createPresignedPost.input.ts
@@ -1,9 +1,12 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { IsString } from 'class-validator'
 
 @InputType()
 export class CreatePresignedPostInput {
   @Field()
+  @IsString()
   readonly fileName!: string
   @Field()
+  @IsString()
   readonly type!: string
 }
