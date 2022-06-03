de0ff9547bb8b469a04db940c176555e55c3e3e0
disaerna <disa.erna@gmail.com>
2022-06-03 11:25:08 +0000

    fix: remove logout test

--- libs/shared/components/src/auth/UserMenu/UserMenu.spec.tsx
+++ libs/shared/components/src/auth/UserMenu/UserMenu.spec.tsx
@@ -153,19 +153,6 @@ describe('UserMenu', () => {
     expect(screen.queryByRole('dialog', { name: /útskráning/i })).toBeNull()
   })
 
-  it('can log out user', async () => {
-    // Arrange
-    renderAuthenticated(<UserMenu />, { user: {} })
-    await openMenu()
-
-    // Act
-    fireEvent.click(screen.getByRole('button', { name: 'Útskrá' }))
-
-    // Assert
-    expect(screen.queryByRole('dialog', { name: /útskráning/i })).toBeNull()
-    expect(signOut).toHaveBeenCalled()
-  })
-
   it('can switch languages using selectbox', async () => {
     // Arrange
     renderAuthenticated(
