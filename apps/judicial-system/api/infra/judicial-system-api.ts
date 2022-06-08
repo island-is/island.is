edabba4f5b7c4fe227d6d392a5ab69980d5b678f
Arnar Kári <arnar.kari@kolibri.is>
2022-06-08 13:02:27 +0000

    feat(j-s): use test contentful envrionment in dev and staging

--- apps/judicial-system/api/infra/judicial-system-api.ts
+++ apps/judicial-system/api/infra/judicial-system-api.ts
@@ -28,6 +28,11 @@ export const serviceSetup = (services: {
         staging: 'cdn.contentful.com',
         prod: 'cdn.contentful.com',
       },
+      CONTENTFUL_ENVIRONMENT: {
+        dev: 'test',
+        staging: 'test',
+        prod: 'master',
+      },
       HIDDEN_FEATURES: {
         dev: '',
         staging: 'MODIFY_RULING',
