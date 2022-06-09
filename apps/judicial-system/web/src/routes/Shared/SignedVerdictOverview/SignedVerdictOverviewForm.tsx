81b94a56f54151ddc98bd3d17e831e7392f669bc
Arnar Kári <arnar.kari@kolibri.is>
2022-06-09 09:30:18 +0000

    fix(j-s): remove unused import features

--- apps/judicial-system/web/src/routes/Shared/SignedVerdictOverview/SignedVerdictOverviewForm.tsx
+++ apps/judicial-system/web/src/routes/Shared/SignedVerdictOverview/SignedVerdictOverviewForm.tsx
@@ -36,7 +36,6 @@ import {
   UserRole,
   isAcceptingCaseDecision,
   Case,
-  Feature,
 } from '@island.is/judicial-system/types'
 import {
   capitalize,
@@ -57,7 +56,6 @@ import { SignedDocument } from '@island.is/judicial-system-web/src/components/Si
 import CaseDates from '@island.is/judicial-system-web/src/components/CaseDates/CaseDates'
 import MarkdownWrapper from '@island.is/judicial-system-web/src/components/MarkdownWrapper/MarkdownWrapper'
 import RestrictionTags from '@island.is/judicial-system-web/src/components/RestrictionTags/RestrictionTags'
-import { FeatureContext } from '@island.is/judicial-system-web/src/components/FeatureProvider/FeatureProvider'
 
 interface Props {
   workingCase: Case
@@ -111,8 +109,6 @@ const SignedVerdictOverviewForm: React.FC<Props> = (props) => {
 
   // skip loading institutions if the user does not have an id
   const { prosecutorsOffices } = useInstitution(!user?.id)
-
-  const { features } = useContext(FeatureContext)
 
   /**
    * If the case is not rejected it must be accepted because
