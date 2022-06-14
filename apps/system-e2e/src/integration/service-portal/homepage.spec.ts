25ac3da904ba79e24d9c548021a36c6d6158f237
Jón Levy <levy@andes.is>
2022-06-14 17:02:20 +0000

    fix: added minarsidur inbox test, fix formatting

--- apps/system-e2e/src/integration/service-portal/homepage.spec.ts
+++ apps/system-e2e/src/integration/service-portal/homepage.spec.ts
@@ -1,10 +1,10 @@
 const phoneNumber = Cypress.env('PHONE_NUMBER')
 
 describe('Home page', () => {
-
   it('should navigate serviceportal', () => {
     cy.visit('/minarsidur/')
 
     cy.idsLogin({ phoneNumber: phoneNumber })
+    cy.contains('Pósthólf')
   })
 })
