8008e29079a5184ef19da1495a423f62edad2ccb
Þórður H <thordur@hugsmidjan.is>
2022-06-14 08:10:40 +0000

    feat(service-portal): Inbox UI updates (#7333)
    * Show unread toggle in document inbox
    
    * Add unopened case
    
    * Add sort to inbox table
    
    * Inbox title mobile size
    
    * Add link wrapper unique color bg
    
    * Update documentBuilder.ts
    
    * Add new filter to inbox documents
    
    * Add filetype url always opened
    
    * Updates after pr comments
    
    * Add translation strings to a message file
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/service-portal/documents/src/utils/messages.ts
@@ -0,0 +1,93 @@
+import { defineMessages } from 'react-intl'
+
+export const messages = defineMessages({
+  foundSingular: {
+    id: 'sp.documents:found-singular',
+    defaultMessage: 'skjal fannst',
+  },
+  found: {
+    id: 'sp.documents:found',
+    defaultMessage: 'skjöl fundust',
+  },
+  title: {
+    id: 'sp.documents:title',
+    defaultMessage: 'Pósthólf',
+  },
+  intro: {
+    id: 'sp.documents:intro',
+    defaultMessage:
+      'Hér getur þú fundið skjöl sem send hafa verið til þín frá opinberum aðilum.',
+  },
+  clearFilter: {
+    id: 'sp.documents:clear-filter',
+    defaultMessage: 'Hreinsa síu',
+  },
+  clearAllFilters: {
+    id: 'sp.documents:clear-all-filters',
+    defaultMessage: 'Hreinsa allar síur',
+  },
+  openFilter: {
+    id: 'sp.documents:open-filter',
+    defaultMessage: 'Opna síu',
+  },
+  closeFilter: {
+    id: 'sp.documents:close-filter',
+    defaultMessage: 'Loka síu',
+  },
+  onlyShowUnread: {
+    id: 'sp.documents:only-show-unread',
+    defaultMessage: 'Sýna einungis ólesið',
+  },
+  clearSelected: {
+    id: 'sp.documents:clear-selected',
+    defaultMessage: 'Hreinsa val',
+  },
+  institutionLabel: {
+    id: 'sp.documents:institution-label',
+    defaultMessage: 'Stofnun',
+  },
+  groupLabel: {
+    id: 'sp.documents:group-label',
+    defaultMessage: 'Flokkur',
+  },
+  datesLabel: {
+    id: 'sp.documents:dates-label',
+    defaultMessage: 'Dagsetningar',
+  },
+  datepickerFromLabel: {
+    id: 'sp.documents:datepicker-dateFrom-label',
+    defaultMessage: 'Dagsetning frá',
+  },
+  datepickerToLabel: {
+    id: 'sp.documents:datepicker-dateTo-label',
+    defaultMessage: 'Dagsetning til',
+  },
+  datepickLabel: {
+    id: 'sp.documents:datepicker-placeholder',
+    defaultMessage: 'Veldu dagsetningu',
+  },
+  clearFilters: {
+    id: 'sp.documents:clear-filters',
+    defaultMessage: 'Hreinsa filter',
+  },
+  tableHeaderDate: {
+    id: 'sp.documents:table-header-date',
+    defaultMessage: 'Dagsetning',
+  },
+  tableHeaderInformation: {
+    id: 'sp.documents:table-header-information',
+    defaultMessage: 'Upplýsingar',
+  },
+  tableHeaderInstitution: {
+    id: 'sp.documents:table-header-institution',
+    defaultMessage: 'Stofnun',
+  },
+  notFound: {
+    id: 'sp.documents:not-found',
+    defaultMessage: 'Engin skjöl fundust fyrir gefin leitarskilyrði.',
+  },
+  error: {
+    id: 'sp.documents:error',
+    defaultMessage: 'Tókst ekki að sækja rafræn skjöl, eitthvað fór úrskeiðis.',
+  },
+})
