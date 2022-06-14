e4cc917466b7ce64dfa962b9888a554161fbcc18
Petar Shomov <petar@andes.is>
2022-06-14 12:06:48 +0000

    Refactoring the search feature to be in its own file

--- apps/system-e2e/src/integration/web/homepage.spec.ts
+++ apps/system-e2e/src/integration/web/homepage.spec.ts
@@ -28,32 +28,4 @@ describe('Home page', () => {
           .should('equal', '/'),
       )
   })
-
-  it('should have search results for common words', function () {
-    cy.visit('/')
-      .get('[data-testid="search-box"]')
-      .click()
-      .type('covid{enter}')
-      .get('[data-testid="search-result"]', { timeout: 15000 })
-      .as('test-results')
-      .should('have.length.at.least', 10)
-      .location('pathname', { timeout: 5000 })
-      .as('searchPath')
-      .get('@test-results')
-      .first()
-      .click()
-      .location('pathname', { timeout: 5000 })
-      .should((currentPath) => {
-        expect(currentPath).to.not.eq(this.searchPath)
-      })
-  })
-
-  it('should have no search results for long bogus search words', function () {
-    cy.visit('/')
-      .get('[data-testid="search-box"]')
-      .click()
-      .type('abcdefhijklmnopqrstuvwxyz1234567890{enter}')
-      .get('[data-testid="search-result"]', { timeout: 5000 })
-      .should('have.length', 0)
-  })
 })
