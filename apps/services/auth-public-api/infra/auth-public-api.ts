37c0d40502b445767f9b9f8a91d05b8ed8a45218
Pétur Neisti Erlingsson <petur@petur.io>
2022-06-14 16:21:30 +0000

    fix(infra): Fix env vars for auth-api/auth-public-api (#7740)
    * Add missing values
    
    * NX Format
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/auth-public-api/infra/auth-public-api.ts
+++ apps/services/auth-public-api/infra/auth-public-api.ts
@@ -1,10 +1,6 @@
 import { service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
-import {
-  Base,
-  Client,
-  NationalRegistry,
-  RskProcuring,
-} from '../../../../infra/src/dsl/xroad'
+import { json } from '../../../../infra/src/dsl/dsl'
+import { Base, Client, RskProcuring } from '../../../../infra/src/dsl/xroad'
 
 export const serviceSetup = (): ServiceBuilder<'services-auth-public-api'> => {
   return service('services-auth-public-api')
@@ -17,7 +13,6 @@ export const serviceSetup = (): ServiceBuilder<'services-auth-public-api'> => {
     })
     .env({
       IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/auth-api',
-      XROAD_NATIONAL_REGISTRY_ACTOR_TOKEN: 'true',
       IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
         staging: 'https://identity-server.staging01.devland.is',
@@ -28,6 +23,29 @@ export const serviceSetup = (): ServiceBuilder<'services-auth-public-api'> => {
         staging: 'https://identity-server.staging01.devland.is/api',
         prod: 'https://innskra.island.is/api',
       },
+      XROAD_TJODSKRA_API_PATH: '/SKRA-Protected/Einstaklingar-v1',
+      XROAD_NATIONAL_REGISTRY_ACTOR_TOKEN: 'true',
+      XROAD_NATIONAL_REGISTRY_SERVICE_PATH: {
+        dev: 'IS-DEV/GOV/10001/SKRA-Protected/Einstaklingar-v1',
+        staging: 'IS-TEST/GOV/6503760649/SKRA-Protected/Einstaklingar-v1',
+        prod: 'IS/GOV/6503760649/SKRA-Protected/Einstaklingar-v1',
+      },
+      XROAD_NATIONAL_REGISTRY_REDIS_NODES: {
+        dev: json([
+          'clustercfg.general-redis-cluster-group.5fzau3.euw1.cache.amazonaws.com:6379',
+        ]),
+        staging: json([
+          'clustercfg.general-redis-cluster-group.ab9ckb.euw1.cache.amazonaws.com:6379',
+        ]),
+        prod: json([
+          'clustercfg.general-redis-cluster-group.dnugi2.euw1.cache.amazonaws.com:6379',
+        ]),
+      },
+      XROAD_TJODSKRA_MEMBER_CODE: {
+        prod: '6503760649',
+        dev: '10001',
+        staging: '6503760649',
+      },
     })
     .secrets({
       IDENTITY_SERVER_CLIENT_SECRET:
@@ -35,7 +53,7 @@ export const serviceSetup = (): ServiceBuilder<'services-auth-public-api'> => {
       NATIONAL_REGISTRY_IDS_CLIENT_SECRET:
         '/k8s/xroad/client/NATIONAL-REGISTRY/IDENTITYSERVER_SECRET',
     })
-    .xroad(Base, Client, RskProcuring, NationalRegistry)
+    .xroad(Base, Client, RskProcuring)
     .ingress({
       primary: {
         host: {
@@ -50,16 +68,19 @@ export const serviceSetup = (): ServiceBuilder<'services-auth-public-api'> => {
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
             'nginx.ingress.kubernetes.io/enable-global-auth': 'false',
+            'nginx.ingress.kubernetes.io/rewrite-target': '/$2',
           },
           staging: {
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
             'nginx.ingress.kubernetes.io/enable-global-auth': 'false',
+            'nginx.ingress.kubernetes.io/rewrite-target': '/$2',
           },
           prod: {
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
             'nginx.ingress.kubernetes.io/enable-global-auth': 'false',
+            'nginx.ingress.kubernetes.io/rewrite-target': '/$2',
           },
         },
       },
