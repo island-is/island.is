9b530e29996c0201b3f2e656ac74f95ae4cc83db
Ívar Oddsson <ivaroddsson@gmail.com>
2022-06-14 13:44:41 +0000

    More ui work

--- apps/judicial-system/web/src/components/Header/Header.css.ts
+++ apps/judicial-system/web/src/components/Header/Header.css.ts
@@ -1,19 +1,10 @@
 import { style } from '@vanilla-extract/css'
 import { theme } from '@island.is/island-ui/theme'
 
-export const header = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  height: 112,
-  borderBottom: `1px solid ${theme.color.blue200}`,
-  padding: `0 ${theme.spacing[3]}px`,
-
-  '@media': {
-    [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
-      padding: `0 ${theme.spacing[6]}px`,
-    },
-  },
+export const container = style({
+  maxWidth: '1440px',
+  padding: `0 ${theme.spacing[6]}px`,
+  margin: '0 auto',
 })
 
 export const logoContainer = style({
@@ -45,16 +36,6 @@ export const logoContainerRvgName = style({
       width: '2px',
       backgroundColor: theme.color.dark200,
       margin: `5px ${theme.spacing[3]}px 0 ${theme.spacing[3]}px`,
-    },
-  },
-})
-
-export const link = style({
-  color: theme.color.blue400,
-
-  selectors: {
-    '&:hover': {
-      textDecoration: 'underline',
     },
   },
 })
--- apps/judicial-system/web/src/components/Header/Header.tsx
+++ apps/judicial-system/web/src/components/Header/Header.tsx
@@ -1,35 +1,21 @@
 import React, { useContext } from 'react'
-import { useWindowSize } from 'react-use'
-import { useRouter } from 'next/router'
 import { useIntl } from 'react-intl'
 
-import {
-  Logo,
-  Text,
-  Box,
-  Button,
-  Header,
-  UserMenu,
-  Icon,
-  Link,
-} from '@island.is/island-ui/core'
+import { Text, Box, Header, UserMenu, Icon } from '@island.is/island-ui/core'
 import { api } from '@island.is/judicial-system-web/src/services'
-import { UserContext } from '../UserProvider/UserProvider'
-import { UserRole } from '@island.is/judicial-system/types'
-import { theme } from '@island.is/island-ui/theme'
-import * as Constants from '@island.is/judicial-system/consts'
-
-import * as styles from './Header.css'
-import { formatPhoneNumber } from '@island.is/judicial-system/formatters'
+import {
+  capitalize,
+  formatPhoneNumber,
+} from '@island.is/judicial-system/formatters'
 import { core } from '@island.is/judicial-system-web/messages'
+
+import { UserContext } from '../UserProvider/UserProvider'
 import MarkdownWrapper from '../MarkdownWrapper/MarkdownWrapper'
+import * as styles from './Header.css'
 
 const HeaderContainer: React.FC = () => {
-  const router = useRouter()
   const { formatMessage } = useIntl()
   const { isAuthenticated, user } = useContext(UserContext)
-  const { width } = useWindowSize()
-  const isMobile = width <= theme.breakpoints.md
 
   const handleLogout = async () => {
     await api.logout()
@@ -37,106 +23,63 @@ const HeaderContainer: React.FC = () => {
   }
 
   return (
-    <Header
-      info={{
-        title: 'Dómsmálaráðuneytið',
-        description: 'Réttarvörslugátt',
-      }}
-      headerItems={
-        <UserMenu
-          language="is"
-          authenticated={isAuthenticated}
-          username={user?.name}
-          dropdownItems={
-            <>
-              <div className={styles.dropdownItem}>
-                <Box marginRight={2}>
-                  <Icon icon="person" type="outline" color="blue400" />
-                </Box>
-                <Box>
-                  <Box marginBottom={2}>
-                    <Text>{user?.role}</Text>
-                  </Box>
-                  <Box marginBottom={2}>
-                    <Text>{user?.institution?.name}</Text>
-                  </Box>
-                  <Box marginBottom={2}>
-                    <Text>{formatPhoneNumber(user?.mobileNumber)}</Text>
+    <div className={styles.container}>
+      <Header
+        info={{
+          title: 'Dómsmálaráðuneytið',
+          description: 'Réttarvörslugátt',
+        }}
+        headerItems={
+          <UserMenu
+            language="is"
+            authenticated={isAuthenticated}
+            username={user?.name}
+            dropdownItems={
+              <>
+                <div className={styles.dropdownItem}>
+                  <Box marginRight={2}>
+                    <Icon icon="person" type="outline" color="blue400" />
                   </Box>
                   <Box>
-                    <a
-                      href={`mailto:${user?.email}`}
-                      className={styles.link}
-                      rel="noopener noreferrer nofollow"
-                      target="_blank"
-                    >
+                    <Box marginBottom={2}>
+                      <Text>{capitalize(user?.title || '')}</Text>
+                    </Box>
+                    <Box marginBottom={2}>
+                      <Text>{user?.institution?.name}</Text>
+                    </Box>
+                    <Box marginBottom={2}>
+                      <Text>{formatPhoneNumber(user?.mobileNumber)}</Text>
+                    </Box>
+                    <Box>
                       <Text as="span">{user?.email}</Text>
-                    </a>
+                    </Box>
                   </Box>
-                </Box>
-              </div>
-              <div className={styles.dropdownItem}>
-                <Box marginRight={2}>
-                  <Icon
-                    icon="informationCircle"
-                    type="outline"
-                    color="blue400"
-                  />
-                </Box>
-                <Box>
-                  <MarkdownWrapper
-                    markdown={formatMessage(core.headerTipDisclaimer, {
-                      linkStart:
-                        '<a href="mailto:gudlaug.thorhallsdottir@dmr.is" rel="noopener noreferrer nofollow" target="_blank">gudlaug.thorhallsdottir@dmr.is',
-                      linkEnd: '</a>',
-                    })}
-                  />
-                </Box>
-              </div>
-            </>
-          }
-          onLogout={handleLogout}
-        />
-      }
-    />
-    // <header className={styles.header}>
-    //   <Link
-    //     href={
-    //       !user || !isAuthenticated
-    //         ? '/'
-    //         : user.role === UserRole.DEFENDER
-    //         ? `${Constants.DEFENDER_ROUTE}/${router.query.id}`
-    //         : user.role === UserRole.ADMIN
-    //         ? Constants.USER_LIST_ROUTE
-    //         : Constants.CASE_LIST_ROUTE
-    //     }
-    //     data-testid="link-to-home"
-    //   >
-    //     <Box display="flex" cursor="pointer" className={styles.logoContainer}>
-    //       <Logo width={isMobile ? undefined : 146} iconOnly={isMobile} />
-    //       {router.pathname !== '/' && (
-    //         <span className={styles.logoContainerRvgName}>
-    //           <Text>Réttarvörslugátt</Text>
-    //         </span>
-    //       )}
-    //     </Box>
-    //   </Link>
-    //   {isAuthenticated && (
-    //     <Button
-    //       variant="ghost"
-    //       icon="logOut"
-    //       iconType="outline"
-    //       size="small"
-    //       onClick={async () => {
-    //         await api.logout()
-    //         window.location.assign('/')
-    //       }}
-    //       data-testid="logout-button"
-    //     >
-    //       {user?.name}
-    //     </Button>
-    //   )}
-    // </header>
+                </div>
+                <div className={styles.dropdownItem}>
+                  <Box marginRight={2}>
+                    <Icon
+                      icon="informationCircle"
+                      type="outline"
+                      color="blue400"
+                    />
+                  </Box>
+                  <Box>
+                    <MarkdownWrapper
+                      markdown={formatMessage(core.headerTipDisclaimer, {
+                        linkStart:
+                          '<a href="mailto:gudlaug.thorhallsdottir@dmr.is" rel="noopener noreferrer nofollow" target="_blank">gudlaug.thorhallsdottir@dmr.is',
+                        linkEnd: '</a>',
+                      })}
+                    />
+                  </Box>
+                </div>
+              </>
+            }
+            onLogout={handleLogout}
+          />
+        }
+      />
+    </div>
   )
 }
 
