a56eb7fd8510e4dc3b411eb9d6b7e6213b627600
Petar Shomov <petar@andes.is>
2022-06-14 12:09:10 +0000

    Update libs/island-ui/utils/src/index.ts
    Fixing the build
    
    Co-authored-by: Kristofer <99652757+kristofer-lp8f@users.noreply.github.com>

--- libs/island-ui/utils/src/index.ts
+++ libs/island-ui/utils/src/index.ts
@@ -1,4 +1,4 @@
 export { escapeGrid } from './lib/escapeGrid'
 export { deorphanize } from './lib/deorphanize'
 export { hexToRgba } from './lib/hexToRgba'
-export { TestSupport } from './lib/testSupport'
+export type { TestSupport } from './lib/testSupport'
