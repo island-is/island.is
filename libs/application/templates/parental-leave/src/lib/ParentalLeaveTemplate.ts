f41bc4ab629b8ea5a0d3c284bf2ce8fd47545cbb
karenbjorg <102811817+karenbjorg@users.noreply.github.com>
2022-06-15 15:38:36 +0000

    fix(parental-leave): change other parent structure for secondary parent (#7771)
    * other parent structure for secondary parent
    
    * test
    
    * add old other parent schema back in so old application dont fail

--- libs/application/templates/parental-leave/src/lib/ParentalLeaveTemplate.ts
+++ libs/application/templates/parental-leave/src/lib/ParentalLeaveTemplate.ts
@@ -709,7 +709,7 @@ const ParentalLeaveTemplate: ApplicationTemplate<
           // have already been any applications created by the primary parent
           set(
             application.answers,
-            'otherParentId',
+            'otherParentObj.otherParentId',
             getOtherParentId(application),
           )
         }
@@ -867,11 +867,11 @@ const ParentalLeaveTemplate: ApplicationTemplate<
         // Current parent is secondary parent, this will set otherParentId to the id of the primary parent
         set(
           answers,
-          'otherParentId',
+          'otherParentObj.otherParentId',
           selectedChild.primaryParentNationalRegistryId,
         )
 
-        set(answers, 'otherParent', MANUAL)
+        set(answers, 'otherParentObj.chooseOtherParent', MANUAL)
 
         return context
       }),
