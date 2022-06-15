b2d6a9e414b58d51969aaa4f1285518c8f8690af
Jón Levy <levy@andes.is>
2022-06-15 19:20:05 +0000

    fix: better handling of baseUrl

--- apps/system-e2e/src/plugins/index.js
+++ apps/system-e2e/src/plugins/index.js
@@ -1,9 +1,7 @@
-const testEnvironment = process.env.TEST_ENVIRONMENT
 module.exports = (on, config) => {
-  config.env.testEnvironment = testEnvironment
-  if (testEnvironment) {
-    config.baseUrl = `https://beta.${config.env.testEnvironment}01.devland.is`
-  }
+  const testEnvironment = process.env.TEST_ENVIRONMENT || 'local'
+  config.baseUrl = config.env[testEnvironment].baseUrl
+
   on('before:browser:launch', (browser, launchOptions) => {
     if (['chrome', 'chromium', 'electron'].includes(browser.name)) {
       launchOptions.preferences.devTools = true
