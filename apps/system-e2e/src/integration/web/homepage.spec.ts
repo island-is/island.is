75538beac0cc4abc53b0bca48aa6a7c3a2f08e2b
Jón Levy <levy@andes.is>
2022-06-16 13:20:31 +0000

    fix: lifeevent-card is now on the anchor

--- apps/system-e2e/src/integration/web/homepage.spec.ts
+++ apps/system-e2e/src/integration/web/homepage.spec.ts
@@ -16,7 +16,7 @@ describe('Home page', () => {
 
   it('should have life events', () => {
     cy.visit('/')
-    cy.get('a:has([data-testid="lifeevent-card"])')
+    cy.get('[data-testid="lifeevent-card"]')
       .should('have.length.at.least', 3)
       .each((link) => {
         cy.visit(link.prop('href'))
