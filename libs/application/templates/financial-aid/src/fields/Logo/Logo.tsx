29910b582d52246484fc9f8432d4b8b783930c7b
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-06-16 16:46:21 +0000

    Add logo to pages

--- libs/application/templates/financial-aid/src/fields/Logo/Logo.tsx
+++ libs/application/templates/financial-aid/src/fields/Logo/Logo.tsx
@@ -1,31 +1,34 @@
-import React from "react";
+import React from 'react'
 
-import { logoKeyFromMunicipalityCode } from "@island.is/financial-aid/shared/lib";
-import { Box } from "@island.is/island-ui/core";
+import { logoKeyFromMunicipalityCode } from '@island.is/financial-aid/shared/lib'
+import { Box } from '@island.is/island-ui/core'
 
-import { FAFieldBaseProps } from "../../lib/types";
+import { FAFieldBaseProps } from '../../lib/types'
 import * as styles from './FileList.css'
 
-const withLogo = (Component: React.ComponentType<FAFieldBaseProps>) => (props: FAFieldBaseProps) => {
-    const { municipality } = props.application.externalData.nationalRegistry?.data
-    const svg = require(`../../svg/${logoKeyFromMunicipalityCode[municipality.municipalityId]}`).default
+const withLogo = (Component: React.ComponentType<FAFieldBaseProps>) => (
+  props: FAFieldBaseProps,
+) => {
+  const { municipality } = props.application.externalData.nationalRegistry?.data
+  const svg = require(`../../svg/${
+    logoKeyFromMunicipalityCode[municipality ? municipality.municipalityId : '']
+  }`).default
 
-    return (
-        <>
-            <Component {...props} />
-            <Box marginTop={[10, 10, 12]} display="flex" justifyContent="flexEnd">
-                <a
-                    href={municipality.homepage}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    className={styles.logo}
-                >
-                    <img src={svg} alt="" />
-                </a>
-            </Box>
-        </>
-
-    )
+  return (
+    <>
+      <Component {...props} />
+      <Box marginTop={[10, 10, 12]} display="flex" justifyContent="flexEnd">
+        <a
+          href={municipality?.homepage}
+          target="_blank"
+          rel="noreferrer noopener"
+          className={styles.logo}
+        >
+          <img src={svg} alt="" />
+        </a>
+      </Box>
+    </>
+  )
 }
 
 export default withLogo
