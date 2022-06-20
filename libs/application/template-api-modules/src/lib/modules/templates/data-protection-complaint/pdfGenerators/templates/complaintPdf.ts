3031ab1bd07ddfa2265cc8a91ae915c8b707a488
egill15 <egillb86@gmail.com>
2022-06-20 10:06:37 +0000

    Update complaintPdf.ts

--- libs/application/template-api-modules/src/lib/modules/templates/data-protection-complaint/pdfGenerators/templates/complaintPdf.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/data-protection-complaint/pdfGenerators/templates/complaintPdf.ts
@@ -24,6 +24,7 @@ import { DocumentInfo } from '@island.is/clients/data-protection-complaint'
 export async function generateComplaintPdf(
   application: Application,
   attachedFiles: DocumentInfo[],
+  r,
 ): Promise<Buffer> {
   const dto = applicationToComplaintPDF(application, attachedFiles)
   return await generatePdf<ComplaintPDF>(dto, dpcApplicationPdf)
@@ -70,7 +71,7 @@ function dpcApplicationPdf(
     const operatesWithinEuropeAnswer =
       c.operatesWithinEurope === 'yes' ? c.countryOfOperation : 'Ekki vitað/nei'
     addformFieldAndValue(
-      'Starfsemi innan Evrópu?',
+      'Veistu hvort viðkomandi aðili er með starfsemi í öðru landi innan Evrópu?',
       operatesWithinEuropeAnswer,
       doc,
       PdfConstants.SMALL_LINE_GAP,
@@ -141,9 +142,6 @@ function renderExternalDataMessages(
   addValue(externalData.userProfileTitle, doc, PdfConstants.BOLD_FONT)
   addValue(externalData.userProfileDescription, doc, PdfConstants.NORMAL_FONT)
   doc.moveDown()
-  addValue(externalData.checkboxText, doc, PdfConstants.BOLD_FONT)
-  addValue('Já', doc, PdfConstants.NORMAL_FONT)
-  doc.moveDown()
 }
 
 function renderInformationMessages(
@@ -203,33 +201,6 @@ function renderContactsAndComplainees(
       ? 'Kvartandi'
       : 'Kvartendur'
 
-  if (complaint.onBehalf === OnBehalf.OTHERS) {
-    addSubheader('Tengiliður', doc)
-  }
-
-  /* Contact if complaint is for the hand of company/organization, it's optional to fill out */
-  if (
-    complaint.contactInfo.contactName.length > 0 ||
-    complaint.contactInfo.contactEmail.length > 0
-  ) {
-    addSubheader('Tengiliður', doc)
-    complaint.contactInfo.contactName.length > 0 &&
-      addformFieldAndValue(
-        'Nafn',
-        complaint.contactInfo.contactName,
-        doc,
-        PdfConstants.SMALL_LINE_GAP,
-      )
-    complaint.contactInfo.contactEmail.length > 0 &&
-      addformFieldAndValue(
-        'Netfang',
-        complaint.contactInfo.contactEmail,
-        doc,
-        PdfConstants.SMALL_LINE_GAP,
-      )
-    doc.moveDown()
-  }
-
   if (
     complaint.onBehalf === OnBehalf.ORGANIZATION_OR_INSTITUTION ||
     complaint.onBehalf !== OnBehalf.OTHERS
@@ -280,6 +251,33 @@ function renderContactsAndComplainees(
   }
 
   renderAgencyComplainees(complaint, doc)
+
+  if (complaint.onBehalf === OnBehalf.OTHERS) {
+    addSubheader('Tengiliður', doc)
+  }
+
+  /* Contact if complaint is for the hand of company/organization, it's optional to fill out */
+  if (
+    complaint.contactInfo.contactName.length > 0 ||
+    complaint.contactInfo.contactEmail.length > 0
+  ) {
+    addSubheader('Tengiliður', doc)
+    complaint.contactInfo.contactName.length > 0 &&
+      addformFieldAndValue(
+        'Nafn',
+        complaint.contactInfo.contactName,
+        doc,
+        PdfConstants.SMALL_LINE_GAP,
+      )
+    complaint.contactInfo.contactEmail.length > 0 &&
+      addformFieldAndValue(
+        'Netfang',
+        complaint.contactInfo.contactEmail,
+        doc,
+        PdfConstants.SMALL_LINE_GAP,
+      )
+    doc.moveDown()
+  }
 }
 
 function renderAgencyComplainees(
