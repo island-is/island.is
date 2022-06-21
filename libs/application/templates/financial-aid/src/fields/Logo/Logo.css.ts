c31fad7921a732a18b4f32243496ac8bbb954296
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-06-21 14:08:35 +0000

    Fix on mobile

--- libs/application/templates/financial-aid/src/fields/Logo/Logo.css.ts
+++ libs/application/templates/financial-aid/src/fields/Logo/Logo.css.ts
@@ -3,7 +3,7 @@ import { theme } from '@island.is/island-ui/theme'
 
 export const logo = style({
   position: 'relative',
-  left: '18px',
+  left: '16px',
   width: '180px',
   '@media': {
     [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
