65c7da5cbb36724c59b30e66d66ef0095d644110
Petar Shomov <petar@andes.is>
2022-06-24 10:48:07 +0000

    rebasing from main

--- apps/system-e2e/src/integration/applications/parental-leave.spec.ts
+++ apps/system-e2e/src/integration/applications/parental-leave.spec.ts
@@ -1,29 +1,28 @@
-const { fakeUsers } = Cypress.env()
-const { authDomain } = Cypress.env(Cypress.env('testEnvironment'))
-
 describe('Parental leave', () => {
-  before(() => {
-    cy.cognitoLogin({
-      cognitoUsername: Cypress.env('cognitoUsername'),
-      cognitoPassword: Cypress.env('cognitoPassword'),
+  const fakeUsers: FakeUser[] = Cypress.env('fakeUsers')
+  const testEnvironment: TestEnvironment = Cypress.env('testEnvironment')
+  const { authUrl }: Pick<TestConfig, 'authUrl'> = Cypress.env(testEnvironment)
+
+  beforeEach(() => {
+    cy.log('fakeUsers', fakeUsers)
+    cy.log('authUrl', authUrl)
+    cy.log('testEnvironment', testEnvironment)
+    cy.idsLogin({
+      phoneNumber: fakeUsers[0].phoneNumber,
+      url: '/umsoknir/faedingarorlof',
     })
   })
+
   it('should navigate serviceportal', () => {
     cy.visit('/umsoknir/faedingarorlof')
 
-    cy.idsLogin({
-      phoneNumber: fakeUsers[0].phoneNumber,
-      authDomain: `https://${authDomain}`,
-    }).then((_) => {
-      cy.wait(2000)
-      cy.get("[data-testid='mockdata-no']").click()
-      cy.get('[data-testid="proceed"]').click()
+    cy.get("[data-testid='mockdata-no']").click()
+    cy.get('[data-testid="proceed"]').click()
 
-      cy.get("[data-testid='agree-to-data-providers']").click()
-      cy.get('[data-testid="proceed"]').click()
+    cy.get("[data-testid='agree-to-data-providers']").click()
+    cy.get('[data-testid="proceed"]').click()
 
-      cy.get("[data-testid='child-0']").click()
-      cy.get('[data-testid="select-child"]').click()
-    })
+    cy.get("[data-testid='child-0']").click()
+    cy.get('[data-testid="select-child"]').click()
   })
 })
