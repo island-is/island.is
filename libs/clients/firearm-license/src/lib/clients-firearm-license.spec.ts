2f9df6b398ccbca6b36e8bfa359d83cdc7dcf8ec
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-06-27 16:06:35 +0000

    feat: initial client creation

---
+++ libs/clients/firearm-license/src/lib/clients-firearm-license.spec.ts
@@ -0,0 +1,7 @@
+import { clientsFirearmLicense } from './clients-firearm-license'
+
+describe('clientsFirearmLicense', () => {
+  it('should work', () => {
+    expect(clientsFirearmLicense()).toEqual('clients-firearm-license')
+  })
+})
