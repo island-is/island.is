db169231b43f1c0c5fb17e24cdcdd57d7eee915b
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2022-06-27 16:56:21 +0000

    fix application configurations import

--- libs/application/ui-shell/src/hooks/useApplicationNamespaces.ts
+++ libs/application/ui-shell/src/hooks/useApplicationNamespaces.ts
@@ -1,5 +1,7 @@
-import { ApplicationConfigurations } from '@island.is/application/core'
-import { ApplicationTypes } from '@island.is/application/types'
+import {
+  ApplicationConfigurations,
+  ApplicationTypes,
+} from '@island.is/application/types'
 import { useNamespaces } from '@island.is/localization'
 
 export const useApplicationNamespaces = (type?: ApplicationTypes) => {
