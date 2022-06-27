57caff51c151c8a08e7413ebd66f04b25739496c
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-06-27 12:45:22 +0000

    feat: add .env

--- infra/src/dsl/xroad.ts
+++ infra/src/dsl/xroad.ts
@@ -198,8 +198,8 @@ export const AdrAndMachine = new XroadConf({
   env: {
     XROAD_ADR_MACHINE_LICENSE_PATH: {
       dev: 'IS-DEV/GOV/10013/Vinnueftirlitid-Protected/rettindi-v1',
-      staging: 'IS-DEV/GOV/10013/Vinnueftirlitid-Protected/rettindi-v1', //TODO: CHANGE THIS
-      prod: 'IS-DEV/GOV/10013/Vinnueftirlitid-Protected/rettindi-v1', //TODO: CHANGE THIS
+      staging: 'IS-TEST/GOV/4201810439/Vinnueftirlitid-Protected/rettindi-v1',
+      prod: 'IS/GOV/4201810439/Vinnueftirlitid-Protected/rettindi-v1',
     },
   },
 })
