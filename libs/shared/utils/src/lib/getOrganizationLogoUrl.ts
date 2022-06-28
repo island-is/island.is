c934d773cc4859382b8eee53b712815f9c24dca4
egill15 <egillb86@gmail.com>
2022-06-28 09:53:28 +0000

    Update getOrganizationLogoUrl.ts

--- libs/shared/utils/src/lib/getOrganizationLogoUrl.ts
+++ libs/shared/utils/src/lib/getOrganizationLogoUrl.ts
@@ -4,17 +4,17 @@ export const getOrganizationLogoUrl = (
   forName: string,
   orgs: Array<Organization>,
 ): string => {
+  const fallBackImage =
+    'https://images.ctfassets.net/8k0h54kbe6bj/6XhCz5Ss17OVLxpXNVDxAO/d3d6716bdb9ecdc5041e6baf68b92ba6/coat_of_arms.svg'
+  const parameters = '?w=60&h=60&fit=pad&bg=white&fm=png'
   if (orgs.length > 0) {
     const qs = String(forName).trim().toLocaleLowerCase()
     const match =
       orgs.find((o: Organization) => o.title?.toLocaleLowerCase() === qs) ||
       orgs.find((o: Organization) => o.logo?.title === 'Skjaldarmerki')
     const c = match?.logo?.url
-    const url =
-      c ??
-      'https://images.ctfassets.net/8k0h54kbe6bj/6XhCz5Ss17OVLxpXNVDxAO/d3d6716bdb9ecdc5041e6baf68b92ba6/coat_of_arms.svg'
-    const uri = `${url}?w=60&h=60&fit=pad&bg=white&fm=png`
-    return uri
+    const url = c ?? fallBackImage
+    return url.concat(parameters)
   }
-  return ''
+  return fallBackImage.concat(parameters)
 }
