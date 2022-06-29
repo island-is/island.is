900f1d9c766fe2fa896bfc0eb28ccf2526b46a38
disaerna <disa.erna@gmail.com>
2022-06-29 14:51:49 +0000

    fix: remove return

--- apps/service-portal/src/screens/Modules/Modules.tsx
+++ apps/service-portal/src/screens/Modules/Modules.tsx
@@ -49,35 +49,29 @@ const RouteLoader: FC<{
   routes: ServicePortalRoute[]
   userInfo: User
   client: ApolloClient<NormalizedCacheObject>
-}> = React.memo(({ routes, userInfo, client }) => {
-  return (
-    <Switch>
-      {routes.map((route) =>
-        route.enabled === false ? (
-          <Route
-            key={Array.isArray(route.path) ? route.path[0] : route.path}
-            path={route.path}
-            component={AccessDenied}
-          />
-        ) : (
-          <Route
-            path={route.path}
-            exact
-            key={Array.isArray(route.path) ? route.path[0] : route.path}
-            render={() => (
-              <RouteComponent
-                route={route}
-                userInfo={userInfo}
-                client={client}
-              />
-            )}
-          />
-        ),
-      )}
-      {routes.length > 0 && <Route component={NotFound} />}
-    </Switch>
-  )
-})
+}> = React.memo(({ routes, userInfo, client }) => (
+  <Switch>
+    {routes.map((route) =>
+      route.enabled === false ? (
+        <Route
+          key={Array.isArray(route.path) ? route.path[0] : route.path}
+          path={route.path}
+          component={AccessDenied}
+        />
+      ) : (
+        <Route
+          path={route.path}
+          exact
+          key={Array.isArray(route.path) ? route.path[0] : route.path}
+          render={() => (
+            <RouteComponent route={route} userInfo={userInfo} client={client} />
+          )}
+        />
+      ),
+    )}
+    {routes.length > 0 && <Route component={NotFound} />}
+  </Switch>
+))
 
 const Modules: FC<{}> = () => {
   const [{ routes }] = useStore()
