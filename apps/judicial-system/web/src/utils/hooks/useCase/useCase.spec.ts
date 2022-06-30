2ccb40d69f42feba83ee92de07a47496551470a0
Ívar Oddsson <ivaroddsson@gmail.com>
2022-06-30 09:38:50 +0000

    Cleanup tests

--- apps/judicial-system/web/src/utils/hooks/useCase/useCase.spec.ts
+++ apps/judicial-system/web/src/utils/hooks/useCase/useCase.spec.ts
@@ -1,80 +1,72 @@
 import { Case, UpdateCase } from '@island.is/judicial-system/types'
+
 import { update, auto } from './'
 
-describe('update', () => {
-  test('should not update field that is already defined', () => {
-    const newCase = { ruling: 'ruling2' } as UpdateCase
-    const workingCase = { ruling: 'ruling1' } as Case
+describe('useCase', () => {
+  describe('update', () => {
+    test('should not update field that is already defined', () => {
+      const newCase = { ruling: 'ruling2' } as UpdateCase
+      const workingCase = { ruling: 'ruling1' } as Case
 
-    const res = update(newCase, workingCase)
-    expect(res.ruling).toBe('ruling1')
+      const res = update(newCase, workingCase)
+      expect(res.ruling).toBe('ruling1')
+    })
+
+    test('should update field that is undefined on the workingCase', () => {
+      const newCase = { ruling: 'ruling2' } as UpdateCase
+      const workingCase = { ruling: undefined } as Case
+
+      const res = update(newCase, workingCase)
+      expect(res.ruling).toBe('ruling2')
+    })
+
+    test('should not update field when update is undefined', () => {
+      const newCase = { ruling: undefined } as UpdateCase
+      const workingCase = { ruling: '' } as Case
+
+      const res = update(newCase, workingCase)
+      expect(res.ruling).toBe('')
+    })
+
+    test('should not update fields when update is empty', () => {
+      const newCase = {} as UpdateCase
+      const workingCase = { ruling: 'some ruling' } as Case
+
+      const res = update(newCase, workingCase)
+      expect(res.ruling).toBe('some ruling')
+    })
   })
 
-  test('should update field that is undefined on the workingCase', () => {
-    const newCase = { ruling: 'ruling2' } as UpdateCase
-    const workingCase = { ruling: undefined } as Case
+  describe('auto', () => {
+    test('should not autofill when field has value in working case and force is not set', () => {
+      const workingCase = { ruling: 'ruling1' } as Case
 
-    const res = update(newCase, workingCase)
-    expect(res.ruling).toBe('ruling2')
-  })
+      const res = auto([{ ruling: 'ruling2' }], workingCase)
 
-  test('should not update field when update is undefined', () => {
-    const newCase = { ruling: undefined } as UpdateCase
-    const workingCase = { ruling: '' } as Case
+      expect(res.ruling).toBe('ruling1')
+    })
 
-    const res = update(newCase, workingCase)
-    expect(res.ruling).toBe('')
-  })
+    test('should overwrite value in workingCase if force is set', () => {
+      const workingCase = { ruling: 'ruling1' } as Case
 
-  test('should not update fields that are not in .....todo', () => {
-    const newCase = {} as UpdateCase
-    const workingCase = { ruling: 'some ruling' } as Case
+      const res = auto([{ ruling: 'ruling2', force: true }], workingCase)
 
-    const res = update(newCase, workingCase)
-    expect(res.ruling).toBe('some ruling')
-  })
-})
+      expect(res.ruling).toBe('ruling2')
+    })
 
-describe('TODO auto', () => {
-  test('1', () => {
-    const workingCase = { ruling: 'ruling1' } as Case
+    test('should only overwrite value in workingCase if force is set', () => {
+      const workingCase = {
+        ruling: 'ruling1',
+        description: 'description1',
+      } as Case
 
-    const res = auto([{ ruling: 'ruling2' }], workingCase)
+      const res = auto(
+        [{ ruling: 'ruling2', force: true }, { description: 'description2' }],
+        workingCase,
+      )
 
-    expect(res.ruling).toBe('ruling1')
-  })
-
-  test('2', () => {
-    const workingCase = { ruling: 'ruling1' } as Case
-
-    const res = auto([{ ruling: 'ruling2', force: true }], workingCase)
-
-    expect(res.ruling).toBe('ruling2')
-  })
-
-  test('3', () => {
-    const workingCase = {
-      ruling: 'ruling1',
-      description: 'description1',
-    } as Case
-
-    const res = auto(
-      [{ ruling: 'ruling2', force: true }, { description: 'description2' }],
-      workingCase,
-    )
-
-    expect(res.ruling).toBe('ruling1')
-    expect(res.description).toBe('description1')
-  })
-
-  test('3', () => {
-    const workingCase = {} as Case
-
-    const res = auto(
-      [{ courtDate: formatIso(new Date('2022-06-06')) }],
-      workingCase,
-    )
-
-    expect(res.courtDate).toBe('ruling1')
+      expect(res.ruling).toBe('ruling1')
+      expect(res.description).toBe('description1')
+    })
   })
 })
