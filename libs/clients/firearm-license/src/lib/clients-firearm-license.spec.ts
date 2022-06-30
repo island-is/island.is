17582dd1445af914a06124d13f8ea6f83c55c486
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-06-30 13:42:42 +0000

    feat: fetch implemented

--- libs/clients/firearm-license/src/lib/clients-firearm-license.spec.ts
+++
@@ -1,7 +0,0 @@
-import { clientsFirearmLicense } from './clients-firearm-license'
-
-describe('clientsFirearmLicense', () => {
-  it('should work', () => {
-    expect(clientsFirearmLicense()).toEqual('clients-firearm-license')
-  })
-})
