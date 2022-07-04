d3060839f36721a1df36674b5435112c7cd25e41
Brynjar Ingimarsson <brynjar@stefna.is>
2022-07-04 17:14:35 +0000

    Update slice styles for 2022 conference

--- apps/web/components/Organization/Slice/EventSlice/EventSlice.tsx
+++ apps/web/components/Organization/Slice/EventSlice/EventSlice.tsx
@@ -24,56 +24,66 @@ export const EventSlice: React.FC<SliceProps> = ({ slice }) => {
       <Box
         className={styles.wrapper}
         style={{
-          background: `url(${slice.backgroundImage?.url}), rgb(31, 101, 251)`,
+          background: `url(${slice.backgroundImage?.url}), rgb(130, 46, 166)`,
         }}
       >
         <GridContainer>
           <GridRow>
-            <GridColumn span={['12/12', '12/12', '2/12']}>
+            <GridColumn span={['12/12', '12/12', '12/12', '12/12', '2/12']}>
               <Box
                 display="flex"
-                flexDirection={['row', 'row', 'column']}
-                paddingBottom={[2, 2, 0]}
+                flexDirection={['row', 'row', 'row', 'row', 'column']}
+                paddingBottom={[2, 2, 2, 2, 0]}
                 justifyContent="spaceBetween"
                 alignItems="center"
                 height="full"
               >
-                <Box display="flex" flexDirection={['row', 'row', 'column']}>
-                  <Text color="white" variant="h1">
+                <Box
+                  display="flex"
+                  flexDirection={['row', 'row', 'row', 'row', 'column']}
+                  className={styles.textWrapper}
+                >
+                  <Text color="mint400" variant="h1">
                     {date[2]}
                   </Text>
-                  <Hidden above="sm">
-                    <Text color="white" variant="h1">
+                  <Hidden above="lg">
+                    <Text color="mint400" variant="h1">
                       .
                     </Text>
                   </Hidden>
-                  <Text color="white" variant="h1">
+                  <Text color="mint400" variant="h1">
                     {date[1]}
-                  </Text>
-                </Box>
-                <Box>
-                  <Text color="white" variant="h2">
-                    {date[0]}
                   </Text>
                 </Box>
               </Box>
             </GridColumn>
             <GridColumn
-              span={['12/12', '12/12', '9/12']}
-              offset={['0', '0', '1/12']}
+              span={['12/12', '12/12', '12/12', '12/12', '6/12']}
+              offset={['0', '0', '0', '0', '1/12']}
             >
-              <Text color="white" variant="h1">
-                {slice.title}
-              </Text>
+              <Box className={styles.textWrapper}>
+                <Text color="white" variant="h1">
+                  {slice.title}
+                </Text>
+              </Box>
               <Text
                 color="white"
                 variant="intro"
+                fontWeight="regular"
                 marginTop={4}
-                marginBottom={20}
               >
                 {slice.subtitle}
               </Text>
-              <Box display="flex" justifyContent="flexEnd">
+            </GridColumn>
+            <GridColumn
+              span={['12/12', '12/12', '12/12', '12/12', '3/12']}
+              paddingTop={[0, 0, 0, 0, 10]}
+            >
+              <Box
+                display="flex"
+                justifyContent="flexEnd"
+                marginTop={[4, 4, 4, 4, 30]}
+              >
                 {!!slice.link && (
                   <Link href={slice.link?.url}>
                     <Button colorScheme="light">{slice.link?.text}</Button>
