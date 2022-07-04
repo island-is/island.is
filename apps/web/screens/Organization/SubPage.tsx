5e34734f451c638a984ea8059374af31b9d66c94
runarvestmann <runarvestmann@gmail.com>
2022-07-04 14:38:33 +0000

    Added margin bottom prop to organization slices

--- apps/web/screens/Organization/SubPage.tsx
+++ apps/web/screens/Organization/SubPage.tsx
@@ -172,13 +172,14 @@ const renderSlices = (
     case 'SliceDropdown':
       return <SliceDropdown slices={slices} sliceExtraText={extraText} />
     default:
-      return slices.map((slice) => (
+      return slices.map((slice, index) => (
         <OrganizationSlice
           key={slice.id}
           slice={slice}
           namespace={namespace}
           organizationPageSlug={organizationPageSlug}
           renderedOnOrganizationSubpage={true}
+          marginBottom={index === slices.length - 1 ? 5 : 0}
         />
       ))
   }
