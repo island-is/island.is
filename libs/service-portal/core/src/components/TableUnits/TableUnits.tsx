877937381a383ed6df9575d7db2715d2602d9a7a
disaerna <disa.erna@gmail.com>
2022-07-05 13:26:10 +0000

    fix: imports after update

--- libs/service-portal/core/src/components/TableUnits/TableUnits.tsx
+++ libs/service-portal/core/src/components/TableUnits/TableUnits.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
-import { m, tableStyles } from '@island.is/service-portal/core'
+import { tableStyles } from '../../utils/utils'
+import { m } from '../../lib/messages'
 import { useLocale } from '@island.is/localization'
 import { Text, Box, Button, Table as T } from '@island.is/island-ui/core'
 
