a6094a2c7af782ce585a978e1702165a0f6c167d
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2022-07-06 15:31:45 +0000

    fix(application-system-payment): Incorporate FJS claim (fella niður kröfu) (#7950)
    * init
    
    * Change prune time and add delete button
    for CR and MC
    
    * Setup ChargeFjsV2Client
    
    * Cleanup ChargeFjsV2
    Delete charge on application prune
    Delete entry in Payment table when deleting application
    Delete charge on applitcation delete
    
    * Fix autoAuth configuration for chargeFjsV2
    
    * Include externalData field when fetching application to
    prune, to be able to fetch the chargeId
    
    * Update example payment template
    
    * Update codeowners
    
    * Move delete-charge function to separate service
    Throw error (and dont continue) if an error occurs while deleting charge
    
    * Continue pruning data, if deleting charge fails
    (but keep chargeId in externalData)
    
    * Change prune time to 1 hour
    
    * Fixed for code review
    
    * Cleanup test
    
    * Fix helm-values-validation error
    
    * Fix lint error
    
    * Fixes from codereview
    
    * Add back api_resource.scope
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/example-payment/src/lib/examplePaymentTemplate.ts
+++ libs/application/templates/example-payment/src/lib/examplePaymentTemplate.ts
@@ -58,7 +58,13 @@ const template: ApplicationTemplate<
         meta: {
           name: 'Payment state',
           progress: 0.9,
-          lifecycle: DefaultStateLifeCycle,
+          // Note: should be pruned at some time, so we can delete the FJS charge with it
+          lifecycle: {
+            shouldBeListed: true,
+            shouldBePruned: true,
+            // Applications that stay in this state for 1 hour will be pruned automatically
+            whenToPrune: 1 * 3600 * 1000,
+          },
           onEntry: {
             apiModuleAction: ApiActions.createCharge,
           },
@@ -71,6 +77,7 @@ const template: ApplicationTemplate<
                 { event: DefaultEvents.SUBMIT, name: 'Panta', type: 'primary' },
               ],
               write: 'all',
+              delete: true, // Note: Should be deletable, so user is able to delete the FJS charge with the application
             },
           ],
         },
