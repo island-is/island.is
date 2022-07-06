1b1ac3741707eb6d5535d934812fa7af1a17f327
disaerna <disa.erna@gmail.com>
2022-07-06 09:52:27 +0000

    fix: remove unnec check

--- libs/service-portal/core/src/utils/amountFormat.ts
+++ libs/service-portal/core/src/utils/amountFormat.ts
@@ -1,5 +1,4 @@
 export const amountFormat = (value: number): string => {
-  if (value === 0) return '0 kr.'
   return `${Number(value.toFixed(1).replace(/\.0$/, '')).toLocaleString(
     'de-DE',
   )} kr.`
