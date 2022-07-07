9fa40bfd84ff5ce109358129b1b78aee4651441a
runarvestmann <runar@stefna.is>
2022-07-07 19:16:26 +0000

    Removing comment

--- apps/web/components/Organization/Slice/OrganizationSlice.tsx
+++ apps/web/components/Organization/Slice/OrganizationSlice.tsx
@@ -75,8 +75,6 @@ const MultipleStatistics = dynamic(() =>
   import('@island.is/web/components').then((mod) => mod.MultipleStatistics),
 )
 
-// Testing changed github settings, ignore this comment
-
 interface OrganizationSliceProps {
   slice: Slice
   namespace?: Namespace
