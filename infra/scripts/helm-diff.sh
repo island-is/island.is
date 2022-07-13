45114ecf620ebe33fe4f638a188d661dd9b831dc
Snorri Hjörvar Jóhannsson <snorri@andes.is>
2022-07-13 13:11:19 +0000

    feat(infra): Script to diff between releases

---
+++ infra/scripts/helm-diff.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Requirements:
+# Install ydiff
+# - brew install ydiff
+# - pip install --upgrade ydiff
+# https://github.com/ymattw/ydiff
+
+if ! command -v ydiff &> /dev/null
+then
+    echo "Please install ydiff via pip or brew"
+    echo "https://github.com/ymattw/ydiff"
+    exit 1
+fi
+
+if [ -z "${1}" ]; then
+    echo "Specify the current release name"
+    exit 1
+fi
+if [ -z "${2}" ]; then
+    echo "Specify the new release name"
+    exit 1
+fi
+
+# Script to bake the helm chart as spinnaker does it
+
+# curl https://api.github.com/repos/island-is/island.is/contents/charts/islandis/values.prod.yaml | jq -r ".content" | base64 --decode
+
+curl -s -H "Accept: application/json" 'https://api.github.com/repos/island-is/island.is/contents/charts/islandis/values.prod.yaml?ref='release/${2}'' | jq -r ".content" | base64 --decode > new-release.json
+curl -s -H "Accept: application/json" 'https://api.github.com/repos/island-is/island.is/contents/charts/islandis/values.prod.yaml?ref='release/${1}'' | jq -r ".content" | base64 --decode > current-release.json
+
+
+diff -u ./current-release.json ./new-release.json | ydiff -w 0 -s
+
+rm -f ./new-release.json
+rm -f ./current-release.json
