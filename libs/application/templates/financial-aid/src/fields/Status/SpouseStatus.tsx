baf06241a28db6eabc0d991778a08f9230c0d838
Davíð Phuong Xuan Nguyen <davidpxn@kolibri.is>
2022-07-14 10:39:22 +0000

    Add loading dots while fetching application

--- libs/application/templates/financial-aid/src/fields/Status/SpouseStatus.tsx
+++ libs/application/templates/financial-aid/src/fields/Status/SpouseStatus.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { ApplicationState } from '@island.is/financial-aid/shared/lib'
-import { Box } from '@island.is/island-ui/core'
+import { Box, LoadingDots } from '@island.is/island-ui/core'
 
 import { FAFieldBaseProps } from '../../lib/types'
 import {
@@ -15,11 +15,15 @@ import useApplication from '../../lib/hooks/useApplication'
 import * as styles from './Status.css'
 
 const SpouseStatus = ({ application, goToScreen }: FAFieldBaseProps) => {
-  const { currentApplication } = useApplication(
+  const { currentApplication, loading } = useApplication(
     application.externalData.veita.data.currentApplicationId,
   )
   const { nationalRegistry } = application.externalData
   const state = currentApplication?.state
+
+  if(loading) {
+    return <LoadingDots />    
+  }    
 
   return (
     <Box paddingBottom={5} className={styles.container}>
