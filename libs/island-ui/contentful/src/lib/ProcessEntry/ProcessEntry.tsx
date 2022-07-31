06cfd6b308a369cd2956d319e916ffc4a0151ba8
runarvestmann <runar@stefna.is>
2022-07-31 12:55:53 +0000

    Remove unnecessary process entry code since we have the same functionality in the mapper

--- libs/island-ui/contentful/src/lib/ProcessEntry/ProcessEntry.tsx
+++ libs/island-ui/contentful/src/lib/ProcessEntry/ProcessEntry.tsx
@@ -34,11 +34,6 @@ export const ProcessEntryLinkButton: FC<
   newTab = true,
   ...buttonProps
 }) => {
-  // Make sure that links without a protocol get prefixed with https:
-  const url = processLink.startsWith('//')
-    ? `https:${processLink}`
-    : processLink
-
   const button = (
     <Button icon="open" iconType="outline" nowrap {...buttonProps}>
       {buttonText}
@@ -50,10 +45,10 @@ export const ProcessEntryLinkButton: FC<
       title={processTitle}
       baseId="process-entry-modal-iframe"
       disclosure={button}
-      src={url}
+      src={processLink}
     />
   ) : (
-    <Link href={url} newTab={newTab} skipTab>
+    <Link href={processLink} newTab={newTab} skipTab>
       {button}
     </Link>
   )
