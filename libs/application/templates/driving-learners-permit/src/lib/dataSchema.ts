7579182dfd6962ce88062f105eb1740f2f1a3de4
kksteini <steini@kosmosogkaos.is>
2022-08-02 10:21:54 +0000

    Some validation cleanup

--- libs/application/templates/driving-learners-permit/src/lib/dataSchema.ts
+++ libs/application/templates/driving-learners-permit/src/lib/dataSchema.ts
@@ -1,14 +1,13 @@
 import * as z from 'zod'
-import { parsePhoneNumberFromString } from 'libphonenumber-js'
 import { m } from './messages'
 
 // todo: set message strings for the error messages
 export const dataSchema = z.object({
-  example: z.string().length(10).optional(),
   approveExternalData: z.boolean().refine((v) => v),
   studentMentorability: z.object({
     studentIsMentorable: z.string().refine((v) => v === 'isMentorable'),
     studentName: z.string().nonempty(),
     studentNationalId: z.string().length(10),
   }),
+  requirementsMet: z.boolean().refine((v) => v),
 })
