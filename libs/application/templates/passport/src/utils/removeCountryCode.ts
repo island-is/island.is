62965b390c680e97c0da87ee0ec4a6aaf01e4f2a
albina <albina@kosmosogkaos.is>
2022-08-08 15:59:42 +0000

    phone number tweak for country code

--- libs/application/templates/passport/src/utils/removeCountryCode.ts
+++ libs/application/templates/passport/src/utils/removeCountryCode.ts
@@ -1,7 +1,9 @@
 export const removeCountryCode = (phone: string) => {
-  return phone?.startsWith('+354')
+  const phoneNumber = phone?.startsWith('+354')
     ? phone.slice(4)
     : phone?.startsWith('00354')
     ? phone?.slice(5)
     : phone
+
+  return phoneNumber.replace(/\D/g, '')
 }
