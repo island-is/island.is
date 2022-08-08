98de2df0fe0c7f3a06dcb426442f4451f3a86308
albina <albina@kosmosogkaos.is>
2022-08-08 16:14:25 +0000

    simplify

--- libs/application/templates/passport/src/utils/removeCountryCode.ts
+++ libs/application/templates/passport/src/utils/removeCountryCode.ts
@@ -1,9 +1,3 @@
 export const removeCountryCode = (phone: string) => {
-  const phoneNumber = phone?.startsWith('+354')
-    ? phone.slice(4)
-    : phone?.startsWith('00354')
-    ? phone?.slice(5)
-    : phone
-
-  return phoneNumber.replace(/\D/g, '')
+  phone.replace(/(^00354|^\+354|\D)/g, '')
 }
