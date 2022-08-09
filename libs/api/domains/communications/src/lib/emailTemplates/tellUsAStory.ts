7003e7a9faa7e9f4b7ae5ce146b256ea14bcfeeb
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2022-08-09 11:41:02 +0000

    chore(island.is): Change email sent to island.is (#8114)
    * Change email sent to island.is so that the sender is the input email.
    
    * revert contact us changes
    
    * Update libs/api/domains/communications/src/lib/emailTemplates/tellUsAStory.ts
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/communications/src/lib/emailTemplates/tellUsAStory.ts
+++ libs/api/domains/communications/src/lib/emailTemplates/tellUsAStory.ts
@@ -5,8 +5,8 @@ import { environment } from '../environments/environment'
 
 export const getTemplate = (input: TellUsAStoryInput): SendMailOptions => ({
   from: {
-    name: 'Island.is communications',
-    address: environment.emailOptions.sendFrom!,
+    name: input.name,
+    address: input.email,
   },
   replyTo: {
     name: input.name,
