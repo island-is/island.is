54e277e1223e62c1388d71512e782c6136d423c8
Þórður H <thordur@hugsmidjan.is>
2022-08-10 16:37:39 +0000

    feat(service-portal): Passport client (#7952)
    * Add passports api client
    
    * Finishing up on passport client
    
    * Add config, readme and finishing touches to passport module
    
    * Update readme
    
    * Update passport readme title
    
    * Update api scope for passport client
    
    * Update passports.config.ts
    
    * Formatting config
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/api/infra/api.ts
+++ apps/api/infra/api.ts
@@ -8,6 +8,7 @@ import {
   HealthInsurance,
   Labor,
   NationalRegistry,
+  Passports,
   Payment,
   Properties,
   PaymentSchedule,
@@ -204,6 +205,7 @@ export const serviceSetup = (services: {
       FishingLicense,
       MunicipalitiesFinancialAid,
       Vehicles,
+      Passports,
     )
     .files({ filename: 'islyklar.p12', env: 'ISLYKILL_CERT' })
     .ingress({
---
+++ libs/clients/passports/src/index.ts
@@ -0,0 +1,3 @@
+export { PassportsClientModule } from './lib/passports.module'
+export { PassportsClientConfig } from './lib/passports.config'
+export * from '../gen/fetch'
