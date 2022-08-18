a3069192ba84e7db52f350ae4f40d426ec8b1f13
egill15 <egillb86@gmail.com>
2022-08-18 08:31:44 +0000

    Add company scope for fishinglicense service

--- libs/api/domains/fishing-license/src/graphql/fishing-license.resolver.ts
+++ libs/api/domains/fishing-license/src/graphql/fishing-license.resolver.ts
@@ -15,7 +15,7 @@ import { FishingLicenseService } from '../lib/fishing-license.service'
 import { ApiScope } from '@island.is/auth/scopes'
 
 @UseGuards(IdsUserGuard, IdsAuthGuard, ScopesGuard)
-@Scopes(ApiScope.internal)
+@Scopes(ApiScope.meDetails, ApiScope.company)
 @Resolver()
 export class FishingLicenseResolver {
   constructor(private fishingLicenseService: FishingLicenseService) {}
