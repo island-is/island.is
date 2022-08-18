8e438ca29b73ae67e4859f42537edcb09d56598e
Jón Levy <levy@andes.is>
2022-08-18 10:57:24 +0000

    build: extend Cypress official docker image, custom webpack for system-e2e

--- apps/system-e2e/webpack.config.js
+++ apps/system-e2e/webpack.config.js
@@ -1,30 +1,73 @@
-const webpack = require('webpack')
-const {resolve, join, relative} = require('path')
+const { parse, resolve, relative, join } = require('path')
 const fs = require('fs')
 const nrwlConfig = require('@nrwl/react/plugins/webpack.js')
-// import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
 
-const getAllFiles = function(ref, dirPath, arrayOfFiles) {
+const getRootFiles = function (files) {
+  return files.reduce((acc, file) => {
+    return { ...acc, [parse(file).name]: [join(__dirname, file)] }
+  }, {})
+}
+const getAllFiles = function (ref, dirPath, arrayOfFiles) {
   const files = fs.readdirSync(dirPath)
 
-  files.forEach(function(file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(ref, dirPath + "/" + file, arrayOfFiles)
-    } else if (file.endsWith('.ts') && !file.endsWith('.d.ts')) {
-      arrayOfFiles[`${relative(ref, dirPath)}/${file.replace('.ts', '')}`] = [join(dirPath, "/", file)]
+  files.forEach(function (file) {
+    if (fs.statSync(join(dirPath, file)).isDirectory()) {
+      arrayOfFiles = getAllFiles(ref, join(dirPath, file), arrayOfFiles)
+    } else if (parse(file).ext === '.ts' && !file.endsWith('.d.ts')) {
+      arrayOfFiles[`${relative(ref, dirPath)}/${parse(file).name}`] = [
+        join(dirPath, file),
+      ]
+      // arrayOfFiles[`${relative(ref, dirPath)}/${file}`] = [join(dirPath, file)]
     }
   })
-
   return arrayOfFiles
 }
+
 module.exports = (config) => {
   nrwlConfig(config) // first call it so that it @nrwl/react plugin adds its configs,
-  config.entry = getAllFiles(resolve(__dirname, './src'),resolve(__dirname, './src'), {})
-  config.output = {
-    ...config.output,
-    filename: '[name].js',
-    clean: true
+  config.entry = {
+    ...getAllFiles(
+      resolve(__dirname, './src'),
+      resolve(__dirname, './src'),
+      {},
+    ),
+    ...getRootFiles(['cypress.config.ts']),
+    'cypress.env.json': [resolve(__dirname, './cypress.env.json')],
   }
 
+  console.log(config.entry)
+  config.module = {
+    ...config.module,
+    rules: [
+      {
+        test: /cypress.env.json/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name]',
+        },
+      },
+      ...config.module.rules,
+    ],
+  }
+  console.log(config.module.rules)
+
+  config.output = {
+    ...config.output,
+    // filename: (name) => {
+    //   console.log(name)
+    //   return name.runtime === 'cypress.env.json' ? '[name].json' : '[name].js'
+    // },
+
+    filename: '[name].js',
+    clean: true,
+    assetModuleFilename: '[name]',
+    // assetModuleFilename: (name) => {
+    //   return name === 'cypress.env.json' ? '[name].js' : 'cypress.env.json'
+    // },
+  }
   return config
 }
+
+// filename: (name) => {
+//   return name === 'cypress.env.json' ? '[name].js' : 'cypress.env.json'
+// },
--- scripts/ci/90_docker-cypress.sh
+++ scripts/ci/90_docker-cypress.sh
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 
-# Building Docker images for Cypress-based apps
-exec "$DIR"/_docker.sh Dockerfile.cypress output-cypress
+# Building Docker images for Cypress-based apps, pass extra args to use in docker cmds if needed
+exec "$DIR"/_podman.sh Dockerfile.cypress.new output-cypress "$@"
