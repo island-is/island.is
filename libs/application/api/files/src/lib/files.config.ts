2434ae4bb421c10296c95daafb50da638bd23d45
obmagnusson <obmagnusson@gmail.com>
2022-08-22 09:40:44 +0000

    env json

--- libs/application/api/files/src/lib/files.config.ts
+++ libs/application/api/files/src/lib/files.config.ts
@@ -18,12 +18,14 @@ export const ApplicationFilesConfig = defineConfig({
     attachmentBucket: env.optional('APPLICATION_ATTACHMENT_BUCKET') ?? '',
     presignBucket: env.optional('FILE_SERVICE_PRESIGN_BUCKET') ?? '',
     redis: {
-      nodes: env
-        .requiredJSON<string>(
-          'REDIS_URL_NODE_01',
-          'localhost:7000,localhost:7001,localhost:7002,localhost:7003,localhost:7004,localhost:7005',
-        )
-        .split(','),
+      nodes: env.requiredJSON<string[]>('REDIS_URL_NODE_01', [
+        'localhost:7000',
+        'localhost:7001',
+        'localhost:7002',
+        'localhost:7003',
+        'localhost:7004',
+        'localhost:7005',
+      ]),
       ssl: isRunningOnEnvironment('production'),
     },
   }),
