d48db3581451fd3b9294514b6c8af15e56e3ec41
Alexandra Diljá <alexandrab18@ru.is>
2022-08-23 13:52:17 +0000

    get marital status and block non married peopled

--- libs/application/templates/marriage-conditions/src/dataProviders/NationalRegistryMaritalStatusProvider.ts
+++ libs/application/templates/marriage-conditions/src/dataProviders/NationalRegistryMaritalStatusProvider.ts
@@ -1,3 +1,4 @@
+import { MaritalStatus, NationalRegistryPerson } from '../types/schema'
 import {
   BasicDataProvider,
   SuccessfulDataProviderResult,
@@ -10,12 +11,11 @@ export class NationalRegistryMaritalStatusProvider extends BasicDataProvider {
   async provide(): Promise<any> {
     const query = `
       query NationalRegistryUserQuery {
-        nationalRegistryUser {
-          maritalStatus
+        nationalRegistryUserV2 {
           spouse {
             name
             nationalId
-            cohabitant
+            maritalStatus
           }
         }
       }
@@ -31,8 +31,25 @@ export class NationalRegistryMaritalStatusProvider extends BasicDataProvider {
           )
           return Promise.reject({})
         }
+        const maritalStatus = this.formatMaritalStatus(
+          (response.data.nationalRegistryUser as NationalRegistryPerson).spouse
+            ?.maritalStatus || '',
+        )
+        if (
+          maritalStatus !==
+          (MaritalStatus.Unmarried ||
+            MaritalStatus.Divorced ||
+            MaritalStatus.Widowed)
+        ) {
+          return Promise.reject({
+            reason: `Applicant marital status ${maritalStatus} not applicable`,
+          })
+        }
 
-        return Promise.resolve(response.data.nationalRegistryUser)
+        return Promise.resolve({
+          ...response.data.nationalRegistryUser,
+          maritalStatus,
+        })
       })
       .catch(() => {
         return Promise.reject({})
@@ -50,5 +67,32 @@ export class NationalRegistryMaritalStatusProvider extends BasicDataProvider {
 
   onProvideSuccess(result: object): SuccessfulDataProviderResult {
     return { date: new Date(), status: 'success', data: result }
+  }
+
+  private formatMaritalStatus(maritalCode: string): MaritalStatus {
+    switch (maritalCode) {
+      case '1':
+        return MaritalStatus.Unmarried
+      case '3':
+        return MaritalStatus.Married
+      case '4':
+        return MaritalStatus.Widowed
+      case '5':
+        return MaritalStatus.Separated
+      case '6':
+        return MaritalStatus.Divorced
+      case '7':
+        return MaritalStatus.MarriedLivingSeparately
+      case '8':
+        return MaritalStatus.MarriedToForeignLawPerson
+      case '9':
+        return MaritalStatus.Unknown
+      case '0':
+        return MaritalStatus.ForeignResidenceMarriedToUnregisteredPerson
+      case 'L':
+        return MaritalStatus.IcelandicResidenceMarriedToUnregisteredPerson
+      default:
+        return MaritalStatus.Unmarried
+    }
   }
 }
