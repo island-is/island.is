c95947a17a6bbcaf8f18f3caa4fdc70a99a977e2
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-08-23 21:25:13 +0000

    Add all input fields to caseFiles in indictments

---
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFiles/CaseFiles.strings.ts
@@ -0,0 +1,51 @@
+import { defineMessage } from 'react-intl'
+
+export const caseFiles = {
+  heading: defineMessage({
+    id: 'judicial.system.indictments:case_files.heading',
+    defaultMessage: 'Dómskjöl',
+    description: 'Notaður sem titill á dómskjöl skrefi í ákærum.',
+  }),
+  sections: {
+    inputFieldLabel: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.input_field_label',
+      defaultMessage: 'Dragðu gögn hingað til að hlaða upp',
+      description: 'Texti í öllum file upload svæðum.',
+    }),
+    buttonLabel: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.button_label',
+      defaultMessage: 'Velja gögn til að hlaða upp',
+      description: 'Texti í tökkum í öllum file upload svæðum.',
+    }),
+    indictmentLetter: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.indictment_letter',
+      defaultMessage: 'Fylgibréf',
+      description: 'Titill á fylgibréf hluta á dómskjalaskjá í ákærum.',
+    }),
+    indictment: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.indictment',
+      defaultMessage: 'Ákæra',
+      description: 'Titill á ákæra hluta á dómskjalaskjá í ákærum.',
+    }),
+    criminalRecord: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.criminal_record',
+      defaultMessage: 'Sakavottorð',
+      description: 'Titill á sakavottorð hluta á dómskjalaskjá í ákærum.',
+    }),
+    bill: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.bill',
+      defaultMessage: 'Sakarkostnaður',
+      description: 'Titill á sakarkostnaður hluta á dómskjalaskjá í ákærum.',
+    }),
+    caseFileRecord: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.case_file_record',
+      defaultMessage: 'Skjalaskrá',
+      description: 'Titill á skjalaskrá hluta á dómskjalaskjá í ákærum.',
+    }),
+    caseFiles: defineMessage({
+      id: 'judicial.system.indictments:case_files.sections.case_files',
+      defaultMessage: 'Málsgögn',
+      description: 'Titill á málsgögn hluta á dómskjalaskjá í ákærum.',
+    }),
+  },
+}
--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFiles/CaseFiles.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFiles/CaseFiles.tsx
@@ -1,7 +1,155 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useIntl } from 'react-intl'
+
+import {
+  FormContentContainer,
+  FormFooter,
+  PageLayout,
+} from '@island.is/judicial-system-web/src/components'
+import { FormContext } from '@island.is/judicial-system-web/src/components/FormProvider/FormProvider'
+import {
+  IndictmentsProsecutorSubsections,
+  Sections,
+} from '@island.is/judicial-system-web/src/types'
+import PageHeader from '@island.is/judicial-system-web/src/components/PageHeader/PageHeader'
+import { titles } from '@island.is/judicial-system-web/messages'
+import { Box, InputFileUpload, Text } from '@island.is/island-ui/core'
+import * as constants from '@island.is/judicial-system/consts'
+
+import * as strings from './CaseFiles.strings'
 
 const CaseFiles: React.FC = () => {
-  return <div>CaseFiles</div>
+  const { workingCase, isLoadingWorkingCase, caseNotFound } = useContext(
+    FormContext,
+  )
+  const { formatMessage } = useIntl()
+
+  return (
+    <PageLayout
+      workingCase={workingCase}
+      activeSection={Sections.PROSECUTOR}
+      activeSubSection={IndictmentsProsecutorSubsections.CASE_FILES}
+      isLoading={isLoadingWorkingCase}
+      notFound={caseNotFound}
+    >
+      <PageHeader
+        title={formatMessage(titles.prosecutor.indictments.caseFiles)}
+      />
+      <FormContentContainer>
+        <Box marginBottom={7}>
+          <Text as="h1" variant="h1">
+            {formatMessage(strings.caseFiles.heading)}
+          </Text>
+        </Box>
+        <Box component="section" marginBottom={5}>
+          <Box marginBottom={3} display="flex">
+            <Text variant="h3" as="h3">
+              {formatMessage(strings.caseFiles.sections.indictmentLetter)}
+            </Text>
+            <Text color="red400" as="span" variant="h3">
+              *
+            </Text>
+          </Box>
+          <InputFileUpload
+            fileList={[]}
+            header={formatMessage(strings.caseFiles.sections.inputFieldLabel)}
+            buttonLabel={formatMessage(strings.caseFiles.sections.buttonLabel)}
+            multiple={false}
+            onRemove={() => ''}
+          />
+        </Box>
+        <Box component="section" marginBottom={5}>
+          <Box marginBottom={3} display="flex">
+            <Text variant="h3" as="h3">
+              {formatMessage(strings.caseFiles.sections.indictment)}
+            </Text>
+            <Text color="red400" as="span" variant="h3">
+              *
+            </Text>
+          </Box>
+          <InputFileUpload
+            fileList={[]}
+            header={formatMessage(strings.caseFiles.sections.inputFieldLabel)}
+            buttonLabel={formatMessage(strings.caseFiles.sections.buttonLabel)}
+            multiple={false}
+            onRemove={() => ''}
+          />
+        </Box>
+        <Box component="section" marginBottom={5}>
+          <Box marginBottom={3} display="flex">
+            <Text variant="h3" as="h3">
+              {formatMessage(strings.caseFiles.sections.criminalRecord)}
+            </Text>
+            <Text color="red400" as="span" variant="h3">
+              *
+            </Text>
+          </Box>
+          <InputFileUpload
+            fileList={[]}
+            header={formatMessage(strings.caseFiles.sections.inputFieldLabel)}
+            buttonLabel={formatMessage(strings.caseFiles.sections.buttonLabel)}
+            onRemove={() => ''}
+          />
+        </Box>
+        <Box component="section" marginBottom={5}>
+          <Box marginBottom={3} display="flex">
+            <Text variant="h3" as="h3">
+              {formatMessage(strings.caseFiles.sections.bill)}
+            </Text>
+            <Text color="red400" as="span" variant="h3">
+              *
+            </Text>
+          </Box>
+          <InputFileUpload
+            fileList={[]}
+            header={formatMessage(strings.caseFiles.sections.inputFieldLabel)}
+            buttonLabel={formatMessage(strings.caseFiles.sections.buttonLabel)}
+            onRemove={() => ''}
+          />
+        </Box>
+        <Box component="section" marginBottom={5}>
+          <Box marginBottom={3} display="flex">
+            <Text variant="h3" as="h3">
+              {formatMessage(strings.caseFiles.sections.caseFileRecord)}
+            </Text>
+            <Text color="red400" as="span" variant="h3">
+              *
+            </Text>
+          </Box>
+          <InputFileUpload
+            fileList={[]}
+            header={formatMessage(strings.caseFiles.sections.inputFieldLabel)}
+            buttonLabel={formatMessage(strings.caseFiles.sections.buttonLabel)}
+            onRemove={() => ''}
+          />
+        </Box>
+        <Box component="section" marginBottom={10}>
+          <Box marginBottom={3} display="flex">
+            <Text variant="h3" as="h3">
+              {formatMessage(strings.caseFiles.sections.caseFiles)}
+            </Text>
+            <Text color="red400" as="span" variant="h3">
+              *
+            </Text>
+          </Box>
+          <InputFileUpload
+            fileList={[]}
+            header={formatMessage(strings.caseFiles.sections.inputFieldLabel)}
+            buttonLabel={formatMessage(strings.caseFiles.sections.buttonLabel)}
+            onRemove={() => ''}
+          />
+        </Box>
+      </FormContentContainer>
+      <FormContentContainer isFooter>
+        <FormFooter
+          previousUrl={`${constants.INDICTMENTS_PROCESSING_ROUTE}/${workingCase.id}`}
+          nextUrl={`${constants.INDICTMENTS_OVERVIEW_ROUTE}/${workingCase.id}`}
+          // nextIsDisabled={!allFilesUploaded || isUploading}
+          nextIsLoading={isLoadingWorkingCase}
+        />
+      </FormContentContainer>
+    </PageLayout>
+  )
 }
 
 export default CaseFiles
