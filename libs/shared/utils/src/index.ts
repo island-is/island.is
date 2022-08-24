ef5646ac533c935823f7831f58f884a0649e8526
Petar Shomov <petar@andes.is>
2022-08-24 14:52:25 +0000

    chore: Removing a dangerous method that is not used and warning about another (#8229)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -4,7 +4,7 @@
  */
 export { getStaticEnv } from './lib/getStaticEnv'
 export { sleep } from './lib/sleep'
-export { isRunningOnEnvironment, getActiveEnvironment } from './lib/environment'
+export { isRunningOnEnvironment } from './lib/environment'
 export { shouldLinkOpenInNewWindow } from './lib/shouldLinkOpenInNewWindow'
 export { getOrganizationLogoUrl } from './lib/getOrganizationLogoUrl'
 export { checkDelegation } from './lib/isDelegation'
