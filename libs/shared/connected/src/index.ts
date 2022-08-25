a8859ab49356950d5ce993698b1bbad07c8767f3
runarvestmann <runar@stefna.is>
2022-08-25 16:10:52 +0000

    Move from slice to a screen

--- libs/shared/connected/src/index.ts
+++ libs/shared/connected/src/index.ts
@@ -1,4 +1,3 @@
 export { CompanyList } from './lib/skilavottord/CompanyList/CompanyList'
 export { CompanyListConnected } from './lib/skilavottord/CompanyList/CompanyListConnected'
 export { GeneralPetitionLists } from './lib/generalPetition/GeneralPetitionLists/GeneralPetitionLists'
-export { Calculator } from './lib/fiskistofaCalculator'
