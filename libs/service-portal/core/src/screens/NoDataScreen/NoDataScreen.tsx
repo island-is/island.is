1013c8b19b9d7b564df3e4206d537e07ba66305d
disaerna <disa.erna@gmail.com>
2022-08-25 10:11:45 +0000

    feat: new empty screen for accesses

--- libs/service-portal/core/src/screens/NoDataScreen/NoDataScreen.tsx
+++ libs/service-portal/core/src/screens/NoDataScreen/NoDataScreen.tsx
@@ -7,22 +7,28 @@ import {
   TagVariant,
   Text,
   Button,
+  IconProps,
 } from '@island.is/island-ui/core'
 import * as styles from '../ErrorScreen/ErrorScreen.css'
 import { Link } from 'react-router-dom'
 import { ServicePortalPath } from '../../lib/navigation/paths'
 
+interface ButtonType {
+  link?: ServicePortalPath | string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick?: any
+  icon?: Pick<IconProps, 'icon' | 'type'>
+  type: 'internal' | 'external' | 'click'
+  text: string
+  variant: 'primary' | 'ghost'
+}
 interface Props {
   title: string
   children?: React.ReactNode
   tag?: string
   tagVariant?: TagVariant
-  button?: {
-    link: ServicePortalPath | string
-    internal: boolean
-    text: string
-    variant: 'primary' | 'ghost'
-  }
+  button?: ButtonType
+  secondaryButton?: ButtonType
   figure?: string
 }
 
@@ -30,75 +36,111 @@ export const NoDataScreen: FC<Props> = ({
   title,
   children,
   button,
+  secondaryButton,
   tag,
   figure,
   tagVariant = 'purple',
 }) => {
-  return (
-    <GridRow>
-      <GridColumn span={['1/1', '10/12']} offset={['0', '0']}>
-        <Box
-          marginTop={6}
-          marginBottom={6}
-          textAlign="center"
-          justifyContent="center"
-        >
-          {tag && (
-            <Box marginBottom={4}>
-              <Tag variant={tagVariant}>{tag}</Tag>
-            </Box>
-          )}
-          <Text variant="h1" as="h1" marginBottom={3}>
-            {title}
-          </Text>
-          <Text variant="default" as="div">
-            {children}
-          </Text>
-          {button && (
-            <Box
-              marginTop={3}
-              marginBottom={3}
-              display="flex"
-              textAlign="center"
-              justifyContent="center"
+  const renderButton = (button: ButtonType) => {
+    return (
+      <>
+        {button.type === 'internal' && button.link && (
+          <Link to={button.link}>
+            <Button
+              variant={button.variant}
+              size="small"
+              icon={button.icon ? button.icon.icon : undefined}
+              iconType={button.icon ? button.icon.type : undefined}
             >
-              {button.internal ? (
-                <Link to={button.link}>
-                  <Button
-                    variant={button.variant}
-                    size="small"
-                    icon="receipt"
-                    iconType="outline"
-                  >
-                    {button.text}
-                  </Button>
-                </Link>
-              ) : (
-                <a href={button.link}>
-                  <Button
-                    variant={button.variant}
-                    size="small"
-                    icon="receipt"
-                    iconType="outline"
-                  >
-                    {button?.text}
-                  </Button>
-                </a>
+              {button.text}
+            </Button>
+          </Link>
+        )}
+        {button.type === 'external' && button.link && (
+          <a href={button.link}>
+            <Button
+              variant={button.variant}
+              size="small"
+              icon={button.icon ? button.icon.icon : undefined}
+              iconType={button.icon ? button.icon.type : undefined}
+            >
+              {button?.text}
+            </Button>
+          </a>
+        )}
+        {button.type === 'click' && button.onClick && (
+          <Button
+            variant={button.variant}
+            size="small"
+            icon={button.icon ? button.icon.icon : undefined}
+            iconType={button.icon ? button.icon.type : undefined}
+            onClick={button.onClick}
+          >
+            {button?.text}
+          </Button>
+        )}
+      </>
+    )
+  }
+  return (
+    <>
+      <GridRow>
+        <GridColumn span={['1/1', '6/12']} offset={['0', '3/12']}>
+          <Box
+            marginTop={[3, 6]}
+            marginBottom={6}
+            textAlign="center"
+            justifyContent="center"
+          >
+            {tag && (
+              <Box marginBottom={4}>
+                <Tag variant={tagVariant}>{tag}</Tag>
+              </Box>
+            )}
+            <Text variant="h1" as="h1" marginBottom={3}>
+              {title}
+            </Text>
+            <Text variant="default" as="div">
+              {children}
+            </Text>
+            <Box
+              display="flex"
+              flexDirection={['column', 'row']}
+              justifyContent="center"
+              alignItems="center"
+              marginTop={4}
+            >
+              {button && (
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  {renderButton(button)}
+                </Box>
+              )}
+              {secondaryButton && (
+                <Box
+                  marginLeft={[0, 2]}
+                  marginTop={[3, 0]}
+                  display="flex"
+                  textAlign="center"
+                  justifyContent="center"
+                >
+                  {renderButton(secondaryButton)}
+                </Box>
               )}
             </Box>
-          )}
-        </Box>
-      </GridColumn>
-
-      <GridColumn span={['1/1', '4/12']} offset={['0', '3/12']}>
-        <Box display="flex" justifyContent="center">
-          <img
-            src={figure ? figure : './assets/images/jobsGrid.svg'}
-            alt=""
-            className={styles.img}
-          />
-        </Box>
-      </GridColumn>
-    </GridRow>
+          </Box>
+        </GridColumn>
+      </GridRow>
+      <GridRow>
+        <GridColumn span={['1/1', '6/12']} offset={['0', '3/12']}>
+          <Box display="flex" justifyContent="center" marginTop={[3, 5]}>
+            <img
+              src={figure ? figure : './assets/images/jobsGrid.svg'}
+              alt=""
+              className={styles.img}
+            />
+          </Box>
+        </GridColumn>
+      </GridRow>
+    </>
   )
 }
