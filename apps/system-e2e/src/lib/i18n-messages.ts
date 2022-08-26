38645778d913010077964864dbd4e760ac7d25ae
Petar Shomov <petar@andes.is>
2022-08-26 14:28:01 +0000

    Update apps/system-e2e/src/lib/i18n-messages.ts
    Co-authored-by: Jón Levy <levy@andes.is>

--- apps/system-e2e/src/lib/i18n-messages.ts
+++ apps/system-e2e/src/lib/i18n-messages.ts
@@ -1,7 +1,7 @@
 //
 // // Create the `intl` object
 import { createIntl, createIntlCache, MessageDescriptor } from '@formatjs/intl'
-
+import { cypressError } from '../support/utils'
 const cache = createIntlCache()
 const intl = createIntl(
   {
