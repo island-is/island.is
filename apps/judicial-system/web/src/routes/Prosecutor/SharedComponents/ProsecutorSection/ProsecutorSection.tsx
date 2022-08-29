0193c9f66de7a671d7c03d4738eba5be031ae9a0
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-08-29 18:05:34 +0000

    Extracts prosecutor section heading component

--- apps/judicial-system/web/src/routes/Prosecutor/SharedComponents/ProsecutorSection/ProsecutorSection.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/SharedComponents/ProsecutorSection/ProsecutorSection.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { useIntl } from 'react-intl'
 
-import { Box, Tooltip } from '@island.is/island-ui/core'
 import { User } from '@island.is/judicial-system/types'
-import { SectionHeading } from '@island.is/judicial-system-web/src/components'
-import { strings } from './ProsecutorSection.strings'
+import ProsecutorSectionHeading from './ProsecutorSectionHeading'
 import ProsecutorSelection from './ProsecutorSelection'
 
 interface Props {
@@ -14,18 +11,9 @@ interface Props {
 const ProsecutorSection: React.FC<Props> = (props) => {
   const { onChange } = props
 
-  const { formatMessage } = useIntl()
-
   return (
     <>
-      <SectionHeading
-        title={`${formatMessage(strings.heading)} `}
-        tooltip={
-          <Box component="span" data-testid="prosecutor-tooltip">
-            <Tooltip text={formatMessage(strings.tooltip)} />
-          </Box>
-        }
-      />
+      <ProsecutorSectionHeading />
       <ProsecutorSelection onChange={onChange} />
     </>
   )
--- apps/judicial-system/web/src/routes/Prosecutor/SharedComponents/ProsecutorSection/ProsecutorSectionHeading.strings.ts
+++ apps/judicial-system/web/src/routes/Prosecutor/SharedComponents/ProsecutorSection/ProsecutorSectionHeading.strings.ts
@@ -1,14 +1,14 @@
 import { defineMessages } from 'react-intl'
 
 export const strings = defineMessages({
-  heading: {
-    id: 'judicial.system.core:select_prosecutor.heading',
+  title: {
+    id: 'judicial.system.core:prosecutor_section_heading.title',
     defaultMessage: 'Ákærandi',
     description:
       'Notaður sem titill fyrir "ákærandi" hlutann á óskir um fyrirtöku skrefi í öllum málstegundum.',
   },
   tooltip: {
-    id: 'judicial.system.core:select_prosecutor.tooltip',
+    id: 'judicial.system.core:prosecutor_section_heading.tooltip',
     defaultMessage:
       'Sá ákærandi sem valinn er hér er skráður fyrir kröfunni í öllum upplýsingaskeytum og skjölum sem tengjast kröfunni, og flytur málið fyrir dómstólum fyrir hönd síns embættis.',
     description:
