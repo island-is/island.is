037d22f48e3b50f93430decce4a34df1dbec7402
Arnar Kári <arnar.kari@kolibri.is>
2022-08-30 12:57:57 +0000

    feat(j-s): Rewrite CaseInfo component to handle multiple police nr

---
+++ apps/judicial-system/web/src/components/CaseInfo/CaseInfo.spec.tsx
@@ -0,0 +1,56 @@
+import { createIntl } from 'react-intl'
+
+import { CaseType, Defendant, Gender } from '@island.is/judicial-system/types'
+
+import { getDefentantLabel } from './CaseInfo'
+
+const formatMessage = createIntl({ locale: 'is', onError: jest.fn })
+  .formatMessage
+
+describe('getDefendantLabel - Indictment', () => {
+  const fn = (defendants: Defendant[]) =>
+    getDefentantLabel(formatMessage, defendants, CaseType.THEFT)
+
+  test('should render label for female', () => {
+    const defendants = [{ gender: Gender.FEMALE }] as Defendant[]
+    expect(fn(defendants)).toBe('ákærða')
+  })
+
+  test('should render label for male', () => {
+    const defendants = [{ gender: Gender.MALE }] as Defendant[]
+    expect(fn(defendants)).toBe('ákærði')
+  })
+
+  test('should render label for other', () => {
+    const defendants = [{ gender: Gender.OTHER }] as Defendant[]
+    expect(fn(defendants)).toBe('ákærða')
+  })
+
+  test('should render label for missing gender', () => {
+    const defendants = [{}] as Defendant[]
+    expect(fn(defendants)).toBe('ákærða')
+  })
+
+  test('should render label for multiple defendants', () => {
+    const defendants = [{}, {}] as Defendant[]
+    expect(fn(defendants)).toBe('ákærðu')
+  })
+})
+
+describe('getDefendantLabel - RestrictionCase/InvestigationCase', () => {
+  const formatMessage = createIntl({ locale: 'is', onError: jest.fn })
+    .formatMessage
+
+  const fn = (defendants: Defendant[]) =>
+    getDefentantLabel(formatMessage, defendants, CaseType.CUSTODY)
+
+  test('should render label for signle defentant', () => {
+    const defendants = [{}] as Defendant[]
+    expect(fn(defendants)).toBe('varnaraðali')
+  })
+
+  test('should render label for multiple defendants', () => {
+    const defendants = [{}, {}] as Defendant[]
+    expect(fn(defendants)).toBe('varnaraðilar')
+  })
+})
