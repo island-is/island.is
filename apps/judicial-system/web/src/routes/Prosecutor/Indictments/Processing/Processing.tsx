fa7b3e4f2598aa4c45145e627b72a63f694f7995
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-08-30 13:41:14 +0000

    fix(j-s): Section Component (#8269)
    * Renames header strings
    
    * Removes redundant code
    
    * Moves select prosecutor strings to the component's folder
    
    * Adds indictment strings to Contentful load
    
    * Adds an optional tooltip to section heading
    
    * Uses section heading in select prosecutor
    
    * Removes unused import
    
    * Moves users query to select prosecutor component
    
    * Moves working case to select prosecutor component
    
    * Renames component
    
    * Extracts prosecutor selection component
    
    * Extracts prosecutor section heading component
    
    * Creates prosecutor section heightened security component
    
    * Moves prosecutor change handling to component
    
    * Moves heightened security checkbox into component
    
    * Fixes string references
    
    * Moves access modal to component
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/Processing/Processing.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Processing/Processing.tsx
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { useIntl } from 'react-intl'
-import { useQuery } from '@apollo/client'
 
 import {
   FormContentContainer,
@@ -17,9 +16,8 @@ import {
   titles,
   processing as m,
 } from '@island.is/judicial-system-web/messages'
-import { Box, Tag, Text } from '@island.is/island-ui/core'
-import { UsersQuery } from '@island.is/judicial-system-web/src/utils/mutations'
-import { Institution, User, UserRole } from '@island.is/judicial-system/types'
+import { Box, Text } from '@island.is/island-ui/core'
+import { Institution } from '@island.is/judicial-system/types'
 import {
   useCase,
   useInstitution,
@@ -28,7 +26,7 @@ import CommentsInput from '@island.is/judicial-system-web/src/components/Comment
 import { isProcessingStepValidIndictments } from '@island.is/judicial-system-web/src/utils/validate'
 import * as constants from '@island.is/judicial-system/consts'
 
-import SelectProsecutor from '../../SharedComponents/SelectProsecutor/SelectProsecutor'
+import ProsecutorSection from '../../SharedComponents/ProsecutorSection/ProsecutorSection'
 import SelectCourt from '../../SharedComponents/SelectCourt/SelectCourt'
 import IndictmentCaseIntro from '../../SharedComponents/IndictmentCaseIntro/IndictmentCaseIntro'
 
@@ -42,51 +40,6 @@ const Processing: React.FC = () => {
   const { setAndSendToServer } = useCase()
   const { formatMessage } = useIntl()
   const { courts } = useInstitution()
-  const { data: userData } = useQuery<{ users: User[] }>(UsersQuery, {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'all',
-  })
-
-  const [prosecutors, setProsecutors] = useState<User[]>()
-
-  useEffect(() => {
-    if (userData?.users && workingCase) {
-      setProsecutors(
-        userData.users.filter(
-          (aUser: User) =>
-            aUser.role === UserRole.PROSECUTOR &&
-            (!workingCase.creatingProsecutor ||
-              aUser.institution?.id ===
-                workingCase.creatingProsecutor?.institution?.id),
-        ),
-      )
-    }
-  }, [userData, workingCase, workingCase?.creatingProsecutor?.institution?.id])
-
-  const setProsecutor = async (prosecutor: User) => {
-    if (workingCase) {
-      return setAndSendToServer(
-        [
-          {
-            prosecutorId: prosecutor.id,
-            force: true,
-          },
-        ],
-        workingCase,
-        setWorkingCase,
-      )
-    }
-  }
-
-  const handleProsecutorChange = (prosecutor: User) => {
-    if (!workingCase) {
-      return false
-    }
-
-    setProsecutor(prosecutor)
-
-    return true
-  }
 
   const handleCourtChange = (court: Institution) => {
     if (workingCase) {
@@ -125,13 +78,7 @@ const Processing: React.FC = () => {
           </Text>
         </Box>
         <IndictmentCaseIntro workingCase={workingCase} />
-        <Box component="section" marginBottom={5}>
-          <SelectProsecutor
-            workingCase={workingCase}
-            prosecutors={prosecutors || []}
-            onChange={handleProsecutorChange}
-          />
-        </Box>
+        <ProsecutorSection />
         <Box component="section" marginBottom={5}>
           <SelectCourt
             workingCase={workingCase}
