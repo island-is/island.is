9eb3e386fa05578807efe43bc4b9c227415efba9
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-08-31 14:37:45 +0200

    feat: assign employer sms temaplate

---
+++ libs/application/template-api-modules/src/lib/modules/templates/parental-leave/smsGenerators/assignEmployerSms.ts
@@ -0,0 +1,19 @@
+import { getApplicationAnswers, getApplicationExternalData } from '@island.is/application/templates/parental-leave'
+import { AssignSmsTemplateGenerator } from '../../../../types'
+
+export const generateAssignEmployerApplicationSms: AssignSmsTemplateGenerator = (
+  application,
+  assignLink,
+) => {
+  const { employerPhoneNumber } = getApplicationAnswers(application.answers)
+  const { applicantName } = getApplicationExternalData(
+    application.externalData,
+  )
+  const applicantId = application.applicant
+
+  return {
+    phoneNumber: employerPhoneNumber,
+    message: `Umsækjandi ${applicantName} kt: ${applicantId} hefur skráð þig sem atvinnuveitanda í umsókn sinni um fæðingarorlof.
+    Ef þú áttir von á þessari beiðni máttu smella á linkinn hér fyrir neðan. Kveðja, Fæðingarorlofssjóður ${assignLink}`,
+  }
+}
