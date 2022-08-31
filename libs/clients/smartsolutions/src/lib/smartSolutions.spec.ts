a07b21cff75c403d858b3cf7c73cbf6425677490
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-08-31 11:25:21 +0000

    fix: update test

--- libs/clients/smartsolutions/src/lib/smartSolutions.spec.ts
+++ libs/clients/smartsolutions/src/lib/smartSolutions.spec.ts
@@ -1,23 +1,35 @@
-import { logger } from '@island.is/logging'
+import { logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { ConfigModule } from '@nestjs/config'
+import { Test } from '@nestjs/testing'
 import { SmartSolutionsApi } from './smartSolutions.api'
+import { SmartSolutionsClientConfig } from './smartsolutionsApi.config'
 
 describe('SmartSolutionsApi', () => {
   let service: SmartSolutionsApi
 
   beforeEach(async () => {
-    service = new SmartSolutionsApi(logger)
+    const module = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [SmartSolutionsClientConfig],
+        }),
+      ],
+      providers: [
+        {
+          provide: LOGGER_PROVIDER,
+          useValue: logger,
+        },
+        SmartSolutionsApi,
+      ],
+    }).compile()
+
+    service = module.get(SmartSolutionsApi)
   })
 
   describe('Module', () => {
     it('should be defined', () => {
       expect(service).toBeTruthy()
-    })
-  })
-
-  describe('listTemplates', () => {
-    it('should return a valid response', async () => {
-      const res = await service.listTemplates()
-      expect(res).not.toBeNull()
     })
   })
 })
