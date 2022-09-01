61da165ce9403fcfe566c8a37ff6c594e1f37a61
disaerna <disa.erna@gmail.com>
2022-09-01 14:42:43 +0000

    fix: test

--- apps/service-portal/src/screens/Dashboard/Dashboard.tsx
+++ apps/service-portal/src/screens/Dashboard/Dashboard.tsx
@@ -140,7 +140,7 @@ export const Dashboard: FC<{}> = () => {
             />
         </Box>
           }
-        <GridRow>
+        <GridRow data-testid={'service-portal-dashboard'}>
           {navigation.map((rootItem, rootIndex) => {
             return rootItem.children?.map(
               (navRoot, index) =>
