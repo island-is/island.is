98a0c746bf55df377893fbadaf7a750a6d97d566
Þórður Hafliðason <tolleinn@gmail.com>
2022-09-01 15:29:44 +0000

    Islyklar pfx reading

--- libs/clients/islykill/src/lib/islykill.module.ts
+++ libs/clients/islykill/src/lib/islykill.module.ts
@@ -13,9 +13,7 @@ export interface IslykillApiModuleConfig {
 }
 
 export class IslykillApiModule {
-  static async register(
-    config: IslykillApiModuleConfig,
-  ): Promise<DynamicModule> {
+  static register(config: IslykillApiModuleConfig): DynamicModule {
     function lykillError(errorMsg: any) {
       logger.error(errorMsg)
     }
@@ -25,11 +23,10 @@ export class IslykillApiModule {
       if (!config.cert) {
         throw Error('IslykillApiModule certificate not provided')
       }
-      const data = await fs.promises.readFile(config.cert, {
-        encoding: 'base64',
-      })
+      const data = fs.readFileSync(config.cert)
+      const data64 = data.toString('base64')
 
-      pfx = Buffer.from(data, 'base64')
+      pfx = Buffer.from(data64, 'base64')
     } catch (err) {
       lykillError(err)
     }
