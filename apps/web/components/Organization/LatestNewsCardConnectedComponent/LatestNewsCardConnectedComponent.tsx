052839054cca8a8c2127f0b86af189f16293aaae
runarvestmann <runar@stefna.is>
2022-09-02 10:50:46 +0000

    Simplified code

--- apps/web/components/Organization/LatestNewsCardConnectedComponent/LatestNewsCardConnectedComponent.tsx
+++ apps/web/components/Organization/LatestNewsCardConnectedComponent/LatestNewsCardConnectedComponent.tsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useQuery } from '@apollo/client'
 import { Box, ProfileCard, Text } from '@island.is/island-ui/core'
-import initApollo from '@island.is/web/graphql/client'
 import {
-  GetNewsQuery,
+  GetNewsWithContentQuery,
+  GetNewsWithContentQueryVariables,
   GetSingleNewsItemQuery,
-  QueryGetNewsArgs,
-  QueryGetSingleNewsArgs,
 } from '@island.is/web/graphql/schema'
 import { linkResolver } from '@island.is/web/hooks'
 import { useI18n } from '@island.is/web/i18n'
-import {
-  GET_NEWS_QUERY,
-  GET_SINGLE_NEWS_ITEM_QUERY,
-} from '@island.is/web/screens/queries'
+import { GET_NEWS_WITH_CONTENT_QUERY } from '@island.is/web/screens/queries'
 import * as styles from './LatestNewsCardConnectedComponent.css'
 
 const extractHeadingsFromContent = (
@@ -26,19 +21,20 @@ const extractHeadingsFromContent = (
   for (const slice of content) {
     if (slice.__typename !== 'Html') continue
 
-    if (!haveSeenText && slice.document?.content?.[0]?.content?.[0]?.value) {
-      headings.push(slice.document.content[0].content[0].value)
-    } else if (
-      haveSeenText &&
-      slice.document?.content?.[0]?.content?.[0]?.value &&
-      slice.document?.content?.[0]?.nodeType === 'heading-3'
-    ) {
-      headings.push(slice.document.content[0].content[0].value)
-    }
+    const value = slice.document?.content?.[0]?.content?.[0]?.value
+    const nodeType = slice.document?.content?.[0]?.nodeType
 
-    haveSeenText = true
+    if (!haveSeenText || nodeType === 'heading-3') {
+      if (value) {
+        headings.push(value)
+        haveSeenText = true
+      }
+    }
   }
 
+  // Skip the last one since that's not a heading we'd like to show
+  // See here: https://island.is/s/stafraent-island/frett/frettabref-juli-2022
+  // The last heading is 'Meðal verkefna Stafræns Íslands þessa dagana eru:'
   headings.pop()
 
   return headings
@@ -49,6 +45,7 @@ interface LatestNewsCardConnectedComponentProps {
   seeMoreText?: string
   tags?: string[]
   imageUrl?: string
+  shouldExtractHeadingsFromContent?: boolean
 }
 
 export const LatestNewsCardConnectedComponent = ({
@@ -56,68 +53,24 @@ export const LatestNewsCardConnectedComponent = ({
   seeMoreText = 'Skoða nánar',
   tags = [],
   imageUrl,
+  shouldExtractHeadingsFromContent,
 }: LatestNewsCardConnectedComponentProps) => {
   const { activeLocale } = useI18n()
 
-  const [card, setCard] = useState<
-    GetSingleNewsItemQuery['getSingleNews'] | null
-  >(null)
+  const response = useQuery<
+    GetNewsWithContentQuery,
+    GetNewsWithContentQueryVariables
+  >(GET_NEWS_WITH_CONTENT_QUERY, {
+    variables: {
+      input: {
+        lang: activeLocale,
+        size: 1,
+        tags,
+      },
+    },
+  })
 
-  useEffect(() => {
-    let isMounted = true
-
-    const apolloClient = initApollo({}, activeLocale)
-
-    const fetchLatestNewsItem = async () => {
-      const getNewsResponse = await apolloClient.query<
-        GetNewsQuery,
-        QueryGetNewsArgs
-      >({
-        query: GET_NEWS_QUERY,
-        variables: {
-          input: {
-            lang: activeLocale,
-            size: 1,
-            tags,
-          },
-        },
-      })
-
-      const newsItemDto = getNewsResponse?.data?.getNews?.items?.[0]
-
-      if (!newsItemDto) {
-        return null
-      }
-
-      const getSingleNewsResponse = await apolloClient.query<
-        GetSingleNewsItemQuery,
-        QueryGetSingleNewsArgs
-      >({
-        query: GET_SINGLE_NEWS_ITEM_QUERY,
-        variables: {
-          input: {
-            slug: newsItemDto.slug,
-            lang: activeLocale,
-          },
-        },
-      })
-
-      return getSingleNewsResponse.data.getSingleNews
-    }
-
-    fetchLatestNewsItem()
-      .then((card) => {
-        if (!isMounted) return
-        setCard(card)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-
-    return () => {
-      isMounted = false
-    }
-  }, [activeLocale])
+  const card = response?.data?.getNews?.items?.[0]
 
   if (!card) return null
 
@@ -125,7 +78,9 @@ export const LatestNewsCardConnectedComponent = ({
     ? linkResolver('organizationnews', [organizationSlug, card.slug]).href
     : linkResolver('news', [card.slug]).href
 
-  const headings = extractHeadingsFromContent(card.content)
+  const headings = shouldExtractHeadingsFromContent
+    ? extractHeadingsFromContent(card.content)
+    : []
 
   return (
     <ProfileCard
