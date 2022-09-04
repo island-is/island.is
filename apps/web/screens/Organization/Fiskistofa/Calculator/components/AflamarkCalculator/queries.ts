8c1decdf6f0f6e32a1e46272ed514cb8472beb40
runarvestmann <runar@stefna.is>
2022-09-04 08:32:14 +0000

    Add skeleton for two components and added an endpoint

--- apps/web/screens/Organization/Fiskistofa/Calculator/components/AflamarkCalculator/queries.ts
+++ apps/web/screens/Organization/Fiskistofa/Calculator/components/AflamarkCalculator/queries.ts
@@ -32,3 +32,38 @@ export const GET_SHIP_STATUS_INFORMATION = gql`
     }
   }
 `
+
+export const GET_UPDATED_SHIP_STATUS_INFORMATION = gql`
+  query GetUpdatedShipStatusInformation(
+    $input: GetShipStatusInformationInput!
+  ) {
+    getUpdatedShipStatusInformation(input: $input) {
+      shipInformation {
+        id
+        shipNumber
+        name
+        timePeriod
+      }
+      allowedCatchCategories {
+        id
+        name
+        allocation
+        specialAlloction
+        betweenYears
+        betweenShips
+        allowedCatch
+        catch
+        status
+        displacement
+        newStatus
+        nextYear
+        excessCatch
+        unused
+        totalAllowedCatch
+        rateOfShare
+        nextYearQuota
+        nextYearFromQuota
+      }
+    }
+  }
+`
