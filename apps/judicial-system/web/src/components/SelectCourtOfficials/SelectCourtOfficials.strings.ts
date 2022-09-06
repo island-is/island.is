2a4b764fb33001c69432b81aa25395f23b80d173
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-09-06 15:04:26 +0000

    Put strings in CF

--- apps/judicial-system/web/src/components/SelectCourtOfficials/SelectCourtOfficials.strings.ts
+++ apps/judicial-system/web/src/components/SelectCourtOfficials/SelectCourtOfficials.strings.ts
@@ -3,32 +3,60 @@ import { defineMessages } from 'react-intl'
 export const selectCourtOfficials = defineMessages({
   setJudgeTitle: {
     id:
-      'judicial.system.restriction_cases:reception_and_assignment.set_judge_title',
+      'judicial.system.restriction_cases:select_court_officials.set_judge_title',
     defaultMessage: 'Dómari',
     description:
-      'Notaður sem titill fyrir "dómari" hlutann á Móttaka og úthlutun skrefi í gæsluvarðhalds- og farbannsmálum.',
+      'Notaður sem titill fyrir "dómari" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
+  },
+  setJudgeLabel: {
+    id:
+      'judicial.system.restriction_cases:select_court_officials.set_judge_label',
+    defaultMessage: 'Veldu dómara',
+    description:
+      'Notaður sem titill á innsláttarsvæði fyrir "Veldu dómara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
+  },
+  setJudgePlaceholder: {
+    id:
+      'judicial.system.restriction_cases:select_court_officials.set_judge_placeholder',
+    defaultMessage: 'Veldu héraðsdómara',
+    description:
+      'Notaður sem upplýsingatexti á innsláttarsvæði fyrir "Veldu dómara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setJudgeTooltip: {
     id:
-      'judicial.system.restriction_cases:reception_and_assignment.set_judge_tooltip',
+      'judicial.system.restriction_cases:select_court_officials.set_judge_tooltip',
     defaultMessage:
       'Dómarinn sem er valinn hér verður skráður á málið og mun fá tilkynningar sendar í tölvupóst. Eingöngu skráður dómari getur svo undirritað úrskurð.',
     description:
-      'Notaður sem upplýsingatexti í upplýsingasvæði við "dómari" titlinn á Móttaka og úthlutun skrefi í gæsluvarðhalds- og farbannsmálum.',
+      'Notaður sem upplýsingatexti í upplýsingasvæði við "dómari" titlinn á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setRegistrarTitle: {
     id:
-      'judicial.system.restriction_cases:reception_and_assignment.set_registrar_title',
+      'judicial.system.restriction_cases:select_court_officials.set_registrar_title',
     defaultMessage: 'Dómritari',
     description:
-      'Notaður sem titill fyrir "Dómritari" hlutann á Móttaka og úthlutun skrefi í gæsluvarðhalds- og farbannsmálum.',
+      'Notaður sem titill fyrir "Dómritari" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
+  },
+  setRegistrarLabel: {
+    id:
+      'judicial.system.restriction_cases:select_court_officials.set_registrar_label',
+    defaultMessage: 'Veldu dómritara',
+    description:
+      'Notaður sem titill á innsláttarsvæði fyrir "Veldu dómritara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
+  },
+  setRegistrarPlaceholder: {
+    id:
+      'judicial.system.restriction_cases:select_court_officials.set_registrar_placeholder',
+    defaultMessage: 'Veldu dómritara',
+    description:
+      'Notaður sem upplýsingatexti á innsláttarsvæði fyrir "Veldu dómritara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setRegistrarTooltip: {
     id:
-      'judicial.system.restriction_cases:reception_and_assignment.set_registrar_tooltip',
+      'judicial.system.restriction_cases:select_court_officials.set_registrar_tooltip',
     defaultMessage:
       'Dómritari sem er valinn hér verður skráður á málið og mun fá tilkynningar sendar í tölvupósti.',
     description:
-      'Notaður sem upplýsingatexti í upplýsingasvæði við "dómritari" titlinn á Móttaka og úthlutun skrefi í gæsluvarðhalds- og farbannsmálum.',
+      'Notaður sem upplýsingatexti í upplýsingasvæði við "dómritari" titlinn á Móttaka og úthlutun skrefi öllum málategundum',
   },
 })
