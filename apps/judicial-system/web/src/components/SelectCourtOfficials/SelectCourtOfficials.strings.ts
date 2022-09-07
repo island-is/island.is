d26d756996458e1d7a8b2152e71becc8ffb1a07b
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-09-07 11:22:48 +0000

    Fix CF namespaces

--- apps/judicial-system/web/src/components/SelectCourtOfficials/SelectCourtOfficials.strings.ts
+++ apps/judicial-system/web/src/components/SelectCourtOfficials/SelectCourtOfficials.strings.ts
@@ -2,50 +2,44 @@ import { defineMessages } from 'react-intl'
 
 export const selectCourtOfficials = defineMessages({
   title: {
-    id: 'judicial.system.select_court_officials:title',
+    id: 'judicial.system.core:select_court_officials:title',
     defaultMessage: 'Hver tekur við málinu?',
     description:
       'Notaður sem titill á Hver tekur við málinu? hlutanum á Móttaka og úthlutun skrefi öllum málategundum.',
   },
   setJudgeLabel: {
-    id:
-      'judicial.system.restriction_cases:select_court_officials.set_judge_label',
+    id: 'judicial.system.core:select_court_officials.set_judge_label',
     defaultMessage: 'Veldu dómara/aðstoðarmann',
     description:
       'Notaður sem titill á innsláttarsvæði fyrir "Veldu dómara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setJudgePlaceholder: {
-    id:
-      'judicial.system.restriction_cases:select_court_officials.set_judge_placeholder',
+    id: 'judicial.system.core:select_court_officials.set_judge_placeholder',
     defaultMessage: 'Veldu héraðsdómara',
     description:
       'Notaður sem upplýsingatexti á innsláttarsvæði fyrir "Veldu dómara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setJudgeTooltip: {
-    id:
-      'judicial.system.restriction_cases:select_court_officials.set_judge_tooltip',
+    id: 'judicial.system.core:select_court_officials.set_judge_tooltip',
     defaultMessage:
       'Dómarinn sem er valinn hér verður skráður á málið og mun fá tilkynningar sendar í tölvupóst. Eingöngu skráður dómari getur svo undirritað úrskurð.',
     description:
       'Notaður sem upplýsingatexti í upplýsingasvæði við "dómari" titlinn á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setRegistrarLabel: {
-    id:
-      'judicial.system.restriction_cases:select_court_officials.set_registrar_label',
+    id: 'judicial.system.core:select_court_officials.set_registrar_label',
     defaultMessage: 'Veldu dómritara',
     description:
       'Notaður sem titill á innsláttarsvæði fyrir "Veldu dómritara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setRegistrarPlaceholder: {
-    id:
-      'judicial.system.restriction_cases:select_court_officials.set_registrar_placeholder',
+    id: 'judicial.system.core:select_court_officials.set_registrar_placeholder',
     defaultMessage: 'Veldu dómritara',
     description:
       'Notaður sem upplýsingatexti á innsláttarsvæði fyrir "Veldu dómritara" hlutann á Móttaka og úthlutun skrefi öllum málategundum',
   },
   setRegistrarTooltip: {
-    id:
-      'judicial.system.restriction_cases:select_court_officials.set_registrar_tooltip',
+    id: 'judicial.system.core:select_court_officials.set_registrar_tooltip',
     defaultMessage:
       'Dómritari sem er valinn hér verður skráður á málið og mun fá tilkynningar sendar í tölvupósti.',
     description:
