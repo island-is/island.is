a6165ef2c50d1bf0ccdbfeb3cfb0d72612931d1e
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-09-07 11:24:19 +0000

    Fix CF namespaces

--- apps/judicial-system/web/src/routes/Court/Indictments/ReceptionAndAssignment/ReceptionAndAssignment.strings.ts
+++ apps/judicial-system/web/src/routes/Court/Indictments/ReceptionAndAssignment/ReceptionAndAssignment.strings.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl'
 
 export const indictmentsReceptionAndAssignment = defineMessages({
   title: {
-    id: 'judicial.system.investigation_cases:reception_and_assignment.title',
+    id: 'judicial.system.indictments:reception_and_assignment.title',
     defaultMessage: 'Móttaka og úthlutun',
     description: 'Notaður sem titill á Móttaka og úthlutun skrefi í ákærum.',
   },
