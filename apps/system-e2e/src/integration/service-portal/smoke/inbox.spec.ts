47e41296701d2938c432a91cc0b90b9b8f960ab5
Kristófer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-09-07 18:33:32 +0200

    Passing inbox test

--- apps/system-e2e/src/integration/service-portal/smoke/inbox.spec.ts
+++ apps/system-e2e/src/integration/service-portal/smoke/inbox.spec.ts
@@ -1,5 +1,6 @@
 import { getFakeUser } from '../../../support/utils'
 import fakeUsers from '../../../fixtures/service-portal/users.json'
+import { Timeout } from '../../../lib/types'
 
 describe('Service Portal', () => {
   const testUser = getFakeUser(fakeUsers, 'María Sól ÞÍ Torp')
@@ -8,11 +9,14 @@ describe('Service Portal', () => {
       phoneNumber: testUser.phoneNumber,
       urlPath: '/minarsidur/',
     })
+    cy.visit('/minarsidur')
+    cy.wait(Timeout.short)
   })
 
   it('should have Pósthólf', () => {
-    cy.visit('/minarsidur/')
     cy.contains('Pósthólf')
-    cy.contains('deleteme')
+    cy.get('a[href="/minarsidur/postholf"]').click()
+    cy.contains('Hér getur þú fundið skjöl')
+    // TODO: mock items in inbox and verify they appear in the inbox
   })
 })
