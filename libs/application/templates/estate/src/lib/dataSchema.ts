5ee24704333b04256b78e140b63cbde0db470e6a
kksteini <steini@kosmosogkaos.is>
2022-09-08 15:32:39 +0000

    Dataschema scaffold, messages cleanup

--- libs/application/templates/estate/src/lib/dataSchema.ts
+++ libs/application/templates/estate/src/lib/dataSchema.ts
@@ -1,10 +1,94 @@
 import * as z from 'zod'
+import { m } from './messages'
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+import { customZodError } from './utils/customZodError'
+
+const isValidPhoneNumber = (phoneNumber: string) => {
+  const phone = parsePhoneNumberFromString(phoneNumber, 'IS')
+  return phone && phone.isValid()
+}
+
+const asset = z
+  .object({
+    assetNumber: customZodError(z.string().nonempty(), m.errorNumberEmpty),
+    description: z.string().optional(),
+  })
+  .array()
+  .optional()
 
 export const estateSchema = z.object({
+  approveExternalData: z.boolean().refine((v) => v),
+  applicantPhone: z.string().refine((v) => isValidPhoneNumber(v), {
+    params: m.errorPhoneNumber,
+  }),
+  applicantEmail: customZodError(z.string().email(), m.errorEmail),
+  applicantRelation: customZodError(z.string().nonempty(), m.errorRelation),
+
+  estateMembers: z
+    .object({
+      initial: z.boolean().optional(),
+      name: z.string().nonempty(),
+      relation: customZodError(z.string().nonempty(), m.errorRelation),
+      nationalId: z.string().optional(),
+      custodian: z.string().length(10).optional(),
+      foreignCitizenship: z.string().array().min(0).max(1).optional(),
+      dateOfBirth: z.string().nonempty().optional(),
+    })
+    .array()
+    .optional(),
+
   selectedEstate: z.enum([
     'Eignalaust dánarbú',
     'Einkaskipti',
     'Opinber skipti',
     'Búsetuleyfi',
   ]),
+  assets: asset, // is: fasteignir
+  flyers: asset,
+  vehicles: asset,
+
+  // is: innbú
+  inventory: z.string().optional(),
+  inventoryValue: z.string().optional(),
+
+  bankAccounts: z
+    .object({
+      accountNumber: z.string().nonempty(),
+      balance: z.string().nonempty(),
+    })
+    .array()
+    .optional(),
+
+  stocks: z
+    .object({
+      organization: z.string().nonempty(),
+      ssn: z.string().length(10),
+      faceValue: z.string().nonempty(), // is: nafnverð
+      rateOfExchange: z.string().nonempty(), // is: gengi
+      value: z.string().nonempty(),
+    })
+    .array()
+    .optional(),
+
+  // is: Peningar og bankahólf
+  moneyAndDepositBoxesInfo: z.string().optional(),
+  moneyAndDepositBoxesValue: z.string().optional(),
+
+  otherAssets: z.string().optional(),
+  otherAssetsValue: z.string().optional(),
+
+  debts: z
+    .object({
+      creditorName: z.string().nonempty(), // is: kröfuhafi
+      ssn: z.string().length(10),
+      balance: z.string().nonempty(),
+    })
+    .array()
+    .optional(),
+
+  // is: Heimild til setu í óskiptu búi skv. erfðaskrá
+  undividedEstateResidencePermission: z.enum(['yes', 'no']),
+
+  // is: Hefur umsækjandi forræði á búi?
+  applicantHasLegalCustodyOverEstate: z.enum(['yes', 'no']),
 })
--- libs/application/templates/estate/src/lib/messages.ts
+++ libs/application/templates/estate/src/lib/messages.ts
@@ -1,146 +1,29 @@
 import { defineMessages } from 'react-intl'
 
 export const m = defineMessages({
-  conditionsSection: {
-    id: 'es.application:conditions.section',
-    defaultMessage: 'Skilyrði',
-    description: 'Some description',
+  // Applicant begin
+  errorPhoneNumber: {
+    id: 'es.application:error.errorPhoneNumber',
+    defaultMessage: 'Símanúmer virðist ekki vera rétt',
+    description: 'Phone number is invalid',
   },
-  institutionName: {
-    id: 'es.application:institution',
-    defaultMessage: 'Institution reference',
-    description: `Institution's name`,
+  errorEmail: {
+    id: 'es.application:error.errorEmail',
+    defaultMessage: 'Netfang virðist ekki vera rétt',
+    description: 'Email is invalid',
   },
-  name: {
-    id: 'es.application:name',
-    defaultMessage: 'Ákvörðun um skipti bús',
-    description: `Application's name`,
+  errorRelation: {
+    id: 'es.application:error.errorRelation',
+    defaultMessage: 'Tengsl virðast ekki vera rétt',
+    description: 'Relation is invalid',
   },
-  draftTitle: {
-    id: 'es.application:draft.title',
-    defaultMessage: 'Drög',
-    description: 'First state title',
+  // Applicant end
+
+  // Assets begin
+  errorNumberEmpty: {
+    id: 'es.application:error.errorNumberEmpty',
+    defaultMessage: 'Númer má ekki vera tómt',
+    description: 'Invalid general asset number error message',
   },
-  draftDescription: {
-    id: 'es.application:draft.description',
-    defaultMessage: 'Notendur hafa ekkert að gera á þessu stigi',
-    description: 'Description of the state',
-  },
-  introSection: {
-    id: 'es.application:intro.section',
-    defaultMessage: 'Upplýsingar',
-    description: 'Some description',
-  },
-  introField: {
-    id: 'es.application:intro.field',
-    defaultMessage: 'Velkomin(n)',
-    description: 'Some description',
-  },
-  introIntroduction: {
-    id: 'es.application:intro.introduction',
-    defaultMessage:
-      '*Hello*, **{name}**! [This is a link to Google!](http://google.com)',
-    description: 'Some description',
-  },
-  about: {
-    id: 'es.application:about',
-    defaultMessage: 'Um þig',
-    description: 'Some description',
-  },
-  personName: {
-    id: 'es.application:person.name',
-    defaultMessage: 'Nafn',
-    description: 'Some description',
-  },
-  nationalId: {
-    id: 'es.application:person.nationalId',
-    defaultMessage: 'Kennitala',
-    description: 'Some description',
-  },
-  age: {
-    id: 'es.application:person.age',
-    defaultMessage: 'Aldur',
-    description: 'Some description',
-  },
-  email: {
-    id: 'es.application:person.email',
-    defaultMessage: 'Netfang',
-    description: 'Some description',
-  },
-  phoneNumber: {
-    id: 'es.application:person.phoneNumber',
-    defaultMessage: 'Símanúmer',
-    description: 'Some description',
-  },
-  career: {
-    id: 'es.application:career',
-    defaultMessage: 'Starfsferill',
-    description: 'Some description',
-  },
-  history: {
-    id: 'es.application:history',
-    defaultMessage: 'Hvar hefur þú unnið áður?',
-    description: 'Some description',
-  },
-  careerHistory: {
-    id: 'es.application:careerHistory',
-    defaultMessage: 'Hefurðu unnið yfir höfuð einhvern tímann áður?',
-    description: 'Some description',
-  },
-  careerHistoryCompanies: {
-    id: 'es.application:careerHistoryCompanies',
-    defaultMessage: 'Hefurðu unnið fyrir eftirfarandi aðila?',
-    description: 'Some description',
-  },
-  future: {
-    id: 'es.application:future',
-    defaultMessage: 'Hvar langar þig að vinna?',
-    description: 'Some description',
-  },
-  dreamJob: {
-    id: 'es.application:dreamJob',
-    defaultMessage: 'Einhver draumavinnustaður?',
-    description: 'Some description',
-  },
-  assigneeTitle: {
-    id: 'es.application:assigneeTitle',
-    defaultMessage: 'Hver á að fara yfir?',
-    description: 'Some description',
-  },
-  assignee: {
-    id: 'es.application:assignee',
-    defaultMessage: 'Assignee email',
-    description: 'Some description',
-  },
-  yesOptionLabel: {
-    id: 'es.application:yes.option.label',
-    defaultMessage: 'Já',
-    description: 'Some description',
-  },
-  noOptionLabel: {
-    id: 'es.application:no.option.label',
-    defaultMessage: 'Nei',
-    description: 'Some description',
-  },
-  governmentOptionLabel: {
-    id: 'es.application:government.option.label',
-    defaultMessage: 'The government',
-    description: 'Some description',
-  },
-  outroMessage: {
-    id: 'es.application:outro.message',
-    defaultMessage:
-      'Your application #{id} is now in review. The ID of the application is returned by the createApplication API action and read from application.externalData',
-    description: 'Some description',
-  },
-  dataSchemePhoneNumber: {
-    id: 'es.application:dataSchema.phoneNumber',
-    defaultMessage: 'Símanúmerið þarf að vera gilt.',
-    description: 'Error message when phone number is invalid.',
-  },
-  dataSchemeNationalId: {
-    id: 'es.application:dataSchema.national.id',
-    defaultMessage: 'Kennitala þarf að vera gild.',
-    description: 'Error message when the kennitala is invalid.',
-  },
+  // Assets end
 })
