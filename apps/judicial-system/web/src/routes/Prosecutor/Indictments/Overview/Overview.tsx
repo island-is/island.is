ab77258a502f45443c81da517ff75d330efc2869
Arnar Kári Ágústsson <arnarkari93@gmail.com>
2022-09-08 11:40:37 +0000

    feat(j-s): Court indictments overview page (#8352)
    * feat(j-s): move info card for active indictments to component
    
    * feat(j-s): add court indictments overview page
    
    * fix(j-s): fix contentful string id
    
    * fix(j-s): fix route const
    
    * test(j-s): remove unused test
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/Overview/Overview.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Overview/Overview.tsx
@@ -7,7 +7,7 @@ import { FormContext } from '@island.is/judicial-system-web/src/components/FormP
 import {
   FormContentContainer,
   FormFooter,
-  InfoCard,
+  InfoCardActiveIndictment,
   Modal,
   PageLayout,
   PdfButton,
@@ -18,13 +18,8 @@ import {
   Sections,
 } from '@island.is/judicial-system-web/src/types'
 import PageHeader from '@island.is/judicial-system-web/src/components/PageHeader/PageHeader'
-import { core, titles } from '@island.is/judicial-system-web/messages'
+import { titles } from '@island.is/judicial-system-web/messages'
 import { Box, Text } from '@island.is/island-ui/core'
-import {
-  capitalize,
-  caseTypes,
-  formatDate,
-} from '@island.is/judicial-system/formatters'
 import {
   useCase,
   useFileList,
@@ -82,44 +77,7 @@ const Overview: React.FC = () => {
         </Box>
         <ProsecutorCaseInfo workingCase={workingCase} />
         <Box component="section" marginBottom={5}>
-          <InfoCard
-            data={[
-              {
-                title: formatMessage(strings.overview.indictmentCreated),
-                value: formatDate(workingCase.created, 'PP'),
-              },
-              {
-                title: formatMessage(strings.overview.prosecutor),
-                value: `${workingCase.creatingProsecutor?.institution?.name}`,
-              },
-              {
-                title: formatMessage(core.policeCaseNumber),
-                value: workingCase.policeCaseNumbers.join(', '),
-              },
-              {
-                title: formatMessage(core.court),
-                value: workingCase.court?.name,
-              },
-              {
-                title: formatMessage(strings.overview.caseType),
-                value: capitalize(caseTypes[workingCase.type]),
-              },
-            ]}
-            defendants={
-              workingCase.defendants
-                ? {
-                    title: capitalize(
-                      workingCase.defendants.length > 1
-                        ? formatMessage(core.indictmentDefendants)
-                        : formatMessage(core.indictmentDefendant, {
-                            gender: workingCase.defendants[0].gender,
-                          }),
-                    ),
-                    items: workingCase.defendants,
-                  }
-                : undefined
-            }
-          />
+          <InfoCardActiveIndictment />
         </Box>
         {workingCase.caseFiles && (
           <Box component="section" marginBottom={10}>
