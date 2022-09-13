d79381dab1620a634de79d574048333eb76bfd50
Snær Seljan <snaer@aranja.com>
2022-09-13 10:37:35 +0000

    remove console.log

--- apps/services/auth-api/src/app/modules/delegations/delegations.controller.spec.ts
+++ apps/services/auth-api/src/app/modules/delegations/delegations.controller.spec.ts
@@ -240,7 +240,6 @@ describe('DelegationsController', () => {
             })
 
             it('should have a an OK return status', () => {
-              console.log(response)
               expect(response.status).toEqual(200)
             })
 
