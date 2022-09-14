1c82b7aa846a1b0f3b43fb023a871408c9ed32c6
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2022-09-14 09:33:09 +0000

    chore(auth): Move all auth services into `apps/services/auth` (#8405)
    * chore(auth): Move all auth services into `apps/services/auth`
    
    * Fix codeowners
    
    * Fix infra scripts
    
    * Fix codeowners
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/services/auth/admin-api/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: '../../../../jest.preset.js',
+  coverageDirectory: '../../../../coverage/apps/services/auth/admin-api',
+  setupFiles: ['./test/environment.jest.ts'],
+  setupFilesAfterEnv: ['./test/setup.ts'],
+  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+  },
+  displayName: 'services-auth-admin-api',
+  testEnvironment: 'node',
+}
