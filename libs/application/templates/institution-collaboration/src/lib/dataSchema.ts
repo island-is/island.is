fa2e967aa2ec12e5c49b5d3727fa5fd16ea51594
Unnar Bjarnason <unnarb@gmail.com>
2022-09-15 09:37:04 +0000

    dataSchema

---
+++ libs/application/templates/institution-collaboration/src/lib/dataSchema.ts
@@ -0,0 +1,49 @@
+import * as z from 'zod'
+import { NO, YES } from '../constants'
+
+export const contactSchema = z.object({
+  name: z.string().nonempty(),
+  email: z.string().email().nonempty(),
+  phoneNumber: z.string().nonempty(),
+})
+
+export const dataSchema = z.object({
+  applicant: z.object({
+    institution: z.object({
+      nationalId: z.string().nonempty(),
+      label: z.string().nonempty(),
+      isat: z.string().optional(),
+    }),
+  }),
+  contact: contactSchema,
+  hasSecondaryContact: z.enum([YES, NO]),
+  secondaryContact: contactSchema.deepPartial(),
+
+  applicantInformation: z.object({
+    constraints: z.object({
+      hasMail: z.boolean().optional(),
+      mail: z.boolean().optional(),
+
+      hasLogin: z.boolean().optional(),
+      login: z.boolean().optional(),
+
+      hasStraumur: z.string().optional(),
+      straumur: z.string().optional(),
+
+      hasWebsite: z.boolean().optional(),
+      website: z.boolean().optional(),
+
+      hasApply: z.string().optional(),
+      apply: z.string().optional(),
+
+      hasMyPages: z.string().optional(),
+      myPages: z.string().optional(),
+
+      hasCert: z.string().optional(),
+      cert: z.string().optional(),
+
+      hasConsult: z.string().optional(),
+      consult: z.string().optional(),
+    }),
+  }),
+})
