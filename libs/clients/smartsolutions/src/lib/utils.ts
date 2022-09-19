6a8ba55f9ae9c67b11d1b4fc3baa538a265789ef
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-09-19 11:28:08 +0000

    fix: fix error codes

--- libs/clients/smartsolutions/src/lib/utils.ts
+++ libs/clients/smartsolutions/src/lib/utils.ts
@@ -1,5 +1,5 @@
 export const ErrorMessageToStatusCodeMap: Record<string, number> = {
-  'Invalid barcode. Please try to refresh the pass.': 2,
-  'Expired barcode. Please refresh the pass.': 2,
+  'Invalid barcode. Please try to refresh the pass.': 3,
+  'Expired barcode. Please refresh the pass.': 3,
   'Request contains some field errors': 4,
 }
