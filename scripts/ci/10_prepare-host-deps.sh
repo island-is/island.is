92c38f7e1c3a9601e077785eabeb59799b54d2a1
peturgq <petur@petur.io>
2022-09-22 17:30:22 +0000

    Add node-gyp, use --immutable instead of frozen-lock

--- scripts/ci/10_prepare-host-deps.sh
+++ scripts/ci/10_prepare-host-deps.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 
-yarn install --frozen-lockfile
+yarn install --immutable
