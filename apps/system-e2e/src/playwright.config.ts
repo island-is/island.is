7b39128417c277567c20360a01b0f99b635baf20
Petar Shomov <petar@andes.is>
2022-09-22 11:25:04 +0000

    Adding reorting to Tesults

--- apps/system-e2e/src/playwright.config.ts
+++ apps/system-e2e/src/playwright.config.ts
@@ -30,7 +30,14 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [
+        [
+          'playwright-tesults-reporter',
+          { 'tesults-target': process.env.TESULTS_TOKEN },
+        ],
+      ]
+    : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
