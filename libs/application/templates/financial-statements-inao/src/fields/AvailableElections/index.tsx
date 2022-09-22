c7e3eb8d1ab25a3cf95883511cc3a3892d6afb03
Helgi Helgason <helgih@kosmosogkaos.is>
2022-09-22 10:41:38 +0000

    add electionname to statement screen

--- libs/application/templates/financial-statements-inao/src/fields/AvailableElections/index.tsx
+++ libs/application/templates/financial-statements-inao/src/fields/AvailableElections/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react'
 import {
   AlertMessage,
   Box,
@@ -14,10 +14,46 @@ import { SelectController } from '@island.is/shared/form-fields'
 import { useFormContext } from 'react-hook-form'
 import { getErrorViaPath } from '@island.is/application/core'
 import { FinancialStatementsInaoElection } from '@island.is/api/schema'
+import { ElectionSelectProps } from '../../lib/utils/types'
+
+const ElectionSelect = ({
+  defaultElections,
+  getDefaultElectionName,
+  options,
+}: ElectionSelectProps) => {
+  const { formatMessage } = useLocale()
+  const { errors, setValue } = useFormContext()
+
+  useEffect(() => {
+    const electionName = getDefaultElectionName()
+    setValue(ABOUTIDS.electionName, electionName)
+  }, [])
+
+  return (
+    <Box width="full">
+      <SelectController
+        id={ABOUTIDS.selectElection}
+        name={ABOUTIDS.selectElection}
+        onSelect={(val) => {
+          setValue(ABOUTIDS.electionName, val.label)
+        }}
+        backgroundColor="blue"
+        label={formatMessage(m.election)}
+        placeholder={formatMessage(m.pickElectionType)}
+        error={errors && getErrorViaPath(errors, ABOUTIDS.selectElection)}
+        options={options}
+        defaultValue={defaultElections}
+      />
+    </Box>
+  )
+}
 
 export const AvailableElections = () => {
   const { formatMessage } = useLocale()
-  const { errors } = useFormContext()
+
+  const { getValues } = useFormContext()
+  const values = getValues()
+
   const { data, loading, error } = useQuery(getAvailableElections)
 
   if (error) {
@@ -39,23 +75,28 @@ export const AvailableElections = () => {
   const financialStatementsInaoElections: FinancialStatementsInaoElection[] =
     data.financialStatementsInaoElections
 
-  const getOptions = () => {
-    return financialStatementsInaoElections.map((option) => {
-      return { label: option.name, value: option.electionId }
-    })
+  const options = financialStatementsInaoElections.map((option) => {
+    return { label: option.name, value: option.electionId }
+  })
+
+  const defaultElections = options?.[0]?.value
+
+  const getDefaultElectionName = () => {
+    const selectedElectionId =
+      values.election.selectElection || defaultElections
+    const electionName = options?.find(
+      (option) => option.value === selectedElectionId,
+    )
+    return electionName?.label || ''
   }
 
   return (
-    <Box width="full">
-      <SelectController
-        id={ABOUTIDS.selectElection}
-        name={ABOUTIDS.selectElection}
-        backgroundColor="blue"
-        label={formatMessage(m.election)}
-        placeholder={formatMessage(m.pickElectionType)}
-        error={errors && getErrorViaPath(errors, ABOUTIDS.selectElection)}
-        options={getOptions()}
+    <Fragment>
+      <ElectionSelect
+        defaultElections={defaultElections}
+        getDefaultElectionName={getDefaultElectionName}
+        options={options}
       />
-    </Box>
+    </Fragment>
   )
 }
--- libs/application/templates/financial-statements-inao/src/lib/constants.ts
+++ libs/application/templates/financial-statements-inao/src/lib/constants.ts
@@ -109,6 +109,7 @@ export const ABOUTIDS = {
   operatingYear: 'conditionalAbout.operatingYear',
   applicationType: 'conditionalAbout.applicationType',
   selectElection: 'election.selectElection',
+  electionName: 'election.electionName',
 }
 
 export const OPERATINGCOST = {
