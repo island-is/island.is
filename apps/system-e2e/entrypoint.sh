a37d0b312fb0d0d234a26ca99d0b2d2ca8b92c02
Petar Shomov <petar@andes.is>
2022-09-23 10:59:42 +0000

    adding playwright report storage in S3

--- apps/system-e2e/entrypoint.sh
+++ apps/system-e2e/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 echo "Current test environment: ${TEST_ENVIRONMENT}"
 echo "Playwright args: $*"
 export PATH=./node_modules/.bin:$PATH
 playwright test -c src "$@"
+TEST_EXIT_CODE=$?
+set -e
+zip -r -0 test-results playwright-report
+aws s3 cp test-results.zip $TEST_RESULTS_S3
+exit $TEST_EXIT_CODE
