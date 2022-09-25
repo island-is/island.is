029807bae2cf9881154555ee78e94ba9aeed7fc0
Petar Shomov <petar@andes.is>
2022-09-25 13:51:14 +0000

    upsi

--- apps/system-e2e/entrypoint.sh
+++ apps/system-e2e/entrypoint.sh
@@ -13,6 +13,6 @@ aws s3 cp test-results.zip $TEST_RESULTS_S3
 echo
 echo To access the report, you can download it from the command line like this: \"aws s3 cp $TEST_RESULTS_S3 .\". Unzip the file and open the file 'index.html' in the playwright-report folder.
 echo
-echo Additionally a web-based overview(experimental) can be found at https://www.tesults.com/results/rsp/view/status/project/ebbe1240-4ed6-420c-b4e0-ee0d808ebfad.
+echo Additionally a web-based overview \(experimental\) can be found at https://www.tesults.com/results/rsp/view/status/project/ebbe1240-4ed6-420c-b4e0-ee0d808ebfad.
 
 exit $TEST_EXIT_CODE
