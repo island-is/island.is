62cc2661c497713dcf0985436ef5431e135fbe40
Petar Shomov <petar@andes.is>
2022-09-26 10:55:04 +0000

    adding slack notification

--- apps/system-e2e/entrypoint.sh
+++ apps/system-e2e/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -uxo pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo "Current test environment: ${TEST_ENVIRONMENT}"
 echo "Playwright args: $*"
 export PATH=./node_modules/.bin:$PATH
@@ -10,6 +12,9 @@ TEST_EXIT_CODE=$?
 set -e
 zip -r -0 test-results playwright-report src/test-results
 aws s3 cp test-results.zip $TEST_RESULTS_S3
+if [ "$TEST_EXIT_CODE" != "0" ] ; then
+  node $DIR/src/notifications/notify.js
+fi
 echo
 echo To access the report, you can download it from the command line like this: \"aws s3 cp $TEST_RESULTS_S3 .\". Unzip the file and open the file 'index.html' in the playwright-report folder.
 echo
