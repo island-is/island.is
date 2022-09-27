21442e272c2ed872d89829355730bf978acfb3d7
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-09-27 14:27:51 +0000

    Remove unused uimport

--- apps/judicial-system/web/src/routes/Court/components/index.ts
+++ apps/judicial-system/web/src/routes/Court/components/index.ts
@@ -1,4 +1,3 @@
 export { default as CourtCaseNumber } from './CourtCaseNumber/CourtCaseNumber'
 export { default as DraftConclusionModal } from './DraftConclusionModal/DraftConclusionModal'
-export { default as Overview } from '../../Shared/Overview/Overview'
 export { default as ReceptionAndAssignment } from './ReceptionAndAssignment/ReceptionAndAssignment'
