bd03fdc0b5dbdb32255dfd56e189885297da304f
Ívar Oddsson <ivaroddsson@gmail.com>
2022-09-27 15:14:02 +0000

    Update apps/judicial-system/backend/src/app/modules/case/case.service.ts
    Co-authored-by: Arnar Kári Ágústsson <arnarkari93@gmail.com>

--- apps/judicial-system/backend/src/app/modules/case/case.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.service.ts
@@ -1125,7 +1125,7 @@ export class CaseService {
 
   async archive(): Promise<ArchiveResponse> {
     const theCase = await this.caseModel.findOne({
-      include: [...includes],
+      include: includes,
       order: [
         defendantsOrder,
         [{ model: CaseFile, as: 'caseFiles' }, 'created', 'ASC'],
