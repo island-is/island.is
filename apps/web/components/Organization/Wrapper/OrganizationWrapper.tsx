6d3238462957c0ad040ad8dda3478bac2904b855
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2022-09-27 16:13:21 +0000

    feat(web): Add header and footer for Ríkislögmaður's organization page (#8521)
    * Add header and footer
    
    * Remove unused styles
    
    * Add break statement

--- apps/web/components/Organization/Wrapper/OrganizationWrapper.tsx
+++ apps/web/components/Organization/Wrapper/OrganizationWrapper.tsx
@@ -54,6 +54,8 @@ import { FiskistofaHeader } from './Themes/FiskistofaTheme/FiskistofaHeader'
 import FiskistofaFooter from './Themes/FiskistofaTheme/FiskistofaFooter'
 import { LandskjorstjornFooter } from './Themes/LandkjorstjornTheme/LandkjorstjornFooter'
 import { LatestNewsCardConnectedComponent } from '../LatestNewsCardConnectedComponent'
+import { RikislogmadurHeader } from './Themes/RikislogmadurTheme/RikislogmadurHeader'
+import { RikislogmadurFooter } from './Themes/RikislogmadurTheme/RikislogmadurFooter'
 import * as styles from './OrganizationWrapper.css'
 
 interface NavigationData {
@@ -109,6 +111,9 @@ export const footerEnabled = [
   'landskjorstjorn',
 
   'hsn',
+
+  'rikislogmadur',
+  'office-of-the-attorney-general-civil-affairs',
 ]
 
 export const getThemeConfig = (
@@ -121,7 +126,7 @@ export const getThemeConfig = (
     footerVersion = 'organization'
   }
 
-  if (theme === 'sjukratryggingar')
+  if (theme === 'sjukratryggingar' || theme === 'rikislogmadur')
     return {
       themeConfig: {
         headerButtonColorScheme: 'blueberry',
@@ -162,6 +167,8 @@ const OrganizationHeader: React.FC<HeaderProps> = ({ organizationPage }) => {
       return <LandlaeknirHeader organizationPage={organizationPage} />
     case 'fiskistofa':
       return <FiskistofaHeader organizationPage={organizationPage} />
+    case 'rikislogmadur':
+      return <RikislogmadurHeader organizationPage={organizationPage} />
     default:
       return <DefaultHeader organizationPage={organizationPage} />
   }
@@ -281,6 +288,16 @@ export const OrganizationFooter: React.FC<FooterProps> = ({
     case 'landskjorstjorn':
       OrganizationFooterComponent = (
         <LandskjorstjornFooter footerItems={organization.footerItems} />
+      )
+      break
+    case 'rikislogmadur':
+    case 'office-of-the-attorney-general-civil-affairs':
+      OrganizationFooterComponent = (
+        <RikislogmadurFooter
+          title={organization.title}
+          footerItems={organization.footerItems}
+          logo={organization.logo?.url}
+        />
       )
       break
   }
