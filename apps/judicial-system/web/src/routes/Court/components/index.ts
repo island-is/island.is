eac6e371e7d5d6e2293046d02064ec6c0af39af1
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-09-27 14:05:43 +0000

    Move Court/Overview to shared because it's shared between defenders and court

--- apps/judicial-system/web/src/routes/Court/components/index.ts
+++ apps/judicial-system/web/src/routes/Court/components/index.ts
@@ -1,4 +1,4 @@
 export { default as CourtCaseNumber } from './CourtCaseNumber/CourtCaseNumber'
 export { default as DraftConclusionModal } from './DraftConclusionModal/DraftConclusionModal'
-export { default as Overview } from './Overview/Overview'
+export { default as Overview } from '../../Shared/Overview/Overview'
 export { default as ReceptionAndAssignment } from './ReceptionAndAssignment/ReceptionAndAssignment'
