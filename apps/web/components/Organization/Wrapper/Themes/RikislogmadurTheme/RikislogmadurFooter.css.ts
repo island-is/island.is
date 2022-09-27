bbe1929a127ac3973fad1c48a216ffdc46f47fa3
runarvestmann <runar@stefna.is>
2022-09-27 15:52:29 +0000

    Add header and footer

---
+++ apps/web/components/Organization/Wrapper/Themes/RikislogmadurTheme/RikislogmadurFooter.css.ts
@@ -0,0 +1,25 @@
+import { globalStyle, style } from '@vanilla-extract/css'
+import { theme } from '@island.is/island-ui/theme'
+
+export const footerBg = style({
+  background:
+    'linear-gradient(178.67deg, rgba(0, 61, 133, 0.2) 1.87%, rgba(0, 61, 133, 0.3) 99.6%)',
+})
+
+export const logoStyle = style({
+  width: 80,
+})
+
+export const footerItemFirst = style({
+  '@media': {
+    [`screen and (max-width: ${theme.breakpoints.lg}px)`]: {
+      maxWidth: 'none',
+      flexBasis: '100%',
+    },
+  },
+})
+
+globalStyle(`${footerBg} a, ${footerBg} a:hover`, {
+  color: 'white',
+  boxShadow: 'inset 0 -1px 0 0 white',
+})
