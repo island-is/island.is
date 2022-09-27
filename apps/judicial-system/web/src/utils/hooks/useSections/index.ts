40e4e8e5d4db48fabef46b2266926fceb689b6c3
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-09-27 15:06:16 +0000

    Fix broken import

--- apps/judicial-system/web/src/utils/hooks/useSections/index.ts
+++ apps/judicial-system/web/src/utils/hooks/useSections/index.ts
@@ -482,57 +482,56 @@ const useSections = () => {
     activeSubSection?: number,
   ) => {
     const { id } = workingCase
-    const subsections = [
-      {
-        type: 'SUB_SECTION',
-        name: formatMessage(sections.indictmentsCourtSection.overview),
-        href: `${constants.INDICTMENTS_COURT_OVERVIEW_ROUTE}/${id}`,
-      },
-      {
-        type: 'SUB_SECTION',
-        name: formatMessage(
-          sections.indictmentsCourtSection.receptionAndAssignment,
-        ),
-        href: `${constants.INDICTMENTS_RECEPTION_AND_ASSIGNMENT_ROUTE}/${workingCase.id}`,
-      },
-      {
-        type: 'SUB_SECTION',
-        name: formatMessage(sections.indictmentsCourtSection.subpoena),
-        href: isReceptionAndAssignmentStepValid(workingCase)
-          ? `${constants.INDICTMENTS_SUBPOENA_ROUTE}/${workingCase.id}`
-          : undefined,
-      },
-      {
-        type: 'SUB_SECTION',
-        name: formatMessage(
-          sections.indictmentsCourtSection.prosecutorAndDefender,
-        ),
-        href:
-          isReceptionAndAssignmentStepValid(workingCase) &&
-          isSubpoenaStepValid(workingCase)
-            ? `${constants.INDICTMENTS_PROSECUTOR_AND_DEFENDER_ROUTE}/${workingCase.id}`
-            : undefined,
-      },
-      {
-        type: 'SUB_SECTION',
-        name: formatMessage(sections.indictmentsCourtSection.courtRecord),
-        href:
-          isReceptionAndAssignmentStepValid(workingCase) &&
-          isSubpoenaStepValid(workingCase) &&
-          isprosecutorAndDefenderStepValid(workingCase)
-            ? `${constants.INDICTMENTS_COURT_RECORD_ROUTE}/${workingCase.id}`
-            : undefined,
-      },
-    ]
 
     return {
       name: formatMessage(sections.courtSection.title),
       children:
         activeSubSection === undefined
-          ? subsections.map((subsection) => {
-              return { name: '', type: '', href: subsection.href }
-            })
-          : subsections,
+          ? []
+          : [
+              {
+                type: 'SUB_SECTION',
+                name: formatMessage(sections.indictmentsCourtSection.overview),
+                href: `${constants.INDICTMENTS_COURT_OVERVIEW_ROUTE}/${id}`,
+              },
+              {
+                type: 'SUB_SECTION',
+                name: formatMessage(
+                  sections.indictmentsCourtSection.receptionAndAssignment,
+                ),
+                href: `${constants.INDICTMENTS_RECEPTION_AND_ASSIGNMENT_ROUTE}/${workingCase.id}`,
+              },
+              {
+                type: 'SUB_SECTION',
+                name: formatMessage(sections.indictmentsCourtSection.subpoena),
+                href: isReceptionAndAssignmentStepValid(workingCase)
+                  ? `${constants.INDICTMENTS_SUBPOENA_ROUTE}/${workingCase.id}`
+                  : undefined,
+              },
+              {
+                type: 'SUB_SECTION',
+                name: formatMessage(
+                  sections.indictmentsCourtSection.prosecutorAndDefender,
+                ),
+                href:
+                  isReceptionAndAssignmentStepValid(workingCase) &&
+                  isSubpoenaStepValid(workingCase)
+                    ? `${constants.INDICTMENTS_PROSECUTOR_AND_DEFENDER_ROUTE}/${workingCase.id}`
+                    : undefined,
+              },
+              {
+                type: 'SUB_SECTION',
+                name: formatMessage(
+                  sections.indictmentsCourtSection.courtRecord,
+                ),
+                href:
+                  isReceptionAndAssignmentStepValid(workingCase) &&
+                  isSubpoenaStepValid(workingCase) &&
+                  isprosecutorAndDefenderStepValid(workingCase)
+                    ? `${constants.INDICTMENTS_COURT_RECORD_ROUTE}/${workingCase.id}`
+                    : undefined,
+              },
+            ],
     }
   }
 
