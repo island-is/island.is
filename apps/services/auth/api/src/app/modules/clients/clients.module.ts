c16cd3aa40efc06ef05716eb162cc43ef23b2990
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2022-09-28 17:06:44 +0000

    chore(auth): Refactor auth-api-lib (#8447)
    * Refactor TranslationModule.
    
    * Refactor UserIdentities module
    
    * Remove ClaimService (it's unused) and move Claim model inside UserIdentities module
    
    * Refactor UserProfileService to AuthApi
    
    It's only used there and doesn't depend on any models.
    
    * Claim
    
    * Refactor Client and Resource module
    
    * Refactor AccessService into resources module
    
    * Refactor GrantType service
    
    * Refactor PersonalRepresentative, IdpProvider, Delegation, Grants, Core modules
    
    * Refactor personal representative services
    
    * Fix AuthPublicAPI problems
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/auth/api/src/app/modules/clients/clients.module.ts
+++ apps/services/auth/api/src/app/modules/clients/clients.module.ts
@@ -1,42 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ClientsController } from './clients.controller'
 import { OriginsController } from './origins.controller'
-import {
-  Client,
-  ClientAllowedScope,
-  ClientAllowedCorsOrigin,
-  ClientPostLogoutRedirectUri,
-  ClientRedirectUri,
-  ClientIdpRestrictions,
-  ClientSecret,
-  ClientGrantType,
-  ClientsService,
-  ClientClaim,
-  IdpProvider,
-  ApiScope,
-  IdentityResource,
-} from '@island.is/auth-api-lib'
-import { SequelizeModule } from '@nestjs/sequelize'
+import { ClientsModule as AuthClientsModule } from '@island.is/auth-api-lib'
 
 @Module({
-  imports: [
-    SequelizeModule.forFeature([
-      Client,
-      ClientAllowedScope,
-      ClientAllowedCorsOrigin,
-      ClientPostLogoutRedirectUri,
-      ClientRedirectUri,
-      ClientIdpRestrictions,
-      ClientSecret,
-      ClientPostLogoutRedirectUri,
-      ClientGrantType,
-      ClientClaim,
-      IdpProvider,
-      ApiScope,
-      IdentityResource,
-    ]),
-  ],
+  imports: [AuthClientsModule],
   controllers: [ClientsController, OriginsController],
-  providers: [ClientsService],
+  providers: [],
 })
 export class ClientsModule {}
