5779269dafdbfcbbf462e87f6222140010034788
runarvestmann <runar@stefna.is>
2022-09-28 15:57:47 +0000

    Change jest preset path

--- apps/tools/contentful-role-permissions/jest.config.js
+++ apps/tools/contentful-role-permissions/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'contentful-role-permissions',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
