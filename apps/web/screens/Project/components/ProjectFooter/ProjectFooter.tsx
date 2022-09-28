b436a13ae8f24b64ec8523acac6a913c277b4864
runarvestmann <runar@stefna.is>
2022-09-28 17:31:01 +0000

    Add footer

--- apps/web/screens/Project/components/ProjectFooter/ProjectFooter.tsx
+++ apps/web/screens/Project/components/ProjectFooter/ProjectFooter.tsx
@@ -1,9 +1,23 @@
+import dynamic from 'next/dynamic'
 import { ProjectPage } from '@island.is/web/graphql/schema'
+
+const OpinberNyskopunFooter = dynamic(() =>
+  import('./themes/OpinberNyskopun/OpinberNyskopunFooter').then(
+    (mod) => mod.OpinberNyskopunFooter,
+  ),
+)
 
 interface ProjectFooterProps {
   projectPage: ProjectPage
 }
 
 export const ProjectFooter = ({ projectPage }: ProjectFooterProps) => {
-  return null
+  const footerItems = projectPage.footerItems ?? []
+
+  switch (projectPage.theme) {
+    case 'opinber-nyskopun':
+      return <OpinberNyskopunFooter footerItems={footerItems} />
+    default:
+      return null
+  }
 }
