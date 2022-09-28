ba2a8f829b2227157ed37fe36782ecbe6794148c
runarvestmann <runar@stefna.is>
2022-09-28 15:56:36 +0000

    Add footer template

---
+++ apps/web/screens/Project/components/ProjectFooter/ProjectFooter.tsx
@@ -0,0 +1,9 @@
+import { ProjectPage } from '@island.is/web/graphql/schema'
+
+interface ProjectFooterProps {
+  projectPage: ProjectPage
+}
+
+export const ProjectFooter = ({ projectPage }: ProjectFooterProps) => {
+  return null
+}
