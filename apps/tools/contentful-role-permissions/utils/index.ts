f5f119aa2370764cde2e412b75df0aaa62f166ee
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2022-09-29 15:17:19 +0000

    feat(web): Contentful role permissions app (#8184)
    * Bootstrap project
    
    * Remove e2e
    
    * Can now edit policies for roles
    
    * Add a way to reset the checkboxes
    
    * Add more options
    
    * Start implementing logic#
    
    * Implemented logic
    
    * Add search input
    
    * Change constants
    
    * Remove todo
    
    * Fix contentful-entry-tagger
    
    * Lint
    
    * Move to tools folder
    
    * Add more stable tag checks by using name instead of id
    
    * Ignore gitbook
    
    * Update README
    
    * Attempt at fixing a lint issue
    
    * Improve readability
    
    * Update code owners
    
    * Remove @nrwl/next from dependencies since it's a dev dependency
    
    * Fix typo in CODEOWNERS file
    
    * Add explicit types
    
    * Change logic a bit
    
    * Add a TODO
    
    * Add class name instead of boolean prop
    
    * Functionality changes
    
    * Populate initial state
    
    * Update yarn.lock
    
    * Fix README typo
    
    * Change README
    
    * Change defaults
    
    * Add warning if tag does not exist
    
    * Change jest preset path
    
    * Add styles folder
    
    * Fix useMemo warning
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/tools/contentful-role-permissions/utils/index.ts
@@ -0,0 +1,303 @@
+import {
+  contentfulManagementAccessToken,
+  spaceId,
+  environmentId,
+} from '../constants'
+import {
+  ContentTypeProps,
+  createClient as createManagementClient,
+  PlainClientAPI,
+  Role,
+  RoleProps,
+  TagProps,
+} from 'contentful-management'
+import slugify from '@sindresorhus/slugify'
+import type { ActionType } from 'contentful-management/dist/typings/entities/role'
+import type { CheckboxState } from '../types'
+
+let client: PlainClientAPI | null = null
+
+export const getContentfulManagementApiClient = (): PlainClientAPI => {
+  if (!client) {
+    client = createManagementClient(
+      {
+        accessToken: contentfulManagementAccessToken,
+      },
+      { type: 'plain', defaults: { spaceId, environmentId } },
+    )
+  }
+  return client
+}
+
+export const getAllRoles = async () => {
+  const client = getContentfulManagementApiClient()
+  const rolesResult = await client.role.getMany({ query: { limit: 1000 } })
+  const roles = rolesResult.items
+  return roles
+}
+
+export const getAllContentTypesInAscendingOrder = async () => {
+  const client = getContentfulManagementApiClient()
+  const contentfulTypesResponse = await client.contentType.getMany({
+    limit: 1000,
+  })
+  const contentTypes = contentfulTypesResponse.items
+  contentTypes.sort((a, b) =>
+    a.name.toLowerCase().localeCompare(b.name.toLowerCase()),
+  )
+  return contentfulTypesResponse.items
+}
+
+export const getAllTags = async () => {
+  const client = getContentfulManagementApiClient()
+  const response = await client.tag.getMany({
+    limit: 1000,
+  })
+  const tags = response.items
+  return tags
+}
+
+export const getTagNameToTagIdMap = (tags: TagProps[]) => {
+  return new Map(tags.map((tag) => [tag.name, tag.sys.id]))
+}
+
+const policiesAreEditable = (
+  role: RoleProps,
+  contentType: ContentTypeProps,
+) => {
+  const policies = []
+  applySingleEntryAllowAllPolicy(policies, contentType.sys.id)
+  return policies.every((p1) =>
+    role.policies.find((p2) => JSON.stringify(p1) === JSON.stringify(p2)),
+  )
+}
+
+const policiesAreReadOnlyEntries = (
+  role: RoleProps,
+  contentType: ContentTypeProps,
+) => {
+  const policies = []
+  applySingleEntryGlobalReadPolicy(policies, contentType.sys.id)
+
+  return (
+    role.policies.find((p) => {
+      return JSON.stringify(policies[0]) === JSON.stringify(p)
+    }) !== undefined
+  )
+}
+
+export const extractInitialCheckboxStateFromRolesAndContentTypes = (
+  roles: RoleProps[],
+  contentTypes: ContentTypeProps[],
+): CheckboxState => {
+  return roles.reduce(
+    (roleAccumulator, role) => ({
+      ...roleAccumulator,
+      [role.name]: contentTypes.reduce(
+        (contentTypeAccumulator, contentType) => ({
+          ...contentTypeAccumulator,
+          [contentType.name]: policiesAreEditable(role, contentType),
+        }),
+        {},
+      ),
+    }),
+    {},
+  )
+}
+
+export const extractInitialReadonlyCheckboxStateFromRolesAndContentTypes = (
+  roles: RoleProps[],
+  contentTypes: ContentTypeProps[],
+): CheckboxState => {
+  return roles.reduce(
+    (roleAccumulator, role) => ({
+      ...roleAccumulator,
+      [role.name]: contentTypes.reduce(
+        (contentTypeAccumulator, contentType) => ({
+          ...contentTypeAccumulator,
+          [contentType.name]: policiesAreReadOnlyEntries(role, contentType),
+        }),
+        {},
+      ),
+    }),
+    {},
+  )
+}
+
+export const extractInitialRoleNamesThatCanReadAllAssetsFromRoles = (
+  roles: RoleProps[],
+  tagsMap: Map<string, string>,
+): string[] => {
+  const roleNames: string[] = []
+  for (const role of roles) {
+    const policies = []
+    applyAssetPolicies(policies, true, tagsMap.get(slugify(role.name)))
+    if (
+      policies.every((p1) =>
+        role.policies.find((p2) => JSON.stringify(p1) === JSON.stringify(p2)),
+      )
+    ) {
+      roleNames.push(role.name)
+    }
+  }
+  return roleNames
+}
+
+export const applyAssetPolicies = (
+  policies: Role['policies'],
+  allowReadingAllAssets = false,
+  tagId: string,
+) => {
+  policies.push({
+    actions: [
+      'create',
+      'read',
+      'update',
+      'delete',
+      'publish',
+      'unpublish',
+      'archive',
+      'unarchive',
+    ],
+    effect: 'allow',
+    constraint: {
+      and: [{ equals: [{ doc: 'sys.type' }, 'Asset'] }],
+    },
+  })
+
+  let disallowedActions: ActionType[] = ['read']
+
+  if (allowReadingAllAssets) {
+    disallowedActions = [
+      'update',
+      'delete',
+      'publish',
+      'unpublish',
+      'archive',
+      'unarchive',
+    ]
+  }
+
+  policies.push({
+    actions: disallowedActions,
+    effect: 'deny',
+    constraint: {
+      and: [
+        { equals: [{ doc: 'sys.type' }, 'Asset'] },
+        {
+          not: {
+            or: [
+              {
+                in: [{ doc: 'metadata.tags.sys.id' }, [tagId]],
+              },
+              {
+                equals: [{ doc: 'sys.createdBy.sys.id' }, 'User.current()'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  })
+}
+
+const applySingleEntryGlobalReadPolicy = (
+  policies: Role['policies'],
+  contentTypeId: string,
+) => {
+  policies.push({
+    actions: ['read'],
+    effect: 'allow',
+    constraint: {
+      and: [
+        { equals: [{ doc: 'sys.type' }, 'Entry'] },
+        {
+          equals: [{ doc: 'sys.contentType.sys.id' }, contentTypeId],
+        },
+      ],
+    },
+  })
+}
+
+export const applyEntryGlobalReadPolicies = (
+  policies: Role['policies'],
+  contentTypeIds: Set<string>,
+) => {
+  for (const id of contentTypeIds) {
+    applySingleEntryGlobalReadPolicy(policies, id)
+  }
+}
+
+const applySingleEntryAllowAllPolicy = (
+  policies: Role['policies'],
+  contentTypeId: string,
+) => {
+  policies.push({
+    actions: 'all',
+    effect: 'allow',
+    constraint: {
+      and: [
+        { equals: [{ doc: 'sys.type' }, 'Entry'] },
+        {
+          equals: [{ doc: 'sys.contentType.sys.id' }, contentTypeId],
+        },
+      ],
+    },
+  })
+}
+
+export const applyEntryEditPolicies = (
+  policies: Role['policies'],
+  scopedEditableContentTypeIds: Set<string>,
+  tagId: string,
+  globallyReadableContentTypeIds: Set<string>,
+) => {
+  const allContentTypeIds = new Set<string>()
+
+  for (const id of scopedEditableContentTypeIds) {
+    allContentTypeIds.add(id)
+    applySingleEntryAllowAllPolicy(policies, id)
+  }
+
+  for (const id of globallyReadableContentTypeIds) {
+    allContentTypeIds.add(id)
+  }
+
+  for (const id of allContentTypeIds) {
+    if (!scopedEditableContentTypeIds.has(id)) continue
+
+    const actions: ActionType[] = [
+      'update',
+      'delete',
+      'publish',
+      'unpublish',
+      'archive',
+      'unarchive',
+    ]
+
+    if (!globallyReadableContentTypeIds.has(id)) {
+      actions.push('read')
+    }
+
+    policies.push({
+      actions,
+      effect: 'deny',
+      constraint: {
+        and: [
+          { equals: [{ doc: 'sys.type' }, 'Entry'] },
+          {
+            equals: [{ doc: 'sys.contentType.sys.id' }, id],
+          },
+          {
+            not: {
+              or: [
+                { equals: [{ doc: 'sys.createdBy.sys.id' }, 'User.current()'] },
+                { in: [{ doc: 'metadata.tags.sys.id' }, [tagId]] },
+              ],
+            },
+          },
+        ],
+      },
+    })
+  }
+}
