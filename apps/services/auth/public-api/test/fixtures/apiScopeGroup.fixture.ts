a4a2ba17094afcbf0c8a769d4a238a578bb70b27
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2022-10-05 12:39:27 +0000

    feat(auth): API scope ordering (#8592)
    * feat(auth): API Scope and Group ordering
    
    Updated database models, auth-api-lib, services-auth-admin-api, auth-admin-web. Missing actual ordering in auth-public-api.
    
    * Implement api scope ordering in auth-public-api response
    
    Uses SQL sorting for now, where order column is limited between 0-999
    
    * Implement API test for scope sorting
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/services/auth/public-api/test/fixtures/apiScopeGroup.fixture.ts
@@ -0,0 +1,29 @@
+import faker from 'faker'
+
+import { ApiScopeGroup } from '@island.is/auth-api-lib'
+
+export type CreateApiScopeGroup = Pick<
+  ApiScopeGroup,
+  'id' | 'name' | 'displayName' | 'description' | 'order' | 'domainName'
+>
+
+export type CreateApiScopeGroupOptions = Partial<CreateApiScopeGroup> &
+  Pick<CreateApiScopeGroup, 'domainName'>
+
+export const createApiScopeGroup = ({
+  id,
+  name,
+  displayName,
+  description,
+  order,
+  domainName,
+}: CreateApiScopeGroupOptions): CreateApiScopeGroup => {
+  return {
+    id: id ?? faker.datatype.uuid(),
+    name: name ?? faker.random.word(),
+    domainName,
+    order: order ?? 0,
+    displayName: displayName ?? faker.random.word(),
+    description: description ?? faker.lorem.sentence(),
+  }
+}
