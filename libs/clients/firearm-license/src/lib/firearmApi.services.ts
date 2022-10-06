fd034de4d7005496789e05f2e7666334a4433e2b
disaerna <disa.erna@gmail.com>
2022-10-06 10:39:47 +0000

    fix: merge conflicts

--- libs/clients/firearm-license/src/lib/firearmApi.services.ts
+++ libs/clients/firearm-license/src/lib/firearmApi.services.ts
@@ -11,9 +11,11 @@ import {
 export class FirearmApi {
   constructor(private readonly api: FirearmApplicationApi) {}
 
+  private firearmApiWithAuth = (user: User) =>
+    this.api.withMiddleware(new AuthMiddleware(user as Auth))
+
   public async getLicenseInfo(user: User): Promise<LicenseInfo | null> {
-    const licenseInfo = await this.api
-      .withMiddleware(new AuthMiddleware(user as Auth))
+    const licenseInfo = await this.firearmApiWithAuth(user)
       .apiFirearmApplicationLicenseInfoGet()
       .catch(handle404)
     return licenseInfo
@@ -22,16 +24,15 @@ export class FirearmApi {
   public async getPropertyInfo(
     user: User,
   ): Promise<FirearmPropertyList | null> {
-    const propertyInfo = await this.api
-      .withMiddleware(new AuthMiddleware(user as Auth))
+    const propertyInfo = await this.firearmApiWithAuth(user)
       .apiFirearmApplicationPropertyInfoGet({ pageNumber: 1, pageSize: 50 })
       .catch(handle404)
     return propertyInfo
   }
   public async getCategories(user: User): Promise<{ [key: string]: string }> {
-    const categories = await this.api
-      .withMiddleware(new AuthMiddleware(user as Auth))
-      .apiFirearmApplicationCategoriesGet()
+    const categories = await this.firearmApiWithAuth(
+      user,
+    ).apiFirearmApplicationCategoriesGet()
     return categories
   }
 }
