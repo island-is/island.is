f4ce7e110c0c7d76d5f8bfe2aa1cb5a2223427df
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2022-10-11 09:56:53 +0000

    feat(transport authority): new applications (base) (#8531)
    * Set up base
    
    * Setup dummy application for each type
    
    * Add 1 service to template-api-modules
    
    * Merge register+deregister into change
    
    * Add rest to template-api-modules
    
    * Cleanup
    
    * Add to codeowners
    
    * Use feature flag
    
    * Use EphemeralStateLifeCycle
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/transport-authority/digital-tachograph-workshop-card/src/index.ts
@@ -0,0 +1,6 @@
+import template from './lib/DigitalTachographWorkshopCardTemplate'
+
+export const getFields = () => import('./fields/')
+export const getDataProviders = () => import('./dataProviders/')
+
+export default template
