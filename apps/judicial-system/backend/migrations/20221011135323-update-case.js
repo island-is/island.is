68a26908bd7419b8d1aabf78d7130ad725cfb81a
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-10-11 14:20:53 +0000

    Add Sequelize

--- apps/judicial-system/backend/migrations/20221011135323-update-case.js
+++ apps/judicial-system/backend/migrations/20221011135323-update-case.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  up: (queryInterface) => {
+  up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((transaction) =>
       Promise.all([
         queryInterface.addColumn(
