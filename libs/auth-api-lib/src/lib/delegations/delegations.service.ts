744770222865f2162b7b52aaae18ca6f11dd6e99
Snær Seljan Þóroddsson <112904566+snaerseljan@users.noreply.github.com>
2022-10-11 16:59:38 +0000

    Update libs/auth-api-lib/src/lib/delegations/delegations.service.ts
    Co-authored-by: Eiríkur Heiðar Nilsson <eirikur@nilsson.is>

--- libs/auth-api-lib/src/lib/delegations/delegations.service.ts
+++ libs/auth-api-lib/src/lib/delegations/delegations.service.ts
@@ -435,7 +435,7 @@ export class DelegationsService {
     }
 
     const delegationsPromises = delegations
-      // Filter out companies, since we do not need to make a natinonal registry call for them.
+      // Filter out companies, since we do not need to make a national registry call for them.
       .filter(({ fromNationalId }) => !kennitala.isCompany(fromNationalId))
       .map(({ fromNationalId }) =>
         this.nationalRegistryClient.getIndividual(fromNationalId),
