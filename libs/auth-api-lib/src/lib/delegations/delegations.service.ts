0cc94b28e039c8d8ec3f0ec6f6ba50e71493dda0
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2022-10-11 11:35:57 +0000

    feat(auth): Support delegations one level deep (#8644)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/auth-api-lib/src/lib/delegations/delegations.service.ts
+++ libs/auth-api-lib/src/lib/delegations/delegations.service.ts
@@ -92,7 +92,10 @@ export class DelegationsService {
     user: User,
     createDelegation: CreateDelegationDTO,
   ): Promise<DelegationDTO | null> {
-    if (createDelegation.toNationalId === user.nationalId) {
+    if (
+      createDelegation.toNationalId === user.nationalId ||
+      this.isDelegationToActor(user, createDelegation)
+    ) {
       throw new BadRequestException(`Can not create delegation to self.`)
     }
 
@@ -150,6 +153,13 @@ export class DelegationsService {
     input: UpdateDelegationDTO,
     delegationId: string,
   ): Promise<DelegationDTO | null> {
+    const delegation = await this.delegationModel.findByPk(delegationId)
+    if (!delegation) {
+      throw new NotFoundException()
+    }
+    if (this.isDelegationToActor(user, delegation)) {
+      throw new BadRequestException('Can not update delegation to self.')
+    }
     if (!(await this.validateScopesAccess(user, input.scopes))) {
       throw new BadRequestException(
         'User does not have access to the requested scopes.',
@@ -296,9 +306,11 @@ export class DelegationsService {
     }
 
     return delegations
-      .filter((d) =>
-        // The user must have access to at least one scope in the delegation
-        d.delegationScopes?.some((s) => this.checkIfScopeAllowed(s, user)),
+      .filter(
+        (d) =>
+          // The user must have access to at least one scope in the delegation
+          d.delegationScopes?.some((s) => this.checkIfScopeAllowed(s, user)) &&
+          !this.isDelegationToActor(user, d),
       )
       .map((d) => {
         // Filter out scopes the user does not have access to
@@ -307,6 +319,13 @@ export class DelegationsService {
         )
         return d.toDTO()
       })
+  }
+
+  private isDelegationToActor(
+    user: User,
+    delegation: { toNationalId: string },
+  ): boolean {
+    return user.actor?.nationalId === delegation.toNationalId
   }
 
   /***** Incoming Delegations *****/
