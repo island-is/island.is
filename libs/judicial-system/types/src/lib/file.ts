91734a64d5e4a604e76fa4b019c6a62b2bc2d74c
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-10-12 11:19:03 +0000

    Add to CreateFile tyoe

--- libs/judicial-system/types/src/lib/file.ts
+++ libs/judicial-system/types/src/lib/file.ts
@@ -70,7 +70,10 @@ export interface CaseFile {
 }
 
 export interface CreateFile {
+  caseId: string
   type: string
+  category?: CaseFileCategory
   key: string
   size: number
+  policeCaseNumber?: string
 }
