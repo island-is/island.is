a29ddc6ff165be8433cbe2d990a5f886d69a1cf7
Snær Seljan Þóroddsson <snaer@aranja.com>
2022-10-13 12:24:09 +0000

    update imports

--- libs/service-portal/access-control/delegations/src/components/Access/AccessForm.tsx
+++ libs/service-portal/access-control/delegations/src/components/Access/AccessForm.tsx
@@ -33,7 +33,7 @@ import {
   SCOPE_PREFIX,
 } from '../../utils/types'
 import { AccessItem } from './AccessItem'
-import { AccessConfirmModal, AccessItemHeader } from '../../components/access'
+import { AccessConfirmModal, AccessItemHeader } from '../../components/Access'
 import { isDefined } from '@island.is/shared/utils'
 
 export type MappedScope = {
