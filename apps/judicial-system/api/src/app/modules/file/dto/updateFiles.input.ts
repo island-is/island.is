002d874648861e3bc39cc6ae85f2197282ac0774
Arnar Kári Ágústsson <arnarkari93@gmail.com>
2022-10-14 12:55:11 +0000

    feat(j-s): bulk case file updates (#8682)
    * feat(j-s): add method to update mulitple case files
    
    * test(j-s): add test for updateFiles route
    
    * feat(j-s): add updateFiles mutation to api
    
    * feat(j-s): add validation to input for updateFiles
    
    * feat(j-s): validate updateFiles input on backend
    
    * chore(j-s): remove unused import
    
    * test(j-s): fix updateFiles test
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/judicial-system/api/src/app/modules/file/dto/updateFiles.input.ts
@@ -0,0 +1,63 @@
+import {
+  Allow,
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+  ValidateIf,
+  ValidateNested,
+} from 'class-validator'
+import { Type } from 'class-transformer'
+import { Field, InputType, Int } from '@nestjs/graphql'
+
+import { UpdateFile as TUpdateFile } from '@island.is/judicial-system/types'
+
+@InputType()
+export class UpdateFile implements TUpdateFile {
+  @Allow()
+  @Field()
+  readonly id!: string
+
+  @Allow()
+  @Field(() => Int, {
+    nullable: true,
+    description:
+      'Number of chapter where file is in. 0 or greater. If provided, then order must also be provided.',
+  })
+  @ValidateIf((file) => typeof file.orderWithinChapter === 'number')
+  @IsNumber()
+  @Min(0)
+  readonly chapter?: number
+
+  @Allow()
+  @Field(() => Int, {
+    nullable: true,
+    description:
+      'Number indicating the order within chapter. 0 or greater. If provided, then chapter must also be provided.',
+  })
+  @ValidateIf((file) => typeof file.chapter === 'number')
+  @IsNumber()
+  @Min(0)
+  readonly orderWithinChapter?: number
+
+  @Allow()
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  readonly userGeneratedFilename?: string
+}
+
+@InputType()
+export class UpdateFilesInput {
+  @Allow()
+  @Field()
+  readonly caseId!: string
+
+  @Allow()
+  @Field(() => [UpdateFile])
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateFile)
+  readonly files!: UpdateFile[]
+}
