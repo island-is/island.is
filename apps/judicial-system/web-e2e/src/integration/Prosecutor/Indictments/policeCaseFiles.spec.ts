c66e9b688cfc4b402502e6413820d51bc3542e3f
Ívar Oddsson <ivaroddsson@gmail.com>
2022-10-17 11:09:55 +0000

    feat(j-s): Create police case files screen (#8600)
    * Create police case files screen
    
    * Basic page setup
    
    * feat(j-s): rewrite useS3Upload hook
    
    * feat(j-s): use new upload hook for each loke number
    
    * feat(j-s): add retry and remove for useS3UploadV2
    
    * feat(j-s): add error handling for upload
    
    Co-authored-by: Ívar Oddsson <ivaroddsson@gmail.com>
    
    * feat(j-s): memorize police file upload list
    
    To avoid maximum depth rendering
    
    * feat(j-s): move string to contentful
    
    * docs(j-s): add to more explaination to comment
    
    * refactor(j-s): change PageTitle to accept children
    
    * fix(j-s): fix missing defaultMessage for contentful string
    
    * fix(j-s): fix retry uploading file
    
    Adding `id` from the beginning to file to be able to refrence it when
    retrying. Setting precent and state to uploading for better user
    experince when retrying a failed upload.
    
    * feat(j-s): handle errors when delete file failes
    
    * test(j-s): fix e2e test for indictments prosecutor flow
    
    * refactor(j-s): move setting file upload state out of uploadToS3
    
    Co-authored-by: Arnar Kári <arnar.kari@kolibri.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web-e2e/src/integration/Prosecutor/Indictments/policeCaseFiles.spec.ts
+++ apps/judicial-system/web-e2e/src/integration/Prosecutor/Indictments/policeCaseFiles.spec.ts
@@ -1,12 +1,12 @@
 import {
-  INDICTMENTS_CASE_FILES_ROUTE,
   INDICTMENTS_POLICE_CASE_FILES_ROUTE,
+  INDICTMENTS_OVERVIEW_ROUTE,
 } from '@island.is/judicial-system/consts'
 import { CaseType, UserRole } from '@island.is/judicial-system/types'
 
 import { makeCourt, mockCase, makeProsecutor, intercept } from '../../../utils'
 
-describe(`${INDICTMENTS_CASE_FILES_ROUTE}/:id`, () => {
+describe(`${INDICTMENTS_POLICE_CASE_FILES_ROUTE}/:id`, () => {
   beforeEach(() => {
     const caseData = mockCase(CaseType.MURDER)
     const caseDataAddition = {
@@ -18,11 +18,11 @@ describe(`${INDICTMENTS_CASE_FILES_ROUTE}/:id`, () => {
     cy.login(UserRole.PROSECUTOR)
     cy.stubAPIResponses()
     intercept(caseDataAddition)
-    cy.visit(`${INDICTMENTS_CASE_FILES_ROUTE}/test_id`)
+    cy.visit(`${INDICTMENTS_POLICE_CASE_FILES_ROUTE}/test_id`)
   })
 
   it('should navigate to the correct page on continue', () => {
     cy.getByTestid('continueButton').click()
-    cy.url().should('contain', INDICTMENTS_POLICE_CASE_FILES_ROUTE)
+    cy.url().should('contain', INDICTMENTS_OVERVIEW_ROUTE)
   })
 })
