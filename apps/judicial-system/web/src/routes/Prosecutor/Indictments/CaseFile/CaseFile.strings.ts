0d884d51abe54cdcd2f8b61820f3233bcc611b74
Ívar Oddsson <ivaroddsson@gmail.com>
2022-10-20 08:56:55 +0000

    Use defineMessages instead of defineMessage

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFile/CaseFile.strings.ts
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFile/CaseFile.strings.ts
@@ -1,21 +1,21 @@
-import { defineMessage } from 'react-intl'
+import { defineMessages } from 'react-intl'
 
-export const caseFile = {
-  heading: defineMessage({
+export const caseFile = defineMessages({
+  heading: {
     id: 'judicial.system.core:case_file.heading',
     defaultMessage: 'Skjalaskrá',
     description: 'Notaður sem titill á skjalaskrá skrefi í ákærum.',
-  }),
-  infoPanel: defineMessage({
+  },
+  infoPanel: {
     id: 'judicial.system.core:case_file.info_panel',
     defaultMessage:
       'Hér er hægt að útbúa gagnapakka með skjalaskrá fyrir hvert málsnúmer. Hægt er að raða skjölum í rétta röð, breyta heiti skjala og dagsetningum í efnisyfirliti.',
     description: 'Texti í upplýsingarboxi á skjalaskrá skrefi í ákærum.',
-  }),
-  accordionTitle: defineMessage({
+  },
+  accordionTitle: {
     id: 'judicial.system.core:case_file.accordion_title',
     defaultMessage: 'Gögn úr LÖKE máli {policeCaseNumber}',
     description:
       'Notaður sem titill á fellilista í skjalaskrá skrefi í ákærum.',
-  }),
-}
+  },
+})
