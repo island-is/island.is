c36fc3715737e6acf25e96894cd3dd828e59451a
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-10-26 15:04:38 +0000

    Rename string

--- apps/judicial-system/web/src/components/PdfButton/PdfButton.strings.ts
+++ apps/judicial-system/web/src/components/PdfButton/PdfButton.strings.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl'
 
 export const pdfButton = defineMessages({
   generatePDFError: {
-    id: 'judicial.system.core:case_file.open_pdf_error',
+    id: 'judicial.system.core:pdf_button.generate_pdf_error',
     defaultMessage: 'Ekki tókst að útbúa gagnapakka',
     description:
       'Notaður sem villuboð sem birtist ef ekki tekst að útbúa gagnapakka í ákærum.',
