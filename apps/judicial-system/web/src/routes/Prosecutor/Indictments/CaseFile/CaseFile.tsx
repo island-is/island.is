41f95f2580848226c6c722ffeab64bedfd3dad6f
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-10-26 13:40:14 +0000

    Checkpoint

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFile/CaseFile.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/CaseFile/CaseFile.tsx
@@ -9,9 +9,16 @@ import {
   IndictmentsCaseFilesAccordionItem,
   PageHeader,
   PageLayout,
+  PdfButton,
   ProsecutorCaseInfo,
 } from '@island.is/judicial-system-web/src/components'
-import { Accordion, AlertMessage, Box, Text } from '@island.is/island-ui/core'
+import {
+  Accordion,
+  AlertMessage,
+  Box,
+  Button,
+  Text,
+} from '@island.is/island-ui/core'
 import {
   IndictmentsProsecutorSubsections,
   Sections,
@@ -51,7 +58,7 @@ const CaseFile = () => {
         <Box marginBottom={7}>
           <AlertMessage type="info" message={formatMessage(m.infoPanel)} />
         </Box>
-        <Box marginBottom={7}>
+        <Box marginBottom={5}>
           <LayoutGroup>
             <Accordion singleExpand>
               {workingCase.policeCaseNumbers.map((policeCaseNumber, index) => (
@@ -71,6 +78,20 @@ const CaseFile = () => {
               ))}
             </Accordion>
           </LayoutGroup>
+        </Box>
+        <Box marginBottom={7}>
+          {workingCase.policeCaseNumbers.map((policeCaseNumber, index) => (
+            <Box marginBottom={1} key={`${policeCaseNumber}-${index}`}>
+              <PdfButton
+                caseId={workingCase.id}
+                title={formatMessage(m.pdfButtonText, {
+                  policeCaseNumber: policeCaseNumber,
+                })}
+                pdfType="caseFiles"
+                policeCaseNumber={policeCaseNumber}
+              />
+            </Box>
+          ))}
         </Box>
       </FormContentContainer>
       <FormContentContainer isFooter>
