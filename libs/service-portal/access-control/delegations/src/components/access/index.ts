da588df5a728c28709e2385d3ff55d342611f94d
Snær Seljan Þóroddsson <snaer@aranja.com>
2022-10-27 09:16:58 +0000

    remove dead code

--- libs/service-portal/access-control/delegations/src/components/access/index.ts
+++ libs/service-portal/access-control/delegations/src/components/access/index.ts
@@ -1,7 +1,6 @@
 export { AccessCard } from './AccessCard'
 export { AccessForm } from './AccessForm'
 export { AccessItem } from './AccessItem'
-export { AccessModal } from './AccessModal'
 export { AccessDeleteModal } from './AccessDeleteModal'
 export { AccessConfirmModal } from './AccessConfirmModal'
 export { AccessItemHeader } from './AccessItemHeader'
