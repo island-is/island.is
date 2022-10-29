7e7cc9bda3fc329481fa449a16d7f1eadd57b2b2
runarvestmann <runar@stefna.is>
2022-10-29 19:15:17 +0000

    Add header

--- apps/web/components/Project/Header/FiskistofaDashboardHeader.tsx
+++ apps/web/components/Project/Header/FiskistofaDashboardHeader.tsx
@@ -1,7 +1,9 @@
-import { Box } from '@island.is/island-ui/core'
+import { useMemo } from 'react'
+import { Box, Inline, Text } from '@island.is/island-ui/core'
 import { ProjectPage } from '@island.is/web/graphql/schema'
 import { useNamespace } from '@island.is/web/hooks'
-import { useMemo } from 'react'
+
+import * as styles from './FiskistofaDashboardHeader.css'
 
 interface FiskistofaDashboardHeaderProps {
   projectPage: ProjectPage
@@ -16,5 +18,28 @@ export const FiskistofaDashboardHeader: React.FC<FiskistofaDashboardHeaderProps>
 
   const n = useNamespace(namespace)
 
-  return <Box display="flex" justifyContent="flexEnd"></Box>
+  return (
+    <Box
+      display="flex"
+      justifyContent="flexEnd"
+      paddingTop={3}
+      paddingRight={3}
+    >
+      <Inline alignY="center" space={[1, 2, 3]} flexWrap="nowrap">
+        <Text variant="h3" as="h1">
+          <span className={styles.title}>
+            {n('fiskistofaDashboard', 'Mælaborð Fiskistofu')}
+          </span>
+        </Text>
+        <img
+          className={styles.logo}
+          src={n(
+            'logo',
+            'https://images.ctfassets.net/8k0h54kbe6bj/3W68zG8kVqVY7DU1qHxVEA/4d403503b05488cc8385578a92ef399e/fiskist-merki_2x.png',
+          )}
+          alt="fiskistofa-header"
+        />
+      </Inline>
+    </Box>
+  )
 }
