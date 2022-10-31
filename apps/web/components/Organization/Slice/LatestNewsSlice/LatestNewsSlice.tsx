5a6c20e0049c18a73e15c14545e7c729847b3be9
runarvestmann <runar@stefna.is>
2022-10-31 14:47:58 +0000

    Add landing page theme

--- apps/web/components/Organization/Slice/LatestNewsSlice/LatestNewsSlice.tsx
+++ apps/web/components/Organization/Slice/LatestNewsSlice/LatestNewsSlice.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import { LatestNewsSlice as LatestNewsSliceSchema } from '@island.is/web/graphql/schema'
 import { NewsItems } from '@island.is/web/components'
-import { Box } from '@island.is/island-ui/core'
+import { Box, BoxProps } from '@island.is/island-ui/core'
 import { LinkType } from '@island.is/web/hooks'
 
 interface SliceProps {
   slice: LatestNewsSliceSchema
   slug: string
-  renderedOnOrganizationSubpage: boolean
   linkType?: LinkType
   overview?: LinkType
+  latestNewsSliceBackground?: BoxProps['background']
+  forceTitleSectionHorizontalPadding?: boolean
 }
 
 export const LatestNewsSlice: React.FC<SliceProps> = ({
   slice,
   slug,
-  renderedOnOrganizationSubpage,
   linkType = 'organizationnews',
   overview = 'organizationnewsoverview',
+  latestNewsSliceBackground = 'purple100',
+  forceTitleSectionHorizontalPadding = false,
 }) => {
   return (
     <Box
       component="section"
-      background={renderedOnOrganizationSubpage ? undefined : 'purple100'}
+      background={latestNewsSliceBackground}
       paddingTop={[5, 5, 8]}
       paddingBottom={[2, 2, 5]}
       aria-labelledby="news-items-title"
@@ -36,7 +38,7 @@ export const LatestNewsSlice: React.FC<SliceProps> = ({
         overview={overview}
         parameters={[slug]}
         seeMoreHref={slice.readMoreLink?.url}
-        forceTitleSectionHorizontalPadding={renderedOnOrganizationSubpage}
+        forceTitleSectionHorizontalPadding={forceTitleSectionHorizontalPadding}
       />
     </Box>
   )
