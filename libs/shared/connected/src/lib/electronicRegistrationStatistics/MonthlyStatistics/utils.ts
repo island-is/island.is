8fc0b6e9bacfa9b72701a85d791ed1b497911f5d
runarvestmann <runar@stefna.is>
2022-11-01 07:33:03 +0000

    Edit comment

--- libs/shared/connected/src/lib/electronicRegistrationStatistics/MonthlyStatistics/utils.ts
+++ libs/shared/connected/src/lib/electronicRegistrationStatistics/MonthlyStatistics/utils.ts
@@ -4,7 +4,7 @@ export const extractRegistrationTypesFromData = (
   data: BrokenDownRegistrationStatistic[],
 ) => {
   const types = new Set<string>()
-  // TODO: i18n
+  // The external service only returns Icelandic words so that's why we can hardcode 'Allt' here to represent all choices in the dropdown
   types.add('Allt')
 
   for (const period of data)
