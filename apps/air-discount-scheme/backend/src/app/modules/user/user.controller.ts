f5f3db310c3bb537d5126b90aaf2214596767b70
kksteini <steini@kosmosogkaos.is>
2022-11-01 15:52:01 +0000

    Initial auth header fix

--- apps/air-discount-scheme/backend/src/app/modules/user/user.controller.ts
+++ apps/air-discount-scheme/backend/src/app/modules/user/user.controller.ts
@@ -5,7 +5,12 @@ import {
   UseGuards,
   BadRequestException,
 } from '@nestjs/common'
-import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger'
+import {
+  ApiBearerAuth,
+  ApiExcludeEndpoint,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger'
 
 import { GetUserRelationsParams } from './dto'
 import { UserService } from './user.service'
@@ -22,15 +27,18 @@ import type { User as AuthUser } from '@island.is/auth-nest-tools'
 @Scopes('@vegagerdin.is/air-discount-scheme-scope')
 @Controller('api/private')
 @ApiTags('Users')
+@ApiBearerAuth()
 export class PrivateUserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('users/:nationalId/relations')
   @ApiExcludeEndpoint(!process.env.ADS_PRIVATE_CLIENT)
+  @ApiOkResponse({ type: [User] })
   async getUserRelations(
     @Param() params: GetUserRelationsParams,
     @CurrentUser() authUser: AuthUser,
   ): Promise<User[]> {
+    console.log('Made it inside!')
     if (params.nationalId !== authUser.nationalId) {
       throw new BadRequestException(
         '[/relations] Request parameters do not correspond with user authentication.',
