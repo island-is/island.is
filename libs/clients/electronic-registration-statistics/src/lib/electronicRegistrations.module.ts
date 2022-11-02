344e22f6b1309f7af5715ab4784803be7a45862a
runarvestmann <runar@stefna.is>
2022-11-02 15:33:51 +0000

    Change formatting and inject api class

--- libs/clients/electronic-registration-statistics/src/lib/electronicRegistrations.module.ts
+++ libs/clients/electronic-registration-statistics/src/lib/electronicRegistrations.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
+import { ApiProvider } from './apiProvider'
 import { ElectronicRegistrationsClientService } from './electronicRegistrations.service'
 
 @Module({
-  providers: [ElectronicRegistrationsClientService],
+  providers: [ElectronicRegistrationsClientService, ApiProvider],
   exports: [ElectronicRegistrationsClientService],
 })
 export class ElectronicRegistrationsClientModule {}
--- libs/clients/electronic-registration-statistics/src/lib/electronicRegistrations.service.ts
+++ libs/clients/electronic-registration-statistics/src/lib/electronicRegistrations.service.ts
@@ -1,40 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common'
-import { Configuration, StatisticsApi } from '../../gen/fetch'
-import { ElectronicRegistrationsClientConfig } from './electronicRegistrations.config'
-import type { ConfigType } from '@island.is/nest/config'
-import { createEnhancedFetch } from '@island.is/clients/middlewares'
+import { Injectable } from '@nestjs/common'
+import { StatisticsApi } from '../../gen/fetch'
 
 @Injectable()
 export class ElectronicRegistrationsClientService {
-  constructor(
-    @Inject(ElectronicRegistrationsClientConfig.KEY)
-    private clientConfig: ConfigType<
-      typeof ElectronicRegistrationsClientConfig
-    >,
-  ) {}
-
-  private async createApi() {
-    return new StatisticsApi(
-      new Configuration({
-        fetchApi: createEnhancedFetch({
-          name: 'clients-electronic-registration-statistics',
-          ...this.clientConfig.fetch,
-        }),
-        basePath: this.clientConfig.url,
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      }),
-    )
-  }
+  constructor(private readonly api: StatisticsApi) {}
 
   async getBrokenDownElectronicRegistrationStatistics(filter: {
     year: number
   }) {
-    const api = await this.createApi()
-
-    const data = await api.faSundurlidadaThinglysingarTolfraediAsync({
+    const data = await this.api.faSundurlidadaThinglysingarTolfraediAsync({
       dateFrom: new Date(filter.year, 0, 1),
       dateTo: new Date(filter.year + 1, 0, 0),
     })
