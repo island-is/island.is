d3f750b204a17a1465d23a040eed49fe39b7f5c1
Kristófer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-11-03 14:52:13 +0000

    Updated mock data

--- libs/api/domains/assets/src/lib/__mock-data__/requestHandlers.ts
+++ libs/api/domains/assets/src/lib/__mock-data__/requestHandlers.ts
@@ -19,183 +19,36 @@ export const MOCK_USER = {
 } as User
 
 export const XROAD_BASE_PATH = 'http://localhost:8081'
-export const XROAD_DRIVING_LICENSE_PATH =
-  'r1/IS-DEV/GOV/10005/Logreglan-Protected/RafraentOkuskirteini-v1'
-export const XROAD_DRIVING_LICENSE_V2_PATH =
-  'r1/IS-DEV/GOV/10005/Logreglan-Protected/RafraentOkuskirteini-v2'
+export const XROAD_ASSET_PATH = '/api/v1'
 
 const url = (path: string) => {
   return new URL(path, XROAD_BASE_PATH).toString()
 }
 
+export class MockAssetsXRoadService {
+  //constructor() 
+}
+
 export const requestHandlers = [
   rest.get(
-    url(`${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/embaetti`),
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(Juristictions))
-    },
-  ),
-
-  rest.get(
-    url(`${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/okukennarar`),
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(Teachers))
-    },
-  ),
-
-  rest.get(
-    url(`${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/:nationalId/all`),
-    (req, res, ctx) => {
-      const response =
-        req.params.nationalId === MOCK_NATIONAL_ID_EXPIRED
-          ? [ExpiredLicense]
-          : [ValidLicense]
-      return res(ctx.status(200), ctx.json(response))
-    },
-  ),
-  rest.get(
-    url(
-      `${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/hasteachingrights/:nationalId`,
-    ),
-    (req, res, ctx) => {
-      const hasTeachingRights =
-        req.params.nationalId === MOCK_NATIONAL_ID_TEACHER
-
-      return res(ctx.status(200), ctx.json(hasTeachingRights ? 1 : 0))
-    },
-  ),
-
-  rest.get(
-    url(
-      `${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/saekjaakstursmat/:nationalId`,
-    ),
-    (req, res, ctx) => {
-      const isFound = req.params.nationalId !== MOCK_NATIONAL_ID_NO_ASSESSMENT
-      if (isFound) {
-        return res(ctx.status(200), ctx.json(DrivingAssessment))
-      } else {
-        return res(ctx.status(404), ctx.text('error message from service'))
-      }
-    },
-  ),
-
-  rest.get(
-    url(
-      `${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/:nationalId/finishedokugerdi`,
-    ),
-    (req, res, ctx) => {
-      const isFound = req.params.nationalId !== MOCK_NATIONAL_ID_EXPIRED
-
-      return res(
-        ctx.status(200),
-        ctx.json(isFound ? FinishedSchool : NotFinishedSchool),
-      )
-    },
-  ),
-
-  rest.get(
-    url(
-      `${XROAD_DRIVING_LICENSE_V2_PATH}/api/okuskirteini/:nationalId/canapplyfor/B/full`,
-    ),
-    (req, res, ctx) => {
-      const canApply = req.params.nationalId === MOCK_NATIONAL_ID
-
-      return res(
-        ctx.status(200),
-        ctx.json(canApply ? CanApplyWithResultSuccess : CanApplyWithResultFail),
-      )
-    },
-  ),
-
-  rest.get(
-    url(
-      `${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/:nationalId/canapplyfor/temporary`,
-    ),
-    (req, res, ctx) => {
-      const canApply = req.params.nationalId === MOCK_NATIONAL_ID
-
-      return res(
-        ctx.status(200),
-        ctx.json(canApply ? CanApplyWithResultSuccess : CanApplyWithResultFail),
-      )
-    },
-  ),
-
-  rest.post(
-    url(`${XROAD_DRIVING_LICENSE_PATH}/api/okuskirteini/new/drivingassesment`),
-    (req, res, ctx) => {
-      const body = req.body as any
-      const isSubmittedByTeacher =
-        body?.kennitalaOkukennara === MOCK_NATIONAL_ID_TEACHER
-
-      if (isSubmittedByTeacher) {
-        return res(ctx.status(200), ctx.text(''))
-      } else {
-        return res(ctx.status(400), ctx.text('error message'))
-      }
-    },
-  ),
-
-  rest.post(
-    url(`${XROAD_DRIVING_LICENSE_V2_PATH}/api/okuskirteini/applications/new/B`),
-    (req, res, ctx) => {
-      const body = req.body as any
-      const canApply = body.personIdNumber !== MOCK_NATIONAL_ID_NO_ASSESSMENT
-
-      if (canApply) {
-        return res(ctx.status(200), ctx.text(''))
-      } else {
-        return res(ctx.status(400), ctx.text('error message'))
-      }
-    },
-  ),
-
-  rest.post(
-    url(
-      `${XROAD_DRIVING_LICENSE_V2_PATH}/api/okuskirteini/applications/new/temporary`,
-    ),
-    (req, res, ctx) => {
-      const body = req.body as any
-      const canApply = body.kennitala !== MOCK_NATIONAL_ID_NO_ASSESSMENT
-
-      if (canApply) {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            result: true,
-            okuskirteiniId: 1,
-            errorCode: null,
-          }),
-        )
-      } else {
-        return res(ctx.status(400), ctx.text('error message'))
-      }
-    },
-  ),
-
-  rest.get(
-    url(`${XROAD_DRIVING_LICENSE_V2_PATH}/api/okuskirteini/:nationalId`),
-    (req, res, ctx) => {
-      const isExpired = req.params.nationalId === MOCK_NATIONAL_ID_EXPIRED
-      return res(
-        ctx.status(isExpired ? 400 : 200),
-        ctx.json(
-          isExpired
-            ? { message: 'Ökuskírteini er ekki í gildi' }
-            : ValidLicense,
-        ),
-      )
-    },
-  ),
-
-  rest.get(
-    url(`${XROAD_DRIVING_LICENSE_PATH}/einstaklingar/:nationalId/buseta`),
-    (req, res, ctx) => {
-      const isExpired = req.params.nationalId === MOCK_NATIONAL_ID_EXPIRED
-      return res(
-        ctx.status(isExpired ? 400 : 200),
-        ctx.json(isExpired ? undefined : ResidenceHistory),
-      )
-    },
-  ),
+    url(`${XROAD_BASE_PATH}/fasteignir`),
+    (_req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({
+          propertyNumber: MOCK_ASSET_ID,
+          defaultAddress: {
+            displayShort:
+              'shortname',
+            display: 'longname',
+            propertyNumber:
+              MOCK_ASSET_ID,
+            municipality:
+              'municipality',
+            postNumber: 101,
+          },
+          registeredOwners: {
+            registeredOwners: [MOCK_USER],
+          },
+        })
+    )}
+  )
 ]
