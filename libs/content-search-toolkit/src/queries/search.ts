3acac1b52be6fb11887fc66b53813b6fab45ca34
Rafn Árnason <rafnarnason@gmail.com>
2022-11-03 12:17:14 +0000

    highlight b

--- libs/content-search-toolkit/src/queries/search.ts
+++ libs/content-search-toolkit/src/queries/search.ts
@@ -145,6 +145,16 @@ export const searchQuery = (
       },
     },
     ...(Object.keys(aggregation.aggs).length ? aggregation : {}), // spread aggregations if we have any
+    highlight : {
+      pre_tags : ["<b>"],
+      post_tags : ["</b>"],
+      number_of_fragments : 3,
+      fragment_size : 150,
+      fields : {
+        title : {},
+        content : {}
+      }
+    },
     size,
     from: (page - 1) * size, // if we have a page number add it as offset for pagination
   }
