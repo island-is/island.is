67461bd4abf0e7f6409f415e40b4985272634502
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2022-11-04 14:30:11 +0000

    fix: temporary fallbacks for build

--- libs/api/domains/license-service/src/lib/client/adr-license-client/genericAdrLicense.config.ts
+++ libs/api/domains/license-service/src/lib/client/adr-license-client/genericAdrLicense.config.ts
@@ -6,11 +6,11 @@ export const GenericAdrLicenseConfig = defineConfig<z.infer<typeof schema>>({
   name: 'GenericAdrLicenseConfig ',
   schema,
   load: (env) => ({
-    apiKey: env.required('VE_PKPASS_API_KEY'),
+    apiKey: env.required('VE_PKPASS_API_KEY', ''),
     apiUrl: env.required(
       'SMART_SOLUTIONS_API_URL',
       'https://smartpages-api-dev.smartsolutions.is/graphql',
     ),
-    passTemplateId: env.required('ADR_LICENSE_PASS_TEMPLATE_ID'),
+    passTemplateId: env.required('ADR_LICENSE_PASS_TEMPLATE_ID', ''),
   }),
 })
--- libs/api/domains/license-service/src/lib/client/machine-license-client/genericMachineLicense.config.ts
+++ libs/api/domains/license-service/src/lib/client/machine-license-client/genericMachineLicense.config.ts
@@ -7,12 +7,12 @@ export const GenericMachineLicenseConfig = defineConfig<z.infer<typeof schema>>(
     name: 'GenericMachineLicenseConfig ',
     schema,
     load: (env) => ({
-      apiKey: env.required('VE_PKPASS_API_KEY'),
+      apiKey: env.required('VE_PKPASS_API_KEY', ''),
       apiUrl: env.required(
         'SMART_SOLUTIONS_API_URL',
         'https://smartpages-api-dev.smartsolutions.is/graphql',
       ),
-      passTemplateId: env.required('MACHINE_LICENSE_PASS_TEMPLATE_ID'),
+      passTemplateId: env.required('MACHINE_LICENSE_PASS_TEMPLATE_ID', ''),
     }),
   },
 )
