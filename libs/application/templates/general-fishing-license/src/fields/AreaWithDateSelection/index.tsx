9990ca431361f031e6c9b6036b6277575eeb7ce9
Edda Steinunn <eddasteinunn@hotmail.com>
2022-11-07 09:11:16 +0000

    adds custom component with date that automatically is cleared when area changes

---
+++ libs/application/templates/general-fishing-license/src/fields/AreaWithDateSelection/index.tsx
@@ -0,0 +1,118 @@
+import React, { FC, useState } from 'react'
+import { getErrorViaPath, getValueViaPath } from '@island.is/application/core'
+import { FieldBaseProps } from '@island.is/application/types'
+import { useLocale } from '@island.is/localization'
+import { Box, Option } from '@island.is/island-ui/core'
+import {
+  DatePickerController,
+  SelectController,
+} from '@island.is/shared/form-fields'
+import { useFormContext } from 'react-hook-form'
+import { fishingLicenseFurtherInformation } from '../../lib/messages'
+
+const MOCKSELECTOPTIONS = [
+  {
+    key: 'A',
+    dateRestriction: {
+      dateFrom: '2022-10-20T00:00:00.000Z',
+      dateTo: '2022-11-30T00:00:00.000Z',
+    },
+    description: 'Veiðisvæði A 20.10 - 30.11',
+    disabled: false,
+    invalidOption: false,
+  },
+  {
+    key: 'B',
+    dateRestriction: {
+      dateFrom: '2022-11-20T00:00:00.000Z',
+      dateTo: '2022-12-30T00:00:00.000Z',
+    },
+    description: 'Veiðisvæði B 20.11 - 30.12',
+    disabled: false,
+    invalidOption: false,
+  },
+]
+
+export const AreaWithDateSelection: FC<FieldBaseProps> = ({
+  field,
+  application,
+  errors,
+}) => {
+  const { setValue } = useFormContext()
+  const datePickerId = `${field.id}.date`
+  const { formatMessage, lang } = useLocale()
+  const initialArea = getValueViaPath(
+    application.answers,
+    `${field.id}.area`,
+    '',
+  ) as string
+  const [selectedArea, setSelectedArea] = useState<string | number>(
+    initialArea || '',
+  )
+
+  // Constructs the lower limit for date picker
+  // Depending on which select option is currently selected
+  const getMinDate = () => {
+    const minDate = MOCKSELECTOPTIONS.find((o) => o.key === selectedArea)
+      ?.dateRestriction.dateFrom
+    if (minDate) {
+      return new Date(minDate)
+    }
+    return new Date()
+  }
+
+  // Constructs the upper limit for date picker
+  // Depending on which select option is currently selected
+  const getMaxDate = () => {
+    const maxDate = MOCKSELECTOPTIONS.find((o) => o.key === selectedArea)
+      ?.dateRestriction.dateTo
+    if (maxDate) {
+      return new Date(maxDate)
+    }
+    return null
+  }
+
+  // Resets datepicker when new area is selected due to
+  // it's date restrictions
+  const handleAreaSelect = (o: Option) => {
+    setSelectedArea(o.value)
+    setValue(datePickerId, null)
+  }
+
+  return (
+    <Box>
+      <Box marginTop={3}>
+        <SelectController
+          id={`${field.id}.area`}
+          name={`${field.id}.area`}
+          backgroundColor="blue"
+          label={formatMessage(fishingLicenseFurtherInformation.labels.area)}
+          placeholder={formatMessage(
+            fishingLicenseFurtherInformation.placeholders.area,
+          )}
+          onSelect={handleAreaSelect}
+          options={MOCKSELECTOPTIONS.map((o) => ({
+            label: o.description,
+            value: o.key,
+          }))}
+        />
+      </Box>
+      <Box marginTop={3}>
+        <DatePickerController
+          id={`${field.id}.date`}
+          name={`${field.id}.date`}
+          disabled={!selectedArea}
+          backgroundColor="blue"
+          locale={lang}
+          label={formatMessage(fishingLicenseFurtherInformation.labels.date)}
+          placeholder={formatMessage(
+            fishingLicenseFurtherInformation.placeholders.date,
+          )}
+          error={errors && getErrorViaPath(errors, `${field.id}.date`)}
+          minDate={getMinDate()}
+          maxDate={getMaxDate()}
+        />
+      </Box>
+    </Box>
+  )
+}
