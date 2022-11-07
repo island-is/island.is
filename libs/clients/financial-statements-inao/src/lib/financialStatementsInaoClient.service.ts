77a6f326a8f0b4144a1c1325c6a72c6a1aeebf05
Helgi Helgason <helgih@kosmosogkaos.is>
2022-11-07 14:06:11 +0000

    console

--- libs/clients/financial-statements-inao/src/lib/financialStatementsInaoClient.service.ts
+++ libs/clients/financial-statements-inao/src/lib/financialStatementsInaoClient.service.ts
@@ -296,7 +296,6 @@ export class FinancialStatementsInaoClientService {
     }
 
     const financialStatementId = await this.postFinancialStatement(body)
-    console.log({ financialStatementId })
     if (!financialStatementId) {
       throw new Error('FinancialStatementId can not be null')
     }
