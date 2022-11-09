8dba6b7322240ab21b6d695152f22428df10f90e
egill15 <egillb86@gmail.com>
2022-11-09 08:54:31 +0000

    fix

--- libs/application/templates/health-insurance/src/lib/dataSchema.ts
+++ libs/application/templates/health-insurance/src/lib/dataSchema.ts
@@ -1,18 +1,10 @@
+import { applicantInformationSchema } from '@island.is/application/ui-forms'
 import * as z from 'zod'
 import { NO, YES } from '../shared'
 
 export const HealthInsuranceSchema = z.object({
   approveExternalData: z.boolean().refine((v) => v),
-  applicant: z.object({
-    name: z.string(),
-    nationalId: z.string(),
-    address: z.string(),
-    postalCode: z.string(),
-    city: z.string(),
-    email: z.string().email(),
-    phoneNumber: z.string().optional(),
-    citizenship: z.string().optional(),
-  }),
+  applicant: applicantInformationSchema,
   children: z.string().min(1),
   hasAdditionalInfo: z.enum([YES, NO]),
   additionalRemarks: z.string().optional(),
