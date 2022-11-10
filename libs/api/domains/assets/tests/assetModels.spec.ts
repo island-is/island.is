6f249514a96a6a194c99e689e5229c4edc8c85a4
Kristófer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-11-10 12:47:26 +0000

    Add model tests

---
+++ libs/api/domains/assets/tests/assetModels.spec.ts
@@ -0,0 +1,19 @@
+import { Test } from '@nestjs/testing'
+import {
+  MOCK_ASSET_ID,
+  MOCK_USER,
+  requestHandlers,
+} from './__mock-data__/requestHandlers'
+import { startMocking } from '@island.is/shared/mocking'
+import { AssetsXRoadService } from '../src/lib/api-domains-assets.service'
+import { FasteignirApi } from '@island.is/clients/assets'
+import { PropertyDetail } from '../src'
+
+startMocking(requestHandlers)
+describe('Asset Models', () => {
+  describe.only('PropertyDetail model', () => {
+    it('User should be only see public data if not owner', async () => {
+      const property: PropertyDetail = {}
+    })
+  })
+})
