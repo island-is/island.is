74a59b82338342925ce46a29563fe9aa0937f171
Petar Shomov <petar@andes.is>
2022-11-14 22:24:55 +0000

    removing .only

--- apps/system-e2e/src/tests/web/smoke/search.spec.ts
+++ apps/system-e2e/src/tests/web/smoke/search.spec.ts
@@ -19,7 +19,7 @@ test.describe('Search feature', () => {
     await context.close()
   })
 
-  test.only('has expected sections', async () => {
+  test('has expected sections', async () => {
     const testPhrase = 'umsókn'
     const page = await context.newPage()
     await page.goto('/')
