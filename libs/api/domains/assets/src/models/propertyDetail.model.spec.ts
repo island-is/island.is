0b1139bd1ff932e0d6f23350b6e5cfc16c18961c
Kristófer <99652757+kristofer-lp8f@users.noreply.github.com>
2022-11-17 11:36:58 +0000

    Add no-owner check

--- libs/api/domains/assets/src/models/propertyDetail.model.spec.ts
+++ libs/api/domains/assets/src/models/propertyDetail.model.spec.ts
@@ -10,6 +10,9 @@ const MOCK_PROPERTY: PropertyDetail = {
     ],
   },
 }
+const MOCK_PROPERTY_SANS_OWNERS: PropertyDetail = {
+  registeredOwners: { registeredOwners: [] },
+}
 
 describe('Asset Models', () => {
   describe.only('PropertyDetail model', () => {
@@ -21,10 +24,20 @@ describe('Asset Models', () => {
       const context = { source: property } as MiddlewareContext
       expect(isPropertyOwner(context)).toBeTruthy()
     })
+
     it('Non-owner should not be allowed to access data', async () => {
       const property: PropertyDetail & { nationalId: string } = {
         nationalId: '4321',
         ...MOCK_PROPERTY,
+      }
+      const context = { source: property } as MiddlewareContext
+      expect(isPropertyOwner(context)).toBeFalsy()
+    })
+
+    it('User should not be allowed to access property with no owners', async () => {
+      const property: PropertyDetail & { nationalId: string } = {
+        nationalId: '1234',
+        ...MOCK_PROPERTY_SANS_OWNERS,
       }
       const context = { source: property } as MiddlewareContext
       expect(isPropertyOwner(context)).toBeFalsy()
