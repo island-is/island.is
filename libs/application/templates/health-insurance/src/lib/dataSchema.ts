fc2191228ca0892e0efdb7042508364ba86b1751
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2022-11-17 11:05:17 +0000

    fixes to health insurance

--- libs/application/templates/health-insurance/src/lib/dataSchema.ts
+++ libs/application/templates/health-insurance/src/lib/dataSchema.ts
@@ -4,9 +4,18 @@ import { NO, YES } from '../shared'
 
 export const HealthInsuranceSchema = z.object({
   approveExternalData: z.boolean().refine((v) => v),
+  status: z.object({
+    type: z.enum(['employed', 'student', 'pensioner', 'other']),
+  }),
   applicant: applicantInformationSchema,
-  citizenship: z.string().optional(),
-  children: z.string().min(1),
+  formerInsurance: z.object({
+    registration: z.enum([YES, NO]),
+    country: z.string().min(1),
+    personalId: z.string().min(1),
+    institution: z.string().min(1),
+    entitlementReason: z.string().optional(),
+  }),
+  children: z.enum([YES, NO]),
   hasAdditionalInfo: z.enum([YES, NO]),
   additionalRemarks: z.string().optional(),
   confirmCorrectInfo: z.boolean().refine((v) => v),
