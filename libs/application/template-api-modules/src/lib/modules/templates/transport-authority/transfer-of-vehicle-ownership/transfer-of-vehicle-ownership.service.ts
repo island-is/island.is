aeb7969895aba9901c2a5346f550d983824a6a2e
johannaagma <johannaagma@gmail.com>
2022-11-21 21:12:39 +0000

    Disable payment validation for now

--- libs/application/template-api-modules/src/lib/modules/templates/transport-authority/transfer-of-vehicle-ownership/transfer-of-vehicle-ownership.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/transport-authority/transfer-of-vehicle-ownership/transfer-of-vehicle-ownership.service.ts
@@ -302,13 +302,34 @@ export class TransferOfVehicleOwnershipService {
     application,
     auth,
   }: TemplateApiModuleActionProps): Promise<void> {
+    // 1. Validate payment
+
+    // 1a. Make sure a paymentUrl was created
+    const { paymentUrl } = application.externalData.createCharge.data as {
+      paymentUrl: string
+    }
+    if (!paymentUrl) {
+      throw new Error(
+        'Ekki er búið að staðfesta greiðslu, hinkraðu þar til greiðslan er staðfest.',
+      )
+    }
+
+    // 1b. Make sure payment is fulfilled (has been paid)
+    const payment:
+      | { fulfilled: boolean }
+      | undefined = await this.sharedTemplateAPIService.getPaymentStatus(
+      auth.authorization,
+      application.id,
+    )
+    if (!payment?.fulfilled) {
+      throw new Error(
+        'Ekki er búið að staðfesta greiðslu, hinkraðu þar til greiðslan er staðfest.',
+      )
+    }
+
+    // 2. Submit the application
+
     const answers = application.answers as TransferOfVehicleOwnershipAnswers
-    const buyerCoOwners = answers.buyerCoOwnerAndOperator?.filter(
-      (x) => x.type === 'coOwner',
-    )
-    const buyerOperators = answers.buyerCoOwnerAndOperator?.filter(
-      (x) => x.type === 'operator',
-    )
 
     // Note: Need to be sure that the user that created the application is the seller when submitting application to SGS
     if (answers?.seller?.nationalId !== application.applicant) {
@@ -317,7 +338,13 @@ export class TransferOfVehicleOwnershipService {
       )
     }
 
-    // 1. Submit the application
+    const buyerCoOwners = answers.buyerCoOwnerAndOperator?.filter(
+      (x) => x.type === 'coOwner',
+    )
+    const buyerOperators = answers.buyerCoOwnerAndOperator?.filter(
+      (x) => x.type === 'operator',
+    )
+
     await this.transferOfVehicleOwnershipApi.saveOwnerChange(auth, {
       permno: answers?.vehicle?.plate,
       seller: {
@@ -347,12 +374,12 @@ export class TransferOfVehicleOwnershipService {
       })),
     })
 
-    // 2. Notify everyone in the process that the application has successfully been submitted
+    // 3. Notify everyone in the process that the application has successfully been submitted
 
-    // 2a. Get list of users that need to be notified
+    // 3a. Get list of users that need to be notified
     const recipientList = getRecipients(answers, getAllRoles())
 
-    // 2b. Send email/sms individually to each recipient about success of submitting application
+    // 3b. Send email/sms individually to each recipient about success of submitting application
     for (let i = 0; i < recipientList.length; i++) {
       if (recipientList[i].email) {
         await this.sharedTemplateAPIService.sendEmail(
