d734ff0e02521bab34eb59bd8742e0ec45f7b48b
Petar Shomov <pshomov@gmail.com>
2022-11-22 15:32:48 +0000

    temporarily adding extra logging to extended fetch

--- libs/clients/middlewares/src/lib/withErrorLog.ts
+++ libs/clients/middlewares/src/lib/withErrorLog.ts
@@ -1,4 +1,6 @@
 import { Logger } from '@island.is/logging'
+// eslint-disable-next-line no-restricted-imports
+import { pick } from 'lodash'
 
 import { FetchMiddlewareOptions, MiddlewareAPI } from './nodeFetch'
 import { FetchError } from './FetchError'
@@ -16,28 +18,51 @@ export function withErrorLog({
   logger,
 }: ErrorLogOptions): MiddlewareAPI {
   return (request) => {
-    return fetch(request).catch((error: Error) => {
-      const logLevel =
-        error instanceof FetchError &&
-        error.status < 500 &&
-        !treat400ResponsesAsErrors
-          ? 'warn'
-          : 'error'
-      const cacheStatus =
-        (error instanceof FetchError &&
-          error.response.headers.get('cache-status')) ??
-        undefined
-
-      logger.log(logLevel, {
-        ...error,
-        stack: error.stack,
-        url: request.url,
-        message: `Fetch failure (${name}): ${error.message}`,
-        cacheStatus,
-        // Do not log large response objects.
-        response: undefined,
+    logger.debug(
+      `Extended fetch request. ${JSON.stringify(
+        pick(request, [
+          'url',
+          'agent',
+          'json',
+          'headers',
+          'auth',
+          'method',
+          'timeout',
+          'body',
+        ]),
+      )}`,
+    )
+    return fetch(request)
+      .then((r) => {
+        logger.debug(
+          `Extended fetch response. ${JSON.stringify(
+            pick(r, ['status', 'statusText', 'body', 'headers', 'url']),
+          )}`,
+        )
+        return r
       })
-      throw error
-    })
+      .catch((error: Error) => {
+        const logLevel =
+          error instanceof FetchError &&
+          error.status < 500 &&
+          !treat400ResponsesAsErrors
+            ? 'warn'
+            : 'error'
+        const cacheStatus =
+          (error instanceof FetchError &&
+            error.response.headers.get('cache-status')) ??
+          undefined
+
+        logger.log(logLevel, {
+          ...error,
+          stack: error.stack,
+          url: request.url,
+          message: `Fetch failure (${name}): ${error.message}`,
+          cacheStatus,
+          // Do not log large response objects.
+          response: undefined,
+        })
+        throw error
+      })
   }
 }
