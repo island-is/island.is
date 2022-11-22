32b7882089cc5dd7b6c06f2f34bebc434822300e
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2022-11-22 10:15:08 +0000

    feat(web): Organization landing page changes (#9100)
    * Only use fields from organization instead of organization page when the organization is toggled as a landing page
    
    * Change og:title
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/web/screens/Organization/Home/LandingPage/index.ts
+++ apps/web/screens/Organization/Home/LandingPage/index.ts
@@ -2,3 +2,6 @@ import dynamic from 'next/dynamic'
 export const LandingPageFooter = dynamic(() => import('./LandingPageFooter'), {
   ssr: false,
 })
+export const LandingPage = dynamic(() => import('./LandingPage'), {
+  ssr: false,
+})
