5c4c18c767fa0d4c95f98e6d33327cd5408a6fd9
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-11-23 14:10:11 +0000

    Remove old CF strings

---
+++ apps/judicial-system/web/src/routes/Court/RestrictionCase/Confirmation/Confirmation.strings.ts
@@ -0,0 +1,48 @@
+import { defineMessages } from 'react-intl'
+
+export const confirmation = defineMessages({
+  title: {
+    id: 'judicial.system.court:confirmation.title',
+    defaultMessage: 'Yfirlit úrskurðar',
+    description:
+      'Notaður sem titill á úrskurðar skrefi í gæsluvarðhalds- og farbannsmálum.',
+  },
+  conclusionTitle: {
+    id: 'judicial.system.core:confirmation.conclusion_title',
+    defaultMessage: 'Úrskurðarorð',
+    description:
+      'Notaður sem titill fyrir "Úrskurðarorð" hlutanum á úrskurðar skrefi í gæsluvarðhalds- og farbannsmálum.',
+  },
+  continueButtonTextAccepting: {
+    id: 'judicial.system.core:confirmation.continue_button_text_accepting',
+    defaultMessage: 'Samþykkja kröfu og undirrita úrskurð',
+    description:
+      'Notaður sem texti í "áfram" takkanum á staðfesingar skrefi í gæslu- og farbannsmálum þegar verið er að samþykkja kröfu',
+  },
+  continueButtonTextAcceptingPartially: {
+    id:
+      'judicial.system.core:confirmation.continue_button_text_accepting_partially',
+    defaultMessage: 'Staðfesta og undirrita úrskurð',
+    description:
+      'Notaður sem texti í "áfram" takkanum á staðfesingar skrefi í gæslu- og farbannsmálum þegar verið er að samþykkja kröfu að hluta',
+  },
+  continueButtonTextRejecting: {
+    id: 'judicial.system.core:confirmation.continue_button_text_rejecting',
+    defaultMessage: 'Hafna kröfu og undirrita úrskurð',
+    description:
+      'Notaður sem texti í "áfram" takkanum á staðfesingar skrefi í gæslu- og farbannsmálum þegar verið er að hafna kröfu',
+  },
+  continueButtonTextDismissing: {
+    id: 'judicial.system.core:confirmation.continue_button_text_dismissing',
+    defaultMessage: 'Vísa kröfu frá og undirrita úrskurð',
+    description:
+      'Notaður sem texti í "áfram" takkanum á staðfesingar skrefi í gæslu- og farbannsmálum þegar verið er að vísa kröfu frá',
+  },
+  continueButtonTextAcceptingAlternativeTravelBan: {
+    id:
+      'judicial.system.core:confirmation.continue_button_text_accepting_alternative_travel_ban',
+    defaultMessage: 'Staðfesta farbann og undirrita úrskurð',
+    description:
+      'Notaður sem texti í "áfram" takkanum á staðfesingar skrefi í gæslu- og farbannsmálum þegar verið er að hafna kröfu en úrskurða í farbann',
+  },
+})
