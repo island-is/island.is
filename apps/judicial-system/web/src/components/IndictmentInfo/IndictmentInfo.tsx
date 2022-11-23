05118472bbbf3487447095aa369b95d4f42babf2
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-11-23 13:32:21 +0000

    Created IndictmentInfo component

---
+++ apps/judicial-system/web/src/components/IndictmentInfo/IndictmentInfo.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useIntl } from 'react-intl'
+
+import { Box, Text } from '@island.is/island-ui/core'
+import {
+  CrimeSceneMap,
+  IndictmentSubtypeMap,
+} from '@island.is/judicial-system/types'
+
+import { indictmentInfo as strings } from './IndictmentInfo.strings'
+import {
+  capitalize,
+  formatDate,
+  indictmentSubtypes,
+} from '@island.is/judicial-system/formatters'
+
+interface Props {
+  policeCaseNumber: string
+  subtypes?: IndictmentSubtypeMap
+  crimeScenes?: CrimeSceneMap
+}
+
+const IndictmentInfo: React.FC<Props> = (props) => {
+  const { policeCaseNumber, subtypes, crimeScenes } = props
+  const { formatMessage } = useIntl()
+
+  if (!subtypes || !crimeScenes) {
+    return null
+  }
+
+  return (
+    <>
+      <Box>
+        <Text variant="small">
+          {formatMessage(strings.subtypes, {
+            subtypes: capitalize(
+              subtypes[policeCaseNumber]
+                ?.map((subtype) => indictmentSubtypes[subtype])
+                .join(', ') || '-',
+            ),
+          })}
+        </Text>
+      </Box>
+      <Text variant="small">
+        {formatMessage(strings.dateAndPlace, {
+          place: crimeScenes[policeCaseNumber]?.place || '-',
+          date: crimeScenes[policeCaseNumber]?.date
+            ? formatDate(crimeScenes[policeCaseNumber]?.date, 'PPPP')
+            : '-',
+        })}
+      </Text>
+    </>
+  )
+}
+
+export default IndictmentInfo
