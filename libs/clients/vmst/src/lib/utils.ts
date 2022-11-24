c894cc7021c497df50986c5aa73a57c67e10e1bf
Quan Dong <6514088+qdd1@users.noreply.github.com>
2022-11-24 13:09:12 +0000

    fix(parental-leave): show less PII in logs

--- libs/clients/vmst/src/lib/utils.ts
+++ libs/clients/vmst/src/lib/utils.ts
@@ -8,18 +8,35 @@ export const createWrappedFetchWithLogging = (
   return new Promise((resolve, reject) => {
     fetch(input, init)
       .then(async (response) => {
+        const body = init?.body ? JSON.parse(init?.body as string) : {}
+
+        const newInit = {
+          ...init,
+          body: {
+            ...body,
+            applicant: 'hidden',
+            otherParentId: '' ?? 'hidden',
+            email: 'hidden',
+            phoneNumber: 'hidden',
+            paymentInfo: {
+              ...body?.paymentInfo,
+              bankAccount: 'hidden',
+            },
+            attachments: body?.attachments?.length,
+          },
+        }
         if (response.ok) {
           logger.info(
             `vmst-module.success: input - ${JSON.stringify(
               input,
-            )}, init - ${JSON.stringify(init)}`,
+            )}, init - ${JSON.stringify(newInit)}`,
           )
         } else {
           const body = await response.json()
           logger.error(
             `vmst-module.error: input - ${JSON.stringify(
               input,
-            )}, init - ${JSON.stringify(init)}, response - ${JSON.stringify(
+            )}, init - ${JSON.stringify(newInit)}, response - ${JSON.stringify(
               body,
             )} status text: ${response.statusText}`,
           )
