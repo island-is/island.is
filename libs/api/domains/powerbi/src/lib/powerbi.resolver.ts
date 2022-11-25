021217014e320ce6da80b356c8a7b86241049b37
runarvestmann <runar@stefna.is>
2022-11-25 10:31:22 +0000

    Add logging

--- libs/api/domains/powerbi/src/lib/powerbi.resolver.ts
+++ libs/api/domains/powerbi/src/lib/powerbi.resolver.ts
@@ -1,5 +1,8 @@
 import { Configuration, ConfidentialClientApplication } from '@azure/msal-node'
+import { Inject } from '@nestjs/common'
 import { Args, Query, Resolver } from '@nestjs/graphql'
+import type { Logger } from '@island.is/logging'
+import { LOGGER_PROVIDER } from '@island.is/logging'
 import { PowerBiEmbedTokenInput } from './dto/powerbiEmbedToken.input'
 import { PowerBiEmbedTokenResponse } from './models/powerbiEmbedTokenResponse'
 
@@ -7,6 +10,11 @@ type Owner = 'Fiskistofa'
 
 @Resolver()
 export class PowerBiResolver {
+  constructor(
+    @Inject(LOGGER_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
+
   private async getAccessToken(owner: Owner) {
     let clientId = ''
     let clientSecret = ''
@@ -21,7 +29,9 @@ export class PowerBiResolver {
         }
         break
       default: {
-        // TODO: Log that the owner didn't match anyone
+        this.logger.info(
+          'User requested embed token without providing an owner',
+        )
         return null
       }
     }
@@ -44,7 +54,37 @@ export class PowerBiResolver {
       )
       return tokenResponse?.accessToken
     } catch (err) {
-      // TODO: log the error
+      this.logger.error('Error occurred when trying to fetch accessToken', err)
+      return null
+    }
+  }
+
+  private async getReport(
+    workspaceId: string,
+    reportId: string,
+    accessToken: string,
+  ) {
+    const url = `https://api.powerbi.com/v1.0/myorg/groups/${workspaceId}/reports/${reportId}`
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      if (!response.ok) {
+        this.logger.error('Report response was not 2xx')
+        return null
+      }
+      const report = await response.json()
+      return report
+    } catch (err) {
+      this.logger.error(`Error occurred when trying to fetch report`, {
+        workspaceId,
+        reportId,
+        err,
+      })
       return null
     }
   }
@@ -55,43 +95,39 @@ export class PowerBiResolver {
     targetWorkspaceId: string,
     accessToken: string,
   ) {
-    // Add report id in the request
     const formData = {
       reports: [
         {
           id: reportId,
         },
       ],
-      datasets: [] as { id: string }[],
+      datasets: datasetIds.map((datasetId) => ({ id: datasetId })),
       targetWorkspaces: [{ id: targetWorkspaceId }],
-    }
-
-    // Add dataset ids in the request
-    for (const datasetId of datasetIds) {
-      formData.datasets.push({
-        id: datasetId,
-      })
     }
 
     const embedTokenApiUrl = 'https://api.powerbi.com/v1.0/myorg/GenerateToken'
 
-    // Generate Embed token for single report, workspace, and multiple datasets. Refer https://aka.ms/MultiResourceEmbedToken
-    const response = await fetch(embedTokenApiUrl, {
-      method: 'POST',
-      body: JSON.stringify(formData),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
+    try {
+      // Generate Embed token for single report, workspace, and multiple datasets. Refer to: https://aka.ms/MultiResourceEmbedToken
+      const response = await fetch(embedTokenApiUrl, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
 
-    if (!response.ok) {
-      // TODO: log that the embed token could not be fetched
+      if (!response.ok) {
+        this.logger.error('Embed token response was not 2xx')
+        return null
+      }
+      const data = await response.json()
+      return data?.token
+    } catch (err) {
+      this.logger.error('Error occurred while fetching embed token', err)
       return null
     }
-    const data = await response.json()
-
-    return data.token
   }
 
   @Query(() => PowerBiEmbedTokenResponse)
@@ -105,32 +141,29 @@ export class PowerBiResolver {
       }
     }
 
-    const url = `https://api.powerbi.com/v1.0/myorg/groups/${input.workspaceId}/reports/${input.reportId}`
+    const report = await this.getReport(
+      input.workspaceId,
+      input.reportId,
+      accessToken,
+    )
 
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-
-    if (!response.ok) {
-      return { token: null, embedUrl: null }
+    if (!report) {
+      return {
+        token: null,
+        embedUrl: null,
+      }
     }
-
-    const result = await response.json()
 
     const embedToken = await this.getEmbedToken(
       input.reportId,
-      [result.datasetId],
+      [report?.datasetId],
       input.workspaceId,
       accessToken,
     )
 
     return {
       token: embedToken,
-      embedUrl: result.embedUrl,
+      embedUrl: report?.embedUrl,
     }
   }
 }
