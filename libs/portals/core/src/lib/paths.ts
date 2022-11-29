4b52d2846c36faca65805c1cf2aa89b997cb5fe1
Snær Seljan Þóroddsson <snaer@aranja.com>
2022-11-29 11:23:47 +0000

    update portal admin paths

--- libs/portals/core/src/lib/paths.ts
+++ libs/portals/core/src/lib/paths.ts
@@ -2,8 +2,4 @@ export enum PortalPaths {
   Root = '/',
   SignInOidc = '/signin-oidc',
   SilentSignInOidc = '/silent/signin-oidc',
-
-  // Electronic Documents
-  ElectronicDocumentsRoot = '/postholf',
-  ElectronicDocumentsFileDownload = '/postholf/:id',
 }
