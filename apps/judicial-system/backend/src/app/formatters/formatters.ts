bce8a8ef530fc3f8605edb91849a086ac34722ea
Ívar Oddsson <oddsson@ivars-imac.lan>
2022-11-29 12:46:27 +0000

    Remove unused imports

--- apps/judicial-system/backend/src/app/formatters/formatters.ts
+++ apps/judicial-system/backend/src/app/formatters/formatters.ts
@@ -5,7 +5,6 @@ import {
   formatDate,
   formatNationalId,
   getSupportedCaseCustodyRestrictions,
-  indictmentSubtypes,
   laws,
 } from '@island.is/judicial-system/formatters'
 
@@ -14,7 +13,6 @@ import {
   CaseCustodyRestrictions,
   CaseLegalProvisions,
   CaseType,
-  IndictmentSubtype,
   isIndictmentCase,
   isInvestigationCase,
   isRestrictionCase,
