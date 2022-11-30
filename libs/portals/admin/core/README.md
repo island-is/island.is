05c48d454f15f9c3e0b3914651219694a9955975
Snær Seljan Þóroddsson <snaer@aranja.com>
2022-11-30 20:14:22 +0000

    remove admin-core library since it was not being used after refactor

--- libs/portals/admin/core/README.md
+++
@@ -1,9 +0,0 @@
-<!-- gitbook-navigation: "Admin Core" -->
-
-# Portals Admin Core
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test portals-admin-core` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/portals/admin/core/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'portals-admin-core',
-  preset: '../../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/portals/admin/core',
-}
