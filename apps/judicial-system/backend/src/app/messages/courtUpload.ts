1be5776178489911b8238de0a50b94768e1d29ae
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-12-01 17:12:00 +0000

    Reintroduces missing Contentful string

--- apps/judicial-system/backend/src/app/messages/courtUpload.ts
+++ apps/judicial-system/backend/src/app/messages/courtUpload.ts
@@ -6,6 +6,11 @@ export const courtUpload = defineMessages({
     defaultMessage: 'Krafa um {caseType}{date}',
     description: 'Notaður sem nafn á kröfuskjali í Auði.',
   },
+  caseFilesRecord: {
+    id: 'judicial.system.backend:court_upload.case_files_record',
+    defaultMessage: 'Skjalaskrá {policeCaseNumber}',
+    description: 'Notaður sem nafn á skjalaskrá í Auði.',
+  },
   courtRecord: {
     id: 'judicial.system.backend:court_upload.court_record',
     defaultMessage: 'Þingbók {courtCaseNumber}',
