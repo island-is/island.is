bc8da8f6a3cbff00b489e55b3889bd3d0dd34f0f
qdd1 <6514088+qdd1@users.noreply.github.com>
2022-12-06 12:54:52 +0000

    fix(parental-leave): Move VMST_ID from secrets to env (#9268)

--- apps/application-system/form/infra/application-system-form.ts
+++ apps/application-system/form/infra/application-system-form.ts
@@ -7,6 +7,13 @@ export const serviceSetup = (_services: {}): ServiceBuilder<'application-system-
     .readiness('/readiness')
     .env({
       BASEPATH: '/umsoknir',
+      VMST_ID: '/k8s/application-system/VMST_ID',
+      SI_PUBLIC_GRAPHQL_PATH: {
+        dev: '',
+        prod: '',
+        staging: '',
+        local: ref((h) => `http://${h.svc(services.api)}`),
+      },
       SI_PUBLIC_IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
         staging: 'https://identity-server.staging01.devland.is',
@@ -18,7 +25,6 @@ export const serviceSetup = (_services: {}): ServiceBuilder<'application-system-
       SI_PUBLIC_CONFIGCAT_SDK_KEY: '/k8s/configcat/CONFIGCAT_SDK_KEY',
       SI_PUBLIC_DD_RUM_APPLICATION_ID: '/k8s/DD_RUM_APPLICATION_ID',
       SI_PUBLIC_DD_RUM_CLIENT_TOKEN: '/k8s/DD_RUM_CLIENT_TOKEN',
-      VMST_ID: '/k8s/application-system/VMST_ID',
     })
     .ingress({
       primary: {
