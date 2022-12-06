381c34e883443af577178863738f743afa212234
Guðjón Guðjónsson <gudjon@kolibri.is>
2022-12-06 12:38:33 +0000

    feat(j-s): New User Role Assistant (#9267)
    * Adds a new role assistant
    
    * Modifies user seed
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/backend/src/app/modules/case/test/caseController/getAllRolesRules.spec.ts
+++ apps/judicial-system/backend/src/app/modules/case/test/caseController/getAllRolesRules.spec.ts
@@ -3,6 +3,7 @@ import {
   prosecutorRule,
   registrarRule,
   representativeRule,
+  assistantRule,
   staffRule,
 } from '../../../../guards'
 import { CaseController } from '../../case.controller'
@@ -15,15 +16,16 @@ describe('CaseController - Get all rules', () => {
     rules = Reflect.getMetadata('roles-rules', CaseController.prototype.getAll)
   })
 
-  it('should give permission to five roles', () => {
-    expect(rules).toHaveLength(5)
+  it('should give permission to six roles', () => {
+    expect(rules).toHaveLength(6)
   })
 
-  it('should give permission to prosecutors, representatives, judges, registrars and staff', () => {
+  it('should give permission to prosecutors, representatives, judges, registrars, assistants and staff', () => {
     expect(rules).toContain(prosecutorRule)
     expect(rules).toContain(representativeRule)
     expect(rules).toContain(judgeRule)
     expect(rules).toContain(registrarRule)
+    expect(rules).toContain(assistantRule)
     expect(rules).toContain(staffRule)
   })
 })
