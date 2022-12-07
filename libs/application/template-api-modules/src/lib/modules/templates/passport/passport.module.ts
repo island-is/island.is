a94d06ecd6f50807308b36674722f4b418c9c704
Alexandra Diljá <alexandrab18@ru.is>
2022-12-07 17:21:57 +0000

    passports submit

--- libs/application/template-api-modules/src/lib/modules/templates/passport/passport.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/passport/passport.module.ts
@@ -2,12 +2,13 @@ import { DynamicModule } from '@nestjs/common'
 import { PassportService } from './passport.service'
 import { SharedTemplateAPIModule } from '../../shared'
 import { BaseTemplateAPIModuleConfig } from '../../../types'
+import { PassportsClientModule } from '@island.is/clients/passports'
 
 export class PassportModule {
   static register(config: BaseTemplateAPIModuleConfig): DynamicModule {
     return {
       module: PassportModule,
-      imports: [SharedTemplateAPIModule.register(config)],
+      imports: [SharedTemplateAPIModule.register(config), PassportsClientModule],
       providers: [PassportService],
       exports: [PassportService],
     }
