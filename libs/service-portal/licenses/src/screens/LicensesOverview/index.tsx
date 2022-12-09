f32b8d0206e48aac083f6510e1aaeaebc9344ac4
Þórður H <thordur@hugsmidjan.is>
2022-12-09 09:38:03 +0000

    feat(service-portal): Passport service + passport view (#8160)
    * Add passport service
    
    * Update passport view and service
    
    * Format dates
    
    * Gitbook
    
    * Add invalid status
    
    * Clean up action card
    
    * Minor cleanup
    
    * Add passport detail to masternavigation
    
    * Match license look
    
    * Minor refactoring
    
    * Remove conditional tag for secondary tag
    
    * Update passport.service.ts
    
    * fix: update messages
    
    * fix: update error screen
    
    * Add number with type in service. Match license buttons
    
    * feat: add date tags like passports to generic licenses
    
    * fix: cardviw placement
    
    * Update clientconfig after service changes
    
    * Passport logging
    
    * Update passport service. Add child view.
    
    * Update passport detail
    
    * Minor refactor
    
    * display child name on card overview
    
    * Add placeholder for applying passport card
    
    * Add passport page feature-flag
    
    * Add passport logo constant
    
    * No passport placeholder in vie
    
    * Update passport styling
    
    * Minor fix
    
    * subtitle string fix
    
    * update error logging in passport service
    
    Co-authored-by: Ásdís Erna Guðmundsdóttir <disa.erna@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/service-portal/licenses/src/screens/LicensesOverview/index.tsx
+++ libs/service-portal/licenses/src/screens/LicensesOverview/index.tsx
@@ -6,9 +6,6 @@ import {
   IntroHeader,
   ServicePortalModuleComponent,
   m as coreMessage,
-  ActionCard,
-  EmptyState,
-  CardLoader,
 } from '@island.is/service-portal/core'
 import { m } from '../../lib/messages'
 import { gql, useQuery } from '@apollo/client'
@@ -16,19 +13,18 @@ import { Locale } from '@island.is/shared/types'
 import {
   GenericLicenseType,
   GenericUserLicenseFetchStatus,
+  useChildrenPassport,
   useUserProfile,
 } from '@island.is/service-portal/graphql'
 import { Query } from '@island.is/api/schema'
-import { Box } from '@island.is/island-ui/core'
-import { ServicePortalPath } from '@island.is/service-portal/core'
-import {
-  getPathFromProviderId,
-  getPathFromType,
-  getTitleAndLogo,
-} from '../../utils/dataMapper'
+import { Box, Tabs } from '@island.is/island-ui/core'
 
 import { useFeatureFlagClient } from '@island.is/react/feature-flags'
 import { FeatureFlagClient } from '@island.is/feature-flags'
+import { usePassport } from '@island.is/service-portal/graphql'
+import UserLicenses from './UserLicenses'
+import ChildrenLicenses from './ChildrenLicenses'
+
 const dataFragment = gql`
   fragment genericLicenseDataFieldFragment on GenericLicenseDataField {
     type
@@ -77,6 +73,7 @@ const GenericLicensesQuery = gql`
         metadata {
           licenseNumber
           expired
+          expireDate
         }
       }
     }
@@ -89,7 +86,6 @@ export const LicensesOverview: ServicePortalModuleComponent = () => {
   const { formatMessage } = useLocale()
   const { data: userProfile } = useUserProfile()
   const locale = (userProfile?.locale as Locale) ?? 'is'
-
   /**
    * Get all licenses is feature flagged
    * If off, all licenses fetched, if on only drivers license is fetched
@@ -99,6 +95,8 @@ export const LicensesOverview: ServicePortalModuleComponent = () => {
   const [licenseTypes, setLicenseTypes] = useState<Array<GenericLicenseType>>([
     GenericLicenseType.DriversLicense,
   ])
+  const [passportEnabled, setPassportEnabled] = useState(false)
+
   useEffect(() => {
     const isFlagEnabled = async () => {
       const ffEnabled = await featureFlagClient.getValue(
@@ -115,6 +113,18 @@ export const LicensesOverview: ServicePortalModuleComponent = () => {
       }
     }
     isFlagEnabled()
+
+    const isPassportFlagEnabled = async () => {
+      const isPassEnabled = Boolean(
+        await featureFlagClient.getValue(
+          `isServicePortalPassportPageEnabled`,
+          false,
+        ),
+      )
+
+      setPassportEnabled(isPassEnabled)
+    }
+    isPassportFlagEnabled()
   }, [])
 
   const { data, loading, error } = useQuery<Query>(GenericLicensesQuery, {
@@ -126,14 +136,28 @@ export const LicensesOverview: ServicePortalModuleComponent = () => {
     },
   })
   const { genericLicenses = [] } = data ?? {}
+  const {
+    data: passportData,
+    loading: passportLoading,
+    error: passportError,
+  } = usePassport()
+
+  const { data: childrenData, loading: childrenLoading } = useChildrenPassport()
+
+  const isLoading = loading || passportLoading
+  const isGenericLicenseEmpty = genericLicenses.every(
+    (item) => item.payload === null,
+  )
+  const hasData = !!(!isGenericLicenseEmpty || passportData)
 
   const isError = genericLicenses?.every(
     (item) => item.fetch.status === GenericUserLicenseFetchStatus.Error,
   )
+  const hasError = !!(error || isError) && !!passportError
 
-  const isEmpty = genericLicenses.every((item) => item.payload === null)
+  const hasChildren = !!childrenData?.length
 
-  if ((error || isError) && !loading) {
+  if (hasError && !loading) {
     return (
       <ErrorScreen
         figure="./assets/images/hourglass.svg"
@@ -148,65 +172,51 @@ export const LicensesOverview: ServicePortalModuleComponent = () => {
   }
   return (
     <>
-      <Box marginBottom={[3, 4, 5]}>
-        <IntroHeader
-          title={defineMessage(m.title)}
-          intro={defineMessage(m.intro)}
-        />
-      </Box>
-      {loading && <CardLoader />}
-      {data &&
-        !isEmpty &&
-        genericLicenses
-          .filter((license) => license.license.status === 'HasLicense')
-          .map((license, index) => {
-            return (
-              <Box marginBottom={3} key={index}>
-                <ActionCard
-                  image={{
-                    type: 'image',
-                    url: getTitleAndLogo(license.license.type).logo,
-                  }}
-                  text={
-                    formatMessage(m.licenseNumber) +
-                    ': ' +
-                    license.payload?.metadata?.licenseNumber
-                  }
-                  heading={formatMessage(
-                    getTitleAndLogo(license.license.type).title,
-                  )}
-                  cta={{
-                    label: formatMessage(m.seeDetails),
-                    url: ServicePortalPath.LicensesDetail.replace(
-                      ':provider',
-                      getPathFromProviderId(license.license.provider.id),
-                    ).replace(':type', getPathFromType(license.license.type)),
-                    variant: 'text',
-                  }}
-                  tag={
-                    license.payload?.metadata?.expired === true
-                      ? {
-                          label: formatMessage(m.isExpired),
-                          variant: 'red',
-                          outlined: false,
-                        }
-                      : license.payload?.metadata?.expired === false
-                      ? {
-                          label: formatMessage(m.isValid),
-                          variant: 'blue',
-                          outlined: false,
-                        }
-                      : undefined
-                  }
-                />
-              </Box>
-            )
-          })}
-
-      {!loading && !error && isEmpty && (
-        <Box marginTop={8}>
-          <EmptyState />
+      <IntroHeader
+        title={defineMessage(m.title)}
+        intro={defineMessage(m.intro)}
+        marginBottom={1}
+      />
+      {hasChildren && passportEnabled ? (
+        <Box>
+          <Tabs
+            label="License tabs"
+            contentBackground="white"
+            tabs={[
+              {
+                label: formatMessage(m.licenseTabPrimary),
+                content: (
+                  <UserLicenses
+                    isLoading={isLoading}
+                    hasData={hasData}
+                    hasError={hasError}
+                    isGenericLicenseEmpty={isGenericLicenseEmpty}
+                    passportData={passportData}
+                    genericLicenses={genericLicenses}
+                  />
+                ),
+              },
+              {
+                label: formatMessage(m.licenseTabSecondary),
+                content: (
+                  <ChildrenLicenses
+                    data={childrenData}
+                    loading={childrenLoading}
+                  />
+                ),
+              },
+            ]}
+          />
         </Box>
+      ) : (
+        <UserLicenses
+          isLoading={isLoading}
+          hasData={hasData}
+          hasError={hasError}
+          isGenericLicenseEmpty={isGenericLicenseEmpty}
+          passportData={passportEnabled ? passportData : null}
+          genericLicenses={genericLicenses}
+        />
       )}
     </>
   )
