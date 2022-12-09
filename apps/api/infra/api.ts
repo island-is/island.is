664aed2a9bb8cdff98a8a16d1d87e9031b613aa2
runarvestmann <runar@stefna.is>
2022-12-09 17:11:01 +0000

    Add chart values

--- apps/api/infra/api.ts
+++ apps/api/infra/api.ts
@@ -158,6 +158,7 @@ export const serviceSetup = (services: {
         prod: 'https://api.thinglysing.is/business/tolfraedi',
       },
       NO_UPDATE_NOTIFIER: 'true',
+      FISKISTOFA_ZENTER_CLIENT_ID: '1114',
     })
 
     .secrets({
@@ -216,6 +217,8 @@ export const serviceSetup = (services: {
         '/k8s/api/FINANCIAL_STATEMENTS_INAO_CLIENT_SECRET',
       FISKISTOFA_ZENTER_EMAIL: '/k8s/api/FISKISTOFA_ZENTER_EMAIL',
       FISKISTOFA_ZENTER_PASSWORD: '/k8s/api/FISKISTOFA_ZENTER_PASSWORD',
+      FISKISTOFA_ZENTER_CLIENT_PASSWORD:
+        '/k8s/api/FISKISTOFA_ZENTER_CLIENT_PASSWORD',
       FISKISTOFA_API_URL: '/k8s/api/FISKISTOFA_API_URL',
       FISKISTOFA_API_ACCESS_TOKEN_SERVICE_CLIENT_SECRET:
         '/k8s/api/FISKISTOFA_API_ACCESS_TOKEN_SERVICE_CLIENT_SECRET',
