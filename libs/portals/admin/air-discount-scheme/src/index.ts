0f18de0cb409f1ed9ce5502a04e7721abae751ac
Eirikur Nilsson <eirikur@nilsson.is>
2022-12-13 23:17:30 +0000

    chore(ap): Merge new ADS admin functionality
    Includes new discount code admin.

--- libs/portals/admin/air-discount-scheme/src/index.ts
+++ libs/portals/admin/air-discount-scheme/src/index.ts
@@ -3,6 +3,9 @@ import { lazy } from 'react'
 import { AdminPortalScope } from '@island.is/auth/scopes'
 import { PortalModule } from '@island.is/portals/core'
 
+const CreateDiscount = lazy(() =>
+  import('./screens/CreateDiscount/CreateDiscount'),
+)
 const OverviewScreen = lazy(() => import('./screens/Overview/Overview'))
 
 export const airDiscountSchemeAdminModule: PortalModule = {
@@ -14,6 +17,12 @@ export const airDiscountSchemeAdminModule: PortalModule = {
       path: '/loftbru',
       enabled: userInfo.scopes.includes(AdminPortalScope.airDiscountScheme),
       render: () => OverviewScreen,
+    },
+    {
+      name: 'Handvirkir kóðar',
+      path: '/loftbru/handvirkir-kodar',
+      enabled: userInfo.scopes.includes(AdminPortalScope.airDiscountScheme),
+      render: () => CreateDiscount,
     },
   ],
 }
