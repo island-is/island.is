dfa39d168386365a171b1dec6011f20ae5413536
Snær Seljan Þóroddsson <112904566+snaerseljan@users.noreply.github.com>
2022-12-14 16:02:32 +0000

    feat(portals): Initial module navigation setup (#9348)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/portals/admin/air-discount-scheme/src/index.ts
+++ libs/portals/admin/air-discount-scheme/src/index.ts
@@ -1,19 +1,4 @@
-import { lazy } from 'react'
+export * from './module'
 
-import { AdminPortalScope } from '@island.is/auth/scopes'
-import { PortalModule } from '@island.is/portals/core'
-
-const OverviewScreen = lazy(() => import('./screens/Overview/Overview'))
-
-export const airDiscountSchemeAdminModule: PortalModule = {
-  name: 'Loftbrú',
-  widgets: () => [],
-  routes: ({ userInfo }) => [
-    {
-      name: 'Yfirlit',
-      path: '/loftbru',
-      enabled: userInfo.scopes.includes(AdminPortalScope.airDiscountScheme),
-      render: () => OverviewScreen,
-    },
-  ],
-}
+// libs
+export * from './lib/navigation'
