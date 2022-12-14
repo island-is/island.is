6d4f49c788ae0eabb7bd7fad21aa50d34c6539c0
runarvestmann <runar@stefna.is>
2022-12-14 15:47:12 +0000

    Add comment

--- apps/web/components/connected/fiskistofa/SidebarShipSearchInput/SidebarShipSearchInput.tsx
+++ apps/web/components/connected/fiskistofa/SidebarShipSearchInput/SidebarShipSearchInput.tsx
@@ -37,7 +37,7 @@ const SidebarShipSearchInput = ({ namespace }: SidebarShipSearchInputProps) => {
       }
 
       const url = `${pathname}?${params}`
-
+      //
       window.open(
         url,
         shouldLinkOpenInNewWindow(pathname) ? '_blank' : '_self',
