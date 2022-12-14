7ad187da54939851fdd0c47b6b89fe309c68d00b
runarvestmann <runar@stefna.is>
2022-12-14 13:02:18 +0000

    Add seperate circularity check

--- libs/content-search-toolkit/src/services/utils/utils.spec.ts
+++ libs/content-search-toolkit/src/services/utils/utils.spec.ts
@@ -1,6 +1,16 @@
 import { filterDoc } from './index'
 
 describe('filterDoc', () => {
+  it('should not recurse infinitely if the node is circular', () => {
+    const test: Record<string, unknown> = {
+      a: 'some-value',
+    }
+    test.b = test
+    const result = filterDoc(test)
+
+    // No field should be pruned
+    expect(result).toBe(false)
+  })
   it('should prune fields that are longer than the letter limit', () => {
     const test: Record<string, unknown> = {
       a: 'some-value',
@@ -8,9 +18,6 @@ describe('filterDoc', () => {
         c: 'some-really-long-value-that-should-get-pruned',
       },
     }
-
-    // Also make the test object circular
-    test.d = test
 
     const result = filterDoc(test, new Set(), 20)
     expect(result).toBe(true)
