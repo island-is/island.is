576bbb7ed0497197daf561d8f60a3bcf680e34cf
runarvestmann <runar@stefna.is>
2022-12-14 11:01:36 +0000

    Renamed param

--- libs/content-search-toolkit/src/services/utils/index.ts
+++ libs/content-search-toolkit/src/services/utils/index.ts
@@ -3,22 +3,22 @@
  * @return {boolean} True iff a document was removed
  * Filter the HUMONGOUS documents in an error object
  */
-export function filterDoc<T>(o: T, visited = new Set()): boolean {
-  if (visited.has(o) || !o) return false
+export function filterDoc<T>(node: T, visited = new Set()): boolean {
+  if (visited.has(node) || !node) return false
 
   // Only add objects to the visited set
-  visited.add(o)
+  visited.add(node)
 
   let deleted = false
-  if (Object.keys(o).length == 0) return false
-  for (const key in o) {
-    const value = o[key]
+  if (Object.keys(node).length == 0) return false
+  for (const key in node) {
+    const value = node[key]
     // Only HUMONGOUS documents should reach this limit
     if (typeof value == 'string' && value.length > 10000) {
-      delete o[key]
+      delete node[key]
       deleted = true
     }
-    return filterDoc(o[key], visited)
+    return filterDoc(node[key], visited)
   }
   return deleted
 }
