b47645fdb76ddc020f0f13740c48e2223cbd936a
Snær Seljan Þóroddsson <snaer@aranja.com>
2022-12-15 10:46:09 +0000

    feat(portals-core): Filter hook for our navigation

--- libs/portals/core/src/hooks/useNavigation.ts
+++ libs/portals/core/src/hooks/useNavigation.ts
@@ -1,13 +1,93 @@
+import * as kennitala from 'kennitala'
+import { useAuth } from '@island.is/auth/react'
+import { User } from '@island.is/shared/types'
+import { isDefined } from '@island.is/shared/utils'
 import { useMemo } from 'react'
+import { useModules } from '../components/ModulesProvider'
 import { usePortalMeta } from '../components/PortalMetaProvider'
+import {
+  PortalModule,
+  PortalNavigationItem,
+  PortalRoute,
+} from '../types/portalCore'
+
+interface FilterNavigation {
+  modules: PortalModule[]
+  routes: PortalRoute[]
+  navItem: PortalNavigationItem
+  isModuleChild: boolean
+  userInfo: User
+}
+
+const filterNavigation = ({
+  modules,
+  navItem,
+  isModuleChild = false,
+  userInfo,
+  routes,
+}: FilterNavigation) => {
+  if (navItem.navHide) return false
+
+  if (!isModuleChild) {
+    const module = modules.find((m) => m.name === navItem.name)
+
+    // Check if navItem exists in modules or if navItem is enabled
+    if (
+      !module ||
+      (isDefined(module.enabled) &&
+        !module.enabled({
+          userInfo: userInfo,
+          isCompany: kennitala.isCompany(userInfo.profile.nationalId),
+        }))
+    ) {
+      return false
+    }
+  } else {
+    const route = routes.find((route) => route.path === navItem.path)
+
+    // Check if navItem exists in routes or if navItem is enabled
+    if (!route || (isDefined(route.enabled) && !route.enabled)) {
+      return false
+    }
+  }
+
+  // Check if navItem has children
+  if (navItem.children?.[0]) {
+    // Check if navItem children exists in modules children
+    navItem.children = navItem.children?.filter((child) =>
+      filterNavigation({
+        modules,
+        navItem: child,
+        isModuleChild: true,
+        userInfo,
+        routes,
+      }),
+    )
+  }
+
+  return true
+}
 
 export const useNavigation = () => {
+  const { userInfo } = useAuth()
   const { masterNav } = usePortalMeta()
+  const { modules, routes } = useModules()
 
   const navigation = useMemo(() => {
-    // TODO filter
-    return masterNav
-  }, [masterNav])
+    if (userInfo) {
+      return masterNav?.children?.filter((navItem) =>
+        filterNavigation({
+          modules,
+          routes,
+          userInfo,
+          navItem,
+          isModuleChild: false,
+        }),
+      )
+    }
+
+    return undefined
+  }, [masterNav?.children, modules, routes, userInfo])
 
   return navigation
 }
