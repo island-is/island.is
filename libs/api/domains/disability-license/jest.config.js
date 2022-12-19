37239c680e7336976bed139e76d4e402c2c9c0e1
Alexandra Diljá <alexandrab18@ru.is>
2022-12-19 13:23:57 +0000

    disability spell check

--- libs/api/domains/disability-license/jest.config.js
+++ libs/api/domains/disability-license/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'api-domains-disablitly-license',
+  displayName: 'api-domains-disability-license',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/api/domains/disablitly-license',
+  coverageDirectory: '../../../../coverage/libs/api/domains/disability-license',
 }
