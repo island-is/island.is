ae51d2faf34b7911477f31989fbc1e4e6be4f029
Valur Einarsson <valure@live.com>
2022-12-22 11:29:19 +0000

    Remove unused code.

--- apps/services/auth/ids-api/test/fixtures/fixture-factory.ts
+++ apps/services/auth/ids-api/test/fixtures/fixture-factory.ts
@@ -7,7 +7,6 @@ import { TestApp } from '@island.is/testing/nest'
 import { createNationalId } from '@island.is/testing/fixtures'
 import {
   ApiScope,
-  ApiScopeGroup,
   ApiScopeUser,
   ApiScopeUserAccess,
   Client,
@@ -15,11 +14,9 @@ import {
   Delegation,
   DelegationScope,
   Domain,
-  Translation,
 } from '@island.is/auth-api-lib'
 import {
   CreateApiScope,
-  CreateApiScopeGroup,
   CreateApiScopeUserAccess,
   CreateCustomDelegation,
   CreateDomain,
@@ -94,34 +91,6 @@ export class FixtureFactory {
     })
   }
 
-  async createApiScopeGroup({
-    apiScopes = [],
-    domainName,
-    ...data
-  }: CreateApiScopeGroup = {}): Promise<ApiScopeGroup> {
-    if (!domainName) {
-      domainName = (await this.createDomain()).name
-    }
-    const group = await this.get(ApiScopeGroup).create({
-      id: data.id ?? faker.datatype.uuid(),
-      name: data.name ?? faker.random.word(),
-      displayName: data.displayName ?? faker.random.word(),
-      description: data.description ?? faker.random.word(),
-      order: data.order ?? 0,
-      domainName,
-    })
-    await Promise.all(
-      apiScopes.map((apiScope) =>
-        this.createApiScope({
-          domainName: domainName,
-          groupId: group.id,
-          ...apiScope,
-        }),
-      ),
-    )
-    return group
-  }
-
   async createApiScopeUserAccess({
     nationalId,
     scope,
@@ -170,27 +139,5 @@ export class FixtureFactory {
     })
 
     return delegation
-  }
-
-  async createTranslations(
-    instance: Model,
-    language: string,
-    translations: Record<string, string>,
-  ): Promise<Translation[]> {
-    const className = instance.constructor.name.toLowerCase()
-    const key = instance.get(
-      (instance.constructor as typeof Model).primaryKeyAttributes[0],
-    ) as string
-    const translationObjs = Object.entries(translations).map(
-      ([property, value]) => ({
-        language,
-        className,
-        key,
-        property,
-        value,
-      }),
-    )
-
-    return this.get(Translation).bulkCreate(translationObjs)
   }
 }
