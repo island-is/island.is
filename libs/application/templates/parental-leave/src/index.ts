3a9f7eba7e0435838a1d161ab9cac42a08f2bee9
qdd1 <6514088+qdd1@users.noreply.github.com>
2023-01-03 09:35:46 +0000

    fix(parental leave): percentage end date issue (#9469) (#9472)
    * fix(parental-leave): Percentage vs endDate issue
    
    * fix(parental-leave): Using D{days} everywhere which ratio is not 100 percent
    
    * fix(parental-leave): fix unitest

--- libs/application/templates/parental-leave/src/index.ts
+++ libs/application/templates/parental-leave/src/index.ts
@@ -7,7 +7,10 @@ export const getFields = () => import('./fields/')
 export default ParentalLeaveTemplate
 
 export * from './lib/parentalLeaveUtils'
-export { calculatePeriodLength } from './lib/directorateOfLabour.utils'
+export {
+  calculatePeriodLength,
+  DAYS_IN_MONTH,
+} from './lib/directorateOfLabour.utils'
 export * from './constants'
 export * from './types'
 export * from './lib/messages'
