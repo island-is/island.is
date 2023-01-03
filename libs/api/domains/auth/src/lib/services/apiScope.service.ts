7c447c088d93312df8f987e6e9bfbd1d67e54983
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-01-03 10:15:53 +0000

    remove delegations feature flags from services

--- libs/api/domains/auth/src/lib/services/apiScope.service.ts
+++ libs/api/domains/auth/src/lib/services/apiScope.service.ts
@@ -1,42 +1,50 @@
-import { Injectable } from '@nestjs/common'
-import { ApiScopeServiceV1 } from '../services-v1/apiScope.service'
-import { ApiScopeServiceV2 } from '../services-v2/apiScope.service'
+import { BadRequestException, Injectable } from '@nestjs/common'
+
 import { ApiScopeServiceInterface } from './types'
-import { FeatureFlagService, Features } from '@island.is/nest/feature-flags'
-import { User } from '@island.is/auth-nest-tools'
+import { Auth, AuthMiddleware, User } from '@island.is/auth-nest-tools'
 import { ScopeTreeNode } from '../models/scopeTreeNode.model'
 import { ApiScope } from '../models'
 import { ApiScopesInput } from '../dto/apiScopes.input'
+import {
+  DomainsApi,
+  DomainsControllerFindScopesDirectionEnum,
+} from '@island.is/clients/auth/delegation-api'
 
 @Injectable()
 export class ApiScopeService implements ApiScopeServiceInterface {
-  constructor(
-    private apiScopeServiceV1: ApiScopeServiceV1,
-    private apiScopeServiceV2: ApiScopeServiceV2,
-    private featureFlagService: FeatureFlagService,
-  ) {}
+  constructor(private domainsApi: DomainsApi) {}
 
-  private async service(user: User): Promise<ApiScopeServiceInterface> {
-    const newDelegations = await this.featureFlagService.getValue(
-      Features.outgoingDelegationsV2,
-      false,
-      user,
-    )
-    return newDelegations ? this.apiScopeServiceV2 : this.apiScopeServiceV1
+  private domainsApiWithAuth(auth: Auth) {
+    return this.domainsApi.withMiddleware(new AuthMiddleware(auth))
+  }
+
+  getApiScopes(user: User, input: ApiScopesInput): Promise<ApiScope[]> {
+    this.checkDomain(input.domain)
+
+    return this.domainsApiWithAuth(user).domainsControllerFindScopes({
+      domainName: input.domain,
+      lang: input.lang,
+      // If we fix our openApi generation to support enumName to alias the enum type we can fix this.
+      direction: (input.direction as unknown) as DomainsControllerFindScopesDirectionEnum,
+    })
   }
 
   getScopeTree(
     user: User,
     input: ApiScopesInput,
   ): Promise<Array<typeof ScopeTreeNode>> {
-    return this.service(user).then((service) =>
-      service.getScopeTree(user, input),
-    )
+    this.checkDomain(input.domain)
+
+    return this.domainsApiWithAuth(user).domainsControllerFindScopeTree({
+      domainName: input.domain,
+      lang: input.lang,
+      direction: input.direction,
+    })
   }
 
-  getApiScopes(user: User, input: ApiScopesInput): Promise<ApiScope[]> {
-    return this.service(user).then((service) =>
-      service.getApiScopes(user, input),
-    )
+  private checkDomain(domain?: string): asserts domain is string {
+    if (domain == null) {
+      throw new BadRequestException(`Domain is required.`)
+    }
   }
 }
