008761dfe5d81ca9d13421264dca90678cd5155c
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-01-04 13:49:07 +0000

    Removes deprecated delivery service

--- apps/judicial-system/message-handler/src/app/messageHandler.service.ts
+++ apps/judicial-system/message-handler/src/app/messageHandler.service.ts
@@ -13,7 +13,6 @@ import {
 import type { CaseMessage } from '@island.is/judicial-system/message'
 import { NotificationType } from '@island.is/judicial-system/types'
 
-import { CaseDeliveryService } from './caseDelivery.service'
 import { InternalDeliveryService } from './internalDelivery.service'
 import { appModuleConfig } from './app.config'
 
@@ -24,7 +23,6 @@ export class MessageHandlerService implements OnModuleDestroy {
 
   constructor(
     private readonly messageService: MessageService,
-    private readonly caseDeliveryService: CaseDeliveryService,
     private readonly internalDeliveryService: InternalDeliveryService,
     @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
   ) {}
