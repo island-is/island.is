089ffe652b226010e345cb55ec15637664e4a182
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-01-04 10:45:22 +0000

    feat: different update types

--- apps/license-api/src/app/modules/license/license.service.ts
+++ apps/license-api/src/app/modules/license/license.service.ts
@@ -1,8 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common'
 import type { Logger } from '@island.is/logging'
 import { LOGGER_PROVIDER } from '@island.is/logging'
-import { DeleteLicenseDto, UpdateLicenseDto } from './dto/license.dto'
+import {
+  DeleteLicenseDto,
+  PullUpdateLicenseDto,
+  PushUpdateLicenseDto,
+  UpdateLicenseDto,
+} from './dto/license.dto'
 import { SmartSolutionsApi } from '@island.is/clients/smartsolutions'
+import { LicenseStatus, LicenseUpdateType } from './license.types'
 
 @Injectable()
 export class LicenseService {
@@ -13,6 +19,14 @@ export class LicenseService {
   ) {}
 
   async updateLicense(inputData: UpdateLicenseDto): Promise<string> {
+    let data
+    if (inputData.licenseUpdateType === LicenseUpdateType.PUSH) {
+      // PUSH
+      data = inputData as PushUpdateLicenseDto
+    } else {
+      // PULL
+      data = inputData as PullUpdateLicenseDto
+    }
     const templates = await this.smartApi.listTemplates()
     this.logger.debug(JSON.stringify(templates?.data))
     return JSON.stringify(templates)
