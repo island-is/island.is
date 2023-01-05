27ca53e6b63ae821150c6016ccb9f4fbd3736f73
Arnar Kári <arnar.kari@kolibri.is>
2023-01-05 15:23:29 +0000

    feat(j-s): allow prosecutor to edit indictments before they are recieved

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/Overview/Overview.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Overview/Overview.tsx
@@ -43,8 +43,9 @@ const Overview: React.FC = () => {
   const isNewIndictment =
     workingCase.state === CaseState.NEW || workingCase.state === CaseState.DRAFT
 
-  const caseHasBeenSentToCourt =
-    workingCase.state !== CaseState.NEW && workingCase.state !== CaseState.DRAFT
+  const caseHasBeenReceivedByCourt =
+    workingCase.courtCaseNumber !== undefined &&
+    workingCase.courtCaseNumber !== null
 
   const handleNextButtonClick = async () => {
     if (isNewIndictment) {
@@ -59,7 +60,7 @@ const Overview: React.FC = () => {
       workingCase={workingCase}
       activeSection={Sections.PROSECUTOR}
       activeSubSection={
-        caseHasBeenSentToCourt
+        caseHasBeenReceivedByCourt
           ? undefined
           : IndictmentsProsecutorSubsections.OVERVIEW
       }
@@ -84,16 +85,16 @@ const Overview: React.FC = () => {
       <FormContentContainer isFooter>
         <FormFooter
           previousUrl={
-            caseHasBeenSentToCourt
+            caseHasBeenReceivedByCourt
               ? constants.CASES_ROUTE
               : `${constants.INDICTMENTS_CASE_FILES_ROUTE}/${workingCase.id}`
           }
           nextButtonText={formatMessage(strings.overview.nextButtonText, {
             isNewIndictment,
           })}
-          hideNextButton={caseHasBeenSentToCourt}
+          hideNextButton={caseHasBeenReceivedByCourt}
           infoBoxText={
-            caseHasBeenSentToCourt
+            caseHasBeenReceivedByCourt
               ? formatMessage(strings.overview.caseSendToCourt)
               : undefined
           }
