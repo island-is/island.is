c823e5c1c512ecdeba1eb865e595578e25aa571b
diljath <diljath@advania.is>
2023-01-05 11:28:07 +0000

    added withnx to next config

--- apps/samradsgatt/next.config.js
+++ apps/samradsgatt/next.config.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-
+const withNx = require('@nrwl/next/plugins/with-nx')
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
 const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { presets: ['next/babel'] }
 
-module.exports = withVanillaExtract(nextConfig)
+module.exports = withNx(withVanillaExtract(nextConfig))
