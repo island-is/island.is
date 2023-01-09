0e9fb0e957ec744c85cde7a3a7583a8fcfd6ae0f
valurefugl <65780958+valurefugl@users.noreply.github.com>
2023-01-09 11:56:58 +0000

    feat(auth): Delegation filtering (#9424)
    * Filter delegations based on available scopes.
    
    * Fix domain name setup, refactor beforeall.
    
    * Remove unused code.
    
    * Add jest to types in tsconfig.
    
    * Revert tsconfig lib change.
    
    * Try import expect.
    
    * Share test setup between tests.
    
    * Share fixture factory.
    
    * Simplify where options.
    
    * Use find in scope checks.
    
    * Lightwweight api scope type.
    
    * Exclude all test folders.
    
    Co-authored-by: Valur Einarsson <valure@live.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/services/auth/testing/src/fixtures/types.ts
+++ libs/services/auth/testing/src/fixtures/types.ts
@@ -1,19 +1,12 @@
 import { Optional } from 'sequelize'
 import {
-  ApiScopeGroupDTO,
   ApiScopesDTO,
   ApiScopeUserAccessDTO,
   DelegationDTO,
   DelegationScopeDTO,
-  DomainDTO,
 } from '@island.is/auth-api-lib'
 
-export type CreateDomain = Partial<DomainDTO> & { apiScopes?: CreateApiScope[] }
 export type CreateApiScope = Partial<ApiScopesDTO>
-export type CreateApiScopeGroup = Partial<ApiScopeGroupDTO> & {
-  id?: string
-  apiScopes?: CreateApiScope[]
-}
 export type CreateApiScopeUserAccess = ApiScopeUserAccessDTO
 export type CreateCustomDelegationScope = Optional<
   Pick<DelegationScopeDTO, 'scopeName' | 'validFrom' | 'validTo'>,
