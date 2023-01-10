5cfb1cc715f5314470a0eccc9d6413e53f482540
kksteini <steini@kosmosogkaos.is>
2023-01-10 09:45:16 +0000

    Remove untested crazy code

--- libs/api/domains/air-discount-scheme/src/lib/flight-leg/flight-leg.resolver.ts
+++ libs/api/domains/air-discount-scheme/src/lib/flight-leg/flight-leg.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common'
-import { Info, Query, Resolver } from '@nestjs/graphql'
+import { Query, Resolver } from '@nestjs/graphql'
 import type { User } from '@island.is/auth-nest-tools'
 import { ApiScope } from '@island.is/auth/scopes'
 import {
@@ -11,8 +11,6 @@ import {
 import { Audit } from '@island.is/nest/audit'
 import { FlightLegService } from './flight-leg.service'
 import { FlightLeg } from '../models/flightLeg.model'
-import { detectGraphqlDepthFromInfo } from '../utils/detect-graphql-depth'
-import { GraphQLError, GraphQLResolveInfo } from 'graphql'
 
 @UseGuards(IdsUserGuard, ScopesGuard)
 @Scopes(ApiScope.internal)
@@ -24,18 +22,7 @@ export class FlightLegResolver {
   @Query(() => [FlightLeg], {
     name: 'airDiscountSchemeUserAndRelationsFlights',
   })
-  async getFlightLegs(
-    @CurrentUser() user: User,
-    @Info() info: GraphQLResolveInfo,
-  ): Promise<FlightLeg[]> {
-    // See Info()
-    // https://docs.nestjs.com/graphql/resolvers#graphql-argument-decorators
-
-    const depth = detectGraphqlDepthFromInfo(info)
-
-    if (depth > 4) {
-      throw new GraphQLError('Query Depth Limit exceeded')
-    }
+  async getFlightLegs(@CurrentUser() user: User): Promise<FlightLeg[]> {
     return this.flightLegService.getThisYearsUserAndRelationsFlightLegs(user)
   }
 }
