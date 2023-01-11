b41b30b2ff06a8eb7304f41d9f60570585f2c719
Ívar Oddsson <oddsson@ivars-imac.lan>
2023-01-11 15:20:03 +0000

    Revert case controller

--- apps/judicial-system/backend/src/app/modules/case/case.controller.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.controller.ts
@@ -180,7 +180,7 @@ export class CaseController {
       )
     }
 
-    return this.caseService.update(theCase, caseToUpdate, user) as Promise<Case>
+    return this.caseService.update(theCase, caseToUpdate, user) as Promise<Case> // Never returns undefined
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard, CaseExistsGuard, CaseWriteGuard)
