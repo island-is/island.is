9249e96a2e090bc470c4e79eea48012512618ca3
Sigrún Tinna Gissurardóttir <ori.sigrung@ORI0594.local>
2023-01-13 14:19:23 +0000

    finished operator application

---
+++ libs/application/template-api-modules/src/lib/modules/templates/transport-authority/change-operator-of-vehicle/smsGenerators/requestReviewSms.ts
@@ -0,0 +1,30 @@
+import { ChangeOperatorOfVehicleAnswers } from '@island.is/application/templates/transport-authority/change-operator-of-vehicle'
+import { SmsMessage } from '../../../../../types'
+import { EmailRecipient } from '../types'
+import { Application } from '@island.is/application/types'
+import { getApplicationPruneDateStr } from '../change-operator-of-vehicle.utils'
+
+export type RequestReviewSms = (
+  application: Application,
+  recipient: EmailRecipient,
+) => SmsMessage
+
+export const generateRequestReviewSms: RequestReviewSms = (
+  application,
+  recipient,
+) => {
+  const answers = application.answers as ChangeOperatorOfVehicleAnswers
+  const permno = answers?.pickVehicle?.plate
+
+  if (!recipient.phone) throw new Error('Recipient phone was undefined')
+  if (!permno) throw new Error('Permno was undefined')
+
+  const pruneDateStr = getApplicationPruneDateStr(application.created)
+
+  return {
+    phoneNumber: recipient.phone || '',
+    message:
+      `Þín bíður ósamþykkt beiðni um breytingu á umráðamönnum fyrir ökutækið ${permno} inn á island.is/umsoknir. ` +
+      `Til þess að breytingin verði skráð þarftu að samþykkja beiðnina fyrir ${pruneDateStr}.`,
+  }
+}
---
+++ libs/application/templates/transport-authority/change-operator-of-vehicle/src/lib/dataSchema.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod'
+
+export const UserInformationSchema = z.object({
+  nationalId: z.string().min(1),
+  name: z.string().min(1),
+  email: z.string().min(1),
+  phone: z.string().min(1),
+  approved: z.boolean().optional(),
+})
+
+export const OperatorInformationSchema = z.object({
+  nationalId: z.string().min(1),
+  name: z.string().min(1),
+  email: z.string().min(1),
+  phone: z.string().min(1),
+  approved: z.boolean().optional(),
+})
+
+export const OldOperatorInformationSchema = z.object({
+  nationalId: z.string().min(1),
+  name: z.string().min(1),
+  wasRemoved: z.string().optional(),
+  startDate: z.string().optional(),
+})
+
+export const RejecterSchema = z.object({
+  plate: z.string(),
+  name: z.string(),
+  nationalId: z.string(),
+  type: z.enum(['coOwner', 'operator']),
+})
+
+export const ChangeOperatorOfVehicleSchema = z.object({
+  approveExternalData: z.boolean().refine((v) => v),
+  pickVehicle: z.object({
+    vehicle: z.string().optional(),
+    plate: z.string().min(1),
+    type: z.string().optional(),
+    color: z.string().optional(),
+  }),
+  owner: UserInformationSchema,
+  ownerCoOwner: z.array(UserInformationSchema),
+  vehicle: z.object({
+    plate: z.string(),
+  }),
+  operators: z.array(OperatorInformationSchema),
+  oldOperators: z.array(OldOperatorInformationSchema),
+  mainOperator: z.object({
+    nationalId: z.string(),
+  }),
+  rejecter: RejecterSchema,
+})
+export type ChangeOperatorOfVehicle = z.TypeOf<
+  typeof ChangeOperatorOfVehicleSchema
+>
