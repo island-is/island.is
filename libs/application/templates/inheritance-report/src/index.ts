54e8397afc2b43791d456bdb0866640ab3a51962
albinagu <47886428+albinagu@users.noreply.github.com>
2023-01-13 15:20:02 +0000

    feat(inheritance-report): Initial Setup of the Inheritance Report (#9338)
    * initial setup of the inheritance report
    
    * adding to codeowners
    
    * template-api service setup
    
    * assets screens
    
    * application type misspell
    
    * set up of new service and module and update on providers
    
    * tweak
    
    * gitbook
    
    * asset overview setup
    
    * build fix
    
    * tsconfig for estate
    
    * format
    
    * adding total count to text repeaterfield
    
    * total calculating tweak (wip)
    
    * total Amounts in assests (wip)
    
    * totals in overview (wip)
    
    * cleanup
    
    * texts and total count fix
    
    * adding debts to the application
    
    * adding business assets and debts
    
    * adding heirs section to the application
    
    * heirs - repeater fields
    
    * wip: calculating value in stocks
    
    * stocks value calculation tweak (still wip)
    
    * cleanup
    
    * codeowners?
    
    * format?
    
    * c
    
    * adding 'Done' screen
    
    * assets overview
    
    * adding calculated values
    
    * a few tweaks in assets and heirs
    
    * texts
    
    * totalDeduction in heirs
    
    * spouses share and feature flag
    
    * real estate and vehicles repeater with initial
    
    * id tweak
    
    * calculating rateOfExchange * faceValue
    
    * estate, vehicle totals
    
    * wip: calculations for heirs
    
    * wip: more calculations
    
    * a bit of cleanup
    
    * net property calculation tweak
    
    * cleanup
    
    * review tweaks
    
    * updates after the review

---
+++ libs/application/templates/inheritance-report/src/index.ts
@@ -0,0 +1,7 @@
+import InheritanceReportTemplate from './lib/InheritanceReportTemplate'
+import { inheritanceReportSchema } from './lib/dataSchema'
+
+export const getFields = () => import('./fields')
+
+export default InheritanceReportTemplate
+export { inheritanceReportSchema }
