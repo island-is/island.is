366ae1b3879829caf8e3ce649acb652191a7e1ff
Þórður Hafliðason <tolleinn@gmail.com>
2023-01-13 09:57:18 +0000

    Add babel jest config

--- libs/portals/admin/regulations-admin/jest.config.js
+++ libs/portals/admin/regulations-admin/jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
   displayName: 'portals-admin-regulations-admin',
   preset: '../../../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory:
