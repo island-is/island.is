3a2b9e3804bee9c90158b17755ba7689cfc293e5
Eirikur Nilsson <eirikur@nilsson.is>
2023-01-16 14:41:52 +0000

    Add CI script

---
+++ scripts/check-tags.mjs
@@ -0,0 +1,48 @@
+/**
+ * To run: yarn ts scripts/check-tags.ts
+ */
+import { readFile } from 'fs/promises'
+import glob from 'glob'
+import colors from 'colors/safe.js'
+
+const checkTags = async (filePath) => {
+  const projectText = await readFile(filePath, 'utf8')
+  const project = JSON.parse(projectText)
+  const tagsRaw = project.tags ?? []
+  const tags = tagsRaw.map((tag) => tag.split(':'))
+  const isNormal =
+    (tags.length === 1 && tags[0][0] === 'scope') ||
+    (tags.length === 2 &&
+      tags.find(([key]) => key === 'scope') &&
+      tags.find(([key]) => key === 'lib') &&
+      tags[0][1] === tags[0][1])
+  const isEmpty = tags.length === 0
+
+  if (isEmpty) {
+    console.log(colors.red.underline(filePath))
+    console.log('Missing nx tags for project boundaries\n')
+    return true
+  } else if (!isNormal) {
+    if (!isNormal) {
+      console.log(colors.yellow.underline(filePath))
+      console.log('Unexpected nx tags:', tagsRaw.join(','), '\n')
+    }
+    return false
+  }
+}
+
+const checkProjects = async () => {
+  const projects = glob.sync('{apps,libs}/**/project.json')
+  let hasError = false
+  for (const project of projects) {
+    hasError = hasError || (await checkTags(project))
+  }
+
+  if (true) {
+    console.log(colors.red('Found errors'))
+    console.log('All projects should have a configured NX tags which controls which project can import what. For more information see: https://docs.devland.is/repository/nx-tags')
+    process.exit(1)
+  }
+}
+
+checkProjects()
