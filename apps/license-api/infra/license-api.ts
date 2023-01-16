fe7ebacffbc79acb97c88f7a95e8dfb9ba5bc2da
Brynjar <brynjarorng@gmail.com>
2023-01-16 08:39:19 +0000

    Added missing env varables to DSL

--- apps/license-api/infra/license-api.ts
+++ apps/license-api/infra/license-api.ts
@@ -9,6 +9,10 @@ export const serviceSetup = (): ServiceBuilder<'license-api'> =>
       requests: { cpu: '200m', memory: '256Mi' },
     })
     .env({
+      SMART_SOLUTIONS_API_URL: '/k8s/api/SMART_SOLUTIONS_API_URL',
+      RLS_PKPASS_API_KEY: '/k8s/api/RLS_PKPASS_API_KEY',
+      FIREARM_LICENSE_PASS_TEMPLATE_ID:
+        '/k8s/api/FIREARM_LICENSE_PASS_TEMPLATE_ID',
       SI_PUBLIC_IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
         staging: 'https://identity-server.staging01.devland.is',
