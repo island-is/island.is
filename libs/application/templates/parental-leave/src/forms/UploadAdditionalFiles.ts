bf146ebfad9bf41e23a69a8ac993937654c2c874
Jonas Carlsen <121940545+jhc-deloitte@users.noreply.github.com>
2023-01-17 15:34:54 +0000

    Fix upload document (#9643)
    * Fix upload document
    
    * Fix PR comments

--- libs/application/templates/parental-leave/src/forms/UploadAdditionalFiles.ts
+++ libs/application/templates/parental-leave/src/forms/UploadAdditionalFiles.ts
@@ -1,10 +1,13 @@
 import {
+  buildCustomField,
+  buildDescriptionField,
   buildFileUploadField,
   buildForm,
+  buildMultiField,
   buildSection,
   buildSubmitField,
 } from '@island.is/application/core'
-import { Form } from '@island.is/application/types'
+import { Form, FormModes } from '@island.is/application/types'
 
 import Logo from '../assets/Logo'
 import { FILE_SIZE_LIMIT } from '../constants'
@@ -14,38 +17,55 @@ export const UploadAdditionalFiles: Form = buildForm({
   id: 'ParentalLeaveUploadAdditionalFiles',
   title: '',
   logo: Logo,
+  mode: FormModes.IN_PROGRESS,
   children: [
     buildSection({
       id: 'additionalDocuments',
-      title: '',
+      title: parentalLeaveFormMessages.confirmation.title,
       children: [
-        buildFileUploadField({
-          id: 'fileUpload.additionalDocuments',
-          title: parentalLeaveFormMessages.attachmentScreen.genericTitle,
-          introduction:
-            parentalLeaveFormMessages.attachmentScreen.genericDescription,
-          maxSize: FILE_SIZE_LIMIT,
-          maxSizeErrorText:
-            parentalLeaveFormMessages.selfEmployed.attachmentMaxSizeError,
-          uploadAccept: '.pdf',
-          uploadHeader: '',
-          uploadDescription: '',
-          uploadButtonLabel:
-            parentalLeaveFormMessages.selfEmployed.attachmentButton,
-        }),
-        buildSubmitField({
-          id: 'additionalDocuments',
+        buildCustomField({
+          id: 'periodSynchronizationScreen',
           title: parentalLeaveFormMessages.confirmation.title,
-          refetchApplicationAfterSubmit: true,
-          actions: [
-            {
-              event: 'SUBMIT',
-              name:
-                parentalLeaveFormMessages.attachmentScreen
-                  .additionalDocumentsEditSubmit,
-              type: 'primary',
-            },
+          component: 'PeriodSynchronization',
+        }),
+        buildMultiField({
+          id: 'additionalDocumentsScreen',
+          title: parentalLeaveFormMessages.confirmation.title,
+          description: parentalLeaveFormMessages.confirmation.description,
+          children: [
+            buildFileUploadField({
+              id: 'fileUpload.additionalDocuments',
+              title: parentalLeaveFormMessages.attachmentScreen.genericTitle,
+              introduction:
+                parentalLeaveFormMessages.attachmentScreen.genericDescription,
+              maxSize: FILE_SIZE_LIMIT,
+              maxSizeErrorText:
+                parentalLeaveFormMessages.selfEmployed.attachmentMaxSizeError,
+              uploadAccept: '.pdf',
+              uploadHeader: '',
+              uploadDescription: '',
+              uploadButtonLabel:
+                parentalLeaveFormMessages.selfEmployed.attachmentButton,
+            }),
+            buildSubmitField({
+              id: 'additionalDocumentsSubmit',
+              title: 'submit',
+              placement: 'footer',
+              refetchApplicationAfterSubmit: true,
+              actions: [
+                {
+                  name: 'Submit',
+                  type: 'primary',
+                  event: 'APPROVE',
+                },
+              ],
+            }),
           ],
+        }),
+        buildDescriptionField({
+          id: 'unused',
+          title: '',
+          description: '',
         }),
       ],
     }),
