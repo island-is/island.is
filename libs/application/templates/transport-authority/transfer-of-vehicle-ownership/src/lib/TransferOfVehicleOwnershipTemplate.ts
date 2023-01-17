b694e1082a8f2441369b28da8b578fbf8bf895d1
Sigrún Tinna Gissurardóttir <39527334+sigruntg@users.noreply.github.com>
2023-01-17 09:39:42 +0000

    fix(transfer of vehicle ownership): bugs (#9632)
    * making pick vehicle work properly
    
    * make draft ephemeral state
    
    * forgot one tel format
    
    Co-authored-by: Sigrún Tinna Gissurardóttir <ori.sigrung@ORI0594.lan>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/transport-authority/transfer-of-vehicle-ownership/src/lib/TransferOfVehicleOwnershipTemplate.ts
+++ libs/application/templates/transport-authority/transfer-of-vehicle-ownership/src/lib/TransferOfVehicleOwnershipTemplate.ts
@@ -9,7 +9,11 @@ import {
   DefaultEvents,
   defineTemplateApi,
 } from '@island.is/application/types'
-import { getValueViaPath, pruneAfterDays } from '@island.is/application/core'
+import {
+  EphemeralStateLifeCycle,
+  getValueViaPath,
+  pruneAfterDays,
+} from '@island.is/application/core'
 import { Events, States, Roles } from './constants'
 import { ApiActions } from '../shared'
 import { Features } from '@island.is/feature-flags'
@@ -73,7 +77,7 @@ const template: ApplicationTemplate<
             },
           },
           progress: 0.25,
-          lifecycle: pruneAfterDays(1),
+          lifecycle: EphemeralStateLifeCycle,
           onExit: defineTemplateApi({
             action: ApiActions.validateApplication,
           }),
