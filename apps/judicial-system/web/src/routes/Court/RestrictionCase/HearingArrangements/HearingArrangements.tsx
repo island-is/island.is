205bfca1615e1ab4abf6e22db89aec29d58d8811
unakb <una@kolibri.is>
2023-01-17 22:06:26 +0000

    fix(j-s): Conditional text for secondary button
    Text on secondary button of hearing arrangements notification modal should only display "send later" if the notification has never been sent before. Otherwise the modal only appears the first time the user continues after changing the time, so "send later" creates confusion as there is no for the user to trigger a resend

--- apps/judicial-system/web/src/routes/Court/RestrictionCase/HearingArrangements/HearingArrangements.tsx
+++ apps/judicial-system/web/src/routes/Court/RestrictionCase/HearingArrangements/HearingArrangements.tsx
@@ -240,6 +240,8 @@ export const HearingArrangements: React.FC = () => {
           primaryButtonText={formatMessage(m.modal.shared.primaryButtonText)}
           secondaryButtonText={formatMessage(
             m.modal.shared.secondaryButtonText,
+              courtDateHasChanged,
+            },
           )}
         />
       )}
