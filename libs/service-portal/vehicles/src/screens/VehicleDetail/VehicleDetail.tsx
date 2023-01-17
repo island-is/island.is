95c3dfb2c5221738ddbeeceb8171f4c3c99a4416
disaerna <disa.erna@gmail.com>
2023-01-17 14:20:11 +0000

    fix: intro header spacing + buttons wrap

--- libs/service-portal/vehicles/src/screens/VehicleDetail/VehicleDetail.tsx
+++ libs/service-portal/vehicles/src/screens/VehicleDetail/VehicleDetail.tsx
@@ -1,5 +1,5 @@
 import isNumber from 'lodash/isNumber'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
 import {
@@ -224,7 +224,7 @@ const VehicleDetail: ServicePortalModuleComponent = () => {
 
   return (
     <>
-      <Box marginBottom={6}>
+      <Box marginBottom={[2, 2, 6]}>
         <GridRow>
           <GridColumn span={['12/12', '12/12', '6/8', '6/8']}>
             <Stack space={2}>
@@ -251,10 +251,16 @@ const VehicleDetail: ServicePortalModuleComponent = () => {
           </GridColumn>
         </GridRow>
         {!loading && downloadServiceURL && (
-          <GridRow marginTop={6}>
+          <GridRow marginTop={[2, 2, 6]}>
             <GridColumn span="12/12">
-              <Box display="flex" justifyContent="flexStart" printHidden>
-                <Box paddingRight={2}>
+              <Box
+                display="flex"
+                flexDirection="row"
+                flexWrap="wrap"
+                justifyContent="flexStart"
+                printHidden
+              >
+                <Box paddingRight={2} marginBottom={[1, 1, 1, 0]}>
                   <Button
                     colorScheme="default"
                     icon="receipt"
@@ -267,7 +273,7 @@ const VehicleDetail: ServicePortalModuleComponent = () => {
                     {formatMessage(messages.vehicleHistoryReport)}
                   </Button>
                 </Box>
-                <Box paddingRight={2}>
+                <Box paddingRight={2} marginBottom={[1, 1, 1, 0]}>
                   <a
                     href={SAMGONGUSTOFA_LINK}
                     target="_blank"
