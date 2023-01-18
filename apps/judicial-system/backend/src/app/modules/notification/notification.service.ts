bc8f0bffb3ada0333bc5beb4dda850e0545fa0aa
Ívar Oddsson <oddsson@ivars-imac.lan>
2023-01-18 14:50:56 +0000

    Send email to prisons when a custody case is rejected

--- apps/judicial-system/backend/src/app/modules/notification/notification.service.ts
+++ apps/judicial-system/backend/src/app/modules/notification/notification.service.ts
@@ -844,6 +844,26 @@ export class NotificationService {
     )
   }
 
+  private async sendRejectedCustodyEmailToPrison(
+    theCase: Case,
+  ): Promise<Recipient> {
+    const subject = this.formatMessage(
+      notifications.rejectedCustodyEmail.subject,
+      { courtCaseNumber: theCase.courtCaseNumber },
+    )
+    const body = this.formatMessage(notifications.rejectedCustodyEmail.body, {
+      court: theCase.court?.name,
+      courtCaseNumber: theCase.courtCaseNumber,
+    })
+
+    return this.sendEmail(
+      subject,
+      body,
+      'Gæsluvarðhaldsfangelsi',
+      this.config.email.prisonEmail,
+    )
+  }
+
   private async sendRulingNotifications(
     theCase: Case,
   ): Promise<SendNotificationResponse> {
@@ -885,7 +905,6 @@ export class NotificationService {
         this.sendRulingEmailNotificationToPrisonAdministration(theCase),
       )
     }
-
     if (
       CaseDecision.ACCEPTING === theCase.decision ||
       CaseDecision.ACCEPTING_PARTIALLY === theCase.decision
@@ -902,6 +921,20 @@ export class NotificationService {
         ) {
           promises.push(this.sendRulingEmailNotificationToPrison(theCase))
         }
+      }
+    } else if (
+      CaseType.CUSTODY === theCase.type &&
+      (CaseDecision.REJECTING === theCase.decision ||
+        CaseDecision.ACCEPTING_ALTERNATIVE_TRAVEL_BAN)
+    ) {
+      const prisonWasNotified = await this.hasReceivedNotification(
+        theCase.id,
+        NotificationType.COURT_DATE,
+        this.config.email.prisonEmail,
+      )
+
+      if (prisonWasNotified) {
+        promises.push(this.sendRejectedCustodyEmailToPrison(theCase))
       }
     }
 
