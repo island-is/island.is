ca101cbe57846548dd034af6b69d537a9c3237a0
runarvestmann <runar@stefna.is>
2023-01-19 13:52:48 +0000

    Reposition the background image

--- apps/web/components/ServiceWeb/Background/Variations/Sjukratryggingar/Sjukratryggingar.css.ts
+++ apps/web/components/ServiceWeb/Background/Variations/Sjukratryggingar/Sjukratryggingar.css.ts
@@ -7,7 +7,8 @@ export const container = style({
   bottom: 0,
   right: 0,
   backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
+  backgroundPositionX: 'center',
+  backgroundPositionY: 'top',
   backgroundSize: 'cover',
   backgroundImage:
     'url(https://images.ctfassets.net/8k0h54kbe6bj/7jzjEatxy6K1aAsKhT2f2V/a3ed3af04ae463c813dee0122a737197/Island-is-vefbanner__1_.jpg)',
