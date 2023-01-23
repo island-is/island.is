ce2ce89b2340d5c7c9990fdcb342e0191a43474a
Ívar Oddsson <oddsson@ivars-imac.lan>
2023-01-23 11:24:34 +0000

    Use generated InstitutionType type

--- apps/judicial-system/web/src/components/Logo/Logo.tsx
+++ apps/judicial-system/web/src/components/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { Box } from '@island.is/island-ui/core'
-
-import { InstitutionType } from '@island.is/judicial-system/types'
+import { InstitutionType } from '@island.is/judicial-system-web/src/graphql/schema'
 
 import { UserContext } from '../UserProvider/UserProvider'
 import LandWightsLogo from './LandWightsLogo'
