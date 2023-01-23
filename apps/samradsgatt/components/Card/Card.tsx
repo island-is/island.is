3588855078ec541e8a51e7404a2551468d09d9f0
diljath <diljath@advania.is>
2023-01-23 14:15:52 +0000

    cards design and layout

--- apps/samradsgatt/components/Card/Card.tsx
+++ apps/samradsgatt/components/Card/Card.tsx
@@ -1,11 +1,86 @@
-import { Tag, Box } from '@island.is/island-ui/core'
+import styled from 'styled-components'
+import { Tag, Box, Divider, Text, ArrowLink } from '@island.is/island-ui/core'
+import getTagVariants from '../../utils/helpers/getTagVariants'
 
-export const Card = () => {
+export const Card = (caseData) => {
   return (
-    <Box>
-      <Tag>Dummy</Tag>
+    <Box
+      style={{
+        width: '358px',
+        minWidth: '287px',
+        height: '460px',
+        flexWrap: 'wrap',
+      }}
+      padding={3}
+      // margin={1}
+      borderRadius="standard"
+      borderWidth="standard"
+      borderColor="purple300"
+      display="flex"
+      flexDirection="column"
+      justifyContent="spaceBetween"
+    >
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="spaceBetween"
+      >
+        <Tag variant={getTagVariants(caseData.status)}>{caseData.status}</Tag>
+        <Text as="p" variant="eyebrow" color="purple400">
+          {caseData.caseNumber}
+        </Text>
+      </Box>
+      <Box display="flex" flexDirection="row" alignItems="center" paddingY={1}>
+        <Text as="p" variant="eyebrow" color="blue600">
+          Stöðumat og valkostir
+        </Text>
+        <TagDivider />
+      </Box>
+      <Box style={{ height: '90px', overflow: 'hidden' }} paddingBottom={2}>
+        <Text as="h4" fontWeight="semiBold">
+          {caseData.name}
+        </Text>
+      </Box>
+
+      <Box display="flex" flexDirection="row" alignItems="center">
+        <Text variant="eyebrow" color="dark400">
+          Umsagnartímabil:
+        </Text>
+        <Text variant="eyebrow" color="blue600" marginY={1}>
+          01.09.22 – 01.12.22
+        </Text>
+      </Box>
+      <Box paddingY={1}>
+        <Divider />
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="spaceBetween"
+      >
+        <Text variant="eyebrow" color="purple400">
+          {`Fjöldi umsagna: ${caseData.adviceCount}`}
+        </Text>
+      </Box>
+      <Box style={{ minHeight: 132, lineBreak: 'anywhere' }}>
+        <Text variant="small" color="dark400">
+          {caseData.shortDescription}
+        </Text>
+      </Box>
+
+      <Box className="ArrowLinkContainer">
+        <ArrowLink href={`case/${caseData.id}`}>Skoða mál</ArrowLink>
+      </Box>
     </Box>
   )
 }
 
 export default Card
+
+const TagDivider = styled.div`
+  border-left: 1px solid #ccdfff;
+  height: 16px;
+  margin: 0 8px 0 8px;
+`
