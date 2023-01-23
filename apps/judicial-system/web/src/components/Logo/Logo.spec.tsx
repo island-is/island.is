4d52bf96f6a3783a26d02917e4cb67917d687968
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-01-23 10:12:58 +0000

    remove react-router-dom from test where since it is a Next.js project

--- apps/judicial-system/web/src/components/Logo/Logo.spec.tsx
+++ apps/judicial-system/web/src/components/Logo/Logo.spec.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { createMemoryRouter, RouterProvider } from 'react-router-dom'
 import { render, screen } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
-
-import * as constants from '@island.is/judicial-system/consts'
 
 import { UserProvider } from '../UserProvider/UserProvider'
 import { mockJudgeQuery } from '../../utils/mocks'
@@ -12,26 +9,14 @@ import Logo from './Logo'
 
 describe('Logo', () => {
   test('should display the current users institution', async () => {
-    // Arrange
-    const routes = [
-      {
-        path: constants.RESTRICTION_CASE_DEFENDANT_ROUTE,
-        element: (
-          <MockedProvider mocks={[...mockJudgeQuery]} addTypename={false}>
-            <UserProvider authenticated={true}>
-              <Logo />
-            </UserProvider>
-          </MockedProvider>
-        ),
-      },
-    ]
-
-    const router = createMemoryRouter(routes, {
-      initialEntries: [constants.RESTRICTION_CASE_DEFENDANT_ROUTE],
-    })
-
     // Act
-    render(<RouterProvider router={router} />)
+    render(
+      <MockedProvider mocks={[...mockJudgeQuery]} addTypename={false}>
+        <UserProvider authenticated={true}>
+          <Logo />
+        </UserProvider>
+      </MockedProvider>,
+    )
 
     // Assert
     expect(await screen.findByText('Héraðsdómur')).toBeInTheDocument()
