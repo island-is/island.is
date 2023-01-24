3a9c12e86acb77a6dd91c45f13e61046a2591efe
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-01-24 10:22:03 +0000

    fix: rename!

--- libs/clients/license-clients/src/lib/clients-license-clients.spec.ts
+++
@@ -1,7 +0,0 @@
-import { clientsLicenseClients } from './clients-license-clients'
-
-describe('clientsLicenseClients', () => {
-  it('should work', () => {
-    expect(clientsLicenseClients()).toEqual('clients-license-clients')
-  })
-})
