e382bbab0e6238fe66e713a15bf3d4480db031cb
diljath <diljath@advania.is>
2023-01-24 08:15:41 +0000

    added proptype to card

--- apps/samradsgatt/components/Card/Card.tsx
+++ apps/samradsgatt/components/Card/Card.tsx
@@ -1,8 +1,15 @@
-import styled from 'styled-components'
 import { Tag, Box, Divider, Text, ArrowLink } from '@island.is/island-ui/core'
 import getTagVariants from '../../utils/helpers/getTagVariants'
+type CardProps = {
+  caseNumber: string
+  status: string
+  name: string
+  adviceCount: number
+  shortDescription: string
+  id: number
+}
 
-export const Card = (caseData) => {
+export const Card = (caseData: CardProps) => {
   return (
     <Box
       style={{
@@ -35,7 +42,14 @@ export const Card = (caseData) => {
         <Text as="p" variant="eyebrow" color="blue600">
           Stöðumat og valkostir
         </Text>
-        <TagDivider />
+        <div
+          style={{
+            marginLeft: 8,
+            marginRight: 8,
+            height: 16,
+            border: '1px solid #ccdfff',
+          }}
+        />
       </Box>
       <Box style={{ height: '90px', overflow: 'hidden' }} paddingBottom={2}>
         <Text as="h4" fontWeight="semiBold">
@@ -78,9 +92,3 @@ export const Card = (caseData) => {
 }
 
 export default Card
-
-const TagDivider = styled.div`
-  border-left: 1px solid #ccdfff;
-  height: 16px;
-  margin: 0 8px 0 8px;
-`
