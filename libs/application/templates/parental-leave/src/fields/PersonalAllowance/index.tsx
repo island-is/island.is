b585438faf50113d3932fdbdc5d5f7877d8276b1
Veronika Sif <veronikasif@hotmail.com>
2023-01-25 14:10:30 +0000

    formatted code

--- libs/application/templates/parental-leave/src/fields/PersonalAllowance/index.tsx
+++ libs/application/templates/parental-leave/src/fields/PersonalAllowance/index.tsx
@@ -63,8 +63,8 @@ export const PersonalAllowance: FC<FieldBaseProps> = ({
               setValue(allowance + '.useAsMuchAsPossible', YES)
 
               // usage set to 1 so the user can go forward, because if user first selects
-              // YES in usePersonalAllowance, NO in useAsMuchAsPossible and inputs a invalid number 
-              // in usage then selects NO in usePersonalAllowance, the user cannot go forward but 
+              // YES in usePersonalAllowance, NO in useAsMuchAsPossible and inputs a invalid number
+              // in usage then selects NO in usePersonalAllowance, the user cannot go forward but
               // doesn't see the error message from usage.
               setValue(allowance + '.usage', '1')
             }
