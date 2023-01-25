3d503d5f34c26e25014c1b948deb01d68f46036b
Valur Einarsson <valure@live.com>
2023-01-25 14:36:42 +0000

    Remove unused dependency.

--- apps/services/activities/src/app/worker/activities.processor.ts
+++ apps/services/activities/src/app/worker/activities.processor.ts
@@ -1,22 +1,12 @@
 import { Process, Processor } from '@nestjs/bull'
 import { Job } from 'bull'
-import { Inject } from '@nestjs/common'
-import { ConfigType } from '@nestjs/config'
-import {
-  ActivitiesConfig,
-  activitiesQueueName,
-  sessionJobName,
-} from '../activities.config'
+import { activitiesQueueName, sessionJobName } from '../activities.config'
 import { SessionsService } from '../sessions/sessions.service'
 import { Session } from '../sessions/session.model'
 
 @Processor(activitiesQueueName)
 export class ActivitiesProcessor {
-  constructor(
-    private readonly sessionsService: SessionsService,
-    @Inject(ActivitiesConfig.KEY)
-    private config: ConfigType<typeof ActivitiesConfig>,
-  ) {}
+  constructor(private readonly sessionsService: SessionsService) {}
 
   @Process(sessionJobName)
   handleSessionActivity(job: Job<Session>): Promise<Session> {
