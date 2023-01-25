8e616b269e335aeeb6e1547da3825bf60a0e9611
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-01-25 10:11:29 +0000

    feat: rename gen

---
+++ libs/clients/license-client/README.md
@@ -0,0 +1,11 @@
+# Clients License Client
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test clients-license-client` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint clients-license-client` to execute the lint via [ESLint](https://eslint.org/).
---
+++ libs/clients/license-client/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'clients-license-client',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/clients/license-client',
+}
