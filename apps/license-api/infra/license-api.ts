c646a888a99b5f7df4c1b1a799d98106bdb68f8f
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-01-26 14:43:05 +0000

    fix: update secrets

--- apps/license-api/infra/license-api.ts
+++ apps/license-api/infra/license-api.ts
@@ -9,20 +9,12 @@ export const serviceSetup = (): ServiceBuilder<'license-api'> =>
       requests: { cpu: '200m', memory: '256Mi' },
     })
     .env({
-      SMART_SOLUTIONS_API_URL: '/k8s/api/SMART_SOLUTIONS_API_URL',
       IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/api',
-      RLS_PKPASS_API_KEY: '/k8s/api/RLS_PKPASS_API_KEY',
-      RLS_OPEN_LOOKUP_API_KEY: '/k8s/api/RLS_OPEN_LOOKUP_API_KEY',
       IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
         staging: 'https://identity-server.staging01.devland.is',
         prod: 'https://innskra.island.is',
       },
-      FIREARM_LICENSE_PASS_TEMPLATE_ID:
-        '/k8s/api/FIREARM_LICENSE_PASS_TEMPLATE_ID',
-      TR_PKPASS_API_KEY: '/k8s/api/TR_PKPASS_API_KEY',
-      DISABILITY_LICENSE_PASS_TEMPLATE_ID:
-        '/k8s/DISABILITY_LICENSE_PASS_TEMPLATE_ID',
       SI_PUBLIC_IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
         staging: 'https://identity-server.staging01.devland.is',
@@ -33,6 +25,14 @@ export const serviceSetup = (): ServiceBuilder<'license-api'> =>
     .secrets({
       IDENTITY_SERVER_CLIENT_SECRET:
         '/k8s/download-service/IDENTITY_SERVER_CLIENT_SECRET',
+      SMART_SOLUTIONS_API_URL: '/k8s/api/SMART_SOLUTIONS_API_URL',
+      RLS_PKPASS_API_KEY: '/k8s/api/RLS_PKPASS_API_KEY',
+      RLS_OPEN_LOOKUP_API_KEY: '/k8s/api/RLS_OPEN_LOOKUP_API_KEY',
+      FIREARM_LICENSE_PASS_TEMPLATE_ID:
+        '/k8s/api/FIREARM_LICENSE_PASS_TEMPLATE_ID',
+      TR_PKPASS_API_KEY: '/k8s/api/TR_PKPASS_API_KEY',
+      DISABILITY_LICENSE_PASS_TEMPLATE_ID:
+        '/k8s/DISABILITY_LICENSE_PASS_TEMPLATE_ID',
     })
     .xroad(Base, Client, Firearm)
     .ingress({
