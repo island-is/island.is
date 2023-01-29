93304fab8a4bc521e356413469e29db49968060d
karenbjorg <khalldorsdottir@deloitte.is>
2023-01-29 21:45:20 +0000

    fix questions!

--- libs/application/templates/parental-leave/src/lib/parentalLeaveUtils.ts
+++ libs/application/templates/parental-leave/src/lib/parentalLeaveUtils.ts
@@ -880,7 +880,7 @@ export const isParentWithoutBirthParent = (answers: Application['answers']) => {
     'noPrimaryParent.questionThree',
   )
 
-  return questionOne === YES && questionTwo === YES && questionThree === YES
+  return questionOne === YES && questionTwo === YES && questionThree === NO
 }
 
 export const isNotEligibleForParentWithoutBirthParent = (
@@ -893,7 +893,7 @@ export const isNotEligibleForParentWithoutBirthParent = (
     'noPrimaryParent.questionThree',
   )
 
-  return questionOne === NO || questionTwo === NO || questionThree === NO
+  return questionOne === NO || questionTwo === NO || questionThree === YES
 }
 
 export const requiresOtherParentApproval = (
