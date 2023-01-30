87762fc9cfd36cfb1239b4b575f94338027e5e82
Petar Shomov <petar@andes.is>
2023-01-30 15:27:47 +0000

    trying to control parallelism of tests

--- scripts/ci/30_test.sh
+++ scripts/ci/30_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-SERVERSIDE_FEATURES_ON=\"\" yarn run test "${APP}" --codeCoverage --verbose
+SERVERSIDE_FEATURES_ON=\"\" yarn run test "${APP}" --codeCoverage --verbose --maxWorkers=2
