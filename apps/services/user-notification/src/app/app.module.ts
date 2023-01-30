66b92d9eef6bd801281f2a221637227075646fff
Rafn Árnason <rafnarnason@gmail.com>
2023-01-30 13:12:10 +0000

    cleanup

--- apps/services/user-notification/src/app/app.module.ts
+++ apps/services/user-notification/src/app/app.module.ts
@@ -1,26 +1,7 @@
-import { CacheModule, Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { NotificationsModule } from './modules/notifications/notifications.module'
 
 @Module({
-  imports: [
-    // CacheModule.register({
-    //   ttl: 5, // seconds
-    //   max: 1000, // maximum number of items in cache
-    // }),
-    NotificationsModule,
-  ],
+  imports: [NotificationsModule],
 })
 export class AppModule {}
-
-/// WRONG PLACE ?'???????????????????????????'
-
-// import { CacheModule, Module, CacheInterceptor } from '@nestjs/common'
-// import { NotificationsController } from './modules/notifications/notifications.controller'
-// import { APP_INTERCEPTOR } from '@nestjs/core'
-// import { NotificationsModule } from './modules/notifications/notifications.module'
-
-// @Module({
-//   imports: [NotificationsModule, CacheModule.register({ isGlobal: true })],
-//   controllers: [NotificationsController],
-// })
-// export class AppModule {}
