5287d9f2e74972f2a64409b4761a2bddce679d71
Petar Shomov <petar@andes.is>
2023-01-31 12:53:27 +0000

    trying to ignore problematic tests that will not produce coverage

--- scripts/ci/30_test.sh
+++ scripts/ci/30_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-SERVERSIDE_FEATURES_ON=\"\" NODE_OPTIONS="--max-old-space-size=4096 --unhandled-rejections=warn" yarn run test "${APP}" --codeCoverage --verbose --runInBand "$@"
+SERVERSIDE_FEATURES_ON=\"\" NODE_OPTIONS="--max-old-space-size=4096 --unhandled-rejections=warn" yarn run test "${APP}" --codeCoverage --verbose --maxWorkers=2 --no-watchman "$@"
