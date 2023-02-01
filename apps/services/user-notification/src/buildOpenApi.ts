33d51e0fd536bfe40eb0f839e92607e3e94c00e8
Rafn Árnason <rafnarnason@gmail.com>
2023-02-01 15:31:29 +0000

    buildmodule culprit?

--- apps/services/user-notification/src/buildOpenApi.ts
+++ apps/services/user-notification/src/buildOpenApi.ts
@@ -1,37 +1,41 @@
-// import { Module } from '@nestjs/common'
-// import { buildOpenApi } from '@island.is/infra-nest-server'
-// import { QueueModule } from '@island.is/message-queue'
-// import { openApi } from './openApi'
-// import { NotificationsController } from './app/modules/notifications/notifications.controller'
-// import { AppModule } from './app/app.module'
+import { CacheModule, Module } from '@nestjs/common'
+import { buildOpenApi } from '@island.is/infra-nest-server'
+import { QueueModule } from '@island.is/message-queue'
+import { openApi } from './openApi'
+import { NotificationsController } from './app/modules/notifications/notifications.controller'
+import { AppModule } from './app/app.module'
 
-// @Module({
-//   imports: [
-//     QueueModule.register({
-//       client: {},
-//       queue: {
-//         name: 'notifications',
-//         queueName: '',
-//       },
-//     }),
-//   ],
-//   controllers: [NotificationsController],
-// })
-// // class BuildModule {}
+@Module({
+  imports: [
+    CacheModule.register({
+      ttl: 60,
+      max: 100,
+    }),
+    QueueModule.register({
+      client: {},
+      queue: {
+        name: 'notifications',
+        queueName: '',
+      },
+    }),
+  ],
+  controllers: [NotificationsController],
+})
+class BuildModule {}
+
+buildOpenApi({
+  path: 'apps/services/user-notification/src/openapi.yaml',
+  appModule: BuildModule,
+  openApi,
+})
+
+// import { buildOpenApi } from '@island.is/infra-nest-server'
+
+// import { AppModule } from './app/app.module'
+// import { openApi } from './openApi'
 
 // buildOpenApi({
 //   path: 'apps/services/user-notification/src/openapi.yaml',
 //   appModule: AppModule,
 //   openApi,
 // })
-
-import { buildOpenApi } from '@island.is/infra-nest-server'
-
-import { AppModule } from './app/app.module'
-import { openApi } from './openApi'
-
-buildOpenApi({
-  path: 'apps/services/user-notification/src/openapi.yaml',
-  appModule: AppModule,
-  openApi,
-})
