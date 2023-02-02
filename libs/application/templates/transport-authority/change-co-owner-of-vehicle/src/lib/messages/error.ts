aae6d4acb3214125b6afb8552a40478f13548d1a
Jóhanna Magnúsdóttir <johanna.agnes.magnusdottir@origo.is>
2023-02-02 14:31:17 +0000

    Dont validate min age for companies Add min age validation to COV and CCOV

--- libs/application/templates/transport-authority/change-co-owner-of-vehicle/src/lib/messages/error.ts
+++ libs/application/templates/transport-authority/change-co-owner-of-vehicle/src/lib/messages/error.ts
@@ -12,4 +12,10 @@ export const error = defineMessages({
     description:
       'Error message if there was no name associated with given national id',
   },
+  minAgeNotFulfilled: {
+    id: 'ta.ccov.application:error.minAgeNotFulfilled',
+    defaultMessage:
+      'Lágmarksaldur til að taka þátt í tilkynnu um eigendaskipti er 18 ára',
+    description: 'Min age not fulfilled error',
+  },
 })
