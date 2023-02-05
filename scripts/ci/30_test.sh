0a64577731fbee5bb574c22e8e35670869b0ea5c
Petar Shomov <petar@andes.is>
2023-02-05 10:15:19 +0000

    ignoring uncollectible services for now

--- scripts/ci/30_test.sh
+++ scripts/ci/30_test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-SERVERSIDE_FEATURES_ON=\"\" NODE_OPTIONS="--max-old-space-size=4096 --unhandled-rejections=warn" yarn run test "${APP}" --codeCoverage --verbose --no-watchman "$@"
+EXTRA_OPTS=""
+
+projects_uncollectible_coverage=("contentful-translation-extension" "application-templates-no-debt-certificate" "api-domains-email-signup" "skilavottord-web" "shared-babel")
+if [[ ! " ${projects_uncollectible_coverage[@]} " =~ " ${APP} " ]]; then
+  EXTRA_OPTS="--codeCoverage"
+fi
+
+SERVERSIDE_FEATURES_ON=\"\" NODE_OPTIONS="--max-old-space-size=4096 --unhandled-rejections=warn" yarn run test "${APP}" "${EXTRA_OPTS}" --verbose --no-watchman "$@"
