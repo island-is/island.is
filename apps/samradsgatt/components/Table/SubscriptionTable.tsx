ff28a5b3a0328e659bfe74054d1534ce101adcd4
karlarnar <karl.arnar.aegisson@advania.is>
2023-02-06 14:02:47 +0000

    mvp is same as figma

--- apps/samradsgatt/components/Table/SubscriptionTable.tsx
+++ apps/samradsgatt/components/Table/SubscriptionTable.tsx
@@ -1,19 +1,50 @@
-import { Table as T } from '@island.is/island-ui/core'
+import { Icon, Table as T, Checkbox, Text, Box } from '@island.is/island-ui/core'
+import { useState } from 'react'
+import isEvenCheck from '../../utils/helpers/isEvenCheck'
+import SubscriptionDummyData from '../DummyData/SubscriptionDummyData'
 
-const SubscriptionTable = () => {
+const Headers = {
+    cases: ['Málsnr.', 'Heiti máls'],
+    institutions: ['Stofnun'],
+    policyAreas: ['Málefnasvið']
+}
+
+
+
+const BackgroundColor = (idx: number) => {
+    const isEven = isEvenCheck(idx)
+    return isEven ? 'blue100' : 'transparent'
+}
+
+const SubscriptionTable = ({ }) => {
+    const chosenTab = "cases"
+    const dummyData = SubscriptionDummyData[chosenTab]
+
+    let headerKey = 0
+
+    const onCheckboxChange = (idx: number) => {
+        
+    }
+
     return (
         <T.Table>
             <T.Head>
                 <T.Row>
-                <T.HeadData box={{ background: 'transparent', borderColor: 'transparent' }}>Header</T.HeadData>
-                <T.HeadData box={{ background: 'transparent', borderColor: 'transparent' }}>Header2</T.HeadData>
+                <T.HeadData box={{ marginRight: "none" ,background: 'transparent', borderColor: 'transparent' }} key={headerKey++}><Icon icon="checkmark" /></T.HeadData>
+                {Headers[chosenTab].map((header) => (
+                    <T.HeadData box={{ background: 'transparent', borderColor: 'transparent' }} key={headerKey++}>{header}</T.HeadData>
+                ))}
                 </T.Row>
+                
             </T.Head>
             <T.Body>
-                <T.Row>
-                <T.Data borderColor="transparent">Data</T.Data>
-                <T.Data borderColor="transparent" >Data2</T.Data>
-                </T.Row>
+                {dummyData.map((data, idx) => (
+                    <T.Row key={data.id }>
+                        <T.Data borderColor="transparent" box={{ borderRadius: 'standard', background: BackgroundColor(idx)}}><Checkbox checked={data.checked} onChange={() => onCheckboxChange(data.id)} /></T.Data>
+                        <T.Data borderColor="transparent" box={{ background: BackgroundColor(idx)}}><Text variant="h5">{data.caseNumber}</Text></T.Data>
+                        <T.Data borderColor="transparent" box={{ borderRadius: 'standard', background: BackgroundColor(idx)}}><Text variant="h5">{data.caseTitle}</Text></T.Data>
+                    </T.Row>
+                ))}
             </T.Body>
         </T.Table>
     )
--- apps/samradsgatt/pages/askriftir/index.tsx
+++ apps/samradsgatt/pages/askriftir/index.tsx
@@ -2,6 +2,8 @@ import {
     Box,
     Breadcrumbs,
     GridContainer,
+    GridRow,
+    ResponsiveSpace,
     Text,
   } from '@island.is/island-ui/core'
   import SubscriptionTable from '../../components/Table/SubscriptionTable'
@@ -12,35 +14,53 @@ import {
   const Subscriptions = () => {
     const location = useLocation()
     const [currentTab, setCurrentTab] = useState('Mál')
+    const paddingXBreadCrumbs = [3, 3, 3, 4] as ResponsiveSpace
+    const paddingYBreadCrumbs = [3, 3, 3, 5] as ResponsiveSpace
+    const paddingXContent = [3, 3, 3, 22] as ResponsiveSpace
+    const paddingXTable = [0, 0, 0, 22] as ResponsiveSpace
+    const paddingBottom = [5, 5, 5, 5] as ResponsiveSpace
   
     return (
       <Layout>
         <GridContainer>
-          <Box paddingY={4}>
-            <Breadcrumbs
-              items={[
-                { title: 'Samráðsgátt', href: '/samradsgatt' },
-                { title: 'Mínar áskriftir ', href: location.href },
-                { title: currentTab, href: `${location.href}/${currentTab}` },
-              ]}
-            />
-          </Box>
-          <Box>
-            <Text variant="h1" color="dark400">
-              {'Áskriftir'}
-            </Text>
-            <Text variant="default">
-              {
-                'Hér er hægt að skrá sig í áskrift að málum. Þú skráir þig inn á Ísland.is, \
-                hakar við einn eða fleiri flokka, velur hvort þú vilt tilkynningar um ný mál \
-                eða fleiri atriði og smellir á „Staðfesta“. ferð svo og staðfestir áskriftina \
-                í gegnum tölvupóstfangið sem þú varst að skrá. Kerfið er uppfært einu sinni á sólarhring.'
-              }
-            </Text>
-          </Box>
-          <Box>
-            <SubscriptionTable />
-          </Box>
+            <GridRow>
+                <Box paddingY={paddingYBreadCrumbs} paddingX={paddingXBreadCrumbs}>
+                    <Breadcrumbs
+                    items={[
+                        { title: 'Samráðsgátt', href: '/samradsgatt' },
+                        { title: 'Mínar áskriftir ', href: location.href },
+                        { title: currentTab, href: `${location.href}/${currentTab}` },
+                    ]}
+                    />
+                </Box>
+            </GridRow>
+            <GridRow>
+                <Box paddingX={paddingXContent} paddingBottom={paddingBottom}>
+                    <Text variant="h1" color="dark400">
+                    {'Áskriftir'}
+                    </Text>
+                </Box>
+            </GridRow>
+            <GridRow>
+                <Box paddingX={paddingXContent} paddingBottom={paddingBottom}>
+                    <Text variant="default">
+                    {
+                        'Hér er hægt að skrá sig í áskrift að málum. Þú skráir þig inn á Ísland.is, \
+                        hakar við einn eða fleiri flokka, velur hvort þú vilt tilkynningar um ný mál \
+                        eða fleiri atriði og smellir á „Staðfesta“. ferð svo og staðfestir áskriftina \
+                        í gegnum tölvupóstfangið sem þú varst að skrá.'
+                    }
+                    </Text>
+                    <Text variant="default" paddingTop={2}>
+                        {'Kerfið er uppfært einu sinni á sólarhring.'}
+                    </Text>
+                </Box>
+            </GridRow>
+          <GridRow>
+            <Box paddingX={paddingXTable}>
+                <SubscriptionTable />
+            </Box>
+          </GridRow>
         </GridContainer>
       </Layout>
     )
