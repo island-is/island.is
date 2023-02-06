a96c90952a660b56525b3369de890cd1936b245d
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2023-02-06 13:10:47 +0000

    Create PhoneInput.spec.tsx

---
+++ libs/island-ui/core/src/lib/PhoneInput/PhoneInput.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import { PhoneInput } from './PhoneInput'
+
+describe(' PhoneInput', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <PhoneInput label="Phone" name="p1" countryCodes={[]} />,
+    )
+    expect(baseElement).toBeTruthy()
+  })
+})
