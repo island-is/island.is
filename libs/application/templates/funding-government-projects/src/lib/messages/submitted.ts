f24f443f0e17706df33847d551d22c95177fc4c3
egill15 <egillb86@gmail.com>
2023-02-06 08:39:42 +0000

    Update submitted.ts

--- libs/application/templates/funding-government-projects/src/lib/messages/submitted.ts
+++ libs/application/templates/funding-government-projects/src/lib/messages/submitted.ts
@@ -4,7 +4,7 @@ const bulletOne =
   'Við munum nú fara yfir verkefnið og við sendum á þig svör innan tíðar.'
 const bulletTwo = 'Við verðum í sambandi ef okkur vantar frekari upplýsingar.'
 const bulletThree =
-  'Ef þú þarft frekari upplýsingar þá getur þú haft samband í síma 847 3759 eða á netfangið [island@island.is](island@island.is)'
+  'Ef þú þarft frekari upplýsingar þá getur þú haft samband í síma 847 3759 eða á netfangið [island@island.is](mailto:island@island.is)'
 
 export const submitted = {
   general: defineMessages({
