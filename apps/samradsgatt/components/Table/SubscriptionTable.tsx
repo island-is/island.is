12e0659d429c2119389632ca95016f974ffee267
karlarnar <karl.arnar.aegisson@advania.is>
2023-02-07 14:18:52 +0000

    KAM-761 data shows up in tab

--- apps/samradsgatt/components/Table/SubscriptionTable.tsx
+++ apps/samradsgatt/components/Table/SubscriptionTable.tsx
@@ -4,61 +4,104 @@ import * as styles from './SubscriptionTable.css'
 import tableRowBackgroundColor from '../../utils/helpers/tableRowBackgroundColor'
 
 const Headers = {
-    'Mál': ['Málsnr.', 'Heiti máls'],
-    'Stofnanir': ['Stofnun'],
-    'Málefnasvið': ['Málefnasvið']
+  Mál: ['Málsnr.', 'Heiti máls'],
+  Stofnanir: ['Stofnun'],
+  Málefnasvið: ['Málefnasvið'],
 }
 
 const MapIsToEn = {
-    'Mál': 'caseIds',
-    'Stofnanir': 'institutionIds',
-    'Málefnasvið': 'policyAreaIds'
+  Mál: 'caseIds',
+  Stofnanir: 'institutionIds',
+  Málefnasvið: 'policyAreaIds',
 }
 
-const SubscriptionTable = ({ data, currentTab, subscriptionArray, setSubscriptionArray }) => {
-    let headerKey = 0
+const SubscriptionTable = ({
+  data,
+  currentTab,
+  subscriptionArray,
+  setSubscriptionArray,
+}) => {
+  let headerKey = 0
 
-    const onCheckboxChange = (id: number, action: boolean) => {
-        const sub = [...subscriptionArray[MapIsToEn[currentTab]]]
-        const subArr = {...subscriptionArray}
-        if(action) {
-            sub.push(id)
-        } else {
-            const idx = sub.indexOf(id)
-            sub.splice(idx, 1)
-        }
-        subArr[MapIsToEn[currentTab]] = sub
-        return setSubscriptionArray(subArr)
+  const onCheckboxChange = (id: number, action: boolean) => {
+    const sub = [...subscriptionArray[MapIsToEn[currentTab]]]
+    const subArr = { ...subscriptionArray }
+    if (action) {
+      sub.push(id)
+    } else {
+      const idx = sub.indexOf(id)
+      sub.splice(idx, 1)
     }
+    subArr[MapIsToEn[currentTab]] = sub
+    return setSubscriptionArray(subArr)
+  }
 
-    const checkboxStatus = (id: number) => {
-        return subscriptionArray[MapIsToEn[currentTab]].includes(id)
-    }
+  const checkboxStatus = (id: number) => {
+    return subscriptionArray[MapIsToEn[currentTab]].includes(id)
+  }
 
-    return (
-        <T.Table>
-            <T.Head>
-                <T.Row>
-                <T.HeadData box={{ background: 'transparent', borderColor: 'transparent' }} key={headerKey++}><Icon icon="checkmark" color="blue400" className={styles.checkmarkIcon} /></T.HeadData>
-                {Headers[currentTab].map((header) => (
-                    <T.HeadData text={{ variant: 'h4'}} box={{ background: 'transparent', borderColor: 'transparent' }} key={headerKey++}>{header}</T.HeadData>
-                ))}
-                </T.Row>
-                
-            </T.Head>
-            <T.Body>
-                {data.map((item, idx) => (
-                    <T.Row key={item.id }>
-                        <T.Data borderColor="transparent" box={{ className: styles.tableRowLeft, background: tableRowBackgroundColor(idx)}}><Checkbox checked={checkboxStatus(item.id)} onChange={(e) => onCheckboxChange(item.id, e.target.checked)} /></T.Data>
-                        {currentTab == 'Mál' && (
-                            <T.Data borderColor="transparent" box={{ background: tableRowBackgroundColor(idx)}}><Text variant="h5">{item.caseNumber}</Text></T.Data>
-                        )}
-                        <T.Data borderColor="transparent" box={{ className: styles.tableRowRight, background: tableRowBackgroundColor(idx)}}><Text variant="medium">{item.name}</Text></T.Data>
-                    </T.Row>
-                ))}
-            </T.Body>
-        </T.Table>
-    )
+  return (
+    <T.Table>
+      <T.Head>
+        <T.Row>
+          <T.HeadData
+            box={{ background: 'transparent', borderColor: 'transparent' }}
+            key={headerKey++}
+          >
+            <Icon
+              icon="checkmark"
+              color="blue400"
+              className={styles.checkmarkIcon}
+            />
+          </T.HeadData>
+          {Headers[currentTab].map((header) => (
+            <T.HeadData
+              text={{ variant: 'h4' }}
+              box={{ background: 'transparent', borderColor: 'transparent' }}
+              key={headerKey++}
+            >
+              {header}
+            </T.HeadData>
+          ))}
+        </T.Row>
+      </T.Head>
+      <T.Body>
+        {data.map((item, idx) => (
+          <T.Row key={item.id}>
+            <T.Data
+              borderColor="transparent"
+              box={{
+                className: styles.tableRowLeft,
+                background: tableRowBackgroundColor(idx),
+              }}
+            >
+              <Checkbox
+                checked={checkboxStatus(item.id)}
+                onChange={(e) => onCheckboxChange(item.id, e.target.checked)}
+              />
+            </T.Data>
+            {currentTab == 'Mál' && (
+              <T.Data
+                borderColor="transparent"
+                box={{ background: tableRowBackgroundColor(idx) }}
+              >
+                <Text variant="h5">{item.caseNumber}</Text>
+              </T.Data>
+            )}
+            <T.Data
+              borderColor="transparent"
+              box={{
+                className: styles.tableRowRight,
+                background: tableRowBackgroundColor(idx),
+              }}
+            >
+              <Text variant="medium">{item.name}</Text>
+            </T.Data>
+          </T.Row>
+        ))}
+      </T.Body>
+    </T.Table>
+  )
 }
 
 export default SubscriptionTable
--- apps/samradsgatt/pages/askriftir/index.tsx
+++ apps/samradsgatt/pages/askriftir/index.tsx
@@ -1,102 +1,119 @@
 import {
-    Box,
-    Breadcrumbs,
-    Button,
-    ContentBlock,
-    GridContainer,
-    ResponsiveSpace,
-    Tabs,
-    Text,
-  } from '@island.is/island-ui/core' 
-  import SubscriptionTable from '../../components/Table/SubscriptionTable'
-  import { useState } from 'react'
-  import { Layout } from '../../components/Layout/Layout'
+  Box,
+  Breadcrumbs,
+  Button,
+  ContentBlock,
+  GridContainer,
+  ResponsiveSpace,
+  Tabs,
+  Text,
+} from '@island.is/island-ui/core'
+import SubscriptionTable from '../../components/Table/SubscriptionTable'
+import { useState } from 'react'
+import { Layout } from '../../components/Layout/Layout'
 import Cases from '../../utils/dummydata/api/Cases'
 import SubscriptionArray from '../../utils/dummydata/api/User/Subscriptions'
-  
-  const Subscriptions = () => {
-    const [currentTab, setCurrentTab] = useState('Mál')
-    const [casesData, setCasesData] = useState(Cases)
-    const [subscriptionArray, setSubscriptionArray] = useState(SubscriptionArray)
-    const settingSubscriptionArray = (newSubscriptionArray) => 
-                                      setSubscriptionArray(newSubscriptionArray)
-    const paddingYBreadCrumbs = [3, 3, 3, 5] as ResponsiveSpace
-    const paddingXContent = [0, 0, 0, 15] as ResponsiveSpace
-    const paddingXTable = [0, 0, 0, 15] as ResponsiveSpace
-    const paddingBottom = [3, 3, 3, 3] as ResponsiveSpace
+import Types from '../../utils/dummydata/api/Types'
 
-    const onLoadMore = () => {
+const Subscriptions = () => {
+  const [currentTab, setCurrentTab] = useState('Mál')
+  const [casesData, setCasesData] = useState(Cases)
+  const [institutionsData, setInstitutionsData] = useState(
+    Object.entries(Types.institutions).map(([id, name]) => ({ id, name })),
+  )
+  const [subscriptionArray, setSubscriptionArray] = useState(SubscriptionArray)
+  const settingSubscriptionArray = (newSubscriptionArray) =>
+    setSubscriptionArray(newSubscriptionArray)
+  const paddingYBreadCrumbs = [3, 3, 3, 5] as ResponsiveSpace
+  const paddingXContent = [0, 0, 0, 15] as ResponsiveSpace
+  const paddingXTable = [0, 0, 0, 15] as ResponsiveSpace
+  const paddingBottom = [3, 3, 3, 3] as ResponsiveSpace
 
-    }
+  const onLoadMore = () => {}
 
-    const CasesContent = () => {
-        return (
-            <SubscriptionTable data={casesData} currentTab={currentTab} 
-                               subscriptionArray={subscriptionArray}
-                               setSubscriptionArray={settingSubscriptionArray} />
-        )
-    }
-
-    const tabTest = [
-        {
-            id: 'Mál',
-            label: 'Mál',
-            content: <CasesContent />,
-            disabled: false
-        },
-        {
-            id: 'Stofnanir',
-            label: 'Stofnanir',
-            content: <Text>boi</Text>,
-            disabled: false
-        }
-    ]
-  
+  const CasesContent = () => {
     return (
-      <Layout>
-        <GridContainer>
-          <Box paddingY={paddingYBreadCrumbs}>
-              <Breadcrumbs
-              items={[
-                  { title: 'Samráðsgátt', href: '/samradsgatt' },
-                  { title: 'Mínar áskriftir ', href: '/samradsgatt/askriftir' },
-                  { title: currentTab },
-              ]}
-              />
-          </Box>
-          <Box paddingX={paddingXContent} paddingBottom={3}>
-              <Text variant="h1" color="dark400">
-              {'Áskriftir'}
-              </Text>
-          </Box>
-                <Box paddingX={paddingXContent} paddingBottom={5}>
-                    <Text variant="default">
-                    {
-                        'Hér er hægt að skrá sig í áskrift að málum. Þú skráir þig inn á Ísland.is, \
+      <SubscriptionTable
+        data={casesData}
+        currentTab={currentTab}
+        subscriptionArray={subscriptionArray}
+        setSubscriptionArray={settingSubscriptionArray}
+      />
+    )
+  }
+
+  const InstitutionsContent = () => {
+    return (
+      <SubscriptionTable
+        data={institutionsData}
+        currentTab={'Stofnanir'}
+        subscriptionArray={subscriptionArray}
+        setSubscriptionArray={settingSubscriptionArray}
+      />
+    )
+  }
+
+  const tabTest = [
+    {
+      id: 'Mál',
+      label: 'Mál',
+      content: <CasesContent />,
+      disabled: false,
+    },
+    {
+      id: 'Stofnanir',
+      label: 'Stofnanir',
+      content: <InstitutionsContent />,
+      disabled: false,
+    },
+  ]
+
+  return (
+    <Layout>
+      <GridContainer>
+        <Box paddingY={paddingYBreadCrumbs}>
+          <Breadcrumbs
+            items={[
+              { title: 'Samráðsgátt', href: '/samradsgatt' },
+              { title: 'Mínar áskriftir ', href: '/samradsgatt/askriftir' },
+              { title: currentTab },
+            ]}
+          />
+        </Box>
+        <Box paddingX={paddingXContent} paddingBottom={3}>
+          <Text variant="h1" color="dark400">
+            {'Áskriftir'}
+          </Text>
+        </Box>
+        <Box paddingX={paddingXContent} paddingBottom={5}>
+          <Text variant="default">
+            {
+              'Hér er hægt að skrá sig í áskrift að málum. Þú skráir þig inn á Ísland.is, \
                         hakar við einn eða fleiri flokka, velur hvort þú vilt tilkynningar um ný mál \
                         eða fleiri atriði og smellir á „Staðfesta“. ferð svo og staðfestir áskriftina \
                         í gegnum tölvupóstfangið sem þú varst að skrá.'
-                    }
-                    </Text>
-                    <Text variant="default" paddingTop={2}>
-                        {'Kerfið er uppfært einu sinni á sólarhring.'}
-                    </Text>
-                </Box>
+            }
+          </Text>
+          <Text variant="default" paddingTop={2}>
+            {'Kerfið er uppfært einu sinni á sólarhring.'}
+          </Text>
+        </Box>
 
-            <ContentBlock>
-                <Tabs label="tabs" tabs={tabTest} />
-            </ContentBlock>
+        <Box paddingX={paddingXTable}>
+          <Tabs label="tabs" tabs={tabTest} />
+        </Box>
+        <Box
+          paddingX={paddingXContent}
+          paddingBottom={paddingBottom}
+          paddingTop={3}
+        >
+          <Button icon="eye" variant="text" onClick={onLoadMore}>
+            Sýna fleiri mál
+          </Button>
+        </Box>
+      </GridContainer>
+    </Layout>
+  )
+}
 
-            <Box paddingX={paddingXTable}>
-                
-            </Box>
-            <Box paddingX={paddingXContent} paddingBottom={paddingBottom} paddingTop={3}>
-                <Button icon="eye" variant="text" onClick={onLoadMore}>Sýna fleiri mál</Button>
-            </Box>
-        </GridContainer>
-      </Layout>
-    )
-  }
-  
-  export default Subscriptions
-  
+export default Subscriptions
