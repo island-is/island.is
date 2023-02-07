d4b425fe34b0301e734b595833e51418cba3296f
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-02-07 12:31:02 +0000

    Add Order enum to nest pagination library.

--- libs/nest/pagination/src/lib/dto/pageinfo.dto.ts
+++ libs/nest/pagination/src/lib/dto/pageinfo.dto.ts
@@ -1,5 +1,5 @@
 import { ObjectType, Field } from '@nestjs/graphql'
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 
 @ObjectType()
 export class PageInfoDto {
@@ -8,17 +8,17 @@ export class PageInfoDto {
   hasNextPage!: boolean
 
   @Field({ nullable: true })
-  @ApiProperty({ example: false })
+  @ApiPropertyOptional({ example: false })
   hasPreviousPage?: boolean
 
   @Field({ nullable: true })
-  @ApiProperty({
+  @ApiPropertyOptional({
     example: 'WyIwM2JmMWUwOS1hNWEwLTQyNDMtOTAxOC1mY2FhYjg4NTVkMTYiXQ==',
   })
   startCursor?: string
 
   @Field({ nullable: true })
-  @ApiProperty({
+  @ApiPropertyOptional({
     example: 'WyJmODY1MDAzMS03YTFkLTRhOTAtOWI2OC00ODg1YjlkZDZjZDgiXQ==',
   })
   endCursor?: string
