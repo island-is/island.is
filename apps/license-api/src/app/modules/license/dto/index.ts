7423aa49d0738958af2ba096eb130e8037fdc015
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-02-08 15:58:40 +0000

    fix: rollback

--- apps/license-api/src/app/modules/license/dto/index.ts
+++ apps/license-api/src/app/modules/license/dto/index.ts
@@ -1,3 +1,3 @@
-export * from './revokeLicense.input'
+export * from './revokeLicense.dto'
 export * from './updateLicense.dto'
-export * from './verifyLicense.input'
+export * from './verifyLicense.dto'
