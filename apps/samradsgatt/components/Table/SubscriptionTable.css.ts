87d2ee125ec0d9e4b493d8877e9a3f3e2f06a599
Karl Arnar <korsbacka@gmail.com>
2023-02-08 13:21:30 +0000

    feat(samradsgatt): KAM-761 fixing lint warnings

--- apps/samradsgatt/components/Table/SubscriptionTable.css.ts
+++ apps/samradsgatt/components/Table/SubscriptionTable.css.ts
@@ -1,4 +1,4 @@
-import { theme } from '@island.is/island-ui/theme'
+// import { theme } from '@island.is/island-ui/theme'
 import { style } from '@vanilla-extract/css'
 
 export const checkmarkIcon = style({
--- apps/samradsgatt/pages/askriftir/index.tsx
+++ apps/samradsgatt/pages/askriftir/index.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Breadcrumbs,
   Button,
-  ContentBlock,
   GridContainer,
   ResponsiveSpace,
   Tabs,
