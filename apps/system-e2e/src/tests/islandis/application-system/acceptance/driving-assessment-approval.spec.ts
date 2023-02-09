3e785b526156481fbe1bbf29e6154377dc7faa3c
Kristófer <kristofer@juni.is>
2023-02-09 14:23:05 +0000

    Updated assessment

--- apps/system-e2e/src/tests/islandis/application-system/acceptance/driving-assessment-approval.spec.ts
+++ apps/system-e2e/src/tests/islandis/application-system/acceptance/driving-assessment-approval.spec.ts
@@ -1,36 +1,56 @@
-import { test, expect } from '@playwright/test'
+import { expect, test as base, Page } from '@playwright/test'
+import {
+  disableI18n,
+  disablePreviousApplications,
+  disableObjectKey,
+} from '../../../../support/disablers'
+import { session } from '../../../../support/session'
 
-test('test', async ({ page }) => {
-  await page.goto(
-    'https://cognito.shared.devland.is/login?approval_prompt=force&client_id=3rhd7sv3ik7ejgbpb6hqc647f&redirect_uri=https%3A%2F%2Fauth.shared.devland.is%2Fdev%2Foauth2%2Fcallback&response_type=code&scope=openid&state=QCaZgBH_YF6YyPfanJVnEtRYiEWsnMxgzaV8tMcPj1Y%3Ahttps%3A%2F%2Fbeta.dev01.devland.is%2Fumsoknir%2Fakstursmat',
-  )
-  await page.getByRole('button', { name: 'Github' }).press('Tab')
-  await page.getByRole('textbox', { name: 'Username' }).press('Tab')
-  await page.getByRole('textbox', { name: 'Password' }).press('Shift+Tab')
-  await page
-    .getByRole('textbox', { name: 'Username' })
-    .fill('kristofer@juni.is')
-  await page.getByRole('textbox', { name: 'Username' }).press('Tab')
-  await page.getByRole('textbox', { name: 'Password' }).press('Control+Shift+V')
-  await page.getByRole('textbox', { name: 'Password' }).press('Enter')
-  await page.getByRole('textbox', { name: 'Símanúmer' }).fill('010-7789')
-  await page.getByRole('textbox', { name: 'Símanúmer' }).press('Enter')
-  await page.goto('https://beta.dev01.devland.is/umsoknir/akstursmat')
-  await page.goto(
-    'https://beta.dev01.devland.is/umsoknir/akstursmat/262fe822-37c5-4463-b766-b2ea80077d37',
-  )
-  await page.getByText('Ég hef kynnt mér ofangreint').first().click()
-  await page.getByTestId('proceed').click()
-  await page.getByLabel('Kennitala nemanda').click()
-  await page.getByLabel('Kennitala nemanda').fill('0101307789')
-  await page.getByLabel('Tölvupóstfang nemanda').click()
-  await page.getByLabel('Tölvupóstfang nemanda').fill('email@email.com')
-  await page.getByTestId('proceed').click()
-  await page
-    .getByText(
-      'Ég staðfesti að akstursmat hefur farið fram í samræmi við ákvæði í reglugerð um ',
-    )
-    .first()
-    .click()
-  await page.getByRole('button', { name: 'Staðfesta' }).click()
+const homeUrl = '/umsoknir/akstursmat'
+
+const applicationTest = base.extend<{ applicationPage: Page }>({
+  applicationPage: async ({ browser }, use) => {
+    const applicationContext = await session({
+      browser,
+      homeUrl,
+      phoneNumber: '0103019',
+      idsLoginOn: true,
+    })
+
+    const applicationPage = await applicationContext.newPage()
+    await disableObjectKey(applicationPage, 'existing')
+    await disablePreviousApplications(applicationPage)
+    await disableI18n(applicationPage)
+    await applicationPage.goto(homeUrl)
+    await expect(applicationPage).toBeApplication()
+    await use(applicationPage)
+
+    await applicationPage.close()
+    await applicationContext.close()
+  },
+})
+
+applicationTest.describe('Driving Asessment Approval', () => {
+  applicationTest('should pass all, except submit', async ({ applicationPage }) => {
+    const page = applicationPage
+    await page.goto(homeUrl);
+
+    // Data Providers
+    await page.getByTestId('agree-to-data-providers').click();
+    await page.getByTestId('proceed').click();
+
+    // Student info
+    await page.getByLabel('Kennitala nemanda').click();
+    await page.getByLabel('Kennitala nemanda').fill('0101307789');
+    await page.getByLabel('Tölvupóstfang nemanda').fill('email@domain.test');
+    await expect(page.getByText('Gervimaður útlönd')).toBeVisible()
+    await page.getByTestId('proceed').click();
+
+    // Confirmation
+    await page.getByText('Ég staðfesti að akstursmat hefur farið fram í samræmi við ákvæði í reglugerð um ').first().click();
+    await page.getByRole('button', { name: 'Staðfesta' }).click();
+
+    // Error message
+    await page.getByText('Nýtt akstursmat: Einstaklingur á fullnaðarskírteini, ekki er leyfilegt að senda ').dblclick();
+  });
 })
