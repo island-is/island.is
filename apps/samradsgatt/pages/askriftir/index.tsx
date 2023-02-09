3080d4c003b427fd242480d41fcfd7c5ca1b364d
Karl Arnar <korsbacka@gmail.com>
2023-02-09 15:23:38 +0000

    KAM-767 checkmark done

--- apps/samradsgatt/pages/askriftir/index.tsx
+++ apps/samradsgatt/pages/askriftir/index.tsx
@@ -2,6 +2,7 @@ import {
   AsyncSearchOption,
   Box,
   Breadcrumbs,
+  Checkbox,
   GridContainer,
   ResponsiveSpace,
   Tabs,
@@ -191,6 +192,25 @@ const Subscriptions = () => {
               }}
             />
           )}
+        </Box>
+
+        <Box paddingX={paddingXTable}>
+          <Text variant="eyebrow">Valin mál</Text>
+          <Box
+            borderColor={'blue400'}
+            borderRadius="large"
+            borderWidth="standard"
+            background="white"
+            paddingX={3}
+            paddingY={3}
+            rowGap={3}
+          >
+            <Box display="flex" flexDirection="row">
+              <Checkbox checked={true} onChange={(e) => {}} />
+              <Text>Name</Text>
+              <Text>Label</Text>
+            </Box>
+          </Box>
         </Box>
 
         <Box paddingX={paddingXTable}>
