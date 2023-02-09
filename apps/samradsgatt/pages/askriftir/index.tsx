b421f7dfbdc7c7c4967316c66016bbc957a723f9
Karl Arnar <korsbacka@gmail.com>
2023-02-09 13:14:58 +0000

    feat(samradsgatt): Kam 761 feature tab tafla af stofnunum (#9873)
    * DummyData added
    
    * mvp is same as figma
    
    * mvp for subscriptions
    
    * refactoring
    
    * checkmark virkni
    
    * KAM-761 dummydata
    
    * KAM-761 checkboxes are seperate from data
    
    * KAM-761 removing setData from SubscriptionTable
    
    * KAM-761 data shows up in tab
    
    * KAM-761 small style changes
    
    * onLoadMore
    
    * yarn.lock
    
    * feat(samradsgatt): KAM-761 fixing lint warnings
    
    * KAM-761 formatting
    
    * KAM-761 fix formatting
    
    ---------
    
    Co-authored-by: karlarnar <karl.arnar.aegisson@advania.is>

--- apps/samradsgatt/pages/askriftir/index.tsx
+++ apps/samradsgatt/pages/askriftir/index.tsx
@@ -4,168 +4,70 @@ import {
   Button,
   GridContainer,
   ResponsiveSpace,
+  Tabs,
   Text,
 } from '@island.is/island-ui/core'
 import SubscriptionTable from '../../components/Table/SubscriptionTable'
 import { useState } from 'react'
 import { Layout } from '../../components/Layout/Layout'
-
-type dummyCases = Array<info>
-type info = {
-  id: number
-  caseNumber: string
-  caseTitle: string
-  checked: boolean
-}
+import Cases from '../../utils/dummydata/api/Cases'
+import SubscriptionArray from '../../utils/dummydata/api/User/Subscriptions'
+import Types from '../../utils/dummydata/api/Types'
 
 const Subscriptions = () => {
-  const cases: dummyCases = [
-    {
-      id: 0,
-      caseNumber: 'Öll mál',
-      caseTitle: 'Tilkynningar um ný mál',
-      checked: false,
-    },
-    {
-      id: 1,
-      caseNumber: 'Öll mál',
-      caseTitle:
-        'Tilkynningar um ný mál, breyttan umsagnarfrest, umsagnarfrest sem er að renna út og birtingu niðurstaðna',
-      checked: false,
-    },
-    {
-      id: 2,
-      caseNumber: '26/2023',
-      caseTitle: 'Brottfall úreltra reglugerða á fjármálamarkaði',
-      checked: true,
-    },
-    {
-      id: 3,
-      caseNumber: '25/2023',
-      caseTitle:
-        'Drög að reglugerð um (2.) breytingu á reglugerð nr. 241/2004 um val, geymslu og meðferð lyfja á sjúkrahúsum og öðrum heilbrigðisstofnunum',
-      checked: false,
-    },
-    {
-      id: 4,
-      caseNumber: '24/2023',
-      caseTitle:
-        'Frumvarp til laga um breytingu á lögum um tæknifrjóvgun og notkun kynfrumna og fósturvísa manna til stofnfrumurannsókna, nr. 55/1996 (geymsla og nýting fósturvísa og kynfrumna)',
-      checked: false,
-    },
-    {
-      id: 5,
-      caseNumber: '23/2023',
-      caseTitle: 'Áform um breytingu á lögum um ríkislögmann, nr. 51/1985',
-      checked: false,
-    },
-    {
-      id: 6,
-      caseNumber: '2/2023',
-      caseTitle:
-        'Tillaga til þingsályktunar um aðgerðaáætlun gegn hatursorðræðu 2023-2026',
-      checked: false,
-    },
-    {
-      id: 7,
-      caseNumber: '22/2023',
-      caseTitle:
-        'Frumvarp til laga um breytingu á lögum nr. 116/2006, um stjórn fiskveiða (svæðaskipting strandveiða)',
-      checked: false,
-    },
-    {
-      id: 8,
-      caseNumber: '20/2023',
-      caseTitle: 'Neyðarbirgðir eldsneytis',
-      checked: false,
-    },
-    {
-      id: 9,
-      caseNumber: '15/2023',
-      caseTitle: 'Drög að reglugerð um notkunar mannalyfja af mannúðarástæðum',
-      checked: false,
-    },
-    {
-      id: 10,
-      caseNumber: '19/2023',
-      caseTitle: 'Reglugerð um atvinnusjúkdóma',
-      checked: false,
-    },
-    {
-      id: 11,
-      caseNumber: '21/2023',
-      caseTitle:
-        'Drög að frumvarpi til laga um breytingu á lögum um heilbrigðisstarfsmenn, nr. 34/2012 (heimilisofbeldi)',
-      checked: false,
-    },
-    {
-      id: 12,
-      caseNumber: '215/2022',
-      caseTitle: 'Reglugerð um íbúakosningar sveitarfélaga',
-      checked: false,
-    },
-    {
-      id: 13,
-      caseNumber: '253/2022',
-      caseTitle:
-        'Drög að þingsályktunartillögu um aðgerðaáætlun um þjónustu við eldra fólk 2023-2027',
-      checked: false,
-    },
-    {
-      id: 14,
-      caseNumber: '7/2023',
-      caseTitle:
-        'Bráðabirgðaniðurstöður starfshópa - Auðlindin okkar - stefna um sjávarútveg',
-      checked: false,
-    },
-    {
-      id: 15,
-      caseNumber: '18/2023',
-      caseTitle:
-        'Frumvarp til laga um breytingu á lögum um réttindi sjúklinga nr. 74/1997',
-      checked: false,
-    },
-    {
-      id: 16,
-      caseNumber: '12/2023',
-      caseTitle:
-        'Drög að reglugerð um útgáfu vottorða, álitsgerða, faglegra yfirlýsinga og skýrslur heilbrigðisstarfsmanna',
-      checked: false,
-    },
-    {
-      id: 17,
-      caseNumber: '17/2023',
-      caseTitle:
-        'Breyting á lögum í tengslum við raforkueftirlit Orkustofnunar',
-      checked: false,
-    },
-    {
-      id: 18,
-      caseNumber: '247/2022',
-      caseTitle:
-        'Drög að þingsályktunartillögu um aðgerðaáætlun í geðheilbrigðismálum 2023-2027',
-      checked: false,
-    },
-    {
-      id: 19,
-      caseNumber: '16/2023',
-      caseTitle:
-        'Drög að reglugerð um breytingu á reglugerð nr. 944/2014, um öryggi leikfanga og markaðssetningu þeirra á Evrópska efnahagssvæðinu',
-      checked: false,
-    },
-  ]
-
   const [currentTab, setCurrentTab] = useState('Mál')
-  const [data, setData] = useState(cases)
-  const settingData = (newData: dummyCases) => setData(newData)
-  const paddingYBreadCrumbs = [3, 3, 5, 5] as ResponsiveSpace
-  const paddingXContent = [0, 0, 15, 15] as ResponsiveSpace
-  const paddingXTable = [0, 0, 15, 15] as ResponsiveSpace
+  const [casesData, setCasesData] = useState(Cases)
+  const [institutionsData, setInstitutionsData] = useState(
+    Object.entries(Types.institutions).map(([id, name]) => ({ id, name })),
+  )
+  const [subscriptionArray, setSubscriptionArray] = useState(SubscriptionArray)
+  const settingSubscriptionArray = (newSubscriptionArray) =>
+    setSubscriptionArray(newSubscriptionArray)
+  const paddingYBreadCrumbs = [3, 3, 3, 5] as ResponsiveSpace
+  const paddingXContent = [0, 0, 0, 15] as ResponsiveSpace
+  const paddingXTable = [0, 0, 0, 15] as ResponsiveSpace
   const paddingBottom = [3, 3, 3, 3] as ResponsiveSpace
 
   const onLoadMore = () => {
     console.log('clicked on load more')
   }
+
+  const CasesContent = () => {
+    return (
+      <SubscriptionTable
+        data={casesData}
+        currentTab={'Mál'}
+        subscriptionArray={subscriptionArray}
+        setSubscriptionArray={settingSubscriptionArray}
+      />
+    )
+  }
+
+  const InstitutionsContent = () => {
+    return (
+      <SubscriptionTable
+        data={institutionsData}
+        currentTab={'Stofnanir'}
+        subscriptionArray={subscriptionArray}
+        setSubscriptionArray={settingSubscriptionArray}
+      />
+    )
+  }
+
+  const tabTest = [
+    {
+      id: 'Mál',
+      label: 'Mál',
+      content: <CasesContent />,
+      disabled: false,
+    },
+    {
+      id: 'Stofnanir',
+      label: 'Stofnanir',
+      content: <InstitutionsContent />,
+      disabled: false,
+    },
+  ]
 
   return (
     <Layout>
@@ -197,8 +99,16 @@ const Subscriptions = () => {
             {'Kerfið er uppfært einu sinni á sólarhring.'}
           </Text>
         </Box>
+
         <Box paddingX={paddingXTable}>
-          <SubscriptionTable data={data} setData={settingData} />
+          <Tabs
+            selected={currentTab}
+            onlyRenderSelectedTab={true}
+            label="Veldu tegund áskrifta"
+            tabs={tabTest}
+            contentBackground="transparent"
+            onChange={(e) => setCurrentTab(e)}
+          />
         </Box>
         <Box
           paddingX={paddingXContent}
