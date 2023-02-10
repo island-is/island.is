e9459f1052bd23b25d9aa2758f2ee473c8858738
Karl Arnar <korsbacka@gmail.com>
2023-02-10 16:21:18 +0000

    KAM-753 working on footer

---
+++ apps/samradsgatt/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import { Box, GridContainer, GridRow } from '@island.is/island-ui/core'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="flexStart"
+        padding={'none'}
+      >
+        <GridContainer>
+            <GridRow>
+                oi
+            </GridRow>
+            <GridRow>
+                boi
+            </GridRow>
+        </GridContainer>
+      </Box>
+    </footer>
+  )
+}
+
+export default Footer
