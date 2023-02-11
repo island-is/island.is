d96458a4271b3580df1ae707fb75ec29eddd2e62
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-02-11 14:51:17 +0000

    Get client and domain in client loader to get client Name and logo url

--- apps/services/sessions/sequelize.config.js
+++ apps/services/sessions/sequelize.config.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 module.exports = {
   development: {
-    username: 'dev_db',
-    password: 'dev_db',
-    database: 'dev_db',
+    username: 'dev_db' ?? process.env.DB_USER_SESSIONS,
+    password: 'dev_db' ?? process.env.DB_PASS_SESSIONS,
+    database: 'dev_db' ?? process.env.DB_NAME_SESSIONS,
     host: 'localhost',
     dialect: 'postgres',
-    port: 5434,
+    port: 5434 ?? process.env.DB_PORT_SESSIONS,
   },
   production: {
     username: process.env.DB_USER,
