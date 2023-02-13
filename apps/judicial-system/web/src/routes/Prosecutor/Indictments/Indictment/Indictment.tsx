6917ce7ee0c4decd6ad9c865bf4ab934b9c88dd6
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-02-13 14:46:25 +0000

    feat(j-s): Indictment Count Legal Arguments (#9920)
    * Adds validation to indictment introduction
    
    * Connect indictment count legal arguments to server
    
    * Generates legal arguments
    
    * Fixes law label and formatting of legal arguments
    
    * Moves legal arguments autofill text to Contentful
    
    * Moves law tag string to contentful
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/Indictment/Indictment.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Indictment/Indictment.tsx
@@ -56,6 +56,11 @@ const Indictment: React.FC = () => {
     deleteIndictmentCount,
     updateIndictmentCountState,
   } = useIndictmentCounts()
+  const [
+    indictmentIntroductionErrorMessage,
+    setIndictmentIntroductionErrorMessage,
+  ] = useState<string>('')
+  const [demandsErrorMessage, setDemandsErrorMessage] = useState<string>('')
 
   const stepIsValid = isTrafficViolationStepValidIndictments(workingCase)
 
@@ -63,7 +68,6 @@ const Indictment: React.FC = () => {
     (destination: string) => router.push(`${destination}/${workingCase.id}`),
     [workingCase.id],
   )
-  const [demandsErrorMessage, setDemandsErrorMessage] = useState<string>('')
 
   useDeb(workingCase, ['indictmentIntroduction', 'demands'])
 
@@ -198,22 +202,27 @@ const Indictment: React.FC = () => {
               strings.indictmentIntroductionPlaceholder,
             )}
             value={workingCase.indictmentIntroduction || ''}
+            errorMessage={indictmentIntroductionErrorMessage}
+            hasError={indictmentIntroductionErrorMessage !== ''}
             onChange={(event) =>
               removeTabsValidateAndSet(
                 'indictmentIntroduction',
                 event.target.value,
-                [],
+                ['empty'],
                 workingCase,
                 setWorkingCase,
+                indictmentIntroductionErrorMessage,
+                setIndictmentIntroductionErrorMessage,
               )
             }
             onBlur={(event) =>
               validateAndSendToServer(
                 'indictmentIntroduction',
                 event.target.value,
-                [],
+                ['empty'],
                 workingCase,
                 updateCase,
+                setIndictmentIntroductionErrorMessage,
               )
             }
             textarea
