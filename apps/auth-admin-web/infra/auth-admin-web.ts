883962efa6dda11c598d422f7e9abc3b4495aca9
Pétur Neisti Erlingsson <petur@petur.io>
2023-02-14 13:06:38 +0000

    fix(infra): Allow large headers in auth-admin-web (#9932)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/auth-admin-web/infra/auth-admin-web.ts
+++ apps/auth-admin-web/infra/auth-admin-web.ts
@@ -36,16 +36,32 @@ export const serviceSetup = (): ServiceBuilder<'auth-admin-web'> => {
         public: true,
         extraAnnotations: {
           dev: {
+            'nginx.ingress.kubernetes.io/client-header-buffer-size': '32k',
+            'nginx.ingress.kubernetes.io/large-client-header-buffers': '4 32k',
+            'nginx.ingress.kubernetes.io/proxy-buffers-number': '8',
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
+            'nginx.ingress.kubernetes.io/server-snippet': `|
+            client_header_buffer_size 32k;
+            large_client_header_buffers 4 32k;`,
           },
           staging: {
+            'nginx.ingress.kubernetes.io/client-header-buffer-size': '32k',
+            'nginx.ingress.kubernetes.io/large-client-header-buffers': '4 32k',
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
+            'nginx.ingress.kubernetes.io/server-snippet': `|
+            client_header_buffer_size 32k;
+            large_client_header_buffers 4 32k;`,
           },
           prod: {
+            'nginx.ingress.kubernetes.io/client-header-buffer-size': '32k',
+            'nginx.ingress.kubernetes.io/large-client-header-buffers': '4 32k',
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
+            'nginx.ingress.kubernetes.io/server-snippet': `|
+            client_header_buffer_size 32k;
+            large_client_header_buffers 4 32k;`,
           },
         },
       },
