b24a8415956e14155d8d2af6518cd2d800f9c221
albina <albina@kosmosogkaos.is>
2023-02-14 15:37:21 +0000

    tweaka

--- libs/application/templates/announcement-of-death/src/forms/done.ts
+++ libs/application/templates/announcement-of-death/src/forms/done.ts
@@ -5,12 +5,15 @@ import {
   buildDescriptionField,
 } from '@island.is/application/core'
 import { Form, FormModes } from '@island.is/application/types'
+import CoatOfArms from '../assets/CoatOfArms'
 import { m } from '../lib/messages'
 
 export const done: Form = buildForm({
   id: 'done',
   title: m.announcementComplete,
-  mode: FormModes.COMPLETED,
+  mode: FormModes.IN_PROGRESS,
+  logo: CoatOfArms,
+  renderLastScreenButton: true,
   children: [
     buildMultiField({
       id: 'done',
