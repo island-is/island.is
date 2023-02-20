e3af5405cd29652ef9d1b7bcb0f01a2750fd05c5
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-02-20 17:48:40 +0000

    fix: test

--- apps/services/license-api/jest.config.js
+++ apps/services/license-api/jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/apps/services/license-api',
-  globalSetup: './test/globalSetup.ts',
-  globalTeardown: './test/globalTeardown.ts',
-  setupFilesAfterEnv: ['./test/setup.ts'],
   moduleFileExtensions: ['ts', 'js', 'html', 'json'],
   globals: {
     'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
