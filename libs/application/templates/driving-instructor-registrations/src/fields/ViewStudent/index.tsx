aab68bccbdc218bad568ed39620344edb53a6c63
Alexandra Diljá <alexandrab18@ru.is>
2023-02-21 15:57:05 +0000

    format completed hours

--- libs/application/templates/driving-instructor-registrations/src/fields/ViewStudent/index.tsx
+++ libs/application/templates/driving-instructor-registrations/src/fields/ViewStudent/index.tsx
@@ -281,7 +281,9 @@ const ViewStudent = ({
                 {formatMessage(m.viewStudentCompleteHours)}
               </Text>
               <Text variant="default">
-                {student.book?.totalLessonCount ?? 0}
+                {student.totalLessonCount % 2 === 0
+                  ? student.totalLessonCount
+                  : student.totalLessonCount.toFixed(2) ?? 0}
               </Text>
             </GridColumn>
             {/* Practice driving */}
