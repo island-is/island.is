d7d2b1454b26f07de59e7971cb206e9c828ad4d2
helgamargreto <helgamo@advania.is>
2023-02-22 20:07:51 +0000

    Add logged in check and styling

--- apps/samradsgatt/components/WriteReviewCard/WriteReviewCard.tsx
+++ apps/samradsgatt/components/WriteReviewCard/WriteReviewCard.tsx
@@ -1,60 +1,70 @@
 import {
   ActionCard,
   Box,
-  GridRow,
   Input,
   Text,
+  Button,
+  Columns,
+  Column,
+  Icon,
 } from '@island.is/island-ui/core'
+import { style } from '@vanilla-extract/css'
 
 import format from 'date-fns/format'
 
-type CardProps = {
+type CardInfo = {
   caseNumber: string
   nameOfReviewer: string
   reviewPeriod: string
 }
 
-export const WriteReviewCard = ({
-  caseNumber,
-  nameOfReviewer,
-  reviewPeriod,
-}: CardProps) => {
-  caseNumber = '76/2022'
-  nameOfReviewer = 'Helga Margret Olafsdottir'
-  reviewPeriod = '01.08.2022 – 01.12.2022'
+type CardProps = {
+  card: CardInfo
+  isLoggedIn: boolean
+}
 
-  return (
-    // Add a check whether user is logged in or not and implement accordingly
-    // This is an implementation when user is not logged in.
+const date = format(new Date(Date.now()), 'dd.MM.yyyy')
+
+export const WriteReviewCard = ({ card, isLoggedIn }: CardProps) => {
+  return isLoggedIn ? (
     <Box
-      padding={3}
+      paddingTop={3}
+      paddingX={3}
       borderRadius="standard"
       borderWidth="standard"
       borderColor="blue300"
     >
-      <GridRow>
-        <Box
-          marginRight={1}
-          borderRightWidth={'standard'}
-          borderColor={'purple300'}
-          paddingRight={1}
-          paddingLeft={2}
-        >
-          <Text variant="eyebrow" color="purple400">
-            Mál nr. {caseNumber}
-          </Text>
-        </Box>
-        <Box>
-          <Text variant="eyebrow" color="purple400">
-            Til umsagnar: {reviewPeriod}
-          </Text>
-        </Box>
-      </GridRow>
+      <Columns>
+        <Column width="content">
+          <Box
+            marginRight={1}
+            borderRightWidth={'standard'}
+            borderColor={'purple300'}
+            paddingRight={1}
+          >
+            <Text variant="eyebrow" color="purple400">
+              Mál nr. {card.caseNumber}
+            </Text>
+          </Box>
+        </Column>
+        <Column width="content">
+          <Box style={{ display: 'flex', justifyItems: 'flex-end' }}>
+            <Text variant="eyebrow" color="purple400">
+              Til umsagnar: {card.reviewPeriod}
+            </Text>
+          </Box>
+        </Column>
+        <Column>
+          <Box style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Text variant="small">{date}</Text>
+          </Box>
+        </Column>
+      </Columns>
       <Text variant="h3" marginTop={1}>
         Skrifa umsögn
       </Text>
       <Text variant="default" marginBottom={2}>
-        Umsagnaraðili: {nameOfReviewer}
+        Umsagnaraðili: {card.nameOfReviewer}
       </Text>
       <Input
         textarea
@@ -63,18 +73,42 @@ export const WriteReviewCard = ({
         placeholder="Hér skal skrifa umsögn"
         rows={10}
       />
+      <Columns>
+        <Column width="content">
+          <Box
+            padding={2}
+            marginY={2}
+            borderRadius="large"
+            borderWidth="standard"
+            borderColor="blue400"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text variant="h5" color="blue400">
+              Hlaða upp viðhengi
+            </Text>
+            <Icon type="outline" icon="documents" color="blue400"></Icon>
+          </Box>
+        </Column>
+        <Column>
+          <Box marginY={2} style={{ display: 'flex', justifyContent: 'end' }}>
+            <Button size="default">Staðfesta umsögn</Button>
+          </Box>
+        </Column>
+      </Columns>
     </Box>
+  ) : (
+    <ActionCard
+      headingVariant="h4"
+      heading="Skrifa umsögn"
+      text="Þú verður að vera skráð(ur) inn til þess að geta skrifað umsögn um tillögur"
+      cta={{ label: 'Skrá mig inn' }}
+    >
+      {' '}
+    </ActionCard>
   )
 }
 
 export default WriteReviewCard
-
-// <ActionCard
-//   headingVariant="h4"
-//   heading="Skrifa umsögn"
-//   text="Lorem ipsum dolar, ............"
-//   cta={{ label: 'Skrá mig inn' }}
-// >
-//   {' '}
-
-// </ActionCard>
