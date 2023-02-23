2d6a15d754ee005168adaf154dbaa83a5e938454
diljath <diljath@advania.is>
2023-02-23 08:49:10 +0000

    added to workspace

--- libs/clients/consultation-portal/src/index.ts
+++ libs/clients/consultation-portal/src/index.ts
@@ -1,4 +1,3 @@
 export { ConsultationPortalClientConfig } from './lib/consultationPortalClient.config'
 export { ConsultationPortalClientModule } from './lib/consultationPortalClient.module'
-
 export * from '../gen/fetch'
