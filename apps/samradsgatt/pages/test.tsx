8ca560ff73650b2c275770399fdf6cc65568db2b
diljath <diljath@advania.is>
2023-02-23 12:21:17 +0000

    fix imports

--- apps/samradsgatt/pages/test.tsx
+++ apps/samradsgatt/pages/test.tsx
@@ -1,13 +1,8 @@
 import { withApollo } from '../graphql/withApollo'
-import { GetServerSideProps } from 'next'
-import { useQuery } from '@apollo/client'
 import { GET_ALL_CASES } from '../graphql/getAllCases'
-import { GetAllCasesQuery, useGetAllCasesQuery } from '../graphql/schema'
 import initApollo from '../graphql/client'
 
 export const Test = (props) => {
-  const { data, error, client, loading } = useGetAllCasesQuery({})
-  if (loading) return <div>loading..</div>
   return <div>Test</div>
 }
 export default withApollo(Test)
