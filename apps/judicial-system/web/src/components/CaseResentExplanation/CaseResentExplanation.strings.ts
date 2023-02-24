20184d8dc5c525848fc6c6d976c2c105739f445b
Ívar Oddsson <ivaroddsson@gmail.com>
2023-02-24 14:31:01 +0000

    Show caseResentExplanation on defender overview screen

---
+++ apps/judicial-system/web/src/components/CaseResentExplanation/CaseResentExplanation.strings.ts
@@ -0,0 +1,11 @@
+import { defineMessage } from 'react-intl'
+
+// Strings on signed verdict overview screen
+export const caseResentExplanation = {
+  title: defineMessage({
+    id: 'judicial.system.core:case_resent_explanation.title',
+    defaultMessage: 'Athugasemdir vegna endursendingar',
+    description:
+      'Notaður sem titill á athugasemdir vegna endursendingar svæði.',
+  }),
+}
