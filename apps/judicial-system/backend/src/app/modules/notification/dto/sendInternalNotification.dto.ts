c9ae194499fd53c2d1edcf98fd89974c18801949
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-02-27 20:26:28 +0000

    Fixes linting issues

--- apps/judicial-system/backend/src/app/modules/notification/dto/sendInternalNotification.dto.ts
+++ apps/judicial-system/backend/src/app/modules/notification/dto/sendInternalNotification.dto.ts
@@ -1,12 +1,10 @@
 import {
-  IsBoolean,
   IsEnum,
   IsNotEmpty,
-  IsOptional,
   IsString,
 } from 'class-validator'
 
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger'
 
 import { NotificationType } from '@island.is/judicial-system/types'
 
