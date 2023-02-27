11df73be6c7c475bf247e64a12b06a42bb5a550f
diljath <diljath@advania.is>
2023-02-27 14:40:20 +0000

    near-operation-file in codegen and call generated query

--- apps/consultation-portal/pages/test.tsx
+++ apps/consultation-portal/pages/test.tsx
@@ -1,6 +1,11 @@
 import { withApollo } from '../graphql/withApollo'
 import { GET_ALL_CASES } from '../graphql/getAllCases'
 import initApollo from '../graphql/client'
+import {
+  ConsultationPortalAllCasesQuery,
+  ConsultationPortalAllCasesQueryVariables,
+  ConsultationPortalAllCasesDocument,
+} from '../graphql/getAllCases.generated'
 
 export const Test = (props) => {
   return <div>Test</div>
@@ -10,6 +15,10 @@ export default withApollo(Test)
 export const getServerSideProps = async (ctx) => {
   const apolloClient = initApollo()
   try {
+    const test2 = await apolloClient.query<
+      ConsultationPortalAllCasesQuery,
+      ConsultationPortalAllCasesQueryVariables
+    >({ query: ConsultationPortalAllCasesDocument })
     const test = await apolloClient.query({ query: GET_ALL_CASES })
   } catch (error) {
     console.error(error)
