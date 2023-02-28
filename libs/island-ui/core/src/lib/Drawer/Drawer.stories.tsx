37bae0c99a7d9529d38642fa26012236d683173a
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2023-02-28 18:29:17 +0000

    improve stories for drawer

--- libs/island-ui/core/src/lib/Drawer/Drawer.stories.tsx
+++ libs/island-ui/core/src/lib/Drawer/Drawer.stories.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Button } from '../Button/Button'
 import { withFigma } from '../../utils/withFigma'
 import { Drawer } from './Drawer'
+import { Box } from '../Box/Box'
+import { Stack } from '../Stack/Stack'
 
 export default {
   title: 'Components/Drawer',
@@ -16,7 +18,35 @@ export const Default = () => {
       baseId="demo_drawer"
       ariaLabel="Use aria-label to explain what this is doing"
       disclosure={<Button variant="primary">Open drawer</Button>}
-      content={<div>Hello world</div>}
-    />
+    >
+      Hello world!
+    </Drawer>
   )
 }
+
+export const PositionLeft = () => (
+  <Drawer
+    baseId="demo_drawer2"
+    ariaLabel="Use aria-label to explain what this is doing"
+    position="left"
+    disclosure={<Button variant="primary">Open drawer on the left side</Button>}
+  >
+    Hello world!
+  </Drawer>
+)
+
+export const OverflowScroll = () => (
+  <Drawer
+    baseId="demo_drawer3"
+    ariaLabel="Use aria-label to explain what this is doing"
+    disclosure={<Button variant="primary">Open overflowing drawer</Button>}
+  >
+    <Stack space={3}>
+      {[...Array.from({ length: 20 })].map((_, i) => (
+        <Box key={i} padding={3} background="blue100" borderRadius="large">
+          Content box {i + 1}
+        </Box>
+      ))}
+    </Stack>
+  </Drawer>
+)
