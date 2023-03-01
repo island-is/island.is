7f948cce48d5bb1cb127cd0a59d4b1b8b1d8b0d9
Karl Arnar <korsbacka@gmail.com>
2023-03-01 10:36:38 +0000

    fix format

--- apps/samradsgatt/pages/um/index.tsx
+++ apps/samradsgatt/pages/um/index.tsx
@@ -4,14 +4,14 @@ import {
   GridColumn,
   GridContainer,
   GridRow,
-} from "@island.is/island-ui/core";
-import MainColumn from "../../components/About/MainColumn";
-import React from "react";
-import RightSideColmn from "../../components/About/RightSideColumn";
-import Layout from "../../components/Layout/Layout";
+} from '@island.is/island-ui/core'
+import MainColumn from '../../components/About/MainColumn'
+import React from 'react'
+import RightSideColmn from '../../components/About/RightSideColumn'
+import Layout from '../../components/Layout/Layout'
 
 interface AboutProps {
-  information: string;
+  information: string
 }
 
 const AboutPage: React.FC<AboutProps> = () => {
@@ -19,26 +19,26 @@ const AboutPage: React.FC<AboutProps> = () => {
     <Layout>
       <GridContainer>
         <GridRow>
-          <GridColumn span={"3/12"} paddingBottom={3}>
+          <GridColumn span={'3/12'} paddingBottom={3}>
             <Box paddingY={[3, 3, 3, 5, 5]}>
               <Breadcrumbs
                 items={[
-                  { title: "Samráðsgátt", href: "/samradsgatt" },
-                  { title: "Um samráðsgátt" },
+                  { title: 'Samráðsgátt', href: '/samradsgatt' },
+                  { title: 'Um samráðsgátt' },
                 ]}
               />
             </Box>
           </GridColumn>
-          <GridColumn span={"6/12"} paddingBottom={3} paddingTop={10}>
+          <GridColumn span={'6/12'} paddingBottom={3} paddingTop={10}>
             <MainColumn />
           </GridColumn>
-          <GridColumn span={"3/12"}>
+          <GridColumn span={'3/12'}>
             <RightSideColmn aboutHeadings={null}></RightSideColmn>
           </GridColumn>
         </GridRow>
       </GridContainer>
     </Layout>
-  );
-};
+  )
+}
 
-export default AboutPage;
+export default AboutPage
