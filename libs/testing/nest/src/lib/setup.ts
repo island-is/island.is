aa773821640f98870a3fcacbdfd0f45103ff5679
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-03-01 11:03:53 +0000

    Adding tests for tenants endpoint

---
+++ libs/testing/nest/src/lib/setup.ts
@@ -0,0 +1,43 @@
+import { Type } from '@nestjs/common'
+import { SequelizeOptionsFactory } from '@nestjs/sequelize'
+
+import { User } from '@island.is/auth-nest-tools'
+import { createCurrentUser } from '@island.is/testing/fixtures'
+import {
+  testServer,
+  useDatabase,
+  useAuth,
+  TestApp,
+} from '@island.is/testing/nest'
+
+interface SetupOptions {
+  AppModule: Type<any>
+  SequelizeConfigService: Type<SequelizeOptionsFactory>
+  user?: User
+}
+
+export const setupApp = ({
+  AppModule,
+  SequelizeConfigService,
+  user = createCurrentUser(),
+}: SetupOptions): Promise<TestApp> => {
+  // Setup app with authentication and database
+  return testServer({
+    appModule: AppModule,
+    enableVersioning: true,
+    hooks: [
+      useAuth({ auth: user }),
+      useDatabase({ type: 'sqlite', provider: SequelizeConfigService }),
+    ],
+  })
+}
+
+export const setupAppWithoutAuth = async ({
+  AppModule,
+  SequelizeConfigService,
+}: SetupOptions): Promise<TestApp> =>
+  testServer({
+    appModule: AppModule,
+    enableVersioning: true,
+    hooks: [useDatabase({ type: 'sqlite', provider: SequelizeConfigService })],
+  })
