b2ae8438519098e1d4c27ef0f943b68451d9ee53
Kristófer <kristofer@andes.is>
2023-03-02 14:51:52 +0000

    Better loacl System-e2e

--- apps/system-e2e/entrypoint.sh
+++ apps/system-e2e/entrypoint.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -euo pipefail
 
+: "${TEST_RESULTS_S3:=}"
+: "${TEST_ENVIRONMENT:=local}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Current test environment: ${TEST_ENVIRONMENT}"
 echo "Playwright args: $*"
 export PATH=./node_modules/.bin:$PATH
-playwright test -c src "$@"
+
+playwright test -c src "$@" || TEST_EXIT_CODE=$?
 TEST_EXIT_CODE=$?
-set -e
-zip -r -0 test-results playwright-report src/test-results
-aws s3 cp test-results.zip $TEST_RESULTS_S3
+
 if [ "$TEST_EXIT_CODE" != "0" ] ; then
-  node $DIR/src/notifications/notify.js
+  echo "Notifying due to test failure"
+  node "$DIR/src/notifications/notify.js"
+  echo "Notified :)"
+fi
+
+ls -lah . /dist
+zip -r test-results.zip playwright-report src/test-results
+if [ -n "$TEST_RESULTS_S3" ]; then
+  aws s3 cp test-results.zip "$TEST_RESULTS_S3"
 fi
 echo ""
 echo "To access the detailed report (with any failure traces), download it from the command line like this: \"aws s3 cp $TEST_RESULTS_S3 .\". Unzip the file and open the file 'index.html' in the playwright-report folder."
--- scripts/ci/_docker.sh
+++ scripts/ci/_docker.sh
@@ -16,8 +16,6 @@ function docker_build() {
   # shellcheck disable=SC2086
   docker buildx build \
     --platform=linux/amd64 \
-    --cache-from=type=local,src="$PROJECT_ROOT"/cache \
-    --cache-from=type=local,src="$PROJECT_ROOT"/cache_output \
     -f "${DIR}"/"$DOCKERFILE" \
     --target="$TARGET" \
     "${PUBLISH_TO_REGISTRY[@]}" \
