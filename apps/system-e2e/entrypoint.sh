256ac1bc0d5ca26526af4b6979bc8dfa84dd8f62
Kristófer <kristofer@andes.is>
2023-03-02 15:38:47 +0000

    Better local system-e2e entrypoint

--- apps/system-e2e/entrypoint.sh
+++ apps/system-e2e/entrypoint.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 : "${TEST_RESULTS_S3:=}"
 : "${TEST_ENVIRONMENT:=local}"
+: "${TEST_OUTPUT:=./test-results}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Current test environment: ${TEST_ENVIRONMENT}"
 echo "Playwright args: $*"
 export PATH=./node_modules/.bin:$PATH
 
-playwright test -c src "$@" || TEST_EXIT_CODE=$?
+playwright test --output "$TEST_OUTPUT" -c src "$@" || TEST_EXIT_CODE=$?
 TEST_EXIT_CODE=$?
 
 if [ "$TEST_EXIT_CODE" != "0" ] ; then
@@ -19,11 +20,11 @@ if [ "$TEST_EXIT_CODE" != "0" ] ; then
   echo "Notified :)"
 fi
 
-ls -lah . /dist
-zip -r test-results.zip playwright-report src/test-results
+zip -r test-results.zip "$TEST_OUTPUT"
 if [ -n "$TEST_RESULTS_S3" ]; then
   aws s3 cp test-results.zip "$TEST_RESULTS_S3"
 fi
+
 echo ""
 echo "To access the detailed report (with any failure traces), download it from the command line like this: \"aws s3 cp $TEST_RESULTS_S3 .\". Unzip the file and open the file 'index.html' in the playwright-report folder."
 echo ""
