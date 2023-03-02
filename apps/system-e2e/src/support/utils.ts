710f20aff091f5da1f2e66888bda307699978fc1
Kristófer <kristofer@andes.is>
2023-03-02 14:53:42 +0000

    Formatting

--- apps/system-e2e/src/support/utils.ts
+++ apps/system-e2e/src/support/utils.ts
@@ -4,7 +4,9 @@ import fs from 'fs'
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
 export function createMockPdf() {
-  fs.writeFile('./mockPdf.pdf', "test", (e: any) => { throw e })
+  fs.writeFile('./mockPdf.pdf', 'test', (e: any) => {
+    throw e
+  })
   /*
   const doc = new PDFDocument()
   doc.text('test')
