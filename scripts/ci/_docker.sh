c23b5f4c44f01fec1ad9d91aff9cf46a09cb791e
Kristófer <kristofer@andes.is>
2023-03-02 17:07:00 +0000

    Refactor docker build to accomodate podman

--- scripts/ci/_docker.sh
+++ scripts/ci/_docker.sh
@@ -13,17 +13,25 @@ TARGET=$2
 ACTION=${3:-docker_build}
 
 function docker_build() {
+  local build_args=()
+
+  build_args+=(--platform=linux/amd64 )
+  build_args+=(-f "${DIR}"/"$DOCKERFILE" )
+  build_args+=(--target="$TARGET" )
+  build_args+=("${PUBLISH_TO_REGISTRY[@]}" )
+  build_args+=(--build-arg APP="${APP}" )
+  build_args+=(--build-arg APP_HOME="${APP_HOME}" )
+  build_args+=(--build-arg APP_DIST_HOME="${APP_DIST_HOME}" )
+  build_args+=(${EXTRA_DOCKER_BUILD_ARGS:-} )
+  build_args+=(-t "${DOCKER_REGISTRY}""${APP}":"${DOCKER_TAG}" )
+
+  if ! docker --version 2>/dev/null | grep -q podman >/dev/null; then
+    build_args+=("--cache-from=type=local,src='$PROJECT_ROOT'/cache")
+    build_args+=("--cache-from=type=local,src='$PROJECT_ROOT'/cache_output")
+  fi
+
   # shellcheck disable=SC2086
-  docker buildx build \
-    --platform=linux/amd64 \
-    -f "${DIR}"/"$DOCKERFILE" \
-    --target="$TARGET" \
-    "${PUBLISH_TO_REGISTRY[@]}" \
-    --build-arg APP=${APP} \
-    --build-arg APP_HOME=${APP_HOME} \
-    --build-arg APP_DIST_HOME=${APP_DIST_HOME} \
-    ${EXTRA_DOCKER_BUILD_ARGS:-} \
-    -t "${DOCKER_REGISTRY}""${APP}":"${DOCKER_TAG}" \
+  docker buildx build "${build_args[@]}" \
     "$PROJECT_ROOT"
 }
 
