f37243298cb8433386609fcb2030810d24661d62
Karl Arnar <korsbacka@gmail.com>
2023-03-03 08:43:01 +0000

    feat(consultation-portal): Kam 866 make page mobile friendly and others (#10290)
    * Make case details page mobile friendly + refactoring
    
    * SearchAndSort component +  Breadcrumbs component
    
    * fix not alike text
    
    * searchAndFilter works
    
    * fixes and filters on the side of index page
    
    * add responsiveness to paginator
    
    * fixing formatting
    
    * small
    
    * Update cases.resolver.ts

--- apps/consultation-portal/types/enums.ts
+++ apps/consultation-portal/types/enums.ts
@@ -3,3 +3,9 @@ export enum Area {
   institution = 'Stofnanir',
   policyArea = 'Málefnasvið',
 }
+
+export enum SortOptions {
+  aToZ = 'Stafrófsröð',
+  latest = 'Nýjast efst',
+  oldest = 'Elst efst',
+}
