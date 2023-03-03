143d3f20f9c905ed46078bba8277529d302590ce
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-03-03 15:36:08 +0000

    fix: add valid test

--- libs/api/domains/license-service/src/lib/client/driving-license-client/drivingLicense.spec.ts
+++ libs/api/domains/license-service/src/lib/client/driving-license-client/drivingLicense.spec.ts
@@ -15,6 +15,18 @@ describe('license-service/client/driving-license', () => {
       expect(result).toBe(GenericUserLicensePkPassStatus.Unknown)
     })
 
+    it('should be valid for a license with a `mynd`', () => {
+      const license: GenericDrivingLicenseResponse = {
+        mynd: {
+          mynd: 'not-empty',
+        },
+      }
+
+      const result = GenericDrivingLicenseApi.licenseIsValidForPkpass(license)
+
+      expect(result).toBe(GenericUserLicensePkPassStatus.Available)
+    })
+
     it('should not be available for a license with `mynd` as null', () => {
       const license: GenericDrivingLicenseResponse = {
         mynd: null,
