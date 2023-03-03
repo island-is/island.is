7191aab1fd9b18e152fe7dc1de1840604507101e
runarvestmann <runar@stefna.is>
2023-03-03 15:03:43 +0000

    Change lock time semantics to improve readability

--- libs/content-search-indexer/src/environments/environment.ts
+++ libs/content-search-indexer/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   syncToken: process.env.API_CMS_SYNC_TOKEN ?? '',
   deletionToken: process.env.API_CMS_DELETION_TOKEN ?? '',
-  lockTime: 1800,
+  lockTime: 30 * 60, // 30 minutes
   locales: ['is', 'en'],
 }
