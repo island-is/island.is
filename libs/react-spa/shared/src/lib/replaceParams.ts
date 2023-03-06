951d532ba51e51d0f9478b1999feea1a642e44f5
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2023-03-06 12:35:17 +0000

    fixed routing for tenants

--- libs/react-spa/shared/src/lib/replaceParams.ts
+++ libs/react-spa/shared/src/lib/replaceParams.ts
@@ -13,5 +13,8 @@ type ReplaceParams = {
  * replaceParams('/users/:id/:friend', { id: 1, friend: 'john' }) -> '/users/1/john'
  */
 export function replaceParams({ href, params }: ReplaceParams) {
-  return href.replace(/\/:(\w+)/g, (_, paramName) => encodeURIComponent(`/${params[paramName]}`))
+  return href.replace(
+    /\/:(\w+)/g,
+    (_, paramName) => `/${encodeURIComponent(params[paramName] as string)}`,
+  )
 }
