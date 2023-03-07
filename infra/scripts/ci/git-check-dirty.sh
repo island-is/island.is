5db191f346d58935c3a12f75284609eeef866114
Jón Levy <levy@andes.is>
2023-03-07 22:00:26 +0000

    chore: auto-commit dirty charts on PRs

---
+++ infra/scripts/ci/git-check-dirty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$(git rev-parse --show-toplevel)"
+
+if [[ $(git diff --stat "$DIR"/charts) != '' ]]; then
+  git config --global user.name "Chart bot"
+  git config --global user.email "username@users.noreply.github.com"
+  git commit -am "chore: update dirty charts"
+  git push
+else
+  echo 'no changes to charts, nothing to commit'
+fi
