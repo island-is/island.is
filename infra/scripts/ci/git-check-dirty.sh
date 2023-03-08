1b453e1623c0a8e48e169e20a48a4a662264545e
Jón Levy <levy@andes.is>
2023-03-08 12:26:59 +0000

    ci(charts): improve git-check-dirty script

--- infra/scripts/ci/git-check-dirty.sh
+++ infra/scripts/ci/git-check-dirty.sh
@@ -2,13 +2,17 @@
 set -euxo pipefail
 
 DIR="$(git rev-parse --show-toplevel)"
-"$DIR"/infra/scripts/generate-chart-values.sh
+rel_path=$1
+abs_path=$DIR/$rel_path
+bot_name=$2
 
-if [[ $(git diff --stat "$DIR"/charts) != '' ]]; then
-  git config --global user.name "Chart bot"
+if [[ $(git diff --stat "$abs_path") != '' ]]; then
+  echo "changes found in $rel_path that will be commited by $bot_name"
+  git diff "$abs_path"
+  git config --global user.name "$bot_name"
   git config --global user.email "username@users.noreply.github.com"
-  git commit -am "chore: update dirty charts"
-  git push
+  git commit -am "chore: $bot_name update dirty files"
+  # git push
 else
   echo 'no changes to charts, nothing to commit'
 fi
