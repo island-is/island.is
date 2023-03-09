70d06ef45cc88fab5e00ca0885ce0c12da36675d
Kristófer <kristofer@andes.is>
2023-03-09 14:53:03 +0000

    Add more & sort test cases

--- apps/system-e2e/src/tests/islandis/service-portal/smoke/vehicles.spec.ts
+++ apps/system-e2e/src/tests/islandis/service-portal/smoke/vehicles.spec.ts
@@ -2,11 +2,11 @@ import { test } from '@playwright/test'
 
 test.describe.skip('Ökutæki', () => {
   for (const { testCase, home } of [
-    { testCase: 'Ökutæki leit - get aðeins leitað 5 x', home: '/' },
-    { testCase: 'Ökutækjaferill', home: '/en' },
-    { testCase: 'Ökutæki ferilskýrsla + eignastöðuvottorð', home: '/en' },
     { testCase: 'Ökutæki detail', home: '/en' },
+    { testCase: 'Ökutæki ferilskýrsla + eignastöðuvottorð', home: '/en' },
+    { testCase: 'Ökutæki leit - get aðeins leitað 5 x', home: '/' },
     { testCase: 'Ökutæki listi', home: '/en' },
+    { testCase: 'Ökutækjaferill', home: '/en' },
   ]) {
     test(testCase, () => {
       return
--- apps/system-e2e/src/tests/islandis/web/smoke/sites-of-institutions.spec.ts
+++ apps/system-e2e/src/tests/islandis/web/smoke/sites-of-institutions.spec.ts
@@ -4,13 +4,21 @@ import { urls } from '../../../../support/urls'
 test.use({ baseURL: urls.islandisBaseUrl })
 
 test.describe.skip('Sites of institutions', () => {
-  for (const { institution, home } of [
-    { institution: 'Utlendingastofnun', home: '/' },
-    { institution: 'HSN', home: '/en' },
-    { institution: 'Sjukratryggingar', home: '/en' },
-    { institution: 'Fiskistofa', home: '/en' },
+  for (const { testCase } of [
+    { testCase: 'Opinberir aðilar', home: '/', },
+    { testCase: 'Fiskistofa' },
+    { testCase: 'HSN' },
+    { testCase: 'Sjúkratryggingar', target: 'Efnisyfirlit' },
+    { testCase: 'Ríkislögmaður' },
+    { testCase: 'Landskjörstjórn' },
+    { testCase: 'Opinber nýsköpun', disabled: true },
+    { testCase: 'Sýslumenn' },
+    { testCase: 'Fjársýslan' },
+    { testCase: 'HSU' },
+    { testCase: 'Landlæknir' },
+    { testCase: 'Útlendingastofnun' },
   ]) {
-    test(`Vefir stofnana - ${institution}`, () => {
+    test(`Vefir stofnana - ${testCase}`, () => {
       return
     })
   }
