189bde56b5a7affdd941dce77bca0119c1cc9fc2
Þórður Hafliðason <tolleinn@gmail.com>
2023-03-10 00:59:43 +0000

    Add missing resolver

--- libs/api/domains/regulations/src/lib/api-domains-regulations.module.ts
+++ libs/api/domains/regulations/src/lib/api-domains-regulations.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { RegulationsClientModule } from '@island.is/clients/regulations'
+import { RegulationsResolver } from './api-domains-regulations.resolver'
 
 @Module({
   imports: [RegulationsClientModule],
+  providers: [RegulationsResolver],
 })
 export class RegulationsModule {}
