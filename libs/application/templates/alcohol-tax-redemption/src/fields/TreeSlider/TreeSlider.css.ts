5f1b2756e49d67edc7c4ac55c6c0907a7fa5f2c3
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2023-03-10 15:26:36 +0000

    treeeeees

--- libs/application/templates/alcohol-tax-redemption/src/fields/TreeSlider/TreeSlider.css.ts
+++ libs/application/templates/alcohol-tax-redemption/src/fields/TreeSlider/TreeSlider.css.ts
@@ -1,6 +1,16 @@
 import { theme } from '@island.is/island-ui/theme'
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 
 export const valueLabel = style({
   color: theme.color.blue400,
 })
+
+export const treeWrapper = style({
+  display: 'flex',
+  alignItems: 'center',
+  transformOrigin: 'bottom',
+  minWidth: 0,
+  flexShrink: 1,
+})
+
+globalStyle(`${treeWrapper} svg`, { width: '1em' })
--- libs/application/templates/alcohol-tax-redemption/src/fields/TreeSlider/TreeSlider.tsx
+++ libs/application/templates/alcohol-tax-redemption/src/fields/TreeSlider/TreeSlider.tsx
@@ -4,10 +4,18 @@ import { useLocale } from '@island.is/localization'
 import React, { useState } from 'react'
 import { Box } from '@island.is/island-ui/core'
 import { useFormContext, Controller } from 'react-hook-form'
-
+import * as styles from './TreeSlider.css'
 import Slider from '../Components/SliderWithGraphic/Slider/Slider'
 import { theme } from '@island.is/island-ui/theme'
 import { Text } from '@island.is/island-ui/core'
+import { Tree } from '../../assets/Tree'
+import { AnimatePresence, motion } from 'framer-motion'
+import random from 'lodash/random'
+
+const MIN_SIZE = 48,
+  MAX_SIZE = 96
+const sizes: number[] = []
+const zIndices: number[] = []
 
 export const TreeSlider = ({ application, field }: FieldBaseProps) => {
   const { formatMessage } = useLocale()
@@ -16,51 +24,85 @@ export const TreeSlider = ({ application, field }: FieldBaseProps) => {
   const [numberOfTrees, setNumberOfTrees] = useState(0)
 
   return (
-    <Box
-      marginBottom={4}
-      marginTop={1}
-      paddingTop={6}
-      paddingX={3}
-      paddingBottom={3}
-      background="blue100"
-    >
-      <Text marginBottom={6} variant="h4">
-        'Tré veijj'
-      </Text>
-      <Box marginBottom={12}>
-        <Controller
-          defaultValue={numberOfTrees}
-          name={'treesomtehing'}
-          render={({ onChange }) => (
-            <Slider
-              min={0}
-              max={20}
-              step={1}
-              currentIndex={numberOfTrees}
-              showMinMaxLabels
-              showToolTip
-              trackStyle={{ gridTemplateRows: 5 }}
-              onChange={(newValue: number) => {
-                console.log(newValue)
-                console.log(numberOfTrees)
-                if (!isNaN(newValue)) {
-                  onChange(newValue)
-                  setNumberOfTrees(newValue)
-                }
-              }}
-              label={{
-                singular: 'Tré',
-                plural: 'Tré',
-              }}
-              calculateCellStyle={() => {
-                return {
-                  background: theme.color.dark200,
-                }
-              }}
-            />
-          )}
-        />
+    <>
+      <Box
+        display="flex"
+        alignItems="flexEnd"
+        style={{ height: `${MAX_SIZE * 2}px` }}
+      >
+        <AnimatePresence>
+          {[...Array.from({ length: numberOfTrees })].map((_, i) => {
+            if (sizes[i] === undefined) {
+              sizes.push(random(MIN_SIZE, MAX_SIZE))
+            }
+            if (zIndices[i] === undefined) {
+              zIndices.push(Math.round(random(1, 2)))
+            }
+            return (
+              <motion.div
+                key={`tree-${i}`}
+                className={styles.treeWrapper}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0, transition: { type: 'linear' } }}
+                transition={{ type: 'spring', bounce: 0.25 }}
+                style={{
+                  fontSize: `${sizes[i]}px`,
+                  zIndex: zIndices[i],
+                  marginLeft: i > 0 ? `-${sizes[i] / 2}px` : 0,
+                }}
+              >
+                <Tree />
+              </motion.div>
+            )
+          })}
+        </AnimatePresence>
       </Box>
-    </Box>
+      <Box
+        marginBottom={4}
+        marginTop={1}
+        paddingTop={6}
+        paddingX={3}
+        paddingBottom={3}
+      >
+        <Text marginBottom={6} variant="h4">
+          'Tré veijj'
+        </Text>
+        <Box marginBottom={12}>
+          <Controller
+            defaultValue={numberOfTrees}
+            name={'treesomtehing'}
+            render={({ onChange }) => (
+              <Slider
+                min={0}
+                max={20}
+                step={1}
+                currentIndex={numberOfTrees}
+                showMinMaxLabels
+                showToolTip
+                trackStyle={{ gridTemplateRows: 5 }}
+                onChange={(newValue: number) => {
+                  console.log(newValue)
+                  console.log(numberOfTrees)
+                  if (!isNaN(newValue)) {
+                    onChange(newValue)
+                    setNumberOfTrees(newValue)
+                  }
+                }}
+                label={{
+                  singular: 'Tré',
+                  plural: 'Tré',
+                }}
+                calculateCellStyle={() => {
+                  return {
+                    background: theme.color.dark200,
+                  }
+                }}
+              />
+            )}
+          />
+        </Box>
+      </Box>
+    </>
   )
 }
