91bf39884e2f659a27ecc23d771f9f1133d624b3
Eirikur Nilsson <eirikur@nilsson.is>
2023-03-11 23:21:22 +0000

    Tweak DatePicker to better match Figma design.
    Extend top divider to input edge. Tweak whitespace

--- libs/island-ui/core/src/lib/DatePicker/DatePicker.css.ts
+++ libs/island-ui/core/src/lib/DatePicker/DatePicker.css.ts
@@ -94,11 +94,21 @@ export const value = style({
 export const customHeaderContainer = style({
   display: 'flex',
   justifyContent: 'space-between',
-  borderTop: `1px solid ${theme.color.blue200}`,
   borderBottom: `1px solid ${theme.color.blue200}`,
   paddingTop: theme.spacing[2],
   paddingBottom: theme.spacing[2],
   marginBottom: theme.spacing[2],
+  position: 'relative',
+  '::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    top: 0,
+    height: '1px',
+    left: `-${theme.spacing[3] - 3}px`,
+    right: `-${theme.spacing[3] - 3}px`,
+    background: theme.color.blue200,
+  },
 })
 
 export const headerSelect = style({
@@ -209,6 +219,7 @@ globalStyle(
     fontWeight: 600,
     backgroundColor: `${theme.color.transparent}`,
     borderBottom: 'none',
+    paddingBottom: '0',
   },
 )
 
@@ -216,7 +227,7 @@ globalStyle(`${root}.island-ui-datepicker .react-datepicker__month-container`, {
   float: 'none',
 })
 globalStyle(`${root}.island-ui-datepicker .react-datepicker__month`, {
-  margin: `0 0 ${theme.spacing[2]}px 0`,
+  margin: `0`,
 })
 globalStyle(`${root}.island-ui-datepicker .react-datepicker__day-names`, {
   marginBottom: '3px',
--- libs/testing/fixtures/src/index.ts
+++ libs/testing/fixtures/src/index.ts
@@ -1,4 +1,4 @@
 export { createCurrentUser } from './lib/currentUser'
 export { createNationalRegistryUser } from './lib/nationalRegistryUser'
-export { createNationalId, NationalIdType } from './lib/nationalId'
+export { createNationalId, type NationalIdType } from './lib/nationalId'
 export { createOpenIDUser } from './lib/openIDUser'
