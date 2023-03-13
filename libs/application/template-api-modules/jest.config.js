c805dd652acce0a851a132d1620483a7729769e4
Sævar Már Atlason <54210288+saevarma@users.noreply.github.com>
2023-03-13 15:31:43 +0000

    feat(auth-nest-tools): Add support for multiple issuers (#10387)
    * Add multi issuer key provider.
    
    * Add multi issuer back to auth admin api
    
    * Make issuer url string for prod in infra.
    
    * Having issuer and jwksUri as separate options to use issuers as key in client map.
    
    * Refactor to create function
    
    * Update packages
    
    * Configure nest-problem lib to use nest jest testEnvironment.
    
    * Fix testEnvironment for nest projects.
    
    * Fix github-actions-cache build issue.
    
    * Simplify createKeyProvider
    
    * Set issuer url list as array in prod
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/template-api-modules/jest.config.js
+++ libs/application/template-api-modules/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
+  testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '<rootDir>/coverage/libs/application/template-api-modules',
   displayName: 'application-template-api-modules',
