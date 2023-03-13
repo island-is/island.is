8a00b23c596e776e57629fa8e2c79003546e81b5
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-03-13 12:34:46 +0000

    fix: scope logic

--- apps/services/license-api/src/app/modules/license/guards/licenseTypeScope.guard.ts
+++ apps/services/license-api/src/app/modules/license/guards/licenseTypeScope.guard.ts
@@ -1,8 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import { Observable } from 'rxjs'
-import { Reflector } from '@nestjs/core'
 import { getRequest } from '@island.is/auth-nest-tools'
-import { LicenseApiScope, getLicenseTypeScopes } from '@island.is/auth/scopes'
 import { licenseTypeToScope } from '../scopeMapper'
 import { LicenseId } from '../license.types'
 
@@ -14,30 +12,12 @@ export class LicenseTypeScopesGuard implements CanActivate {
     const request = getRequest(context)
 
     const licenseId = request.params.licenseId
-    const isVerify = licenseId === 'verify'
-
     if (!licenseId) {
       return false
     }
 
-    const scopes = isVerify
-      ? [LicenseApiScope.licensesVerify]
-      : getLicenseTypeScopes()
-
     const authScope = request.auth?.scope
 
-    if (scopes && !this.hasScope(scopes, authScope)) {
-      return false
-    }
-
-    return !!authScope?.includes(
-      isVerify
-        ? LicenseApiScope.licensesVerify
-        : licenseTypeToScope[licenseId as LicenseId],
-    )
-  }
-
-  private hasScope(needScopes: string[], haveScopes: string[] = []): boolean {
-    return needScopes.some((scope) => haveScopes.includes(scope))
+    return !!authScope?.includes(licenseTypeToScope[licenseId as LicenseId])
   }
 }
