a35e36a3284f9fbb2b14013fe81511f18866b170
disaerna <disa.erna@gmail.com>
2023-03-14 13:42:33 +0000

    feat: add comment

--- libs/service-portal/health/src/utils/constants.ts
+++ libs/service-portal/health/src/utils/constants.ts
@@ -3,6 +3,7 @@ export const PHYSIO_ACCIDENT_THERAPY = 'accident'
 export const PHYSIO_HOME_THERAPY = 'home'
 export const SPEECH_THERAPY = 'speech'
 export const OCCUPATIONAL_THERAPY = 'occupational'
+// TODO: Decide an ID for "Ljósböð" therapy and talk to Sjúkratryggingar (fasi 2)
 
 export enum TherapyStatus {
   ALM = 'ALM',
