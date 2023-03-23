5cad496ecaffa241e9dd47b5d40711903f22b0b4
Kristófer <kristofer@andes.is>
2023-03-23 15:04:15 +0000

    Move nightly script to ts

---
+++ scripts/ci/spinnaker.ts
@@ -0,0 +1,34 @@
+
+function getLatestDockerTag() {
+  gql('getLatest')
+  return 'local'
+}
+
+const dockerTag = process.env.DOCKER_TAG ?? getLatestDockerTag()
+// TODO: better way to determine "current" branch?
+const gitBranch = process.env.GIT_BRANCH ?? 'main'
+const spinnakerWebhookToken = process.env.SPINNAKER_WEBHOOK_TOKEN
+if (!spinnakerWebhookToken) throw Error("Need Spinnaker webhook token 😡")
+
+/*
+          |
+          echo "Getting commit-sha for branch '$GIT_BRANCH'"
+          GIT_LATEST_COMMIT=$( curl \
+            -H "Authorization: bearer $GIT_API_TOKEN" \
+            -X POST \
+            -d '{ "query": "$GIT_LATEST_COMMIT_GQL"}' \
+            | jq -r '.data.repository.ref.target.oid' \
+          )
+          echo "Found commit-sha for $GIT_BRANCH: $GIT_LATEST_COMMIT"
+          echo "Sending webhook with branch: '$GIT_BRANCH'"
+          curl $SPINNAKER_URL/webhooks/webhook/islandis -H "content-type: application/json" --data-binary @- <<BODY
+          {
+            "token": "$SPINNAKER_WEBHOOK_TOKEN",
+            "branch": "$GIT_BRANCH",
+            "parameters": {
+              "docker_tag": "$DOCKER_TAG",
+              "feature_name": "system-e2e-nightly",
+            }
+          }
+          BODY
+  */
