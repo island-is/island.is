25c0591e475d53700a2156407ddc11446c090603
Ívar Oddsson <ivaroddsson@gmail.com>
2023-03-24 08:21:31 +0000

    chore(j-s): Open traffic-violation for a specific user  (#10632)
    Co-authored-by: Ívar Oddsson <oddsson@ivars-imac.lan>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/utils/hooks/useSections/index.ts
+++ apps/judicial-system/web/src/utils/hooks/useSections/index.ts
@@ -397,7 +397,8 @@ const useSections = (
                   : undefined,
             },
             ...((features.includes(Feature.INDICTMENT_ROUTE) ||
-              user?.name === 'Árni Bergur Sigurðsson') &&
+              user?.name === 'Árni Bergur Sigurðsson' ||
+              user?.name === 'Ásmundur Jónsson') &&
             workingCase.type === CaseType.Indictment &&
             isTrafficViolationCase(workingCase.indictmentSubtypes)
               ? [
