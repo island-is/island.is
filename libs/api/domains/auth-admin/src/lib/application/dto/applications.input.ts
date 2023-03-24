455b331e297df0d79e28ab4e5a1ec685a038d103
Gunnlaugur Guðmundsson <34029342+GunnlaugurG@users.noreply.github.com>
2023-03-24 09:27:16 +0000

    refactor(auth-admin): Refactored Application details screen (#10534)
    * refactored changes from previous PR
    
    * changed name of lists to a singular
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/api/domains/auth-admin/src/lib/application/dto/applications.input.ts
+++ libs/api/domains/auth-admin/src/lib/application/dto/applications.input.ts
@@ -4,6 +4,6 @@ import { Field, InputType } from '@nestjs/graphql'
 export class ApplicationsInput {
   @Field(() => String, { nullable: false })
   tenantId!: string
-  @Field(() => String, { nullable: true })
-  applicationId?: string
+  @Field(() => String, { nullable: false })
+  applicationId!: string
 }
