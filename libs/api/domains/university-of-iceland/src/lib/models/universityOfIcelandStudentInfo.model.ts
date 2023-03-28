655b740d3c20ef4d9796bc3b8ceea3902c281f2b
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-03-28 16:01:24 +0000

    feat :init

---
+++ libs/api/domains/university-of-iceland/src/lib/models/universityOfIcelandStudentInfo.model.ts
@@ -0,0 +1,86 @@
+import { Field, ObjectType } from '@nestjs/graphql'
+
+@ObjectType()
+export class UniversityOfIcelandStudentInfoModel {
+  @Field(() => [StudentModel], { nullable: true })
+  transcripts?: StudentModel[]
+  @Field(() => StudentTrackModel, { nullable: true })
+  track?: StudentTrackModel
+}
+
+@ObjectType()
+export class StudentTrackModel {
+  @Field(() => StudentModel)
+  transcript!: StudentModel
+
+  @Field(() => [StudentFiles])
+  files!: StudentFiles[]
+
+  @Field(() => StudentDescription)
+  body!: StudentDescription
+
+  @Field(() => String, { nullable: true })
+  downloadServiceURL?: string
+}
+
+@ObjectType()
+export class StudentModel {
+  @Field(() => String)
+  name!: string
+
+  @Field(() => String)
+  nationalId!: string
+
+  @Field(() => String)
+  graduationDate!: string
+
+  @Field(() => Number)
+  trackNumber!: number
+
+  @Field(() => InstitutionModel)
+  institution!: InstitutionModel
+
+  @Field(() => String)
+  school!: string
+
+  @Field(() => String)
+  faculty!: string
+
+  @Field(() => String)
+  studyProgram!: string
+
+  @Field(() => String)
+  degree!: string
+}
+@ObjectType()
+export class StudentFiles {
+  @Field(() => String)
+  type!: string
+
+  @Field(() => String)
+  locale!: string
+
+  @Field(() => String)
+  displayName!: string
+
+  @Field(() => String)
+  fileName!: string
+}
+
+@ObjectType()
+export class StudentDescription {
+  @Field(() => String)
+  description!: string
+
+  @Field(() => String)
+  footer!: string
+}
+
+@ObjectType()
+export class InstitutionModel {
+  @Field(() => String)
+  id!: string
+
+  @Field(() => String)
+  displayName!: string
+}
