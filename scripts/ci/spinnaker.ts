448ee0c524c08f84d005e94493ce4ae4c84c3448
Kristófer <kristofer@andes.is>
2023-03-29 14:05:15 +0000

    Requestify spinnaker

--- scripts/ci/spinnaker.ts
+++ scripts/ci/spinnaker.ts
@@ -1,30 +1,23 @@
 
-const dockerTag = process.env.DOCKER_TAG
-// TODO: better way to determine "current" branch?
-const gitBranch = process.env.GIT_BRANCH ?? 'main'
+const dockerTag = process.env.DOCKER_TAG || process.env.LAST_GOOD_BUILD_DOCKER_TAG
+const gitBranch = (process.env.GIT_BRANCH || process.env.BASE) ?? 'main'
 const spinnakerWebhookToken = process.env.SPINNAKER_WEBHOOK_TOKEN
+const spinnakerUrl = `${process.env.SPINNAKER_URL}/webhooks/webhook/islandis`
 if (!dockerTag) throw Error("Need latest docker tag cached ⛄")
 if (!spinnakerWebhookToken) throw Error("Need Spinnaker webhook token 😡")
 
-/*
-          |
-          echo "Getting commit-sha for branch '$GIT_BRANCH'"
-          GIT_LATEST_COMMIT=$( curl \
-            -H "Authorization: bearer $GIT_API_TOKEN" \
-            -X POST \
-            -d '{ "query": "$GIT_LATEST_COMMIT_GQL"}' \
-            | jq -r '.data.repository.ref.target.oid' \
-          )
-          echo "Found commit-sha for $GIT_BRANCH: $GIT_LATEST_COMMIT"
-          echo "Sending webhook with branch: '$GIT_BRANCH'"
-          curl $SPINNAKER_URL/webhooks/webhook/islandis -H "content-type: application/json" --data-binary @- <<BODY
-          {
-            "token": "$SPINNAKER_WEBHOOK_TOKEN",
-            "branch": "$GIT_BRANCH",
-            "parameters": {
-              "docker_tag": "$DOCKER_TAG",
-              "feature_name": "system-e2e-nightly",
-            }
-          }
-          BODY
-  */
+const trigger = new Request(
+  spinnakerUrl,
+  {
+    method: 'POST',
+    body:
+      JSON.stringify({
+        token: spinnakerWebhookToken,
+        branch: gitBranch,
+        parameters: {
+          docker_tag: dockerTag,
+          feature_name: "system-e2e-nightly",
+        },
+      })
+  })
+fetch(trigger)
