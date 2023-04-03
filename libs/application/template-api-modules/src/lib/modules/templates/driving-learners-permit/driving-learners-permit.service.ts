becab8c51e64252cb9c48c9355844232562d620f
kksteini <steini@kosmosogkaos.is>
2023-04-03 16:29:17 +0000

    Remove troublesome code that's failing tests

--- libs/application/template-api-modules/src/lib/modules/templates/driving-learners-permit/driving-learners-permit.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/driving-learners-permit/driving-learners-permit.service.ts
@@ -3,30 +3,13 @@ import { Injectable } from '@nestjs/common'
 import { SharedTemplateApiService } from '../../shared'
 import { TemplateApiModuleActionProps } from '../../../types'
 
-import { generateAssignApplicationEmail } from './emailGenerators'
 import { StudentMentorability } from './types'
 
-const TWO_HOURS_IN_SECONDS = 2 * 60 * 60
 @Injectable()
 export class DrivingLearnersPermitService {
   constructor(
     private readonly sharedTemplateAPIService: SharedTemplateApiService,
   ) {}
-
-  async createApplication({ application }: TemplateApiModuleActionProps) {
-    // Pretend to be doing stuff for a short while
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-
-    await this.sharedTemplateAPIService.assignApplicationThroughEmail(
-      generateAssignApplicationEmail,
-      application,
-      TWO_HOURS_IN_SECONDS,
-    )
-
-    return {
-      id: 1337,
-    }
-  }
 
   async completeApplication({ application }: TemplateApiModuleActionProps) {
     // Pretend to be doing stuff for a short while
