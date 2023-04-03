54ebe04824f055a8f68ec7116eb20b349685a7aa
Kristleifur Þorsteinsson <batwigg@gmail.com>
2023-04-03 16:17:19 +0000

    Initial commit of generated service

---
+++ apps/services/universitygateway/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'services-universitygateway',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/services/universitygateway',
+}
