b7c9a9cc70c2fbc9ae8b100ba5deac1cb8da5001
unakb <una@kolibri.is>
2023-04-04 11:03:14 +0000

    feat(j-s): Transition handles case appeal state transition  (#10688)
    * feat(j-s): Transition handles case appeal state transition and sends notifications
    
    * feat(j-s): Display banner when case has been appealed
    
    * fix(j-s): Remove import
    
    * feat(j-s): Add handle message test for appeal
    
    * feat(j-s): Combined transitions for case state and case appeal state
    
    * fix(j-s): Allow representative to trigger appeal transition and added transition tests for limited access case controller
    
    * fix(j-s): Call right controller when transitioning case
    
    * fix(j-s): clean up test
    
    * fix(j-s): ccleanup tests
    
    * fix(j-s): cleanup
    
    * fix(j-s): e2e test
    
    * Remove unused imports
    
    ---------
    
    Co-authored-by: Ívar Oddsson <ivaroddsson@Ivars-MacBook-Pro.local>

--- apps/judicial-system/backend/src/app/modules/case/guards/rolesRules.ts
+++ apps/judicial-system/backend/src/app/modules/case/guards/rolesRules.ts
@@ -148,6 +148,7 @@ export const prosecutorTransitionRule: RolesRule = {
     CaseTransition.OPEN,
     CaseTransition.SUBMIT,
     CaseTransition.DELETE,
+    CaseTransition.APPEAL,
   ],
 }
 
@@ -164,6 +165,15 @@ export const representativeTransitionRule: RolesRule = {
   ],
 }
 
+// Allows defenders to appeal cases.
+// Note that defenders can not appeal indictment cases.
+export const defenderTransitionRule: RolesRule = {
+  role: UserRole.DEFENDER,
+  type: RulesType.FIELD_VALUES,
+  dtoField: 'transition',
+  dtoFieldValues: [CaseTransition.APPEAL],
+}
+
 // Allows judges to receive, accept, reject and dismiss cases,
 // and to reopen non indictment cases
 export const judgeTransitionRule: RolesRule = {
@@ -176,6 +186,7 @@ export const judgeTransitionRule: RolesRule = {
     CaseTransition.REJECT,
     CaseTransition.DISMISS,
     CaseTransition.REOPEN,
+    CaseTransition.RECEIVE_APPEAL,
   ],
   canActivate: (request) => {
     const theCase = request.case
