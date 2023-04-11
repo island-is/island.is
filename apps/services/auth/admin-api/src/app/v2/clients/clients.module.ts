ca1edb2722b9c47e2a2a2c25088e9db746f3b2da
Valur Einarsson <valure@live.com>
2023-04-11 09:33:15 +0100

    Client secrets module.

--- apps/services/auth/admin-api/src/app/v2/clients/clients.module.ts
+++ apps/services/auth/admin-api/src/app/v2/clients/clients.module.ts
@@ -3,22 +3,18 @@ import { SequelizeModule } from '@nestjs/sequelize'
 
 import {
   Client,
-  ClientSecret,
   ClientsModule as ClientsLibModule,
   ResourcesModule as ResourcesLibModule,
 } from '@island.is/auth-api-lib'
 
-import { ClientSecretsService } from './client-secrets.services'
-import { MeClientSecretsController } from './me-client-secrets.controller'
 import { MeClientsController } from './me-clients.controller'
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([ClientSecret, Client]),
+    SequelizeModule.forFeature([Client]),
     ClientsLibModule,
     ResourcesLibModule,
   ],
-  controllers: [MeClientsController, MeClientSecretsController],
-  providers: [ClientSecretsService],
+  controllers: [MeClientsController],
 })
 export class ClientsModule {}
