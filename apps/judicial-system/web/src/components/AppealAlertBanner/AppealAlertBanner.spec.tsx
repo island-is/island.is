a3852c5c018b7e733d0ba9aeef9be68db144e71f
unakb <una@kolibri.is>
2023-04-12 14:43:08 +0000

    fix(j-s): fix tests

--- apps/judicial-system/web/src/components/AppealAlertBanner/AppealAlertBanner.spec.tsx
+++ apps/judicial-system/web/src/components/AppealAlertBanner/AppealAlertBanner.spec.tsx
@@ -1,7 +1,10 @@
+import { getAppealInfo } from './AppealAlertBanner'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
 import { CaseAppealDecision } from '@island.is/judicial-system/types'
 import { CaseAppealState, UserRole } from '../../graphql/schema'
 import { TempCase } from '../../types'
-import { getAppealInfo } from './AppealAlertBanner'
 
 describe('getAppealInfo', () => {
   test('should return that case can be appealed and the correct appeal deadline', () => {
@@ -13,11 +16,13 @@ describe('getAppealInfo', () => {
 
     const appealInfo = getAppealInfo(workingCase)
 
-    expect(appealInfo).toEqual({
-      canBeAppealed: true,
-      appealDeadline: '18. júní 2022 kl. 19:50',
-      hasBeenAppealed: false,
-    })
+    expect(appealInfo).toEqual(
+      expect.objectContaining({
+        canBeAppealed: true,
+        appealDeadline: '18. júní 2022 kl. 19:50',
+        hasBeenAppealed: false,
+      }),
+    )
   })
 
   test('should return that case has been appealed by the prosecutor, and return the correct appeal date', () => {
@@ -28,12 +33,14 @@ describe('getAppealInfo', () => {
 
     const appealInfo = getAppealInfo(workingCase)
 
-    expect(appealInfo).toEqual({
-      canBeAppealed: false,
-      appealedByRole: UserRole.Prosecutor,
-      appealedDate: '2022-06-15T19:50:08.033Z',
-      hasBeenAppealed: true,
-    })
+    expect(appealInfo).toEqual(
+      expect.objectContaining({
+        canBeAppealed: false,
+        appealedByRole: UserRole.Prosecutor,
+        appealedDate: '2022-06-15T19:50:08.033Z',
+        hasBeenAppealed: true,
+      }),
+    )
   })
 
   test('should return that case has been appealed by the defender', () => {
@@ -44,12 +51,14 @@ describe('getAppealInfo', () => {
 
     const appealInfo = getAppealInfo(workingCase)
 
-    expect(appealInfo).toEqual({
-      canBeAppealed: false,
-      appealedByRole: UserRole.Defender,
-      appealedDate: '2022-06-15T19:50:08.033Z',
-      hasBeenAppealed: true,
-    })
+    expect(appealInfo).toEqual(
+      expect.objectContaining({
+        canBeAppealed: false,
+        appealedByRole: UserRole.Defender,
+        appealedDate: '2022-06-15T19:50:08.033Z',
+        hasBeenAppealed: true,
+      }),
+    )
   })
 
   test('should return that case has not and cannot be appealed if appeal deadline has expired', () => {
@@ -61,11 +70,13 @@ describe('getAppealInfo', () => {
 
     const appealInfo = getAppealInfo(workingCase)
 
-    expect(appealInfo).toEqual({
-      appealDeadline: '18. júní 2022 kl. 19:50',
-      canBeAppealed: false,
-      hasBeenAppealed: false,
-    })
+    expect(appealInfo).toEqual(
+      expect.objectContaining({
+        appealDeadline: '18. júní 2022 kl. 19:50',
+        canBeAppealed: false,
+        hasBeenAppealed: false,
+      }),
+    )
   })
 
   test('should return that the case cannot be appealed if neither party has postponed the appeal', () => {
@@ -78,10 +89,12 @@ describe('getAppealInfo', () => {
 
     const appealInfo = getAppealInfo(workingCase)
 
-    expect(appealInfo).toEqual({
-      appealDeadline: '18. júní 2022 kl. 19:50',
-      canBeAppealed: false,
-      hasBeenAppealed: false,
-    })
+    expect(appealInfo).toEqual(
+      expect.objectContaining({
+        appealDeadline: '18. júní 2022 kl. 19:50',
+        canBeAppealed: false,
+        hasBeenAppealed: false,
+      }),
+    )
   })
 })
