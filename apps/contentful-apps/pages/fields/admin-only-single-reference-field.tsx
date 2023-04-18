126f43ab2998de638505bda7839ec3a32bee20b2
runarvestmann <runar@stefna.is>
2023-04-18 12:13:23 +0000

    Remove unused import

--- apps/contentful-apps/pages/fields/admin-only-single-reference-field.tsx
+++ apps/contentful-apps/pages/fields/admin-only-single-reference-field.tsx
@@ -1,4 +1,4 @@
-import { Paragraph, Box } from '@contentful/f36-components'
+import { Paragraph } from '@contentful/f36-components'
 import { FieldExtensionSDK } from '@contentful/app-sdk'
 import { useSDK } from '@contentful/react-apps-toolkit'
 import { SingleEntryReferenceEditor } from '@contentful/field-editor-reference'
