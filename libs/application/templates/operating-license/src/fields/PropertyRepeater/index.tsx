6c46e4d20213ef34828e92594abce44b795b58e9
kksteini <steini@kosmosogkaos.is>
2023-04-18 13:55:52 +0000

    Empty address fields that do not have a proper number are emptied

--- libs/application/templates/operating-license/src/fields/PropertyRepeater/index.tsx
+++ libs/application/templates/operating-license/src/fields/PropertyRepeater/index.tsx
@@ -128,15 +128,14 @@ const PropertyItem = ({
     // https://www.skra.is/um-okkur/frettir/frett/2018/03/01/Nytt-fasteignanumer-og-itarlegri-skraning-stadfanga/
     // The property number is a seven digit informationless sequence.
     // Has the prefix F.
-    if (
-      /F\d{7}$/.test(propertyNumberInput.trim().toUpperCase()) ||
-      /\d{7}$/.test(propertyNumberInput.trim().toUpperCase())
-    ) {
+    if (/[Ff]{0,1}\d{7}$/.test(propertyNumberInput.trim())) {
       getProperty({
         variables: {
           input: propertyNumberInput,
         },
       })
+    } else {
+      setValue(addressField, '')
     }
   }, [getProperty, address, addressField, propertyNumberInput, setValue])
 
