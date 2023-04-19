62a139cc70d6f61dd5e9fc21836289230f398aa7
Brynjar <brynjarorng@gmail.com>
2023-04-19 15:48:27 +0000

    retag wip

--- scripts/ci/_retag.sh
+++ scripts/ci/_retag.sh
@@ -7,10 +7,16 @@ set -euo pipefail
 # This is a script to re-tag a Docker image without pulling the pushing the image but rather directly in the registry.
 
 MANIFEST=$(aws ecr batch-get-image --repository-name "$IMAGE" --image-ids imageTag="$LAST_GOOD_BUILD_DOCKER_TAG" --query 'images[].imageManifest' --output text)
+if [[ -z $MANIFEST ]]; then
+  echo "Image not found: $IMAGE"
+  exit 255
+fi
 #now checking if tag already exits by picking up the version number from the response
 EXISTING_TAG=$(aws ecr batch-get-image --repository-name "$IMAGE" --image-ids imageTag="$DOCKER_TAG" --query 'images[].imageManifest' --output text | jq -r '.schemaVersion')
+echo "$MANIFEST"
 if [[ "$EXISTING_TAG" != "2" ]]; then
-  aws ecr put-image --repository-name "$IMAGE" --image-tag "$DOCKER_TAG" --image-manifest "$MANIFEST"
+  # aws ecr put-image --repository-name "$IMAGE" --image-tag "$DOCKER_TAG" --image-manifest "$MANIFEST"
+  echo Pushing
 else
   echo "Tag $DOCKER_TAG already existed for image $IMAGE. Hopefully this is a re-run in which case it should be ok."
 fi
