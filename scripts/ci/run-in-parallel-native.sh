4ec74c48ff852bccd9a644532aa195d9007146ae
Jón Levy <levy@andes.is>
2023-04-21 19:09:43 +0000

    ci: have more PR jobs use NX cloud

--- scripts/ci/run-in-parallel-native.sh
+++ scripts/ci/run-in-parallel-native.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ -n "$USE_NX_CLOUD" ];
+then
+  echo "USE_NX_CLOUD enabled, patching nx.json to use nx cloud runner ..."
+  tmp=$(mktemp)
+  jq '.tasksRunnerOptions.default.runner = "@nrwl/nx-cloud"' nx.json > "$tmp" && mv "$tmp" nx.json
+fi
 target=$1
 shift # remove target from remaining args
 
