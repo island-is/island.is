b287bb74f3abd38bb76103a49ae2a234bc42cdc8
Jón Levy <levy@andes.is>
2023-04-21 17:12:05 +0000

    ci: exclude .github dir from auto format:write

--- infra/scripts/ci/git-check-dirty.sh
+++ infra/scripts/ci/git-check-dirty.sh
@@ -11,7 +11,7 @@ if [[ $(git diff --stat "$abs_path") != '' ]]; then
   git diff "$abs_path"
   git config user.name 'github-actions[bot]'
   git config user.email 'github-actions[bot]@users.noreply.github.com'
-  git add "$abs_path"
+  git add "$abs_path" -- ':!.github/*'
   git commit -m "chore: $action update dirty files"
   git push
 else
