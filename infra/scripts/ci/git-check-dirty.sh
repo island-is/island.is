b478aa1798be9a7a9db0d5588d32dd3ba4f2db26
Jón Levy <levy@andes.is>
2023-04-21 17:42:22 +0000

    ci: patch nx.json to use nx cloud

--- infra/scripts/ci/git-check-dirty.sh
+++ infra/scripts/ci/git-check-dirty.sh
@@ -5,13 +5,14 @@ DIR="$(git rev-parse --show-toplevel)"
 rel_path=$1
 abs_path=$DIR/$rel_path
 action=$2
+exclude="':!.github/*'"
 
-if [[ $(git diff --stat "$abs_path") != '' ]]; then
+if [[ $(git diff --stat "$abs_path" -- "$exclude") != '' ]]; then
   echo "changes found in $rel_path that will be commited"
   git diff "$abs_path"
   git config user.name 'github-actions[bot]'
   git config user.email 'github-actions[bot]@users.noreply.github.com'
-  git add "$abs_path" -- ':!.github/*'
+  git add "$abs_path" -- "$exclude"
   git commit -m "chore: $action update dirty files"
   git push
 else
