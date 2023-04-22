dbbe3b1bfd9af558055cbd3df255a73aadd2a9eb
Jón Levy <levy@andes.is>
2023-04-22 15:47:46 +0000

    ci: fix typo

--- scripts/ci/40_e2e.sh
+++ scripts/ci/40_e2e.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # FIXME: use proper feature flag
 if [[ -n "$USE_NX_CLOUD" ]]; then
   source "$DIR"/patch-nx-json.sh
-  yarn nx run affected --target=e2e-ci --parallel=3
+  yarn run nx affected --target=e2e-ci --parallel=3
 else
   yarn nx run "$1":e2e-ci
 fi
