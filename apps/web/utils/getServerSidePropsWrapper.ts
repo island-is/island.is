72bed7bcba58d83e78de03f50b71157fb5991b76
runarvestmann <runar@stefna.is>
2023-04-23 14:11:28 +0000

    Stop logging 404 errors as errors

--- apps/web/utils/getServerSidePropsWrapper.ts
+++ apps/web/utils/getServerSidePropsWrapper.ts
@@ -1,6 +1,8 @@
 import { NormalizedCacheObject } from '@apollo/client'
+import { logger } from '@island.is/logging'
 import type { GetServerSideProps } from 'next'
 import type { ScreenContext } from '../types'
+import { CustomNextError } from '../units/errors'
 
 // Hack needed to avoid JSON-Serialization validation error from Next.js https://github.com/zeit/next.js/discussions/11209
 // >>> Reason: `undefined` cannot be serialized as JSON. Please use `null` or omit this value all together.
@@ -36,9 +38,21 @@ type Component = {
 export const getServerSidePropsWrapper: (
   screen: Component,
 ) => GetServerSideProps = (screen) => async (ctx) => {
-  const props = screen.getProps ? await screen.getProps(ctx) : ctx
-  deleteUndefined(props)
-  return {
-    props,
+  try {
+    const props = screen.getProps ? await screen.getProps(ctx) : ctx
+    deleteUndefined(props)
+    return {
+      props,
+    }
+  } catch (error) {
+    if (error instanceof CustomNextError) {
+      if (error.statusCode === 404) {
+        logger.info(error.title || '404 error occurred on web', error) // TODO: verify that this logging method works
+        return {
+          notFound: true,
+        }
+      }
+    }
+    throw error
   }
 }
