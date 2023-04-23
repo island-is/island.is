82d0e122f6fb17ab4e9c5061a14409cc833a0a3c
runarvestmann <runar@stefna.is>
2023-04-23 10:53:36 +0000

    Re-add api mocks and fix imports

--- apps/web/pages/[slug]/[subSlug]/index.ts
+++ apps/web/pages/[slug]/[subSlug]/index.ts
@@ -1,2 +1,10 @@
-import page from '..'
-export default page
+import withApollo from '@island.is/web/graphql/withApollo'
+import { withLocale } from '@island.is/web/i18n'
+import articleScreen from '@island.is/web/screens/Article'
+import { getServerSidePropsWrapper } from '@island.is/web/utils/getServerSidePropsWrapper'
+
+const Screen = withApollo(withLocale('is')(articleScreen))
+
+export default Screen
+
+export const getServerSideProps = getServerSidePropsWrapper(Screen)
--- apps/web/pages/en/o/[slug]/services/index.ts
+++ apps/web/pages/en/o/[slug]/services/index.ts
@@ -1,3 +1,5 @@
+import '@island.is/api/mocks'
+
 import withApollo from '@island.is/web/graphql/withApollo'
 import { withLocale } from '@island.is/web/i18n'
 import ServicesPage from '@island.is/web/screens/Organization/Services'
