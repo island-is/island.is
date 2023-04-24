7a124e3baa4af3b6d350c470772668873f534abc
kksteini <steini@kosmosogkaos.is>
2023-04-24 14:52:02 +0000

    Simplifying application

--- libs/application/template-api-modules/src/lib/modules/templates/driving-learners-permit/driving-learners-permit.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/driving-learners-permit/driving-learners-permit.service.ts
@@ -21,17 +21,13 @@ export class DrivingLearnersPermitService extends BaseTemplateApiService {
     application,
     auth,
   }: TemplateApiModuleActionProps) {
+    const studentSSN = getValueViaPath<string>(application.answers, 'studentMentorability.studentNationalId', '') ?? ''
     const practicePermitApplication = await this.drivingLicenseService.postPracticePermitApplication(
       {
         token: auth.authorization.split(' ')[1], // Used to remove the bearer part
         mentorSSN: application.applicant,
-        studentSSN:
-          getValueViaPath<string>(
-            application.answers,
-            'studentMentorability.studentNationalId',
-            '',
-          ) ?? '',
-      },
+        studentSSN
+      }
     )
 
     return {
