d9099c5e8cf7dbf15dd4f4831722c9e214a6048e
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-04-27 09:37:45 +0000

    update admin patch client

--- libs/auth-api-lib/src/lib/clients/admin/dto/admin-client.dto.ts
+++ libs/auth-api-lib/src/lib/clients/admin/dto/admin-client.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 
 import { TranslatedValueDto } from '../../../translation/dto/translated-value.dto'
 import { ClientType, RefreshTokenExpiration } from '../../../types'
+import { ClientAllowedScopeDTO } from '../../dto/client-allowed-scope.dto'
 import { ClientAllowedScope } from '../../models/client-allowed-scope.model'
 import { AdminClientClaimDto } from './admin-client-claim.dto'
 
@@ -105,6 +106,7 @@ export class AdminClientDto {
 
   @ApiProperty({
     description: 'Allowed scopes for the client',
+    type: [ClientAllowedScopeDTO],
   })
-  allowedScopes?: Pick<ClientAllowedScope, 'scopeName' | 'clientId'>[]
+  allowedScopes?: ClientAllowedScopeDTO[]
 }
