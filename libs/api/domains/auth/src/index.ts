a693c55b36d96e483c91f2679154505c497c5259
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-05-02 11:46:01 +0000

    revert unnecessary export

--- libs/api/domains/auth/src/index.ts
+++ libs/api/domains/auth/src/index.ts
@@ -2,7 +2,5 @@ export { AuthModule } from './lib/auth.module'
 
 export { ClientLoader } from './lib/loaders/client.loader'
 export type { ClientDataLoader } from './lib/loaders/client.loader'
-export type { ApiScopeDataLoader } from './lib/loaders/apiScope.loader'
-export { ApiScopeLoader } from './lib/loaders/apiScope.loader'
 
 export { Client } from './lib/models/client.model'
