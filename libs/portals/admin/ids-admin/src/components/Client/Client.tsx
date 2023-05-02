117a4bf3aad05e77c9cfc0cba2a48b71b6338a0e
Hedinn Eiriksson <hedinne@gmail.com>
2023-05-02 13:02:19 +0000

    feat(ids-admin): Permissions section (#10975)
    * feat(ids-admin): Permissions section
    
    * Permission confirm dialog init
    
    * Form intent and modal fixes
    
    * Remove DialogPrompt, Move Shadowbox to a new file
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/portals/admin/ids-admin/src/components/Client/Client.tsx
+++ libs/portals/admin/ids-admin/src/components/Client/Client.tsx
@@ -15,6 +15,7 @@ import ClientsUrl from './ClientsUrl'
 import Lifetime from './Lifetime'
 import Translations from './Translations'
 import Delegation from './Delegation'
+import Permissions from './Permissions'
 import AdvancedSettings from './AdvancedSettings'
 import { ClientContext } from '../../shared/context/ClientContext'
 import { ClientFormTypes } from '../forms/EditApplication/EditApplication.action'
@@ -125,6 +126,7 @@ const Client = () => {
             'slidingRefreshTokenLifetime',
             'customClaims',
           ],
+          [ClientFormTypes.permissions]: [],
           [ClientFormTypes.none]: [],
         },
         publishData: publishData,
@@ -217,6 +219,7 @@ const Client = () => {
             AuthAdminRefreshTokenExpiration.Sliding
           }
         />
+        <Permissions />
         <Delegation
           key={`${selectedEnvironment.environment}-Delegation`}
           supportsProcuringHolders={
