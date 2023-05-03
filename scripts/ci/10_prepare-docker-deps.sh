54b6bbcacd7949e1cbd4fe39e86ee2597fef2b30
Brynjar <brynjarorng@gmail.com>
2023-05-03 13:10:10 +0000

    added missing arg to second build step

--- scripts/ci/10_prepare-docker-deps.sh
+++ scripts/ci/10_prepare-docker-deps.sh
@@ -13,7 +13,6 @@ docker buildx build \
   --platform=linux/amd64 \
   --cache-to=type=local,dest="$PROJECT_ROOT"/cache \
   -f "${DIR}"/Dockerfile \
-  --progress=plain \
   --target=deps \
   --build-arg NPM_AUTH_TOKEN=$NPM_AUTH_TOKEN \
   "$PROJECT_ROOT"
@@ -23,7 +22,6 @@ docker buildx build \
   --cache-from=type=local,src="$PROJECT_ROOT"/cache \
   --cache-to=type=local,dest="$PROJECT_ROOT"/cache_output \
   -f "${DIR}"/Dockerfile \
-  --progress=plain \
   --target=output-base \
   --build-arg NPM_AUTH_TOKEN=$NPM_AUTH_TOKEN \
   "$PROJECT_ROOT"
