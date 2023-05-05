1bd093fcb8537a2c703b70facffaf455d1fa629b
runarvestmann <runar@stefna.is>
2023-05-05 16:15:46 +0000

    Add plausible tracking

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '@island.is/api/mocks'
 import { globalStyles } from '@island.is/island-ui/core'
-import App from 'next/app'
+import Head from 'next/head'
+import { PLAUSIBLE_SCRIPT_SRC } from '../constants'
 
 globalStyles()
 
-export default App
+export default ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <script
+          defer
+          data-domain={process.env.TRACKING_DOMAIN || 'island.is'}
+          src={PLAUSIBLE_SCRIPT_SRC}
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
