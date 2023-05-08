3ce72ca2a5c8038d534abf4034fbf4fe2980730e
Alex Diljar <alex@juni.is>
2023-05-08 13:30:50 +0000

    query

--- libs/portals/admin/petition/src/screens/Overview/Overview.tsx
+++ libs/portals/admin/petition/src/screens/Overview/Overview.tsx
@@ -1,15 +1,30 @@
-import React, { useEffect, useState } from 'react'
 import { Box, Text } from '@island.is/island-ui/core'
-
+import {useEndorsementSystemFindEndorsementListsQuery} from '../../queries/overview.generated'
 import { useLocale } from '@island.is/localization'
 import { m } from '../../lib/messages'
+import { EndorsementListControllerFindByTagsTagsEnum } from '@island.is/api/schema'
 
 const Overview = () => {
   const { formatMessage } = useLocale()
 
+  const { data, loading: queryLoading } = useEndorsementSystemFindEndorsementListsQuery({
+    variables: {
+      input: {
+        tags: [EndorsementListControllerFindByTagsTagsEnum.generalPetition],
+        limit: 1000,
+      },
+    },
+    onCompleted: (q) => {
+      console.log(q)
+    },
+  })
+  console.log(queryLoading)
+  console.log(data)
   return (
     <Box>
       <Text> {formatMessage(m.petitions)}</Text>
+      <Text> {queryLoading}</Text>
+      <Text> {data}</Text>
     </Box>
   )
 }
