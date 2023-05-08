7a5edc9547359072da8f419ac59a6d2122c9307b
unakb <una@kolibri.is>
2023-05-08 08:51:55 +0000

    fix(j-s): Import from schema

--- apps/judicial-system/web/src/components/SigningModal/SigningModal.spec.tsx
+++ apps/judicial-system/web/src/components/SigningModal/SigningModal.spec.tsx
@@ -1,6 +1,6 @@
 import { ApolloError } from '@apollo/client'
 
-import { RulingSignatureConfirmationQuery } from '../../graphql/schema'
+import { RulingSignatureConfirmationQuery } from '@island.is/judicial-system-web/src/graphql/schema'
 import { getSigningProgress } from './SigningModal'
 
 type SignatureConfirmation = RulingSignatureConfirmationQuery['rulingSignatureConfirmation']
