90f367be5b2b6f4002b81e9d9370e0467d72958a
runarvestmann <runar@stefna.is>
2023-05-08 10:35:54 +0000

    Use router events to trigger pageview

--- apps/web/hooks/usePlausiblePageView.ts
+++ apps/web/hooks/usePlausiblePageView.ts
@@ -1,19 +1,39 @@
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
+let hasSentInitialPageview = false
+
 export const usePlausiblePageview = (domain?: string) => {
+  const router = useRouter()
+
   useEffect(() => {
-    if (!domain) return
-    fetch('https://plausible.io/api/event', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-      body: JSON.stringify({
-        d: domain,
-        n: 'pageview',
-        u: window.location.href,
-        r: window.document.referrer || null,
-      }),
-    })
-  }, [domain])
+    const onRouteChangeComplete = () => {
+      if (!domain) return
+      fetch('https://plausible.io/api/event', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: JSON.stringify({
+          d: domain,
+          n: 'pageview',
+          u: window.location.href,
+          r: window.document.referrer || null,
+        }),
+      })
+    }
+
+    // Client side routing should trigger a pageview
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Initial page load should trigger a pageview
+    if (!hasSentInitialPageview && domain) {
+      onRouteChangeComplete()
+      hasSentInitialPageview = true
+    }
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [domain, router.events])
 }
