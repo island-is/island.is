5d810323474cc40aa7575492b60a605c9b52584b
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-05-08 11:20:18 +0000

    add test for getScopes findAll

--- libs/api/domains/auth-admin/src/lib/scope/scope.service.spec.ts
+++ libs/api/domains/auth-admin/src/lib/scope/scope.service.spec.ts
@@ -17,24 +17,64 @@ import { TestApp, testServer, useAuth } from '@island.is/testing/nest'
 
 import { ScopeResolver } from './scope.resolver'
 import { ScopeService } from './scope.service'
-import { ClientCreateScopeDTO } from '@island.is/auth-api-lib'
+import { AdminScopeDTO, ClientCreateScopeDTO } from '@island.is/auth-api-lib'
 
-const mockScopes = {
+const TENANT_ID = '@island.is'
+
+const mockCreateScopes = {
   scope1: {
-    tenantId: '@island.is',
+    tenantId: TENANT_ID,
     name: '@island.is/scope',
     displayName: 'Scope display name',
     description: 'Scope description',
   },
 }
 
-const createMockAdminApi = (scope: ClientCreateScopeDTO) => ({
+const mockFindAllScopes = (env: Environment) => [
+  {
+    name: `@island.is/scope:${env}`,
+    displayName: `Scope ${env} display name`,
+    description: `Scope ${env} display name`,
+  },
+  {
+    name: `@island.is/scope2:${env}`,
+    displayName: `Scope 2 ${env} display name`,
+    description: `Scope 2 ${env} display name`,
+  },
+  {
+    name: `@island.is/scope3:${env}`,
+    displayName: `Scope 3 ${env} display name`,
+    description: `Scope 3 ${env} display name`,
+  },
+]
+
+const createFindAllScopesMock = (environment: Environment) =>
+  mockFindAllScopes(environment).map((scope) => ({
+    scope,
+    environment,
+  }))
+
+const createMockAdminApi = (
+  createData: ClientCreateScopeDTO,
+  findAllData: AdminScopeDTO[],
+) => ({
   withMiddleware: jest.fn().mockReturnThis(),
-  meScopesControllerCreate: jest.fn().mockResolvedValue(scope),
+  meScopesControllerCreate: jest.fn().mockResolvedValue(createData),
+  meScopesControllerFindAll: jest.fn().mockResolvedValue(findAllData),
 })
-const mockAdminDevApi = createMockAdminApi(mockScopes.scope1)
-const mockAdminStagingApi = createMockAdminApi(mockScopes.scope1)
-const mockAdminProdApi = createMockAdminApi(mockScopes.scope1)
+
+const mockAdminDevApi = createMockAdminApi(
+  mockCreateScopes.scope1,
+  mockFindAllScopes(Environment.Development),
+)
+const mockAdminStagingApi = createMockAdminApi(
+  mockCreateScopes.scope1,
+  mockFindAllScopes(Environment.Staging),
+)
+const mockAdminProdApi = createMockAdminApi(
+  mockCreateScopes.scope1,
+  mockFindAllScopes(Environment.Production),
+)
 
 @Module({
   imports: [
@@ -54,9 +94,14 @@ describe('ScopeService', () => {
   })
 
   beforeEach(() => {
+    // Create
     mockAdminDevApi.meScopesControllerCreate.mockClear()
     mockAdminStagingApi.meScopesControllerCreate.mockClear()
     mockAdminProdApi.meScopesControllerCreate.mockClear()
+    // Find all
+    mockAdminDevApi.meScopesControllerFindAll.mockClear()
+    mockAdminStagingApi.meScopesControllerFindAll.mockClear()
+    mockAdminProdApi.meScopesControllerFindAll.mockClear()
   })
 
   describe('with multiple environments', () => {
@@ -87,7 +132,7 @@ describe('ScopeService', () => {
 
     it('should create scope for specific tenant for all environments', async () => {
       const scopeResponses = await scopeService.createScope(currentUser, {
-        ...mockScopes.scope1,
+        ...mockCreateScopes.scope1,
         environments: [
           Environment.Development,
           Environment.Staging,
@@ -100,15 +145,15 @@ describe('ScopeService', () => {
       expect(mockAdminProdApi.meScopesControllerCreate).toBeCalledTimes(1)
       expect(scopeResponses).toEqual([
         {
-          scopeName: mockScopes.scope1.name,
+          scopeName: mockCreateScopes.scope1.name,
           environment: Environment.Development,
         },
         {
-          scopeName: mockScopes.scope1.name,
+          scopeName: mockCreateScopes.scope1.name,
           environment: Environment.Staging,
         },
         {
-          scopeName: mockScopes.scope1.name,
+          scopeName: mockCreateScopes.scope1.name,
           environment: Environment.Production,
         },
       ])
@@ -116,7 +161,7 @@ describe('ScopeService', () => {
 
     it('should create scope for specific tenant for one environment', async () => {
       const scopeResponses = await scopeService.createScope(currentUser, {
-        ...mockScopes.scope1,
+        ...mockCreateScopes.scope1,
         environments: [Environment.Production],
       })
 
@@ -125,10 +170,38 @@ describe('ScopeService', () => {
       expect(mockAdminProdApi.meScopesControllerCreate).toBeCalledTimes(1)
       expect(scopeResponses).toEqual([
         {
-          scopeName: mockScopes.scope1.name,
+          scopeName: mockCreateScopes.scope1.name,
           environment: Environment.Production,
         },
       ])
+    })
+
+    it('should get scopes for specific tenant for all environments', async () => {
+      // Arrange
+      const mockedScopes = [
+        ...createFindAllScopesMock(Environment.Development),
+        ...createFindAllScopesMock(Environment.Staging),
+        ...createFindAllScopesMock(Environment.Production),
+      ]
+
+      // Act
+      const scopeResponses = await scopeService.getScopes(
+        currentUser,
+        TENANT_ID,
+      )
+
+      // Assert
+      expect(mockAdminDevApi.meScopesControllerFindAll).toBeCalledTimes(1)
+      expect(mockAdminStagingApi.meScopesControllerFindAll).toBeCalledTimes(1)
+      expect(mockAdminProdApi.meScopesControllerFindAll).toBeCalledTimes(1)
+
+      expect(scopeResponses).toEqual({
+        data: mockedScopes,
+        totalCount: mockedScopes.length,
+        pageInfo: {
+          hasNextPage: false,
+        },
+      })
     })
   })
 })
