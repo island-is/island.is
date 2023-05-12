28115c4694a1eb072fd52f0dc130d97e6b0a4270
Karl Arnar <korsbacka@gmail.com>
2023-05-12 13:07:30 +0000

    fix spacing when no documents

--- apps/consultation-portal/screens/Case/Case.tsx
+++ apps/consultation-portal/screens/Case/Case.tsx
@@ -69,14 +69,18 @@ const CaseScreen = ({ chosenCase, caseId }: Props) => {
               <Divider />
               <CaseTimeline chosenCase={chosenCase} />
               <Divider />
-              <RenderDocumentsBox
-                title="Skjöl til samráðs"
-                documents={chosenCase?.documents}
-              />
-              <RenderDocumentsBox
-                title="Fylgiskjöl"
-                documents={chosenCase?.additionalDocuments}
-              />
+              {chosenCase?.documents?.length > 0 && (
+                <RenderDocumentsBox
+                  title="Skjöl til samráðs"
+                  documents={chosenCase?.documents}
+                />
+              )}
+              {chosenCase?.documents?.length > 0 && (
+                <RenderDocumentsBox
+                  title="Fylgiskjöl"
+                  documents={chosenCase?.additionalDocuments}
+                />
+              )}
               {chosenCase?.statusName !==
                 CaseStatusFilterOptions.resultsPublished && (
                 <CaseEmailBox
