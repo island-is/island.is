a437d2d2876d80d9635597dff60a7be7ea9d01e6
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-05-15 10:26:11 +0000

    fix: import

--- libs/clients/work-machines/src/lib/workMachinesApiProvider.ts
+++ libs/clients/work-machines/src/lib/workMachinesApiProvider.ts
@@ -1,7 +1,7 @@
 import { createEnhancedFetch } from '@island.is/clients/middlewares'
 import { ConfigType, LazyDuringDevScope } from '@island.is/nest/config'
 import { Configuration, MachinesApi } from '../../gen/fetch'
-import { WorkMachinesClientConfig } from './work-machines.config'
+import { WorkMachinesClientConfig } from './workMachines.config'
 import { Provider } from '@nestjs/common'
 
 export const WorkMachinesApiProvider: Provider<MachinesApi> = {
