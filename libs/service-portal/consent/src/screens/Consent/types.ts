43e8922a56dffbbc13ff03992782da25e49c69bb
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-05-17 21:24:19 +0000

    Update consent mutation on toggle.

--- libs/service-portal/consent/src/screens/Consent/types.ts
+++ libs/service-portal/consent/src/screens/Consent/types.ts
@@ -7,11 +7,12 @@ import type { ReactNode } from 'react'
 type PermissionGroup = GetConsentListQuery['consentsList']['data'][0]['tenants'][0]
 
 export type ConsentSectionProps = {
+  clientId: string
   isLast?: boolean
 } & PermissionGroup
 
 export interface ConsentLineProps extends AuthConsentScopeNodeFragment {
-  onChange: (newChecked: boolean) => void
+  clientId: string
   isLast: boolean
 }
 
