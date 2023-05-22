e3cc908c5d62037445fe67e7d020250bd319e705
Karl Arnar <korsbacka@gmail.com>
2023-05-22 14:04:20 +0000

    translation files for applayout

--- apps/consultation-portal/components/index.ts
+++ apps/consultation-portal/components/index.ts
@@ -1,3 +1,4 @@
 export * from './Card/Card'
 export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs'
 export { default as Eyebrows } from './Eyebrows/Eyebrows'
+export { default as AppLayout } from './AppLayout/AppLayout'
