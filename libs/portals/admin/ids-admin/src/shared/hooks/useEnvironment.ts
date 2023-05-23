ce5881d6a030329515406548364445d4369e645f
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-05-23 17:44:29 +0000

    Fix out of sync logic after refactor

--- libs/portals/admin/ids-admin/src/shared/hooks/useEnvironment.ts
+++ libs/portals/admin/ids-admin/src/shared/hooks/useEnvironment.ts
@@ -15,18 +15,20 @@ type EnvironmentResult<T> = {
  * This hook is used to get the current environment from the URL query string,
  * or the first item in environments array if none query string is provided.
  * It also provides a function to update the environment manually with an update function.
- * @param environments
  */
 export const useEnvironment = <T extends EnvironmentResult<T>>(
   environments: Array<T>,
 ) => {
   const [searchParams] = useSearchParams()
-  const [environmentName, setEnvironmentName] = useState(
-    searchParams.get('env') ?? '',
-  )
+  const env = searchParams.get('env') ?? ''
+  const [environmentName, setEnvironmentName] = useState(env)
+
+  const findEnvironment = (environment: string) =>
+    environments.find((env) => env.environment === environment)
+
+  const [prevEnvironments, setPrevEnvironments] = useState(environments)
   const [environmentResult, setEnvironmentResult] = useState(
-    environments.find(({ environment }) => environment === environmentName) ??
-      environments[0],
+    findEnvironment(environmentName) ?? environments[0],
   )
 
   useSyncedQueryStringValueWithoutNavigation(
@@ -35,11 +37,12 @@ export const useEnvironment = <T extends EnvironmentResult<T>>(
     true,
   )
 
-  const getEnvironment = (environment: AuthAdminEnvironment) =>
-    environments.find((env) => env.environment === environment)
-
+  /**
+   * This function is used to update the environment manually.
+   * It both updates the environment name and the environment result.
+   */
   const updateEnvironment = (env: AuthAdminEnvironment) => {
-    const newEnvironmentResult = getEnvironment(env)
+    const newEnvironmentResult = findEnvironment(env)
 
     if (!newEnvironmentResult) return
 
@@ -47,6 +50,12 @@ export const useEnvironment = <T extends EnvironmentResult<T>>(
     setEnvironmentResult(newEnvironmentResult)
 
     return newEnvironmentResult
+  }
+
+  if (prevEnvironments !== environments) {
+    // Make sure to update the environment result if the environments array changes.
+    setEnvironmentResult(findEnvironment(environmentName) ?? environments[0])
+    setPrevEnvironments(environments)
   }
 
   return {
