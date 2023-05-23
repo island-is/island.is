7ff20d7da9951dfee8a8b3b8d55982182b08a1f7
Alex Diljar <alex@juni.is>
2023-05-23 12:58:26 +0000

    add admin scope

--- libs/auth-api-lib/seeders/data/scope-endorsements-system-admin.ts
+++ libs/auth-api-lib/seeders/data/scope-endorsements-system-admin.ts
@@ -1,17 +1,31 @@
 // scope-test.ts
-import { createScope } from './helpers'
-export const up = createScope({
-  // Required:
-  name: '@island.is/endorsements:admin',
-  displayName: 'Endorsements Admin Scope ',
-  description: 'Admin actions - lock unlock edit lists',
-  // Optional:
-  delegation: {
-    custom: false,
-    legalGuardians: false,
-    procuringHolders: false,
-  },
-  accessControlled: true,
-  addToResource: '@island.is',
-  addToClients: ['@island.is/web'],
-})
+import { compose, createScope } from './helpers'
+export const up = compose(
+  createScope({
+    // Required:
+    name: '@island.is/endorsements:admin',
+    displayName: 'Endorsements Admin Scope ',
+    description: 'Admin actions - lock unlock edit lists',
+    // Optional:
+    delegation: {
+      custom: false,
+      legalGuardians: false,
+      procuringHolders: false,
+    },
+    accessControlled: true,
+    addToResource: '@island.is',
+    addToClients: ['@island.is/web'],
+  }),
+  createScope({
+    name: '@admin.island.is/petitions',
+    displayName: 'Undirskriftalistar',
+    description: 'Umsjón með undirskriftalistum.',
+    accessControlled: true,
+    addToClients: ['@admin.island.is/web'],
+    addToResource: '@admin.island.is',
+    delegation: {
+      custom: true,
+      procuringHolders: true,
+    },
+  }),
+)
