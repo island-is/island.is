716619364ecd40d716e7bd6a5c00401e48475450
Karl Arnar <korsbacka@gmail.com>
2023-05-24 14:28:27 +0000

    card with localization

--- apps/consultation-portal/components/index.ts
+++ apps/consultation-portal/components/index.ts
@@ -1,4 +1,3 @@
-export * from './Card/Card'
 export { default as Eyebrows } from './Eyebrows/Eyebrows'
 
 export { default as ActionCard } from './ActionCard/ActionCard'
@@ -6,3 +5,4 @@ export { default as AppLayout } from './AppLayout/AppLayout'
 export { default as AuthProvider } from './AuthProvier/AuthProvider'
 export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs'
 export { default as CardSkeleton } from './CardSkeleton/CardSkeleton'
+export { default as Card } from './Card/Card'
