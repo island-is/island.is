8d1fcdef72ef477012fb8599c2082ad7638495fe
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-05-25 15:49:51 +0000

    feat: add graphql

---
+++ libs/service-portal/vehicles/src/screens/WorkMachinesOverview/WorkMachinesOverview.tsx
@@ -0,0 +1,6 @@
+import { useLocale, useNamespaces } from '@island.is/localization'
+
+const WorkMachinesOverview = () => {
+  useNamespaces('sp.work-machines')
+  const { formatMessage } = useLocale()
+}
