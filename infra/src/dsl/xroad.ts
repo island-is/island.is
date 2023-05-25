dba98a0cffa77816f435b8c65cfb8ad825fd7e41
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-05-25 10:17:55 +0000

    fix: config update

--- infra/src/dsl/xroad.ts
+++ infra/src/dsl/xroad.ts
@@ -123,7 +123,7 @@ export const DrivingLicense = new XroadConf({
       prod: 'r1/IS/GOV/5309672079/Logreglan-Protected/Okuskirteini-v4',
     },
     XROAD_DRIVING_LICENSE_V5_PATH: {
-      dev: 'r1/IS-DEV/GOV/10005/Logreglan-Protected/okuskirteini-v5',
+      dev: 'r1/IS-DEV/GOV/10005/Logreglan-Protected/Okuskirteini-v5',
       staging: 'r1/IS/GOV/5309672079/Logreglan-Protected/Okuskirteini-v5',
       prod: 'r1/IS/GOV/5309672079/Logreglan-Protected/Okuskirteini-v5',
     },
