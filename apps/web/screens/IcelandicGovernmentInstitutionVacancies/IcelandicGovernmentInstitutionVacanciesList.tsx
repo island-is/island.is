ba345539608bdf980c49e20a88fbe51726e58759
runarvestmann <runar@stefna.is>
2023-05-26 11:45:23 +0000

    Add filter clearing logic

--- apps/web/screens/IcelandicGovernmentInstitutionVacancies/IcelandicGovernmentInstitutionVacanciesList.tsx
+++ apps/web/screens/IcelandicGovernmentInstitutionVacancies/IcelandicGovernmentInstitutionVacanciesList.tsx
@@ -12,16 +12,20 @@ import {
   Select,
   Stack,
   Option,
+  Button,
 } from '@island.is/island-ui/core'
 import { Screen } from '@island.is/web/types'
 import { withMainLayout } from '@island.is/web/layouts/main'
 import {
   GetIcelandicGovernmentInstitutionVacanciesQuery,
   GetIcelandicGovernmentInstitutionVacanciesQueryVariables,
+  GetNamespaceQuery,
+  GetNamespaceQueryVariables,
   IcelandicGovernmentInstitutionVacanciesResponse,
 } from '@island.is/web/graphql/schema'
-import { useLinkResolver } from '@island.is/web/hooks'
+import { useLinkResolver, useNamespace } from '@island.is/web/hooks'
 import { GET_ICELANDIC_GOVERNMENT_INSTITUTION_VACANCIES } from '../queries/IcelandicGovernmentInstitutionVacancies'
+import { GET_NAMESPACE_QUERY } from '../queries'
 
 type Vacancy = IcelandicGovernmentInstitutionVacanciesResponse['vacancies'][number]
 
@@ -38,12 +42,15 @@ const mapVacanciesField = (vacancies: Vacancy[], fieldName: keyof Vacancy) => {
 
 interface IcelandicGovernmentInstitutionVacanciesListProps {
   vacancies: Vacancy[]
+  namespace: Record<string, string>
 }
 
 const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentInstitutionVacanciesListProps> = ({
   vacancies,
+  namespace,
 }) => {
-  console.log(vacancies)
+  const n = useNamespace(namespace)
+
   const [searchTerm, setSearchTerm] = useState('')
   const [
     selectedFieldOfWorkOption,
@@ -101,29 +108,49 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentIns
     [vacancies],
   )
 
+  const clearFilters = () => {
+    setSelectedFieldOfWorkOption(null)
+    setSelectedLocationOption(null)
+    setSelectedInstitutionOption(null)
+  }
+
   return (
     <Box>
       <GridContainer>
         <Stack space={2}>
-          <Breadcrumbs
-            items={[
-              { title: 'Ísland.is', href: '/' },
-              {
-                title: 'Starfatorg',
-                isCurrentPage: true,
-                href: linkResolver('vacancies').href,
-              },
-            ]}
-          />
-          <Text variant="h1">Starfatorg - laus störf hjá ríkinu</Text>
+          <Box display="flex" justifyContent="spaceBetween">
+            <Stack space={2}>
+              <Breadcrumbs
+                items={[
+                  { title: 'Ísland.is', href: '/' },
+                  {
+                    title: 'Starfatorg',
+                    isCurrentPage: true,
+                    href: linkResolver('vacancies').href,
+                  },
+                ]}
+              />
 
-          <Input
-            value={searchTerm}
-            onChange={(ev) => setSearchTerm(ev.target.value)}
-            name="vacancies-search"
-            placeholder="Leita í Starfatorgi"
-            icon={{ name: 'search', type: 'outline' }}
-          />
+              <Text variant="h1">Starfatorg - laus störf hjá ríkinu</Text>
+
+              <Box marginTop={5}>
+                <Input
+                  value={searchTerm}
+                  onChange={(ev) => setSearchTerm(ev.target.value)}
+                  name="vacancies-search"
+                  placeholder="Leita í Starfatorgi"
+                  icon={{ name: 'search', type: 'outline' }}
+                />
+              </Box>
+            </Stack>
+            <img
+              src={n(
+                'starfatorgIcon',
+                'https://images.ctfassets.net/8k0h54kbe6bj/1SY4juL47FNJT7kBNIsdqv/452a41bd6b685915bf9d07fb99bd2090/LE_-_Jobs_-_S3.svg',
+              )}
+              alt=""
+            />
+          </Box>
 
           <Inline space={2}>
             <Select
@@ -135,6 +162,7 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentIns
               onChange={(option) =>
                 setSelectedFieldOfWorkOption(option as Option)
               }
+              value={selectedFieldOfWorkOption}
             />
             <Select
               label="Veldu staðsetningu"
@@ -143,6 +171,7 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentIns
               name="location-picker"
               options={locationOptions}
               onChange={(option) => setSelectedLocationOption(option as Option)}
+              value={selectedLocationOption}
             />
             <Select
               label="Veldu stofnun/ráðuneyti"
@@ -153,12 +182,39 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentIns
               onChange={(option) =>
                 setSelectedInstitutionOption(option as Option)
               }
+              value={selectedInstitutionOption}
             />
           </Inline>
+          <Box
+            marginBottom={3}
+            style={{
+              visibility:
+                selectedFieldOfWorkOption ||
+                selectedLocationOption ||
+                selectedInstitutionOption
+                  ? 'visible'
+                  : 'hidden',
+            }}
+          >
+            <Button
+              size="small"
+              onClick={clearFilters}
+              icon="reload"
+              variant="text"
+            >
+              {n('clearFilters', 'Núllstila leit')}
+            </Button>
+          </Box>
         </Stack>
       </GridContainer>
       <Box paddingBottom={6}>
         <GridContainer>
+          <Text>
+            {filteredVacancies.length}{' '}
+            {filteredVacancies.length === 1
+              ? n('singleJobFound', 'starf fannst')
+              : n('jobsFound', 'störf fundust')}
+          </Text>
           <GridRow>
             {filteredVacancies.map((vacancy) => (
               <GridColumn key={vacancy.id} span={['1/1', '1/1', '1/2']}>
@@ -179,11 +235,13 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentIns
                   <Text color="blue400" variant="h3">
                     {vacancy.title}
                   </Text>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: vacancy.description?.slice(0, 80).concat('...'),
-                    }}
-                  ></div>
+                  <Text truncate={true} as="div">
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: vacancy.description,
+                      }}
+                    />
+                  </Text>
                 </FocusableBox>
               </GridColumn>
             ))}
@@ -196,20 +254,37 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<IcelandicGovernmentIns
 
 IcelandicGovernmentInstitutionVacanciesList.getInitialProps = async ({
   apolloClient,
+  locale,
 }) => {
-  const vacanciesResponse = await apolloClient.query<
-    GetIcelandicGovernmentInstitutionVacanciesQuery,
-    GetIcelandicGovernmentInstitutionVacanciesQueryVariables
-  >({
-    query: GET_ICELANDIC_GOVERNMENT_INSTITUTION_VACANCIES,
-    variables: {
-      input: {},
-    },
-  })
+  const [vacanciesResponse, namespaceResponse] = await Promise.all([
+    apolloClient.query<
+      GetIcelandicGovernmentInstitutionVacanciesQuery,
+      GetIcelandicGovernmentInstitutionVacanciesQueryVariables
+    >({
+      query: GET_ICELANDIC_GOVERNMENT_INSTITUTION_VACANCIES,
+      variables: {
+        input: {},
+      },
+    }),
+    apolloClient.query<GetNamespaceQuery, GetNamespaceQueryVariables>({
+      query: GET_NAMESPACE_QUERY,
+      variables: {
+        input: {
+          lang: locale,
+          namespace: 'Starfatorg',
+        },
+      },
+    }),
+  ])
+
+  const namespace = JSON.parse(
+    namespaceResponse?.data?.getNamespace?.fields || '{}',
+  ) as Record<string, string>
 
   return {
     vacancies:
       vacanciesResponse.data.icelandicGovernmentInstitutionVacancies.vacancies,
+    namespace,
   }
 }
 
