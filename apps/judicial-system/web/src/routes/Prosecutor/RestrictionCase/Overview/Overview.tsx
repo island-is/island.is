c5aeb344185ced0b1ed5c14878dd8ba03f1f88ae
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-06-06 10:09:21 +0000

    Removes faulty imports

--- apps/judicial-system/web/src/routes/Prosecutor/RestrictionCase/Overview/Overview.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/RestrictionCase/Overview/Overview.tsx
@@ -48,7 +48,6 @@ import type { CaseLegalProvisions } from '@island.is/judicial-system/types'
 import * as constants from '@island.is/judicial-system/consts'
 
 import * as styles from './Overview.css'
-import { CopyLinkForDefenderButton } from '../../components'
 
 export const Overview: React.FC = () => {
   const [modal, setModal] = useState<
