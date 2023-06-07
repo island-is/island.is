1a4867c4d324c5aaa908a97324361066da97f8cb
Kristófer <kristofer@andes.is>
2023-06-07 15:41:53 +0000

    Init of chunking by prefix

--- scripts/ci/_chunk.js
+++ scripts/ci/_chunk.js
@@ -1,13 +1,46 @@
 // This a script that given a list of strings, returns a list of comma-separated chunks of the original list
 
+
+const apps = [
+  { substr: 'templates', name: 'Templates' },
+  { substr: 'judicial', name: 'Judicial System' },
+  { substr: 'domains', name: 'Domains' },
+  { substr: 'service', name: 'Services' },
+  { substr: 'clients', name: 'Clients' },
+]
+
 // source: https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-265.php
-const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+function chunk(arr, size) {
+  arr.sort()
+  const parts = new Map()
+
+  for (const item of arr) {
+    const key = item.split('-')[0]
+    if (!parts.has(key)) { parts.set(key, []) }
+    parts.get(key).push(item)
+  }
+
+  console.log(parts)
+
+
+  return parts.forEach((part) => parts[part].join(','))
+
+  for (const app of apps) {
+    const subPart = arr.filter((item) => item.indexOf(app.substr) !== -1)
+    parts[part] = subPart
+  }
+  console.log('parts: ', parts)
+
+
+  const result = Array.from({ length: Math.ceil(arr.length / size) }, (_v, i) =>
     arr.slice(i * size, i * size + size),
   )
 
+  return result
+}
+
 // Chunk size
-const chunkSize = parseInt(process.env['CHUNK_SIZE'] || '2')
+const chunkSize = 3  // parseInt(process.env['CHUNK_SIZE'] || '2')
 
 let chunks = chunk(
   process.argv[2].split(',').map((s) => s.trim()),
