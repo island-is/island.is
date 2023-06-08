6260c46adfbfd1878aab76d727a36f2a0d7cc197
Kristófer <kristofer@andes.is>
2023-06-08 09:18:33 +0000

    Revert changes to unrelated sccrcipt

--- scripts/ci/generate-build-chunks.sh
+++ scripts/ci/generate-build-chunks.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
 
-source ./scripts/ci/_common.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck disable=SC1091
+source "$DIR"/_common.sh
 
 chunks='[]'
-for target in "$@"; do
-  affected_chunks=$("$PROJECT_ROOT"/scripts/ci/generate-chunks.sh "$target")
-  if [[ "$affected_chunks" != "" ]]; then
-    chunks=$(echo "$chunks" | jq -cM --argjson target "$affected_chunks" '. + ($target | map({projects: ., docker_type: "'"$target"'"}))')
-  fi
+for target in "$@"
+do
+    affected_chunks=$("$PROJECT_ROOT"/scripts/ci/generate-chunks.sh $target)
+    if [[ "$affected_chunks" != "" ]]; then
+        chunks=$(echo "$chunks" | jq -cM --argjson target "$affected_chunks" '. + ($target | map({projects: ., docker_type: "'$target'"}))')
+    fi
 done
 
-echo >&2 "Map: ${chunks}"
+>&2 echo "Map: ${chunks}"
 echo "$chunks" | jq -cM '. | map("\(.|tostring)")'
