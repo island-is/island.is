100a3a1e1d072e6e3c430b1ed2a0be062879df65
Kristófer <kristofer@andes.is>
2023-06-08 09:19:29 +0000

    Revert changes to unrelated scrcipt

--- scripts/ci/generate-chunks.sh
+++ scripts/ci/generate-chunks.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -euxo pipefail
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 
 BRANCH=${BRANCH:-$GITHUB_HEAD_REF}
 SKIP_TESTS_ON_BRANCH=${SKIP_TESTS_ON_BRANCH:-}
-if [[ "$SKIP_TESTS_ON_BRANCH" == "7913-$BRANCH" ]]; then
+if [[ "$SKIP_TESTS_ON_BRANCH" == "7913-$BRANCH" ]]
+then
   #Skipping tests
   echo "[]"
 else
   PROJECTS=$("$PROJECT_ROOT"/scripts/ci/_nx-affected-targets.sh "$1")
-  echo >&2 "Projects: ${PROJECTS}"
+  >&2 echo "Projects: ${PROJECTS}"
   CHUNKS=$(node "$PROJECT_ROOT"/scripts/ci/_chunk.js "${PROJECTS}")
-  echo >&2 "Chunks: $CHUNKS"
+  >&2 echo "Chunks: $CHUNKS"
   echo "$CHUNKS"
 fi
+
