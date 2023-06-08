e6c99974d2e8e01897d8e04ab11ceba0d2734ac4
Kristófer <kristofer@andes.is>
2023-06-08 11:46:54 +0000

    Revert "Force j-s tests"
    This reverts commit 630393e0c83f6210a29bb942e1a6a0e567b2e867.

--- apps/judicial-system/backend/src/app/test/random.ts
+++ apps/judicial-system/backend/src/app/test/random.ts
@@ -3,11 +3,11 @@ export const randomDate = () => {
   const latest = new Date(2999, 11, 31)
   return new Date(
     earliest.getTime() +
-    Math.random() * (latest.getTime() - earliest.getTime()),
+      Math.random() * (latest.getTime() - earliest.getTime()),
   )
 }
 
-export const randomBoolean = () => Math.random() < 0.5
+export const randomBoolean = () => Math.random() >= 0.5
 
 export function randomEnum<T>(anEnum: T): T[keyof T] {
   const enumValues = (Object.keys(anEnum as never) as unknown) as T[keyof T][]
