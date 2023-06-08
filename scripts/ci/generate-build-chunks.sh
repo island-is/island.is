536693bd5c0cdb49cf223a1c611141ba6b3459eb
Kristófer <kristofer@andes.is>
2023-06-08 08:57:52 +0000

    Formatting

--- scripts/ci/generate-build-chunks.sh
+++ scripts/ci/generate-build-chunks.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
+[[ -z "${CI:-}" ]] || set -x
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# shellcheck disable=SC1091
-source "$DIR"/_common.sh
+source ./scripts/ci/_common.sh
 
 chunks='[]'
-for target in "$@"
-do
-    affected_chunks=$("$PROJECT_ROOT"/scripts/ci/generate-chunks.sh $target)
-    if [[ "$affected_chunks" != "" ]]; then
-        chunks=$(echo "$chunks" | jq -cM --argjson target "$affected_chunks" '. + ($target | map({projects: ., docker_type: "'$target'"}))')
-    fi
+for target in "$@"; do
+  affected_chunks=$("$PROJECT_ROOT"/scripts/ci/generate-chunks.sh "$target")
+  if [[ "$affected_chunks" != "" ]]; then
+    chunks=$(echo "$chunks" | jq -cM --argjson target "$affected_chunks" '. + ($target | map({projects: ., docker_type: "'"$target"'"}))')
+  fi
 done
 
->&2 echo "Map: ${chunks}"
+echo >&2 "Map: ${chunks}"
 echo "$chunks" | jq -cM '. | map("\(.|tostring)")'
--- scripts/ci/generate-chunks.sh
+++ scripts/ci/generate-chunks.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -euxo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 
 BRANCH=${BRANCH:-$GITHUB_HEAD_REF}
 SKIP_TESTS_ON_BRANCH=${SKIP_TESTS_ON_BRANCH:-}
-if [[ "$SKIP_TESTS_ON_BRANCH" == "7913-$BRANCH" ]]
-then
+if [[ "$SKIP_TESTS_ON_BRANCH" == "7913-$BRANCH" ]]; then
   #Skipping tests
   echo "[]"
 else
   PROJECTS=$("$PROJECT_ROOT"/scripts/ci/_nx-affected-targets.sh "$1")
-  >&2 echo "Projects: ${PROJECTS}"
+  echo >&2 "Projects: ${PROJECTS}"
   CHUNKS=$(node "$PROJECT_ROOT"/scripts/ci/_chunk.js "${PROJECTS}")
-  >&2 echo "Chunks: $CHUNKS"
+  echo >&2 "Chunks: $CHUNKS"
   echo "$CHUNKS"
 fi
-
