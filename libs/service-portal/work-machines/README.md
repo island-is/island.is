039b14e5bb738e6059be5a498f31d71f7955231b
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-06-09 15:21:46 +0000

    fix: away with this

--- libs/service-portal/work-machines/README.md
+++
@@ -1,7 +0,0 @@
-# Service Portal Work Machines
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test service-portal-work-machines` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/service-portal/work-machines/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'service-portal-work-machines',
-  preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/service-portal/work-machines',
-}
