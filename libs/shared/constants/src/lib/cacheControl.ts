8458373e0b0726ba0ee49bbd13132f92be780806
Eirikur Nilsson <eirikur@nilsson.is>
2023-06-12 23:44:45 +0000

    Web caching

--- libs/shared/constants/src/lib/cacheControl.ts
+++ libs/shared/constants/src/lib/cacheControl.ts
@@ -3,6 +3,7 @@
  *
  * - GraphQL API - Redis 300 seconds
  * - GraphQL API - CDN 30-300 seconds
+ * - Website - CDN 30-300 seconds
  *
  * The CDN caches content for 30 seconds. After that they'll continue to serve up to 300 second old content
  * while revalidating in the background. The next request/user gets a fresher response.
@@ -12,3 +13,6 @@
  */
 
 export const CACHE_CONTROL_MAX_AGE = 300
+export const CACHE_CONTROL_MAX_AGE_SHARED = CACHE_CONTROL_MAX_AGE / 10
+export const CACHE_CONTROL_STALE_WHILE_REVALIDATE = CACHE_CONTROL_MAX_AGE
+export const CACHE_CONTROL_HEADER = `s-maxage=${CACHE_CONTROL_MAX_AGE_SHARED}, stale-while-revalidate=${CACHE_CONTROL_STALE_WHILE_REVALIDATE}, public`
