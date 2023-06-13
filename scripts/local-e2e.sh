149235ecb7d879d70ebe4803f43b9ec13439776b
Kristófer <kristofer@andes.is>
2023-06-13 14:14:11 +0000

    Consolidate _{run,build} scripts

--- scripts/local-e2e.sh
+++ scripts/local-e2e.sh
@@ -1,215 +1,220 @@
 #!/bin/bash
 
-#########################################
-# Script for testing system-e2e locally #
-#########################################
+set -euo pipefail
+
+print_usage() {
+  echo "Usage: build-container.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -t, --target            Set the target build stage"
+  echo "  -f, --dockerfile        Specify the Dockerfile to use (default: Dockerfile)"
+  echo "  -p, --publish           Publish the image to a registry with the specified tag"
+  echo "  -c, --cache-from        Cache images from the specified source (defaut: localhost)"
+  echo "  -h, --help              Show this help message"
+}
+
+parse_build_args() {
+  local target="localrun"
+  local dockerfile="Dockerfile"
+  local publish=""
+  local tag="latest"
+  local cache_from="local"
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    -t | --target)
+      target="$2"
+      shift 2
+      ;;
+    -f | --dockerfile)
+      dockerfile="$2"
+      shift 2
+      ;;
+    -p | --publish)
+      publish="$2"
+      shift 2
+      ;;
+    -c | --cache-from)
+      cache_from="$2"
+      shift 2
+      ;;
+    --tag)
+      tag="$2"
+      shift 2
+      ;;
+    -h | --help)
+      print_usage
+      exit 0
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+    esac
+  done
+
+  build_container "$dockerfile" "$target" "$tag" "$publish" "$cache_from"
+}
+
+build_container() {
+  local dockerfile=$1
+  local target=$2
+  local tag=$3
+  local _publish=$4
+  local cache_from=$5
+
+  local build_cmd=""
+  if command -v podman &>/dev/null; then
+    build_cmd="podman build"
+    if [[ -n $cache_from ]]; then
+      build_cmd+=" --cache-from $cache_from"
+    fi
+  elif command -v docker &>/dev/null; then
+    build_cmd="DOCKER_BUILDKIT=1 docker build"
+    if [[ -n $cache_from ]]; then
+      build_cmd+=" --cache-from $cache_from"
+    fi
+  else
+    echo "Neither Podman nor Docker is installed. Please install one of them."
+    exit 1
+  fi
+
+  if [[ -n $tag ]]; then
+    build_cmd+=" --tag $target:$tag"
+  fi
+
+  if [[ -n $target ]]; then
+    build_cmd+=" --target $target"
+  fi
+
+  if [[ -n $dockerfile ]]; then
+    build_cmd+=" --file $dockerfile"
+  fi
+
+  build_cmd+=" ."
+
+  echo "Building container using: $build_cmd"
+  $build_cmd
+}
 
 set -euo pipefail
 
-. ./scripts/utils.sh
-
-export \
-  APP="system-e2e" \
-  TEST_ENVIRONMENT="local"
-
-PROG_NAME=$(basename "${0}")
-PROJECT_DIR=$(git rev-parse --show-toplevel)
-APP_HOME=$(jq ".projects[\"${APP}\"]" -r <"${PROJECT_DIR}"/workspace.json)
-APP_DIST_HOME=$(jq ".targets.build.options.outputPath" -r <"${PROJECT_DIR}"/"${APP_HOME}"/project.json)
-ENV_FILE="${PROJECT_DIR}/.env.secret"
-
-LE2E_ACTION="run"
-LE2E_ARGS=()
-LE2E_SECRETS=()
-LE2E_ENVIRONMENT="local"
-LE2E_TEST_TYPE="smoke"
-LE2E_HEADEDEDNESS="headless"
-LE2E_BROWSER="chrome"
-LE2E_RUN_MODE="container"
-
-DOCKERFILE="Dockerfile"
-DOCKER_TAG=local-e2e #"$(git rev-parse --short HEAD)"
-DOCKER_IMAGE="localhost/${APP}":"${DOCKER_TAG}"
-DOCKER_TARGET="output-playwright"
-DOCKER_BUILD_ARGS="--build-arg APP=${APP} --build-arg APP_HOME=${APP_HOME} --build-arg APP_DIST_HOME=${APP_DIST_HOME} --build-arg GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) --build-arg GIT_SHA=${DOCKER_TAG}"
-
-# shellcheck disable=SC1091,SC1090
-. "${ENV_FILE}"
-
-function _get_builder_name() {
-  if command -v podman >/dev/null; then
-    echo "podman"
-  else
-    echo "docker"
-  fi
+print_usage() {
+  echo "Usage: run-container.sh [OPTIONS] <yarn run arguments>"
+  echo "Options:"
+  # echo "  -v, --volume <directory>   Mount the specified directory as a volume for caching"
+  # echo "  -c, --cache                Use yarn cache in current directory"
+  echo "  -i, --image <name>         Specify the name of the container image (default: localrun)"
+  echo "  -t, --tag <tag>            Specify the tag of the container image (default: latest)"
+  echo "  -h, --help                 Show this help message"
 }
 
-function builder() {
-  if command -v podman >/dev/null; then
-    podman "$@"
-  else
-    docker "$@"
-  fi
-}
+parse_run_args() {
+  local image="localhost/localrun"
+  local yarn_args=""
+  local tag="latest"
+  # local volume="${image##*/}-cache"
 
-function image_exists() {
-  builder images -q "${1}" 2>/dev/null
-  return $?
-}
-function _ensure_image() {
-
-  if image_exists "${DOCKER_IMAGE}"; then
-    warning "${DOCKER_IMAGE} has not been built yet, starting build now ..."
-    build_image
-    log debug "Successfully built image"
-  fi
-  return 0
-}
-
-function _get_source_path() {
-  [ "${LE2E_CODE_SOURCE}" = "source" ] && echo "${APP_HOME}"
-  [ "${LE2E_CODE_SOURCE}" = "dist" ] && echo "${APP_DIST_HOME}"
-}
-
-function _build_app() {
-  yarn nx run ${APP}:build
-}
-
-function run_container() {
-  debug "run_container START"
-  local tmp_dir="${PROJECT_DIR}/tmp"
-  local secrets_file="${PROJECT_DIR}/.env.secret"
-  local secrets_env_file="${secrets_file}.docker"
-
-  debug "run_container mkdir"
-  mkdir -p "${tmp_dir}"
-
-  debug "run_container cp"
-  cp "${secrets_file}" "${secrets_env_file}"
-
-  debug "run_container creating .env"
-  debug "run_container Secrets file: ${secrets_file}"
-  debug "run_container Secrets file (docker): ${secrets_env_file}"
-  # transform .env secrets file to actual .env file
-  grep -v '^#' "${secrets_file}" | cut -d ' ' -f 2- >"${secrets_env_file}" || true
-
-  if ! image_exists "${DOCKER_IMAGE}"; then
-    log warning "Missing image ${DOCKER_IMAGE}"
-    log warning "Run $0 build"
-    return 1
-  fi
-
-  log info "Running $APP in container with args '$*'"
-  runner \
-    -v "${tmp_dir}":/out:Z \
-    --env-file "${secrets_env_file}" \
-    "${DOCKER_IMAGE}" "$@"
-  # These options are breaking, causing 'playwright not found'
-  # TODO: But are still needed, so we don't have to re-build every time 😿
-  #  -v "${PROJECT_DIR}/${APP_DIST_HOME}":"/${APP_DIST_HOME}":z \
-  #  -v "${PROJECT_DIR}/${APP_HOME}/entrypoint.sh":"/${APP_DIST_HOME}/entrypoint.sh":z \
-  return $?
-
-  log info "Checking container pre-requisites"
-  log debug "Building app"
-  _build_app
-  log debug "Making sure the image exists"
-  _image_exists
-  log info "Running $APP in container"
-  log debug "Running $APP in container with args '$*'"
-  (
-    set -x
-    runner \
-      -v "${tmp_dir}":/out:Z \
-      -v "${PROJECT_DIR}/${APP_DIST_HOME}":"/${APP_DIST_HOME}":Z \
-      -v "${PROJECT_DIR}/${APP_HOME}/entrypoint.sh":"/${APP_DIST_HOME}/entrypoint.sh":Z \
-      --env-file "${secrets_env_file}" \
-      "${DOCKER_IMAGE}" "$@"
-  )
-}
-
-function build_image() {
-  (
-    export DOCKER_TAG
-    exec ./scripts/ci/_podman.sh "$DOCKERFILE" "$DOCKER_TARGET"
-  )
-  return 0
-}
-
-function runner() {
-  if command -v podman >/dev/null; then
-    podman run --stop-signal SIGKILL --userns=keep-id "$@"
-  else
-    docker run "$@"
-  fi
-}
-
-usage() {
-  cat <<EOF
-Usage: ${PROG_NAME} <build|run>
-
-build     APP_NAME
-run       [--container|--host] [--environment (local, dev, staging)]
-
-EOF
-  exit 1
-}
-
-function le2e_test() {
-  info "Running ${LE2E_TEST_TYPE} tests..."
-  podman run --env-file .env.secret --env-file ~/.env.secret --rm -it --name local-e2e -e TEST_ENVIRONMENT="${LE2E_ENVIRONMENT}" --userns=keep-id localhost/system-e2e:local-e2e
-  # yarn playwright test -c apps/system-e2e/src "${LE2E_TEST_TYPE}" "${LE2E_ARGS[@]}"
-  #playwright test -c apps/system-e2e/src "--${LE2E_HEADEDEDNESS}" "${LE2E_TARGET}" "${LE2E_ARGS[@]}"
-}
-
-function cli() {
-  # Parse command/action
-  local cmd="${1:-help}"
-  case "${cmd}" in
-  build | test | run) LE2E_ACTION="${cmd}" ;;
-  --help | -h | help) usage ;;
-  *) LE2E_ACTION="passthrough" ;;
-  esac
-  shift
-
-  # Parse options
   while [[ $# -gt 0 ]]; do
-    local opt="${1:-}"
-    local arg="${2:-}"
-    local n_args=1
-    case "${opt}" in
-    --smoke) LE2E_TEST_TYPE="smoke" ;;
-    --acceptance) LE2E_TEST_TYPE="acceptance" ;;
-    --container) LE2E_RUN_MODE="container" ;;
-    --host) LE2E_RUN_MODE="host" ;;
-    --environment | -e) LE2E_ENVIRONMENT="${arg}" n_args=2 ;;
-    --secrets-file) LE2E_SECRETS+=("${arg}") n_args=2 ;;
-    --) LE2E_ARGS=("${@:2}") n_args=$# ;;
-    *) LE2E_TARGET="${opt}" LE2E_ARGS=("${@:1}") n_args=$# ;;
+    case $1 in
+    -h | --help)
+      print_usage
+      exit 0
+      ;;
+    -i | --image)
+      image="$2"
+      shift 2
+      ;;
+    -t | --tag)
+
+      shift 2
+      ;;
+    -*)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+    run | yarn)
+      shift
+      ;;
+    *)
+      yarn_args="$*"
+      break
+      ;;
     esac
-    shift ${n_args}
   done
 
-  echo "ENV:"
-  log debug "$(set | grep "^LE2E_*")"
-
-  return
+  run_container "$image" "$tag" "$yarn_args"
 }
 
-function main() {
-  cli "$@"
-  debug "Running action: ${LE2E_ACTION}"
-  debug "     with args: ${LE2E_ARGS[*]}"
+run_container() {
+  local image=$1
+  local tag=$2
+  local yarn_args=$3
+  local container_name=${image##*/}
 
-  case "${LE2E_ACTION}" in
-  build) build_image ;;
-  test) le2e_test ;;
-  run) run_container "${LE2E_ARGS[@]}" ;;
-  *) usage ;;
-  esac
+  local run_cmd=""
+  if command -v podman &>/dev/null; then
+    run_cmd="podman run"
+  elif command -v docker &>/dev/null; then
+    run_cmd="docker run"
+  else
+    echo "Neither Podman nor Docker is installed. Please install one of them."
+    exit 1
+  fi
+
+  run_cmd+=" --rm"
+  run_cmd+=" -it"
+  run_cmd+=" --name ${container_name##*/}"
+  run_cmd+=" --publish-all"
+
+  run_cmd+=" --volume $PWD:/data:z"
+
+  run_cmd+=" $image:$tag"
+
+  if [[ -n $yarn_args ]]; then
+    run_cmd+=" yarn $yarn_args"
+  fi
+
+  echo "Running container using: $run_cmd"
+  $run_cmd
 }
 
-log info "START main"
-main "$@"
-log info "DONE"
-exit $?
+set -euo pipefail
+
+DIR="$(dirname $0)"
+PROGRAM_NAME="$(basename $0)"
+
+print_usage() {
+  echo "Usage: container.sh [COMMAND]"
+  echo "Commands:"
+  echo "  build [OPTIONS]        Build the container image"
+  echo "  run [OPTIONS]          Run the container"
+}
+
+parse_arguments() {
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    build)
+      shift
+      . $DIR/_build_container.sh "$@"
+      return
+      ;;
+    run)
+      shift
+      . $DIR/_run_container.sh "$@"
+      return
+      ;;
+    -h | --help)
+      print_usage
+      exit 0
+      ;;
+    *)
+      echo "Unknown command: $1"
+      print_usage
+      exit 1
+      ;;
+    esac
+  done
+  print_usage
+}
+
+parse_arguments "$@"
