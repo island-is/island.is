6148f190f743bf62f4c5ede84ae8e2f60796df7d
unakb <una@kolibri.is>
2023-06-14 13:24:33 +0000

    fix(j-s): Gap for filter checkboxes

--- apps/judicial-system/web/src/routes/Defender/Cases/components/FilterCheckboxes.css.ts
+++ apps/judicial-system/web/src/routes/Defender/Cases/components/FilterCheckboxes.css.ts
@@ -4,7 +4,7 @@ import { style } from '@vanilla-extract/css'
 export const gridRow = style({
   display: 'grid',
   gridTemplateColumns: '1fr 1fr 8fr',
-  gridGap: theme.spacing[1],
+  gridGap: theme.spacing[2],
   marginBottom: theme.spacing[3],
   marginTop: theme.spacing[4],
 })
