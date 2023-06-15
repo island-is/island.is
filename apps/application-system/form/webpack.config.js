65d6b64b297f75d39977b15c487465074cd07509
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-06-15 13:06:50 +0000

    Application system form webpack.config.js update

--- apps/application-system/form/webpack.config.js
+++ apps/application-system/form/webpack.config.js
@@ -1,21 +1,48 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/webpack')
+const { withReact } = require('@nx/react')
 const nrwlConfig = require('./../../../libs/shared/webpack/nrwl-config')
 
-module.exports = (config) => {
-  // Add our common webpack config
-  nrwlConfig(config)
+// module.exports = (config) => {
+//   // Add our common webpack config
+//   nrwlConfig(config)
+//
+//   // App specific config
+//   config.stats.chunks = false
+//   config.stats.modules = false
+//
+//   return {
+//     ...config,
+//     ...(process.env.PORT
+//       ? { devServer: { ...config.devServer, port: process.env.PORT } }
+//       : {}),
+//     node: {
+//       global: true,
+//     },
+//   }
+// }
 
-  // App specific config
-  config.stats.chunks = false
-  config.stats.modules = false
-
-  return {
-    ...config,
-    ...(process.env.PORT
-      ? { devServer: { ...config.devServer, port: process.env.PORT } }
-      : {}),
-    node: {
-      global: true,
+module.exports = composePlugins(
+  withNx({
+    nx: {
+      svgr: true,
     },
-  }
-}
+  }),
+  withReact({ svgr: true }),
+  nrwlConfig,
+  (config, { options, context }) => {
+    // App specific config
+    config.stats.chunks = false
+    config.stats.modules = false
+
+    return {
+      ...config,
+      ...(process.env.PORT
+        ? { devServer: { ...config.devServer, port: process.env.PORT } }
+        : {}),
+      node: {
+        global: true,
+      },
+    }
+  },
+)
