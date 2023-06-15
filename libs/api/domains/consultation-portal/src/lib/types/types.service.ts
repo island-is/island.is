d354da7149e9cb61863c6179b1213f502cebf1ac
Karl Arnar <korsbacka@gmail.com>
2023-06-15 19:45:47 +0000

    Update types.service.ts

--- libs/api/domains/consultation-portal/src/lib/types/types.service.ts
+++ libs/api/domains/consultation-portal/src/lib/types/types.service.ts
@@ -1,3 +1,4 @@
+// toRemove
 import { TypesApi } from '@island.is/clients/consultation-portal'
 import { LOGGER_PROVIDER } from '@island.is/logging'
 import { Inject, Injectable } from '@nestjs/common'
