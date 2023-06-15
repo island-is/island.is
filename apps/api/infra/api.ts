7c659cba694f68b2ddd809cf4c8e7fa60a0db042
Karl Arnar <korsbacka@gmail.com>
2023-06-15 10:44:49 +0000

    Update api.ts #2

--- apps/api/infra/api.ts
+++ apps/api/infra/api.ts
@@ -177,7 +177,11 @@ export const serviceSetup = (services: {
         staging: 'https://samradapi-test.devland.is',
         prod: 'https://samradapi.island.is',
       },
-      CONSULTATION_PORTAL_SCOPE: '@stjornarradid.is/samradsgatt',
+      CONSULTATION_PORTAL_SCOPE: {
+        dev: '@stjornarradid.is/samradsgatt',
+        staging: '@stjornarradid.is/samradsgatt',
+        prod: '@stjornarradid.is/samradsgatt'
+      },
       NO_UPDATE_NOTIFIER: 'true',
       FISKISTOFA_ZENTER_CLIENT_ID: '1114',
       SOFFIA_SOAP_URL: {
