bb0b1c046b22ee690fca2dffb80e2373fe3dff11
albinagu <47886428+albinagu@users.noreply.github.com>
2023-06-16 13:50:13 +0000

    fix(estate): Estate refactor (#11463)
    * fix(estate): Estate refactor
    
    * overview
    
    * schema
    
    * id tweaks
    
    * commenting forms
    
    * adding checkbox - acceptAssets
    
    * tweak
    
    * chore: nx format:write update dirty files
    
    * file upload tweak
    
    * upload restrictions
    
    * Official division tweak
    
    * form tweak - private exchange
    
    * overview checkbox for "Eignalaust"
    
    * adding spouse of the deceased in private exchange
    
    * overview private exchange
    
    * forms refactor - set together section by section
    
    * cleanup
    
    * round2
    
    * a bit more :3
    
    * and final spin
    
    * texts
    
    * file format
    
    * delete unused schema
    
    * Rename spouse of the deceased to lower camel case
    
    * removing console log
    
    * Add spouse to upload data and pdf for einkaskipti
    
    * Remove buffer testing
    
    * Remove unused import
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: kksteini <steini@kosmosogkaos.is>
    Co-authored-by: kksteini <77672665+kksteini@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/estate/src/forms/Forms.ts
@@ -0,0 +1,89 @@
+import { buildForm } from '@island.is/application/core'
+import { Form, FormModes } from '@island.is/application/types'
+import { dataCollection } from './Sections/dataCollection'
+import { announcerInfo } from './Sections/announcerInfo'
+import { spouseOfTheDeceased } from './Sections/spouseOfTheDeceased'
+import { estateMembers } from './Sections/estateMembers'
+import { testamentInfo } from './Sections/testamentInfo'
+import { estateAssets } from './Sections/estateAssets'
+import { estateDebts } from './Sections/estateDebts'
+import { attachments } from './Sections/attachments'
+import { representative } from './Sections/representative'
+import { approvePrivateDivisionSubmission } from './Sections/approveSubmission'
+import { overview } from './Overviews'
+import { estateWithoutAssets } from './Sections/estateWithoutAssets'
+
+/* EINKASKIPTI */
+
+export const privateDivisionForm: Form = buildForm({
+  id: 'privateDivisionForm',
+  title: '',
+  mode: FormModes.DRAFT,
+  renderLastScreenButton: true,
+  renderLastScreenBackButton: true,
+  children: [
+    dataCollection,
+    announcerInfo,
+    spouseOfTheDeceased,
+    estateMembers,
+    testamentInfo,
+    estateAssets,
+    estateDebts,
+    attachments,
+    representative,
+    approvePrivateDivisionSubmission,
+    overview,
+  ],
+})
+
+/* SETA Í ÓSKIPTU BÚI */
+
+export const undividedEstateForm: Form = buildForm({
+  id: 'undividedEstateForm',
+  title: '',
+  mode: FormModes.DRAFT,
+  renderLastScreenButton: true,
+  renderLastScreenBackButton: true,
+  children: [
+    dataCollection,
+    announcerInfo,
+    estateMembers,
+    testamentInfo,
+    estateAssets,
+    estateDebts,
+    attachments,
+    overview,
+  ],
+})
+
+/* EIGNALAUST DÁNARBU */
+
+export const estateWithoutAssetsForm: Form = buildForm({
+  id: 'estateWithoutAssetsForm',
+  title: '',
+  mode: FormModes.DRAFT,
+  renderLastScreenButton: true,
+  renderLastScreenBackButton: true,
+  children: [
+    dataCollection,
+    announcerInfo,
+    estateMembers,
+    testamentInfo,
+    estateWithoutAssets,
+    estateAssets,
+    estateDebts,
+    attachments,
+    overview,
+  ],
+})
+
+/* OPINBER SKIPTI */
+
+export const officialDivisionForm: Form = buildForm({
+  id: 'officialDivisionForm',
+  title: '',
+  mode: FormModes.DRAFT,
+  renderLastScreenButton: true,
+  renderLastScreenBackButton: true,
+  children: [dataCollection, announcerInfo, overview],
+})
