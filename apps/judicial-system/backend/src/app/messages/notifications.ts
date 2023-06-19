fd5ba4959dfeb8b4d14ed4261e41e34f1c8e28da
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2023-06-19 14:33:32 +0000

    fixing notification

--- apps/judicial-system/backend/src/app/messages/notifications.ts
+++ apps/judicial-system/backend/src/app/messages/notifications.ts
@@ -198,7 +198,7 @@ export const notifications = {
       id:
         'judicial.system.backend:notifications.prosecutor_court_date_email.body',
       defaultMessage:
-        '{scheduledCaseText}<br /><br />{courtDateText}<br /><br />{courtRoomText}<br /><br />{judgeText}{registrarText, select, NONE {} other {<br /><br />{registrarText}}}{sessionArrangements, select, PROSECUTOR_PRESENT {<br /><br /> Krafan verður tekin fyrir án boðunar í þinghald.}, NONE_PRESENT {}, other {<br /><br />{defenderText}.}}',
+        '{scheduledCaseText}<br /><br />{courtDateText}<br /><br />{courtRoomText}<br /><br />{judgeText}{registrarText, select, NONE {} other {<br /><br />{registrarText}}}{sessionArrangements, select, PROSECUTOR_PRESENT {} NONE_PRESENT {<br /><br /> Krafa tekin fyrir án boðunar í þinghald.} other {<br /><br />{defenderText}.}}',
       description:
         'Notaður fyrir beinagrind á pósti til sækjanda þegar fyrirtökutími er staðfestur',
     },
