8fbacc0eb75e0bc7aa50dc5e4a7b7029d08c3c81
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-06-23 14:18:27 +0000

    chore: cleanup

--- libs/clients/icelandic-health-insurance/rights-portal/src/lib/clients-rights-portal.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { ApiConfig } from './rightsPortalProvider'
-import { exportedApis } from './providers'
-
-@Module({
-  providers: [ApiConfig, ...exportedApis],
-  exports: exportedApis,
-})
-export class RightsPortalClientModule {}
