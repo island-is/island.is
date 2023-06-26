d971855e8f0e9d81e5b87fd6c3774e98b1b56bdf
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2023-06-26 20:32:31 +0000

    attempt to figure out the error

--- apps/services/auth/public-api/src/buildOpenApi.ts
+++ apps/services/auth/public-api/src/buildOpenApi.ts
@@ -3,8 +3,12 @@ import { buildOpenApi } from '@island.is/infra-nest-server'
 import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
 
-buildOpenApi({
-  path: 'apps/services/auth/public-api/src/openapi.yaml',
-  appModule: AppModule,
-  openApi,
-})
+try {
+  buildOpenApi({
+    path: 'apps/services/auth/public-api/src/openapi.yaml',
+    appModule: AppModule,
+    openApi,
+  })
+} catch (err) {
+  console.error(err)
+}
