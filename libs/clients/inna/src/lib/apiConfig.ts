dab270c3c8813e73676c3c4290d03cd0e4aecc66
Þórður Hafliðason <tolleinn@gmail.com>
2023-06-26 11:50:43 +0000

    Update codeowners

--- libs/clients/inna/src/lib/apiConfig.ts
+++ libs/clients/inna/src/lib/apiConfig.ts
@@ -10,13 +10,31 @@ import { InnaClientConfig } from './innaClient.config'
 export const ApiConfig = {
   provide: 'InnaApiProviderConfiguration',
   scope: LazyDuringDevScope,
-  useFactory: (config: ConfigType<typeof InnaClientConfig>) =>
+  useFactory: (
+    config: ConfigType<typeof InnaClientConfig>,
+    xroadConfig: ConfigType<typeof XRoadConfig>,
+  ) =>
     new Configuration({
       fetchApi: createEnhancedFetch({
         name: 'clients-inna',
         timeout: config.fetch.timeout,
+        autoAuth: idsClientConfig.isConfigured
+          ? {
+              mode: 'auto',
+              issuer: idsClientConfig.issuer,
+              clientId: idsClientConfig.clientId,
+              clientSecret: idsClientConfig.clientSecret,
+              scope: config.tokenExchangeScope,
+              tokenExchange: {
+                requestActorToken: config.requestActorToken,
+              },
+            }
+          : undefined,
       }),
-      basePath: `https://api-test.inna.is/namsferlaveita/`,
+      basePath: `${xroadConfig.xRoadBasePath}/r1/${config.xRoadServicePath}`,
+      headers: {
+        'X-Road-Client': xroadConfig.xRoadClient,
+      },
     }),
-  inject: [InnaClientConfig.KEY],
+  inject: [InnaClientConfig.KEY, XRoadConfig.KEY],
 }
