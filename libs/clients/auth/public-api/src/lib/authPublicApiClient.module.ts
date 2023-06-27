241b8a3fe380548ad9f5e61551c7e3633bb4ce49
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2023-06-27 09:55:58 +0000

    Testing a different config provider

--- libs/clients/auth/public-api/src/lib/authPublicApiClient.module.ts
+++ libs/clients/auth/public-api/src/lib/authPublicApiClient.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common'
 
 import { ApiConfiguration } from './apiConfiguration'
 import { exportedApis } from './apis'
+import { AuthPublicApiClientConfig } from './authPublicApiClient.config'
 
+export const AuthPublicApiClientConfigProvider = {
+  provide: AuthPublicApiClientConfig.KEY,
+  useValue: AuthPublicApiClientConfig,
+}
 @Module({
-  providers: [ApiConfiguration, ...exportedApis],
+  providers: [
+    ApiConfiguration,
+    AuthPublicApiClientConfigProvider,
+    ...exportedApis,
+  ],
   exports: exportedApis,
 })
 export class AuthPublicApiClientModule {}
