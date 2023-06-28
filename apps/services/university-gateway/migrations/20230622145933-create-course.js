493f53a7d4e4c5f82500691d50f803d8ff5adff7
Gunnar Traustason <gunnar.traustason@origo.is>
2023-06-28 13:46:33 +0000

    Course and University create migrations.

--- apps/services/university-gateway/migrations/20230622145933-create-course.js
+++ apps/services/university-gateway/migrations/20230622145933-create-course.js
@@ -1,21 +1,66 @@
 'use strict';
 
+'use strict'
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.createTable(
+        'course',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          external_id: {
+            type: Sequelize.STRING,
+          },
+          name_is: {
+            type: Sequelize.STRING,
+          },
+          name_en: {
+            type: Sequelize.STRING,
+          },
+          university_id: {
+            type: Sequelize.UUID,
+            references: {
+              model: 'university',
+              key: 'id',
+            },
+            allowNull: false,
+          },
+          credits: {
+            type: Sequelize.INTEGER,
+          },
+          semester_year: {
+            type: Sequelize.INTEGER,
+          },
+          semester_season: {
+            type: Sequelize.ENUM('FALL', 'SPRING', 'SUMMER'),
+          },
+          description_is: {
+            type: Sequelize.STRING,
+          },
+          description_en: {
+            type: Sequelize.STRING,
+          },
+        external_url_is: {
+            type: Sequelize.STRING,
+          },
+        external_url_en: {
+            type: Sequelize.STRING,
+          },
+        },
+        { transaction: t },
+      ),
+    )
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+  down: (queryInterface) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.dropTable('course', { transaction: t }),
+    )
+  },
+}
---
+++ apps/services/university-gateway/migrations/20230622145933-create-univresity.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.createTable(
+        'university',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          national_id: {
+            type: Sequelize.STRING,
+          },
+          contentful_key: {
+            type: Sequelize.STRING,
+          },
+        },
+        { transaction: t },
+      ),
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.dropTable('university', { transaction: t }),
+    )
+  },
+}
