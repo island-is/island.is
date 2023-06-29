4f8f826c7ca03badc85e4b643518b3cf6a7172c6
Jón Levy <levy@andes.is>
2023-06-29 15:29:26 +0000

    ci: nx distributed agents

--- scripts/ci/run-nx-affected.sh
+++ scripts/ci/run-nx-affected.sh
@@ -18,4 +18,20 @@ MAX_JOBS=${MAX_JOBS:-2}
 echo "[DEBUG]: NX version is $(yarn run nx --version)"
 echo "[DEBUG]: yarn version is $(yarn --version)"
 
-npx nx affected --base="$BASE" --head="$HEAD" --target="$target" --parallel="$MAX_JOBS" "$@"
+npx nx-cloud start-ci-run
+
+pids=()
+
+# list of commands to be run on agents
+npx nx affected --base="$BASE" --head="$HEAD" --target "$target" --parallel="$MAX_JOBS" "$@"
+pids+=($!)
+
+# run all commands in parallel and bail if one of them fails
+for pid in \${pids[*]}; do
+  if ! wait $pid; then
+    exit 1
+  fi
+done
+
+exit 0
+
