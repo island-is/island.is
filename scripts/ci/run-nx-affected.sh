a9887f636d2604ac26f77889a1af2d17a3e2d90a
Jón Levy <levy@andes.is>
2023-06-30 18:05:01 +0000

    ci: improve pid wait

--- scripts/ci/run-nx-affected.sh
+++ scripts/ci/run-nx-affected.sh
@@ -14,12 +14,23 @@ source "$DIR"/_common.sh
 
 MAX_JOBS=${MAX_JOBS:-2}
 
+pids=()
+
 for target in "$@"
 do
   yarn dlx nx affected --base="$BASE" --head="$HEAD" --target="$target" --parallel="$MAX_JOBS"
+  pids+=($!)
 done
 
-wait
+for pid in ${pids[*]}; do
+  if ! wait "$pid"; then
+    exit 1
+  fi
+done
+
+exit 0
+
+
 # # run all commands in parallel and bail if one of them fails
 # for pid in "${pids[@]}"; do
 #   if ! wait "$pid"; then
@@ -27,4 +38,3 @@ wait
 #   fi
 # done
 
-# exit 0
