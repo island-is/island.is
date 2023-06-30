b11f0ebc186f9eae43ac242d2bc25c4905c2428a
Jón Levy <levy@andes.is>
2023-06-30 11:59:13 +0000

    ci: more nx.json patching

--- scripts/ci/patch-nx-json.sh
+++ scripts/ci/patch-nx-json.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 project_root=$(git rev-parse --show-toplevel)
 tmp=$(mktemp)
-jq '.tasksRunnerOptions.default.runner = "@nrwl/nx-cloud"' "${project_root}/nx.json" > "$tmp" && mv "$tmp" "${project_root}/nx.json"
+jq '.tasksRunnerOptions.default.runner = "@nrwl/nx-cloud" | .taskRunnerOptions.default.options.cacheableOperations += ["generateDevIndexHTML"]' "${project_root}/nx.json" > "$tmp" && mv "$tmp" "${project_root}/nx.json"
