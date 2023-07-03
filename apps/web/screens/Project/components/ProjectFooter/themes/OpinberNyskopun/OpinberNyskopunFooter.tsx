f9f439fc513fbd9cd89179d45478ac79b22b6f2d
runarvestmann <runar@stefna.is>
2023-07-03 15:53:04 +0000

    Add logos

--- apps/web/screens/Project/components/ProjectFooter/themes/OpinberNyskopun/OpinberNyskopunFooter.tsx
+++ apps/web/screens/Project/components/ProjectFooter/themes/OpinberNyskopun/OpinberNyskopunFooter.tsx
@@ -6,19 +6,24 @@ import {
   GridContainer,
   GridRow,
   Hyphen,
+  Inline,
   Text,
 } from '@island.is/island-ui/core'
 import { FooterItem } from '@island.is/web/graphql/schema'
 import { webRichText } from '@island.is/web/utils/richText'
 import * as styles from './OpinberNyskopunFooter.css'
+import { useNamespace } from '@island.is/web/hooks'
 
 interface OpinberNyskopunFooterProps {
   footerItems: FooterItem[]
+  namespace: Record<string, string>
 }
 
 export const OpinberNyskopunFooter: React.FC<OpinberNyskopunFooterProps> = ({
   footerItems,
+  namespace,
 }) => {
+  const n = useNamespace(namespace)
   return (
     <footer className={styles.container}>
       <GridContainer>
@@ -58,6 +63,26 @@ export const OpinberNyskopunFooter: React.FC<OpinberNyskopunFooterProps> = ({
                   })}
                 </Box>
               ))}
+              <Box marginLeft="auto">
+                <Inline space={[1, 1, 1, 3]} alignY="center">
+                  <img
+                    width={420}
+                    src={n(
+                      'opinbernyskopun-logo1',
+                      'https://images.ctfassets.net/8k0h54kbe6bj/1i6yzfj8HFtKGx2n2mKKzE/0b9715a5f13ea8cf0727c97ccdaca870/FJR__IS_1L_utlinad_hvitt_0219.svg',
+                    )}
+                    alt=""
+                  />
+                  <img
+                    width={420}
+                    src={n(
+                      'opinbernyskopun-logo2',
+                      'https://images.ctfassets.net/8k0h54kbe6bj/1j0LduNFs0zmTfuBKDaEM1/8723d91686558b2f90e359aa47cd6446/HVIN_IS_2L_utlindad_hvitt_2022.svg',
+                    )}
+                    alt=""
+                  />
+                </Inline>
+              </Box>
             </Box>
           </GridColumn>
         </GridRow>
