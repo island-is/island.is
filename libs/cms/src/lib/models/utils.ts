fe8279510cff64af54e56499b2fedeb7babcf254
runarvestmann <runar@stefna.is>
2023-07-06 14:04:22 +0000

    Reduce spacing

--- libs/cms/src/lib/models/utils.ts
+++ libs/cms/src/lib/models/utils.ts
@@ -1,10 +1,8 @@
 /** Make sure that links are relative in case someone links to production directly */
 export const getRelativeUrl = (url: string) => {
   const urlObject = new URL(url.trim(), 'https://island.is')
-
   if (urlObject.host === 'island.is') {
     return urlObject.pathname
   }
-
   return urlObject.href
 }
