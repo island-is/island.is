7df6af3c433f79234f2c3ba63bcc5281bce4e9f4
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2023-07-06 11:02:19 +0000

    split up test into steps and add support function for changing users

--- apps/system-e2e/src/tests/islandis/application-system/acceptance/no-debt-certificate.spec.ts
+++ apps/system-e2e/src/tests/islandis/application-system/acceptance/no-debt-certificate.spec.ts
@@ -4,6 +4,7 @@ import { session } from '../../../../support/session'
 import { createApplication } from '../../../../support/application'
 import { label } from '../../../../support/i18n'
 import { m as messages } from '@island.is/application/templates/no-debt-certificate'
+import { switchUser } from '../../../../support/login'
 
 const homeUrl = '/umsoknir/skuldleysisvottord'
 
@@ -26,46 +27,40 @@ const applicationTest = base.extend<{ applicationPage: Page }>({
   },
 })
 
-const switchUser = async (page: Page, name?: string) => {
-  await page.locator('data-testid=user-menu >> visible=true').click()
-  await page.locator('role=button[name="Skipta um notanda"]').click()
-  if (name) {
-    const loc = await page.locator(`role=button[name*="${name}"]`).click()
-    console.log(loc)
-    await page.waitForURL(new RegExp(homeUrl), {
-      waitUntil: 'domcontentloaded',
-    })
-  }
-}
-
 applicationTest.describe('Data protection complaint application', () => {
   applicationTest(
-    'Should be able to start the application after changing to procuration user',
+    'User should be able to submit the application as a company',
     async ({ applicationPage }) => {
       const page = applicationPage
 
-      await applicationTest.step(
-        'Proceed with the application to reach the overview state and verify text',
-        async () => {
-          await switchUser(page, 'Icelandic Offshore ehf')
-        },
-      )
+      await applicationTest.step('Switch user', async () => {
+        await switchUser(page, homeUrl, 'Icelandic Offshore ehf')
+      })
 
-      await createApplication(page)
+      await applicationTest.step('Create application', async () => {
+        await createApplication(page)
+      })
+
       await applicationTest.step(
-        'Proceed with the application to reach the draft state',
+        'Proceed with the application and reach the submitted state',
         async () => {
           await page.getByTestId('agree-to-data-providers').check()
           await page.getByTestId('proceed').click()
           await expect(page.getByText('Gervimaður Afríka')).toBeVisible()
         },
       )
-      await expect(
-        page.getByRole('region').getByText(label(messages.confirmation)),
-      ).toBeVisible()
-      await expect(
-        page.getByRole('heading', { name: label(messages.successTitle) }),
-      ).toBeVisible()
+
+      await applicationTest.step(
+        'Verify confirmation and success',
+        async () => {
+          await expect(
+            page.getByRole('region').getByText(label(messages.confirmation)),
+          ).toBeVisible()
+          await expect(
+            page.getByRole('heading', { name: label(messages.successTitle) }),
+          ).toBeVisible()
+        },
+      )
     },
   )
 })
