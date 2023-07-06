2b220686bcbd29b4bb3fc5463a18637b51e674a7
runarvestmann <runar@stefna.is>
2023-07-06 11:58:57 +0000

    Use built-in pathname retrieval

--- libs/cms/src/lib/models/utils.ts
+++ libs/cms/src/lib/models/utils.ts
@@ -1,15 +1,3 @@
-const prefix = 'https://island.is'
-
 /** Make sure that links are relative in case someone links to production directly */
-export const getRelativeUrl = (url: string) => {
-  const trimmedUrl = url.trim()
-
-  if (trimmedUrl.startsWith(prefix)) {
-    if (trimmedUrl.length === prefix.length) {
-      return '/'
-    }
-    return trimmedUrl.slice(prefix.length)
-  }
-
-  return trimmedUrl
-}
+export const getRelativeUrl = (url: string) =>
+  new URL(url.trim(), 'https://island.is').pathname
