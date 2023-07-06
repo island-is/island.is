aaab502c09a159d6441db0ebc8c270cbfb9f14cd
runarvestmann <runar@stefna.is>
2023-07-06 14:03:41 +0000

    Change host matching to allow external links

--- libs/cms/src/lib/models/utils.ts
+++ libs/cms/src/lib/models/utils.ts
@@ -1,3 +1,10 @@
 /** Make sure that links are relative in case someone links to production directly */
-export const getRelativeUrl = (url: string) =>
-  new URL(url.trim(), 'https://island.is').pathname
+export const getRelativeUrl = (url: string) => {
+  const urlObject = new URL(url.trim(), 'https://island.is')
+
+  if (urlObject.host === 'island.is') {
+    return urlObject.pathname
+  }
+
+  return urlObject.href
+}
