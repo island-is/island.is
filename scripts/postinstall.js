4393241a962a187f71f7018c3ae620f32e0be863
Kristófer <kristofer@andes.is>
2023-07-12 08:36:40 +0000

    init

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,16 @@
 const { spawn } = require('child_process')
 
-if (!process.env.RUN_POSTINSTALL_SCHEMAS) process.exit(0)
-const cmd = spawn('yarn schemas', {
-  shell: true,
-  stdio: 'inherit',
-})
-cmd.on('exit', (code) => {
-  process.exit(code)
-})
+function schemas() {
+  if (!process.env.RUN_POSTINSTALL_SCHEMAS) process.exit(0)
+  const cmd = spawn('yarn schemas', {
+    shell: true,
+    stdio: 'inherit',
+  })
+  cmd.on('exit', (code) => {
+    if (code !== 0) process.exit(code)
+  })
+}
+
+for (const job of [schemas]) {
+  job()
+}
