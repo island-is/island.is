3541d928f06627e8df0bdf2b8d1b22da0a6e0cdd
Kristófer <kristofer@andes.is>
2023-07-12 08:43:26 +0000

    Logging in postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,15 +2,18 @@ const { spawn } = require('child_process')
 
 function schemas() {
   if (!process.env.GENERATE_SCHEMAS_ON_INSTALL) process.exit(0)
+  console.log('Generating schemas...')
   const cmd = spawn('yarn schemas', {
     shell: true,
     stdio: 'inherit',
   })
   cmd.on('exit', (code) => {
+    console.log(`Schema generation completed with exit code ${code}`)
     if (code !== 0) process.exit(code)
   })
 }
 
 for (const job of [schemas]) {
+  console.log('Running job: ' + job.name)
   job()
 }
