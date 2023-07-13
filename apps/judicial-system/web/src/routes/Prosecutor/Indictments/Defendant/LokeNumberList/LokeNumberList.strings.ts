e3800aa779be4898e8e71e3a9ca1eb82295bd73f
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2023-07-13 12:07:23 +0000

    feat(j-s): Adding Löke number selection to front-end  (#11543)
    * adding description
    
    * adding lokenumberlist component
    
    * adding strings
    
    * creating folder instead of many files
    
    * adding todo
    
    * feat(j-s): Retrieve case info from LÖKE
    
    * feat(j-s): Allow user to pick LOKE police case numbers and retrieve info from police service
    
    ---------
    
    Co-authored-by: unakb <una@kolibri.is>

---
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Defendant/LokeNumberList/LokeNumberList.strings.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl'
+
+export const lokeNumberList = defineMessages({
+  selectAllCheckbox: {
+    id: 'judicial.system.core:loke_number_list.select_all_checkbox',
+    defaultMessage: 'Velja öll',
+    description:
+      'Notaður sem titill í hak til að velja öll Velja öll Löke númerin sem er í boði',
+  },
+  selectNumberButton: {
+    id: 'judicial.system.core:loke_number_list.select_number_button',
+    defaultMessage: 'Velja númer',
+    description:
+      'Textin á takkanum sem velur Löke númerið sem er valið í töflunni',
+  },
+})
