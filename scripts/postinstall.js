eb2539fa78dda80b6e99a76b07032b9f40be6ef2
Kristófer <kristofer@andes.is>
2023-07-13 10:20:05 +0000

    Revert "fix(yarn): Better post-install env variable (#11725)"
    This reverts commit 47b2e13622fedf3f15b4b158941b3905195c4bad.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,19 +1,10 @@
 const { spawn } = require('child_process')
 
-function schemas() {
-  if (!process.env.GENERATE_SCHEMAS_ON_INSTALL) process.exit(0)
-  console.log('Generating schemas...')
-  const cmd = spawn('yarn schemas', {
-    shell: true,
-    stdio: 'inherit',
-  })
-  cmd.on('exit', (code) => {
-    console.log(`Schema generation completed with exit code ${code}`)
-    if (code !== 0) process.exit(code)
-  })
-}
-
-for (const job of [schemas]) {
-  console.log('Running job: ' + job.name)
-  job()
-}
+if (!process.env.RUN_POSTINSTALL_SCHEMAS) process.exit(0)
+const cmd = spawn('yarn schemas', {
+  shell: true,
+  stdio: 'inherit',
+})
+cmd.on('exit', (code) => {
+  process.exit(code)
+})
