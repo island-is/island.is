e6bdecf937c43ef00078d732a05a1516c9a4f5a3
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2023-07-17 14:19:47 +0000

    possible scopes

---
+++ libs/auth/scopes/src/lib/judicalSystem.scope.ts
@@ -0,0 +1,9 @@
+export enum JudicalSystemScope {
+  read = '@rettarvorslugatt.island.is/judical-system:read',
+  write = '@rettarvorslugatt.island.is/judical-system:write',
+  admin = '@rettarvorslugatt.island.is/judical-system:admin',
+  defender = '@rettarvorslugatt.island.is/judical-system:defender',
+  prosecutor = '@rettarvorslugatt.island.is/judical-system:prosecutor',
+}
+
+// TODO fix
