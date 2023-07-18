a618e4120e4390fce590157c7f60b3101778cf91
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2023-07-18 17:55:19 +0000

    check if reverting the authPublicApiClient.module.ts will build in the ci

--- libs/clients/auth/public-api/src/lib/authPublicApiClient.module.ts
+++ libs/clients/auth/public-api/src/lib/authPublicApiClient.module.ts
@@ -2,18 +2,9 @@ import { Module } from '@nestjs/common'
 
 import { ApiConfiguration } from './apiConfiguration'
 import { exportedApis } from './apis'
-import { AuthPublicApiClientConfig } from './authPublicApiClient.config'
 
-export const AuthPublicApiClientConfigProvider = {
-  provide: AuthPublicApiClientConfig.KEY,
-  useValue: AuthPublicApiClientConfig,
-}
 @Module({
-  providers: [
-    ApiConfiguration,
-    AuthPublicApiClientConfigProvider,
-    ...exportedApis,
-  ],
+  providers: [ApiConfiguration, ...exportedApis],
   exports: exportedApis,
 })
 export class AuthPublicApiClientModule {}
