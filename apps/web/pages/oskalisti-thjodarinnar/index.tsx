a3f6cb531cce9ca0f3f0c99d065b857cae177471
Rafn Árnason <rafnarnason@gmail.com>
2023-07-18 13:01:33 +0000

    cleanup main format

--- apps/web/pages/oskalisti-thjodarinnar/index.tsx
+++ apps/web/pages/oskalisti-thjodarinnar/index.tsx
@@ -7,64 +7,59 @@ import {
   Text,
   Box,
   Navigation,
-
   Link,
 } from '@island.is/island-ui/core'
 import { withMainLayout } from '@island.is/web/layouts/main'
 import SidebarLayout from '@island.is/web/screens/Layouts/SidebarLayout'
 
-
-const title = "Óskalisti þjóðarinnar"
+const title = 'Óskalisti þjóðarinnar'
 
 const renderNavigation = (isMobile: boolean) => {
-    return (
-      <Navigation
-        baseId={isMobile ? 'mobileNav' : 'desktopNav'}
-        isMenuDialog={isMobile}
-        activeItemTitle={title}
-        title={title}
-        items={[]}
-        renderLink={(link, { typename, slug }) => {
-          return (
-            <Link href="">{link}</Link>
-          )
-        }}
-      />
-    )
-  }
-
-
-function Stuff() {
-   
-    return (
-        <SidebarLayout sidebarContent={renderNavigation(false)}>
-          <Stack space={2}>
-            <Box display={['none', 'none', 'inlineBlock']}>
-              <Breadcrumbs
-                items={[
-                  {
-                    title: 'Ísland.is',
-                    href: '/',
-                  },
-                  {
-                    title: title,
-                  },
-                ]}
-              />
-            </Box>
-            <Box display={['block', 'block', 'none']}>{renderNavigation(true)}</Box>
-            <Text variant="h1" as="h1">
-              {title}
-            </Text>
-            <iframe frameBorder="0" width="100%" height="1500px" src="https://islandis.featureupvote.com/"></iframe>
-
-          </Stack>
-          
-        </SidebarLayout>
-      )
+  return (
+    <Navigation
+      baseId={isMobile ? 'mobileNav' : 'desktopNav'}
+      isMenuDialog={isMobile}
+      activeItemTitle={title}
+      title={title}
+      items={[]}
+      renderLink={(link, { typename, slug }) => {
+        return <Link href="">{link}</Link>
+      }}
+    />
+  )
 }
 
-  
+function Stuff() {
+  return (
+    <SidebarLayout sidebarContent={renderNavigation(false)}>
+      <Stack space={2}>
+        <Box display={['none', 'none', 'inlineBlock']}>
+          <Breadcrumbs
+            items={[
+              {
+                title: 'Ísland.is',
+                href: '/',
+              },
+              {
+                title: title,
+              },
+            ]}
+          />
+        </Box>
+        <Box display={['block', 'block', 'none']}>{renderNavigation(true)}</Box>
+        <Text variant="h1" as="h1">
+          {title}
+        </Text>
+        <iframe
+          frameBorder="0"
+          width="100%"
+          height="1500px"
+          src="https://islandis.featureupvote.com/"
+        ></iframe>
+      </Stack>
+    </SidebarLayout>
+  )
+}
 
 const screen = withMainLayout(Stuff)
 
