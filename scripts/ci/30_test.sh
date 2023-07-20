88e9fa32985c10c2a52d824bdc59d7bb9b9544f3
Kristófer <kristofer@andes.is>
2023-07-20 12:51:35 +0000

    Move environment variable-setting to script

--- scripts/ci/30_test.sh
+++ scripts/ci/30_test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
+: "${NODE_OPTIONS:=--max-old-space-size=8192}"
+: "${DD_CIVISIBILITY_AGENTLESS_ENABLED:=true}"
+: "${DD_SITE:=datadoghq.eu}"
+: "${DD_ENV:=dev}"
+: "${DD_SERVICE:=unit-test-action}"
+: "${DD_API_KEY:='<set-api-key>'}"
+
+# Default to big old-space, and more options for testing, but allow overriding
+NODE_OPTIONS="--max-old-space-size=8193 --unhandled-rejections=warn --require=dd-trace/ci/init ${NODE_OPTIONS:-}"
+SERVERSIDE_FEATURES_ON=\"\"
 EXTRA_OPTS=""
 
 projects_uncollectible_coverage=("contentful-translation-extension" "application-templates-no-debt-certificate" "api-domains-email-signup" "skilavottord-web" "shared-babel")
@@ -9,13 +19,7 @@ if [[ ! " ${projects_uncollectible_coverage[*]} " =~ " ${APP} " ]]; then
   EXTRA_OPTS="--codeCoverage"
 fi
 
-DD_ENV=ci \
-  DD_SERVICE="${APP}" \
-  DD_SITE=datadoghq.eu \
-  DD_CIVISIBILITY_AGENTLESS_ENABLED=true \
-  SERVERSIDE_FEATURES_ON=\"\" \
-  NODE_OPTIONS="--max-old-space-size=8192 --unhandled-rejections=warn --require=dd-trace/ci/init ${NODE_OPTIONS:-}" \
-  yarn run test \
+yarn run test \
   "${APP}" \
   ${EXTRA_OPTS} \
   --verbose \
