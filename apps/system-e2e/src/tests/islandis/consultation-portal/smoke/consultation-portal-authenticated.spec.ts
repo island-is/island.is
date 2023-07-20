b87e28eaa4a514be975b08b1cd72b9e0aa9fc503
Kristófer <kristofer@andes.is>
2023-07-20 12:42:58 +0000

    test(system-e2e): Fix broken test

--- apps/system-e2e/src/tests/islandis/consultation-portal/smoke/consultation-portal-authenticated.spec.ts
+++ apps/system-e2e/src/tests/islandis/consultation-portal/smoke/consultation-portal-authenticated.spec.ts
@@ -4,36 +4,17 @@ import { session } from '../../../../support/session'
 
 test.describe('Consultation portal authenticated', () => {
   let context: BrowserContext
-  const authLink = new RegExp(`^${urls.authUrl}`)
 
-  test.use({ baseURL: `${urls.islandisBaseUrl}/samradsgatt` })
-
-  const checkIfLoggedOutOrLoggedIn = async (page: Page) => {
-    await page.goto(icelandicAndNoPopupUrl('/samradsgatt'))
-    const loggedOut = page.getByTestId('menu-login-btn')
-    const loggedIn = page.getByRole('button', { name: 'Gervimaður Afríka' })
-    if (await loggedOut.isVisible()) {
-      await loggedOut.click()
-      await page.waitForURL(authLink)
-      await page.locator('#phoneUserIdentifier').fill('0103019')
-      await page.locator('#submitPhoneNumber').isEnabled()
-      await page.locator('#submitPhoneNumber').click()
-      await page.waitForURL(`**${icelandicAndNoPopupUrl('/samradsgatt')}`)
-    } else {
-      await loggedIn.isVisible()
-    }
-  }
-
-  test.beforeAll(async ({ browser, baseURL }) => {
+  test.beforeAll(async ({ browser }) => {
     context = await session({
       browser: browser,
       storageState: 'consultation-auth.json',
-      idsLoginOn: false,
+      idsLoginOn: true,
       phoneNumber: '0103019',
-      homeUrl: baseURL,
+      homeUrl: '/samradsgatt',
+      authTriggerUrl: '/minarsidur',
     })
     const page = await context.newPage()
-    await checkIfLoggedOutOrLoggedIn(page)
   })
 
   test.afterAll(async () => {
