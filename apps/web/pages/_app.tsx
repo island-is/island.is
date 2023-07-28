9d07c0397a1439313fda86d41d2e3ae9e8d1260f
runarvestmann <runar@stefna.is>
2023-07-28 13:20:43 +0000

    Add document file again to make sure lang property is set

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,21 +1,7 @@
 import '@island.is/api/mocks'
 import { globalStyles } from '@island.is/island-ui/core'
-import Head from 'next/head'
-import { PLAUSIBLE_SCRIPT_SRC } from '../constants'
+import App from 'next/app'
 
 globalStyles()
 
-export default ({ Component, pageProps }) => {
-  return (
-    <>
-      <Head>
-        <script
-          defer
-          data-domain={process.env.TRACKING_DOMAIN || 'island.is'}
-          src={PLAUSIBLE_SCRIPT_SRC}
-        ></script>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
-}
+export default App
