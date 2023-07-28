d28c8b4f5b3276b003a038dab98f4246366f6454
runarvestmann <runar@stefna.is>
2023-07-28 15:49:53 +0000

    No need to have same function twice

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,32 +1,10 @@
 import withApollo from '@island.is/web/graphql/withApollo'
 import { withLocale } from '@island.is/web/i18n'
 import homeScreen from '@island.is/web/screens/Home/Home'
-import { GetServerSideProps } from 'next'
-
-// Hack needed to avoid JSON-Serialization validation error from Next.js https://github.com/zeit/next.js/discussions/11209
-// >>> Reason: `undefined` cannot be serialized as JSON. Please use `null` or omit this value all together.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const deleteUndefined = (obj: Record<string, any> | undefined): void => {
-  if (obj) {
-    Object.keys(obj).forEach((key: string) => {
-      if (obj[key] && typeof obj[key] === 'object') {
-        deleteUndefined(obj[key])
-      } else if (typeof obj[key] === 'undefined') {
-        delete obj[key] // eslint-disable-line no-param-reassign
-      }
-    })
-  }
-}
+import { getServerSidePropsWrapper } from '../utils/getServerSidePropsWrapper'
 
 const Screen = withApollo(withLocale('is')(homeScreen))
 
 export default Screen
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const props = Screen.getProps ? await Screen.getProps(ctx) : ctx
-  deleteUndefined(props)
-
-  return {
-    props,
-  }
-}
+export const getServerSideProps = getServerSidePropsWrapper(Screen)
