464e901263349fc4cf73b333ce8bd934e8012b40
Pétur Neisti Erlingsson <petur@petur.io>
2023-07-31 11:34:08 +0000

    revert: "chore: Move islyklar to EFS (#11754)" (#11780)
    This reverts commit f98d4986680a43d51506a2d7875411047d0bb0b7.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- infra/src/dsl/volumes.spec.ts
+++ infra/src/dsl/volumes.spec.ts
@@ -23,7 +23,7 @@ describe('Volume Support', () => {
     {
       name: 'something',
       size: '1Gi',
-      accessModes: 'ReadWrite',
+      accessModes: 'ReadOnly',
       mountPath: '/storage_one',
     },
     {
@@ -44,18 +44,25 @@ describe('Volume Support', () => {
     })) as SerializeSuccess<HelmService>
   })
   it('Support multi volume definitions', () => {
-    expect(stagingWithVolumes.serviceDef[0].pvcs![1]).toEqual({
-      name: 'somethingelse',
+    expect(stagingWithVolumes.serviceDef[0].pvcs![0]).toEqual({
+      name: 'something',
       size: '1Gi',
-      accessModes: 'ReadWriteMany',
-      mountPath: '/storage_two',
+      accessModes: 'ReadOnlyMany',
+      mountPath: '/storage_one',
       storageClass: 'efs-csi',
-    })
+    }),
+      expect(stagingWithVolumes.serviceDef[0].pvcs![1]).toEqual({
+        name: 'somethingelse',
+        size: '1Gi',
+        accessModes: 'ReadWriteMany',
+        mountPath: '/storage_two',
+        storageClass: 'efs-csi',
+      })
   })
   it('Support default name for volumes', async () => {
     const sut: ServiceBuilder<'api'> = service('api').volumes({
       size: '1Gi',
-      accessModes: 'ReadWrite',
+      accessModes: 'ReadOnly',
       mountPath: '/storage_one',
     })
     const stagingWithDefaultVolume = (await generateOutputOne({
@@ -67,7 +74,7 @@ describe('Volume Support', () => {
     expect(stagingWithDefaultVolume.serviceDef[0].pvcs![0]).toEqual({
       name: 'api',
       size: '1Gi',
-      accessModes: 'ReadWriteMany',
+      accessModes: 'ReadOnlyMany',
       mountPath: '/storage_one',
       storageClass: 'efs-csi',
     })
