07742be6e91bb9ca0a3a9f6d408361facc39acd9
runarvestmann <runar@stefna.is>
2023-08-01 15:06:25 +0000

    Use get initial props for regulations homepage

--- apps/web/pages/reglugerdir/index.ts
+++ apps/web/pages/reglugerdir/index.ts
@@ -1,10 +1,13 @@
 import withApollo from '@island.is/web/graphql/withApollo'
 import { withLocale } from '@island.is/web/i18n'
 import RegulationsHome from '@island.is/web/screens/Regulations/RegulationsHome'
-import { getServerSidePropsWrapper } from '@island.is/web/utils/getServerSidePropsWrapper'
 
 const Screen = withApollo(withLocale('is')(RegulationsHome))
 
-export default Screen
+const ScreenWithGetInitialProps: typeof Screen & {
+  getInitialProps?: typeof Screen.getProps
+} = Screen
 
-export const getServerSideProps = getServerSidePropsWrapper(Screen)
+ScreenWithGetInitialProps.getInitialProps = Screen.getProps
+
+export default ScreenWithGetInitialProps
