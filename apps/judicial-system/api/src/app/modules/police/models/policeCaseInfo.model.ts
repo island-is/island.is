f949e2b36af00db24344564d196164473299720d
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-08-02 08:50:42 +0000

    Attempts to force rebuild

--- apps/judicial-system/api/src/app/modules/police/models/policeCaseInfo.model.ts
+++ apps/judicial-system/api/src/app/modules/police/models/policeCaseInfo.model.ts
@@ -4,8 +4,10 @@ import { Field, ID, ObjectType } from '@nestjs/graphql'
 export class PoliceCaseInfo {
   @Field(() => ID)
   readonly policeCaseNumber!: string
+
   @Field(() => String, { nullable: true })
   readonly place?: string
+
   @Field(() => String, { nullable: true })
   readonly date?: string
 }
