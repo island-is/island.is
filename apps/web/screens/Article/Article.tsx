d617fba3ce746b6a7b1dbf3ac488260f4b972908
runarvestmann <runar@stefna.is>
2023-08-04 13:35:57 +0000

    Change button styles

--- apps/web/screens/Article/Article.tsx
+++ apps/web/screens/Article/Article.tsx
@@ -616,7 +616,7 @@ const ArticleScreen: Screen<ArticleProps> = ({
             />
           )}
           {!inStepperView && (
-            <Box display="flex" columnGap={3}>
+            <Box display="flex" alignItems="center" columnGap={2}>
               {!inStepperView && (
                 <Webreader readId={null} readClass="rs_read" />
               )}
