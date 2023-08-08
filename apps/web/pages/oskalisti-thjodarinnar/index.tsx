8193f20b2a3f8023f719464a4968d8f203bbebcc
Rafn Árnason <rafnarnason@gmail.com>
2023-08-08 15:49:47 +0000

    cleanup demo folders

--- apps/web/pages/oskalisti-thjodarinnar/index.tsx
+++ apps/web/pages/oskalisti-thjodarinnar/index.tsx
@@ -1,65 +1,33 @@
 import withApollo from '@island.is/web/graphql/withApollo'
 import { withLocale } from '@island.is/web/i18n'
 import React from 'react'
-import {
-  Breadcrumbs,
-  Stack,
-  Text,
-  Box,
-  Navigation,
-  Link,
-} from '@island.is/island-ui/core'
 import { withMainLayout } from '@island.is/web/layouts/main'
-import SidebarLayout from '@island.is/web/screens/Layouts/SidebarLayout'
 
 const title = 'Óskalisti þjóðarinnar'
-
-const renderNavigation = (isMobile: boolean) => {
-  return (
-    <Navigation
-      baseId={isMobile ? 'mobileNav' : 'desktopNav'}
-      isMenuDialog={isMobile}
-      activeItemTitle={title}
-      title={title}
-      items={[]}
-      renderLink={(link, { typename, slug }) => {
-        return <Link href="">{link}</Link>
-      }}
-    />
-  )
-}
-
+// todo heading and breadcrumbs
 function Stuff() {
   return (
-    <SidebarLayout sidebarContent={renderNavigation(false)}>
-      <Stack space={2}>
-        <Box display={['none', 'none', 'inlineBlock']}>
-          <Breadcrumbs
-            items={[
-              {
-                title: 'Ísland.is',
-                href: '/',
-              },
-              {
-                title: title,
-              },
-            ]}
-          />
-        </Box>
-        <Box display={['block', 'block', 'none']}>{renderNavigation(true)}</Box>
-        <Text variant="h1" as="h1">
-          {title}
-        </Text>
-        <iframe
-          frameBorder="0"
-          scrolling="no"
-          width="100%"
-          height="2500px"
-          src="https://islandis.featureupvote.com/"
-        ></iframe>
-      </Stack>
-    </SidebarLayout>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <iframe
+        src="https://islandis.featureupvote.com/"
+        style={{
+          marginTop: '0px', // adjust this value to match the height of your header
+          width: '100%',
+          height: 'calc(100% - 0px)', // adjust this value to match the height of your header
+          border: 'none',
+          overflowY: 'scroll',
+          // textAlign: 'center',
+        }}
+      />
+    </div>
   )
 }
 
-export default withApollo(withLocale('is')(withMainLayout(Stuff)))
+export default withApollo(
+  withLocale('is')(
+    withMainLayout(Stuff, {
+      showHeader: true,
+      showFooter: false,
+    }),
+  ),
+)
