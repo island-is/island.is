40168d436f1d75394ecc7e0557fd52f87ed55b8a
Rafn Árnason <rafnarnason@gmail.com>
2023-08-09 10:18:43 +0000

    getServerSidePropsWrapper

--- apps/web/pages/oskalisti-thjodarinnar/index.tsx
+++ apps/web/pages/oskalisti-thjodarinnar/index.tsx
@@ -1,65 +1,47 @@
 import withApollo from '@island.is/web/graphql/withApollo'
 import { withLocale } from '@island.is/web/i18n'
 import React from 'react'
-import {
-  Breadcrumbs,
-  Stack,
-  Text,
-  Box,
-  Navigation,
-  Link,
-} from '@island.is/island-ui/core'
 import { withMainLayout } from '@island.is/web/layouts/main'
-import SidebarLayout from '@island.is/web/screens/Layouts/SidebarLayout'
+import { getServerSidePropsWrapper } from '@island.is/web/utils/getServerSidePropsWrapper'
 
-const title = 'Óskalisti þjóðarinnar'
-
-const renderNavigation = (isMobile: boolean) => {
+function displayFeatureUpvote() {
   return (
-    <Navigation
-      baseId={isMobile ? 'mobileNav' : 'desktopNav'}
-      isMenuDialog={isMobile}
-      activeItemTitle={title}
-      title={title}
-      items={[]}
-      renderLink={(link, { typename, slug }) => {
-        return <Link href="">{link}</Link>
-      }}
-    />
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <iframe
+        src="https://islandis.featureupvote.com/"
+        style={{
+          marginTop: '0px',
+          width: '100%',
+          height: 'calc(100% - 0px)',
+          border: 'none',
+          overflowY: 'scroll',
+        }}
+      />
+    </div>
   )
 }
 
-function Stuff() {
-  return (
-    <SidebarLayout sidebarContent={renderNavigation(false)}>
-      <Stack space={2}>
-        <Box display={['none', 'none', 'inlineBlock']}>
-          <Breadcrumbs
-            items={[
-              {
-                title: 'Ísland.is',
-                href: '/',
-              },
-              {
-                title: title,
-              },
-            ]}
-          />
-        </Box>
-        <Box display={['block', 'block', 'none']}>{renderNavigation(true)}</Box>
-        <Text variant="h1" as="h1">
-          {title}
-        </Text>
-        <iframe
-          frameBorder="0"
-          scrolling="no"
-          width="100%"
-          height="2500px"
-          src="https://islandis.featureupvote.com/"
-        ></iframe>
-      </Stack>
-    </SidebarLayout>
-  )
-}
 
-export default withApollo(withLocale('is')(withMainLayout(Stuff)))
+const Screen = withApollo(
+  withLocale('is')(
+    withMainLayout(displayFeatureUpvote, {
+      showHeader: true,
+      showFooter: false,
+    }),
+  ),
+)
+
+export default Screen
+
+export const getServerSideProps = getServerSidePropsWrapper(Screen)
+
+
+
+// export default withApollo(
+//   withLocale('is')(
+//     withMainLayout(displayFeatureUpvote, {
+//       showHeader: true,
+//       showFooter: false,
+//     }),
+//   ),
+// )
