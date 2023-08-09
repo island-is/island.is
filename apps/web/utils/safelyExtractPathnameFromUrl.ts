63ee33fec6f6de8847e338d3f41d11890a5f8ede
runarvestmann <runar@stefna.is>
2023-08-09 16:30:44 +0000

    Fix pathname extraction

--- apps/web/utils/safelyExtractPathnameFromUrl.ts
+++ apps/web/utils/safelyExtractPathnameFromUrl.ts
@@ -1,7 +1,7 @@
 export const safelyExtractPathnameFromUrl = (url?: string) => {
   try {
-    return new URL(url).pathname
+    return new URL(url, 'https://island.is').pathname
   } catch {
-    return ''
+    return url ?? ''
   }
 }
