77f5882ee18392567be88ebbf8e3a33dad06f6ac
runarvestmann <runar@stefna.is>
2023-08-11 11:01:32 +0000

    Add comment

--- apps/web/utils/safelyExtractPathnameFromUrl.ts
+++ apps/web/utils/safelyExtractPathnameFromUrl.ts
@@ -1,7 +1,6 @@
+// Use fallback domain in case url is relative since we just want the pathname
+const FALLBACK_DOMAIN = 'https://island.is'
+
 export const safelyExtractPathnameFromUrl = (url?: string) => {
-  try {
-    return new URL(url, 'https://island.is').pathname
-  } catch {
-    return url ?? ''
-  }
+  return new URL(url, FALLBACK_DOMAIN).pathname
 }
