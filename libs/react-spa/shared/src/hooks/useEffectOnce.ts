664de55366338252fb7759414d059f98aa029c9f
Snær Seljan Þóroddsson <snaer@aranja.com>
2023-08-14 11:27:29 +0000

    update comment to link

--- libs/react-spa/shared/src/hooks/useEffectOnce.ts
+++ libs/react-spa/shared/src/hooks/useEffectOnce.ts
@@ -2,7 +2,10 @@ import { EffectCallback, useEffect, useRef } from 'react'
 
 /**
  * @Note: Use with caution. Please read before usage.
- * This is a hack to get around ${@link https://react.dev/reference/react/StrictMode|React strict mode} in useEffect
+ *
+ * This is a hack to get around React strict mode in useEffect.
+ * @see https://react.dev/reference/react/StrictMode
+ *
  * This hook is used to run an effect only once, on mount, regardless of React >= 18 strict-mode.
  * This hook should generally not be used, but is useful for some edge cases, such as in our AuthProvider.
  */
