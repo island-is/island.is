5cadf608417bb6f793972f9a963a1b43d894176e
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-08-18 12:26:27 +0000

    Fixes Contentful string

--- apps/judicial-system/backend/src/app/messages/notifications.ts
+++ apps/judicial-system/backend/src/app/messages/notifications.ts
@@ -348,9 +348,9 @@ export const notifications = {
         'Fyrirsögn í pósti til fangeslis þegar vistunarseðill og þingbók eru send',
     },
     body: {
-      id: 'judicial.system.backend:notifications.prison_ruling_email.body_v3',
+      id: 'judicial.system.backend:notifications.prison_ruling_email.body_v4',
       defaultMessage:
-        '{institutionName} hefur úrskurðað aðila í {caseType, select, ADMISSION_TO_FACILITY {vistun á viðeigandi stofnun} other {gæsluvarðhald}}{isModifyingRuling {} other { í þinghaldi sem lauk rétt í þessu}}. Hægt er að nálgast þingbók og vistunarseðil á {linkStart}yfirlitssíðu málsins í Réttarvörslugátt{linkEnd}.',
+        '{institutionName} hefur úrskurðað aðila í {caseType, select, ADMISSION_TO_FACILITY {vistun á viðeigandi stofnun} other {gæsluvarðhald}}{isModifyingRuling, select, true {} other { í þinghaldi sem lauk rétt í þessu}}. Hægt er að nálgast þingbók og vistunarseðil á {linkStart}yfirlitssíðu málsins í Réttarvörslugátt{linkEnd}.',
       description: 'Texti í pósti til fangelis með link á réttarvörslugátt',
     },
   }),
