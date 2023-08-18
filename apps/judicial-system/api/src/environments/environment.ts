560caa48f0d005c9e935e25fff5dc768fba704ad
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-08-18 16:49:50 +0000

    Cleans up api environment file

--- apps/judicial-system/api/src/environments/environment.ts
+++ apps/judicial-system/api/src/environments/environment.ts
@@ -7,14 +7,8 @@ const devConfig = {
   backend: {
     url: 'http://localhost:3344',
   },
-  features: {
-    hidden: process.env.HIDDEN_FEATURES ?? '',
-  },
 }
 if (process.env.NODE_ENV === 'production') {
-  if (!process.env.SAML_ENTRY_POINT) {
-    throw new Error('Missing SAML_ENTRY_POINT environment.')
-  }
   if (!process.env.AUTH_JWT_SECRET) {
     throw new Error('Missing AUTH_JWT_SECRET environment.')
   }
