02232dea3cbb2a3b37701b6f1698aaa4273234eb
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-08-21 18:27:54 +0000

    chore(j-s): Unit Test Performance (#11838)
    * Improves uniit test performance
    
    * Moves notifications unit test
    
    * Removes old test
    
    * Closes testing case module after use
    
    * Closes testing modules
    
    * Rewrites tests
    
    * Reverst project config change
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/backend/src/app/modules/file/test/fileController/getCaseFileSignedUrlRolesRules.spec.ts
+++ apps/judicial-system/backend/src/app/modules/file/test/fileController/getCaseFileSignedUrlRolesRules.spec.ts
@@ -20,9 +20,6 @@ describe('FileController - Get case file signed url rules', () => {
 
   it('should give permission to five roles', () => {
     expect(rules).toHaveLength(5)
-  })
-
-  it('should give permission to prosecutors, representatives, judges, registrars and assistants', () => {
     expect(rules).toContain(prosecutorRule)
     expect(rules).toContain(representativeRule)
     expect(rules).toContain(judgeRule)
