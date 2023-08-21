0f15810af56388c90e0eaf0fd9e7d8ed8e67bb7e
runarvestmann <runar@stefna.is>
2023-08-21 17:36:13 +0000

    Add column data to footer

--- apps/web/components/Footer/Footer.tsx
+++ apps/web/components/Footer/Footer.tsx
@@ -1,31 +1,101 @@
+import { BLOCKS } from '@contentful/rich-text-types'
+import { theme } from '@island.is/island-ui/theme'
 import {
   Box,
   GridColumn,
   GridContainer,
   GridRow,
+  Hyphen,
   Text,
 } from '@island.is/island-ui/core'
+import type { SliceType } from '@island.is/island-ui/contentful'
+import type { FooterItem } from '@island.is/web/graphql/schema'
+import { useWindowSize } from '@island.is/web/hooks/useViewport'
+import { webRichText } from '@island.is/web/utils/richText'
+
 import * as styles from './Footer.css'
 
+const IMAGE_WIDTH = 75
+const IMAGE_MARGIN_RIGHT = 3
+
 interface FooterProps {
-  imageUrl: string
+  imageUrl?: string
   heading: string
+  columns: FooterItem[]
 }
 
-export const Footer = ({ imageUrl, heading }: FooterProps) => {
+export const Footer = ({ imageUrl, heading, columns }: FooterProps) => {
+  const { width } = useWindowSize()
+
+  const isMobileScreenWidth = width < theme.breakpoints.sm
+
+  // TODO: figure out heading padding and why it's not aligning with first column
+
   return (
     <footer className={styles.footer}>
-      <Box paddingY={3}>
+      <Box paddingTop={3} paddingBottom={5}>
         <GridContainer>
-          <GridRow alignItems="center" rowGap={3}>
-            <GridColumn hiddenBelow="sm">
-              <img width={75} src={imageUrl} alt="" />
-            </GridColumn>
-            <GridColumn>
+          <GridRow alignItems="center" marginBottom={3}>
+            {imageUrl && (
+              <GridColumn hiddenBelow="sm">
+                <Box marginRight={IMAGE_MARGIN_RIGHT}>
+                  <img width={IMAGE_WIDTH} src={imageUrl} alt="" />
+                </Box>
+              </GridColumn>
+            )}
+            <GridColumn offset={isMobileScreenWidth ? '2/12' : undefined}>
               <Text variant="h2">{heading}</Text>
             </GridColumn>
           </GridRow>
-          <GridRow>{/* TODO: add columns */}</GridRow>
+          <GridRow className={styles.noWrap}>
+            {imageUrl && !isMobileScreenWidth && (
+              <GridColumn>
+                <Box marginRight={IMAGE_MARGIN_RIGHT}>
+                  <img
+                    style={{
+                      visibility: 'hidden',
+                    }}
+                    src={imageUrl}
+                    width={IMAGE_WIDTH}
+                    alt=""
+                  />
+                </Box>
+              </GridColumn>
+            )}
+            <GridRow>
+              {columns.map((column, index) => (
+                <GridColumn
+                  key={index}
+                  span={isMobileScreenWidth ? '1/1' : undefined}
+                  paddingBottom={3}
+                  offset={isMobileScreenWidth ? '2/12' : undefined}
+                >
+                  <Box marginRight={3}>
+                    {column.title && (
+                      <Text fontWeight="semiBold" marginBottom={1}>
+                        <Hyphen>{column.title}</Hyphen>
+                      </Text>
+                    )}
+                    {webRichText((column?.content ?? []) as SliceType[], {
+                      renderNode: {
+                        [BLOCKS.PARAGRAPH]: (_node, children) => {
+                          return (
+                            <Text
+                              variant="medium"
+                              marginBottom={1}
+                              lineHeight="lg"
+                            >
+                              {children}
+                            </Text>
+                          )
+                        },
+                      },
+                    })}
+                  </Box>
+                </GridColumn>
+              ))}
+            </GridRow>
+          </GridRow>
         </GridContainer>
       </Box>
     </footer>
