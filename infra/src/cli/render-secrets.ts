3620d4c079c24f857847143ba7b844ba165dafc9
Jón Levy <levy@andes.is>
2023-08-23 00:30:08 +0000

    chore: refactored get-secrets bash script as infra cli command
    infra is now a hoisted NX app

--- infra/src/cli/render-secrets.ts
+++ infra/src/cli/render-secrets.ts
@@ -2,6 +2,7 @@ import { Envs } from '../environments'
 import { Charts } from '../uber-charts/all-charts'
 import { renderHelmServices } from '../dsl/exports/helm'
 import { getSsmParams } from '../dsl/adapters/get-ssm-params'
+import { escapeValue } from './utils'
 
 const EXCLUDED_ENVIRONMENT_NAMES = [
   'DB_PASSWORD',
@@ -13,8 +14,12 @@ const OVERRIDE_ENVIRONMENT_NAMES: Record<string, string> = {
   IDENTITY_SERVER_CLIENT_SECRET: '/k8s/local-dev/IDENTITY_SERVER_CLIENT_SECRET',
 }
 
+const SHARED_ENVIRONMENT_NAMES: Record<string, string> = {
+  NX_CLOUD_ACCESS_TOKEN: '/local/NX_CLOUD_ACCESS_TOKEN',
+}
+
 export const renderSecretsCommand = async (service: string) => {
-  renderSecrets(service).catch((error) => {
+  return await renderSecrets(service).catch((error) => {
     if (error.name === 'CredentialsProviderError') {
       console.error(
         'Could not load AWS credentials from any providers. Did you forget to configure environment variables, aws profile or run `aws sso login`?',
@@ -26,7 +31,9 @@ export const renderSecretsCommand = async (service: string) => {
   })
 }
 
-export const renderSecrets = async (service: string) => {
+export const renderSecrets = async (
+  service: string,
+): Promise<[string, string][]> => {
   const services = await Promise.all(
     Object.values(Charts).map(
       async (chart) =>
@@ -56,15 +63,15 @@ export const renderSecrets = async (service: string) => {
       }
       return request
     })
-
-  const values = await getSsmParams(
-    secretRequests.map(([_, ssmName]) => ssmName),
+  const sharedRequests: [string, string][] = Object.entries(
+    SHARED_ENVIRONMENT_NAMES,
   )
-
-  secretRequests.forEach(([envName, ssmName]) => {
-    const escapedValue = values[ssmName]
-      .replace(/\s+/g, ' ')
-      .replace(/'/g, "'\\''")
-    console.log(`export ${envName}='${escapedValue}'`)
+  const finalRequests = [...secretRequests, ...sharedRequests]
+  const values = await getSsmParams(
+    finalRequests.map(([_, ssmName]) => ssmName),
+  )
+  return finalRequests.map(([envName, ssmName]) => {
+    const escapedValue = escapeValue(values[ssmName])
+    return [envName, escapedValue]
   })
 }
