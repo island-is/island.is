8eea8c7363c681224424ff576df8967751abd53d
runarvestmann <runar@stefna.is>
2023-08-28 14:11:17 +0000

    Improve UI

--- apps/web/components/connected/HousingBenefitCalculator/HousingBenefitCalculator.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css'
-
-export const radioButtons = style({
-  gap: '0.2rem',
-})
-
-export const inputContainer = style({
-  maxWidth: '70%',
-})
--- apps/web/components/connected/HousingBenefitCalculator/HousingBenefitCalculator.tsx
+++ apps/web/components/connected/HousingBenefitCalculator/HousingBenefitCalculator.tsx
@@ -1,163 +1,179 @@
-import { ConnectedComponent } from '@island.is/web/graphql/schema'
+import {
+  ConnectedComponent,
+  GetHousingBenefitCalculationQuery,
+  GetHousingBenefitCalculationQueryVariables,
+} from '@island.is/web/graphql/schema'
 import {
   Box,
   Button,
-  Column,
-  GridContainer,
-  GridRow,
   Inline,
   Input,
   RadioButton,
   Stack,
   Text,
 } from '@island.is/island-ui/core'
-import * as style from './HousingBenefitCalculator.css'
 import { useState } from 'react'
-import { ConsoleLogger } from '@nestjs/common'
-import { set } from 'lodash'
-import { value } from '../../Organization/Slice/TimelineSlice/Event.css'
+
+import { useLazyQuery } from '@apollo/client'
+import { GET_HOUSING_BENEFIT_CALCULATION } from '@island.is/web/screens/queries/HousingBenefitCalculator'
+import { useNamespace } from '@island.is/web/hooks'
+import { formatCurrency } from '@island.is/application/ui-components'
 
 interface HousingBenefitCalculatorProps {
   slice: ConnectedComponent
 }
 
 const HousingBenefitCalculator = ({ slice }: HousingBenefitCalculatorProps) => {
-  //const { title, body } = slice
-  const [radioValue, setRadioValue] = useState(0)
-  const [result, setResult] = useState(0)
+  const n = useNamespace(slice.json ?? {})
   const [state, setState] = useState({
-    radioValue: radioValue, //TODO: broken
+    houseHoldMembers: 0,
     income: 0,
     assets: 0,
     housingCost: 0,
   })
   const handleChange = (event: any) => {
-    setState({ ...state, [event.target.name]: event.target.value })
-  }
-  const calculator = () => {
-    handleChange
-    // if ((outputBox.hidden = true)) {
-    //   outputBox.hidden = false
-    // }
-    // const res =
-    //   state.radioValue + state.income + state.assets + state.housingCost
-    // setResult(res)
-    console.log(state)
+    if (event.target.name.includes('houseHoldMembers')) {
+      setState({ ...state, houseHoldMembers: Number(event.target.value) })
+    } else {
+      event.target.value = event.target.value.replace('.', '')
+      setState({ ...state, [event.target.name]: Number(event.target.value) })
+    }
   }
 
-  //const outputBox = document.getElementById('outputContainer')
+  const [getCalculations, { data, loading, error, called }] = useLazyQuery<
+    GetHousingBenefitCalculationQuery,
+    GetHousingBenefitCalculationQueryVariables
+  >(GET_HOUSING_BENEFIT_CALCULATION)
+
+  const calculate = () => {
+    getCalculations({
+      variables: {
+        input: {
+          numberOfHouseholdMembers: state.houseHoldMembers,
+          totalMonthlyIncome: state.income,
+          totalAssets: state.assets,
+          housingCostsPerMonth: state.housingCost,
+        },
+      },
+    })
+  }
+
+  const formatInput = (event: any) => {
+    event.target.value.replace(/\B(?=(\d{3})+(?!\d))/g, '')
+  }
+
+  const maximumHousingBenefits =
+    data?.housingBenefitCalculatorCalculation.maximumHousingBenefits
+  const reduction = data?.housingBenefitCalculatorCalculation.reductions
+  const estimatedHousingBenefits =
+    data?.housingBenefitCalculatorCalculation?.estimatedHousingBenefits
   return (
     <Box>
-      <Box
-        id="calculator"
-        background="blue100"
-        paddingX={15}
-        paddingY={8}
-        marginBottom={5}
-      >
+      <Box background="blue100" paddingX={15} paddingY={8} marginBottom={5}>
         <Stack space={5}>
           <Box>
             <Text variant="medium" fontWeight="light" paddingBottom={2}>
-              Fjöldi heimilismanna í húsnæði?
+              {n(
+                'Number of household members?',
+                'Fjöldi heimilismanna í húsnæði?',
+              )}
             </Text>
 
             <Inline space={5}>
               <RadioButton
-                //name="radioValue"
                 label="1"
-                //value={1}
-                //onChange={handleChange}
-                onChange={() => setRadioValue(1)}
-                checked={radioValue === 1}
+                value={1}
+                name="houseHoldMembers1"
+                onChange={handleChange}
+                checked={state.houseHoldMembers == 1}
               />
               <RadioButton
-                //name="radioValue"
                 label="2"
-                //value={2}
-                onChange={() => setRadioValue(2)}
-                checked={radioValue === 2}
+                value={2}
+                name="houseHoldMembers2"
+                onChange={handleChange}
+                checked={state.houseHoldMembers == 2}
               />
               <RadioButton
-                //name="radioValue"
                 label="3"
-                //value={3}
-                onChange={() => setRadioValue(3)}
-                checked={radioValue === 3}
+                value={3}
+                name="houseHoldMembers3"
+                onChange={handleChange}
+                checked={state.houseHoldMembers == 3}
               />
               <RadioButton
-                //name="radioValue"
-                label="4 eða fleiri"
-                //value={4}
-                onChange={() => setRadioValue(4)}
-                checked={radioValue === 4}
+                label={n('4 or more', '4 eða fleiri')}
+                value={4}
+                name="houseHoldMembers4"
+                onChange={handleChange}
+                checked={state.houseHoldMembers == 4}
               />
             </Inline>
           </Box>
           <Box>
             <Text variant="medium" fontWeight="light" paddingBottom={2}>
-              Samanlagðar mánaðartekjur heimilsmanna 18 ára og eldri (tekjur f.
-              skatt)?
+              {n(
+                'Total monthly income of household members aged 18 and over (income before tax)?',
+                'Samtals mánaðarlegar tekjur heimilismanna 18 ára og eldri (tekjur f.skatt)?',
+              )}
             </Text>
             <Input
               size="sm"
-              label="Tekjur"
+              label={n('Income', 'Tekjur')}
               name="income"
-              placeholder="Tekjur"
+              placeholder={n('Income', 'Tekjur')}
               type="number"
               onChange={handleChange}
             />
           </Box>
           <Box>
             <Text variant="medium" fontWeight="light" paddingBottom={2}>
-              Eignir Heimilismanna 18 ára og eldri?
+              {n(
+                'Assets of household members aged 18 years and over?',
+                'Eignir Heimilismanna 18 ára og eldri?',
+              )}
             </Text>
             <Input
               size="sm"
-              label="Eignir"
+              label={n('Assets', 'Eignir')}
               name="assets"
-              placeholder="Eignir"
+              placeholder={n('Assets', 'Eignir')}
               type="number"
               onChange={handleChange}
             />
           </Box>
           <Box>
             <Text variant="medium" fontWeight="light" paddingBottom={2}>
-              Húsnæðiskostnaður á mánuði?
+              {n('Housing costs per month?', 'Húsnæðiskostnaður á mánuði?')}
             </Text>
             <Input
               size="sm"
-              label="Húsnæðiskostnaður"
+              label={n('Housing costs', 'Húsnæðiskostnaður')}
               name="housingCost"
-              placeholder="Húsnæðiskostnaður"
+              placeholder={n('Housing costs', 'Húsnæðiskostnaður')}
               type="number"
               onChange={handleChange}
             />
           </Box>
           <Text variant="eyebrow">
-            Útreikningur húsnæðisbóta samkvæmt reiknivélinni byggir á þeim
-            forsendum sem þú gafst upp og telst ekki bindandi ákvörðun um
-            húsnæðisbætur. Útreikningur miðast við greiðslur húsnæðisbóta fyrir
-            heilt almanaksár.
+            {n(
+              'The calculation of housing benefit is based on the assumptions you provided and is not considered a binding decision on housing benefit. The calculation is based on housing benefit payments for an entire calendar year.',
+              'Útreikningur húsnæðisbóta samkvæmt reiknivélinni byggir á þeim forsendum sem þú gafst upp og telst ekki bindandi ákvörðun um húsnæðisbætur. Útreikningur miðast við greiðslur húsnæðisbóta fyrir heilt almanaksár.',
+            )}
           </Text>
-          <Button
-            id="calculateButton"
-            // todo add click handler
-            //onClick={() => setResult(calculator(radioValue, 1, 1, 1))}
-            onClick={calculator}
-          >
-            Reikna
+          <Button onClick={() => calculate()}>
+            {n('Calculate', 'Reikna')}
           </Button>
         </Stack>
       </Box>
       <Box
-        id="outputContainer"
         background="blue100"
         paddingX={15}
         paddingY={5}
-        hidden
+        style={{ display: called ? 'block' : 'none' }}
       >
         <Text variant="h3">
-          <strong>Niðurstöður</strong>
+          <strong>{n('Results', 'Niðurstöður')}</strong>
         </Text>
         <Text
           variant="medium"
@@ -165,13 +181,24 @@ const HousingBenefitCalculator = ({ slice }: HousingBenefitCalculatorProps) => {
           paddingBottom={2}
           paddingTop={5}
         >
-          Hámarksbætur miðað við fjölda heimilismanna eru {result} kr. á mánuði.
+          {n(
+            'Maximum benefits based on the number of household members are ',
+            'Hámarksbætur miðað við fjölda heimilismanna eru ',
+          )}
+          {formatCurrency(String(maximumHousingBenefits))}{' '}
+          {n(' per month.', ' á mánuði.')}
         </Text>
-        <Text variant="medium" fontWeight="light" paddingBottom={3}>
-          Skerðing vegna húsnæðiskostnaðar eru {result + 1} kr. á mánuði.
+        <Text variant="medium" fontWeight="light" paddingBottom={5}>
+          {n(
+            'Reductions due to income are ',
+            'Skerðing vegna húsnæðiskostnaðar eru ',
+          )}
+          {formatCurrency(String(reduction))} {n(' per month.', ' á mánuði.')}
         </Text>
         <Text variant="medium" fontWeight="light">
-          Áætlaðar húsnæðisbætur eru <strong>{result + 2} kr.</strong> á mánuði.
+          {n('Estimated housing benefits are ', 'Áætlaðar húsnæðisbætur eru ')}
+          <strong>{formatCurrency(String(estimatedHousingBenefits))}</strong>
+          {n(' per month.', ' á mánuði.')}
         </Text>
       </Box>
     </Box>
