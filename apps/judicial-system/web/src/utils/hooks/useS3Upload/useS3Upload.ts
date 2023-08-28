f6757c9be37b9a5d7ab27c4f7203fae91ef4baaf
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-08-28 16:40:00 +0000

    Adds the possibility to create a file with chapter and order within chapter

--- apps/judicial-system/web/src/utils/hooks/useS3Upload/useS3Upload.ts
+++ apps/judicial-system/web/src/utils/hooks/useS3Upload/useS3Upload.ts
@@ -37,6 +37,8 @@ import { useUploadPoliceCaseFileMutation } from './uploadPoliceCaseFile.generate
 export interface TUploadFile extends UploadFile {
   category?: CaseFileCategory
   policeCaseNumber?: string
+  chapter?: number
+  orderWithinChapter?: number
 }
 
 const createFormData = (presignedPost: PresignedPost, file: File): FormData => {
@@ -277,14 +279,11 @@ export const useS3Upload = (caseId: string) => {
           },
         })
 
-        if (
-          !uploadPoliceCaseFileData ||
-          !uploadPoliceCaseFileData.uploadPoliceCaseFile
-        ) {
+        if (!uploadPoliceCaseFileData?.uploadPoliceCaseFile) {
           throw Error('failed to upload police case file')
         }
 
-        const data2 = await createFile({
+        const { data: createFileData } = await createFile({
           variables: {
             input: {
               caseId,
@@ -293,11 +292,13 @@ export const useS3Upload = (caseId: string) => {
               size: uploadPoliceCaseFileData.uploadPoliceCaseFile.size,
               policeCaseNumber: file.policeCaseNumber,
               category: file.category,
+              chapter: file.chapter,
+              orderWithinChapter: file.orderWithinChapter,
             },
           },
         })
 
-        if (!data2.data?.createFile.id) {
+        if (!createFileData?.createFile.id) {
           throw Error('failed to add file to case')
         }
 
@@ -310,9 +311,11 @@ export const useS3Upload = (caseId: string) => {
             status: 'done',
             category: file.category,
             policeCaseNumber: file.policeCaseNumber,
+            chapter: file.chapter,
+            orderWithinChapter: file.orderWithinChapter,
           },
           // We need to set the id so we are able to delete the file later
-          data2.data.createFile.id,
+          createFileData.createFile.id,
         )
 
         return uploadPoliceCaseFileData?.uploadPoliceCaseFile
