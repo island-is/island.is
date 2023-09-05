e24b8ab8bec9f0c9d22fd9349d28472d2ca7a25d
runarvestmann <runar@stefna.is>
2023-09-05 10:02:32 +0000

    Reuse token

--- libs/clients/housing-benefit-calculator/src/lib/housing-benefit-calculator.service.ts
+++ libs/clients/housing-benefit-calculator/src/lib/housing-benefit-calculator.service.ts
@@ -1,14 +1,24 @@
 import { Inject } from '@nestjs/common'
 import type { ConfigType } from '@island.is/nest/config'
+import { FetchError } from '@island.is/clients/middlewares'
 import { AuthHeaderMiddleware } from '@island.is/auth-nest-tools'
 import { AuthenticateApi, ReiknivelApi } from '../../gen/fetch'
 import { HousingBenefitCalculatorClientConfig } from './housing-benefit-calculator.config'
+
+interface CalculateInput {
+  numberOfHouseholdMembers: number
+  totalMonthlyIncome: number
+  totalAssets: number
+  housingCostsPerMonth: number
+}
 
 const round = (value: number | null | undefined) => {
   return typeof value === 'number' ? Math.round(value) : value
 }
 
 export class HousingBenefitCalculatorClientService {
+  private loginToken: string | null | undefined = null
+
   constructor(
     @Inject(HousingBenefitCalculatorClientConfig.KEY)
     private clientConfig: ConfigType<
@@ -18,28 +28,27 @@ export class HousingBenefitCalculatorClientService {
     private readonly calculationApi: ReiknivelApi,
   ) {}
 
-  private async getAuthenticatedCalculationApi() {
-    const loginData = await this.authenticationApi.apiV1AuthenticateTokenPost({
-      loginModel: {
-        username: this.clientConfig.username,
-        password: this.clientConfig.password,
-      },
-    })
+  private async getAuthenticatedCalculationApi(fetchNewToken = false) {
+    if (fetchNewToken || !this.loginToken) {
+      const loginData = await this.authenticationApi.apiV1AuthenticateTokenPost(
+        {
+          loginModel: {
+            username: this.clientConfig.username,
+            password: this.clientConfig.password,
+          },
+        },
+      )
+      this.loginToken = loginData.token
+    }
 
     return this.calculationApi.withMiddleware(
-      new AuthHeaderMiddleware(`Bearer ${loginData.token}`),
+      new AuthHeaderMiddleware(`Bearer ${this.loginToken}`),
     )
   }
 
-  async calculate(input: {
-    numberOfHouseholdMembers: number
-    totalMonthlyIncome: number
-    totalAssets: number
-    housingCostsPerMonth: number
-  }) {
-    // TODO: only authenticate if we get a rejection instead of for every request
+  private async fetchCalculation(input: CalculateInput, fetchNewToken = false) {
     const authenticatedCalculationApi =
-      await this.getAuthenticatedCalculationApi()
+      await this.getAuthenticatedCalculationApi(fetchNewToken)
 
     const calculationData =
       await authenticatedCalculationApi.apiV1ReiknivelReiknivelPost({
@@ -48,13 +57,27 @@ export class HousingBenefitCalculatorClientService {
         heildarEignir: input.totalAssets,
         husnaedisKostnadur: input.housingCostsPerMonth,
       })
-
     return {
       maximumHousingBenefits: round(calculationData.manadarlegarHamarksBaetur),
       reductions: round(calculationData.manadarlegHusnaedisKostnadarSkerding),
       estimatedHousingBenefits: round(
         calculationData.manadarlegarHusnaedisbaetur,
       ),
+    }
+  }
+
+  async calculate(input: CalculateInput) {
+    try {
+      const data = await this.fetchCalculation(input)
+      return data
+    } catch (error) {
+      if (error instanceof FetchError) {
+        // Renew token if we are unauthorized (most likely due to token expiring)
+        if (error.status === 401) {
+          return this.fetchCalculation(input, true)
+        }
+      }
+      throw error
     }
   }
 }
