8ee794f619e000ebafb5a65c71054c0988d6e18a
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-09-05 09:04:33 +0000

    Resolves linting issues

--- apps/judicial-system/backend/test/globalTeardown.ts
+++ apps/judicial-system/backend/test/globalTeardown.ts
@@ -4,4 +4,5 @@ import { register } from 'tsconfig-paths'
 const tsConfig = require(`../${require('../tsconfig.json').extends}`)
 register({ baseUrl: './', paths: tsConfig.compilerOptions.paths })
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export default async () => {}
