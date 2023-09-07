949c34ed4dd3218cb36ea76d9a4ff66a50ecf8a4
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2023-09-07 10:58:08 +0000

    Added api scope to routes, fixed wrong message in navigation, and fixed messages file

--- libs/service-portal/core/src/lib/messages.ts
+++ libs/service-portal/core/src/lib/messages.ts
@@ -506,7 +506,10 @@ export const m = defineMessages({
     defaultMessage:
       'Hér er markmiðið að þú getir fundið yfirlit yfir leyfisbréf og vottorð til starfsréttinda. Unnið er að því að koma öllum leyfisbréfum og vottorðum um starfsréttindi á einn stað.',
   },
-
+  occupationalLicensesNavIntro: {
+    id: 'service.portal:occupational-licenses-description',
+    defaultMessage: 'Hér er munu birtast starfsleyfi sem þú hefur fengið.',
+  },
   finance: {
     id: 'service.portal:finance',
     defaultMessage: 'Fjármál',
