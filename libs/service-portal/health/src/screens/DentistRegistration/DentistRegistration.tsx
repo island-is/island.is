dc4b995e083a3253215c376ad9747fb6380421d0
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2023-09-08 11:30:33 +0000

    Adding pagination

--- libs/service-portal/health/src/screens/DentistRegistration/DentistRegistration.tsx
+++ libs/service-portal/health/src/screens/DentistRegistration/DentistRegistration.tsx
@@ -26,6 +26,7 @@ export const DentistRegistration = () => {
 
   const [page, setPage] = useState(DEFAULT_PAGE_NUMBER)
   const [cursor, setCursor] = useState(DEFAULT_CURSOR)
+  const [direction, setDirection] = useState(true)
 
   const {
     data: status,
@@ -46,99 +47,135 @@ export const DentistRegistration = () => {
       input: {
         contractType,
         limit: DEFAULT_PAGE_SIZE,
-        after: cursor,
+        after: direction === true ? cursor : undefined,
+        before: direction === false ? cursor : undefined,
       },
     },
   })
 
-  console.log(data)
+  console.log(data?.response?.pageInfo)
 
-  const handlePaginate = (prev: number, current: number) => {
-    useEffect(() => {
-      console.log(page)
-      setCursor(data?.response?.pageInfo.startCursor ?? DEFAULT_CURSOR)
-    }, [page])
+  const onPaginate = (prev: number, current: number) => {
+    console.log(prev, current)
 
-    if (!canRegister && !statusLoading && !statusError)
-      return (
-        <AlertMessage
-          type="error"
-          title={formatMessage(messages.dentistRegisterForbiddenTitle)}
-          message={formatMessage(messages.dentistRegisterForbiddenInfo)}
-        />
-      )
+    if (prev < current) {
+      setDirection(true) // going forward
+      setCursor(data?.response?.pageInfo?.endCursor ?? DEFAULT_CURSOR)
+    } else {
+      setDirection(false) // going backward
+      setCursor(data?.response?.pageInfo?.startCursor ?? DEFAULT_CURSOR)
+    }
 
-    if (statusLoading || loading)
-      return (
-        <Box paddingY={2}>
-          <Stack space={4}>
-            <CardLoader />
-            <SkeletonLoader
-              borderRadius="large"
-              space={2}
-              height={40}
-              repeat={3}
-            />
-          </Stack>
-        </Box>
-      )
+    return current
+    // if (prev < current) {
+    //   fetchMore({
+    //     variables: {
+    //       input: {
+    //         contractType,
+    //         limit: DEFAULT_PAGE_SIZE,
+    //         after: cursor,
+    //       },
+    //     },
+    //   })
+    //   setCursor(data?.response?.pageInfo?.endCursor ?? DEFAULT_CURSOR)
+    // } else {
+    //   fetchMore({
+    //     variables: {
+    //       input: {
+    //         contractType,
+    //         limit: DEFAULT_PAGE_SIZE,
+    //         before: cursor,
+    //       },
+    //     },
+    //   })
+    //   setCursor(data?.response?.pageInfo?.endCursor ?? DEFAULT_CURSOR)
+    // }
 
+    // return current
+  }
+
+  if (!canRegister && !statusLoading && !statusError)
+    return (
+      <AlertMessage
+        type="error"
+        title={formatMessage(messages.dentistRegisterForbiddenTitle)}
+        message={formatMessage(messages.dentistRegisterForbiddenInfo)}
+      />
+    )
+
+  if (statusLoading || loading)
     return (
       <Box paddingY={2}>
-        <IntroHeader
-          title={formatMessage(messages.dentistRegisterationPageTitle)}
-          intro={formatMessage(messages.healthCenterRegistrationInfo)}
-        />
-        {loading ? (
-          <SkeletonLoader repeat={3} space={2} height={40} />
-        ) : (
-          <>
-            <T.Table>
-              <T.Head>
-                <T.Row>
-                  <T.HeadData>{formatMessage(m.dentist)}</T.HeadData>
-                  <T.HeadData>
-                    {formatMessage(messages.dentistPractice)}
-                  </T.HeadData>
-                  <T.HeadData>{formatMessage(m.postcode)}</T.HeadData>
-                  <T.HeadData>{formatMessage(m.address)}</T.HeadData>
-                  <T.HeadData></T.HeadData>
-                </T.Row>
-              </T.Head>
-              <T.Body>
-                {data?.response?.dentists.map((dentist, key) => (
-                  <T.Row key={key}>
-                    <T.Data>{dentist?.name}</T.Data>
-                    <T.Data>{dentist?.practice}</T.Data>
-                    <T.Data>{`${dentist?.address?.postalCode} ${dentist.address?.municipality}`}</T.Data>
-                    <T.Data>{dentist?.address?.streetAddress}</T.Data>
-                    <T.Data>
-                      {dentist.name === data.current?.dentist?.name
-                        ? formatMessage(messages.dentistCurrent)
-                        : ''}
-                    </T.Data>
-                  </T.Row>
-                ))}
-              </T.Body>
-            </T.Table>
-            {data?.response?.totalCount && (
-              <Pagination
-                totalPages={Math.ceil(
-                  data?.response.totalCount / DEFAULT_PAGE_SIZE,
-                )}
-                page={page}
-                renderLink={(page, className, children) => (
-                  <button className={className} onClick={(p) => console.log(p)}>
-                    {children}
-                  </button>
-                )}
-              />
-            )}
-          </>
-        )}
+        <Stack space={4}>
+          <CardLoader />
+          <SkeletonLoader
+            borderRadius="large"
+            space={2}
+            height={40}
+            repeat={3}
+          />
+        </Stack>
       </Box>
     )
-  }
+
+  return (
+    <Box paddingY={2}>
+      <IntroHeader
+        title={formatMessage(messages.dentistRegisterationPageTitle)}
+        intro={formatMessage(messages.healthCenterRegistrationInfo)}
+      />
+      {loading ? (
+        <SkeletonLoader repeat={3} space={2} height={40} />
+      ) : (
+        <>
+          <T.Table>
+            <T.Head>
+              <T.Row>
+                <T.HeadData>{formatMessage(m.dentist)}</T.HeadData>
+                <T.HeadData>
+                  {formatMessage(messages.dentistPractice)}
+                </T.HeadData>
+                <T.HeadData>{formatMessage(m.postcode)}</T.HeadData>
+                <T.HeadData>{formatMessage(m.address)}</T.HeadData>
+                <T.HeadData></T.HeadData>
+              </T.Row>
+            </T.Head>
+            <T.Body>
+              {data?.response?.dentists.map((dentist, key) => (
+                <T.Row key={key}>
+                  <T.Data>{dentist?.name}</T.Data>
+                  <T.Data>{dentist?.practice}</T.Data>
+                  <T.Data>{`${dentist?.address?.postalCode} ${dentist.address?.municipality}`}</T.Data>
+                  <T.Data>{dentist?.address?.streetAddress}</T.Data>
+                  <T.Data>
+                    {dentist.name === data.current?.dentist?.name
+                      ? formatMessage(messages.dentistCurrent)
+                      : ''}
+                  </T.Data>
+                </T.Row>
+              ))}
+            </T.Body>
+          </T.Table>
+          {data?.response?.totalCount && (
+            <Pagination
+              totalPages={Math.ceil(
+                data?.response.totalCount / DEFAULT_PAGE_SIZE,
+              )}
+              page={page}
+              renderLink={(page, className, children) => (
+                <button
+                  className={className}
+                  onClick={() => setPage((prev) => onPaginate(prev, page))}
+                >
+                  {children}
+                </button>
+              )}
+            />
+          )}
+        </>
+      )}
+    </Box>
+  )
 }
 
 export default DentistRegistration
