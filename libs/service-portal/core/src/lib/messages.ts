7391bba34c98236f3a4a65b614298b04a758809b
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2023-09-08 10:12:31 +0000

    Same id in messages twice now fixed

--- libs/service-portal/core/src/lib/messages.ts
+++ libs/service-portal/core/src/lib/messages.ts
@@ -507,7 +507,7 @@ export const m = defineMessages({
       'Hér er markmiðið að þú getir fundið yfirlit yfir leyfisbréf og vottorð til starfsréttinda. Unnið er að því að koma öllum leyfisbréfum og vottorðum um starfsréttindi á einn stað.',
   },
   occupationalLicensesNavIntro: {
-    id: 'service.portal:occupational-licenses-description',
+    id: 'service.portal:occupational-licenses-nav-intro',
     defaultMessage: 'Hér er munu birtast starfsleyfi sem þú hefur fengið.',
   },
   finance: {
