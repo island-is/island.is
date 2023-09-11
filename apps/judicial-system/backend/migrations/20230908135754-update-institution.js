2307e5d1e75af8efcd6e716d646e357441236832
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-09-11 14:52:04 +0000

    Updates migration

--- apps/judicial-system/backend/migrations/20230908135754-update-institution.js
+++ apps/judicial-system/backend/migrations/20230908135754-update-institution.js
@@ -3,17 +3,26 @@
 module.exports = {
   async up(queryInterface) {
     return queryInterface.sequelize.transaction((transaction) =>
-      queryInterface.bulkUpdate(
-        'institution',
-        { active: true },
-        {
-          id: [
-            '0be621ec-c063-4df3-ab15-61f6e421ed7c', // Lögreglustjórinn á Norðurlandi Vestra]
-            '7299ab8f-2fcc-40be-8194-8c2f749b4791', // Héraðsdómur Norðurlands Vestra
-          ],
-        },
-        { transaction },
-      ),
+      queryInterface
+        .bulkUpdate(
+          'institution',
+          { active: true },
+          {
+            id: [
+              '0be621ec-c063-4df3-ab15-61f6e421ed7c', // Lögreglustjórinn á Norðurlandi Vestra]
+              '7299ab8f-2fcc-40be-8194-8c2f749b4791', // Héraðsdómur Norðurlands Vestra
+            ],
+          },
+          { transaction },
+        )
+        .then(() =>
+          queryInterface.bulkUpdate(
+            'institution',
+            { police_case_number_prefix: '314' },
+            { id: 'affee2cd-5519-450e-b11c-bdd61229e1ad' },
+            { transaction },
+          ),
+        ),
     )
   },
 
