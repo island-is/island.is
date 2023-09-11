914523495dc13b270fe1eee8581f7e2debc11c86
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2023-09-11 09:53:01 +0000

    updated jest package

--- scripts/build-graphql-schema.ts
+++ scripts/build-graphql-schema.ts
@@ -3,9 +3,10 @@ import yargs from 'yargs'
 import { createApp } from '@island.is/infra-nest-server'
 
 const argv = yargs(process.argv.slice(2))
-const args = argv.string('_').help().argv
 
 const main = async () => {
+  const args = await argv.string('_').help().argv
+
   const { AppModule } = require('../' + args._[0])
 
   const app = await createApp({
