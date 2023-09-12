f3c91f10ac9279f8111fa6229f064ba9259f0ede
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2023-09-12 14:24:52 +0000

    moved translation string to messages file

--- libs/portals/shared-modules/delegations/src/lib/messages.ts
+++ libs/portals/shared-modules/delegations/src/lib/messages.ts
@@ -205,4 +205,8 @@ export const m = defineMessages({
     id: 'sp.access-control-delegations:close-modal',
     defaultMessage: 'Loka glugga',
   },
+  validToTranslation: {
+    id: 'sp.settings-access-control:access-valid-to',
+    defaultMessage: 'Í gildi til',
+  },
 })
