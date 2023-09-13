4e834893b51257c73aae3a3a85eac9e4d26f8e3b
Jón Levy <levy@andes.is>
2023-09-13 00:18:31 +0000

    chore: ff cleanup

--- libs/feature-flags/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigCatModule } from './types'
-
-export const getConfigCatModule = async (): Promise<ConfigCatModule> => {
-  if (typeof window === 'undefined') {
-    return import('configcat-node')
-  } else {
-    return import('configcat-js')
-  }
-}
