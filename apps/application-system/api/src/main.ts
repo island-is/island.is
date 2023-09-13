55c03ff713d848b0242e782b9636ff599453b2dd
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-09-13 11:16:17 +0000

    Fix application-system-api after yargs update

--- apps/application-system/api/src/main.ts
+++ apps/application-system/api/src/main.ts
@@ -1,6 +1,14 @@
 import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
-const { argv } = yargs(process.argv.slice(2))
+const argv = yargs(hideBin(process.argv))
+  .option('job', {
+    string: true,
+    choices: ['worker', 'server'],
+    description:
+      'Indicate if application-system-api should run as a worker or server',
+  })
+  .parseSync()
 
 if (argv.job === 'worker') {
   import('./worker').then((app) => {
