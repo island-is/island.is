08b025fff9b6d562d54a90fc0d77a19e35078e2c
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2023-09-14 10:02:05 +0000

    feat(search-indexer): Use feature flag instead of env variable for turning nested entry resolving on or off (#12120)

--- libs/cms/src/lib/search/cmsSync.module.ts
+++ libs/cms/src/lib/search/cmsSync.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ElasticService } from '@island.is/content-search-toolkit'
+import { ConfigModule } from '@island.is/nest/config'
+import {
+  FeatureFlagConfig,
+  FeatureFlagModule,
+} from '@island.is/nest/feature-flags'
+
 import { ContentfulService } from './contentful.service'
 import { ArticleSyncService } from './importers/article.service'
 import { CmsSyncService } from './cmsSync.service'
@@ -20,6 +26,13 @@ import { EnhancedAssetSyncService } from './importers/enhancedAsset.service'
 import { VacancySyncService } from './importers/vacancy.service'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [FeatureFlagConfig],
+    }),
+    FeatureFlagModule,
+  ],
   providers: [
     ElasticService,
     ContentfulService,
