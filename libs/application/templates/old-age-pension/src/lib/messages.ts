3f194813c57ef4fae65e07d0ddd984eaed780526
karenbjorg <khalldorsdottir@deloitte.is>
2023-09-19 10:56:03 +0000

    clean messages

--- libs/application/templates/old-age-pension/src/lib/messages.ts
+++ libs/application/templates/old-age-pension/src/lib/messages.ts
@@ -36,23 +36,6 @@ export const oldAgePensionFormMessage: MessageDir = {
       defaultMessage: 'Forsendur',
       description: 'Prerequisites',
     },
-    forInfoSubSection: {
-      id: 'oap.application:for.info.sub.section',
-      defaultMessage: 'Til upplýsinga',
-      description: 'For Information',
-    },
-    forInfoDescription: {
-      id: 'oap.application:for.info.description',
-      defaultMessage:
-        'TR sækir nauðsynlegar upplýsingar til úrvinnslu umsókna og afgreiðslu mála. Þær upplýsingar geta varðað bæði tekjur og aðrar aðstæður þínar.\n \nMisjafnt er eftir tegund umsóknar hvaða upplýsingar þarf til úrvinnslu en ekki eru sóttar meiri upplýsingar en nauðsynlegt er hverju sinni.',
-      description: 'english translation',
-    },
-    forInfoSecondDescription: {
-      id: 'oap.application:for.info.second.description',
-      defaultMessage:
-        'Ef tekjur eða aðrar aðstæður þínar breytast verður þú að láta TR vita þar sem það getur haft áhrif á greiðslur þínar.\n \nFrekari upplýsingar um gagnaöflun og meðferð persónuupplýsinga má finna í persónuverndarstefnu Tryggingastofnunar, www.tr.is/personuvernd. \n \nÞeim umsóknum sem sendar eru TR í gegnum Mínar síður Ísland.is verður svarað rafrænt',
-      description: 'english translation',
-    },
     externalDataSubSection: {
       id: 'oap.application:externalData.sub.section',
       defaultMessage: 'Gagnaöflun',
