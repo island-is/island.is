9812841518e58f47bc2e4eda0b2994fc7daffc15
unakb <una@kolibri.is>
2023-09-21 16:36:20 +0000

    fix(j-s): We dont yet need a controller for the event log

--- apps/judicial-system/backend/src/app/modules/event-log/eventLog.module.ts
+++ apps/judicial-system/backend/src/app/modules/event-log/eventLog.module.ts
@@ -3,11 +3,9 @@ import { SequelizeModule } from '@nestjs/sequelize'
 
 import { EventLog } from './models/eventLog.model'
 import { EventLogService } from './eventLog.service'
-import { EventLogController } from './eventLog.controller'
 
 @Module({
   imports: [SequelizeModule.forFeature([EventLog])],
-  controllers: [EventLogController],
   providers: [EventLogService],
   exports: [EventLogService],
 })
