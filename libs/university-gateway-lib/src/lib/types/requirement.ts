896c678e5d0f1c7d42dd8e386d88072cf90b41e4
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2023-09-27 13:36:16 +0000

    Cleanup mapping course.requirement Fix missing contentfulKey

--- libs/university-gateway-lib/src/lib/types/requirement.ts
+++ libs/university-gateway-lib/src/lib/types/requirement.ts
@@ -2,4 +2,5 @@ export enum Requirement {
   MANDATORY = 'MANDATORY',
   FREE_ELECTIVE = 'FREE_ELECTIVE',
   RESTRICTED_ELECTIVE = 'RESTRICTED_ELECTIVE',
+  UNDEFINED = 'UNDEFINED', //TODO possibly stop allowing this value
 }
