665df152fecbe37b4be2f8977d14c5d8e19f794b
snaerseljan <snaer@aranja.com>
2023-09-27 10:18:39 +0000

    Update webpack config

--- apps/application-system/api/webpack.config.js
+++ apps/application-system/api/webpack.config.js
@@ -1,7 +1,7 @@
 const { composePlugins, withNx } = require('@nx/webpack')
 const { withReact } = require('@nx/react')
 
-module.exports = composePlugins(withNx(), withReact(), (config) => {
+module.exports = composePlugins(withReact({ ssr: true }), (config) => {
   // App specific config
   config.stats.chunks = false
   config.stats.modules = false
