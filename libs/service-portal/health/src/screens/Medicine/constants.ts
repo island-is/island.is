9d15fb01b82db4c677f2e6ce88d7d329b446968f
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2023-09-28 09:34:58 +0000

    Moved MedicinePurchase to indiviudal component

---
+++ libs/service-portal/health/src/screens/Medicine/constants.ts
@@ -0,0 +1,10 @@
+export enum MedicineTabs {
+  PURCHASE = 'lyfjakaup',
+  CALCULATOR = 'reiknivel',
+  LICENSE = 'skirteini',
+}
+
+export const SECTION_GAP = 5
+export const CONTENT_GAP = 2
+export const DATE_FORMAT = 'dd.MM.yyyy'
+export const DATE_FORMAT_SHORT = 'd.M.yyyy'
