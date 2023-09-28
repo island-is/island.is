3924f54a4c9e7217c16cad4cabafff2c0b9a26fa
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-09-28 19:12:40 +0000

    Sorts types

--- libs/judicial-system/types/src/lib/case.ts
+++ libs/judicial-system/types/src/lib/case.ts
@@ -1,9 +1,9 @@
+import type { CourtDocument } from './courtDocument'
 import type { Defendant } from './defendant'
+import { CaseFile } from './file'
 import type { Institution } from './institution'
 import type { Notification } from './notification'
-import { CaseFile } from './file'
 import { User, UserRole } from './user'
-import type { CourtDocument } from './courtDocument'
 
 export enum CaseOrigin {
   UNKNOWN = 'UNKNOWN',
