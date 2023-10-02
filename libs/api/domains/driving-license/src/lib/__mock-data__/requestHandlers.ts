5e474ba13dc831129ea00ce7259893c25f36a907
kksteini <steini@kosmosogkaos.is>
2023-10-02 14:17:16 +0000

    Add driving assesment post point

--- libs/api/domains/driving-license/src/lib/__mock-data__/requestHandlers.ts
+++ libs/api/domains/driving-license/src/lib/__mock-data__/requestHandlers.ts
@@ -82,7 +82,16 @@ const url = (path: string) => {
 }
 
 export const requestHandlers = [
-  rest.get(/\/api\/drivinglicense\/v5\/drivingassessment/, (req, res, ctx) => {
+  rest.post(
+    /api\/drivinglicense\/v5\/drivingassessment/, 
+    (_req, res, ctx) => {
+    // TODO, ambiguate with mock auths
+    return res(ctx.status(200)) 
+  }),
+
+  rest.get(
+    /api\/drivinglicense\/v5\/drivingassessment/, 
+    (req, res, ctx) => {
     // TODO, ambiguate with mock auths
     return res(ctx.status(200), ctx.json(DrivingAssessmentV5))
   }),
