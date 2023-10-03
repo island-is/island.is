39f578fb6f026c9755f63f90aaf449ed80686702
Berglind Ómarsdóttir <berglind.omarsdottir@origo.is>
2023-10-03 08:19:10 +0000

    removing console logs

--- libs/application/templates/transport-authority/change-co-owner-of-vehicle/src/fields/CoOwner/index.tsx
+++ libs/application/templates/transport-authority/change-co-owner-of-vehicle/src/fields/CoOwner/index.tsx
@@ -48,9 +48,7 @@ export const CoOwner: FC<React.PropsWithChildren<FieldBaseProps>> = (props) => {
     },
   )
 
-  console.log('___')
   useEffect(() => {
-    console.log('---')
     setFieldLoadingState?.(loading || !!error)
   }, [loading, error])
 
