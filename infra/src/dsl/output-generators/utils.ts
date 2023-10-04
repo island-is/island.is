642aaf87276cb4f8fa6464cb98d4a6f4e75a52cf
Jón Levy <levy@andes.is>
2023-10-04 10:26:18 +0000

    chore: better naming

--- infra/src/dsl/output-generators/utils.ts
+++ infra/src/dsl/output-generators/utils.ts
@@ -10,9 +10,12 @@
  * getScaledValue("100", 8);     // Returns 80 (80% of 100)
  * getScaledValue("2048", 7);    // Returns 1433 (70% of 2048)
  */
-export const getScaledValue = (value: string, ratio: number = 9): number => {
-  if (ratio < 0 || ratio > 10) {
-    throw new Error('Ratio should be between 0 and 10 inclusive.')
+export const getScaledValue = (
+  value: string,
+  percentage: number = 90,
+): number => {
+  if (percentage < 0 || percentage > 100) {
+    throw new Error('Ratio should be between 0 and 100 inclusive.')
   }
-  return Math.floor(parseInt(value, 10) * (ratio / 10))
+  return Math.floor(parseInt(value, 10) * (percentage / 100))
 }
