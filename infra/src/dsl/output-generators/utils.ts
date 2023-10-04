61a85ba4b1dac08211c0dc12c17a9304c8e17cd5
Jón Levy <levy@andes.is>
2023-10-04 10:33:35 +0000

    chore: rename ratio to percentage

--- infra/src/dsl/output-generators/utils.ts
+++ infra/src/dsl/output-generators/utils.ts
@@ -2,13 +2,13 @@
  * Calculates a given ratio of a numeric string's value and then floors the result.
  *
  * @param value A string representing a numeric value.
- * @param ratio The ratio out of 10 to scale the value by. Default is 9 (i.e., 90%).
+ * @param percentage The percentage to scale the value by. Default is 90 (i.e., 90%).
  * @returns The floored result of the scaled value.
  * @example
  *
- * getScaledValue("100");        // Returns 90 (default ratio is 9, i.e., 90%)
- * getScaledValue("100", 8);     // Returns 80 (80% of 100)
- * getScaledValue("2048", 7);    // Returns 1433 (70% of 2048)
+ * getScaledValue("100");        // Returns 90 (default percentage is 90, i.e., 90%)
+ * getScaledValue("100", 80);     // Returns 80 (80% of 100)
+ * getScaledValue("2048", 70);    // Returns 1433 (70% of 2048)
  */
 export const getScaledValue = (
   value: string,
