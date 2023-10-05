ba03cfd0ed53d84dc43caf1d5f452605531b40da
andes-it <builders@andes.is>
2023-10-05 10:46:04 +0000

    chore: nx format:write update dirty files

--- apps/services/university-gateway/backend/infra/university-gateway-backend.ts
+++ apps/services/university-gateway/backend/infra/university-gateway-backend.ts
@@ -1,39 +1,40 @@
 import { service, ServiceBuilder } from '../../../../../infra/src/dsl/dsl'
 
-export const serviceSetup = (): ServiceBuilder<'services-university-gateway-backend'> =>
-  service('services-university-gateway-backend')
-    .namespace('university-gateway')
-    .secrets({
-      AUTH_JWT_SECRET: '/k8s/university-gateway/AUTH_JWT_SECRET',
-      BACKEND_ACCESS_TOKEN: '/k8s/university-gateway/BACKEND_ACCESS_TOKEN',
-    })
-    .resources({
-      limits: { cpu: '150m', memory: '384Mi' },
-      requests: { cpu: '15m', memory: '256Mi' },
-    })
-    .image('university-gateway')
-    .secrets({})
-    .postgres({
-      username: 'university-gateway',
-      name: 'university-gateway',
-      passwordSecret: '/k8s/university-gateway/DB_PASSWORD',
-    })
-    .ingress({
-      primary: {
-        host: {
-          dev: 'university-gateway',
-          staging: 'university-gateway',
-          prod: 'university-gateway',
+export const serviceSetup =
+  (): ServiceBuilder<'services-university-gateway-backend'> =>
+    service('services-university-gateway-backend')
+      .namespace('university-gateway')
+      .secrets({
+        AUTH_JWT_SECRET: '/k8s/university-gateway/AUTH_JWT_SECRET',
+        BACKEND_ACCESS_TOKEN: '/k8s/university-gateway/BACKEND_ACCESS_TOKEN',
+      })
+      .resources({
+        limits: { cpu: '150m', memory: '384Mi' },
+        requests: { cpu: '15m', memory: '256Mi' },
+      })
+      .image('university-gateway')
+      .secrets({})
+      .postgres({
+        username: 'university-gateway',
+        name: 'university-gateway',
+        passwordSecret: '/k8s/university-gateway/DB_PASSWORD',
+      })
+      .ingress({
+        primary: {
+          host: {
+            dev: 'university-gateway',
+            staging: 'university-gateway',
+            prod: 'university-gateway',
+          },
+          paths: ['/'],
+          public: true,
         },
-        paths: ['/'],
-        public: true,
-      },
-    })
-    .replicaCount({
-      default: 2,
-      min: 2,
-      max: 10,
-    })
-    .liveness('/liveness')
-    .readiness('/liveness')
-    .grantNamespaces('nginx-ingress-internal')
+      })
+      .replicaCount({
+        default: 2,
+        min: 2,
+        max: 10,
+      })
+      .liveness('/liveness')
+      .readiness('/liveness')
+      .grantNamespaces('nginx-ingress-internal')
