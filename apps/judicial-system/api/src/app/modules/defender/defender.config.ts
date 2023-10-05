36874c5ebb47be5a23b62a675f780f0a68d4c2a2
unakb <una@kolibri.is>
2023-10-05 10:10:48 +0000

    fix(j-s): local config

--- apps/judicial-system/api/src/app/modules/defender/defender.config.ts
+++ apps/judicial-system/api/src/app/modules/defender/defender.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from '@island.is/nest/config'
 export const defenderModuleConfig = defineConfig({
   name: 'DefenderModule',
   load: (env) => ({
-    lawyerRegistryAPI: env.required('https://lmfi.is/api'),
+    lawyerRegistryAPI: env.required(
+      'https://lmfi.is/api',
+      'https://lmfi.is/api',
+    ),
     lawyerRegistryAPIKey: env.required('LAWYERS_ICELAND_API_KEY'),
   }),
 })
