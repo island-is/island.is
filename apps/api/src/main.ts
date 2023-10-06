0f2d63ef6bf8d90c669609a9047af92894876bec
Kristófer <kristofer@andes.is>
2023-10-06 13:30:00 +0000

    Force ci change detection in API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrap } from '@island.is/infra-nest-server'
 import { AppModule } from './app/app.module'
 
+// Force CI change detection
+
 bootstrap({
   appModule: AppModule,
   name: 'api',
