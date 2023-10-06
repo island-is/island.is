c2c81ccae678eff557afee560312a920c76a24dd
runarvestmann <runar@stefna.is>
2023-10-06 17:37:16 +0000

    Fix subarticle entry-hyperlink

---
+++ libs/cms/src/lib/search/importers/utils.spec.ts
@@ -0,0 +1,42 @@
+import { pruneEntryHyperlink } from './utils'
+
+describe('', () => {
+  it('s', () => {
+    const test = {
+      data: {
+        target: {
+          fields: {
+            // Depth 1
+            parent: {
+              fields: {
+                slug: 'something',
+                relatedArticles: [{ slug: 'something-else' }], // This is at depth 2 (if we start counting from 1)
+              },
+            },
+            title: 'some title',
+            slug: 'some-slug',
+          },
+        },
+      },
+    }
+
+    pruneEntryHyperlink(test)
+
+    expect(test).toStrictEqual({
+      data: {
+        target: {
+          fields: {
+            // Only depth 1 remains
+            parent: {
+              fields: {
+                slug: 'something',
+              },
+            },
+            title: 'some title',
+            slug: 'some-slug',
+          },
+        },
+      },
+    })
+  })
+})
