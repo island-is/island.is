876c18bd0ab1d8a7e7b51979c6cfef8d59a7ec0b
Þórður Hafliðason <tolleinn@gmail.com>
2023-10-06 12:53:53 +0000

    Update narrowlayout styling for screensizes

--- apps/service-portal/src/components/Layout/SidebarLayout.css.ts
+++ apps/service-portal/src/components/Layout/SidebarLayout.css.ts
@@ -2,16 +2,20 @@ import { style } from '@vanilla-extract/css'
 import { theme, themeUtils } from '@island.is/island-ui/theme'
 import { SERVICE_PORTAL_HEADER_HEIGHT_LG } from '@island.is/service-portal/constants'
 const top = SERVICE_PORTAL_HEADER_HEIGHT_LG
+const sidebarWidth = {
+  desktop: '318px',
+  tablet: '230px',
+}
 
 export const sidebarWrapper = style({
   top: top + theme.spacing[3],
-  maxWidth: '230px',
-  minWidth: '230px',
+  maxWidth: sidebarWidth.tablet,
+  minWidth: sidebarWidth.tablet,
   ...themeUtils.responsiveStyle({
     lg: {
       top: top + theme.spacing[9],
-      minWidth: '318px',
-      maxWidth: '318px',
+      minWidth: sidebarWidth.desktop,
+      maxWidth: sidebarWidth.desktop,
     },
   }),
 })
@@ -22,5 +26,13 @@ export const sticky = style({
 })
 
 export const sidebarWrap = style({
-  overflow: 'hidden',
+  maxWidth: '100%',
+  ...themeUtils.responsiveStyle({
+    md: {
+      maxWidth: `calc(100% - ${sidebarWidth.tablet})`,
+    },
+    lg: {
+      maxWidth: `calc(100% - ${sidebarWidth.desktop})`,
+    },
+  }),
 })
