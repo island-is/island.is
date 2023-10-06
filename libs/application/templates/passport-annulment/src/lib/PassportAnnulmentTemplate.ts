b39144a94c79d689304ba425753e2034c3ac6d64
Alex Diljar Birkisbur Hellsing <42963845+alexdiljar@users.noreply.github.com>
2023-10-06 12:24:15 +0000

    feat(passport-annulment): new template for Passport Annulment (#10219)
    * initialze template and start
    
    * overview
    
    * submit passport annulment
    
    * cleanup
    
    * Id not ID
    
    * errors
    
    * format
    
    * minor cleanup
    
    * tweaka, setja upp contentful
    
    * chore: nx format:write update dirty files
    
    * correct FF
    
    * chore: nx format:write update dirty files
    
    * tweaks
    
    * format
    
    * image
    
    * with api action
    
    * adding to codeowners
    
    * revert on random change
    
    * cleanup
    
    * typo fix
    
    * revert
    
    * review fix
    
    * const
    
    * chore: nx format:write update dirty files
    
    ---------
    
    Co-authored-by: albina <albina@kosmosogkaos.is>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: albinagu <47886428+albinagu@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/passport-annulment/src/lib/PassportAnnulmentTemplate.ts
@@ -0,0 +1,124 @@
+import {
+  Application,
+  ApplicationContext,
+  ApplicationRole,
+  ApplicationStateSchema,
+  ApplicationTemplate,
+  ApplicationTypes,
+  DefaultEvents,
+  defineTemplateApi,
+  NationalRegistryUserApi,
+  PassportsApi,
+} from '@island.is/application/types'
+import { Features } from '@island.is/feature-flags'
+import { m } from './messages'
+import {
+  ApiActions,
+  Events,
+  Roles,
+  sixtyDays,
+  States,
+  twoDays,
+} from './constants'
+import { dataSchema } from './dataSchema'
+
+const pruneAfter = (time: number) => {
+  return {
+    shouldBeListed: true,
+    shouldBePruned: true,
+    whenToPrune: time,
+  }
+}
+
+const PassportAnnulmentTemplate: ApplicationTemplate<
+  ApplicationContext,
+  ApplicationStateSchema<Events>,
+  Events
+> = {
+  type: ApplicationTypes.PASSPORT_ANNULMENT,
+  name: m.formName.defaultMessage,
+  featureFlag: Features.passportAnnulmentApplication,
+  dataSchema,
+  stateMachineConfig: {
+    initial: States.DRAFT,
+    states: {
+      draft: {
+        meta: {
+          name: m.formName.defaultMessage,
+          status: 'draft',
+          progress: 0.33,
+          lifecycle: pruneAfter(twoDays),
+          roles: [
+            {
+              id: Roles.APPLICANT,
+              formLoader: () =>
+                import('../forms/Draft').then((val) =>
+                  Promise.resolve(val.Draft),
+                ),
+              actions: [
+                {
+                  event: DefaultEvents.SUBMIT,
+                  name: 'Staðfesta',
+                  type: 'primary',
+                },
+              ],
+              write: 'all',
+              delete: true,
+              api: [PassportsApi, NationalRegistryUserApi],
+            },
+          ],
+        },
+        on: {
+          [DefaultEvents.SUBMIT]: { target: States.DONE },
+        },
+      },
+
+      [States.DONE]: {
+        meta: {
+          name: 'Done',
+          status: 'completed',
+          progress: 1,
+          lifecycle: pruneAfter(sixtyDays),
+          actionCard: {
+            tag: {
+              label: m.actionCardDoneTag,
+            },
+          },
+          onEntry: defineTemplateApi({
+            action: ApiActions.submitApplication,
+          }),
+          roles: [
+            {
+              id: Roles.APPLICANT,
+              formLoader: () =>
+                import('../forms/Done').then((val) =>
+                  Promise.resolve(val.Done),
+                ),
+              read: {
+                externalData: ['submitPassportApplication'],
+                answers: [
+                  'submitPassportApplication',
+                  'childsPersonalInfo',
+                  'personalInfo',
+                  'passport',
+                ],
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+
+  mapUserToRole(
+    nationalId: string,
+    application: Application,
+  ): ApplicationRole | undefined {
+    if (nationalId === application.applicant) {
+      return Roles.APPLICANT
+    }
+    return undefined
+  },
+}
+
+export default PassportAnnulmentTemplate
