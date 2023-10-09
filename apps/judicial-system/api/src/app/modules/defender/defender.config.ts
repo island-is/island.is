ebc71b4cf0980ef4c2a64698af954b586a13827f
unakb <una@kolibri.is>
2023-10-09 11:47:32 +0000

    fix(j-s): rename

--- apps/judicial-system/api/src/app/modules/defender/defender.config.ts
+++ apps/judicial-system/api/src/app/modules/defender/defender.config.ts
@@ -4,8 +4,8 @@ export const defenderModuleConfig = defineConfig({
   name: 'DefenderModule',
   load: (env) => ({
     lawyerRegistryAPI: env.required(
-      'LAWYERS_ICELAND_API_PATH',
-      'https://lmfi.is/api/',
+      'LAWYERS_ICELAND_API_URL',
+      'https://lmfi.is/api',
     ),
     lawyerRegistryAPIKey: env.required('LAWYERS_ICELAND_API_KEY'),
   }),
