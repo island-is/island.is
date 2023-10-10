8c2ff70e041d48104a13db6c1135e02135cffa86
Þórður Hafliðason <tolleinn@gmail.com>
2023-10-10 11:15:12 +0000

    Formatting and update paths + remove networkidle

--- apps/system-e2e/src/tests/islandis/service-portal/smoke/work-machines.spec.ts
+++ apps/system-e2e/src/tests/islandis/service-portal/smoke/work-machines.spec.ts
@@ -2,7 +2,7 @@ import { test, BrowserContext, expect } from '@playwright/test'
 import { icelandicAndNoPopupUrl, urls } from '../../../../support/urls'
 import { session } from '../../../../support/session'
 import { label } from '../../../../support/i18n'
-import { messages } from '@island.is/service-portal/vehicles/messages'
+import { messages } from '@island.is/service-portal/assets/messages'
 import { m } from '@island.is/service-portal/core/messages'
 import { disableI18n } from '../../../../support/disablers'
 
@@ -32,40 +32,33 @@ test.describe('MS - Work Machines', () => {
 
     await test.step('should display data and filter overview', async () => {
       // Arrange
-      await page.goto(icelandicAndNoPopupUrl('/minarsidur/okutaeki/vinnuvelar'))
+      await page.goto(icelandicAndNoPopupUrl('/minarsidur/eignir/vinnuvelar'))
 
       // Act
       const filterButton = page
-        .getByRole('button', {name: label(m.openFilter)})
+        .getByRole('button', { name: label(m.openFilter) })
         .first()
       const inputField = page.getByRole('textbox', {
         name: label(m.searchLabel),
       })
       await inputField.click()
       await inputField.type('hys', { delay: 200 })
-      const actionCardButton = page
-        .getByTestId("action-card-cta")
-        .first()
+      const actionCardButton = page.getByTestId('action-card-cta').first()
 
       await filterButton.click()
 
       // Assert
       await expect(filterButton).toBeVisible()
       await expect(actionCardButton).toBeVisible()
-      await expect(
-        page.getByLabel(label(m.clearAllFilters)),
-      ).toBeVisible()
+      await expect(page.getByTestId('icon-reload').first()).toBeVisible()
     })
 
     await test.step('should display detail', async () => {
       // Arrange
-      await page.goto(icelandicAndNoPopupUrl('/minarsidur/okutaeki/vinnuvelar'))
-      await page.waitForLoadState('networkidle')
+      await page.goto(icelandicAndNoPopupUrl('/minarsidur/eignir/vinnuvelar'))
 
       // Act
-      const actionCardButton = page
-        .getByTestId"action-card-cta")
-        .first()
+      const actionCardButton = page.getByTestId('action-card-cta').first()
       await actionCardButton.click()
 
       const basicInfoText = page
@@ -73,7 +66,9 @@ test.describe('MS - Work Machines', () => {
         .first()
 
       // Assert
-      await expect(page).toHaveURL(/minarsidur\/okutaeki\/vinnuvelar(\/[^\/]+){2}/)
+      await expect(page).toHaveURL(
+        /minarsidur\/eignir\/vinnuvelar(\/[^\/]+){2}/,
+      )
       await expect(basicInfoText).toBeVisible()
     })
   })
