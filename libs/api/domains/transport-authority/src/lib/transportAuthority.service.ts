82442c891ed487363af7ed1af539cb6d870eeee6
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2023-10-11 15:06:17 +0000

    Add mileage input field and send with API to SGS

--- libs/api/domains/transport-authority/src/lib/transportAuthority.service.ts
+++ libs/api/domains/transport-authority/src/lib/transportAuthority.service.ts
@@ -136,6 +136,9 @@ export class TransportAuthorityApi {
         dateOfPurchase: new Date(answers?.vehicle?.date),
         dateOfPurchaseTimestamp: todayStr.substring(11, todayStr.length),
         saleAmount: Number(answers?.vehicle?.salePrice || '0') || 0,
+        mileage: answers?.vehicle?.salePrice
+          ? Number(answers?.vehicle?.salePrice) || 0
+          : null,
         insuranceCompanyCode: answers?.insurance?.value || '',
         coOwners: buyerCoOwners?.map((coOwner) => ({
           ssn: coOwner.nationalId,
