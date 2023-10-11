0e5e664c12895cd902fbdb8399eba773bffeade8
Jón Levy <levy@andes.is>
2023-10-11 20:25:30 +0000

    fix(sessions): use jobs for geoip (#12371)
    * fix: added startingDeadlineSeconds for starting cronjobs on create
    
    * fix(infra): added Job for session service, improved getScaledValue function
    
    * chore: charts update dirty files
    
    * fix(sessions): use pre-install,pre-upgrade hooks for jobs
    
    * fix(infra): add missing redis
    
    * chore: charts update dirty files
    
    * chore: run yarn charts
    
    * chore: charts update dirty files
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>

--- infra/src/dsl/utils/scale-value.ts
+++ infra/src/dsl/utils/scale-value.ts
@@ -9,10 +9,16 @@
  * getScaledValue("100");        // Returns 90 (default percentage is 90, i.e., 90%)
  * getScaledValue("100", 80);     // Returns 80 (80% of 100)
  * getScaledValue("2048", 70);    // Returns 1433 (70% of 2048)
+ * getScaledValue("2Gi", 70);     // Returns 1433 (70% of 2048Mi)
  */
 export const getScaledValue = (value: string, percentage = 90): number => {
   if (percentage < 0 || percentage > 100) {
     throw new Error('Ratio should be between 0 and 100 inclusive.')
   }
-  return Math.floor(parseInt(value, 10) * (percentage / 100))
+
+  const isGiga = value.endsWith('Gi')
+  const numericValue = parseInt(value, 10)
+  const scaledValue = isGiga ? numericValue * 1024 : numericValue // Convert Gi to Mi if needed
+
+  return Math.floor(scaledValue * (percentage / 100))
 }
