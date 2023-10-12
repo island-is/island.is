efa885065df1f056f77e565bcd3245fa357cae59
Þórður Hafliðason <tolleinn@gmail.com>
2023-10-12 13:40:48 +0000

    Add back error

--- libs/service-portal/documents/src/screens/Overview/Overview.tsx
+++ libs/service-portal/documents/src/screens/Overview/Overview.tsx
@@ -490,6 +490,7 @@ export const ServicePortalDocuments = () => {
               }
             }}
             loading={loading}
+            error={error}
           />
         </GridColumn>
       </GridRow>
