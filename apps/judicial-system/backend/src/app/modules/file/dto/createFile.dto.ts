2d77e869fed28a23641997141ded5ff43dbbaa3f
unakb <una@kolibri.is>
2023-10-17 15:29:08 +0000

    fix(j-s): Remove unused dto field

--- apps/judicial-system/backend/src/app/modules/file/dto/createFile.dto.ts
+++ apps/judicial-system/backend/src/app/modules/file/dto/createFile.dto.ts
@@ -46,9 +46,4 @@ export class CreateFileDto {
   @IsString()
   @ApiPropertyOptional()
   readonly policeFileId?: string
-
-  @IsOptional()
-  @IsString()
-  @ApiPropertyOptional()
-  readonly userGeneratedFilename?: string
 }
