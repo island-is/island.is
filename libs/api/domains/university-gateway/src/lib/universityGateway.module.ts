1e708dbadb99e14f4541f702b8d3e9f38d1994c5
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2023-10-27 14:58:21 +0000

    Move ConfigModule.forRoot into main gql app module

--- libs/api/domains/university-gateway/src/lib/universityGateway.module.ts
+++ libs/api/domains/university-gateway/src/lib/universityGateway.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common'
 import { MainResolver } from './graphql/main.resolver'
-import {
-  UniversityGatewayApiClientConfig,
-  UniversityGatewayApiClientModule,
-} from '@island.is/clients/university-gateway-api'
+import { UniversityGatewayApiClientModule } from '@island.is/clients/university-gateway-api'
 import { UniversityGatewayApi } from './universityGateway.service'
-import { ConfigModule } from '@nestjs/config'
 import { CmsModule } from '@island.is/cms'
 
 @Module({
-  imports: [
-    UniversityGatewayApiClientModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [UniversityGatewayApiClientConfig],
-    }),
-    CmsModule,
-  ],
+  imports: [UniversityGatewayApiClientModule, CmsModule],
   providers: [MainResolver, UniversityGatewayApi],
   exports: [UniversityGatewayApi],
 })
