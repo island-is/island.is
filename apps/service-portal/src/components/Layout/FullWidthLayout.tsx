31f60788fceb7b8d22289d21b3589cd7ec3e7efb
Þórður Hafliðason <tolleinn@gmail.com>
2023-11-01 15:32:01 +0000

    Add tab navigation as button navigation

--- apps/service-portal/src/components/Layout/FullWidthLayout.tsx
+++ apps/service-portal/src/components/Layout/FullWidthLayout.tsx
@@ -17,6 +17,7 @@ import { Link, matchPath, useNavigate } from 'react-router-dom'
 import { ServicePortalPaths } from '../../lib/paths'
 import { DocumentsPaths } from '@island.is/service-portal/documents'
 import { theme } from '@island.is/island-ui/theme'
+import { TabNavigation } from '../TabNavigation/TabNavigation'
 
 interface FullWidthLayoutProps {
   activeParent?: PortalNavigationItem
@@ -50,12 +51,6 @@ export const FullWidthLayout: FC<FullWidthLayoutProps> = ({
   const isDocuments = Object.values(DocumentsPaths).find((route) =>
     matchPath(route, pathname),
   )
-
-  const tabChangeHandler = (id: string) => {
-    if (id !== pathname) {
-      navigate(id)
-    }
-  }
 
   return (
     <Box
@@ -128,25 +123,18 @@ export const FullWidthLayout: FC<FullWidthLayoutProps> = ({
           </>
         )}
         {navItems && navItems?.length > 0 ? (
-          <Box paddingTop={4}>
+          <Box paddingTop={[0, 0, 4]}>
             <GridContainer position="none">
               <GridRow>
                 <GridColumn span="12/12">
-                  <Tabs
-                    selected={pathname}
-                    key={navItems?.length}
-                    onChange={tabChangeHandler}
+                  <TabNavigation
                     label={
                       activeParent?.name ? formatMessage(activeParent.name) : ''
                     }
-                    tabs={navItems?.map((item) => ({
-                      id: item.path,
-                      label: formatMessage(item.name),
-                      content: children,
-                    }))}
-                    contentBackground="white"
-                    onlyRenderSelectedTab
+                    pathname={pathname}
+                    items={navItems}
                   />
+                  {children}
                 </GridColumn>
               </GridRow>
             </GridContainer>
