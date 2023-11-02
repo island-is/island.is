93b577a761e33eac8c62400d2e0ad023953fb632
Jón Levy <levy@andes.is>
2023-11-02 11:35:20 +0000

    fix: depcruiser enhancements

--- scripts/cruise.ts
+++ scripts/cruise.ts
@@ -1,8 +1,24 @@
 import path from 'path'
 import yargs from 'yargs/yargs'
-import { exec } from './utils'
-
+import { exec } from 'child_process'
+import chalk from 'chalk'
 const repoUrl = 'https://github.com/island-is/island.is'
+
+const runCommand = (cmd: string, log: string): Promise<string> => {
+  console.log(chalk.cyan(log))
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error || stderr || !stdout) {
+        reject(
+          new Error(`Error getting project root directory: ${error || stderr}`),
+        )
+        return
+      }
+
+      resolve(stdout.trim())
+    })
+  })
+}
 
 yargs(process.argv.slice(2))
   .command(
@@ -14,16 +30,23 @@ yargs(process.argv.slice(2))
           type: 'string',
           demandOption: true,
           alias: 'p',
-          description: 'Relative path to an NX project',
+          description: 'NX project name',
         },
       })
     },
     async (argv) => {
-      const cwd = process.cwd()
-      const projectPath = path.join(cwd, argv.project)
+      const workspacePath = await runCommand(
+        'git rev-parse --show-toplevel',
+        "Resolving workspace's root path ...",
+      )
+
+      const projectPath = await runCommand(
+        `yarn nx show project ${argv.project} | jq -r '.root'`,
+        `Resolving ${argv.project} path ...`,
+      )
       const cmd = `yarn depcruise`
       const args = [
-        `-c ${path.join(cwd, '.dependency-cruiser.js')}`,
+        `-c ${path.join(workspacePath, '.dependency-cruiser.js')}`,
         `--ts-config ${path.join(projectPath, 'tsconfig.json')}`,
         `--output-type dot`,
         projectPath,
@@ -32,15 +55,15 @@ yargs(process.argv.slice(2))
         `|`,
         `npx depcruise-wrap-stream-in-html`,
         `>`,
-        `${cwd}/depgraph.html`,
+        `${workspacePath}/depgraph.html`,
       ].join(' ')
+      console.log(args)
       try {
-        const { stdout } = await exec(`git symbolic-ref --quiet --short HEAD`)
-        console.log(stdout)
-        await exec(
-          `REPO_PREFIX=${repoUrl}/tree/${stdout.trim()} ${cmd} ${args}`,
+        const repoPrefix = runCommand(
+          'git symbolic-ref --quiet --short HEAD',
+          'Resolving current branch ...',
         )
-        // await exec(`${cmd} ${args}`)
+        exec(`REPO_PREFIX=${repoUrl}/tree/${repoPrefix} ${cmd} ${args}`)
         console.log('Cruise operation completed successfully.')
       } catch (error) {
         console.error(
