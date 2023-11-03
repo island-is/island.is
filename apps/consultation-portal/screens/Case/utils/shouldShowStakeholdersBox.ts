b16264b58caf420070229cd3b72c7cb7042480aa
Karl Arnar <karl.arnar.aegisson@advania.is>
2023-11-03 12:28:26 +0000

    KAM-1845: Hide boð um þátttöku box

---
+++ apps/consultation-portal/screens/Case/utils/shouldShowStakeholdersBox.ts
@@ -0,0 +1,21 @@
+import { Case } from '../../../types/interfaces'
+
+interface ShouldShowStakeholdersBoxProps {
+  chosenCase: Case
+}
+
+const shouldShowStakeholdersBox = ({
+  chosenCase,
+}: ShouldShowStakeholdersBoxProps) => {
+  if (chosenCase?.created && chosenCase?.created !== null) {
+    // start date is the date when stakeholders box can be shown
+    const startDate = "2023-11-06T13:00:00.000Z"
+    const createdDate = new Date(chosenCase?.created)
+
+    const dateWhenBoxCanBeShown = new Date(startDate)
+    return createdDate >= dateWhenBoxCanBeShown
+  }
+  return false
+}
+
+export default shouldShowStakeholdersBox
