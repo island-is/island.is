83c8329839e0939195a853614efbbfc8a20200f7
runarvestmann <runar@stefna.is>
2023-11-06 19:42:31 +0000

    Test

--- apps/web/hooks/usePlausiblePageview.ts
+++ apps/web/hooks/usePlausiblePageview.ts
@@ -12,21 +12,23 @@ export const usePlausiblePageview = (domain?: string) => {
   useEffect(() => {
     const onRouteChangeComplete = () => {
       // Only track pageviews in production
-      if (publicRuntimeConfig.environment !== 'prod' || !domain) return
+      if (!domain) return
+
+      console.log(window.location.href)
 
       // Documentation: https://plausible.io/docs/events-api
-      fetch('https://plausible.io/api/event', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: JSON.stringify({
-          domain: domain,
-          name: 'pageview',
-          url: window.location.href,
-          referrer: window.document.referrer || null,
-        }),
-      })
+      // fetch('https://plausible.io/api/event', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'text/plain',
+      //   },
+      //   body: JSON.stringify({
+      //     domain: domain,
+      //     name: 'pageview',
+      //     url: window.location.href,
+      //     referrer: window.document.referrer || null,
+      //   }),
+      // })
     }
 
     // Client side routing should trigger a pageview
