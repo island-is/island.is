c1fa0435789faf3f5dc489f2c20517b913946cdd
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-11-06 14:00:00 +0000

    chore: remove import

--- libs/service-portal/core/src/components/Gallery/Gallery.css.ts
+++ libs/service-portal/core/src/components/Gallery/Gallery.css.ts
@@ -1,7 +1,6 @@
 import { style } from '@vanilla-extract/css'
 import { theme } from '@island.is/island-ui/theme'
 import { hexToRgba } from '@island.is/island-ui/utils'
-import { relative } from 'path'
 
 export const gallery = style({
   contain: 'layout',
