3cdf3f0135387cb58775a6142796fa5013216c2b
Karl Arnar <karl.arnar.aegisson@advania.is>
2023-11-06 10:29:04 +0000

    KAM-1847: Subscription option should not show up until logged in and with verified email

--- apps/consultation-portal/hooks/api/useFetchEmail.ts
+++ apps/consultation-portal/hooks/api/useFetchEmail.ts
@@ -2,14 +2,18 @@ import { useQuery } from '@apollo/client'
 import { SUB_GET_EMAIL } from '../../graphql/queries.graphql'
 
 interface Props {
-  isAuthenticated: boolean
+  isAuthenticated?: boolean
+  isMySubscriptions?: boolean
 }
 
-export const useFetchEmail = ({ isAuthenticated }: Props) => {
+export const useFetchEmail = ({
+  isAuthenticated = false,
+  isMySubscriptions = false,
+}: Props) => {
   const { data, loading } = useQuery(SUB_GET_EMAIL, {
     ssr: false,
     fetchPolicy: 'network-only',
-    skip: !isAuthenticated,
+    skip: !isAuthenticated || isMySubscriptions,
   })
 
   const { consultationPortalUserEmail: userEmail = [] } = data ?? {}
