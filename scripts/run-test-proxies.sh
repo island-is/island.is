67e49715746022889a523f86746082a0542cfc02
Kristófer <kristofer@andes.is>
2023-11-06 13:54:00 +0000

    Better proxy background handling

--- scripts/run-test-proxies.sh
+++ scripts/run-test-proxies.sh
@@ -2,9 +2,19 @@
 
 set -euo pipefail
 
-./scripts/run-es-proxy.sh "$@" &
-./scripts/run-soffia-proxy.sh "$@" &
-./scripts/run-xroad-proxy.sh "$@" &
-./scripts/run-redis-proxy.sh "$@" &
+RESTART_INTERVAL_TIME=3
+for proxy in es soffia xroad redis; do
+  echo "Starting $proxy proxy"
+  (
+    while true; do
+      code=0
+      ./scripts/run-$proxy-proxy.sh "$@" || code=$?
+      echo "Exit code for $proxy proxy: $code"
+      if [ $code -eq 1 ]; then exit 1; fi
+      echo "Restarting $proxy proxy in $RESTART_INTERVAL_TIME seconds..."
+      sleep $RESTART_INTERVAL_TIME
+    done
+  ) &
+done
 
 wait
