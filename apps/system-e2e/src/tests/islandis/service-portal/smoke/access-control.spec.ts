927187e6e913abcc1a74d38d460b59f1b3e9593c
Þórður Hafliðason <tolleinn@gmail.com>
2023-11-06 16:01:47 +0000

    remove accidental only

--- apps/system-e2e/src/tests/islandis/service-portal/smoke/access-control.spec.ts
+++ apps/system-e2e/src/tests/islandis/service-portal/smoke/access-control.spec.ts
@@ -41,7 +41,7 @@ test.describe('Service portal, in access control', () => {
   // Smoke test: Aðgangstýring - kanna að öll umboð sjáist
   // Smoke test: Aðgangstýring - Gefa umboð sem einstaklingur
   // Smoke test: Innskráning umboð gefið af öðrum
-  test.only('can remove, create and use custom delegations', async () => {
+  test('can remove, create and use custom delegations', async () => {
     // Arrange
     const granterPage = await contextGranter.newPage()
     await granterPage.goto(
