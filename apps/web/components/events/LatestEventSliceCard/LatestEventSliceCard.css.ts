90106123ad5361a72398e2450bb7bbafbe88583b
runarvestmann <runar@stefna.is>
2023-11-07 15:42:44 +0000

    Remove transition

--- apps/web/components/events/LatestEventSliceCard/LatestEventSliceCard.css.ts
+++ apps/web/components/events/LatestEventSliceCard/LatestEventSliceCard.css.ts
@@ -3,7 +3,6 @@ import { style } from '@vanilla-extract/css'
 import { theme } from '@island.is/island-ui/theme'
 
 export const container = style({
-  transition: 'outline 150ms ease',
   ':hover': {
     outlineWidth: '1px',
     outlineColor: theme.color.blue400,
