725173991dfbb4f4459e54bf91ed278607b0caf4
Karl Arnar <karl.arnar.aegisson@advania.is>
2023-11-07 11:51:15 +0000

    KAM-1853: Show my subscriptions

--- apps/consultation-portal/hooks/api/useFetchEmail.ts
+++ apps/consultation-portal/hooks/api/useFetchEmail.ts
@@ -3,17 +3,15 @@ import { SUB_GET_EMAIL } from '../../graphql/queries.graphql'
 
 interface Props {
   isAuthenticated?: boolean
-  isMySubscriptions?: boolean
 }
 
 export const useFetchEmail = ({
   isAuthenticated = false,
-  isMySubscriptions = false,
 }: Props) => {
   const { data, loading } = useQuery(SUB_GET_EMAIL, {
     ssr: false,
     fetchPolicy: 'network-only',
-    skip: !isAuthenticated || isMySubscriptions,
+    skip: !isAuthenticated,
   })
 
   const { consultationPortalUserEmail: userEmail = [] } = data ?? {}
