9c216e8f162187277111071657e08d9ec27fb9fe
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2023-11-08 10:01:01 +0000

    Revert back to DynamicModule (instead of @Module without register function)

--- libs/application/template-api-modules/src/lib/modules/shared/api/directorate-of-immigration/directorate-of-immigration.module.ts
+++ libs/application/template-api-modules/src/lib/modules/shared/api/directorate-of-immigration/directorate-of-immigration.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common'
+import { DynamicModule } from '@nestjs/common'
 import { DirectorateOfImmigrationService } from './directorate-of-immigration.service'
 import { DirectorateOfImmigrationClientModule } from '@island.is/clients/directorate-of-immigration'
-
-@Module({
-  imports: [DirectorateOfImmigrationClientModule],
-  providers: [DirectorateOfImmigrationService],
-  exports: [DirectorateOfImmigrationService],
-})
-export class DirectorateOfImmigrationModule {}
+export class DirectorateOfImmigrationModule {
+  static register(): DynamicModule {
+    return {
+      module: DirectorateOfImmigrationModule,
+      imports: [DirectorateOfImmigrationClientModule],
+      providers: [DirectorateOfImmigrationService],
+      exports: [DirectorateOfImmigrationService],
+    }
+  }
+}
