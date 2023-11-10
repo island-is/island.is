4892e3e691a0fd00a8955184682ddfb558844b9c
Kristófer <kristofer@andes.is>
2023-11-10 15:49:16 +0000

    Big updated value-files-generator for Mountebank

--- infra/src/dsl/value-files-generators/local-setup.ts
+++ infra/src/dsl/value-files-generators/local-setup.ts
@@ -9,6 +9,7 @@ import { readFile, writeFile } from 'fs/promises'
 import { globSync } from 'glob'
 import { join } from 'path'
 import { rootDir } from '../consts'
+import { readdirSync, existsSync, mkdirSync } from 'fs'
 
 type MountebankConfig = {
   protocol: string
@@ -174,18 +175,110 @@ export const getLocalrunValueFile = async (
     },
     { ports: [] as number[], configs: [] as MountebankConfig[] },
   )
-  const defaultMountebankConfig = 'mountebank-imposter-config.json'
+
+  const baseDir = `${process.cwd()}/mountebank`
+  const dataDir = `${baseDir}/data`
+  const configDir = `${baseDir}/config`
+  const configFile = `.baked.json`
+
+  // Ensure directories are present
+  mkdirSync(dataDir, { recursive: true, mode: 0o775 })
+  mkdirSync(configDir, { recursive: true, mode: 0o775 })
+
+  for (const config of mocksConfigs.configs) {
+    await writeFile(
+      `${configDir}/${config.name}.json`,
+      JSON.stringify(config),
+      {
+        encoding: 'utf-8',
+        mode: 0o664,
+      },
+    )
+  }
+
   await writeFile(
-    defaultMountebankConfig,
-    JSON.stringify({ imposters: mocksConfigs.configs }),
-    { encoding: 'utf-8' },
+    `${configDir}/.baked.json`,
+    'INVALID\n' +
+      JSON.stringify({
+        imposters: [
+          ...mocksConfigs.configs.map(
+            (config) => `<%- include ${config.name}.json %>`,
+          ),
+          ...readdirSync(configDir)
+            .filter((file) => file.endsWith('.js'))
+            .map((file) => {
+              try {
+                console.log('file', file)
+                const module = require(join(configDir, file))
+                console.log('module', module)
+                return JSON.stringify(module())
+              } catch (e) {
+                return {
+                  name: file,
+                  error: e,
+                  message: 'Failed to load',
+                }
+              }
+            }),
+
+          // ...mocksConfigs.configs.map((config) => `include-${config.name}.json`),
+          // ...readdirSync(configDir)
+          //   .filter((file) => file.endsWith('.js'))
+          //   .map((file) => `js-${file}`),
+          // const fs = require('fs');
+          //
+          // const files = fs.readdirSync(__dirname).filter(file => file.endsWith('.js'));
+          //
+          // JSON.stringify({
+          //   imposters: [
+          //     ...files.map(file => `include-${file.replace('.js', '')}`),
+          //   ],
+          // })
+        ],
+      })
+        .replace(/"include-([^"]+)"/g, '<%- include $1 %>')
+        .replace(/"js-([^"]+)"/g, '<%- include $1 %>')
+        .replace(/"jsanchor"/g, '<% '),
+    {
+      encoding: 'utf-8',
+      mode: 0o664,
+    },
   )
 
-  const mocks = `docker run -it --rm -p 2525:2525 ${mocksConfigs.ports
-    .map((port) => `-p ${port}:${port}`)
-    .join(
-      ' ',
-    )} -v ${process.cwd()}/${defaultMountebankConfig}:/app/default.json docker.io/bbyars/mountebank:2.8.1 start --configfile=/app/default.json`
+  const mockPorts = mocksConfigs.ports
+    .map((port) => `-p=${port}:${port}`)
+    .join(' ')
+  const mockMounts = [`${dataDir}:/app/.mbdb:z`, `${configDir}:/app/config:z`]
+    .filter(Boolean)
+    // Ensure z option is set, but don't overwrite other options
+    .map((x) => {
+      const [mountHost, mountGuest, options] = x.split(':')
+      return `${mountHost}:${mountGuest}:${(options ?? '').replace(/z/g, '')}z`
+    })
+    // Prepend -v to mounts
+    .map((x) => `-v=${x}`)
+    .join(' ')
+  const startOptions = [
+    existsSync(`${configDir}/${configFile}`)
+      ? `--configfile=config/${configFile}`
+      : '',
+    existsSync(dataDir) ? `--datadir=.mbdb/datadir` : '',
+  ]
+    .filter(Boolean)
+    .join(' ')
+  const mocks = [
+    `docker run`,
+    `--userns=keep-id:uid=1001,gid=1001`,
+    `-it`,
+    `--rm`,
+    `--name=mountebank`,
+    `-p=2525:2525`,
+    `${mockPorts}`,
+    `${mockMounts}`,
+    `docker.io/bbyars/mountebank:2.8.1`,
+    `start`,
+    `${startOptions}`,
+  ].join(' ')
 
   return {
     services: Object.entries(dockerComposeServices).reduce(
