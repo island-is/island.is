f74fcbed595ec02e015c3ce2e48c0e23be4c817f
Sigurgeir Gudmundsson <essgje@gmail.com>
2023-11-10 12:57:03 +0000

    remove debug-logging

--- libs/auth-nest-tools/src/lib/ids-user.guard.ts
+++ libs/auth-nest-tools/src/lib/ids-user.guard.ts
@@ -24,32 +24,19 @@ export class IdsUserGuard extends AuthGuard('jwt') {
     status?: unknown,
   ) {
     // Throws if there is an error or missing user.
-    console.log(
-      'WWWWWWWWWWWWWWW--handleRequest start-super--WWWWWWWWWWWWWWWWWWWW',
-    )
     super.handleRequest(error, user, info, context, status)
-    console.log(
-      'WWWWWWWWWWWWWWW--handleRequest end-super--WWWWWWWWWWWWWWWWWWWW',
-    )
-
     // Check if there's a nationalId claim for an authenticated user.
     if (!user.nationalId) {
-      console.log('WWWWWWWWWWWWWWW--EXCEPTION--WWWWWWWWWWWWWWWWWWWW')
       throw new UnauthorizedException()
     }
-
     // AuthGuard saves user in request.user. We'll also save it as request.auth.
     const request = this.getRequest(context)
     request.auth = user
-    console.log('WWWWWWWWWWWWWWWW--return-user-start--WWWWWWWWWWWWWWWWWWWW')
-    console.log(JSON.stringify(user))
-    console.log('WWWWWWWWWWWWWWWW--return-user-end--WWWWWWWWWWWWWWWWWWWW')
     return user
   }
 
   canActivate(context: ExecutionContext) {
     // we check for metadata set by the bypass auth decorator
-    console.log('WWWWWWWWWWWWWWWWW--can-activate-1.0--WWWWWWWWWWWWWWWWWWWWW')
     const bypassAuth = this.reflector.getAllAndOverride<boolean>(
       BYPASS_AUTH_KEY,
       [context.getHandler(), context.getClass()],
@@ -57,22 +44,9 @@ export class IdsUserGuard extends AuthGuard('jwt') {
 
     // if the bypass auth exists and is truthy we bypass auth
     if (bypassAuth) {
-      console.log('WWWWWWWWWWWWWWWWW--bypass-auth--WWWWWWWWWWWWWWWWWWWWW')
       return true
     }
-    console.log('WWWWWWWWWWWWWWWWW--return-val-start--WWWWWWWWWWWWWWWWWWWWW-')
-    const canActv = super.canActivate(context)
-    console.log(JSON.stringify(canActv))
-    if (!canActv) {
-      console.log('NO canactivate...')
-    } else {
-      console.log('YES canactivate...')
-    }
-    console.log('WWWWWWWWWWWWWWWWW--return-val-end--WWWWWWWWWWWWWWWWWWWWW-')
     // pass auth checking to JWT auth guard
-    // return super.canActivate(context)
-    return canActv //Unauthorized villa þó bearer ok.
-    // return true // virkar 1x server fer síðan á villu.
-    // return false //forbidden resource
+    return super.canActivate(context)
   }
 }
