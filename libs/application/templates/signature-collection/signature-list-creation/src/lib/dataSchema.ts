f6822af510fe081e22954ccb35ecb81fb400c5ca
albina <albina@kosmosogkaos.is>
2023-11-13 10:31:10 +0000

    tweaks

--- libs/application/templates/signature-collection/signature-list-creation/src/lib/dataSchema.ts
+++ libs/application/templates/signature-collection/signature-list-creation/src/lib/dataSchema.ts
@@ -5,7 +5,7 @@ export const dataSchema = z.object({
   applicant: z.object({
     name: z.string(),
     nationalId: z.string(),
-    phone: z.string().min(1),
+    phone: z.string().min(11),
     email: z.string().email(),
   }),
   collection: z.object({
--- libs/application/templates/signature-collection/signature-list-creation/src/lib/utils.ts
+++ libs/application/templates/signature-collection/signature-list-creation/src/lib/utils.ts
@@ -1,11 +1,5 @@
 import { formatPhoneNumber } from '@island.is/application/ui-components'
-import { parsePhoneNumberFromString } from 'libphonenumber-js'
 
 export const formatPhone = (phone: string) => {
   return formatPhoneNumber(phone.replace(/(^00354|^\+354|\D)/g, ''))
-}
-
-export const isValidPhoneNumber = (phoneNumber: string) => {
-  const phone = parsePhoneNumberFromString(phoneNumber, 'IS')
-  return phone && phone.isValid()
 }
