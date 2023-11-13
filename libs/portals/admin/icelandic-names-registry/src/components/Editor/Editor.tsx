99b03f4149fe0892cd2d7a88ba24aaad56676625
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2023-11-13 13:37:42 +0000

    Address PR comment and use lodash.omit instead to make it cleaner.

--- libs/portals/admin/icelandic-names-registry/src/components/Editor/Editor.tsx
+++ libs/portals/admin/icelandic-names-registry/src/components/Editor/Editor.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useMutation, gql } from '@apollo/client'
+import { useMutation } from '@apollo/client'
+import omit from 'lodash/omit'
 import {
   Box,
   Input,
-  Text,
   ModalBase,
   Button,
   ToastContainer,
@@ -140,8 +140,7 @@ const Editor = () => {
   }, [currentName])
 
   const onSubmit = async (formState: IcelandicNameType) => {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const { id, __typename, ...rest } = formState
+    const { id, ...rest } = omit(formState, '__typename')
 
     const body: CreateIcelandicNameInput = {
       ...rest,
