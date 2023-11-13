d2fb7d644f183c9553ae001dc09d2399966ad2e0
albinagu <47886428+albinagu@users.noreply.github.com>
2023-11-13 13:06:30 +0000

    Update libs/application/templates/signature-collection/signature-list-signing/src/lib/messages.ts
    Co-authored-by: kksteini <77672665+kksteini@users.noreply.github.com>

--- libs/application/templates/signature-collection/signature-list-signing/src/lib/messages.ts
+++ libs/application/templates/signature-collection/signature-list-signing/src/lib/messages.ts
@@ -64,7 +64,7 @@ export const m = defineMessages({
   userProfileProviderSubtitle: {
     id: 'sls.application:userProfileProviderSubtitle',
     defaultMessage:
-      'Persónustillingar þínar (sími og netfang) þínar frá Ísland.is.',
+      'Persónustillingar þínar (sími og netfang) frá Ísland.is.',
     description: '',
   },
 
