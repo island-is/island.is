f18e19f810f3363cf7f5bb2e7ccc3b1dfcd2c540
runarvestmann <runar@stefna.is>
2023-11-14 16:17:06 +0000

    Simplify shouldLinkBeAnAnchorTag function

--- libs/shared/utils/src/lib/shouldLinkBeAnAnchorTag.ts
+++ libs/shared/utils/src/lib/shouldLinkBeAnAnchorTag.ts
@@ -3,9 +3,5 @@ import { ProjectBasePath } from '@island.is/shared/constants'
 /** Some links point users to another project (like if a user clicks on a /minarsidur link in the web project),
  * then the link should be an anchor tag instead of a nextlink for example
  * */
-export const shouldLinkBeAnAnchorTag = (path: string) => {
-  for (const basePath of Object.values(ProjectBasePath)) {
-    if (path.includes(basePath)) return true
-  }
-  return false
-}
+export const shouldLinkBeAnAnchorTag = (path: string) =>
+  Object.values(ProjectBasePath).some((basePath) => path.includes(basePath))
