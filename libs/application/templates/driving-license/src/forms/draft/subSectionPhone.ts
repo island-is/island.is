e39a8ef7e2eb799ad7b9facc92f2e00ec5a2c077
kksteini <steini@kosmosogkaos.is>
2023-11-15 12:28:57 +0000

    fix(application-driving-license): Add phone field explicitly, since it's strictly required for submission success.

---
+++ libs/application/templates/driving-license/src/forms/draft/subSectionPhone.ts
@@ -0,0 +1,35 @@
+import {
+  buildDescriptionField,
+  buildMultiField,
+  buildPhoneField,
+  buildSubSection,
+  buildTextField,
+} from '@island.is/application/core'
+import { m } from '../../lib/messages'
+import { Application } from '@island.is/api/schema'
+
+export const subSectionPhone = buildSubSection({
+  id: 'user',
+  title: m.phoneNumberTitle,
+  children: [
+    buildMultiField({
+      id: 'info',
+      title: m.phoneNumberTitle,
+      space: 1,
+      children: [
+        buildDescriptionField({
+          id: 'phoneNumberTitle',
+          title: m.phoneNumberTitle,
+          titleVariant: 'h4',
+          description: m.phoneNumberDescription,
+        }),
+        buildPhoneField({
+          id: 'phone',
+          title: m.phoneNumberTitle,
+          defaultValue: (application: Application) =>
+            application.externalData.userProfile.data.mobilePhoneNumber ?? '',
+        }),
+      ],
+    }),
+  ],
+})
