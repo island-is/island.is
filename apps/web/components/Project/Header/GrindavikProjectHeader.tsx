2e7fab2acdd4d39435fc0dd84eac532bd637848c
runarvestmann <runar@stefna.is>
2023-11-16 16:14:06 +0000

    Add fallback

--- apps/web/components/Project/Header/GrindavikProjectHeader.tsx
+++ apps/web/components/Project/Header/GrindavikProjectHeader.tsx
@@ -36,7 +36,7 @@ export const GrindavikProjectHeader = ({
     return JSON.parse(projectPage.namespace?.fields || '{}')
   }, [projectPage])
 
-  const n = useNamespace(namespace)
+  const n = useNamespace(namespace ?? {})
 
   return (
     <Box className={styles.headerWrapper}>
