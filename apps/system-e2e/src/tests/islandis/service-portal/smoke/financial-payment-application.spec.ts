2ab70d4c49f70caf596567674eb6db0d00b9ef86
Valur Sverrisson <valur@hugsmidjan.is>
2023-11-16 14:25:26 +0000

    fix: messages import

--- apps/system-e2e/src/tests/islandis/service-portal/smoke/financial-payment-application.spec.ts
+++ apps/system-e2e/src/tests/islandis/service-portal/smoke/financial-payment-application.spec.ts
@@ -2,7 +2,7 @@ import { test, BrowserContext, expect } from '@playwright/test'
 import { icelandicAndNoPopupUrl, urls } from '../../../../support/urls'
 import { session } from '../../../../support/session'
 import { label } from '../../../../support/i18n'
-import { m } from '@island.is/service-portal/finance/messages'
+import { messages as m } from '@island.is/service-portal/finance/messages'
 import { disableI18n } from '../../../../support/disablers'
 
 test.use({ baseURL: urls.islandisBaseUrl })
