fee258f88eca36c1117fc426f6b1297c6859b5cb
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2023-11-17 14:46:56 +0000

    fix skip verification check

--- apps/services/user-profile/src/app/v2/user-profile.service.ts
+++ apps/services/user-profile/src/app/v2/user-profile.service.ts
@@ -261,6 +261,9 @@ export class UserProfileService {
      * Remove dashes from mobile phone number and compare last 7 digits of mobilePhoneNumber with the audkenni Phone number
      * Removing the dashes prevents misreading string with format +354-765-4321 as 65-4321
      */
-    return mobilePhoneNumber.replace(/-/g, '').slice(-7) === audkenniSimNumber
+    return (
+      mobilePhoneNumber.replace(/-/g, '').slice(-7) ===
+      audkenniSimNumber.replace(/-/g, '').slice(-7)
+    )
   }
 }
