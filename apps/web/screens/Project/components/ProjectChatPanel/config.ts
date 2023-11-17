93ccdcb47afb7db82e2c5f2a6430da2aa928a1e9
runarvestmann <runar@stefna.is>
2023-11-17 15:57:17 +0000

    Fix type error

--- apps/web/screens/Project/components/ProjectChatPanel/config.ts
+++ apps/web/screens/Project/components/ProjectChatPanel/config.ts
@@ -58,7 +58,7 @@ export const watsonConfig: Record<
     // Information for Ukrainian citizens
     // https://app.contentful.com/spaces/8k0h54kbe6bj/entries/7GtuCCd7MEZhZKe0oXcHdb
     '7GtuCCd7MEZhZKe0oXcHdb': {
-      integrationID: '53c6e788-8178-448d-94c3-f5d71ec3b80e',
+      integrationID: '89a03e83-5c73-4642-b5ba-cd3771ceca54',
       region: 'eu-gb',
       serviceInstanceID: 'bc3d8312-d862-4750-b8bf-529db282050a',
       showLauncher: true,
