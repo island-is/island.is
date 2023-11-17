7f284bd7fa139032619f9e681709fdd05c32852f
Þórður Hafliðason <tolleinn@gmail.com>
2023-11-17 09:47:56 +0000

    revert accidental commit

--- libs/service-portal/documents/src/components/DocumentActionBar/DocumentActionBar.css.ts
+++ libs/service-portal/documents/src/components/DocumentActionBar/DocumentActionBar.css.ts
@@ -3,26 +3,17 @@ import { theme } from '@island.is/island-ui/theme'
 
 export const filterBtns = style({})
 
-globalStyle(`${filterBtns} button:hover, ${filterBtns} a > span:hover`, {
+globalStyle(`${filterBtns} button:hover`, {
   backgroundColor: theme.color.blue100,
 })
 
-globalStyle(`${filterBtns} a:focus`, {
-  outline: 0,
-})
-
-globalStyle(`${filterBtns} a:focus > span`, {
-  color: '#00003c',
-  backgroundColor: '#00e4ca',
-})
-
-globalStyle(`${filterBtns} button, ${filterBtns} a > span`, {
+globalStyle(`${filterBtns} button`, {
   backgroundColor: theme.color.white,
   width: 40,
   height: 40,
 })
 
-globalStyle(`${filterBtns} button svg, ${filterBtns} a > span svg`, {
+globalStyle(`${filterBtns} button svg`, {
   width: 20,
   height: 20,
 })
