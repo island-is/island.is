bc93789aa33de7130b8396cbb3e1edfbf30d4fa6
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-11-20 17:31:03 +0000

    Migrates court documents

---
+++ apps/judicial-system/backend/migrations/20231120164318-update-case.js
@@ -0,0 +1,79 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface) {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.sequelize
+        .query(
+          'SELECT id, court_documents FROM "case" WHERE court_documents IS NOT NULL',
+          { transaction: t },
+        )
+        .then((res) =>
+          Promise.all(
+            res[0].map((c) => {
+              const fix = c.court_documents.reduce(
+                (acc, d, i) =>
+                  acc +
+                  (i === 0 ? '"' : ',"') +
+                  JSON.stringify({
+                    name: d.name,
+                    submittedBy:
+                      d.submittedBy === 'JUDGE'
+                        ? 'DISTRICT_COURT_JUDGE'
+                        : d.submittedBy,
+                  })
+                    .replace(/\\/g, '\\\\')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "''") +
+                  '"',
+                '',
+              )
+              console.log(c.court_documents, fix)
+              return queryInterface.sequelize.query(
+                `UPDATE "case" SET court_documents = '{${fix}}' WHERE id = '${c.id}'`,
+                { transaction: t },
+              )
+            }),
+          ),
+        ),
+    )
+  },
+
+  async down(queryInterface) {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.sequelize
+        .query(
+          'SELECT id, court_documents FROM "case" WHERE court_documents IS NOT NULL',
+          { transaction: t },
+        )
+        .then((res) =>
+          Promise.all(
+            res[0].map((c) => {
+              const fix = c.court_documents.reduce(
+                (acc, d, i) =>
+                  acc +
+                  (i === 0 ? '"' : ',"') +
+                  JSON.stringify({
+                    name: d.name,
+                    submittedBy:
+                      d.submittedBy === 'DISTRICT_COURT_JUDGE'
+                        ? 'JUDGE'
+                        : d.submittedBy,
+                  })
+                    .replace(/\\/g, '\\\\')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "''") +
+                  '"',
+                '',
+              )
+              console.log(c.court_documents, fix)
+              return queryInterface.sequelize.query(
+                `UPDATE "case" SET court_documents = '{${fix}}' WHERE id = '${c.id}'`,
+                { transaction: t },
+              )
+            }),
+          ),
+        ),
+    )
+  },
+}
