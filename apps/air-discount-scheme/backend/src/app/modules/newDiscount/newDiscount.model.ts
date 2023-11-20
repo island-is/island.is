f6fe12e72a8b702ecce22c4b3ef1f40b458fad8b
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2023-11-20 10:28:58 +0000

    Formatting and fixing type import

--- apps/air-discount-scheme/backend/src/app/modules/newDiscount/newDiscount.model.ts
+++ apps/air-discount-scheme/backend/src/app/modules/newDiscount/newDiscount.model.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 
-import {
+import type {
   NewDiscount as TDiscount,
   AirDiscount as TAirDiscount,
   DiscountedFlight as TDiscountedFlight,
--- libs/air-discount-scheme/types/src/index.ts
+++ libs/air-discount-scheme/types/src/index.ts
@@ -3,6 +3,7 @@ export type {
   Discount,
   ExplicitCode,
 } from './lib/discount'
+
 export type {
   NewDiscount,
   AirDiscount,
