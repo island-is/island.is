b177147a053543da7025ec96b730e6e1e0162f6b
runarvestmann <runar@stefna.is>
2023-11-20 15:30:10 +0000

    Reuse constants in queries

--- apps/web/screens/queries/Manual.ts
+++ apps/web/screens/queries/Manual.ts
@@ -1,29 +1,17 @@
 import gql from 'graphql-tag'
 
-import { nestedFields, slices } from './fragments'
+import {
+  assetFields,
+  htmlFields,
+  imageFields,
+  nestedFields,
+  slices,
+} from './fragments'
 
 export const GET_SINGLE_MANUAL_QUERY = gql`
-  fragment HtmlFields on Html {
-    __typename
-    id
-    document
-  }
-  fragment AssetFields on Asset {
-    __typename
-    id
-    title
-    url
-    contentType
-  }
-  fragment ImageFields on Image {
-    __typename
-    id
-    title
-    url
-    contentType
-    width
-    height
-  }
+  ${htmlFields}
+  ${assetFields}
+  ${imageFields}
   query GetSingleManual($input: GetSingleManualInput!) {
     getSingleManual(input: $input) {
       id
