c8275c332b8c1131aaa5004861789fa94cfdc06e
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-11-20 14:34:24 +0000

    fix: audio breakpoints

--- libs/service-portal/core/src/components/AudioPlayer/AudioPlayer.css.ts
+++ libs/service-portal/core/src/components/AudioPlayer/AudioPlayer.css.ts
@@ -1,9 +1,16 @@
-import { theme } from '@island.is/island-ui/theme'
+import { theme, themeUtils } from '@island.is/island-ui/theme'
 import { style } from '@vanilla-extract/css'
 
 export const container = style({
-  maxWidth: '546px',
+  maxWidth: '100%',
   marginLeft: 0,
+
+  ...themeUtils.responsiveStyle({
+    sm: {
+      width: '546px',
+      marginRight: theme.spacing[2],
+    },
+  }),
 })
 
 export const audio = style({
