19604311fe9af2db32e9d5cf27a42a012f7da336
Þórður Hafliðason <tolleinn@gmail.com>
2023-11-21 14:19:39 +0000

    Drop IV. Test perf.

--- libs/shared/utils/src/lib/simpleEncryption.spec.ts
+++ libs/shared/utils/src/lib/simpleEncryption.spec.ts
@@ -1,12 +1,10 @@
-// simpleEncryption.spec.ts
-
 import { encrypt, decrypt } from './simpleEncryption'
+
+const originalText = 'Original Jest Text!'
+const secretKey = 'not-really-secret-key'
 
 describe('Encryption and Decryption Functions', () => {
   test('Encrypt and decrypt a string successfully', () => {
-    const originalText = 'Hello, Jest!'
-    const secretKey = 'test-secret-key'
-
     const encrypted = encrypt(originalText, secretKey)
     const decrypted = decrypt(encrypted, secretKey)
 
@@ -14,3 +12,29 @@ describe('Encryption and Decryption Functions', () => {
     expect(decrypted).toBe(originalText)
   })
 })
+
+// const iterations = 1000
+// test.concurrent('Performance: encrypt and decrypt', async () => {
+//   const startEncrypt = Date.now()
+//   for (let i = 0; i < iterations; i++) {
+//     encrypt(originalText, secretKey)
+//   }
+//   const endEncrypt = Date.now()
+
+//   const encryptedText = encrypt(originalText, secretKey)
+
+//   const startDecrypt = Date.now()
+//   for (let i = 0; i < iterations; i++) {
+//     decrypt(encryptedText, secretKey)
+//   }
+//   const endDecrypt = Date.now()
+
+//   const encryptTime = endEncrypt - startEncrypt
+//   const decryptTime = endDecrypt - startDecrypt
+
+//   console.log(`Encrypt Time: ${encryptTime}ms`)
+//   console.log(`Decrypt Time: ${decryptTime}ms`)
+
+//   expect(encryptTime).toBeLessThan(100)
+//   expect(decryptTime).toBeLessThan(100)
+// })
--- libs/shared/utils/src/lib/simpleEncryption.ts
+++ libs/shared/utils/src/lib/simpleEncryption.ts
@@ -1,10 +1,5 @@
 import { Base64 } from 'js-base64'
-import {
-  randomBytes,
-  createCipheriv,
-  createDecipheriv,
-  createHash,
-} from 'crypto'
+import { createCipheriv, createDecipheriv, createHash } from 'crypto'
 
 const ALGORITHM = 'aes-256-cbc'
 
@@ -15,37 +10,32 @@ const ALGORITHM = 'aes-256-cbc'
  * @returns URL safe Base64
  */
 export const encrypt = (text: string, key: string): string => {
-  const iv = randomBytes(16)
   const derivedKey = hashKey(key)
 
-  const cipher = createCipheriv(ALGORITHM, derivedKey, iv)
+  const cipher = createCipheriv(ALGORITHM, derivedKey, Buffer.alloc(16))
   const encrypted =
     cipher.update(text, 'utf-8', 'base64') + cipher.final('base64')
 
-  return Base64.encodeURI(`${iv.toString('base64')}:${encrypted}`)
+  return Base64.encodeURI(encrypted)
 }
 
 /**
  * @param encryptedText Base64 returned from encrypt()
- * @param key You secret key you used in encrypt()
+ * @param key The secret key you used in encrypt()
  * @returns Reveals the string hidden by encrypt()
  */
 export const decrypt = (encryptedText: string, key: string): string => {
   try {
-    const [receivedIv, encryptedData] = Base64.decode(encryptedText).split(':')
+    const encryptedData = Base64.decode(encryptedText)
     const derivedKey = hashKey(key)
-    const decipher = createDecipheriv(
-      ALGORITHM,
-      derivedKey,
-      Buffer.from(receivedIv, 'base64'),
-    )
+    const decipher = createDecipheriv(ALGORITHM, derivedKey, Buffer.alloc(16))
 
     return (
       decipher.update(encryptedData, 'base64', 'utf-8') +
       decipher.final('utf-8')
     )
   } catch (e) {
-    return ''
+    return encryptedText
   }
 }
 
