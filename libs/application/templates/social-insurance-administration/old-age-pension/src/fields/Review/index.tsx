1b31ac70fc126afd85a6f7e7fc24089d1538f5e3
karenbjorg <khalldorsdottir@deloitte.is>
2023-11-22 09:02:01 +0000

    clean up

--- libs/application/templates/social-insurance-administration/old-age-pension/src/fields/Review/index.tsx
+++ libs/application/templates/social-insurance-administration/old-age-pension/src/fields/Review/index.tsx
@@ -96,9 +96,7 @@ export const Review: FC<ReviewScreenProps> = ({
     state === States.TRYGGINGASTOFNUN_SUBMITTED ||
     state === States.TRYGGINGASTOFNUN_IN_REVIEW ||
     state === States.APPROVED ||
-    state === States.REJECTED ||
-    state === States.PENDING ||
-    state === States.DISMISSED
+    state === States.REJECTED 
 
   return (
     <>
