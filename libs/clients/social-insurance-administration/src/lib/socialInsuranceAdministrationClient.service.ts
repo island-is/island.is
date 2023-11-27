5197cc1fab3a9ae4b4f578bfbff3e56546c63ff0
karenbjorg <khalldorsdottir@deloitte.is>
2023-11-27 13:41:08 +0000

    remove unused import

--- libs/clients/social-insurance-administration/src/lib/socialInsuranceAdministrationClient.service.ts
+++ libs/clients/social-insurance-administration/src/lib/socialInsuranceAdministrationClient.service.ts
@@ -1,5 +1,5 @@
 import { Auth, AuthMiddleware, User } from '@island.is/auth-nest-tools'
-import { Inject, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import {
   OldAgePension,
   SendApplicationApi,
