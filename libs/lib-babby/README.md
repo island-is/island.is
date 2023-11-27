68c1b1720e02ca4bc45eecc69376acfdccd03edb
Kristófer <kristofer@andes.is>
2023-11-27 11:41:29 +0000

    Add lib-babby

---
+++ libs/lib-babby/README.md
@@ -0,0 +1,7 @@
+# lib-babby
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test lib-babby` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/lib-babby/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'lib-babby',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/lib-babby',
+}
