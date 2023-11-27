306491efca466d8c390ee5e0b20adc38c38a3c8e
ylfahfa <yhafsteinsdottir@deloitte.is>
2023-11-27 14:49:06 +0000

    nx upgrade

--- libs/application/templates/household-supplement/src/index.ts
+++ libs/application/templates/household-supplement/src/index.ts
@@ -4,3 +4,9 @@ export const getDataProviders = () => import('./dataProviders')
 export const getFields = () => import('./fields')
 
 export default template
+ 
+export * from './lib/householdSupplementUtils'
+export * from './lib/messages'
+export * from './lib/constants'
+export * from './types'
+ 
