2a89c49a97a9f39aecace59185c3544b821f994a
Rafn Árnason <rafnarnason@gmail.com>
2023-11-28 10:41:15 +0000

    tests sequelize mock

--- apps/services/user-notification/src/app/modules/notifications/messageProcessor.service.spec.ts
+++ apps/services/user-notification/src/app/modules/notifications/messageProcessor.service.spec.ts
@@ -1,96 +1,103 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { MessageProcessorService } from './messageProcessor.service'
+import { LoggingModule } from '@island.is/logging'
+import { logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { HnippTemplate } from './dto/hnippTemplate.response'
+import { CreateHnippNotificationDto } from './dto/createHnippNotification.dto'
+import { CacheModule } from '@nestjs/cache-manager'
+import { NotificationsService } from './notifications.service'
+import {
+  UserProfile,
+  UserProfileLocaleEnum,
+} from '@island.is/clients/user-profile'
+import { getModelToken } from '@nestjs/sequelize'
+import { Notification } from './notification.model'
 
+const mockHnippTemplate: HnippTemplate = {
+  templateId: 'HNIPP.DEMO.ID',
+  notificationTitle: 'Demo title',
+  notificationBody: 'Demo body {{arg1}}',
+  notificationDataCopy: 'Demo data copy',
+  clickAction: '//demo/{{arg2}}',
+  category: 'DEMO',
+  args: ['arg1', 'arg2'],
+}
+const mockTemplates = [mockHnippTemplate, mockHnippTemplate, mockHnippTemplate]
 
-// import { Test, TestingModule } from '@nestjs/testing'
-// import { MessageProcessorService } from './messageProcessor.service'
-// import { LoggingModule } from '@island.is/logging'
-// import { logger, LOGGER_PROVIDER } from '@island.is/logging'
-// import { HnippTemplate } from './dto/hnippTemplate.response'
-// import { CreateHnippNotificationDto } from './dto/createHnippNotification.dto'
-// import { CacheModule } from '@nestjs/cache-manager'
-// import { NotificationsService } from './notifications.service'
-// import {
-//   UserProfile,
-//   UserProfileLocaleEnum,
-// } from '@island.is/clients/user-profile'
+const mockCreateHnippNotificationDto: CreateHnippNotificationDto = {
+  recipient: '1234567890',
+  templateId: 'HNIPP.DEMO.ID',
+  args: [
+    { key: 'arg1', value: 'hello' },
+    { key: 'arg2', value: 'world' },
+  ],
+}
 
-// const mockHnippTemplate: HnippTemplate = {
-//   templateId: 'HNIPP.DEMO.ID',
-//   notificationTitle: 'Demo title',
-//   notificationBody: 'Demo body {{arg1}}',
-//   notificationDataCopy: 'Demo data copy',
-//   clickAction: '//demo/{{arg2}}',
-//   category: 'DEMO',
-//   args: ['arg1', 'arg2'],
-// }
-// const mockTemplates = [mockHnippTemplate, mockHnippTemplate, mockHnippTemplate]
+const mockProfile: UserProfile = {
+  nationalId: '1234567890',
+  mobilePhoneNumber: '1234567',
+  email: 'foo@bar.com',
+  locale: UserProfileLocaleEnum.Is,
+  documentNotifications: true,
+  created: new Date(),
+  modified: new Date(),
+  id: '1234567',
+  emailVerified: true,
+  mobilePhoneNumberVerified: true,
+  profileImageUrl: '',
+  emailStatus: 'VERIFIED',
+  mobileStatus: 'VERIFIED',
+  lastNudge: new Date(), // ... confirm this property hasright value
+}
 
-// const mockCreateHnippNotificationDto: CreateHnippNotificationDto = {
-//   recipient: '1234567890',
-//   templateId: 'HNIPP.DEMO.ID',
-//   args: [
-//     { key: 'arg1', value: 'hello' },
-//     { key: 'arg2', value: 'world' },
-//   ],
-// }
+describe('MessageProcessorService', () => {
+  let service: MessageProcessorService
+  let notificationsService: NotificationsService
 
-// const mockProfile: UserProfile = {
-//   nationalId: '1234567890',
-//   mobilePhoneNumber: '1234567',
-//   email: 'foo@bar.com',
-//   locale: UserProfileLocaleEnum.Is,
-//   documentNotifications: true,
-//   created: new Date(),
-//   modified: new Date(),
-//   id: '1234567',
-//   emailVerified: true,
-//   mobilePhoneNumberVerified: true,
-//   profileImageUrl: '',
-//   emailStatus: 'VERIFIED',
-//   mobileStatus: 'VERIFIED',
-//   lastNudge: new Date(),
-// }
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        CacheModule.register({}),
+        LoggingModule      ],
+        
+      providers: [
+        MessageProcessorService,
+        NotificationsService,
+        {
+          provide: LOGGER_PROVIDER,
+          useValue: logger,
+        },
+        {
+          provide: getModelToken(Notification),
+          useClass: jest.fn(() => ({})),
+        },
+      ],
+    }).compile()
 
-// describe('MessageProcessorService', () => {
-//   let service: MessageProcessorService
-//   let notificationsService: NotificationsService
+    service = module.get<MessageProcessorService>(MessageProcessorService)
+    notificationsService =
+      module.get<NotificationsService>(NotificationsService)
+  })
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       imports: [CacheModule.register({}), LoggingModule],
-//       providers: [
-//         MessageProcessorService,
-//         NotificationsService,
-//         {
-//           provide: LOGGER_PROVIDER,
-//           useValue: logger,
-//         },
-//       ],
-//     }).compile()
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
 
-//     service = module.get<MessageProcessorService>(MessageProcessorService)
-//     notificationsService =
-//       module.get<NotificationsService>(NotificationsService)
-//   })
+  it('should process message', async () => {
+    jest
+      .spyOn(notificationsService, 'getTemplates')
+      .mockImplementation(() => Promise.resolve(mockTemplates))
+    jest
+      .spyOn(notificationsService, 'getTemplate')
+      .mockImplementation(() => Promise.resolve(mockHnippTemplate))
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined()
-//   })
-
-//   it('should process message', async () => {
-//     jest
-//       .spyOn(notificationsService, 'getTemplates')
-//       .mockImplementation(() => Promise.resolve(mockTemplates))
-//     jest
-//       .spyOn(notificationsService, 'getTemplate')
-//       .mockImplementation(() => Promise.resolve(mockHnippTemplate))
-
-//     const notification = await service.convertToNotification(
-//       mockCreateHnippNotificationDto,
-//       mockProfile,
-//     )
-//     expect(notification.title).toMatch('Demo title')
-//     expect(notification.body).toMatch('Demo body hello')
-//     expect(notification.category).toMatch('DEMO')
-//     expect(notification.appURI).toMatch('//demo/world')
-//   })
-// })
+    const notification = await service.convertToNotification(
+      mockCreateHnippNotificationDto,
+      mockProfile,
+    )
+    expect(notification.title).toMatch('Demo title')
+    expect(notification.body).toMatch('Demo body hello')
+    expect(notification.category).toMatch('DEMO')
+    expect(notification.appURI).toMatch('//demo/world')
+  })
+})
--- apps/services/user-notification/src/app/modules/notifications/notifications.service.spec.ts
+++ apps/services/user-notification/src/app/modules/notifications/notifications.service.spec.ts
@@ -1,123 +1,130 @@
-// import { Test, TestingModule } from '@nestjs/testing'
-// import { NotificationsService } from './notifications.service'
-// import { LoggingModule } from '@island.is/logging'
-// import { logger, LOGGER_PROVIDER } from '@island.is/logging'
-// import { HnippTemplate } from './dto/hnippTemplate.response'
-// import { CreateHnippNotificationDto } from './dto/createHnippNotification.dto'
-// import { CacheModule } from '@nestjs/cache-manager'
+import { Test, TestingModule } from '@nestjs/testing'
+import { NotificationsService } from './notifications.service'
+import { LoggingModule } from '@island.is/logging'
+import { logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { HnippTemplate } from './dto/hnippTemplate.response'
+import { CreateHnippNotificationDto } from './dto/createHnippNotification.dto'
+import { CacheModule } from '@nestjs/cache-manager'
+import { SequelizeConfigService } from '@island.is/auth-api-lib'
+import { getModelToken } from '@nestjs/sequelize'
+import { Notification } from './notification.model'
 
-// const mockHnippTemplate: HnippTemplate = {
-//   templateId: 'HNIPP.DEMO.ID',
-//   notificationTitle: 'Demo title ',
-//   notificationBody: 'Demo body {{arg1}}',
-//   notificationDataCopy: 'Demo data copy',
-//   clickAction: 'Demo click action {{arg2}}',
-//   category: 'Demo category',
-//   args: ['arg1', 'arg2'],
-// }
-// const mockTemplates = [mockHnippTemplate, mockHnippTemplate, mockHnippTemplate]
+const mockHnippTemplate: HnippTemplate = {
+  templateId: 'HNIPP.DEMO.ID',
+  notificationTitle: 'Demo title ',
+  notificationBody: 'Demo body {{arg1}}',
+  notificationDataCopy: 'Demo data copy',
+  clickAction: 'Demo click action {{arg2}}',
+  category: 'Demo category',
+  args: ['arg1', 'arg2'],
+}
+const mockTemplates = [mockHnippTemplate, mockHnippTemplate, mockHnippTemplate]
 
-// const mockCreateHnippNotificationDto: CreateHnippNotificationDto = {
-//   recipient: '1234567890',
-//   templateId: 'HNIPP.DEMO.ID',
-//   args: [
-//     { key: 'arg1', value: 'hello' },
-//     { key: 'arg2', value: 'world' },
-//   ],
-// }
+const mockCreateHnippNotificationDto: CreateHnippNotificationDto = {
+  recipient: '1234567890',
+  templateId: 'HNIPP.DEMO.ID',
+  args: [
+    { key: 'arg1', value: 'hello' },
+    { key: 'arg2', value: 'world' },
+  ],
+}
 
-// describe('NotificationsService', () => {
-//   let service: NotificationsService
+describe('NotificationsService', () => {
+  let service: NotificationsService
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       imports: [CacheModule.register({}), LoggingModule],
-//       providers: [
-//         NotificationsService,
-//         {
-//           provide: LOGGER_PROVIDER,
-//           useValue: logger,
-//         },
-//       ],
-//     }).compile()
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register({}), LoggingModule],
+      providers: [
+        NotificationsService,
+        {
+          provide: LOGGER_PROVIDER,
+          useValue: logger,
+        },
+        {
+          provide: getModelToken(Notification),
+          useClass: jest.fn(() => ({})),
+        },
+      ],
+    }).compile()
 
-//     service = module.get<NotificationsService>(NotificationsService)
-//   })
+    service = module.get<NotificationsService>(NotificationsService)
+  })
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined()
-//   })
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
 
-//   it('should get templates', async () => {
-//     jest
-//       .spyOn(service, 'getTemplates')
-//       .mockImplementation(() => Promise.resolve(mockTemplates))
+  it('should get templates', async () => {
+    jest
+      .spyOn(service, 'getTemplates')
+      .mockImplementation(() => Promise.resolve(mockTemplates))
 
-//     const templates = await service.getTemplates()
-//     expect(templates).toBeInstanceOf(Array)
-//     expect(templates).toEqual(mockTemplates)
-//   })
+    const templates = await service.getTemplates()
+    expect(templates).toBeInstanceOf(Array)
+    expect(templates).toEqual(mockTemplates)
+  })
 
-//   it('should get template', async () => {
-//     jest
-//       .spyOn(service, 'getTemplates')
-//       .mockImplementation(() => Promise.resolve(mockTemplates))
-//     const template = await service.getTemplate(mockHnippTemplate.templateId)
-//     expect(template).toBeInstanceOf(Object)
-//   })
+  it('should get template', async () => {
+    jest
+      .spyOn(service, 'getTemplates')
+      .mockImplementation(() => Promise.resolve(mockTemplates))
+    const template = await service.getTemplate(mockHnippTemplate.templateId)
+    expect(template).toBeInstanceOf(Object)
+  })
 
-//   it('should validate true argument count match', () => {
-//     const counts = service.validateArgCounts(
-//       mockCreateHnippNotificationDto,
-//       mockHnippTemplate,
-//     )
-//     expect(mockCreateHnippNotificationDto.args.length).toBe(2)
-//     expect(counts).toBeTruthy()
-//   })
+  it('should validate true argument count match', () => {
+    const counts = service.validateArgCounts(
+      mockCreateHnippNotificationDto,
+      mockHnippTemplate,
+    )
+    expect(mockCreateHnippNotificationDto.args.length).toBe(2)
+    expect(counts).toBeTruthy()
+  })
 
-//   it('should validate false on argument count mismatch +', () => {
-//     mockCreateHnippNotificationDto.args = [
-//       { key: 'arg1', value: 'hello' },
-//       { key: 'arg2', value: 'world' },
-//       { key: 'arg3', value: 'extra' },
-//     ]
-//     const counts = service.validateArgCounts(
-//       mockCreateHnippNotificationDto,
-//       mockHnippTemplate,
-//     )
-//     expect(mockCreateHnippNotificationDto.args.length).toBe(3)
-//     expect(counts).toBe(false)
-//   })
-//   it('should validate false on argument count mismatch -', () => {
-//     mockCreateHnippNotificationDto.args = [{ key: 'arg2', value: 'world' }]
-//     const counts = service.validateArgCounts(
-//       mockCreateHnippNotificationDto,
-//       mockHnippTemplate,
-//     )
-//     expect(mockCreateHnippNotificationDto.args.length).toBe(1)
-//     expect(counts).toBe(false)
-//   })
+  it('should validate false on argument count mismatch +', () => {
+    mockCreateHnippNotificationDto.args = [
+      { key: 'arg1', value: 'hello' },
+      { key: 'arg2', value: 'world' },
+      { key: 'arg3', value: 'extra' },
+    ]
+    const counts = service.validateArgCounts(
+      mockCreateHnippNotificationDto,
+      mockHnippTemplate,
+    )
+    expect(mockCreateHnippNotificationDto.args.length).toBe(3)
+    expect(counts).toBe(false)
+  })
+  it('should validate false on argument count mismatch -', () => {
+    mockCreateHnippNotificationDto.args = [{ key: 'arg2', value: 'world' }]
+    const counts = service.validateArgCounts(
+      mockCreateHnippNotificationDto,
+      mockHnippTemplate,
+    )
+    expect(mockCreateHnippNotificationDto.args.length).toBe(1)
+    expect(counts).toBe(false)
+  })
 
-//   it('should validate false on argument count mismatch 0', () => {
-//     mockCreateHnippNotificationDto.args = []
-//     const counts = service.validateArgCounts(
-//       mockCreateHnippNotificationDto,
-//       mockHnippTemplate,
-//     )
-//     expect(mockCreateHnippNotificationDto.args.length).toBe(0)
-//     expect(counts).toBe(false)
-//   })
+  it('should validate false on argument count mismatch 0', () => {
+    mockCreateHnippNotificationDto.args = []
+    const counts = service.validateArgCounts(
+      mockCreateHnippNotificationDto,
+      mockHnippTemplate,
+    )
+    expect(mockCreateHnippNotificationDto.args.length).toBe(0)
+    expect(counts).toBe(false)
+  })
 
-//   it('should replace template {{placeholders}} with args', () => {
-//     mockCreateHnippNotificationDto.args = [
-//       { key: 'arg1', value: 'hello' },
-//       { key: 'arg2', value: 'world' },
-//     ]
-//     const template = service.formatArguments(
-//       mockCreateHnippNotificationDto,
-//       mockHnippTemplate,
-//     )
-//     expect(template.notificationBody).toEqual('Demo body hello')
-//     expect(template.clickAction).toEqual('Demo click action world')
-//   })
-// })
+  it('should replace template {{placeholders}} with args', () => {
+    mockCreateHnippNotificationDto.args = [
+      { key: 'arg1', value: 'hello' },
+      { key: 'arg2', value: 'world' },
+    ]
+    const template = service.formatArguments(
+      mockCreateHnippNotificationDto,
+      mockHnippTemplate,
+    )
+    expect(template.notificationBody).toEqual('Demo body hello')
+    expect(template.clickAction).toEqual('Demo click action world')
+  })
+})
