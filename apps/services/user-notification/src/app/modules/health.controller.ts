ec7599310b67c2c260d7ebda64eb91c17eadb640
Rafn Árnason <rafnarnason@gmail.com>
2023-11-30 14:46:09 +0000

    worker test

---
+++ apps/services/user-notification/src/app/modules/health.controller.ts
@@ -0,0 +1,19 @@
+import { InfraController } from '@island.is/infra-nest-server'
+import { BadRequestException, Controller, Get } from '@nestjs/common'
+// import { ApiOkResponse } from '@nestjs/swagger'
+// import { Readiness } from './dto/readinessDto'
+// import dns from 'dns'
+
+@Controller()
+export class NotificationsInfraController extends InfraController {
+  constructor() {
+    super()
+  }
+
+  @Get('readiness')
+//   @ApiOkResponse({ type: {ok:boolean} })
+  async readiness(): Promise<{ok:boolean}> {
+    const result = true
+    return { ok: result }
+  }
+}
