dd4b37e5c353c7f94e7119b18d4bb1a650af014d
Helgi Helgason <helgih@kosmosogkaos.is>
2023-12-04 14:37:36 +0000

    fix(estate): check if representative is over 18

--- libs/application/templates/estate/src/lib/dataSchema.ts
+++ libs/application/templates/estate/src/lib/dataSchema.ts
@@ -478,7 +478,7 @@ export const estateSchema = z.object({
     .refine(
       ({ name, nationalId, phone, email }) => {
         return name !== '' || phone !== '' || email !== ''
-          ? nationalId && kennitala.isPerson(nationalId)
+          ? nationalId && kennitala.isPerson(nationalId) && kennitala.info(nationalId).age >= 18
           : true
       },
       {
