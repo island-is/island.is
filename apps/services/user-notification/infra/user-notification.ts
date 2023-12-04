bb8d5cf76aa750a8662a9d557fbba2cf6037020c
Jón Levy <levy@andes.is>
2023-12-04 14:43:05 +0000

    fix: reuse serviceName variable

--- apps/services/user-notification/infra/user-notification.ts
+++ apps/services/user-notification/infra/user-notification.ts
@@ -1,94 +1,86 @@
 import { ref, service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
 
-const MAIN_QUEUE_NAME = 'user-notification'
-const DEAD_LETTER_QUEUE_NAME = 'user-notification-failure'
+const serviceName = 'user-notification'
+const serviceWorkerName = `${serviceName}-worker`
+const dbName = `services_${serviceName.replace('-', '_')}`
+const MAIN_QUEUE_NAME = serviceName
+const DEAD_LETTER_QUEUE_NAME = `${serviceName}-failure`
 
-const dbName = 'services_user_notification'
-
-const servicePostgresInfo = {
-  // The service has only read permissions
-  username: 'services_user_notification_read',
+const postgresInfo = {
+  username: dbName,
   name: dbName,
-  passwordSecret: '/k8s/user-notification/readonly/DB_PASSWORD',
+  passwordSecret: `/k8s/${serviceName}/DB_PASSWORD`,
 }
 
-const workerPostgresInfo = {
-  // Worker has write permissions
-  username: 'services_user_notification',
-  name: dbName,
-  passwordSecret: '/k8s/user-notification/DB_PASSWORD',
-  // extensions: ['uuid-ossp'],
-}
-
-export const userNotificationServiceSetup =
-  (): ServiceBuilder<'user-notification'> =>
-    service('user-notification')
-      .image('services-user-notification')
-      .namespace('user-notification')
-      .serviceAccount('user-notification')
-      .postgres(workerPostgresInfo) // TODO REVERT ME LATER servicePostgresInfo ?
-      .command('node')
-      .args('--no-experimental-fetch', 'main.js')
-      .env({
-        MAIN_QUEUE_NAME,
-        DEAD_LETTER_QUEUE_NAME,
-        IDENTITY_SERVER_ISSUER_URL: {
-          dev: 'https://identity-server.dev01.devland.is',
-          staging: 'https://identity-server.staging01.devland.is',
-          prod: 'https://innskra.island.is',
+export const userNotificationServiceSetup = (): ServiceBuilder<
+  typeof serviceName
+> =>
+  service(serviceName)
+    .image(serviceName)
+    .namespace(serviceName)
+    .serviceAccount(serviceName)
+    .postgres(postgresInfo)
+    .command('node')
+    .args('--no-experimental-fetch', 'main.js')
+    .env({
+      MAIN_QUEUE_NAME,
+      DEAD_LETTER_QUEUE_NAME,
+      IDENTITY_SERVER_ISSUER_URL: {
+        dev: 'https://identity-server.dev01.devland.is',
+        staging: 'https://identity-server.staging01.devland.is',
+        prod: 'https://innskra.island.is',
+      },
+    })
+    .secrets({
+      FIREBASE_CREDENTIALS: `/k8s/${serviceName}/firestore-credentials`,
+      CONTENTFUL_ACCESS_TOKEN: `/k8s/${serviceName}/CONTENTFUL_ACCESS_TOKEN`,
+    })
+    .liveness('/liveness')
+    .readiness('/liveness') // change to readiness - when ready ;)
+    .ingress({
+      primary: {
+        host: {
+          dev: `${serviceName}-xrd`,
+          staging: `${serviceName}-xrd`,
+          prod: `${serviceName}-xrd`,
         },
-      })
-      .secrets({
-        FIREBASE_CREDENTIALS: '/k8s/user-notification/firestore-credentials',
-        CONTENTFUL_ACCESS_TOKEN:
-          '/k8s/user-notification/CONTENTFUL_ACCESS_TOKEN',
-      })
-      .liveness('/liveness')
-      .readiness('/liveness') // change to readiness - when ready ;)
-      .ingress({
-        primary: {
-          host: {
-            dev: 'user-notification-xrd',
-            staging: 'user-notification-xrd',
-            prod: 'user-notification-xrd',
+        paths: ['/'],
+        public: false,
+        extraAnnotations: {
+          dev: {
+            'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
+            'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
           },
-          paths: ['/'],
-          public: false,
-          extraAnnotations: {
-            dev: {
-              'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
-              'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
-            },
-            staging: {
-              'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
-              'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
-            },
-            prod: {
-              'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
-              'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
-            },
+          staging: {
+            'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
+            'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
+          },
+          prod: {
+            'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
+            'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
           },
         },
-      })
-      .resources({
-        limits: {
-          cpu: '200m',
-          memory: '384Mi',
-        },
-        requests: {
-          cpu: '15m',
-          memory: '256Mi',
-        },
-      })
-      .grantNamespaces('nginx-ingress-internal')
+      },
+    })
+    .resources({
+      limits: {
+        cpu: '200m',
+        memory: '384Mi',
+      },
+      requests: {
+        cpu: '15m',
+        memory: '256Mi',
+      },
+    })
+    .grantNamespaces('nginx-ingress-internal')
 
 export const userNotificationWorkerSetup = (services: {
-  userProfileApi: ServiceBuilder<'service-portal-api'>
-}): ServiceBuilder<'user-notification-worker'> =>
-  service('user-notification-worker')
-    .image('services-user-notification')
-    .namespace('user-notification')
-    .serviceAccount('user-notification-worker')
+  userProfileApi: ServiceBuilder<typeof serviceWorkerName>
+}): ServiceBuilder<typeof serviceWorkerName> =>
+  service(serviceWorkerName)
+    .image(serviceName)
+    .namespace(serviceName)
+    .serviceAccount(serviceWorkerName)
     .command('node')
     .args('--no-experimental-fetch', 'main.js', '--job=worker')
     .extraAttributes({
@@ -96,10 +88,10 @@ export const userNotificationWorkerSetup = (services: {
       staging: { schedule: '30 03 * * *' },
       prod: { schedule: '30 03 * * *' },
     })
-    .postgres(workerPostgresInfo)
+    .postgres(postgresInfo)
     .initContainer({
       containers: [{ command: 'npx', args: ['sequelize-cli', 'db:migrate'] }],
-      postgres: workerPostgresInfo,
+      postgres: postgresInfo,
     })
     .env({
       MAIN_QUEUE_NAME,
@@ -124,12 +116,10 @@ export const userNotificationWorkerSetup = (services: {
       },
     })
     .secrets({
-      FIREBASE_CREDENTIALS: '/k8s/user-notification/firestore-credentials',
-      USER_NOTIFICATION_CLIENT_ID:
-        '/k8s/user-notification/USER_NOTIFICATION_CLIENT_ID',
-      USER_NOTIFICATION_CLIENT_SECRET:
-        '/k8s/user-notification/USER_NOTIFICATION_CLIENT_SECRET',
-      CONTENTFUL_ACCESS_TOKEN: '/k8s/user-notification/CONTENTFUL_ACCESS_TOKEN',
+      FIREBASE_CREDENTIALS: `/k8s/${serviceName}/firestore-credentials`,
+      USER_NOTIFICATION_CLIENT_ID: `/k8s/${serviceName}/USER_NOTIFICATION_CLIENT_ID`,
+      USER_NOTIFICATION_CLIENT_SECRET: `/k8s/${serviceName}/USER_NOTIFICATION_CLIENT_SECRET`,
+      CONTENTFUL_ACCESS_TOKEN: `/k8s/${serviceName}/CONTENTFUL_ACCESS_TOKEN`,
     })
     .liveness('/liveness')
     .readiness('/liveness') // change to readiness - when ready ;)
