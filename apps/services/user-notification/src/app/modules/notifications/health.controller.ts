c9d3c85f37c4256ae2dc3aa2cf1c0bc23952295d
Rafn Árnason <rafnarnason@gmail.com>
2023-12-05 10:29:49 +0000

    health controller readiness

--- apps/services/user-notification/src/app/modules/notifications/health.controller.ts
+++ apps/services/user-notification/src/app/modules/notifications/health.controller.ts
@@ -1,47 +1,47 @@
-// import { InfraController } from '@island.is/infra-nest-server'
-// import { Controller, Get } from '@nestjs/common'
-// // import { ApiOkResponse } from '@nestjs/swagger'
-// // import { Readiness } from './dto/readinessDto'
-// // import dns from 'dns'
+import { InfraController } from '@island.is/infra-nest-server'
+import { Controller, Get } from '@nestjs/common'
+// import { ApiOkResponse } from '@nestjs/swagger'
+// import { Readiness } from './dto/readinessDto'
+// import dns from 'dns'
 
-// @Controller()
-// export class NotificationsInfraController extends InfraController {
-//   constructor() {
-//     super()
-//   }
-
-//   @Get('/readiness')
-//   //   @ApiOkResponse({ type: {ok:boolean} }) // TODO HOOOK THIS UP
-//   async readiness(): Promise<{ ok: boolean }> {
-//     const result = true
-//     return { ok: result }
-//   }
-// }
-
-import { LOGGER_PROVIDER } from '@island.is/logging';
-import { Controller, Get, Res, HttpStatus, Version, VERSION_NEUTRAL, Inject, Logger } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { Sequelize } from 'sequelize-typescript';
-
-@Controller('')
-export class HealthController {
-  constructor(
-    private sequelize: Sequelize,
-    @Inject(LOGGER_PROVIDER) private logger: Logger,
-    ) {}
+@Controller()
+export class HealthController extends InfraController {
+  constructor() {
+    super()
+  }
 
   @Get('/readiness')
-  @Version(VERSION_NEUTRAL)
-  @ApiTags('internal')
-  async checkReadiness(@Res() response: any) {
-    try {
-      await this.sequelize.authenticate();
-      // return response.status(HttpStatus.OK).json({ status: 'ok' });
-      return {ok:true}
-    } catch (error) {
-      // return response.status(HttpStatus.SERVICE_UNAVAILABLE).json({ status: 'error', message: error.message });
-      this.logger.error(error);
-      return {ok:false}
-    }
+  //   @ApiOkResponse({ type: {ok:boolean} }) // TODO HOOOK THIS UP
+  async readiness(): Promise<{ ok: boolean }> {
+    const result = true
+    return { ok: result }
   }
 }
+
+// import { LOGGER_PROVIDER } from '@island.is/logging';
+// import { Controller, Get, Res, HttpStatus, Version, VERSION_NEUTRAL, Inject, Logger } from '@nestjs/common';
+// import { ApiTags } from '@nestjs/swagger';
+// import { Sequelize } from 'sequelize-typescript';
+
+// @Controller('')
+// export class HealthController {
+//   constructor(
+//     private sequelize: Sequelize,
+//     @Inject(LOGGER_PROVIDER) private logger: Logger,
+//     ) {}
+
+//   @Get('/readiness')
+//   @Version(VERSION_NEUTRAL)
+//   @ApiTags('internal')
+//   async checkReadiness(@Res() response: any) {
+//     try {
+//       // await this.sequelize.authenticate();
+//       // return response.status(HttpStatus.OK).json({ status: 'ok' });
+//       return {ok:true}
+//     } catch (error) {
+//       // return response.status(HttpStatus.SERVICE_UNAVAILABLE).json({ status: 'error', message: error.message });
+//       // this.logger.error(error);
+//       return {ok:false}
+//     }
+//   }
+// }
