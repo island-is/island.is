26eca5c763e0317126135afb13a3386b64df17a2
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2023-12-08 14:33:53 +0000

    feat: add featuer flags

--- libs/api/domains/intellectual-properties/src/lib/intellectualProperties.resolver.ts
+++ libs/api/domains/intellectual-properties/src/lib/intellectualProperties.resolver.ts
@@ -21,9 +21,15 @@ import { IntellectualPropertiesDesignImagesInput } from './dto/designImages.inpu
 import { IntellectualPropertiesInput } from './dto/ip.input'
 import { IntellectualPropertiesService } from './intellectualProperties.service'
 import { IntellectualPropertiesResponse } from './models/intellectualProperty.model'
+import {
+  FeatureFlagGuard,
+  FeatureFlag,
+  Features,
+} from '@island.is/nest/feature-flags'
 
 @Resolver()
-@UseGuards(IdsUserGuard, ScopesGuard)
+@FeatureFlag(Features.isIntellectualPropertyModuleEnabled)
+@UseGuards(IdsUserGuard, ScopesGuard, FeatureFlagGuard)
 @Audit({ namespace: '@island.is/api/intellectual-properties' })
 export class IntellectualPropertiesResolver {
   constructor(
