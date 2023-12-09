c93891dcff9672052d121a431bbfc27f627eeda8
Sigurgeir Gudmundsson <essgje@gmail.com>
2023-12-09 11:37:28 +0000

    test for vehicle create

--- apps/skilavottord/ws/src/app/modules/vehicle/test/vehicle.service.spec.ts
+++ apps/skilavottord/ws/src/app/modules/vehicle/test/vehicle.service.spec.ts
@@ -1,9 +1,12 @@
 import { VehicleService } from '../vehicle.service'
 import { setup } from '../../../../../test/setup'
 import { INestApplication } from '@nestjs/common'
+import { VehicleModel } from '../vehicle.model'
+import { Test } from '@nestjs/testing'
 
 let app: INestApplication
 let vehicleService: VehicleService
+let vehicleModel: VehicleModel
 
 //sjá->apps/air-discount-scheme/backend/src/app/modules/flight/test/integration/flight.service.spec.ts
 beforeAll(async () => {
@@ -11,17 +14,44 @@ beforeAll(async () => {
   vehicleService = app.get<VehicleService>(VehicleService)
 })
 
+const vehicleMoc = {
+  vehicleId: '123345',
+  ownerNationalId: '1111111111',
+  vehicleType: 'Kia-soul',
+  vehicleColor: 'White',
+} as VehicleModel
+
 describe('PublicVehicleServiceTest', () => {
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        {
+          provide: VehicleModel,
+          useClass: jest.fn(() => ({
+            findOne: () => ({}),
+          })),
+        },
+      ],
+    }).compile()
+    vehicleModel = moduleRef.get<VehicleModel>(VehicleModel)
+  })
+
   describe('run test', () => {
     it('run test test', () => {
       const testRes = vehicleService.test()
       expect(testRes).toBe('test')
     })
     it('run test findByVehicleId', async () => {
-      const testRes = await vehicleService.findByVehicleId('fox33')
-      console.log(testRes)
+      jest
+        .spyOn(vehicleService as any, 'findByVehicleId')
+        .mockImplementation(() => Promise.resolve(vehicleMoc))
+      const testRes1 = await vehicleService.findByVehicleId('HRX53')
+      console.log(`----> testRest1 ----> ` + JSON.stringify(testRes1))
       // expect(true).toBe(true)
-      expect(testRes).toBe(null)
+      expect(testRes1).toBe(vehicleMoc)
+      const testRes2 = await vehicleService.create(vehicleMoc)
+      console.log(`----> testRest2 ----> ${testRes2}`)
+      expect(testRes2).toBe(true)
     })
   })
 })
