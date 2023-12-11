94a3090934a5b637055805e2b07d6f4ef11a0655
Guðjón Guðjónsson <gudjon@kolibri.is>
2023-12-11 15:03:17 +0000

    Uses near generation in cases tests

--- apps/judicial-system/web/src/routes/Shared/Cases/Cases.spec.tsx
+++ apps/judicial-system/web/src/routes/Shared/Cases/Cases.spec.tsx
@@ -10,22 +10,22 @@ import {
   CaseType,
 } from '@island.is/judicial-system-web/src/graphql/schema'
 import {
-  mockCourtOfAppealsQuery,
   mockJudgeQuery,
   mockPrisonUserQuery,
   mockProsecutorQuery,
 } from '@island.is/judicial-system-web/src/utils/mocks'
-import { CasesQuery } from '@island.is/judicial-system-web/src/utils/mutations'
 import { LocaleProvider } from '@island.is/localization'
 
 import Cases from './Cases'
+import { CasesDocument } from './cases.generated'
+import { PrisonCasesDocument } from './prisonCases.generated'
 
 import '@testing-library/jest-dom'
 
 const mockCasesQuery = [
   {
     request: {
-      query: CasesQuery,
+      query: CasesDocument,
     },
     result: {
       data: {
@@ -111,7 +111,7 @@ const mockCasesQuery = [
 const mockCourtCasesQuery = [
   {
     request: {
-      query: CasesQuery,
+      query: CasesDocument,
     },
     result: {
       data: {
@@ -186,7 +186,7 @@ const mockCourtCasesQuery = [
 const mockPrisonUserCasesQuery = [
   {
     request: {
-      query: CasesQuery,
+      query: PrisonCasesDocument,
     },
     result: {
       data: {
@@ -380,27 +380,6 @@ describe('Cases', () => {
     })
   })
 
-  describe('Court of appeals users', () => {
-    test('should only have a single table of cases', async () => {
-      render(
-        <MockedProvider
-          mocks={[...mockCasesQuery, ...mockCourtOfAppealsQuery]}
-          addTypename={false}
-        >
-          <UserProvider authenticated={true}>
-            <LocaleProvider locale="is" messages={{}}>
-              <Cases />
-            </LocaleProvider>
-          </UserProvider>
-        </MockedProvider>,
-      )
-
-      await waitFor(() => {
-        expect(screen.getByTestId('pastCasesTable')).toBeInTheDocument()
-      })
-    })
-  })
-
   describe('Prison users', () => {
     test('should list active and past cases in separate tables based on validToDate', async () => {
       render(
@@ -535,7 +514,7 @@ describe('Cases', () => {
           mocks={[
             {
               request: {
-                query: CasesQuery,
+                query: CasesDocument,
               },
               error: { name: 'error', message: 'message' },
             },
