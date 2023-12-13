2ba72b4895ae04671b1c08e232d20cac49a19a36
Kristófer <kristofer@andes.is>
2023-12-13 15:34:17 +0000

    Updated common logging

--- infra/src/common.ts
+++ infra/src/common.ts
@@ -1,10 +1,40 @@
-const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
+export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error'
+
+const logLevelEnv = (process.env.LOG_LEVEL ?? 'info').toLowerCase()
+export const LOG_LEVEL: LogLevel =
+  (
+    {
+      trace: 'trace',
+      debug: 'debug',
+      info: 'info',
+      warn: 'warn',
+      warning: 'warn',
+      error: 'error',
+    } as const
+  )[logLevelEnv] ?? 'info'
+
 export class Logger {
-  trace = ['trace'].includes(LOG_LEVEL) ? console.trace : () => {}
-  debug = ['debug', 'trace'].includes(LOG_LEVEL) ? console.debug : () => {}
-  log = console.log
-  info = console.info
-  warn = console.warn
-  error = console.error
+  readonly logLevel = LOG_LEVEL
+  log = (level: LogLevel, ...args: any[]) => this._logger(level)(...args)
+  trace = this._logger('trace')
+  debug = this._logger('debug')
+  info = this._logger('info')
+  warn = this._logger('warn')
+  error = this._logger('error')
+
+  // Returns a function that logs at the correct level
+  private _logger(level: LogLevel) {
+    const l2l = {
+      trace: 0,
+      debug: 10,
+      info: 20,
+      warn: 30,
+      error: 40,
+    }
+    if (l2l[level] >= l2l[this.logLevel]) {
+      return console[level]
+    }
+    return () => {}
+  }
 }
 export const logger = new Logger()
