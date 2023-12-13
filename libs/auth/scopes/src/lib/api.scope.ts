019ab9171918c7628dc1b58bde5e728d318f6c24
Alex Diljar <alex@juni.is>
2023-12-13 16:05:43 +0000

    signature collection scope service portal

--- libs/auth/scopes/src/lib/api.scope.ts
+++ libs/auth/scopes/src/lib/api.scope.ts
@@ -25,4 +25,5 @@ export enum ApiScope {
   samgongustofaVehicles = '@island.is/applications/samgongustofa-vehicles',
   carRecycling = '@island.is/applications/urvinnslusjodur',
   energyFunds = '@island.is/applications/orkusjodur',
+  signatureCollection = '@island.is/signature-collection'
 }
