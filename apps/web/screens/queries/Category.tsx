517ddc8eaf170238508b7a2691ce8211b2f40a2c
runarvestmann <runar@stefna.is>
2023-12-15 15:51:51 +0000

    Add missing query

--- apps/web/screens/queries/Category.tsx
+++ apps/web/screens/queries/Category.tsx
@@ -13,6 +13,51 @@ export const GET_CATEGORIES_QUERY = gql`
   }
 `
 
+export const GET_ARTICLES_QUERY = gql`
+  query getArticles($input: GetArticlesInput!) {
+    getArticles(input: $input) {
+      intro
+      importance
+      category {
+        title
+      }
+      slug
+      title
+      body {
+        ...ProcessEntryFields
+      }
+      processEntryButtonText
+      processEntry {
+        ...ProcessEntryFields
+      }
+      group {
+        slug
+        title
+        description
+        importance
+      }
+      subgroup {
+        title
+        importance
+      }
+      otherCategories {
+        title
+      }
+      otherSubgroups {
+        title
+        slug
+        importance
+      }
+      otherGroups {
+        title
+        slug
+        importance
+      }
+    }
+  }
+  ${processEntryFields}
+`
+
 export const GET_CATEGORY_PAGES_QUERY = gql`
   ${processEntryFields}
   query getCategoryPages($input: GetCategoryPagesInput!) {
