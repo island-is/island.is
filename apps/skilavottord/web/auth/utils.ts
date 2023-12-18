1acc4460c02c9b533dacf06777bc5eabb9e85204
bkristmundsson <bikkibo@gmail.com>
2023-12-18 13:23:41 +0000

    Remove debug code

--- apps/skilavottord/web/auth/utils.ts
+++ apps/skilavottord/web/auth/utils.ts
@@ -14,8 +14,6 @@ type Page =
 export const isDeveloper = (role: Role) => role === Role.developer
 
 export const hasPermission = (page: Page, role: Role) => {
-  console.log('roleXXXX', role)
-  role = Role.developer
   if (!role) return false
 
   if (role === Role.developer) return true
