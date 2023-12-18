b42dd399f28f1e6e67d55ece2b73aa0c48b0f1d7
Berglind Ómarsdóttir <berglind.omarsdottir@origo.is>
2023-12-18 14:54:47 +0000

    updates

--- apps/web/types.tsx
+++ apps/web/types.tsx
@@ -1,7 +1,7 @@
 import { ComponentType } from 'react'
-import { ApolloClient } from '@apollo/client/core'
-import { NormalizedCacheObject } from '@apollo/client/cache'
 import { GetServerSidePropsContext } from 'next'
+import { NormalizedCacheObject } from '@apollo/client/cache'
+import { ApolloClient } from '@apollo/client/core'
 
 export type ScreenContext = {
   query: GetServerSidePropsContext['query']
