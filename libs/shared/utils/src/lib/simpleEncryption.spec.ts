5dd50b948ca8509bf5603b54a1917b56f02ab9e2
Þórður H <thordur@hugsmidjan.is>
2023-12-18 10:11:43 +0000

    remove commented out code simpleEncryption.spec.ts

--- libs/shared/utils/src/lib/simpleEncryption.spec.ts
+++ libs/shared/utils/src/lib/simpleEncryption.spec.ts
@@ -27,29 +27,3 @@ describe('Encryption and Decryption Functions', () => {
     expect(decryptedFailure).toBeNull()
   })
 })
-
-// const iterations = 1000
-// test.concurrent('Performance: encrypt and decrypt', async () => {
-//   const startEncrypt = Date.now()
-//   for (let i = 0; i < iterations; i++) {
-//     encrypt(originalText, secretKey)
-//   }
-//   const endEncrypt = Date.now()
-
-//   const encryptedText = encrypt(originalText, secretKey)
-
-//   const startDecrypt = Date.now()
-//   for (let i = 0; i < iterations; i++) {
-//     decrypt(encryptedText, secretKey)
-//   }
-//   const endDecrypt = Date.now()
-
-//   const encryptTime = endEncrypt - startEncrypt
-//   const decryptTime = endDecrypt - startDecrypt
-
-//   console.log(`Encrypt Time: ${encryptTime}ms`)
-//   console.log(`Decrypt Time: ${decryptTime}ms`)
-
-//   expect(encryptTime).toBeLessThan(100)
-//   expect(decryptTime).toBeLessThan(100)
-// })
