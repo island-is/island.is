e39aea94fb9144bb080e3f1082e523fb05cb5861
Ívar Oddsson <ivaroddsson@gmail.com>
2023-12-19 11:32:09 +0000

    chore(j-s): Disable multiple click on the same case (#13109)
    * Checkpoint
    
    * Checkpoint
    
    * Add loading indicator to cases while opening and disable sub mouse clicks
    
    * Add loading indicator to mobile category cards
    
    * Add opening indcator to past cases
    
    * Add loading indicator to mobile cards
    
    * Remove duplicate code
    
    * Cleanup
    
    * Clear timeouts
    
    * Add aria-diabled to mobile cards when loading
    
    * Fix tests
    
    * Minor css fix
    
    ---------
    
    Co-authored-by: unakb <una@kolibri.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/components/Table/AppealCasesTable/AppealCasesTable.tsx
+++ apps/judicial-system/web/src/components/Table/AppealCasesTable/AppealCasesTable.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react'
 import { useIntl } from 'react-intl'
 import cn from 'classnames'
+import { AnimatePresence } from 'framer-motion'
 
 import { Box, Text } from '@island.is/island-ui/core'
 import { theme } from '@island.is/island-ui/theme'
@@ -27,20 +28,22 @@ import {
   useSortAppealCases,
   useViewport,
 } from '@island.is/judicial-system-web/src/utils/hooks'
+import useCaseList from '@island.is/judicial-system-web/src/utils/hooks/useCaseList'
 
 import MobileAppealCase from './MobileAppealCase'
 import * as styles from '../Table.css'
 
 interface Props {
   cases: CaseListEntry[]
-  onRowClick: (id: string) => void
   loading: boolean
   showingCompletedCases?: boolean
 }
 
 const AppealCasesTable: React.FC<Props> = (props) => {
-  const { cases, onRowClick, loading, showingCompletedCases } = props
+  const { cases, loading, showingCompletedCases } = props
   const { formatMessage } = useIntl()
+  const { isOpeningCaseId, handleOpenCase, LoadingIndicator, showLoading } =
+    useCaseList()
   const { sortedData, requestSort, getClassNamesFor, isActiveColumn } =
     useSortAppealCases('appealedDate', 'descending', cases)
   const activeCasesData = useMemo(
@@ -59,7 +62,8 @@ const AppealCasesTable: React.FC<Props> = (props) => {
         <Box marginTop={2} key={theCase.id}>
           <MobileAppealCase
             theCase={theCase}
-            onClick={() => onRowClick(theCase.id)}
+            onClick={() => handleOpenCase(theCase.id)}
+            isLoading={isOpeningCaseId === theCase.id && showLoading}
           >
             {showingCompletedCases && (
               <Text fontWeight={'medium'} variant="small">
@@ -89,7 +93,6 @@ const AppealCasesTable: React.FC<Props> = (props) => {
             />
           </th>
           <TableHeaderText title={formatMessage(tables.type)} />
-
           <TableHeaderText title={formatMessage(tables.state)} />
           {showingCompletedCases ? (
             <TableHeaderText title={formatMessage(tables.duration)} />
@@ -104,6 +107,7 @@ const AppealCasesTable: React.FC<Props> = (props) => {
               />
             </th>
           )}
+          <th></th>
         </>
       }
     >
@@ -111,7 +115,7 @@ const AppealCasesTable: React.FC<Props> = (props) => {
         return (
           <tr
             className={styles.row}
-            onClick={() => onRowClick(column.id)}
+            onClick={() => handleOpenCase(column.id)}
             key={column.id}
           >
             <td>
@@ -131,7 +135,6 @@ const AppealCasesTable: React.FC<Props> = (props) => {
                 parentCaseId={column.parentCaseId ?? ''}
               />
             </td>
-
             <td>
               <TagAppealState
                 appealState={column.appealState}
@@ -158,6 +161,13 @@ const AppealCasesTable: React.FC<Props> = (props) => {
                     : '-'}
                 </Text>
               )}
+            </td>
+            <td className={styles.loadingContainer}>
+              <AnimatePresence>
+                {isOpeningCaseId === column.id && showLoading && (
+                  <LoadingIndicator />
+                )}
+              </AnimatePresence>
             </td>
           </tr>
         )
