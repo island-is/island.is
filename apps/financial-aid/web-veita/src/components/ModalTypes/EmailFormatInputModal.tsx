6a14afb6be846c3445e2c5d3fd4b4ad490d7a872
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2023-12-22 10:41:09 +0000

    adding break space

--- apps/financial-aid/web-veita/src/components/ModalTypes/EmailFormatInputModal.tsx
+++ apps/financial-aid/web-veita/src/components/ModalTypes/EmailFormatInputModal.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useRef, ChangeEvent } from 'react'
 
 import { InputModal } from '@island.is/financial-aid-web/veita/src/components'
-import { Text, Box, Input } from '@island.is/island-ui/core'
+import { Box } from '@island.is/island-ui/core'
 import { ApplicationState } from '@island.is/financial-aid/shared/lib'
 
 import * as styles from './ModalTypes.css'
-import is from 'libs/island-ui/core/src/lib/Hyphen/patterns/is'
 
 interface Props {
   onCancel: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -49,6 +48,33 @@ const EmailFormatInputModal = ({
     }
   }, [hasError])
 
+  useEffect(() => {
+    if (ref.current) {
+      if (state === ApplicationState.REJECTED) {
+        ref.current.innerHTML = `${prefixText}<br/><br/><b>[SKRIFA ÁSTÆÐU HÉR]</b><br/><br/>${postfixText}         <br />
+              <br /> 
+              <b>Málskot</b>
+              Bent skal á að unnt er að skjóta ákvörðun þessari til
+              áfrýjunarnefndar þíns sveitarfélags. Skal það gert skriflega og
+              innan fjögurra vikna. Fyrir frekari upplýsingar um málskot hafðu
+              samband með tölvupósti á netfangið
+         
+              <a href={"mailto:${municipalityEmail}"} rel="noreferrer noopener">
+                <span className="linkInText">${municipalityEmail}.</span>
+              </a>
+       
+              <br />
+              <br />
+              Ákvörðun ráðsins má síðan skjóta til úrskurðarnefndar
+              velferðarmála, Katrínartúni 2, 105 Reykjavík innan þriggja mánaða.
+              <br />
+              <br />`
+      } else {
+        ref.current.innerHTML = `${prefixText}<br/><br/><b>[SKRIFA ÁSTÆÐU HÉR]</b><br/><br/>${postfixText}`
+      }
+    }
+  }, [isModalVisable])
+
   const onPaste = (event: any) => {
     document.execCommand(
       'insertText',
@@ -83,29 +109,19 @@ const EmailFormatInputModal = ({
     return false
   }
 
-  const handleInput = (e: ChangeEvent<HTMLDivElement>) => {
-    console.log('innerText:', e.target.innerText, ref.current?.innerHTML)
-    const innerText = e.target.innerText
-    if (
-      !innerText.includes('[SKRIFA ÁSTÆÐU HÉR]') &&
-      ref.current?.innerHTML.includes('<b>')
-    ) {
-      ref.current.innerHTML = ref.current.innerHTML.replace('<b>', '')
-      ref.current.innerHTML = ref.current.innerHTML.replace('</b>', '')
-    }
-  }
-
   return (
     <InputModal
       headline={headline}
       onCancel={onCancel}
       onSubmit={() => {
-        if (errorCheckOnTextContent(ref.current?.textContent)) {
+        if (
+          !ref.current?.innerText ||
+          errorCheckOnTextContent(ref?.current?.innerText)
+        ) {
           setHasError(true)
           return
         }
-        console.log('fer í gegn')
-        // onSaveApplication(ref.current.textContent)
+        onSaveApplication(ref.current.innerText)
       }}
       submitButtonText={submitButtonText}
       isModalVisable={isModalVisable}
@@ -118,34 +134,7 @@ const EmailFormatInputModal = ({
           contentEditable={true}
           onPaste={onPaste}
           className={styles.textAreaEditable}
-        >
-          {prefixText}
-          <br />
-          <br />
-          <b>[SKRIFA ÁSTÆÐU HÉR]</b>
-          <br />
-          {postfixText}
-          {state === ApplicationState.REJECTED && (
-            <>
-              <br />
-              <br /> <br />
-              <b>Málskot</b>
-              Bent skal á að unnt er að skjóta ákvörðun þessari til
-              áfrýjunarnefndar þíns sveitarfélags. Skal það gert skriflega og
-              innan fjögurra vikna. Fyrir frekari upplýsingar um málskot hafðu
-              samband með tölvupósti á netfangið{' '}
-              <a href={`mailto:${municipalityEmail}`} rel="noreferrer noopener">
-                <span className="linkInText">{municipalityEmail}.</span>
-              </a>
-              <br />
-              <br />
-              Ákvörðun ráðsins má síðan skjóta til úrskurðarnefndar
-              velferðarmála, Katrínartúni 2, 105 Reykjavík innan þriggja mánaða.
-              <br />
-              <br />
-            </>
-          )}
-        </div>
+        />
       </Box>
     </InputModal>
   )
