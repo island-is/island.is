78a3d81fbe385944cd6146f821ccaf91d82fc691
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2023-12-22 10:47:38 +0000

    Fixing duplicate ID and updating extract strings script

--- libs/application/templates/official-journal-of-iceland/src/lib/messages/prerequisites.ts
+++ libs/application/templates/official-journal-of-iceland/src/lib/messages/prerequisites.ts
@@ -3,18 +3,18 @@ import { defineMessages } from 'react-intl'
 export const prerequisites = {
   general: defineMessages({
     formTitle: {
-      id: 'ojoi.application:prerequisites.general.title',
+      id: 'ojoi.application:prerequisites.general.formTitle',
       defaultMessage: 'Upplýsingar um innsendingar',
       description: 'Title of prerequisites form',
     },
     formIntro: {
-      id: 'ojoi.application:prerequisites.general.description',
+      id: 'ojoi.application:prerequisites.general.formIntro',
       defaultMessage:
         'Þú ert að fara að senda mál til birtingar í Stjórnartíðindum. {br} Dómsmálaráðuneytið gefur út Stjórnartíðindi sem skiptast í A-, B- og C-deild. Birting í Stjórnartíðindum hefur réttaráhrif. Í Stjórnartíðindum skal birta öll lög, stjórnvaldsfyrirmæli og samninga við önnur ríki, svo og auglýsingar varðandi gildi þeirra, sbr. lög nr. 15/2005. {br} Á þínu svæði færð þú uppfærða yfirsýn yfir allar auglýsingar þínar, innsendar, í vinnslu og útgefnar. Auglýsendur fá rafrænan reikning á island.is vegna birtingarkostnaðar eftir hvern mánuð.',
       description: 'Description of prerequisites form',
     },
     sectionTitle: {
-      id: 'ojoi.application:prerequisites.general.title',
+      id: 'ojoi.application:prerequisites.general.sectionTitle',
       defaultMessage: 'Upplýsingar',
       description: 'Title of prerequisites section',
     },
