c61236ad31fc324be0861f0a0a3f4ffa92f1c950
snaerseljan <snaer@aranja.com>
2023-12-22 11:12:42 +0000

    Add feature flag to client

--- libs/service-portal/restrictions/src/module.tsx
+++ libs/service-portal/restrictions/src/module.tsx
@@ -1,11 +1,13 @@
 import { lazy } from 'react'
+
 import { ApiScope } from '@island.is/auth/scopes'
+import { Features } from '@island.is/feature-flags'
 import { PortalModule } from '@island.is/portals/core'
 
-import { restrictionsLoader } from './screens/restrictions/Restrictions.loader'
-import { restrictionsAction } from './screens/restrictions/Restrictions.action'
-import { Paths } from './lib/paths'
 import { m } from './lib/messages'
+import { Paths } from './lib/paths'
+import { restrictionsAction } from './screens/restrictions/Restrictions.action'
+import { restrictionsLoader } from './screens/restrictions/Restrictions.loader'
 
 const allowedScopes: string[] = [ApiScope.internal]
 
@@ -13,6 +15,7 @@ const Restrictions = lazy(() => import('./screens/restrictions/Restrictions'))
 
 export const restrictionsModule: PortalModule = {
   name: m.restrictions,
+  featureFlag: Features.disableNewDeviceLogins,
   enabled({ userInfo }) {
     return userInfo.scopes.some((scope) => allowedScopes.includes(scope))
   },
