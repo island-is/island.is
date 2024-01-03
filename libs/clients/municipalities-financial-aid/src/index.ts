e8197938b302b5b1be7463a2c2fb589510153f50
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-01-03 16:09:13 +0000

    importing service

--- libs/clients/municipalities-financial-aid/src/index.ts
+++ libs/clients/municipalities-financial-aid/src/index.ts
@@ -1,3 +1,5 @@
 export { MunicipalitiesFinancialAidClientModule } from './lib/municipalitiesFinancialAidClient.module'
+export { MunicipalitiesFinancialAidClientService } from './lib/municipalitiesFinancialAidClient.service'
+
 export * from '../gen/fetch'
 export * from './lib/municipalitiesFinancialAid.config'
--- libs/clients/municipalities-financial-aid/src/lib/municipalitiesFinancialAidClient.module.ts
+++ libs/clients/municipalities-financial-aid/src/lib/municipalitiesFinancialAidClient.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { exportedApis } from './apis'
+import { MunicipalitiesFinancialAidClientService } from './municipalitiesFinancialAidClient.service'
 
 @Module({
   providers: exportedApis,
