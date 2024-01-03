37c2a7518264db7a278df742e1ff5563d621f440
runarvestmann <runar@stefna.is>
2024-01-03 15:40:36 +0000

    Add functions for fetching data

--- libs/clients/icelandic-government-institution-vacancies-v2/src/lib/icelandic-government-institution-vacancies-v2.service.ts
+++ libs/clients/icelandic-government-institution-vacancies-v2/src/lib/icelandic-government-institution-vacancies-v2.service.ts
@@ -5,16 +5,59 @@ import {
   VacanciesGetLanguageEnum,
 } from '../../gen/fetch'
 
+const PAGE_SIZE = 10 // TODO: perhaps have a param in "getVacancies" that controls this number
+
 @Injectable()
 export class IcelandicGovernmentInstitutionVacanciesV2Service {
   constructor(private readonly api: DefaultApi) {}
 
-  async getVacancies() {
-    const response = await this.api.vacanciesGet({
+  async getVacancies(input: {
+    query: string
+    page: number
+    vacancyType: string
+    location: string
+    institution: string
+  }) {
+    const vacancies = await this.api.vacanciesGet({
       accept: VacanciesGetAcceptEnum.Json,
-      language: VacanciesGetLanguageEnum.IS,
-      stofnun: '',
-      query: 'asdf',
+      fetchSize: PAGE_SIZE,
+      fetchOffset: (input.page - 1) * PAGE_SIZE,
+      jobtype: input.vacancyType,
+      language: VacanciesGetLanguageEnum.IS, // TODO: check other languages
+      location: input.location,
+      query: input.query,
+      stofnun: input.institution,
     })
+    return {
+      total: vacancies.attributes.totalRows,
+      vacancies: [], // TODO: return vacancies
+    }
+  }
+
+  async getInstitutions() {
+    const institutions = await this.api.stofnanirGet()
+    // TODO: perhaps fetch all items (check to see if there are more)
+    return institutions.items.map((institution) => ({
+      id: institution.lookupCode,
+      label: institution.meaning,
+    }))
+  }
+
+  async getVacancyTypes() {
+    const types = await this.api.jobtypesGet()
+    // TODO: perhaps fetch all items (check to see if there are more)
+    return types.items.map((type) => ({
+      id: type.lookupCode,
+      label: type.meaning,
+    }))
+  }
+
+  async getLocations() {
+    const locations = await this.api.locationsGet()
+    // TODO: perhaps fetch all locatiitemsons (check to see if there are more)
+    return locations.items.map((location) => ({
+      id: location.lookupCode,
+      label: location.meaning,
+    }))
   }
 }
