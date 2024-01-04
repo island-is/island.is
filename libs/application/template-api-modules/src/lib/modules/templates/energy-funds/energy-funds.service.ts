e7d8630454127f5599d789f77141af9e72bb3eef
berglindoma13 <berglindis@gmail.com>
2024-01-04 08:13:35 +0000

    fix(energy-fund): adding import code (#13266)
    * messag error fixed
    
    * fixing missing code
    
    * item code added to answers

--- libs/application/template-api-modules/src/lib/modules/templates/energy-funds/energy-funds.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/energy-funds/energy-funds.service.ts
@@ -80,8 +80,8 @@ export class EnergyFundsService extends BaseTemplateApiService {
       onlyElectricVehiclesWithGrant?.map(async (vehicle) => {
         let hasReceivedSubsidy: boolean | undefined
 
-        // Only validate if fewer than 5 items
-        if (onlyElectricVehiclesWithGrant.length < 5) {
+        // Only validate if fewer than 6 items
+        if (onlyElectricVehiclesWithGrant.length < 6) {
           // Get subsidy status
           hasReceivedSubsidy =
             await this.energyFundsClientService.checkVehicleSubsidyAvilability(
@@ -152,9 +152,7 @@ export class EnergyFundsService extends BaseTemplateApiService {
       vIN: currentvehicleDetails?.vin || '',
       carNumber: applicationAnswers?.selectVehicle.plate,
       carType: (currentvehicleDetails && currentvehicleDetails.make) || '',
-      itemcode:
-        (currentvehicleDetails && currentvehicleDetails.vehicleGrantItemCode) ||
-        '',
+      itemcode: applicationAnswers?.selectVehicle.grantItemCode || '',
       vehicleGroup: currentvehicleDetails?.vehicleRegistrationCode || '',
       purchasePrice:
         (applicationAnswers?.vehicleDetails.price &&
@@ -172,8 +170,7 @@ export class EnergyFundsService extends BaseTemplateApiService {
             'yyyy-MM-dd',
           )
         : '',
-      subsidyAmount:
-        (currentvehicleDetails && currentvehicleDetails.vehicleGrant) || 0,
+      subsidyAmount: applicationAnswers?.selectVehicle.grantAmount || 0,
     }
 
     await this.energyFundsClientService.submitEnergyFundsApplication(auth, {
