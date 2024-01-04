0507b27b424e433d8139b6f214e17f65820dcd6f
Alex Diljar Birkisbur Hellsing <42963845+alexdiljar@users.noreply.github.com>
2024-01-04 16:02:04 +0000

    feat(singature-collection): max reached check (#13276)
    Co-authored-by: Alex Diljar <alex@juni.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/clients/signature-collection/src/lib/types/bulkUpload.dto.ts
+++ libs/clients/signature-collection/src/lib/types/bulkUpload.dto.ts
@@ -2,12 +2,10 @@ import { Signature } from './signature.dto'
 
 export interface FailedNationalIds {
   nationalId: string
-
   reason?: string
 }
 
 export interface BulkUpload {
   success: Signature[]
-
   failed: FailedNationalIds[]
 }
