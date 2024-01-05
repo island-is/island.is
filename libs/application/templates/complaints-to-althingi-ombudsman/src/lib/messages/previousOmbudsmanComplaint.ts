1f30ca849cbd2ddc7a141e77e9342c7f6659493a
Halli1414 <42970319+Halli1414@users.noreply.github.com>
2024-01-05 15:18:50 +0000

    update strings

--- libs/application/templates/complaints-to-althingi-ombudsman/src/lib/messages/previousOmbudsmanComplaint.ts
+++ libs/application/templates/complaints-to-althingi-ombudsman/src/lib/messages/previousOmbudsmanComplaint.ts
@@ -12,23 +12,10 @@ export const previousOmbudsmanComplaint = {
       defaultMessage: 'Hefur þú leitað áður til umboðsmanns vegna málsins?',
       description: 'Main heading for the section',
     },
-  }),
-  moreInfo: defineMessages({
-    title: {
-      id: `ctao.application:general.previousOmbudsmanComplaint.moreInfo.title`,
-      defaultMessage: 'Nánari rökstuðningur kvörtunar og önnur fylgiskjöl',
-      description: 'Title for the description field',
-    },
     label: {
       id: `ctao.application:general.previousOmbudsmanComplaint.moreInfo.label`,
       defaultMessage: 'Nánari skýring',
       description: 'Label for the description field',
-    },
-    description: {
-      id: `ctao.application:general.previousOmbudsmanComplaint.moreInfo.description`,
-      defaultMessage:
-        'Ef þú vilt láta nánari rökstuðning fyrir kvörtuninni fylgja er hægt að setja hann fram á sérstöku skjali í lok kvörtunarferlisins. Það er líka hægt að koma með gögn á skrifstofu umboðsmanns Alþingis að Templarasundi 5 í Reykjavík eða senda þau með pósti.',
-      description: 'Description field for textbox',
     },
   }),
 }
