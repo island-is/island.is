63c2ffe5edd5588fc50663353122d4b89dbd1a4a
Veronika Sif <veronikasif@hotmail.com>
2024-01-05 14:57:39 +0000

    Updated hs e2e tests to use sia core messages

--- apps/system-e2e/src/tests/islandis/application-system/acceptance/household-supplement.spec.ts
+++ apps/system-e2e/src/tests/islandis/application-system/acceptance/household-supplement.spec.ts
@@ -5,8 +5,9 @@ import {
 } from '../../../../support/disablers'
 import { session } from '../../../../support/session'
 import { helpers } from '../../../../support/locator-helpers'
-import { householdSupplementFormMessage } from '@island.is/application/templates/household-supplement/messages'
+import { householdSupplementFormMessage } from '@island.is/application/templates/social-insurance-administration/household-supplement'
 import { label } from '../../../../support/i18n'
+import { socialInsuranceAdministrationMessage } from '@island.is/application/templates/social-insurance-administration-core/lib/messages'
 
 const homeUrl = '/umsoknir/heimilisuppbot'
 
@@ -15,7 +16,7 @@ const applicationTest = base.extend<{ applicationPage: Page }>({
     const applicationContext = await session({
       browser,
       homeUrl,
-      // phoneNumber: '0103019',
+      // phoneNumber: '0103019', // Gervimaður Afríka
       phoneNumber: '0104929', // Gervimaður Bretland
       idsLoginOn: true,
     })
@@ -41,170 +42,184 @@ applicationTest.describe('Household Supplement', () => {
 
       // await page.pause()
 
-      // External Data
-      await expect(
-        page.getByRole('heading', {
-          name: label(householdSupplementFormMessage.pre.externalDataSection),
-        }),
-      ).toBeVisible()
-      await page.getByTestId('agree-to-data-providers').click()
-      await page
-        .getByRole('button', {
-          name: label(householdSupplementFormMessage.pre.startApplication),
-        })
-        .click()
-
-      // Applicant info
-      await expect(
-        page.getByRole('heading', {
-          name: label(householdSupplementFormMessage.info.subSectionTitle),
-        }),
-      ).toBeVisible()
-
-      const emailBox = page.getByRole('textbox', {
-        name: label(householdSupplementFormMessage.info.applicantEmail),
-      })
-      await emailBox.selectText()
-      // TODO: Do we need to create a test email and add it here like in the parental leave application?
-      await emailBox.type('mockEmail@island.is')
-
-      // TODO: Should we select country code?
-      const phoneNumber = page.getByRole('textbox', {
-        name: label(householdSupplementFormMessage.info.applicantPhonenumber),
-      })
-      await phoneNumber.selectText()
-      await phoneNumber.type('6555555')
-      await proceed()
-
-      // Payment information
-      await expect(
-        page.getByRole('heading', {
-          name: label(householdSupplementFormMessage.info.paymentTitle),
-        }),
-      ).toBeVisible()
-      const paymentBank = page.getByRole('textbox', {
-        name: label(householdSupplementFormMessage.info.paymentBank),
-      })
-      await paymentBank.selectText()
-      await paymentBank.type('051226054678')
-      await proceed()
-
-      // Household supplement
-      // TODO: What should i select here?
-      await expect(
-        page.getByRole('heading', {
-          name: label(
-            householdSupplementFormMessage.shared.householdSupplement,
-          ),
-        }),
-      ).toBeVisible()
-
-      await page
-        .getByRole('region', {
-          name: label(
-            householdSupplementFormMessage.info.householdSupplementHousing,
-          ),
-        })
-        .getByRole('radio', {
-          name: label(
-            householdSupplementFormMessage.info.householdSupplementHousingOwner,
-          ),
-        })
-        .click()
-
-      await page
-        .getByRole('region', {
-          name: label(
-            householdSupplementFormMessage.info
-              .householdSupplementChildrenBetween18And25,
-          ),
-        })
-        .getByRole('radio', {
-          name: label(householdSupplementFormMessage.shared.no),
-        })
-        .click()
-      await proceed()
-
-      // Period
-      await expect(
-        page.getByRole('heading', {
-          name: label(householdSupplementFormMessage.info.periodTitle),
-        }),
-      ).toBeVisible()
-
-      await page.getByTestId('select-period.year').click()
-      await page.keyboard.press('Enter')
-
-      await page.getByTestId('select-period.month').click()
-      await page.keyboard.press('Enter')
-      await proceed()
-
-      // Additional documents
-      await expect(
-        page.getByRole('heading', {
-          name: label(
-            householdSupplementFormMessage.fileUpload.additionalFileTitle,
-          ),
-        }),
-      ).toBeVisible()
-      await proceed()
-
-      // Comment
-      await expect(
-        page.getByRole('heading', {
-          name: label(householdSupplementFormMessage.comment.commentSection),
-        }),
-      ).toBeVisible()
-      await page
-        .getByPlaceholder(
-          label(householdSupplementFormMessage.comment.placeholder),
-        )
-        .fill(
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula malesuada augue, sit amet pulvinar tortor pellentesque at. Nulla facilisi. Nunc vel mi ac mi commodo rhoncus sit amet ut neque.',
-        )
-      await proceed()
-
-      // Submit application
-      await expect(
-        page
-          .locator('form')
-          .getByRole('paragraph')
-          .filter({
-            hasText: label(householdSupplementFormMessage.confirm.title),
-          }),
-      ).toBeVisible()
-      await page
-        .getByRole('button', {
-          name: label(householdSupplementFormMessage.confirm.title),
-        })
-        .click()
-
-      // Conclusion screen
-      await expect(
-        page.getByRole('heading', {
-          name: label(householdSupplementFormMessage.conclusionScreen.title),
-        }),
-      ).toBeVisible()
-      await page
-        .getByRole('button', {
-          name: label(
-            householdSupplementFormMessage.conclusionScreen
-              .buttonsViewApplication,
-          ),
-        })
-        .click()
-
-      // Review application
-      await expect(
-        page
-          .locator('form')
-          .getByRole('paragraph')
-          .filter({
-            hasText: label(
-              householdSupplementFormMessage.confirm.overviewTitle,
+      await applicationTest.step('Agree to data providers', async () => {
+        await expect(
+          page.getByRole('heading', {
+            name: label(
+              socialInsuranceAdministrationMessage.pre.externalDataSection,
             ),
           }),
-      ).toBeVisible()
+        ).toBeVisible()
+        await page.getByTestId('agree-to-data-providers').click()
+        await page
+          .getByRole('button', {
+            name: label(
+              socialInsuranceAdministrationMessage.pre.startApplication,
+            ),
+          })
+          .click()
+      })
+
+      await applicationTest.step('Fill in applicant info', async () => {
+        await expect(
+          page.getByRole('heading', {
+            name: label(
+              socialInsuranceAdministrationMessage.info.subSectionTitle,
+            ),
+          }),
+        ).toBeVisible()
+
+        const phoneNumber = page.getByRole('textbox', {
+          name: label(
+            socialInsuranceAdministrationMessage.info.applicantPhonenumber,
+          ),
+        })
+        await phoneNumber.selectText()
+        await phoneNumber.type('6555555')
+        await proceed()
+      })
+
+      await applicationTest.step('Fill in payment information', async () => {
+        await expect(
+          page.getByRole('heading', {
+            name: label(socialInsuranceAdministrationMessage.payment.title),
+          }),
+        ).toBeVisible()
+        // // TODO: Setja aftur inn? eða er í lagi að sleppa? (virkar ekki að senda inn umsókn ef breyti banka, skilar villu "An error occurred while saving bank information.")
+        // const paymentBank = page.getByRole('textbox', {
+        //   name: label(socialInsuranceAdministrationMessage.payment.bank),
+        // })
+        // await paymentBank.selectText()
+        // await paymentBank.type('051226054678')
+        await proceed()
+      })
+
+      await applicationTest.step('Household supplement', async () => {
+        // TODO: What should i select here?
+        await expect(
+          page.getByRole('heading', {
+            name: label(
+              householdSupplementFormMessage.shared.householdSupplement,
+            ),
+          }),
+        ).toBeVisible()
+
+        await page
+          .getByRole('region', {
+            name: label(
+              householdSupplementFormMessage.info.householdSupplementHousing,
+            ),
+          })
+          .getByRole('radio', {
+            name: label(
+              householdSupplementFormMessage.info
+                .householdSupplementHousingOwner,
+            ),
+          })
+          .click()
+
+        await page
+          .getByRole('region', {
+            name: label(
+              householdSupplementFormMessage.info
+                .householdSupplementChildrenBetween18And25,
+            ),
+          })
+          .getByRole('radio', {
+            name: label(socialInsuranceAdministrationMessage.shared.no),
+          })
+          .click()
+        await proceed()
+      })
+
+      await applicationTest.step('Select period', async () => {
+        await expect(
+          page.getByRole('heading', {
+            name: label(socialInsuranceAdministrationMessage.period.title),
+          }),
+        ).toBeVisible()
+        await page.getByTestId('select-period.year').click()
+        await page.keyboard.press('Enter')
+
+        // TODO: Þurfum að skoða þetta betur, getur komið upp að mánuður er ekki gildur
+        await page.getByTestId('select-period.month').click()
+        await page.keyboard.press('ArrowUp')
+        await page.keyboard.press('Enter')
+        await proceed()
+      })
+
+      await applicationTest.step(
+        'Check that additional documents header is visible',
+        async () => {
+          await expect(
+            page.getByRole('heading', {
+              name: label(
+                socialInsuranceAdministrationMessage.fileUpload
+                  .additionalFileTitle,
+              ),
+            }),
+          ).toBeVisible()
+          await proceed()
+        },
+      )
+
+      await applicationTest.step('Write comment', async () => {
+        await expect(
+          page.getByRole('heading', {
+            name: label(
+              socialInsuranceAdministrationMessage.additionalInfo
+                .commentSection,
+            ),
+          }),
+        ).toBeVisible()
+        await page
+          .getByPlaceholder(
+            label(
+              socialInsuranceAdministrationMessage.additionalInfo
+                .commentPlaceholder,
+            ),
+          )
+          .fill(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula malesuada augue, sit amet pulvinar tortor pellentesque at. Nulla facilisi. Nunc vel mi ac mi commodo rhoncus sit amet ut neque.',
+          )
+        await proceed()
+      })
+
+      await applicationTest.step('Submit application', async () => {
+        await expect(
+          page
+            .locator('form')
+            .getByRole('paragraph')
+            .filter({
+              hasText: label(
+                socialInsuranceAdministrationMessage.confirm.overviewTitle,
+              ),
+            }),
+        ).toBeVisible()
+        await page
+          .getByRole('button', {
+            name: label(
+              socialInsuranceAdministrationMessage.confirm.submitButton,
+            ),
+          })
+          .click()
+      })
+
+      await applicationTest.step(
+        'Check that conclusion screen header is visible to view the application',
+        async () => {
+          await expect(
+            page
+              .getByRole('heading', {
+                name: label(
+                  socialInsuranceAdministrationMessage.conclusionScreen
+                    .receivedTitle,
+                ),
+              })
+              .first(),
+          ).toBeVisible()
+        },
+      )
     },
   )
 })
