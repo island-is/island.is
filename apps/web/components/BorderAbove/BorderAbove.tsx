86de6f8b7d370b942b033f66854e306606966757
runarvestmann <runar@stefna.is>
2024-01-08 11:23:37 +0000

    Change slice spacing

--- apps/web/components/BorderAbove/BorderAbove.tsx
+++ apps/web/components/BorderAbove/BorderAbove.tsx
@@ -1,7 +1,12 @@
 import { Box } from '@island.is/island-ui/core'
+import { SLICE_SPACING } from '@island.is/web/constants'
 
 export const BorderAbove = () => {
   return (
-    <Box borderTopWidth="standard" borderColor="standard" paddingBottom={6} />
+    <Box
+      borderTopWidth="standard"
+      borderColor="standard"
+      paddingBottom={SLICE_SPACING}
+    />
   )
 }
