7a58b0e74b0c22c6dbd90d63ce69cd977366af2b
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-01-08 14:43:40 +0000

    fix: missing injection

--- libs/clients/social-insurance-administration/src/lib/apis.ts
+++ libs/clients/social-insurance-administration/src/lib/apis.ts
@@ -3,15 +3,19 @@ import {
   ApplicationApi,
   GeneralApi,
   Configuration,
+  PaymentPlanApi,
 } from '../../gen/fetch'
 import { ApiConfiguration } from './apiConfiguration'
 
-export const exportedApis = [ApplicantApi, ApplicationApi, GeneralApi].map(
-  (Api) => ({
-    provide: Api,
-    useFactory: (configuration: Configuration) => {
-      return new Api(configuration)
-    },
-    inject: [ApiConfiguration.provide],
-  }),
-)
+export const exportedApis = [
+  ApplicantApi,
+  ApplicationApi,
+  GeneralApi,
+  PaymentPlanApi,
+].map((Api) => ({
+  provide: Api,
+  useFactory: (configuration: Configuration) => {
+    return new Api(configuration)
+  },
+  inject: [ApiConfiguration.provide],
+}))
