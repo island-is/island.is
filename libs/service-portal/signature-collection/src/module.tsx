16d051ec37c2e32daa4dd410bcfa27b94b32432d
Alex Diljar <alex@juni.is>
2024-01-10 10:11:25 +0000

    service portal scope

--- libs/service-portal/signature-collection/src/module.tsx
+++ libs/service-portal/signature-collection/src/module.tsx
@@ -3,6 +3,7 @@ import { m } from '@island.is/service-portal/core'
 import { PortalModule, PortalRoute } from '@island.is/portals/core'
 import { SignatureCollectionPaths } from './lib/paths'
 import { Features } from '@island.is/feature-flags'
+import { ApiScope } from '@island.is/auth/scopes'
 
 const SignatureLists = lazy(() => import('./screens/SignatureLists'))
 const ViewList = lazy(() => import('./screens/ViewList'))
@@ -10,16 +11,18 @@ const ViewList = lazy(() => import('./screens/ViewList'))
 export const signatureCollectionModule: PortalModule = {
   name: m.signatureCollectionLists,
   featureFlag: Features.servicePortalSignatureCollection,
-  routes: () => {
+  routes: ({userInfo}) => {
     const applicationRoutes: PortalRoute[] = [
       {
         name: m.signatureCollectionLists,
         path: SignatureCollectionPaths.Lists,
+        enabled: userInfo.scopes.includes(ApiScope.signatureCollection),
         element: <SignatureLists />,
       },
       {
         name: m.signatureCollectionLists,
         path: SignatureCollectionPaths.ViewList,
+        enabled: userInfo.scopes.includes(ApiScope.signatureCollection),
         element: <ViewList />,
       },
     ]
