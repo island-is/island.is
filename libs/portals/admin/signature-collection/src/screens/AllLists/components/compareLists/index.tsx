845b4d987f47310ecdeeb9ab500de1c382a0e6c7
albina <albina@kosmosogkaos.is>
2024-01-11 14:20:50 +0000

    admin unsign functionality

--- libs/portals/admin/signature-collection/src/screens/AllLists/components/compareLists/index.tsx
+++ libs/portals/admin/signature-collection/src/screens/AllLists/components/compareLists/index.tsx
@@ -16,6 +16,7 @@ import { format as formatNationalId } from 'kennitala'
 import { SignatureCollectionSignature } from '@island.is/api/schema'
 import { createFileList, getFileData } from '../../../../lib/utils'
 import { Skeleton } from './skeleton'
+import { useUnsignAdminMutation } from './removeSignatureFromList.generated'
 
 const CompareLists = () => {
   const { formatMessage } = useLocale()
@@ -23,6 +24,7 @@ const CompareLists = () => {
   const [fileList, setFileList] = useState<Array<UploadFile>>([])
   const [uploadResults, setUploadResults] = useState<Array<any>>()
   const [compareMutation, { loading }] = useBulkCompareMutation()
+  const [unSignMutation] = useUnsignAdminMutation()
 
   const compareLists = async (nationalIds: Array<string>) => {
     try {
@@ -38,6 +40,31 @@ const CompareLists = () => {
         setUploadResults(
           res.data?.signatureCollectionBulkCompareSignaturesAllLists,
         )
+      }
+    } catch (e) {
+      toast.error(e.message)
+    }
+  }
+
+  const unSignFromList = async (signatureId: string) => {
+    try {
+      const res = await unSignMutation({
+        variables: {
+          input: {
+            id: signatureId,
+          },
+        },
+      })
+
+      if (res.data) {
+        if (res.data.signatureCollectionUnsignAdmin.success) {
+          toast.success(formatMessage(m.unsignFromList))
+          setUploadResults(
+            uploadResults?.filter((result: SignatureCollectionSignature) => {
+              return result.id !== signatureId
+            }),
+          )
+        }
       }
     } catch (e) {
       toast.error(e.message)
@@ -139,8 +166,13 @@ const CompareLists = () => {
                               </T.Data>
                               <T.Data>{'todo: nafn á lista'}</T.Data>
                               <T.Data style={{ minWidth: '160px' }}>
-                                <Button variant="utility">
-                                  {formatMessage(m.deleteFromList)}
+                                <Button
+                                  variant="utility"
+                                  onClick={() => {
+                                    unSignFromList(result.id)
+                                  }}
+                                >
+                                  {formatMessage(m.unsignFromList)}
                                 </Button>
                               </T.Data>
                             </T.Row>
