7e522f39a965af3ce83599130c888c8039949895
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-01-17 09:42:41 +0000

    fix: first column width

--- libs/service-portal/core/src/components/ScrollableMiddleTable/ScrollableMiddleTable.css.ts
+++ libs/service-portal/core/src/components/ScrollableMiddleTable/ScrollableMiddleTable.css.ts
@@ -32,12 +32,27 @@ export const firstColumn = style({
   left: 0,
   overflow: 'hidden',
   textOverflow: 'ellipsis',
+  width: '140px',
+  ...themeUtils.responsiveStyle({
+    md: {
+      width: theme.spacing[23],
+    },
+  }),
 })
 
 export const lastColumn = style({
   borderLeft: `1px solid ${theme.border.color.blue200}`,
   boxShadow: `-4px 0px 8px -2px ${theme.border.color.blue200}`,
   right: 0,
+})
+
+export const lastColumnSticky = style({
+  position: 'initial',
+  ...themeUtils.responsiveStyle({
+    md: {
+      position: 'sticky',
+    },
+  }),
 })
 
 export const sticky = style({
