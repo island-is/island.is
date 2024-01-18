a6bf688b575b5d143700dc1a5b3af587e81b18d9
lommi <jonorn@gmail.com>
2024-01-18 14:12:56 +0000

    fix: test

--- scripts/ci/_nx-affected-targets.sh
+++ scripts/ci/_nx-affected-targets.sh
@@ -12,10 +12,6 @@ export BASE=${BASE:-main}
 AFFECTED_ALL=${AFFECTED_ALL:-} # Could be used for forcing all projects to be affected (set or create `secret` in GitHub with the name of this variable set to the name of the branch that should be affected, prefixed with the magic string `7913-`)
 BRANCH=${BRANCH:-$GITHUB_HEAD_REF}
 
-if [[ -n "$BRANCH" && -n "$AFFECTED_ALL" && "$AFFECTED_ALL" == "7913-$BRANCH" ]]; then
-  EXTRA_ARGS=""
-else
-  EXTRA_ARGS=(--affected --base "$BASE" --head "$HEAD")
-fi
+EXTRA_ARGS=""
 
 npx nx show projects --withTarget="$1" "${EXTRA_ARGS[@]}" --json | jq -r 'join(", ")'
