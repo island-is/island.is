c1611c28158ba9f18efb90ff0068a2bfae8079ef
lommi <jonorn@gmail.com>
2024-01-18 14:09:05 +0000

    fix: node version

--- scripts/ci/_docker.sh
+++ scripts/ci/_docker.sh
@@ -4,6 +4,11 @@ if [[ -n "${DEBUG:-}" || -n "${CI:-}" ]]; then set -x; fi
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+# To get NODE_VERSION
+PACKAGE_JSON_ROOT="$(cd "$DIR/../../" >/dev/null 2>&1 && pwd)"
+PACKAGE_JSON="$PACKAGE_JSON_ROOT/package.json"
+NODE_VERSION="$(cat $PACKAGE_JSON | jq -r ".engines.node")"
+
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 
@@ -12,6 +17,7 @@ APP_DIST_HOME=$(yarn nx show project $APP | jq ".targets.build.options | .output
 DOCKERFILE=${1:-Dockerfile}
 TARGET=${TARGET:-${2:-'<You need to set a target (e.g. output-local, output-jest)>'}}
 ACTION=${3:-docker_build}
+NODE_VERSION=${cat $PACKAGE_JSON | jq -r '.engines.node'}
 PLAYWRIGHT_VERSION="$(yarn info --json @playwright/test | jq -r '.children.Version')"
 CONTAINER_BUILDER=${CONTAINER_BUILDER:-docker}
 DOCKER_LOCAL_CACHE="${DOCKER_LOCAL_CACHE:-true}"
@@ -19,54 +25,55 @@ DOCKER_LOCAL_CACHE="${DOCKER_LOCAL_CACHE:-true}"
 BUILD_ARGS=()
 
 mkargs() {
-  local local_cache="${1:-local-cache=yes}"
-  BUILD_ARGS=(
-    --platform=linux/amd64
-    --file="${DIR}/$DOCKERFILE"
-    --target="$TARGET"
-    "${PUBLISH_TO_REGISTRY[@]}"
-    --build-arg="APP=${APP}"
-    --build-arg="APP_HOME=${APP_HOME}"
-    --build-arg="APP_DIST_HOME=${APP_DIST_HOME}"
-    -t "${DOCKER_REGISTRY}""${APP}":"${DOCKER_TAG}"
-    --build-arg="PLAYWRIGHT_VERSION=${PLAYWRIGHT_VERSION}"
-  )
-  for extra_arg in ${EXTRA_DOCKER_BUILD_ARGS:-}; do
-    BUILD_ARGS+=("$extra_arg")
-  done
-  if [[ "${local_cache}" =~ local-cache=(yes|y|true) ]] && [[ "${DOCKER_LOCAL_CACHE}" == true ]]; then
-    BUILD_ARGS+=(--cache-from="type=local,src=$PROJECT_ROOT/cache")
-    BUILD_ARGS+=(--cache-from="type=local,src=$PROJECT_ROOT/cache_output")
-  fi
+    local local_cache="${1:-local-cache=yes}"
+    BUILD_ARGS=(
+        --platform=linux/amd64
+        --file="${DIR}/$DOCKERFILE"
+        --target="$TARGET"
+        "${PUBLISH_TO_REGISTRY[@]}"
+        --build-arg="APP=${APP}"
+        --build-arg="NODE_VERSION=${NODE_VERSION}"
+        --build-arg="APP_HOME=${APP_HOME}"
+        --build-arg="APP_DIST_HOME=${APP_DIST_HOME}"
+        -t "${DOCKER_REGISTRY}""${APP}":"${DOCKER_TAG}"
+        --build-arg="PLAYWRIGHT_VERSION=${PLAYWRIGHT_VERSION}"
+    )
+    for extra_arg in ${EXTRA_DOCKER_BUILD_ARGS:-}; do
+        BUILD_ARGS+=("$extra_arg")
+    done
+    if [[ "${local_cache}" =~ local-cache=(yes|y|true) ]] && [[ "${DOCKER_LOCAL_CACHE}" == true ]]; then
+        BUILD_ARGS+=(--cache-from="type=local,src=$PROJECT_ROOT/cache")
+        BUILD_ARGS+=(--cache-from="type=local,src=$PROJECT_ROOT/cache_output")
+    fi
 }
 
 container_build() {
-  $CONTAINER_BUILDER buildx build "${BUILD_ARGS[@]}" "$PROJECT_ROOT"
+    $CONTAINER_BUILDER buildx build "${BUILD_ARGS[@]}" "$PROJECT_ROOT"
 }
 
 docker_build() {
-  mkargs local-cache=true
-  container_build docker
+    mkargs local-cache=true
+    container_build docker
 }
 
 _set_publish() {
-  case $PUBLISH in
-  true)
-    PUBLISH_TO_REGISTRY=(--push)
-    ;;
-  local)
-    PUBLISH_TO_REGISTRY=(--load)
-    ;;
-  *)
-    # Just build the container but do not publish it to the registry
-    PUBLISH_TO_REGISTRY=()
-    ;;
-  esac
+    case $PUBLISH in
+        true)
+            PUBLISH_TO_REGISTRY=(--push)
+        ;;
+        local)
+            PUBLISH_TO_REGISTRY=(--load)
+        ;;
+        *)
+            # Just build the container but do not publish it to the registry
+            PUBLISH_TO_REGISTRY=()
+        ;;
+    esac
 }
 
 main() {
-  # Support overriding docker_build
-  eval "${ACTION}"
+    # Support overriding docker_build
+    eval "${ACTION}"
 }
 
 # Exit if not directly run
