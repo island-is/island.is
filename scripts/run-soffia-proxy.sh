e2ee5965fe98034ea5cdb66e5f780215ed506f97
Kristófer <kristofer@andes.is>
2024-01-22 15:47:20 +0000

    Point all old proxies to new merged proxy script

--- scripts/run-soffia-proxy.sh
+++ scripts/run-soffia-proxy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-
 # This scripts sets up a local proxy for X-Road running in our Dev env
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-"$DIR"/_run-aws-eks-commands.js proxy --namespace socat --service socat-soffia --port 443 --proxy-port 8443 "$@"
+./scripts/run-proxies.sh soffia
