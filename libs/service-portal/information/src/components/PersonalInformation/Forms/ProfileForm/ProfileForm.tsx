0316c7e517986305bbed742a18f5c3ea712842dc
snaerseljan <snaer@aranja.com>
2024-01-23 10:58:03 +0000

    fix(service-portal): Return url fix

--- libs/service-portal/information/src/components/PersonalInformation/Forms/ProfileForm/ProfileForm.tsx
+++ libs/service-portal/information/src/components/PersonalInformation/Forms/ProfileForm/ProfileForm.tsx
@@ -98,8 +98,12 @@ export const ProfileForm: FC<React.PropsWithChildren<Props>> = ({
    * By setting the continue_onboarding to false, the user won´t be forced to finish the onboarding.
    */
   const getIDSLink = (linkPath: IdsUserProfileLinks) => {
+    const searchParams = new URLSearchParams(window.location.search)
+    searchParams.set('continue_onboarding', 'false')
     const returnUrl = encodeURIComponent(
-      `${window.location}&continue_onboarding=false`,
+      `${window.location.protocol}//${window.location.host}${
+        window.location.pathname
+      }?${searchParams.toString()}`,
     )
 
     return `${authority}${linkPath}?returnUrl=${returnUrl}`
