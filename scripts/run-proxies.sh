cc4de7eea7306d6c277d10094d49a4978084a8d8
Róberta Andersen <roberta@andes.is>
2024-01-25 11:27:26 +0000

    fix: Removing unused functions, unbound variables inlined, args processing

--- scripts/run-proxies.sh
+++ scripts/run-proxies.sh
@@ -10,7 +10,6 @@ if [[ -n "${DEBUG:-}" || -n "${CI:-}" ]]; then set -x; fi
 : "${RESTART_MAX_RETRIES:=3}"
 : "${DRY:=}"
 
-ARGS=()
 PROXIES=()
 PROXY_PIDS=()
 
@@ -106,11 +105,6 @@ parse_cli() {
       DRY=true
       ;;
 
-    --)
-      shift
-      ARGS+=("$@")
-      break
-      ;;
     -h | --help)
       print_usage
       exit 0
@@ -122,7 +116,6 @@ parse_cli() {
     shift
   done
   # echo "DEBUG: PROXIES=${PROXIES[*]}"
-  # echo "DEBUG: ARGS=${ARGS[*]}"
   # echo "DEBUG: REMOVE_CONTAINERS_ON_START=${REMOVE_CONTAINERS_ON_START}"
   # echo "DEBUG: REMOVE_CONTAINERS_ON_FAIL=${REMOVE_CONTAINERS_ON_FAIL}"
   # echo "DEBUG: REMOVE_CONTAINERS_FORCE=${REMOVE_CONTAINERS_FORCE}"
@@ -133,22 +126,17 @@ parse_cli() {
     return
   fi
 
-  local unknown_proxies
-  unknown_proxies=()
   for proxy in "${PROXIES[@]}"; do
     if ! [[ "$proxy" =~ ^(es|soffia|xroad|redis|db)$ ]]; then
-      unknown_proxies+=("$proxy")
+      # Exit with error if there are unknown proxies
+      echo "Unknown proxies: $proxy"
+      exit 1
     fi
   done
-  # Exit with error if there are unknown proxies
-  if [[ "${unknown_proxies[*]}" != "" ]]; then
-    echo "Unknown proxies: '${unknown_proxies[*]}'"
-    exit 1
-  fi
 }
 
 run-proxy() {
-    local service_port="${1}"
+  local service_port="${1}"
   local host_port="${2}"
   local service_name="${3}"
   local service
@@ -186,10 +174,10 @@ run-proxy() {
 
   cmd "$DIR"/_run-aws-eks-commands.js proxy \
   --namespace "${namespace}" \
-     --service "${service}" \
-     --port "${service_port}" \
-     --proxy-port "${host_port}" \
-     --cluster "${CLUSTER:-dev-cluster01}"
+  --service "${service}" \
+  --port "${service_port}" \
+  --proxy-port "${host_port}" \
+  --cluster "${CLUSTER:-dev-cluster01}"
 }
 run-db-proxy() {
   run-proxy 5432 5432 db
@@ -209,22 +197,6 @@ run-xroad-proxy() {
 
 parse_cli "$@"
 
-loop_proxy() {
-  local proxy="${1}"
-  local container_name="socat-$proxy"
-  [ "$proxy" == "es" ] && container_name="es-proxy"
-
-  while true; do
-    run-"${proxy}"-proxy "${ARGS[@]}" || echo "Exit code for $proxy proxy: $?"
-    if [ -n "${REMOVE_CONTAINERS_ON_FAIL:-}" ]; then
-      echo "Removing container $container_name on fail..."
-      containerer rm ${REMOVE_CONTAINERS_FORCE:+-f} "$container_name"
-    fi
-    echo "Restarting $proxy proxy in $RESTART_INTERVAL_TIME seconds..."
-    sleep "$RESTART_INTERVAL_TIME"
-  done
-}
-
 main() {
   PROXY_PIDS=()
   for proxy in "${PROXIES[@]}"; do
@@ -241,7 +213,7 @@ main() {
     echo "Starting $proxy proxy"
     (
       for ((i = 1; i <= RESTART_MAX_RETRIES; i++)); do
-        run-"${proxy}"-proxy "${ARGS[@]-}" || echo "Exit code for $proxy proxy: $?"
+        run-"${proxy}"-proxy || echo "Exit code for $proxy proxy: $?"
         if [ -n "${REMOVE_CONTAINERS_ON_FAIL:-}" ]; then
         echo "Removing container $container_name on fail..."
         containerer rm ${REMOVE_CONTAINERS_FORCE:+-f} "$container_name"
