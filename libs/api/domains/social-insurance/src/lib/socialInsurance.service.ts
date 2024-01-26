622e251b0058bb75e9ae0c8e59630376b8a94759
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-01-26 10:13:52 +0000

    feat: add mocks

--- libs/api/domains/social-insurance/src/lib/socialInsurance.service.ts
+++ libs/api/domains/social-insurance/src/lib/socialInsurance.service.ts
@@ -20,81 +20,191 @@ export class SocialInsuranceService {
     user: User,
     year?: number,
   ): Promise<PaymentPlan | undefined> {
-    const [paymentPlan, payments] = await Promise.all([
-      this.socialInsuranceApi
-        .getPaymentPlan(user, year ?? addYears(new Date(), -1).getFullYear())
-        .catch(handle404),
-      this.socialInsuranceApi.getPayments(user).catch(handle404),
-    ])
-
-    if (!paymentPlan && !payments) {
-      return undefined
-    }
-
-    const paymentGroups: Array<PaymentGroup> =
-      paymentPlan?.paymentPlan?.groups
-        ?.map((g) => {
-          if (!g.group) {
-            return null
-          }
-          return {
-            type: g.group,
-            totalYearCumulativeAmount: g.monthTotals?.reduce(
-              (total, current) => (total += current.amount ?? 0),
-              0,
-            ),
-            monthlyPaymentHistory:
-              g.monthTotals
-                ?.map((mt) => {
-                  if (!mt.month) {
-                    return null
-                  }
-                  return {
-                    monthIndex: mt.month,
-                    amount: mt.amount ?? undefined,
-                  }
-                })
-                .filter(isDefined) ?? [],
-            payments:
-              g.rows
-                ?.map((r) => {
-                  if (!r.name) {
-                    return null
-                  }
-                  return {
-                    type: r.name,
-                    totalYearCumulativeAmount: r.months?.reduce(
-                      (total, current) => (total += current.amount ?? 0),
-                      0,
-                    ),
-                    monthlyPaymentHistory:
-                      r.months
-                        ?.map((m) => {
-                          if (!m.month) {
-                            return null
-                          }
-                          return {
-                            monthIndex: m.month,
-                            amount: m.amount ?? undefined,
-                          }
-                        })
-                        .filter(isDefined) ?? [],
-                  }
-                })
-                .filter(isDefined) ?? [],
-          }
-        })
-        .filter(isDefined) ?? []
-
     const data = {
-      nextPayment: payments?.nextPayment ?? undefined,
-      previousPayment: payments?.previousPayment ?? undefined,
-      paymentGroups: paymentGroups,
-    }
-
-    //if no data
-    if (!data.nextPayment && !data.previousPayment && !paymentGroups.length) {
-      return undefined
+      nextPayment: 186954,
+      previousPayment: 12000,
+      paymentGroups: [
+        {
+          type: 'Greiðslugreiðar',
+          totalYearCumulativeAmount: 18990,
+          payments: [
+            {
+              type: 'Greiður',
+              totalYearCumulativeAmount: 1111,
+              monthlyPaymentHistory: [
+                {
+                  monthIndex: 3,
+                  amount: 1000,
+                },
+                {
+                  monthIndex: 8,
+                  amount: 111,
+                },
+              ],
+            },
+            {
+              type: 'Tryggingar',
+              totalYearCumulativeAmount: 199999,
+              monthlyPaymentHistory: [
+                {
+                  monthIndex: 10,
+                  amount: 1000,
+                },
+                {
+                  monthIndex: 2,
+                  amount: 111,
+                },
+                {
+                  monthIndex: 4,
+                  amount: 198988,
+                },
+              ],
+            },
+            {
+              type: 'eitthvað',
+              totalYearCumulativeAmount: 500000,
+              monthlyPaymentHistory: [
+                {
+                  monthIndex: 2,
+                  amount: 250,
+                },
+                {
+                  monthIndex: 3,
+                  amount: 250,
+                },
+                {
+                  monthIndex: 7,
+                  amount: 250000,
+                },
+                {
+                  monthIndex: 11,
+                  amount: 249500,
+                },
+              ],
+            },
+          ],
+          monthlyPaymentHistory: [
+            {
+              monthIndex: 2,
+              amount: 18990,
+            },
+          ],
+        },
+        {
+          type: 'Frádráttur',
+          totalYearCumulativeAmount: 400000,
+          payments: [
+            {
+              type: 'Borga borga',
+              totalYearCumulativeAmount: 40000,
+              monthlyPaymentHistory: [
+                {
+                  monthIndex: 2,
+                  amount: 40000,
+                },
+                {
+                  monthIndex: 12,
+                  amount: 360001,
+                },
+              ],
+            },
+          ],
+          monthlyPaymentHistory: [
+            {
+              monthIndex: 2,
+              amount: 5,
+            },
+          ],
+        },
+        {
+          type: 'Flokkur 2',
+          totalYearCumulativeAmount: 999999,
+          payments: [
+            {
+              type: 'Bingó',
+              totalYearCumulativeAmount: 1111,
+              monthlyPaymentHistory: [
+                {
+                  monthIndex: 3,
+                  amount: 1000,
+                },
+                {
+                  monthIndex: 8,
+                  amount: 111,
+                },
+              ],
+            },
+            {
+              type: 'Bangó',
+              totalYearCumulativeAmount: 199999,
+              monthlyPaymentHistory: [
+                {
+                  monthIndex: 10,
+                  amount: 1000,
+                },
+                {
+                  monthIndex: 2,
+                  amount: 111,
+                },
+                {
+                  monthIndex: 4,
+                  amount: 198988,
+                },
+              ],
+            },
+          ],
+          monthlyPaymentHistory: [
+            {
+              monthIndex: 1,
+              amount: 5000,
+            },
+            {
+              monthIndex: 2,
+              amount: 12345,
+            },
+            {
+              monthIndex: 3,
+              amount: 56789,
+            },
+            {
+              monthIndex: 4,
+              amount: 13689,
+            },
+            {
+              monthIndex: 5,
+              amount: 572,
+            },
+            {
+              monthIndex: 6,
+              amount: 1,
+            },
+            {
+              monthIndex: 7,
+              amount: 8079,
+            },
+            {
+              monthIndex: 8,
+              amount: 1863,
+            },
+            {
+              monthIndex: 9,
+              amount: 32,
+            },
+            {
+              monthIndex: 10,
+              amount: 7654,
+            },
+            {
+              monthIndex: 11,
+              amount: 13261,
+            },
+            {
+              monthIndex: 12,
+              amount: 9743,
+            },
+          ],
+        },
+      ],
     }
 
     return data
