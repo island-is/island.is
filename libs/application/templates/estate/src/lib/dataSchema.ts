ca307dfc2685d4aeb4d02e637042b688cea4d85f
helgi helgason <78020509+HelgiHH@users.noreply.github.com>
2024-01-29 10:34:53 +0000

    fix(estate): add default value to autonomous radio (#13586)

--- libs/application/templates/estate/src/lib/dataSchema.ts
+++ libs/application/templates/estate/src/lib/dataSchema.ts
@@ -74,7 +74,7 @@ export const estateSchema = z.object({
     email: customZodError(z.string().email(), m.errorEmail),
     address: z.string(),
     relationToDeceased: z.string().optional(),
-    autonomous: z.enum([YES, NO]),
+    autonomous: z.enum([YES, NO]).optional(),
   }),
 
   selectedEstate: z.enum([
