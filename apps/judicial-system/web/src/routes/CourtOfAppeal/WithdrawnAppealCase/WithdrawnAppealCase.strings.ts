03323e498a829613a3e757419340736c5cb08b25
unakb <una@kolibri.is>
2024-01-31 13:56:31 +0000

    feat(j-s): New page for withdrawn appeal case

---
+++ apps/judicial-system/web/src/routes/CourtOfAppeal/WithdrawnAppealCase/WithdrawnAppealCase.strings.ts
@@ -0,0 +1,42 @@
+import { defineMessages } from 'react-intl'
+
+export const strings = defineMessages(
+  {
+    title: {
+      id: 'judicial.system.withdrawn_appeal_case:title',
+      defaultMessage: 'Niðurfelling máls',
+      description: 'Notaður sem titill á niðurfellingarmála síðu',
+    },
+    uploadBoxTitle: {
+      id: 'judicial.system.withdrawn_appeal_case:upload_box_title',
+      defaultMessage: 'Dragðu gögn hingað til að hlaða upp',
+      description:
+        'Notaður sem titill á drag and drop boxi á niðurfellingarmála síðu',
+    },
+    uploadBoxButtonLabel: {
+      id: 'judicial.system.withdrawn_appeal_case:upload_box_button_label',
+      defaultMessage: 'Velja gögn til að hlaða upp',
+      description:
+        'Notaður sem titill á upload takka á niðurfellingarmála síðu',
+    },
+    courtRecordFileTitle: {
+      id: 'judicial.system.withdrawn_appeal_case:court_record',
+      defaultMessage: 'Þingbók',
+      description: 'Notaður sem titill á þingbók á niðurfellingarmála síðu',
+    },
+    courtRecordFileSubtitle: {
+      id: 'judicial.system.withdrawn_appeal_case:court_record_subtitle',
+      defaultMessage:
+        'Ef úrskurðað var um málskostnað er hægt að hlaða upp þingbók sem deilt verður með verjanda. (og fleirum?)',
+      description:
+        'Notaður sem titill á þingbók máls á niðurfellingarmála síðu',
+    },
+    continueButton: {
+      id: 'judicial.system.withdrawn_appeal_case:continue_button',
+      defaultMessage: 'Halda áfram',
+      description: 'Notaður sem texti á takka til að halda áfram',
+    },
+  },
+
+  // Add more strings here
+)
---
+++ apps/judicial-system/web/src/routes/CourtOfAppeal/WithdrawnAppealCase/WithdrawnAppealCase.tsx
@@ -0,0 +1,102 @@
+import React, { useContext } from 'react'
+import { useIntl } from 'react-intl'
+import { useRouter } from 'next/router'
+
+import { Box, InputFileUpload, Text } from '@island.is/island-ui/core'
+import * as constants from '@island.is/judicial-system/consts'
+import { core, titles } from '@island.is/judicial-system-web/messages'
+import {
+  FormContentContainer,
+  FormContext,
+  FormFooter,
+  PageHeader,
+  PageLayout,
+  SectionHeading,
+} from '@island.is/judicial-system-web/src/components'
+import { CaseFileCategory } from '@island.is/judicial-system-web/src/graphql/schema'
+import {
+  useS3Upload,
+  useUploadFiles,
+} from '@island.is/judicial-system-web/src/utils/hooks'
+
+import CaseNumbers from '../components/CaseNumbers/CaseNumbers'
+import { strings } from './WithdrawnAppealCase.strings'
+
+const WithdrawnAppealCase = () => {
+  const { workingCase } = useContext(FormContext)
+  const { formatMessage } = useIntl()
+  const router = useRouter()
+  const {
+    uploadFiles,
+    allFilesUploaded,
+    addUploadFiles,
+    updateUploadFile,
+    removeUploadFile,
+  } = useUploadFiles(workingCase.caseFiles)
+  const { handleUpload, handleRetry, handleRemove } = useS3Upload(
+    workingCase.id,
+  )
+
+  const previousUrl = `${constants.COURT_OF_APPEAL_CASES_ROUTE}`
+
+  const isStepValid = allFilesUploaded
+
+  return (
+    <PageLayout workingCase={workingCase} isLoading={false} notFound={false}>
+      <PageHeader title={formatMessage(titles.shared.appealToCourtOfAppeals)} />
+      <FormContentContainer>
+        <Box marginBottom={2}>
+          <Text as="h1" variant="h1">
+            {formatMessage(strings.title)}
+          </Text>
+        </Box>
+        <Box marginBottom={2}>
+          <CaseNumbers />
+        </Box>
+
+        <Box component="section">
+          <SectionHeading
+            title={formatMessage(strings.courtRecordFileTitle)}
+            marginBottom={1}
+          />
+          <Text marginBottom={3}>
+            {formatMessage(strings.courtRecordFileSubtitle)}
+            {'\n'}
+          </Text>
+          <InputFileUpload
+            fileList={uploadFiles.filter(
+              (file) => file.category === CaseFileCategory.APPEAL_COURT_RECORD,
+            )}
+            accept={'application/pdf'}
+            header={formatMessage(strings.uploadBoxTitle)}
+            description={formatMessage(core.uploadBoxDescription, {
+              fileEndings: '.pdf',
+            })}
+            buttonLabel={formatMessage(strings.uploadBoxButtonLabel)}
+            onChange={(files) =>
+              handleUpload(
+                addUploadFiles(files, CaseFileCategory.APPEAL_COURT_RECORD),
+                updateUploadFile,
+              )
+            }
+            onRemove={(file) => handleRemove(file, removeUploadFile)}
+            onRetry={(file) => handleRetry(file, updateUploadFile)}
+          />
+        </Box>
+      </FormContentContainer>
+      <FormContentContainer isFooter>
+        <FormFooter
+          previousUrl={previousUrl}
+          onNextButtonClick={async () => {
+            router.push(previousUrl)
+          }}
+          nextButtonText={formatMessage(strings.continueButton)}
+          nextIsDisabled={!isStepValid}
+          nextButtonIcon="arrowForward"
+        />
+      </FormContentContainer>
+    </PageLayout>
+  )
+}
+
+export default WithdrawnAppealCase
