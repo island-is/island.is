5759864f62dbab2cb02708be2f70dc608bbc6040
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-01-31 11:57:43 +0000

    updating description on api

--- apps/financial-aid/open-api/src/openApi.ts
+++ apps/financial-aid/open-api/src/openApi.ts
@@ -2,7 +2,9 @@ import { DocumentBuilder } from '@nestjs/swagger'
 
 export const openApi = new DocumentBuilder()
   .setTitle('Finanical Aid API')
-  .setDescription('This is the API for the financial aid.')
+  .setDescription(
+    'This is an API for municipalities to access applications for filing systems',
+  )
   .setVersion('1.0')
   .addTag('financial-aid')
   .build()
