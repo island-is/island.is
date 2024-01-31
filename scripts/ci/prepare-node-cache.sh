182d49d3186d8aea12fa1d831fd2c3b613775226
Jón Levy <levy@andes.is>
2024-01-31 10:16:34 +0000

    ci: cleanup

--- scripts/ci/prepare-node-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-GIT_ROOT=$(git rev-parse --show-toplevel)
-cat "${GIT_ROOT}/package.json" | jq '{resolutions,dependencies,devDependencies}' | sha1sum -t | cut -f1 -d" "
