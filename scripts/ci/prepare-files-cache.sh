2ee1c76f41b4df029bdf9e81f47e0191050df625
Jón Levy <levy@andes.is>
2024-01-31 07:59:12 +0000

    ci: revert cache key step for generated files

--- scripts/ci/prepare-files-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-GIT_ROOT=$(git rev-parse --show-toplevel)
-FILES_LIST=$(cat "${GIT_ROOT}/.cache-generating-files" | xargs printf "'%s'," | sed 's/,$//')
-echo "${FILES_LIST}"
