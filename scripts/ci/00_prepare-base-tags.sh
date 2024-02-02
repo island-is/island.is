3022b8a082332ee6a7421926b946a9bd6c22ba11
Kristófer <kristofer@andes.is>
2024-02-02 15:58:04 +0000

    Adapt prepare-base-tags

--- scripts/ci/00_prepare-base-tags.sh
+++ scripts/ci/00_prepare-base-tags.sh
@@ -17,19 +17,25 @@ set -euxo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT="$DIR/../.."
+: "${HEAD_REF:="$GITHUB_HEAD_REF"}"
+: "${BASE_REF:="$GITHUB_BASE_REF"}"
+: "${PR_REF:="$GITHUB_SHA"}"
+: "${SHELL:=/usr/bin/bash}"
+: "${WORKFLOW_ID:=pullrequest}"
+export HEAD_REF BASE_REF PR_REF SHELL WORKFLOW_ID
 
 tempRepo=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 cp -r "$ROOT/.github/actions/dist/." "$tempRepo"
 
 LAST_GOOD_BUILD=$(DEBUG="*,-simple-git" REPO_ROOT="$ROOT" node "$tempRepo/main.js")
-echo "Stickman done"
+echo >&2 "Stickman done"
 LAST_GOOD_BUILD_SHA=$(echo "$LAST_GOOD_BUILD" | jq -r '.sha')
 LAST_GOOD_BUILD_BRANCH=$(echo "$LAST_GOOD_BUILD" | jq -r '.branch')
 LAST_GOOD_BUILD_RUN_NUMBER=$(echo "$LAST_GOOD_BUILD" | jq -r '.run_number')
 BUILD_REF=$(echo "$LAST_GOOD_BUILD" | jq -r '.ref')
 echo >&2 "Last successful build is with SHA '$LAST_GOOD_BUILD_SHA', branch '$LAST_GOOD_BUILD_BRANCH' and number '$LAST_GOOD_BUILD_RUN_NUMBER'"
 if [[ "$BUILD_REF" != "$LAST_GOOD_BUILD_SHA" ]]; then
-  echo "This will be an incremental build from a previous successful run in this PR. See parents of the commit below."
+  echo >&2 "This will be an incremental build from a previous successful run in this PR. See parents of the commit below."
   git log -1 "$BUILD_REF"
 fi
 LAST_GOOD_BUILD_DOCKER_BRANCH_TAG=$(echo "${LAST_GOOD_BUILD_BRANCH}" | tr "/." "-")
@@ -40,5 +46,8 @@ if [[ "$BUILD_REF" == "null" || "$BUILD_REF" == "" ]]; then
 else
   BASE="$BUILD_REF"
 fi
-export BASE
+
+export HEAD BASE
 echo >&2 "Last successful docker tag '$LAST_GOOD_BUILD_DOCKER_TAG'"
+echo "HEAD=$GITHUB_SHA" >>"$GITHUB_ENV"
+echo "BASE=$BASE" >>"$GITHUB_ENV"
