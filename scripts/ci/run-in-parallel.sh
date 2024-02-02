f2f31f37c1c4f52e73520955097f931c88fe82c3
Jón Levy <levy@andes.is>
2024-02-02 10:12:31 +0000

    ci: fix run-in-parallel.sh

--- scripts/ci/run-in-parallel.sh
+++ scripts/ci/run-in-parallel.sh
@@ -6,16 +6,14 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck disable=SC1091
 source "$DIR"/_common.sh
 MAX_JOBS=${MAX_JOBS:-2}
-AFFECTED_PROJECTS=$(echo "${AFFECTED_PROJECTS}" | tr '\n,' ' ')
+
+AFFECTED_PROJECTS=$(./scripts/ci/generate-chunks.sh test)
+
+# Parse the JSON array to get a space-separated list of project names
+AFFECTED_PROJECTS=$(echo "${AFFECTED_PROJECTS}" | jq -r '.[]' | tr ',' '\n')
+
 echo "Running '$AFFECTED_PROJECTS' in parallel of ${MAX_JOBS} for target $1"
 
-IFS=" " read -ra AFFECTED_PROJECTS <<<"$AFFECTED_PROJECTS"
-
-if [ -n "$AFFECTED_PROJECTS" ]; then
-  echo "Affected projects for target $1 are '$AFFECTED_PROJECTS'"
-  yarn run nx run-many --target="$1" --projects="$AFFECTED_PROJECTS" --parallel=5 --maxParallel="$MAX_JOBS"
-else
-  echo "No affected projects for target $1"
-fi
-# This is a helper script to run in parallel a list of provided projects for a specific target
-# exec parallel --halt soon,fail=1 --lb -j "$MAX_JOBS" APP={} "$DIR"/"$1".sh ::: "${AFFECTED_PROJECTS[@]}"
+# Now AFFECTED_PROJECTS should be a clean, space-separated list of project names
+echo "$AFFECTED_PROJECTS" | parallel --halt soon,fail=1 --lb -j "$MAX_JOBS" APP={} "$DIR"/"$1".sh
+exec parallel --halt soon,fail=1 --lb -j "$MAX_JOBS" APP={} "$DIR"/"$1".sh ::: "${AFFECTED_PROJECTS[@]}"
