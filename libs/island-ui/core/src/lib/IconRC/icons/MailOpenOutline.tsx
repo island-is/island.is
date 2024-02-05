dc9a7cbd88ffe0e033ab97335cd76c1b60d5fe79
Þórður Hafliðason <tolleinn@gmail.com>
2024-02-05 13:26:04 +0000

    Remove classname

--- libs/island-ui/core/src/lib/IconRC/icons/MailOpenOutline.tsx
+++ libs/island-ui/core/src/lib/IconRC/icons/MailOpenOutline.tsx
@@ -7,12 +7,7 @@ const SvgMailOpenOutline = ({
   ...props
 }: React.SVGProps<SVGSVGElement> & SVGRProps) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="ionicon"
-      viewBox="0 0 512 512"
-      {...props}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
       {title ? <title id={titleId}>{title}</title> : null}
       <path
         fill="none"
