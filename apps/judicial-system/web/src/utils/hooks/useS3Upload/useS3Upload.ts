b86503837a004a4a28207b680394836eb8c26eed
Ívar Oddsson <ivaroddsson@gmail.com>
2024-02-05 15:34:50 +0000

    Remove debug code

--- apps/judicial-system/web/src/utils/hooks/useS3Upload/useS3Upload.ts
+++ apps/judicial-system/web/src/utils/hooks/useS3Upload/useS3Upload.ts
@@ -273,9 +273,6 @@ const useS3Upload = (caseId: string) => {
 
       for (const file of files) {
         try {
-          if (file.name === 'skirteini.pdf') {
-            throw Error('Failed to upload file to S3')
-          }
           const presignedPost = await getPresignedPost(file)
 
           await uploadToS3(file, presignedPost, (percent) => {
