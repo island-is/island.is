176ddf2eea76c5bc9ca09c74302e91805b329652
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-02-06 10:48:45 +0000

    feat(fa): Allowing admins to create and update api keys for api  (#13689)
    * adding sortable feature
    
    * Revert "adding sortable feature"
    
    This reverts commit d9691c54de2c2fb244cf89e49c0bfe6cd7857603.
    
    * adding municipality api user table
    
    * wip
    
    * api works running locally
    
    * making get request
    
    * gets all applications
    
    * adding api keys to header and guards
    
    * adding municode to request
    
    * upating table
    
    * removing console log
    
    * searching by api key
    
    * making dates ready
    
    * adding start date and enddate
    
    * clean up
    
    * more clean up
    
    * updating description on api
    
    * adding package
    
    * making connection between veita and backend for api keys
    
    * allowing admins to see name and key
    
    * allowing veita to create api key
    
    * adding copy button for api key
    
    * working on updating key
    
    * allowing user to update key
    
    * decription key
    
    * making updating settings into modal
    
    * uðdating design
    
    * muni not updating..wip
    
    * reafactoring adding hooks to settings
    
    * refactoring code and adding decryption to update
    
    * updating spelling mistakes

---
+++ apps/financial-aid/api/src/app/modules/apiKeys/apiKey.resolver.ts
@@ -0,0 +1,52 @@
+import { Query, Resolver, Context, Mutation, Args } from '@nestjs/graphql'
+
+import { Inject, UseGuards } from '@nestjs/common'
+
+import type { Logger } from '@island.is/logging'
+import { LOGGER_PROVIDER } from '@island.is/logging'
+
+import { BackendAPI } from '../../../services'
+
+import { ApiKeysModel } from './models'
+
+import { IdsUserGuard } from '@island.is/auth-nest-tools'
+import type { ApiKeysForMunicipality } from '@island.is/financial-aid/shared/lib'
+import { CreateApiKeyInput, UpdateApiKeyInput } from './dto'
+
+@UseGuards(IdsUserGuard)
+@Resolver(() => ApiKeysModel)
+export class ApiKeysResolver {
+  constructor(
+    @Inject(LOGGER_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
+
+  @Query(() => [ApiKeysModel], { nullable: false })
+  apiKeysForMunicipality(
+    @Context('dataSources') { backendApi }: { backendApi: BackendAPI },
+  ): Promise<ApiKeysForMunicipality[]> {
+    this.logger.debug(`Getting municipalities by ids`)
+    return backendApi.getApiKeys()
+  }
+
+  @Mutation(() => ApiKeysModel, { nullable: false })
+  createApiKey(
+    @Args('input', { type: () => CreateApiKeyInput })
+    input: CreateApiKeyInput,
+    @Context('dataSources') { backendApi }: { backendApi: BackendAPI },
+  ): Promise<ApiKeysForMunicipality> {
+    this.logger.debug('Creating api key for municipality')
+    return backendApi.createApiKey(input)
+  }
+
+  @Mutation(() => ApiKeysModel, { nullable: false })
+  updateApiKey(
+    @Args('input', { type: () => UpdateApiKeyInput })
+    input: UpdateApiKeyInput,
+    @Context('dataSources') { backendApi }: { backendApi: BackendAPI },
+  ): Promise<ApiKeysModel> {
+    this.logger.debug('Updating api key name')
+
+    return backendApi.updateApiKey(input)
+  }
+}
