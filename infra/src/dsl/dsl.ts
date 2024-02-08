38939d800082b163ce584a97185541c0ce4f4453
Kristófer <kristofer@andes.is>
2024-02-08 12:45:27 +0000

    Add custom extensions to db()

--- infra/src/dsl/dsl.ts
+++ infra/src/dsl/dsl.ts
@@ -358,7 +358,7 @@ export class ServiceBuilder<ServiceType extends string> {
       passwordSecret:
         pg.passwordSecret ?? `/k8s/${this.serviceDef.name}/DB_PASSWORD`,
       name: pg.name ?? postgresIdentifier(this.serviceDef.name),
-      extensions: this.serviceDef.initContainers?.postgres?.extensions,
+      extensions: [...pg.extensions ?? [], ...this.serviceDef.initContainers?.postgres?.extensions ?? []],
     }
   }
 }
