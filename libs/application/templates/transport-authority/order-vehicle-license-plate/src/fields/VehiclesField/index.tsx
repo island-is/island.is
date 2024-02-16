9219957bdfd60c26bfcd294802e586b63fdce26b
davidkef <52493597+davidkef@users.noreply.github.com>
2024-02-16 09:02:24 +0000

    fix(SGS Applications): Fixing bug when selecting vehicles is slow if too many cars are owned (#13786)
    * initial commit
    
    * Fixed the UI for the searching for specific vehicle by plate and functionality,
    still needs some error handling
    
    * Adding not found error
    
    * Fix for too many vehicles in some forms,
    not ready
    
    * Added fix for rest of the forms, still need to look into order-vehiucle-license-plate and order-vehicle-registration-certificate,
    as the API for SGS wasn't working
    
    * Fixing rest of SGS applications
    
    * cleaning up console logs
    
    * Resolving PR comments
    
    * Adding a shared FindVehicle Component
    
    * removing unused
    
    * removing console.logs
    
    * Using field.Id in the FindVehicleFormField
    
    * fixing based on PR comment, cleaning up FindVehicleFormField
    
    * chore: nx format:write update dirty files
    
    * changing vlaue for FindVehicleFormFiled
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>

--- libs/application/templates/transport-authority/order-vehicle-license-plate/src/fields/VehiclesField/index.tsx
+++ libs/application/templates/transport-authority/order-vehicle-license-plate/src/fields/VehiclesField/index.tsx
@@ -1,25 +1,74 @@
-import { FieldBaseProps } from '@island.is/application/types'
+import {
+  FieldBaseProps,
+  FieldComponents,
+  FieldTypes,
+} from '@island.is/application/types'
 import { Box } from '@island.is/island-ui/core'
 import { FC } from 'react'
-import { VehiclesCurrentVehicle } from '../../shared'
-import { VehicleSelectField } from './VehicleSelectField'
+import { CurrentVehiclesAndRecords } from '../../shared'
 import { VehicleRadioField } from './VehicleRadioField'
+import { FindVehicleFormField } from '@island.is/application/ui-fields'
+import { information, error } from '../../lib/messages'
+import { useLazyVehicleDetails } from '../../hooks/useLazyVehicleDetails'
+import { ApolloQueryResult } from '@apollo/client'
+import { VehicleSelectField } from './VehicleSelectField'
 
 export const VehiclesField: FC<React.PropsWithChildren<FieldBaseProps>> = (
   props,
 ) => {
   const { application } = props
   const currentVehicleList = application.externalData.currentVehicleList
-    .data as VehiclesCurrentVehicle[]
+    .data as CurrentVehiclesAndRecords
+
+  const getVehicleDetails = useLazyVehicleDetails()
+  const createGetVehicleDetailsWrapper = (
+    getVehicleDetailsFunction: (variables: {
+      permno: string
+    }) => Promise<ApolloQueryResult<any>>,
+  ) => {
+    return async (plate: string) => {
+      const variables = { permno: plate }
+      const result = await getVehicleDetailsFunction(variables)
+      return result.data.vehiclePlateOrderChecksByPermno // Adjust based on your query
+    }
+  }
   return (
     <Box paddingTop={2}>
-      {currentVehicleList.length > 5 ? (
+      {currentVehicleList.totalRecords > 20 ? (
+        <FindVehicleFormField
+          application={application}
+          setFieldLoadingState={props.setFieldLoadingState}
+          setSubmitButtonDisabled={props.setSubmitButtonDisabled}
+          field={{
+            id: 'pickVehicle',
+            title: information.labels.pickVehicle.title,
+            type: FieldTypes.FIND_VEHICLE,
+            component: FieldComponents.FIND_VEHICLE,
+            children: undefined,
+            getVehicleDetails:
+              createGetVehicleDetailsWrapper(getVehicleDetails),
+            additionalErrors: true,
+            fallbackErrorMessage: error.validationFallbackErrorMessage,
+            findPlatePlaceholder:
+              information.labels.pickVehicle.findPlatePlaceholder,
+            findVehicleButtonText: information.labels.pickVehicle.findButton,
+            hasErrorTitle: information.labels.pickVehicle.hasErrorTitle,
+            notFoundErrorMessage:
+              information.labels.pickVehicle.notFoundMessage,
+            notFoundErrorTitle: information.labels.pickVehicle.notFoundTitle,
+            requiredValidVehicleErrorMessage: error.requiredValidVehicle,
+          }}
+        />
+      ) : currentVehicleList.totalRecords > 5 ? (
         <VehicleSelectField
-          currentVehicleList={currentVehicleList}
+          currentVehicleList={currentVehicleList.vehicles}
           {...props}
         />
       ) : (
-        <VehicleRadioField currentVehicleList={currentVehicleList} {...props} />
+        <VehicleRadioField
+          currentVehicleList={currentVehicleList?.vehicles}
+          {...props}
+        />
       )}
     </Box>
   )
