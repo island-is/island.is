564db37ee863440cb156f12b5704ece7c4b42179
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-02-16 09:39:20 +0000

    fix: revert

--- libs/clients/license-client/src/lib/clients/adr-license-client/adrLicenseClient.module.ts
+++ libs/clients/license-client/src/lib/clients/adr-license-client/adrLicenseClient.module.ts
@@ -1,12 +1,5 @@
 import { AdrAndMachineLicenseClientModule } from '@island.is/clients/adr-and-machine-license'
-import {
-  SmartSolutionsApiClientModule,
-  SmartSolutionsConfig,
-} from '@island.is/clients/smartsolutions'
-import {
-  SmartSolutionsModule,
-  SmartSolutionsModuleOptions,
-} from '@island.is/clients/smart-solutions'
+import { SmartSolutionsApiClientModule } from '@island.is/clients/smartsolutions'
 import { Module } from '@nestjs/common'
 import { ConfigType } from '@island.is/nest/config'
 import { AdrLicenseClient } from './adrLicenseClient.service'
@@ -15,19 +8,9 @@ import { AdrDigitalLicenseClientConfig } from './adrLicenseClient.config'
 @Module({
   imports: [
     AdrAndMachineLicenseClientModule,
-    SmartSolutionsModule.registerAsync({
-      useFactory: (
-        config: ConfigType<typeof AdrDigitalLicenseClientConfig>,
-      ) => {
-        const smartConfig: SmartSolutionsModuleOptions = {
-          config: {
-            apiKey: config.apiKey,
-            apiUrl: config.apiUrl,
-            passTemplateId: config.passTemplateId,
-          },
-        }
-        return smartConfig
-      },
+    SmartSolutionsApiClientModule.registerAsync({
+      useFactory: (config: ConfigType<typeof AdrDigitalLicenseClientConfig>) =>
+        config,
       inject: [AdrDigitalLicenseClientConfig.KEY],
     }),
   ],
