03d3f329b78292830caef9fc2703377c4af7f7c1
ori.jonab <Jon.Arnar.Briem@origo.is>
2024-02-19 10:09:21 +0000

    fixed error in down migration

--- apps/services/university-gateway/migrations/20240215161647-change-program-duration-type.js
+++ apps/services/university-gateway/migrations/20240215161647-change-program-duration-type.js
@@ -1,29 +1,34 @@
-'use strict';
+'use strict'
 
 module.exports = {
-
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) =>
-      queryInterface.changeColumn('program', 'duration_in_years',
+      queryInterface.changeColumn(
+        'program',
+        'duration_in_years',
         {
           type: Sequelize.FLOAT,
         },
         {
           transaction: t,
-        }
+        },
       ),
     )
   },
 
-
   //Down will most likely result in loss of data as we are casting from FLOAT to INTEGER
-  down (queryInterface, Sequelize) {
-    return Promise.all([
-      queryInterface.changeColumn('program', 'duration_in_years', {
-        type: Sequelize.INTEGER,
-      }, {
-        transaction: t,
-      })
-    ])
-  }
-};
+  down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.changeColumn(
+        'program',
+        'duration_in_years',
+        {
+          type: Sequelize.INTEGER,
+        },
+        {
+          transaction: t,
+        },
+      ),
+    )
+  },
+}
