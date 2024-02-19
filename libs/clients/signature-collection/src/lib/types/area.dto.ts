6dca89c3dd7c5d5581ef1737cc59387a96a80709
Alex Diljar <alex@juni.is>
2024-02-19 11:28:43 +0000

    rm todo

--- libs/clients/signature-collection/src/lib/types/area.dto.ts
+++ libs/clients/signature-collection/src/lib/types/area.dto.ts
@@ -10,6 +10,5 @@ export const mapArea = (area: SvaediDTO): Area => ({
   id: area?.id?.toString() ?? '',
   name: area?.nafn?.toString() ?? '',
   min: area?.fjoldi ?? 0,
-  // TODO: update when max is available
   max: area?.fjoldiMax ?? 0,
 })
