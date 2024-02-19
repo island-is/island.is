ab20607201c2b834ca9db28890d8e69ae46ebf68
Alex Diljar <alex@juni.is>
2024-02-19 11:08:54 +0000

    rm lint warn

--- libs/clients/signature-collection/src/lib/types/area.dto.ts
+++ libs/clients/signature-collection/src/lib/types/area.dto.ts
@@ -6,12 +6,10 @@ export interface Area {
   max: number
 }
 
-export function mapArea(area: SvaediDTO): Area {
-  return {
-    id: area?.id?.toString() ?? '',
-    name: area?.nafn?.toString() ?? '',
-    min: area?.fjoldi ?? 0,
-    // TODO: update when max is available
-    max: area?.fjoldiMax ?? 0,
-  }
-}
+export const mapArea = (area: SvaediDTO): Area => ({
+  id: area?.id?.toString() ?? '',
+  name: area?.nafn?.toString() ?? '',
+  min: area?.fjoldi ?? 0,
+  // TODO: update when max is available
+  max: area?.fjoldiMax ?? 0,
+})
