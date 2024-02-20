5ef78687404fb46ab76a2adf74d428e4ec9a1955
andes-it <builders@andes.is>
2024-02-20 13:18:35 +0000

    chore: nx format:write update dirty files

--- infra/src/logging.spec.ts
+++ infra/src/logging.spec.ts
@@ -1,33 +1,33 @@
-import { getLogger, } from './logging';
+import { getLogger } from './logging'
 
 describe('logging same level', () => {
-  let logger;
+  let logger
   beforeEach(() => {
-    console.log = jest.fn();
-  });
+    console.log = jest.fn()
+  })
   it('should log error messages', () => {
-    logger = getLogger('error');
-    logger.error('test message');
-    expect(console.log).toHaveBeenCalledWith('error: test message');
-  });
+    logger = getLogger('error')
+    logger.error('test message')
+    expect(console.log).toHaveBeenCalledWith('error: test message')
+  })
   it('should log warn messages', () => {
-    logger = getLogger('warn');
-    logger.warn('test message');
-    expect(console.log).toHaveBeenCalledWith('warn: test message');
-  });
+    logger = getLogger('warn')
+    logger.warn('test message')
+    expect(console.log).toHaveBeenCalledWith('warn: test message')
+  })
   it('should log info messages', () => {
-    logger = getLogger('info');
-    logger.info('test message');
-    expect(console.log).toHaveBeenCalledWith('info: test message');
-  });
+    logger = getLogger('info')
+    logger.info('test message')
+    expect(console.log).toHaveBeenCalledWith('info: test message')
+  })
   it('should log debug messages', () => {
-    logger = getLogger('debug');
-    logger.debug('test message');
-    expect(console.log).toHaveBeenCalledWith('debug: test message');
-  });
+    logger = getLogger('debug')
+    logger.debug('test message')
+    expect(console.log).toHaveBeenCalledWith('debug: test message')
+  })
   it('should log silly messages', () => {
-    logger = getLogger('silly');
-    logger.silly('test message');
-    expect(console.log).toHaveBeenCalledWith('silly: test message');
-  });
+    logger = getLogger('silly')
+    logger.silly('test message')
+    expect(console.log).toHaveBeenCalledWith('silly: test message')
+  })
 })
--- infra/src/logging.ts
+++ infra/src/logging.ts
@@ -14,7 +14,7 @@ export const getLogger = (logLevel = LOG_LEVEL) => ({
     acc[curr] = (message: string, ...rest: any) =>
       LOG_LEVELS.indexOf(curr) <= LOG_LEVELS.indexOf(logLevel)
         ? console.log(`${curr}: ${message}`, ...rest)
-        : () => { }
+        : () => {}
     return acc
   }, {} as { [key in typeof LOG_LEVELS[number]]: (message: string, ...rest: any) => void }),
 })
