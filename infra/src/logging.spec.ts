dfd0a3fe36cbec523978085ad0e74c99eb6c167f
Kristófer <kristofer@andes.is>
2024-02-20 13:12:49 +0000

    Tests for logging

---
+++ infra/src/logging.spec.ts
@@ -0,0 +1,33 @@
+import { getLogger, } from './logging';
+
+describe('logging same level', () => {
+  let logger;
+  beforeEach(() => {
+    console.log = jest.fn();
+  });
+  it('should log error messages', () => {
+    logger = getLogger('error');
+    logger.error('test message');
+    expect(console.log).toHaveBeenCalledWith('error: test message');
+  });
+  it('should log warn messages', () => {
+    logger = getLogger('warn');
+    logger.warn('test message');
+    expect(console.log).toHaveBeenCalledWith('warn: test message');
+  });
+  it('should log info messages', () => {
+    logger = getLogger('info');
+    logger.info('test message');
+    expect(console.log).toHaveBeenCalledWith('info: test message');
+  });
+  it('should log debug messages', () => {
+    logger = getLogger('debug');
+    logger.debug('test message');
+    expect(console.log).toHaveBeenCalledWith('debug: test message');
+  });
+  it('should log silly messages', () => {
+    logger = getLogger('silly');
+    logger.silly('test message');
+    expect(console.log).toHaveBeenCalledWith('silly: test message');
+  });
+})
--- infra/src/logging.ts
+++ infra/src/logging.ts
@@ -9,12 +9,13 @@ export const LOG_LEVELS = [
 export const LOG_LEVEL = (process.env.LOG_LEVEL ||
   'warn') as typeof LOG_LEVELS[number]
 
-export const logger = {
+export const getLogger = (logLevel = LOG_LEVEL) => ({
   ...LOG_LEVELS.reduce((acc, curr) => {
     acc[curr] = (message: string, ...rest: any) =>
-      LOG_LEVELS.indexOf(curr) <= LOG_LEVELS.indexOf(LOG_LEVEL)
+      LOG_LEVELS.indexOf(curr) <= LOG_LEVELS.indexOf(logLevel)
         ? console.log(`${curr}: ${message}`, ...rest)
-        : () => {}
+        : () => { }
     return acc
   }, {} as { [key in typeof LOG_LEVELS[number]]: (message: string, ...rest: any) => void }),
-}
+})
+export const logger = getLogger()
