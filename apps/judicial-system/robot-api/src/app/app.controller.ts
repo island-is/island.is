b975a311e4d88c4f451db7d26f6c47ca6b6d64b2
unakb <una@kolibri.is>
2024-02-21 13:12:10 +0000

    feat(j-s): changed name of robot api

--- apps/judicial-system/robot-api/src/app/app.controller.ts
+++ apps/judicial-system/robot-api/src/app/app.controller.ts
@@ -1,20 +1,18 @@
-import { Controller, Get, Inject, UseInterceptors } from '@nestjs/common'
+import { Controller, Get, Inject } from '@nestjs/common'
 import { ApiCreatedResponse } from '@nestjs/swagger'
 
 import type { Logger } from '@island.is/logging'
 import { LOGGER_PROVIDER } from '@island.is/logging'
 
-import { EventInterceptor } from './app.interceptor'
 import { AppService } from './app.service'
 
-@Controller('api/v1')
+@Controller('api')
 export class AppController {
   constructor(
     private readonly appService: AppService,
     @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
   ) {}
 
-  @UseInterceptors(EventInterceptor)
   @Get('test')
   @ApiCreatedResponse({ type: String, description: 'Test connection' })
   async test(): Promise<string> {
