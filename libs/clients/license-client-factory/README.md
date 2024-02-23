1a73d43e6ff6fb2cfd301612f1374061bb07f446
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-02-23 09:55:24 +0000

    feat: init

---
+++ libs/clients/license-client-factory/README.md
@@ -0,0 +1,7 @@
+# clients-license-client-factory
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test clients-license-client-factory` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/clients/license-client-factory/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'clients-license-client-factory',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/clients/license-client-factory',
+}
