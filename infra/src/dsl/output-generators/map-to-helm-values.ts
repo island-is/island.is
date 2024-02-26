e536dfff350ba9ef700face25df75308db3cba55
andes-it <builders@andes.is>
2024-02-26 06:22:23 +0000

    chore: nx format:write update dirty files

--- infra/src/dsl/output-generators/map-to-helm-values.ts
+++ infra/src/dsl/output-generators/map-to-helm-values.ts
@@ -52,8 +52,9 @@ const serializeService: SerializeMethod<HelmService> = async (
     grantNamespacesEnabled: grantNamespacesEnabled,
     namespace: namespace,
     image: {
-      repository: `821090935708.dkr.ecr.eu-west-1.amazonaws.com/${serviceDef.image ?? serviceDef.name
-        }`,
+      repository: `821090935708.dkr.ecr.eu-west-1.amazonaws.com/${
+        serviceDef.image ?? serviceDef.name
+      }`,
     },
     env: {
       SERVERSIDE_FEATURES_ON: env1.featuresOn.join(','),
