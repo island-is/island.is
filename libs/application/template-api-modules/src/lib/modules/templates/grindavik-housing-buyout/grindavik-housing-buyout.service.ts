d694d73e305ef4ca7d6305792b67eb4f6b801264
obmagnusson <obmagnusson@gmail.com>
2024-02-27 09:50:54 +0000

    submit function init

--- libs/application/template-api-modules/src/lib/modules/templates/grindavik-housing-buyout/grindavik-housing-buyout.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/grindavik-housing-buyout/grindavik-housing-buyout.service.ts
@@ -1,14 +1,25 @@
 import { LOGGER_PROVIDER } from '@island.is/logging'
 import { Inject, Injectable } from '@nestjs/common'
 import { BaseTemplateApiService } from '../../base-template-api.service'
-import { ApplicationTypes } from '@island.is/application/types'
+import {
+  ApplicationTypes,
+  NationalRegistryIndividual,
+  UserProfile,
+} from '@island.is/application/types'
 import { TemplateApiModuleActionProps } from '../../../types'
 
-import { SyslumennService } from '@island.is/clients/syslumenn'
+import {
+  Person,
+  PersonType,
+  SyslumennService,
+} from '@island.is/clients/syslumenn'
 import { NationalRegistryClientService } from '@island.is/clients/national-registry-v2'
 import getDomicileAtPostalCodeOnDate from './grindavik-housing-buyout.utils'
 import { TemplateApiError } from '@island.is/nest/problem'
-import { notEligible } from '@island.is/application/templates/grindavik-housing-buyout'
+import {
+  notEligible,
+  GrindavikHousingBuyoutAnswers,
+} from '@island.is/application/templates/grindavik-housing-buyout'
 
 import { FasteignirApi } from '@island.is/clients/assets'
 
@@ -26,6 +37,38 @@ export class GrindavikHousingBuyoutService extends BaseTemplateApiService {
     super(ApplicationTypes.GRINDAVIK_HOUSING_BUYOUT)
   }
 
+  async submitApplication({ application, auth }: TemplateApiModuleActionProps) {
+    const { nationalRegistry, userProfile } = application.externalData
+    const answers = application.answers as GrindavikHousingBuyoutAnswers
+
+    const applicant: Person = {
+      name: answers.applicant.name,
+      ssn: answers.applicant.nationalId,
+      phoneNumber: answers.applicant.phoneNumber ?? '',
+      email: answers.applicant.email ?? '',
+      homeAddress: answers.applicant.address ?? '',
+      postalCode: answers.applicant.postalCode ?? '',
+      city: answers.applicant.city ?? '',
+      signed: false,
+      type: PersonType.Plaintiff,
+    }
+
+    const extraData: { [key: string]: string } = {
+      applicationId: application.id,
+    }
+
+    const uploadDataName = 'Umsókn um kaup á íbúðarhúsnæði í Grindavík'
+    const uploadDataId = 'grindavik-umsokn-1'
+    const response = await this.syslumennService.uploadData(
+      [applicant],
+      [],
+      extraData,
+      uploadDataName,
+      uploadDataId,
+    )
+    return { ...response, applicationId: application.id }
+  }
+
   async checkResidence({
     application,
     auth,
@@ -35,9 +78,7 @@ export class GrindavikHousingBuyoutService extends BaseTemplateApiService {
     const data = await this.nationalRegistryApi.getResidenceHistory(
       auth.nationalId,
     )
-    return {
-      realEstateId: '12345',
-    }
+
     console.log('residence history : ', data)
 
     let dateInQuestion = '2023-10-11'
@@ -91,7 +132,6 @@ export class GrindavikHousingBuyoutService extends BaseTemplateApiService {
   }: TemplateApiModuleActionProps) {
     const result = ''
 
-    return result
     console.log(application.externalData)
     const { realEstateId } =
       (application.externalData.checkResidence.data as CheckResidence) ??
@@ -102,6 +142,83 @@ export class GrindavikHousingBuyoutService extends BaseTemplateApiService {
     const property = await this.propertiesApi.fasteignirGetFasteign({
       fasteignanumer: realEstateId,
     })
+
+    const fasteign = {
+      fasteignanumer: 'F12345',
+      sjalfgefidStadfang: {
+        stadfanganumer: 1234,
+        landeignarnumer: 567,
+        postnumer: 113,
+        sveitarfelagBirting: 'Reykjavík',
+        birting: 'Reykjavík',
+        birtingStutt: 'RVK',
+      },
+      fasteignamat: {
+        gildandiFasteignamat: 50000000,
+        fyrirhugadFasteignamat: 55000000,
+        gildandiMannvirkjamat: 30000000,
+        fyrirhugadMannvirkjamat: 35000000,
+        gildandiLodarhlutamat: 20000000,
+        fyrirhugadLodarhlutamat: 25000000,
+        gildandiAr: 2024,
+        fyrirhugadAr: 2025,
+      },
+      landeign: {
+        landeignarnumer: '123456',
+        lodamat: 75000000,
+        notkunBirting: 'Íbúðarhúsalóð',
+        flatarmal: '300000',
+        flatarmalEining: 'm²',
+      },
+      thinglystirEigendur: {
+        thinglystirEigendur: [
+          {
+            nafn: 'Gervimaður Danmörk',
+            kennitala: '2222222222',
+            eignarhlutfall: 0.5,
+            kaupdagur: new Date(),
+            heimildBirting: 'A+',
+          },
+          {
+            nafn: 'Jóna Jónasdóttir',
+            kennitala: '3333333333',
+            eignarhlutfall: 0.5,
+            kaupdagur: new Date(),
+            heimildBirting: 'A+',
+          },
+        ],
+      },
+      notkunareiningar: {
+        brunabotamat: 1000000,
+      },
+    }
+    /*
+
+
+
+
+    birtStaerdMaelieining?: string | null;
+
+    notkunareininganumer?: string | null;
+
+    fasteignanumer?: string | null;
+
+    stadfang?: Stadfang | null;
+
+    merking?: string | null;
+
+    notkunBirting?: string | null;
+
+    skyring?: string | null;
+    byggingararBirting?: string | null;
+
+    birtStaerd?: number;
+
+    fasteignamat?: Fasteignamat | null;
+ 
+    brunabotamat?: number;
+
+    */
 
     const {
       fasteignamat,
@@ -127,10 +244,5 @@ export class GrindavikHousingBuyoutService extends BaseTemplateApiService {
     )
 
     return eining
-  }
-
-  async submitApplication({ application, auth }: TemplateApiModuleActionProps) {
-    const result = ''
-    return result
   }
 }
