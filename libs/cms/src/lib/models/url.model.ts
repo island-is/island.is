c38b61f1a0a96e3365b654fff0bdee978bad1aa3
runarvestmann <runar@stefna.is>
2024-02-27 09:47:34 +0000

    Trim whitespace from slug fields during mapping

--- libs/cms/src/lib/models/url.model.ts
+++ libs/cms/src/lib/models/url.model.ts
@@ -21,10 +21,14 @@ export class Url {
   explicitRedirect?: string
 }
 
-export const mapUrl = ({ fields, sys }: IUrl): Url => ({
-  id: sys.id,
-  title: fields.title ?? '',
-  page: fields.page ? mapReferenceLink(fields.page) : null,
-  explicitRedirect: fields.explicitRedirect ?? '',
-  urlsList: fields.urlsList ?? [],
-})
+export const mapUrl = ({ fields, sys }: IUrl): Url => {
+  const urlsList = (fields.urlsList ?? []).map((url) => url?.trim())
+
+  return {
+    id: sys.id,
+    title: fields.title ?? '',
+    page: fields.page ? mapReferenceLink(fields.page) : null,
+    explicitRedirect: fields.explicitRedirect ?? '',
+    urlsList,
+  }
+}
