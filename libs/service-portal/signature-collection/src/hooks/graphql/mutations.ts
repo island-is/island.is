c2735bee363b7c33767d2ec43213f89009d5dfb6
Alex Diljar <alex@juni.is>
2024-02-27 09:43:44 +0000

    updateinputs

--- libs/service-portal/signature-collection/src/hooks/graphql/mutations.ts
+++ libs/service-portal/signature-collection/src/hooks/graphql/mutations.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export const cancelCollectionMutation = gql`
-  mutation signatureCollectionCancel($input: SignatureCollectionIdInput!) {
+  mutation signatureCollectionCancel($input: SignatureCollectionCancelListsInput!) {
     signatureCollectionCancel(input: $input) {
       success
     }
@@ -9,7 +9,7 @@ export const cancelCollectionMutation = gql`
 `
 
 export const unSignList = gql`
-  mutation removeSignature($input: SignatureCollectionSignatureIdInput!) {
+  mutation removeSignature($input: SignatureCollectionListIdInput!) {
     signatureCollectionUnsign(input: $input) {
       success
       reasons
