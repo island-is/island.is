d7d1016e4ce03f06c9d0cd05a4ad072845ba61a4
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2024-02-27 10:33:29 +0000

    add extdata typings

--- libs/application/templates/grindavik-housing-buyout/src/index.ts
+++ libs/application/templates/grindavik-housing-buyout/src/index.ts
@@ -3,6 +3,10 @@ import { GrindavikHousingBuyout } from './lib/dataSchema'
 
 export const getDataProviders = () => import('./dataProviders/')
 export type GrindavikHousingBuyoutAnswers = GrindavikHousingBuyout
+export type {
+  GrindavikHousingBuyoutApplication,
+  GrindavikHousingBuyoutExternalData,
+} from './types'
 
 export * from './lib/messages'
 
