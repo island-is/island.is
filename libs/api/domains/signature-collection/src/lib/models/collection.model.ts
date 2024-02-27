8d35546c3771cffa6a39bd823b46361d237fcd03
Alex Diljar <alex@juni.is>
2024-02-27 10:20:23 +0000

    use enum

--- libs/api/domains/signature-collection/src/lib/models/collection.model.ts
+++ libs/api/domains/signature-collection/src/lib/models/collection.model.ts
@@ -1,7 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql'
 import { SignatureCollectionArea } from './area.model'
 import { SignatureCollectionCandidate } from './candidate.model'
-
+import { CollectionStatus } from './status.model'
 @ObjectType()
 export class SignatureCollection {
   @Field(() => ID)
@@ -28,6 +28,6 @@ export class SignatureCollection {
   @Field(() => [SignatureCollectionCandidate])
   candidates!: SignatureCollectionCandidate[]
 
-  @Field()
-  status!: string
+  @Field(() => CollectionStatus)
+  status!: CollectionStatus
 }
