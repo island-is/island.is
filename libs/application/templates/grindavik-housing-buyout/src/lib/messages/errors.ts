a0530aa2cbf904a0133ca74d37bdab631e8164aa
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2024-02-28 15:20:52 +0000

    fixx extract strings

--- libs/application/templates/grindavik-housing-buyout/src/lib/messages/errors.ts
+++ libs/application/templates/grindavik-housing-buyout/src/lib/messages/errors.ts
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl'
 export const errors = {
   fields: defineMessages({
     required: {
-      id: 'hst.application:errors.contacts.required',
+      id: 'ghb.application:errors.contacts.required',
       defaultMessage: 'Þennan reit þarf að fylla út',
       description: 'Error message a required field is empty',
     },
