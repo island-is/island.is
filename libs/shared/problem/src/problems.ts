a2bb8c1c48bbbe3dac2040b7eceb808b19b751f5
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2024-03-01 09:11:37 +0000

    Update problems.ts

--- libs/shared/problem/src/problems.ts
+++ libs/shared/problem/src/problems.ts
@@ -39,8 +39,8 @@ export interface AttemptFailedProblem extends BaseProblem {
 }
 
 export interface ProviderErrorReason {
-  title: StaticText & { values?: Record<string, string> }
-  summary: StaticText & { values?: Record<string, string> }
+  title: StaticText
+  summary: StaticText
 }
 
 export interface TemplateApiErrorProblem extends BaseProblem {
