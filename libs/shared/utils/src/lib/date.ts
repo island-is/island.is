7dfe09f77154d1b31dc59849d769b8e4b7de9bad
andes-it <builders@andes.is>
2024-03-01 12:34:56 +0000

    chore: nx format:write update dirty files

--- libs/shared/utils/src/lib/date.ts
+++ libs/shared/utils/src/lib/date.ts
@@ -11,6 +11,4 @@ export const getDayCountThisYear = (): number => {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 366 : 365
 }
 
-
 export const isLeapYear = (): boolean => getDayCountThisYear() === 366
-
