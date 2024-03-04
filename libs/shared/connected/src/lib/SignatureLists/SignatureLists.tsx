e1516f2b5fc42bddbf6d481e923bd024fcc81d90
albina <albina@kosmosogkaos.is>
2024-03-04 10:39:51 +0000

    fix(signature-collections): paginate candidates web

--- libs/shared/connected/src/lib/SignatureLists/SignatureLists.tsx
+++ libs/shared/connected/src/lib/SignatureLists/SignatureLists.tsx
@@ -1,6 +1,12 @@
-import { Box, Text, Stack, ActionCard } from '@island.is/island-ui/core'
+import {
+  Box,
+  Text,
+  Stack,
+  ActionCard,
+  Pagination,
+} from '@island.is/island-ui/core'
 import format from 'date-fns/format'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import {
   ConnectedComponent,
   SignatureCollectionCandidate,
@@ -23,51 +29,57 @@ export const SignatureLists: FC<
   const { openLists, openListsLoading } = useGetOpenLists(collection?.id || '')
   const t = useLocalization(slice.json)
 
+  const [page, setPage] = useState(1)
+  const pageSize = 5
+
   return (
     !loading &&
     !openListsLoading && (
-      <Box marginTop={7}>
-        {(collection?.candidates.length > 0 || openLists?.length > 0) && (
-          <Box marginBottom={3}>
-            <Text variant="h4">
+      <Box marginTop={8}>
+        {(collection?.candidates?.length > 0 || openLists?.length > 0) && (
+          <Box marginBottom={4}>
+            <Text variant="h3">
               {t('title', 'Frambjóðendur sem hægt er að mæla með')}
             </Text>
           </Box>
         )}
-        <Stack space={4}>
-          {/* if collection time is over yet there are still open lists, show them */}
-          {new Date() > new Date(collection.endTime) &&
-          openLists?.length > 0 ? (
-            openLists?.map((list: SignatureCollectionListBase) => {
-              return (
-                <ActionCard
-                  eyebrow={
-                    t('openTil', 'Lokadagur:') +
-                    ' ' +
-                    format(new Date(list.endTime), 'dd.MM.yyyy')
-                  }
-                  key={list.id}
-                  backgroundColor="white"
-                  heading={list.title}
-                  text={t('collectionName', 'Forsetakosningar 2024')}
-                  cta={{
-                    label: t('sign', 'Mæla með framboði'),
-                    variant: 'text',
-                    icon: 'open',
-                    iconType: 'outline',
-                    size: 'small',
-                    onClick: () =>
-                      window.open(
-                        `${window.location.origin}${list.slug}`,
-                        '_blank',
-                      ),
-                  }}
-                />
-              )
-            })
-          ) : collection?.candidates.length > 0 ? (
-            collection.candidates.map(
-              (candidate: SignatureCollectionCandidate) => {
+        <Stack space={3}>
+          {/* collection closed but there are open lists */}
+          {!collection.isActive && openLists?.length > 0 ? (
+            openLists
+              .slice((page - 1) * pageSize, page * pageSize)
+              .map((list: SignatureCollectionListBase) => {
+                return (
+                  <ActionCard
+                    /*eyebrow={
+                      t('openTil', 'Lokadagur:') +
+                      ' ' +
+                      format(new Date(list.endTime), 'dd.MM.yyyy')
+                    }*/
+                    key={list.id}
+                    backgroundColor="white"
+                    heading={list.title}
+                    text={t('collectionName', 'Forsetakosningar 2024')}
+                    cta={{
+                      label: t('sign', 'Mæla með framboði'),
+                      variant: 'text',
+                      icon: 'open',
+                      iconType: 'outline',
+                      size: 'small',
+                      onClick: () =>
+                        window.open(
+                          `${window.location.origin}${list.slug}`,
+                          '_blank',
+                        ),
+                    }}
+                  />
+                )
+              })
+          ) : /* collection is open */
+          collection.candidates?.length > 0 ? (
+            collection.candidates
+              .slice((page - 1) * pageSize, page * pageSize)
+              .map((candidate: SignatureCollectionCandidate) => {
                 return (
                   <ActionCard
                     eyebrow={
@@ -105,13 +117,37 @@ export const SignatureLists: FC<
                     }
                   />
                 )
-              },
-            )
+              })
           ) : (
             <Text variant="h4">
               {t('noLists', 'Engin meðmælasöfnun er í gangi í augnablikinu.')}
             </Text>
           )}
+          {collection.isActive
+            ? collection.candidates?.length > 10
+            : openLists.length > 10 && (
+                <Box marginTop={[0, 3]}>
+                  <Pagination
+                    totalItems={
+                      collection.isActive
+                        ? collection.candidates.length
+                        : openLists.length
+                    }
+                    itemsPerPage={pageSize}
+                    page={page}
+                    renderLink={(page, className, children) => (
+                      <Box
+                        cursor="pointer"
+                        className={className}
+                        onClick={() => setPage(page)}
+                        component="button"
+                      >
+                        {children}
+                      </Box>
+                    )}
+                  />
+                </Box>
+              )}
         </Stack>
       </Box>
     )
