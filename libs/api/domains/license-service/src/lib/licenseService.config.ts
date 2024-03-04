18099f71f63954a84e6746ab0aed0ec2145950c5
andes-it <builders@andes.is>
2024-03-04 12:11:51 +0000

    chore: nx format:write update dirty files

--- libs/api/domains/license-service/src/lib/licenseService.config.ts
+++ libs/api/domains/license-service/src/lib/licenseService.config.ts
@@ -15,9 +15,7 @@ export const LicenseServiceConfig = defineConfig({
   name: 'LicenseServiceConfig',
   schema: LicenseServiceConfigSchema,
   load: (env) => ({
-    barcodeSecretKey: env.required(
-      'LICENSE_SERVICE_BARCODE_SECRET_KEY',''
-    ),
+    barcodeSecretKey: env.required('LICENSE_SERVICE_BARCODE_SECRET_KEY', ''),
     redis: {
       nodes: env.requiredJSON('LICENSE_SERVICE_REDIS_NODES', [
         'localhost:7000',
