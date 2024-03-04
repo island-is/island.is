800fb2756b50c29c650a9c01b030bce5e4de8850
Brynjar <brynjarorng@gmail.com>
2024-03-04 13:02:55 +0000

    added judicial system to the feature deploy DSL

--- infra/src/feature-env.ts
+++ infra/src/feature-env.ts
@@ -7,6 +7,7 @@ import {
   FeatureDeploymentServices,
   Services as IslandisServices,
 } from './uber-charts/islandis'
+import { Services as JudicialServices } from './uber-charts/judicial-system'
 import { Services as IDSServices } from './uber-charts/identity-server'
 import { EnvironmentServices } from './dsl/types/charts'
 import { HelmService, Services } from './dsl/types/output-types'
@@ -21,11 +22,12 @@ import {
 import { ServiceBuilder } from './dsl/dsl'
 import { logger } from './common'
 
-type ChartName = 'islandis' | 'identity-server'
+type ChartName = 'islandis' | 'identity-server' | 'judicial-system'
 
 const charts: { [name in ChartName]: EnvironmentServices } = {
   islandis: IslandisServices,
   'identity-server': IDSServices,
+  'judicial-system': JudicialServices,
 }
 
 interface Arguments {
