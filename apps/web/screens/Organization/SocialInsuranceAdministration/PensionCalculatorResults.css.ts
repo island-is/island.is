52f32996ad1fe2b42fee5721bfd7a9983bc30aee
runarvestmann <runar@stefna.is>
2024-03-05 16:11:14 +0000

    Fix heading visuals for results page

--- apps/web/screens/Organization/SocialInsuranceAdministration/PensionCalculatorResults.css.ts
+++ apps/web/screens/Organization/SocialInsuranceAdministration/PensionCalculatorResults.css.ts
@@ -2,8 +2,10 @@ import { globalStyle, style } from '@vanilla-extract/css'
 
 import { theme } from '@island.is/island-ui/theme'
 
+const lineWidth = '2px'
+
 export const line = style({
-  borderRight: `2px solid ${theme.color.blue200}`,
+  borderRight: `${lineWidth} solid ${theme.color.blue200}`,
   height: '86px',
 })
 
@@ -17,4 +19,17 @@ export const textMaxWidth = style({
 
 globalStyle('tbody', {
   borderBottom: '32px solid white',
+})
+
+export const grid = style({
+  display: 'grid',
+  gridTemplateColumns: `140px ${lineWidth} auto`,
+})
+
+export const fitContent = style({
+  width: 'fit-content',
+})
+
+export const alignSelfToFlexEnd = style({
+  alignSelf: 'flex-end',
 })
