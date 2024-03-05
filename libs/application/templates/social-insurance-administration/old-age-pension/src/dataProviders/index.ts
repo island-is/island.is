ff7bb18e639f528054005fbef1bfb71ff16452c2
karenbjorg <khalldorsdottir@deloitte.is>
2024-03-05 13:40:47 +0000

    add order to api calls

--- libs/application/templates/social-insurance-administration/old-age-pension/src/dataProviders/index.ts
+++ libs/application/templates/social-insurance-administration/old-age-pension/src/dataProviders/index.ts
@@ -16,12 +16,16 @@ export const SocialInsuranceAdministrationIsApplicantEligibleApi =
     action: 'getIsEligible',
     externalDataId: 'socialInsuranceAdministrationIsApplicantEligible',
     namespace: 'SocialInsuranceAdministration',
+    order: 2,
   })
 
+// This needs to run before eligible is called because if applicant isn't vskm at TR
+// they register one in this call.
 export const SocialInsuranceAdministrationApplicantApi = defineTemplateApi({
   action: 'getApplicant',
   externalDataId: 'socialInsuranceAdministrationApplicant',
   namespace: 'SocialInsuranceAdministration',
+  order: 1,
 })
 
 export const SocialInsuranceAdministrationCurrenciesApi = defineTemplateApi({
