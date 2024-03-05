e0574f308144ff71e748123731fda040f10c8fa3
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-03-05 13:23:55 +0000

    fix: mock data

---
+++ libs/clients/social-insurance-administration/src/lib/mockData.ts
@@ -0,0 +1,605 @@
+import { TrWebCommonsExternalPortalsApiModelsPaymentPlanPaymentPlanDto } from '../../gen/fetch'
+
+export const MOCK_DATA: TrWebCommonsExternalPortalsApiModelsPaymentPlanPaymentPlanDto =
+  {
+    paymentPlan: {
+      groups: [
+        {
+          name: '',
+          group: 'Skattskyldar greiðslutegundir',
+          groupId: 10,
+          type: 'LAU',
+          overviewType: 'A',
+          order: '01-1.11-77',
+          monthTotals: [
+            {
+              month: 1,
+              amount: 232892,
+            },
+            {
+              month: 2,
+              amount: 232892,
+            },
+            {
+              month: 3,
+              amount: 232892,
+            },
+            {
+              month: 4,
+              amount: 232892,
+            },
+            {
+              month: 5,
+              amount: 232892,
+            },
+            {
+              month: 6,
+              amount: 232892,
+            },
+            {
+              month: 7,
+              amount: 21460,
+            },
+            {
+              month: 8,
+              amount: 151072,
+            },
+            {
+              month: 9,
+              amount: 151072,
+            },
+            {
+              month: 10,
+              amount: 232892,
+            },
+            {
+              month: 11,
+              amount: 232892,
+            },
+            {
+              month: 12,
+              amount: 32669,
+            },
+          ],
+          rows: [
+            {
+              name: 'Örorkulífeyrir',
+              months: [
+                {
+                  month: 1,
+                  amount: 46036,
+                },
+                {
+                  month: 2,
+                  amount: 46036,
+                },
+                {
+                  month: 3,
+                  amount: 46036,
+                },
+                {
+                  month: 4,
+                  amount: 46036,
+                },
+                {
+                  month: 5,
+                  amount: 46036,
+                },
+                {
+                  month: 6,
+                  amount: 46036,
+                },
+                {
+                  month: 7,
+                  amount: 0,
+                },
+                {
+                  month: 8,
+                  amount: 30484,
+                },
+                {
+                  month: 9,
+                  amount: 30484,
+                },
+                {
+                  month: 10,
+                  amount: 46036,
+                },
+                {
+                  month: 11,
+                  amount: 46036,
+                },
+                {
+                  month: 12,
+                  amount: 0,
+                },
+              ],
+            },
+            {
+              name: 'Aldurstengd örorkuuppbót',
+              months: [
+                {
+                  month: 1,
+                  amount: 46036,
+                },
+                {
+                  month: 2,
+                  amount: 46036,
+                },
+                {
+                  month: 3,
+                  amount: 46036,
+                },
+                {
+                  month: 4,
+                  amount: 46036,
+                },
+                {
+                  month: 5,
+                  amount: 46036,
+                },
+                {
+                  month: 6,
+                  amount: 46036,
+                },
+                {
+                  month: 7,
+                  amount: 0,
+                },
+                {
+                  month: 8,
+                  amount: 30484,
+                },
+                {
+                  month: 9,
+                  amount: 30484,
+                },
+                {
+                  month: 10,
+                  amount: 46036,
+                },
+                {
+                  month: 11,
+                  amount: 46036,
+                },
+                {
+                  month: 12,
+                  amount: 0,
+                },
+              ],
+            },
+            {
+              name: 'Tekjutrygging örorkulífeyrisþega',
+              months: [
+                {
+                  month: 1,
+                  amount: 140820,
+                },
+                {
+                  month: 2,
+                  amount: 140820,
+                },
+                {
+                  month: 3,
+                  amount: 140820,
+                },
+                {
+                  month: 4,
+                  amount: 140820,
+                },
+                {
+                  month: 5,
+                  amount: 140820,
+                },
+                {
+                  month: 6,
+                  amount: 140820,
+                },
+                {
+                  month: 7,
+                  amount: 0,
+                },
+                {
+                  month: 8,
+                  amount: 90104,
+                },
+                {
+                  month: 9,
+                  amount: 90104,
+                },
+                {
+                  month: 10,
+                  amount: 140820,
+                },
+                {
+                  month: 11,
+                  amount: 140820,
+                },
+                {
+                  month: 12,
+                  amount: 0,
+                },
+              ],
+            },
+            {
+              name: 'Orlofsuppbót á tekjutryggingu',
+              months: [
+                {
+                  month: 1,
+                  amount: 0,
+                },
+                {
+                  month: 2,
+                  amount: 0,
+                },
+                {
+                  month: 3,
+                  amount: 0,
+                },
+                {
+                  month: 4,
+                  amount: 0,
+                },
+                {
+                  month: 5,
+                  amount: 0,
+                },
+                {
+                  month: 6,
+                  amount: 0,
+                },
+                {
+                  month: 7,
+                  amount: 21460,
+                },
+                {
+                  month: 8,
+                  amount: 0,
+                },
+                {
+                  month: 9,
+                  amount: 0,
+                },
+                {
+                  month: 10,
+                  amount: 0,
+                },
+                {
+                  month: 11,
+                  amount: 0,
+                },
+                {
+                  month: 12,
+                  amount: 0,
+                },
+              ],
+            },
+            {
+              name: 'Desemberuppbót á tekjutryggingu',
+              months: [
+                {
+                  month: 1,
+                  amount: 0,
+                },
+                {
+                  month: 2,
+                  amount: 0,
+                },
+                {
+                  month: 3,
+                  amount: 0,
+                },
+                {
+                  month: 4,
+                  amount: 0,
+                },
+                {
+                  month: 5,
+                  amount: 0,
+                },
+                {
+                  month: 6,
+                  amount: 0,
+                },
+                {
+                  month: 7,
+                  amount: 0,
+                },
+                {
+                  month: 8,
+                  amount: 0,
+                },
+                {
+                  month: 9,
+                  amount: 0,
+                },
+                {
+                  month: 10,
+                  amount: 0,
+                },
+                {
+                  month: 11,
+                  amount: 0,
+                },
+                {
+                  month: 12,
+                  amount: 32669,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '',
+          group: 'Óskattskyldar greiðslutegundir',
+          monthTotals: [
+            {
+              month: 1,
+              amount: 23293,
+            },
+            {
+              month: 2,
+              amount: 23293,
+            },
+            {
+              month: 3,
+              amount: 23293,
+            },
+            {
+              month: 4,
+              amount: 23293,
+            },
+            {
+              month: 5,
+              amount: 23293,
+            },
+            {
+              month: 6,
+              amount: 23293,
+            },
+            {
+              month: 7,
+              amount: 0,
+            },
+            {
+              month: 8,
+              amount: 23293,
+            },
+            {
+              month: 9,
+              amount: 23293,
+            },
+            {
+              month: 10,
+              amount: 23293,
+            },
+            {
+              month: 11,
+              amount: 23293,
+            },
+            {
+              month: 12,
+              amount: 0,
+            },
+          ],
+          rows: [
+            {
+              name: 'Uppbót v/reksturs bifreiðar (örorkulífeyrir)- Óskattskyld',
+              months: [
+                {
+                  month: 1,
+                  amount: 23293,
+                },
+                {
+                  month: 2,
+                  amount: 23293,
+                },
+                {
+                  month: 3,
+                  amount: 23293,
+                },
+                {
+                  month: 4,
+                  amount: 23293,
+                },
+                {
+                  month: 5,
+                  amount: 23293,
+                },
+                {
+                  month: 6,
+                  amount: 23293,
+                },
+                {
+                  month: 7,
+                  amount: 0,
+                },
+                {
+                  month: 8,
+                  amount: 23293,
+                },
+                {
+                  month: 9,
+                  amount: 23293,
+                },
+                {
+                  month: 10,
+                  amount: 23293,
+                },
+                {
+                  month: 11,
+                  amount: 23293,
+                },
+                {
+                  month: 12,
+                  amount: 0,
+                },
+              ],
+            },
+            {
+              name: 'Aldurstengd örorkuuppbót',
+              months: [
+                {
+                  month: 1,
+                  amount: 46036,
+                },
+                {
+                  month: 2,
+                  amount: 46036,
+                },
+                {
+                  month: 3,
+                  amount: 46036,
+                },
+                {
+                  month: 4,
+                  amount: 46036,
+                },
+                {
+                  month: 5,
+                  amount: 46036,
+                },
+                {
+                  month: 6,
+                  amount: 46036,
+                },
+                {
+                  month: 7,
+                  amount: 0,
+                },
+                {
+                  month: 8,
+                  amount: 30484,
+                },
+                {
+                  month: 9,
+                  amount: 30484,
+                },
+                {
+                  month: 10,
+                  amount: 46036,
+                },
+                {
+                  month: 11,
+                  amount: 46036,
+                },
+                {
+                  month: 12,
+                  amount: 0,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '',
+          group: 'Frádráttur',
+          monthTotals: [
+            {
+              month: 1,
+              amount: 88383,
+            },
+            {
+              month: 2,
+              amount: 88522,
+            },
+            {
+              month: 3,
+              amount: 88452,
+            },
+            {
+              month: 4,
+              amount: 88452,
+            },
+            {
+              month: 5,
+              amount: 88452,
+            },
+            {
+              month: 6,
+              amount: 88452,
+            },
+            {
+              month: 7,
+              amount: 8151,
+            },
+            {
+              month: 8,
+              amount: 57377,
+            },
+            {
+              month: 9,
+              amount: 57377,
+            },
+            {
+              month: 10,
+              amount: 88452,
+            },
+            {
+              month: 11,
+              amount: 88452,
+            },
+            {
+              month: 12,
+              amount: 12408,
+            },
+          ],
+          rows: [
+            {
+              name: 'Staðgreiðsla',
+              months: [
+                {
+                  month: 1,
+                  amount: 88383,
+                },
+                {
+                  month: 2,
+                  amount: 88522,
+                },
+                {
+                  month: 3,
+                  amount: 88452,
+                },
+                {
+                  month: 4,
+                  amount: 88452,
+                },
+                {
+                  month: 5,
+                  amount: 88452,
+                },
+                {
+                  month: 6,
+                  amount: 88452,
+                },
+                {
+                  month: 7,
+                  amount: 8151,
+                },
+                {
+                  month: 8,
+                  amount: 57377,
+                },
+                {
+                  month: 9,
+                  amount: 57377,
+                },
+                {
+                  month: 10,
+                  amount: 88452,
+                },
+                {
+                  month: 11,
+                  amount: 88452,
+                },
+                {
+                  month: 12,
+                  amount: 12408,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  }
