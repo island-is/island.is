2882b692af3a94f322a9a41e07a3de645477df2d
Kristófer <kristofer@andes.is>
2024-03-07 11:51:11 +0000

    Default to 'dev' env in CI only if TEST_ENVIRONMENT isn't set

--- apps/system-e2e/src/support/urls.ts
+++ apps/system-e2e/src/support/urls.ts
@@ -104,9 +104,8 @@ const envs: {
   },
 }
 
-export const env = !!process.env.CI
-  ? 'dev'
-  : ((process.env.TEST_ENVIRONMENT ?? 'local') as TestEnvironment)
+export const env = ((process.env.TEST_ENVIRONMENT ?? (!!process.env.CI
+  ? 'dev' : 'local')) as TestEnvironment)
 const hotEnv = process.env.TEST_URL
   ? { islandisBaseUrl: process.env.TEST_URL }
   : {}
