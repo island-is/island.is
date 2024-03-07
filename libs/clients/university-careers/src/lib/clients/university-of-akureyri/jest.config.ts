99cb449e76d058aaaa53428bc5225aa45c4e475e
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-03-07 14:31:36 +0000

    fix: n again

--- libs/clients/university-careers/src/lib/clients/university-of-akureyri/jest.config.ts
+++ libs/clients/university-careers/src/lib/clients/university-of-akureyri/jest.config.ts
@@ -1,12 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'clients-careers-university-of-akureyri',
-  preset: '../../../../../jest.preset.js',
+  preset: '../../../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
-    '../../../../../coverage/libs/clients/university-careers/clients/university-of-akureyri',
+    '../../../../../../../coverage/libs/clients/university-careers/src/lib/clients/university-of-akureyri',
 }
