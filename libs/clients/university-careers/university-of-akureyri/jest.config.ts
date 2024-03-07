09b4b31fe09e3cdf032d4fcf55ad0ea7d6af0ef2
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-03-07 14:29:01 +0000

    feat: gen

---
+++ libs/clients/university-careers/university-of-akureyri/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'clients-careers-university-of-akureyri',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../../../coverage/libs/clients/university-careers/university-of-akureyri',
+}
---
+++ libs/clients/university-careers/university-of-akureyri/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/clients-careers-university-of-akureyri.module'
