8ba08004ba44c411085474f705390863170fa901
Baldur Óli Barkarson <ori.baldurb@ORI0746.local>
2024-03-08 10:53:22 +0000

    cherry pick conflict

--- apps/web/screens/UniversitySearch/UniversitySearch.tsx
+++ apps/web/screens/UniversitySearch/UniversitySearch.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
 import Fuse from 'fuse.js'
-import getConfig from 'next/config'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/client/react/hooks/useQuery'
@@ -45,7 +44,6 @@ import {
   GetNamespaceQuery,
   GetNamespaceQueryVariables,
   GetUniversityGatewayProgramFiltersQuery,
-  GetUniversityGatewayProgramsQuery,
   GetUniversityGatewayUniversitiesQuery,
   Query,
   QueryGetOrganizationPageArgs,
@@ -71,8 +69,6 @@ import { Comparison } from './ComparisonComponent'
 import { TranslationDefaults } from './TranslationDefaults'
 import * as organizationStyles from '../../components/Organization/Wrapper/OrganizationWrapper.css'
 import * as styles from './UniversitySearch.css'
-
-const { publicRuntimeConfig = {} } = getConfig() ?? {}
 
 const ITEMS_PER_PAGE = 18
 const NUMBER_OF_FILTERS = 6
@@ -264,6 +260,7 @@ const UniversitySearch: Screen<UniversitySearchProps> = ({
     threshold: 0.3,
     findAllMatches: true,
     ignoreLocation: true,
+    includeScore: true,
     keys: [
       `name${locale === 'is' ? 'Is' : 'En'}`,
       `specializationName${locale === 'is' ? 'Is' : 'En'}`,
--- apps/web/utils/useUniversitySearch.tsx
+++ apps/web/utils/useUniversitySearch.tsx
@@ -1,5 +1,7 @@
 import Fuse from 'fuse.js'
 
+import { UniversityGatewayProgram } from '../graphql/schema'
+
 interface FilterProps {
   key: string
   value: Array<string>
@@ -12,9 +14,20 @@ interface SearchProductsProps {
   locale: string
 }
 
+interface FuseQueryResult {
+  item: UniversityGatewayProgram
+  refIndex: number
+  score: number
+}
+
 interface QueryMakerProps {
   $and: Array<any>
 }
+
+interface Bucket {
+  [roundedScore: string]: FuseQueryResult[]
+}
+
 export const SearchProducts = ({
   fuseInstance,
   query,
@@ -40,6 +53,68 @@ export const SearchProducts = ({
     queryMaker.$and.push({ $or: orFilters })
   })
   const result = fuseInstance.search(queryMaker)
+  const test = sortIntoBuckets(result as FuseQueryResult[])
+  return [...Object.values(test)].flat() || []
+}
 
-  return result
+// Function to sort and group objects into buckets
+const sortIntoBuckets = (dataArray: FuseQueryResult[]) => {
+  // Initialize an object to store buckets
+  const buckets: Bucket = {}
+
+  // Group objects into buckets based on the rounded score
+  dataArray.forEach((item) => {
+    const roundedScore = item.score.toFixed(2)
+
+    if (!buckets[roundedScore]) {
+      buckets[roundedScore] = []
+    }
+
+    buckets[roundedScore].push(item)
+  })
+
+  console.log('buckets', buckets)
+
+  const orderedBuckets: Bucket = {}
+
+  Object.values(buckets).forEach((bucket, index) => {
+    const universityGroups: Record<string, FuseQueryResult[]> = {}
+    bucket.forEach((item) => {
+      universityGroups[item.item.universityId] =
+        universityGroups[item.item.universityId] || []
+      universityGroups[item.item.universityId].push(item)
+    })
+
+    // Get an array of all keys in the object
+    const keys = Object.keys(universityGroups)
+
+    // Find the maximum length among all arrays
+    const maxLength = Math.max(
+      ...keys.map((key) => universityGroups[key].length),
+    )
+
+    const orderedList: FuseQueryResult[] = []
+    // Outer loop to iterate over each index up to the maximum length
+    for (let i = 0; i < maxLength; i++) {
+      // Inner loop to iterate over each key
+      for (let j = 0; j < keys.length; j++) {
+        const currentKey = keys[j]
+
+        // Check if the current index is within the bounds of the current array
+        if (i < universityGroups[currentKey].length) {
+          const currentItem = universityGroups[currentKey][i]
+
+          orderedList.push(currentItem)
+
+          // Your action goes here
+          // Example: Do something with the current key and item
+          // YourFunction(currentKey, currentItem);
+        }
+      }
+    }
+
+    orderedBuckets[index] = orderedList
+  })
+
+  return orderedBuckets
 }
