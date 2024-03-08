d99a284a32308726a77f6be47ce1c77c002e322e
Kristófer <kristofer@andes.is>
2024-03-08 16:59:52 +0000

    Revert "Better tags checker"
    This reverts commit b2044eade44cc4f7817c0ff7b01f464c5b67d37e.

--- scripts/check-tags.ts
+++ scripts/check-tags.ts
@@ -9,53 +9,45 @@ interface Project {
   tags: string[]
 }
 
-const hasValidTags = async (filePath: string) => {
+const checkTags = async (filePath: string) => {
   const projectText = await readFile(filePath, 'utf8')
   const project = JSON.parse(projectText) as Project
   const tagsRaw = project.tags ?? []
   const tags = tagsRaw.map((tag) => tag.split(':'))
-  // Only allow a single "scope:*" tag or a tuple of "scope:*" and "lib:*"
   const isNormal =
     (tags.length === 1 && tags[0][0] === 'scope') ||
     (tags.length === 2 &&
-      tags.find(([key]) => key === 'scope' || key === 'lib') &&
-      tags[0][1] === tags[1][1])
+      tags.find(([key]) => key === 'scope') &&
+      tags.find(([key]) => key === 'lib') &&
+      tags[0][1] === tags[0][1])
   const isEmpty = tags.length === 0
 
   if (isEmpty) {
     console.log(chalk.red.underline(filePath))
     console.log('Missing nx tags for project boundaries\n')
-    return false
+    return true
   } else if (!isNormal) {
     console.log(chalk.yellow.underline(filePath))
     console.log('Unexpected nx tags:', tagsRaw.join(','), '\n')
   }
 
-  return true
+  return false
 }
 
 const checkProjects = async () => {
   const projects = globSync('{apps,libs}/**/project.json', {
     ignore: '**/node_modules/**',
   })
-  let invalidProjects = []
+  let hasError = false
   for (const project of projects) {
-    // console.log("Checking project", project)
-    // Old
-    // hasError = hasError || (await checkTags(project))
-
-    // New, and faster
-    if (!(await hasValidTags(project).catch(() => false))) {
-      invalidProjects.push(project)
-    }
+    hasError = hasError || (await checkTags(project))
   }
 
-  if (invalidProjects.length > 0) {
-    console.log(chalk.red('Found errors in project files'))
+  if (hasError) {
+    console.log(chalk.red('Found errors'))
     console.log(
       'All projects should have a configured NX tags which controls which project can import what. For more information see: https://docs.devland.is/repository/nx-tags',
     )
-    console.log('Invalid projects:', invalidProjects)
     process.exit(1)
   }
 }
