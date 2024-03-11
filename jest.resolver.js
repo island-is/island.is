d2a14b70d87f75fbf9c9289128f1b30b344f4d5d
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2024-03-11 21:53:39 +0000

    Updating jest.resolver.js to match @nx/jest/plugins/resolver v16.9.0

--- jest.resolver.js
+++ jest.resolver.js
@@ -1,45 +1,39 @@
 // UPGRADE WARNING
-// Copied from @nx/jest/plugins/resolver.js v11.4.0
+// Copied from @nx/jest/plugins/resolver.ts v16.9.0 and converted to JS
 // Removed .css extension override to support vanilla-extract.
 // May need an update whenever @nx/jest changes.
 
-const path_1 = require('path')
-const fs = require('fs')
+const { dirname, extname, join } = require('path')
+const { resolve: resolveExports } = require('resolve.exports')
 const ts = require('typescript')
-const defaultResolver_1 = require('jest-resolve/build/defaultResolver')
-const { readProjectConfiguration, workspaceRoot } = require('@nx/devkit')
-/**
- * Nx UPGRADE WARNING - Nx 16.3.2:
- * This is a direct import as the FsTree is not a public API of Nx.
- * We are using FsTree to find the project configuration to read the project root.
- * This may break in future versions of Nx.
- */
-const { FsTree } = require('nx/src/generators/tree')
+
+let compilerSetup
 
 function getCompilerSetup(rootDir) {
   const tsConfigPath =
     ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.spec.json') ||
     ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.test.json') ||
     ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.jest.json')
+
   if (!tsConfigPath) {
     console.error(
       `Cannot locate a tsconfig.spec.json. Please create one at ${rootDir}/tsconfig.spec.json`,
     )
   }
+
   const readResult = ts.readConfigFile(tsConfigPath, ts.sys.readFile)
   const config = ts.parseJsonConfigFileContent(
     readResult.config,
     ts.sys,
-    path_1.dirname(tsConfigPath),
+    dirname(tsConfigPath),
   )
   const compilerOptions = config.options
   const host = ts.createCompilerHost(compilerOptions, true)
   return { compilerOptions, host }
 }
 
-let compilerSetup
 module.exports = function (path, options) {
-  const ext = path_1.extname(path)
+  const ext = extname(path)
   if (
     ext === '.scss' ||
     ext === '.sass' ||
@@ -48,24 +42,43 @@ module.exports = function (path, options) {
   ) {
     return require.resolve('identity-obj-proxy')
   }
-  // Try to use the defaultResolver
   try {
-    return defaultResolver_1.default(path, options)
-  } catch (e) {
-    // Fallback to using typescript
-    const fsTree = new FsTree(workspaceRoot, false)
-    const project = readProjectConfiguration(
-      fsTree,
-      process.env.NX_TASK_TARGET_PROJECT,
-    )
+    try {
+      // Try to use the defaultResolver with default options
+      return options.defaultResolver(path, options)
+    } catch {
+      // Try to use the defaultResolver with a packageFilter
+      return options.defaultResolver(path, {
+        ...options,
+        packageFilter: (pkg) => ({
+          ...pkg,
+          main: pkg.main || pkg.es2015 || pkg.module,
+        }),
+        pathFilter: (pkg) => {
+          if (!pkg.exports) {
+            return path
+          }
 
-    compilerSetup =
-      compilerSetup ||
-      getCompilerSetup(
-        (options.rootDir ?? options.basedir) + `/${project.root}`,
-      )
+          return resolveExports(pkg, path) || path
+        },
+      })
+    }
+  } catch (e) {
+    if (
+      path === 'jest-sequencer-@jest/test-sequencer' ||
+      path === '@jest/test-sequencer'
+    ) {
+      return
+    }
+    // Fallback to using typescript
+    ts = ts || require('typescript')
+    compilerSetup = compilerSetup || getCompilerSetup(options.rootDir)
     const { compilerOptions, host } = compilerSetup
-    return ts.resolveModuleName(path, options.basedir, compilerOptions, host)
-      .resolvedModule.resolvedFileName
+    return ts.resolveModuleName(
+      path,
+      join(options.basedir, 'fake-placeholder.ts'),
+      compilerOptions,
+      host,
+    ).resolvedModule.resolvedFileName
   }
 }
