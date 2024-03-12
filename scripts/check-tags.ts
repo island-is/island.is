0a248d7c3c140926193d119969be2377da7914a6
Kristófer <kristofer@andes.is>
2024-03-12 05:48:23 +0000

    Allow TODO tags

--- scripts/check-tags.ts
+++ scripts/check-tags.ts
@@ -25,7 +25,7 @@ const hasValidTags = async (filePath: string) => {
 
   const isEmpty = tags.length === 0
   // Only allow 'scope:*' or 'lib:*' tags
-  const validPrefix = tags.every(([key]) => key === 'scope' || key === 'lib')
+  const validPrefix = tags.every(([key]) => key === 'scope' || key === 'lib' || key === 'TODO')
   // Must have at least one scope tag or be empty
   const hasScopePrefix = tags.some(([key]) => key === 'scope') || isEmpty
   // Are all tags the same?
