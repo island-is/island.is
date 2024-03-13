a441a8fb07050611aa5e6aea10e4ab1bfa02afff
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-03-13 14:05:26 +0000

    fix: better seeder

--- libs/auth-api-lib/seeders/data/client-licenses-ust.ts
+++ libs/auth-api-lib/seeders/data/client-licenses-ust.ts
@@ -1,21 +1,18 @@
 import { compose, createClient, createScope } from './helpers'
 
 export const up = compose(
-  createScope({
-    name: '@island.is/licenses:hunting',
-    displayName: 'UST digital license update scope',
-    description:
-      'Veitir aðgang að uppfærsluvirkni á skírteinum útgefnum af UST',
-  }),
   createClient({
     clientId: '@ust.is/clients/licenses',
     clientType: 'machine',
     displayName: 'UST digital license machine client',
     description: 'Enables acccess to the license-api',
-    allowedScopes: [
-      'openid',
-      '@island.is/licenses:hunting',
-      '@island.is/licenses:verify',
-    ],
+    allowedScopes: ['openid', '@island.is/licenses:verify'],
+  }),
+  createScope({
+    name: '@island.is/licenses:hunting',
+    displayName: 'UST digital license update scope',
+    description:
+      'Veitir aðgang að uppfærsluvirkni á skírteinum útgefnum af UST',
+    addToClients: ['@ust.is/clients/licenses'],
   }),
 )
