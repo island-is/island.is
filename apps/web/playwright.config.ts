d5ad1a39fcad5bfcf1bcde3d4bc02118a65abab7
Kristófer <kristofer@andes.is>
2024-03-20 17:40:54 +0000

    Playwright config and timeout

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -4,12 +4,15 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 const localConfig: PlaywrightTestConfig = {
   testDir: './e2e',
+  timeout: 3 * 60 * 1000,
   webServer: {
     port: 4200,
     command:
       'yarn infra run-local-env --service=web --dependencies=api --proxies',
-    timeout: 180 * 1000, // 2 minutes for the slow web 🐌
+    timeout: 2 * 60 * 1000, // 2 minutes for the slow web 🐌
     reuseExistingServer: true,
+    stdout: 'pipe',
+    stderr: 'pipe',
   },
 }
 export default { ...playwrightPreset, ...localConfig }
