2e9b07539717b55ab4a011fa3c5b7d51da1291b0
Kristófer <kristofer@andes.is>
2024-03-20 13:23:16 +0000

    Added comment for easy project disabling

--- apps/system-e2e/playwright.config.ts
+++ apps/system-e2e/playwright.config.ts
@@ -42,7 +42,10 @@ export default defineConfig({
       ]
     : undefined,
 
-  /* Run your local dev server before starting the tests */ // webServer: {
+  // projects: undefined, // Disable preset recommendation
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
   //   command: 'npm run start',
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
