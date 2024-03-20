63d6b6e640035ec7bb34d18cc8d920d8d491ba60
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-03-20 09:10:01 +0000

    fix: log errors

--- libs/clients/smart-solutions-v2/src/lib/graphql/errorLink.ts
+++ libs/clients/smart-solutions-v2/src/lib/graphql/errorLink.ts
@@ -1,20 +1,12 @@
 import { onError, ErrorResponse } from '@apollo/client/link/error'
 
 export default onError(({ graphQLErrors, networkError }: ErrorResponse) => {
-  if (networkError) {
-    return
-  }
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    )
 
-  if (graphQLErrors) {
-    const errorCodes = graphQLErrors.map((err) => err.extensions?.['code'])
-    const errorMessage = graphQLErrors.map((err) => err.message)
-    if (
-      errorCodes.includes('UNAUTHENTICATED') ||
-      errorMessage.includes('Unauthorized')
-    ) {
-      return
-    } else if (errorCodes.includes('FORBIDDEN')) {
-      return
-    }
-  }
+  if (networkError) console.log(`[Network error]: ${networkError}`)
 })
