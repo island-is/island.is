1b66e7372dafedeefba99fc059bf177f9362b6ba
Jón Levy <levy@andes.is>
2024-03-21 20:55:36 +0000

    Revert "chore(tests): System-e2e to NX (#13968)" (#14249)
    This reverts commit b15c7549cabe6ccb86d5d533a16a05eb9e1bf2e6.

--- apps/system-e2e/entrypoint.sh
+++ apps/system-e2e/entrypoint.sh
@@ -23,7 +23,7 @@ echo "Playwright project: $TEST_PROJECT"
 echo "Playwright version: $(yarn playwright --version)"
 
 TEST_EXIT_CODE=0
-yarn playwright test -c src "$@" || TEST_EXIT_CODE=$?
+yarn playwright test -c src --project="$TEST_PROJECT" "$@" || TEST_EXIT_CODE=$?
 
 # Upload results
 if [[ -n "$TEST_RESULTS_S3" ]]; then
@@ -46,4 +46,4 @@ Additionally a web-based overview can be found at
   https://www.tesults.com/digital-iceland/monorepo
 EOF
 
-exit "$TEST_EXIT_CODE"
+exit $TEST_EXIT_CODE
--- apps/system-e2e/src/support/utils.ts
+++ apps/system-e2e/src/support/utils.ts
@@ -25,14 +25,6 @@ export function deleteMockPdf() {
 }
 
 // Set NODE_DEBUG=system-e2e in your environment when testing to show debug messages
-export const debug = (msg?: string | object, ...args: unknown[]) => {
-  if (msg) {
-    if (typeof msg === 'object') {
-      debuglog('system-e2e')('', msg, ...args)
-      return
-    }
-    debuglog('system-e2e')(msg, ...args)
-    return
-  }
-  debuglog('system-e2e')('debug enabled')
+export function debug(msg: string, ...args: unknown[]) {
+  debuglog('system-e2e')(msg, ...args)
 }
