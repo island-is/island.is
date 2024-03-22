332759ede6c873e0e85cd3ff6ab946343c415067
Jón Levy <levy@andes.is>
2024-03-22 09:01:27 +0000

    Revert "feat(web): Load service web form email config from cms instead of having it in the code (#14230)" (#14256)
    This reverts commit 3b4ace9635a9ebecb982f06ea05980b9edd9bd2a.

--- libs/cms/src/lib/models/serviceWebPage.model.ts
+++ libs/cms/src/lib/models/serviceWebPage.model.ts
@@ -7,21 +7,6 @@ import { safelyMapSliceUnion, SliceUnion } from '../unions/slice.union'
 import { FooterItem, mapFooterItem } from './footerItem.model'
 
 @ObjectType()
-class ServiceWebPageEmailConfigItem {
-  @Field(() => String)
-  supportCategoryId!: string
-
-  @CacheField(() => [String])
-  emailList!: string[]
-}
-
-@ObjectType()
-class ServiceWebPageEmailConfig {
-  @CacheField(() => [ServiceWebPageEmailConfigItem])
-  emails!: ServiceWebPageEmailConfigItem[]
-}
-
-@ObjectType()
 export class ServiceWebPage {
   @Field(() => ID)
   id!: string
@@ -37,28 +22,6 @@ export class ServiceWebPage {
 
   @CacheField(() => [FooterItem], { nullable: true })
   footerItems?: Array<FooterItem>
-
-  @CacheField(() => ServiceWebPageEmailConfig, { nullable: true })
-  emailConfig?: ServiceWebPageEmailConfig
-}
-
-const mapServiceWebPageEmailConfig = (
-  emailConfig: IServiceWebPage['fields']['emailConfig'],
-): ServiceWebPageEmailConfig => {
-  const emails = [] as ServiceWebPageEmailConfig['emails']
-
-  for (const { supportCategoryId, emailList } of emailConfig?.emails ?? []) {
-    if (supportCategoryId && Array.isArray(emailList)) {
-      emails.push({
-        emailList,
-        supportCategoryId,
-      })
-    }
-  }
-
-  return {
-    emails,
-  }
 }
 
 export const mapServiceWebPage = ({
@@ -72,7 +35,4 @@ export const mapServiceWebPage = ({
     : null,
   slices: (fields.slices ?? []).map(safelyMapSliceUnion).filter(Boolean),
   footerItems: (fields.footerItems ?? []).map(mapFooterItem),
-  emailConfig: fields.emailConfig
-    ? mapServiceWebPageEmailConfig(fields.emailConfig)
-    : { emails: [] },
 })
