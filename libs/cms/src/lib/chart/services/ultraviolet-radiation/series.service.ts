5d64e1aa3f18070e52d39b32e0f41c147f5a7d92
runarvestmann <runar@stefna.is>
2024-03-25 17:29:11 +0000

    Reuse filtering function

--- libs/cms/src/lib/chart/services/ultraviolet-radiation/series.service.ts
+++ libs/cms/src/lib/chart/services/ultraviolet-radiation/series.service.ts
@@ -6,6 +6,7 @@ import {
   ChartDataSourceExternalJsonProviderService,
 } from '../../types'
 import { isValidMeasurement } from './utils'
+import { getMultipleStatistics } from '@island.is/clients/statistics'
 
 @Injectable()
 export class UltravioletRadiationSeriesService
@@ -15,24 +16,45 @@ export class UltravioletRadiationSeriesService
     private readonly clientService: UltravioletRadiationClientService,
   ) {}
 
-  async getChartData(_: ChartDataInput): Promise<ChartDataOutput> {
+  async getChartData(input: ChartDataInput): Promise<ChartDataOutput> {
     const data = await this.clientService.getMeasurementSeries()
-    // TODO: check out how the data looks
+
+    const statistics =
+      data.body?.dataAll?.filter(isValidMeasurement).map(({ time, uvVal }) => ({
+        uvVal, // TODO: perhaps add this automatically in the chart.service.ts file
+        header: String(Date.parse(time)),
+        headerType: 'date',
+        statisticsForHeader: [
+          {
+            key: 'uvVal',
+            value: uvVal,
+          },
+        ],
+      })) ?? []
+
+    const filteredStatistics = await getMultipleStatistics(
+      { ...input, sourceDataKeys: ['uvVal'] },
+      {
+        data: {
+          uvVal: statistics.map((stat) => ({
+            header: stat.header,
+            value: stat.uvVal,
+          })),
+        },
+      },
+    )
+
+    // TODO: combine this logic, it's also here apps/web/components/Charts/v2/hooks/data.ts
     return {
-      statistics:
-        data.body?.dataAll
-          ?.filter(isValidMeasurement)
-          .map(({ time, uvVal }) => ({
-            uvVal, // TODO: perhaps add this automatically in the chart.service.ts file
-            header: String(Date.parse(time)),
-            headerType: 'date',
-            statisticsForHeader: [
-              {
-                key: 'uvVal',
-                value: uvVal,
-              },
-            ],
-          })) ?? [],
+      statistics: filteredStatistics.map((stat) => ({
+        ...stat,
+        ...stat.statisticsForHeader.reduce((prev, curr) => {
+          if (curr.value !== undefined) {
+            prev[curr.key] = curr.value
+          }
+          return prev
+        }, {} as Record<string, string | number | null>),
+      })),
     }
   }
 }
