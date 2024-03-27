7a58a322434ef86af744ebed4214c7ec9a9b3d61
Herdismaria <herdismaria90@gmail.com>
2024-03-27 09:36:09 +0000

    simplify wait function

--- apps/services/user-notification/src/app/modules/notifications/notificationsWorker/helpers.ts
+++ apps/services/user-notification/src/app/modules/notifications/notificationsWorker/helpers.ts
@@ -1,6 +1,3 @@
 export const wait = async (seconds = 2) => {
-  const start = Date.now()
-  while (Date.now() - start < seconds * 1_000) {
-    await new Promise((r) => setTimeout(r, 10))
-  }
+  await new Promise((resolve) => setTimeout(resolve, seconds * 1_000))
 }
