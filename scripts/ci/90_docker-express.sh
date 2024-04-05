c475901ee6e912d7bf37fa921b046b4c9e258598
Kristofer <kristofer@andes.is>
2024-04-05 14:55:59 +0000

    Update scripts/ci/90_docker-express.sh

--- scripts/ci/90_docker-express.sh
+++ scripts/ci/90_docker-express.sh
@@ -7,7 +7,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$DIR"/_common.sh
 
 # Building Docker images for ExpressJS-based apps
-if command -v podman &>/dev/null; then
+if { ! command -v docker &>/dev/null } && { command -v podman &>/dev/null }; then
   exec "$DIR"/_podman.sh Dockerfile output-express
 fi
 exec "$DIR"/_docker.sh Dockerfile output-express
