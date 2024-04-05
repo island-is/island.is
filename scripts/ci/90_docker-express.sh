41dbefd2097447b6d9f9dc76c193e759e85a54b0
Kristófer <kristofer@andes.is>
2024-04-05 17:44:24 +0000

    Reviewfix

--- scripts/ci/90_docker-express.sh
+++ scripts/ci/90_docker-express.sh
@@ -7,7 +7,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$DIR"/_common.sh
 
 # Building Docker images for ExpressJS-based apps
-if { ! command -v docker &>/dev/null } && { command -v podman &>/dev/null }; then
+if ! command -v docker &>/dev/null && command -v podman &>/dev/null; then
+  # Use podan only if docker is missing and is present
   exec "$DIR"/_podman.sh Dockerfile output-express
 fi
+# Optimistically assume docker is installed
 exec "$DIR"/_docker.sh Dockerfile output-express
