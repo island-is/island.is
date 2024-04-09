20ba233275952650a3947023d980d66d268ac2f7
Ólafur Björn Magnússon <obmagnusson@gmail.com>
2024-04-09 11:07:18 +0000

    chore(gbh): Extend inreview lifetime (#14409)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/grindavik-housing-buyout/src/lib/GrindavikHousingBuyoutTemplate.ts
+++ libs/application/templates/grindavik-housing-buyout/src/lib/GrindavikHousingBuyoutTemplate.ts
@@ -20,6 +20,7 @@ import { States, TWENTY_FOUR_HOURS_IN_MS } from './constants'
 import {
   DefaultStateLifeCycle,
   coreHistoryMessages,
+  pruneAfterDays,
 } from '@island.is/application/core'
 import { Features } from '@island.is/feature-flags'
 import { application, states } from './messages'
@@ -142,7 +143,7 @@ const GrindavikHousingBuyoutTemplate: ApplicationTemplate<
         meta: {
           status: FormModes.IN_PROGRESS,
           name: application.general.name.defaultMessage,
-          lifecycle: DefaultStateLifeCycle,
+          lifecycle: pruneAfterDays(90),
           onEntry: defineTemplateApi({
             action: 'submitApplication',
             shouldPersistToExternalData: true,
