39343a319556958fa48d7da04b2bde7bd75a5fd3
Valur Einarsson <valure@fuglar.com>
2024-04-11 14:45:58 +0100

    Use logger.

--- apps/services/sessions/src/app/cleanup/cleanup-worker.ts
+++ apps/services/sessions/src/app/cleanup/cleanup-worker.ts
@@ -1,21 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 
+import { logger } from '@island.is/logging'
+
 import { SessionsCleanupWorkerModule } from './cleanup-worker.module'
 import { SessionsCleanupService } from './cleanup-worker.service'
 
 export const worker = async () => {
   try {
-    console.log('Sessions cleanup worker started.')
+    logger.info('Sessions cleanup worker started.')
     const app = await NestFactory.createApplicationContext(
       SessionsCleanupWorkerModule,
     )
     app.enableShutdownHooks()
     await app.get(SessionsCleanupService).run()
     await app.close()
-    console.log('Sessions cleanup worker finished successfully.')
+    logger.info('Sessions cleanup worker finished successfully.')
     process.exit(0)
   } catch (error) {
-    console.error('Sessions cleanup worker encountered an error:', error)
+    logger.error('Sessions cleanup worker encountered an error:', error)
     process.exit(1)
   }
 }
