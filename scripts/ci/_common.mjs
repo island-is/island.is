21ac3863236d98203c7b4310bdcf8556c6df1d21
andes-it <builders@andes.is>
2024-04-12 12:22:09 +0000

    chore: nx format:write update dirty files

--- scripts/ci/_common.mjs
+++ scripts/ci/_common.mjs
@@ -1,12 +1,11 @@
-import { readFile } from "node:fs/promises";
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { readFile } from 'node:fs/promises'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-export const ROOT = resolve(__dirname, "..", "..");
+const __dirname = dirname(fileURLToPath(import.meta.url))
+export const ROOT = resolve(__dirname, '..', '..')
 
-export async function getPackageJSON(filePath = resolve(ROOT, "package.json")) {
-    const content = JSON.parse(await readFile(filePath, 'utf-8'));
-    return content;
-}  
-
+export async function getPackageJSON(filePath = resolve(ROOT, 'package.json')) {
+  const content = JSON.parse(await readFile(filePath, 'utf-8'))
+  return content
+}
--- scripts/ci/get-node-version.mjs
+++ scripts/ci/get-node-version.mjs
@@ -1,51 +1,61 @@
 #!/usr/bin/env node
-import fetch from 'node-fetch';
-import { resolve } from 'path';
+import fetch from 'node-fetch'
+import { resolve } from 'path'
 
-import { ROOT, getPackageJSON } from "./_common.mjs";
+import { ROOT, getPackageJSON } from './_common.mjs'
 
-
-const nodeVersion = await getPackageVersion();
-const version = await getVersion(nodeVersion);
+const nodeVersion = await getPackageVersion()
+const version = await getVersion(nodeVersion)
 if (!version) {
-  process.stderr.write(`Failed getting docker image for ${nodeVersion}`);
-  process.exit(1);
+  process.stderr.write(`Failed getting docker image for ${nodeVersion}`)
+  process.exit(1)
 }
-process.stdout.write(version);
+process.stdout.write(version)
 
-async function getVersion(version, withAlpine = true, architecture = 'amd64', url = null) {
+async function getVersion(
+  version,
+  withAlpine = true,
+  architecture = 'amd64',
+  url = null,
+) {
   try {
-    const baseURL = url ?? 'https://hub.docker.com/v2/repositories/library/node/tags?page_size=100'
-    const response = await fetch(baseURL);
-    const data = await response.json();
+    const baseURL =
+      url ??
+      'https://hub.docker.com/v2/repositories/library/node/tags?page_size=100'
+    const response = await fetch(baseURL)
+    const data = await response.json()
 
     const filteredTags = data.results.filter((tag) => {
-      const isVersionMatch = tag.name.startsWith(version); 
-      const isAlpine = withAlpine ? tag.name.includes('alpine') : true;
-      const isArchitectureMatch = tag.images.some((image) => image.architecture === architecture);
-      return isVersionMatch && isAlpine && isArchitectureMatch;
-    });
+      const isVersionMatch = tag.name.startsWith(version)
+      const isAlpine = withAlpine ? tag.name.includes('alpine') : true
+      const isArchitectureMatch = tag.images.some(
+        (image) => image.architecture === architecture,
+      )
+      return isVersionMatch && isAlpine && isArchitectureMatch
+    })
 
-    const latestTag = filteredTags.sort((a, b) => b.last_updated.localeCompare(a.last_updated))[0];
+    const latestTag = filteredTags.sort((a, b) =>
+      b.last_updated.localeCompare(a.last_updated),
+    )[0]
 
     if (latestTag) {
-      return latestTag.name;
+      return latestTag.name
     }
-    const nextUrl = data.next;
+    const nextUrl = data.next
     if (!nextUrl) {
-      return null;
+      return null
     }
-    return getVersion(version, withAlpine, architecture, nextUrl);
+    return getVersion(version, withAlpine, architecture, nextUrl)
   } catch (error) {
-    console.error('Failed to fetch the Docker tags', error);
+    console.error('Failed to fetch the Docker tags', error)
   }
 }
 
-async function getPackageVersion(filePath = resolve(ROOT, "package.json")) {
-  const content = await getPackageJSON();
-  const version = content.engines?.node;
+async function getPackageVersion(filePath = resolve(ROOT, 'package.json')) {
+  const content = await getPackageJSON()
+  const version = content.engines?.node
   if (!version) {
-    throw new Error(`Cannot find node version`);
+    throw new Error(`Cannot find node version`)
   }
-  return version;
+  return version
 }
