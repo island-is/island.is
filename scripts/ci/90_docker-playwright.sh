a7789bfba2f03d73b21a89e92fa1ac86574a36be
Kristófer <kristofer@andes.is>
2024-04-16 15:12:15 +0000

    squash-cherry-pick #14094

--- scripts/ci/90_docker-playwright.sh
+++ scripts/ci/90_docker-playwright.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
+if [[ -n "${DEBUG:-}" || -n "${CI:-}" ]]; then set -x; fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck disable=SC1091
-source "$DIR"/_common.sh
+. "$DIR"/_common.sh
 
 # Building Docker images for Cypress-based apps, pass extra args to use in docker cmds if needed
 exec "$DIR"/_docker.sh Dockerfile output-playwright
