c26594a72eddae06ea3e85b9629618a613bde407
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-04-16 09:00:46 +0000

    Messages and extract strings to project.json

--- libs/application/templates/healthcare-work-permit/src/lib/messages/confirmation.ts
+++ libs/application/templates/healthcare-work-permit/src/lib/messages/confirmation.ts
@@ -14,12 +14,12 @@ export const confirmation = {
     },
     alertTitle: {
       id: 'hwp.application:confirmation.general.alertTitle',
-      defaultMessage: 'Vottorð vegna starfsleyfis hefur verið útbúið / sent',
+      defaultMessage: 'Starfsleyfi hefur verið útbúið / sent',
       description: 'Confirmation alert title',
     },
     alertMessage: {
       id: 'hwp.application:confirmation.general.alertMessage',
-      defaultMessage: 'Þú getur nálgast vottorðið á mínum síðum á island.is',
+      defaultMessage: 'Þú getur nálgast starfsleyfið á mínum síðum á island.is',
       description: 'Confirmation alert message',
     },
   }),
