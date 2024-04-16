a6cf94c1bfa004e2f08274b9b494083d7acf66f3
Kristófer <kristofer@andes.is>
2024-04-16 13:41:52 +0000

    Merge _podman.sh and _docker.sh and lint

--- scripts/ci/_docker.sh
+++ scripts/ci/_docker.sh
@@ -5,25 +5,25 @@ if [[ -n "${DEBUG:-}" || -n "${CI:-}" ]]; then set -x; fi
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck disable=SC1091
-source "$DIR"/_common.sh
+source "${DIR}"/_common.sh
 
-APP_HOME=$(yarn nx show project $APP | jq ".root" -r)
-APP_DIST_HOME=$(yarn nx show project $APP | jq ".targets.build.options | .outputPath // .outputDir" -r)
-DOCKERFILE=${1:-Dockerfile}
-TARGET=${TARGET:-${2:-'<You need to set a target (e.g. output-local, output-jest)>'}}
-ACTION=${3:-docker_build}
+APP_HOME="$(yarn nx show project "${APP}" | jq ".root" -r)"
+APP_DIST_HOME="$(yarn nx show project "${APP}" | jq ".targets.build.options | .outputPath // .outputDir" -r)"
+DOCKERFILE="${1:-Dockerfile}"
+TARGET="${2:-${TARGET:-'<You need to set a target (e.g. output-local, output-jest)>'}}"
+ACTION="${3:-docker_build}"
 PLAYWRIGHT_VERSION="$(yarn info --json @playwright/test | jq -r '.children.Version')"
-CONTAINER_BUILDER=${CONTAINER_BUILDER:-docker}
+CONTAINER_BUILDER="${CONTAINER_BUILDER:-docker}"
 DOCKER_LOCAL_CACHE="${DOCKER_LOCAL_CACHE:-true}"
+: "${LOCAL_CACHE:=true}"
 
 BUILD_ARGS=()
 
-mkargs() {
-  local local_cache="${1:-local-cache=yes}"
+set-containerer-args() {
   BUILD_ARGS=(
     --platform=linux/amd64
-    --file="${DIR}/$DOCKERFILE"
-    --target="$TARGET"
+    --file="${DIR}/${DOCKERFILE}"
+    --target="${TARGET}"
     "${PUBLISH_TO_REGISTRY[@]}"
     --build-arg="APP=${APP}"
     --build-arg="APP_HOME=${APP_HOME}"
@@ -32,25 +32,25 @@ mkargs() {
     --build-arg="PLAYWRIGHT_VERSION=${PLAYWRIGHT_VERSION}"
   )
   for extra_arg in ${EXTRA_DOCKER_BUILD_ARGS:-}; do
-    BUILD_ARGS+=("$extra_arg")
+    BUILD_ARGS+=("${extra_arg}")
   done
-  if [[ "${local_cache}" =~ local-cache=(yes|y|true) ]] && [[ "${DOCKER_LOCAL_CACHE}" == true ]]; then
-    BUILD_ARGS+=(--cache-from="type=local,src=$PROJECT_ROOT/cache")
-    BUILD_ARGS+=(--cache-from="type=local,src=$PROJECT_ROOT/cache_output")
+  if [[ "${LOCAL_CACHE}" == true ]]; then
+    BUILD_ARGS+=(--cache-from="type=local,src=${PROJECT_ROOT}/cache")
+    BUILD_ARGS+=(--cache-from="type=local,src=${PROJECT_ROOT}/cache_output")
   fi
 }
 
 container_build() {
-  $CONTAINER_BUILDER buildx build "${BUILD_ARGS[@]}" "$PROJECT_ROOT"
+  "${CONTAINER_BUILDER}" buildx build "${BUILD_ARGS[@]}" "${PROJECT_ROOT}"
 }
 
 docker_build() {
-  mkargs local-cache=true
+  set-containerer-args local-cache=true
   container_build docker
 }
 
 _set_publish() {
-  case $PUBLISH in
+  case "${PUBLISH}" in
   true)
     PUBLISH_TO_REGISTRY=(--push)
     ;;
@@ -69,7 +69,11 @@ main() {
   eval "${ACTION}"
 }
 
-# Exit if not directly run
-return 2>/dev/null || true
+# Set podman-specific config (if `docker` is missing, but `podman` is present)
+if ! command -v docker >/dev/null && command -v podman >/dev/null; then
+  export CONTAINER_BUILDER=podman LOCAL_CACHE=false
+fi
+
 _set_publish
-main "$@"
+set-containerer-args
+main "${@}"
