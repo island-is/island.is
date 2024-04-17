757c02764eed11e30b370e142958b95a1dd5ee8a
Ívar Oddsson <ivaroddsson@gmail.com>
2024-04-17 13:28:43 +0000

    Remove modified column from date-log table

--- apps/judicial-system/backend/migrations/20240409142123-create-dates.js
+++ apps/judicial-system/backend/migrations/20240409142123-create-dates.js
@@ -22,11 +22,6 @@ module.exports = {
               type: Sequelize.STRING,
               allowNull: false,
             },
-            modified: {
-              type: 'TIMESTAMP WITH TIME ZONE',
-              defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-              allowNull: false,
-            },
             case_id: {
               type: Sequelize.UUID,
               references: {
