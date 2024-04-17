d1e488339afd338f669163664c2d080502b3cede
Kristofer <kristofer@andes.is>
2024-04-17 12:55:38 +0000

    reviewfix

--- apps/system-e2e/e2e/example.spec.ts
+++ apps/system-e2e/e2e/example.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 
+// Very simple dummy test to verify that tests in `e2e/` also pass,
+// also used for a _minimal_ tests to verify that Playwright works at all
 test('has title', async ({ page }) => {
   await page.goto('/')
 
