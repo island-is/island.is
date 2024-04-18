bae1d1b398fd671c74ba5db6091b7e000a99ab2c
Kristófer <kristofer@andes.is>
2024-04-18 14:28:43 +0000

    Remove unused code

--- apps/system-e2e/playwright.config.ts
+++ apps/system-e2e/playwright.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from '@playwright/test'
 import { nxE2EPreset } from '@nx/playwright/preset'
 import { urls } from './src/support/urls'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 // For CI, you may want to set BASE_URL to the deployed application.
 // const baseURL = process.env['BASE_URL'] || 'http://localhost:3000'
 const baseURL: string = urls.islandisBaseUrl
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -43,47 +36,4 @@ export default defineConfig({
     : undefined,
 
   projects: undefined, // Disable preset recommendation
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-  // projects: [
-  //   {
-  //     name: 'chromium',
-  //     use: { ...devices['Desktop Chrome'] },
-  //   },
-  //
-  //   {
-  //     name: 'firefox',
-  //     use: { ...devices['Desktop Firefox'] },
-  //   },
-  //
-  //   {
-  //     name: 'webkit',
-  //     use: { ...devices['Desktop Safari'] },
-  //   },
-  //
-  //   // Uncomment for mobile browsers support
-  //   /* {
-  //     name: 'Mobile Chrome',
-  //     use: { ...devices['Pixel 5'] },
-  //   },
-  //   {
-  //     name: 'Mobile Safari',
-  //     use: { ...devices['iPhone 12'] },
-  //   }, */
-  //
-  //   // Uncomment for branded browsers
-  //   /* {
-  //     name: 'Microsoft Edge',
-  //     use: { ...devices['Desktop Edge'], channel: 'msedge' },
-  //   },
-  //   {
-  //     name: 'Google Chrome',
-  //     use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-  //   } */
-  // ],
 })
