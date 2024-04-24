b5b7d28e4ae5a87c5d61a57f61d67c4f34e5e72d
Þórður Hafliðason <tolleinn@gmail.com>
2024-04-24 10:29:45 +0000

    Add housing benefits scope

---
+++ libs/auth-api-lib/seeders/data/scope-housing-benefits.ts
@@ -0,0 +1,15 @@
+import { compose, createScope } from './helpers'
+
+export const up = compose(
+  createScope({
+    name: '@hms.is/housing-benefits',
+    displayName: 'Húsnæðisbætur',
+    description: 'Bakendascope fyrir húsnæðisbætur',
+    addToClients: ['@island.is/clients/api'],
+    accessControlled: false,
+    delegation: {
+      custom: true,
+      legalGuardians: false,
+    },
+  }),
+)
