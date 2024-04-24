5f92d533c504583997971f0d6c54441d33760c7d
Hyo-Sam Nandkisore <hyo-sam.nandkisore@advania.is>
2024-04-24 15:56:46 +0000

    removed form-builder app

--- apps/form-builder/components/MainContent/components/Preview/components/PropertyNumber/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PropertyNumberList } from './PropertyNumberList'
-export { default as PropertyNumberInput } from './PropertyNumberInput'
-export { default as PropertyNumberCombined } from './PropertyNumberCombined'
--- apps/form-builder/screens/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as Forms } from './Forms'
-export { default as Form } from './Form'
