db1094a0d251267717c407f8f57872728471f3e7
Guðjón Guðjónsson <gudjon@kolibri.is>
2024-04-30 15:27:57 +0000

    Cleans up string ids

--- apps/judicial-system/web/src/components/InfoCard/InfoCard.strings.ts
+++ apps/judicial-system/web/src/components/InfoCard/InfoCard.strings.ts
@@ -21,18 +21,18 @@ export const infoCardActiveIndictment = defineMessages({
 
 export const infoCardCaseScheduled = defineMessages({
   scheduled: {
-    id: 'judicial.system.indictments:overview.scheduled',
+    id: 'judicial.system.core:info_card_case_scheduled.scheduled',
     defaultMessage: 'Á dagskrá',
     description: 'Notaður sem texti sem tilgreinir að málið sé á dagskrá',
   },
   postponed: {
-    id: 'judicial.system.indictments:overview.postponed',
+    id: 'judicial.system.core:info_card_case_scheduled.postponed',
     defaultMessage: 'Frestað',
     description:
       'Notaður sem texti sem tilgreinir að málinu hafi verið sé frestað',
   },
   courtRoom: {
-    id: 'judicial.system.indictments:overview.court_room',
+    id: 'judicial.system.core:info_card_case_scheduled.court_room',
     defaultMessage:
       '{courtRoom, select, NONE {Dómsalur hefur ekki verið skráður} other {Dómsalur {courtRoom}}}',
     description: 'Notaður sem texti sem tilgreinir hvaða dómsalur er skráður',
