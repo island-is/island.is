c06d58a12bec0b96f4c171370837a46d110b07ec
unakb <una@kolibri.is>
2024-05-02 18:01:43 +0000

    Update index.ts

--- apps/judicial-system/web/src/components/index.ts
+++ apps/judicial-system/web/src/components/index.ts
@@ -62,7 +62,6 @@ export { default as SignedDocument } from './SignedDocument/SignedDocument'
 export { default as OverviewHeader } from './OverviewHeader/OverviewHeader'
 export { default as TagAppealState } from './TagAppealState/TagAppealState'
 export { default as TagCaseState } from './TagCaseState/TagCaseState'
-export { default as TagIndictmentState } from './TagIndictmentState/TagIndictmentState'
 
 export {
   SigningModal,
