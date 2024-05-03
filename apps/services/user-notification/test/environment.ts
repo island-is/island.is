80621b49974ae245fad7b9af0eab6ca64e74b19e
Sævar Már Atlason <54210288+saevarma@users.noreply.github.com>
2024-05-03 13:04:23 +0000

    refactor(user-notification): Refactor user notification to use the config object in services. Set emailFromAddress in config. (#14664)
    * Refactor few providers to the config.
    Add emailFromAddress to config.
    Add global queue setup to prevent concurrent race condition between test files on queue creation.
    
    * chore: charts update dirty files
    
    * Add try/catch around sqs setup to log hint to developer about sqs setup failed.
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/services/user-notification/test/environment.ts
+++ apps/services/user-notification/test/environment.ts
@@ -1,6 +1,13 @@
-process.env.MAIN_QUEUE_NAME = 'test-main'
-process.env.DEAD_LETTER_QUEUE_NAME = 'test-failure'
-process.env.SQS_REGION = 'eu-west-1'
-process.env.SQS_ACCESS_KEY = 'testing'
-process.env.SQS_SECRET_ACCESS_KEY = 'testing'
-process.env.SERVICE_PORTAL_CLICK_ACTION_URL = 'https://island.is/minarsidur'
+export const environment = {
+  MAIN_QUEUE_NAME: 'test-main',
+  DEAD_LETTER_QUEUE_NAME: 'test-failure',
+  SQS_REGION: 'eu-west-1',
+  SQS_ACCESS_KEY: 'testing',
+  SQS_SECRET_ACCESS_KEY: 'testing',
+  SERVICE_PORTAL_CLICK_ACTION_URL: 'https://island.is/minarsidur',
+} as const
+
+process.env = {
+  ...process.env,
+  ...environment,
+}
