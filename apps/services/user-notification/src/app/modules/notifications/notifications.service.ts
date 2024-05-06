2b83cc35a713a9fff073f1efc4322c536ddeb434
Rafn Árnason <rafnarnason@gmail.com>
2024-05-06 15:12:24 +0000

    cleanup

--- apps/services/user-notification/src/app/modules/notifications/notifications.service.ts
+++ apps/services/user-notification/src/app/modules/notifications/notifications.service.ts
@@ -498,7 +498,6 @@ export class NotificationsService {
   }
 
   async markAllAsRead(user: User): Promise<void> {
-    console.log('markAllAsRead********************************')
     try {
       await this.notificationModel.update(
         { read: true },
