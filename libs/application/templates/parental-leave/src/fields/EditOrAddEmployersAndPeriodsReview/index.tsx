88e2fc5c5488610b5183911f939bedf4aa14ba0a
ylfahfa <yhafsteinsdottir@deloitte.is>
2024-05-06 10:22:54 +0000

    format

--- libs/application/templates/parental-leave/src/fields/EditOrAddEmployersAndPeriodsReview/index.tsx
+++ libs/application/templates/parental-leave/src/fields/EditOrAddEmployersAndPeriodsReview/index.tsx
@@ -24,7 +24,9 @@ const EditOrAddEmployersAndPeriodsReview: FC<
   React.PropsWithChildren<ReviewScreenProps>
 > = ({ application, goToScreen }) => {
   const { formatMessage } = useLocale()
-  const { addEmployer, addPeriods, changeEmployerFile } = getApplicationAnswers(application.answers)
+  const { addEmployer, addPeriods, changeEmployerFile } = getApplicationAnswers(
+    application.answers,
+  )
 
   const childProps = {
     application,
