fa2a17372f52f802f54d86bdd34b770dbedb21dc
andes-it <builders@andes.is>
2024-05-10 10:49:48 +0000

    chore: nx format:write update dirty files

--- apps/web/components/Charts/v2/utils/chart.tsx
+++ apps/web/components/Charts/v2/utils/chart.tsx
@@ -78,7 +78,11 @@ export const getCartesianGridComponents = ({
 
   const xAxisFormatter = tickFormatter
   const yAxisFormatter = (v: string | number) =>
-    formatValueForPresentation(activeLocale, v, slice.reduceAndRoundValue ?? true)
+    formatValueForPresentation(
+      activeLocale,
+      v,
+      slice.reduceAndRoundValue ?? true,
+    )
 
   return [
     <CartesianGrid
