5423000b79237ffac7d664306f06260765410e4a
Sigrún Tinna Gissurardóttir <sigrun.tinna.gissurardottir@origo.is>
2024-05-15 13:53:20 +0000

    changes to reject conclusion screen

--- libs/application/templates/id-card/src/lib/messages/confirmation.ts
+++ libs/application/templates/id-card/src/lib/messages/confirmation.ts
@@ -59,3 +59,42 @@ export const reviewConfirmation = {
     },
   }),
 }
+
+export const reviewRejection = {
+  general: defineMessages({
+    sectionTitle: {
+      id: 'id.application:reviewRejection.general.sectionTitle',
+      defaultMessage: 'Umsókn hafnað',
+      description: 'Review confirmation section title',
+    },
+    alertTitle: {
+      id: 'id.application:reviewRejection.general.alertTitle',
+      defaultMessage: `Þú hefur hafnað umsókn um nafnskírteini með ferðaheimild fyrir {name}.`,
+      description: 'Review confirmation alert title',
+    },
+    accordionTitle: {
+      id: 'id.application:reviewRejection.general.accordionTitle',
+      defaultMessage: 'Hvað gerist næst?',
+      description: 'Review confirmation accordion title',
+    },
+    accordionText: {
+      id: 'id.application:reviewRejection.general.accordionText',
+      defaultMessage: `* Umsækjandi, {guardian1}, fær tilkynningu um að umsókninni 
+      hafi verið hafnað.
+      \n* Et aliquet commodo vel et et netus. Pellentesque commodo 
+      porttitor malesuada tellus mi et non. Tincidunt leo vivamus 
+      egestas sed adipiscing cursus feugiat habitant. Facilisi 
+      iaculis ut leo tortor imperdiet erat. Volutpat bibendum 
+      faucibus integer habitant. Dolor.
+      \n* Augue fames nulla ipsum ultrices non. Ornare nisi amet 
+      aliquet ullamcorper nunc.`,
+      description: 'Review confirmation accordion text',
+    },
+    bottomButtonMessage: {
+      id: 'id.application:reviewRejection.general.bottomButtonMessage',
+      defaultMessage:
+        'Á mínum síðum og í appi eru nú margvíslegar upplýsingar s.s. stafrænt pósthólf, þínar upplýsingar, fjármál, umsóknir, menntun, ökutæki, skírteini, starfsleyfi o.fl.',
+      description: 'Review confirmation bottom button message',
+    },
+  }),
+}
