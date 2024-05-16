e06b1259006ddfd484d273f2081130369afcfdfe
snaerseljan <snaer@aranja.com>
2024-05-16 10:08:35 +0000

    Added general problem component and translation hook wrapper

---
+++ apps/native/app/src/ui/lib/problem/problem.tsx
@@ -0,0 +1,96 @@
+import { useEffect } from 'react'
+import { useTranslate } from '../../../hooks/use-translate'
+import { useOfflineStore } from '../../../stores/offline-store'
+import { ProblemTemplate } from './problem-template'
+
+enum ProblemTypes {
+  internalServiceError = 'internal_service_error',
+  noData = 'no_data',
+}
+
+type ProblemBaseProps = {
+  /**
+   * Type of problem
+   * @default 'internal_service_error'
+   * 'internal_service_error' is a generic error that is not caused by the user
+   * 'not_found' is a 404 error, i.e. the page is not found
+   * 'no_data' is a 200 response, i.e. no data
+   */
+  type?: `${ProblemTypes}`
+  error?: Error
+  title?: string
+  message?: string
+  tag?: string
+  logError?: boolean
+}
+
+interface InternalServiceErrorProps extends ProblemBaseProps {
+  type?: 'internal_service_error'
+  error?: Error
+  title?: string
+  message?: string
+}
+
+interface NoDataProps extends ProblemBaseProps {
+  type: 'no_data'
+  error?: never
+  title?: string
+  message?: string
+}
+
+type ProblemProps = InternalServiceErrorProps | NoDataProps
+
+export const Problem = ({
+  type = ProblemTypes.internalServiceError,
+  error,
+  title,
+  message,
+  tag,
+  logError = false,
+}: ProblemProps) => {
+  const t = useTranslate()
+  const { isConnected } = useOfflineStore()
+
+  const fallbackProps = {
+    title: title ?? t('problem.error.title'),
+    message: message ?? t('problem.error.message'),
+    tag: tag ?? t('problem.error.tag'),
+    variant: 'error',
+  } as const
+
+  useEffect(() => {
+    if (logError && error) {
+      console.error(error)
+    }
+  }, [logError, error])
+
+  // When offline prioritize showing offline template
+  if (!isConnected) {
+    return (
+      <ProblemTemplate
+        showIcon
+        variant="warning"
+        title={title ?? t('problem.offline.title')}
+        message={message ?? t('problem.offline.message')}
+      />
+    )
+  }
+
+  switch (type) {
+    case ProblemTypes.internalServiceError:
+      return <ProblemTemplate {...fallbackProps} />
+
+    case ProblemTypes.noData:
+      return (
+        <ProblemTemplate
+          tag={tag}
+          variant="info"
+          title={title ?? t('problem.noData.title')}
+          message={message ?? t('problem.noData.message')}
+        />
+      )
+
+    default:
+      return <ProblemTemplate {...fallbackProps} />
+  }
+}
