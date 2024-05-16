e098beb796801e062ddfc604bfb8194d83f50897
helgifr <helgifreyr95@gmail.com>
2024-05-16 14:14:34 +0000

    Update libs/application/ui-fields/src/lib/SliderFormField/SliderFormField.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- libs/application/ui-fields/src/lib/SliderFormField/SliderFormField.tsx
+++ libs/application/ui-fields/src/lib/SliderFormField/SliderFormField.tsx
@@ -4,11 +4,13 @@ import { Controller, useFormContext } from 'react-hook-form'
 import { Slider } from '@island.is/application/ui-components'
 import { getValueViaPath } from '@island.is/application/core'
 
+type SliderFormFieldProps = {
+  field: Omit<SliderField, 'type' | 'component' | 'title' | 'children'>
+  application: Application
+}
+
 export const SliderFormField: FC<
-  React.PropsWithChildren<{
-    field: Omit<SliderField, 'type' | 'component' | 'title' | 'children'>
-    application: Application
-  }>
+  React.PropsWithChildren<SliderFormFieldProps>
 > = ({ application, field }) => {
   const { clearErrors } = useFormContext()
   return (
@@ -16,16 +18,31 @@ export const SliderFormField: FC<
       name={field.id}
       defaultValue={
         Number(getValueViaPath(application.answers, field.id)) ||
-        field.currentIndex
+        field.currentIndex ||
+        field.min
       }
       render={({ field: { onChange, value } }) => (
         <Slider
-          {...field}
+          min={field.min}
+          max={field.max}
+          step={field.step}
+          snap={field.snap}
+          trackStyle={field.trackStyle}
+          calculateCellStyle={field.calculateCellStyle}
+          showLabel={field.showLabel}
+          showMinMaxLabels={field.showMinMaxLabels}
+          showRemainderOverlay={field.showRemainderOverlay}
+          showProgressOverlay={field.showProgressOverlay}
+          showToolTip={field.showToolTip}
+          label={field.label}
+          rangeDates={field.rangeDates}
+          currentIndex={value}
           onChange={(val) => {
             clearErrors(field.id)
             onChange(val)
           }}
-          currentIndex={value}
+          onChangeEnd={field.onChangeEnd}
+          labelMultiplier={field.labelMultiplier}
         />
       )}
     />
