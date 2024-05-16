97508aa00e2a649a97ef1ecc29e2668cc62fe8f1
Ívar Oddsson <ivaroddsson@gmail.com>
2024-05-16 23:56:09 +0000

    Refactor

--- apps/judicial-system/backend/src/app/modules/notification/notification.service.ts
+++ apps/judicial-system/backend/src/app/modules/notification/notification.service.ts
@@ -1194,6 +1194,50 @@ export class NotificationService {
   //#endregion
 
   //#region MODIFIED notifications
+  private async sendModifiedNotificationToDefender(
+    subject: string,
+    theCase: Case,
+    user: User,
+  ): Promise<Recipient> {
+    return this.sendEmail(
+      subject,
+      theCase.isCustodyIsolation
+        ? this.formatMessage(notifications.modified.isolationHtmlDefender, {
+            caseType: theCase.type,
+            actorInstitution: user.institution?.name,
+            actorName: user.name,
+            actorTitle: user.title,
+            courtCaseNumber: theCase.courtCaseNumber,
+            defenderHasAccessToRvg: Boolean(theCase.defenderNationalId),
+            linkStart: `<a href="${formatDefenderRoute(
+              this.config.clientUrl,
+              theCase.type,
+              theCase.id,
+            )}">`,
+            linkEnd: '</a>',
+            validToDate: formatDate(theCase.validToDate, 'PPPp'),
+            isolationToDate: formatDate(theCase.isolationToDate, 'PPPp'),
+          })
+        : this.formatMessage(notifications.modified.htmlDefender, {
+            caseType: theCase.type,
+            actorInstitution: user.institution?.name,
+            actorName: user.name,
+            actorTitle: user.title,
+            courtCaseNumber: theCase.courtCaseNumber,
+            defenderHasAccessToRvg: Boolean(theCase.defenderNationalId),
+            linkStart: `<a href="${formatDefenderRoute(
+              this.config.clientUrl,
+              theCase.type,
+              theCase.id,
+            )}">`,
+            linkEnd: '</a>',
+            validToDate: formatDate(theCase.validToDate, 'PPPp'),
+          }),
+      theCase.defenderName,
+      theCase.defenderEmail,
+    )
+  }
+
   private async sendModifiedNotifications(
     theCase: Case,
     user: User,
@@ -1283,46 +1327,8 @@ export class NotificationService {
     }
 
     if (theCase.defenderEmail) {
-      const defenderHtml = theCase.isCustodyIsolation
-        ? this.formatMessage(notifications.modified.isolationHtmlDefender, {
-            caseType: theCase.type,
-            actorInstitution: user.institution?.name,
-            actorName: user.name,
-            actorTitle: user.title,
-            courtCaseNumber: theCase.courtCaseNumber,
-            defenderHasAccessToRvg: Boolean(theCase.defenderNationalId),
-            linkStart: `<a href="${formatDefenderRoute(
-              this.config.clientUrl,
-              theCase.type,
-              theCase.id,
-            )}">`,
-            linkEnd: '</a>',
-            validToDate: formatDate(theCase.validToDate, 'PPPp'),
-            isolationToDate: formatDate(theCase.isolationToDate, 'PPPp'),
-          })
-        : this.formatMessage(notifications.modified.htmlDefender, {
-            caseType: theCase.type,
-            actorInstitution: user.institution?.name,
-            actorName: user.name,
-            actorTitle: user.title,
-            courtCaseNumber: theCase.courtCaseNumber,
-            defenderHasAccessToRvg: Boolean(theCase.defenderNationalId),
-            linkStart: `<a href="${formatDefenderRoute(
-              this.config.clientUrl,
-              theCase.type,
-              theCase.id,
-            )}">`,
-            linkEnd: '</a>',
-            validToDate: formatDate(theCase.validToDate, 'PPPp'),
-          })
-
       promises.push(
-        this.sendEmail(
-          subject,
-          defenderHtml,
-          theCase.defenderName,
-          theCase.defenderEmail,
-        ),
+        this.sendModifiedNotificationToDefender(subject, theCase, user),
       )
     }
 
