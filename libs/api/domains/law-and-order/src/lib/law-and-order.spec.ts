7f3726960f286d140ba697622718d26296ec0380
disaerna <disa.erna@gmail.com>
2024-05-20 22:43:50 +0000

    fix: cleanup generated test file

--- libs/api/domains/law-and-order/src/lib/law-and-order.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiDomainsLawAndOrder } from './api-domains-law-and-order'
-
-describe('apiDomainsLawAndOrder', () => {
-  it('should work', () => {
-    expect(apiDomainsLawAndOrder()).toEqual('api-domains-law-and-order')
-  })
-})
