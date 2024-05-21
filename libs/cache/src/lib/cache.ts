67a665b0aa35db306111e3150a703061f8fd9d28
lommi <jonorn@gmail.com>
2024-05-21 17:35:09 +0000

    fix: reconnect on error

--- libs/cache/src/lib/cache.ts
+++ libs/cache/src/lib/cache.ts
@@ -102,12 +102,7 @@ const getRedisClusterOptions = (
     redisOptions,
     reconnectOnError: (err) => {
       logger.error(`Reconnect on error: ${err}`)
-      const targetError = 'READONLY'
-      if (err.message.slice(0, targetError.length) === targetError) {
-        // Only reconnect when the error starts with "READONLY"
-        return true
-      }
-      return false
+      return true
     },
     retryStrategy: (times) => {
       logger.info(`Redis Retry: ${times}`)
