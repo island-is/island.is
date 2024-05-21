fedb7d8cec7da7cf92ae5ad36757c153cb1d5093
andes-it <builders@andes.is>
2024-05-21 14:58:15 +0000

    chore: nx format:write update dirty files

--- apps/services/user-notification/src/app/modules/notifications/notificationDispatch.service.ts
+++ apps/services/user-notification/src/app/modules/notifications/notificationDispatch.service.ts
@@ -125,31 +125,43 @@ export class NotificationDispatchService {
       case 'messaging/invalid-argument':
       case 'messaging/registration-token-not-registered':
       case 'messaging/invalid-recipient':
-        await this.removeInvalidToken(nationalId, token, messageId);
-        break;
+        await this.removeInvalidToken(nationalId, token, messageId)
+        break
       case 'messaging/invalid-payload':
       case 'messaging/invalid-data-key':
       case 'messaging/invalid-options':
-        this.logger.warn('Invalid message payload or options', { error, messageId });
-        break;
+        this.logger.warn('Invalid message payload or options', {
+          error,
+          messageId,
+        })
+        break
       case 'messaging/quota-exceeded':
-        this.logger.error('Quota exceeded for sending messages', { error, messageId });
-        break;
+        this.logger.error('Quota exceeded for sending messages', {
+          error,
+          messageId,
+        })
+        break
       case 'messaging/server-unavailable':
       case 'messaging/unavailable':
-        this.logger.warn('FCM server unavailable, retrying', { error, messageId });
-        break;
+        this.logger.warn('FCM server unavailable, retrying', {
+          error,
+          messageId,
+        })
+        break
       case 'messaging/message-rate-exceeded':
-        throw new BadRequestException(error.code);
+        throw new BadRequestException(error.code)
       case 'auth/invalid-credential':
-        throw new InternalServerErrorException(error.code);
+        throw new InternalServerErrorException(error.code)
       case 'messaging/too-many-messages':
-        this.logger.warn('Too many messages sent to the device', { error, messageId });
-        break;
+        this.logger.warn('Too many messages sent to the device', {
+          error,
+          messageId,
+        })
+        break
       case 'internal-error':
       case 'messaging/unknown-error':
       default:
-        throw new InternalServerErrorException(error.code);
+        throw new InternalServerErrorException(error.code)
     }
   }
 
