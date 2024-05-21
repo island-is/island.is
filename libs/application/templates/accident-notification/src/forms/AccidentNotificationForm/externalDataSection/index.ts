fd18b36c7dba2af351d86051b6644df871e78faf
Jonni <jonas@norda.is>
2024-05-21 09:50:07 +0000

    chore: revert

--- libs/application/templates/accident-notification/src/forms/AccidentNotificationForm/externalDataSection/index.ts
+++
@@ -1,9 +0,0 @@
-import { buildSection } from '@island.is/application/core'
-import { agreementDescriptionMultiField } from './agreementDescriptionMultiField'
-import { accidentNotificationSubSection } from './accidentNotificationSubSection'
-
-export const externalDataSection = buildSection({
-  id: 'ExternalDataSection',
-  title: 'Meðferð á gögnum',
-  children: [agreementDescriptionMultiField, accidentNotificationSubSection],
-})
