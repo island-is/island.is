4cc71d508fa1bb86c899ec7981be729b1252d094
peturgq <petur@petur.io>
2024-05-22 15:51:54 +0000

    chore: Add manual trace correlation for winston

--- libs/infra-tracing/src/index.ts
+++ libs/infra-tracing/src/index.ts
@@ -1,1 +1,3 @@
+import tracing from './lib/datadog-tracer'
 export * from './lib/datadog-tracer' // must come before importing any instrumented module.
+export default tracing
--- libs/logging/src/lib/logging.ts
+++ libs/logging/src/lib/logging.ts
@@ -1,9 +1,9 @@
 import { createLogger, format, LoggerOptions, transports } from 'winston'
 import { utilities } from 'nest-winston'
-
+import tracer from '@island.is/infra-tracing'
 import { maskNationalIdFormatter } from './formatters'
-
 // Default log settings for debug mode
+
 let logLevel = 'debug'
 let logFormat = format.combine(
   format.errors({ stack: true }),
@@ -11,6 +11,16 @@ let logFormat = format.combine(
   utilities.format.nestLike('App'),
   maskNationalIdFormatter(),
 )
+const correlateFormat = format((info) => {
+  const span = tracer.scope().active();
+  if (span) {
+    info.dd = {
+      trace_id: span.context().toTraceId(),
+      span_id: span.context().toSpanId(),
+    };
+  }
+  return info;
+});
 
 // Production overrides
 if (process.env.NODE_ENV === 'production') {
@@ -20,11 +30,11 @@ if (process.env.NODE_ENV === 'production') {
     format.timestamp(),
     format.json(),
     maskNationalIdFormatter(),
+    correlateFormat()
   )
 }
 
 const logTransports = [new transports.Console()]
-
 export const logger = createLogger({
   level: logLevel,
   format: logFormat,
