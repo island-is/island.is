f6e065a08878a33e7277348996e9a61d0ce776c0
unakb <una@kolibri.is>
2024-05-22 12:10:02 +0000

    Update auth.guard.ts

--- apps/judicial-system/digital-mailbox-api/src/app/guards/auth.guard.ts
+++ apps/judicial-system/digital-mailbox-api/src/app/guards/auth.guard.ts
@@ -6,7 +6,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err: unknown, user: any) {
     if (err || !user) {
       console.error('Auth error:', err)
-      throw err || new UnauthorizedException()
+      throw err || new UnauthorizedException() //TODO: better error handling
     }
     console.log('Auth user national id:', user)
     return user
