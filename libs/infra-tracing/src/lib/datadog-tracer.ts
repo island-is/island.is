98ca4eabe2600b8b50228409064767781780ed13
lommi <jonorn@gmail.com>
2024-05-22 10:50:40 +0000

    chore: dd-trace ioredis

--- libs/infra-tracing/src/lib/datadog-tracer.ts
+++ libs/infra-tracing/src/lib/datadog-tracer.ts
@@ -23,6 +23,9 @@ export const maskSpan = (
 
 if (process.env.NODE_ENV !== 'development') {
   tracer.init({ logInjection: true }) // initialized in a different file to avoid hoisting.
+  tracer.use('ioredis', {
+    enabled: true
+  })
   tracer.use('express', {
     blacklist: ['/liveness', '/readiness', '/metrics'],
     hooks: {
