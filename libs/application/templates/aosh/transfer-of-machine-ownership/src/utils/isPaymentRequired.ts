b4d478c96e947112e9e2dad14d2df46adc90a66a
davidkef <david.birgisson@origo.is>
2024-05-23 13:38:40 +0000

    removing console.log

--- libs/application/templates/aosh/transfer-of-machine-ownership/src/utils/isPaymentRequired.ts
+++ libs/application/templates/aosh/transfer-of-machine-ownership/src/utils/isPaymentRequired.ts
@@ -2,16 +2,6 @@ import { ApplicationContext } from '@island.is/application/types'
 import { getSelectedMachine } from './getSelectedMachine'
 
 export const isPaymentRequired = ({ application }: ApplicationContext) => {
-  console.log(
-    'paymentRequiredForOwnerChange',
-    getSelectedMachine(application.externalData, application.answers)
-      ?.paymentRequiredForOwnerChange,
-  )
-  console.log(
-    'ispayment required',
-    getSelectedMachine(application.externalData, application.answers)
-      ?.paymentRequiredForOwnerChange ?? true,
-  )
   return (
     getSelectedMachine(application.externalData, application.answers)
       ?.paymentRequiredForOwnerChange ?? true
