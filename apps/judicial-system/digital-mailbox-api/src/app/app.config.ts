0b5cf0ef5fb0be37255b06220c68764ef31a6b2b
unakb <una@kolibri.is>
2024-05-27 17:26:01 +0000

    Update app.config.ts

--- apps/judicial-system/digital-mailbox-api/src/app/app.config.ts
+++ apps/judicial-system/digital-mailbox-api/src/app/app.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from '@island.is/nest/config'
 export const digitalMailboxModuleConfig = defineConfig({
   name: 'DigitalMailboxModule',
   load: (env) => ({
-    issuer: env.required(
-      'IDENTITY_SERVER_ISSUER_URL',
-      'https://innskra.island.is',
-    ),
     backendUrl: env.required('BACKEND_URL', 'http://localhost:3344'),
     secretToken: env.required(
       'BACKEND_ACCESS_TOKEN',
