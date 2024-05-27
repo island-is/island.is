c09c2a8074a3ecdafe2dfdc34bf95e66a48be5b9
Rafn Árnason <rafnarnason@gmail.com>
2024-05-27 13:49:18 +0000

    cleanup setup

--- libs/clients/contentful-graphql/src/lib/contentfulClient.service.ts
+++ libs/clients/contentful-graphql/src/lib/contentfulClient.service.ts
@@ -1,10 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common'
-import { print } from 'graphql'
-
 import { User } from '@island.is/auth-nest-tools'
 import type { EnhancedFetchAPI } from '@island.is/clients/middlewares'
 import type { ConfigType } from '@island.is/nest/config'
-
 import { ContentfulClientConfig } from './contentfulClient.config'
 import { ContentfulFetchProviderKey } from './contentfulFetchProvider'
 
@@ -12,6 +9,7 @@ const baseGqlRequestOptions = {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`
   },
 }
 
--- libs/clients/contentful-graphql/src/lib/contentfulFetchProvider.ts
+++ libs/clients/contentful-graphql/src/lib/contentfulFetchProvider.ts
@@ -1,12 +1,9 @@
 import { Provider } from '@nestjs/common'
-
 import {
   createEnhancedFetch,
   type EnhancedFetchAPI,
 } from '@island.is/clients/middlewares'
 import {
-  type ConfigType,
-  IdsClientConfig,
   LazyDuringDevScope,
 } from '@island.is/nest/config'
 
@@ -15,20 +12,10 @@ export const ContentfulFetchProviderKey = 'ContentfulFetchProviderKey'
 export const ContentfulFetchProvider: Provider<EnhancedFetchAPI> = {
   provide: ContentfulFetchProviderKey,
   scope: LazyDuringDevScope,
-  useFactory: (idsClientConfig: ConfigType<typeof IdsClientConfig>) =>
+  useFactory: () =>
     createEnhancedFetch({
-      // search ..................................................
       name: 'clients-contentful-graphql',
-      autoAuth: undefined, // bamm bamm bamm
-      // idsClientConfig.isConfigured
-      //   ? {
-      //       mode: 'tokenExchange',
-      //       issuer: idsClientConfig.issuer,
-      //       clientId: idsClientConfig.clientId,
-      //       clientSecret: idsClientConfig.clientSecret,
-      //       scope: [RecyclingFundScope.carRecycling],
-      //     }
-      //   : undefined,
+      // autoAuth: undefined
     }),
-  inject: [IdsClientConfig.KEY],
+  inject: [ContentfulFetchProviderKey],
 }
