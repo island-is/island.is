e63f09f7c23e71be332938113d72f8d69a99ef7e
unakb <una@kolibri.is>
2024-05-29 12:32:43 +0000

    import types as types

--- libs/judicial-system/lawyers/src/lib/lawyers.service.ts
+++ libs/judicial-system/lawyers/src/lib/lawyers.service.ts
@@ -1,14 +1,11 @@
-import fetch from 'isomorphic-fetch'
-
 import { Inject, Injectable } from '@nestjs/common'
 
 import type { Logger } from '@island.is/logging'
 import { LOGGER_PROVIDER } from '@island.is/logging'
-import { type ConfigType } from '@island.is/nest/config'
+import type { ConfigType } from '@island.is/nest/config'
 
 import { lawyersModuleConfig } from './lawyers.config'
-import { type Lawyer } from './lawyers.types'
-
+import type { Lawyer } from './lawyers.types'
 @Injectable()
 export class LawyersService {
   constructor(
