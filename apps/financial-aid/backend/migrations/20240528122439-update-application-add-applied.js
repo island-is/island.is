cf93504c285aa43f9230691ce13d96566898aa22
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-05-29 13:33:17 +0000

    fixing migration

--- apps/financial-aid/backend/migrations/20240528122439-update-application-add-applied.js
+++ apps/financial-aid/backend/migrations/20240528122439-update-application-add-applied.js
@@ -2,25 +2,42 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.sequelize.transaction((t) =>
-      Promise.all([
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
         // Add the new column
-        queryInterface.addColumn('applications', 'applied', {
-          type: Sequelize.DATE,
-          allowNull: true,
-        }),
+        await queryInterface.addColumn(
+          'applications',
+          'applied',
+          {
+            type: Sequelize.DATE,
+            allowNull: true,
+          },
+          { transaction: t },
+        )
+
         // Update the applied column with the createdAt date
-        queryInterface.sequelize.query(`
-          UPDATE "applications"
-          SET "applied" = "created"
-        `),
+        await queryInterface.sequelize.query(
+          `
+          UPDATE applications
+          SET applied = created;
+        `,
+          { transaction: t },
+        )
+
         // Change the column to not allow null values if necessary
-        queryInterface.changeColumn('applications', 'applied', {
-          type: Sequelize.DATE,
-          allowNull: false,
-        }),
-      ]),
-    )
+        await queryInterface.changeColumn(
+          'applications',
+          'applied',
+          {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          { transaction: t },
+        )
+      } catch (err) {
+        throw err
+      }
+    })
   },
 
   async down(queryInterface, Sequelize) {
