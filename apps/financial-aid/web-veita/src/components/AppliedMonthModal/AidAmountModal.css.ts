b620b956b9607b217b3cdb4fe67f4d43d36057a9
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-05-29 10:37:27 +0000

    remvoing unused css

--- apps/financial-aid/web-veita/src/components/AppliedMonthModal/AidAmountModal.css.ts
+++ apps/financial-aid/web-veita/src/components/AppliedMonthModal/AidAmountModal.css.ts
@@ -1,5 +1,4 @@
 import { style } from '@vanilla-extract/css'
-import { theme } from '@island.is/island-ui/theme'
 
 export const modal = style({
   display: 'block',
@@ -7,5 +6,4 @@ export const modal = style({
   maxWidth: '440px',
   boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.08)',
   borderRadius: '12px',
-  // padding: theme.spacing[4],
 })
