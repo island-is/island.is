410bc30e2277226314648a88c2a986f5c77c679e
albina <albina@kosmosogkaos.is>
2024-05-30 14:49:58 +0000

    fix(inheritance-report): efs updates 30/5

--- libs/application/templates/inheritance-report/src/fields/Overview/OverviewAssets/index.tsx
+++ libs/application/templates/inheritance-report/src/fields/Overview/OverviewAssets/index.tsx
@@ -89,7 +89,7 @@ export const OverviewAssets: FC<React.PropsWithChildren<FieldBaseProps>> = ({
   // Bank accounts
   const bankAccountsDataRow = getBankAccountsDataRow(answers)
   const bankAccountsDataTotal = formatCurrency(
-    String(getValueViaPath(answers, 'assets.inventory.value')) ?? '',
+    String(getValueViaPath(answers, 'assets.bankAccounts.total')) ?? '',
   )
 
   sections.push({
