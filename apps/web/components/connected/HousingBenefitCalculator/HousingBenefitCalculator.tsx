d2f97104c8466d791b037af40d19b2cee89db329
runarvestmann <runar@stefna.is>
2024-05-31 11:43:17 +0000

    Add feature flag

--- apps/web/components/connected/HousingBenefitCalculator/HousingBenefitCalculator.tsx
+++ apps/web/components/connected/HousingBenefitCalculator/HousingBenefitCalculator.tsx
@@ -107,14 +107,19 @@ const HousingBenefitCalculator = ({ slice }: HousingBenefitCalculatorProps) => {
   }, [inputState])
 
   const householdMemberOptions = useMemo<Option<number>[]>(() => {
-    return [
+    const options = [
       { label: '1', value: 1 },
       { label: '2', value: 2 },
       { label: '3', value: 3 },
       { label: '4', value: 4 },
-      { label: '5', value: 5 },
-      { label: n('sixOrMore', '6 eða fleiri'), value: 6 },
     ]
+
+    if (slice?.configJson?.showSixOptions) {
+      options.push({ label: '5', value: 5 })
+      options.push({ label: n('sixOrMore', '6 eða fleiri'), value: 6 })
+    }
+
+    return options
   }, [])
 
   return (
@@ -134,7 +139,7 @@ const HousingBenefitCalculator = ({ slice }: HousingBenefitCalculatorProps) => {
             <Controller
               control={control}
               name="householdMemberCount"
-              defaultValue=""
+              defaultValue={1}
               rules={{ required: true }}
               render={({ field: { onChange, value } }) => (
                 <Select
