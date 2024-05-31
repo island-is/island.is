223f6a2d7d568ad698b707fd9783fdb302079616
unakb <una@kolibri.is>
2024-05-31 00:07:25 +0000

    chore(j-s): Defenders API  (#14980)
    * fix(j-s): Digital mailbox api config
    
    * fix(j-s): Config
    
    * fix(j-s): Added missing config value
    
    * Update judicial-system-xrd-robot-api.ts
    
    * Update app.config.ts
    
    * Update yaml
    
    * chore(j-s): Lawyers lib and controller added to digital mailbox api
    
    * Moving some files around
    
    * fix(j-s): cleanup
    
    * chore(j-s): Cleanup
    
    * import types as types
    
    * Update lawyers.service.ts
    
    * Update defender.controller.ts
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/digital-mailbox-api/src/app/modules/cases/case.controller.ts
+++ apps/judicial-system/digital-mailbox-api/src/app/modules/cases/case.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common'
-import { ApiCreatedResponse } from '@nestjs/swagger'
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
 
 import type { User } from '@island.is/auth-nest-tools'
 import { CurrentUser, IdsUserGuard } from '@island.is/auth-nest-tools'
 import { type Logger, LOGGER_PROVIDER } from '@island.is/logging'
 
 import { CasesResponse } from './models/cases.response'
-import { AppService } from './app.service'
+import { CaseService } from './case.service'
 
 @Controller('api')
+@ApiTags('cases')
 @UseGuards(IdsUserGuard)
-export class AppController {
+export class CaseController {
   constructor(
-    private readonly appService: AppService,
+    private readonly caseService: CaseService,
     @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
   ) {}
 
@@ -21,7 +22,7 @@ export class AppController {
   async test(@CurrentUser() user: User): Promise<string> {
     this.logger.debug('Testing connection')
 
-    return this.appService.testConnection(user.nationalId)
+    return this.caseService.testConnection(user.nationalId)
   }
 
   @Get('cases')
@@ -32,6 +33,6 @@ export class AppController {
   ): Promise<CasesResponse[]> {
     this.logger.debug('Getting all cases')
 
-    return this.appService.getCases(user.nationalId, query?.lang)
+    return this.caseService.getCases(user.nationalId, query?.lang)
   }
 }
