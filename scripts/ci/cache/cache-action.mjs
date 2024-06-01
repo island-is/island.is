e0c965984e18a20f94f5873c950aab2773b06171
lommi <jonorn@gmail.com>
2024-06-01 11:14:13 +0000

    fix: type error

--- scripts/ci/cache/cache-action.mjs
+++ scripts/ci/cache/cache-action.mjs
@@ -1,15 +1,20 @@
+// @ts-check
 import { resolve } from 'path'
 import { cacheSuccess, caches, initCache } from './config.mjs'
+import { ROOT } from './_common.mjs'
 
 const enabledCaches = caches.filter((value) => value.enabled)
 
 for (const cache of enabledCaches) {
   const fileName = resolve(ROOT, cache.fileName)
-  const isOkay = await cache.check(cacheSuccess[cache.id] === 'true', fileName)
+  const isOkay = cache.check ?  await cache.check(cacheSuccess[cache.id] === 'true', fileName) : cacheSuccess[cache.id] === 'true'
   if (!isOkay) {
     if (!initCache) {
       throw new Error(`Cache ${cache.name} is not valid`)
     }
-    await cache.init(fileName)
+    if (cache.init) {
+      console.log(`Init cache ${cache.name}`)
+      await cache.init()
+    }
   }
 }
