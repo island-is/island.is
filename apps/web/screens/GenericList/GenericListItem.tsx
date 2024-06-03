b7e229855b0d9fa699634e46d825e165aa8f08a8
runarvestmann <runar@stefna.is>
2024-06-03 14:17:50 +0000

    Add org subpage layout

--- apps/web/screens/GenericList/GenericListItem.tsx
+++ apps/web/screens/GenericList/GenericListItem.tsx
@@ -1,37 +1,50 @@
-import { GridContainer, Stack, Text } from '@island.is/island-ui/core'
+import { Box, GridContainer, Stack, Text } from '@island.is/island-ui/core'
+import { HeadWithSocialSharing, Webreader } from '@island.is/web/components'
 import {
   GenericListItem,
   Query,
   QueryGetGenericListItemBySlugArgs,
 } from '@island.is/web/graphql/schema'
 import { useDateUtils } from '@island.is/web/i18n/useDateUtils'
-import { withMainLayout } from '@island.is/web/layouts/main'
 import { CustomNextError } from '@island.is/web/units/errors'
 import { webRichText } from '@island.is/web/utils/richText'
 
 import type { Screen } from '../../types'
 import { GET_GENERIC_LIST_ITEM_BY_SLUG_QUERY } from '../queries/GenericList'
 
-interface GenericListItemPageProps {
+export interface GenericListItemPageProps {
   item: GenericListItem
+  showReadspeaker?: boolean
 }
 
-const GenericListItemPage: Screen<GenericListItemPageProps> = ({ item }) => {
+const GenericListItemPage: Screen<GenericListItemPageProps> = ({
+  item,
+  showReadspeaker = true,
+}) => {
   const { format } = useDateUtils()
 
   return (
-    <GridContainer>
-      <Stack space={0}>
-        {item.date && (
-          <Text variant="eyebrow">
-            {format(new Date(item.date), 'dd.MM.yyyy')}
-          </Text>
-        )}
-        <Text variant="h1" as="h1">
-          {item.title}
-        </Text>
-        <Text as="div">{webRichText(item.content ?? [])}</Text>
-      </Stack>
+    <GridContainer className="rs_read">
+      <Box paddingBottom={2}>
+        <HeadWithSocialSharing
+          title={item.title}
+          // TODO: implement and perhaps use subpage looking title
+        />
+        <Stack space={0}>
+          {item.date && (
+            <Text variant="eyebrow">
+              {format(new Date(item.date), 'dd.MM.yyyy')}
+            </Text>
+          )}
+          <Stack space={1}>
+            <Text variant="h1" as="h1">
+              {item.title}
+            </Text>
+            {showReadspeaker && <Webreader readClass="rs_read" marginTop={0} />}
+          </Stack>
+          <Text as="div">{webRichText(item.content ?? [])}</Text>
+        </Stack>
+      </Box>
     </GridContainer>
   )
 }
@@ -71,4 +84,4 @@ GenericListItemPage.getProps = async ({ apolloClient, query, locale }) => {
   }
 }
 
-export default withMainLayout(GenericListItemPage)
+export default GenericListItemPage
