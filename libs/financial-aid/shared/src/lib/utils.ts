ceca6c8e7101bb72c88aa1fa2ad7879c21816fc2
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-06-03 16:34:28 +0000

    updating comment from code rabbit

--- libs/financial-aid/shared/src/lib/utils.ts
+++ libs/financial-aid/shared/src/lib/utils.ts
@@ -129,7 +129,7 @@ export const addUserTypeDirectPayments = (
 
 export const truncateString = (str: string, maxLength: number) => {
   if (str.length > maxLength) {
-    return str.substring(0, maxLength) + '...'
+    return `${str.substring(0, maxLength)}...`
   }
   return str
 }
