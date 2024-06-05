295da1513ca03831077c1bf2416ab389ab1dcec8
albinagu <47886428+albinagu@users.noreply.github.com>
2024-06-05 16:34:03 +0000

    fix(inheritance-report): EFS fixes 4.06 (#15097)
    * fix(inheritance-report): EFS fixes 4.06
    
    * splitting up the overviews
    
    * chore: nx format:write update dirty files
    
    * required selection of application type
    
    * tweaks
    
    * format
    
    * prepaid - overview
    
    * application name
    
    * revert
    
    * more fixes
    
    * taxfree overview
    
    * text tweaks
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/inheritance-report/src/forms/sections/prepaidInheritance/overview.ts
+++ libs/application/templates/inheritance-report/src/forms/sections/prepaidInheritance/overview.ts
@@ -4,202 +4,173 @@ import {
   buildDividerField,
   buildKeyValueField,
   buildMultiField,
-  buildSection,
   buildSubSection,
   getValueViaPath,
 } from '@island.is/application/core'
 import { m } from '../../../lib/messages'
 import { formatCurrency } from '@island.is/application/ui-components'
 import { InheritanceReport } from '../../../lib/dataSchema'
-import { roundedValueToNumber, valueToNumber } from '../../../lib/utils/helpers'
+import {
+  roundedValueToNumber,
+  showTaxFreeInOverview,
+  valueToNumber,
+} from '../../../lib/utils/helpers'
 import { calculateTotalAssets } from '../../../lib/utils/calculateTotalAssets'
 
-export const prepaidOverview = buildSection({
+export const prepaidOverview = buildSubSection({
   id: 'prepaidOverview',
   title: m.overview,
   children: [
-    buildSubSection({
-      id: 'assetOverview',
-      title: m.assetOverview,
-      children: [
-        buildCustomField({
-          title: m.assetOverview,
-          description: m.assetOverviewDescription,
-          id: 'overviewAssets',
-          doesNotRequireAnswer: true,
-          component: 'OverviewAssets',
-        }),
-      ],
-    }),
-    buildSubSection({
+    buildMultiField({
       id: 'heirsOverview',
       title: m.overviewHeirsTitle,
+      description: m.overviewHeirsDescription,
       children: [
-        buildMultiField({
-          id: 'heirsOverview',
-          title: m.overviewHeirsTitle,
-          description: m.overviewHeirsDescription,
-          children: [
-            buildDescriptionField({
-              id: 'overviewAssetsTitle',
-              title: m.properties,
-              titleVariant: 'h3',
-              space: 'gutter',
-              marginBottom: 'gutter',
-            }),
-            buildDividerField({}),
-            buildKeyValueField({
-              label: m.totalValueOfAssets,
-              display: 'flex',
-              value: ({ answers }) =>
-                formatCurrency(
-                  String(roundedValueToNumber(calculateTotalAssets(answers))),
-                ),
-            }),
-            buildDescriptionField({
-              id: 'space',
-              title: '',
-              space: 'gutter',
-            }),
-            buildDividerField({}),
-            buildDescriptionField({
-              id: 'overviewHeirsTitle',
-              title: m.heirs,
-              titleVariant: 'h3',
-              space: 'gutter',
-              marginBottom: 'gutter',
-            }),
-            buildCustomField({
-              title: '',
-              id: 'overviewHeirs',
-              doesNotRequireAnswer: true,
-              component: 'HeirsOverview',
-            }),
-            buildDividerField({}),
-            buildDescriptionField({
-              id: 'overviewTotalInheritance',
-              title: m.overviewTotalInheritance,
-              titleVariant: 'h3',
-              space: 'gutter',
-              marginBottom: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.heirsInheritanceRate,
-              display: 'flex',
-              value: ({ answers }) =>
-                String(getValueViaPath<number>(answers, 'heirs.total')),
-            }),
-            buildDescriptionField({
-              id: 'heirs_space1',
-              title: '',
-              space: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.inheritanceAmount,
-              display: 'flex',
-              value: ({ answers }) => {
-                const total = (
-                  answers as InheritanceReport
-                )?.heirs?.data?.reduce(
-                  (sum, heir) => sum + valueToNumber(heir.inheritance),
-                  0,
-                )
+        buildDividerField({}),
+        buildDescriptionField({
+          id: 'overviewAssetsTitle',
+          title: m.properties,
+          titleVariant: 'h3',
+          space: 'gutter',
+          marginBottom: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.netProperty,
+          display: 'flex',
+          value: ({ answers }) =>
+            formatCurrency(
+              String(roundedValueToNumber(calculateTotalAssets(answers))),
+            ),
+        }),
+        buildDescriptionField({
+          id: 'space',
+          title: '',
+          space: 'gutter',
+        }),
+        buildDividerField({}),
+        buildCustomField({
+          title: '',
+          id: 'overviewHeirs',
+          doesNotRequireAnswer: true,
+          component: 'HeirsOverview',
+        }),
+        buildDividerField({}),
+        buildDescriptionField({
+          id: 'overviewTotalInheritance',
+          title: m.overviewTotalInheritance,
+          titleVariant: 'h3',
+          space: 'gutter',
+          marginBottom: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.heirsInheritanceRate,
+          display: 'flex',
+          value: ({ answers }) =>
+            String(getValueViaPath<number>(answers, 'heirs.total')),
+        }),
+        buildDescriptionField({
+          id: 'heirs_space1',
+          title: '',
+          space: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.inheritanceAmount,
+          display: 'flex',
+          value: ({ answers }) => {
+            const total = (answers as InheritanceReport)?.heirs?.data?.reduce(
+              (sum, heir) => sum + valueToNumber(heir.inheritance),
+              0,
+            )
 
-                return formatCurrency(String(total ?? '0'))
-              },
-            }),
-            buildDescriptionField({
-              id: 'heirs_space2',
-              title: '',
-              space: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.taxFreeInheritance,
-              display: 'flex',
-              value: ({ answers }) => {
-                const total = (
-                  answers as InheritanceReport
-                )?.heirs?.data?.reduce(
-                  (sum, heir) => sum + valueToNumber(heir.taxFreeInheritance),
-                  0,
-                )
+            return formatCurrency(String(total ?? '0'))
+          },
+        }),
+        buildDescriptionField({
+          id: 'heirs_space2',
+          title: '',
+          space: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.taxFreeInheritance,
+          display: 'flex',
+          condition: showTaxFreeInOverview,
+          value: ({ answers }) => {
+            const total = (answers as InheritanceReport)?.heirs?.data?.reduce(
+              (sum, heir) => sum + valueToNumber(heir.taxFreeInheritance),
+              0,
+            )
 
-                return formatCurrency(String(total ?? '0'))
-              },
-            }),
-            buildDescriptionField({
-              id: 'heirs_space3',
-              title: '',
-              space: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.taxableInheritance,
-              display: 'flex',
-              value: ({ answers }) => {
-                const total = (
-                  answers as InheritanceReport
-                )?.heirs?.data?.reduce(
-                  (sum, heir) => sum + valueToNumber(heir.taxableInheritance),
-                  0,
-                )
+            return formatCurrency(String(total ?? '0'))
+          },
+        }),
+        buildDescriptionField({
+          id: 'heirs_space3',
+          title: '',
+          space: 'gutter',
+          condition: showTaxFreeInOverview,
+        }),
+        buildKeyValueField({
+          label: m.taxableInheritance,
+          display: 'flex',
+          value: ({ answers }) => {
+            const total = (answers as InheritanceReport)?.heirs?.data?.reduce(
+              (sum, heir) => sum + valueToNumber(heir.taxableInheritance),
+              0,
+            )
 
-                return formatCurrency(String(total ?? '0'))
-              },
-            }),
-            buildDescriptionField({
-              id: 'heirs_space4',
-              title: '',
-              space: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.inheritanceTax,
-              display: 'flex',
-              value: ({ answers }) => {
-                const total = (
-                  answers as InheritanceReport
-                )?.heirs?.data?.reduce(
-                  (sum, heir) => sum + valueToNumber(heir.inheritanceTax),
-                  0,
-                )
+            return formatCurrency(String(total ?? '0'))
+          },
+        }),
+        buildDescriptionField({
+          id: 'heirs_space4',
+          title: '',
+          space: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.inheritanceTax,
+          display: 'flex',
+          value: ({ answers }) => {
+            const total = (answers as InheritanceReport)?.heirs?.data?.reduce(
+              (sum, heir) => sum + valueToNumber(heir.inheritanceTax),
+              0,
+            )
 
-                return formatCurrency(String(total ?? '0'))
-              },
-            }),
-            buildDividerField({}),
-            buildDescriptionField({
-              id: 'overviewAdditionalInfo',
-              title: m.heirAdditionalInfo,
-              titleVariant: 'h3',
-              space: 'gutter',
-              marginBottom: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.info,
-              value: ({ answers }) =>
-                getValueViaPath<string>(answers, 'heirsAdditionalInfo'),
-            }),
-            buildDescriptionField({
-              id: 'heirs_space5',
-              title: '',
-              space: 'gutter',
-            }),
-            buildKeyValueField({
-              label: m.fileUploadOtherDocumentsPrePaid,
-              value: ({ answers }) => {
-                const files = getValueViaPath<any>(
-                  answers,
-                  'heirsAdditionalInfoFilesOtherDocuments',
-                )
-                return files.map((file: any) => file.name).join(', ')
-              },
-            }),
-            buildCustomField({
-              title: '',
-              id: 'overviewPrint',
-              doesNotRequireAnswer: true,
-              component: 'PrintScreen',
-            }),
-          ],
+            return formatCurrency(String(total ?? '0'))
+          },
+        }),
+        buildDividerField({}),
+        buildDescriptionField({
+          id: 'overviewAdditionalInfo',
+          title: m.heirAdditionalInfo,
+          titleVariant: 'h3',
+          space: 'gutter',
+          marginBottom: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.info,
+          value: ({ answers }) =>
+            getValueViaPath<string>(answers, 'heirsAdditionalInfo'),
+        }),
+        buildDescriptionField({
+          id: 'heirs_space5',
+          title: '',
+          space: 'gutter',
+        }),
+        buildKeyValueField({
+          label: m.fileUploadOtherDocumentsPrePaid,
+          value: ({ answers }) => {
+            const files = getValueViaPath<any>(
+              answers,
+              'heirsAdditionalInfoFilesOtherDocuments',
+            )
+            return files.map((file: any) => file.name).join(', ')
+          },
+        }),
+        buildCustomField({
+          title: '',
+          id: 'overviewPrint',
+          doesNotRequireAnswer: true,
+          component: 'PrintScreen',
         }),
       ],
     }),
