56ca1cf2195f834d8e7a07766fca0a2426f84f8e
unakb <una@kolibri.is>
2024-06-10 15:44:45 +0000

    Update transition.spec.ts

--- apps/judicial-system/backend/src/app/modules/case/test/caseController/transition.spec.ts
+++ apps/judicial-system/backend/src/app/modules/case/test/caseController/transition.spec.ts
@@ -501,6 +501,14 @@ describe('CaseController - Transition', () => {
                   body: { type: NotificationType.RECEIVED_BY_COURT },
                 },
                 {
+                  type: MessageType.DELIVERY_TO_COURT_INDICTMENT_INFO,
+                  user: {
+                    ...defaultUser,
+                    canConfirmIndictment: isIndictmentCase(theCase.type),
+                  },
+                  caseId,
+                },
+                {
                   type: MessageType.DELIVERY_TO_POLICE_INDICTMENT,
                   user: {
                     ...defaultUser,
