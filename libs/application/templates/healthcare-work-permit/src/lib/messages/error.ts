f02be593c336d5fefda848d8ff30f1a378bab8cf
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-06-10 15:10:11 +0000

    changing ids from hlc to hwp

--- libs/application/templates/healthcare-work-permit/src/lib/messages/error.ts
+++ libs/application/templates/healthcare-work-permit/src/lib/messages/error.ts
@@ -7,86 +7,86 @@ export const error = defineMessages({
     description: 'Unhandled error in data provider',
   },
   emptyCareerResponseTitle: {
-    id: 'hlc.application:error.emptyCareerResponseTitle',
+    id: 'hwp.application:error.emptyCareerResponseTitle',
     defaultMessage:
       'Samkvæmt þjónustu Háskóla Íslands ertu ekki með brautskráningu á skrá',
     description: 'Empty career response',
   },
   emptyCareerResponseMessage: {
-    id: 'hlc.application:error.emptyCareerResponseMessage',
+    id: 'hwp.application:error.emptyCareerResponseMessage',
     defaultMessage:
       'Samkvæmt þjónustu Háskóla Íslands ertu ekki með brautskráningu á skrá',
     description: 'Empty career response',
   },
   noResponseEducationInfoTitle: {
-    id: 'hlc.application:error.noResponseEducationInfoTitle',
+    id: 'hwp.application:error.noResponseEducationInfoTitle',
     defaultMessage:
       'Ekki tókst að sækja gögn frá Landlækni, vinsamlegast prufaðu aftur seinna',
     description:
       'No response or faulty response when fetching info on education paths',
   },
   noResponseEducationInfoMessage: {
-    id: 'hlc.application:error.noResponseEducationInfoMessage',
+    id: 'hwp.application:error.noResponseEducationInfoMessage',
     defaultMessage:
       'Ekki tókst að sækja gögn frá Landlækni, vinsamlegast prufaðu aftur seinna',
     description:
       'No response or faulty response when fetching info on education paths',
   },
   healthcareLicenseErrorTitle: {
-    id: 'hlc.application:error.healthcareLicenseErrorTitle',
+    id: 'hwp.application:error.healthcareLicenseErrorTitle',
     defaultMessage:
       'Ekki tókst að sækja gögn frá Landlækni, vinsamlegast prufaðu aftur seinna',
     description:
       'No response or faulty response when fetching info on healthcare licenses',
   },
   healthcareLicenseErrorMessage: {
-    id: 'hlc.application:error.healthcareLicenseErrorMessage',
+    id: 'hwp.application:error.healthcareLicenseErrorMessage',
     defaultMessage:
       'Ekki tóks að sækja gögn frá Landlækni, vinsamlegast prufaðu aftur seinna',
     description:
       'No response or faulty response when fetching healthcare licenses',
   },
   nationalRegistryOutsideEESErrorTitle: {
-    id: 'hlc.application:error.nationalRegistryOutsideEESErrorTitle',
+    id: 'hwp.application:error.nationalRegistryOutsideEESErrorTitle',
     defaultMessage: 'Þú ert ekki ríkisborgari innan EES',
     description: 'You are not a citizen within EES',
   },
   nationalRegistryOutsideEESErrorMessage: {
-    id: 'hlc.application:error.nationalRegistryOutsideEESErrorMessage',
+    id: 'hwp.application:error.nationalRegistryOutsideEESErrorMessage',
     defaultMessage: 'Þú ert ekki ríkisborgari innan EES',
     description: 'You are not a citizen within EES',
   },
   nationalRegistryFetchErrorTitle: {
-    id: 'hlc.application:error.nationalRegistryFetchErrorTitle',
+    id: 'hwp.application:error.nationalRegistryFetchErrorTitle',
     defaultMessage:
       'Ekki tókst að sækja gögn frá þjóðskrá, vinsamlegast prufaðu aftur seinna',
     description:
       'No response or faulty response when fetching info on current user from nation registry',
   },
   nationalRegistryFetchErrorMessage: {
-    id: 'hlc.application:error.nationalRegistryFetchErrorMessage',
+    id: 'hwp.application:error.nationalRegistryFetchErrorMessage',
     defaultMessage: 'Lorem ipsum',
     description:
       'No response or faulty response when fetching info on current user from nation registry',
   },
   noPermitValidGraduationFoundTitle: {
-    id: 'hlc.application:error.noPermitValidGraduationFoundTitle',
+    id: 'hwp.application:error.noPermitValidGraduationFoundTitle',
     defaultMessage: 'Ekki fundust brautskráningar sem gefa starfsleyfi',
     description: 'No graduated programs found that are valid form work permit',
   },
   noPermitValidGraduationFoundMessage: {
-    id: 'hlc.application:error.noPermitValidGraduationFoundMessage',
+    id: 'hwp.application:error.noPermitValidGraduationFoundMessage',
     defaultMessage: 'Ekki fundust brautskráningar sem gefa starfsleyfi',
     description: 'No graduated programs found that are valid form work permit',
   },
   noPermitValidForSelfServiceTitle: {
-    id: 'hlc.application:error.noPermitValidForSelfServiceTitle',
+    id: 'hwp.application:error.noPermitValidForSelfServiceTitle',
     defaultMessage:
       'Ekki fundust brautskráningar sem geta farið í gegnum sjálfsafgreiðslu, vinsamlega hafðu samband við Embætti Landlæknis',
     description: 'No graduated programs found that can go through self service',
   },
   noPermitValidForSelfServiceMessage: {
-    id: 'hlc.application:error.noPermitValidForSelfServiceMessage',
+    id: 'hwp.application:error.noPermitValidForSelfServiceMessage',
     defaultMessage:
       'Ekki fundust brautskráningar sem geta farið í gegnum sjálfsafgreiðslu, vinsamlega hafðu samband við Embætti Landlæknis',
     description: 'No graduated programs found that can go through self service',
