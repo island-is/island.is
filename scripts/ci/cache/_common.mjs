5418aeb54d7ac07d98d863abcb064e8c176b7f72
lommi <jonorn@gmail.com>
2024-06-10 11:04:55 +0000

    fix: fix

--- scripts/ci/cache/_common.mjs
+++ scripts/ci/cache/_common.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
-import { ENV_HASHES_KEY } from './_const.mjs'
+import { ENV_KEYS } from './_const.mjs'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 export const SCRIPTS_DIR = __dirname
 export const ROOT = resolve(__dirname, '..', '..', '..')
 export const MOBILE_APP_DIR = resolve(ROOT, ...'apps/native/app'.split('/'))
 export const HAS_HASH_KEYS = !!(
-  process.env[ENV_HASHES_KEY] && process.env[ENV_HASHES_KEY] != 'false'
+  process.env[ENV_KEYS] && process.env[ENV_KEYS] != 'false'
 )
--- scripts/ci/cache/_const.mjs
+++ scripts/ci/cache/_const.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-export const ENV_HASHES_KEY = 'CACHE_HASHES'
 export const ENV_INIT_CACHE = '_INIT_CACHE'
 export const ENV_KEYS = '_CACHE_KEYS'
 export const ENV_YAML_FILE = 'YAML_FILE'
