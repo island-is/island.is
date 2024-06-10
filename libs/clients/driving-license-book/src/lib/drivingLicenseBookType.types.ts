2a2ce054e57bc58e190470226d40508d1b89ff4b
Haukur Ingi <haukur@juni.is>
2024-06-10 15:36:06 +0000

    address comments

--- libs/clients/driving-license-book/src/lib/drivingLicenseBookType.types.ts
+++ libs/clients/driving-license-book/src/lib/drivingLicenseBookType.types.ts
@@ -68,7 +68,7 @@ export interface DrivingLicenseBookStudentForTeacher {
 
 export interface DrivingLicenseBookStudentInput {
   nationalId: string
-  licenseCategory?: 'B' | 'BE' | undefined
+  licenseCategory?: 'B' | 'BE'
 }
 
 export interface DrivingLicenseBook {
