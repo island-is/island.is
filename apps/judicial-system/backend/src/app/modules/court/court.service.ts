e9b5864e5f41c6cce891a1214c60906f4d1dedda
unakb <una@kolibri.is>
2024-06-12 13:50:19 +0000

    feat(j-s): Deliver defender info to robot for indictment cases (#15191)
    * feat(j-s): Deliver defender info to robot for indictment cases
    
    * Update deliverIndictmentDefenderInfoToCourt.spec.ts
    
    * Update court.service.ts
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/backend/src/app/modules/court/court.service.ts
+++ apps/judicial-system/backend/src/app/modules/court/court.service.ts
@@ -24,6 +24,7 @@ import {
 } from '@island.is/judicial-system/types'
 
 import { nowFactory } from '../../factories'
+import { Defendant } from '../defendant'
 import { EventService } from '../event'
 import { RobotLog } from './models/robotLog.model'
 import { courtModuleConfig } from './court.config'
@@ -127,6 +128,7 @@ enum RobotEmailType {
   APPEAL_CASE_CONCLUSION = 'APPEAL_CASE_CONCLUSION',
   APPEAL_CASE_FILE = 'APPEAL_CASE_FILE',
   NEW_INDICTMENT_INFO = 'INDICTMENT_INFO',
+  INDICTMENT_CASE_DEFENDER_INFO = 'INDICTMENT_CASE_DEFENDER_INFO',
 }
 
 @Injectable()
@@ -603,6 +605,44 @@ export class CourtService {
     }
   }
 
+  async updateIndictmentWithDefenderInfo(
+    user: User,
+    caseId: string,
+    courtCaseNumber?: string,
+    defendants?: Defendant[],
+  ): Promise<unknown> {
+    try {
+      const defendantInfo = defendants?.map((defendant) => ({
+        nationalId: defendant.nationalId,
+        defenderName: defendant.defenderName,
+        defenderEmail: defendant.defenderEmail,
+      }))
+
+      const subject = `Ákæra - ${courtCaseNumber} - verjanda upplýsingar`
+      const content = JSON.stringify(defendantInfo)
+
+      return this.sendToRobot(
+        subject,
+        content,
+        RobotEmailType.INDICTMENT_CASE_DEFENDER_INFO,
+        caseId,
+      )
+    } catch (error) {
+      this.eventService.postErrorEvent(
+        'Failed to update indictment with defender info',
+        {
+          caseId,
+          actor: user.name,
+          institution: user.institution?.name,
+          courtCaseNumber,
+        },
+        error,
+      )
+
+      throw error
+    }
+  }
+
   async updateAppealCaseWithReceivedDate(
     user: User,
     caseId: string,
@@ -784,7 +824,7 @@ export class CourtService {
       .then((log) => [log.id, log.seqNumber])
   }
 
-  private async sendToRobot(
+  async sendToRobot(
     subject: string,
     content: string,
     type: RobotEmailType,
