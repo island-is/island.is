e660bb38c151bf7d273dc81f4544fa3577c5e285
snaerseljan <snaer@aranja.com>
2024-06-12 10:20:09 +0000

    update rn necesserray files for v 0.74.2

--- apps/native/app/ios/IslandApp/AppDelegate.mm
+++ apps/native/app/ios/IslandApp/AppDelegate.mm
@@ -23,7 +23,7 @@
   RCTAppSetupPrepareApp(application, enableTM);
 
   [FIRApp configure];
-  
+
   UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   if (@available(iOS 13.0, *)) {
       window.backgroundColor = [UIColor systemBackgroundColor];
@@ -31,11 +31,11 @@
       window.backgroundColor = [UIColor whiteColor];
   }
   [window makeKeyWindow];
-  
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];  
+
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
   [ReactNativeNavigation bootstrapWithBridge:bridge];
-  
+
   return YES;
 }
 
@@ -45,21 +45,16 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [CodePush bundleURL];
 #endif
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  return true;
 }
 
 - (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL succeeded)) completionHandler {
