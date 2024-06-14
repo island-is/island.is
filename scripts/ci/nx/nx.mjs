3b60b32bf4c288806bae0aa121ebf4612f146b0c
lommi <jonorn@gmail.com>
2024-06-14 15:12:46 +0000

    fix: format

--- scripts/ci/nx/nx.mjs
+++ scripts/ci/nx/nx.mjs
@@ -1,6 +1,6 @@
 import { IS_PULLREQUEST } from "./_const.mjs";
 import { NxAgents } from "./_nx_agent.mjs";
-import { hasGitChanges, runNxAffected } from "./_utils.mjs";
+import { hasGitChanges, runNxCommand } from "./_utils.mjs";
 
 console.log(IS_PULLREQUEST ? 'Running in pull request mode' : 'Running in push mode');
 console.log('Starting agents');
@@ -10,7 +10,7 @@ if (IS_PULLREQUEST) {
     // If this is a pull request we want to start by formatting
     // and exiting early on changes
     console.log(`Running format:write`);
-    await runNxAffected('format:write');
+    await runNxCommand('format:write');
     console.log(`Done format:write`);
     if (await hasGitChanges()) {
         console.log('Changes detected after formatting, exiting');
