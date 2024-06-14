04c7b40560914f1c529775ec14af75ec11e28f49
lommi <jonorn@gmail.com>
2024-06-14 16:00:14 +0000

    fix: push

--- scripts/ci/nx/_git_push.mjs
+++ scripts/ci/nx/_git_push.mjs
@@ -3,7 +3,7 @@ import { runCommand } from "./_utils.mjs";
 // @ts-check
 export async function gitPushChanges(message) {
     await runCommand("git config user.name 'andes-it'");
-    await runCommand("git config user.email 'builders@andes.is");
+    await runCommand("git config user.email 'builders@andes.is'");
     await runCommand("git add -A");
     await runCommand(`git commit -m '${message}'`);
     await runCommand("git push");
