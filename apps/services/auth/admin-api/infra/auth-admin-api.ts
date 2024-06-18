ef8994c75af53602200be390d9afe8996375b81f
lommi <jonorn@gmail.com>
2024-06-18 20:16:15 +0000

    Revert "chore: Change scaling nginx rate of increase metric for IDS (#14646)"
    This reverts commit e5b94e45f1d8724d85249d1cced60be871a0f665.

--- apps/services/auth/admin-api/infra/auth-admin-api.ts
+++ apps/services/auth/admin-api/infra/auth-admin-api.ts
@@ -65,7 +65,6 @@ export const serviceSetup = (): ServiceBuilder<'services-auth-admin-api'> => {
       },
     })
     .replicaCount({
-      scalingMagicNumber: 10,
       default: 2,
       min: 2,
       max: 10,
