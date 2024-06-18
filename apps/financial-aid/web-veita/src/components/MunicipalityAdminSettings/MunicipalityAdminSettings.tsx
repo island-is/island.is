6daa2c6fab4d3bcf038434367b68710655bbe3ef
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-06-18 10:46:24 +0000

    adding error states to hook

--- apps/financial-aid/web-veita/src/components/MunicipalityAdminSettings/MunicipalityAdminSettings.tsx
+++ apps/financial-aid/web-veita/src/components/MunicipalityAdminSettings/MunicipalityAdminSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   Text,
   Box,
@@ -10,12 +10,10 @@ import {
 } from '@island.is/island-ui/core'
 
 import {
-  Aid,
   AidName,
   ApiKeysForMunicipality,
   ChildrenAid,
   Municipality,
-  scrollToId,
 } from '@island.is/financial-aid/shared/lib'
 import MunicipalityNumberInput from './MunicipalityNumberInput/MunicipalityNumberInput'
 import {
@@ -25,6 +23,7 @@ import {
 import useCurrentMunicipalityState from '@island.is/financial-aid-web/veita/src/utils/useCurrentMunicipalityState'
 import ApiKeysSettings from './ApiKeysSettings/ApiKeysSettings'
 import ApiKeyInfo from './ApiKeysSettings/ApiKeysInfo'
+import { useErrorInSettings } from '../../utils/useErrorInSettings'
 
 interface Props {
   currentMunicipality: Municipality
@@ -38,75 +37,32 @@ const MunicipalityAdminSettings = ({ currentMunicipality }: Props) => {
 
   const { apiKeyInfo, municipalityId } = state
 
-  const [hasNavError, setHasNavError] = useState(false)
-  const [hasAidError, setHasAidError] = useState(false)
-  const [hasDecemberCompensationError, setHasDecemberCompensationError] =
-    useState(false)
-
   const INDIVIDUAL = 'individual'
   const COHABITATION = 'cohabitation'
   const aidNames = Object.values(AidName).map(String)
 
-  const errorCheckAid = (aid: Aid, prefix: string, scrollToError: boolean) => {
-    const firstErrorAid = Object.entries(aid).find(
-      (a) => aidNames.includes(a[0]) && a[1] <= 0,
-    )
-
-    if (firstErrorAid === undefined) {
-      return false
-    }
-
-    setHasAidError(true)
-    if (scrollToError) {
-      scrollToId(`${prefix}${firstErrorAid[0]}`)
-    }
-    return true
-  }
-
-  const errorCheckNav = () => {
-    if (
-      state.usingNav &&
-      (!state.navUrl || !state.navUsername || !state.navPassword)
-    ) {
-      setHasNavError(true)
-      scrollToId('navSettings')
-      return true
-    }
-
-    return false
-  }
-
-  const errorCheckDesemberAid = () => {
-    if (state.decemberCompensation === 0) {
-      setHasDecemberCompensationError(true)
-      scrollToId('input-desember')
-      return true
-    }
-
-    return false
-  }
+  const {
+    hasNavError,
+    hasAidError,
+    hasDecemberCompensationError,
+    errorCheckNav,
+    errorCheckAid,
+    errorCheckDesemberAid,
+    aidChangeHandler,
+    navChangeHandler,
+  } = useErrorInSettings(aidNames)
 
   const submit = () => {
-    const errorNav = errorCheckNav()
+    const errorNav = errorCheckNav(state)
     const errorAid =
       errorCheckAid(state.individualAid, INDIVIDUAL, !errorNav) ||
       errorCheckAid(state.cohabitationAid, COHABITATION, !errorNav)
-    const errorDesember = errorCheckDesemberAid()
+    const errorDesember = errorCheckDesemberAid(state.decemberCompensation)
 
     if (errorNav || errorAid || errorDesember) {
       return
     }
     updateMunicipality()
-  }
-
-  const aidChangeHandler = (update: () => void) => {
-    setHasAidError(false)
-    update()
-  }
-
-  const navChangeHandler = (update: () => void) => {
-    setHasNavError(false)
-    update()
   }
 
   //This is because of animation on select doesnt work stand alone
