cdba6fe548ab28257d7b7fe24547357de445429b
berglindoma13 <berglindis@gmail.com>
2024-06-21 14:14:14 +0000

    small update to test

--- libs/clients/passports/src/index.ts
+++ libs/clients/passports/src/index.ts
@@ -3,8 +3,8 @@ export { PassportsClientModule } from './lib/passportsApi.module'
 export { PassportsClientConfig } from './lib/passports.config'
 export * from '../gen/fetch'
 export {
-  Passport,
   IdentityDocument,
   IdentityDocumentChild,
   IdentityDocumentTypes,
 } from './lib/passportsApi.types'
+export type { Passport } from './lib/passportsApi.types'
