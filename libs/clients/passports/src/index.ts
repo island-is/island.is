66c2fc5584466ead9ceb308254b3224bfd88d22d
berglindoma13 <berglindis@gmail.com>
2024-06-21 14:36:48 +0000

    small update to test

--- libs/clients/passports/src/index.ts
+++ libs/clients/passports/src/index.ts
@@ -2,9 +2,9 @@ export { PassportsService } from './lib/passportsApi.service'
 export { PassportsClientModule } from './lib/passportsApi.module'
 export { PassportsClientConfig } from './lib/passports.config'
 export * from '../gen/fetch'
-export {
+export type {
   Passport,
+  IdentityDocument,
   IdentityDocumentChild,
   IdentityDocumentTypes,
 } from './lib/passportsApi.types'
-export type { IdentityDocument } from './lib/passportsApi.types'
