6706da9489c394e5a46acf33aa8c5d418f9635fd
Ívar Oddsson <ivaroddsson@gmail.com>
2024-06-24 22:06:30 +0000

    Tests correct

--- apps/judicial-system/web/src/components/Table/Table.spec.tsx
+++ apps/judicial-system/web/src/components/Table/Table.spec.tsx
@@ -72,8 +72,10 @@ describe('Table', () => {
     })
 
     const tableRows = await screen.findAllByTestId('tableRow')
-    expect(tableRows[0]).toHaveTextContent('2021-01-02T00:00:00Z')
-    expect(tableRows[1]).toHaveTextContent('2021-01-01T00:00:00Z')
+
+    // The first click sorts by ascending order, so the first row should be the one with the earliest date
+    expect(tableRows[0]).toHaveTextContent('2021-01-01T00:00:00Z')
+    expect(tableRows[1]).toHaveTextContent('2021-01-02T00:00:00Z')
 
     await act(async () => {
       await user.click(
@@ -81,8 +83,9 @@ describe('Table', () => {
       )
     })
 
+    // The second click sorts by descending order, so the first row should be the one with the latest date
     const tableRows2 = await screen.findAllByTestId('tableRow')
-    expect(tableRows2[0]).toHaveTextContent('2021-01-01T00:00:00Z')
-    expect(tableRows2[1]).toHaveTextContent('2021-01-02T00:00:00Z')
+    expect(tableRows2[0]).toHaveTextContent('2021-01-02T00:00:00Z')
+    expect(tableRows2[1]).toHaveTextContent('2021-01-01T00:00:00Z')
   })
 })
