f769ca86503dcacee09103f533b760cd5fef4cb6
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-06-25 13:12:45 +0000

    Swapping out UserProfile for V2UsersApi in user-profile.service and .module

--- libs/application/template-api-modules/src/lib/modules/shared/api/user-profile/user-profile.module.ts
+++ libs/application/template-api-modules/src/lib/modules/shared/api/user-profile/user-profile.module.ts
@@ -1,12 +1,7 @@
 import { DynamicModule } from '@nestjs/common'
 import { BaseTemplateAPIModuleConfig } from '../../../../types'
 import { UserProfileService } from './user-profile.service'
-
-<<<<<<< HEAD
-import { Configuration, UserProfileApi } from '@island.is/clients/user-profile'
-=======
 import { Configuration, V2UsersApi } from '@island.is/clients/user-profile'
->>>>>>> 4378172a27 (updating module file and imports in service)
 import { IslykillApiModule } from '@island.is/clients/islykill'
 export class UserProfileModule {
   static register(config: BaseTemplateAPIModuleConfig): DynamicModule {
