f835e34cacc13c5a37342b2347823cc55a32b2da
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-06-26 11:30:07 +0000

    chore: add to resolver

--- libs/api/mocks/src/resolvers.ts
+++ libs/api/mocks/src/resolvers.ts
@@ -8,6 +8,7 @@ import { resolvers as assetResolvers } from './domains/assets'
 import { resolvers as authResolvers } from './domains/auth/resolvers'
 import { resolvers as identityResolvers } from './domains/identity/resolvers'
 import { resolvers as financeResolvers } from './domains/finance'
+import { resolvers as licenseServiceResolvers } from './domains/license-service'
 import { resolvers as airDiscountResolvers } from './domains/air-discount-scheme'
 import { resolvers as rightsPortalPaymentResolver } from './domains/rights-portal'
 
@@ -20,6 +21,7 @@ export const resolvers = createResolvers<Resolvers>(
     searchResolvers,
     applicationsResolvers,
     assetResolvers,
+    licenseServiceResolvers,
     financeResolvers,
     airDiscountResolvers,
     rightsPortalPaymentResolver,
