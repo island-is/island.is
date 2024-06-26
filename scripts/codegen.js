9f5322ad798de3583e5d576ac6d4a3005edd7a53
andes-it <builders@andes.is>
2024-06-26 10:34:10 +0000

    chore: nx format:write update dirty files

--- scripts/codegen.js
+++ scripts/codegen.js
@@ -2,22 +2,20 @@ const { exec } = require('./utils')
 
 const skipCache = process.argv && process.argv[2] === '--skip-cache'
 
-
 const main = async () => {
-  
-    try {
-      await exec(
-        `nx run-many --target=codegen/backend-schema,codegen/bootstrap,codegen/backend-post,codegen/frontend-client,codegen --all --parallel --maxParallel=6 $NX_OPTIONS`,
-        {
-          env: skipCache
-            ? { ...process.env, NX_OPTIONS: '--skip-nx-cache' }
-            : process.env,
-        },
-      )
-    } catch (err) {
-      console.error(`Error running command: ${err.message}`)
-      process.exit(err.code || 1)
-    }
+  try {
+    await exec(
+      `nx run-many --target=codegen/backend-schema,codegen/bootstrap,codegen/backend-post,codegen/frontend-client,codegen --all --parallel --maxParallel=6 $NX_OPTIONS`,
+      {
+        env: skipCache
+          ? { ...process.env, NX_OPTIONS: '--skip-nx-cache' }
+          : process.env,
+      },
+    )
+  } catch (err) {
+    console.error(`Error running command: ${err.message}`)
+    process.exit(err.code || 1)
+  }
 }
 
 main()
