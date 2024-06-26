5d97c1dadd39b19ed6536be833d86249e6112520
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-06-26 11:27:39 +0000

    chore: add unchanged license service

---
+++ libs/api/domains/license-service/src/lib/licenseService.constants.ts
@@ -0,0 +1,103 @@
+import {
+  GenericLicenseMetadata,
+  GenericLicenseOrganizationSlug,
+  GenericLicenseProviderId,
+  GenericLicenseType,
+} from './licenceService.type'
+
+export const LICENSE_MAPPER_FACTORY = 'license-mapper-factory'
+
+export const DEFAULT_LICENSE_ID = 'default'
+
+export const AVAILABLE_LICENSES: GenericLicenseMetadata[] = [
+  {
+    type: GenericLicenseType.FirearmLicense,
+    provider: {
+      id: GenericLicenseProviderId.NationalPoliceCommissioner,
+    },
+    pkpass: true,
+    pkpassVerify: true,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.FirearmLicense,
+  },
+  {
+    type: GenericLicenseType.DriversLicense,
+    provider: {
+      id: GenericLicenseProviderId.NationalPoliceCommissioner,
+    },
+    pkpass: true,
+    pkpassVerify: true,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.DriversLicense,
+  },
+  {
+    type: GenericLicenseType.AdrLicense,
+    provider: {
+      id: GenericLicenseProviderId.AdministrationOfOccupationalSafetyAndHealth,
+    },
+    pkpass: true,
+    pkpassVerify: true,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.AdrLicense,
+  },
+  {
+    type: GenericLicenseType.MachineLicense,
+    provider: {
+      id: GenericLicenseProviderId.AdministrationOfOccupationalSafetyAndHealth,
+    },
+    pkpass: true,
+    pkpassVerify: true,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.MachineLicense,
+  },
+  {
+    type: GenericLicenseType.DisabilityLicense,
+    provider: {
+      id: GenericLicenseProviderId.SocialInsuranceAdministration,
+    },
+    pkpass: true,
+    pkpassVerify: true,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.DisabilityLicense,
+  },
+  {
+    type: GenericLicenseType.HuntingLicense,
+    provider: {
+      id: GenericLicenseProviderId.EnvironmentAgency,
+    },
+    pkpass: true,
+    pkpassVerify: true,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.HuntingLicense,
+  },
+  {
+    type: GenericLicenseType.PCard,
+    provider: {
+      id: GenericLicenseProviderId.DistrictCommissioners,
+    },
+    pkpass: false,
+    pkpassVerify: false,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.PCard,
+  },
+  {
+    type: GenericLicenseType.Ehic,
+    provider: {
+      id: GenericLicenseProviderId.IcelandicHealthInsurance,
+    },
+    pkpass: false,
+    pkpassVerify: false,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.EHIC,
+  },
+  {
+    type: GenericLicenseType.Passport,
+    provider: {
+      id: GenericLicenseProviderId.RegistersIceland,
+    },
+    pkpass: false,
+    pkpassVerify: false,
+    timeout: 100,
+    orgSlug: GenericLicenseOrganizationSlug.Passport,
+  },
+]
