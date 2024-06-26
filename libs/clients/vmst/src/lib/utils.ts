a028649b9fbb92cff88db15fa123b7e9fd52f37a
Kristofer <kristofer@andes.is>
2024-06-26 07:58:10 +0000

    Remove unsed Init type

--- libs/clients/vmst/src/lib/utils.ts
+++ libs/clients/vmst/src/lib/utils.ts
@@ -3,49 +3,6 @@ import { logger } from '@island.is/logging'
 import omit from 'lodash/omit'
 import pick from 'lodash/pick'
 
-// A parental leave request body:
-type Init = {
-  adoptionDate: string
-  applicationId: string
-  applicationFundId: string
-  applicant: string
-  otherParentId: string
-  expectedDateOfBirth: string
-  dateOfBirth: string
-  email: string
-  phoneNumber: string
-  paymentInfo: {
-    bankAccount: string
-    personalAllowance: number
-    personalAllowanceFromSpouse: number
-    union: { id: string; name: string }
-    pensionFund: { id: string; name: string }
-    privatePensionFund: { id: string; name: string }
-    privatePensionFundRatio: number
-  }
-  periods: [
-    {
-      from: string
-      to: string
-      ratio: string
-      approved: boolean
-      paid: boolean
-      rightsCodePeriod: string
-    },
-  ]
-  applicationComment: string
-  employers: {
-    email: string
-    nationalRegistryId: string
-    approverNationalRegistryId: string
-  }[]
-  status: string
-  rightsCode: string
-  attachments: number
-  testData: string
-  otherParentBlocked: boolean
-}
-
 export const createWrappedFetchWithLogging = (
   input: RequestInfo,
   init?: RequestInit,
