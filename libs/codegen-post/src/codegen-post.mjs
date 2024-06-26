e07898aa0b9edf2f24ac8d4e034498dd9b1e7609
lommi <jonorn@gmail.com>
2024-06-26 10:21:32 +0000

    fix: changes

--- libs/codegen-post/src/codegen-post.mjs
+++ libs/codegen-post/src/codegen-post.mjs
@@ -1,19 +1,10 @@
 // @ts-check
-import { exec } from 'child_process';
-import { IGNORE_PROJECTS, POST_TARGETS, PROJECTJSON } from './const.mjs';
+import { IGNORE_PROJECTS, PROJECTJSON } from './const.mjs';
 import { readFile } from 'fs/promises';
+import { getProjectList } from './get-list.mjs';
 
 const projectJSON = JSON.parse(await readFile(PROJECTJSON, 'utf-8'));
-const currentProjects = Array.from(new Set((await Promise.all(POST_TARGETS.map(async (target) => {
-    return new Promise((resolve, reject) => {
-        exec(`nx show projects --target=${target}`, (error, stdout) => {
-        if (error) {
-            reject(error);
-        }
-        resolve(stdout.split("\n").filter((line) => line).map((project) => `${project}`));
-        });
-    });
-}))).flat()));
+const currentProjects = await getProjectList();
 
 const definedProjects = (projectJSON.implicitDependencies ?? []).filter(e => e != "codegen-post");
 const missingProjects = currentProjects.filter((project) => !IGNORE_PROJECTS.includes(project) && !definedProjects.includes(project));
