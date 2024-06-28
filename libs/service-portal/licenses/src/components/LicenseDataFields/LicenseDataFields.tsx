3865524d682aa5378d613388e131f60430091095
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-06-28 10:28:43 +0000

    chore: move datafields

---
+++ libs/service-portal/licenses/src/components/LicenseDataFields/LicenseDataFields.tsx
@@ -0,0 +1,217 @@
+import {
+  LicenseServiceV2GenericLicenseDataField as GenericLicenseDataField,
+  LicenseServiceV2GenericUserLicenseDataFieldTagColor,
+  LicenseServiceV2GenericUserLicenseDataFieldTagType,
+} from '@island.is/api/schema'
+import {
+  Box,
+  Divider,
+  Icon,
+  Text,
+  Table as T,
+  Pagination,
+} from '@island.is/island-ui/core'
+import { UserInfoLine } from '@island.is/service-portal/core'
+import { useState } from 'react'
+import ExpandableLine from '../ExpandableLine/ExpandableLine'
+
+const getTagColor = (
+  color: LicenseServiceV2GenericUserLicenseDataFieldTagColor,
+): 'red600' | 'yellow600' | 'mint600' | undefined => {
+  switch (color) {
+    case 'red':
+      return 'red600'
+    case 'yellow':
+      return 'yellow600'
+    case 'green':
+      return 'mint600'
+    default:
+      return
+  }
+}
+
+export const LicenseDataFields = ({
+  fields,
+  licenseType,
+}: {
+  fields: GenericLicenseDataField[]
+  licenseType?: string
+}) => {
+  const [page, setPage] = useState(1)
+  const pageSize = 15
+
+  if (!fields || fields.length === 0) {
+    return null
+  }
+
+  return (
+    <>
+      {fields.map((field, i) => {
+        if (field.hideFromServicePortal) return undefined
+        return (
+          <Box key={`data-field-${i}`}>
+            {field.type === 'Value' && (
+              <>
+                <UserInfoLine
+                  title={field.name ?? ''}
+                  label={field.label ?? ''}
+                  editLink={
+                    field.link
+                      ? {
+                          url: field.link.value ?? '',
+                          title: field.link.label ?? undefined,
+                        }
+                      : undefined
+                  }
+                  renderContent={
+                    field.value
+                      ? () => (
+                          <Box display="flex" alignItems="center">
+                            <Text>{field.value}</Text>
+                            <Box
+                              marginLeft={2}
+                              display="flex"
+                              flexDirection="row"
+                              alignItems="center"
+                              textAlign="center"
+                            >
+                              <Box
+                                marginRight={1}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                                textAlign="center"
+                              >
+                                {field.tag?.icon && field.tag?.iconColor && (
+                                  <Icon
+                                    icon={
+                                      field.tag.icon ===
+                                      LicenseServiceV2GenericUserLicenseDataFieldTagType.closeCircle
+                                        ? 'closeCircle'
+                                        : 'checkmarkCircle'
+                                    }
+                                    color={getTagColor(field.tag.iconColor)}
+                                    type="filled"
+                                  />
+                                )}
+                              </Box>
+                              {field.tag?.text && (
+                                <Text variant="eyebrow">
+                                  {field.tag.iconText}
+                                </Text>
+                              )}
+                            </Box>
+                          </Box>
+                        )
+                      : undefined
+                  }
+                  paddingY={3}
+                />
+                <Divider />
+              </>
+            )}
+            {field.type === 'Category' && (
+              <ExpandableLine
+                title={
+                  field.value
+                    ? field.name ?? ''
+                    : [field.name, field.label].filter(Boolean).join(' ')
+                }
+                data={field.fields ?? []}
+                description={field.description ?? undefined}
+                type={licenseType}
+              />
+            )}
+            {field.type === 'Group' && (
+              <>
+                <Text
+                  variant="eyebrow"
+                  color="purple400"
+                  paddingBottom={2}
+                  paddingTop={7}
+                >
+                  {field.label}
+                </Text>
+
+                <LicenseDataFields
+                  fields={field.fields ?? []}
+                  licenseType={licenseType}
+                />
+              </>
+            )}
+            {field.type === 'Table' && (
+              <>
+                <Text
+                  variant="eyebrow"
+                  color="purple400"
+                  paddingBottom={2}
+                  paddingTop={7}
+                >
+                  {field.label}
+                </Text>
+                <T.Table>
+                  <T.Head>
+                    <T.Row>
+                      {/* Double mapping needed to get to nested header and values */}
+                      {field.fields?.map((x, xIndex) => {
+                        return x?.fields?.map((y, yIndex) => {
+                          return (
+                            xIndex === 0 && (
+                              <T.HeadData
+                                key={`license-table-head-item-${xIndex}-${yIndex}`}
+                              >
+                                {y.label}
+                              </T.HeadData>
+                            )
+                          )
+                        })
+                      })}
+                    </T.Row>
+                  </T.Head>
+                  <T.Body>
+                    {field.fields
+                      ?.slice((page - 1) * pageSize, page * pageSize)
+                      .map((x, xIndex) => {
+                        return (
+                          <T.Row key={`license-table-item-row-${xIndex}`}>
+                            {x.fields?.map((y, yIndex) => {
+                              return (
+                                <T.Data
+                                  key={`license-table-item-${xIndex}-${yIndex}`}
+                                >
+                                  {y.value}
+                                </T.Data>
+                              )
+                            })}
+                          </T.Row>
+                        )
+                      })}
+                  </T.Body>
+                </T.Table>
+                {field.fields && field.fields.length > pageSize && (
+                  <Box marginY={3}>
+                    <Pagination
+                      totalItems={field.fields.length}
+                      itemsPerPage={pageSize}
+                      page={page}
+                      renderLink={(page, className, children) => (
+                        <Box
+                          cursor="pointer"
+                          className={className}
+                          onClick={() => setPage(page)}
+                          component="button"
+                        >
+                          {children}
+                        </Box>
+                      )}
+                    />
+                  </Box>
+                )}
+              </>
+            )}
+          </Box>
+        )
+      })}
+    </>
+  )
+}
