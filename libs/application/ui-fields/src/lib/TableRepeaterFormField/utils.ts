b994f0670520f5bf590d0780edec5093b50ea020
albina <albina@kosmosogkaos.is>
2024-07-01 12:50:25 +0000

    review tweaks

--- libs/application/ui-fields/src/lib/TableRepeaterFormField/utils.ts
+++ libs/application/ui-fields/src/lib/TableRepeaterFormField/utils.ts
@@ -4,26 +4,32 @@ type Item = {
   id: string
 } & TableRepeaterItem
 
+interface Value<T> {
+  [key: string]: T
+}
+
 export const checkForCustomMappedComponents = (
   items: Array<Item>,
-  values: any,
+  values: Array<Value<object>>,
 ) => {
   items.some((item) => {
     if (item.component === 'nationalIdWithName') {
-      handleNationalIdWithName(item, values)
+      handleNationalIdWithNameItem(item, values)
     }
   })
 }
 
-const handleNationalIdWithName = (item: Item, values: any) => {
+const handleNationalIdWithNameItem = (item: Item, values: Array<Value<object>>) => {
+  if (!values) {
+    return
+  }
+
   // nationalIdWithName returns an object that we
   // need to extract entries from and add to values
-  if (values) {
-    values.forEach((value: any, index: string) => {
-      if (typeof value[item.id] === 'object') {
-        const { [item.id]: nestedObject, ...rest } = value
-        values[index] = { ...nestedObject, ...rest }
-      }
-    })
-  }
+  values.forEach((value: Value<object>, index: number) => {
+    if (typeof value[item.id] === 'object' && value[item.id] !== null) {
+      const { [item.id]: nestedObject, ...rest } = value
+      values[index] = { ...nestedObject, ...rest }
+    }
+  })
 }
