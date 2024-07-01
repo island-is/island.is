6c7d5e3e88ccf41136c7f6066eebaf9d60389d28
Sigrún Tinna Gissurardóttir <sigrun.tinna.gissurardottir@origo.is>
2024-07-01 10:04:54 +0000

    removing unused import

--- libs/application/templates/mortgage-certificate/src/fields/SelectProperty/PropertyTypeSearchField/index.tsx
+++ libs/application/templates/mortgage-certificate/src/fields/SelectProperty/PropertyTypeSearchField/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react'
 import { FieldBaseProps } from '@island.is/application/types'
 import { Box, LoadingDots, AlertMessage } from '@island.is/island-ui/core'
-import { ManyPropertyDetail, PropertyDetail } from '@island.is/api/schema'
+import { ManyPropertyDetail } from '@island.is/api/schema'
 import { gql, useLazyQuery } from '@apollo/client'
 import { SEARCH_ALL_PROPERTIES_QUERY } from '../../../graphql/queries'
 import { useLocale } from '@island.is/localization'
