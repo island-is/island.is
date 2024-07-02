50594fec9e9a225c8ada4de830655686a3eadc78
lommi <jonorn@gmail.com>
2024-07-02 16:22:29 +0000

    fix: empty

--- scripts/ci/find-output-path.mjs
+++ scripts/ci/find-output-path.mjs
@@ -12,7 +12,8 @@ const {
   },
 } = data
 
-let output = outputs[0]
+let output = outputs?.[0] ?? options.outputPath;
+
 for (const [key, value] of Object.entries(options)) {
   output = output.replace(`{options.${key}}`, value)
 }
