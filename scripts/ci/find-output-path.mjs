07f06fb2dc23135300b2853116efa163d0f0d750
lommi <jonorn@gmail.com>
2024-07-02 16:35:38 +0000

    fix: output

--- scripts/ci/find-output-path.mjs
+++ scripts/ci/find-output-path.mjs
@@ -4,18 +4,23 @@
 import { runCommand } from './cache/_utils.mjs'
 
 const APP = process.argv[2]
-
 const data = JSON.parse(await runCommand(`yarn nx show project ${APP}`))
 const {
   targets: {
-    build: { outputs, options },
+    build,
   },
 } = data
 
-let output = outputs?.[0] ?? options.outputPath
+const options = build?.options ?? {};
+let output = build?.outputs?.[0] ?? options.outputPath
+
+if (!output) {
+  throw new Error(`Could not find output path for ${APP}`)
+}
 
 for (const [key, value] of Object.entries(options)) {
-  output = output.replace(`{options.${key}}`, value)
+  output = output.replaceAll(`{options.${key}}`, value)
 }
+output = output.replaceAll(`{workspaceRoot}/`, '');
 
 console.log(output)
