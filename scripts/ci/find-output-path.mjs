62448b0044a9631368ae6661d2b1800aac343266
lommi <jonorn@gmail.com>
2024-07-02 14:48:09 +0000

    fix: format

--- scripts/ci/find-output-path.mjs
+++ scripts/ci/find-output-path.mjs
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 // @ts-check
 
-import { runCommand } from "./cache/_utils.mjs";
+import { runCommand } from './cache/_utils.mjs'
 
-const APP = process.argv[2];
+const APP = process.argv[2]
 
 const data = JSON.parse(await runCommand(`yarn nx show project ${APP}`))
 const {
@@ -17,4 +17,4 @@ for (const [key, value] of Object.entries(options)) {
   output = output.replace(`{options.${key}}`, value)
 }
 
-console.log(output);
+console.log(output)
