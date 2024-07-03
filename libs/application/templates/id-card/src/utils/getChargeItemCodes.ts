9d44e1bf7c0d3e66d34594da4eb5b7aad07d8cb9
berglindoma13 <berglindis@gmail.com>
2024-07-03 10:16:59 +0000

    payment code changes

--- libs/application/templates/id-card/src/utils/getChargeItemCodes.ts
+++ libs/application/templates/id-card/src/utils/getChargeItemCodes.ts
@@ -12,91 +12,18 @@ export const getChargeItemCodes = (application: Application): Array<string> => {
     application.answers,
     `${Routes.PRICELIST}.priceChoice`,
   )
-
-  const chosenTypeOfId = getValueViaPath(application.answers, Routes.TYPEOFID)
-
-  const applicantInformation = getValueViaPath(
-    application.externalData,
-    'nationalRegistry,data',
-  ) as NationalRegistryIndividual
-
-  const paymentAsChild = isUnderAge(application, applicantInformation.age)
-  const hasDisability = getValueViaPath<boolean>(
-    application.answers,
-    'applicantInformation.hasDisabilityLicense',
-  ) as boolean | undefined
-  const isOver66 = applicantInformation.age > 66
-
-  if (isOver66) {
-    if (chosenPaymentForm === Services.REGULAR) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_OLDER_TRAVEL_REGULAR.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_OLDER_REGULAR.toString()]
-      }
-    }
-    if (chosenPaymentForm === Services.EXPRESS) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_OLDER_TRAVEL_EXPRESS.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_OLDER_EXPRESS.toString()]
-      }
-    }
-  } else if (paymentAsChild) {
-    if (chosenPaymentForm === Services.REGULAR) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_CHILDREN_TRAVEL_REGULAR.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_CHILDREN_REGULAR.toString()]
-      }
-    }
-    if (chosenPaymentForm === Services.EXPRESS) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_CHILDREN_TRAVEL_EXPRESS.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_CHILDREN_EXPRESS.toString()]
-      }
-    }
-  } else if (hasDisability) {
-    if (chosenPaymentForm === Services.REGULAR) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_DISABILITY_TRAVEL_REGULAR.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_DISABILITY_REGULAR.toString()]
-      }
-    }
-    if (chosenPaymentForm === Services.EXPRESS) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_DISABILITY_TRAVEL_EXPRESS.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_DISABILITY_EXPRESS.toString()]
-      }
-    }
-  } else {
-    if (chosenPaymentForm === Services.REGULAR) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_TRAVEL_REGULAR.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_REGULAR.toString()]
-      }
-    }
-    if (chosenPaymentForm === Services.EXPRESS) {
-      if (chosenTypeOfId === 'II') {
-        return [ChargeItemCode.ID_CARD_TRAVEL_EXPRESS.toString()]
-      }
-      if (chosenTypeOfId === 'ID') {
-        return [ChargeItemCode.ID_CARD_EXPRESS.toString()]
-      }
-    }
+  if (chosenPaymentForm === Services.REGULAR) {
+    return [ChargeItemCode.ID_CARD_REGULAR.toString()]
   }
-
+  // if (chosenPaymentForm === Services.REGULAR_DISCOUNT) {
+  //   return [ChargeItemCode.ID_CARD_DISCOUNT_REGULAR.toString()]
+  // }
+  if (chosenPaymentForm === Services.EXPRESS) {
+    return [ChargeItemCode.ID_CARD_EXPRESS.toString()]
+  }
+  // if (chosenPaymentForm === Services.EXPRESS_DISCOUNT) {
+  //   return [ChargeItemCode.ID_CARD_DISCOUNT_EXPRESS.toString()]
+  // }
   return []
 }
 
