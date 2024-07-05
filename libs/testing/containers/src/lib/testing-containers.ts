6e388fc28edc344f55c732e11184fb76135f7914
Kristofer <kristofer@andes.is>
2024-07-05 12:58:46 +0000

    DRY port config

--- libs/testing/containers/src/lib/testing-containers.ts
+++ libs/testing/containers/src/lib/testing-containers.ts
@@ -3,6 +3,12 @@ import { GenericContainer, StartedTestContainer, Wait } from 'testcontainers'
 let postgresContainer: StartedTestContainer
 let redisClusterContainer: StartedTestContainer
 
+const portConfig = {
+  SQS: parseInt(process.env.SQS_PORT || '9324', 10),
+  postgres: parseInt(process.env.DB_PORT || '5432', 10),
+  redis: [7000, 7001, 7002, 7003, 7004, 7005],
+}
+
 export const startPostgres = async () => {
   const name = 'test_db'
   postgresContainer = await new GenericContainer(
@@ -20,7 +26,7 @@ export const startPostgres = async () => {
       startPeriod: 1000,
     })
     .withWaitStrategy(Wait.forHealthCheck())
-    .withExposedPorts(5432)
+    .withExposedPorts(portConfig.postgres)
     .start()
 
   const port = postgresContainer.getMappedPort(5432)
@@ -33,13 +39,12 @@ export const stopPostgres = async (): Promise<void> => {
 }
 
 export const startRedisCluster = async () => {
-  const ports = [7000, 7001, 7002, 7003, 7004, 7005]
   redisClusterContainer = await new GenericContainer(
     'public.ecr.aws/bitnami/redis-cluster:5.0.14',
   )
     .withName(`redis-cluster-${Math.random().toString(16).slice(2, 8)}`)
     .withEnv('IP', '0.0.0.0')
-    .withExposedPorts(...ports)
+    .withExposedPorts(...portConfig.redis)
     .start()
 }
 
@@ -52,14 +57,16 @@ export const startLocalstack = async () => {
     'public.ecr.aws/localstack/localstack:3',
   )
     .withName(`localstack-${Math.random().toString(16).slice(2, 8)}`)
-    .withExposedPorts(4566)
+    .withExposedPorts(portConfig.SQS)
     .withWaitStrategy(Wait.forLogMessage('Ready.'))
     .start()
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ;(global as any).__localstack__ = lc
 
-  process.env.SQS_ENDPOINT = `http://${lc.getHost()}:${lc.getMappedPort(4566)}`
+  process.env.SQS_ENDPOINT = `http://${lc.getHost()}:${lc.getMappedPort(
+    portConfig.SQS,
+  )}`
 }
 
 export const stopLocalstack = async () => {
