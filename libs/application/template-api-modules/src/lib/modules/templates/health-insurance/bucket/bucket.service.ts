d56c0cee38ca12e5de020b81bac4549fd1cfd3b2
Kristofer <kristofer@andes.is>
2024-07-08 15:31:42 +0000

    Unify to shared AWS module in templates/health-insurance/

--- libs/application/template-api-modules/src/lib/modules/templates/health-insurance/bucket/bucket.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/health-insurance/bucket/bucket.service.ts
@@ -1,45 +1,23 @@
 import { Inject, Injectable } from '@nestjs/common'
-import * as AWS from 'aws-sdk'
-import * as S3 from 'aws-sdk/clients/s3'
 import type { Logger } from '@island.is/logging'
 import { LOGGER_PROVIDER } from '@island.is/logging'
-import AmazonS3URI from 'amazon-s3-uri'
+import { AwsService } from '@island.is/nest/aws'
 
 @Injectable()
 export class BucketService {
-  private s3 = new AWS.S3({ apiVersion: '2006-03-01' })
-  constructor(@Inject(LOGGER_PROVIDER) private logger: Logger) {}
+  constructor(
+    @Inject(LOGGER_PROVIDER) private logger: Logger,
+    private aws: AwsService,
+  ) {}
 
   async getFileContentAsBase64(filename: string): Promise<string> {
-    this.logger.info('getFileContent base64...')
+    this.logger.info('Getting file content', { filename })
 
-    const { region, bucket, key } = AmazonS3URI(filename)
-    const sm = await this.getFile(key, bucket)
+    const sm = await this.aws.getFile(filename)
     if (sm.Body) {
-      this.logger.info('found file:' + key)
-      return sm.Body.toString('base64')
+      return sm.Body.transformToString('base64')
     } else {
-      throw new Error('error getting file:' + key)
+      throw new Error('error getting file')
     }
-  }
-
-  async getFile(
-    filename: string,
-    bucketName: string,
-  ): Promise<S3.GetObjectOutput> {
-    this.logger.info('get bucket file:' + filename)
-    return new Promise((resolve, reject) => {
-      const params = {
-        Bucket: bucketName,
-        Key: filename,
-      }
-      this.s3.getObject(params, function (err, data) {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
-    })
   }
 }
