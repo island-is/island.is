c656905798787c5df108cd4f3e8bb49951ed9db9
thoreyjona <thoreyjona@gmail.com>
2024-07-08 14:23:15 +0000

    feat: cache hello image in app

--- apps/native/app/src/screens/home/hello-module.tsx
+++ apps/native/app/src/screens/home/hello-module.tsx
@@ -1,13 +1,17 @@
 import { Typography, Skeleton } from '@ui'
+import * as FileSystem from 'expo-file-system'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Image, SafeAreaView } from 'react-native'
 import styled, { useTheme } from 'styled-components/native'
 
 import { useAuthStore } from '../../stores/auth-store'
 import { usePreferencesStore } from '../../stores/preferences-store'
-import { useGetFrontPageImageQuery } from '../../graphql/types/schema'
+import {
+  useGetFrontPageImageLazyQuery,
+  useGetFrontPageImageTitleQuery,
+} from '../../graphql/types/schema'
 
 const Host = styled.View`
   margin-bottom: ${({ theme }) => theme.spacing[2]}px;
@@ -21,11 +25,55 @@ export const HelloModule = React.memo(() => {
   const theme = useTheme()
   const { dismissed } = usePreferencesStore()
   const { userInfo } = useAuthStore()
+  const [imageSrc, setImageSrc] = React.useState<string | undefined>(undefined)
 
-  const { data, loading, error } = useGetFrontPageImageQuery({
+  const [getFrontPageImageUrl] = useGetFrontPageImageLazyQuery({
     variables: { input: { pageIdentifier: 'frontpage' } },
   })
-  const imageSrc = data?.getFrontpage?.imageMobile?.url
+
+  const {
+    data: title,
+    loading,
+    error,
+  } = useGetFrontPageImageTitleQuery({
+    variables: { input: { pageIdentifier: 'frontpage' } },
+  })
+
+  const imageTitle = title?.getFrontpage?.imageMobile?.title
+
+  const checkImage = async () => {
+    // Check if title of image is the same as the one we have in cache
+    const fileInfo = await FileSystem.getInfoAsync(
+      `${FileSystem.documentDirectory}${imageTitle}`,
+    )
+
+    if (fileInfo.exists) {
+      // We have the correct image in the cache, use that one
+      setImageSrc(fileInfo.uri)
+      return
+    }
+
+    // We don't have the correct image in the cache, fetch it, download it and save to cache
+    getFrontPageImageUrl().then(async (data) => {
+      const imageSrcFromServer = data.data?.getFrontpage?.imageMobile?.url
+      if (imageSrcFromServer) {
+        setImageSrc(imageSrcFromServer)
+        const downloadResumable = FileSystem.createDownloadResumable(
+          imageSrcFromServer,
+          `${FileSystem.documentDirectory}${imageTitle}`,
+        )
+        try {
+          await downloadResumable.downloadAsync()
+        } catch {
+          // Do nothing, try again next time
+        }
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkImage()
+  }, [])
 
   // If the onboardingWidget is shown, don't show this module
   if (!dismissed.includes('onboardingWidget')) {
