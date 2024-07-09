949484235d13728b2edbe857bc3876039f2aa651
Ívar Oddsson <ivaroddsson@gmail.com>
2024-07-09 10:26:35 +0000

    Remove unused code

--- apps/judicial-system/web/src/routes/Shared/Cases/Cases.css.ts
+++ apps/judicial-system/web/src/routes/Shared/Cases/Cases.css.ts
@@ -19,15 +19,6 @@ export const filterContainer = style({
   maxWidth: '432px',
 })
 
-export const table = style({
-  borderSpacing: 0,
-  borderCollapse: 'collapse',
-  overflow: 'hidden',
-
-  // Needed for Safari.
-  width: '100%',
-})
-
 export const infoContainer = style({
   marginBottom: theme.spacing[3],
 
@@ -36,47 +27,4 @@ export const infoContainer = style({
       maxWidth: '50%',
     },
   },
-})
-
-export const thead = style({
-  background: theme.color.blue100,
-  textAlign: 'left',
-})
-
-export const tableRowContainer = style({
-  borderBottom: `1px solid ${theme.color.blue200}`,
-  cursor: 'pointer',
-  margin: `0 ${theme.spacing[2]}px`,
-})
-
-export const blockColumn = style({
-  display: 'block',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-})
-
-export const th = style({
-  padding: `${theme.spacing[2]}px 0`,
-
-  selectors: {
-    '&:first-child': {
-      paddingLeft: theme.spacing[2],
-    },
-  },
-})
-
-export const td = style({
-  padding: `${theme.spacing[2]}px 0`,
-  selectors: {
-    '&:first-child': {
-      paddingLeft: theme.spacing[2],
-    },
-  },
-})
-
-export const deleteButtonWrapper = style({
-  margin: '0 auto',
-  padding: 10,
-  width: 44,
-  height: 44,
 })
