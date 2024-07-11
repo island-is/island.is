966fb472a5a3d61a0c1c922ba8d9703989f8b18f
Guðjón Guðjónsson <gudjon@kolibri.is>
2024-07-11 14:22:50 +0000

    Eliminates env file in api

--- apps/judicial-system/api/src/app/modules/backend/backend.config.ts
+++ apps/judicial-system/api/src/app/modules/backend/backend.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@island.is/nest/config'
 
-export const eventLogModuleConfig = defineConfig({
-  name: 'EventLogModule',
+export const backendModuleConfig = defineConfig({
+  name: 'AuthModule',
   load: (env) => ({
     backendUrl: env.required('BACKEND_URL', 'http://localhost:3344'),
     secretToken: env.required(
