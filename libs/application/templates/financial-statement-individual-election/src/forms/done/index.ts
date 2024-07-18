778ac0746c55ff0cd7eb0957915df4257526bbe8
Jonni <jonas@norda.is>
2024-07-18 13:13:58 +0000

    chore: remove commented import

--- libs/application/templates/financial-statement-individual-election/src/forms/done/index.ts
+++ libs/application/templates/financial-statement-individual-election/src/forms/done/index.ts
@@ -1,7 +1,6 @@
 import { buildForm } from '@island.is/application/core'
 import { Form, FormModes } from '@island.is/application/types'
 import { conclusionSection } from './conclusionSection'
-// import { conclusionSection } from './conclusionSection'
 
 export const done: Form = buildForm({
   id: 'done',
