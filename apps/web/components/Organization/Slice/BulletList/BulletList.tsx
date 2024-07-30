f5c7985e1ada7629f617458e165e38fbbb4b2377
runarvestmann <runar@stefna.is>
2024-07-30 14:02:05 +0000

    Make sure null is not passed

--- apps/web/components/Organization/Slice/BulletList/BulletList.tsx
+++ apps/web/components/Organization/Slice/BulletList/BulletList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BulletList } from '@island.is/web/components'
+import { BulletEntry, BulletList } from '@island.is/web/components'
 import { BorderAbove } from '@island.is/web/components'
 import { BulletListSlice as BulletListProps } from '@island.is/web/graphql/schema'
 
@@ -19,22 +19,24 @@ export const BulletListSlice: React.FC<React.PropsWithChildren<SliceProps>> = ({
     >
       {slice.dividerOnTop && <BorderAbove />}
       <BulletList
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore make web strict
-        bullets={slice.bullets.map((bullet) => {
-          switch (bullet.__typename) {
-            case 'IconBullet':
-              return {
-                ...bullet,
-                type: 'IconBullet',
-                icon: bullet.icon.url,
+        bullets={
+          slice.bullets
+            .map((bullet) => {
+              switch (bullet.__typename) {
+                case 'IconBullet':
+                  return {
+                    ...bullet,
+                    type: 'IconBullet',
+                    icon: bullet.icon.url,
+                  }
+                case 'NumberBulletGroup':
+                  return { ...bullet, type: 'NumberBulletGroup' }
+                default:
+                  return null
               }
-            case 'NumberBulletGroup':
-              return { ...bullet, type: 'NumberBulletGroup' }
-            default:
-              return null
-          }
-        })}
+            })
+            .filter(Boolean) as BulletEntry[]
+        }
       />
     </section>
   )
