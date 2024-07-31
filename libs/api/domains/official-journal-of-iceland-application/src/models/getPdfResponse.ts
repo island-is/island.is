213410b36c262b2da47025709b63ea57e5fac718
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-07-31 14:34:15 +0000

    Updating buffer to correct type

--- libs/api/domains/official-journal-of-iceland-application/src/models/getPdfResponse.ts
+++ libs/api/domains/official-journal-of-iceland-application/src/models/getPdfResponse.ts
@@ -3,5 +3,5 @@ import { Field, ObjectType } from '@nestjs/graphql'
 @ObjectType('OfficialJournalOfIcelandApplicationGetPdfResponse')
 export class GetPdfResponse {
   @Field(() => String)
-  buffer!: string
+  buffer!: Buffer
 }
