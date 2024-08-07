070e2669eea0bdc2dc9d567dba06a05dcf090665
Jónas G. Sigurðsson <jonas@norda.is>
2024-08-07 16:29:53 +0000

    feat(financial-statement-cemetery): Make financial statement cemetery application (#15511)
    * feat: Make cemetery its own application
    
    * feat: submit application and success screen
    
    * chore: remove console.log
    
    * chore: remove unused imports
    
    * chore: remove console.log
    
    * chore: remove unused imports
    
    * chore: remove unused prop
    
    * chore: remove component typing
    
    * chore: remove typing
    
    * chore: remove console.log
    
    * chore: move between types and constants
    
    * chore: fix imports
    
    * chore: fixx imports
    
    * Update libs/application/templates/financial-statement-cemetery/src/fields/CemeteryCareteker/CemeteryCaretakerRepeaterItem.tsx
    
    Co-authored-by: Þórarinn Gunnar Árnason <torarinng@gmail.com>
    
    * Update libs/application/templates/financial-statement-cemetery/src/fields/CemeteryCareteker/CemeteryCaretakerRepeaterItem.tsx
    
    Co-authored-by: Þórarinn Gunnar Árnason <torarinng@gmail.com>
    
    * Update libs/application/templates/financial-statement-cemetery/src/hooks/useSubmitApplication.ts
    
    Co-authored-by: Þórarinn Gunnar Árnason <torarinng@gmail.com>
    
    * chore: nx format:write update dirty files
    
    * chore: fix comments and add featureflag
    
    * chore: fix lint
    
    * pin swc versions
    
    * minor fixes
    
    ---------
    
    Co-authored-by: Þórarinn Gunnar Árnason <torarinng@gmail.com>
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: FridrikMProgramm <85297370+FridrikMProgramm@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/application/templates/financial-statement-cemetery/src/fields/OperatingYear/index.tsx
@@ -0,0 +1,84 @@
+import {
+  AlertMessage,
+  Box,
+  ContentBlock,
+  SkeletonLoader,
+} from '@island.is/island-ui/core'
+import { useLocale } from '@island.is/localization'
+import { m } from '../../lib/messages'
+
+import * as styles from './styles.css'
+import { useFormContext } from 'react-hook-form'
+import { getErrorViaPath } from '@island.is/application/core'
+import { SelectController } from '@island.is/shared/form-fields'
+import { useQuery } from '@apollo/client'
+
+import { getAuditConfig } from '../../graphql'
+import {
+  getConfigInfoForKey,
+  possibleOperatingYears,
+} from '../../utils/helpers'
+import {
+  ABOUTIDS,
+  CemeteriesBackwardLimit,
+  CemeteriesYearAllowed,
+} from '../../utils/constants'
+
+export const OperatingYear = () => {
+  const { data, loading, error } = useQuery(getAuditConfig)
+  const { formatMessage } = useLocale()
+  const {
+    formState: { errors },
+  } = useFormContext()
+
+  if (loading) {
+    return (
+      <Box width="half" className={styles.selectSpace}>
+        <SkeletonLoader height={70} />
+      </Box>
+    )
+  }
+
+  if (error || data?.financialStatementsInaoConfig?.length <= 0) {
+    return (
+      <ContentBlock>
+        <AlertMessage
+          type="error"
+          title={formatMessage(m.fetchErrorTitle)}
+          message={formatMessage(m.fetchErrorMsg)}
+        />
+      </ContentBlock>
+    )
+  }
+
+  const { financialStatementsInaoConfig } = data
+
+  const backwardsYearLimit = getConfigInfoForKey(
+    financialStatementsInaoConfig,
+    CemeteriesBackwardLimit,
+  )
+
+  const countYearBackwardsFrom = getConfigInfoForKey(
+    financialStatementsInaoConfig,
+    CemeteriesYearAllowed,
+  )
+
+  const operatingYear = possibleOperatingYears(
+    backwardsYearLimit,
+    countYearBackwardsFrom,
+  )
+
+  return (
+    <Box width="half" className={styles.selectSpace}>
+      <SelectController
+        id={ABOUTIDS.operatingYear}
+        name={ABOUTIDS.operatingYear}
+        backgroundColor="blue"
+        label={formatMessage(m.operatingYear)}
+        placeholder={formatMessage(m.selectOperatingYear)}
+        error={errors && getErrorViaPath(errors, ABOUTIDS.operatingYear)}
+        options={operatingYear}
+      />
+    </Box>
+  )
+}
