ac1d6c9b8cd03cf10dca4c4b385bdbb87a4e6c89
Rafn Árnason <rafnarnason@gmail.com>
2024-08-08 14:44:02 +0000

    feat(endorsement-system): Data cleanup worker envs (#15605)
    * add missing envs
    
    * chore: charts update dirty files
    
    * charts
    
    * fixname
    
    * chore: charts update dirty files
    
    * charts
    
    * Trigger Build
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: Kristofer <kristofer@andes.is>

--- apps/services/endorsements/api/infra/endorsement-system-api.ts
+++ apps/services/endorsements/api/infra/endorsement-system-api.ts
@@ -59,6 +59,25 @@ export const endorsementSystemCleanUpWorkerSetup = (): ServiceBuilder<
     .args('--no-experimental-fetch', 'main.js', '--job=cleanup')
     .db({ name: 'services-endorsements-api' })
     .migrations()
+    .env({
+      EMAIL_REGION: 'eu-west-1',
+      EMAIL_FROM_NAME: {
+        dev: 'devland.is',
+        staging: 'devland.is',
+        prod: 'island.is',
+      },
+      EMAIL_FROM_ADDRESS: {
+        dev: 'development@island.is',
+        staging: 'development@island.is',
+        prod: 'noreply@island.is',
+      },
+      IDENTITY_SERVER_ISSUER_URL: {
+        dev: 'https://identity-server.dev01.devland.is',
+        staging: 'https://identity-server.staging01.devland.is',
+        prod: 'https://innskra.island.is',
+      },
+      IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/endorsement',
+    })
     .secrets({
       IDENTITY_SERVER_CLIENT_SECRET:
         '/k8s/endorsement-system-api/IDS-shared-secret',
