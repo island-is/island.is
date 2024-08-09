f75cac3e1f7a3cdea51f77f756d693e3f8308d04
OlafurGeorg <121111792+OlafurGeorg@users.noreply.github.com>
2024-08-09 13:04:46 +0000

    feat(form-system-backend): Add functionality to create and edit custom lists (#15352)
    * form builder 'init'
    
    * Added functionality for relevant parties
    
    * redirect to the correct url when creating a new form
    
    * removed secret from comment
    
    * chore: nx format:write update dirty files
    
    * Property number component
    
    * Fixed the header
    
    * Reverted to next 13.3.0
    
    * Deleted the e2e project, updated to project to adhere to strict typescript, removed all console.logs and removed all code that was commented out
    
    * chore: nx format:write update dirty files
    
    * Started implementing zod validation
    
    * chore: nx format:write update dirty files
    
    * Generated a nestjs client from our openapi schema
    
    * Generated a client and created a domain client for graphql
    
    * Update Form, Group, Input, and Step DTOs
    
    * Refactor input settings in FormsService and Input model
    
    * Update API and authentication configurations
    
    * Added form-system client and module to the app/api and added a codegen + script to generate graphql schemas
    
    * Remove form-system module and add formSystemNavigation to masterNavigation
    
    * Refactor form-system API code and formBuilder into portals/admin
    
    * Fixng typing erros in form system graphql domain
    
    * Added another Object type and a bunch of inputTypes in order for codegen to work
    
    * Update import statements for LanguageTypeInput
    
    * Update form system module and related files
    
    * Update FormSystemClient.config and Forms component
    
    * Saving progress
    
    * Working version. Current issue: inputsettings are not being saved due to graphql not handling type discriminators
    
    * took out the formsysteminputsettings and replaced it with a graphqlTypeJson
    
    * spring cleaning and creating dependencies within the form
    
    * removed form-builder app
    
    * chore: charts update dirty files
    
    * chore: nx format:write update dirty files
    
    * form-system backend initial commit
    
    * initial configuration
    
    * adjust for PR
    
    * fix lint warnings
    
    * add models and modules
    
    * helstu módel, services og controllerar
    
    * make lint satisfied
    
    * Add functionality to create and edit custom lists
    
    * application part start
    
    * change step to section and group to page
    
    * tiny lint things
    
    * change naming of page to screen
    
    * changed naming of input to field
    
    * fix lint warnings
    
    * change naming of input to field
    
    * work on application part
    
    * change tablenames to singular
    
    * changed naming of testimony to certification
    
    * fixing apiproperty configurations
    
    * changed naming of urlName to slug
    
    * introduce lodash pick
    
    * refactor vegna PR
    
    * add openapi.yaml build
    
    * Refactor project setup
    
    * Add class validation for CreateFormDto
    
    * add class-validation
    
    * use predefined types for cases in switch
    
    * Fix build
    
    * Fix lint
    
    * Update tags
    
    * Remove empty test files
    
    * Skip tests until implemented
    
    * Add advania as codeowner for new service
    
    * add infra definition
    
    * add the service to uber-charts
    
    * chore: charts update dirty files
    
    * update grantNamespaces
    
    * chore: charts update dirty files
    
    * added seed instruction to DLS
    
    * chore: charts update dirty files
    
    ---------
    
    Co-authored-by: Hyo-Sam Nandkisore <hyo-sam.nandkisore@advania.is>
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: Sævar Már Atlason <saevar.m.atlason@gmail.com>
    Co-authored-by: Bæring Gunnar Steinþórsson <baering@aranja.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ apps/services/form-system/src/app/modules/listItems/listItems.module.ts
@@ -0,0 +1,22 @@
+import { SequelizeModule } from '@nestjs/sequelize'
+import { ListItem } from './models/listItem.model'
+import { ListItemsService } from './listItems.service'
+import { Module } from '@nestjs/common'
+import { FieldSettingsService } from '../fieldSettings/fieldSettings.service'
+import { FieldSettings } from '../fieldSettings/models/fieldSettings.model'
+import { FieldSettingsMapper } from '../fieldSettings/models/fieldSettings.mapper'
+import { ListItemsController } from './listItems.controller'
+import { ListItemMapper } from './models/listItem.mapper'
+
+@Module({
+  imports: [SequelizeModule.forFeature([ListItem, FieldSettings])],
+  controllers: [ListItemsController],
+  providers: [
+    ListItemsService,
+    FieldSettingsService,
+    FieldSettingsMapper,
+    ListItemMapper,
+  ],
+  exports: [ListItemsService, ListItemMapper],
+})
+export class ListItemsModule {}
