8044e1de4b8a5b222e44e7180620942818edbd10
brynjarorng <brynjarorng@gmail.com>
2024-08-13 16:47:01 +0000

    added debug printing

--- scripts/ci/10_prepare-docker-deps.sh
+++ scripts/ci/10_prepare-docker-deps.sh
@@ -10,6 +10,8 @@ mkdir -p "$PROJECT_ROOT"/cache
 
 NODE_IMAGE_TAG=${NODE_IMAGE_TAG:-$(./scripts/ci/get-node-version.mjs)}
 
+echo "APP value: $APP"
+
 if yq e "select(.$APP.armBetaEnrolled == true) | length > 0" charts/islandis/values.prod.yaml; then
   PLATFORM=linux/aarch64
 else
