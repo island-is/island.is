ac617ea81a07e336b352b4e81807df2290530d1b
disaerna <disa.erna@gmail.com>
2024-08-13 12:46:32 +0000

    fix: rewrite to fix order of checkboxes

--- libs/service-portal/health/src/screens/OrganDonationRegistration/Limitations.tsx
+++ libs/service-portal/health/src/screens/OrganDonationRegistration/Limitations.tsx
@@ -1,43 +1,85 @@
-import { Box, Checkbox, Divider, Stack } from '@island.is/island-ui/core'
-import React from 'react'
+import {
+  Box,
+  Checkbox,
+  Divider,
+  GridColumn,
+  GridContainer,
+  GridRow,
+  Input,
+  Stack,
+} from '@island.is/island-ui/core'
+import React, { useState } from 'react'
 import { OptionsLimitations } from '../../utils/OrganDonationMock'
-import { OtherLimitations } from './OtherLimitations'
+import { messages } from '../..'
+import { useLocale, useNamespaces } from '@island.is/localization'
+
 interface LimitationsProps {
   data: OptionsLimitations[]
 }
 
 const Limitations = ({ data }: LimitationsProps) => {
+  useNamespaces('sp.health')
+  const { formatMessage } = useLocale()
+  const [checked, setChecked] = useState<Array<string>>([])
+
+  const handleCheckboxChange = (id: string, isChecked: boolean) => {
+    setChecked((prevState) =>
+      isChecked ? [...prevState, id] : prevState.filter((item) => item !== id),
+    )
+  }
+
+  const input = data.find((x) => x.type === 'input')
+
   return (
-    <Box marginTop={2}>
+    <Box marginTop={2} position="relative">
       <Stack space={2}>
         <Divider />
         <Box display="flex" flexDirection="row" flexWrap="wrap" width="full">
-          {data?.map((y, yi) => {
-            if (y.type === 'input') {
-              return (
-                <OtherLimitations
+          {data?.map(
+            (y, yi) =>
+              y.type === 'checkbox' && (
+                <Box
                   key={`organ-donation-limitation-${yi}`}
-                  type={y.type}
-                  value={y.value}
-                />
-              )
-            }
-            return (
-              <React.Fragment key={`organ-donation-limitation-${yi}`}>
-                <Box width="half" marginY="smallGutter">
+                  width="half"
+                  marginY="smallGutter"
+                >
                   <Checkbox
                     id={`organ-registration-form-${y.value.toLowerCase()}`}
                     name={`selected-limitations-${y.value.toLowerCase()}`}
                     label={y.value}
                     value={y.value.toLowerCase()}
-                    key={`organ-donation-limitation-checkbox-${yi}`}
+                    onChange={(e) =>
+                      handleCheckboxChange(
+                        y.value.toLowerCase(),
+                        e.target.checked,
+                      )
+                    }
                   />
                 </Box>
-              </React.Fragment>
-            )
-          })}
+              ),
+          )}
         </Box>
       </Stack>
+      {input && checked.includes(input.value.toLowerCase()) && (
+        <GridContainer>
+          <GridRow>
+            <GridColumn span={['7/7', '5/7']}>
+              <Box marginY="gutter">
+                <Input
+                  id="organ-donation-limitation"
+                  name="otherLimitatons"
+                  textarea
+                  label={formatMessage(messages.organRegistrationOtherLabel)}
+                  placeholder={formatMessage(
+                    messages.organRegistrationOtherText,
+                  )}
+                  maxLength={220}
+                />
+              </Box>
+            </GridColumn>
+          </GridRow>
+        </GridContainer>
+      )}
     </Box>
   )
 }
