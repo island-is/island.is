072c5bdb1d125041c77cd1cadf1f9d1b8add44ba
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-14 11:43:32 +0000

    Remove unused cf string

--- apps/judicial-system/web/src/components/InfoCard/DefendantInfo/DefendantInfo.strings.ts
+++ apps/judicial-system/web/src/components/InfoCard/DefendantInfo/DefendantInfo.strings.ts
@@ -49,11 +49,6 @@ export const strings = defineMessages({
     defaultMessage: 'Talsmaður',
     description: 'Notaður sem titill á "talsmanni" á upplýsingaspjaldi máls.',
   },
-  defenders: {
-    id: 'judicial.system.core:info_card.defenders',
-    defaultMessage: 'Verjendur',
-    description: 'Notaður sem titill á "verjendum" á upplýsingaspjaldi máls.',
-  },
   noDefender: {
     id: 'judicial.system.core:info_card.no_defender',
     defaultMessage: 'Hefur ekki verið skráður',
