666b553459a8651a858c13839141aa841fdc28d0
disaerna <disa.erna@gmail.com>
2024-08-14 15:22:45 +0000

    feat: add scopes

--- libs/auth/scopes/src/lib/api.scope.ts
+++ libs/auth/scopes/src/lib/api.scope.ts
@@ -23,6 +23,7 @@ export enum ApiScope {
   intellectualProperties = '@island.is/assets/ip',
   healthDentists = '@island.is/health/dentists',
   healthRightsStatus = '@island.is/health/rights-status',
+  healthOrganDonation = '@island.is/health/organ-donation',
   workMachines = '@island.is/work-machines',
   vinnueftirlitid = '@island.is/applications/ver',
   samgongustofaVehicles = '@island.is/applications/samgongustofa-vehicles',
