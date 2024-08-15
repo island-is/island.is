899a3ff0e29449bdeb6e1e4e7cc73164dfc05c38
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-15 13:33:18 +0000

    Fix

--- apps/judicial-system/backend/sequelize.config.js
+++ apps/judicial-system/backend/sequelize.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   development: {
-    username: 'judicial_system',
-    password: 'qhcNDnWVLZsBLeTfkhU8jHzFr3fjxJFQ',
-    database: 'judicial_system',
+    username: 'dev_db',
+    password: 'dev_db',
+    database: 'dev_db',
     host: 'localhost',
     dialect: 'postgres',
   },
