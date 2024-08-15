72dd90a2a51913649e2de5b43e61f3b10e4b9ce6
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-15 14:36:10 +0000

    chore(j-s): Refactor defender in InfoCard (#15652)
    * Fix sideStepper for cases in WAITING_FOR_CANCELLATION state
    
    * Add Phonenr to RenderPersonalInfo component
    
    * Add tests
    
    * Refactor
    
    * Refactor
    
    * Move UniqueDefenders to DefendantInfo component
    
    * Semantic fixes
    
    * Disallow multiple defenders
    
    * Remove unused cf string
    
    * Layout fixes
    
    * Layout fixes
    
    * Layout fixes
    
    * Fix tests
    
    * Remove console log
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/components/InfoCard/RenderPersonalInfo/RenderPersonalInfo.tsx
+++ apps/judicial-system/web/src/components/InfoCard/RenderPersonalInfo/RenderPersonalInfo.tsx
@@ -1,6 +1,18 @@
+import { FC, PropsWithChildren } from 'react'
+
 import { Box, LinkV2, Text } from '@island.is/island-ui/core'
 
 import { link } from '../../MarkdownWrapper/MarkdownWrapper.css'
+
+interface RenderItemProps {
+  breakSpaces: boolean
+}
+
+const RenderItem: FC<PropsWithChildren<RenderItemProps>> = (props) => (
+  <Box display={props.breakSpaces ? 'block' : 'flex'} component="span">
+    {props.children}
+  </Box>
+)
 
 const RenderPersonalData = (
   name?: string | null,
@@ -8,28 +20,34 @@ const RenderPersonalData = (
   phoneNumber?: string | null,
   breakSpaces = true,
 ) => (
-  <Box display={breakSpaces ? 'block' : 'flex'} dataTestId="personalInfo">
+  <Box
+    display={breakSpaces ? 'block' : ['block', 'block', 'block', 'flex']}
+    dataTestId="personalInfo"
+    component="p"
+  >
     {name && (
-      <Text key={name} whiteSpace="pre">{`${name}${
-        (email || phoneNumber) && !breakSpaces ? `, ` : ''
-      }`}</Text>
+      <RenderItem breakSpaces={breakSpaces}>
+        <Text as="span" whiteSpace="pre">{`${name}${
+          (email || phoneNumber) && !breakSpaces ? `, ` : ''
+        }`}</Text>
+      </RenderItem>
     )}
     {email && (
-      <>
+      <RenderItem breakSpaces={breakSpaces}>
         <LinkV2 href={`mailto:${email}`} className={link} key={email}>
           <Text as="span" whiteSpace="pre">
             {email}
           </Text>
         </LinkV2>
-        <Text as="span" whiteSpace="pre">
-          {phoneNumber ? `, ` : ''}
-        </Text>
-      </>
+        {phoneNumber && <Text as="span" whiteSpace="pre">{`, `}</Text>}
+      </RenderItem>
     )}
     {phoneNumber && (
-      <Text key={phoneNumber} whiteSpace="pre">
-        {`s. ${phoneNumber}`}
-      </Text>
+      <RenderItem breakSpaces={breakSpaces}>
+        <Text key={phoneNumber} whiteSpace="pre" as="span">
+          {`s. ${phoneNumber}`}
+        </Text>
+      </RenderItem>
     )}
   </Box>
 )
