83b74a27dbcd031948ee706cb7b8743cc6f93612
Rafn Árnason <rafnarnason@gmail.com>
2024-08-15 12:37:09 +0000

    cleanup

--- apps/services/user-notification/src/main.ts
+++ apps/services/user-notification/src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
 
 const job = processJob()
-console.log(
-  `Starting job: ${job} ***************************************************************************`,
-)
 
 if (job === 'cleanup') {
   import('./cleanup').then((app) => app.cleanup())
