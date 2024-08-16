d0d5716e1ccd17bc4b9da8a5a39a04f789b7061f
disaerna <disa.erna@gmail.com>
2024-08-16 13:32:06 +0000

    fix: client type error

--- libs/clients/health-directorate/src/lib/clients/index.ts
+++ libs/clients/health-directorate/src/lib/clients/index.ts
@@ -3,4 +3,6 @@ export * from './vaccinations'
 export {
   HealthDirectorateOrganDonationService,
   HealthDirectorateOrganDonationClientConfig,
+  OrganDonorDto,
+  Locale as organLocale,
 } from './organ-donation'
