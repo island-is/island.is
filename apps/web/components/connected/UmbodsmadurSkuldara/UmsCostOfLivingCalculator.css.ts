af6ba89bcc0194561c7ec6e7f81bba79951250d0
Ármann Pétur Ævarsson <armann@stefna.is>
2024-08-16 07:57:38 +0000

    Fix styles for ums cost of living calculator

---
+++ apps/web/components/connected/UmbodsmadurSkuldara/UmsCostOfLivingCalculator.css.ts
@@ -0,0 +1,14 @@
+import { style } from '@vanilla-extract/css'
+
+export const readOnlyValues = style({
+  padding: '8px 16px 8px 8px',
+  boxShadow: 'inset 0 0 0 1px #ccdfff',
+  borderRadius: '8px',
+})
+
+export const total = style({
+  padding: '8px 16px 8px 8px',
+  boxShadow: 'inset 0 0 0 1px #ccdfff',
+  borderRadius: '8px',
+  background: 'white',
+})
