aa796345e5970186aa13d10ddfc6319b2fdf6b0b
Ásdís Erna Guðmundsdóttir <disa@hugsmidjan.is>
2024-08-16 17:16:57 +0000

    feat(api): new domain for health directorate on service portal (#15691)
    * feat: add domain for healt directorate
    
    * WIP add apis
    
    * WIP
    
    * WIPWIP
    
    * revert test changes
    
    * fix: project.json scripts
    
    * fix: return type
    
    * fix: service calls
    
    * feat: add graphql for health directorate
    
    * fix: service calls
    
    * fix: scope naming
    
    * chore: error handling
    
    * fix: tags
    
    * fix: client type error
    
    * fix: type error
    
    * chore: add audit to resolver
    
    * chore: add codeowneres
    
    ---------
    
    Co-authored-by: Tryggvi Geir Magnússon <tryggvi@hugsmidjan.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/api/domains/health-directorate/src/lib/health-directorate.resolver.ts
@@ -0,0 +1,66 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+
+import { UseGuards } from '@nestjs/common'
+
+import {
+  CurrentUser,
+  IdsUserGuard,
+  Scopes,
+  ScopesGuard,
+} from '@island.is/auth-nest-tools'
+import type { User } from '@island.is/auth-nest-tools'
+import { ApiScope } from '@island.is/auth/scopes'
+import { Audit } from '@island.is/nest/audit'
+import {
+  DonationException,
+  DonorStatus,
+  DonorStatusInput,
+} from './models/organ-donation.model'
+import type { Locale } from '@island.is/shared/types'
+import { Vaccinations } from './models/vaccinations.model'
+import { HealthDirectorateService } from './health-directorate.service'
+
+@UseGuards(IdsUserGuard, ScopesGuard)
+@Scopes(ApiScope.internal)
+@Audit({ namespace: '@island.is/api/health-directorate' })
+@Resolver()
+export class HealthDirectorateResolver {
+  constructor(private api: HealthDirectorateService) {}
+
+  /* Organ Donation */
+  @Query(() => DonorStatus)
+  @Audit()
+  getDonorStatus(@CurrentUser() user: User): Promise<DonorStatus> {
+    return this.api.getDonorStatus(user)
+  }
+
+  @Query(() => DonationException)
+  @Audit()
+  getDonationExceptions(
+    @Args('locale', { type: () => String, nullable: true })
+    locale: Locale = 'is',
+    @CurrentUser() user: User,
+  ): Promise<DonationException> {
+    return this.api.getDonationExceptions(user, locale)
+  }
+
+  @Mutation()
+  @Audit()
+  async updateDonorStatus(
+    @Args('input') input: DonorStatusInput,
+    @CurrentUser() user: User,
+  ): Promise<void> {
+    return this.api.updateDonorStatus(user, input)
+  }
+
+  /* Vaccinations */
+  @Query(() => [Vaccinations])
+  @Audit()
+  getVaccinations(
+    @Args('locale', { type: () => String, nullable: true })
+    locale: Locale = 'is',
+    @CurrentUser() user: User,
+  ): Promise<Array<Vaccinations>> {
+    return this.api.getVaccinations(user, locale)
+  }
+}
