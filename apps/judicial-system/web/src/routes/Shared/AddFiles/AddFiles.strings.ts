a9fb9a6206b4d68b37b4dfd51a74e324152ff200
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-19 15:19:28 +0000

    Build the add files page

---
+++ apps/judicial-system/web/src/routes/Shared/AddFiles/AddFiles.strings.ts
@@ -0,0 +1,9 @@
+import { defineMessage } from 'react-intl'
+
+export const strings = {
+  heading: defineMessage({
+    id: 'judicial.system.indictments:add_files.heading',
+    defaultMessage: 'Gögn',
+    description: 'Notaður sem titill á Bæta við gögnum síðu í ákærum.',
+  }),
+}
--- apps/judicial-system/web/src/routes/Shared/AddFiles/AddFiles.tsx
+++ apps/judicial-system/web/src/routes/Shared/AddFiles/AddFiles.tsx
@@ -1,7 +1,50 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
+import { useIntl } from 'react-intl'
+
+import { Box, Text } from '@island.is/island-ui/core'
+import * as constants from '@island.is/judicial-system/consts'
+import { titles } from '@island.is/judicial-system-web/messages'
+import {
+  FormContentContainer,
+  FormContext,
+  FormFooter,
+  PageHeader,
+  PageLayout,
+  ProsecutorCaseInfo,
+} from '@island.is/judicial-system-web/src/components'
+
+import { strings } from './AddFiles.strings'
 
 const AddFiles: FC = () => {
-  return <div>AddFiles</div>
+  const { workingCase, isLoadingWorkingCase, caseNotFound } =
+    useContext(FormContext)
+
+  const { formatMessage } = useIntl()
+
+  return (
+    <PageLayout
+      workingCase={workingCase}
+      isLoading={isLoadingWorkingCase}
+      notFound={caseNotFound}
+    >
+      <PageHeader
+        title={formatMessage(titles.prosecutor.indictments.overview)}
+      />
+      <FormContentContainer>
+        <Box marginBottom={7}>
+          <Text as="h1" variant="h1">
+            {formatMessage(strings.heading)}
+          </Text>
+        </Box>
+        <ProsecutorCaseInfo workingCase={workingCase} />
+      </FormContentContainer>
+      <FormContentContainer isFooter>
+        <FormFooter
+          previousUrl={`${constants.INDICTMENTS_OVERVIEW_ROUTE}/${workingCase.id}`}
+        />
+      </FormContentContainer>
+    </PageLayout>
+  )
 }
 
 export default AddFiles
