37274a93d8fb64f613b397a0458f62247dce316e
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-19 12:06:52 +0000

    chore(j-s): InfoCard refactoring (#15683)
    * Fix sideStepper for cases in WAITING_FOR_CANCELLATION state
    
    * Add Phonenr to RenderPersonalInfo component
    
    * Add tests
    
    * Refactor
    
    * Refactor
    
    * Move UniqueDefenders to DefendantInfo component
    
    * Semantic fixes
    
    * Disallow multiple defenders
    
    * Remove unused cf string
    
    * Layout fixes
    
    * Layout fixes
    
    * Layout fixes
    
    * Fix tests
    
    * Refactor InfoCard
    
    * Refactor InfoCard
    
    * Replace InfoCard on SignedVerdictOverview screen
    
    * Add defender info for R-cases
    
    * Use new info card component on result screen
    
    * Use new info card on court of appeal overview
    
    * Us new InfoCard in closed indictment component
    
    * Use new info card component on investigation case overview screen
    
    * Use new info card component on investigation case overview screen
    
    * Use new info card component on investigation case overview screen
    
    * Use new info card component on restrictioncase overview screen
    
    * Use new info card component on restrictioncase overview screen
    
    * Update tests
    
    * InfoCard -- BlueBoxWithIcon
    
    * InfoCard -- BlueBoxWithIcon
    
    * Refactor
    
    * InfoCard -- refactor
    
    * Remove unused tests
    
    * Add columns
    
    * Add columns
    
    * Add columns
    
    * Add key to DefendantInfo
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/components/InfoCard/InfoCard.tsx
+++ apps/judicial-system/web/src/components/InfoCard/InfoCard.tsx
@@ -1,146 +1,63 @@
-import React, { FC } from 'react'
+import { FC, ReactNode } from 'react'
+import cn from 'classnames'
 
-import { Box, Icon, IconMapIcon, Text } from '@island.is/island-ui/core'
-import {
-  Defendant,
-  SessionArrangements,
-} from '@island.is/judicial-system-web/src/graphql/schema'
+import { Box, Text } from '@island.is/island-ui/core'
 
-import {
-  DefendantInfo,
-  DefendantInfoActionButton,
-} from './DefendantInfo/DefendantInfo'
 import * as styles from './InfoCard.css'
 
-export interface Defender {
-  name?: string | null
-  defenderNationalId?: string | null
-  sessionArrangement?: SessionArrangements | null
-  email?: string | null
-  phoneNumber?: string | null
+interface Section {
+  id: string // Used as a key
+  items: Item[]
+  columns?: number
 }
 
-export interface DataSection {
-  data: { title: string; value?: React.ReactNode }[]
+export interface Item {
+  id: string // Used as a key
+  title: string
+  values: string[] | ReactNode[]
 }
 
 interface Props {
-  courtOfAppealData?: { title: string; value?: React.ReactNode }[]
-  data: { title: string; value?: React.ReactNode }[]
-  defendants?: {
-    title: string
-    items: Defendant[]
-    defendantInfoActionButton?: DefendantInfoActionButton
-    displayAppealExpirationInfo?: boolean
-    displayVerdictViewDate?: boolean
-  }
-  defender?: Defender
-  icon?: IconMapIcon
-  additionalDataSections?: DataSection[]
+  sections: Section[]
 }
 
 const InfoCard: FC<Props> = (props) => {
-  const {
-    courtOfAppealData,
-    data,
-    defendants,
-    defender,
-    icon,
-    additionalDataSections,
-  } = props
+  const { sections } = props
 
   return (
-    <Box
-      className={styles.infoCardContainer}
-      padding={[2, 2, 3, 3]}
-      data-testid="infoCard"
-    >
-      <Box
-        className={(defendants || defender) && styles.infoCardTitleContainer}
-        marginBottom={(defendants || defender) && [2, 2, 3, 3]}
-        paddingBottom={[2, 2, 2, 2]}
-      >
-        {defendants && (
-          <>
-            <Text variant="h4">{defendants.title}</Text>
-            <Box marginTop={1}>
-              {defendants.items.map((defendant) => (
-                <DefendantInfo
-                  key={defendant.id}
-                  defendant={defendant}
-                  displayAppealExpirationInfo={
-                    defendants.displayAppealExpirationInfo
-                  }
-                  defendantInfoActionButton={
-                    defendants.defendantInfoActionButton
-                  }
-                  displayVerdictViewDate={defendants.displayVerdictViewDate}
-                  defender={defender}
-                />
-              ))}
-            </Box>
-          </>
-        )}
-      </Box>
-      <Box className={styles.infoCardDataContainer}>
-        {data.map((dataItem, index) => {
-          return (
-            <Box
-              data-testid={`infoCardDataContainer${index}`}
-              key={dataItem.title}
-              marginBottom={1}
-            >
-              <Text variant="h4">{dataItem.title}</Text>
-              {typeof dataItem.value === 'string' ? (
-                <Text>{dataItem.value}</Text>
+    <Box className={styles.infoCardContainer} paddingX={[2, 2, 3, 3]}>
+      {sections.map((section, index) => (
+        <Box
+          className={cn(
+            styles.grid,
+            index < sections.length - 1 ? styles.infoCardTitleContainer : null,
+            {
+              [styles.twoCols]: section.columns === 2,
+            },
+          )}
+          paddingY={[2, 2, 3, 3]}
+          key={section.id}
+        >
+          {section.items.map((item) => (
+            <Box key={item.id}>
+              {typeof item.title === 'string' ? (
+                <Text variant="h4" as="h4">
+                  {item.title}
+                </Text>
               ) : (
-                dataItem.value
+                item.title
               )}
-            </Box>
-          )
-        })}
-      </Box>
-      {courtOfAppealData && (
-        <Box className={styles.infoCardAdditionalSectionContainer}>
-          {courtOfAppealData.map((dataItem, index) => {
-            return (
-              <Box
-                data-testid={`infoCardDataContainer${index}`}
-                key={dataItem.title}
-              >
-                <Text variant="h4">{dataItem.title}</Text>
-                {typeof dataItem.value === 'string' ? (
-                  <Text>{dataItem.value}</Text>
+              {item.values.map((value, index) =>
+                typeof value === 'string' ? (
+                  <Text key={`${value}-${index}`}>{value}</Text>
                 ) : (
-                  dataItem.value
-                )}
-              </Box>
-            )
-          })}
-        </Box>
-      )}
-      {additionalDataSections?.map((section, index) => (
-        <Box className={styles.infoCardAdditionalSectionContainer} key={index}>
-          {section.data.map((dataItem, dataIndex) => (
-            <Box
-              key={dataItem.title}
-              data-testid={`infoCardDataContainer-${index}-${dataIndex}`}
-            >
-              <Text variant="h4">{dataItem.title}</Text>
-              {typeof dataItem.value === 'string' ? (
-                <Text>{dataItem.value}</Text>
-              ) : (
-                dataItem.value
+                  <Box key={`${value}-${index}`}>{value}</Box>
+                ),
               )}
             </Box>
           ))}
         </Box>
       ))}
-      {icon && (
-        <Box position="absolute" top={[2, 2, 3, 3]} right={[2, 2, 3, 3]}>
-          <Icon icon={icon} type="outline" color="blue400" size="large" />
-        </Box>
-      )}
     </Box>
   )
 }
