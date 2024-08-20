aba58f81154b96315108a3d4d8ea57b19b31692d
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-20 10:19:25 +0000

    Add UploadFiles component

---
+++ apps/judicial-system/web/src/components/UploadFiles/UploadFiles.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react'
+import { useIntl } from 'react-intl'
+
+import { Box, Button, Text } from '@island.is/island-ui/core'
+
+import { strings } from './UploadFiles.strings'
+import * as styles from './UploadFiles.css'
+
+interface Props {
+  files: { name: string }[]
+}
+
+const UploadFiles: FC<Props> = (props) => {
+  const { files } = props
+  const { formatMessage } = useIntl()
+
+  return (
+    <div className={styles.container}>
+      <Box marginBottom={1}>
+        <Text variant="h4" as="h4">
+          {formatMessage(strings.heading)}
+        </Text>
+      </Box>
+      <Box marginBottom={2}>
+        <Text>{formatMessage(strings.acceptFiles)}</Text>
+      </Box>
+      <Box marginBottom={2}>
+        <Button variant="ghost" size="small" icon="attach">
+          {formatMessage(strings.buttonText)}
+        </Button>
+      </Box>
+      {files.map((file, index) => (
+        <Box key={index} marginBottom={1}>
+          <Text>{file.name}</Text>
+        </Box>
+      ))}
+    </div>
+  )
+}
+
+export default UploadFiles
