383137ac50c77598629c46cc8a381cbfdfb6b661
Þórarinn Gunnar Árnason <torarinng@gmail.com>
2024-08-22 12:32:40 +0000

    Update GrindavikHousingBuyoutForm.ts

--- libs/application/templates/grindavik-housing-buyout/src/forms/GrindavikHousingBuyoutForm.ts
+++ libs/application/templates/grindavik-housing-buyout/src/forms/GrindavikHousingBuyoutForm.ts
@@ -161,7 +161,6 @@ export const GrindavikHousingBuyoutForm: Form = buildForm({
               id: 'loanProviders.loans',
               marginTop: 2,
               title: '',
-              startOpen: true,
               addItemButtonText: m.application.loanStatus.addNewLoan,
               saveItemButtonText: m.application.loanStatus.saveNewLoan,
               fields: {
