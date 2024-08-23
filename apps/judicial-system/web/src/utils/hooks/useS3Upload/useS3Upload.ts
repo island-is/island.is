f1d5cf053bd83c2b9a8a2c63324233156a8e690a
Ívar Oddsson <ivaroddsson@gmail.com>
2024-08-23 11:51:57 +0000

    Remove debug code

--- apps/judicial-system/web/src/utils/hooks/useS3Upload/useS3Upload.ts
+++ apps/judicial-system/web/src/utils/hooks/useS3Upload/useS3Upload.ts
@@ -282,10 +282,6 @@ const useS3Upload = (caseId: string) => {
     ) => {
       const promises = files.map(async (file) => {
         try {
-          if (file.name === 'CV.pdf') {
-            throw new Error()
-          }
-
           updateFile({ ...file, name: file.name, status: 'uploading' })
 
           const presignedPost = await getPresignedPost(file)
