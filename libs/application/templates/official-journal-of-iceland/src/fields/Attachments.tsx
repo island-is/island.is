0f5615e7736e8787bb017fd36052b46726969b97
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-08-23 13:47:45 +0000

    WIP updating attachments inputs

--- libs/application/templates/official-journal-of-iceland/src/fields/Attachments.tsx
+++ libs/application/templates/official-journal-of-iceland/src/fields/Attachments.tsx
@@ -1,63 +1,40 @@
-import { Box } from '@island.is/island-ui/core'
-import { FormGroup } from '../components/form/FormGroup'
-import { UPLOAD_ACCEPT, FILE_SIZE_LIMIT, FileNames } from '../lib/constants'
-import { attachments } from '../lib/messages'
-import { InputFields, OJOIFieldBaseProps } from '../lib/types'
-import { FileUploadController } from '@island.is/application/ui-components'
-import { Application } from '@island.is/application/types'
-import { RadioController } from '@island.is/shared/form-fields'
-import { useLocale } from '@island.is/localization'
-import { getErrorViaPath } from '@island.is/application/core'
+import { OJOIFieldBaseProps } from '../lib/types'
+import {
+  Box,
+  InputFileUpload,
+  UploadFile,
+  fileToObject,
+} from '@island.is/island-ui/core'
+import { useState } from 'react'
 
-export const Attachments = ({ application, errors }: OJOIFieldBaseProps) => {
-  const { formatMessage: f } = useLocale()
+export const Attachments = ({ application }: OJOIFieldBaseProps) => {
+  const [files, setFiles] = useState<UploadFile[]>([])
 
-  // TODO: Create wrapper around file upload component to handle file upload
+  const onUploadChange = (newFiles: File[]) => {
+    if (!newFiles) {
+      setFiles([])
+      return
+    }
+
+    console.log(newFiles)
+
+    const uploadFiles: UploadFile[] = newFiles.map((file) => fileToObject(file))
+
+    setFiles([...files, ...uploadFiles])
+  }
 
   return (
-    <FormGroup>
-      {/* <Box width="full">
-          <FileUploadController
-            application={application as unknown as Application}
-            id={InputFields.attachments.files}
-            accept={UPLOAD_ACCEPT}
-            maxSize={FILE_SIZE_LIMIT}
-            header={f(attachments.inputs.fileUpload.header)}
-            description={f(attachments.inputs.fileUpload.description)}
-            buttonLabel={f(attachments.inputs.fileUpload.buttonLabel)}
-            error={
-              errors && getErrorViaPath(errors, InputFields.attachments.files)
-            }
-          />
-        </Box>
-      </FormGroup>
-      <FormGroup title={f(attachments.headings.fileNames)}>
-        <Box width="full">
-          <Box marginBottom={2}>
-            <RadioController
-              largeButtons={false}
-              id={InputFields.attachments.fileNames}
-              defaultValue={
-                application.answers?.attachments?.fileNames ||
-                FileNames.DOCUMENT
-              }
-              options={[
-                {
-                  value: FileNames.DOCUMENT,
-                  label: f(attachments.inputs.radio.additions.label),
-                },
-                {
-                  value: FileNames.ADDITIONS,
-                  label: f(attachments.inputs.radio.documents.label),
-                },
-              ]}
-              error={
-                errors &&
-                getErrorViaPath(errors, InputFields.attachments.fileNames)
-              }
-            />
-          </Box>
-        </Box> */}
-    </FormGroup>
+    <Box padding={[2, 2, 3]} background="blue100">
+      <InputFileUpload
+        fileList={files}
+        header="Drag documents here to upload"
+        description="Documents accepted with extension: .pdf, .docx, .rtf"
+        buttonLabel="Select documents to upload"
+        onChange={onUploadChange}
+        onRemove={(file) => {
+          setFiles(files.filter((f) => f !== file))
+        }}
+      />
+    </Box>
   )
 }
