7cd7f4618ec0bf944eb9f635cd84719c00d4d178
Þórey Jóna <thoreyjona@gmail.com>
2024-08-26 16:18:46 +0000

    feat(native-app): add inbox-filters  (#15734)
    * feat: inbox rewrite, refresh only left to fix
    
    * feat: skip refresh param to inbox screen and implement pull to refresh like other tab screens
    
    * feat: update cache when marking all documents as read
    
    * fix: default page size back to 50 like before
    
    * feat: update design of accordion in app
    
    * feat: base for filtering by  category and senders
    
    * chore: add calendar icon
    
    * fix: update styles for TextField component
    
    * fix: don't show undefined for vehicle weight if something goes wrong
    
    * feat: add more translations
    
    * feat: first version of date picker
    
    * feat: add checkbox icons
    
    * feat: add date picker and finish checkbox
    
    * fix: remove icon for clear button since it cannot be used
    
    * feat: finish date picker and add apply and clear buttons
    
    * fix: clear dates on clear
    
    * fix: add back in other date picker
    
    * fix: fallback to id if name is not found for tag
    
    * fix: don't decrease unread count twice when opening a document
    
    * fix: small improvements
    
    * fix: rename checkbox-item to checkbox
    
    * fix: one api query for categories and senders
    
    * fix: use theme when I can for styling
    
    * fix: small updates to inbox-filter
    
    * fix: revert using theme for font-weight since it made the app crash
    
    * feat: add isFilterApplied variable
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/native/app/src/screens/inbox/inbox.tsx
+++ apps/native/app/src/screens/inbox/inbox.tsx
@@ -35,6 +35,7 @@ import {
   useMarkAllDocumentsAsReadMutation,
   DocumentV2,
   useListDocumentsQuery,
+  useGetDocumentsCategoriesAndSendersQuery,
 } from '../../graphql/types/schema'
 import { createNavigationOptionHooks } from '../../hooks/create-navigation-option-hooks'
 import { useConnectivityIndicator } from '../../hooks/use-connectivity-indicator'
@@ -72,6 +73,7 @@ const TagsWrapper = styled.View`
   margin-top: -4px;
   flex-direction: row;
   gap: ${({ theme }) => theme.spacing[2]}px;
+  flex-wrap: wrap;
 `
 
 const { useNavigationOptions, getNavigationOptions } =
@@ -152,6 +154,10 @@ type Filters = {
   archived?: boolean
   bookmarked?: boolean
   subjectContains?: string
+  senderNationalId?: string[]
+  categoryIds?: string[]
+  dateFrom?: Date
+  dateTo?: Date
 }
 
 function applyFilters(filters?: Filters) {
@@ -160,6 +166,10 @@ function applyFilters(filters?: Filters) {
     bookmarked: filters?.bookmarked ? true : undefined,
     opened: filters?.opened ? false : undefined,
     subjectContains: filters?.subjectContains ?? '',
+    senderNationalId: filters?.senderNationalId ?? [],
+    categoryIds: filters?.categoryIds ?? [],
+    dateFrom: filters?.dateFrom,
+    dateTo: filters?.dateTo,
   }
 }
 
@@ -167,26 +177,42 @@ export const InboxScreen: NavigationFunctionComponent<{
   opened?: boolean
   archived?: boolean
   bookmarked?: boolean
+  senderNationalId?: string[]
+  categoryIds?: string[]
+  dateFrom?: Date
+  dateTo?: Date
 }> = ({
   componentId,
   opened = false,
   archived = false,
   bookmarked = false,
+  senderNationalId = [],
+  categoryIds = [],
+  dateFrom = undefined,
+  dateTo = undefined,
 }) => {
   useNavigationOptions(componentId)
   const ui = useUiStore()
   const intl = useIntl()
+  const theme = useTheme()
   const scrollY = useRef(new Animated.Value(0)).current
   const flatListRef = useRef<FlatList>(null)
   const client = useApolloClient()
   const [query, setQuery] = useState('')
   const [loadingMore, setLoadingMore] = useState(false)
   const queryString = useThrottleState(query)
-  const theme = useTheme()
   const [hiddenContent, setHiddenContent] = useState(isIos)
   const [refetching, setRefetching] = useState(false)
   const pageRef = useRef(1)
   const loadingTimeout = useRef<ReturnType<typeof setTimeout>>()
+  const isFilterApplied =
+    opened ||
+    archived ||
+    bookmarked ||
+    senderNationalId.length ||
+    categoryIds.length ||
+    dateFrom ||
+    dateTo
 
   const incomingFilters = useMemo(() => {
     return {
@@ -194,8 +220,21 @@ export const InboxScreen: NavigationFunctionComponent<{
       archived,
       bookmarked,
       subjectContains: queryString,
+      senderNationalId,
+      categoryIds,
+      dateTo,
+      dateFrom,
     }
-  }, [opened, archived, bookmarked, queryString])
+  }, [
+    opened,
+    archived,
+    bookmarked,
+    queryString,
+    senderNationalId,
+    categoryIds,
+    dateFrom,
+    dateTo,
+  ])
 
   const [filters, setFilters] = useState(applyFilters(incomingFilters))
 
@@ -204,9 +243,17 @@ export const InboxScreen: NavigationFunctionComponent<{
       input: {
         pageSize: DEFAULT_PAGE_SIZE,
         ...filters,
+        dateFrom: dateFrom?.toISOString(),
+        dateTo: dateTo?.toISOString(),
       },
     },
   })
+
+  const sendersAndCategories = useGetDocumentsCategoriesAndSendersQuery()
+
+  const availableSenders = sendersAndCategories.data?.getDocumentSenders ?? []
+  const availableCategories =
+    sendersAndCategories.data?.getDocumentCategories ?? []
 
   const [markAllAsRead, { loading: markAllAsReadLoading }] =
     useMarkAllDocumentsAsReadMutation({
@@ -429,7 +476,8 @@ export const InboxScreen: NavigationFunctionComponent<{
     )
   }
 
-  // Fix for a bug in react-native-navigation where the large title is not visible on iOS with bottom tabs https://github.com/wix/react-native-navigation/issues/6717
+  // Fix for a bug in react-native-navigation/react-native where the large title is not visible on iOS with
+  // bottom tabs https://github.com/wix/react-native-navigation/issues/6717
   if (hiddenContent) {
     return null
   }
@@ -481,6 +529,12 @@ export const InboxScreen: NavigationFunctionComponent<{
                     opened,
                     archived,
                     bookmarked,
+                    availableSenders,
+                    availableCategories,
+                    selectedSenders: senderNationalId,
+                    selectedCategories: categoryIds,
+                    dateFrom,
+                    dateTo,
                   })
                 }}
               />
@@ -498,7 +552,7 @@ export const InboxScreen: NavigationFunctionComponent<{
                 onPress={onPressMarkAllAsRead}
               />
             </ListHeaderWrapper>
-            {opened || archived || bookmarked ? (
+            {isFilterApplied ? (
               <TagsWrapper>
                 {opened && (
                   <Tag
@@ -530,6 +584,72 @@ export const InboxScreen: NavigationFunctionComponent<{
                     closable
                     onClose={() =>
                       Navigation.updateProps(componentId, { bookmarked: false })
+                    }
+                  />
+                )}
+                {!!senderNationalId.length &&
+                  senderNationalId.map((senderId) => {
+                    const name = availableSenders.find(
+                      (sender) => sender.id === senderId,
+                    )
+                    return (
+                      <Tag
+                        key={senderId}
+                        title={name?.name?.trim() ?? senderId}
+                        closable
+                        onClose={() =>
+                          Navigation.updateProps(componentId, {
+                            senderNationalId: senderNationalId.filter(
+                              (id) => id !== senderId,
+                            ),
+                          })
+                        }
+                      />
+                    )
+                  })}
+                {!!categoryIds.length &&
+                  categoryIds.map((categoryId) => {
+                    const name = availableCategories.find(
+                      (category) => category.id === categoryId,
+                    )
+                    return (
+                      <Tag
+                        key={categoryId}
+                        title={name?.name ?? categoryId}
+                        closable
+                        onClose={() =>
+                          Navigation.updateProps(componentId, {
+                            categoryIds: categoryIds.filter(
+                              (id) => id !== categoryId,
+                            ),
+                          })
+                        }
+                      />
+                    )
+                  })}
+                {dateFrom && (
+                  <Tag
+                    title={`${intl.formatMessage({
+                      id: 'inbox.filterDateFromLabel',
+                    })} - ${intl.formatDate(dateFrom)}`}
+                    closable
+                    onClose={() =>
+                      Navigation.updateProps(componentId, {
+                        dateFrom: undefined,
+                      })
+                    }
+                  />
+                )}
+                {dateTo && (
+                  <Tag
+                    title={`${intl.formatMessage({
+                      id: 'inbox.filterDateToLabel',
+                    })} - ${intl.formatDate(dateTo)}`}
+                    closable
+                    onClose={() =>
+                      Navigation.updateProps(componentId, {
+                        dateTo: undefined,
+                      })
                     }
                   />
                 )}
