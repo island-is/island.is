0b9d9d8758b53d17ce6774d38a2d912596ca4174
albinagu <47886428+albinagu@users.noreply.github.com>
2024-09-02 16:08:27 +0000

    fix(aod): finishups (#15853)
    * fix(aod): finishups
    
    * sms tweak
    
    * Revert "sms tweak"
    
    This reverts commit fb2ccc51ff7f21fb1252165dab26c0582a7e4262.
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/announcement-of-death/src/lib/messages.ts
+++ libs/application/templates/announcement-of-death/src/lib/messages.ts
@@ -232,9 +232,14 @@ export const m = defineMessages({
     defaultMessage: 'Skráning vörsluaðila:',
     description: '',
   },
+  firearmsApplicant: {
+    id: 'aod.application:firearmsApplicant',
+    defaultMessage: 'Skráður vörsluaðili',
+    description: '',
+  },
   firearmsApplicantOverviewHeader: {
     id: 'aod.application:firearmsApplicantOverviewHeader',
-    defaultMessage: 'Skotvopn - vörsluaðili',
+    defaultMessage: 'Skotvopn',
     description: '',
   },
   firearmsApplicantName: {
