114e1aabfbc6ddbc2349f1d6643196e38483891e
Veronika Sif <veronikasif@hotmail.com>
2024-09-05 17:06:09 +0000

    Remove comments

--- libs/application/templates/new-primary-school/src/forms/NewPrimarySchoolForm/childrenNParentsSection/childInfoSubSection.ts
+++ libs/application/templates/new-primary-school/src/forms/NewPrimarySchoolForm/childrenNParentsSection/childInfoSubSection.ts
@@ -48,8 +48,6 @@ export const childInfoSubSection = buildSubSection({
           title: newPrimarySchoolMessages.shared.address,
           width: 'half',
           disabled: true,
-          // TODO: Nota gögn frá Júní
-          // TODO: Hægt að nota heimilisfang innskráðs foreldris? (foreldri getur ekki sótt um nema barn sé með sama lögheimili)
           defaultValue: (application: Application) =>
             getApplicationExternalData(application.externalData)
               .applicantAddress,
@@ -59,8 +57,6 @@ export const childInfoSubSection = buildSubSection({
           title: newPrimarySchoolMessages.shared.postalCode,
           width: 'half',
           disabled: true,
-          // TODO: Nota gögn frá Júní
-          // TODO: Hægt að nota heimilisfang innskráðs foreldris? (foreldri getur ekki sótt um nema barn sé með sama lögheimili)
           defaultValue: (application: Application) =>
             getApplicationExternalData(application.externalData)
               .applicantPostalCode,
@@ -70,8 +66,6 @@ export const childInfoSubSection = buildSubSection({
           title: newPrimarySchoolMessages.shared.municipality,
           width: 'half',
           disabled: true,
-          // TODO: Nota gögn frá Júní
-          // TODO: Hægt að nota heimilisfang innskráðs foreldris? (foreldri getur ekki sótt um nema barn sé með sama lögheimili)
           defaultValue: (application: Application) =>
             getApplicationExternalData(application.externalData).applicantCity,
         }),
