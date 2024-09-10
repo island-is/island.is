83d28e9f94cfa153e426fc8549b4cc2015925f75
albina <albina@kosmosogkaos.is>
2024-09-10 13:14:00 +0000

    fix(driving-license): adding back JurisdictionApi check

---
+++ libs/application/templates/driving-license/src/forms/draft/subSectionDelivery.ts
@@ -0,0 +1,51 @@
+import {
+  buildDescriptionField,
+  buildMultiField,
+  buildSelectField,
+  buildSubSection,
+} from '@island.is/application/core'
+import { m } from '../../lib/messages'
+import {
+  chooseDistrictCommissionerDescription,
+  hasNoDrivingLicenseInOtherCountry,
+} from '../../lib/utils'
+
+import { Jurisdiction } from '@island.is/clients/driving-license'
+
+export const subSectionDelivery = buildSubSection({
+  id: 'user',
+  title: m.informationSectionTitle,
+  condition: hasNoDrivingLicenseInOtherCountry,
+  children: [
+    buildMultiField({
+      id: 'info',
+      title: m.pickupLocationTitle,
+      space: 1,
+      children: [
+        buildDescriptionField({
+          id: 'afhending',
+          title: m.districtCommisionerTitle,
+          titleVariant: 'h4',
+          description: chooseDistrictCommissionerDescription,
+        }),
+        buildSelectField({
+          id: 'jurisdiction',
+          title: m.districtCommisionerPickup,
+          disabled: false,
+          required: true,
+          options: ({
+            externalData: {
+              jurisdictions: { data },
+            },
+          }) => {
+            return (data as Jurisdiction[]).map(({ id, name, zip }) => ({
+              value: `${id}`,
+              label: name,
+              tooltip: `Póstnúmer ${zip}`,
+            }))
+          },
+        }),
+      ],
+    }),
+  ],
+})
