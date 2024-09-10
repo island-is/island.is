06396de53cd92fe274b10d7b7108f2b88b1687fa
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-09-10 12:27:53 +0000

    starting client setup, getting open-api from VER

---
+++ libs/clients/work-accident-ver/src/lib/workAccident.module.ts
@@ -0,0 +1,8 @@
+// import { Module } from '@nestjs/common'
+// import { WorkAccidentClientService } from './workAccident.service'
+
+// @Module({
+//   providers: [WorkAccidentClientService, PROVIDER],
+//   exports: [WorkAccidentClientService],
+// })
+// export class WorkMachinesClientModule {}
