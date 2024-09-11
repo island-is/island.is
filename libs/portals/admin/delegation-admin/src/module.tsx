1cc48459b3e26420496842f4dab69d3bc3ffe554
Gunnlaugur Guðmundsson <34029342+GunnlaugurG@users.noreply.github.com>
2024-09-11 12:51:37 +0000

    feat(auth-admin): Delegation admin module (#15867)
    * created new admin module for paper delegations and get route
    
    * field resolver DelegationAdminModel
    
    * lookup for delegations with detailed view
    
    * Cleanup and rest and graphql for DeleteDelegation
    
    * small cleanup
    
    * chore: nx format:write update dirty files
    
    * move delegationAdmin service to admin-api from delegation-api
    
    * chore: nx format:write update dirty files
    
    * fix config value
    
    * chore: charts update dirty files
    
    * fix api build issues
    
    * fix pr comments
    
    * remove resolved comment
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/portals/admin/delegation-admin/src/module.tsx
@@ -0,0 +1,42 @@
+import { PortalModule } from '@island.is/portals/core'
+import { m } from './lib/messages'
+import { DelegationAdminPaths } from './lib/paths'
+import { lazy } from 'react'
+import { AdminPortalScope } from '@island.is/auth/scopes'
+import { delegationAdminLoader } from './screens/DelegationAdminDetails/DelegationAdmin.loader'
+import { FindDelegationForNationalId } from './screens/Root.action'
+
+const DelegationAdminScreen = lazy(() =>
+  import('./screens/DelegationAdminDetails/DelegationAdmin'),
+)
+const RootScreen = lazy(() => import('./screens/Root'))
+
+const allowedScopes: string[] = [
+  AdminPortalScope.delegationSystem,
+  AdminPortalScope.delegationSystemAdmin,
+]
+
+export const delegationAdminModule: PortalModule = {
+  name: m.delegationAdmin,
+  enabled({ userInfo }) {
+    return userInfo.scopes.some((scope) => allowedScopes.includes(scope))
+  },
+  routes(props) {
+    return [
+      {
+        name: m.delegationAdmin,
+        path: DelegationAdminPaths.Root,
+        index: true,
+        action: FindDelegationForNationalId(props),
+        element: <RootScreen />,
+        children: [],
+      },
+      {
+        name: m.delegationAdmin,
+        path: DelegationAdminPaths.DelegationAdmin,
+        element: <DelegationAdminScreen />,
+        loader: delegationAdminLoader(props),
+      },
+    ]
+  },
+}
