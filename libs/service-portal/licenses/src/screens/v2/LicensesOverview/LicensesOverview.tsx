dec2a6c91f5c4146c811250b53939f731986e877
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-09-12 13:28:24 +0000

    chore: remove mocks

--- libs/service-portal/licenses/src/screens/v2/LicensesOverview/LicensesOverview.tsx
+++ libs/service-portal/licenses/src/screens/v2/LicensesOverview/LicensesOverview.tsx
@@ -132,42 +132,12 @@ export const LicensesOverviewV2 = () => {
                   licenses.filter((license) => !license.isOwnerChildOfUser),
                 ),
               },
-              /*{
+              {
                 label: formatMessage(m.licenseTabSecondary),
                 content: generateLicenseStack(
                   licenses.filter((license) => license.isOwnerChildOfUser),
                 ),
               },
-              {
-                label: 'GRILLIÐ',
-                content: generateLicenseStack(
-                  licenses.filter((license) => license.isOwnerChildOfUser),
-                ),
-              },
-              {
-                label: 'ER',
-                content: generateLicenseStack(
-                  licenses.filter((license) => license.isOwnerChildOfUser),
-                ),
-              },
-              {
-                label: 'ALVEG',
-                content: generateLicenseStack(
-                  licenses.filter((license) => license.isOwnerChildOfUser),
-                ),
-              },*/
-              {
-                label: 'AÐ',
-                content: generateLicenseStack(
-                  licenses.filter((license) => license.isOwnerChildOfUser),
-                ),
-              },
-              /*{
-                label: 'LOKA',
-                content: generateLicenseStack(
-                  licenses.filter((license) => license.isOwnerChildOfUser),
-                ),
-              },*/
             ]}
           />
         </Box>
