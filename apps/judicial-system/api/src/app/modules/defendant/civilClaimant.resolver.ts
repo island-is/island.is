d2afd927b13cee1900b77d9334308b2047e3f713
unakb <una@kolibri.is>
2024-09-13 10:48:28 +0000

    fix(j-s): backend usage

--- apps/judicial-system/api/src/app/modules/defendant/civilClaimant.resolver.ts
+++ apps/judicial-system/api/src/app/modules/defendant/civilClaimant.resolver.ts
@@ -1,7 +1,17 @@
-import { UseGuards } from '@nestjs/common'
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Inject, Logger, UseGuards } from '@nestjs/common'
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
 
-import { JwtGraphQlAuthGuard } from '@island.is/judicial-system/auth'
+import { LOGGER_PROVIDER } from '@island.is/logging'
+
+import {
+  AuditedAction,
+  AuditTrailService,
+} from '@island.is/judicial-system/audit-trail'
+import {
+  CurrentGraphQlUser,
+  JwtGraphQlAuthGuard,
+} from '@island.is/judicial-system/auth'
+import type { User } from '@island.is/judicial-system/types'
 
 import { BackendService } from '../backend'
 import { CreateCivilClaimantInput } from './dto/createCivilClaimant.input'
@@ -10,19 +20,31 @@ import { CivilClaimant } from './models/civilClaimant.model'
 @UseGuards(JwtGraphQlAuthGuard)
 @Resolver(() => CivilClaimant)
 export class CivilClaimantResolver {
-  constructor(private readonly backendService: BackendService) {}
+  constructor(
+    private readonly auditTrailService: AuditTrailService,
+    @Inject(LOGGER_PROVIDER)
+    private readonly logger: Logger,
+  ) {}
 
   @Mutation(() => CivilClaimant)
   async createCivilClaimant(
     @Args('createCivilClaimantDto')
     input: CreateCivilClaimantInput,
+    @CurrentGraphQlUser() user: User,
+    @Context('dataSources')
+    { backendService }: { backendService: BackendService },
   ): Promise<CivilClaimant> {
     const { caseId, defendantId, ...createCivilClaimant } = input
 
-    return this.backendService.createCivilClaimant(
-      caseId,
-      defendantId,
-      createCivilClaimant,
+    return this.auditTrailService.audit(
+      user.id,
+      AuditedAction.CREATE_CIVIL_CLAIMANT,
+      backendService.createCivilClaimant(
+        caseId,
+        defendantId,
+        createCivilClaimant,
+      ),
+      (civilClaimant) => civilClaimant.id,
     )
   }
 }
