ce93bf8b5b447166804cfe3edbe5f5c4836d6f97
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2024-09-14 21:44:06 +0000

    add unique constraint to reference_id in delegations table

--- libs/auth-api-lib/migrations/20240910092516-delegation-reference-id.js
+++ libs/auth-api-lib/migrations/20240910092516-delegation-reference-id.js
@@ -7,12 +7,21 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       }),
+
+      queryInterface.sequelize.query(`
+        CREATE UNIQUE INDEX unique_reference_id_not_null
+        ON delegation(reference_id)
+        WHERE reference_id IS NOT NULL;
+      `),
     ])
   },
 
   async down(queryInterface, Sequelize) {
     return Promise.all([
       queryInterface.removeColumn('delegation', 'reference_id'),
+      queryInterface.sequelize.query(`
+        DROP INDEX IF EXISTS unique_reference_id_not_null;
+      `),
     ])
   },
 }
