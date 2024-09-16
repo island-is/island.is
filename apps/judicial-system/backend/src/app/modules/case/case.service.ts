c2ab2f5400df8f94bd1f74cfdd30c1a716e5a558
unakb <una@kolibri.is>
2024-09-16 20:21:19 +0000

    feat(j-s): Subpoena delivery and endpoint for updates (#15918)
    * feat(j-s): New endpoints for subpoenas in xrd-api
    
    * test(j-s): Tests
    
    * feat(j-s): WIP create subpoena
    
    * feat(j-s): New subpoena table
    
    * chore(j-s): Add institution addresses to db
    
    * chore(j-s): Use institution from db for subpoena
    
    * chore(j-s): Subpoena work
    
    * fix(j-s): remove unused code
    
    * Update case.service.ts
    
    * Update message.service.ts
    
    * fix(j-s): Renaming and exception handling
    
    * Delete 20240815104210-update-explanatory_comment.js
    
    * fix(j-s): Complete rewrite to create a new subpoena module to handle subpoenas
    
    Rather than having the logic all over and confusing
    
    * Cleanup
    
    * fix(j-s): More cleanup
    
    * feat(j-s): Update defendant info
    
    * feat(j-s): Add guards
    
    * fix(j-s): cleanup
    
    * Update app.service.ts
    
    * Update app.service.ts
    
    * fix(j-s): Removed unused files
    
    * Update subpoena.dto.ts
    
    * fix(j-s): Feedback resolved
    
    * fix(j-s): cleanup
    
    * fix(j-s): review fixes
    
    * feat(j-s): Add field for registered by
    
    ---------
    
    Co-authored-by: Guðjón Guðjónsson <gudjon@kolibri.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/backend/src/app/modules/case/case.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.service.ts
@@ -597,7 +597,7 @@ export class CaseService {
     ]
   }
 
-  private addMessagesForSubmittedIndicitmentCaseToQueue(
+  private addMessagesForSubmittedIndictmentCaseToQueue(
     theCase: Case,
     user: TUser,
   ): Promise<void> {
@@ -1181,7 +1181,7 @@ export class CaseService {
           await this.addMessagesForCompletedCaseToQueue(updatedCase, user)
         }
       } else if (updatedCase.state === CaseState.SUBMITTED && isIndictment) {
-        await this.addMessagesForSubmittedIndicitmentCaseToQueue(
+        await this.addMessagesForSubmittedIndictmentCaseToQueue(
           updatedCase,
           user,
         )
