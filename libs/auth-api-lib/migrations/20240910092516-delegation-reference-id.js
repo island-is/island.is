86a893a51a9b96210acd36bfc8dd2c72f676c375
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2024-09-16 11:07:40 +0000

    simplifying the migration script

--- libs/auth-api-lib/migrations/20240910092516-delegation-reference-id.js
+++ libs/auth-api-lib/migrations/20240910092516-delegation-reference-id.js
@@ -6,22 +6,14 @@ module.exports = {
       queryInterface.addColumn('delegation', 'reference_id', {
         type: Sequelize.STRING,
         allowNull: true,
+        unique: true,
       }),
-
-      queryInterface.sequelize.query(`
-        CREATE UNIQUE INDEX unique_reference_id_not_null
-        ON delegation(reference_id)
-        WHERE reference_id IS NOT NULL;
-      `),
     ])
   },
 
   async down(queryInterface, Sequelize) {
     return Promise.all([
       queryInterface.removeColumn('delegation', 'reference_id'),
-      queryInterface.sequelize.query(`
-        DROP INDEX IF EXISTS unique_reference_id_not_null;
-      `),
     ])
   },
 }
