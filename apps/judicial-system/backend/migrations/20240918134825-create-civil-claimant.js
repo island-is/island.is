42e551a379cc1d019d047afd003d34e830c32fc4
Ívar Oddsson <ivaroddsson@gmail.com>
2024-09-18 14:36:14 +0000

    Add modified field to create civil claimant migration

--- apps/judicial-system/backend/migrations/20240918134825-create-civil-claimant.js
+++ apps/judicial-system/backend/migrations/20240918134825-create-civil-claimant.js
@@ -17,6 +17,11 @@ module.exports = {
             defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
             allowNull: false,
           },
+          modified: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
           case_id: {
             type: Sequelize.UUID,
             references: {
