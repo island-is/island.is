c4a2dd2667b1e4c50cb93983a3adda965e8b512e
Ívar Oddsson <ivaroddsson@gmail.com>
2024-09-19 08:16:01 +0000

    Checkpoint

---
+++ apps/judicial-system/web/src/components/Inputs/InputName.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, FC, FocusEvent, useEffect, useState } from 'react'
+import { useIntl } from 'react-intl'
+
+import { Input } from '@island.is/island-ui/core'
+import { core } from '@island.is/judicial-system-web/messages'
+
+import { validate } from '../../utils/validate'
+import { InputProps } from './types'
+
+const InputName: FC<InputProps> = (props) => {
+  const { value, onChange, onBlur, label, placeholder } = props
+
+  const { formatMessage } = useIntl()
+
+  const [errorMessage, setErrorMessage] = useState<string>()
+  const [inputValue, setInputValue] = useState<string>(value || '')
+
+  const handleBlur = (
+    evt: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>,
+  ) => {
+    const inputValidator = validate([[evt.target.value, ['empty']]])
+
+    if (inputValidator.isValid) {
+      setErrorMessage(undefined)
+      onBlur(inputValue)
+    } else {
+      setErrorMessage(inputValidator.errorMessage)
+    }
+  }
+
+  const handleChange = (
+    evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setInputValue(evt.target.value)
+    onChange && onChange(inputValue)
+  }
+
+  useEffect(() => {
+    if (!value) {
+      return
+    }
+
+    setInputValue(value)
+  }, [value])
+
+  return (
+    <Input
+      data-testid="inputName"
+      name="inputName"
+      autoComplete="off"
+      label={label ? label : formatMessage(core.fullName)}
+      placeholder={placeholder ? placeholder : formatMessage(core.fullName)}
+      errorMessage={errorMessage}
+      hasError={errorMessage !== undefined}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={inputValue}
+    />
+  )
+}
+
+export default InputName
---
+++ apps/judicial-system/web/src/components/Inputs/types.d.ts
@@ -0,0 +1,12 @@
+export interface InputProps {
+  onBlur: (value: string) => void
+  onChange?: (value: string) => void
+  disabled?: boolean
+  value?: string
+
+  // Use `label` to overwrite the default label
+  label?: string
+
+  // Use `placeholder` to overwrite the default placeholder text
+  placeholder?: string
+}
