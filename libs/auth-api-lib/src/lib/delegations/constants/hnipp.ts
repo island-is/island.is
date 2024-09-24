3c6683c221b472e9c743a7155a1b66827684e9ce
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2024-09-24 13:50:49 +0000

    adds translations for domain name
    const for now, will be revisited later

--- libs/auth-api-lib/src/lib/delegations/constants/hnipp.ts
+++ libs/auth-api-lib/src/lib/delegations/constants/hnipp.ts
@@ -1,2 +1,5 @@
 export const NEW_DELEGATION_TEMPLATE_ID = 'HNIPP.UMBOD.NYTT'
 export const UPDATED_DELEGATION_TEMPLATE_ID = 'HNIPP.UMBOD.UPPFAERT'
+
+export const DOMAIN_NAME_IS = 'opinberar stafrænar þjónustur'
+export const DOMAIN_NAME_EN = 'public digital services'
