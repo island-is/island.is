651f3377f2855851debbb95e480ffa0f4346f19e
Gunnlaugur Guðmundsson <34029342+GunnlaugurG@users.noreply.github.com>
2024-09-25 20:14:38 +0000

    feat(auth-admin): Create paper delegation (#15992)
    * created new admin module for paper delegations and get route
    
    * field resolver DelegationAdminModel
    
    * lookup for delegations with detailed view
    
    * Cleanup and rest and graphql for DeleteDelegation
    
    * small cleanup
    
    * chore: nx format:write update dirty files
    
    * move delegationAdmin service to admin-api from delegation-api
    
    * adds getTicket function to Zendesk service
    
    Fetches Zendesk ticket by id
    
    * Adds create delegation route
    
    Form for creating delegation, wip form action
    
    * chore: nx format:write update dirty files
    
    * fix config value
    
    * chore: charts update dirty files
    
    * fix api build issues
    
    * wip gql for create delegation
    
    * fix pr comments
    
    * delegation reference id added
    
    * added back the spec files
    
    * validate form data and show error messages
    
    * fix get tests
    
    * chore: nx format:write update dirty files
    
    * test for delete
    
    * post method to create delegation between two national id's
    
    * zendesk integration complete
    
    * remove console log
    
    * merged with main
    
    * chore: charts update dirty files
    
    * chore: nx format:write update dirty files
    
    * adds CreateDelegationConfirm modal and prefills Create form with values from search params
    
    * chore: nx format:write update dirty files
    
    * use identity resolver
    
    fixes in response to comments
    
    * chore: nx format:write update dirty files
    
    * created delegation-delegation-type.model.ts and updated findAllScopesTo in delegation-scope.service.ts
    
    * fix broken tests
    
    * tests for findAllScopesTo
    
    * added validTo to delegationDelegationType
    
    * set general mandate as type in ids select account prompt
    
    * Get general mandate to delegations-to on service-portal
    
    * remove duplicate case
    
    * small refactor
    
    * chore: nx format:write update dirty files
    
    * Mask nationalId in url
    
    * format national id
    
    * fix tests after merge with main
    
    * chore: nx format:write update dirty files
    
    * fix duplicate referenceId's
    
    * fix import
    
    * remove console log and unused variables
    
    * chore: nx format:write update dirty files
    
    * move general mandate tests to new file
    
    * add zendesk validation
    
    * feat(auth-admin): Delete delegation UI (#16073)
    
    * Changes includeArchived from Param to Query
    
    Fixes openapi.yaml error
    
    * adds Oauth2 to openApi document builder
    
    * validates Zendesk ticket when creating delegation
    
    * Adds delete button to delegation access card and call delete mutation
    
    * chore: nx format:write update dirty files
    
    * add referenceId to delegation query
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    
    * feat(auth-admin): Create paper delegation zendesk integration (#16074)
    
    * Changes includeArchived from Param to Query
    
    Fixes openapi.yaml error
    
    * adds Oauth2 to openApi document builder
    
    * validates Zendesk ticket when creating delegation
    
    * chore: nx format:write update dirty files
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    
    * connect changes and modify incoming delegations for new ddt table
    
    * fix comments from PR
    
    * fix pr comment
    
    * chore: nx format:write update dirty files
    
    * chore: nx format:write update dirty files
    
    * fix pr comment
    
    * add tests for create
    
    * fix pr comments
    
    * simplify var names
    
    * chore: nx format:write update dirty files
    
    * add index for general mandate
    
    * fix pr comments
    
    * fix pr comments
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ libs/auth-api-lib/src/lib/delegations/constants/zendesk.ts
@@ -0,0 +1,5 @@
+export const DELEGATION_TAG = 'umsokn_um_umboð_a_mínum_síðum'
+export const ZENDESK_CUSTOM_FIELDS = {
+  DelegationFromReferenceId: 21401464004498,
+  DelegationToReferenceId: 21401435545234,
+}
