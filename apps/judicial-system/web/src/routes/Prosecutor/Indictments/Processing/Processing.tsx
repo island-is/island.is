7f04ec80dc012681d0781a1f5afc1bfcb47ccc16
Ívar Oddsson <ivaroddsson@gmail.com>
2024-09-25 14:38:57 +0000

    Checkpoint

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/Processing/Processing.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Processing/Processing.tsx
@@ -256,7 +256,11 @@ const Processing: FC = () => {
   )
 
   useEffect(() => {
-    if (!personData || !personData.items || personData.items.length === 0) {
+    if (!personData || !personData.items) {
+      return
+    }
+
+    if (personData.items.length === 0) {
       setNationalIdNotFound(true)
       return
     }
