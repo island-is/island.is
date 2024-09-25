5d0f7090b8002b51d7764b36f2f60dc302df564e
andes-it <builders@andes.is>
2024-09-25 13:25:39 +0000

    chore: nx format:write update dirty files

--- libs/application/template-api-modules/src/lib/modules/templates/accident-notification/accident-notification.utils.spec.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/accident-notification/accident-notification.utils.spec.ts
@@ -175,31 +175,34 @@ describe('applicationAnswersToXml', () => {
   )
 })
 
-const createMockPartialApplication = (documentId?: number): Application => ({
+const createMockPartialApplication = (documentId?: number): Application =>
+  ({
     externalData: {
       submitApplication: {
-        data: documentId ? { documentId, sentDocuments: ['hello', 'there'] } : undefined,
+        data: documentId
+          ? { documentId, sentDocuments: ['hello', 'there'] }
+          : undefined,
       },
     },
-  } as unknown as Application
-)
+  } as unknown as Application)
 
 describe('getApplicationDocumentId', () => {
-    it('should return a valid application submission document id', () => {
-      const expectedId = 5555
-      const application = createMockPartialApplication(expectedId)
+  it('should return a valid application submission document id', () => {
+    const expectedId = 5555
+    const application = createMockPartialApplication(expectedId)
 
-      const result = utils.getApplicationDocumentId(application)
-      expect(result).toEqual(expectedId)
-    })
+    const result = utils.getApplicationDocumentId(application)
+    expect(result).toEqual(expectedId)
+  })
 
-    it('should throw when there is no valid application submission document id', () => {
-      const application = createMockPartialApplication(undefined)
+  it('should throw when there is no valid application submission document id', () => {
+    const application = createMockPartialApplication(undefined)
 
-      expect(() => utils.getApplicationDocumentId(application)).toThrowError('No documentId found on application')
+    expect(() => utils.getApplicationDocumentId(application)).toThrowError(
+      'No documentId found on application',
+    )
   })
 })
-
 
 const getDefaultAttachments = (): AccidentNotificationAttachment[] => {
   return [
