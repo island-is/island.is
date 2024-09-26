8e17b19f59e5c89e9c9b82277305f90ddd1c4d12
unakb <una@kolibri.is>
2024-09-26 13:51:31 +0000

    feat(j-s): Judge can add and remove advocates

--- apps/judicial-system/web/src/routes/Court/Indictments/Advocates/SelectCivilClaimantAdvocate.tsx
+++ apps/judicial-system/web/src/routes/Court/Indictments/Advocates/SelectCivilClaimantAdvocate.tsx
@@ -1,7 +1,14 @@
 import { FC, useContext } from 'react'
 import { useIntl } from 'react-intl'
 
-import { Box, Checkbox, RadioButton, Text } from '@island.is/island-ui/core'
+import {
+  AlertMessage,
+  Box,
+  Button,
+  Checkbox,
+  RadioButton,
+  Text,
+} from '@island.is/island-ui/core'
 import {
   BlueBox,
   FormContext,
@@ -41,79 +48,113 @@ const SelectCivilClaimantAdvocate: FC<Props> = ({ civilClaimant }) => {
       <Box marginBottom={2}>
         <Text variant="h4">{civilClaimant.name}</Text>
       </Box>
-      <Box display="flex" marginY={2}>
-        <Box width="half" marginRight={1}>
-          <RadioButton
-            name="civilClaimantAdvocateType"
-            id={`civil_claimant_lawyer-${civilClaimant.id}`}
-            label={formatMessage(strings.lawyer)}
-            large
-            backgroundColor="white"
-            checked={civilClaimant.spokespersonIsLawyer === true}
-            onChange={() =>
-              updateCivilClaimant({
-                spokespersonIsLawyer: true,
-              } as UpdateCivilClaimantInput)
+      {civilClaimant.hasSpokesperson ? (
+        <>
+          <Box display="flex" marginY={2}>
+            <Box width="half" marginRight={1}>
+              <RadioButton
+                name="civilClaimantAdvocateType"
+                id={`civil_claimant_lawyer-${civilClaimant.id}`}
+                label={formatMessage(strings.lawyer)}
+                large
+                backgroundColor="white"
+                checked={civilClaimant.spokespersonIsLawyer === true}
+                onChange={() =>
+                  updateCivilClaimant({
+                    spokespersonIsLawyer: true,
+                  } as UpdateCivilClaimantInput)
+                }
+              />
+            </Box>
+            <Box width="half" marginLeft={1}>
+              <RadioButton
+                name="civilClaimantAdvocateType"
+                id={`civil_claimant_legal_rights_protector-${civilClaimant.id}`}
+                label={formatMessage(strings.legalRightsProtector)}
+                large
+                backgroundColor="white"
+                checked={civilClaimant.spokespersonIsLawyer === false}
+                onChange={() =>
+                  updateCivilClaimant({
+                    spokespersonIsLawyer: false,
+                  } as UpdateCivilClaimantInput)
+                }
+              />
+            </Box>
+          </Box>
+          <Box marginBottom={2}>
+            <InputAdvocate
+              clientId={civilClaimant.id}
+              advocateType={
+                civilClaimant.spokespersonIsLawyer
+                  ? 'defender'
+                  : 'legal_rights_protector'
+              }
+              disabled={
+                civilClaimant.spokespersonIsLawyer === null ||
+                civilClaimant.spokespersonIsLawyer === undefined
+              }
+              isCivilClaim={true}
+            />
+          </Box>
+          <Checkbox
+            name={`shareFilesWithCivilClaimantAdvocate-${civilClaimant.id}`}
+            label={formatMessage(strings.shareFilesWithCivilClaimantAdvocate, {
+              defenderIsLawyer: civilClaimant.spokespersonIsLawyer,
+            })}
+            checked={Boolean(civilClaimant.caseFilesSharedWithSpokesperson)}
+            disabled={
+              civilClaimant.spokespersonIsLawyer === null ||
+              civilClaimant.spokespersonIsLawyer === undefined
             }
-          />
-        </Box>
-        <Box width="half" marginLeft={1}>
-          <RadioButton
-            name="civilClaimantAdvocateType"
-            id={`civil_claimant_legal_rights_protector-${civilClaimant.id}`}
-            label={formatMessage(strings.legalRightsProtector)}
-            large
-            backgroundColor="white"
-            checked={civilClaimant.spokespersonIsLawyer === false}
-            onChange={() =>
+            onChange={() => {
               updateCivilClaimant({
-                spokespersonIsLawyer: false,
+                caseFilesSharedWithSpokesperson:
+                  !civilClaimant.caseFilesSharedWithSpokesperson,
               } as UpdateCivilClaimantInput)
-            }
+            }}
+            tooltip={formatMessage(
+              strings.shareFilesWithCivilClaimantAdvocateTooltip,
+              {
+                defenderIsLawyer: civilClaimant.spokespersonIsLawyer,
+              },
+            )}
+            backgroundColor="white"
+            large
+            filled
           />
-        </Box>
-      </Box>
-      <Box marginBottom={2}>
-        <InputAdvocate
-          clientId={civilClaimant.id}
-          advocateType={
-            civilClaimant.spokespersonIsLawyer
-              ? 'defender'
-              : 'legal_rights_protector'
-          }
-          disabled={
-            civilClaimant.spokespersonIsLawyer === null ||
-            civilClaimant.spokespersonIsLawyer === undefined
-          }
-          isCivilClaim={true}
+        </>
+      ) : (
+        <AlertMessage
+          message={formatMessage(strings.noCivilClaimantAdvocate)}
+          type="info"
         />
+      )}
+      <Box display="flex" justifyContent="flexEnd" marginTop={2}>
+        <Button
+          variant="text"
+          colorScheme={
+            civilClaimant.hasSpokesperson ? 'destructive' : 'default'
+          }
+          onClick={() => {
+            updateCivilClaimant({
+              hasSpokesperson: !civilClaimant.hasSpokesperson,
+              spokespersonEmail: null,
+              spokespersonPhoneNumber: null,
+              spokespersonName: null,
+              spokespersonIsLawyer: null,
+              spokespersonNationalId: null,
+              caseFilesSharedWithSpokesperson: null,
+            } as UpdateCivilClaimantInput)
+          }}
+        >
+          {civilClaimant.hasSpokesperson
+            ? formatMessage(strings.removeCivilClaimantAdvocate, {
+                defenderIsLawyer: civilClaimant.spokespersonIsLawyer,
+              })
+            : formatMessage(strings.addCivilClaimantAdvocate)}
+        </Button>
       </Box>
-      <Checkbox
-        name={`shareFilesWithCivilClaimantAdvocate-${civilClaimant.id}`}
-        label={formatMessage(strings.shareFilesWithCivilClaimantAdvocate, {
-          defenderIsLawyer: civilClaimant.spokespersonIsLawyer,
-        })}
-        checked={Boolean(civilClaimant.caseFilesSharedWithSpokesperson)}
-        disabled={
-          civilClaimant.spokespersonIsLawyer === null ||
-          civilClaimant.spokespersonIsLawyer === undefined
-        }
-        onChange={() => {
-          updateCivilClaimant({
-            caseFilesSharedWithSpokesperson:
-              !civilClaimant.caseFilesSharedWithSpokesperson,
-          } as UpdateCivilClaimantInput)
-        }}
-        tooltip={formatMessage(
-          strings.shareFilesWithCivilClaimantAdvocateTooltip,
-          {
-            defenderIsLawyer: civilClaimant.spokespersonIsLawyer,
-          },
-        )}
-        backgroundColor="white"
-        large
-        filled
-      />
     </BlueBox>
   )
 }
