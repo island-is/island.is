116602b1c1d2fa35138d5e7a6e6fcd318f29dd79
andes-it <builders@andes.is>
2024-09-27 16:09:58 +0000

    chore: nx format:write update dirty files

--- libs/application/templates/accident-notification/src/utils/hasMissingDocuments.spec.ts
+++ libs/application/templates/accident-notification/src/utils/hasMissingDocuments.spec.ts
@@ -1,80 +1,91 @@
-import { getErrorMessageForMissingDocuments, hasMissingDocuments } from './hasMissingDocuments'
+import {
+  getErrorMessageForMissingDocuments,
+  hasMissingDocuments,
+} from './hasMissingDocuments'
 import { WhoIsTheNotificationForEnum, AttachmentsEnum } from '../types'
 import { YES } from '../constants'
 import { FormatMessage } from '@island.is/localization'
 import { FormValue } from '@island.is/application/types'
 
 describe('hasMissingDocuments', () => {
+  it('should return true when missing documents', () => {
+    expect(hasMissingDocuments(getMissingDocuments())).toEqual(true)
+  })
 
-
-    it('should return true when missing documents', () => {
-        expect(hasMissingDocuments(getMissingDocuments())).toEqual(true)
-    })
-
-    it('should return false when no missing documents', () => {
-        expect(hasMissingDocuments(getNoMissingDocuments())).toEqual(false)
-    })
+  it('should return false when no missing documents', () => {
+    expect(hasMissingDocuments(getNoMissingDocuments())).toEqual(false)
+  })
 })
 
 describe('getErrorMessageForMissingDocuments', () => {
-    const formatMessage: FormatMessage = jest.fn().mockReturnValue('test.pdf')
-    it('should return error message for missing documents', () => {
-        const result = getErrorMessageForMissingDocuments(getMissingDocuments(), formatMessage, false)
-        expect(result).toEqual('test.pdf, test.pdf, test.pdf')
-    })
+  const formatMessage: FormatMessage = jest.fn().mockReturnValue('test.pdf')
+  it('should return error message for missing documents', () => {
+    const result = getErrorMessageForMissingDocuments(
+      getMissingDocuments(),
+      formatMessage,
+      false,
+    )
+    expect(result).toEqual('test.pdf, test.pdf, test.pdf')
+  })
 })
 
 const getMissingDocuments = (): FormValue => {
-    return {
-        whoIsTheNotificationFor: {
-            answer: WhoIsTheNotificationForEnum.POWEROFATTORNEY
-        },
-        wasTheAccidentFatal: YES,
-        injuryCertificate: {
-            answer: AttachmentsEnum.SENDCERTIFICATELATER
-        },
-        attachments: {
-            injuryCertificateFile: {
-                file: []
-            },
-            deathCertificateFile: {
-                file: []
-            },
-            powerOfAttorneyFile: {
-                file: []
-            }
-        }
-    }
+  return {
+    whoIsTheNotificationFor: {
+      answer: WhoIsTheNotificationForEnum.POWEROFATTORNEY,
+    },
+    wasTheAccidentFatal: YES,
+    injuryCertificate: {
+      answer: AttachmentsEnum.SENDCERTIFICATELATER,
+    },
+    attachments: {
+      injuryCertificateFile: {
+        file: [],
+      },
+      deathCertificateFile: {
+        file: [],
+      },
+      powerOfAttorneyFile: {
+        file: [],
+      },
+    },
+  }
 }
 
 const getNoMissingDocuments = (): FormValue => {
-    return {
-        whoIsTheNotificationFor: {
-            answer: WhoIsTheNotificationForEnum.POWEROFATTORNEY
-        },
-        wasTheAccidentFatal: YES,
-        injuryCertificate: {
-            answer: AttachmentsEnum.SENDCERTIFICATELATER
-        },
-        attachments: {
-            injuryCertificateFile: {
-                file: [{
-                    name: 'test.pdf',
-                    url: 'https://test.pdf'
-                }]
-            },
-            deathCertificateFile: {
-                file: [{
-                    name: 'test.pdf',
-                    url: 'https://test.pdf'
-                }]
-            },
-            powerOfAttorneyFile: {
-                file: [{
-                    name: 'test.pdf',
-                    url: 'https://test.pdf'
-                }]
-            }    
-        }
-    }
+  return {
+    whoIsTheNotificationFor: {
+      answer: WhoIsTheNotificationForEnum.POWEROFATTORNEY,
+    },
+    wasTheAccidentFatal: YES,
+    injuryCertificate: {
+      answer: AttachmentsEnum.SENDCERTIFICATELATER,
+    },
+    attachments: {
+      injuryCertificateFile: {
+        file: [
+          {
+            name: 'test.pdf',
+            url: 'https://test.pdf',
+          },
+        ],
+      },
+      deathCertificateFile: {
+        file: [
+          {
+            name: 'test.pdf',
+            url: 'https://test.pdf',
+          },
+        ],
+      },
+      powerOfAttorneyFile: {
+        file: [
+          {
+            name: 'test.pdf',
+            url: 'https://test.pdf',
+          },
+        ],
+      },
+    },
+  }
 }
--- libs/application/templates/accident-notification/src/utils/isRepresentativeOfCompanyOrInstitue.spec.ts
+++ libs/application/templates/accident-notification/src/utils/isRepresentativeOfCompanyOrInstitue.spec.ts
@@ -1,9 +1,9 @@
 import { FormValue } from '@island.is/application/types'
 import { WhoIsTheNotificationForEnum } from '../types'
 
-import { 
-  isInjuredAndRepresentativeOfCompanyOrInstitute, 
-  isRepresentativeOfCompanyOrInstitute 
+import {
+  isInjuredAndRepresentativeOfCompanyOrInstitute,
+  isRepresentativeOfCompanyOrInstitute,
 } from './isRepresentativeOfCompanyOrInstitue'
 
 const emptyObject = {}
@@ -36,20 +36,22 @@ describe('isRepresentativeOfCompanyOrInstitue', () => {
 
 describe('isInjuredAndRepresentativeOfCompanyOrInstitute', () => {
   const representative: FormValue = {
-    isRepresentativeOfCompanyOrInstitue: 'yes'
+    isRepresentativeOfCompanyOrInstitue: 'yes',
   }
 
   const notRepresentative: FormValue = {
-    isRepresentativeOfCompanyOrInstitue: 'no'
+    isRepresentativeOfCompanyOrInstitue: 'no',
   }
 
   it('should return true for someone that is a representative of the company or institue', () => {
-    expect(isInjuredAndRepresentativeOfCompanyOrInstitute(representative)).toEqual(true)
+    expect(
+      isInjuredAndRepresentativeOfCompanyOrInstitute(representative),
+    ).toEqual(true)
   })
   it('should return false for someone that isnt a representative of the company or institue', () => {
-    expect(isInjuredAndRepresentativeOfCompanyOrInstitute(notRepresentative)).toEqual(
-      false,
-    )
+    expect(
+      isInjuredAndRepresentativeOfCompanyOrInstitute(notRepresentative),
+    ).toEqual(false)
   })
   it('should return false for empty object', () => {
     expect(isRepresentativeOfCompanyOrInstitute(emptyObject)).toEqual(false)
