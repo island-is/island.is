19edc356ff003482a12060b293cbd858fd5b91b2
Ásdís Erna Guðmundsdóttir <disa.erna@gmail.com>
2024-10-02 12:39:56 +0000

    feat: wip grants home page

---
+++ apps/web/screens/Grants/messages.ts
@@ -0,0 +1,41 @@
+import { defineMessages } from 'react-intl'
+
+export const m = {
+  general: defineMessages({
+    seeMore: {
+      id: 'web.grants:general.seeMore',
+      defaultMessage: 'Skoða nánar',
+    },
+  }),
+  home: defineMessages({
+    title: {
+      id: 'web.grants:home.title',
+      defaultMessage: 'Styrkjatorg',
+    },
+    description: {
+      id: 'web.grants:home.description',
+      defaultMessage: 'Styrkjatorg lýsing  .....',
+    },
+    featuredImage: {
+      id: 'web.grants:home.featuredImage',
+      defaultMessage:
+        'https://images.ctfassets.net/8k0h54kbe6bj/5LqU9yD9nzO5oOijpZF0K0/b595e1cf3e72bc97b2f9d869a53f5da9/LE_-_Jobs_-_S3.png',
+    },
+    inputPlaceholder: {
+      id: 'web.grants:home.inputPlaceholder',
+      defaultMessage: 'Leitaðu á styrkjatorgi',
+    },
+    mostVisited: {
+      id: 'web.grants:home.mostVisited',
+      defaultMessage: 'Mest sótt',
+    },
+    popularCategories: {
+      id: 'web.grants:home.popularCategories',
+      defaultMessage: 'Vinsælir flokkar',
+    },
+    allGrants: {
+      id: 'web.grants:home.allGrants',
+      defaultMessage: 'Allir styrkir',
+    },
+  }),
+}
