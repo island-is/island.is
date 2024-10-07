f70c24c4d50902948d8bc36d4a3a45cd186e81b5
Gunnar K Vilbergsson <gunnar@norda.is>
2024-10-07 10:18:36 +0000

    Remove unused import

--- libs/application/template-api-modules/src/lib/modules/templates/reference-template/reference-template.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/reference-template/reference-template.module.ts
@@ -3,10 +3,6 @@ import { Module } from '@nestjs/common'
 // This is a shared module that gives you access to common methods
 import { SharedTemplateAPIModule } from '../../shared'
 
-// The base config that template api modules are registered with by default
-// (configurable inside `template-api.module.ts`)
-import { BaseTemplateAPIModuleConfig } from '../../../types'
-
 // Here you import your module service
 import { ReferenceTemplateService } from './reference-template.service'
 
