76f875053dedf842cea3e1cd18929ed8cba1eb86
Kristofer <kristofer@andes.is>
2024-10-07 19:38:45 +0000

    chore(scripts): Stricter shell script checking (#16242)
    * Set style level for shellcheck
    
    * Linting & formatting scripts
    
    * Remove _podman.sh script
    
    * Format all scripts
    
    * Add reviewdog/action-shfmt step
    
    * Configure shfmt
    
    * Merge from main
    
    * Linting
    
    * Move shfmt to before lint
    
    * Remove reviewdog
    
    * Allow external sources in shellcheck
    
    * Use Reviewdog for shellcheck
    
    * Set version for Reviewdog
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- scripts/ci/_podman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-if [[ -n "${DEBUG:-}" || -n "${CI:-}" ]]; then set -x; fi
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-. "${DIR}/_docker.sh" "$@" || true
-
-mkargs local-cache=false
-CONTAINER_BUILDER=podman container_build
