994ec8886120aa6a933b9f525482b4f73ee17b26
Kristján Albert <kal@juni.is>
2024-10-08 08:25:00 +0000

    feat(driving-license): check if 65+ renewal is possible (#16292) (#16307)
    * check if 65 renewal is possible
    
    * remove console log
    
    * cleanup
    
    * coderabbit tweaks
    
    * coderabbit changes
    
    * quick fix
    
    * add type?
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/templates/driving-license/src/lib/constants.ts
+++ libs/application/templates/driving-license/src/lib/constants.ts
@@ -17,6 +17,20 @@ export enum Pickup {
 
 export const otherLicenseCategories = ['C', 'C1', 'CE', 'D', 'D1', 'DE']
 export const codesRequiringHealthCertificate = ['400', '01.06']
+export const codesExtendedLicenseCategories = [
+  'C1',
+  'C1E',
+  'C',
+  'CE',
+  'D1',
+  'D1E',
+  'D',
+  'DE',
+  'Bfar',
+  'Far',
+  'FAR',
+]
+export const remarksCannotRenew65 = ['400', '450', '95']
 
 export type DrivingLicenseApplicationFor =
   | typeof B_FULL
