3ffb9d77459018d3754c548ea800d1dd931a4c32
Kristofer <kristofer@andes.is>
2024-10-08 18:34:06 +0000

    More robust generate-changelog.sh

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+# Empty-call to ensure it's installed
+echo "Verifying 'generate-changelog' is installed..."
+npx generate-changelog
+
 # List of preferred copy tools
 copy_tools=("xclip" "wl-copy" "pbcopy")
 
@@ -15,17 +19,20 @@ done
 
 # Check if a tool was found
 if [[ -z "$selected_tool" ]]; then
-  echo "No suitable copy tool found. Please install xclip, wl-copy, or pbcopy."
-  exit 1
+  echo "No suitable copy tool found. Please install xclip, wl-copy, or pbcopy for automatic clipboard copying."
+  selected_tool="tee"
 fi
 
 git fetch
 
 # Get two most recent release branches
 branches=()
+n=0
 while IFS= read -r line; do
+  echo -ne "Found $((n++)) branches\r"
   branches+=("$line")
 done < <(git for-each-ref --sort=-committerdate refs/heads/release/ --format='%(refname:short)' | head -n2)
-npx generate-changelog -t "${branches[1]}".."${branches[0]}" -a -f - | $selected_tool
+echo -e "Found $n branches in total"
+npx generate-changelog -t "${branches[1]}".."${branches[0]}" -a -f - | "$selected_tool"
 
 echo "Changelog copied to clipboard using $selected_tool"
