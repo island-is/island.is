fb6d2cff99b070d70414acce826b87c687a6cea9
norda-gunni <161026627+norda-gunni@users.noreply.github.com>
2024-10-09 16:08:50 +0000

    feat(application-system): Add validated config (#15872)
    * feat(application-system): Add validated config
    
    * Add sharedConfig to appModule
    
    * Cleanup
    
    * PR comment
    
    * PR comment fixes
    
    * Remove unused import
    
    * SharedTemplateAPIModule no longer dynamically registered
    
    * local fallback for attachment bucket
    
    * Add error logging
    
    * Modules that no longer need to be dynamic are no longer dynamic
    
    * Update readme
    
    * Post merge conflict fix
    
    * Cleanup
    
    * fix shared.utils
    
    * Cleanup
    
    * Fixes
    
    * Fix test
    
    * fix tests
    
    * Fix lint
    
    * Update social-insurance-administration.service to static module
    
    * Remove unused import
    
    * transition parental leave module to static
    
    * Transition children residence change to static module
    
    * cleanup shared utils
    
    * fix test
    
    * coderabbit suggestion
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/application/template-api-modules/src/lib/modules/templates/health-insurance-declaration/health-insurance-declaration.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/health-insurance-declaration/health-insurance-declaration.module.ts
@@ -1,25 +1,17 @@
-import { DynamicModule } from '@nestjs/common'
-import { BaseTemplateAPIModuleConfig } from '../../../types'
+import { Module } from '@nestjs/common'
 import { HealthInsuranceDeclarationService } from './health-insurance-declaration.service'
 import { RightsPortalClientModule } from '@island.is/clients/icelandic-health-insurance/rights-portal'
 import { FileStorageModule } from '@island.is/file-storage'
 import { ApplicationAttachmentProvider } from './attachments/provider'
 import { SharedTemplateAPIModule } from '../../shared'
 
-export class HealthInsuranceDeclarationModule {
-  static register(config: BaseTemplateAPIModuleConfig): DynamicModule {
-    return {
-      module: HealthInsuranceDeclarationModule,
-      imports: [
-        RightsPortalClientModule,
-        FileStorageModule,
-        SharedTemplateAPIModule.register(config),
-      ],
-      providers: [
-        HealthInsuranceDeclarationService,
-        ApplicationAttachmentProvider,
-      ],
-      exports: [HealthInsuranceDeclarationService],
-    }
-  }
-}
+@Module({
+  imports: [
+    RightsPortalClientModule,
+    FileStorageModule,
+    SharedTemplateAPIModule,
+  ],
+  providers: [HealthInsuranceDeclarationService, ApplicationAttachmentProvider],
+  exports: [HealthInsuranceDeclarationService],
+})
+export class HealthInsuranceDeclarationModule {}
