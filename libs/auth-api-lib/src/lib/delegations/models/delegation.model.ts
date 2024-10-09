acd65c389e6c99c8176cc4c8e663a9da12732749
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2024-10-09 13:45:24 +0000

    feat(auth-admin): View delegation and refactor access card (#16243)
    * refactor AccessCard component and make change to modal to view delegation in access control
    
    * remove unused createdBy
    
    * chore: nx format:write update dirty files
    
    * refactor
    
    * fix typo
    
    * chore: nx format:write update dirty files
    
    * fix type
    
    * small refactor
    
    * fix delete function
    
    * chore: nx format:write update dirty files
    
    * Adds delete modal
    
    * chore: nx format:write update dirty files
    
    * Update DelegationDeleteModal.tsx
    
    * adds loading to create modal
    
    * chore: nx format:write update dirty files
    
    * small fixes and use view delegation modal in admin portal
    
    * Update AccessCard.tsx
    
    * chore: nx format:write update dirty files
    
    * fix query and types
    
    * chore: nx format:write update dirty files
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/auth-api-lib/src/lib/delegations/models/delegation.model.ts
+++ libs/auth-api-lib/src/lib/delegations/models/delegation.model.ts
@@ -141,6 +141,7 @@ export class Delegation extends Model<
       fromNationalId: this.fromNationalId,
       toNationalId: this.toNationalId,
       toName: this.toName,
+      createdByNationalId: this.createdByNationalId,
       validTo: this.validTo,
       scopes: this.delegationScopes
         ? this.delegationScopes.map((scope) => scope.toDTO())
