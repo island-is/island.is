fe28097c805c4afdbd2b1bd09c38319200c63a98
thoreyjona <thoreyjona@gmail.com>
2024-10-09 09:03:12 +0000

    fix: translation for Loftbrú more card

--- apps/native/app/src/messages/en.ts
+++ apps/native/app/src/messages/en.ts
@@ -344,7 +344,7 @@ export const en: TranslatedMessages = {
   'profile.vehicles': 'Vehicles',
   'profile.assets': 'Assets',
   'profile.finance': 'Finance',
-  'profile.airDiscount': 'Air discount scheme',
+  'profile.airDiscount': 'Air discount',
   'profile.health': 'Health',
 
   // vehicles
