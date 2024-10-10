ea4d3b4d06aa282cb31bc4a75caff89fbe278ade
unakb <una@kolibri.is>
2024-10-10 14:33:19 +0000

    Update getSubpoenaPdf.spec.ts

--- apps/judicial-system/backend/src/app/modules/subpoena/test/limitedAccessSubpoenaController/getSubpoenaPdf.spec.ts
+++ apps/judicial-system/backend/src/app/modules/subpoena/test/limitedAccessSubpoenaController/getSubpoenaPdf.spec.ts
@@ -4,7 +4,7 @@ import { uuid } from 'uuidv4'
 import { createTestingSubpoenaModule } from '../createTestingSubpoenaModule'
 
 import { Case, PdfService } from '../../../case'
-import { Defendant } from '../../../defendant'
+import { Defendant } from '../../../defendant/models/defendant.model'
 import { Subpoena } from '../../models/subpoena.model'
 
 interface Then {
@@ -30,7 +30,6 @@ describe('LimitedAccessSubpoenaController - Get subpoena pdf', () => {
       await createTestingSubpoenaModule()
 
     mockPdfService = pdfService
-
     const getSubpoenaPdfMock = mockPdfService.getSubpoenaPdf as jest.Mock
     getSubpoenaPdfMock.mockResolvedValueOnce(pdf)
 
