1136f602a65bc81e515abf06204ee716cb44ab04
unakb <una@kolibri.is>
2024-10-11 15:24:13 +0000

    Revert change

--- libs/auth-api-lib/seeders/data/scope-law-and-order.ts
+++ libs/auth-api-lib/seeders/data/scope-law-and-order.ts
@@ -1,12 +1,9 @@
-import { compose, createScope } from './helpers'
+import { createScope } from './helpers'
 
-export const up = compose(
-  createScope({
-    name: '@rettarvorslugatt.island.is/law-and-order',
-    displayName: 'Dómsmál og fyrirköll',
-    description: 'Bakendascope fyrir dómsmála virkni á mínum síðum',
-    accessControlled: false,
-    addToClients: ['@island.is/clients/api'],
-    addToResource: '@island.is',
-  }),
-)
+createScope({
+  name: '@rettarvorslugatt.island.is/law-and-order',
+  displayName: 'Dómsmál og fyrirköll',
+  description: 'Bakendascope fyrir dómsmála virkni á mínum síðum',
+  accessControlled: false,
+  addToClients: ['@island.is/clients/api'],
+})
