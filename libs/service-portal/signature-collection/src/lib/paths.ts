afd3bf650cdba03ea3aa2a42c3c325884b0e7b28
albina <albina@kosmosogkaos.is>
2024-10-14 11:32:53 +0000

    p

--- libs/service-portal/signature-collection/src/lib/paths.ts
+++ libs/service-portal/signature-collection/src/lib/paths.ts
@@ -6,10 +6,10 @@ export enum SignatureCollectionPaths {
   SignatureCollectionParliamentaryLists = '/min-gogn/listar/althingis-medmaelasofnun',
   ViewParliamentaryList = '/min-gogn/listar/althingis-medmaelasofnun/:id',
 
+  // Parliamentary - Company
+  CompanySignatureCollectionParliamentaryLists = '/fyrirtaeki/listar/althingis-medmaelasofnun',
+
   // Presidential
   SignatureCollectionLists = '/min-gogn/listar/medmaelasofnun',
   ViewList = '/min-gogn/listar/medmaelasofnun/:id',
-
-  // Company
-  CompanySignatureCollectionParliamentaryLists = '/fyrirtaeki/listar/althingis-medmaelasofnun',
 }
