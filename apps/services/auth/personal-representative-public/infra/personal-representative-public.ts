6b38b0660d4179b1ff8eec37fa558fb60de8ac08
andes-it <builders@andes.is>
2024-10-15 13:10:14 +0000

    chore: nx format:write update dirty files

--- apps/services/auth/personal-representative-public/infra/personal-representative-public.ts
+++ apps/services/auth/personal-representative-public/infra/personal-representative-public.ts
@@ -1,46 +1,47 @@
 import { service, ServiceBuilder } from '../../../../../infra/src/dsl/dsl'
 
-export const serviceSetup = (): ServiceBuilder<'services-auth-personal-representative-public'> => {
-  return service('services-auth-personal-representative-public')
-    .namespace('personal-representative')
-    .image('services-auth-personal-representative-public')
-    .db({
-      name: 'servicesauth',
-    })
-    .env({
-      IDENTITY_SERVER_ISSUER_URL: {
-        dev: 'https://identity-server.dev01.devland.is',
-        staging: 'https://identity-server.staging01.devland.is',
-        prod: 'https://innskra.island.is',
-      },
-    })
-    .ingress({
-      primary: {
-        host: {
-          dev: 'personal-representative-public-xrd.internal.dev01.devland.is',
-          staging:
-            'personal-representative-public-xrd.internal.staging01.devland.is',
-          prod: 'personal-representative-public-xrd.internal.innskra.island.is',
+export const serviceSetup =
+  (): ServiceBuilder<'services-auth-personal-representative-public'> => {
+    return service('services-auth-personal-representative-public')
+      .namespace('personal-representative')
+      .image('services-auth-personal-representative-public')
+      .db({
+        name: 'servicesauth',
+      })
+      .env({
+        IDENTITY_SERVER_ISSUER_URL: {
+          dev: 'https://identity-server.dev01.devland.is',
+          staging: 'https://identity-server.staging01.devland.is',
+          prod: 'https://innskra.island.is',
         },
-        paths: ['/'],
-        public: false,
-      },
-    })
-    .readiness('/health/check')
-    .liveness('/liveness')
-    .resources({
-      limits: {
-        cpu: '400m',
-        memory: '512Mi',
-      },
-      requests: {
-        cpu: '100m',
-        memory: '256Mi',
-      },
-    })
-    .replicaCount({
-      default: 2,
-      min: 2,
-      max: 10,
-    })
-}
+      })
+      .ingress({
+        primary: {
+          host: {
+            dev: 'personal-representative-public-xrd.internal.dev01.devland.is',
+            staging:
+              'personal-representative-public-xrd.internal.staging01.devland.is',
+            prod: 'personal-representative-public-xrd.internal.innskra.island.is',
+          },
+          paths: ['/'],
+          public: false,
+        },
+      })
+      .readiness('/health/check')
+      .liveness('/liveness')
+      .resources({
+        limits: {
+          cpu: '400m',
+          memory: '512Mi',
+        },
+        requests: {
+          cpu: '100m',
+          memory: '256Mi',
+        },
+      })
+      .replicaCount({
+        default: 2,
+        min: 2,
+        max: 10,
+      })
+  }
