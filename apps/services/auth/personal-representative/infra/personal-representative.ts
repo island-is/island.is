84017cec3c0b64d8c93f341fc79f5e338a7bd4df
Kristofer <kristofer@andes.is>
2024-10-15 10:26:13 +0000

    Remove dead ingress

--- apps/services/auth/personal-representative/infra/personal-representative.ts
+++ apps/services/auth/personal-representative/infra/personal-representative.ts
@@ -14,84 +14,73 @@ const REDIS_NODE_CONFIG = {
   ]),
 }
 
-export const serviceSetup =
-  (): ServiceBuilder<'services-auth-personal-representative'> => {
-    return service('services-auth-personal-representative')
-      .namespace('personal-representative')
-      .image('services-auth-personal-representative')
-      .db({ name: 'servicesauth' })
-      .env({
-        IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/auth-api',
-        IDENTITY_SERVER_ISSUER_URL: {
-          dev: 'https://identity-server.dev01.devland.is',
-          staging: 'https://identity-server.staging01.devland.is',
-          prod: 'https://innskra.island.is',
+export const serviceSetup = (): ServiceBuilder<'services-auth-personal-representative'> => {
+  return service('services-auth-personal-representative')
+    .namespace('personal-representative')
+    .image('services-auth-personal-representative')
+    .db({ name: 'servicesauth' })
+    .env({
+      IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/auth-api',
+      IDENTITY_SERVER_ISSUER_URL: {
+        dev: 'https://identity-server.dev01.devland.is',
+        staging: 'https://identity-server.staging01.devland.is',
+        prod: 'https://innskra.island.is',
+      },
+      XROAD_NATIONAL_REGISTRY_ACTOR_TOKEN: 'true',
+      XROAD_RSK_PROCURING_ACTOR_TOKEN: 'true',
+      XROAD_NATIONAL_REGISTRY_SERVICE_PATH: {
+        dev: 'IS-DEV/GOV/10001/SKRA-Protected/Einstaklingar-v1',
+        staging: 'IS-TEST/GOV/6503760649/SKRA-Protected/Einstaklingar-v1',
+        prod: 'IS/GOV/6503760649/SKRA-Protected/Einstaklingar-v1',
+      },
+      XROAD_NATIONAL_REGISTRY_REDIS_NODES: REDIS_NODE_CONFIG,
+      XROAD_RSK_PROCURING_REDIS_NODES: REDIS_NODE_CONFIG,
+      COMPANY_REGISTRY_XROAD_PROVIDER_ID: {
+        dev: 'IS-DEV/GOV/10006/Skatturinn/ft-v1',
+        staging: 'IS-TEST/GOV/5402696029/Skatturinn/ft-v1',
+        prod: 'IS/GOV/5402696029/Skatturinn/ft-v1',
+      },
+      COMPANY_REGISTRY_REDIS_NODES: REDIS_NODE_CONFIG,
+      SYSLUMENN_HOST: {
+        dev: 'https://api.syslumenn.is/staging',
+        staging: 'https://api.syslumenn.is/staging',
+        prod: 'https://api.syslumenn.is/api',
+      },
+      SYSLUMENN_TIMEOUT: '3000',
+    })
+    .secrets({
+      IDENTITY_SERVER_CLIENT_SECRET:
+        '/k8s/services-auth/IDENTITY_SERVER_CLIENT_SECRET',
+      SYSLUMENN_USERNAME: '/k8s/services-auth/SYSLUMENN_USERNAME',
+      SYSLUMENN_PASSWORD: '/k8s/services-auth/SYSLUMENN_PASSWORD',
+    })
+    .xroad(Base, Client, RskProcuring)
+    .ingress({
+      primary: {
+        host: {
+          dev: 'personal-representative-xrd.internal.dev01.devland.is',
+          staging: 'personal-representative-xrd.internal.staging01.devland.is',
+          prod: 'personal-representative-xrd.internal.innskra.island.is',
         },
-        XROAD_NATIONAL_REGISTRY_ACTOR_TOKEN: 'true',
-        XROAD_RSK_PROCURING_ACTOR_TOKEN: 'true',
-        XROAD_NATIONAL_REGISTRY_SERVICE_PATH: {
-          dev: 'IS-DEV/GOV/10001/SKRA-Protected/Einstaklingar-v1',
-          staging: 'IS-TEST/GOV/6503760649/SKRA-Protected/Einstaklingar-v1',
-          prod: 'IS/GOV/6503760649/SKRA-Protected/Einstaklingar-v1',
-        },
-        XROAD_NATIONAL_REGISTRY_REDIS_NODES: REDIS_NODE_CONFIG,
-        XROAD_RSK_PROCURING_REDIS_NODES: REDIS_NODE_CONFIG,
-        COMPANY_REGISTRY_XROAD_PROVIDER_ID: {
-          dev: 'IS-DEV/GOV/10006/Skatturinn/ft-v1',
-          staging: 'IS-TEST/GOV/5402696029/Skatturinn/ft-v1',
-          prod: 'IS/GOV/5402696029/Skatturinn/ft-v1',
-        },
-        COMPANY_REGISTRY_REDIS_NODES: REDIS_NODE_CONFIG,
-        SYSLUMENN_HOST: {
-          dev: 'https://api.syslumenn.is/staging',
-          staging: 'https://api.syslumenn.is/staging',
-          prod: 'https://api.syslumenn.is/api',
-        },
-        SYSLUMENN_TIMEOUT: '3000',
-      })
-      .secrets({
-        IDENTITY_SERVER_CLIENT_SECRET:
-          '/k8s/services-auth/IDENTITY_SERVER_CLIENT_SECRET',
-        SYSLUMENN_USERNAME: '/k8s/services-auth/SYSLUMENN_USERNAME',
-        SYSLUMENN_PASSWORD: '/k8s/services-auth/SYSLUMENN_PASSWORD',
-      })
-      .xroad(Base, Client, RskProcuring)
-      .ingress({
-        primary: {
-          host: {
-            dev: 'personal-representative-xrd.internal.dev01.devland.is',
-            staging:
-              'personal-representative-xrd.internal.staging01.devland.is',
-            prod: 'personal-representative-xrd.internal.innskra.island.is',
-          },
-          paths: ['/'],
-          public: false,
-        },
-        demo: {
-          host: {
-            dev: 'personal-representative-xrd.dev01.devland.is',
-            staging: MissingSetting,
-            prod: MissingSetting,
-          },
-          paths: ['/'],
-          public: true,
-        },
-      })
-      .readiness('/health/check')
-      .liveness('/liveness')
-      .resources({
-        limits: {
-          cpu: '400m',
-          memory: '512Mi',
-        },
-        requests: {
-          cpu: '100m',
-          memory: '256Mi',
-        },
-      })
-      .replicaCount({
-        default: 2,
-        min: 2,
-        max: 10,
-      })
-  }
+        paths: ['/'],
+        public: false,
+      },
+    })
+    .readiness('/health/check')
+    .liveness('/liveness')
+    .resources({
+      limits: {
+        cpu: '400m',
+        memory: '512Mi',
+      },
+      requests: {
+        cpu: '100m',
+        memory: '256Mi',
+      },
+    })
+    .replicaCount({
+      default: 2,
+      min: 2,
+      max: 10,
+    })
+}
