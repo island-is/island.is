a038d6f57665f4b004bb4804af9d481f49783d7f
Veronika Sif <veronikasif@hotmail.com>
2024-10-15 11:19:24 +0000

    [TS-106] e2e tests (Old-age-pension)

--- libs/application/templates/social-insurance-administration/old-age-pension/src/forms/OldAgePensionForm.ts
+++ libs/application/templates/social-insurance-administration/old-age-pension/src/forms/OldAgePensionForm.ts
@@ -292,6 +292,7 @@ export const OldAgePensionForm: Form = buildForm({
                     socialInsuranceAdministrationMessage.payment
                       .personalAllowancePercentage,
                   suffix: '%',
+                  dataTestId: 'personal-allowance-usage',
                   condition: (answers) => {
                     const { personalAllowance } = getApplicationAnswers(answers)
                     return personalAllowance === YES
