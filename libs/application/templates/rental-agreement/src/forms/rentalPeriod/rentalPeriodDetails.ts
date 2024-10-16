ae5a0b97b5e2eb721305abf6b8d257e5fc64fb68
Árni Jónsson <addininja@gmail.com>
2024-10-16 17:45:19 +0200

    added support for rental period.

--- libs/application/templates/rental-agreement/src/forms/rentalPeriod/rentalPeriodDetails.ts
+++ libs/application/templates/rental-agreement/src/forms/rentalPeriod/rentalPeriodDetails.ts
@@ -1,20 +1,54 @@
 import {
   buildSubSection,
   buildMultiField,
-  buildTextField,
+  buildDescriptionField,
+  buildDateField,
+  buildCheckboxField,
 } from '@island.is/application/core'
+import * as m from '../../lib/messages'
 
 export const RentalPeriodDetails = buildSubSection({
   id: 'rentalPeriodDetails',
-  title: 'Leigutímabil',
+  title: m.rentalPeriodDetails.subSectionName,
   children: [
     buildMultiField({
-      title: 'Leigutími',
+      title: m.rentalPeriodDetails.pageTitle,
+      description: m.rentalPeriodDetails.pageDescription,
       children: [
-        buildTextField({
-          id: 'rentalPeriodInput',
-          title: 'Leigutími',
-          format: 'text',
+        buildDateField({
+          id: 'rentalPeriodStartDate',
+          title: m.rentalPeriodDetails.startDateTitle,
+          placeholder: m.rentalPeriodDetails.startDatePlaceholder,
+        }),
+        buildDateField({
+          id: 'rentalPeriodEndDate',
+          title: m.rentalPeriodDetails.endDateTitle,
+          placeholder: m.rentalPeriodDetails.endDatePlaceholder,
+          condition: (answers) => {
+            const rentalPeriodDefinite: string[] =
+              answers.rentalPeriodDefinite as string[]
+
+            return rentalPeriodDefinite && rentalPeriodDefinite.includes('true')
+          },
+        }),
+        buildCheckboxField({
+          id: 'rentalPeriodDefinite',
+          title: '',
+          options: [
+            {
+              value: 'true',
+              label: m.rentalPeriodDetails.rentalPeriodDefiniteLabel,
+            },
+          ],
+          spacing: 0,
+        }),
+
+        buildDescriptionField({
+          id: 'rentalPeriodTermination',
+          title: m.rentalPeriodDetails.terminationLabel,
+          titleVariant: 'h3',
+          space: 3,
+          description: m.rentalPeriodDetails.terminationDescription,
         }),
       ],
     }),
