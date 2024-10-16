c5bbed41656c1382b10a327dab79e08ab1b6ed0f
andes-it <builders@andes.is>
2024-10-16 10:32:15 +0000

    chore: nx format:write update dirty files

--- apps/services/endorsements/api/src/app/modules/endorsement/endorsement.service.ts
+++ apps/services/endorsements/api/src/app/modules/endorsement/endorsement.service.ts
@@ -58,9 +58,6 @@ export class EndorsementService {
     private readonly awsService: AwsService,
   ) {}
 
-
-  
-
   async updateEndorsementCountOnList(listId: string): Promise<void> {
     const count = await this.endorsementModel.count({
       where: { endorsementListId: listId },
