7b85642d6fa9912e719f3a337a84653b4fec1665
Alex Diljar Birkisbur Hellsing <42963845+alexdiljar@users.noreply.github.com>
2024-10-16 14:33:38 +0000

    fix(signature-colletion): remove candidate if all lists removed (#16425)
    * fix: remove candidate if all lists removed
    
    * fix: remove candidate if all lists removed make sure has candidate id
    
    * chore: nx format:write update dirty files
    
    * Update libs/clients/signature-collection/src/lib/signature-collection.service.ts
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- libs/clients/signature-collection/src/lib/signature-collection.service.ts
+++ libs/clients/signature-collection/src/lib/signature-collection.service.ts
@@ -360,7 +360,19 @@ export class SignatureCollectionClientService {
         }),
       ),
     )
+    // If no lists remain remove Candidate so that they can start a new collection through applications again
+    await this.checkIfRemoveCandidate(candidate.id, auth)
+
     return { success: true }
+  }
+
+  private async checkIfRemoveCandidate(id: string, auth: User) {
+    const { ownedLists, candidate } = await this.getSignee(auth)
+    if ((!ownedLists || ownedLists.length === 0) && candidate?.id) {
+      await this.getApiWithAuth(this.candidateApi, auth).frambodIDDelete({
+        iD: parseInt(id),
+      })
+    }
   }
 
   async getSignedList(auth: User): Promise<SignedList[] | null> {
@@ -404,7 +416,7 @@ export class SignatureCollectionClientService {
   }: CanSignInput): Promise<Success> {
     // User is not allowed to have more than one signature
     // They are marked as invalid but count as participation
-    const noInvalidSignature = !signatures?.find((s) => !s.valid) ?? true
+    const noInvalidSignature = !signatures?.find((s) => !s.valid)
 
     const reasons = mapReasons({
       ...canSignInfo,
