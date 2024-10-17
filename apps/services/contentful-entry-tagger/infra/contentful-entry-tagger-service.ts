a0abb7b4649ec47a113b59f3722bd867550a6b06
andes-it <builders@andes.is>
2024-10-17 09:57:47 +0000

    chore: nx format:write update dirty files

--- apps/services/contentful-entry-tagger/infra/contentful-entry-tagger-service.ts
+++ apps/services/contentful-entry-tagger/infra/contentful-entry-tagger-service.ts
@@ -1,25 +1,26 @@
 import { service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
 
-export const serviceSetup = (): ServiceBuilder<'contentful-entry-tagger-service'> =>
-  service('contentful-entry-tagger-service')
-    .image('services-contentful-entry-tagger')
-    .namespace('contentful-entry-tagger')
-    .serviceAccount('contentful-entry-tagger')
-    .secrets({
-      CONTENTFUL_MANAGEMENT_ACCESS_TOKEN:
-        '/k8s/contentful-entry-tagger/CONTENTFUL_MANAGEMENT_ACCESS_TOKEN',
-      CONTENTFUL_REQUEST_TOKEN:
-        '/k8s/contentful-entry-tagger/CONTENTFUL_REQUEST_TOKEN',
-    })
-    .ingress({
-      primary: {
-        host: {
-          dev: 'contentful-entry-tagger-service',
-          staging: 'contentful-entry-tagger-service',
-          prod: 'contentful-entry-tagger-service.devland.is',
+export const serviceSetup =
+  (): ServiceBuilder<'contentful-entry-tagger-service'> =>
+    service('contentful-entry-tagger-service')
+      .image('services-contentful-entry-tagger')
+      .namespace('contentful-entry-tagger')
+      .serviceAccount('contentful-entry-tagger')
+      .secrets({
+        CONTENTFUL_MANAGEMENT_ACCESS_TOKEN:
+          '/k8s/contentful-entry-tagger/CONTENTFUL_MANAGEMENT_ACCESS_TOKEN',
+        CONTENTFUL_REQUEST_TOKEN:
+          '/k8s/contentful-entry-tagger/CONTENTFUL_REQUEST_TOKEN',
+      })
+      .ingress({
+        primary: {
+          host: {
+            dev: 'contentful-entry-tagger-service',
+            staging: 'contentful-entry-tagger-service',
+            prod: 'contentful-entry-tagger-service.devland.is',
+          },
+          paths: ['/'],
         },
-        paths: ['/'],
-      },
-    })
-    .liveness('/liveness')
-    .readiness('/readiness')
+      })
+      .liveness('/liveness')
+      .readiness('/readiness')
--- libs/island-ui/storybook/infra/storybook.ts
+++ libs/island-ui/storybook/infra/storybook.ts
@@ -1,28 +1,29 @@
 import { service, ServiceBuilder } from '../../../../infra/src/dsl/dsl'
 
-export const serviceSetup = (services: {}): ServiceBuilder<'island-ui-storybook'> =>
-  service('island-ui-storybook')
-    .namespace('storybook')
-    .liveness('/liveness')
-    .readiness('/readiness')
-    .resources({
-      limits: {
-        cpu: '200m',
-        memory: '256Mi',
-      },
-      requests: {
-        cpu: '10m',
-        memory: '128Mi',
-      },
-    })
-    .ingress({
-      primary: {
-        host: {
-          dev: 'ui',
-          staging: 'ui',
-          prod: 'ui.devland.is',
+export const serviceSetup =
+  (services: {}): ServiceBuilder<'island-ui-storybook'> =>
+    service('island-ui-storybook')
+      .namespace('storybook')
+      .liveness('/liveness')
+      .readiness('/readiness')
+      .resources({
+        limits: {
+          cpu: '200m',
+          memory: '256Mi',
         },
-        paths: ['/'],
-      },
-    })
-    .grantNamespaces('nginx-ingress-external')
+        requests: {
+          cpu: '10m',
+          memory: '128Mi',
+        },
+      })
+      .ingress({
+        primary: {
+          host: {
+            dev: 'ui',
+            staging: 'ui',
+            prod: 'ui.devland.is',
+          },
+          paths: ['/'],
+        },
+      })
+      .grantNamespaces('nginx-ingress-external')
