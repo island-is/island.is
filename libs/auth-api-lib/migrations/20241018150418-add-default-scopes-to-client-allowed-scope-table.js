a4df4d085f255cb6d2031639394310bcabdf21c7
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2024-10-18 15:12:52 +0000

    Adds migration for inserting default scopes to existing clients

---
+++ libs/auth-api-lib/migrations/20241018150418-add-default-scopes-to-client-allowed-scope-table.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const clients = await queryInterface.sequelize.query(`SELECT "client_id" FROM "client" WHERE "client_type" IN ('web', 'native', 'spa');`, { type: queryInterface.sequelize.QueryTypes.SELECT });
+    const clientIds = clients.map(client => client.client_id);
+
+    if (!clientIds.length) {
+      return;
+    }
+
+    const existingScopes = await queryInterface.sequelize.query(      `SELECT "client_id", "scope_name" FROM "client_allowed_scope" WHERE "client_id" IN (:clientIds);`,
+      { type: queryInterface.sequelize.QueryTypes.SELECT, replacements: { clientIds } });
+
+    const existingScopesLookup = new Set(
+      existingScopes.map(scope => `${scope.client_id}-${scope.scope_name}`)
+    );
+
+    const rows = [];
+    const defaultScopes = ['email', 'address', 'phone']
+
+    for (const clientId of clientIds) {
+      rows.push(...defaultScopes.filter((scope => {
+        return !existingScopesLookup.has(`${clientId}-${scope}`);
+      })).map(scope => ({ client_id: clientId, scope_name: scope })));
+    }
+
+    if (!rows.length) {
+      return;
+    }
+
+    await queryInterface.bulkInsert('client_allowed_scope', rows);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * There is no need to go back
+     *
+     */
+  }
+};
