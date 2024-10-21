2d24aecfaad6beb5132c3f6b6a26db25fdb00d4a
Jón Levy <levy@andes.is>
2024-10-21 11:41:53 +0000

    test: update portal-env test for service building

--- infra/src/dsl/portal-env.spec.ts
+++ infra/src/dsl/portal-env.spec.ts
@@ -1,4 +1,4 @@
-import { service } from './dsl'
+import { ServiceBuilder, service } from './dsl'
 import { Kubernetes } from './kubernetes-runtime'
 import { SerializeSuccess, HelmService } from './types/output-types'
 import { EnvironmentConfig } from './types/charts'
@@ -14,10 +14,13 @@ const ONE_HOUR_IN_MS = 60 * 60 * 1000
 const ONE_WEEK_IN_MS = ONE_HOUR_IN_MS * 24 * 7
 
 const bffName = 'services-bff'
-const bffType = 'stjornbord'
 const clientName = 'portals-admin'
 const serviceName = `${bffName}-${clientName}`
+const key = 'stjornbord'
 
+export type BffInfraServices = {
+  api: ServiceBuilder<'api'>
+}
 const Staging: EnvironmentConfig = {
   auroraHost: 'a',
   redisHost: 'b',
@@ -33,20 +36,21 @@ const Staging: EnvironmentConfig = {
 }
 
 describe('BFF PortalEnv serialization', () => {
+  const services = { api: service('api') }
   const sut = service(serviceName)
     .namespace(clientName)
     .image(bffName)
     .redis()
     .serviceAccount(bffName)
-    .env(createPortalEnv(bffType))
+    .env(createPortalEnv(key, services))
     .secrets({
       BFF_TOKEN_SECRET_BASE64: `/k8s/${bffName}/${clientName}/BFF_TOKEN_SECRET_BASE64`,
       IDENTITY_SERVER_CLIENT_SECRET: `/k8s/${bffName}/${clientName}/IDENTITY_SERVER_CLIENT_SECRET`,
     })
     .command('node')
     .args('main.js')
-    .readiness('/health/check')
-    .liveness('/liveness')
+    .readiness(`/${key}/bff/health/check`)
+    .liveness(`/${key}/bff/liveness`)
     .replicaCount({
       default: 2,
       min: 2,
@@ -84,7 +88,7 @@ describe('BFF PortalEnv serialization', () => {
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
           },
         },
-        paths: [`/${bffType}/bff`],
+        paths: [`/${key}/bff`],
       },
     })
   let result: SerializeSuccess<HelmService>
@@ -140,20 +144,19 @@ describe('BFF PortalEnv serialization', () => {
   it('environment variables', () => {
     expect(result.serviceDef[0].env).toEqual({
       IDENTITY_SERVER_CLIENT_SCOPES: json(adminPortalScopes),
-      IDENTITY_SERVER_CLIENT_ID: `@admin.island.is/bff-${bffType}`,
+      IDENTITY_SERVER_CLIENT_ID: `@admin.island.is/bff-${key}`,
       IDENTITY_SERVER_ISSUER_URL: 'https://identity-server.dev01.devland.is',
       // BFF
       BFF_NAME: 'stjornbord',
-      BFF_CLIENT_KEY_PATH: `/${bffType}`,
+      BFF_CLIENT_KEY_PATH: `/${key}`,
       BFF_PAR_SUPPORT_ENABLED: 'false',
       BFF_ALLOWED_REDIRECT_URIS: json([
         'https://featbff-beta.dev01.devland.is',
       ]),
       BFF_CLIENT_BASE_URL: 'https://featbff-beta.dev01.devland.is',
       BFF_LOGOUT_REDIRECT_URI: 'https://featbff-beta.dev01.devland.is',
-      BFF_CALLBACKS_BASE_PATH: `https://featbff-beta.dev01.devland.is/${bffType}/bff/callbacks`,
-      BFF_PROXY_API_ENDPOINT:
-        'https://featbff-beta.dev01.devland.is/api/graphql',
+      BFF_CALLBACKS_BASE_PATH: `https://featbff-beta.dev01.devland.is/${key}/bff/callbacks`,
+      BFF_PROXY_API_ENDPOINT: 'http://web-api.islandis.svc.cluster.local',
       BFF_ALLOWED_EXTERNAL_API_URLS: json(['https://api.dev01.devland.is']),
       BFF_CACHE_USER_PROFILE_TTL_MS: (
         ONE_HOUR_IN_MS - FIVE_SECONDS_IN_MS
