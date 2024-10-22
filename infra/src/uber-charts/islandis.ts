ae0e9723f6e30b5933d7029e3d1d08c93c710451
peturgq <petur@petur.io>
2024-10-22 11:24:16 +0000

    chore: Add deps to appSystemApi

--- infra/src/uber-charts/islandis.ts
+++ infra/src/uber-charts/islandis.ts
@@ -69,11 +69,16 @@ const skilavottordWeb = skilavottordWebSetup({ api: skilavottordWs })
 const documentsService = serviceDocumentsSetup()
 const servicePortalApi = servicePortalApiSetup()
 
+const userNotificationService = userNotificationServiceSetup({
+  userProfileApi: servicePortalApi,
+})
+
 const appSystemApi = appSystemApiSetup({
   documentsService,
   servicesEndorsementApi: endorsement,
   skilavottordWs,
   servicePortalApi,
+  userNotificationService
 })
 const appSystemApiWorker = appSystemApiWorkerSetup()
 
@@ -94,9 +99,6 @@ const authAdminApi = authAdminApiSetup()
 const universityGatewayService = universityGatewaySetup()
 const universityGatewayWorker = universityGatewayWorkerSetup()
 
-const userNotificationService = userNotificationServiceSetup({
-  userProfileApi: servicePortalApi,
-})
 
 const api = apiSetup({
   appSystemApi,
