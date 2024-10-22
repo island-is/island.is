0ed1ae9ad99b905543436ea38cc0aa7c63017f31
andes-it <builders@andes.is>
2024-10-22 22:20:25 +0000

    chore: nx format:write update dirty files

--- infra/src/uber-charts/islandis.ts
+++ infra/src/uber-charts/islandis.ts
@@ -138,7 +138,8 @@ const downloadService = downloadServiceSetup({
 const userNotificationWorkerService = userNotificationWorkerSetup({
   userProfileApi: servicePortalApi,
 })
-const userNotificationCleanupWorkerService = userNotificationCleanUpWorkerSetup()
+const userNotificationCleanupWorkerService =
+  userNotificationCleanUpWorkerSetup()
 
 const githubActionsCache = githubActionsCacheSetup()
 
