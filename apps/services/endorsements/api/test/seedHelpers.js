6c878ed7308096f1820d19ca8dceb798b7868745
Rafn Árnason <rafnarnason@gmail.com>
2024-10-23 10:20:49 +0000

    test setup edit

--- apps/services/endorsements/api/test/seedHelpers.js
+++ apps/services/endorsements/api/test/seedHelpers.js
@@ -1,29 +1,29 @@
-const faker = require('faker')
-const today = new Date()
+// const faker = require('faker')
+// const today = new Date()
 
-module.exports = {
-  getGenericEndorsementList: (tags) => ({
-    id: faker.datatype.uuid(),
-    title: faker.lorem.words(2),
-    description: faker.lorem.paragraph(1),
-    opened_date: today,
-    closed_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000),
-    admin_lock: false,
-    endorsement_meta: '{}', // default empty array in postgres
-    endorsement_metadata: '[]', // default empty array in postgres
-    tags: tags ?? '{}', // default empty array in postgres
-    meta: '{}',
-    owner: faker.phone.phoneNumber('##########'),
-    created: new Date(),
-    modified: new Date(),
-  }),
+// module.exports = {
+//   getGenericEndorsementList: (tags) => ({
+//     id: faker.datatype.uuid(),
+//     title: faker.lorem.words(2),
+//     description: faker.lorem.paragraph(1),
+//     opened_date: today,
+//     closed_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000),
+//     admin_lock: false,
+//     endorsement_meta: '{}', // default empty array in postgres
+//     endorsement_metadata: '[]', // default empty array in postgres
+//     tags: tags ?? '{}', // default empty array in postgres
+//     meta: '{}',
+//     owner: faker.phone.phoneNumber('##########'),
+//     created: new Date(),
+//     modified: new Date(),
+//   }),
 
-  getGenericEndorsement: () => ({
-    id: faker.datatype.uuid(),
-    endorser: faker.phone.phoneNumber('##########'),
-    endorsement_list_id: '9c0b4106-4213-43be-a6b2-ff324f4ba0c1',
-    meta: '{}',
-    created: new Date(),
-    modified: new Date(),
-  }),
-}
+//   getGenericEndorsement: () => ({
+//     id: faker.datatype.uuid(),
+//     endorser: faker.phone.phoneNumber('##########'),
+//     endorsement_list_id: '9c0b4106-4213-43be-a6b2-ff324f4ba0c1',
+//     meta: '{}',
+//     created: new Date(),
+//     modified: new Date(),
+//   }),
+// }
