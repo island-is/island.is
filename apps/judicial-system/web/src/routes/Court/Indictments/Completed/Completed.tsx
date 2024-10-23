0ccadfe5d91a01c59d5664a94606e877be473e0c
Guðjón Guðjónsson <gudjon@kolibri.is>
2024-10-23 18:56:38 +0000

    fix(j-s): Withdraw Indictment Case (#16525)
    * Fixes imports
    
    * Withdraws rather than cancels indictments cancelled by prosecutors
    
    * Comments out some temporarily unused imports
    
    ---------
    
    Co-authored-by: unakb <una@kolibri.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/judicial-system/web/src/routes/Court/Indictments/Completed/Completed.tsx
+++ apps/judicial-system/web/src/routes/Court/Indictments/Completed/Completed.tsx
@@ -19,7 +19,7 @@ import {
   FormContext,
   FormFooter,
   IndictmentCaseFilesList,
-  IndictmentsLawsBrokenAccordionItem,
+  // IndictmentsLawsBrokenAccordionItem, NOTE: Temporarily hidden while list of laws broken is not complete
   InfoCardClosedIndictment,
   Modal,
   PageHeader,
