36f9d6bbbe32bc8fad41c0654d4a7abb74157f86
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-10-24 08:54:16 +0000

    adding messages

--- libs/application/templates/university/src/lib/messages/application.ts
+++ libs/application/templates/university/src/lib/messages/application.ts
@@ -61,4 +61,68 @@ export const application = defineMessages({
     defaultMessage: 'Vinsamlegast staðfestu skólavist',
     description: 'pending action from student label',
   },
+
+  // TODO(ballioli) These are part of organization access prototype
+  actionCardInSchoolReview: {
+    id: 'uni.application:actionCardInSchoolReview',
+    defaultMessage: 'Í vinnslu hjá skóla',
+    description:
+      'Description of application state/status when the application is in review at school',
+  },
+  actionCardSchoolAccepted: {
+    id: 'uni.application:actionCardSchoolAccepted',
+    defaultMessage: 'Samþykkt frá skóla',
+    description:
+      'Description of application state/status when the application has been accepted by school',
+  },
+  actionCardStudentRejected: {
+    id: 'uni.application:actionCardStudentRejected',
+    defaultMessage: 'Hafnað af umsækjanda',
+    description:
+      'Description of application state/status when the application has been rejected by user',
+  },
+  actionCardSchoolRejected: {
+    id: 'uni.application:actionCardSchoolAccepted',
+    defaultMessage: 'Hafnað af skóla',
+    description:
+      'Description of application state/status when the application has been rejected by school',
+  },
+  actionCardSchoolAcceptedHistory: {
+    id: 'uni.application:actionCardSchoolAcceptedHistory',
+    defaultMessage: 'Umsókn samþykkt',
+    description:
+      'Description of application history message when the application has been accepted by school',
+  },
+  actionCardSchoolRejectedHistory: {
+    id: 'uni.application:actionCardSchoolRejectedHistory',
+    defaultMessage: 'Umsókn hafnað af skóla',
+    description:
+      'Description of application history message when the application has been rejected by school',
+  },
+  pendingActionContentSchool: {
+    id: 'uni.application:pendingActionContentSchool',
+    defaultMessage: 'Skóli fer núna yfir umsóknina þína',
+    description: 'pending action from school label (content)',
+  },
+  pendingActionSchoolRejected: {
+    id: 'uni.application:pendingActionSchoolRejected',
+    defaultMessage: 'Háskólinn hefur hafnað umsókn',
+    description: 'pending action from school rejected (content)',
+  },
+  pendingActionSchoolTitleRejected: {
+    id: 'uni.application:pendingActionSchoolTitleRejected',
+    defaultMessage: 'Háskólinn hafnaði umsókn',
+    description: 'pending action from school title',
+  },
+  pendingActionPendingStudentAnswerContent: {
+    id: 'uni.application:pendingActionPendingStudentAnswerContent',
+    defaultMessage: 'Vinsamlegast staðfestu skólavist',
+    description: 'pending action, applicant please confirm',
+  },
+  pendingActionSchoolAcceptedHistory: {
+    id: 'uni.application:pendingActionSchoolAcceptedHistory',
+    defaultMessage: 'Umsókn samþykkt',
+    description:
+      'Description of pending action message when the application has been accepted by school',
+  },
 })
