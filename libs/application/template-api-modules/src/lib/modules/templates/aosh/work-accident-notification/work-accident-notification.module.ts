e7b51574b9d589ef78e06daca93df7618f5317c0
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-10-29 10:42:30 +0000

    fixing config module after recent main changes

--- libs/application/template-api-modules/src/lib/modules/templates/aosh/work-accident-notification/work-accident-notification.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/aosh/work-accident-notification/work-accident-notification.module.ts
@@ -1,6 +1,5 @@
-import { DynamicModule } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { SharedTemplateAPIModule } from '../../../shared'
-import { BaseTemplateAPIModuleConfig } from '../../../../types'
 import { WorkAccidentNotificationTemplateService } from './work-accident-notification.service'
 import {
   WorkAccidentClientConfig,
@@ -8,20 +7,16 @@ import {
 } from '@island.is/clients/work-accident-ver'
 import { ConfigModule } from '@nestjs/config'
 
-export class WorkAccidentNotificationTemplateModule {
-  static register(config: BaseTemplateAPIModuleConfig): DynamicModule {
-    return {
-      module: WorkAccidentNotificationTemplateModule,
-      imports: [
-        SharedTemplateAPIModule.register(config), // TODO Remember this changed on main https://github.com/island-is/island.is/pull/15872
-        WorkAccidentClientModule,
-        ConfigModule.forRoot({
-          isGlobal: true,
-          load: [WorkAccidentClientConfig],
-        }),
-      ],
-      providers: [WorkAccidentNotificationTemplateService],
-      exports: [WorkAccidentNotificationTemplateService],
-    }
-  }
-}
+@Module({
+  imports: [
+    SharedTemplateAPIModule,
+    WorkAccidentClientModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [WorkAccidentClientConfig],
+    }),
+  ],
+  providers: [WorkAccidentNotificationTemplateService],
+  exports: [WorkAccidentNotificationTemplateService],
+})
+export class WorkAccidentNotificationTemplateModule {}
