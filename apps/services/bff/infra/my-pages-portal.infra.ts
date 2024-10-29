5ccc148059c7ef929ce1530de38e5d06616114ce
snaerseljan <snaer@aranja.com>
2024-10-29 14:11:29 +0000

    disable global auth on dev

--- apps/services/bff/infra/my-pages-portal.infra.ts
+++ apps/services/bff/infra/my-pages-portal.infra.ts
@@ -57,11 +57,11 @@ export const serviceSetup = (
         },
         extraAnnotations: {
           dev: {
+            'nginx.ingress.kubernetes.io/enable-global-auth': 'false',
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
           },
           staging: {
-            'nginx.ingress.kubernetes.io/enable-global-auth': 'false',
             'nginx.ingress.kubernetes.io/proxy-buffering': 'on',
             'nginx.ingress.kubernetes.io/proxy-buffer-size': '8k',
           },
