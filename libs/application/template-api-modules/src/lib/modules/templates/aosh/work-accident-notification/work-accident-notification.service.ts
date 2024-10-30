bb7045c3bfdf204f3d2f5b87115b0473251f7b53
Sigrún Tinna Gissurardóttir <sigrun.tinna.gissurardottir@origo.is>
2024-10-30 08:59:25 +0000

    fixing submit

--- libs/application/template-api-modules/src/lib/modules/templates/aosh/work-accident-notification/work-accident-notification.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/aosh/work-accident-notification/work-accident-notification.service.ts
@@ -39,23 +39,24 @@ export class WorkAccidentNotificationTemplateService extends BaseTemplateApiServ
   }: TemplateApiModuleActionProps): Promise<void> {
     const answers = application.answers as unknown as WorkAccidentNotification
 
-    console.log({
+    await this.workAccidentClientService.createAccident(auth, {
       accidentForCreationDto: {
         companySSN: answers.companyInformation.nationalId,
         sizeOfEnterprise: parseInt(
           answers.companyInformation.numberOfEmployees,
           10,
         ),
-        nameOfBranchOrDepartment: answers.companyInformation.nameOfBranch,
+        nameOfBranchOrDepartment:
+          answers.companyInformation.nameOfBranch ?? '1', // TODO: Vinnueftirlit will decide what number is default
         address: answers.companyInformation.address,
-        postcode: answers.companyInformation.postnumber,
+        postcode: answers.companyInformation.postnumber.slice(0, 3),
         workplaceHealthAndSafety:
           answers.companyLaborProtection.workhealthAndSafetyOccupation?.map(
             (code: string) => {
               return parseInt(code, 10)
             },
           ),
-        buyersSSN: answers.projectPurchase.nationalId,
+        buyersSSN: answers.projectPurchase.nationalId ?? '',
         dateAndTimeOfAccident: getDateAndTime(
           answers.accident.date,
           answers.accident.time.slice(0, 2),
@@ -64,7 +65,7 @@ export class WorkAccidentNotificationTemplateService extends BaseTemplateApiServ
         aoshCame: answers.accident.didAoshCome === 'yes',
         policeCame: answers.accident.didPoliceCome === 'yes',
         numberOfVictims: answers.employee.length,
-        municipalityWhereAccidentOccured: answers.accident.municipality, // Vilja þau code eða name til baka?
+        municipalityWhereAccidentOccured: answers.accident.municipality,
         specificLocationOfAccident: answers.accident.exactLocation,
         detailedDescriptionOfAccident: answers.accident.wasDoing.concat(
           '\n',
@@ -94,120 +95,6 @@ export class WorkAccidentNotificationTemplateService extends BaseTemplateApiServ
               employee.startOfWorkdayDate,
               employee.startTime.slice(0, 2),
               employee.startTime.slice(2, 4),
-            ),
-            workStation: employee.workstation
-              ? parseInt(employee.workstation, 10)
-              : 0,
-            victimsOccupation: employee.victimsOccupation.value,
-            absenceDueToAccident: answers.absence[index]
-              ? parseInt(answers.absence[index], 10)
-              : 0,
-            specificPhysicalActivities: getValueList(
-              application.answers,
-              `circumstances[${index}].physicalActivities`,
-            ),
-            specificPhysicalActivityMostSevere: getValueViaPath(
-              application.answers,
-              `circumstances[${index}].physicalActivitiesMostSerious`,
-              null,
-            ) as string | null,
-            workDeviations: getValueList(
-              application.answers,
-              `deviations[${index}].workDeviations`,
-            ),
-            workDeviationMostSevere: getValueViaPath(
-              application.answers,
-              `deviations[${index}].workDeviationsMostSerious`,
-              null,
-            ) as string | null,
-            contactModeOfInjuries: getValueList(
-              application.answers,
-              `causeOfInjury[${index}].contactModeOfInjury`,
-            ),
-            contactModeOfInjuryMostSevere: getValueViaPath(
-              application.answers,
-              `causeOfInjury[${index}].contactModeOfInjuryMostSerious`,
-              null,
-            ) as string | null,
-            partsOfBodyInjured: getValueList(
-              application.answers,
-              `injuredBodyParts[${index}].partOfBodyInjured`,
-            ),
-            partOfBodyInjuredMostSevere: getValueViaPath(
-              application.answers,
-              `injuredBodyParts[${index}].partOfBodyInjuredMostSerious`,
-              null,
-            ) as string | null,
-            typesOfInjury: getValueList(
-              application.answers,
-              `typeOfInjury[${index}].typeOfInjury`,
-            ),
-            typeOfInjuryMostSevere: getValueViaPath(
-              application.answers,
-              `typeOfInjury[${index}].typeOfInjuryMostSerious`,
-              null,
-            ) as string | null,
-          }
-        }),
-        userPhoneNumber: answers.companyInformation.phonenumber,
-        userEmail: answers.companyInformation.email,
-      },
-    })
-    await this.workAccidentClientService.createAccident(auth, {
-      accidentForCreationDto: {
-        companySSN: answers.companyInformation.nationalId,
-        sizeOfEnterprise: parseInt(
-          answers.companyInformation.numberOfEmployees,
-          10,
-        ),
-        nameOfBranchOrDepartment: answers.companyInformation.nameOfBranch,
-        address: answers.companyInformation.address,
-        postcode: answers.companyInformation.postnumber,
-        workplaceHealthAndSafety:
-          answers.companyLaborProtection.workhealthAndSafetyOccupation?.map(
-            (code: string) => {
-              return parseInt(code, 10)
-            },
-          ),
-        buyersSSN: answers.projectPurchase.nationalId ?? '',
-        dateAndTimeOfAccident: getDateAndTime(
-          answers.accident.date,
-          answers.accident.time.slice(0, 2),
-          answers.accident.time.slice(2, 4),
-        ),
-        aoshCame: answers.accident.didAoshCome === 'yes',
-        policeCame: answers.accident.didPoliceCome === 'yes',
-        numberOfVictims: answers.employee.length,
-        municipalityWhereAccidentOccured: answers.accident.municipality, // Vilja þau code eða name til baka?
-        specificLocationOfAccident: answers.accident.exactLocation,
-        detailedDescriptionOfAccident: answers.accident.wasDoing.concat(
-          '\n',
-          answers.accident.wentWrong,
-          '\n',
-          answers.accident.how,
-        ),
-        workingEnvironment: answers.accident.accidentLocation.value,
-        victims: answers.employee.map((employee, index) => {
-          return {
-            victimsSSN: employee.nationalField.nationalId,
-            employmentStatusOfVictim: employee.employmentStatus
-              ? parseInt(employee.employmentStatus, 10)
-              : 0,
-            employmentAgencySSN: employee.tempEmploymentSSN ?? '',
-            startedEmploymentForCompany: new Date(employee.startDate),
-            lengthOfEmployment: employee.employmentTime
-              ? parseInt(employee.employmentTime, 10)
-              : 0,
-            percentageOfFullWorkTime: employee.employmentRate
-              ? parseInt(employee.employmentRate, 10)
-              : 0,
-            workhourArrangement: employee.workhourArrangement
-              ? parseInt(employee.workhourArrangement, 10)
-              : 0,
-            startOfWorkingDay: getDateAndTime(
-              employee.startOfWorkdayDate,
-              employee.startTime.split(':')[0],
-              employee.startTime.split(':')[1],
             ),
             workStation: employee.workstation
               ? parseInt(employee.workstation, 10)
