8fc91c70c9f3cdd0bf1303c22e7cba3796152a5b
Hjortur Johann Vignissson <hjortur@norda.is>
2024-10-30 12:55:32 +0000

    undoing accidental change to return type in file upload resolver

--- libs/api/domains/file-upload/src/lib/file-upload.resolver.ts
+++ libs/api/domains/file-upload/src/lib/file-upload.resolver.ts
@@ -1,13 +1,14 @@
 import { Args, Resolver, Mutation } from '@nestjs/graphql'
 import { PresignedPost } from './presignedPost.model'
 import { FileStorageService } from '@island.is/file-storage'
+import { PresignedPost as S3PresignedPost } from '@aws-sdk/s3-presigned-post'
 
 @Resolver()
 export class FileUploadResolver {
   constructor(private fileStorageService: FileStorageService) {}
 
   @Mutation(() => PresignedPost)
-  createUploadUrl(@Args('filename') filename: string): Promise<PresignedPost> {
+  createUploadUrl(@Args('filename') filename: string): Promise<S3PresignedPost> {
     return this.fileStorageService.generatePresignedPost(filename)
   }
 }
