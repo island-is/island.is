83fb98c7598bd03e6e6f6c8806ed09322f3819b4
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2024-10-30 13:38:24 +0000

    removing table since it is not a table

--- apps/financial-aid/open-api/src/app/backendModels/pdfResponse.model.ts
+++ apps/financial-aid/open-api/src/app/backendModels/pdfResponse.model.ts
@@ -4,10 +4,6 @@ import { ApiProperty } from '@nestjs/swagger'
 
 import { Staff } from '@island.is/financial-aid/shared/lib'
 
-@Table({
-  tableName: 'pdfResponses',
-  timestamps: false,
-})
 export class PdfResponseBackendModel extends Model<Staff> {
   @Column({
     type: DataType.STRING,
