3525f5d5ae2480b7e6c2e496e74062d9132b1fdb
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-11-01 09:28:05 +0000

    fix: build

--- libs/clients/license-client/src/lib/clients/machine-license-client/machineLicenseClient.spec.ts
+++ libs/clients/license-client/src/lib/clients/machine-license-client/machineLicenseClient.spec.ts
@@ -3,18 +3,19 @@ import ValidMachineLicense from '../__mock_data/validMachineLicense.json'
 
 describe('license-service/client/machine-license', () => {
   describe('check if expired', () => {
-  describe('get latest license qualification expiration date', () => {
-    it('should return the latest license right date', async () => {
-      const result = mapper.findLatestExpirationDate(ValidMachineLicense)
+    describe('get latest license qualification expiration date', () => {
+      it('should return the latest license right date', async () => {
+        const result = mapper.findLatestExpirationDate(ValidMachineLicense)
 
-      const expectedDate = new Date('2080-01-01T00:00:00Z').toISOString()
+        const expectedDate = new Date('2080-01-01T00:00:00Z').toISOString()
 
-      expect(result).toMatch(expectedDate)
-    })
-    it('should return undefined if no license info', async () => {
-      const result = mapper.findLatestExpirationDate({})
+        expect(result).toMatch(expectedDate)
+      })
+      it('should return undefined if no license info', async () => {
+        const result = mapper.findLatestExpirationDate({})
 
-      expect(result).toBeNull
+        expect(result).toBeNull
+      })
     })
   })
 })
