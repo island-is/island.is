36f7a3180953f434c53d0acced8144a5faf951ba
Baldur Óli Barkarson <ballioli1@gmail.com>
2024-11-06 09:16:02 +0000

    Remove fake sections

--- libs/application/templates/aosh/work-accident-notification/src/forms/Conclusion.ts
+++ libs/application/templates/aosh/work-accident-notification/src/forms/Conclusion.ts
@@ -1,12 +1,5 @@
-import { buildForm, buildSection } from '@island.is/application/core'
+import { buildForm } from '@island.is/application/core'
 import { Form, FormModes } from '@island.is/application/types'
-import {
-  information,
-  externalData,
-  sections,
-  overview,
-  employee,
-} from '../lib/messages'
 import { Logo } from '../assets/Logo'
 import { conclusionSection } from './WorkAccidentNotificationForm/ConclusionSection'
 
@@ -15,42 +8,5 @@ export const Conclusion: Form = buildForm({
   title: '',
   logo: Logo,
   mode: FormModes.COMPLETED,
-  children: [
-    buildSection({
-      id: 'externalData',
-      title: externalData.dataProvider.sectionTitle,
-      children: [],
-    }),
-    buildSection({
-      id: 'announcement',
-      title: externalData.dataProvider.announcement,
-      children: [],
-    }),
-    buildSection({
-      id: 'informationSection',
-      title: information.general.pageTitle,
-      children: [],
-    }),
-    buildSection({
-      id: 'accidentSection',
-      title: sections.draft.accident,
-      children: [],
-    }),
-    buildSection({
-      id: `employeeSection`,
-      title: employee.employee.pageTitle,
-      children: [],
-    }),
-    buildSection({
-      id: `causeAndConsequencesSection`,
-      title: sections.draft.causes,
-      children: [],
-    }),
-    buildSection({
-      id: `overviewSection`,
-      title: overview.general.sectionTitle,
-      children: [],
-    }),
-    conclusionSection,
-  ],
+  children: [conclusionSection],
 })
--- libs/application/templates/aosh/work-accident-notification/src/forms/Prerequisites.ts
+++ libs/application/templates/aosh/work-accident-notification/src/forms/Prerequisites.ts
@@ -1,8 +1,7 @@
-import { buildForm, buildSection } from '@island.is/application/core'
+import { buildForm } from '@island.is/application/core'
 import { Form, FormModes } from '@island.is/application/types'
 import { prerequisitesSection } from './WorkAccidentNotificationForm/prerequisitesSection'
 import { Logo } from '../assets/Logo'
-import { sections } from '../lib/messages'
 
 export const Prerequisites: Form = buildForm({
   id: 'PrerequisitesForm',
@@ -11,32 +10,5 @@ export const Prerequisites: Form = buildForm({
   mode: FormModes.NOT_STARTED,
   renderLastScreenButton: true,
   renderLastScreenBackButton: true,
-  children: [
-    prerequisitesSection,
-    buildSection({
-      id: 'information',
-      title: sections.prerequisites.information,
-      children: [],
-    }),
-    buildSection({
-      id: 'accident',
-      title: sections.prerequisites.accident,
-      children: [],
-    }),
-    buildSection({
-      id: 'employee',
-      title: sections.prerequisites.employee,
-      children: [],
-    }),
-    buildSection({
-      id: 'overview',
-      title: sections.prerequisites.overview,
-      children: [],
-    }),
-    buildSection({
-      id: 'conclusion',
-      title: sections.prerequisites.conclusion,
-      children: [],
-    }),
-  ],
+  children: [prerequisitesSection],
 })
