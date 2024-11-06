751f1fe75a7db80768178eaea46f00af6f5d9816
Jón Levy <levy@andes.is>
2024-11-06 06:08:25 +0000

    fix: rename app

--- apps/unicorn-app/jest.config.ts
+++ apps/unicorn-app/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'cd-test',
+  displayName: 'unicorn-app',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/cd-test',
+  coverageDirectory: '../../coverage/apps/unicorn-app',
 }
