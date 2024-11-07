af6218d8b86dd976220cc3ca79fb0be3f6c7588f
Ívar Oddsson <ivaroddsson@gmail.com>
2024-11-07 15:09:07 +0000

    Stop using cf

--- libs/judicial-system/consts/src/lib/messages.ts
+++ libs/judicial-system/consts/src/lib/messages.ts
@@ -1,106 +1,42 @@
-import { defineMessage, MessageDescriptor } from '@formatjs/intl'
-
 import { Gender } from '@island.is/judicial-system/types'
 
 const strings = {
-  intro: defineMessage({
-    id: 'judicial.system.core:subpoena.intro',
-    defaultMessage:
-      'Ákærði er kvaddur til að koma fyrir dóm, hlýða á ákæru, halda uppi vörnum og sæta dómi.',
-    description: 'Notaður sem inngangur.',
-  }),
-  intro_female: defineMessage({
-    id: 'judicial.system.core:subpoena.female',
-    defaultMessage:
-      'Ákærða er kvödd til að koma fyrir dóm, hlýða á ákæru, halda uppi vörnum og sæta dómi.',
-    description: 'Notaður sem inngangur þegar ákærða er kona.',
-  }),
-  intro_non_binary: defineMessage({
-    id: 'judicial.system.core:subpoena.intro_non_binary',
-    defaultMessage:
-      'Ákært er kvatt til að koma fyrir dóm, hlýða á ákæru, halda uppi vörnum og sæta dómi.',
-    description: 'Notaður sem inngangur þegar ákært er kynsegin/annað.',
-  }),
-  introEn: defineMessage({
-    id: 'judicial.system.core:subpoena.intro_en',
-    defaultMessage:
-      'The accused is summoned to the court to hear the charge, make their defence and be subject to judgement.',
-    description: 'Notaður sem inngangur þegar enska er valið tungumál.',
-  }),
-  arrestIntro: defineMessage({
-    id: 'judicial.system.core:subpoena.type_intro',
-    defaultMessage:
-      'Sæki ákærði ekki þing, við þingfestingu máls eða á síðari stigum þess, má hann búast við því að verða handtekinn og færður fyrir dóm.',
-    description: 'Notaður sem inngangur fyrir handtökufyrirkall.',
-  }),
-  arrestIntroFemale: defineMessage({
-    id: 'judicial.system.core:subpoena.arrest_intro_female',
-    defaultMessage:
-      'Sæki ákærða ekki þing, við þingfestingu máls eða á síðari stigum þess, má hún búast við því að verða handtekin og færð fyrir dóm.',
-    description:
-      'Notaður sem inngangur fyrir handtökufyrirkall þegar ákærða er kona.',
-  }),
-  arrestIntroNonBinary: defineMessage({
-    id: 'judicial.system.core:subpoena.arrest_intro_non_binary',
-    defaultMessage:
-      'Sæki ákært ekki þing, við þingfestingu máls eða á síðari stigum þess, má hán búast við því að verða handtekið og fært fyrir dóm.',
-    description:
-      'Notaður sem inngangur fyrir handtökufyrirkall þegar ákærða er kynsegin/annað.',
-  }),
-  arrestIntroEn: defineMessage({
-    id: 'judicial.system.core:subpoena.arrest_intro_en',
-    defaultMessage:
-      'If the accused does not attend the hearing, they will be arrested and brought before the court.',
-    description:
-      'Notaður sem inngangur fyrir handtökufyrirkall þegar enska er valið tungumál.',
-  }),
-  absenceIntro: defineMessage({
-    id: 'judicial.system.core:subpoena.absence_intro',
-    defaultMessage:
-      'Sæki ákærði ekki þing, við þingfestingu máls eða á síðari stigum þess, má hann búast við því að fjarvist hans verði metin til jafns við það að hann viðurkenni að hafa framið brot það sem hann er ákærður fyrir og dómur verði lagður á málið að honum fjarstöddum.',
-    description: 'Notaður sem inngangur fyrir útivistarfyrirkall.',
-  }),
-  absenceIntroFemale: defineMessage({
-    id: 'judicial.system.core:subpoena.absence_intro_female',
-    defaultMessage:
-      'Sæki ákærða ekki þing, við þingfestingu máls eða á síðari stigum þess, má hún búast við því að fjarvist hennar verði metin til jafns við það að hún viðurkenni að hafa framið brot það sem hún er ákærð fyrir og dómur verði lagður á málið að henni fjarstaddri.',
-    description:
-      'Notaður sem inngangur fyrir útivistarfyrirkall þegar ákærða er kona.',
-  }),
-  absenceIntroNonBinary: defineMessage({
-    id: 'judicial.system.core:subpoena.absence_intro_non_binary',
-    defaultMessage:
-      'Sæki ákært ekki þing, við þingfestingu máls eða á síðari stigum þess, má hán búast við því að fjarvist háns verði metin til jafns við það að hán viðurkenni að hafa framið brot það sem hán er ákært fyrir og dómur verði lagður á málið að háni fjarstöddu.',
-    description:
-      'Notaður sem inngangur fyrir útivistarfyrirkall þegar ákærða er kynsegin/annað.',
-  }),
-  absenceIntroEn: defineMessage({
-    id: 'judicial.system.core:subpoena.absence_intro_en',
-    defaultMessage:
-      'If the accused does not attend the hearing they may expect their absence to be evaluated equal to an admission of the infraction of which they is accused of and the sentencing to be delivered in their absence.',
-    description:
-      'Notaður sem inngangur fyrir útivistarfyrirkall þegar enska er valið tungumál.',
-  }),
-  deadline: defineMessage({
-    id: 'judicial.system.core:subpoena.deadline',
-    defaultMessage: 'Birtingarfrestur er þrír sólarhringar.',
-    description: 'Notaður sem texti fyrir frest.',
-  }),
-  deadlineEn: defineMessage({
-    id: 'judicial.system.core:subpoena.deadline_en',
-    defaultMessage: 'Notification deadline is three days.',
-    description: 'Notaður sem texti fyrir frest þegar enska er valið tungumál.',
-  }),
+  intro:
+    'Ákærði er kvaddur til að koma fyrir dóm, hlýða á ákæru, halda uppi vörnum og sæta dómi.',
+  intro_female:
+    'Ákærða er kvödd til að koma fyrir dóm, hlýða á ákæru, halda uppi vörnum og sæta dómi.',
+  intro_non_binary:
+    'Ákært er kvatt til að koma fyrir dóm, hlýða á ákæru, halda uppi vörnum og sæta dómi.',
+  introEn:
+    'The accused is summoned to the court to hear the charge, make their defence and be subject to judgement.',
+  arrestIntro:
+    'Sæki ákærði ekki þing, við þingfestingu máls eða á síðari stigum þess, má hann búast við því að verða handtekinn og færður fyrir dóm.',
+  arrestIntroFemale:
+    'Sæki ákærða ekki þing, við þingfestingu máls eða á síðari stigum þess, má hún búast við því að verða handtekin og færð fyrir dóm.',
+  arrestIntroNonBinary:
+    'Sæki ákært ekki þing, við þingfestingu máls eða á síðari stigum þess, má hán búast við því að verða handtekið og fært fyrir dóm.',
+  arrestIntroEn:
+    'If the accused does not attend the hearing, they will be arrested and brought before the court.',
+  absenceIntro:
+    'Sæki ákærði ekki þing, við þingfestingu máls eða á síðari stigum þess, má hann búast við því að fjarvist hans verði metin til jafns við það að hann viðurkenni að hafa framið brot það sem hann er ákærður fyrir og dómur verði lagður á málið að honum fjarstöddum.',
+  absenceIntroFemale:
+    'Sæki ákærða ekki þing, við þingfestingu máls eða á síðari stigum þess, má hún búast við því að fjarvist hennar verði metin til jafns við það að hún viðurkenni að hafa framið brot það sem hún er ákærð fyrir og dómur verði lagður á málið að henni fjarstaddri.',
+  absenceIntroNonBinary:
+    'Sæki ákært ekki þing, við þingfestingu máls eða á síðari stigum þess, má hán búast við því að fjarvist háns verði metin til jafns við það að hán viðurkenni að hafa framið brot það sem hán er ákært fyrir og dómur verði lagður á málið að háni fjarstöddu.',
+  absenceIntroEn:
+    'If the accused does not attend the hearing they may expect their absence to be evaluated equal to an admission of the infraction of which they is accused of and the sentencing to be delivered in their absence.',
+  deadline: 'Birtingarfrestur er þrír sólarhringar.',
+  deadlineEn: 'Notification deadline is three days.',
 }
 
 export const getIntro = (
   gender?: Gender,
   lang?: string,
 ): {
-  intro: MessageDescriptor
-  absenceIntro: MessageDescriptor
-  arrestIntro: MessageDescriptor
-  deadline: MessageDescriptor
+  intro: string
+  absenceIntro: string
+  arrestIntro: string
+  deadline: string
 } => {
   if (lang === 'en') {
     return {
