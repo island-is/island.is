f6bdcecd807b9c61a8c08b6df3da2c9d10b3c31e
unakb <una@kolibri.is>
2024-11-08 14:11:14 +0000

    test(j-s): Added more civil claimant tests

---
+++ apps/judicial-system/backend/src/app/modules/defendant/test/civilClaimantController/deleteGuards.spec.ts
@@ -0,0 +1,26 @@
+import { CanActivate } from '@nestjs/common'
+
+import { CaseExistsGuard, CaseWriteGuard } from '../../../case'
+import { CivilClaimantController } from '../../civilClaimant.controller'
+
+describe('CivilClaimantController - Delete guards', () => {
+  let guards: Array<new () => CanActivate>
+
+  beforeEach(() => {
+    guards = Reflect.getMetadata(
+      '__guards__',
+      CivilClaimantController.prototype.delete,
+    )
+  })
+
+  it('should have the correct guards in the correct order', () => {
+    const expectedGuards = [CaseExistsGuard, CaseWriteGuard]
+
+    expect(guards).toHaveLength(expectedGuards.length)
+
+    expectedGuards.forEach((expectedGuard, index) => {
+      const guardInstance = new guards[index]()
+      expect(guardInstance).toBeInstanceOf(expectedGuard)
+    })
+  })
+})
