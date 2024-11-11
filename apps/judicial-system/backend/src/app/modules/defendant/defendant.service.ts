5bf6bccb28fd089669fca5faf8e33bd5425c63f4
Guðjón Guðjónsson <gudjon@kolibri.is>
2024-11-11 19:57:29 +0000

    Refactors defendant update messages

--- apps/judicial-system/backend/src/app/modules/defendant/defendant.service.ts
+++ apps/judicial-system/backend/src/app/modules/defendant/defendant.service.ts
@@ -96,26 +96,28 @@ export class DefendantService {
     oldDefendant: Defendant,
     user: User,
   ) {
+    if (!theCase.courtCaseNumber) {
+      return
+    }
+
     // Handling of updates sent to the court system
-    if (theCase.courtCaseNumber) {
-      // A defendant is updated after the case has been received by the court.
-      if (updatedDefendant.noNationalId !== oldDefendant.noNationalId) {
-        // A defendant nationalId is added or removed. Attempt to add the defendant to the court case.
-        // In case there is no national id, the court will be notified.
-        await this.messageService.sendMessagesToQueue([
-          this.getMessageForDeliverDefendantToCourt(updatedDefendant, user),
-        ])
-      } else if (updatedDefendant.nationalId !== oldDefendant.nationalId) {
-        // A defendant is replaced. Attempt to add the defendant to the court case,
-        // but also ask the court to verify defendants.
-        await this.messageService.sendMessagesToQueue([
-          this.getMessageForSendDefendantsNotUpdatedAtCourtNotification(
-            theCase,
-            user,
-          ),
-          this.getMessageForDeliverDefendantToCourt(updatedDefendant, user),
-        ])
-      }
+    // A defendant is updated after the case has been received by the court.
+    if (updatedDefendant.noNationalId !== oldDefendant.noNationalId) {
+      // A defendant nationalId is added or removed. Attempt to add the defendant to the court case.
+      // In case there is no national id, the court will be notified.
+      await this.messageService.sendMessagesToQueue([
+        this.getMessageForDeliverDefendantToCourt(updatedDefendant, user),
+      ])
+    } else if (updatedDefendant.nationalId !== oldDefendant.nationalId) {
+      // A defendant is replaced. Attempt to add the defendant to the court case,
+      // but also ask the court to verify defendants.
+      await this.messageService.sendMessagesToQueue([
+        this.getMessageForSendDefendantsNotUpdatedAtCourtNotification(
+          theCase,
+          user,
+        ),
+        this.getMessageForDeliverDefendantToCourt(updatedDefendant, user),
+      ])
     }
   }
 
@@ -125,24 +127,29 @@ export class DefendantService {
     oldDefendant: Defendant,
     user: User,
   ) {
-    // Handling of updates sent to the court system
-    if (theCase.courtCaseNumber) {
-      if (updatedDefendant.defenderEmail !== oldDefendant.defenderEmail) {
-        // A defendant's defender email is updated.
-        // Attempt to update the defendant in the court case.
-        await this.messageService.sendMessagesToQueue([
-          this.getMessageForDeliverDefendantToCourt(updatedDefendant, user),
-        ])
-      }
+    if (!theCase.courtCaseNumber) {
+      return
+    }
+
+    const isDefenderConfirmed =
+      updatedDefendant.isDefenderChoiceConfirmed &&
+      (updatedDefendant.defenderChoice === DefenderChoice.CHOOSE ||
+        updatedDefendant.defenderChoice === DefenderChoice.DELEGATE)
+
+    if (!isDefenderConfirmed) {
+      return
+    }
+
+    // A defendant's defender email is updated.
+    if (updatedDefendant.defenderEmail !== oldDefendant.defenderEmail) {
+      // TODO: Replace by email to robot
+      await this.messageService.sendMessagesToQueue([
+        this.getMessageForDeliverDefendantToCourt(updatedDefendant, user),
+      ])
     }
 
     // Defender was just confirmed by judge
-    if (
-      updatedDefendant.isDefenderChoiceConfirmed &&
-      !oldDefendant.isDefenderChoiceConfirmed &&
-      (updatedDefendant.defenderChoice === DefenderChoice.CHOOSE ||
-        updatedDefendant.defenderChoice === DefenderChoice.DELEGATE)
-    ) {
+    if (!oldDefendant.isDefenderChoiceConfirmed) {
       await this.messageService.sendMessagesToQueue([
         {
           type: MessageType.DEFENDANT_NOTIFICATION,
