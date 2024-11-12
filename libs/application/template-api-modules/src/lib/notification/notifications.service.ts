152fcde98a624e7abb052676b4cca4efc82b0d3a
Gunnar K Vilbergsson <gunnar@norda.is>
2024-11-12 13:33:57 +0000

    chore(application-system): Add logging to notification service and make it awaitable

--- libs/application/template-api-modules/src/lib/notification/notifications.service.ts
+++ libs/application/template-api-modules/src/lib/notification/notifications.service.ts
@@ -1,11 +1,21 @@
-import { NotificationsApi } from '@island.is/clients/user-notification'
-import { Injectable } from '@nestjs/common'
+import {
+  CreateNotificationResponse,
+  NotificationsApi,
+} from '@island.is/clients/user-notification'
+import type { Logger } from '@island.is/logging'
+import { LOGGER_PROVIDER } from '@island.is/logging'
+import { Inject, Injectable } from '@nestjs/common'
 import { NotificationArgs } from './notificationTypes'
 import { NotificationConfig, NotificationType } from './notificationsTemplates'
 
 @Injectable()
 export class NotificationsService {
-  constructor(private readonly notificationApi: NotificationsApi) {}
+  constructor(
+    private readonly notificationApi: NotificationsApi,
+    @Inject(LOGGER_PROVIDER) private logger: Logger,
+  ) {
+    this.logger = logger.child({ context: 'NotificationsService' })
+  }
 
   /**
    * Sends a notification using the specified type and arguments.
@@ -14,6 +24,7 @@ export class NotificationsService {
    * @param data.type - The type of notification (User, Order, or System)
    * @param data.messageParties.recipient - The recipient nationalId of the notification
    * @param data.messageParties.sender - Optional. The sender nationalId of the notification
+   * @param data.applicationId - Optional. The applicationId of the notification
    * @param data.args - The arguments specific to the notification type
  
    *
@@ -24,6 +35,7 @@ export class NotificationsService {
    *   messageParties: {
    *     recipient: 'user@example.com',
    *   },
+   *   applicationId: '12345',
    *   args: {
    *     username: 'johndoe',
    *     email: 'johndoe@example.com'
@@ -37,6 +49,7 @@ export class NotificationsService {
    *   messageParties: {
    *     recipient: 'customer@example.com',
    *   },
+   *   applicationId: '12345',
    *   args: {
    *     orderId: '12345',
    *     orderStatus: 'Shipped'
@@ -44,14 +57,15 @@ export class NotificationsService {
    *   sender: 'orders@company.com'
    * });
    */
-  public sendNotification<T extends NotificationType>(data: {
+  public async sendNotification<T extends NotificationType>(data: {
     type: T
     messageParties: {
       recipient: string
       sender?: string
     }
+    applicationId?: string
     args?: NotificationArgs<T>
-  }) {
+  }): Promise<CreateNotificationResponse> {
     const templateId = NotificationConfig[data.type].templateId
 
     const notification = {
@@ -64,8 +78,17 @@ export class NotificationsService {
       })),
     }
 
-    this.notificationApi.notificationsControllerCreateHnippNotification({
-      createHnippNotificationDto: notification,
-    })
+    const response =
+      await this.notificationApi.notificationsControllerCreateHnippNotification(
+        {
+          createHnippNotificationDto: notification,
+        },
+      )
+
+    this.logger.info(
+      `Notification with id ${response.id} sent for application ${data.applicationId}`,
+    )
+
+    return response
   }
 }
