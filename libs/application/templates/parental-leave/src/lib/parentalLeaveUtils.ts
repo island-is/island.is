8532ae8b11fbe49ee7dc9ab6354ddeb6c80f6718
hfhelgason <hfhelgason@deloitte.is>
2024-11-12 10:21:30 +0000

    If startDate is the 31st then the endDate should be 1 day later

--- libs/application/templates/parental-leave/src/lib/parentalLeaveUtils.ts
+++ libs/application/templates/parental-leave/src/lib/parentalLeaveUtils.ts
@@ -1450,6 +1450,9 @@ export const calculateEndDateForPeriodWithStartAndLength = (
 
   // If startDay is first day of the month and daysToAdd = 0
   if (daysToAdd === 0) {
+    if (start.getDate() === 31) {
+      endDate = addDays(endDate, 1)
+    }
     return endDate
   }
 
