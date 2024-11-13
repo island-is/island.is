2c87a9800aa3a2d8e50c196c5fa9fe7f7ba29e12
Kristofer <kristofer@andes.is>
2024-11-13 10:45:34 +0000

    init

--- infra/src/dsl/adapters/get-ssm-params.ts
+++ infra/src/dsl/adapters/get-ssm-params.ts
@@ -1,11 +1,7 @@
-import { SSM } from '@aws-sdk/client-ssm'
+import { GetParametersCommand, SSMClient } from '@aws-sdk/client-ssm'
 import { logger } from '../../common'
 
-const API_INITIALIZATION_OPTIONS = {
-  region: 'eu-west-1',
-  maxAttempts: 10,
-}
-const client = new SSM(API_INITIALIZATION_OPTIONS)
+const client = new SSMClient({})
 export async function getSsmParams(
   ssmNames: string[],
 ): Promise<{ [name: string]: string }> {
@@ -18,7 +14,7 @@ export async function getSsmParams(
 
   const allParams = await Promise.all(
     chunks.map((Names) =>
-      client.getParameters({ Names, WithDecryption: true }),
+      client.send(new GetParametersCommand({ Names, WithDecryption: true })),
     ),
   ).catch(handleCredentialsProviderError)
   const params = allParams
@@ -37,6 +33,12 @@ export async function getSsmParams(
 }
 
 function handleCredentialsProviderError(err: Error): never {
+  logger.debug('AWS environment is not configured properly', {
+    err,
+    AWS_ENV: Object.fromEntries(
+      Object.entries(process.env).filter(([k, _]) => k.startsWith('AWS_')),
+    ),
+  })
   if (err.name === 'CredentialsProviderError') {
     console.error(
       'Could not load AWS credentials from any providers. Did you forget to configure environment variables, aws profile or run `aws sso login`?',
