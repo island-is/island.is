4759fbd54975dc4a0a81b14c730d13d816db25a8
Kristofer <kristofer@andes.is>
2024-11-13 11:09:50 +0000

    Restore irrelevant files to main

--- infra/src/dsl/adapters/get-ssm-params.ts
+++ infra/src/dsl/adapters/get-ssm-params.ts
@@ -1,25 +1,15 @@
-import { GetParametersCommand, SSMClient } from '@aws-sdk/client-ssm'
-import { fromNodeProviderChain } from '@aws-sdk/credential-providers'
+import { SSM } from '@aws-sdk/client-ssm'
 import { logger } from '../../common'
 
-const credentials = fromNodeProviderChain()
-
-// const client = new SSMClient({ credentials: fromEnv() })
-const client = new SSMClient({
-  // region: 'eu-west-1',
-  // credentials: fromEnv(),
-  credentials,
-})
-
+const API_INITIALIZATION_OPTIONS = {
+  region: 'eu-west-1',
+  maxAttempts: 10,
+}
+const client = new SSM(API_INITIALIZATION_OPTIONS)
 export async function getSsmParams(
   ssmNames: string[],
 ): Promise<{ [name: string]: string }> {
   logger.debug('getSsmParams', { numSsmNames: ssmNames.length })
-  logger.debug('AWS env vars', {
-    AWS_ENV: Object.fromEntries(
-      Object.entries(process.env).filter(([k]) => k.startsWith('AWS_')),
-    ),
-  })
   const chunks = ssmNames.reduce((all: string[][], one: string, i: number) => {
     const ch = Math.floor(i / 10)
     all[ch] = ([] as string[]).concat(all[ch] || [], one)
@@ -27,11 +17,8 @@ export async function getSsmParams(
   }, [])
 
   const allParams = await Promise.all(
-    chunks.map(
-      async (Names) =>
-        await client.send(
-          new GetParametersCommand({ Names, WithDecryption: true }),
-        ),
+    chunks.map((Names) =>
+      client.getParameters({ Names, WithDecryption: true }),
     ),
   ).catch(handleCredentialsProviderError)
   const params = allParams
@@ -51,7 +38,6 @@ export async function getSsmParams(
 
 function handleCredentialsProviderError(err: Error): never {
   if (err.name === 'CredentialsProviderError') {
-    logger.debug(`CredentialsProviderError: ${err.message}`, { err })
     console.error(
       'Could not load AWS credentials from any providers. Did you forget to configure environment variables, aws profile or run `aws sso login`?',
     )
