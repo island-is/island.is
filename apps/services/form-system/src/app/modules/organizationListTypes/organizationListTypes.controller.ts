aca802d1d529c5c3825e3b22f7f2215aa3e58612
OlafurGeorg <olafur.georg.gylfason@advania.is>
2024-11-14 11:02:18 +0000

    add organization-listtype endpoints

---
+++ apps/services/form-system/src/app/modules/organizationListTypes/organizationListTypes.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  VERSION_NEUTRAL,
+} from '@nestjs/common'
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger'
+import { OrganizationListTypesService } from './organizationListTypes.service'
+import { OrganizationListTypeDto } from './models/dto/organizationListType.dto'
+import { CreateOrganizationListTypeDto } from './models/dto/createOrganizationListType.dto'
+
+@ApiTags('organization list types')
+@Controller({
+  path: 'organizationListTypes',
+  version: ['1', VERSION_NEUTRAL],
+})
+export class OrganizationListTypesController {
+  constructor(
+    private organizationListTypesService: OrganizationListTypesService,
+  ) {}
+
+  @ApiOperation({ summary: 'Add organization list type' })
+  @ApiCreatedResponse({
+    description: 'Add organization list type',
+    type: OrganizationListTypeDto,
+  })
+  @ApiBody({ type: CreateOrganizationListTypeDto })
+  @Post()
+  create(
+    @Body()
+    createOrganizationListTypeDto: CreateOrganizationListTypeDto,
+  ): Promise<OrganizationListTypeDto> {
+    return this.organizationListTypesService.create(
+      createOrganizationListTypeDto,
+    )
+  }
+
+  @ApiOperation({ summary: 'Remove organization list type' })
+  @ApiNoContentResponse({
+    description: 'Remove organization list type',
+  })
+  @ApiParam({ name: 'id', type: String })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.organizationListTypesService.delete(id)
+  }
+}
