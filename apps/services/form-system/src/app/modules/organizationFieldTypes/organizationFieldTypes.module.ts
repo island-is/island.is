dbc0834fbc110ecb9eca9d36fe4556c1965d156b
OlafurGeorg <olafur.georg.gylfason@advania.is>
2024-11-14 10:31:13 +0000

    add organization-fieldtype endpoints

--- apps/services/form-system/src/app/modules/organizationFieldTypes/organizationFieldTypes.module.ts
+++ apps/services/form-system/src/app/modules/organizationFieldTypes/organizationFieldTypes.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { OrganizationFieldType } from './models/organizationFieldType.model'
+import { OrganizationFieldTypesController } from './organizationFieldTypes.controller'
+import { OrganizationFieldTypesService } from './organizationFieldTypes.service'
 
 @Module({
   imports: [SequelizeModule.forFeature([OrganizationFieldType])],
-  // controllers: [OrganizationCertificationsController],
-  // providers: [OrganizationCertificationsService],
+  controllers: [OrganizationFieldTypesController],
+  providers: [OrganizationFieldTypesService],
 })
 export class OrganizationFieldTypesModule {}
