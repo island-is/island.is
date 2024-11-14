b106a88972452560b61a75972761d455d245547e
OlafurGeorg <olafur.georg.gylfason@advania.is>
2024-11-14 09:55:13 +0000

    add organization-certification endpoints

--- apps/services/form-system/src/app/modules/organizationCertificationTypes/models/dto/organizationCertificationType.dto.ts
+++ apps/services/form-system/src/app/modules/organizationCertificationTypes/models/dto/organizationCertificationType.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 
-export class FormCertificationTypeDto {
+export class OrganizationCertificationTypeDto {
   @ApiProperty()
   id!: string
 
--- apps/services/form-system/src/app/modules/organizationCertificationTypes/organizationCertificationTypes.controller.ts
+++ apps/services/form-system/src/app/modules/organizationCertificationTypes/organizationCertificationTypes.controller.ts
@@ -1,7 +1,56 @@
-import { Controller, VERSION_NEUTRAL } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  VERSION_NEUTRAL,
+} from '@nestjs/common'
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger'
+import { OrganizationCertificationTypesService } from './organizationCertificationTypes.service'
+import { OrganizationCertificationTypeDto } from './models/dto/organizationCertificationType.dto'
+import { CreateOrganizationCertificationTypeDto } from './models/dto/createOrganizationCertificationType.dto'
 
+@ApiTags('organization certification types')
 @Controller({
   path: 'organizationCertificationTypes',
   version: ['1', VERSION_NEUTRAL],
 })
-export class OrganizationCertificationTypesController {}
+export class OrganizationCertificationTypesController {
+  constructor(
+    private organizationCertificationTypesService: OrganizationCertificationTypesService,
+  ) {}
+
+  @ApiOperation({ summary: 'Add organization certification type' })
+  @ApiCreatedResponse({
+    description: 'Add organization certification type',
+    type: OrganizationCertificationTypeDto,
+  })
+  @ApiBody({ type: CreateOrganizationCertificationTypeDto })
+  @Post()
+  create(
+    @Body()
+    createOrganizationCertificationTypeDto: CreateOrganizationCertificationTypeDto,
+  ): Promise<OrganizationCertificationTypeDto> {
+    return this.organizationCertificationTypesService.create(
+      createOrganizationCertificationTypeDto,
+    )
+  }
+
+  @ApiOperation({ summary: 'Remove organization certification type' })
+  @ApiNoContentResponse({
+    description: 'Remove organization certification type',
+  })
+  @ApiParam({ name: 'id', type: String })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.organizationCertificationTypesService.delete(id)
+  }
+}
