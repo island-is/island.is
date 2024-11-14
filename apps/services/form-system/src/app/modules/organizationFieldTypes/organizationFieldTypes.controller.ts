9967ca64676545f42abdb88d2e9aa906d1c87789
OlafurGeorg <olafur.georg.gylfason@advania.is>
2024-11-14 10:31:13 +0000

    add organization-fieldtype endpoints

---
+++ apps/services/form-system/src/app/modules/organizationFieldTypes/organizationFieldTypes.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  VERSION_NEUTRAL,
+} from '@nestjs/common'
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger'
+import { OrganizationFieldTypesService } from './organizationFieldTypes.service'
+import { OrganizationFieldTypeDto } from './models/organizationFieldType.dto'
+import { CreateOrganizationFieldTypeDto } from './models/createOrganizationFieldType.dto'
+
+@ApiTags('organization field types')
+@Controller({
+  path: 'organizationFieldTypes',
+  version: ['1', VERSION_NEUTRAL],
+})
+export class OrganizationFieldTypesController {
+  constructor(
+    private organizationFieldTypesService: OrganizationFieldTypesService,
+  ) {}
+
+  @ApiOperation({ summary: 'Add organization field type' })
+  @ApiCreatedResponse({
+    description: 'Add organization field type',
+    type: OrganizationFieldTypeDto,
+  })
+  @ApiBody({ type: CreateOrganizationFieldTypeDto })
+  @Post()
+  create(
+    @Body()
+    createOrganizationFieldTypeDto: CreateOrganizationFieldTypeDto,
+  ): Promise<OrganizationFieldTypeDto> {
+    return this.organizationFieldTypesService.create(
+      createOrganizationFieldTypeDto,
+    )
+  }
+
+  @ApiOperation({ summary: 'Remove organization field type' })
+  @ApiNoContentResponse({
+    description: 'Remove organization field type',
+  })
+  @ApiParam({ name: 'id', type: String })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.organizationFieldTypesService.delete(id)
+  }
+}
