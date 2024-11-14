e6f7f912d347be2d4ea0bff366cfc5923a3e3e55
HjorturJ <hjorturjohann94@gmail.com>
2024-11-14 17:22:25 +0000

    bucket change and debug error log

--- libs/file-storage/src/lib/file-storage.configuration.ts
+++ libs/file-storage/src/lib/file-storage.configuration.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 const FileStorageModule = z.object({
   uploadBucket: z.string(),
+  secondUploadBucket: z.string(),
 })
 
 export const FileStorageConfig = defineConfig({
@@ -12,6 +13,10 @@ export const FileStorageConfig = defineConfig({
     uploadBucket: env.optional(
       'FILE_STORAGE_UPLOAD_BUCKET',
       'island-is-dev-upload-api',
+    ),
+    secondUploadBucket: env.optional(
+      'APPLICATION_ATTACHMENT_BUCKET',
+      'bruh'
     ),
   }),
 })
