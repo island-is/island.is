63d6f8103e46080e7569ee16e0d9f9ea62d06138
Kristofer <kristofer@andes.is>
2024-11-15 01:33:57 +0000

    Revert non-upgrades to `main`

--- infra/scripts/build-docker-container.sh
+++ infra/scripts/build-docker-container.sh
@@ -2,10 +2,9 @@
 set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-DOCKER_REGISTRY=${DOCKER_REGISTRY:-821090935708.dkr.ecr.eu-west-1.amazonaws.com}
-DOCKER_REGISTRY=${DOCKER_REGISTRY%/} # Strip trailing slash
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-821090935708.dkr.ecr.eu-west-1.amazonaws.com/}
 PUBLISH=${PUBLISH:-false}
-DOCKER_IMAGE="${DOCKER_REGISTRY}/helm-config"
+DOCKER_IMAGE="${DOCKER_REGISTRY}helm-config"
 DOCKER_TAG=$1
 
 # shellcheck disable=SC2086
--- infra/scripts/format-yaml.sh
+++ infra/scripts/format-yaml.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT="$DIR/.."
 
-(
-  cd "$ROOT"
-  node -r esbuild-register "$ROOT"/src/process.ts
-)
+cd "$ROOT"
+node -r esbuild-register "$ROOT"/src/process.ts
+cd -
