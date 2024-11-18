0d9addf1adc72ccbb915a26de78f137e9047863b
Svana <svanhildur@andes.is>
2024-11-18 13:59:08 +0000

    Change web start command e2e playwright

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -2,7 +2,8 @@ import { createPlaywrightConfig } from '@island.is/testing/e2e'
 
 const playwrightConfig = createPlaywrightConfig({
   webServerUrl: 'http://localhost:4200',
-  command: '(yarn dev-init web && yarn dev web)',
+  command:
+    '(cd infra && yarn cli run-local-env web --dependencies api --print --no-secrets --proxies)',
   cwd: '../../',
 })
 
