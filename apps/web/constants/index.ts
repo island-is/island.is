54290b21a87df9d85783ff33b675033c8cb710b1
Rúnar Vestmann <43557895+RunarVestmann@users.noreply.github.com>
2024-11-18 14:33:32 +0000

    feat(web): Outbound link tracking in Plausible (#16899)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/web/constants/index.ts
+++ apps/web/constants/index.ts
@@ -7,6 +7,7 @@ export const PROJECT_STORIES_TAG_ID = '9yqOTwQYzgyej5kItFTtd'
 export const ADGERDIR_INDIVIDUALS_TAG_ID = '4kLt3eRht5yJoakIHWsusb'
 export const ADGERDIR_COMPANIES_TAG_ID = '4ZWcwoW2IiB2AhtzQpzdIW'
 export const FRONTPAGE_NEWS_TAG_ID = 'forsidufrettir'
-export const PLAUSIBLE_SCRIPT_SRC = 'https://plausible.io/js/plausible.js'
+export const PLAUSIBLE_SCRIPT_SRC =
+  'https://plausible.io/js/script.outbound-links.js'
 export const DIGITAL_ICELAND_PLAUSIBLE_TRACKING_DOMAIN =
   'island.is/s/stafraent-island'
