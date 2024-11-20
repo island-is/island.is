74babb086edd55e3fd65341416736437cd8c5471
andes-it <builders@andes.is>
2024-11-20 15:16:00 +0000

    chore: nx format:write update dirty files

--- libs/localization/scripts/extract.ts
+++ libs/localization/scripts/extract.ts
@@ -120,9 +120,11 @@ globSync('libs/localization/messages.json')
     Object.entries<MessageDict>(f).forEach(
       async ([namespaceId, namespaceMessages]) => {
         const namespace = await getNamespace(namespaceId)
-        const locales = ((await getLocales()) as {
-          items: Record<string, any>[]
-        }).items.map((locale) => ({ id: locale.code }))
+        const locales = (
+          (await getLocales()) as {
+            items: Record<string, any>[]
+          }
+        ).items.map((locale) => ({ id: locale.code }))
 
         // If namespace does exist we update it, else we create it
         if (namespace) {
