f13c96599ae6111ab3aeb2e66e23396f70c9ad76
albina <albina@kosmosogkaos.is>
2024-11-20 11:14:27 +0000

    f

--- libs/application/templates/inheritance-report/src/fields/OverviewOtherDocuments/index.tsx
+++ libs/application/templates/inheritance-report/src/fields/OverviewOtherDocuments/index.tsx
@@ -4,18 +4,14 @@ import { Text } from '@island.is/island-ui/core'
 import { FC } from 'react'
 
 export const OverviewOtherDocuments: FC<
-React.PropsWithChildren<FieldBaseProps>
+  React.PropsWithChildren<FieldBaseProps>
 > = ({ application }) => {
   const { answers } = application
-  const files = getValueViaPath<{key: string, name: string}[]>(
+  const files = getValueViaPath<{ key: string; name: string }[]>(
     answers,
     'heirsAdditionalInfoFilesOtherDocuments',
   )
-  return (
-    files?.map((file) => (
-      <Text key={file.key}>{file.name}</Text>
-    ))
-  )
+  return files?.map((file) => <Text key={file.key}>{file.name}</Text>)
 }
 
 export default OverviewOtherDocuments
