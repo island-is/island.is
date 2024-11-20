8ff39a850cab0322989a6ffabf977e3898efbe26
Kristofer <kristofer@andes.is>
2024-11-20 18:35:42 +0000

    Add --load flag to docker build

--- infra/scripts/build-docker-container.sh
+++ infra/scripts/build-docker-container.sh
@@ -8,7 +8,7 @@ DOCKER_IMAGE="${DOCKER_REGISTRY}helm-config"
 DOCKER_TAG=$1
 
 # shellcheck disable=SC2086
-docker build -f "$DIR"/Dockerfile ${EXTRA_DOCKER_BUILD_ARGS:-} -t "$DOCKER_IMAGE":"${DOCKER_TAG}" "$DIR"/../..
+docker build --load -f "$DIR"/Dockerfile ${EXTRA_DOCKER_BUILD_ARGS:-} -t "$DOCKER_IMAGE":"${DOCKER_TAG}" "$DIR"/../..
 
 if [[ "true" = "$PUBLISH" ]]; then
   docker push "$DOCKER_IMAGE":"${DOCKER_TAG}"
