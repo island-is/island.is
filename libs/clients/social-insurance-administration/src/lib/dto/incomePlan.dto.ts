2c9c975e7a366797c7b1bc3023bd49b98be8ed9d
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-11-22 15:45:40 +0000

    chore: remove mocks

--- libs/clients/social-insurance-administration/src/lib/dto/incomePlan.dto.ts
+++ libs/clients/social-insurance-administration/src/lib/dto/incomePlan.dto.ts
@@ -12,7 +12,6 @@ export interface IncomePlanDto {
   incomeTypeLines?: Array<TrWebCommonsExternalPortalsApiModelsIncomePlanIncomeTypeLine>
 }
 
-/*
 export const mapIncomePlanDto = (
   data: TrWebCommonsExternalPortalsApiModelsIncomePlanIncomePlanDto,
 ): IncomePlanDto | undefined => ({
@@ -21,37 +20,4 @@ export const mapIncomePlanDto = (
   status: data.status as IncomePlanStatus,
   origin: data.origin ?? undefined,
   incomeTypeLines: data.incomeTypeLines ?? undefined,
-}) */
-
-export const mapIncomePlanDto = (
-  data: TrWebCommonsExternalPortalsApiModelsIncomePlanIncomePlanDto,
-): IncomePlanDto | undefined => ({
-  year: 1993,
-  registrationDate: new Date(),
-  status: 'InProgress',
-  origin: 'islandis',
-  incomeTypeLines: [
-    {
-      incomeCategoryNumber: 1,
-      incomeCategoryName: 'ben',
-      incomeCategoryCode: '8',
-      incomeTypeNumber: 1,
-      incomeTypeName: 'bong',
-      incomeTypeCode: '8',
-      amountJan: 1,
-      amountFeb: 2,
-      amountMar: 3,
-      amountApr: 4,
-      amountMay: 5,
-      amountJun: 6,
-      amountJul: 7,
-      amountAug: 8,
-      amountSep: 9,
-      amountNov: 11,
-      amountDec: 12,
-      amountOct: 10,
-      totalSum: 78,
-      currency: 'ISK',
-    },
-  ],
 })
