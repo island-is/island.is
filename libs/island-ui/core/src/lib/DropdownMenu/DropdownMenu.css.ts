6b420bf91a763b902a9978d6570a56f8c975c30b
runarvestmann <runar@stefna.is>
2024-11-22 16:12:09 +0000

    Revert island-ui changes

--- libs/island-ui/core/src/lib/DropdownMenu/DropdownMenu.css.ts
+++ libs/island-ui/core/src/lib/DropdownMenu/DropdownMenu.css.ts
@@ -2,8 +2,8 @@ import { theme, zIndex } from '@island.is/island-ui/theme'
 import { style, globalStyle } from '@vanilla-extract/css'
 
 export const menu = style({
-  width: 193,
-  zIndex: zIndex.aboveHeader,
+  width: 150,
+  zIndex: zIndex.above,
   boxShadow: '0px 4px 30px rgba(0, 97, 255, 0.16)',
   ':focus': {
     outline: 'none',
