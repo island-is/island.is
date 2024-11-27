830580aab4bd6de9f6303942ebda0708fc3e272f
Róberta Andersen <roberta@andes.is>
2024-11-27 20:37:24 +0000

    chore: Adding pipeline or unicorns

---
+++ infra/src/unicorn-utils.ts
@@ -0,0 +1,31 @@
+import { argv } from 'process';
+import yargs  from 'yargs';
+
+const unicornApps = [
+ 'unicorn-app'
+]
+
+
+export interface AffectedProjects {
+    affected: string[]
+  }
+
+yargs(process.argv.slice(2))
+.command<AffectedProjects>(
+    ['unicorn-affected'],
+    'Returns true if the affected projects contain a unicorn',
+    (yargs)=>
+      yargs
+        .option('affected', {
+          requiresArg: true,
+          alias: ['affected'],
+          type: 'array',
+          demandOption: true,
+          default: [],
+        }),
+        function(args: AffectedProjects) {console.log(args.affected.some(item => unicornApps.includes(item)));}
+  )
+.parse();
+
+
+
