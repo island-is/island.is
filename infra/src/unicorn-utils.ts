daf9ad0efe284c934114d4a19297a8f831d396d9
andes-it <builders@andes.is>
2024-11-28 08:52:21 +0000

    chore: nx format:write update dirty files

--- infra/src/unicorn-utils.ts
+++ infra/src/unicorn-utils.ts
@@ -1,31 +1,26 @@
-import { argv } from 'process';
-import yargs  from 'yargs';
+import { argv } from 'process'
+import yargs from 'yargs'
 
-const unicornApps = [
- 'unicorn-app'
-]
-
+const unicornApps = ['unicorn-app']
 
 export interface AffectedProjects {
-    affected: string[]
-  }
+  affected: string[]
+}
 
 yargs(process.argv.slice(2))
-.command<AffectedProjects>(
+  .command<AffectedProjects>(
     ['unicorn-affected'],
     'Returns true if the affected projects contain a unicorn',
-    (yargs)=>
-      yargs
-        .option('affected', {
-          requiresArg: true,
-          alias: ['affected'],
-          type: 'array',
-          demandOption: true,
-          default: [],
-        }),
-        function(args: AffectedProjects) {console.log(args.affected.some(item => unicornApps.includes(item)));}
+    (yargs) =>
+      yargs.option('affected', {
+        requiresArg: true,
+        alias: ['affected'],
+        type: 'array',
+        demandOption: true,
+        default: [],
+      }),
+    function (args: AffectedProjects) {
+      console.log(args.affected.some((item) => unicornApps.includes(item)))
+    },
   )
-.parse();
-
-
-
+  .parse()
