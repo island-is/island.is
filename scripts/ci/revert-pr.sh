e512d651adecffa6cb73ae2ef04530b3529670c0
Jón Levy <levy@andes.is>
2024-11-28 07:29:09 +0000

    chore: debug

--- scripts/ci/revert-pr.sh
+++ scripts/ci/revert-pr.sh
@@ -3,6 +3,7 @@
 PR=$1
 read -r NODE_ID TITLE < <(gh api "repos/island-is/island.is/pulls/${PR}" --jq '[.node_id, .title] | @tsv')
 
+# shellcheck disable=SC2016
 gh api graphql -f query='
   mutation ($pr: ID!, $title: String, $body: String) {
     revertPullRequest(
