eaeb0b568ec8dc543fa94440ae3f99bfca335d7f
Jón Levy <levy@andes.is>
2024-11-28 00:06:43 +0000

    ci: auto revert PRs when they break main

---
+++ scripts/ci/revert-pr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PR=$1
+read -r NODE_ID TITLE < <(gh api "repos/island-is/island.is/pulls/${PR}" --jq '[.node_id, .title] | @tsv')
+
+gh api graphql -f query='
+  mutation ($pr: ID!, $title: String, $body: String) {
+    revertPullRequest(
+      input: {
+        title: $title,
+        body: $body,
+        pullRequestId: $pr
+      }
+    ) {
+      revertPullRequest {
+        number
+      }
+    }
+  }' \
+  -f "pr=${NODE_ID}" \
+  -f "title=revert: ⚠️  ${TITLE}" \
+  -f "body=Reverts PR #${PR}" --jq '.data.revertPullRequest.revertPullRequest.number'
