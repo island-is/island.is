f0bfeb15d2d0efa40d217a2c9f3f7365b54a2d13
Eirikur Nilsson <eirikur@nilsson.is>
2024-11-29 16:48:10 +0000

    feat(core): Context support in logger including code owner support

---
+++ libs/logging/src/lib/context.ts
@@ -0,0 +1,33 @@
+import { CodeOwners } from '@island.is/shared/constants'
+import { AsyncLocalStorage } from 'async_hooks'
+import { format } from 'winston'
+
+const loggingContextStorage = new AsyncLocalStorage<Record<string, unknown>>()
+
+/**
+ * Adds context which will be included with all logging inside the callback.
+ */
+export const withLoggingContext = <R, TArgs extends unknown[]>(
+  context: Record<string, unknown> & { codeOwner?: CodeOwners },
+  callback: (...args: TArgs) => R,
+  ...args: TArgs
+): R => {
+  const extendedContext = {
+    ...loggingContextStorage.getStore(),
+    ...context,
+  }
+  return loggingContextStorage.run(extendedContext, callback, ...args)
+}
+
+export const includeContextFormatter = format((info) => {
+  const defaultCodeOwner = process.env.CODE_OWNER
+  const context = loggingContextStorage.getStore()
+
+  if (defaultCodeOwner) {
+    info.codeOwner = defaultCodeOwner
+  }
+  if (context) {
+    Object.assign(info, context)
+  }
+  return info
+})
--- libs/shared/constants/src/index.ts
+++ libs/shared/constants/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/agent'
 export * from './lib/cacheControl'
+export * from './lib/codeOwners'
 export * from './lib/locale'
 export * from './lib/date'
 export * from './lib/chargeItemCode'
