8ff1f2d620617f9225a3302fc8ffb5635dfaeedf
thoreyjona <thoreyjona@gmail.com>
2024-11-29 17:28:50 +0000

    feat: renaming app folder and removing old native folder

--- apps/mobile/app/src/hooks/use-locale.ts
+++
@@ -1,6 +0,0 @@
-import { usePreferencesStore } from '../stores/preferences-store'
-
-export const useLocale = () => {
-  const locale = usePreferencesStore(({ locale }) => locale)
-  return locale === 'is-IS' ? 'is' : 'en'
-}
--- apps/mobile/app/src/lib/lowercase.ts
+++
@@ -1,8 +0,0 @@
-export function lowerCase(str: string): string {
-  const replacements = ['ÁÐÉÍÓÚÝÞÆÖ', 'áðéíóúýþæö']
-  return str
-    .replace(/[ÁÐÉÍÓÚÝÞÆÖ]/g, (m) =>
-      replacements[1].charAt(replacements[0].indexOf(m)),
-    )
-    .toLowerCase()
-}
