e3f87ba12c7b6095374a64f309f1e59ebe7dfa1e
Róberta Andersen <roberta@andes.is>
2024-12-02 15:21:47 +0000

    Unicorn pipe (#17094)
    * chore: Adding pipeline for unicorns
    
    * unicorn pipe
    
    * unicorn pipe
    
    * unicorn pipe
    
    * unicorn pipe
    
    * pipetest
    
    * pipetest
    
    * pipetest
    
    * chore: nx format:write update dirty files
    
    * Update .github/workflows/unicorns.yml
    
    Co-authored-by: Jón Levy <levy@andes.is>
    
    * suggestions
    
    * chore: nx format:write update dirty files
    
    * unicorn pipe
    
    * suggestions
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: Jón Levy <levy@andes.is>

---
+++ scripts/ci/unicorn-utils.mjs
@@ -0,0 +1,16 @@
+import { execSync } from 'child_process'
+import { workspaceRoot } from '@nx/devkit'
+
+const unicornApps = ['unicorn-app']
+
+try {
+  const affected = JSON.parse(
+    execSync(
+      `cd ${workspaceRoot} && yarn nx show projects --affected --base origin/main --json | jq -r`,
+    ).toString(),
+  )
+  console.log(affected.some((item) => unicornApps.includes(item)))
+} catch (e) {
+  console.error(e.message)
+  process.exit(1)
+}
