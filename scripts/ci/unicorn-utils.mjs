627df11e7e7a638dc531d4d41b0f29f0918acca0
andes-it <builders@andes.is>
2024-12-02 13:11:50 +0000

    chore: nx format:write update dirty files

--- scripts/ci/unicorn-utils.mjs
+++ scripts/ci/unicorn-utils.mjs
@@ -5,8 +5,10 @@ const unicornApps = ['unicorn-app']
 
 try {
   const affected = JSON.parse(
-    execSync(`cd ${workspaceRoot} && yarn nx show projects --affected --base origin/main --json | jq -r`)
-      .toString())
+    execSync(
+      `cd ${workspaceRoot} && yarn nx show projects --affected --base origin/main --json | jq -r`,
+    ).toString(),
+  )
   console.log(affected.some((item) => unicornApps.includes(item)))
 } catch (e) {
   console.error(e.message)
