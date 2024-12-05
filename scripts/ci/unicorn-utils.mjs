ab5ff346158b2e4ae54893c0f622c32bb778b093
Róberta Andersen <roberta@andes.is>
2024-12-05 09:46:36 +0000

    unicorn pipe

--- scripts/ci/unicorn-utils.mjs
+++ scripts/ci/unicorn-utils.mjs
@@ -4,12 +4,13 @@ import { workspaceRoot } from '@nx/devkit'
 const unicornApps = ['unicorn-app']
 
 const arg = JSON.parse(process.argv.slice(2))
-console.log(`Comparing nx affected for ${arg.sourceBranch} using ${arg.targetBranch} as base branch`);
+console.log(arg)
+console.log(`Comparing nx affected for ${arg.mergeBranch} using ${arg.baseBranch} as base branch`);
 
 try {
   const affected = JSON.parse(
     execSync(
-      `cd ${workspaceRoot} && yarn nx show projects --affected --base ${arg.targetBranch} --json | jq -r`,
+      `cd ${workspaceRoot} && yarn nx show projects --affected --base ${arg.baseBranch} --json | jq -r`,
     ).toString(),
   )
   console.log(affected.some((item) => unicornApps.includes(item)))
