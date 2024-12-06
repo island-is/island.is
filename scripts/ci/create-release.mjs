cdade5c86d7a2f54d9ca33e320d2f9755346838b
andes-it <builders@andes.is>
2024-12-06 16:46:55 +0000

    chore: nx format:write update dirty files

--- scripts/ci/create-release.mjs
+++ scripts/ci/create-release.mjs
@@ -12,21 +12,24 @@ const { data: pullRequest } = await octokit.rest.pulls.get({
   pull_number: arg[0],
 })
 
-const SHA = pullRequest.head.sha;
+const SHA = pullRequest.head.sha
 // This is a temporary commit that is created behind the scenes for
 // the test merge that validated no conflicts exist with the base branch.
 // It is not committed to the repository.
 // After the PR is merged, this value instead represents the SHA of the merge commit
-octokit.rest.repos.createRelease({
-  owner: owner,
-  repo: repo,
-  target_commitish: SHA,
-  tag_name: "SomeTag",
-  name: "Test 123",
-  generate_release_notes: true,
-}).then(({ data }) => {
-  console.log(data)
-}).catch((error) => {
-  console.log(error)
-  process.exit(1)
-});
+octokit.rest.repos
+  .createRelease({
+    owner: owner,
+    repo: repo,
+    target_commitish: SHA,
+    tag_name: 'SomeTag',
+    name: 'Test 123',
+    generate_release_notes: true,
+  })
+  .then(({ data }) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
