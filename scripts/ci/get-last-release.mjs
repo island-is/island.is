d75f7f65bcf2b1aecd5f626cadfa3feb42f834a6
andes-it <builders@andes.is>
2024-12-06 21:01:55 +0000

    chore: nx format:write update dirty files

--- scripts/ci/get-last-release.mjs
+++ scripts/ci/get-last-release.mjs
@@ -1,18 +1,17 @@
-const releases = process.argv.slice(2)
+const releases = process.argv
+  .slice(2)
   .map((release) => release.replace('origin/release/', ''))
   .filter((release) => /^\d+\.\d+\.\d+$/.test(release))
   .sort((a, b) => {
-    const [aMajor, aMinor, aPatch] = a.split('.').map(Number);
-    const [bMajor, bMinor, bPatch] = b.split('.').map(Number);
+    const [aMajor, aMinor, aPatch] = a.split('.').map(Number)
+    const [bMajor, bMinor, bPatch] = b.split('.').map(Number)
 
     if (aMajor !== bMajor) {
-      return bMajor - aMajor;
+      return bMajor - aMajor
     } else if (aMinor !== bMinor) {
-      return bMinor - aMinor;
+      return bMinor - aMinor
     } else {
-      return bPatch - aPatch;
+      return bPatch - aPatch
     }
   })
 console.log(releases[0])
-
-
