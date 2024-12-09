84deac6934a889e0248b478f582d28fec42d168a
HjorturJ <hjorturjohann94@gmail.com>
2024-12-09 13:16:55 +0000

    Reverting test changes

--- libs/application/templates/directorate-of-immigration/citizenship/src/forms/CitizenshipForm/index.ts
+++ libs/application/templates/directorate-of-immigration/citizenship/src/forms/CitizenshipForm/index.ts
@@ -1,11 +1,4 @@
-import {
-  buildDescriptionField,
-  buildForm,
-  buildMultiField,
-  buildRadioField,
-  buildSection,
-  getValueViaPath,
-} from '@island.is/application/core'
+import { buildForm, buildSection } from '@island.is/application/core'
 import { Form, FormModes, Section } from '@island.is/application/types'
 import { confirmation, externalData, payment } from '../../lib/messages'
 import { InformationSection } from './InformationSection'
@@ -37,61 +30,7 @@ export const CitizenshipForm: Form = buildForm({
       title: externalData.dataProvider.sectionTitle,
       children: [],
     }),
-    buildSection({
-      id: 'NeverGetToMe',
-      title: 'Never get to me',
-      condition: (_answers) => false,
-      children: [
-        buildDescriptionField({
-          id: `neverBeGottenTo`,
-          title: 'I should never be gotten to',
-          titleVariant: 'h5',
-        }),
-      ],
-    }),
     PersonalSection,
-    buildSection({
-      id: 'MultiSection',
-      title: 'Answer test',
-      children: [
-        buildMultiField({
-          id: 'blabla',
-          title: 'Mega title',
-          description: 'something',
-          children: [
-            buildDescriptionField({
-              id: `huehuehue`,
-              title: 'Answer me',
-              titleVariant: 'h5',
-            }),
-            buildRadioField({
-              id: 'somethingToBeAnswered',
-              title: '',
-              backgroundColor: 'blue',
-              width: 'half',
-              required: true,
-              options: [
-                { value: 'yes', label: 'Ye' },
-                { value: 'no', label: 'Naw' },
-              ],
-            }),
-          ],
-        }),
-      ],
-    }),
-    buildSection({
-      id: 'OnlyWhenYesWasAnswered',
-      title: 'OnlyWhenYes',
-      condition: (formValue) =>
-        getValueViaPath(formValue, 'somethingToBeAnswered') === 'yes',
-      children: [
-        buildDescriptionField({
-          id: `haha`,
-          title: 'Answered!!!!',
-          titleVariant: 'h5',
-        }),
-      ],
-    }),
     InformationSection,
     SupportingDocumentsSection,
     ...buildSupportingDocumentsSections(),
