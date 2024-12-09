78ea814ef952ba6f8f8f1468957a7a123a6a1d78
Jonni <jonas@norda.is>
2024-12-09 17:40:35 +0000

    chore: safer handling of conversion to number

--- libs/application/templates/inao/financial-statement-cemetery/src/utils/helpers.ts
+++ libs/application/templates/inao/financial-statement-cemetery/src/utils/helpers.ts
@@ -140,22 +140,17 @@ export const isPositiveNumberInString = (input: string) => {
 }
 
 export const sumIncome = (answers: FormValue) => {
-  const careIncome = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.careIncome,
-  )
-  const burialRevenue = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.burialRevenue,
-  )
-  const grantFromTheCemeteryFund = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.grantFromTheCemeteryFund,
-  )
-  const otherIncome = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.otherIncome,
-  )
+  const careIncome =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.careIncome) || '0'
+  const burialRevenue =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.burialRevenue) || '0'
+  const grantFromTheCemeteryFund =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYOPERATIONIDS.grantFromTheCemeteryFund,
+    ) || '0'
+  const otherIncome =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.otherIncome) || '0'
 
   return `${
     Number(careIncome) +
@@ -167,30 +162,23 @@ export const sumIncome = (answers: FormValue) => {
 
 export const sumExpenses = (answers: FormValue) => {
   const payroll = getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.payroll)
-  const funeralCost = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.funeralCost,
-  )
-  const chapelExpense = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.chapelExpense,
-  )
-  const donationsToCemeteryFund = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.donationsToCemeteryFund,
-  )
-  const donationsToOther = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.donationsToOther,
-  )
-  const otherOperationCost = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.otherOperationCost,
-  )
-  const depreciation = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.depreciation,
-  )
+  const funeralCost =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.funeralCost) || '0'
+  const chapelExpense =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.chapelExpense) || '0'
+  const donationsToCemeteryFund =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYOPERATIONIDS.donationsToCemeteryFund,
+    ) || '0'
+  const donationsToOther =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.donationsToOther) ||
+    '0'
+  const otherOperationCost =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.otherOperationCost) ||
+    '0'
+  const depreciation =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.depreciation) || '0'
 
   return `${
     Number(payroll) +
@@ -204,80 +192,79 @@ export const sumExpenses = (answers: FormValue) => {
 }
 
 export const sumOperatingResults = (answers: FormValue) => {
-  const income = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.totalIncome,
-  )
-  const expenses = getValueViaPath<string>(
-    answers,
-    CEMETERYOPERATIONIDS.totalExpense,
-  )
+  const income =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.totalIncome) || '0'
+  const expenses =
+    getValueViaPath<string>(answers, CEMETERYOPERATIONIDS.totalExpense) || '0'
 
   return `${Number(income) - Number(expenses)}`
 }
 
 export const sumCapitalNumbers = (answers: FormValue) => {
-  const capitalIncome = getValueViaPath<string>(
-    answers,
-    CAPITALNUMBERS.capitalIncome,
-  )
-  const capitalCost = getValueViaPath<string>(
-    answers,
-    CAPITALNUMBERS.capitalCost,
-  )
+  const capitalIncome =
+    getValueViaPath<string>(answers, CAPITALNUMBERS.capitalIncome) || '0'
+  const capitalCost =
+    getValueViaPath<string>(answers, CAPITALNUMBERS.capitalCost) || '0'
   return `${Number(capitalIncome) - Number(capitalCost)}`
 }
 
 export const sumAssets = (answers: FormValue) => {
-  const fixedAssetsTotal = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.fixedAssetsTotal,
-  )
-  const currentAssets = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.currentAssets,
-  )
+  const fixedAssetsTotal =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.fixedAssetsTotal,
+    ) || '0'
+  const currentAssets =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.currentAssets,
+    ) || '0'
   return `${Number(fixedAssetsTotal) + Number(currentAssets)}`
 }
 
 export const sumLiabilities = (answers: FormValue) => {
-  const longTerm = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.longTerm,
-  )
-  const shortTerm = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.shortTerm,
-  )
+  const longTerm =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.longTerm,
+    ) || '0'
+  const shortTerm =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.shortTerm,
+    ) || '0'
   return `${Number(longTerm) + Number(shortTerm)}`
 }
 
 export const operationResult = (answers: FormValue) => {
-  const operatingTotalCost = getValueViaPath<string>(
-    answers,
-    OPERATINGCOST.total,
-  )
-  const capitalTotal = getValueViaPath<string>(answers, CAPITALNUMBERS.total)
+  const operatingTotalCost =
+    getValueViaPath<string>(answers, OPERATINGCOST.total) || '0'
+  const capitalTotal =
+    getValueViaPath<string>(answers, CAPITALNUMBERS.total) || '0'
   return `${Number(operatingTotalCost) + Number(capitalTotal)}`
 }
 
 export const sumTotalEquity = (answers: FormValue) => {
-  const equityAtTheBeginningOfTheYear = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.equityAtTheBeginningOfTheYear,
-  )
-  const revaluationDueToPriceChanges = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.revaluationDueToPriceChanges,
-  )
-  const reevaluateOther = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.reevaluateOther,
-  )
-  const operationResult = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.operationResult,
-  )
+  const equityAtTheBeginningOfTheYear =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.equityAtTheBeginningOfTheYear,
+    ) || '0'
+  const revaluationDueToPriceChanges =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.revaluationDueToPriceChanges,
+    ) || '0'
+  const reevaluateOther =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.reevaluateOther,
+    ) || '0'
+  const operationResult =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.operationResult,
+    ) || '0'
 
   return `${
     Number(equityAtTheBeginningOfTheYear) +
@@ -288,56 +275,65 @@ export const sumTotalEquity = (answers: FormValue) => {
 }
 
 export const sumTotalEquityAndLiabilities = (answers: FormValue) => {
-  const liabilityTotal = getValueViaPath<string>(
-    answers,
-    EQUITYANDLIABILITIESTOTALS.liabilitiesTotal,
-  )
-  const totalEquity = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.equityTotal,
-  )
+  const liabilityTotal =
+    getValueViaPath<string>(
+      answers,
+      EQUITYANDLIABILITIESTOTALS.liabilitiesTotal,
+    ) || '0'
+  const totalEquity =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.equityTotal,
+    ) || '0'
 
   return `${Number(totalEquity) + Number(liabilityTotal)}`
 }
 
 export const showEquitiesAndLiabilitiesAlert = (answers: FormValue) => {
-  const fixedAssetsTotal = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.fixedAssetsTotal,
-  )
-  const currentAssets = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.currentAssets,
-  )
-  const longTerm = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.longTerm,
-  )
-  const shortTerm = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.shortTerm,
-  )
-  const equityAtTheBeginningOfTheYear = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.equityAtTheBeginningOfTheYear,
-  )
-  const revaluationDueToPriceChanges = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.revaluationDueToPriceChanges,
-  )
-  const reevaluateOther = getValueViaPath<string>(
-    answers,
-    CEMETERYEQUITIESANDLIABILITIESIDS.reevaluateOther,
-  )
+  const fixedAssetsTotal =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.fixedAssetsTotal,
+    ) || '0'
+  const currentAssets =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.currentAssets,
+    ) || '0'
+  const longTerm =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.longTerm,
+    ) || '0'
+  const shortTerm =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.shortTerm,
+    ) || '0'
+  const equityAtTheBeginningOfTheYear =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.equityAtTheBeginningOfTheYear,
+    ) || '0'
+  const revaluationDueToPriceChanges =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.revaluationDueToPriceChanges,
+    ) || '0'
+  const reevaluateOther =
+    getValueViaPath<string>(
+      answers,
+      CEMETERYEQUITIESANDLIABILITIESIDS.reevaluateOther,
+    ) || '0'
 
-  const totalAssets = getValueViaPath<string>(
-    answers,
-    EQUITYANDLIABILITIESTOTALS.assetsTotal,
-  )
-  const totalEquityAndLiabilities = getValueViaPath<string>(
-    answers,
-    EQUITYANDLIABILITIESTOTALS.equityAndLiabilitiesTotal,
-  )
+  const totalAssets =
+    getValueViaPath<string>(answers, EQUITYANDLIABILITIESTOTALS.assetsTotal) ||
+    '0'
+  const totalEquityAndLiabilities =
+    getValueViaPath<string>(
+      answers,
+      EQUITYANDLIABILITIESTOTALS.equityAndLiabilitiesTotal,
+    ) || '0'
 
   if (
     !fixedAssetsTotal ||
