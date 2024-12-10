1ebd6719163fca2ac571d7d7b68184f9a58119e6
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2024-12-10 11:27:11 +0000

    feat: Add codeOwner to logging context (#17172)
    * feat: Add codeOwner to logging context
    
    * Fix tests
    
    * fix: Silence warning in development

--- libs/infra-tracing/src/lib/code-owner.ts
+++ libs/infra-tracing/src/lib/code-owner.ts
@@ -1,22 +1,26 @@
-import { logger } from '@island.is/logging'
+import { logger, withLoggingContext } from '@island.is/logging'
 import { CodeOwners } from '@island.is/shared/constants'
 import tracer from 'dd-trace'
 
 /**
- * Sets a code owner for the current dd-trace span.
+ * Sets a code owner for the current dd-trace span and all nested log entries.
  *
  * The assumption here is that each trace / request has only one "dynamic"
  * code owner. This way we skip cluttering the trace with extra spans.
  */
-export const setCodeOwner = (codeOwner: CodeOwners) => {
+export const withCodeOwner = <R, TArgs extends unknown[]>(
+  codeOwner: CodeOwners,
+  callback: (...args: TArgs) => R,
+  ...args: TArgs
+) => {
   const span = tracer.scope().active()
   if (span) {
     span.setTag('codeOwner', codeOwner)
-  } else {
-    const stack = new Error().stack
+  } else if (process.env.NODE_ENV !== 'development') {
     logger.warn(
       `Setting code owner "${codeOwner}" with no active dd-trace span`,
-      { stack },
     )
   }
+
+  return withLoggingContext({ codeOwner }, callback, ...args)
 }
