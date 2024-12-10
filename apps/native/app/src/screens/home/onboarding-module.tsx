870b6888657d6025753bab61aa8d90bec3aed18b
Þórey Jóna <thoreyjona@gmail.com>
2024-12-10 15:07:29 +0000

    feat(native-app): Move app to nx (#17098)
    * feat: init nx create app stuff
    
    * feat: copy src folder from app project
    
    * feat: add dependencies
    
    * feat: updating podFile
    
    * feat copy ios folder from older project
    
    * feat: copy android folder
    
    * feat: final fixes for ios app working locally
    
    * feat: remove old src folder
    
    * feat: add readme
    
    * feat: add storybook folder
    
    * feat: add back test stuff in
    
    * feat: add more stuff from old project
    
    * feat: add all scripts to package.json
    
    * feat: update packages that still have * to proper version
    
    * feat: final additions
    
    * feat: renaming app folder and removing old native folder
    
    * fix: remove mobile from workspaces
    
    * fix: remove extra extension
    
    * feat: add back root level read me
    
    * feat: add prettier and bundle config files
    
    * remove google-services.json file
    
    * feat: update gitignore file
    
    * fix: path for e2e project.json
    
    * chore: bump version to 1.4.8
    
    * remove cache: true from nx.json
    
    * add tags to project.json
    
    * feat: fix yarn.lock
    
    * fix: valid json in nx.json
    
    * fix: remove app-e2e folder
    
    * fix: update package.json
    
    * feat: add license to package.json in app
    
    * remove private: true to fix license check
    
    * chore: nx format:write update dirty files
    
    * fix: remove dom from lib in tsconfig.json
    
    * update entryFilein project.json
    
    * fix: update settings.gradle after android build failing
    
    * fix: update import from build.gradle
    
    * feat: use relative import for ui
    
    * fix: remove duplicates from gitignore
    
    * fix: remove from tsconfig things that are already inherited
    
    * chore: remove babel-plugin-module-resolver
    
    * remove webpack.config.js
    
    * fix: update package.json to have same version as package.json in app
    
    * fix: update yarn.lock
    
    * fix: linting
    
    * chore: update react version to match root project
    
    * chore: update Podfile
    
    * chore: remove patch for old version of rn
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/native/app/src/screens/home/onboarding-module.tsx
+++ apps/native/app/src/screens/home/onboarding-module.tsx
@@ -1,13 +1,13 @@
-import { Close, Heading, ViewPager, WelcomeCard } from '@ui'
 import React from 'react'
 import { SafeAreaView, TouchableOpacity } from 'react-native'
 import { useTheme } from 'styled-components/native'
+import { useIntl } from 'react-intl'
+
+import { Close, Heading, ViewPager, WelcomeCard } from '../../ui'
 import illustration1 from '../../assets/illustrations/digital-services-m3.png'
 import illustration3 from '../../assets/illustrations/le-company-s2.png'
 import illustration2 from '../../assets/illustrations/le-retirement-s3-large.png'
 import illustration4 from '../../assets/illustrations/le_jobs_s5.png'
-
-import { useIntl } from 'react-intl'
 import { useAuthStore } from '../../stores/auth-store'
 import { usePreferencesStore } from '../../stores/preferences-store'
 
