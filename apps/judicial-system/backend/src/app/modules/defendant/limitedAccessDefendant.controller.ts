768de8b40879f559712a48de9b6b63ab89077834
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2024-12-11 11:38:41 +0000

    fix(j-s): formatting

--- apps/judicial-system/backend/src/app/modules/defendant/limitedAccessDefendant.controller.ts
+++ apps/judicial-system/backend/src/app/modules/defendant/limitedAccessDefendant.controller.ts
@@ -23,7 +23,7 @@ import { CurrentDefendant } from './guards/defendant.decorator'
 import { DefendantExistsGuard } from './guards/defendantExists.guard'
 import { Defendant } from './models/defendant.model'
 import type { Logger } from '@island.is/logging'
-import { prisonSystemStaffUpdateRule } from "./guards/rolesRules"
+import { prisonSystemStaffUpdateRule } from './guards/rolesRules'
 
 interface LimitedAccessUpdateDefendant
   extends Pick<UpdateDefendantDto, 'punishmentType'> {}
