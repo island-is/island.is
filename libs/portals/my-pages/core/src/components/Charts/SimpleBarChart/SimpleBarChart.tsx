2d88d053c0a32aacb433edbd10eb1f880982d262
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-12-12 15:22:55 +0000

    chore: imports

--- libs/portals/my-pages/core/src/components/Charts/SimpleBarChart/SimpleBarChart.tsx
+++ libs/portals/my-pages/core/src/components/Charts/SimpleBarChart/SimpleBarChart.tsx
@@ -17,8 +17,6 @@ import {
 import { Box } from '@island.is/island-ui/core'
 import * as styles from './styles.css'
 import { theme } from '@island.is/island-ui/theme'
-import cn from 'classnames'
-import { useWindowSize } from 'react-use'
 
 interface Axis {
   label?: string
