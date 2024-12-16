96c688f2a3d300d5e08eb1944e429035258f79e9
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-12-16 16:33:46 +0000

    feat: works a bit

--- apps/web/components/Organization/Slice/GrantCardsList/GrantCardsListSlice.tsx
+++ apps/web/components/Organization/Slice/GrantCardsList/GrantCardsListSlice.tsx
@@ -1,31 +1,36 @@
+import format from 'date-fns/format'
 import { useQuery } from '@apollo/client'
 
-import { Box, LinkV2, Text } from '@island.is/island-ui/core'
+import { Box, InfoCardGrid } from '@island.is/island-ui/core'
+import { isDefined } from '@island.is/shared/utils'
 import {
   ContentLanguage,
   GrantCardsList,
+  GrantStatus,
   Query,
 } from '@island.is/web/graphql/schema'
-import { LinkType, useLinkResolver } from '@island.is/web/hooks'
+import { useLinkResolver, useNamespace } from '@island.is/web/hooks'
 import { useI18n } from '@island.is/web/i18n'
 import { GET_GRANTS_QUERY } from '@island.is/web/screens/queries'
 
-import * as styles from './GrantCardsListSlice.css'
-
 interface SliceProps {
   slice: GrantCardsList
+  namespace?: Record<string, string>
 }
 
-export const GrantCardsListSlice = ({ slice }: SliceProps) => {
+const generateStatus = (grant: GrantStatus) => 'Opið fyrir umsóknir'
+
+export const GrantCardsListSlice = ({ slice, namespace }: SliceProps) => {
   const { activeLocale } = useI18n()
   const { linkResolver } = useLinkResolver()
+  const translations = useNamespace(namespace)
 
-  const { data, loading, error } = useQuery<Query>(GET_GRANTS_QUERY, {
+  const { data } = useQuery<Query>(GET_GRANTS_QUERY, {
     variables: {
       input: {
         lang: activeLocale as ContentLanguage,
-        size: slice.maxNumberOfCards ?? 4,
-        funds: slice.funds ? slice.funds.map((f) => f.id).join(',') : undefined,
+        size: slice.maxNumberOfCards ?? 50,
+        //funds: slice.funds ? slice.funds.map((f) => f.id).join(',') : undefined,
       },
     },
   })
@@ -34,25 +39,67 @@ export const GrantCardsListSlice = ({ slice }: SliceProps) => {
     return undefined
   }
 
-  return (
-    <Box component="ul" className={styles.unorderedList}>
-      {slice.funds.map((fund) => {
-        if (!fund.link) {
-          return null
+  const cards = data?.getGrants?.items
+    ?.map((grant) => {
+      if (grant.id) {
+        return {
+          id: grant.id,
+          eyebrow: grant.fund?.title ?? grant.name ?? '',
+          subEyebrow: grant.fund?.parentOrganization?.title,
+          title: grant.name ?? '',
+          description: grant.description ?? '',
+          logo:
+            grant.fund?.featuredImage?.url ??
+            grant.fund?.parentOrganization?.logo?.url ??
+            '',
+          logoAlt:
+            grant.fund?.featuredImage?.title ??
+            grant.fund?.parentOrganization?.logo?.title ??
+            '',
+          link: {
+            label: 'Skoða nánar',
+            href: linkResolver(
+              'styrkjatorggrant',
+              [grant?.applicationId ?? ''],
+              activeLocale,
+            ).href,
+          },
+          detailLines: [
+            grant.dateFrom && grant.dateTo
+              ? {
+                  icon: 'calendar' as const,
+                  text: `${format(
+                    new Date(grant.dateFrom),
+                    'dd.MM.yyyy',
+                  )} - ${format(new Date(grant.dateTo), 'dd.MM.yyyy')}`,
+                }
+              : null,
+            grant.status
+              ? {
+                  icon: 'time' as const,
+                  //todo: fix when the text is ready
+                  text: generateStatus(grant?.status),
+                }
+              : undefined,
+            grant.categoryTags
+              ? {
+                  icon: 'informationCircle' as const,
+                  text: grant.categoryTags
+                    .map((ct) => ct.title)
+                    .filter(isDefined)
+                    .join(', '),
+                }
+              : undefined,
+          ].filter(isDefined),
         }
-        return (
-          <Box component="li" className={styles.listItem}>
-            <LinkV2
-              color="blue400"
-              {...linkResolver(fund.link?.type as LinkType, [
-                fund.link.slug ?? '',
-              ])}
-            >
-              {fund.title}
-            </LinkV2>
-          </Box>
-        )
-      })}
+      }
+      return null
+    })
+    .filter(isDefined)
+
+  return (
+    <Box padding={1} borderColor="blue100" borderRadius="large">
+      <InfoCardGrid columns={1} variant="detailed" cards={cards ?? []} />
     </Box>
   )
 }
