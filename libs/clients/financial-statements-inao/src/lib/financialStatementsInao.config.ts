f567a35ec1b395837474a0bfd3357f18086bd0c4
Jónas G. Sigurðsson <jonas@norda.is>
2024-12-16 12:55:10 +0000

    fix(inao-cem): remove custom components that cause problems (#17244)
    * chore: houskeeping, split up files and make code more radable
    
    * chore: refactor out custom components
    
    * chore: add loading state
    
    * chore: simplify logic
    
    * fix: coderabbit comments
    
    * chore: undo codeowners
    
    * chore: use correct path to fetch

--- libs/clients/financial-statements-inao/src/lib/financialStatementsInao.config.ts
+++ libs/clients/financial-statements-inao/src/lib/financialStatementsInao.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from '@island.is/nest/config'
+import { isRunningOnEnvironment } from '@island.is/shared/utils'
 
+const isLocal = isRunningOnEnvironment('local')
 export const FinancialStatementsInaoClientConfig = defineConfig({
   name: 'DataverseClient',
   load: (env) => ({
     basePath: env.required(
       'FINANCIAL_STATEMENTS_INAO_BASE_PATH',
-      'https://dev-re.crm4.dynamics.com/api/data/v9.1',
+      isLocal
+        ? 'https://dev-re.crm4.dynamics.com/api/data/v9.1'
+        : 'https://star-re.crm4.dynamics.com/api/data/v9.1',
     ),
     issuer: env.required(
       'FINANCIAL_STATEMENTS_INAO_ISSUER',
@@ -13,7 +17,9 @@ export const FinancialStatementsInaoClientConfig = defineConfig({
     ),
     scope: env.required(
       'FINANCIAL_STATEMENTS_INAO_SCOPE',
-      'https://dev-re.crm4.dynamics.com/.default',
+      isLocal
+        ? 'https://dev-re.crm4.dynamics.com/.default'
+        : 'https://star-re.crm4.dynamics.com/.default',
     ),
     tokenEndpoint: env.required(
       'FINANCIAL_STATEMENTS_INAO_TOKEN_ENDPOINT',
