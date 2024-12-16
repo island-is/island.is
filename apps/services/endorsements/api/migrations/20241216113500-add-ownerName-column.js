26c89b56d0611b4669668606e44ff1236924ce84
Rafn Árnason <rafnarnason@gmail.com>
2024-12-16 11:52:22 +0000

    ownername db and logic

---
+++ apps/services/endorsements/api/migrations/20241216113500-add-ownerName-column.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Endorsements', 'ownerName', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Endorsements', 'ownerName');
+  },
+};
