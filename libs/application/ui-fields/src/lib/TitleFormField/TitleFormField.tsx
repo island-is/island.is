611a057366e7b33281b64d5238546099fe8b0e22
andes-it <builders@andes.is>
2024-12-17 17:19:15 +0000

    chore: nx format:write update dirty files

--- libs/application/ui-fields/src/lib/TitleFormField/TitleFormField.tsx
+++ libs/application/ui-fields/src/lib/TitleFormField/TitleFormField.tsx
@@ -13,18 +13,24 @@ export const TitleFormField: FC<
   }>
 > = ({ field, application }) => {
   const { formatMessage, lang: locale } = useLocale()
-  const { title, titleVariant = 'h5', color = 'blue400', marginTop = 5, marginBottom = 1 } = field
+  const {
+    title,
+    titleVariant = 'h5',
+    color = 'blue400',
+    marginTop = 5,
+    marginBottom = 1,
+  } = field
 
   return (
     <Box marginTop={marginTop} marginBottom={marginBottom}>
-        <Text variant={titleVariant} color={color ?? 'blue400'}>
-            {formatTextWithLocale(
-            title,
-            application,
-            locale as Locale,
-            formatMessage,
-            )}
-        </Text>
+      <Text variant={titleVariant} color={color ?? 'blue400'}>
+        {formatTextWithLocale(
+          title,
+          application,
+          locale as Locale,
+          formatMessage,
+        )}
+      </Text>
     </Box>
   )
 }
