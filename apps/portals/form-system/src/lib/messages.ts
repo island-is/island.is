fbc9698158e4a81dd061011b8a896538aa47bde7
Hyosam <hyo-sam.nandkisore@advania.is>
2024-12-19 11:35:55 +0000

    removed form-system application from apps/portals/

--- apps/portals/form-system/src/lib/messages.ts
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl'
-
-export const m = defineMessages({
-  loginMessage: {
-    id: 'form.system:loginLoading',
-    defaultMessage: 'Er að vinna í innskráningu',
-  },
-})
