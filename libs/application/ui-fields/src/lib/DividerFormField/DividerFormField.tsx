e119ff6c61e548c6986310695bf903c9059f2e4a
HjorturJ <hjorturjohann94@gmail.com>
2024-12-19 10:17:50 +0000

    Update DividerFormField.tsx

--- libs/application/ui-fields/src/lib/DividerFormField/DividerFormField.tsx
+++ libs/application/ui-fields/src/lib/DividerFormField/DividerFormField.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react'
 
-import { DividerField, Application } from '@island.is/application/types'
+import { DividerField } from '@island.is/application/types'
 import { Box, Divider } from '@island.is/island-ui/core'
 
 export const DividerFormField: FC<
   React.PropsWithChildren<{
     field: DividerField
-    application: Application
   }>
 > = ({ field }) => {
   const { useDividerLine, marginTop, marginBottom } = field
