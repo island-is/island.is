e707339e2e3bf9f3b37a6f4fbbacaf10b01248c3
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2024-12-20 10:18:33 +0000

    fix: add asterisk for some values*

--- libs/portals/my-pages/social-insurance-maintenance/src/screens/PaymentPlan/PaymentPlan.tsx
+++ libs/portals/my-pages/social-insurance-maintenance/src/screens/PaymentPlan/PaymentPlan.tsx
@@ -1,10 +1,4 @@
-import {
-  AlertMessage,
-  Box,
-  Divider,
-  Stack,
-  Text,
-} from '@island.is/island-ui/core'
+import { Box, Divider, Stack, Text } from '@island.is/island-ui/core'
 import { useLocale, useNamespaces } from '@island.is/localization'
 import { Problem } from '@island.is/react-spa/shared'
 import {
