b8b336e38214b154dc868b57abe7578601ef46c2
Róberta Andersen <roberta@andes.is>
2024-12-20 11:12:38 +0000

    chore: testing mergequeue (#17317)

--- apps/unicorn-app/src/app/page.tsx
+++ apps/unicorn-app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span role="img" aria-label="unicorn">
-                Welcome unicorn 🦄 Q-Test2
+                Welcome unicorn 🦄 Q-Test 6
               </span>
             </h1>
           </div>
