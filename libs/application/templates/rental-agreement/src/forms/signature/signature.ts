22f79d4c74892582c0648e6b7b36596f732a5462
Heba <heba.farestveit@gmail.com>
2025-01-06 10:43:22 +0000

    Add info about the signing process and list of signees

--- libs/application/templates/rental-agreement/src/forms/signature/signature.ts
+++ libs/application/templates/rental-agreement/src/forms/signature/signature.ts
@@ -1,9 +1,13 @@
 import {
+  buildActionCardListField,
   buildDescriptionField,
   buildMultiField,
   buildSection,
+  buildStaticTableField,
 } from '@island.is/application/core'
 import { signature } from '../../lib/messages'
+import { RentalAgreement } from '../../lib/dataSchema'
+import { formatNationalId, formatPhoneNumber } from '../../lib/utils'
 
 export const Signature = buildSection({
   id: 'signature',
@@ -11,13 +15,71 @@ export const Signature = buildSection({
   children: [
     buildMultiField({
       id: 'signature',
-      title: signature.pageTitle,
+      title: signature.sectionName,
       description: signature.pageDescription,
+      nextButtonText: '',
       children: [
         buildDescriptionField({
-          id: 'signature.description',
-          title: 'signature.signatureDescription',
+          id: 'signature.info',
+          title: signature.infoHeading,
+          description: signature.infoBullets,
           titleVariant: 'h3',
+          space: 0,
+        }),
+        buildStaticTableField({
+          title: signature.tableTitle,
+          marginTop: 6,
+          header: [
+            signature.tableHeaderName,
+            signature.tableHeaderId,
+            signature.tableHeaderPhone,
+            signature.tableHeaderEmail,
+          ],
+          rows: (application) => {
+            const { landlordInfo, tenantInfo } =
+              application.answers as RentalAgreement
+
+            const filterLandlords = landlordInfo.table.filter(
+              (landlord) => landlord.isRepresentative?.length === 0,
+            )
+
+            const filterTenants = tenantInfo.table.filter(
+              (tenant) => tenant.isRepresentative?.length === 0,
+            )
+
+            const signees = [...filterLandlords, ...filterTenants]
+
+            return signees.map((person) => [
+              person.nationalIdWithName.name ?? '',
+              formatNationalId(person.nationalIdWithName.nationalId) ?? '',
+              formatPhoneNumber(person.phone as string) ?? '',
+              person.email ?? '',
+            ])
+          },
+        }),
+        buildActionCardListField({
+          id: 'actionCardList',
+          title: 'Action cards with buttons',
+          marginTop: 12,
+          items: () => {
+            return [
+              {
+                backgroundColor: 'blue',
+                heading: 'Undirrita leigusamning',
+                headingVariant: 'h3',
+                buttonType: 'primary',
+                cta: {
+                  label: 'Senda í undirritun',
+                  variant: 'primary',
+                  icon: 'pencil',
+                  onClick: () => {
+                    // TODO: Create function to open modal with button to Taktikal signing
+                    console.log('Should open modal with signing button')
+                  },
+                },
+              },
+            ]
+          },
         }),
       ],
     }),
--- libs/application/templates/rental-agreement/src/lib/messages/signature.ts
+++ libs/application/templates/rental-agreement/src/lib/messages/signature.ts
@@ -2,19 +2,50 @@ import { defineMessages } from 'react-intl'
 
 export const signature = defineMessages({
   sectionName: {
-    id: 'ra.application:summary.sectionName',
+    id: 'ra.application:signture.sectionName',
     defaultMessage: 'Undirritun',
-    description: 'Name of the summary section',
-  },
-  pageTitle: {
-    id: 'ra.application:summary.pageTitle',
-    defaultMessage: 'Undirritun',
-    description: 'Title of the summary page',
+    description: 'Name of the signature section',
   },
   pageDescription: {
-    id: 'ra.application:summary.pageDescription',
+    id: 'ra.application:signture.pageDescription',
     defaultMessage:
       'Þegar samningur er sendur í undirritun fá aðilar samnings SMS og tölvupóst frá HMS með beiðni um rafræna undirritun. Þegar allir aðilar hafa undirritað er samningurinn fullkláraður og skráist sjálfkrafa í leiguskrá HMS.',
-    description: 'Description of the summary page',
+    description: 'Description of the signature page',
+  },
+  infoHeading: {
+    id: 'ra.application:signture.pageInfoHeading',
+    defaultMessage: 'Gott að vita',
+    description: 'Heading for the signature page info',
+  },
+  infoBullets: {
+    id: 'ra.application:signture.pageInfoBullets#markdown',
+    defaultMessage:
+      '- Ekki er hægt að gera breytingar á samningi eftir að búið er að senda í undirritun \n- Ef nauðsynlegt þykir að gera breytingu þarf að útbúa nýjan samning \n- Samningur tekur ekki gildi fyrr en allir aðilar samnings hafa undirritað',
+    description: 'Bullets for the signature page info',
+  },
+  tableTitle: {
+    id: 'ra.application:signture.tableTitle',
+    defaultMessage: 'Aðilar sem undirrita',
+    description: 'Title for the signature table',
+  },
+  tableHeaderName: {
+    id: 'ra.application:signture.tableHeaderName',
+    defaultMessage: 'Fullt nafn',
+    description: 'Header for the name column in signature table',
+  },
+  tableHeaderId: {
+    id: 'ra.application:signture.tableHeaderId',
+    defaultMessage: 'Kennitala',
+    description: 'Header for the ID column in signature table',
+  },
+  tableHeaderPhone: {
+    id: 'ra.application:signture.tableHeaderPhone',
+    defaultMessage: 'Símanúmer',
+    description: 'Header for the phone column in signature table',
+  },
+  tableHeaderEmail: {
+    id: 'ra.application:signture.tableHeaderEmail',
+    defaultMessage: 'Netfang',
+    description: 'Header for the email column in signature table',
   },
 })
