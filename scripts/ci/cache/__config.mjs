bb851f7bee11a09cc2aed90b111d5b015f87d0f5
Kristofer <kristofer@andes.is>
2025-01-07 15:45:26 +0000

    Only override cypress cache when no env variable is set

--- scripts/ci/cache/__config.mjs
+++ scripts/ci/cache/__config.mjs
@@ -33,7 +33,8 @@ export const ENABLED_MODULES = (process.env[ENV_ENABLED_CACHE] || '')
     return a
   }, {})
 
-export const cypressPath = '/github/home/.cypress-cache'
+export const cypressPath =
+  process.env.CYPRESS_CACHE_FOLDER || `${process.env.HOME}/.cypress-cache`
 export const cacheSuccess = JSON.parse(process.env[ENV_CACHE_SUCCESS] ?? '{}')
 export const initCache = process.env[ENV_INIT_CACHE] === 'true'
 
