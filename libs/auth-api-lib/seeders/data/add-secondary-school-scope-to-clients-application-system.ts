a3a22b1e383c0438fe6c7bce3c27f9d63ec15ab5
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-01-09 10:38:20 -0500

    Also add scope to @island.is/clients/api

--- libs/auth-api-lib/seeders/data/add-secondary-school-scope-to-clients-application-system.ts
+++ libs/auth-api-lib/seeders/data/add-secondary-school-scope-to-clients-application-system.ts
@@ -2,6 +2,10 @@ import { addScopesToClient, compose } from './helpers'
 
 export const up = compose(
   addScopesToClient({
+    clientId: '@island.is/clients/api',
+    scopeNames: ['@mms.is/framhaldsskolaumsokn'],
+  }),
+  addScopesToClient({
     clientId: '@island.is/clients/application-system',
     scopeNames: ['@mms.is/framhaldsskolaumsokn'],
   }),
