e1cdc36661b89ee3f6d7aae620c1c46216936698
Kristofer <kristofer@andes.is>
2025-01-16 16:48:14 +0000

    Add flag support for git-check-dirty.sh

--- infra/scripts/ci/git-check-dirty.sh
+++ infra/scripts/ci/git-check-dirty.sh
@@ -1,11 +1,71 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euxo pipefail
 
 DIR="$(git rev-parse --show-toplevel)"
-rel_path=$1
-abs_path=$DIR/$rel_path
-action=$2
-owner="${3:-github actions}"
+
+# Defaults
+owner="github actions"
+rel_path=""
+actions=()
+positional_args=()
+
+show_help() {
+  cat <<EOF
+Usage: $0 [OPTIONS] [<rel_path> <action> [<owner>]]
+
+Options:
+  -p, --path <path>        Path to track changes on
+  -a, --action <action>    Action(s) to be performed (can be repeated)
+  -o, --owner <owner>      Owner name (default: "github actions")
+  -h, --help               Show this help message
+
+Example usage:
+  $0 my/path someAction dirtybot
+  $0 --path my/path --action someAction --action anotherAction --owner dirtybot
+EOF
+  exit 0
+}
+
+# Parse flags
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  -p | --path)
+    rel_path="$2"
+    shift 2
+    ;;
+  -a | --action)
+    actions+=("$2")
+    shift 2
+    ;;
+  -o | --owner)
+    owner="$2"
+    shift 2
+    ;;
+  -h | --help)
+    show_help
+    ;;
+  # Collect remaining as positional
+  *)
+    positional_args+=("$1")
+    shift
+    ;;
+  esac
+done
+
+# Backwards compatibility:
+# If the user didn't specify flags, we interpret positional arguments
+# in the old style: [rel_path] [action] [owner]
+if [[ ${#positional_args[@]} -ge 1 ]]; then
+  rel_path="${positional_args[0]}"
+fi
+if [[ ${#positional_args[@]} -ge 2 ]]; then
+  actions+=("${positional_args[1]}")
+fi
+if [[ ${#positional_args[@]} -ge 3 ]]; then
+  owner="${positional_args[2]}"
+fi
+
+abs_path="$DIR/$rel_path"
 
 commit_as_github_actions() {
   git config user.name 'github-actions[bot]'
@@ -17,10 +77,15 @@ commit_as_dirty_bot() {
   git config user.email 'builders@andes.is'
 }
 
+# Forward everything to yarn.
+# If you only want to forward flags (and not the old positional arguments), you can pass just "$@".
+yarn "${actions[@]}"
+
 if [[ $(git diff --stat "$abs_path") != '' ]]; then
-  echo "changes found in $rel_path that will be commited"
+  echo "Changes found in $rel_path that will be committed:"
   git diff "$abs_path"
   git add "$abs_path"
+
   if [ "$owner" == "github actions" ]; then
     commit_as_github_actions
   elif [ "$owner" == "dirtybot" ]; then
@@ -29,8 +94,10 @@ if [[ $(git diff --stat "$abs_path") != '' ]]; then
     echo "Error: Unknown owner!"
     exit 1
   fi
-  git commit -m "chore: $action update dirty files"
+
+  commit_message="chore: ${actions[*]} update dirty files"
+  git commit -m "$commit_message"
   git push
 else
-  echo "found no unstaged files from $action, nothing to commit"
+  echo "Found no unstaged files from ${actions[*]:-unknown}, nothing to commit."
 fi
