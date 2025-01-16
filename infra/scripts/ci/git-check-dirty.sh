c5003670bf4ac3600765436f24b88327b8d38023
Kristofer <kristofer@andes.is>
2025-01-16 16:55:27 +0000

    Compat script

--- infra/scripts/ci/git-check-dirty.sh
+++ infra/scripts/ci/git-check-dirty.sh
@@ -11,58 +11,80 @@ positional_args=()
 
 show_help() {
   cat <<EOF
-Usage: $0 [OPTIONS] [<rel_path> <action> [<owner>]]
+Usage:
+
+Old (positional) usage (backwards-compatible):
+  $0 <rel_path> [<actions>] [<owner>]
+  Example:
+    $0 my/path "build test" dirtybot
+
+Flag-based usage:
+  $0 [OPTIONS] --action <action1> [action2 ...]
 
 Options:
-  -p, --path <path>        Path to track changes on
-  -a, --action <action>    Action(s) to be performed (can be repeated)
-  -o, --owner <owner>      Owner name (default: "github actions")
+  -p, --path <path>        Relative path to track changes on
+  -o, --owner <owner>      Git owner name (default: "github actions")
+  -a, --action <values...> One or more actions (build, test, etc.)
+                           Provide multiple actions separated by spaces
   -h, --help               Show this help message
 
-Example usage:
-  $0 my/path someAction dirtybot
-  $0 --path my/path --action someAction --action anotherAction --owner dirtybot
+Examples:
+  $0 --path my/path --action build test --owner dirtybot
+  $0 my/path "build test" dirtybot
 EOF
   exit 0
 }
 
-# Parse flags
+# CLI parsing
 while [[ $# -gt 0 ]]; do
   case "$1" in
   -p | --path)
     rel_path="$2"
     shift 2
     ;;
-  -a | --action)
-    actions+=("$2")
-    shift 2
-    ;;
   -o | --owner)
     owner="$2"
     shift 2
     ;;
+  -a | --action)
+    # Consume everything until the next dash-option or end of line
+    shift
+    while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+      actions+=("$1")
+      shift
+    done
+    ;;
   -h | --help)
     show_help
     ;;
-  # Collect remaining as positional
   *)
+    # Anything that doesn't match a flag goes into positional_args
     positional_args+=("$1")
     shift
     ;;
   esac
 done
 
-# Backwards compatibility:
-# If the user didn't specify flags, we interpret positional arguments
-# in the old style: [rel_path] [action] [owner]
-if [[ ${#positional_args[@]} -ge 1 ]]; then
-  rel_path="${positional_args[0]}"
-fi
-if [[ ${#positional_args[@]} -ge 2 ]]; then
-  actions+=("${positional_args[1]}")
-fi
-if [[ ${#positional_args[@]} -ge 3 ]]; then
-  owner="${positional_args[2]}"
+# Backwards-compatibility block for old usage (if no flags were used):
+#   script.sh <rel_path> [<action(s)>] [<owner>]
+#
+# - If the user only specified positionals, parse them in the old way.
+# - This also allows a "mixed" scenario, but typically one uses all flags or none.
+if [[ -n "${positional_args[*]}" && "${#actions[@]}" -eq 0 ]]; then
+  # At least 1 positional => rel_path
+  if [[ ${#positional_args[@]} -ge 1 ]]; then
+    rel_path="${positional_args[0]}"
+  fi
+  # At least 2 => actions (possibly space-separated in quotes)
+  if [[ ${#positional_args[@]} -ge 2 ]]; then
+    # We split the second positional argument on spaces to get multiple actions
+    IFS=' ' read -r -a old_actions <<<"${positional_args[1]}"
+    actions=("${old_actions[@]}")
+  fi
+  # At least 3 => owner
+  if [[ ${#positional_args[@]} -ge 3 ]]; then
+    owner="${positional_args[2]}"
+  fi
 fi
 
 abs_path="$DIR/$rel_path"
@@ -77,27 +99,36 @@ commit_as_dirty_bot() {
   git config user.email 'builders@andes.is'
 }
 
-# Forward everything to yarn.
-# If you only want to forward flags (and not the old positional arguments), you can pass just "$@".
+# ---------------
+# RUN THE ACTIONS
+# ---------------
 yarn "${actions[@]}"
 
+# ---------------
+# CHECK & COMMIT
+# ---------------
 if [[ $(git diff --stat "$abs_path") != '' ]]; then
-  echo "Changes found in $rel_path that will be committed:"
+  echo "Changes found in $rel_path that will be committed."
   git diff "$abs_path"
   git add "$abs_path"
 
-  if [ "$owner" == "github actions" ]; then
+  case "$owner" in
+  "github actions")
     commit_as_github_actions
-  elif [ "$owner" == "dirtybot" ]; then
+    ;;
+  "dirtybot")
     commit_as_dirty_bot
-  else
-    echo "Error: Unknown owner!"
+    ;;
+  *)
+    echo "Error: Unknown owner '$owner'!"
     exit 1
-  fi
+    ;;
+  esac
 
+  # Join all actions into a single commit message
   commit_message="chore: ${actions[*]} update dirty files"
   git commit -m "$commit_message"
   git push
 else
-  echo "Found no unstaged files from ${actions[*]:-unknown}, nothing to commit."
+  echo "Found no unstaged files from actions: '${actions[*]}' - nothing to commit."
 fi
