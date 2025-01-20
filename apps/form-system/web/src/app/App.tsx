16c8bee11b67368537aa19a0f73d0b1f713d645f
Hyosam <hyo-sam.nandkisore@advania.is>
2025-01-20 11:19:19 +0000

    init, codegen still works

---
+++ apps/form-system/web/src/app/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.scss'
+
+import NxWelcome from './nx-welcome'
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="form-system-web" />
+    </div>
+  )
+}
+
+export default App
