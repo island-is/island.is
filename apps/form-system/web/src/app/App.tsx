df43adb550b5da1c20ce414557affb5d28499e05
Hyosam <hyo-sam.nandkisore@advania.is>
2025-01-20 14:17:02 +0000

    feat(form-system): Integrate Apollo Client and set up routing with BFF support

--- apps/form-system/web/src/app/App.tsx
+++ apps/form-system/web/src/app/App.tsx
@@ -1,14 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './App.module.scss'
+import { ApolloProvider } from '@apollo/client'
 
-import NxWelcome from './nx-welcome'
+import { client } from '@island.is/application/graphql'
+import { LocaleProvider } from '@island.is/localization'
+import { FeatureFlagProvider } from '@island.is/react/feature-flags'
+import { defaultLanguage } from '@island.is/shared/constants'
 
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="form-system-web" />
-    </div>
-  )
-}
+import { applicationSystemScopes } from '@island.is/auth/scopes'
+import { BffProvider, createMockedInitialState } from '@island.is/react-spa/bff'
+import { BASE_PATH } from '../lib/routes'
+import { isMockMode } from '../mocks'
+import { Router } from '../components/Router/Router'
+
+const mockedInitialState = isMockMode
+  ? createMockedInitialState({
+    scopes: applicationSystemScopes,
+  })
+  : undefined
+
+export const App = () => (
+  <ApolloProvider client={client}>
+    <LocaleProvider locale={defaultLanguage} messages={{}}>
+      <BffProvider
+        applicationBasePath={BASE_PATH}
+        mockedInitialState={mockedInitialState}
+      >
+        {/* <FeatureFlagProvider sdkKey={environment.featureFlagSdkKey}> */}
+        <Router />
+        {/* </FeatureFlagProvider> */}
+      </BffProvider>
+    </LocaleProvider>
+  </ApolloProvider>
+)
 
 export default App
---
+++ apps/form-system/web/src/context/HeaderInfoProvider.tsx
@@ -0,0 +1,40 @@
+import { createContext, Dispatch, SetStateAction, useContext, useState, ReactNode } from "react"
+
+interface FormInfo {
+  applicationName?: string
+  organisationName?: string
+}
+
+interface HeaderInfoProvider {
+  info: FormInfo
+  setInfo: Dispatch<SetStateAction<FormInfo>>
+}
+
+export const HeaderInfoContext = createContext<HeaderInfoProvider>({
+  info: {
+    applicationName: undefined,
+    organisationName: undefined,
+  },
+  setInfo: () => undefined,
+})
+
+export const HeaderInfoProvider = ({ children }: { children: ReactNode }) => {
+  const [info, setInfo] = useState<FormInfo>({
+    applicationName: undefined,
+    organisationName: undefined,
+  })
+
+  return (
+    <HeaderInfoContext.Provider
+      value={{
+        info,
+        setInfo
+      }}
+    >
+      {children}
+    </HeaderInfoContext.Provider>
+  )
+}
+
+export const useHeaderInfo = () => useContext(HeaderInfoContext)
+
