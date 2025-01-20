aecde7c3c0d46649aa54c656aef663eb292700c1
runarvestmann <runar@stefna.is>
2025-01-20 14:43:25 +0000

    Change query

--- apps/web/components/connected/syslumenn/CardLists/BurningPermitList/queries.ts
+++ apps/web/components/connected/syslumenn/CardLists/BurningPermitList/queries.ts
@@ -3,13 +3,15 @@ import gql from 'graphql-tag'
 export const GET_BURNING_PERMITS_QUERY = gql`
   query GetBurningPermits {
     getBurningPermits {
-      date
-      type
-      subtype
-      responsibleParty
-      office
-      licensee
-      place
+      list {
+        date
+        type
+        subtype
+        responsibleParty
+        office
+        licensee
+        place
+      }
     }
   }
 `
