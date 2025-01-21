1c67458c0e864ca523b74a26f951f2086731c966
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-01-21 11:08:17 +0000

    fix(j-s): add test for appeal deadline date

--- libs/judicial-system/types/src/lib/dates.spec.ts
+++ libs/judicial-system/types/src/lib/dates.spec.ts
@@ -1,4 +1,4 @@
-import { getIndictmentAppealDeadlineDate } from './dates'
+import { getAppealDeadlineDate, getIndictmentAppealDeadlineDate } from './dates'
 
 describe('getIndictmentAppealDeadlineDate', () => {
   test('should return fine appeal deadline', () => {
@@ -25,3 +25,17 @@ describe('getIndictmentAppealDeadlineDate', () => {
     expect(actualDate).toStrictEqual(new Date(2024, 1, 29, 23, 59, 59, 999))
   })
 })
+
+describe('getAppealDeadlineDate', () => {
+    test('should return expected appeal deadline', () => {
+      // Arrange
+      const baseDate = new Date(2024, 1, 1)
+  
+      // Act
+      const actualDate = getAppealDeadlineDate({ baseDate })
+  
+      // Assert
+      expect(actualDate).toStrictEqual(new Date(2024, 1, 4, 23, 59, 59, 999))
+    })
+  })
+  
