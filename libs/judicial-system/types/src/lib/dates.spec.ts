0e2fa85b4d3f469f1b3d8fb5a46e97dd8c3f2359
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-01-21 11:54:38 +0000

    fix(j-s): formatting and tiny adjustments

--- libs/judicial-system/types/src/lib/dates.spec.ts
+++ libs/judicial-system/types/src/lib/dates.spec.ts
@@ -27,15 +27,14 @@ describe('getIndictmentAppealDeadlineDate', () => {
 })
 
 describe('getAppealDeadlineDate', () => {
-    test('should return expected appeal deadline', () => {
-      // Arrange
-      const baseDate = new Date(2024, 1, 1)
-  
-      // Act
-      const actualDate = getAppealDeadlineDate({ baseDate })
-  
-      // Assert
-      expect(actualDate).toStrictEqual(new Date(2024, 1, 4, 23, 59, 59, 999))
-    })
+  test('should return expected appeal deadline', () => {
+    // Arrange
+    const baseDate = new Date(2024, 1, 1)
+
+    // Act
+    const actualDate = getAppealDeadlineDate({ baseDate })
+
+    // Assert
+    expect(actualDate).toStrictEqual(new Date(2024, 1, 4, 23, 59, 59, 999))
   })
-  
+})
