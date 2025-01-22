d8a4d3899e65a1e1fa11feaaa9d599283a3ef618
Kristofer <kristofer@andes.is>
2025-01-22 18:02:01 +0000

    lint --fix --all

--- apps/contentful-apps/pages/fields/admin-only-single-reference-field.tsx
+++ apps/contentful-apps/pages/fields/admin-only-single-reference-field.tsx
@@ -1,8 +1,8 @@
-import { Paragraph } from '@contentful/f36-components'
-import { FieldExtensionSDK } from '@contentful/app-sdk'
-import { useSDK } from '@contentful/react-apps-toolkit'
-import { SingleEntryReferenceEditor } from '@contentful/field-editor-reference'
 import { useEffect } from 'react'
+import { FieldExtensionSDK } from '@contentful/app-sdk'
+import { Paragraph } from '@contentful/f36-components'
+import { SingleEntryReferenceEditor } from '@contentful/field-editor-reference'
+import { useSDK } from '@contentful/react-apps-toolkit'
 
 const AdminOnlySingleReferenceField = () => {
   const sdk = useSDK<FieldExtensionSDK>()
