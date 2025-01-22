719bcb476eeea44706ec5ecc5d17fa69006f623c
kksteini <steini@kosmosogkaos.is>
2025-01-22 13:02:43 +0000

    Prepare fake data

--- libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
+++ libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
@@ -6,19 +6,21 @@ import { EinstaklingarApi } from '../../gen/fetch'
 export class NationalRegistryV3ApplicationsClientService {
   constructor(private individualApi: EinstaklingarApi) {}
 
-  async get18YearOlds(): Promise<string[]> {
+  async get18YearOlds(useFakeData?: boolean): Promise<string[]> {
+    if (useFakeData) {
+      const day = new Date().getDay() // Sunday - Saturday : 0 - 6
+      const birthdaySsn = [
+        '0101303019', // Sunday
+        '0101302399', // Monday
+        '0101307789', // Tuesday
+        '0101302989', // Wednesday
+        '0101302719', // Thursday
+        '0101305069', // Friday
+        '0101304929', // Saturday
+      ][day]
+      return [birthdaySsn]
+    }
+
     return await this.individualApi.einstaklingar18IDagGet()
   }
-
-  //getDomicileData(
-  //  nationalId: string,
-  //): Promise<EinstaklingurDTOLoghTengsl | null> {
-  //  return handle204(
-  //    this.individualApi.midlunV1EinstaklingarNationalIdLogheimilistengslGetRaw(
-  //      {
-  //        nationalId,
-  //      },
-  //    ),
-  //  )
-  //}
 }
