7a6cec7ea46aedf104624380e5d3ed17430d33bf
unakb <una@kolibri.is>
2025-01-23 11:12:59 +0000

    Update 20250121143923-update-case.js

--- apps/judicial-system/backend/migrations/20250121143923-update-case.js
+++ apps/judicial-system/backend/migrations/20250121143923-update-case.js
@@ -3,12 +3,21 @@
 const replaceEnum = require('sequelize-replace-enum-postgres').default
 
 module.exports = {
-  up: async (queryInterface) => {
-    // Safely add the new enum value to the type without replacing the entire enum
-    await queryInterface.sequelize.query(`
-      ALTER TYPE enum_case_decision 
-      ADD VALUE IF NOT EXISTS 'COMPLETED_WITHOUT_RULING';
-    `)
+  up: (queryInterface) => {
+    return replaceEnum({
+      queryInterface,
+      tableName: 'case',
+      columnName: 'decision',
+      newValues: [
+        'ACCEPTING',
+        'REJECTING',
+        'ACCEPTING_ALTERNATIVE_TRAVEL_BAN',
+        'ACCEPTING_PARTIALLY',
+        'DISMISSING',
+        'COMPLETED_WITHOUT_RULING', // new value - will be changed to string in future
+      ],
+      enumName: 'enum_case_decision',
+    })
   },
 
   down: (queryInterface) => {
