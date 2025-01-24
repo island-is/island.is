dd50a24fe501f5351964bf1fc2936da362df4980
Valur Einarsson <valure@live.com>
2025-01-24 16:37:50 +0000

    Check ids issuer env var also for local dev.

--- apps/services/sessions/src/environments/environment.ts
+++ apps/services/sessions/src/environments/environment.ts
@@ -11,7 +11,8 @@ export default {
     issuer: isProduction
       ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         process.env.IDENTITY_SERVER_ISSUER_URL!
-      : 'https://identity-server.dev01.devland.is',
+      : process.env.IDENTITY_SERVER_ISSUER_URL ??
+        'https://identity-server.dev01.devland.is',
   },
   port: 3333,
 }
