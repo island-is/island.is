02086fda2d12afa451d8e5de838c3d6d212fcbd1
Jón Levy <levy@apro.is>
2025-01-27 23:35:03 +0000

    fix: increase timeout

--- jest.preset.js
+++ jest.preset.js
@@ -8,6 +8,7 @@ module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   testPathIgnorePatterns: ['<rootDir>/apps/.*/e2e'],
   resolver: customResolver,
+  testTimeout: 20000,
   detectOpenHandles: true,
   runInBand: true,
   detectLeaks: true,
