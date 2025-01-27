8d9165bb030a2f5e401be767b47b4e754db0b3a6
Heba <heba.farestveit@gmail.com>
2025-01-27 12:40:45 +0000

    Update property search mock data options.

--- libs/application/templates/rental-agreement/src/fields/PropertySearch/index.tsx
+++ libs/application/templates/rental-agreement/src/fields/PropertySearch/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react'
 import { CustomField, FieldBaseProps } from '@island.is/application/types'
 import { AsyncSearch, AsyncSearchOption } from '@island.is/island-ui/core'
+import { size } from 'lodash'
 
 interface Props extends FieldBaseProps {
   field: CustomField
@@ -28,10 +29,10 @@ const properties = [
     cityName: 'Reykjavík',
     propertyIds: [
       {
-        propertyId: 'P67890',
-        unit: ['U125'],
-        size: '150 sqm',
-        numberOfRooms: 4,
+        propertyId: '2042190',
+        size: '174.5',
+        propertyType: 'Raðhús',
+        merking: '040101',
       },
     ],
   },
@@ -42,149 +43,171 @@ const properties = [
     propertyIds: [
       {
         propertyId: '2507085',
-        unit: ['020101'],
+        propertyType: 'Íbúð',
         size: '67.3',
-        numberOfRooms: 3,
+        merking: [
+          {
+            id: '020102',
+            units: [
+              {
+                unitId: '2383972',
+                merking: '0101',
+                unitType: 'íbúð',
+                size: '67.3',
+                numberOfRooms: '2',
+              },
+              {
+                unitId: '2374580',
+                merking: 'E035',
+                unitType: 'Stæði í bílageymslu',
+                size: '0',
+                numberOfRooms: undefined,
+              },
+            ],
+          },
+        ],
       },
       {
         propertyId: '2507086',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
+        propertyType: 'Íbúð',
+        size: '57.2',
+        merking: [
+          {
+            id: '020103',
+            units: [
+              {
+                unitId: '2383973',
+                merking: '0102',
+                unitType: 'íbúð',
+                size: '57.2',
+                numberOfRooms: '2',
+              },
+              {
+                unitId: '2374581',
+                merking: 'E036',
+                unitType: 'Stæði í bílageymslu',
+                size: '0',
+                numberOfRooms: undefined,
+              },
+            ],
+          },
+        ],
       },
       {
         propertyId: '2507087',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
+        propertyType: 'Íbúð',
+        size: '117.3',
+        merking: [
+          {
+            id: '020201',
+            units: [
+              {
+                unitId: '2383974',
+                merking: '0103',
+                unitType: 'íbúð',
+                size: '117.3',
+                numberOfRooms: '3',
+              },
+              {
+                unitId: '2374580',
+                merking: 'E025',
+                unitType: 'Stæði í bílageymslu',
+                size: '0',
+                numberOfRooms: undefined,
+              },
+            ],
+          },
+        ],
       },
       {
         propertyId: '2507404',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507405',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507405',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507406',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507410',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507411',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507412',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507416',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507417',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507418',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507422',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
-      },
-      {
-        propertyId: '2507423',
-        unit: ['020101'],
-        size: '120 sqm',
-        numberOfRooms: 3,
+        propertyType: 'Íbúð',
+        size: '99.4',
+        merking: [
+          {
+            id: '020202',
+            units: [
+              {
+                unitId: '2383978',
+                merking: '0201',
+                unitType: 'íbúð',
+                size: '99.4',
+                numberOfRooms: '3',
+              },
+              {
+                unitId: '2374582',
+                merking: 'E037',
+                unitType: 'Stæði í bílageymslu',
+                size: '0',
+                numberOfRooms: undefined,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
   {
-    streetAddress: '456 Elm St',
-    houseNumber: '2B',
-    regionNumber: '102',
-    cityName: 'Akureyri',
+    streetAddress: 'Eggertsgata 22',
+    regionNumber: '112',
+    cityName: 'Reykjavík',
     propertyIds: [
       {
-        propertyId: 'P67890',
-        unitIds: ['U125'],
-        size: '150 sqm',
-        numberOfRooms: 4,
-      },
-    ],
-  },
-  {
-    streetAddress: '789 Oak St',
-    houseNumber: '3C',
-    regionNumber: '103',
-    cityName: 'Kopavogur',
-    propertyIds: [
-      {
-        propertyId: 'P54321',
-        unitIds: ['U126', 'U127'],
-        size: '100 sqm',
-        numberOfRooms: 2,
-      },
-    ],
-  },
-  {
-    streetAddress: '101 Pine St',
-    houseNumber: '4D',
-    regionNumber: '104',
-    cityName: 'Hafnarfjordur',
-    propertyIds: [
-      {
-        propertyId: 'P98765',
-        unitIds: ['U128'],
-        size: '130 sqm',
-        numberOfRooms: 3,
-      },
-    ],
-  },
-  {
-    streetAddress: '202 Birch St',
-    houseNumber: '5E',
-    regionNumber: '105',
-    cityName: 'Selfoss',
-    propertyIds: [
-      {
-        propertyId: 'P11223',
-        unitIds: ['U129', 'U130'],
-        size: '140 sqm',
-        numberOfRooms: 4,
+        propertyId: '2215524',
+        merking: '160003',
+        propertyType: 'Íbúð',
+        size: '952.8',
+        units: [
+          {
+            unitId: '2215526',
+            merking: '0004',
+            unitType: 'íbúð',
+            size: '36.1',
+            numberOfRooms: '1',
+          },
+          {
+            unitId: '2215527',
+            merking: '0005',
+            unitType: 'íbúð',
+            size: '52',
+            numberOfRooms: '2',
+          },
+          {
+            unitId: '2215528',
+            merking: '0101',
+            unitType: 'íbúð',
+            size: '53.1',
+            numberOfRooms: '2',
+          },
+          {
+            unitId: '2215531',
+            merking: '0102',
+            unitType: 'íbúð',
+            size: '52.6',
+            numberOfRooms: '2',
+          },
+          {
+            unitId: '2215532',
+            merking: '0103',
+            unitType: 'íbúð',
+            size: '36.1',
+            numberOfRooms: '1',
+          },
+          {
+            unitId: '2215533',
+            merking: '0004',
+            unitType: 'íbúð',
+            size: '36.1',
+            numberOfRooms: '1',
+          },
+          {
+            unitId: '2215534',
+            merking: '0004',
+            unitType: 'íbúð',
+            size: '52.6',
+            numberOfRooms: '2',
+          },
+        ],
       },
     ],
   },
