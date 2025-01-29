ccce7a56412996c27ae71b88f4c3568bb924a66d
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-01-29 15:06:19 +0000

    chore(j-s): create new offense table for with FK to indictment_count

---
+++ apps/judicial-system/backend/migrations/20250129131852-update-create-charges.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.createTable(
+        'offense',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          indictment_count_id: {
+            type: Sequelize.UUID,
+            references: {
+              model: 'indictment_count',
+              key: 'id',
+            },
+            allowNull: false,
+          },          
+          type: {
+            type: Sequelize.STRING, // DRIVING_WITHOUT_LICENCE, DRUNK_DRIVING, ...
+            allowNull: false, 
+          },
+          substances: {
+            type: Sequelize.JSON,
+            allowNull: true,
+          },
+        },
+        { transaction: t },
+      ),
+    )
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.dropTable('offense', { transaction: t }),
+    )
+  },
+}
