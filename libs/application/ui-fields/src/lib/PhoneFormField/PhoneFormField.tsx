c615285eb8b319412782794fae90aeaa8256a60c
Hjörtur Jóhann Vignisson <hjorturjohann94@gmail.com>
2025-01-30 10:14:56 +0000

    chore(application-system): Making title an optional property on form fields (#17634)
    * defaulting every title to empty
    
    * chore: nx format:write update dirty files
    
    * Adding a few more optionals to titles
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>

--- libs/application/ui-fields/src/lib/PhoneFormField/PhoneFormField.tsx
+++ libs/application/ui-fields/src/lib/PhoneFormField/PhoneFormField.tsx
@@ -29,7 +29,7 @@ export const PhoneFormField: FC<React.PropsWithChildren<Props>> = ({
   const {
     id,
     disabled,
-    title,
+    title = '',
     description,
     placeholder,
     backgroundColor,
