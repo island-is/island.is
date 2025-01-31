bb0c49309f404902371eec999dd01868f65b3bc5
Kristofer <kristofer@andes.is>
2025-01-31 14:33:00 +0000

    Only use major version (via config object)

--- scripts/ci/get-node-version.mjs
+++ scripts/ci/get-node-version.mjs
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import { getPackageJSON } from './_common.mjs'
 
-const version = await getPackageVersion()
+const onlyMajor = true
+const version = await getPackageVersion({ onlyMajor })
 
 if (!version) {
   console.error(`Failed getting docker image for ${nodeVersion}`)
@@ -9,11 +10,14 @@ if (!version) {
 }
 process.stdout.write(version)
 
-async function getPackageVersion() {
+async function getPackageVersion({ onlyMajor = false } = {}) {
   const content = await getPackageJSON()
   const version = content.engines?.node
   if (!version) {
     throw new Error(`Cannot find node version`)
   }
   return version
+    .split('.')
+    .slice(0, onlyMajor ? 1 : 3)
+    .join('.')
 }
