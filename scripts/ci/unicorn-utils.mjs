fe516c2b3465c797acf3ce310b796127e4bcce28
Róberta Andersen <roberta@andes.is>
2025-02-03 14:47:11 +0000

    chore: setup unicorn affected

--- scripts/ci/unicorn-utils.mjs
+++ scripts/ci/unicorn-utils.mjs
@@ -4,10 +4,13 @@ import { workspaceRoot } from '@nx/devkit'
 const unicornApps = ['unicorn-app']
 
 const arg = JSON.parse(process.argv.slice(2))
+
+console.log(arg)
+
 try {
   const affected = JSON.parse(
     execSync(
-      `cd ${workspaceRoot} && yarn nx show projects --affected --base ${arg.baseBranch} --json | jq -r`,
+      `cd ${workspaceRoot} && yarn nx show projects --affected --base ${arg.base} --head ${arg.head} --json`,
     ).toString(),
   )
   console.log(affected.some((item) => unicornApps.includes(item)))
