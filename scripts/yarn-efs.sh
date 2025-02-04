e401d8a076400146e5578eab9890bffea55aa422
Jón Levy <levy@apro.is>
2025-02-04 15:40:55 +0000

    fix: upgrade yarn

--- scripts/yarn-efs.sh
+++ scripts/yarn-efs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 YARN_LOCK_SHA=$1
-export NODE_PATH=/home/runner/${YARN_LOCK_SHA}/node_modules/
+BASE_DIR=/home/runner
+
+export YARN_CACHE_DIR=${BASE_DIR}/.yarn/cache
+export NODE_PATH=${BASE_DIR}/${YARN_LOCK_SHA}/node_modules/
 echo "NODE_PATH=$NODE_PATH" >>"$GITHUB_ENV"
-yarn install --cached
+yarn install --immutable
