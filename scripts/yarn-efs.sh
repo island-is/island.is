3e0b171efe1a29231e967b9c7fcccdc499f4271e
Jón Levy <levy@apro.is>
2025-02-04 15:41:29 +0000

    fix: typo

--- scripts/yarn-efs.sh
+++ scripts/yarn-efs.sh
@@ -3,7 +3,7 @@
 YARN_LOCK_SHA=$1
 BASE_DIR=/home/runner
 
-export YARN_CACHE_DIR=${BASE_DIR}/.yarn/cache
+export YARN_CACHE_FOLDER=${BASE_DIR}/.yarn/cache
 export NODE_PATH=${BASE_DIR}/${YARN_LOCK_SHA}/node_modules/
 echo "NODE_PATH=$NODE_PATH" >>"$GITHUB_ENV"
 yarn install --immutable
