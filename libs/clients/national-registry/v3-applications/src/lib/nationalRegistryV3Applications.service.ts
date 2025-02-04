ba82b993e9cc82d0324780b4745ec93e804f5f8a
kksteini <steini@kosmosogkaos.is>
2025-02-04 12:26:52 +0000

    Removed test code

--- libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
+++ libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
@@ -6,21 +6,7 @@ import { EinstaklingarApi } from '../../gen/fetch'
 export class NationalRegistryV3ApplicationsClientService {
   constructor(private individualApi: EinstaklingarApi) {}
 
-  async get18YearOlds(useFakeData?: boolean): Promise<string[]> {
-    if (useFakeData) {
-      const day = new Date().getDay() // Sunday - Saturday : 0 - 6
-      const birthdaySsn = [
-        '0101303019', // Sunday
-        '0101302399', // Monday
-        '0101307789', // Tuesday
-        '0101302989', // Wednesday
-        '0101302719', // Thursday
-        '0101305069', // Friday
-        '0101304929', // Saturday
-      ][day]
-      return [birthdaySsn]
-    }
-
+  async get18YearOlds(): Promise<string[]> {
     return await this.individualApi.einstaklingar18IDagGet()
   }
 }
