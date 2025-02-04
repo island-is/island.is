3bda976faa58df93a6039b04e9408ec919fb389f
Jón Levy <levy@apro.is>
2025-02-04 15:50:46 +0000

    ci: disable hardened mode

--- scripts/yarn-efs.sh
+++ scripts/yarn-efs.sh
@@ -3,6 +3,7 @@
 YARN_LOCK_SHA=$1
 BASE_DIR=/home/runner
 
+export YARN_ENABLE_HARDENED_MODE=0
 export YARN_CACHE_FOLDER=${BASE_DIR}/.yarn/cache
 export NODE_PATH=${BASE_DIR}/${YARN_LOCK_SHA}/node_modules/
 echo "NODE_PATH=$NODE_PATH" >>"$GITHUB_ENV"
