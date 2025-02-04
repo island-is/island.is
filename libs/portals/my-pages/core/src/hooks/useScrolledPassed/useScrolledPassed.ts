ad10c9a5b091750cd5990ffe0f3d7f1d30338046
disaerna <disa@hugsmidjan.is>
2025-02-04 12:44:05 +0000

    refactor hook

--- libs/portals/my-pages/core/src/hooks/useScrolledPassed/useScrolledPassed.ts
+++ libs/portals/my-pages/core/src/hooks/useScrolledPassed/useScrolledPassed.ts
@@ -1,43 +1,47 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useScrolledPassed = (id: string) => {
   const [isScrolled, setIsScrolled] = useState(false)
-  const lastScrollY = useRef(0) // To track the last scroll position
-  const ticking = useRef(false) // To track if requestAnimationFrame is already in progress
 
   useEffect(() => {
+    let timeoutId: NodeJS.Timeout
+
     const handleScroll = () => {
-      if (!ticking.current) {
-        window.requestAnimationFrame(() => {
-          const element = document.getElementById(id)
-          const scrollY = window.scrollY
-
-          if (element) {
-            const rect = element.getBoundingClientRect()
-
-            // Check if the user scrolled past the element or is scrolling up
-            if (scrollY <= rect.top) setIsScrolled(false)
-            else if (rect.top <= 0 || scrollY < lastScrollY.current) {
-              setIsScrolled(true)
-            }
-          }
-
-          // Update the last scroll position
-          lastScrollY.current = scrollY
-          ticking.current = false
-        })
-        ticking.current = true
+      if (timeoutId) {
+        clearTimeout(timeoutId)
       }
+
+      timeoutId = setTimeout(() => {
+        const element = document.getElementById(id)
+        const scrollY = window.scrollY
+
+        if (element) {
+          const rect = element.getBoundingClientRect()
+
+          // Check if the user scrolled past the element
+          if (scrollY > rect.top) {
+            setIsScrolled(true)
+          } else {
+            setIsScrolled(false)
+          }
+        }
+      }, 200)
     }
 
     window.addEventListener('scroll', handleScroll)
     window.addEventListener('touchmove', handleScroll, { passive: true })
 
+    // Initial check in case the user is already scrolled past the element
+    handleScroll()
+
     return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
       window.removeEventListener('scroll', handleScroll)
       window.removeEventListener('touchmove', handleScroll)
     }
-  }, [])
+  }, [id])
 
   return isScrolled
 }
