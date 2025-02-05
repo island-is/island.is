40c166eadbfffe9f29451b4e38d71411387eb27a
Jón Levy <levy@apro.is>
2025-02-05 19:18:58 +0000

    fix: use rsync

--- scripts/yarn-s3.sh
+++ scripts/yarn-s3.sh
@@ -2,15 +2,13 @@
 
 set -euox pipefail
 
-YARN_LOCK_SHA=$1
 CACHE_DIR=/mnt/cache
 
 export YARN_ENABLE_HARDENED_MODE=0
 export YARN_CACHE_FOLDER=${CACHE_DIR}/.yarn/cache
-export NODE_PATH=${CACHE_DIR}/${YARN_LOCK_SHA}/node_modules/
 
 mkdir -p "$YARN_CACHE_FOLDER"
-mkdir -p "$NODE_PATH"
 
-echo "NODE_PATH=$NODE_PATH" >>"$GITHUB_ENV"
+rsync -ah --delete "${YARN_CACHE_FOLDER}/" .yarn/cache/ || true
 yarn install --immutable
+rsync -ah --delete .yarn/cache/ "${YARN_CACHE_FOLDER}/"
