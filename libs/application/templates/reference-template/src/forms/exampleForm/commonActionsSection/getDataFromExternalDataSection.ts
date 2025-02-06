47dac33c17b7696a97a6bc70016ec7030322dcf8
Jonni <jonas@norda.is>
2025-02-06 15:23:52 +0000

    chore: undo reference template changes

--- libs/application/templates/reference-template/src/forms/exampleForm/commonActionsSection/getDataFromExternalDataSection.ts
+++ libs/application/templates/reference-template/src/forms/exampleForm/commonActionsSection/getDataFromExternalDataSection.ts
@@ -1,10 +1,7 @@
 import {
   buildDescriptionField,
-  buildFieldsRepeaterField,
   buildMultiField,
-  buildSelectField,
   buildSubSection,
-  buildTextField,
   getValueViaPath,
 } from '@island.is/application/core'
 import { Application } from '@island.is/application/types'
@@ -60,74 +57,6 @@ export const getDataFromExternalDataSubsection = buildSubSection({
             return value ?? 'Not found'
           },
         }),
-        buildTextField({
-          id: 'testTextField',
-          title: 'Text field',
-          width: 'half',
-        }),
-        buildFieldsRepeaterField({
-          id: 'testRepeater',
-          title: 'Repeater',
-          fields: {
-            select: {
-              component: 'select',
-              label: 'Select',
-              width: 'half',
-              options: [
-                { label: 'Option 1', value: 'option1' },
-                { label: 'Option 2', value: 'option2' },
-                { label: 'Option 3', value: 'option3' },
-                { label: 'Option 4', value: 'option4' },
-                { label: 'Option 5', value: 'option5' },
-              ],
-            },
-          },
-        }),
-        buildSelectField({
-          condition: (answers) => {
-            const test = getValueViaPath<Array<{ select: string }>>(
-              answers,
-              'testRepeater',
-            )
-            return test && test.length > 1 ? true : false
-          },
-          id: 'externalDataSuccessRepeaterSelect',
-          title: 'Select',
-          width: 'half',
-          options: (application: Application) => {
-            const testSelect = getValueViaPath<Array<{ select: string }>>(
-              application.answers,
-              'testRepeater',
-            )
-
-            return testSelect
-              ?.map((option) => {
-                if (!option.select) {
-                  return null
-                }
-                return {
-                  label: option.select,
-                  value: option.select,
-                }
-              })
-              .filter(Boolean) as Array<{ label: string; value: string }>
-          },
-        }),
-        // buildAsyncSelectField({
-        //   id: 'asyncTest',
-        //   title: 'Async select',
-        //   width: 'half',
-        //   updateOnSelect: 'testRepeater',
-        //   loadOptions: async () => {
-        //     return [
-        //       { label: 'Option 1', value: 'option1' },
-        //       { label: 'Option 2', value: 'option2' },
-        //       { label: 'Option 3', value: 'option3' },
-        //       { label: 'Option 4', value: 'option4' },
-        //       { label: 'Option 5', value: 'option5' },
-        //     ]
-        //   },
-        // }),
       ],
     }),
   ],
