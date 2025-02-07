712e58070ea49832824212ebef06daeeb642f32a
Jón Levy <levy@apro.is>
2025-02-07 21:13:13 +0000

    fix: typo

--- scripts/ci/10_prepare-docker-deps.sh
+++ scripts/ci/10_prepare-docker-deps.sh
@@ -14,19 +14,15 @@ DOCKER_REGISTRY=${DOCKER_REGISTRY:-821090935708.dkr.ecr.eu-west-1.amazonaws.com}
 docker buildx rm actions_builder || true
 docker buildx create --name actions_builder --driver docker-container --use --driver-opt image="${DOCKER_REGISTRY}/moby/buildkit:buildx-stable-1"
 
-LAYERS=("base" "deps")
-
 # Build and push deps layer to S3
-for LAYER in "${LAYERS[@]}"; do
-  echo "Building and pushing ${LAYER} layer to S3..."
-  docker buildx build \
-    --platform=linux/amd64 \
-    --cache-from="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache-${LAYER}" \
-    --cache-to="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache-${LAYER},mode=max" \
-    --build-arg NODE_IMAGE_VERSION="$NODE_IMAGE_VERSION" \
-    -f "${DIR}"/Dockerfile \
-    --target="${LAYER}" \
-    "$PROJECT_ROOT"
-done
+echo "Building and pushing deps layer to S3..."
+docker buildx build \
+  --platform=linux/amd64 \
+  --cache-from="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache-deps" \
+  --cache-to="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache-deps,mode=max" \
+  --build-arg NODE_IMAGE_VERSION="$NODE_IMAGE_VERSION" \
+  -f "${DIR}"/Dockerfile \
+  --target=deps \
+  "$PROJECT_ROOT"
 
 echo "Build complete!"
