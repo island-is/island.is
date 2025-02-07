ca1ad1d348cc6ccdc183f299a35c32424e84a7af
Jón Levy <levy@apro.is>
2025-02-07 20:53:47 +0000

    fix: cache

--- scripts/ci/10_prepare-docker-deps.sh
+++ scripts/ci/10_prepare-docker-deps.sh
@@ -9,17 +9,24 @@ source "$DIR"/_common.sh
 mkdir -p "$PROJECT_ROOT"/cache
 
 NODE_IMAGE_VERSION=${NODE_IMAGE_VERSION:-20}
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-821090935708.dkr.ecr.eu-west-1.amazonaws.com}
 
-docker buildx create --driver docker-container --use || true
+docker buildx rm actions_builder || true
+docker buildx create --name actions_builder --driver docker-container --use --driver-opt image="${DOCKER_REGISTRY}/moby/buildkit:buildx-stable-1"
+
+LAYERS=("deps" "yarn")
 
 # Build and push deps layer to S3
-echo "Building and pushing deps layer to S3..."
-docker buildx build \
-  --platform=linux/amd64 \
-  --cache-to="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache,mode=max" \
-  --build-arg NODE_IMAGE_VERSION="$NODE_IMAGE_VERSION" \
-  -f "${DIR}"/Dockerfile \
-  --target=deps \
-  "$PROJECT_ROOT"
+for LAYER in "${LAYERS[@]}"; do
+  echo "Building and pushing ${LAYER} layer to S3..."
+  docker buildx build \
+    --platform=linux/amd64 \
+    --cache-from="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache-${LAYER}" \
+    --cache-to="type=s3,region=eu-west-1,bucket=${S3_DOCKER_CACHE_BUCKET},name=cache-${LAYER},mode=max" \
+    --build-arg NODE_IMAGE_VERSION="$NODE_IMAGE_VERSION" \
+    -f "${DIR}"/Dockerfile \
+    --target="${LAYER}" \
+    "$PROJECT_ROOT"
+done
 
 echo "Build complete!"
