edc21c8ed3da7a7391a4cbbb660cd0e44fc52be3
Jón Levy <levy@apro.is>
2025-02-09 08:33:34 +0000

    fix: build chunks

--- scripts/ci/generate-build-chunks.sh
+++ scripts/ci/generate-build-chunks.sh
@@ -8,22 +8,10 @@ source "$DIR"/_common.sh
 chunks='[]'
 for target in "$@"; do
   affected_chunks=$("$PROJECT_ROOT"/scripts/ci/generate-chunks.sh "$target")
-  if [[ "$affected_chunks" != "[]" ]]; then
-    # Process each chunk (which may contain multiple projects)
-    while IFS= read -r project_list; do
-      IFS=',' read -ra projects <<<"$project_list"
-      for project in "${projects[@]}"; do
-        home=$(yarn nx show project "$project" | jq -r '.root')
-        new_chunk=$(jq -n \
-          --arg project "$project" \
-          --arg home "$home" \
-          --arg docker_type "$target" \
-          '{projects: $project, home: $home, docker_type: $docker_type}')
-        chunks=$(echo "$chunks" | jq -c '. + ['"$new_chunk"']')
-      done
-    done < <(echo "$affected_chunks" | jq -r '.[]')
+  if [[ "$affected_chunks" != "" ]]; then
+    chunks=$(echo "$chunks" | jq -cM --argjson target "$affected_chunks" '. + ($target | map({projects: ., docker_type: "'"$target"'"}))')
   fi
 done
 
 >&2 echo "Map: ${chunks}"
-echo "$chunks"
+echo "$chunks" | jq -cM '. | map("\(.|tostring)")'
