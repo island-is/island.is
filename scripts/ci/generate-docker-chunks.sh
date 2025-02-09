0bee54bf90ce895bca82c93fd7238c2a3f70056d
Jón Levy <levy@apro.is>
2025-02-09 12:10:25 +0000

    fix: build chunks

--- scripts/ci/generate-docker-chunks.sh
+++ scripts/ci/generate-docker-chunks.sh
@@ -7,22 +7,19 @@ source "$DIR"/_common.sh
 
 chunks='[]'
 for target in "$@"; do
-  affected_chunks=$("$PROJECT_ROOT"/scripts/ci/generate-chunks.sh "$target")
-  if [[ "$affected_chunks" != "" ]]; then
-    processed_chunks=$(echo "$affected_chunks" | jq -r '.[]' | while read -r project_list; do
-      IFS=',' read -ra projects <<<"$project_list"
-      for project in "${projects[@]}"; do
-        home=$(yarn nx show project "$project" | jq -r '.root')
-        dist=$(yarn nx show project "$project" | jq -r '.targets.build.options.outputPath')
-        if [[ -n "$dist" ]]; then
-          jq -n \
-            --arg project "$project" \
-            --arg docker_type "$target" \
-            --arg home "$home" \
-            --arg dist "$dist" \
-            '{projects: $project, docker_type: $docker_type, home: $home, dist: $dist}'
-        fi
-      done
+  affected_apps=$("$DIR"/_nx-affected-targets.sh "$target")
+  if [[ -n "$affected_apps" ]]; then
+    processed_chunks=$(echo "$affected_apps" | tr ',' '\n' | while read -r project; do
+      home=$(yarn nx show project "$project" | jq -r '.root')
+      dist=$(yarn nx show project "$project" | jq -r '.targets.build.options.outputPath')
+      if [[ -n "$dist" ]]; then
+        jq -n \
+          --arg project "$project" \
+          --arg docker_type "$target" \
+          --arg home "$home" \
+          --arg dist "$dist" \
+          '{projects: $project, docker_type: $docker_type, home: $home, dist: $dist}'
+      fi
     done | jq -s '.')
     chunks=$(echo "$chunks" | jq -cM --argjson new_chunks "$processed_chunks" '. + $new_chunks')
   fi
