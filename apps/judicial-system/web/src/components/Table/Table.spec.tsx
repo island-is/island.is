1573d80cd3f0aa89548ed4aa94ef8ba1e43ade70
Ívar Oddsson <ivaroddsson@gmail.com>
2025-02-10 15:22:45 +0000

    Fix tests

--- apps/judicial-system/web/src/components/Table/Table.spec.tsx
+++ apps/judicial-system/web/src/components/Table/Table.spec.tsx
@@ -3,7 +3,10 @@ import { act, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup'
 
-import { CaseListEntry } from '@island.is/judicial-system-web/src/graphql/schema'
+import {
+  CaseListEntry,
+  CaseState,
+} from '@island.is/judicial-system-web/src/graphql/schema'
 import {
   ApolloProviderWrapper,
   IntlProviderWrapper,
@@ -138,6 +141,59 @@ describe('Table', () => {
     expect(tableRows[1]).toHaveTextContent('Jon Harring Sr.')
 
     await clickButtonByTestId('defendantsSortButton')
+
+    const tableRows2 = await screen.findAllByTestId('tableRow')
+    expect(tableRows2[0]).toHaveTextContent('Jon Harring Sr.')
+    expect(tableRows2[1]).toHaveTextContent('Bono Stingsson')
+  })
+
+  // TODO FIX THESE TESTS, THEY DONT WORK
+  it('should sort by state', async () => {
+    const thead = [
+      {
+        title: 'State',
+        sortBy: 'state' as sortableTableColumn,
+      },
+    ]
+
+    const data: CaseListEntry[] = [
+      {
+        created: '2021-01-01T00:00:00Z',
+        id: faker.datatype.uuid(),
+        defendants: [{ id: '', nationalId: 'string', name: 'Jon Harring Sr.' }],
+        state: CaseState.DRAFT,
+      },
+      {
+        created: '2021-01-02T00:00:00Z',
+        id: faker.datatype.uuid(),
+        defendants: [{ id: '', nationalId: 'string', name: 'Bono Stingsson' }],
+        state: CaseState.SUBMITTED,
+      },
+    ]
+
+    const columns: { cell: (row: CaseListEntry) => JSX.Element }[] =
+      data.flatMap(
+        (dataItem) =>
+          dataItem.defendants?.map((defendant) => ({
+            cell: () => <p>{defendant.name}</p>,
+          })) || [],
+      )
+
+    render(
+      <IntlProviderWrapper>
+        <ApolloProviderWrapper>
+          <Table thead={thead} data={data} columns={columns} />
+        </ApolloProviderWrapper>
+      </IntlProviderWrapper>,
+    )
+
+    await clickButtonByTestId('stateSortButton')
+
+    const tableRows = await screen.findAllByTestId('tableRow')
+    expect(tableRows[0]).toHaveTextContent('Bono Stingsson')
+    expect(tableRows[1]).toHaveTextContent('Jon Harring Sr.')
+
+    await clickButtonByTestId('stateSortButton')
 
     const tableRows2 = await screen.findAllByTestId('tableRow')
     expect(tableRows2[0]).toHaveTextContent('Jon Harring Sr.')
