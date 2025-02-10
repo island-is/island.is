6247ba3e93ddd1b903f710ca663ee94f3cc2af7c
unakb <una@kolibri.is>
2025-02-10 12:22:15 +0000

    Update 20250206152023-update-case.js

--- apps/judicial-system/backend/migrations/20250206152023-update-case.js
+++ apps/judicial-system/backend/migrations/20250206152023-update-case.js
@@ -3,44 +3,48 @@
 const replaceEnum = require('sequelize-replace-enum-postgres').default
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    // Add the new boolean field
-    await queryInterface.addColumn('case', 'is_completed_without_ruling', {
-      type: Sequelize.BOOLEAN,
-      allowNull: true,
-    })
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize
+      .transaction(async (transaction) => {
+        await queryInterface.addColumn(
+          'case',
+          'is_completed_without_ruling',
+          {
+            type: Sequelize.BOOLEAN,
+            allowNull: true,
+          },
+          { transaction },
+        )
 
-    await queryInterface.sequelize.query(`
-      UPDATE "case"
-      SET "is_completed_without_ruling" = true
-      WHERE "decision" = 'COMPLETED_WITHOUT_RULING'
-    `)
-
-    await queryInterface.sequelize.query(`
-      UPDATE "case"
-      SET "decision" = 'ACCEPTING'
-      WHERE "decision" = 'COMPLETED_WITHOUT_RULING'
-    `)
-
-    // Replace the enum values
-    await replaceEnum({
-      queryInterface,
-      tableName: 'case',
-      columnName: 'decision',
-      newValues: [
-        'ACCEPTING',
-        'REJECTING',
-        'ACCEPTING_ALTERNATIVE_TRAVEL_BAN',
-        'ACCEPTING_PARTIALLY',
-        'DISMISSING',
-      ],
-      enumName: 'enum_case_decision',
-    })
+        await queryInterface.sequelize.query(
+          `UPDATE "case"
+         SET "is_completed_without_ruling" = TRUE,
+             "decision" = 'ACCEPTING'
+         WHERE "decision" = 'COMPLETED_WITHOUT_RULING'`,
+          { transaction },
+        )
+      })
+      .then(() => {
+        // replaceEnum does not support transactions
+        return replaceEnum({
+          queryInterface,
+          tableName: 'case',
+          columnName: 'decision',
+          newValues: [
+            'ACCEPTING',
+            'REJECTING',
+            'ACCEPTING_ALTERNATIVE_TRAVEL_BAN',
+            'ACCEPTING_PARTIALLY',
+            'DISMISSING',
+          ],
+          enumName: 'enum_case_decision',
+        })
+      })
   },
 
-  down: async (queryInterface) => {
-    // Revert the enum values
-    await replaceEnum({
+  async down(queryInterface) {
+    // replaceEnum does not support transactions
+    return replaceEnum({
       queryInterface,
       tableName: 'case',
       columnName: 'decision',
@@ -50,19 +54,24 @@ module.exports = {
         'ACCEPTING_ALTERNATIVE_TRAVEL_BAN',
         'ACCEPTING_PARTIALLY',
         'DISMISSING',
-        'COMPLETED_WITHOUT_RULING', // Adding back the removed value
+        'COMPLETED_WITHOUT_RULING',
       ],
       enumName: 'enum_case_decision',
-    })
+    }).then(() =>
+      queryInterface.sequelize.transaction(async (transaction) => {
+        await queryInterface.sequelize.query(
+          `UPDATE "case"
+           SET "decision" = 'COMPLETED_WITHOUT_RULING'
+           WHERE "decision" = 'ACCEPTING' AND "is_completed_without_ruling" = TRUE`,
+          { transaction },
+        )
 
-    // Revert the enum value changes
-    await queryInterface.sequelize.query(`
-      UPDATE "case"
-      SET "decision" = 'COMPLETED_WITHOUT_RULING'
-      WHERE "decision" = 'ACCEPTING' AND "is_completed_without_ruling" = true
-    `)
-
-    // Remove the new boolean field
-    await queryInterface.removeColumn('case', 'is_completed_without_ruling')
+        await queryInterface.removeColumn(
+          'case',
+          'is_completed_without_ruling',
+          { transaction },
+        )
+      }),
+    )
   },
 }
