bb8cb9116229f982fbea2d4b7951ded6fe8c2ad9
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-02-10 09:24:13 +0000

    chore(j-s): Create new offenses db table (#17790)
    * fix(j-s): tooltip with correct defender label
    
    * chore(j-s): rename field that we are deprecating
    
    * fix(j-s): finish rename and fix migration
    
    * chore(j-s): migration for new table + bakend model
    
    * chore(j-s): add three new controller methods in the backend
    
    * chore(j-s): add api input models and resolver
    
    * fix(j-s): imports
    
    * fix(j-s): format
    
    * fix(j-s): format
    
    * fix(j-s): refactor offense exists guard
    
    * fix(j-s): refactor shared models and fix imports
    
    * fix(j-s): reference indictment count guards on new offense endpoints
    
    * fix(j-s): format
    
    * chore(j-s): add create offense test
    
    * chore(j-s): add test for update and delete offense endpoints
    
    * fix(j-s): format
    
    * fix(j-s): offense exists guard
    
    * fix(j-s): fix copy paste bugs
    
    * fix(j-s): unit test
    
    * fix(j-s): type for substance map

--- apps/judicial-system/backend/src/app/modules/indictment-count/test/deleteOffense.spec.ts
+++ apps/judicial-system/backend/src/app/modules/indictment-count/test/deleteOffense.spec.ts
@@ -3,7 +3,7 @@ import { uuid } from 'uuidv4'
 import { createTestingIndictmentCountModule } from './createTestingIndictmentCountModule'
 
 import { DeleteResponse } from '../models/delete.response'
-import { IndictmentCount } from '../models/indictmentCount.model'
+import { Offense } from '../models/offense.model'
 
 interface Then {
   result: DeleteResponse
@@ -13,25 +13,31 @@ interface Then {
 type GivenWhenThen = (
   caseId: string,
   indictmentCountId: string,
+  offenseId: string,
 ) => Promise<Then>
 
-describe('IndictmentCountController - Delete', () => {
-  let mockIndictmentCountModel: typeof IndictmentCount
+describe('IndictmentCountController - Delete offense', () => {
+  let mockOffenseModel: typeof Offense
   let givenWhenThen: GivenWhenThen
 
   beforeEach(async () => {
-    const { indictmentCountModel, indictmentCountController } =
+    const { offenseModel, indictmentCountController } =
       await createTestingIndictmentCountModule()
 
-    mockIndictmentCountModel = indictmentCountModel
+    mockOffenseModel = offenseModel
 
-    givenWhenThen = async (caseId: string, indictmentCountId: string) => {
+    givenWhenThen = async (
+      caseId: string,
+      indictmentCountId: string,
+      offenseId: string,
+    ) => {
       const then = {} as Then
 
       try {
-        then.result = await indictmentCountController.delete(
+        then.result = await indictmentCountController.deleteOffense(
           caseId,
           indictmentCountId,
+          offenseId,
         )
       } catch (error) {
         then.error = error as Error
@@ -41,36 +47,38 @@ describe('IndictmentCountController - Delete', () => {
     }
   })
 
-  describe('indictment count deleted', () => {
+  describe('offense deleted', () => {
     const caseId = uuid()
     const indictmentCountId = uuid()
+    const offenseId = uuid()
     let then: Then
 
     beforeEach(async () => {
-      const mockDestroy = mockIndictmentCountModel.destroy as jest.Mock
+      const mockDestroy = mockOffenseModel.destroy as jest.Mock
       mockDestroy.mockResolvedValueOnce(1)
 
-      then = await givenWhenThen(caseId, indictmentCountId)
+      then = await givenWhenThen(caseId, indictmentCountId, offenseId)
     })
 
-    it('should delete the indictment count', () => {
-      expect(mockIndictmentCountModel.destroy).toHaveBeenCalledWith({
-        where: { id: indictmentCountId, caseId },
+    it('should delete the offense', () => {
+      expect(mockOffenseModel.destroy).toHaveBeenCalledWith({
+        where: { id: offenseId, indictmentCountId },
       })
       expect(then.result).toEqual({ deleted: true })
     })
   })
 
-  describe('indictment count deletion fails', () => {
+  describe('offense deletion fails', () => {
     const caseId = uuid()
     const indictmentCountId = uuid()
+    const offenseId = uuid()
     let then: Then
 
     beforeEach(async () => {
-      const mockDestroy = mockIndictmentCountModel.destroy as jest.Mock
+      const mockDestroy = mockOffenseModel.destroy as jest.Mock
       mockDestroy.mockRejectedValueOnce(new Error('Some error'))
 
-      then = await givenWhenThen(caseId, indictmentCountId)
+      then = await givenWhenThen(caseId, indictmentCountId, offenseId)
     })
 
     it('should throw Error', () => {
