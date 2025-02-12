27fbaa9506352a03f7c4a52e06fae8ebe60dd6c5
disaerna <disa@hugsmidjan.is>
2025-02-12 11:00:12 +0000

    feat: text and layout

--- libs/portals/my-pages/health/src/screens/Vaccinations/VaccinationsWrapper.tsx
+++ libs/portals/my-pages/health/src/screens/Vaccinations/VaccinationsWrapper.tsx
@@ -1,28 +1,19 @@
-import {
-  Box,
-  Button,
-  Inline,
-  SkeletonLoader,
-  Tabs,
-  Tag,
-  Text,
-} from '@island.is/island-ui/core'
+import { Box, Button, SkeletonLoader, Tabs } from '@island.is/island-ui/core'
 import { useLocale, useNamespaces } from '@island.is/localization'
 import {
   EmptyTable,
   HEALTH_DIRECTORATE_SLUG,
   IntroWrapper,
   LinkButton,
-  Modal,
 } from '@island.is/portals/my-pages/core'
 import { Problem } from '@island.is/react-spa/shared'
 import { isDefined } from '@island.is/shared/utils'
+import { useState } from 'react'
 import { messages as m } from '../../lib/messages'
 import { SECTION_GAP } from '../../utils/constants'
+import StatusModal from './StatusModal'
 import { useGetVaccinationsQuery } from './Vaccinations.generated'
 import { SortedVaccinationsTable } from './tables/SortedVaccinationsTable'
-import { useState } from 'react'
-import StatusModal from './StatusModal'
 
 export const VaccinationsWrapper = () => {
   useNamespaces('sp.health')
@@ -99,48 +90,6 @@ export const VaccinationsWrapper = () => {
       {/* Tabs content */}
       {!loading && !error && (
         <Box paddingY={SECTION_GAP}>
-          <Box display="flex" flexWrap="wrap" marginBottom={3}>
-            <Box width="half">
-              <Inline>
-                <Tag variant="blue" outlined disabled>
-                  {formatMessage(m.vaccineUnfinished)}
-                </Tag>
-                <Text fontWeight="medium" variant="medium">
-                  'bleblebleb'
-                </Text>
-              </Inline>
-            </Box>
-            <Box width="half">
-              <Text fontWeight="medium" variant="medium">
-                {formatMessage(m.vaccineExpired) + ': '}
-              </Text>
-            </Box>
-            <Box width="half">
-              <Text fontWeight="medium" variant="medium">
-                {formatMessage(m.vaccineUnvaccined) + ': '}
-              </Text>
-            </Box>
-            <Box width="half">
-              <Text fontWeight="medium" variant="medium">
-                {formatMessage(m.vaccineValid) + ': '}
-              </Text>
-            </Box>
-            <Box width="half">
-              <Text fontWeight="medium" variant="medium">
-                {formatMessage(m.vaccineFinished) + ': '}
-              </Text>
-            </Box>
-            <Box width="half">
-              <Text fontWeight="medium" variant="medium">
-                {formatMessage(m.vaccineUncertain) + ': '}
-              </Text>
-            </Box>
-            <Box width="half">
-              <Text fontWeight="medium" variant="medium">
-                {formatMessage(m.vaccineUnregistered) + ': '}
-              </Text>
-            </Box>
-          </Box>
           <Tabs
             label={''}
             tabs={tabs}
--- libs/portals/my-pages/health/src/utils/tagSelector.ts
+++ libs/portals/my-pages/health/src/utils/tagSelector.ts
@@ -4,27 +4,30 @@ import type { TagVariant } from '@island.is/island-ui/core'
 
 // Tag selector for expandable, sorting table in vaccinations
 export const tagSelector = (
-  status?: HealthDirectorateVaccinationStatusEnum,
+  status?: HealthDirectorateVaccinationStatusEnum | string,
 ): TagVariant => {
   if (!isDefined(status)) return 'blue'
 
   switch (status) {
     case HealthDirectorateVaccinationStatusEnum.complete:
-      return 'blue'
+      return 'mint'
+    case HealthDirectorateVaccinationStatusEnum.valid:
+      return 'mint'
+
+    case HealthDirectorateVaccinationStatusEnum.unvaccinated:
+      return 'red'
+
     case HealthDirectorateVaccinationStatusEnum.expired:
       return 'blue'
     case HealthDirectorateVaccinationStatusEnum.incomplete:
       return 'blue'
     case HealthDirectorateVaccinationStatusEnum.rejected:
-      return 'purple'
+      return 'blue'
     case HealthDirectorateVaccinationStatusEnum.undetermined:
       return 'purple'
     case HealthDirectorateVaccinationStatusEnum.undocumented:
       return 'purple'
-    case HealthDirectorateVaccinationStatusEnum.unvaccinated:
-      return 'red'
-    case HealthDirectorateVaccinationStatusEnum.valid:
-      return 'mint'
+
     default:
       return 'blue'
   }
