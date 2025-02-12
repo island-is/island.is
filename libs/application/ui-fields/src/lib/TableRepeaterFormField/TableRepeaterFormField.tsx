7a337ded34f2da107d42740aa454de13a3680494
hfhelgason <hfhelgason@deloitte.is>
2025-02-12 15:14:21 +0000

    fix(application-system): Cancel button deleting table repeater entry when editing

--- libs/application/ui-fields/src/lib/TableRepeaterFormField/TableRepeaterFormField.tsx
+++ libs/application/ui-fields/src/lib/TableRepeaterFormField/TableRepeaterFormField.tsx
@@ -71,6 +71,7 @@ export const TableRepeaterFormField: FC<Props> = ({
     control: methods.control,
     name: data.id,
   })
+  const [isEditing, setIsEditing] = useState(false)
 
   const values = useWatch({ name: data.id, control: methods.control })
   const activeField = activeIndex >= 0 ? fields[activeIndex] : null
@@ -90,13 +91,18 @@ export const TableRepeaterFormField: FC<Props> = ({
     })
 
     if (isValid) {
+      // methods.setValue(`${data.id}[${index}]`, values[index])
       setActiveIndex(-1)
     }
+    setIsEditing(false)
   }
 
   const handleCancelItem = (index: number) => {
     setActiveIndex(-1)
-    remove(index)
+    if (!isEditing) {
+      remove(index)
+    }
+    setIsEditing(false)
   }
 
   const handleNewItem = () => {
@@ -113,6 +119,7 @@ export const TableRepeaterFormField: FC<Props> = ({
 
   const handleEditItem = (index: number) => {
     setActiveIndex(index)
+    setIsEditing(true)
   }
 
   const formatTableValue = (key: string, item: Record<string, string>) => {
