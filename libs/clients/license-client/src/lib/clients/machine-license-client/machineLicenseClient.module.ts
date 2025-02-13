3daeec7e1d2f32b768c004d8f4267072e01e0502
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-02-13 09:59:51 +0000

    fix: remove import

--- libs/clients/license-client/src/lib/clients/machine-license-client/machineLicenseClient.module.ts
+++ libs/clients/license-client/src/lib/clients/machine-license-client/machineLicenseClient.module.ts
@@ -5,12 +5,10 @@ import { MachineLicenseClient } from './machineLicenseClient.service'
 import { MachineDigitalLicenseClientConfig } from './machineLicenseClient.config'
 import { SmartSolutionsApiClientModule } from '@island.is/clients/smartsolutions'
 import { SmartSolutionsModule } from '@island.is/clients/smart-solutions-v2'
-import { FeatureFlagModule } from '@island.is/nest/feature-flags'
 import { PkPassService } from '../../helpers/pk-pass-service/pkPass.service'
 
 @Module({
   imports: [
-    FeatureFlagModule,
     AdrAndMachineLicenseClientModule,
     SmartSolutionsApiClientModule.registerAsync({
       useFactory: (
