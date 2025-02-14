877a8393ad026068afccdce63864a343747573a0
Ívar Oddsson <ivaroddsson@gmail.com>
2025-02-14 17:43:52 +0000

    feat(j-s): Don't complete indictments until they are sent to the public prosecutors office (#17928)
    * Dont complete indictments until they are sent to PP
    
    * Only apply to fine and ruling cases

--- apps/judicial-system/web/src/routes/Shared/Cases/Cases.tsx
+++ apps/judicial-system/web/src/routes/Shared/Cases/Cases.tsx
@@ -31,9 +31,12 @@ import { PastCasesTable } from '@island.is/judicial-system-web/src/components/Ta
 import { TableWrapper } from '@island.is/judicial-system-web/src/components/Table/Table'
 import {
   CaseDecision,
+  CaseIndictmentRulingDecision,
   CaseListEntry,
   CaseState,
   CaseTransition,
+  EventType,
+  IndictmentDecision,
   User,
   UserRole,
 } from '@island.is/judicial-system-web/src/graphql/schema'
@@ -169,7 +172,18 @@ export const Cases: FC = () => {
 
       if (isDistrictCourtUser(user)) {
         if (isIndictmentCase(c.type)) {
-          return !isCompletedCase(c.state)
+          const sentToPublicProsecutor = c.eventLogs?.some(
+            (log) =>
+              log.eventType === EventType.INDICTMENT_SENT_TO_PUBLIC_PROSECUTOR,
+          )
+          const isFineOrRuling =
+            c.indictmentRulingDecision === CaseIndictmentRulingDecision.FINE ||
+            c.indictmentRulingDecision === CaseIndictmentRulingDecision.RULING
+
+          return (
+            !isCompletedCase(c.state) ||
+            (isFineOrRuling && !sentToPublicProsecutor)
+          )
         } else {
           return !(
             isCompletedCase(c.state) &&
