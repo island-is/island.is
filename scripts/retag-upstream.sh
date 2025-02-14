219568252d7987ca90ddc140e6c339da5a8dc219
Kristofer <kristofer@andes.is>
2025-02-14 09:53:38 +0000

    Add retag script

---
+++ scripts/retag-upstream.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ -z "${1:-}" ]]; then
+  echo "usage: $0 <image>"
+  exit 1
+fi
+
+: "${IMAGE:=${1}}"
+: "${UPSTREAM_REGISTRY:=docker.io}"
+: "${PUSH_REGISTRY:=$(aws configure get sso_account_id).dkr.ecr.eu-west-1.amazonaws.com}"
+
+set -x
+docker pull "${UPSTREAM_REGISTRY}/${IMAGE}"
+docker tag "${UPSTREAM_REGISTRY}/${IMAGE}" "${PUSH_REGISTRY}/${IMAGE}"
+docker push "${PUSH_REGISTRY}/${IMAGE}"
+set +x
+
+echo "DONE"
