1bfb7412e8286aa8ef14682539ef239c1cc40115
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-02-14 10:53:26 +0000

    use checkDelegation

--- libs/application/templates/secondary-school/src/utils/checkIsActor.ts
+++ libs/application/templates/secondary-school/src/utils/checkIsActor.ts
@@ -1,6 +1,6 @@
 import { BffUser } from '@island.is/shared/types'
+import { checkDelegation } from '@island.is/shared/utils'
 
 export const checkIsActor = (user: BffUser | null): boolean => {
-  const isActor = !!user?.profile?.actor?.nationalId
-  return isActor
+  return user ? checkDelegation(user) : false
 }
