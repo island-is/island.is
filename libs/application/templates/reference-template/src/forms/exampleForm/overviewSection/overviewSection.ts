f2bc1d25f20694099d193afaa77a2890569ebfec
Jónas G. Sigurðsson <jonas@norda.is>
2025-02-17 10:44:55 +0000

    feat(app-sys): shared overview component (#17758)
    * feat: start of shared overview component
    
    * feat: rest of shared Overview component
    
    * fix: message change
    
    * chore: remove as keyword
    
    * chore: make thickness config for Divider component
    
    * feat: start of table overview
    
    * feat: rest of table overview and some attachments styles
    
    * chore: remove duplicate section
    
    ---------
    
    Co-authored-by: Jón Levy <levy@andes.is>

--- libs/application/templates/reference-template/src/forms/exampleForm/overviewSection/overviewSection.ts
+++ libs/application/templates/reference-template/src/forms/exampleForm/overviewSection/overviewSection.ts
@@ -2,11 +2,17 @@ import {
   buildSection,
   buildMultiField,
   buildDescriptionField,
-  buildCustomField,
   buildSubmitField,
+  buildOverviewField,
 } from '@island.is/application/core'
 import { m } from '../../../lib/messages'
 import { DefaultEvents } from '@island.is/application/types'
+import {
+  getAttachmentsData,
+  getOverviewItems,
+  getSumItems,
+  getTableData,
+} from '../../../utils/overviewUtils'
 
 export const overviewSection = buildSection({
   id: 'overview',
@@ -18,11 +24,36 @@ export const overviewSection = buildSection({
         buildDescriptionField({
           id: 'overview',
           title: 'Overview',
-          description: m.overviewDescription,
+          description: m.overviewDescriptionText,
         }),
-        buildCustomField({
-          id: 'customComponent',
-          component: 'Overview',
+        buildOverviewField({
+          id: 'overviewX',
+          title: 'Upplýsingar um þig',
+          description: m.overviewInfoDescripton,
+          backId: 'testBack.id',
+          bottomLine: false,
+          items: getOverviewItems,
+        }),
+        buildOverviewField({
+          id: 'overviewY',
+          title: 'Summing up numbers',
+          backId: 'testBack.id',
+          bottomLine: false,
+          items: getSumItems,
+        }),
+        buildOverviewField({
+          id: 'overviewZ',
+          title: 'Table overview',
+          backId: 'testBack.id',
+          tableData: getTableData,
+        }),
+        buildOverviewField({
+          id: 'overviewXX',
+          title: 'File overview',
+          description: m.overviewFileDescription,
+          backId: 'testBack.id',
+          bottomLine: true,
+          attachments: getAttachmentsData,
         }),
         buildSubmitField({
           id: 'submitApplication',
