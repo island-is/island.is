41a4c96f13544ca08e698c872778155a3082c765
Verónika Sif Sigurðardóttir <veronikasif@hotmail.com>
2025-02-17 10:51:44 +0000

    feat(new-primary-school): Update sendApplication (#17962)
    * [TS-968] Update sendApplication
    
    * Fix after review
    
    * Changed guardian role after review

--- libs/application/templates/new-primary-school/src/lib/newPrimarySchoolUtils.ts
+++ libs/application/templates/new-primary-school/src/lib/newPrimarySchoolUtils.ts
@@ -129,19 +129,19 @@ export const getApplicationAnswers = (answers: Application['answers']) => {
     'allergiesAndIntolerances.hasConfirmedMedicalDiagnoses',
   ) as YesOrNo
 
-  const requestMedicationAssistance = getValueViaPath(
+  const requestsMedicationAdministration = getValueViaPath(
     answers,
-    'allergiesAndIntolerances.requestMedicationAssistance',
+    'allergiesAndIntolerances.requestsMedicationAdministration',
   ) as YesOrNo
 
-  const developmentalAssessment = getValueViaPath(
+  const hasDiagnoses = getValueViaPath(
     answers,
-    'support.developmentalAssessment',
+    'support.hasDiagnoses',
   ) as YesOrNo
 
-  const specialSupport = getValueViaPath(
+  const hasHadSupport = getValueViaPath(
     answers,
-    'support.specialSupport',
+    'support.hasHadSupport',
   ) as YesOrNo
 
   const hasIntegratedServices = getValueViaPath(
@@ -164,13 +164,16 @@ export const getApplicationAnswers = (answers: Application['answers']) => {
     'support.caseManager.email',
   ) as string
 
-  const requestMeeting = getValueViaPath(
+  const requestingMeeting = getValueViaPath(
     answers,
-    'support.requestMeeting[0]',
+    'support.requestingMeeting[0]',
     NO,
   ) as YesOrNo
 
-  const startDate = getValueViaPath(answers, 'startDate') as string
+  const expectedStartDate = getValueViaPath(
+    answers,
+    'expectedStartDate',
+  ) as string
 
   const schoolMunicipality = getValueViaPath(
     answers,
@@ -218,15 +221,15 @@ export const getApplicationAnswers = (answers: Application['answers']) => {
     otherAllergies,
     usesEpiPen,
     hasConfirmedMedicalDiagnoses,
-    requestMedicationAssistance,
-    developmentalAssessment,
-    specialSupport,
+    requestsMedicationAdministration,
+    hasDiagnoses,
+    hasHadSupport,
     hasIntegratedServices,
     hasCaseManager,
     caseManagerName,
     caseManagerEmail,
-    requestMeeting,
-    startDate,
+    requestingMeeting,
+    expectedStartDate,
     schoolMunicipality,
     selectedSchool,
     currentNurseryMunicipality,
