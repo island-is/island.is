b3005bf99a5288d12b1701ac6d59657f72c0743e
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-02-18 17:02:07 +0000

    fix: change structure

---
+++ libs/clients/grants/src/clients/rannis/rannisGrants.types.ts
@@ -0,0 +1,11 @@
+export type RannisGrantResponse = [
+  {
+    fundid: string
+    fund_name_is: string
+    fund_name_en: string
+    datefrom: string
+    dateto: string
+    fund_url: string
+    is_open: string
+  },
+]
--- libs/clients/grants/src/lib/clients/rannis/src/lib/dtos/rannisGrant.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface RannisGrantDto {
-  id: string
-  nameIs: string
-  nameEn: string
-  dateFrom: string
-  dateTo: string
-  url: string
-  isOpen: boolean
-}
