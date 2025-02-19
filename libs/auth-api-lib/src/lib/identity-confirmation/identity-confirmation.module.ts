511b95d7118b9f3e7767326814edb6dffcdc4752
Gunnlaugur Guðmundsson <34029342+GunnlaugurG@users.noreply.github.com>
2025-02-19 11:27:54 +0000

    feat(auth-admin): Remove unused email dependency (#18032)
    * fix missing nova url
    
    * missing email contact
    
    * remove email client

--- libs/auth-api-lib/src/lib/identity-confirmation/identity-confirmation.module.ts
+++ libs/auth-api-lib/src/lib/identity-confirmation/identity-confirmation.module.ts
@@ -3,29 +3,20 @@ import { ConfigModule } from '@nestjs/config'
 import { SequelizeModule } from '@nestjs/sequelize'
 
 import { SmsModule, smsModuleConfig } from '@island.is/nova-sms'
-import { EmailModule, emailModuleConfig } from '@island.is/email-service'
 import { ZendeskModule, ZendeskServiceConfig } from '@island.is/clients/zendesk'
 
 import { IdentityConfirmationService } from './identity-confirmation.service'
 import { IdentityConfirmation } from './models/Identity-Confirmation.model'
 import { XRoadConfig } from '@island.is/nest/config'
-import { IdentityConfirmationApiConfig } from './config'
 
 @Module({
   imports: [
     ZendeskModule,
     SmsModule,
-    EmailModule,
     SequelizeModule.forFeature([IdentityConfirmation]),
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [
-        XRoadConfig,
-        ZendeskServiceConfig,
-        smsModuleConfig,
-        emailModuleConfig,
-        IdentityConfirmationApiConfig,
-      ],
+      load: [XRoadConfig, ZendeskServiceConfig, smsModuleConfig],
       envFilePath: ['.env', '.env.secret'],
     }),
   ],
