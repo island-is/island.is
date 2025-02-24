b24f72582584f1771c61978df687b3c751fbf489
lommi <jonorn@gmail.com>
2025-02-24 12:30:24 +0000

    fix: ok hehe

--- scripts/ci/docker/get-data.mjs
+++ scripts/ci/docker/get-data.mjs
@@ -3,10 +3,17 @@ import fs from "node:fs";
 import { execSync } from "node:child_process";
 import core from "@actions/core";
 import github from "@actions/github";
-const name = 'pr-18084';
+
+const context = github.context;
+const branch = getBranch();
+const typeOfDeployment = getTypeOfDeployment();
+const sha = context.sha;
+const name = getArtifactname();
 const url = `https://api.github.com/repos/island-is/island.is/actions/artifacts?name=${name}`;
+
 const _KEY_HAS_OUTPUT = 'MQ_HAS_OUTPUT';
 const _KEY_OUTPUT = 'MQ_OUTPUT';
+
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 await download();
@@ -29,7 +36,6 @@ async function download() {
         },
     });
     const zipBuffer = Buffer.from(await artifactZipResponse.arrayBuffer());
-    console.log(zipBuffer.toString('utf-8'));
     const zipFileName = '/tmp/artifact.zip';
     console.log(`Saved artifact to ${zipFileName}.`);
 
@@ -37,7 +43,6 @@ async function download() {
     fs.writeFileSync(zipFileName, zipBuffer);
 
     try {
-        execSync(`cat ${zipFileName}`, {stdio: 'inherit'});
         execSync(`unzip -o ${zipFileName} -d ${outputDir}`, { stdio: 'inherit' });
         console.log(`Unzipped artifact to ${outputDir}/`);
     } catch (err) {
@@ -55,4 +60,41 @@ async function download() {
     console.log(JSON.stringify(parsedData, null, 2));
     core.setOutput(_KEY_HAS_OUTPUT, "true");
     core.setOutput(_KEY_OUTPUT, parsedData);
+}
+
+function getBranch() {
+    if (context.eventName === "push") {
+        return context.ref.replace('refs/heads/', '');
+    }
+    throw new Error(`Unsupported event: ${context.eventName}`);
+}
+
+function getTypeOfDeployment() {
+    if (branch === 'main') {
+        return {
+            dev: true,
+            prod: false
+        }
+    }
+    if (branch.startsWith('release')) {
+        return {
+            dev: false,
+            prod: true
+        }
+    }
+    return {
+        dev: false,
+        prod: false
+    }
+}
+
+function getArtifactname() {
+    if (typeOfDeployment.dev) {
+        return `main-${sha}`;
+    }
+    if (typeOfDeployment.prod) {
+        return `release-${sha}`;
+    }
+    console.error(`UNSUPPORTED - WILL BE USING TEST CONST`);
+    return 'pr-18084';
 }
