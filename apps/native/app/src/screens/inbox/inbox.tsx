57f607f459a94555b0d56b57a41e8f5e9ff6a1ec
Þórey Jóna <thoreyjona@gmail.com>
2025-02-24 11:11:29 +0000

    feat(native-app): inbox bulk selection (#17927)
    * feat: first version of select all documents functionality
    
    * feat: add action bar and actions
    
    * feat: add loading to action bar and update cache for star and archive actions
    
    * feat: animate action bar in and out
    
    * feat: add feedback underlay color when action are clicked and wrap on smaller devices
    
    * feat: add toast to use for bulk selection actions
    
    * feat: add star back to inbox list items
    
    * fix: only show star if bookmarked and update cache when using bulk select to star documents
    
    * feat: short cut to selected state when pressing icon in list
    
    * fix: smaller screen width to wrap action bar items
    
    * fix: remove console.log
    
    * feat: make sure to reset select state when switching tabs
    
    * fix: minor fixes
    
    * fix: toast should be variant error when action errors
    
    * feat: add accessibility role and label to action items
    
    * fix: reusable function for toast errors

--- apps/native/app/src/screens/inbox/inbox.tsx
+++ apps/native/app/src/screens/inbox/inbox.tsx
@@ -15,6 +15,7 @@ import {
   NavigationFunctionComponent,
 } from 'react-native-navigation'
 import { useNavigationComponentDidAppear } from 'react-native-navigation-hooks/dist'
+import { useNavigationButtonPress } from 'react-native-navigation-hooks'
 import styled, { useTheme } from 'styled-components/native'
 
 import {
@@ -36,17 +37,23 @@ import {
   DocumentV2,
   useListDocumentsQuery,
   useGetDocumentsCategoriesAndSendersQuery,
+  usePostMailActionMutationMutation,
 } from '../../graphql/types/schema'
 import { createNavigationOptionHooks } from '../../hooks/create-navigation-option-hooks'
 import { useConnectivityIndicator } from '../../hooks/use-connectivity-indicator'
 import { navigateTo } from '../../lib/deep-linking'
 import { useOrganizationsStore } from '../../stores/organizations-store'
 import { useUiStore } from '../../stores/ui-store'
-import { ComponentRegistry } from '../../utils/component-registry'
+import {
+  ButtonRegistry,
+  ComponentRegistry,
+} from '../../utils/component-registry'
 import { testIDs } from '../../utils/test-ids'
 import { isAndroid } from '../../utils/devices'
 import { useApolloClient } from '@apollo/client'
 import { isIos } from '../../utils/devices'
+import { ActionBar } from './components/action-bar'
+import { Toast, ToastVariant } from './components/toast'
 
 type ListItem =
   | { id: string; type: 'skeleton' | 'empty' }
@@ -114,8 +121,23 @@ const { useNavigationOptions, getNavigationOptions } =
   )
 
 const PressableListItem = React.memo(
-  ({ item, listParams }: { item: DocumentV2; listParams: any }) => {
+  ({
+    item,
+    listParams,
+    selectable,
+    selectedItems,
+    setSelectedItems,
+    setSelectedState,
+  }: {
+    item: DocumentV2
+    listParams: any
+    selectable: boolean
+    selectedItems: string[]
+    setSelectedItems: (items: string[]) => void
+    setSelectedState: (state: boolean) => void
+  }) => {
     const { getOrganizationLogoUrl } = useOrganizationsStore()
+    const isSelected = selectable && selectedItems.includes(item.id)
     return (
       <InboxCard
         key={item.id}
@@ -126,12 +148,23 @@ const PressableListItem = React.memo(
         senderName={item.sender.name}
         icon={item.sender.name && getOrganizationLogoUrl(item.sender.name, 75)}
         isUrgent={item.isUrgent}
+        bookmarked={item.bookmarked}
+        selectable={selectable}
+        selected={isSelected}
+        onPressIcon={() => {
+          setSelectedState(true)
+          setSelectedItems([...selectedItems, item.id])
+        }}
         onPress={() =>
-          navigateTo(`/inbox/${item.id}`, {
-            title: item.sender.name,
-            isUrgent: item.isUrgent,
-            listParams,
-          })
+          selectable
+            ? isSelected
+              ? setSelectedItems(selectedItems.filter((id) => id !== item.id))
+              : setSelectedItems([...selectedItems, item.id])
+            : navigateTo(`/inbox/${item.id}`, {
+                title: item.sender.name,
+                isUrgent: item.isUrgent,
+                listParams,
+              })
         }
       />
     )
@@ -204,6 +237,17 @@ export const InboxScreen: NavigationFunctionComponent<{
   const queryString = useThrottleState(query)
   const [hiddenContent, setHiddenContent] = useState(isIos)
   const [refetching, setRefetching] = useState(false)
+
+  const [toastInfo, setToastInfo] = useState<{
+    variant: ToastVariant
+    title: string
+    message?: string
+  }>({
+    variant: 'success',
+    title: '',
+  })
+  const [toastVisible, setToastVisible] = useState(false)
+
   const pageRef = useRef(1)
   const loadingTimeout = useRef<ReturnType<typeof setTimeout>>()
   const isFilterApplied =
@@ -238,6 +282,26 @@ export const InboxScreen: NavigationFunctionComponent<{
   ])
 
   const [filters, setFilters] = useState(applyFilters(incomingFilters))
+  const [selectState, setSelectedState] = useState(false)
+  const [selectedItems, setSelectedItems] = useState<string[]>([])
+
+  const rightButtons = selectState
+    ? [
+        {
+          id: ButtonRegistry.InboxBulkSelectCancelButton,
+          text: intl.formatMessage({
+            id: 'inbox.bulkSelectCancelButton',
+          }),
+        },
+      ]
+    : [
+        {
+          id: ButtonRegistry.InboxBulkSelectButton,
+          text: intl.formatMessage({
+            id: 'inbox.bulkSelectButton',
+          }),
+        },
+      ]
 
   const res = useListDocumentsQuery({
     variables: {
@@ -250,11 +314,16 @@ export const InboxScreen: NavigationFunctionComponent<{
     },
   })
 
+  const [bulkSelectActionMutation, { loading: bulkSelectActionLoading }] =
+    usePostMailActionMutationMutation()
+
   const sendersAndCategories = useGetDocumentsCategoriesAndSendersQuery()
 
   const availableSenders = sendersAndCategories.data?.getDocumentSenders ?? []
   const availableCategories =
     sendersAndCategories.data?.getDocumentCategories ?? []
+  const allDocumentsSelected =
+    selectedItems.length === res.data?.documentsV2?.data?.length
 
   const [markAllAsRead, { loading: markAllAsReadLoading }] =
     useMarkAllDocumentsAsReadMutation({
@@ -290,8 +359,24 @@ export const InboxScreen: NavigationFunctionComponent<{
   useConnectivityIndicator({
     componentId,
     queryResult: res,
+    rightButtons: rightButtons,
     refetching: refetching || markAllAsReadLoading,
   })
+
+  useNavigationButtonPress(({ buttonId }) => {
+    if (buttonId === ButtonRegistry.InboxBulkSelectButton) {
+      setSelectedState(true)
+    }
+    if (buttonId === ButtonRegistry.InboxBulkSelectCancelButton) {
+      resetSelectState()
+    }
+    if (buttonId === ButtonRegistry.InboxBulkSelectAllButton) {
+      setSelectedItems(res.data?.documentsV2?.data.map((doc) => doc.id) ?? [])
+    }
+    if (buttonId === ButtonRegistry.InboxBulkDeselectAllButton) {
+      setSelectedItems([])
+    }
+  }, componentId)
 
   useEffect(() => {
     const appliedFilters = applyFilters(incomingFilters)
@@ -305,6 +390,39 @@ export const InboxScreen: NavigationFunctionComponent<{
     // Reset page on filter changes
     setFilters(appliedFilters)
   }, [incomingFilters, filters])
+
+  useEffect(() => {
+    // Make sure to reset select state when switching tabs
+    if (ui.selectedTab !== 0 && selectState) {
+      resetSelectState()
+    }
+  }, [ui.selectedTab, selectState])
+
+  useEffect(() => {
+    Navigation.mergeOptions(componentId, {
+      topBar: {
+        leftButtons: selectState
+          ? allDocumentsSelected
+            ? [
+                {
+                  id: ButtonRegistry.InboxBulkDeselectAllButton,
+                  text: intl.formatMessage({
+                    id: 'inbox.bulkDeselectAllButton',
+                  }),
+                },
+              ]
+            : [
+                {
+                  id: ButtonRegistry.InboxBulkSelectAllButton,
+                  text: intl.formatMessage({
+                    id: 'inbox.bulkSelectAllButton',
+                  }),
+                },
+              ]
+          : [],
+      },
+    })
+  }, [componentId, intl, selectState, allDocumentsSelected])
 
   const items = useMemo(() => res.data?.documentsV2?.data ?? [], [res.data])
   const isSearch = ui.inboxQuery.length > 2
@@ -356,6 +474,37 @@ export const InboxScreen: NavigationFunctionComponent<{
     setLoadingMore(false)
   }
 
+  const handleBulkActionError = (
+    actionType: 'star' | 'archive' | 'markAsRead',
+  ) => {
+    showToastForBulkSelectAction({
+      variant: 'error',
+      title: intl.formatMessage({
+        id: `inbox.bulkSelect.${actionType}Error`,
+      }),
+      message: intl.formatMessage({
+        id: 'inbox.bulkSelect.pleaseTryAgain',
+      }),
+    })
+  }
+
+  const showToastForBulkSelectAction = ({
+    variant,
+    title,
+    message,
+  }: {
+    variant: ToastVariant
+    title: string
+    message?: string
+  }) => {
+    setToastInfo({
+      variant: variant,
+      title: title,
+      message: message ?? undefined,
+    })
+    setToastVisible(true)
+  }
+
   useEffect(() => {
     Navigation.mergeOptions(ComponentRegistry.InboxScreen, {
       bottomTab: {
@@ -371,12 +520,36 @@ export const InboxScreen: NavigationFunctionComponent<{
         badge: unreadCount > 0 ? unreadCount.toString() : (null as any),
         badgeColor: theme.color.red400,
       },
+      topBar: {
+        rightButtons: selectState
+          ? [
+              {
+                id: ButtonRegistry.InboxBulkSelectCancelButton,
+                text: intl.formatMessage({
+                  id: 'inbox.bulkSelectCancelButton',
+                }),
+              },
+            ]
+          : [
+              {
+                id: ButtonRegistry.InboxBulkSelectButton,
+                text: intl.formatMessage({
+                  id: 'inbox.bulkSelectButton',
+                }),
+              },
+            ],
+      },
     })
-  }, [intl, theme, unreadCount])
+  }, [intl, theme, unreadCount, selectState])
 
   const keyExtractor = useCallback((item: ListItem) => {
     return item.id.toString()
   }, [])
+
+  const resetSelectState = () => {
+    setSelectedItems([])
+    setSelectedState(false)
+  }
 
   const onRefresh = useCallback(async () => {
     try {
@@ -422,13 +595,17 @@ export const InboxScreen: NavigationFunctionComponent<{
       return (
         <PressableListItem
           item={item as DocumentV2}
+          selectable={selectState}
+          selectedItems={selectedItems}
+          setSelectedItems={setSelectedItems}
+          setSelectedState={setSelectedState}
           listParams={{
             ...filters,
           }}
         />
       )
     },
-    [intl, filters],
+    [intl, filters, selectState, selectedItems, setSelectedItems],
   )
 
   const data = useMemo(() => {
@@ -510,6 +687,11 @@ export const InboxScreen: NavigationFunctionComponent<{
                 })}
                 value={query}
                 onChangeText={(text) => setQuery(text)}
+                onFocus={() => {
+                  if (selectState) {
+                    resetSelectState()
+                  }
+                }}
               />
               <Button
                 title={intl.formatMessage({
@@ -525,6 +707,7 @@ export const InboxScreen: NavigationFunctionComponent<{
                 icon={filterIcon}
                 iconStyle={{ tintColor: theme.color.blue400 }}
                 onPress={() => {
+                  resetSelectState()
                   navigateTo('/inbox-filter', {
                     opened,
                     archived,
@@ -674,6 +857,89 @@ export const InboxScreen: NavigationFunctionComponent<{
             </LoadingWrapper>
           ) : null
         }
+      />
+      {selectState && selectedItems.length ? (
+        <ActionBar
+          loading={bulkSelectActionLoading}
+          onClickStar={async () => {
+            const result = await bulkSelectActionMutation({
+              variables: {
+                input: { action: 'bookmark', documentIds: selectedItems },
+              },
+            })
+            if (result.data?.postMailActionV2?.success) {
+              // If success, update selected documents to be starred in the cache
+              selectedItems.forEach((id) => {
+                client.cache.modify({
+                  id: client.cache.identify({ __typename: 'DocumentV2', id }),
+                  fields: {
+                    bookmarked: () => true,
+                  },
+                })
+              })
+              resetSelectState()
+              showToastForBulkSelectAction({
+                variant: 'success',
+                title: intl.formatMessage({
+                  id: 'inbox.bulkSelect.starSuccess',
+                }),
+              })
+            } else {
+              handleBulkActionError('star')
+            }
+          }}
+          onClickArchive={async () => {
+            const result = await bulkSelectActionMutation({
+              variables: {
+                input: { action: 'archive', documentIds: selectedItems },
+              },
+            })
+            if (result.data?.postMailActionV2?.success) {
+              resetSelectState()
+              res.refetch()
+              showToastForBulkSelectAction({
+                variant: 'success',
+                title: intl.formatMessage({
+                  id: 'inbox.bulkSelect.archiveSuccess',
+                }),
+              })
+            } else {
+              handleBulkActionError('archive')
+            }
+          }}
+          onClickMarkAsRead={async () => {
+            const result = await bulkSelectActionMutation({
+              variables: {
+                input: { action: 'read', documentIds: selectedItems },
+              },
+            })
+            if (result.data?.postMailActionV2?.success) {
+              // If success, update selected documents to be marked as read in the cache
+              selectedItems.forEach((id) => {
+                client.cache.modify({
+                  id: client.cache.identify({ __typename: 'DocumentV2', id }),
+                  fields: {
+                    opened: () => true,
+                  },
+                })
+              })
+              resetSelectState()
+              showToastForBulkSelectAction({
+                variant: 'success',
+                title: intl.formatMessage({
+                  id: 'inbox.bulkSelect.markAsReadSuccess',
+                }),
+              })
+            } else {
+              handleBulkActionError('markAsRead')
+            }
+          }}
+        />
+      ) : null}
+      <Toast
+        {...toastInfo}
+        visible={toastVisible}
+        onHide={() => setToastVisible(false)}
       />
       <BottomTabsIndicator index={0} total={5} />
       {!isSearch && <TopLine scrollY={scrollY} />}
