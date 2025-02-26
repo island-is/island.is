2bdc85e7d6a0103a0579947f0c0ff9e05f66c3a2
Kristofer <kristofer@andes.is>
2025-02-26 17:12:51 +0000

    Tag unicorn-project, and use it

--- scripts/ci/unicorn-utils.mjs
+++ scripts/ci/unicorn-utils.mjs
@@ -1,13 +1,11 @@
 import { execSync } from 'child_process'
 import { workspaceRoot } from '@nx/devkit'
 
-const unicornApps = ['unicorn-app']
-
 const arg = JSON.parse(process.argv.slice(2))
 try {
   const affected = JSON.parse(
     execSync(
-      `cd ${workspaceRoot} && yarn nx show projects --affected --base ${arg.baseBranch} --json | jq -r`,
+      `cd ${workspaceRoot} && yarn nx show projects --affected --projects=tag:unicorn --base ${arg.baseBranch} --json | jq -r`,
     ).toString(),
   )
   console.log(affected.some((item) => unicornApps.includes(item)))
