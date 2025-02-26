5bed01245ad43de7764ef2d32c9368931ab9af95
lommi <jonorn@gmail.com>
2025-02-26 15:12:36 +0000

    fix: install 2

--- scripts/ci/docker/get-github-token.mjs
+++ scripts/ci/docker/get-github-token.mjs
@@ -11,19 +11,30 @@ if (!APP_ID || !PRIVATE_KEY) {
 }
 
 const authData = await githubAppJwt({
-    id: APP_ID,           
-    privateKey: PRIVATE_KEY  
+    id: APP_ID,
+    privateKey: PRIVATE_KEY
 });
 
+const response = await fetch('https://api.github.com/app/installations', {
+    method: 'GET',
+    headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `Bearer ${authData.token}`
+    }
+});
+const installations = await response.json();
+const INSTALL_ID = installations[0].id;
 
-const installationRest = await fetch(`https://api.github.com/app/installations/${APP_ID}/access_tokens`, {
+const actionRest = await fetch(`https://api.github.com/app/installations/${INSTALL_ID}/access_tokens`, {
+    method: 'post',
     headers: {
         Accept: "application/vnd.github.v3+json",
         Authorization: `Bearer ${authData.token}`
     }
 });
 
-const installationJson = await installationRest.json();
-core.setSecret(installationJson.token);
-core.setOutput('token', installationJson.token);
+const actionJson = await actionRest.json();
+
+core.setSecret(actionJson.token);
+core.setOutput('token', actionJson.token);
 
