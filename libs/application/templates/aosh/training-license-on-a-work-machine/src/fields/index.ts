46cd1ec46fb03c20927a146a3e2557e6fa935ffb
Sigrún Tinna Gissurardóttir <sigrun.tinna.gissurardottir@origo.is>
2025-02-27 08:33:06 +0000

    remove overview export

--- libs/application/templates/aosh/training-license-on-a-work-machine/src/fields/index.ts
+++ libs/application/templates/aosh/training-license-on-a-work-machine/src/fields/index.ts
@@ -1,4 +1,3 @@
-export { Overview } from './Overview'
 export { SameAsApplicantCheck } from './SameAsApplicantCheck'
 export { HandleReject } from './HandleReject'
 export { SetAnswersForCertificateOfTenure } from './SetAnswersForCertificateOfTenure'
