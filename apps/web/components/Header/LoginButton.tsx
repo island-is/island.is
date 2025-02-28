8a6293b6df47bfd6730564c725865b4a9032268e
runarvestmann <runar@stefna.is>
2025-02-28 13:24:52 +0000

    Revert "feat(web): Change my pages login button from being a dropdown to now being a link (#18068)"
    This reverts commit b9abff9ad9a3f07fae0f2ea6bea01361994bb554.

--- apps/web/components/Header/LoginButton.tsx
+++ apps/web/components/Header/LoginButton.tsx
@@ -1,30 +1,133 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import { useWindowSize } from 'react-use'
+import cn from 'classnames'
+import { useRouter } from 'next/router'
 
-import { Button, ButtonTypes } from '@island.is/island-ui/core'
+import {
+  Button,
+  ButtonTypes,
+  DropdownMenu,
+  Inline,
+  Logo,
+} from '@island.is/island-ui/core'
 import { theme } from '@island.is/island-ui/theme'
-import { ProjectBasePath } from '@island.is/shared/constants'
+import { webLoginButtonSelect } from '@island.is/plausible'
 import { useI18n } from '@island.is/web/i18n'
+import { LayoutProps } from '@island.is/web/layouts/main'
 
-export const LoginButton = (props: {
+const minarsidurLink = '/minarsidur/'
+const minarsidurDelegationsLink = '/bff/login?prompt=select_account'
+
+export function LoginButton(props: {
   colorScheme: ButtonTypes['colorScheme']
-}) => {
+  topItem?: LayoutProps['customTopLoginButtonItem']
+}) {
   const { t } = useI18n()
+  const router = useRouter()
   const { width } = useWindowSize()
+
+  function trackAndNavigate(
+    buttonType: 'Dropdown - Individuals' | 'Dropdown - Companies' | string,
+    event: MouseEvent<HTMLAnchorElement>,
+  ) {
+    event.preventDefault()
+    const href = event.currentTarget.href
+
+    // If the plausible script is not loaded (For example in case of adBlocker) the user will be navigated directly to /minarsidur.
+    if (window?.plausible) {
+      // In case the script is there, but the event is not firing (different adBlock settings) then the user is navigated without Plausible callback.
+      const id = window.setTimeout(() => {
+        window.location.assign(href)
+      }, 500)
+
+      // The plausible custom event.
+      webLoginButtonSelect(buttonType, () => {
+        window.clearTimeout(id)
+        window.location.assign(href)
+      })
+    } else {
+      window.location.assign(href)
+    }
+  }
+
+  const items = [
+    {
+      href: minarsidurLink,
+      title: (
+        <Inline alignY="center" space={1} flexWrap="nowrap">
+          {props.topItem && (
+            <Logo
+              width={17}
+              height={17}
+              iconOnly={true}
+              id="minar-sidur-individuals"
+              solid={false}
+            />
+          )}
+          {t.loginIndividuals}
+        </Inline>
+      ),
+      onClick: trackAndNavigate.bind(null, 'Dropdown - Individuals'),
+    },
+    {
+      href: minarsidurDelegationsLink,
+      title: (
+        <Inline alignY="center" space={1} flexWrap="nowrap">
+          {props.topItem && (
+            <Logo
+              width={17}
+              height={17}
+              iconOnly={true}
+              id="minar-sidur-companies"
+              solid={false}
+            />
+          )}
+          {t.loginDelegations}
+        </Inline>
+      ),
+      onClick: trackAndNavigate.bind(null, 'Dropdown - Companies'),
+    },
+  ]
+
+  if (
+    props.topItem &&
+    !props.topItem.blacklistedPathnames?.includes(
+      new URL(router.asPath, 'https://island.is').pathname,
+    )
+  ) {
+    items.unshift({
+      href: props.topItem.href,
+      title: (
+        <Inline alignY="center" space={1} flexWrap="nowrap">
+          {props.topItem.imgSrc && (
+            <img width={17} height={17} src={props.topItem.imgSrc} alt="" />
+          )}
+          {props.topItem.label}
+        </Inline>
+      ),
+      onClick: trackAndNavigate.bind(null, props.topItem.buttonType),
+    })
+  }
 
   const isMobile = width < theme.breakpoints.md
 
   return (
-    <a href={ProjectBasePath.ServicePortal} tabIndex={-1}>
-      <Button
-        colorScheme={props.colorScheme}
-        variant="utility"
-        icon="person"
-        title={isMobile ? t.login : undefined}
-        as="span"
-      >
-        {!isMobile && t.login}
-      </Button>
-    </a>
+    <DropdownMenu
+      fixed
+      disclosure={
+        <Button
+          colorScheme={props.colorScheme}
+          variant="utility"
+          icon="person"
+          title={isMobile ? t.login : undefined}
+        >
+          {!isMobile && t.login}
+        </Button>
+      }
+      openOnHover={!isMobile}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore make web strict
+      items={items}
+    />
   )
 }
--- apps/web/layouts/main.tsx
+++ apps/web/layouts/main.tsx
@@ -116,6 +116,7 @@ export interface LayoutProps {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error make web strict
   megaMenuData
+  customTopLoginButtonItem: LayoutComponentProps['customTopLoginButtonItem']
   children?: React.ReactNode
 }
 
@@ -158,6 +159,7 @@ const Layout: Screen<LayoutProps> = ({
   respOrigin,
   children,
   megaMenuData,
+  customTopLoginButtonItem,
 }) => {
   const { activeLocale, t } = useI18n()
   const { linkResolver } = useLinkResolver()
@@ -411,6 +413,7 @@ const Layout: Screen<LayoutProps> = ({
                       )
                     : undefined
                 }
+                customTopLoginButtonItem={customTopLoginButtonItem}
               />
             </ColorSchemeContext.Provider>
           )}
@@ -693,6 +696,13 @@ interface LayoutComponentProps {
   article?: GetSingleArticleQuery['getSingleArticle']
   customAlertBanner?: GetAlertBannerQuery['getAlertBanner']
   languageToggleQueryParams?: LayoutProps['languageToggleQueryParams']
+  customTopLoginButtonItem?: {
+    href: string
+    imgSrc: string
+    label: string
+    buttonType: string
+    blacklistedPathnames?: string[]
+  }
 }
 
 export const withMainLayout = <T, C extends ScreenContext>(
@@ -738,6 +748,9 @@ export const withMainLayout = <T, C extends ScreenContext>(
     const languageToggleQueryParams =
       layoutComponentProps?.languageToggleQueryParams
 
+    const customTopLoginButtonItem =
+      layoutComponentProps?.customTopLoginButtonItem
+
     return {
       layoutProps: {
         ...layoutProps,
@@ -747,6 +760,7 @@ export const withMainLayout = <T, C extends ScreenContext>(
         articleAlertBannerContent,
         customAlertBannerContent,
         languageToggleQueryParams,
+        customTopLoginButtonItem,
       },
       componentProps,
     }
