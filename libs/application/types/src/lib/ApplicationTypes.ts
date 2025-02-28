32ece8e01b44097a0e3f3e42a252823f772727c0
Heba <heba.farestveit@gmail.com>
2025-02-28 15:12:04 +0000

    Fix conflict.

--- libs/application/types/src/lib/ApplicationTypes.ts
+++ libs/application/types/src/lib/ApplicationTypes.ts
@@ -394,15 +394,13 @@ export const ApplicationConfigurations = {
     slug: 'nyskraning-taekis',
     translation: ['aosh.rnm.application'],
   },
-<<<<<<< HEAD
   [ApplicationTypes.RENTAL_AGREEMENT]: {
     slug: 'leigusamningur',
     translation: 'ra.application',
-=======
+  },
   [ApplicationTypes.TRAINING_LICENSE_ON_A_WORK_MACHINE]: {
     slug: 'kennslurettindi-a-vinnuvel',
     translation: ['aosh.tlwm.application'],
->>>>>>> 5bfb902b27a31707cfc64548f447ec689c99e293
   },
   [ApplicationTypes.SECONDARY_SCHOOL]: {
     slug: 'framhaldsskoli',
