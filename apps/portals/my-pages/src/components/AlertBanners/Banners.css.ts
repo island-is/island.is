c23bb0a53a6aa57282425fb34f429083dd7ef362
Ásdís Erna Guðmundsdóttir <disa@hugsmidjan.is>
2025-02-28 14:04:46 +0000

    fix(my-pages): navigation + alert bannar and animation speed (#18179)
    * fix: speed up animations
    
    * fix: speed and animation

--- apps/portals/my-pages/src/components/AlertBanners/Banners.css.ts
+++ apps/portals/my-pages/src/components/AlertBanners/Banners.css.ts
@@ -2,5 +2,5 @@ import { zIndex } from '@island.is/portals/my-pages/constants'
 import { style } from '@vanilla-extract/css'
 
 export const container = style({
-  zIndex: zIndex.header,
+  zIndex: zIndex.header + 10,
 })
