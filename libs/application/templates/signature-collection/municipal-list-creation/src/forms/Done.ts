5972e87c6ac45909300e20ebbe0979b8a69e1c70
albina <albina@kosmosogkaos.is>
2025-03-04 11:12:28 +0000

    messagecolor

--- libs/application/templates/signature-collection/municipal-list-creation/src/forms/Done.ts
+++ libs/application/templates/signature-collection/municipal-list-creation/src/forms/Done.ts
@@ -80,6 +80,7 @@ export const Done: Form = buildForm({
               url: '/minarsidur/min-gogn/listar/althingis-medmaelasofnun',
               buttonTitle: m.linkFieldButtonTitle,
               message: m.linkFieldMessage,
+              messageColor: 'blue600',
             }),
             buildMessageWithLinkButtonField({
               condition: (_, externalData) => {
@@ -94,6 +95,7 @@ export const Done: Form = buildForm({
               url: '/minarsidur/fyrirtaeki/listar/althingis-medmaelasofnun',
               buttonTitle: m.linkFieldButtonCompanyTitle,
               message: m.linkFieldCompanyMessage,
+              messageColor: 'blue600',
             }),
             buildDescriptionField({
               id: 'doneScreenSpace',
