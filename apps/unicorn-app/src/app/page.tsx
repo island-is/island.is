cacef6d3864aacbe9639be352551d0eb0bec0bbf
Róberta Andersen <roberta@andes.is>
2025-03-05 14:42:30 +0000

    fix: unicorn touched

--- apps/unicorn-app/src/app/page.tsx
+++ apps/unicorn-app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span role="img" aria-label="unicorn">
-                Welcome unicorn 🦄 Q-Test
+                Welcome unicorn 🦄 Hey there prod
               </span>
             </h1>
           </div>
