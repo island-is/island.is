fdf6ef1fbadf255eea493b6925dd06cac062fc29
Jonni <jonas@norda.is>
2025-03-06 11:04:58 +0000

    fix: typo

--- libs/application/templates/examples/example-inputs/src/forms/mainForm/compositeFieldsSection/applicantInfoSubsection.ts
+++ libs/application/templates/examples/example-inputs/src/forms/mainForm/compositeFieldsSection/applicantInfoSubsection.ts
@@ -2,7 +2,7 @@ import { buildSubSection } from '@island.is/application/core'
 import { applicantInformationMultiField } from '@island.is/application/ui-forms'
 
 export const applicantInfoSubsection = buildSubSection({
-  id: 'aplicantInfoSubsection',
+  id: 'applicantInfoSubsection',
   title: 'Applicant Info Subsection',
   // Common form, fills automatically with applicant information
   children: [applicantInformationMultiField()],
