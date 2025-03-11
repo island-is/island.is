61bd93c621de0ae4881ce8c4b4ab3f67fe5d8781
Jón Levy <levy@apro.is>
2025-03-11 10:59:46 +0000

    ci: declare future release branches

--- scripts/ci/docker/const.mjs
+++ scripts/ci/docker/const.mjs
@@ -1,2 +1,13 @@
 export const MAIN_BRANCHES = ['main']
-export const RELEASE_BRANCHES = ['release', 'release/35.1.0'];
+
+// NOTE: hack until we move to stable release branch
+export const RELEASE_BRANCHES = [
+  'release',
+  'release/35.1.0',
+  'release/35.2.0',
+  'release/35.3.0',
+  'release/36.1.0',
+  'release/36.1.0',
+  'release/36.1.0',
+  'release/35.1.0',
+]
