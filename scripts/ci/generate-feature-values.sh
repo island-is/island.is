7434ce29e49d3297708326f52a4650277338bfbd
Jón Levy <levy@apro.is>
2025-03-14 14:22:00 +0000

    fix: added generate-feature-values script

--- scripts/ci/generate-feature-values.sh
+++ scripts/ci/generate-feature-values.sh
@@ -6,21 +6,21 @@ DOCKER_TAG=$2
 IMAGES=$3
 
 GIT_ROOT="$(git rev-parse --show-toplevel)"
-INFRA_ROOT="${GIT_ROOT}/infra"
+# INFRA_ROOT="${GIT_ROOT}"
 ECR_REPO='821090935708.dkr.ecr.eu-west-1.amazonaws.com/helm-config'
 FEATURE_DIR="${GIT_ROOT}/charts/features"
 mkdir -p "$FEATURE_DIR"
 
-cd "$INFRA_ROOT"
+# cd "$INFRA_ROOT"
 
-yarn feature-env \
+yarn --cwd infra feature-env \
   jobs \
   --feature "$FEATURE_NAME" \
   --images "$IMAGES" \
   --chart islandis \
   --jobImage "${ECR_REPO}:${DOCKER_TAG}" >"${FEATURE_DIR}/${FEATURE_NAME}-job-manifest.yaml"
 
-yarn feature-env \
+yarn --cwd infra feature-env \
   values \
   --chart islandis \
   --feature "$FEATURE_NAME" \
