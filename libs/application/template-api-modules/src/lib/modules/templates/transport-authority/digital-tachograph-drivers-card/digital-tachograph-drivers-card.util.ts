f619524680fc0f9d75fd29af9b5455e2a6a48ba3
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-03-14 09:27:27 +0000

    fix(digital tachograph drivers card): Change api fetch endpoint for photo and signature (#18373)
    * Call SGS when fetching photo + signature
    
    * Dont remove first and last char in base64 for photo/signature

--- libs/application/template-api-modules/src/lib/modules/templates/transport-authority/digital-tachograph-drivers-card/digital-tachograph-drivers-card.util.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/transport-authority/digital-tachograph-drivers-card/digital-tachograph-drivers-card.util.ts
@@ -2,6 +2,6 @@ export const getUriFromImageStr = (
   imageData: string | undefined | null,
 ): string | null => {
   return imageData?.length
-    ? `data:image/jpeg;base64,${imageData.substring(1, imageData.length - 1)}`
+    ? `data:image/jpeg;base64,${imageData.substring(0, imageData.length)}`
     : null
 }
