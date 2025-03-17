4f62c7461eb3c302967d27c3dff9a6c6e3521209
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-03-17 16:14:33 +0000

    fix(j-s): fix selector

--- apps/judicial-system/web/src/routes/Shared/AddFiles/SelectCaseFileRepresentative.tsx
+++ apps/judicial-system/web/src/routes/Shared/AddFiles/SelectCaseFileRepresentative.tsx
@@ -1,27 +1,37 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { useIntl } from 'react-intl'
 
 import { Box, Select } from '@island.is/island-ui/core'
+import { capitalize } from '@island.is/judicial-system/formatters'
 import {
   BlueBox,
   DateTime,
   FormContext,
   SectionHeading,
 } from '@island.is/judicial-system-web/src/components'
+import { CaseRepresentative } from '@island.is/judicial-system-web/src/graphql/schema'
+import { ReactSelectOption } from '@island.is/judicial-system-web/src/types'
 
 import { strings } from './AddFiles.strings'
 import * as styles from './SelectCaseFileRepresentative.css'
+
+type RepresentativeSelectOption = ReactSelectOption & {
+  caseRepresentative: CaseRepresentative
+}
 
 export const SelectCaseFileRepresentative = () => {
   const { workingCase } = useContext(FormContext)
   const { formatMessage } = useIntl()
   const { caseRepresentatives } = workingCase
-  const options = caseRepresentatives?.map(({ name }) => ({
-    label: name,
-    value: name,
+  const options = caseRepresentatives?.map((representative) => ({
+    label: `${representative.name} (${capitalize(representative.title)})`,
+    value: representative.name,
+    caseRepresentative: representative,
   }))
+  const [caseFileRepresentative, setCaseFileRepresentative] = useState(
+    {} as RepresentativeSelectOption,
+  )
 
-  const defaultCaseRepresentative = null
   return (
     <div className={styles.selectCaseFileRepresentative}>
       <SectionHeading
@@ -33,13 +43,14 @@ export const SelectCaseFileRepresentative = () => {
             name="caseRepresentative"
             label={formatMessage(strings.caseRepresentativeLabel)}
             placeholder={formatMessage(strings.caseRepresentativePlaceholder)}
-            value={defaultCaseRepresentative}
+            value={caseFileRepresentative}
             options={options}
-            // onChange={(selectedOption) =>
-            //   setJudge((selectedOption as JudgeSelectOption).judge.id)
-            // }
+            onChange={(selectedOption) =>
+              setCaseFileRepresentative(
+                selectedOption as RepresentativeSelectOption,
+              )
+            }
             required
-            // isDisabled={usersLoading}
           />
           <DateTime
             name="fileSubmittedDate"
