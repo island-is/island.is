35611003a9b0c0e525ad56cb544be363139b140b
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-03-18 11:24:35 +0000

    fix: adds

--- libs/portals/my-pages/assets/src/screens/VehicleBulkMileage/VehicleBulkMileageFileDownloader.tsx
+++ libs/portals/my-pages/assets/src/screens/VehicleBulkMileage/VehicleBulkMileageFileDownloader.tsx
@@ -1,7 +1,16 @@
-import { DropdownMenu } from '@island.is/island-ui/core'
+import { Button, DropdownMenu } from '@island.is/island-ui/core'
 import { useLocale } from '@island.is/localization'
 import { vehicleMessage } from '../../lib/messages'
 import { downloadFile } from '@island.is/portals/my-pages/core'
+import { useVehiclesListQuery } from './VehicleBulkMileage.generated'
+import { useMemo } from 'react'
+
+interface TableData {
+  bilnumber: unknown[]
+  'seinasta skraning': unknown[]
+  'seinasta skrada stada': unknown[]
+  kilometrastada: unknown[]
+}
 
 interface Props {
   onError: (error: string) => void
@@ -10,20 +19,54 @@ interface Props {
 const VehicleBulkMileageFileDownloader = ({ onError }: Props) => {
   const { formatMessage } = useLocale()
 
-  const downloadExampleFile = async (type: 'csv' | 'xlsx') => {
+  const { data, error, loading } = useVehiclesListQuery({
+    variables: {
+      input: {
+        page: 1,
+        pageSize: 1000,
+        filterOnlyRequiredMileageRegistration: true,
+      },
+    },
+  })
+
+  const table = useMemo(() => {
+    if (data?.vehiclesListV3?.data) {
+      const table: TableData = {
+        bilnumber: [],
+        'seinasta skraning': [],
+        'seinasta skrada stada': [],
+        kilometrastada: [],
+      }
+
+      data.vehiclesListV3.data.forEach((vehicle) => {
+        table['bilnumber'].push(vehicle.vehicleId)
+        table['seinasta skraning'].push(
+          vehicle.mileageDetails?.lastMileageRegistration?.date,
+        )
+        table['seinasta skrada stada'].push(
+          vehicle.mileageDetails?.lastMileageRegistration?.mileage ?? 0,
+        )
+      })
+      return table
+    }
+  }, [data])
+
+  const downloadExampleFile = async (type: 'csv' | 'xlsx', data: TableData) => {
+    const header = Object.keys(data)
+    const rows = Object.values(data)
     try {
-      downloadFile(
-        `magnskraning_kilometrastodu_example`,
-        ['bilnumer', 'kilometrastada'],
-        [
-          ['ABC001', 10000],
-          ['DEF002', 99999],
-        ],
-        type,
-      )
+      downloadFile(`magnskraning_kilometrastodu_example`, header, rows, type)
     } catch (error) {
       onError(error)
     }
+  }
+
+  if (loading) {
+    return <Button loading />
+  }
+
+  if (!table) {
+    return
   }
 
   return (
@@ -32,7 +75,7 @@ const VehicleBulkMileageFileDownloader = ({ onError }: Props) => {
       menuLabel={formatMessage(vehicleMessage.downloadTemplate)}
       items={(['csv', 'xlsx'] as const).map((type) => ({
         title: `.${type}`,
-        onClick: () => downloadExampleFile(type),
+        onClick: () => downloadExampleFile(type, table),
       }))}
       title={formatMessage(vehicleMessage.downloadTemplate)}
     />
