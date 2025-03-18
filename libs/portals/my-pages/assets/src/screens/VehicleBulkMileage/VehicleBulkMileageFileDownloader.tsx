16b57cc33984f9636bdb7c6cdb9dbb8788b549bd
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-03-18 14:24:10 +0000

    feat: update file

--- libs/portals/my-pages/assets/src/screens/VehicleBulkMileage/VehicleBulkMileageFileDownloader.tsx
+++ libs/portals/my-pages/assets/src/screens/VehicleBulkMileage/VehicleBulkMileageFileDownloader.tsx
@@ -1,83 +1,66 @@
-import { Button, DropdownMenu } from '@island.is/island-ui/core'
+import { DropdownMenu } from '@island.is/island-ui/core'
 import { useLocale } from '@island.is/localization'
 import { vehicleMessage } from '../../lib/messages'
-import { downloadFile } from '@island.is/portals/my-pages/core'
-import { useVehiclesListQuery } from './VehicleBulkMileage.generated'
-import { useMemo } from 'react'
-
-interface TableData {
-  bilnumber: unknown[]
-  'seinasta skraning': unknown[]
-  'seinasta skrada stada': unknown[]
-  kilometrastada: unknown[]
-}
+import {
+  downloadFile,
+  formatDateWithTime,
+} from '@island.is/portals/my-pages/core'
+import { TableData } from './types'
+import { useCallback, useMemo } from 'react'
 
 interface Props {
   onError: (error: string) => void
+  data?: TableData
+  loading?: boolean
+  disabled?: boolean
 }
 
-const VehicleBulkMileageFileDownloader = ({ onError }: Props) => {
+const FILE_TYPES = ['csv', 'xlsx'] as const
+
+const VehicleBulkMileageFileDownloader = ({
+  onError,
+  data,
+  loading,
+  disabled,
+}: Props) => {
   const { formatMessage } = useLocale()
 
-  const { data, error, loading } = useVehiclesListQuery({
-    variables: {
-      input: {
-        page: 1,
-        pageSize: 1000,
-        filterOnlyRequiredMileageRegistration: true,
-      },
-    },
-  })
-
-  const table = useMemo(() => {
-    if (data?.vehiclesListV3?.data) {
-      const table: TableData = {
-        bilnumber: [],
-        'seinasta skraning': [],
-        'seinasta skrada stada': [],
-        kilometrastada: [],
+  const downloadExampleFile = useCallback(
+    async (type: 'csv' | 'xlsx', data: TableData) => {
+      const header = Object.keys(data)
+      const rows: Array<Array<unknown>> = []
+      for (let i = 0; i < data['bilnumer'].length; i++) {
+        rows.push(header.map((key) => data[key as keyof TableData][i]))
       }
 
-      data.vehiclesListV3.data.forEach((vehicle) => {
-        table['bilnumber'].push(vehicle.vehicleId)
-        table['seinasta skraning'].push(
-          vehicle.mileageDetails?.lastMileageRegistration?.date,
-        )
-        table['seinasta skrada stada'].push(
-          vehicle.mileageDetails?.lastMileageRegistration?.mileage ?? 0,
-        )
-      })
-      return table
+      try {
+        downloadFile(`magnskraning_kilometrastodu_template`, header, rows, type)
+      } catch (error) {
+        onError(error)
+      }
+    },
+    [onError],
+  )
+
+  const menuItems = useMemo(() => {
+    if (!data) {
+      return []
     }
-  }, [data])
-
-  const downloadExampleFile = async (type: 'csv' | 'xlsx', data: TableData) => {
-    const header = Object.keys(data)
-    const rows = Object.values(data)
-    try {
-      downloadFile(`magnskraning_kilometrastodu_example`, header, rows, type)
-    } catch (error) {
-      onError(error)
-    }
-  }
-
-  if (loading) {
-    return <Button loading />
-  }
-
-  if (!table) {
-    return
-  }
+    const options = FILE_TYPES.map((type) => ({
+      title: `.${type}`,
+      onClick: () => downloadExampleFile(type, data),
+    }))
+    return options
+  }, [data, downloadExampleFile])
 
   return (
     <DropdownMenu
       icon="ellipsisHorizontal"
       menuLabel={formatMessage(vehicleMessage.downloadTemplate)}
-      items={(['csv', 'xlsx'] as const).map((type) => ({
-        title: `.${type}`,
-        onClick: () => downloadExampleFile(type, table),
-      }))}
+      items={menuItems}
       title={formatMessage(vehicleMessage.downloadTemplate)}
+      loading={loading}
+      disabled={disabled}
     />
   )
 }
