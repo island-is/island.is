d3a47d233156598307203ddb78c4cac3bdf08b68
Hyosam <hyo-sam.nandkisore@advania.is>
2025-03-18 15:50:09 +0000

    fix: update API paths to '/form-system-api' in configuration and routes

--- apps/form-system/web/src/lib/routes.tsx
+++ apps/form-system/web/src/lib/routes.tsx
@@ -11,7 +11,7 @@ export const routes: RouteObject[] = [
   {
     element: (
       <HeaderInfoProvider>
-        <UserProfileLocale />
+        {/* <UserProfileLocale /> */}
         <Header />
       </HeaderInfoProvider>
     ),
