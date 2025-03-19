c5836b5ff4f45c8e3ce189c05389d90c6447e490
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-19 10:43:21 +0000

    fix(ojoi): Removed screen from missing record id (#18437)
    Removed screen from MissingRecordId

--- apps/web/screens/OfficialJournalOfIceland/OJOIMissingRecordId.tsx
+++ apps/web/screens/OfficialJournalOfIceland/OJOIMissingRecordId.tsx
@@ -1,32 +1,18 @@
+import type { GetServerSideProps } from 'next'
 import { parseAsString } from 'next-usequerystate'
 
-import { CustomPageUniqueIdentifier } from '@island.is/web/graphql/schema'
-import { withMainLayout } from '@island.is/web/layouts/main'
 import { CustomNextError } from '@island.is/web/units/errors'
 
-import {
-  CustomScreen,
-  withCustomPageWrapper,
-} from '../CustomPage/CustomPageWrapper'
-
-const OJOIMissingRecordIdPage: CustomScreen<{}> = () => {
-  return <div></div>
-}
-
 const idParser = parseAsString
-
-OJOIMissingRecordIdPage.getProps = async ({ query }) => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const recordId = idParser.parseServerSide(query.recordId)
   const recordIdCapitalized = idParser.parseServerSide(query.RecordId)
-
   const idToUse = recordIdCapitalized || recordId
-
   if (!idToUse) {
     throw new CustomNextError(404, 'Advert not found')
   }
 
   const redirectUrl = '/stjornartidindi/nr/' + idToUse
-
   return {
     redirect: {
       permanent: true,
@@ -35,9 +21,6 @@ OJOIMissingRecordIdPage.getProps = async ({ query }) => {
   }
 }
 
-export default withMainLayout(
-  withCustomPageWrapper(
-    CustomPageUniqueIdentifier.OfficialJournalOfIceland,
-    OJOIMissingRecordIdPage,
-  ),
-)
+export default () => {
+  return null
+}
