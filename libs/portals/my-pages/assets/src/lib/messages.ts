79594f20847424550ef1add0a4cd115a3b670767
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-03-20 15:00:21 +0000

    chore: revert fix

--- libs/portals/my-pages/assets/src/lib/messages.ts
+++ libs/portals/my-pages/assets/src/lib/messages.ts
@@ -1159,7 +1159,7 @@ export const ipMessages = defineMessages({
   },
   description: {
     id: 'sp.intellectual-property:description',
-    defaultMessage: 'Hér getur þú skoðað yfirlit yfir þín hugverkaréttindi',
+    defaultMessage: 'Lýsing',
   },
   baseInfo: {
     id: 'sp.intellectual-property:base-info',
