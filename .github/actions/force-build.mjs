fb42fdb15488db4e781c825ef64aa414b78e8c3a
lommi <jonorn@gmail.com>
2025-03-21 02:26:31 +0000

    fix: ok

--- .github/actions/force-build.mjs
+++ .github/actions/force-build.mjs
@@ -1,6 +1,6 @@
 /**
  * Changing this value forces a rebuild
  */
-const FORCE_REBUILD_VALUE = "2025-03-19-02---rebuild-99";
+const FORCE_REBUILD_VALUE = "2025-03-19-02---rebuild-100";
 
 console.log(FORCE_REBUILD_VALUE);
