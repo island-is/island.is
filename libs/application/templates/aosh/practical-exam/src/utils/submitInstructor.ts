4f613f34fe9576c7bad307f54775759e9f8217de
Baldur Óli Barkarson <ballioli1@gmail.com>
2025-03-24 08:00:34 +0000

    util file

---
+++ libs/application/templates/aosh/practical-exam/src/utils/submitInstructor.ts
@@ -0,0 +1,10 @@
+import { ApolloClient } from '@apollo/client'
+import { Application } from '@island.is/application/types'
+
+export const submitInstructor = async (
+  application: Application,
+  tableItems: Array<any>,
+  apolloClient: ApolloClient<object>,
+) => {
+  return null
+}
