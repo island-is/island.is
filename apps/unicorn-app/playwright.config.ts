7ff30227853dba87ad50b8f26d46086a6fd05e64
Kristofer <kristofer@andes.is>
2025-03-26 15:10:03 +0000

    Playwright config extension

--- apps/unicorn-app/playwright.config.ts
+++ apps/unicorn-app/playwright.config.ts
@@ -1,29 +1,9 @@
+import { baseConfig } from '../../playwright.base'
 import { defineConfig, devices } from '@playwright/test'
-import { nxE2EPreset } from '@nx/playwright/preset'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { workspaceRoot } from '@nx/devkit'
-
-// For CI, you may want to set BASE_URL to the deployed application.
-const basePort = 4200 // This seems to be hard-coded somewhere, so we can't change it :(
-const baseURL = process.env['BASE_URL'] || `http://localhost:${basePort}`
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...nxE2EPreset(__filename, { testDir: './e2e' }),
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    baseURL,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: process.env.CI ? 'on' : 'on-first-retry',
-  },
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'yarn nx serve unicorn-app',
-    url: baseURL,
-    reuseExistingServer: !process.env.CI,
-    stdout: 'pipe',
-    cwd: workspaceRoot,
-  },
+  ...baseConfig('unicorn-app'),
 })
