7f58fbcc49d758424256247e5a19a9fe5cb597f4
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-03-26 13:54:30 +0000

    Add IdsClientConfig to TestingModules

--- libs/clients/syslumenn/src/lib/syslumennClient.module.ts
+++ libs/clients/syslumenn/src/lib/syslumennClient.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { SyslumennService } from './syslumennClient.service'
-import { IdsClientConfig } from '@island.is/nest/config'
 
 @Module({
-  imports: [IdsClientConfig.registerOptional()],
   providers: [SyslumennService],
   exports: [SyslumennService],
 })
