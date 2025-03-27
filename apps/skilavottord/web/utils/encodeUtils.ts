5ca5f73361024c71f4f33276eb5ce5ba1f2127ac
bkristmundsson <bikkibo@gmail.com>
2025-03-27 16:27:32 +0000

    TS-1010 Fix after code rabbit review

--- apps/skilavottord/web/utils/encodeUtils.ts
+++ apps/skilavottord/web/utils/encodeUtils.ts
@@ -8,7 +8,7 @@ export const encode = (input: string): string => {
   replaced = replaced.replace(/[æÆ]/g, 'ae')
 
   // Remove diacritical marks
-  const noDiacritics = replaced.replace(/[\u0300-\u036f]/g, '')
+  const noDiacritics = replaced.replace(/(?:[\u0300-\u036f])/g, '')
 
   // Remove special characters
   const cleaned = noDiacritics.replace(/[^a-zA-Z0-9]/g, '')
