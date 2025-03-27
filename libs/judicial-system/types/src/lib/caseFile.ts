a1a696200e51184340d86c68c281d371cbd7aec6
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-03-27 14:18:23 +0000

    fix(j-s): refactor

--- libs/judicial-system/types/src/lib/caseFile.ts
+++
@@ -1,7 +0,0 @@
-// MD5 was used as file hashing algorithm until (TODO: add date) but was updated to SHA256 to avoid the probability 
-// of hash collision between files in our system. Since we still store MD5 alg types with each file hash 
-// in the db for historical purposes, we support both types here. 
-export enum HashAlgorithm {
-  MD5 = 'MD5',
-  SHA256 = 'SHA256',
-}
