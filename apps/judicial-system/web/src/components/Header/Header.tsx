0e1420d59843528e6aabed85b58c221a026cf039
Ívar Oddsson <ivaroddsson@gmail.com>
2025-03-27 14:44:25 +0000

    Remove debug code

--- apps/judicial-system/web/src/components/Header/Header.tsx
+++ apps/judicial-system/web/src/components/Header/Header.tsx
@@ -31,7 +31,6 @@ import {
 import { api } from '@island.is/judicial-system-web/src/services'
 
 import { useGeoLocation } from '../../utils/hooks'
-import IconButton from '../IconButton/IconButton'
 import { LawyerRegistryContext } from '../LawyerRegistryProvider/LawyerRegistryProvider'
 import MarkdownWrapper from '../MarkdownWrapper/MarkdownWrapper'
 import { UserContext } from '../UserProvider/UserProvider'
@@ -198,9 +197,6 @@ const HeaderContainer = () => {
                       </Text>
                     </Box>
                   </Box>
-                  <IconButton icon="swapHorizontal" colorScheme="blue">
-                    asddas
-                  </IconButton>
                 </div>
                 <div className={styles.dropdownItem}>
                   <Box marginRight={2}>
