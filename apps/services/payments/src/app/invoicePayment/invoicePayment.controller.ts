45e45f111e6ebd81d993837d88b96676655b2ddd
Gunnar K Vilbergsson <gunnar@norda.is>
2025-03-27 10:08:26 +0000

    Fix build in invoicePayment.controller

--- apps/services/payments/src/app/invoicePayment/invoicePayment.controller.ts
+++ apps/services/payments/src/app/invoicePayment/invoicePayment.controller.ts
@@ -41,13 +41,18 @@ export class InvoicePaymentController {
     @Body() createInvoiceInput: CreateInvoiceInput,
   ): Promise<CreateInvoiceResponse> {
     try {
-      const {
-        paymentFlow,
-        paymentDetails: { catalogItems, totalPrice },
-        paymentStatus,
-      } = await this.paymentFlowService.getPaymentFlowWithPaymentDetails(
+      const paymentFlow = await this.paymentFlowService.getPaymentFlowDetails(
         createInvoiceInput.paymentFlowId,
       )
+
+      const [{ catalogItems, totalPrice }, { paymentStatus }] =
+        await Promise.all([
+          this.paymentFlowService.getPaymentFlowChargeDetails(
+            paymentFlow.organisationId,
+            paymentFlow.charges,
+          ),
+          this.paymentFlowService.getPaymentFlowStatus(paymentFlow),
+        ])
 
       if (paymentStatus === 'paid') {
         throw new BadRequestException(PaymentServiceCode.PaymentFlowAlreadyPaid)
