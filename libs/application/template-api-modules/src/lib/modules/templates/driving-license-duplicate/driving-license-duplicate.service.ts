eae91608a27b4da3cebcc4755944b7df2521791d
albina <albina@kosmosogkaos.is>
2025-03-27 10:44:33 +0000

    fix(dl-duplicate): minor updates

--- libs/application/template-api-modules/src/lib/modules/templates/driving-license-duplicate/driving-license-duplicate.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/driving-license-duplicate/driving-license-duplicate.service.ts
@@ -77,7 +77,7 @@ export class DrivingLicenseDuplicateService extends BaseTemplateApiService {
 
     await this.drivingLicenseService
       .drivingLicenseDuplicateSubmission({
-        pickUpLicense: pickUpLicense,
+        pickUpLicense: pickUpLicense ? true : false,
         districtId: pickUpLicense ? parseInt(districtId) : 37,
         token: auth.authorization,
         // Always true since submission doesn't happen before
