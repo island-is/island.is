98d57edd75e4c9f0befb4f109c97d61df2bc4d0b
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-03-28 15:04:08 +0000

    chore(j-s): enable court to add files on behalf of case representatives (#18329)
    * chore(j-s): add state management for new upload files in court component
    
    * fix(j-s): clean up state management and use routes and a share add files component
    
    * fix(j-s): support file component for court
    
    * feat(j-s): add case representative on a file upload
    
    * fix(j-s): fix selector
    
    * chore(j-s): add file representative field
    
    * fix(j-s): add a migration and fix file representative
    
    * fix(j-s): temp
    
    * fix(j-s): clean-up and fix case file list on submit
    
    * fix(j-s): clean-up
    
    * fix(j-s): fix export
    
    * fix(j-s): small index.ts fix
    
    * fix(j-s): types
    
    * fix(j-s): address feedback
    
    * fix(j-s): error state
    
    * fix(j-s): remove tempCase
    
    * fix(j-s): remove enum from migration

---
+++ apps/judicial-system/web/src/routes/Shared/AddFiles/SelectCaseFileRepresentative.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css'
+
+import { theme } from '@island.is/island-ui/theme'
+
+export const selectCaseFileRepresentative = style({
+  marginBottom: `${theme.spacing[10]}px`,
+})
