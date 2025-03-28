b89f0a7f9579339bfd9ba402bf009ee2b066fbe8
Kristofer <kristofer@andes.is>
2025-03-28 16:48:54 +0000

    fixable code

--- apps/unicorn-app/server.ts
+++ apps/unicorn-app/server.ts
@@ -2,6 +2,11 @@ import { bootstrap } from '@island.is/infra-next-server'
 
 // AFFECTME
 
+const f = (g: () => string) => g()
+const b = f(function foo() {
+  return 'b'
+})
+
 bootstrap({
   name: 'unicorn-app',
   appDir: 'apps/unicorn-app',
