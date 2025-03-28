f7e979e51f20455cbc8d64ab2e73bac4e74070a2
Ásdís Erna Guðmundsdóttir <disa@hugsmidjan.is>
2025-03-28 14:50:51 +0000

    Update IcelandicGovernmentInstitutionVacanciesList.tsx

--- apps/web/screens/IcelandicGovernmentInstitutionVacancies/IcelandicGovernmentInstitutionVacanciesList.tsx
+++ apps/web/screens/IcelandicGovernmentInstitutionVacancies/IcelandicGovernmentInstitutionVacanciesList.tsx
@@ -389,7 +389,7 @@ const IcelandicGovernmentInstitutionVacanciesList: Screen<
             variant={isMobile ? 'dialog' : 'popover'}
             labelClear={n('clearFilter', 'Hreinsa síu')}
             labelClearAll={n('clearAllFilters', 'Hreinsa allar síur')}
-            labelOpen={n('openFilter', 'Sía niðurstöður')}
+            labelOpen={n('openFilter', 'Opna síu')}
             labelClose={n('closeFilter', 'Loka síu')}
             labelResult={n('viewResults', 'Skoða niðurstöður')}
             labelTitle={n('filterMenuTitle', 'Opna síu')}
