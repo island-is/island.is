6a9da67afd18842dca8fb31aacdaa78bd8c51c88
Tryggvi Geir Magnússon <tryggvi@hugsmidjan.is>
2025-03-31 10:03:17 +0000

    removed rss

--- apps/web/screens/OfficialJournalOfIceland/OJOIHome.tsx
+++ apps/web/screens/OfficialJournalOfIceland/OJOIHome.tsx
@@ -111,11 +111,6 @@ const OJOIHomePage: CustomScreen<OJOIHomeProps> = ({
               href: searchUrl + '?tegund=b-deild-gjaldskra',
               variant: 'purple',
             },
-            {
-              title: 'Rss',
-              href: '/stjornartidindi/rss',
-              variant: 'purple',
-            },
           ]}
           breadCrumbs={
             breadcrumbItems && (
