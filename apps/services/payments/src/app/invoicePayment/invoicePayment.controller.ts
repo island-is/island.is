741e3d1930481dca9f0b6454cf2bc8991a0ffbe2
Gunnar K Vilbergsson <gunnar@norda.is>
2025-03-31 14:08:48 +0000

    Revert to upstream version of invoicepayment controller

--- apps/services/payments/src/app/invoicePayment/invoicePayment.controller.ts
+++ apps/services/payments/src/app/invoicePayment/invoicePayment.controller.ts
@@ -44,15 +44,13 @@ export class InvoicePaymentController {
       const paymentFlow = await this.paymentFlowService.getPaymentFlowDetails(
         createInvoiceInput.paymentFlowId,
       )
-
-      const [{ catalogItems, totalPrice }, { paymentStatus }] =
-        await Promise.all([
-          this.paymentFlowService.getPaymentFlowChargeDetails(
-            paymentFlow.organisationId,
-            paymentFlow.charges,
-          ),
-          this.paymentFlowService.getPaymentFlowStatus(paymentFlow),
-        ])
+      const { catalogItems, totalPrice } =
+        await this.paymentFlowService.getPaymentFlowChargeDetails(
+          paymentFlow.organisationId,
+          paymentFlow.charges,
+        )
+      const { paymentStatus } =
+        await this.paymentFlowService.getPaymentFlowStatus(paymentFlow)
 
       if (paymentStatus === 'paid') {
         throw new BadRequestException(PaymentServiceCode.PaymentFlowAlreadyPaid)
