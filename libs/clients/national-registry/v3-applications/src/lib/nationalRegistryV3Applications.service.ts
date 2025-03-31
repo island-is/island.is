f9b97bf560287ba396ca7b999eadd77dce7616f3
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-03-31 10:47:33 +0000

    Get custodians from nationalRegistryV3

--- libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
+++ libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common'
-
 import { EinstaklingarApi } from '../../gen/fetch'
+import { Auth, AuthMiddleware, User } from '@island.is/auth-nest-tools'
 
 @Injectable()
 export class NationalRegistryV3ApplicationsClientService {
   constructor(private individualApi: EinstaklingarApi) {}
 
+  private individualApiWithAuth(auth: Auth) {
+    return this.individualApi.withMiddleware(new AuthMiddleware(auth))
+  }
+
   async get18YearOlds(): Promise<string[]> {
     return await this.individualApi.einstaklingar18IDagGet()
+  }
+
+  async getMyCustodians(auth: User): Promise<string[]> {
+    return this.individualApiWithAuth(auth).einstaklingarKennitalaForsjaYfirGet(
+      {
+        kennitala: auth.nationalId,
+      },
+    )
   }
 }
