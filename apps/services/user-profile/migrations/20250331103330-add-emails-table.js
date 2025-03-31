d0a30e36714f4391c73bd3164ceeee8fe75f40b2
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2025-03-31 13:53:21 +0000

    change domain name

---
+++ apps/services/user-profile/migrations/20250331103330-add-emails-table.js
@@ -0,0 +1,103 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('emails', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      national_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email_status: {
+        type: Sequelize.ENUM(
+          'NOT_DEFINED',
+          'NOT_VERIFIED',
+          'VERIFIED',
+          'EMPTY',
+        ),
+        defaultValue: 'NOT_DEFINED',
+        allowNull: false,
+      },
+      primary: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created: {
+        type: 'TIMESTAMP WITH TIME ZONE',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+      },
+      modified: {
+        type: 'TIMESTAMP WITH TIME ZONE',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+      },
+    })
+
+    // Add foreign key constraint and delete cascade
+    await queryInterface.addConstraint('emails', {
+      fields: ['national_id'],
+      type: 'foreign key',
+      name: 'emails_national_id_fkey',
+      references: {
+        table: 'user_profile',
+        field: 'national_id',
+      },
+      onDelete: 'cascade',
+    })
+
+    // Avoid duplicate entries
+    await queryInterface.addConstraint('emails', {
+      fields: ['national_id', 'email'],
+      type: 'unique',
+      name: 'emails_national_id_email_unique',
+    })
+
+    // Add index for national_id
+    await queryInterface.addIndex('emails', ['national_id'], {
+      name: 'emails_national_id_index',
+    })
+
+    /** ACTOR PROFILE CHANGES **/
+
+    // Add column emails to actor_profile that references emails instance
+    await queryInterface.addColumn('actor_profile', 'emails_id', {
+      type: Sequelize.UUID,
+      references: {
+        model: 'emails',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+    })
+  },
+
+  async down(queryInterface) {
+    // Remove foreign key constraint
+    await queryInterface.removeConstraint('emails', 'emails_national_id_fkey')
+
+    // Remove unique constraint
+    await queryInterface.removeConstraint(
+      'emails',
+      'emails_national_id_email_unique',
+    )
+
+    // Remove index
+    await queryInterface.removeIndex('emails', 'emails_national_id_index')
+
+    // Remove column from actor_profile
+    await queryInterface.removeColumn('actor_profile', 'emails')
+
+    // Drop table
+    await queryInterface.dropTable('emails')
+  },
+}
