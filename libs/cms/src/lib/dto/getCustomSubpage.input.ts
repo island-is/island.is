67ac1096472446752ade4ba70f040cb0bfe887e5
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-03-31 14:03:49 +0000

    chore:remove unnecessary properties

--- libs/cms/src/lib/dto/getCustomSubpage.input.ts
+++ libs/cms/src/lib/dto/getCustomSubpage.input.ts
@@ -1,16 +1,11 @@
 import { IsString } from 'class-validator'
 import { Field, InputType } from '@nestjs/graphql'
 import { ElasticsearchIndexLocale } from '@island.is/content-search-index-manager'
-import { CustomPageUniqueIdentifier as CustomPageUniqueIdentifierEnum } from '@island.is/shared/types'
-import { CacheField } from '@island.is/nest/graphql'
 
 @InputType()
 export class GetCustomSubpageInput {
   @Field(() => String)
   parentPageId!: string
-
-  @CacheField(() => CustomPageUniqueIdentifierEnum, { nullable: true })
-  parentPageUniqueIdentifier?: CustomPageUniqueIdentifierEnum
 
   @Field(() => String)
   @IsString()
