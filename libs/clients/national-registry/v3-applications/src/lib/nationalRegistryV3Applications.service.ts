669f07e623cfb19c6ae86115ea38e0ed2e044687
kksteini <steini@kosmosogkaos.is>
2025-04-01 10:45:50 +0000

    Revert fakedata from library

--- libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
+++ libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
@@ -7,8 +7,6 @@ export class NationalRegistryV3ApplicationsClientService {
   constructor(private individualApi: EinstaklingarApi) {}
 
   async get18YearOlds(): Promise<string[]> {
-    // TODO: revert this. Only for dev-deployment test purposes
-    return ['0101303019', '0101302399', '0101307789']
-    //return await this.individualApi.einstaklingar18IDagGet()
+    return await this.individualApi.einstaklingar18IDagGet()
   }
 }
