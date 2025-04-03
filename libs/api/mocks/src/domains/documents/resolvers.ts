f736da2d0fb18c320b404b952f6e332371c39b91
thoreyjona <thoreyjona@gmail.com>
2025-04-03 10:57:50 +0000

    feat: wip inbox mocking

---
+++ libs/api/mocks/src/domains/documents/resolvers.ts
@@ -0,0 +1,15 @@
+import { Resolvers } from '../../types'
+import { store } from './store'
+
+export const resolvers: Resolvers = {
+  Slice: {
+    __resolveType: (parent) => {
+      return parent.__typename as never
+    },
+  },
+  Query: {
+    documentsV2: () => {
+      return store.documents
+    },
+  },
+}
