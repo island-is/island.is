89833aebfb00a56898cb2c1bf9f5cedc01abfddb
andes-it <builders@andes.is>
2025-04-03 12:41:20 +0000

    chore: lintfix update dirty files

--- apps/services/form-system/src/app/modules/applications/applications.controller.ts
+++ apps/services/form-system/src/app/modules/applications/applications.controller.ts
@@ -144,9 +144,9 @@ export class ApplicationsController {
 @Get('form/:formId')
 async findAllByTypeAndUser(
   @Param('formId') formId: string,
-  @Query('page') page: number = 1,
-  @Query('limit') limit: number = 10,
-  @Query('isTest') isTest: boolean = false
+  @Query('page') page = 1,
+  @Query('limit') limit = 10,
+  @Query('isTest') isTest = false
 ): Promise<ApplicationListDto> {
   // TODO: Implement proper filtering by form and pagination
   return await this.applicationsService.findAllByTypeAndUser()
