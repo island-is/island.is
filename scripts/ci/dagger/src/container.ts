5898109bd75bca8d188c4807a4d8a99f35702167
lommi <jonorn@gmail.com>
2025-04-07 15:59:01 +0000

    fix: update

---
+++ scripts/ci/dagger/src/container.ts
@@ -0,0 +1,31 @@
+import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+
+
+interface MonorepoContainerGithub {
+    action: 'sha';
+    sha: string;
+}
+
+interface MonorepoContainerGithubBranch {
+    action: 'branch';
+    branch: string;
+}
+
+
+interface MonorepoContainerLocal {
+    action: 'local';
+}
+
+type MonorepoContainer = MonorepoContainerGithub | MonorepoContainerLocal | MonorepoContainerGithubBranch;
+
+export function monorepoContainer(props: MonorepoContainer) {
+    const files = (( ) => {
+        if (props.action ===  'sha') {
+            //dag.git()
+        }
+        if (props.action === 'local' {
+            throw new Error('Not implemented');
+        }
+        throw new Error('Invalid action');
+    })();
+}
--- scripts/ci/dagger/src/index.ts
+++ scripts/ci/dagger/src/index.ts
@@ -11,36 +11,5 @@ export class Dagger {
   test() {
     console.log("Hello from Dagger!");
   }
-  /**
-   * Returns a container that echoes whatever string argument is provided
-   */
-  @func()
-  outputManifest(prepareState: JobState, testsState: JobState, dockerBuildState: JobState,
-  shouldRunBuild: boolean = false,
-  manifest: Record<string, any>,
-
-  ) {
-    // Check all states
-    if (prepareState !== 'success') {
-      throw new Error(`Prepare job failed with state: ${prepareState}`);
-    }
-    Object.entries({
-      tests: testsState,
-      dockerBuild: dockerBuildState,
-    }).forEach(([jobName, state]) => {
-      if (failedStates.includes(state)) {
-        throw new Error(`${jobName} job failed with state: ${state}`);
-      }
-    });
-
-    if (!shouldRunBuild) {
-      return;
-    }
-    
-    if (!manifest) {
-      console.error(`Manifest not defined`);
-    }
-    console.log(manifest);
-    return;
-  }
+ 
 }
