77da8f8985dda4e6c6bb6dfd0336fe5bd89a84ed
Þorkell Máni Þorkelsson <wowthorkell@gmail.com>
2025-04-09 09:24:00 +0000

    feat(ojoi): dynamic loading of ojoi subpages (#18618)
    * feat:init
    
    * feat: dynamically routed
    
    * fix: nullables
    
    * chore: Remove locale

--- apps/web/screens/CustomPage/index.ts
+++ apps/web/screens/CustomPage/index.ts
@@ -1,2 +1,1 @@
 export * from './CustomPageWrapper'
-export * from './CustomSubpageWrapper'
--- apps/web/screens/OfficialJournalOfIceland/constants.ts
+++ apps/web/screens/OfficialJournalOfIceland/constants.ts
@@ -1,3 +1,1 @@
-export const OJOI_BASE_CMS_ID = '1Ai5vf7uJ5ye0TsK3zFiaA'
-
 export const ORGANIZATION_SLUG = 'stjornartidindi'
