72f41a128ef3d06da0f4900ad94b234db02bb331
kksteini <steini@kosmosogkaos.is>
2025-04-09 11:24:18 +0000

    chore(clients-national-registry-v3-application): Standardizing english

--- libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
+++ libs/clients/national-registry/v3-applications/src/lib/nationalRegistryV3Applications.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common'
 
-import { Einstaklingur18IDagItemDTO, IslandIsApi } from '../../gen/fetch'
+import { IslandIsApi } from '../../gen/fetch'
+import { BirthdayIndividual, mapBirthdayIndividual } from './mappers'
 
 @Injectable()
 export class NationalRegistryV3ApplicationsClientService {
   constructor(private islandIsApi: IslandIsApi) {}
 
-  async get18YearOlds(): Promise<Array<Einstaklingur18IDagItemDTO>> {
+  async get18YearOlds(): Promise<Array<BirthdayIndividual>> {
     const res = await this.islandIsApi.islandIs18IDagGet()
-    return res.eins18IDagList ?? []
+    return (res.eins18IDagList ?? []).map(mapBirthdayIndividual)
   }
 }
