73f45b7c196db9061d337dadd7b6618848f22cb6
lommi <jonorn@gmail.com>
2025-04-10 08:53:32 +0000

    fix: test

--- scripts/ci/dagger/src/const.ts
+++ scripts/ci/dagger/src/const.ts
@@ -1,6 +1,6 @@
 import {Platform} from "@dagger.io/dagger";
 
-export const GITHUB_URL = "git@github.com:island-is/island.is.git";
+export const GITHUB_URL = "https://github.com/island-is/island.is/";
 
 // export const GITHUB_SSH_URL = "git@github.com:island-is/island.is.git";
 export const RUNNER_IMAGE = '821090935708.dkr.ecr.eu-west-1.amazonaws.com/actions-runner:arc-efs_e8a77b7ccb_535';
