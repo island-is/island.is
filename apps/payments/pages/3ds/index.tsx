b1cc139a0a87e114db16f8a1170058a2494afc3d
Bæring Gunnar Steinþórsson <baering@aranja.com>
2025-04-16 11:33:49 +0000

    Update 3ds success page to match design

---
+++ apps/payments/pages/3ds/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import { LoadingDots, Text } from '@island.is/island-ui/core'
+import { useLocale } from '@island.is/localization'
+
+import { PaymentCompleteIcon } from '../../components/PaymentCompleteIcon'
+import { threeDSecure } from '../../messages'
+
+import * as styles from './index.css'
+
+export default function ThreeDSecureSuccessPage() {
+  const { formatMessage } = useLocale()
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.close()
+    }, 5000)
+  })
+
+  return (
+    <div className={styles.container}>
+      <PaymentCompleteIcon />
+
+      <Text as="h1" variant="h1" marginTop={2}>
+        {formatMessage(threeDSecure.title)}
+      </Text>
+      <Text marginTop={1} marginBottom={4}>
+        {formatMessage(threeDSecure.pleaseWait)}
+      </Text>
+
+      <LoadingDots />
+    </div>
+  )
+}
