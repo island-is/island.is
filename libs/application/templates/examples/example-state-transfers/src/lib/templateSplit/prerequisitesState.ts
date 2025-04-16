29076f718653215475010d28cccf1df06fce8d95
Gunnar K Vilbergsson <gunnar@norda.is>
2025-04-16 13:07:09 +0000

    Attempt to force application deployment

--- libs/application/templates/examples/example-state-transfers/src/lib/templateSplit/prerequisitesState.ts
+++ libs/application/templates/examples/example-state-transfers/src/lib/templateSplit/prerequisitesState.ts
@@ -9,6 +9,8 @@ import {
 import { EphemeralStateLifeCycle } from '@island.is/application/core'
 import { StateNodeConfig } from 'xstate'
 
+// Deployme
+
 export const prerequisitesState: StateNodeConfig<
   ApplicationContext,
   ApplicationStateSchema<Events>,
