06e85a1924a999766b865205ce8a758cfc1fc02e
Jonni <jonas@norda.is>
2025-04-25 13:15:27 +0000

    fix: use the right message prefix

--- libs/application/templates/examples/example-auth-delegation/src/lib/messages.ts
+++ libs/application/templates/examples/example-auth-delegation/src/lib/messages.ts
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl'
 
 export const m = defineMessages({
   delegationDescription: {
-    id: 'expro.application:procureDescription',
+    id: 'exad.application:delegationDescription',
     defaultMessage:
       'You are now logged in with an auth delegation for a **{type}**. The applicant is **{name}** ({applicantNationalId}) and the actors that have edited this application are **{actors}**',
     description: 'Auth delegation description',
