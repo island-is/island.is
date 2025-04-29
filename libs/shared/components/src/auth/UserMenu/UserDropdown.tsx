e3b631bc572fcb44555eb6131b478c5cc0618e12
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-04-29 09:42:18 +0000

    fix: user button a11y labels

--- libs/shared/components/src/auth/UserMenu/UserDropdown.tsx
+++ libs/shared/components/src/auth/UserMenu/UserDropdown.tsx
@@ -58,15 +58,8 @@ export const UserDropdown = ({
     AuthDelegationType.ProcurationHolder,
   )
 
-  const [isMobile, setIsMobile] = useState(false)
   const { width } = useWindowSize()
-
-  useEffect(() => {
-    if (width < theme.breakpoints.md) {
-      return setIsMobile(true)
-    }
-    setIsMobile(false)
-  }, [width])
+  const isMobile = width < theme.breakpoints.md
 
   const closeButton = (
     <button
@@ -167,7 +160,7 @@ export const UserDropdown = ({
       isVisible={isVisible}
       hideOnClickOutside={true}
       hideOnEsc={true}
-      modalLabel={formatMessage(userMessages.userButtonAria)}
+      modalLabel={formatMessage(userMessages.userButtonAriaModalLabel)}
       removeOnClose={true}
       preventBodyScroll={false}
       onVisibilityChange={(visibility: boolean) => {
