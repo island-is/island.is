0e58ba53ad038d3b8513aa34dfaa5d585cfce5c1
Gunnar K Vilbergsson <gunnar@norda.is>
2025-04-29 09:16:01 +0000

    Still trying to deploy user-profile

--- apps/services/user-profile/src/main.ts
+++ apps/services/user-profile/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app/app.module'
 import { environment } from './environments'
 import { openApi } from './openApi'
 
+// DEPLOYME
+
 bootstrap({
   appModule: AppModule,
   name: 'services-user-profile',
