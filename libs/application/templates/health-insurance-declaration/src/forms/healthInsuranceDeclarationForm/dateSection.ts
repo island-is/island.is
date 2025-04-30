acf647cc3df62ec00130cffc4e0fd15c84b5e732
Jonni <jonas@norda.is>
2025-04-30 10:30:32 +0000

    fix: date picker ranges

--- libs/application/templates/health-insurance-declaration/src/forms/healthInsuranceDeclarationForm/dateSection.ts
+++ libs/application/templates/health-insurance-declaration/src/forms/healthInsuranceDeclarationForm/dateSection.ts
@@ -2,11 +2,19 @@ import {
   buildDateField,
   buildMultiField,
   buildSection,
+  getValueViaPath,
 } from '@island.is/application/core'
 import { buildAlertMessageField } from '@island.is/application/core'
 import { ApplicantType } from '../../shared/constants'
 import sub from 'date-fns/sub'
+import startOfMonth from 'date-fns/startOfMonth'
+import endOfMonth from 'date-fns/endOfMonth'
+import addMonths from 'date-fns/addMonths'
+import addYears from 'date-fns/addYears'
+import parseISO from 'date-fns/parseISO'
+
 import * as m from '../../lib/messages'
+import { dateFromMinDate, dateToMaxDate, dateToMinDate } from '../../utils'
 
 export const dateSection = buildSection({
   id: 'dateSection',
@@ -16,34 +24,49 @@ export const dateSection = buildSection({
       id: 'dateMultifield',
       title: m.application.date.sectionDescription,
       children: [
+        buildAlertMessageField({
+          condition: (answers) =>
+            answers.studentOrTouristRadioFieldTourist === ApplicantType.TOURIST,
+          id: 'dateTouristAlertMessage',
+          alertType: 'info',
+          title: m.application.date.dateAlertTitle,
+          message: m.application.date.touristAlertMessage,
+        }),
+        buildAlertMessageField({
+          condition: (answers) =>
+            answers.studentOrTouristRadioFieldTourist === ApplicantType.STUDENT,
+          id: 'dateVacationAlertMessage',
+          alertType: 'info',
+          title: m.application.date.dateAlertTitle,
+          message: m.application.date.studentAlertMessage,
+        }),
         buildDateField({
           id: 'period.dateFieldFrom',
-          minDate: (application) =>
-            application.answers.studentOrTouristRadioFieldTourist ===
-            ApplicantType.STUDENT
-              ? sub(new Date(), { years: 1 })
-              : new Date(0),
+          minDate: dateFromMinDate,
           title: m.application.date.dateFromTitle,
           placeholder: m.application.date.datePlaceholderText,
           required: true,
+          clearOnChange: ['period.dateFieldTo'],
           width: 'half',
           defaultValue: '',
         }),
         buildDateField({
           id: 'period.dateFieldTo',
+          minDate: dateToMinDate,
+          maxDate: dateToMaxDate,
           title: m.application.date.dateToTitle,
           placeholder: m.application.date.datePlaceholderText,
           required: true,
           width: 'half',
           defaultValue: '',
-        }),
-        buildAlertMessageField({
-          id: 'dateAlertMessage',
-          alertType: 'warning',
-          title: m.application.date.studentMinDateWarningTitle,
-          message: m.application.date.studentMinDateWarning,
-          condition: (answers) =>
-            answers.studentOrTouristRadioFieldTourist === ApplicantType.STUDENT,
+          tempDisabled: (application) => {
+            const dateFrom = getValueViaPath<string>(
+              application.answers,
+              'period.dateFieldFrom',
+            )
+            console.log(application.answers.period)
+            return dateFrom && dateFrom !== '' ? false : true
+          },
         }),
       ],
     }),
