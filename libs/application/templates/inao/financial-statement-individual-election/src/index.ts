f50c1dc113a8c7c8b4909ff910f06a186ccc2eb0
Jonni <jonas@norda.is>
2025-05-02 15:38:53 +0000

    fix: import FSIUSERTYPE elsewere

--- libs/application/templates/inao/financial-statement-individual-election/src/index.ts
+++ libs/application/templates/inao/financial-statement-individual-election/src/index.ts
@@ -1,5 +1,7 @@
 import FinancialStatementIndividualElectionTemplate from './lib/financial-statement-individual-election'
+import { FSIUSERTYPE } from './types/types'
 
 export const getDataProviders = () => import('./dataProviders')
+export { FSIUSERTYPE }
 
 export default FinancialStatementIndividualElectionTemplate
