bf5e2ad645825d58347149c63e7275a45f99f173
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-05-02 09:17:32 +0000

    chore:dummy

--- libs/portals/my-pages/licenses/src/screens/v1/LicensesOverview/UserLicenses.tsx
+++ libs/portals/my-pages/licenses/src/screens/v1/LicensesOverview/UserLicenses.tsx
@@ -40,6 +40,9 @@ export const UserLicenses: FC<React.PropsWithChildren<Props>> = ({
   const { formatMessage } = useLocale()
   const currentDate = new Date()
 
+  //DUMMY
+  const k = 8
+
   const getLabel = (
     type: GenericLicenseType | 'Passport',
     isInvalid = false,
