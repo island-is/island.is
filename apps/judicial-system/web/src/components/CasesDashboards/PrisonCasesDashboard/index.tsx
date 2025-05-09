ba6725d04e8ad7e1523039386343a07c834b2a9a
Ívar Oddsson <ivaroddsson@gmail.com>
2025-05-09 14:38:41 +0000

    Remove unused component

--- apps/judicial-system/web/src/components/CasesDashboards/PrisonCasesDashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-import CasesDashboardLayout from '../../Layouts/CasesDashboardLayout'
-
-const PrisonCasesDashboard: FC = () => {
-  return <CasesDashboardLayout title={'jkansd'}>jndsf</CasesDashboardLayout>
-}
-
-export default PrisonCasesDashboard
