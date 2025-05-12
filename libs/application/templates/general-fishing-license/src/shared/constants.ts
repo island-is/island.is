913504bc525d6845fc84c58c75096802e4f1dc2a
Gunnar K Vilbergsson <gunnar@norda.is>
2025-05-12 12:17:08 +0000

    Add chargeItem validation to fishing license

--- libs/application/templates/general-fishing-license/src/shared/constants.ts
+++ libs/application/templates/general-fishing-license/src/shared/constants.ts
@@ -3,3 +3,19 @@ export enum ApiActions {
   createCharge = 'createCharge',
   getShips = 'getShips',
 }
+
+export enum FishingLicenseChargeType {
+  hookCatchLimit = 'L5102',
+  fishWithDanishSeine = 'L5103',
+  greyslepp = 'L5105',
+  northIceOceanCod = 'L5111',
+  catchMark = 'L5101',
+  lumpfish = 'L5107',
+  coastalFisheries = 'L5108',
+  freetime = 'L5104',
+  freetimeHook = 'L5104',
+  freetimeHookMed = 'L5104',
+  commonWhelk = 'L5109',
+  oceanQuahogin = 'L5110',
+  crustaceans = 'L5106',
+}
