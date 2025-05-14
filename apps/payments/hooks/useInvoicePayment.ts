de5fbffdc76a8d61e55d522d20d98d1ae66bce2a
Bæring Gunnar Steinþórsson <baering@aranja.com>
2025-05-14 15:00:48 +0000

    Refactor

--- apps/payments/hooks/useInvoicePayment.ts
+++ apps/payments/hooks/useInvoicePayment.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useCallback } from 'react'
 import { CardErrorCode } from '@island.is/shared/constants'
 import { useCreateInvoiceMutation } from '../graphql/mutations.graphql.generated'
 import { PaymentError } from '../utils/error/error'
@@ -16,7 +16,6 @@ export const useInvoicePayment = ({
 }: UseInvoicePaymentProps) => {
   const [createInvoiceMutationHook, { loading: createInvoiceLoading }] =
     useCreateInvoiceMutation()
-  // Removed local isProcessing as createInvoiceLoading from the mutation hook can be used directly
 
   const processInvoicePayment = useCallback(async () => {
     if (!paymentFlowId) {
@@ -24,7 +23,6 @@ export const useInvoicePayment = ({
       return
     }
 
-    // setIsProcessing(true); // Not needed if using createInvoiceLoading directly
     try {
       const response = await createInvoiceMutationHook({
         variables: {
@@ -43,8 +41,6 @@ export const useInvoicePayment = ({
       onPaymentError({
         code: (e.message as CardErrorCode) || CardErrorCode.UnknownCardError,
       })
-    } finally {
-      // setIsProcessing(false); // Not needed
     }
   }, [
     paymentFlowId,
@@ -55,6 +51,6 @@ export const useInvoicePayment = ({
 
   return {
     processInvoicePayment,
-    isInvoicePaymentProcessing: createInvoiceLoading, // Directly use loading state from mutation
+    isInvoicePaymentProcessing: createInvoiceLoading,
   }
 }
