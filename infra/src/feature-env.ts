6ed4a9f57cb1f27ea4884999bb1ce1dbec06a185
Róberta Andersen <roberta@andes.is>
2025-05-15 15:56:56 +0000

    formatting

--- infra/src/feature-env.ts
+++ infra/src/feature-env.ts
@@ -16,7 +16,7 @@ import {
   renderHelmJobForFeature,
   renderHelmServices,
   renderHelmValueFileContent,
-  renderCleanUpForFeature
+  renderCleanUpForFeature,
 } from './dsl/exports/helm'
 import { ServiceBuilder } from './dsl/dsl'
 import { logger } from './logging'
@@ -351,24 +351,25 @@ yargs(process.argv.slice(2))
     describe: 'get kubernetes jobs to cleanup feature environment',
     builder: (yargs) => {
       return yargs
-      .option('cleanupImage', {
-        type: 'string',
-        description: 'Image to run feature cleanup jobs',
-        demandOption: true,
-      })
-      .option('writeDest', {
-        type: 'string',
-        description: 'Template location where to write files to for down stream apps',
-        default: ''
-      })
-      .option('containerCommand', {
-        type: 'string',
-        description: 'Command to run in the container',
-        default: 'cleanup',
-      })
+        .option('cleanupImage', {
+          type: 'string',
+          description: 'Image to run feature cleanup jobs',
+          demandOption: true,
+        })
+        .option('writeDest', {
+          type: 'string',
+          description:
+            'Template location where to write files to for down stream apps',
+          default: '',
+        })
+        .option('containerCommand', {
+          type: 'string',
+          description: 'Command to run in the container',
+          default: 'cleanup',
+        })
     },
     handler: async (argv) => {
-      const typedArgv = (argv as unknown) as Arguments
+      const typedArgv = argv as unknown as Arguments
       const { affectedServices, env, writeDest } = parseArguments(typedArgv)
       const featureYaml = await renderCleanUpForFeature(
         env,
@@ -384,12 +385,90 @@ yargs(process.argv.slice(2))
 
       if (writeDest != '') {
         try {
-          fs.mkdirSync(`${writeDest}/${affectedServices[0].name()}`, { recursive: true })
-        console.log(`writing file to: ${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml}`)
-        fs.writeFileSync(`${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml`, svcString);
+          fs.mkdirSync(`${writeDest}/${affectedServices[0].name()}`, {
+            recursive: true,
+          })
+          console.log(
+            `writing file to: ${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml}`,
+          )
+          fs.writeFileSync(
+            `${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml`,
+            svcString,
+          )
         } catch (error) {
-          console.log(`Failed to write values file for ${affectedServices[0].name()}:`, error)
-          throw new Error(`Failed to write values for ${affectedServices[0].name()}`);
+          console.log(
+            `Failed to write values file for ${affectedServices[0].name()}:`,
+            error,
+          )
+          throw new Error(
+            `Failed to write values for ${affectedServices[0].name()}`,
+          )
+        }
+      } else {
+        await writeToOutput(svcString, typedArgv.output)
+      }
+    },
+  })
+  .command({
+    command: 'cleanup',
+    describe: 'get kubernetes jobs to cleanup feature environment',
+    builder: (yargs) => {
+      return yargs
+        .option('cleanupImage', {
+          type: 'string',
+          description: 'Image to run feature cleanup jobs',
+          demandOption: true,
+        })
+        .option('writeDest', {
+          type: 'string',
+          description:
+            'Template location where to write files to for down stream apps',
+          default: '',
+        })
+        .option('containerCommand', {
+          type: 'string',
+          description: 'Command to run in the container',
+          default: 'cleanup',
+        })
+    },
+    handler: async (argv) => {
+      const typedArgv = argv as unknown as Arguments
+      const { affectedServices, env, writeDest } = parseArguments(typedArgv)
+      const featureYaml = await renderCleanUpForFeature(
+        env,
+        typedArgv.cleanupImage!,
+        affectedServices,
+      )
+
+      if (
+        featureYaml.spec.template.spec.containers.length <= 0 ||
+        affectedServices.length <= 0
+      ) {
+        return
+      }
+
+      const svcString = dumpJobYaml(featureYaml)
+
+      if (writeDest != '') {
+        try {
+          fs.mkdirSync(`${writeDest}/${affectedServices[0].name()}`, {
+            recursive: true,
+          })
+          console.log(
+            `writing file to: ${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml}`,
+          )
+          fs.writeFileSync(
+            `${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml`,
+            svcString,
+          )
+        } catch (error) {
+          console.log(
+            `Failed to write values file for ${affectedServices[0].name()}:`,
+            error,
+          )
+          throw new Error(
+            `Failed to write values for ${affectedServices[0].name()}`,
+          )
         }
       } else {
         await writeToOutput(svcString, typedArgv.output)
@@ -419,5 +498,3 @@ yargs(process.argv.slice(2))
     },
   })
   .demandCommand().argv
-
-
