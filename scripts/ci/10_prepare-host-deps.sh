6bc9fd70fab39f53b0543812e9becb0ddb9cbfa4
Kristófer <kristofer@apro.is>
2025-05-19 13:16:20 +0000

    Remove unused script

--- scripts/ci/10_prepare-host-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-# shellcheck disable=SC1091
-source "$DIR"/_common.sh
-
-YARN_ENABLE_HARDENED_MODE=0 yarn install --immutable
