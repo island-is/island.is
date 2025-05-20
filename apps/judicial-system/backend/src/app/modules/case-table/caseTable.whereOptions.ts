9aaba0fee0064aef16a7ad425eccae61aa46be81
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-05-20 18:53:49 +0000

    Updates where options

--- apps/judicial-system/backend/src/app/modules/case-table/caseTable.whereOptions.ts
+++ apps/judicial-system/backend/src/app/modules/case-table/caseTable.whereOptions.ts
@@ -116,62 +116,109 @@ const prosecutorsOfficeIndictmentInReviewWhereOptions = {
   indictment_review_decision: null,
 }
 
-const prosecutorsOfficeIndictmentAppealDeadlineActiveWhereOptions = {
-  [Op.or]: [
-    {
-      indictment_ruling_decision: CaseIndictmentRulingDecision.FINE,
-      [Op.and]: [
-        where(
-          literal(`"ruling_date"::date + INTERVAL '4 days'`),
-          Op.gt,
-          fn('NOW'),
-        ),
-      ],
-    },
-    {
-      indictment_ruling_decision: CaseIndictmentRulingDecision.RULING,
-      [Op.and]: [
-        literal(`EXISTS (
-              SELECT 1 FROM defendant
-              WHERE defendant.case_id = "Case".id
-                AND defendant.service_requirement = '${ServiceRequirement.NOT_REQUIRED}'
-            )`),
-        where(
-          literal(`"ruling_date"::date + INTERVAL '4 days'`),
-          Op.gt,
-          fn('NOW'),
-        ),
-      ],
-    },
-    {
-      indictment_ruling_decision: CaseIndictmentRulingDecision.RULING,
-      [Op.and]: [
-        literal(`EXISTS (
-              SELECT 1 FROM defendant
-              WHERE defendant.case_id = "Case".id
-                AND defendant.service_requirement = 'REQUIRED'
-                AND (
-                  defendant.verdict_view_date IS NULL
-                  OR defendant.verdict_view_date + INTERVAL '29 days' > NOW()
-                )
-            )`),
-      ],
-    },
-  ],
-}
-
 const prosecutorsOfficeIndictmentReviewedWhereOptions = {
   ...prosecutorsOfficeIndictmentSharedWhereOptions,
   indictment_reviewer_id: { [Op.not]: null },
   indictment_review_decision: IndictmentCaseReviewDecision.ACCEPT,
-  ...prosecutorsOfficeIndictmentAppealDeadlineActiveWhereOptions,
+  [Op.or]: [
+    {
+      indictment_ruling_decision: CaseIndictmentRulingDecision.FINE,
+      [Op.and]: [
+        literal(`EXISTS (
+          SELECT 1 FROM defendant
+          WHERE defendant.case_id = "Case".id
+            AND (defendant.is_sent_to_prison_admin IS NULL or defendant.is_sent_to_prison_admin = false)
+        )`),
+      ],
+    },
+    {
+      indictment_ruling_decision: CaseIndictmentRulingDecision.RULING,
+      [Op.and]: [
+        literal(`EXISTS (
+          SELECT 1 FROM defendant
+          WHERE defendant.case_id = "Case".id
+            AND defendant.verdict_appeal_date IS NULL
+            AND (defendant.is_sent_to_prison_admin IS NULL or defendant.is_sent_to_prison_admin = false)
+            AND defendant.service_requirement = '${ServiceRequirement.NOT_REQUIRED}'
+        )`),
+        where(
+          literal(`"ruling_date"::date + INTERVAL '29 days'`),
+          Op.gt,
+          fn('NOW'),
+        ),
+      ],
+    },
+    {
+      indictment_ruling_decision: CaseIndictmentRulingDecision.RULING,
+      [Op.and]: [
+        literal(`EXISTS (
+          SELECT 1 FROM defendant
+          WHERE defendant.case_id = "Case".id
+            AND defendant.verdict_appeal_date IS NULL
+            AND (defendant.is_sent_to_prison_admin IS NULL or defendant.is_sent_to_prison_admin = false)
+            AND defendant.service_requirement in ('${ServiceRequirement.REQUIRED}', '${ServiceRequirement.NOT_APPLICABLE}')
+            AND (
+              defendant.verdict_view_date IS NULL
+              OR defendant.verdict_view_date + INTERVAL '29 days' > NOW()
+            )
+        )`),
+      ],
+    },
+  ],
 }
 
 const prosecutorsOfficeIndictmentAppealPeriodExpiredWhereOptions = {
   ...prosecutorsOfficeIndictmentSharedWhereOptions,
   indictment_reviewer_id: { [Op.not]: null },
   indictment_review_decision: IndictmentCaseReviewDecision.ACCEPT,
-  [Op.not]: prosecutorsOfficeIndictmentAppealDeadlineActiveWhereOptions,
+  [Op.and]: [
+    { indictment_ruling_decision: CaseIndictmentRulingDecision.RULING },
+    {
+      [Op.not]: {
+        [Op.and]: [
+          literal(`EXISTS (
+            SELECT 1 FROM defendant
+            WHERE defendant.case_id = "Case".id
+              AND defendant.verdict_appeal_date IS NULL
+              AND (defendant.is_sent_to_prison_admin IS NULL or defendant.is_sent_to_prison_admin = false)
+              AND defendant.service_requirement = '${ServiceRequirement.NOT_REQUIRED}'
+          )`),
+          where(
+            literal(`"ruling_date"::date + INTERVAL '29 days'`),
+            Op.gt,
+            fn('NOW'),
+          ),
+        ],
+      },
+    },
+    {
+      [Op.not]: {
+        [Op.and]: [
+          literal(`EXISTS (
+            SELECT 1 FROM defendant
+            WHERE defendant.case_id = "Case".id
+              AND defendant.verdict_appeal_date IS NULL
+              AND (defendant.is_sent_to_prison_admin IS NULL or defendant.is_sent_to_prison_admin = false)
+              AND defendant.service_requirement in ('${ServiceRequirement.REQUIRED}', '${ServiceRequirement.NOT_APPLICABLE}')
+              AND (
+                defendant.verdict_view_date IS NULL
+                OR defendant.verdict_view_date + INTERVAL '29 days' > NOW()
+              )
+          )`),
+        ],
+      },
+    },
+    {
+      [Op.and]: [
+        literal(`EXISTS (
+          SELECT 1 FROM defendant
+          WHERE defendant.case_id = "Case".id
+            AND defendant.verdict_appeal_date IS NULL
+            AND (defendant.is_sent_to_prison_admin IS NULL or defendant.is_sent_to_prison_admin = false)
+        )`),
+      ],
+    },
+  ],
 }
 
 const prosecutorsOfficeIndictmentSentToPrisonAdminWhereOptions = {
@@ -187,7 +234,22 @@ const prosecutorsOfficeIndictmentSentToPrisonAdminWhereOptions = {
   ],
 }
 
-const prosecutorsOfficeIndictmentAppealedWhereOptions = {}
+const prosecutorsOfficeIndictmentAppealedWhereOptions = {
+  ...prosecutorsOfficeIndictmentSharedWhereOptions,
+  indictment_reviewer_id: { [Op.not]: null },
+  [Op.or]: [
+    { indictment_review_decision: IndictmentCaseReviewDecision.APPEAL },
+    {
+      [Op.and]: [
+        literal(`EXISTS (
+          SELECT 1 FROM defendant
+          WHERE defendant.case_id = "Case".id
+            AND defendant.verdict_appeal_date IS NOT NULL
+        )`),
+      ],
+    },
+  ],
+}
 
 export const caseTableWhereOptions: Record<CaseTableType, WhereOptions> = {
   [CaseTableType.COURT_OF_APPEALS_IN_PROGRESS]:
