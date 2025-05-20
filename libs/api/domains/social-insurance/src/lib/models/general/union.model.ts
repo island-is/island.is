121dd9142266c3dc527c719ff3f08dc07d4f46e6
bkristmundsson <bikkibo@gmail.com>
2025-05-20 13:33:40 +0000

    Fix after review

--- libs/api/domains/social-insurance/src/lib/models/general/union.model.ts
+++ libs/api/domains/social-insurance/src/lib/models/general/union.model.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { Field, ObjectType } from '@nestjs/graphql'
 
 @ObjectType('SocialInsuranceGeneralUnionModel')
 export class UnionModel {
