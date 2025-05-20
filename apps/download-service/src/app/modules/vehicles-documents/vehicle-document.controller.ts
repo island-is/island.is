dacf61e5e1efc33979e74f260d0dc4c8318c064a
Þorkell Máni Þorkelsson <wowthorkell@gmail.com>
2025-05-20 08:02:08 +0000

    feat(clients-vehicles): update vehicle ownership certificate endpoints (#19011)
    * feat. update
    
    * fix: download works
    
    * fix: update endpoint
    
    * chore: ADD DUMMY ENV
    
    * chore: add dummy -- TO REVERT
    
    * fix: readd download config
    
    * chore: remove dummy

--- apps/download-service/src/app/modules/vehicles-documents/vehicle-document.controller.ts
+++ apps/download-service/src/app/modules/vehicles-documents/vehicle-document.controller.ts
@@ -1,7 +1,6 @@
 import type { User } from '@island.is/auth-nest-tools'
-import { AuthMiddleware } from '@island.is/auth-nest-tools'
 import { ApiScope } from '@island.is/auth/scopes'
-import { Controller, Header, Param, Post, Res, UseGuards } from '@nestjs/common'
+import { Controller, Header, Inject, Param, Post, Res, UseGuards } from '@nestjs/common'
 import { ApiOkResponse } from '@nestjs/swagger'
 import { Response } from 'express'
 
@@ -11,16 +10,18 @@ import {
   Scopes,
   ScopesGuard,
 } from '@island.is/auth-nest-tools'
-import { PdfApi } from '@island.is/clients/vehicles'
+import {  VehiclesClientService } from '@island.is/clients/vehicles'
 import { AuditService } from '@island.is/nest/audit'
+import { LOGGER_PROVIDER, type Logger } from '@island.is/logging'
 
 @UseGuards(IdsUserGuard, ScopesGuard)
 @Scopes(ApiScope.vehicles)
 @Controller('vehicles')
 export class VehicleController {
   constructor(
-    private readonly vehiclePDFService: PdfApi,
     private readonly auditService: AuditService,
+    private readonly vehicleService: VehiclesClientService,
+    @Inject(LOGGER_PROVIDER) private readonly logger: Logger
   ) {}
 
   @Post('/history/:permno')
@@ -34,9 +35,7 @@ export class VehicleController {
     @CurrentUser() user: User,
     @Res() res: Response,
   ) {
-    const documentResponse = await this.vehiclePDFService
-      .withMiddleware(new AuthMiddleware(user))
-      .vehicleReportPdfGet({ permno: permno })
+    const documentResponse = await this.vehicleService.vehicleReport(user, {vehicleId: permno})
 
     if (documentResponse) {
       this.auditService.audit({
@@ -62,20 +61,54 @@ export class VehicleController {
     return res.end()
   }
 
-  @Post('/ownership/:ssn')
+
+  @Post('/ownership/excel')
+  @Header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+  @Header('Pragma', 'no-cache')
+  @Header('Cache-Control', 'no-cache')
+  @Header('Cache-Control', 'nmax-age=0')
+  @ApiOkResponse({
+    content: { 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': {} },
+    description: 'Get an excel export from the work machines service',
+  })
+  async getVehicleOwnershipExcel(
+    @CurrentUser() user: User,
+    @Res() res: Response,
+  ) {
+    const documentResponse = await this.vehicleService.ownershipReportExcel(user)
+
+    if (documentResponse) {
+      this.auditService.audit({
+        action: 'getVehicleOwnershipExcel',
+        auth: user,
+      })
+
+      const contentArrayBuffer = await documentResponse.arrayBuffer()
+      const buffer = Buffer.from(contentArrayBuffer)
+
+
+      res.header('Content-length', buffer.length.toString())
+      res.header(
+        'Content-Disposition',
+        `inline; filename=eignastoduvottord-${user.nationalId}.xlsx`,
+      )
+      return res.status(200).end(buffer)
+    }
+    return res.end()
+  }
+
+  @Post('/ownership/pdf/:ssn')
   @Header('Content-Type', 'application/pdf')
   @ApiOkResponse({
     content: { 'application/pdf': {} },
     description: 'Get a pdf ownership document from the Vehicle service',
   })
-  async getVehicleOwnership(
+  async getVehicleOwnershipPdf(
     @Param('ssn') ssn: string,
     @CurrentUser() user: User,
     @Res() res: Response,
   ) {
-    const documentResponse = await this.vehiclePDFService
-      .withMiddleware(new AuthMiddleware(user))
-      .ownershipReportPdfGet({ ssn: ssn })
+    const documentResponse = await this.vehicleService.ownershipReportPdf(user, {personNationalId: ssn})
 
     if (documentResponse) {
       this.auditService.audit({
@@ -83,6 +116,7 @@ export class VehicleController {
         auth: user,
         resources: ssn,
       })
+
 
       const contentArrayBuffer = await documentResponse.arrayBuffer()
       const buffer = Buffer.from(contentArrayBuffer)
@@ -100,4 +134,5 @@ export class VehicleController {
     }
     return res.end()
   }
+
 }
