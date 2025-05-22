e232086e39dc4e3d361bbd5db1cedf8eea8e16f2
Kristófer <kristofer@apro.is>
2025-05-22 09:29:31 +0000

    Deploy application system

--- apps/application-system/api/src/app.ts
+++ apps/application-system/api/src/app.ts
@@ -3,6 +3,8 @@ import { bootstrap } from '@island.is/infra-nest-server'
 import { AppModule } from './app/app.module'
 import { openApi } from './openApi'
 
+// DEPLOYME
+
 export const bootstrapServer = () =>
   bootstrap({
     appModule: AppModule,
