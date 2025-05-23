4f8ef742a614f9ec702b628afc2ead9d167a94e2
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-05-23 16:44:19 +0000

    Restoring mappers file

--- libs/api/domains/health-directorate/src/lib/utils/mappers.ts
+++ libs/api/domains/health-directorate/src/lib/utils/mappers.ts
@@ -48,8 +48,8 @@ export const mapPrescriptionRenewalBlockedReason = (
       return PrescribedItemRenewalBlockedReasonEnum.NotFullyDispensed
     case PrescribedItemDtoRenewalBlockedReasonEnum.IsRegiment:
       return PrescribedItemRenewalBlockedReasonEnum.IsRegiment
-    // case PrescribedItemDtoRenewalBlockedReasonEnum.NoMedCard:
-    //   return PrescribedItemRenewalBlockedReasonEnum.NoMedCard
+    case PrescribedItemDtoRenewalBlockedReasonEnum.NoMedCard:
+      return PrescribedItemRenewalBlockedReasonEnum.NoMedCard
     default:
       return PrescribedItemRenewalBlockedReasonEnum.PendingRequest
   }
