78e82864162f9a3bc3613674fb7f54b93ea2f5a9
Veronika Sif <veronikasif@hotmail.com>
2025-05-23 15:14:27 +0000

    Removed unused from query and model

--- libs/api/domains/social-insurance/src/lib/models/medicalDocuments/rehabilitationPlan.model.ts
+++ libs/api/domains/social-insurance/src/lib/models/medicalDocuments/rehabilitationPlan.model.ts
@@ -1,28 +1,4 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql'
-
-@ObjectType('SocialInsuranceMedicalDocumentsPersonModel')
-class PersonModel {
-  @Field({ nullable: true })
-  name?: string
-
-  @Field({ nullable: true })
-  nationalId?: string
-
-  @Field({ nullable: true })
-  address?: string
-
-  @Field({ nullable: true })
-  postalCode?: string
-
-  @Field({ nullable: true })
-  city?: string
-
-  @Field({ nullable: true })
-  phoneNumber?: string
-
-  @Field({ nullable: true })
-  email?: string
-}
+import { Field, ObjectType } from '@nestjs/graphql'
 
 @ObjectType('SocialInsuranceMedicalDocumentsServiceProviderModel')
 class ServiceProviderModel {
@@ -33,9 +9,6 @@ class ServiceProviderModel {
   coordinatorName?: string
 
   @Field({ nullable: true })
-  coordinatorNationalId?: string
-
-  @Field({ nullable: true })
   coordinatorTitle?: string
 
   @Field({ nullable: true })
@@ -43,75 +16,6 @@ class ServiceProviderModel {
 
   @Field({ nullable: true })
   phoneNumber?: string
-
-  @Field({ nullable: true })
-  email?: string
-}
-
-@ObjectType('SocialInsuranceMedicalDocumentsFirstEvaluationModel')
-class FirstEvaluationModel {
-  @Field({ nullable: true })
-  applicantStatus?: string
-
-  @Field({ nullable: true })
-  previousMedicalOrRehab?: string
-
-  @Field(() => Int, { nullable: true })
-  attitudeTowardsWork?: number
-}
-
-@ObjectType('SocialInsuranceMedicalDocumentsFollowUpEvaluationModel')
-class FollowUpEvaluationModel {
-  @Field(() => Int, { nullable: true })
-  rehabilitationProgress?: number
-
-  @Field({ nullable: true })
-  rehabilitationProgressDetails?: string
-
-  @Field(() => Int, { nullable: true })
-  rehabilitationMeasuresProgress?: number
-
-  @Field({ nullable: true })
-  rehabilitationMeasuresProgressDetails?: string
-
-  @Field(() => Int, { nullable: true })
-  rehabilitationChanges?: number
-
-  @Field({ nullable: true })
-  rehabilitationChangesDetails?: string
-
-  @Field(() => Int, { nullable: true })
-  applicantCircumstancesChanges?: number
-
-  @Field({ nullable: true })
-  applicantCircumstancesChangesDetails?: string
-}
-
-@ObjectType('SocialInsuranceMedicalDocumentsComprehensiveEvaluationModel')
-class ComprehensiveEvaluationModel {
-  @Field(() => Int, { nullable: true })
-  learningAndApplyingKnowledge?: number
-
-  @Field(() => Int, { nullable: true })
-  generalTasksAndDemands?: number
-
-  @Field(() => Int, { nullable: true })
-  communicationAndRelationships?: number
-
-  @Field(() => Int, { nullable: true })
-  mobility?: number
-
-  @Field(() => Int, { nullable: true })
-  selfCare?: number
-
-  @Field(() => Int, { nullable: true })
-  domesticLife?: number
-
-  @Field(() => Int, { nullable: true })
-  mainDailyLifeAreas?: number
-
-  @Field(() => Int, { nullable: true })
-  leisureAndHobbies?: number
 }
 
 @ObjectType('SocialInsuranceMedicalDocumentsHealthGoalsModel')
@@ -125,23 +29,8 @@ class HealthGoalsModel {
 
 @ObjectType('SocialInsuranceMedicalDocumentsRehabilitationPlanModel')
 export class RehabilitationPlanModel {
-  @Field(() => PersonModel, { nullable: true })
-  person?: PersonModel
-
   @Field(() => ServiceProviderModel, { nullable: true })
   serviceProvider?: ServiceProviderModel
-
-  @Field(() => Int, { nullable: true })
-  applicantEmploymentStatus?: number
-
-  @Field(() => FirstEvaluationModel, { nullable: true })
-  firstEvaluation?: FirstEvaluationModel
-
-  @Field(() => FollowUpEvaluationModel, { nullable: true })
-  followUpEvaluation?: FollowUpEvaluationModel
-
-  @Field(() => ComprehensiveEvaluationModel, { nullable: true })
-  comprehensiveEvaluation?: ComprehensiveEvaluationModel
 
   @Field(() => Date, { nullable: true })
   startDate?: Date
--- libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/graphql/queries.ts
+++ libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/graphql/queries.ts
@@ -3,49 +3,12 @@ import gql from 'graphql-tag'
 export const siaRehabilitationPlanQuery = gql`
   query SiaRehabilitationPlan {
     siaGetRehabilitationPlan {
-      person {
-        name
-        nationalId
-        address
-        postalCode
-        city
-        phoneNumber
-        email
-      }
       serviceProvider {
         serviceProviderName
         coordinatorName
-        coordinatorNationalId
         coordinatorTitle
         workplace
         phoneNumber
-        email
-      }
-      applicantEmploymentStatus
-      firstEvaluation {
-        applicantStatus
-        previousMedicalOrRehab
-        attitudeTowardsWork
-      }
-      followUpEvaluation {
-        rehabilitationProgress
-        rehabilitationProgressDetails
-        rehabilitationMeasuresProgress
-        rehabilitationMeasuresProgressDetails
-        rehabilitationChanges
-        rehabilitationChangesDetails
-        applicantCircumstancesChanges
-        applicantCircumstancesChangesDetails
-      }
-      comprehensiveEvaluation {
-        learningAndApplyingKnowledge
-        generalTasksAndDemands
-        communicationAndRelationships
-        mobility
-        selfCare
-        domesticLife
-        mainDailyLifeAreas
-        leisureAndHobbies
       }
       startDate
       plannedEndDate
