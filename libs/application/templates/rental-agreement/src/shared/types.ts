480b37f9fea21e17c1c428caf6e001be342ea3cb
Heba <heba.farestveit@gmail.com>
2025-05-26 14:56:31 +0000

    Break applicationAnswers function to smaller functions and add types.

--- libs/application/templates/rental-agreement/src/shared/types.ts
+++ libs/application/templates/rental-agreement/src/shared/types.ts
@@ -1,4 +1,10 @@
-import { HmsSearchAddress, Unit } from '@island.is/api/schema'
+import {
+  ApplicationFileInput,
+  HmsSearchAddress,
+  Unit,
+} from '@island.is/api/schema'
+import { YesOrNoEnum } from '@island.is/application/core'
+import { NextStepInReviewOptions } from '../utils/enums'
 
 export type ApplicantsInfo = {
   nationalIdWithName: { name: string; nationalId: string }
@@ -14,13 +20,103 @@ export type CostField = {
   hasError?: boolean
 }
 
-export interface PropertyUnit extends Unit {
+export type PropertyUnit = Unit & {
   checked?: boolean
   changedSize?: number
   numOfRooms?: number
 }
 
-export interface AddressProps extends HmsSearchAddress {
+export type AddressProps = HmsSearchAddress & {
   label: string
   value: string
 }
+
+export interface ParticipantsSection {
+  landlords: ApplicantsInfo[]
+  tenants: ApplicantsInfo[]
+}
+
+export interface PropertySection {
+  searchResults: AddressProps | undefined
+  searchResultLabel: string | undefined
+  units: PropertyUnit[]
+  categoryType: string | undefined
+  categoryClass: string | undefined
+  categoryClassGroup: string | undefined
+}
+
+export interface ProvisionsAndConditionSection {
+  description: string | undefined
+  rules: string | undefined
+  conditionDescription: string | undefined
+  inspector: string | undefined
+  inspectorName: string | undefined
+  files: ApplicationFileInput[] | undefined
+}
+
+export interface FireProtectionSection {
+  fireBlanket: string | undefined
+  smokeDetectors: string | undefined
+  fireExtinguisher: string | undefined
+  emergencyExits: string | undefined
+}
+
+export interface RentalPeriodSection {
+  startDate: string | undefined
+  endDate: string | undefined
+  isDefinite: string | undefined
+}
+
+export interface RentalAmountSection {
+  rentalAmount: string | undefined
+  isIndexConnected: string | undefined
+  indexType: string | undefined
+  paymentMethod: string | undefined
+  paymentMethodOther: string | undefined
+  paymentDay: string | undefined
+  paymentDayOther: string | undefined
+  bankAccountNumber: string | undefined
+  nationalIdOfAccountOwner: string | undefined
+  securityDepositRequired: YesOrNoEnum | undefined
+}
+
+export interface SecurityDepositSection {
+  securityDepositType: string | undefined
+  bankGuaranteeInfo: string | undefined
+  thirdPartyGuaranteeInfo: string | undefined
+  insuranceCompanyInfo: string | undefined
+  landlordsMutualFundInfo: string | undefined
+  otherInfo: string | undefined
+  securityDepositAmount: string | undefined
+  securityDepositAmountOther: string | undefined
+  securityAmountCalculated: string | undefined
+}
+
+export interface OtherFeesSection {
+  housingFundPayee: string | undefined
+  housingFundAmount: string | undefined
+  electricityCostPayee: string | undefined
+  electricityCostMeterStatusDate: string | undefined
+  electricityCostMeterNumber: string | undefined
+  electricityCostMeterStatus: string | undefined
+  heatingCostPayee: string | undefined
+  heatingCostMeterStatusDate: string | undefined
+  heatingCostMeterNumber: string | undefined
+  heatingCostMeterStatus: string | undefined
+  otherCostItems: CostField[] | undefined
+}
+
+export interface ReviewSection {
+  nextStepInReviewOptions: NextStepInReviewOptions | undefined
+}
+
+export interface RentalAgreementAnswers
+  extends ParticipantsSection,
+    PropertySection,
+    ProvisionsAndConditionSection,
+    FireProtectionSection,
+    RentalPeriodSection,
+    RentalAmountSection,
+    SecurityDepositSection,
+    OtherFeesSection,
+    ReviewSection {}
