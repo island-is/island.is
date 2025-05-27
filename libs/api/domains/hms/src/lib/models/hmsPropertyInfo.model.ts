4dfd0db8c4a78b317d3f959af619e7e0044e8980
Heba <heba.farestveit@gmail.com>
2025-05-27 09:53:54 +0000

    test change of types to generate

--- libs/api/domains/hms/src/lib/models/hmsPropertyInfo.model.ts
+++ libs/api/domains/hms/src/lib/models/hmsPropertyInfo.model.ts
@@ -103,6 +103,9 @@ export class PropertyInfo {
 
   @Field(() => [AppraisalUnit], { nullable: true })
   readonly appraisalUnits?: AppraisalUnit[]
+
+  @Field(() => String, { nullable: true })
+  readonly test?: string
 }
 
 @ObjectType('HmsPropertyInfos')
