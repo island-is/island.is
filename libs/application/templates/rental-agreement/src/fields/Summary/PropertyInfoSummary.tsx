0454ca56fc2e5b88d312c251eca3ee045b58cfe4
Sonja Jonsdottir <sonjajonsd@gmail.com>
2025-05-28 12:01:52 +0000

    fix(rental-agreement): Emergency Exit input changed to radio button, summary updated

--- libs/application/templates/rental-agreement/src/fields/Summary/PropertyInfoSummary.tsx
+++ libs/application/templates/rental-agreement/src/fields/Summary/PropertyInfoSummary.tsx
@@ -5,6 +5,8 @@ import { FieldBaseProps } from '@island.is/application/types'
 import {
   getPropertyTypeOptions,
   getPropertyClassGroupOptions,
+  getEmergencyExitOptions,
+  getRentalPropetySize,
 } from '../../utils/utils'
 import {
   Routes,
@@ -77,16 +79,7 @@ export const PropertyInfoSummary: FC<Props> = ({ ...props }) => {
     ?.reduce((total, unit) => total + (unit.numOfRooms || 0), 0)
     .toString()
 
-  const propertySize = searchResultUnits
-    ?.reduce(
-      (total, unit) =>
-        total +
-        (unit.changedSize && unit.changedSize !== 0
-          ? unit.changedSize
-          : unit.size || 0),
-      0,
-    )
-    .toString()
+  const propertySize = getRentalPropetySize(searchResultUnits).toString()
 
   return (
     <SummaryCard cardLabel={formatMessage(summary.propertyInfoHeader)}>
@@ -282,14 +275,20 @@ export const PropertyInfoSummary: FC<Props> = ({ ...props }) => {
         </GridColumn>
         <GridColumn span={['12/12', '6/12', '6/12', '6/12', '3/12']}>
           <KeyValue
-            label={summary.fireProtectionsExitsLabel}
-            value={emergencyExits || '-'}
+            label={summary.fireProtectionsFireBlanketLabel}
+            value={fireBlanket || '-'}
           />
         </GridColumn>
         <GridColumn span={['12/12', '6/12', '6/12', '6/12', '3/12']}>
           <KeyValue
-            label={summary.fireProtectionsFireBlanketLabel}
-            value={fireBlanket || '-'}
+            label={summary.fireProtectionsEmergencyExitsLabel}
+            value={
+              getOptionLabel(
+                emergencyExits || '',
+                getEmergencyExitOptions,
+                '',
+              ) || '-'
+            }
           />
         </GridColumn>
       </SummaryCardRow>
