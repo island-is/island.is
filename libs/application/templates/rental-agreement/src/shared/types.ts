7293270e4e34377e86988ea1dc6f4f69a8c65940
Heba <heba.farestveit@gmail.com>
2025-05-28 11:29:44 +0000

    Update shared types for no 'any' warnings in lint

--- libs/application/templates/rental-agreement/src/shared/types.ts
+++ libs/application/templates/rental-agreement/src/shared/types.ts
@@ -9,12 +9,6 @@ export type Scalars = {
   Boolean: boolean
   Int: number
   Float: number
-  /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
-  DateTime: any
-  /** The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
-  JSON: any
-  /** The `JSONObject` scalar type represents JSON objects as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
-  JSONObject: any
 }
 
 export type ApplicantsInfo = {
