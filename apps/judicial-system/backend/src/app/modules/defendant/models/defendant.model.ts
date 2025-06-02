9d9662f465a74fa42b309bf829fbef423bc4e9aa
unakb <una@kolibri.is>
2025-06-02 14:50:07 +0000

    Update defendant.model.ts

--- apps/judicial-system/backend/src/app/modules/defendant/models/defendant.model.ts
+++ apps/judicial-system/backend/src/app/modules/defendant/models/defendant.model.ts
@@ -178,7 +178,9 @@ export class Defendant extends Model {
   verdictAppealDate?: Date
 
   // This is the currenly selected subpoena type per defendant but we also
-  // store the subpoena type in the subpoenas table to keep the history
+  // store the subpoena type in the subpoenas table to keep the history.
+  // We will later remove it from the defendant table when we fix the
+  // handling of new subpoenas per defendant.
   @Column({
     type: DataType.ENUM,
     allowNull: true,
