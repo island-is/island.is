458f40eb493b54c0233d100f02c885558a1524fe
Kristófer <kristofer@apro.is>
2025-06-03 17:52:18 +0000

    Add basePath for consultation portal (is at /samradsgatt)

--- apps/consultation-portal/playwright.config.ts
+++ apps/consultation-portal/playwright.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from '@playwright/test'
 import { baseConfig } from '@island.is/testing/e2e'
-import { type } from '@nx/devkit'
 
 export default defineConfig({
-  ...baseConfig({ filename: __filename, project: 'consultation-portal' }),
+  ...baseConfig({
+    filename: __filename,
+    project: 'consultation-portal',
+    basePath: '/samradsgatt',
+  }),
 })
