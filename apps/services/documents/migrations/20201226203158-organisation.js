1e1eb9a33203ab3b9d90b61a636e2349171ad5d6
disaerna <disa@hugsmidjan.is>
2025-06-03 09:22:03 +0000

    fix: cannot edtir migrations

--- apps/services/documents/migrations/20201226203158-organisation.js
+++ apps/services/documents/migrations/20201226203158-organisation.js
@@ -28,9 +28,6 @@ module.exports = {
         phone_number: {
           type: Sequelize.STRING,
         },
-        zendesk_id: {
-          type: Sequelize.STRING,
-        },
         created: {
           type: 'TIMESTAMP WITH TIME ZONE',
           defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
