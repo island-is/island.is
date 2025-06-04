6eb3ffaba76be8e7169a4ab0155c97caf739b3dd
disaerna <disa@hugsmidjan.is>
2025-06-04 14:57:28 +0000

    fix:lint

--- apps/services/documents/migrations/20250603130429-organisation-w-zendeskid.js
+++ apps/services/documents/migrations/20250603130429-organisation-w-zendeskid.js
@@ -17,7 +17,7 @@ module.exports = {
     )
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface) => {
     return queryInterface.sequelize.transaction((t) =>
       Promise.all([
         queryInterface.removeColumn('organisation', 'zendesk_id', {
