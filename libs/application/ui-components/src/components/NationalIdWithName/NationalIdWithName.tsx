25a72649aab959674481e0f1254e56b81d8811a8
Baldur Óli Barkarson <ballioli1@gmail.com>
2025-06-05 14:23:21 +0200

    Moving statement to correct line

--- libs/application/ui-components/src/components/NationalIdWithName/NationalIdWithName.tsx
+++ libs/application/ui-components/src/components/NationalIdWithName/NationalIdWithName.tsx
@@ -217,6 +217,7 @@ export const NationalIdWithName: FC<
     if (kennitala.isValid(nationalIdInput)) {
       setInvalidNationalId(false)
       searchPersons &&
+        kennitala.isPerson(nationalIdInput) &&
         getIdentity({
           variables: {
             input: {
