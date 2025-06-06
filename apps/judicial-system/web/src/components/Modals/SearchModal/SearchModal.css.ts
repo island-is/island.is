8eafa12df2ef685132164c3d39a075ce127fea18
MargretFinnboga <62568905+MargretFinnboga@users.noreply.github.com>
2025-06-06 15:12:54 +0000

    refactoring

--- apps/judicial-system/web/src/components/Modals/SearchModal/SearchModal.css.ts
+++ apps/judicial-system/web/src/components/Modals/SearchModal/SearchModal.css.ts
@@ -1,11 +1,10 @@
 import { style } from '@vanilla-extract/css'
 
-import { theme } from '@island.is/island-ui/theme'
-
 export const resultButton = style({
   width: '100%',
 })
 
 export const searchModal = style({
   minWidth: '634px',
+  overflow: 'hidden',
 })
