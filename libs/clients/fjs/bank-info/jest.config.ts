647dc5ab871198bf5d9aa8e8874b3b5f4dd317a1
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2025-06-06 15:15:04 +0000

    more renaming

--- libs/clients/fjs/bank-info/jest.config.ts
+++ libs/clients/fjs/bank-info/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/clients/fjs/bankinfo',
+  coverageDirectory: '../../../../coverage/libs/clients/fjs/bank-info',
 }
