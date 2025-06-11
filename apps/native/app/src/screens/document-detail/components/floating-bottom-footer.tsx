4a0e7bcfec039b86b85a5044e7d05ae13d5730e3
snaerseljan <snaerth@gmail.com>
2025-06-11 15:34:00 +0000

    refactor: Fixes for Android to work properly
    - Updated the replyable logic to ensure accurate state handling based on loading conditions.
    - Improved modal dismissal handling to trigger document refetching.
    - Adjusted layout and padding in FloatingBottomFooter and other components for better visual consistency.
    - Streamlined the DocumentReplyScreen navigation logic for improved user experience.

--- apps/native/app/src/screens/document-detail/components/floating-bottom-footer.tsx
+++ apps/native/app/src/screens/document-detail/components/floating-bottom-footer.tsx
@@ -6,20 +6,19 @@ export const FloatingBottomFooter = styled.SafeAreaView`
   left: 0;
   right: 0;
   background-color: ${({ theme }) => theme.color.white};
-  min-height: 100px;
 
-  /* iOS shadow */
   shadow-color: ${({ theme }) => theme.color.blue400};
-  shadow-offset: 0px -4px;
+  shadow-offset-width: 0px;
+  shadow-offset-height: -4px;
   shadow-opacity: 0.08;
   shadow-radius: 12px;
 
   /* Android shadow */
-  elevation: 1;
+  elevation: 4;
 `
 
 export const FloatingBottomContent = styled.View`
   padding-horizontal: ${({ theme }) => theme.spacing.p4}px;
-  padding-top: ${({ theme }) => theme.spacing.p2}px;
+  padding-vertical: ${({ theme }) => theme.spacing.p2}px;
   row-gap: ${({ theme }) => theme.spacing.p2}px;
 `
