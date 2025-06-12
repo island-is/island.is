8282b154b2a2cc93ed3d7eed8ff1a43e255b014c
Ívar Oddsson <ivaroddsson@gmail.com>
2025-06-12 13:53:23 +0000

    feat(j-s): Add court case number to appeal pages (#19298)
    Add court case number to appeal page

--- apps/judicial-system/web/src/components/DateLabel/RulingDateLabel.tsx
+++ apps/judicial-system/web/src/components/DateLabel/RulingDateLabel.tsx
@@ -6,12 +6,15 @@ import { rulingDateLabel as strings } from './RulingDateLabel.strings'
 
 interface Props {
   rulingDate: string
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p'
 }
 
-const RulingDateLabel: FC<Props> = ({ rulingDate }) => {
+const RulingDateLabel: FC<Props> = ({ rulingDate, as }) => {
   const { formatMessage } = useIntl()
 
-  return <DateLabel text={formatMessage(strings.text)} date={rulingDate} />
+  return (
+    <DateLabel text={formatMessage(strings.text)} date={rulingDate} as={as} />
+  )
 }
 
 export default RulingDateLabel
