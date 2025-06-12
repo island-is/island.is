887ebcc0d78740cedfce119a804600fd09983739
Jonni <jonas@norda.is>
2025-06-12 13:32:58 +0000

    chore: remove f from fasteignanumer in mock

--- libs/application/templates/hms/fire-compensation-appraisal/src/lib/template.ts
+++ libs/application/templates/hms/fire-compensation-appraisal/src/lib/template.ts
@@ -105,7 +105,7 @@ const template: ApplicationTemplate<
         },
         on: {
           [DefaultEvents.PAYMENT]: {
-            target: States.PAYMENT,
+            target: States.DONE,
           },
         },
       },
