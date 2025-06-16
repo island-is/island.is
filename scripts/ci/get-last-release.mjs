b7911c8b58a3da72cca03cc5162eccd663efb736
Róberta Andersen <roberta@andes.is>
2025-06-16 15:12:42 +0000

    ensure array

--- scripts/ci/get-last-release.mjs
+++ scripts/ci/get-last-release.mjs
@@ -1,11 +1,12 @@
 const releases = process.argv
   .slice(2)
+  .flatMap(arg => arg.split(/\s+/))
+  .filter((arg) => arg.startsWith('origin/release/'))
   .map((release) => release.replace('origin/release/', ''))
   .filter((release) => /^\d+\.\d+\.\d+$/.test(release))
   .sort((a, b) => {
     const [aMajor, aMinor, aPatch] = a.split('.').map(Number)
     const [bMajor, bMinor, bPatch] = b.split('.').map(Number)
-
     if (aMajor !== bMajor) {
       return bMajor - aMajor
     } else if (aMinor !== bMinor) {
