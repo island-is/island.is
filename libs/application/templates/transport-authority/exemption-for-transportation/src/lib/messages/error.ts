20d217c4fcaca0d9fc33fdd0f6ab9d5dad3caa2a
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-06-18 09:45:52 +0000

    Update readme

--- libs/application/templates/transport-authority/exemption-for-transportation/src/lib/messages/error.ts
+++ libs/application/templates/transport-authority/exemption-for-transportation/src/lib/messages/error.ts
@@ -2,17 +2,17 @@ import { defineMessages } from 'react-intl'
 
 export const error = defineMessages({
   numberValueIsNotWithinLimit: {
-    id: 'ta.eft:error.numberValueIsNotWithinLimit',
+    id: 'ta.eft.application:error.numberValueIsNotWithinLimit',
     defaultMessage: 'Verður að vera á milli {min} og {max}',
     description: 'Error number value is not within limit',
   },
   submitErrorTitle: {
-    id: 'ta.eft:error.submitErrorTitle',
+    id: 'ta.eft.application:error.submitErrorTitle',
     defaultMessage: 'Það kom upp villa',
     description: 'Error title when submitting application',
   },
   submitErrorFallbackMessage: {
-    id: 'ta.eft:error.submitErrorFallbackMessage',
+    id: 'ta.eft.application:error.submitErrorFallbackMessage',
     defaultMessage: 'Það kom upp villa við að senda inn umsókn',
     description: 'Error fallback message when submitting application',
   },
