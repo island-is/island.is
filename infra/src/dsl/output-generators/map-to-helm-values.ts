f2f13ac4d83f23ca125572b1bd98e561f345aaad
Kristófer <kristofer@apro.is>
2025-06-18 14:49:06 +0000

    Revert "Formatting"
    This reverts commit 795f5d99b8cbfb2587b631e4bf6ddb2bc1690d4e.

--- infra/src/dsl/output-generators/map-to-helm-values.ts
+++ infra/src/dsl/output-generators/map-to-helm-values.ts
@@ -353,9 +353,8 @@ function serializeIngress(
   ingressConf: IngressForEnv,
   env: EnvironmentConfig,
 ): NonNullable<HelmService['ingress']>[string] {
-  const hosts = (typeof ingressConf.host === 'string'
-    ? [ingressConf.host]
-    : ingressConf.host
+  const hosts = (
+    typeof ingressConf.host === 'string' ? [ingressConf.host] : ingressConf.host
   ).map((host) =>
     ingressConf.public ?? true
       ? hostFullName(host, env)
