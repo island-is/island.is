5084983cd08c33d9655125500a2c8fa5aec61b1f
Kristófer <kristofer@apro.is>
2025-06-18 16:22:37 +0000

    Fix names for config

--- apps/judicial-system/web/playwright.config.ts
+++ apps/judicial-system/web/playwright.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from '@playwright/test'
 import { baseConfig } from '@island.is/testing/e2e'
 
 export default defineConfig({
-  ...baseConfig({ filename: __filename, project: 'judicial-system' }),
+  ...baseConfig({ filename: __filename, project: 'judicial-system-web' }),
 })
+
