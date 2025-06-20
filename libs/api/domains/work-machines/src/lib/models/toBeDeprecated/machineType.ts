e55aca248e7ccbd07e9e3e88e724d91c684044fb
Þorkell Máni Þorkelsson <wowthorkell@gmail.com>
2025-06-20 11:41:20 +0000

    feat(work-machines): Domain refactor (#18737)
    * fix: wip
    
    * feat: update and reuse
    
    * feat: some updates
    
    * feat: upudate
    
    * feat: download works
    
    * chore: update models
    
    * fix: add owrk machine
    
    * feat: work machine updates
    
    * feat: more updates
    
    * feat: big refactor
    
    * feat: more updates
    
    * feat: more updates
    
    * chore: remove mocks
    
    * fix: stuff
    
    * fix: add better locale
    
    * feat: works
    gp
    
    * fix: chores
    
    * fix: type import
    
    * feat: totally works now
    
    gp
    
    * Update queries.ts
    
    * fix: weird build error
    
    * fix: revert event list change
    
    * fix: update type
    
    * variableName changes to name
    
    * chore: revert
    
    * chore: more revertt
    
    * chore: pls
    
    * fix: review-rabbit comments
    
    * fix: review comments
    
    * chore: linting
    
    ---------
    
    Co-authored-by: Sigrún Tinna Gissurardóttir <sigrun.tinna.gissurardottir@origo.is>

--- libs/api/domains/work-machines/src/lib/models/toBeDeprecated/machineType.ts
+++ libs/api/domains/work-machines/src/lib/models/toBeDeprecated/machineType.ts
@@ -1,5 +1,6 @@
-import { ObjectType, Field } from '@nestjs/graphql'
+import { ObjectType, Field, Directive } from '@nestjs/graphql'
 
+@Directive('@deprecated(reason: "Up for removal")')
 @ObjectType('WorkMachinesMachineType')
 export class MachineType {
   @Field(() => String, { nullable: true })
