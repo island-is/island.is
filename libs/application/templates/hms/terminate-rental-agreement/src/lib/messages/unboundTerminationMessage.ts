59202013e4c15ee3c14b1c25e2f25dc615b9bd77
Jónas G. Sigurðsson <jonas@norda.is>
2025-06-23 13:28:16 +0000

    feat(hms): application to terminate rental agreements (#19004)
    * feat: make a boilerplate application for termination of rental agreements
    
    * feat: start of application
    
    * feat: add back button to the overview screen
    
    * feat: start of rentalAgreements dataProvider
    
    * feat: overview screen
    
    * feat: add fileUpload and send it in
    
    * feat: add noContracts form
    
    * feat: dataSchema
    
    * revert: changes
    
    * chore: simplify terminationReasonOptions
    
    * chore: update codeowners
    
    * feat: add featureflag
    
    * chore: remove unused import
    
    * fix: codeRabbit comments
    
    * chore: code rabbit comments
    
    * fix: typo

---
+++ libs/application/templates/hms/terminate-rental-agreement/src/lib/messages/unboundTerminationMessage.ts
@@ -0,0 +1,19 @@
+import { defineMessages } from 'react-intl'
+
+export const unboundTerminationMessages = defineMessages({
+  title: {
+    id: 'tra.application:unboundTermination.title',
+    defaultMessage: 'Uppsögn ótímabundins leigusamnings',
+    description: 'Unbound termination title',
+  },
+  dateTitle: {
+    id: 'tra.application:unboundTermination.dateTitle',
+    defaultMessage: 'Dagsetning leigusamningsloka',
+    description: 'Unbound termination date title',
+  },
+  selectTitle: {
+    id: 'tra.application:unboundTermination.selectTitle',
+    defaultMessage: 'Ástæða uppsagnar',
+    description: 'Unbound termination select title',
+  },
+})
