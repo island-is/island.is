da60714d6f82561c8b7acfa6163e77862d54def4
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2025-06-24 13:40:50 +0000

    feat(form-system): add delete functionality for sections, screens, and fields (#19419)
    * feat(MainColumns): add delete functionality for sections, screens, and fields
    fix(NavComponent): refactor NavButtons to accept props and improve layout
    fix(truncateText): adjust maxLength values for truncating names
    
    * feat(MainColumns): integrate DeleteButton component for item removal

--- libs/portals/admin/form-system/src/components/FormLayout/components/MainColumns.tsx
+++ libs/portals/admin/form-system/src/components/FormLayout/components/MainColumns.tsx
@@ -1,22 +1,111 @@
-import { Box } from '@island.is/island-ui/core'
+import { Box, Button, DialogPrompt } from '@island.is/island-ui/core'
 import { MainContent } from '../../MainContent/MainContent'
+import React, { useContext } from 'react'
+import { ControlContext } from '../../../context/ControlContext'
+import { m } from '@island.is/form-system/ui'
+import { useIntl } from 'react-intl'
+import { useMutation } from '@apollo/client'
+import { DELETE_SCREEN, DELETE_FIELD, DELETE_SECTION } from '@island.is/form-system/graphql'
+import { DeleteButton } from './DeleteButton'
 
 export const MainContentColumn = () => {
+  const { control, controlDispatch } = useContext(ControlContext)
+  const { activeItem, form } = control
+  const { screens, fields } = form
+  const { type } = activeItem
+  const { formatMessage } = useIntl()
+  const deleteScreen = useMutation(DELETE_SCREEN)
+  const deleteField = useMutation(DELETE_FIELD)
+  const deleteSection = useMutation(DELETE_SECTION)
+
+  const containsGroupOrInput = (): boolean => {
+    if (type === 'Section') {
+      return screens?.some(
+        (screen) => screen?.sectionId === activeItem?.data?.id,
+      ) || false;
+    }
+    if (type === 'Screen') {
+      return fields?.some((field) => field?.screenId === activeItem?.data?.id) || false;
+    }
+    return false
+  }
+
+
+
+  const remove = async () => {
+    const id = activeItem?.data?.id as string
+    try {
+      if (activeItem.type === 'Section') {
+        await deleteSection[0]({
+          variables: {
+            input: {
+              id: id,
+            },
+          },
+        })
+        controlDispatch({ type: 'REMOVE_SECTION', payload: { id: id } })
+      } else if (activeItem.type === 'Screen') {
+        await deleteScreen[0]({
+          variables: {
+            input: {
+              id: id,
+            },
+          },
+        })
+        controlDispatch({ type: 'REMOVE_SCREEN', payload: { id: id } })
+      } else if (activeItem.type === 'Field') {
+        await deleteField[0]({
+          variables: {
+            input: {
+              id: id,
+            },
+          },
+        })
+        controlDispatch({ type: 'REMOVE_FIELD', payload: { id: id } })
+      }
+    } catch (error) {
+      console.error('Error removing item:', error)
+    }
+  }
+
   return (
     <Box
-      border="standard"
-      borderRadius="standard"
-      width="full"
       style={{
-        minHeight: '500px',
-        overflow: 'auto',
-        maxHeight: '70vh',
         maxWidth: '1200px',
         width: '64%',
+        position: 'fixed',
       }}
-      position="fixed"
     >
-      <MainContent />
+      {containsGroupOrInput() ? (
+        <DialogPrompt
+          baseId="remove"
+          title={formatMessage(m.areYouSure)}
+          description={formatMessage(m.completelySure)}
+          ariaLabel="Remove item"
+          buttonTextConfirm={formatMessage(m.confirm)}
+          buttonTextCancel={formatMessage(m.cancel)}
+          onConfirm={remove}
+          disclosureElement={
+            <DeleteButton onClick={() => { }} label={formatMessage(m.delete)} />
+          }
+        />
+      ) : (
+        <DeleteButton onClick={remove} label={formatMessage(m.delete)} />
+      )}
+
+      <Box
+        border="standard"
+        borderRadius="standard"
+        width="full"
+        style={{
+          minHeight: '500px',
+          overflow: 'auto',
+          maxHeight: '70vh',
+        }}
+      >
+        <MainContent />
+      </Box>
     </Box>
   )
 }
+
--- libs/portals/admin/form-system/src/components/NavComponent/components/NavButtons.tsx
+++ libs/portals/admin/form-system/src/components/NavComponent/components/NavButtons.tsx
@@ -1,4 +1,4 @@
-import { Box, DialogPrompt, Icon, Tooltip } from '@island.is/island-ui/core'
+import { Box, Icon, Tooltip } from '@island.is/island-ui/core'
 import { useContext } from 'react'
 import { FormSystemScreen, FormSystemField } from '@island.is/api/schema'
 import { ControlContext } from '../../../context/ControlContext'
@@ -8,14 +8,16 @@ import { useMutation } from '@apollo/client'
 import {
   CREATE_FIELD,
   CREATE_SCREEN,
-  DELETE_FIELD,
-  DELETE_SCREEN,
-  DELETE_SECTION,
 } from '@island.is/form-system/graphql'
 import { m } from '@island.is/form-system/ui'
 import { FieldTypesEnum } from '@island.is/form-system/enums'
 
-export const NavButtons = () => {
+interface Props {
+  id: string
+  type: 'Section' | 'Screen' | 'Field'
+}
+
+export const NavButtons = ({ id, type }: Props) => {
   const { control, controlDispatch } = useContext(ControlContext)
   const { activeItem, form } = control
   const { screens, fields } = form
@@ -25,32 +27,17 @@ export const NavButtons = () => {
       ? formatMessage(m.addScreenHover)
       : formatMessage(m.addFieldHover)
 
-  const containsGroupOrInput = (): boolean | undefined => {
-    const { type } = activeItem
-    if (type === 'Section') {
-      return screens?.some(
-        (screen) => screen?.sectionId === activeItem?.data?.id,
-      )
-    }
-    if (type === 'Screen') {
-      return fields?.some((field) => field?.screenId === activeItem?.data?.id)
-    }
-    return false
-  }
-
   const createScreen = useMutation(CREATE_SCREEN)
   const createField = useMutation(CREATE_FIELD)
-  const deleteScreen = useMutation(DELETE_SCREEN)
-  const deleteField = useMutation(DELETE_FIELD)
-  const deleteSection = useMutation(DELETE_SECTION)
+
 
   const addItem = async () => {
-    if (activeItem.type === 'Section') {
+    if (type === 'Section') {
       const newScreen = await createScreen[0]({
         variables: {
           input: {
             createScreenDto: {
-              sectionId: activeItem?.data?.id,
+              sectionId: id,
               displayOrder: screens?.length,
             },
           },
@@ -66,12 +53,12 @@ export const NavButtons = () => {
           },
         })
       }
-    } else if (activeItem.type === 'Screen') {
+    } else if (type === 'Screen') {
       const newField = await createField[0]({
         variables: {
           input: {
             createFieldDto: {
-              screenId: activeItem?.data?.id,
+              screenId: id,
               fieldType: FieldTypesEnum.TEXTBOX,
               displayOrder: fields?.length ?? 0,
             },
@@ -91,77 +78,20 @@ export const NavButtons = () => {
     }
   }
 
-  const remove = async () => {
-    const id = activeItem?.data?.id as string
-    if (activeItem.type === 'Section') {
-      await deleteSection[0]({
-        variables: {
-          input: {
-            id: id,
-          },
-        },
-      })
-      controlDispatch({ type: 'REMOVE_SECTION', payload: { id: id } })
-    } else if (activeItem.type === 'Screen') {
-      await deleteScreen[0]({
-        variables: {
-          input: {
-            id: id,
-          },
-        },
-      })
-      controlDispatch({ type: 'REMOVE_SCREEN', payload: { id: id } })
-    } else if (activeItem.type === 'Field') {
-      await deleteField[0]({
-        variables: {
-          input: {
-            id: id,
-          },
-        },
-      })
-      controlDispatch({ type: 'REMOVE_FIELD', payload: { id: id } })
-    }
-  }
-
   return (
     <Box display="flex" flexDirection="row">
-      {activeItem.type !== 'Field' && (
+      {type !== 'Field' && (
         <Box
           style={{ paddingTop: '5px', cursor: 'pointer' }}
           marginRight={1}
-          onClick={addItem}
+          onClick={(e) => {
+            addItem()
+            e.stopPropagation()
+          }}
         >
           <Tooltip text={hoverText} color="yellow200">
             <span>
               <Icon icon="add" color="blue400" size="medium" />
-            </span>
-          </Tooltip>
-        </Box>
-      )}
-      {containsGroupOrInput() ? (
-        <DialogPrompt
-          baseId="remove"
-          title={formatMessage(m.areYouSure)}
-          description={formatMessage(m.completelySure)}
-          ariaLabel="Remove item"
-          buttonTextConfirm={formatMessage(m.confirm)}
-          buttonTextCancel={formatMessage(m.cancel)}
-          onConfirm={remove}
-          disclosureElement={
-            <Box style={{ paddingTop: '5px', cursor: 'pointer' }}>
-              <Tooltip text={formatMessage(m.delete)}>
-                <span>
-                  <Icon icon="trash" size="medium" />
-                </span>
-              </Tooltip>
-            </Box>
-          }
-        />
-      ) : (
-        <Box style={{ paddingTop: '5px', cursor: 'pointer' }} onClick={remove}>
-          <Tooltip text={formatMessage(m.delete)}>
-            <span>
-              <Icon icon="trash" size="medium" />
             </span>
           </Tooltip>
         </Box>
