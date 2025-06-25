a87ca8727d23590bfc1ee00aafe931a509d85554
Hjörtur Jóhann Vignisson <hjorturjohann94@gmail.com>
2025-06-25 15:43:02 +0000

    fix(application-system): Generating new paths and simplifying client setup (#19450)
    Generating new paths and simplifying client setup

--- libs/clients/rsk/rental-day-rate/src/lib/RskRentalDayRateClientConfig.ts
+++ libs/clients/rsk/rental-day-rate/src/lib/RskRentalDayRateClientConfig.ts
@@ -4,12 +4,6 @@ import { defineConfig } from '@island.is/nest/config'
 
 const schema = z.object({
   xRoadServicePath: z.string(),
-  tokenExchangeScope: z.array(z.string()),
-  requestActorToken: z.boolean(),
-  redis: z.object({
-    nodes: z.array(z.string()),
-    ssl: z.boolean(),
-  }),
 })
 
 export const RskRentalDayRateClientConfig = defineConfig({
@@ -17,17 +11,8 @@ export const RskRentalDayRateClientConfig = defineConfig({
   schema,
   load: (env) => ({
     xRoadServicePath: env.required(
-      'XROAD_RSK_PROCURING_PATH',
+      'XROAD_RSK_RENTAL_RATE_PATH',
       'IS-DEV/GOV/10006/Skatturinn/rentaldayrate-v1',
     ),
-    tokenExchangeScope: env.optionalJSON('XROAD_RSK_PROCURING_SCOPE') ?? [
-      '@rsk.is/prokura',
-    ],
-    requestActorToken:
-      env.optionalJSON('XROAD_RSK_PROCURING_ACTOR_TOKEN') ?? false,
-    redis: {
-      nodes: env.optionalJSON('XROAD_RSK_PROCURING_REDIS_NODES') ?? [],
-      ssl: env.optionalJSON('XROAD_RSK_PROCURING_REDIS_SSL', false) ?? true,
-    },
   }),
 })
--- libs/clients/rsk/rental-day-rate/src/lib/apiConfiguration.ts
+++ libs/clients/rsk/rental-day-rate/src/lib/apiConfiguration.ts
@@ -1,5 +1,4 @@
 import {
-  buildCacheControl,
   createEnhancedFetch,
 } from '@island.is/clients/middlewares'
 import {
@@ -10,8 +9,6 @@ import {
 import { Configuration } from '../../gen/fetch'
 import { RskRentalDayRateClientConfig } from './RskRentalDayRateClientConfig'
 
-import { createRedisCacheManager } from '@island.is/cache'
-
 export const RskRentalDayRateConfigurationProvider = {
   provide: Configuration,
   // Necessary because of cache-manager.
@@ -19,38 +16,11 @@ export const RskRentalDayRateConfigurationProvider = {
   useFactory: async (
     config: ConfigType<typeof RskRentalDayRateClientConfig>,
     xRoadConfig: ConfigType<typeof XRoadConfig>,
-    idsClientConfig: ConfigType<typeof IdsClientConfig>,
   ) => {
     return new Configuration({
       fetchApi: createEnhancedFetch({
         name: 'clients-rental-day-rate',
         organizationSlug: 'skatturinn',
-        cache:
-          config.redis.nodes.length === 0
-            ? undefined
-            : {
-                cacheManager: await createRedisCacheManager({
-                  name: 'clients-rental-day-rate',
-                  nodes: config.redis.nodes,
-                  ssl: config.redis.ssl,
-                  noPrefix: true,
-                  ttl: 0,
-                }),
-                shared: false,
-                overrideCacheControl: buildCacheControl({ maxAge: 60 * 10 }),
-              },
-        autoAuth: idsClientConfig.isConfigured
-          ? {
-              mode: 'tokenExchange',
-              issuer: idsClientConfig.issuer,
-              clientId: idsClientConfig.clientId,
-              clientSecret: idsClientConfig.clientSecret,
-              scope: config.tokenExchangeScope,
-              tokenExchange: {
-                requestActorToken: config.requestActorToken,
-              },
-            }
-          : undefined,
       }),
       basePath: `${xRoadConfig.xRoadBasePath}/r1/${config.xRoadServicePath}`,
       headers: {
