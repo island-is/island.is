9f884ab415b4dcc4880a239138c3ba0b0bb25286
Albina Guseynova <albina@juni.is>
2025-06-27 11:45:41 +0000

    fix(signature-collection): minor ui tweaks my-pages (#19488)
    * fix(signature-collection): minor ui tweaks my-pages
    
    * editpage tweak
    
    * upload signature button tweak
    
    * linting
    
    ---------
    
    Co-authored-by: albina <albina@kosmosogkaos.is>

--- libs/portals/my-pages/signature-collection/src/screens/Municipal/OwnerView/ViewList/ListActions/index.tsx
+++ libs/portals/my-pages/signature-collection/src/screens/Municipal/OwnerView/ViewList/ListActions/index.tsx
@@ -27,15 +27,15 @@ const ListActions = ({ listId }: { listId: string }) => {
         <Text marginTop={6} marginBottom={8}>
           {formatMessage(m.listActionsDescription)}
         </Text>
-        <Box display="flex" justifyContent="spaceBetween" marginBottom={10}>
-          <Box>
+        <Box display={["block", "block", "flex"]} justifyContent="spaceBetween" marginBottom={10}>
+          <Box marginBottom={[2, 2, 0]}>
             <Text variant="h4">{formatMessage(m.downloadPdf)}</Text>
             <Text>{formatMessage(m.downloadPdfDescription)}</Text>
           </Box>
           <PdfReport listId={listId} collectionType={collectionType} />
         </Box>
-        <Box display="flex" justifyContent="spaceBetween">
-          <Box>
+        <Box display={["block", "block", "flex"]} justifyContent="spaceBetween">
+          <Box marginBottom={[2, 2, 0]}>
             <Text variant="h4">{formatMessage(m.deleteCollection)}</Text>
             <Text>{formatMessage(m.deleteCollectionDescription)}</Text>
           </Box>
