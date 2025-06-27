7392390e5e754385c5e1019e8b8334fc0c44a8e4
Kristofer <kristofer@andes.is>
2025-06-27 14:52:21 +0000

    chore: formatting (#19500)
    formatting

--- libs/portals/my-pages/signature-collection/src/screens/Municipal/OwnerView/ViewList/ListActions/index.tsx
+++ libs/portals/my-pages/signature-collection/src/screens/Municipal/OwnerView/ViewList/ListActions/index.tsx
@@ -27,14 +27,18 @@ const ListActions = ({ listId }: { listId: string }) => {
         <Text marginTop={6} marginBottom={8}>
           {formatMessage(m.listActionsDescription)}
         </Text>
-        <Box display={["block", "block", "flex"]} justifyContent="spaceBetween" marginBottom={10}>
+        <Box
+          display={['block', 'block', 'flex']}
+          justifyContent="spaceBetween"
+          marginBottom={10}
+        >
           <Box marginBottom={[2, 2, 0]}>
             <Text variant="h4">{formatMessage(m.downloadPdf)}</Text>
             <Text>{formatMessage(m.downloadPdfDescription)}</Text>
           </Box>
           <PdfReport listId={listId} collectionType={collectionType} />
         </Box>
-        <Box display={["block", "block", "flex"]} justifyContent="spaceBetween">
+        <Box display={['block', 'block', 'flex']} justifyContent="spaceBetween">
           <Box marginBottom={[2, 2, 0]}>
             <Text variant="h4">{formatMessage(m.deleteCollection)}</Text>
             <Text>{formatMessage(m.deleteCollectionDescription)}</Text>
