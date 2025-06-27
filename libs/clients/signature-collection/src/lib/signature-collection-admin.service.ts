c8784e43b4515a2baae467d6b67031ec7e9554ec
Haukur Ingi <haukur@juni.is>
2025-06-27 11:29:44 +0000

    chore: lint

--- libs/clients/signature-collection/src/lib/signature-collection-admin.service.ts
+++ libs/clients/signature-collection/src/lib/signature-collection-admin.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { KosningApi, MedmaeliBulkItemDTO } from '../../gen/fetch'
 import {
   GetListInput,
