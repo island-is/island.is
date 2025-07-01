02ed1d7158cb00e810f63bce3fc7f14713b2c7db
Haukur Ingi <haukur@juni.is>
2025-07-01 14:51:57 +0000

    fix(signature-collection): include inactive areas

--- libs/clients/signature-collection/src/lib/types/collection.dto.ts
+++ libs/clients/signature-collection/src/lib/types/collection.dto.ts
@@ -116,7 +116,7 @@ const getStatus = ({
 
 export const mapCollection = (
   collection: MedmaelasofnunExtendedDTO,
-  activeAreas: string[],
+  participatingAreas: Area[],
 ): Collection => {
   const {
     id,
@@ -163,13 +163,7 @@ export const mapCollection = (
     candidates: candidates
       ? candidates.map((candidate) => mapCandidate(candidate, governmentalArea))
       : [],
-    areas: areas.map((area) =>
-      mapArea(
-        area,
-        activeAreas.some((a) => a === area.id?.toString()),
-        id.toString(),
-      ),
-    ),
+    areas: participatingAreas,
     processed,
     status,
     collectionType,
