5762aee8926212ff427d782c0ae8835e076df7f2
Ingvar Sigurðsson <ingvar@apro.is>
2025-07-03 10:47:55 +0000

    fix: syntax error in destroy-db script

--- infra/scripts/container-scripts/destroy-dbs.sh
+++ infra/scripts/container-scripts/destroy-dbs.sh
@@ -13,14 +13,14 @@ echo "feature name is $FEATURE_NAME"
 
 psql -tc "SELECT datname FROM pg_database WHERE datname like 'feature_${FEATURE_DB_NAME}_%'" --field-separator ' ' --no-align --quiet |
   while read -r dbname; do
-    psql -c "DROP DATABASE IF EXISTS '$dbname' WITH(FORCE);"
-    echo "Dropping database with dbname '$dbname'"
+    psql -c "DROP DATABASE IF EXISTS $dbname WITH(FORCE);"
+    echo "Dropping database with dbname $dbname"
   done
 
 psql -tc "SELECT rolname FROM pg_roles WHERE rolname like 'feature_${FEATURE_DB_NAME}_%'" --field-separator ' ' --no-align --quiet |
   while read -r rolname; do
-    psql -c "DROP USER '$rolname' WITH(FORCE);"
-    echo "Deleting the rolname '$rolname'"
+    psql -c "DROP ROLE IF EXISTS $rolname;"
+    echo "Deleting the role $rolname"
   done
 
 node secrets delete "/k8s/feature-$FEATURE_NAME"
