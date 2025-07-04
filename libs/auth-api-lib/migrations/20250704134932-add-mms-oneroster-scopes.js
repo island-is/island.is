d663e340355a2d3dafd47f42c4c7adacfd7d0ad4
Sævar Már Atlason <saevar.m.atlason@gmail.com>
2025-07-04 14:02:44 +0000

    Fixing syntax error.

--- libs/auth-api-lib/migrations/20250704134932-add-mms-oneroster-scopes.js
+++ libs/auth-api-lib/migrations/20250704134932-add-mms-oneroster-scopes.js
@@ -10,8 +10,7 @@ module.exports = {
       INSERT INTO api_scope (name, display_name, description, domain_name, enabled, show_in_discovery_document, required, emphasize, allow_explicit_delegation_grant, automatic_delegation_grant, also_for_delegated_user, is_access_controlled, grant_to_legal_guardians, grant_to_procuring_holders, grant_to_personal_representatives)
       VALUES
         ('https://purl.imsglobal.org/spec/or/v1p2/scope/roster-core.readonly','Roster Core ReadOnly', 'The core set of read operations to enable information about collections or a single object to be obtained.', '@mms.is', true, false, false, false, false, false, false, false, false, false, false),
-        ('https://purl.imsglobal.org/spec/or/v1p2/scope/roster-demographics.readonly', 'Roster Demographics ReadOnly', 'The read operations to provide all demographics or a single demographics object to be obtained.', '
-        '@mms.is', true, false, false, false, false, false, false, false, false, false, false),
+        ('https://purl.imsglobal.org/spec/or/v1p2/scope/roster-demographics.readonly', 'Roster Demographics ReadOnly', 'The read operations to provide all demographics or a single demographics object to be obtained.', '@mms.is', true, false, false, false, false, false, false, false, false, false, false),
         ('https://purl.imsglobal.org/spec/or/v1p2/scope/roster.readonly', 'Roster ReadOnly', 'Support for all of the read operations (excluding demographics) to enable information about collections or a single object to be obtained.', '@mms.is', true, false, false, false, false, false, false, false, false, false, false);
     `)
   },
