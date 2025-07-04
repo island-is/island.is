d97d42bbfc7c604fb0e13df9ea0e9298e53c4182
Ívar Oddsson <ivaroddsson@gmail.com>
2025-07-04 13:27:06 +0000

    Remove lawyer registry config

--- apps/judicial-system/api/src/app/modules/defender/defender.service.ts
+++ apps/judicial-system/api/src/app/modules/defender/defender.service.ts
@@ -2,18 +2,13 @@ import { Inject, Injectable } from '@nestjs/common'
 
 import type { Logger } from '@island.is/logging'
 import { LOGGER_PROVIDER } from '@island.is/logging'
-import { type ConfigType } from '@island.is/nest/config'
 
 import { LawyersService } from '@island.is/judicial-system/lawyers'
 import { type Lawyer, mapToLawyer } from '@island.is/judicial-system/types'
 
-import { defenderModuleConfig } from './defender.config'
-
 @Injectable()
 export class DefenderService {
   constructor(
-    @Inject(defenderModuleConfig.KEY)
-    private readonly config: ConfigType<typeof defenderModuleConfig>,
     private readonly lawyersService: LawyersService,
     @Inject(LOGGER_PROVIDER)
     private readonly logger: Logger,
