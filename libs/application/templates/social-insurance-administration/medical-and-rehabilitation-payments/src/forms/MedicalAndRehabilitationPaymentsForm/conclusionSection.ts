7c9a979cc03e328cd15c4f770488770e54a2db64
Ylfa Hafsteinsdóttir <yhafsteinsdottir@deloitte.is>
2025-07-08 11:09:36 +0000

    feat(medical-and-rehabilitation-payments): Application Updates (#19564)
    * feat(medical-and-rehabilitation-payments): Self-assessment Questions 2 (#19480)
    
    [TS-1042] Self-assessment Questions 2
    
    * feat(medical-and-rehabilitation-payments): Conclusion screen (#19449)
    
    * add text to conclusion screen
    
    * format
    
    * feat(medical-and-rehabilitation-payments): Update union and employee sick pay screens (#19504)
    
    * update union and employee sick pay screens
    
    * remove everything connected to union fileUpload
    
    * add function to avoid duplicate code
    
    * Added removed message that is still used
    
    ---------
    
    Co-authored-by: Veronika Sif <veronikasif@hotmail.com>
    
    * feat(medical-and-rehabilitation-payments): Update Questions Screen (#19459)
    
    * [TS-1017] Questions Screen
    
    * Update RehabilitationPlan - If confirmed, allow submission
    
    * Updated validation for Country ID number/personal ID number
    
    * Updated model names to singular
    
    * Updated General graphql after review
    
    * Moved Benefits from another country to a separate page
    
    * feat(medical-and-rehabilitation-payments): Certificate for Sickness and Rehabilitation data implementation (#19548)
    
    [TS-1021] Certificate for Sickness and Rehabilitation Screen
    
    * [TS-1022] Rehabilitation Plan - Update
    
    * Updated rehabilitationPlan and clientConfig
    
    * Update buildOverviewField and buildTableRepeaterField
    
    ---------
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    Co-authored-by: Veronika Sif <veronikasif@hotmail.com>

--- libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/forms/MedicalAndRehabilitationPaymentsForm/conclusionSection.ts
+++ libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/forms/MedicalAndRehabilitationPaymentsForm/conclusionSection.ts
@@ -1,6 +1,12 @@
 import { buildFormConclusionSection } from '@island.is/application/ui-forms'
+import { medicalAndRehabilitationPaymentsFormMessage } from '../../lib/messages'
 
 export const conclusionSection = buildFormConclusionSection({
-  expandableIntro: '',
-  expandableDescription: '',
+  multiFieldTitle: medicalAndRehabilitationPaymentsFormMessage.conclusion.title,
+  alertTitle: medicalAndRehabilitationPaymentsFormMessage.conclusion.alertTitle,
+  alertMessage:
+    medicalAndRehabilitationPaymentsFormMessage.conclusion.alertMessage,
+  expandableIntro: medicalAndRehabilitationPaymentsFormMessage.conclusion.next,
+  expandableDescription:
+    medicalAndRehabilitationPaymentsFormMessage.conclusion.entitlements,
 })
