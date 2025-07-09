df0a2fa943ceb1cfa1e41942a7871a73dcf22814
bkristmundsson <bikkibo@gmail.com>
2025-07-09 13:54:43 +0000

    Code refactor

--- libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/forms/MedicalAndRehabilitationPaymentsForm/rehabilitationPlanSection.ts
+++ libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/forms/MedicalAndRehabilitationPaymentsForm/rehabilitationPlanSection.ts
@@ -4,22 +4,13 @@ import {
   buildSection,
 } from '@island.is/application/core'
 import { medicalAndRehabilitationPaymentsFormMessage } from '../../lib/messages'
-import { getApplicationExternalData } from '../../utils/medicalAndRehabilitationPaymentsUtils'
-import { MedicalAndRehabilitationPaymentsApplicationType } from '../../utils/constants'
+import { isEHApplication } from '../../utils/conditionUtils'
 
 export const rehabilitationPlanSection = buildSection({
   id: 'rehabilitationPlanSection',
   title:
     medicalAndRehabilitationPaymentsFormMessage.rehabilitationPlan.sectionTitle,
-  condition: (_, externalData) => {
-    const { marpApplicationType } = getApplicationExternalData(externalData)
-    return (
-      marpApplicationType ===
-        MedicalAndRehabilitationPaymentsApplicationType.EH1 ||
-      marpApplicationType ===
-        MedicalAndRehabilitationPaymentsApplicationType.EH2
-    )
-  },
+  condition: (_, externalData) => isEHApplication(externalData),
   children: [
     buildMultiField({
       id: 'rehabilitationPlan',
