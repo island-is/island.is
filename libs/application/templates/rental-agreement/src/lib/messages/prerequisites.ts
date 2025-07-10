5ed2c0d036d67037315aab00321a3cf612719958
Árni Jónsson <addininja@gmail.com>
2025-07-10 11:31:03 +0200

    Add approve action to rental agreement template
    Introduces an 'approve' action to the RentalAgreementTemplate with a corresponding button label in the messages. This allows users to approve applications directly from the template.

--- libs/application/templates/rental-agreement/src/lib/messages/prerequisites.ts
+++ libs/application/templates/rental-agreement/src/lib/messages/prerequisites.ts
@@ -51,6 +51,11 @@ export const application = defineMessages({
     defaultMessage: 'Senda í undirritun',
     description: `Button text for going to signing`,
   },
+  approve: {
+    id: 'ra.application:application.approve',
+    defaultMessage: 'Samþyggja',
+    description: `Button text for approving`,
+  },
 })
 
 export const prerequisites = {
