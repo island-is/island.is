bff8a7e06b2211ac6d333d09d98fcd8625020c15
Kristófer <kristofer@apro.is>
2025-07-16 11:20:14 +0000

    Copy apps changes

--- apps/unicorn-app/playwright.config.ts
+++ apps/unicorn-app/playwright.config.ts
@@ -6,5 +6,5 @@ import { baseConfig } from '@island.is/testing/e2e'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...baseConfig({ project: 'unicorn-app' }),
+  ...baseConfig({ filename: __filename, project: 'unicorn-app' }),
 })
