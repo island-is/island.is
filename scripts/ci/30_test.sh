f7e730127cee8241d95a5a8c042a687ad610ed14
lommi <jonorn@gmail.com>
2025-07-21 12:10:29 +0000

    chore: remove code coverage (#19629)
    * chore: remove code coverage
    
    * chore: formatting

--- scripts/ci/30_test.sh
+++ scripts/ci/30_test.sh
@@ -11,15 +11,6 @@ set -euxo pipefail
 
 # Default to big old-space, and more options for testing, but allow overriding
 NODE_OPTIONS="--max-old-space-size=8193 --unhandled-rejections=warn --trace-warnings --require=dd-trace/ci/init ${NODE_OPTIONS:-}"
-EXTRA_OPTS=""
-
-projects_uncollectible_coverage=(
-  "application-templates-no-debt-certificate"
-  "api-domains-email-signup"
-  "skilavottord-web"
-  "shared-babel"
-  "portals-my-pages-core"
-)
 
 # Array of services to skip during testing
 services_to_skip=(
@@ -33,24 +24,6 @@ export DD_CIVISIBILITY_AGENTLESS_ENABLED \
   NODE_OPTIONS \
   SERVERSIDE_FEATURES_ON=\"\" # disable server-side features
 unset DD_SERVICE
-
-# Determine if any project requires code coverage
-requires_code_coverage() {
-  IFS=',' read -ra PROJECTS <<<"$AFFECTED_PROJECTS"
-  for project in "${PROJECTS[@]}"; do
-    if [[ ! " ${projects_uncollectible_coverage[*]} " =~ \ ${project}\  ]]; then
-      return 0
-    fi
-  done
-  return 1
-}
-
-# Set code coverage if required
-if requires_code_coverage; then
-  EXTRA_OPTS="--codeCoverage"
-fi
-
-echo $EXTRA_OPTS
 
 # Set Datadog config per-project
 jq -n --arg p "${AFFECTED_PROJECTS}" '$p | split(",") | .[]' | xargs -I% yarn nx show project --json % | jq -re '"echo DD_SERVICE=\(.name) >> \(.root)/.env"' | xargs -I% sh -c '%'
