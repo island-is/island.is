600272640c73d91075b963305b5d149b3cb7baf4
Rúnar Vestmann <runar@stefna.is>
2025-07-21 15:17:02 +0000

    Create connected components

---
+++ apps/web/components/connected/landspitali/MemorialCardsAndGrants/translation.strings.ts
@@ -0,0 +1,357 @@
+import { defineMessages } from 'react-intl'
+
+export const m = {
+  memorialCardSection: {
+    info: defineMessages({
+      fundLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.fundLabel',
+        defaultMessage: 'Veldu nafn minningarsjóðs úr listanum',
+        description: 'Veldu nafn minningarsjóðs úr listanum',
+      },
+      inMemoryOfLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.inMemoryOfLabel',
+        defaultMessage: 'Til minningar um',
+        description: 'Til minningar um',
+      },
+      amountOfMoneyTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountOfMoneyTitle',
+        defaultMessage: 'Veldu fjárhæð',
+        description: 'Veldu fjárhæð',
+      },
+      amountOfMoneyExtraInfo: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountOfMoneyExtraInfo',
+        defaultMessage: 'Ath. lágmark 1.000 kr.',
+        description: 'Ath. lágmark 1.000 kr.',
+      },
+      amountOfMoneyOtherRadioLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountOfMoneyOtherRadioLabel',
+        defaultMessage: 'Önnur fjárhæð',
+        description: 'Önnur fjárhæð',
+      },
+      amountOfMoneyOtherInputLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountOfMoneyOtherInputLabel',
+        defaultMessage: 'Skrá aðra upphæð',
+        description: 'Skrá aðra upphæð',
+      },
+      senderSignatureLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderSignatureLabel',
+        defaultMessage:
+          'Undirskrift sendanda (Á korti stendur fyrir ofan undirskrift: "Með innilegri samúðarkveðju").',
+        description:
+          'Undirskrift sendanda (Á korti stendur fyrir ofan undirskrift: "Með innilegri samúðarkveðju").',
+      },
+      recipientLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.recipientLabel',
+        defaultMessage: 'Upplýsingar um viðtakanda minningarkorts',
+        description: 'Upplýsingar um viðtakanda minningarkorts',
+      },
+      recipientNameLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.recipientNameLabel',
+        defaultMessage: 'Nafn viðtakanda korts',
+        description: 'Nafn viðtakanda korts',
+      },
+      recipientAddressLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.recipientAddressLabel',
+        defaultMessage: 'Heimilisfang',
+        description: 'Heimilisfang',
+      },
+      recipientPostalCodeLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.recipientPostalCodeLabel',
+        defaultMessage: 'Póstnúmer',
+        description: 'Póstnúmer',
+      },
+      recipientPlaceLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.recipientPlaceLabel',
+        defaultMessage: 'Staður',
+        description: 'Staður',
+      },
+      senderTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderTitle',
+        defaultMessage: 'Upplýsingar um greiðanda',
+        description: 'Upplýsingar um greiðanda',
+      },
+      senderNameLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderNameLabel',
+        defaultMessage: 'Nafn',
+        description: 'Nafn',
+      },
+      senderNationalIdLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderNationalIdLabel',
+        defaultMessage: 'Kennitala',
+        description: 'Kennitala',
+      },
+      senderAddressLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderAddressLabel',
+        defaultMessage: 'Heimilisfang',
+        description: 'Heimilisfang',
+      },
+      senderPostalCodeLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderPostalCodeLabel',
+        defaultMessage: 'Póstnúmer',
+        description: 'Póstnúmer',
+      },
+      senderPlaceLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.senderPlaceLabel',
+        defaultMessage: 'Staður',
+        description: 'Staður',
+      },
+      amountISKPrefix: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountISKPrefix',
+        defaultMessage: 'Fjárhæð:',
+        description: 'Fjárhæð',
+      },
+      amountISKCurrency: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountISKCurrency',
+        defaultMessage: 'kr.',
+        description: 'gjaldmiðill',
+      },
+      amountISKExtra: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.amountISKExtra',
+        defaultMessage: '500 króna kostnaður bætist við',
+        description: 'Texti fyrir auka kostnað',
+      },
+      continue: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.info.continue',
+        defaultMessage: 'Áfram',
+        description: 'Halda áfram takki',
+      },
+    }),
+    overview: defineMessages({
+      title: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.title',
+        defaultMessage: 'Yfirlit',
+        description: 'Titill á yfirlitssíðu',
+      },
+      senderTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderTitle',
+        defaultMessage: 'Greiðandi',
+        description: 'Titill á upplýsingar um greiðanda',
+      },
+      senderName: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderName',
+        defaultMessage: 'Nafn:',
+        description: 'Nafn',
+      },
+      senderNationalId: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderNationalId',
+        defaultMessage: 'Kennitala:',
+        description: 'Kennitala',
+      },
+      senderAddress: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderAddress',
+        defaultMessage: 'Heimilisfang:',
+        description: 'Heimilisfang',
+      },
+      senderPostalCode: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderPostalCode',
+        defaultMessage: 'Póstnúmer:',
+        description: 'Póstnúmer',
+      },
+      recipientTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.recipientTitle',
+        defaultMessage: 'Viðtakandi',
+        description: 'Titill á upplýsingar um viðtakanda',
+      },
+      recipientName: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.recipientName',
+        defaultMessage: 'Nafn:',
+        description: 'Nafn',
+      },
+      recipientAddress: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.recipientAddress',
+        defaultMessage: 'Heimilisfang:',
+        description: 'Heimilisfang',
+      },
+      recipientPostalCode: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.recipientPostalCode',
+        defaultMessage: 'Póstnúmer:',
+        description: 'Póstnúmer',
+      },
+      inMemoryOf: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.inMemoryOf',
+        defaultMessage: 'Til minningar um:',
+        description: 'Til minningar um',
+      },
+      senderSignatureTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderSignatureTitle',
+        defaultMessage: 'Sendandi',
+        description: 'Titill fyrir undirskrift á korti',
+      },
+      senderSignature: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.senderSignature',
+        defaultMessage: 'Undirskrift á korti:',
+        description: 'Undirskrift á korti',
+      },
+      memorialCardTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.memorialCardTitle',
+        defaultMessage: 'Minningarkort',
+        description: 'Minningarkort',
+      },
+      amountISK: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.amountISK',
+        defaultMessage: 'Upphæð:',
+        description: 'Upphæð',
+      },
+      fund: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.fund',
+        defaultMessage: 'Sjóður:',
+        description: 'Sjóður',
+      },
+      goBack: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.goBack',
+        defaultMessage: 'Breyta upplýsingum',
+        description: 'Breyta upplýsingum',
+      },
+      pay: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.overview.pay',
+        defaultMessage: 'Greiða',
+        description: 'Greiða',
+      },
+    }),
+    validation: defineMessages({
+      invalidNationalId: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.validation.invalidNationalId',
+        defaultMessage: 'Kennitala verður að vera 10 tölustafir',
+        description: 'Kennitala verður að vera 10 tölustafir',
+      },
+      required: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.validation.required',
+        defaultMessage: 'Það þarf að fylla út þennan reit',
+        description: 'Villuskilaboð á óútfylltan reit',
+      },
+      minimumAmount: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.validation.minimumAmount',
+        defaultMessage: 'Lágmark er 1.000 kr.',
+        description: 'Villuskilaboð fyrir lágmarksupphæð',
+      },
+      requiredAmount: {
+        id: 'web.landspitali.memorialCardsAndGrants:memorialCardSection.validation.requiredAmount',
+        defaultMessage: 'Veldu upphæð eða sláðu inn aðra upphæð.',
+        description: 'Villuskilaboð þegar ekkert er valið í fjárhæð',
+      },
+    }),
+  },
+  grantSection: {
+    info: defineMessages({
+      grantLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.grantLabel',
+        defaultMessage: 'Veldu nafn styrktarsjóðs úr listanum',
+        description: 'Veldu nafn styrktarsjóðs úr listanum',
+      },
+      projectTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.projectTitle',
+        defaultMessage: 'Veldu verkefni',
+        description: 'Veldu verkefni',
+      },
+      amountOfMoneyTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.amountOfMoneyTitle',
+        defaultMessage: 'Veldu fjárhæð',
+        description: 'Veldu fjárhæð',
+      },
+      amountOfMoneyOtherRadioLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.amountOfMoneyOtherRadioLabel',
+        defaultMessage: 'Önnur fjárhæð',
+        description: 'Önnur fjárhæð',
+      },
+      amountOfMoneyOtherInputLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.amountOfMoneyOtherInputLabel',
+        defaultMessage: 'Skrá aðra upphæð',
+        description: 'Skrá aðra upphæð',
+      },
+      senderTitle: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderTitle',
+        defaultMessage: 'Upplýsingar um greiðanda',
+        description: 'Upplýsingar um greiðanda',
+      },
+      senderNameLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderNameLabel',
+        defaultMessage: 'Nafn',
+        description: 'Nafn',
+      },
+      senderNationalIdLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderNationalIdLabel',
+        defaultMessage: 'Kennitala',
+        description: 'Kennitala',
+      },
+      senderAddressLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderAddressLabel',
+        defaultMessage: 'Heimilisfang',
+        description: 'Heimilisfang',
+      },
+      senderPostalCodeLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderPostalCodeLabel',
+        defaultMessage: 'Póstnúmer',
+        description: 'Póstnúmer',
+      },
+      senderPlaceLabel: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderPlaceLabel',
+        defaultMessage: 'Staður',
+        description: 'Staður',
+      },
+      senderGrantExplanation: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.senderGrantExplanation',
+        defaultMessage: 'Skýring á styrk',
+        description: 'Skýring á styrk',
+      },
+      amountISKPrefix: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.amountISKPrefix',
+        defaultMessage: 'Fjárhæð:',
+        description: 'Fjárhæð',
+      },
+      amountISKCurrency: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.amountISKCurrency',
+        defaultMessage: 'kr.',
+        description: 'gjaldmiðill',
+      },
+      amountISKExtra: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.amountISKExtra',
+        defaultMessage: '500 króna kostnaður bætist við',
+        description: 'Texti fyrir auka kostnað',
+      },
+      pay: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.info.pay',
+        defaultMessage: 'Greiða',
+        description: 'Halda áfram í greiðslu takki',
+      },
+    }),
+    validation: defineMessages({
+      invalidNationalId: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.validation.invalidNationalId',
+        defaultMessage: 'Kennitala verður að vera 10 tölustafir',
+        description: 'Kennitala verður að vera 10 tölustafir',
+      },
+      required: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.validation.required',
+        defaultMessage: 'Það þarf að fylla út þennan reit',
+        description: 'Villuskilaboð á óútfylltan reit',
+      },
+      minimumAmount: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.validation.minimumAmount',
+        defaultMessage: 'Lágmark er 1.000 kr.',
+        description: 'Villuskilaboð fyrir lágmarksupphæð',
+      },
+      requiredAmount: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.validation.requiredAmount',
+        defaultMessage: 'Veldu upphæð eða sláðu inn aðra upphæð.',
+        description: 'Villuskilaboð þegar ekkert er valið í fjárhæð',
+      },
+      requiredProject: {
+        id: 'web.landspitali.memorialCardsAndGrants:grantSection.validation.requiredProject',
+        defaultMessage: 'Vinsamlegast veldu verkefni',
+        description: 'Villuskilaboð þegar ekkert er valið í fjárhæð',
+      },
+    }),
+  },
+  shared: defineMessages({
+    memorialCard: {
+      id: 'web.landspitali.memorialCardsAndGrants:shared.memorialCard',
+      defaultMessage: 'Minningarkort',
+      description: 'Minningarkort',
+    },
+    grant: {
+      id: 'web.landspitali.memorialCardsAndGrants:shared.grant',
+      defaultMessage: 'Beinn styrkur',
+      description: 'Beinn styrkur',
+    },
+  }),
+}
