e37f5284919142e3f593a989e97e9373f18202bb
Eirikur Nilsson <eirikur@nilsson.is>
2025-07-25 18:17:03 +0000

    wip2
    TODO: Refactor auth.interceptor.ts to infra project?

--- libs/auth-nest-tools/src/lib/auth.interceptor.ts
+++ libs/auth-nest-tools/src/lib/auth.interceptor.ts
@@ -1,19 +1,42 @@
-import { withLoggingContext } from '@island.is/logging'
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common'
+import { Logger, LOGGER_PROVIDER, withLoggingContext } from '@island.is/logging'
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  NestInterceptor,
+} from '@nestjs/common'
+import { tap } from 'rxjs'
+import { AuthRequest } from './auth.request'
 import { getRequest } from './getRequest'
 
 export class AuthInterceptor implements NestInterceptor {
+  private readonly logger: Logger
+
+  constructor(@Inject(LOGGER_PROVIDER) logger: Logger) {
+    this.logger = logger.child({ context: 'AuthInterceptor' })
+  }
+
   async intercept(executionContext: ExecutionContext, next: CallHandler) {
     const req = getRequest(executionContext)
     const authSid = req.auth?.sid
+    if (!authSid) {
+      return next.handle()
+    }
+
+    const handlerClass = executionContext.getClass()
+    const handlerFn = executionContext.getHandler()
+    const handlerName = `${handlerClass.name}#${handlerFn.name}`
+    this.logger.info(`Authenticated Request: ${req.originalUrl}`, {
+      authSid,
+      req: {
+        path: req.originalUrl,
+        userAgent: req.headers['user-agent'],
+      },
+      handlerName,
+    })
 
     if (authSid) {
-      return withLoggingContext(
-        {
-          authSid,
-        },
-        () => next.handle(),
-      )
+      return withLoggingContext({ authSid }, () => next.handle())
     } else {
       return next.handle()
     }
--- libs/infra-nest-server/src/lib/infra/apm.interceptor.ts
+++ libs/infra-nest-server/src/lib/infra/apm.interceptor.ts
@@ -1,3 +1,4 @@
+import { getRequest } from '@island.is/auth-nest-tools'
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common'
 import { from, lastValueFrom, Observable } from 'rxjs'
 import tracer from 'dd-trace'
@@ -9,12 +10,16 @@ export class ApmInterceptor implements NestInterceptor {
   ): Promise<Observable<any>> {
     const handlerClass = executionContext.getClass()
     const handlerFn = executionContext.getHandler()
+    const request = getRequest(executionContext)
 
     return from(
       tracer.trace(
         'nestjs.handler',
         {
           resource: `${handlerClass.name}#${handlerFn.name}`,
+          tags: {
+            authSid: request.auth?.sid,
+          },
         },
         () => lastValueFrom(next.handle()),
       ),
