2a477c42e8d44e6ea3c44106b8d3e06b0d0a0fd2
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2025-07-28 12:08:39 +0000

    fix: fix some assets and add svg support in esbuild config

--- tools/esbuild.config.js
+++ tools/esbuild.config.js
@@ -1,10 +1,18 @@
 // TODO: Where should this file live?
 const { esbuildDecorators } = require('@anatine/esbuild-decorators')
+const svgrPlugin = require('esbuild-plugin-svgr')
 
 module.exports = {
   plugins: [
     // Add eslint plugin for basic emitDecoratorMetadata support.
     // This is required by many features in NestJS.
-    esbuildDecorators()
+    esbuildDecorators(),
+    // Add SVG plugin for React components
+    svgrPlugin({
+      // Configure for both static and dynamic imports
+      typescript: true,
+      // Handle both default exports and named exports
+      exportType: 'default'
+    })
   ]
 }
