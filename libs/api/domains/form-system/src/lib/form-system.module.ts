097ec13429a9a3dd08fd8f24bef158da7dcae20f
GrimurValur <grimur.valur.gunnarsson@advania.is>
2025-07-28 10:06:12 +0000

    conflict

--- libs/api/domains/form-system/src/lib/form-system.module.ts
+++ libs/api/domains/form-system/src/lib/form-system.module.ts
@@ -26,13 +26,10 @@ import { TranslationsService } from './translations/translations.service'
 import { TranslationsResolver } from './translations/translations.resolver'
 import { OrganizationUrlsResolver } from './organizationUrls/organizationUrls.resolver'
 import { OrganizationUrlsService } from './organizationUrls/organizationUrls.service'
-<<<<<<< HEAD
 import { NationalRegistryResolver } from './nationalRegistry/nationalRegistry.resolver'
 import { NationalRegistryV3ClientModule } from '@island.is/clients/national-registry-v3'
-=======
 import { FormUrlsResolver } from './formUrls/formUrls.resolver'
 import { FormUrlsService } from './formUrls/formUrls.service'
->>>>>>> b258c0282119f66c4c79e13cae0305add50dc8b5
 
 @Module({
   providers: [
