e411310acc84a42420a58410d89647b53c55ab8a
Rúnar Vestmann <runar@stefna.is>
2025-07-29 14:22:07 +0000

    feat(contentful-apps): Hotfix, Sitemap contentful app, visual changes (#19668) (#19677)
    feat(contentful-apps): Sitemap contentful app, visual changes (#19668)
    
    * Show node type
    
    * Allow for organization subpage entries
    
    * Add english fields
    
    * Add slug validation
    
    * Remove object only used in one place
    
    * Move divs around
    
    * Change default on submit

--- apps/contentful-apps/components/sitemap/SitemapTreeFieldDialog.tsx
+++ apps/contentful-apps/components/sitemap/SitemapTreeFieldDialog.tsx
@@ -1,22 +1,31 @@
 import { useState } from 'react'
 import { DialogExtensionSDK } from '@contentful/app-sdk'
 import {
+  Box,
   Button,
   FormControl,
   IconButton,
+  Select,
+  Text,
   Textarea,
   TextInput,
 } from '@contentful/f36-components'
 import { CloseIcon } from '@contentful/f36-icons'
 import { useSDK } from '@contentful/react-apps-toolkit'
 
+import { SitemapUrlType } from '@island.is/shared/types'
+
+import { slugify } from '../../utils'
 import { TreeNode, TreeNodeType } from './utils'
 import * as styles from './SitemapTreeFieldDialog.css'
 
 interface CategoryState {
   label: string
+  labelEN?: string
   slug: string
+  slugEN?: string
   description: string
+  descriptionEN?: string
 }
 
 interface FormProps<State> {
@@ -24,12 +33,18 @@ interface FormProps<State> {
   onSubmit: (props: State) => void
 }
 
-const CategoryForm = ({ initialState, onSubmit }: FormProps<CategoryState>) => {
+const CategoryForm = ({
+  initialState,
+  onSubmit,
+  formError,
+}: FormProps<CategoryState> & {
+  formError: { slug: string; slugEN: string }
+}) => {
   const [state, setState] = useState<CategoryState>(initialState)
   return (
     <FormControl className={styles.formContainer}>
       <div>
-        <FormControl.Label>Label</FormControl.Label>
+        <FormControl.Label>Label (Icelandic)</FormControl.Label>
         <TextInput
           value={state.label}
           onChange={(ev) => {
@@ -38,16 +53,51 @@ const CategoryForm = ({ initialState, onSubmit }: FormProps<CategoryState>) => {
         />
       </div>
       <div>
-        <FormControl.Label>Slug</FormControl.Label>
+        <FormControl.Label>Label (English)</FormControl.Label>
+        <div>
+          <TextInput
+            value={state.labelEN}
+            onChange={(ev) => {
+              setState((prevState) => ({
+                ...prevState,
+                labelEN: ev.target.value,
+              }))
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        <FormControl.Label>Slug (Icelandic)</FormControl.Label>
         <TextInput
+          placeholder={slugify(state.label)}
           value={state.slug}
           onChange={(ev) => {
             setState((prevState) => ({ ...prevState, slug: ev.target.value }))
           }}
         />
+        {formError.slug && <Text fontColor="red600">{formError.slug}</Text>}
       </div>
       <div>
-        <FormControl.Label>Description</FormControl.Label>
+        <FormControl.Label>Slug (English)</FormControl.Label>
+
+        <div>
+          <TextInput
+            placeholder={slugify(state.labelEN)}
+            value={state.slugEN}
+            onChange={(ev) => {
+              setState((prevState) => ({
+                ...prevState,
+                slugEN: ev.target.value,
+              }))
+            }}
+          />
+          {formError.slugEN && (
+            <Text fontColor="red600">{formError.slugEN}</Text>
+          )}
+        </div>
+      </div>
+      <div>
+        <FormControl.Label>Description (Icelandic)</FormControl.Label>
         <Textarea
           value={state.description}
           onChange={(ev) => {
@@ -58,11 +108,31 @@ const CategoryForm = ({ initialState, onSubmit }: FormProps<CategoryState>) => {
           }}
         />
       </div>
-
+      <div>
+        <FormControl.Label>Description (English)</FormControl.Label>
+        <div>
+          <Textarea
+            value={state.descriptionEN}
+            onChange={(ev) => {
+              setState((prevState) => ({
+                ...prevState,
+                descriptionEN: ev.target.value,
+              }))
+            }}
+          />
+        </div>
+      </div>
       <Button
         variant="primary"
         onClick={() => {
-          onSubmit(state)
+          const stateToSubmit = { ...state }
+          if (!stateToSubmit.slug) {
+            stateToSubmit.slug = slugify(stateToSubmit.label)
+          }
+          if (!stateToSubmit.slugEN) {
+            stateToSubmit.slugEN = slugify(stateToSubmit.labelEN)
+          }
+          onSubmit(stateToSubmit)
         }}
       >
         Save changes
@@ -73,36 +143,109 @@ const CategoryForm = ({ initialState, onSubmit }: FormProps<CategoryState>) => {
 
 interface UrlState {
   label: string
+  labelEN?: string
   url: string
+  urlEN?: string
+  urlType?: SitemapUrlType
 }
 
 const UrlForm = ({ initialState, onSubmit }: FormProps<UrlState>) => {
   const [state, setState] = useState<UrlState>(initialState)
+
   return (
     <FormControl className={styles.formContainer}>
       <div>
-        <FormControl.Label>Label</FormControl.Label>
-        <TextInput
-          value={state.label}
-          onChange={(ev) => {
-            setState((prevState) => ({ ...prevState, label: ev.target.value }))
-          }}
-        />
+        <Box paddingBottom="spacingXs">
+          <FormControl.Label>Label (Icelandic)</FormControl.Label>
+          <TextInput
+            value={state.label}
+            onChange={(ev) => {
+              setState((prevState) => ({
+                ...prevState,
+                label: ev.target.value,
+              }))
+            }}
+          />
+        </Box>
+        <div>
+          <FormControl.Label>Label (English)</FormControl.Label>
+          <TextInput
+            value={state.labelEN}
+            onChange={(ev) => {
+              setState((prevState) => ({
+                ...prevState,
+                labelEN: ev.target.value,
+              }))
+            }}
+          />
+        </div>
       </div>
       <div>
-        <FormControl.Label>URL</FormControl.Label>
-        <TextInput
-          value={state.url}
+        <FormControl.Label>Type</FormControl.Label>
+        <Select
+          value={state.urlType}
           onChange={(ev) => {
-            setState((prevState) => ({ ...prevState, url: ev.target.value }))
+            setState((prevState) => ({
+              ...prevState,
+              urlType: ev.target.value as typeof state.urlType,
+            }))
           }}
-        />
+        >
+          <Select.Option value="organizationFrontpage">
+            Organization frontpage
+          </Select.Option>
+          <Select.Option value="organizationNewsOverview">
+            Organization news overview
+          </Select.Option>
+          <Select.Option value="organizationPublishedMaterial">
+            Organization published material
+          </Select.Option>
+          <Select.Option value="organizationEventOverview">
+            Organization event overview
+          </Select.Option>
+          <Select.Option value="custom">Custom</Select.Option>
+        </Select>
       </div>
+
+      {state.urlType === 'custom' && (
+        <div>
+          <Box paddingBottom="spacingXs">
+            <div>
+              <FormControl.Label>URL (Icelandic)</FormControl.Label>
+              <TextInput
+                value={state.url}
+                onChange={(ev) => {
+                  setState((prevState) => ({
+                    ...prevState,
+                    url: ev.target.value,
+                  }))
+                }}
+              />
+            </div>
+          </Box>
+
+          <div>
+            <FormControl.Label>URL (English)</FormControl.Label>
+            <TextInput
+              value={state.urlEN}
+              onChange={(ev) => {
+                setState((prevState) => ({
+                  ...prevState,
+                  urlEN: ev.target.value,
+                }))
+              }}
+            />
+          </div>
+        </div>
+      )}
 
       <Button
         variant="primary"
         onClick={() => {
-          onSubmit(state)
+          onSubmit({
+            ...state,
+            urlType: state.urlType || 'organizationFrontpage',
+          })
         }}
       >
         Save changes
@@ -114,7 +257,17 @@ const UrlForm = ({ initialState, onSubmit }: FormProps<UrlState>) => {
 export const SitemapTreeFieldDialog = () => {
   const sdk = useSDK<DialogExtensionSDK>()
 
-  const node = (sdk.parameters.invocation as { node: TreeNode }).node
+  const { node, otherCategorySlugs, otherCategorySlugsEN } = sdk.parameters
+    .invocation as {
+    node: TreeNode
+    otherCategorySlugs?: string[]
+    otherCategorySlugsEN?: string[]
+  }
+
+  const [categoryFormError, setCategoryFormError] = useState({
+    slug: '',
+    slugEN: '',
+  })
 
   return (
     <div className={styles.container}>
@@ -128,7 +281,26 @@ export const SitemapTreeFieldDialog = () => {
         />
       </div>
       {node.type === TreeNodeType.CATEGORY && (
-        <CategoryForm initialState={node} onSubmit={sdk.close} />
+        <CategoryForm
+          formError={categoryFormError}
+          initialState={node}
+          onSubmit={(state) => {
+            const error = { slug: '', slugEN: '' }
+            if (otherCategorySlugs?.includes(state.slug)) {
+              error.slug = 'Slug already exists'
+            }
+            if (otherCategorySlugsEN?.includes(state.slugEN)) {
+              error.slugEN = 'Slug already exists'
+            }
+
+            if (error.slug || error.slugEN) {
+              setCategoryFormError(error)
+              return
+            }
+
+            sdk.close(state)
+          }}
+        />
       )}
       {node.type === TreeNodeType.URL && (
         <UrlForm initialState={node} onSubmit={sdk.close} />
