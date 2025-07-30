46c2a7b6453a0ccf14b9e9e504e770b7a0f62c7d
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-07-30 13:59:40 +0000

    Sort LÖKE numbers fetched from LÖKE

--- apps/judicial-system/web/src/routes/Prosecutor/Indictments/Defendant/Defendant.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/Indictments/Defendant/Defendant.tsx
@@ -125,7 +125,7 @@ const Defendant = () => {
     fetchPolicy: 'no-cache',
     errorPolicy: 'all',
     onCompleted: (data) => {
-      if (!data.policeCaseInfo) {
+      if (!data.policeCaseInfo || data.policeCaseInfo.length === 0) {
         return
       }
 
