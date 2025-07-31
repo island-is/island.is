2de6def74b5457c8e8a6ebbbdeb8b042043fc54b
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-07-31 17:14:02 +0000

    feat(j-s): Court Date Notification Info (#19679)
    * Modifies court arrangement comments
    
    * Extracts a shared component for arraignment alert
    
    * Removes unnecessary space

--- apps/judicial-system/web/src/routes/Court/InvestigationCase/HearingArrangements/HearingArrangements.tsx
+++ apps/judicial-system/web/src/routes/Court/InvestigationCase/HearingArrangements/HearingArrangements.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from 'react'
+import { useCallback, useContext, useMemo, useState } from 'react'
 import { useIntl } from 'react-intl'
 import router from 'next/router'
 
@@ -13,6 +13,7 @@ import * as constants from '@island.is/judicial-system/consts'
 import { isDistrictCourtUser } from '@island.is/judicial-system/types'
 import { titles } from '@island.is/judicial-system-web/messages'
 import {
+  ArraignmentAlert,
   BlueBox,
   CourtArrangements,
   CourtCaseInfo,
@@ -91,6 +92,15 @@ const HearingArrangements = () => {
 
   useOnceOn(isCaseUpToDate, initialize)
 
+  const courtDateNotification = useMemo(
+    () =>
+      hasSentNotification(
+        NotificationType.COURT_DATE,
+        workingCase.notifications,
+      ),
+    [workingCase.notifications],
+  )
+
   const isCorrectingRuling = Boolean(workingCase.requestCompletedDate)
 
   const handleNavigationTo = async (destination: keyof stepValidationsType) => {
@@ -98,11 +108,7 @@ const HearingArrangements = () => {
 
     if (
       isCorrectingRuling ||
-      (hasSentNotification(
-        NotificationType.COURT_DATE,
-        workingCase.notifications,
-      ).hasSent &&
-        !courtDateHasChanged)
+      (courtDateNotification.hasSent && !courtDateHasChanged)
     ) {
       router.push(`${destination}/${workingCase.id}`)
     } else {
@@ -141,15 +147,7 @@ const HearingArrangements = () => {
                 />
               </Box>
             )}
-          {workingCase.comments && (
-            <Box marginBottom={5}>
-              <AlertMessage
-                type="warning"
-                title={formatMessage(m.comments.title)}
-                message={workingCase.comments}
-              />
-            </Box>
-          )}
+          <ArraignmentAlert />
           <PageTitle>{formatMessage(m.title)}</PageTitle>
           <CourtCaseInfo workingCase={workingCase} />
           <Box component="section" marginBottom={8}>
