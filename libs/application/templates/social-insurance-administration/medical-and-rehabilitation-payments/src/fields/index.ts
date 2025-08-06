892391d5a1f49e8498156767d59f6f773ea977ef
Veronika Sif <veronikasif@hotmail.com>
2025-08-06 15:19:02 +0000

    Format

--- libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/fields/index.ts
+++ libs/application/templates/social-insurance-administration/medical-and-rehabilitation-payments/src/fields/index.ts
@@ -3,4 +3,3 @@ export { ConfirmationOfIllHealth } from './ConfirmationOfIllHealth'
 export { ConfirmationOfPendingResolution } from './ConfirmationOfPendingResolution'
 export { ConfirmedTreatment } from './ConfirmedTreatment'
 export { RehabilitationPlan } from './RehabilitationPlan'
-
