5d184a234b52d2f4e8115498c06c406ca16a2ff4
Gunnar K Vilbergsson <gunnar@norda.is>
2025-08-07 14:13:42 +0000

    lintfix

--- libs/application/templates/hms/fire-compensation-appraisal/src/lib/template.ts
+++ libs/application/templates/hms/fire-compensation-appraisal/src/lib/template.ts
@@ -29,7 +29,7 @@ import { getChargeItems } from '../utils/paymentUtils'
 import { Features } from '@island.is/feature-flags'
 import { ApiScope, HmsScope } from '@island.is/auth/scopes'
 import { AuthDelegationType } from '@island.is/shared/types'
-import { PaymentForm } from '@island.is/application/ui-forms'
+// import { PaymentForm } from '@island.is/application/ui-forms'
 
 const template: ApplicationTemplate<
   ApplicationContext,
