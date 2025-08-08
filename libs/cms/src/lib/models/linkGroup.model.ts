5fa6959bbb8e60d6e8ab9de73ee7f89ab6bc43e4
Rúnar Vestmann <runar@stefna.is>
2025-08-08 14:50:40 +0000

    Make sure org subpage links have parent subpage above if they are a chapter

--- libs/cms/src/lib/models/linkGroup.model.ts
+++ libs/cms/src/lib/models/linkGroup.model.ts
@@ -85,6 +85,12 @@ export const generateOrganizationSubpageLink = (
 ) => {
   const prefix = getOrganizationPageUrlPrefix(subpage.sys.locale)
 
+  const slug =
+    'organizationParentSubpage' in subpage.fields &&
+    subpage.fields.organizationParentSubpage?.fields.slug
+      ? `${subpage.fields.organizationParentSubpage.fields.slug}/${subpage.fields.slug}`
+      : subpage.fields.slug
+
   return mapLink({
     ...subpage,
     sys: {
@@ -98,7 +104,7 @@ export const generateOrganizationSubpageLink = (
     },
     fields: {
       text: subpage.fields.shortTitle || subpage.fields.title,
-      url: `/${prefix}/${subpage.fields.organizationPage.fields.slug}/${subpage.fields.slug}`,
+      url: `/${prefix}/${subpage.fields.organizationPage.fields.slug}/${slug}`,
     },
   })
 }
