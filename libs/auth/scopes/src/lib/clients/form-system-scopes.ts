ef91f0bf3b1a10450448e8ae1a29921cde11715d
GrimurValur <grimur.valur.gunnarsson@advania.is>
2025-08-12 10:26:36 +0000

    fix(form-system): remove unused scope imports and simplify formSystemScopes

--- libs/auth/scopes/src/lib/clients/form-system-scopes.ts
+++ libs/auth/scopes/src/lib/clients/form-system-scopes.ts
@@ -1,11 +1,3 @@
 import { ApplicationScope } from '../application.scope'
-import { UserProfileScope } from '../userProfile.scope'
-import { NationalRegistryScope } from '../nationalRegistry.scope'
 
-export const formSystemScopes = [
-    ApplicationScope.read,
-    ApplicationScope.write,
-    UserProfileScope.read,
-    UserProfileScope.write,
-    NationalRegistryScope.individuals
-]
+export const formSystemScopes = [ApplicationScope.read, ApplicationScope.write,]
