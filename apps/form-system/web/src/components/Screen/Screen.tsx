0fb672986cd351418945816dcd56eb1156f77673
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2025-08-12 10:35:02 +0000

    feat(form-system): Initial submit application button (#19744)
    * fixed radio errors
    
    * feat: Implement update application dependencies mutation and related logic
    
    * feat(application): add submit application functionality and summary display
    
    * refactor: clean up code formatting and improve readability across multiple components

--- apps/form-system/web/src/components/Screen/Screen.tsx
+++ apps/form-system/web/src/components/Screen/Screen.tsx
@@ -1,25 +1,64 @@
-import { Box, GridColumn, Text } from '@island.is/island-ui/core'
+import { Box, Button, GridColumn, Text } from '@island.is/island-ui/core'
 import { Footer } from '../Footer/Footer'
 import { useApplicationContext } from '../../context/ApplicationProvider'
 import { SectionTypes } from '@island.is/form-system/ui'
 import { ExternalData } from './components/ExternalData/ExternalData'
 import { Field } from './components/Field/Field'
 import { useState } from 'react'
-import { useLocale } from '@island.is/localization'
+// import { useLocale } from '@island.is/localization'
 import { Applicants } from './components/Applicants/Applicants'
 import { FormSystemApplicant } from '@island.is/api/schema'
+import { useMutation } from '@apollo/client'
+import { SUBMIT_APPLICATION } from '@island.is/form-system/graphql'
 
 export const Screen = () => {
   const { state } = useApplicationContext()
-  const { lang } = useLocale()
-  const { currentSection, currentScreen } = state
-  const screenTitle = currentScreen
-    ? state.screens?.[currentScreen.index]?.name?.[lang]
-    : state.sections?.[currentSection.index]?.name?.[lang]
+  // const { lang } = useLocale()
+  const { currentSection, currentScreen, showSummary } = state
+  // const screenTitle = currentScreen
+  //   ? state.screens?.[currentScreen.index]?.name?.[lang]
+  //   : state.sections?.[currentSection.index]?.name?.[lang]
   const currentSectionType = state.sections?.[currentSection.index]?.sectionType
   const [externalDataAgreement, setExternalDataAgreement] = useState(
     state.sections?.[0].isCompleted ?? false,
   )
+
+  const [submitApplication] = useMutation(SUBMIT_APPLICATION)
+  const handleSubmit = () => {
+    submitApplication({
+      variables: {
+        input: {
+          id: state.application.id,
+        },
+      },
+    })
+  }
+
+  if (showSummary) {
+    return (
+      <Box
+        component="form"
+        display="flex"
+        flexDirection="column"
+        justifyContent="spaceBetween"
+        height="full"
+      >
+        <GridColumn
+          span={['12/12', '12/12', '10/12', '7/9']}
+          offset={['0', '0', '1/12', '1/9']}
+        >
+          <Text variant="h2" as="h2" marginBottom={1}>
+            Summary
+          </Text>
+          {/* Summary component can be added here */}
+          <Button variant="primary" onClick={handleSubmit}>
+            Senda inn umsókn
+          </Button>
+        </GridColumn>
+        <Footer externalDataAgreement={externalDataAgreement} />
+      </Box>
+    )
+  }
 
   return (
     <Box
@@ -33,11 +72,11 @@ export const Screen = () => {
         span={['12/12', '12/12', '10/12', '7/9']}
         offset={['0', '0', '1/12', '1/9']}
       >
-        <Text variant="h2" as="h2" marginBottom={1}>
+        {/* <Text variant="h2" as="h2" marginBottom={1}>
           {currentSectionType !== SectionTypes.PREMISES &&
             currentSectionType !== SectionTypes.PARTIES &&
             screenTitle}
-        </Text>
+        </Text> */}
         {currentSectionType === SectionTypes.PREMISES && (
           <ExternalData setExternalDataAgreement={setExternalDataAgreement} />
         )}
@@ -50,10 +89,12 @@ export const Screen = () => {
             }
           />
         )}
+
         {currentScreen &&
           currentScreen?.data?.fields
             ?.filter(
-              (field): field is NonNullable<typeof field> => field != null,
+              (field): field is NonNullable<typeof field> =>
+                field != null && !field.isHidden,
             )
             .map((field, index) => {
               return <Field field={field} key={index} />
