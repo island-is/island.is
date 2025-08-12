96789cfffef2d9f3b0f9f82da84a5ee04b990ab0
Ingvar Sigurðsson <ingvar@apro.is>
2025-08-12 09:29:26 +0000

    fix: remove duplicate code

--- infra/src/feature-env.ts
+++ infra/src/feature-env.ts
@@ -164,7 +164,7 @@ yargs(process.argv.slice(2))
         })
     },
     handler: async (argv) => {
-      const typedArgv = argv as unknown as Arguments
+      const typedArgv = (argv as unknown) as Arguments
       const {
         habitat,
         affectedServices,
@@ -222,7 +222,7 @@ yargs(process.argv.slice(2))
       return yargs
     },
     handler: async (argv) => {
-      const typedArgv = argv as unknown as Arguments
+      const typedArgv = (argv as unknown) as Arguments
       const {
         habitat,
         affectedServices,
@@ -265,15 +265,17 @@ yargs(process.argv.slice(2))
     describe: 'get helm values file',
     builder: (yargs) => yargs,
     handler: async (argv) => {
-      const typedArgv = argv as unknown as Arguments
+      const typedArgv = (argv as unknown) as Arguments
       const { habitat, affectedServices, env } = parseArguments(typedArgv)
-      const { included: featureYaml, excluded } =
-        await getFeatureAffectedServices(
-          habitat,
-          affectedServices.slice(),
-          ExcludedFeatureDeploymentServices,
-          env,
-        )
+      const {
+        included: featureYaml,
+        excluded,
+      } = await getFeatureAffectedServices(
+        habitat,
+        affectedServices.slice(),
+        ExcludedFeatureDeploymentServices,
+        env,
+      )
       const ingressComment = buildComment(
         (await renderHelmServices(env, habitat, featureYaml, 'no-mocks'))
           .services,
@@ -303,7 +305,7 @@ yargs(process.argv.slice(2))
         })
     },
     handler: async (argv) => {
-      const typedArgv = argv as unknown as Arguments
+      const typedArgv = (argv as unknown) as Arguments
       const { affectedServices, env, writeDest } = parseArguments(typedArgv)
       const featureYaml = await renderHelmJobForFeature(
         env,
@@ -369,73 +371,7 @@ yargs(process.argv.slice(2))
         })
     },
     handler: async (argv) => {
-      const typedArgv = argv as unknown as Arguments
-      const { affectedServices, env, writeDest } = parseArguments(typedArgv)
-      const featureYaml = await renderCleanUpForFeature(
-        env,
-        typedArgv.cleanupImage!,
-        affectedServices,
-      )
-
-      if (
-        featureYaml.spec.template.spec.containers.length <= 0 ||
-        affectedServices.length <= 0
-      ) {
-        return
-      }
-
-      const svcString = dumpJobYaml(featureYaml)
-
-      if (writeDest != '') {
-        try {
-          fs.mkdirSync(`${writeDest}/${affectedServices[0].name()}`, {
-            recursive: true,
-          })
-          console.log(
-            `writing file to: ${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml}`,
-          )
-          fs.writeFileSync(
-            `${writeDest}/${affectedServices[0].name()}/cleanup-fd-job.yaml`,
-            svcString,
-          )
-        } catch (error) {
-          console.log(
-            `Failed to write values file for ${affectedServices[0].name()}:`,
-            error,
-          )
-          throw new Error(
-            `Failed to write values for ${affectedServices[0].name()}`,
-          )
-        }
-      } else {
-        await writeToOutput(svcString, typedArgv.output)
-      }
-    },
-  })
-  .command({
-    command: 'cleanup',
-    describe: 'get kubernetes jobs to cleanup feature environment',
-    builder: (yargs) => {
-      return yargs
-        .option('cleanupImage', {
-          type: 'string',
-          description: 'Image to run feature cleanup jobs',
-          demandOption: true,
-        })
-        .option('writeDest', {
-          type: 'string',
-          description:
-            'Template location where to write files to for down stream apps',
-          default: '',
-        })
-        .option('containerCommand', {
-          type: 'string',
-          description: 'Command to run in the container',
-          default: 'cleanup',
-        })
-    },
-    handler: async (argv) => {
-      const typedArgv = argv as unknown as Arguments
+      const typedArgv = (argv as unknown) as Arguments
       const { affectedServices, env, writeDest } = parseArguments(typedArgv)
       const featureYaml = await renderCleanUpForFeature(
         env,
