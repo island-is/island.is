cbb70d7d72e464ccf0bef9750866c218f874a4c6
Ívar Oddsson <ivaroddsson@gmail.com>
2025-08-12 09:05:25 +0000

    fix(j-s): Make cases cards a bit smaller (#19745)
    Make boxes a little smaller

--- apps/judicial-system/web/src/components/Layouts/CasesDashboardLayout/index.css.ts
+++ apps/judicial-system/web/src/components/Layouts/CasesDashboardLayout/index.css.ts
@@ -10,10 +10,9 @@ export const gridContainer = style({
   '@media': {
     [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
       gridTemplateColumns: 'repeat(3, 1fr)',
-
+      gridAutoRows: 'minmax(12rem, auto)',
       columnGap: theme.spacing[2],
       rowGap: theme.spacing[2],
-      gridAutoRows: '240px',
     },
     [`screen and (min-width: ${theme.breakpoints.lg}px)`]: {
       gridTemplateColumns: 'repeat(4, 1fr)',
