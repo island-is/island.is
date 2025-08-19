215904b7799f25b134916ae9002e02a7da90acec
Hyosam <hyo-sam.nandkisore@advania.is>
2025-08-19 16:49:01 +0000

    refactor(Applications): reorganize imports for better structure

--- apps/form-system/web/src/routes/Applications.tsx
+++ apps/form-system/web/src/routes/Applications.tsx
@@ -14,9 +14,10 @@ import {
   Text,
   Button,
 } from '@island.is/island-ui/core'
-import { ApplicationList, ApplicationLoading } from '@island.is/form-system/ui'
+import { ApplicationList } from '@island.is/form-system/ui'
 import { useIntl } from 'react-intl'
 import { m } from '@island.is/form-system/ui'
+import { ApplicationLoading } from '../components/ApplicationsLoading/ApplicationLoading'
 
 interface Params {
   slug?: string
