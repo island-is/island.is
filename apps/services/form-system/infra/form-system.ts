16ae879ffc1d549b70d40b5c750c80abf0b76379
lommi <jonorn@gmail.com>
2025-08-19 10:17:19 +0000

    fix: add national registry to env and secret (#19798)
    * fix: add national registry to env and secret
    
    * chore: charts update dirty files
    
    * fix: add national registry to env and secret (#19797)
    
    * fix: add national registry to env and secret
    
    * chore: charts update dirty files
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>
    
    * fix: form system
    
    * fix: ok
    
    * chore: charts update dirty files
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>

--- apps/services/form-system/infra/form-system.ts
+++ apps/services/form-system/infra/form-system.ts
@@ -3,8 +3,15 @@ import {
   service,
   ServiceBuilder,
 } from '../../../../infra/src/dsl/dsl'
+import {
+  Base,
+  Client,
+  NationalRegistry,
+  NationalRegistryB2C,
+} from '../../../../infra/src/dsl/xroad'
 
 const serviceName = 'services-form-system-api'
+
 export const serviceSetup = (): ServiceBuilder<typeof serviceName> =>
   service(serviceName)
     .image(serviceName)
@@ -12,6 +19,12 @@ export const serviceSetup = (): ServiceBuilder<typeof serviceName> =>
     .codeOwner(CodeOwners.Advania)
     .db()
     .migrations()
+    .secrets({
+      NATIONAL_REGISTRY_B2C_CLIENT_SECRET:
+        '/k8s/api/NATIONAL_REGISTRY_B2C_CLIENT_SECRET',
+    })
+    .xroad(Base, Client, NationalRegistry, NationalRegistryB2C)
+
     .env({
       IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
