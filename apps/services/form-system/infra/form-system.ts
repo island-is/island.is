bbf3cea6b7be9614c9001b64afebac5ba6c677be
lommi <jonorn@gmail.com>
2025-08-19 09:58:34 +0000

    fix: add registry to infra (#19799)
    * fix: add national registry to env and secret
    
    * chore: charts update dirty files
    
    * fix: form system
    
    * fix: ok
    
    * chore: charts update dirty files
    
    ---------
    
    Co-authored-by: andes-it <builders@andes.is>

--- apps/services/form-system/infra/form-system.ts
+++ apps/services/form-system/infra/form-system.ts
@@ -3,7 +3,13 @@ import {
   service,
   ServiceBuilder,
 } from '../../../../infra/src/dsl/dsl'
-import { NationalRegistryB2C } from '../../../../infra/src/dsl/xroad'
+import {
+  Base,
+  Client,
+  NationalRegistry,
+  NationalRegistryB2C,
+} from '../../../../infra/src/dsl/xroad'
+
 const serviceName = 'services-form-system-api'
 
 export const serviceSetup = (): ServiceBuilder<typeof serviceName> =>
@@ -17,7 +23,8 @@ export const serviceSetup = (): ServiceBuilder<typeof serviceName> =>
       NATIONAL_REGISTRY_B2C_CLIENT_SECRET:
         '/k8s/api/NATIONAL_REGISTRY_B2C_CLIENT_SECRET',
     })
-    .xroad(NationalRegistryB2C)
+    .xroad(Base, Client, NationalRegistry, NationalRegistryB2C)
+
     .env({
       IDENTITY_SERVER_ISSUER_URL: {
         dev: 'https://identity-server.dev01.devland.is',
