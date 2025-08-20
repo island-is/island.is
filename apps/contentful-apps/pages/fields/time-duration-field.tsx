a1ff3d03a7eb5f86020de2ac9e8e4e27f0df7a98
Rúnar Vestmann <runar@stefna.is>
2025-08-20 15:41:45 +0200

    WIP

--- apps/contentful-apps/pages/fields/time-duration-field.tsx
+++ apps/contentful-apps/pages/fields/time-duration-field.tsx
@@ -1,60 +1,141 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import addDays from 'date-fns/addDays'
 import { FieldExtensionSDK } from '@contentful/app-sdk'
-import { FormControl, TextInput } from '@contentful/f36-components'
+import {
+  Checkbox,
+  Datepicker,
+  Flex,
+  FormControl,
+  Text,
+  TextInput,
+} from '@contentful/f36-components'
 import { useSDK } from '@contentful/react-apps-toolkit'
 
 interface TimeDuration {
   startTime?: string
   endTime?: string
+  endDate?: string
 }
 
 const TimeDurationField = () => {
   const sdk = useSDK<FieldExtensionSDK>()
   const [time, setTime] = useState<TimeDuration>(sdk.field.getValue() ?? {})
+  const [startDateIsSameAsEndDate, setStartDateIsSameAsEndDate] = useState(true)
+  const [startDateString, setStartDateString] = useState<string | undefined>(
+    sdk.entry.fields.startDate.getForLocale(sdk.field.locale)?.getValue(),
+  )
 
   useEffect(() => {
-    sdk.window.startAutoResizer()
-  }, [sdk.window])
-
-  const updateTime = (field: keyof TimeDuration, value: string) => {
-    setTime((prevTime) => {
-      const updatedTime = {
-        ...prevTime,
-        [field]: value,
+    if (startDateIsSameAsEndDate) {
+      sdk.window.startAutoResizer()
+      return () => {
+        sdk.window.stopAutoResizer()
       }
-      sdk.field.setValue(updatedTime)
-      return updatedTime
-    })
-  }
+    }
+    sdk.window.stopAutoResizer()
+    sdk.window.updateHeight(420)
+  }, [sdk.window, startDateIsSameAsEndDate])
+
+  const updateTime = useCallback(
+    (field: keyof TimeDuration, value: string) => {
+      setTime((prevTime) => {
+        const updatedTime = {
+          ...prevTime,
+          [field]: value,
+        }
+        sdk.field.setValue(updatedTime)
+        return updatedTime
+      })
+    },
+    [sdk.field],
+  )
+
+  useEffect(() => {
+    return sdk.entry.fields.startDate
+      .getForLocale(sdk.field.locale)
+      .onValueChanged((value) => {
+        setStartDateString(value)
+        updateTime('endDate', undefined)
+      })
+  }, [sdk.entry.fields.startDate, sdk.field.locale, updateTime])
+
+  const startDate = startDateString ? new Date(startDateString) : undefined
+
+  const fromDate = startDate ? addDays(startDate, 1) : undefined
 
   return (
     <FormControl>
-      <TextInput.Group spacing="spacingS">
-        <TextInput
-          type="time"
-          value={time.startTime}
-          style={{ width: '150px' }}
-          size="small"
-          name="start-time"
-          placeholder="Start time"
-          onChange={(ev) => {
-            updateTime('startTime', ev.target.value)
+      <Flex gap="spacingS" flexDirection="column" paddingBottom="spacingS">
+        <Checkbox
+          id="starts-and-ends-on-the-same-day"
+          isChecked={startDateIsSameAsEndDate}
+          onChange={() => {
+            setStartDateIsSameAsEndDate(!startDateIsSameAsEndDate)
           }}
-        ></TextInput>
-        {' - '}
-        <TextInput
-          type="time"
-          value={time.endTime}
-          style={{ width: '150px' }}
-          size="small"
-          name="end-time"
-          placeholder="End time"
-          onChange={(ev) => {
-            updateTime('endTime', ev.target.value)
-          }}
-        ></TextInput>
-      </TextInput.Group>
-      <FormControl.HelpText>Ex. 12:00 - 14:00</FormControl.HelpText>
+        >
+          Starts and ends on the same day
+        </Checkbox>
+      </Flex>
+      {startDateIsSameAsEndDate && (
+        <>
+          <TextInput.Group spacing="spacingS">
+            <TextInput
+              type="time"
+              value={time.startTime}
+              style={{ width: '150px' }}
+              size="small"
+              name="start-time"
+              placeholder="Start time"
+              onChange={(ev) => {
+                updateTime('startTime', ev.target.value)
+              }}
+            />
+            {' - '}
+            <TextInput
+              type="time"
+              value={time.endTime}
+              style={{ width: '150px' }}
+              size="small"
+              name="end-time"
+              placeholder="End time"
+              onChange={(ev) => {
+                updateTime('endTime', ev.target.value)
+              }}
+            />
+          </TextInput.Group>
+          <FormControl.HelpText>Ex. 12:00 - 14:00</FormControl.HelpText>
+        </>
+      )}
+      {!startDateIsSameAsEndDate && startDate && (
+        <div>
+          <FormControl.Label>End date</FormControl.Label>
+          <Datepicker
+            selected={time.endDate ? new Date(time.endDate) : fromDate}
+            fromDate={fromDate}
+            onSelect={(day) => {
+              updateTime('endDate', day.toISOString())
+            }}
+          />
+
+          <TextInput.Group spacing="spacingS">
+            <TextInput
+              type="time"
+              value={time.endTime}
+              style={{ width: '150px' }}
+              size="small"
+              name="end-time"
+              placeholder="End time"
+              onChange={(ev) => {
+                updateTime('endTime', ev.target.value)
+              }}
+            />
+          </TextInput.Group>
+          <FormControl.HelpText>Ex. 12:00 - 14:00</FormControl.HelpText>
+        </div>
+      )}
+      {!startDateIsSameAsEndDate && !startDate && (
+        <Text>Please select a start date</Text>
+      )}
     </FormControl>
   )
 }
