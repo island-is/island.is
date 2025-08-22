062000eec4e6fe4754fa9eb4942cc7fffb2c22b3
Ingvar Sigurðsson <ingvar@apro.is>
2025-08-22 15:04:55 +0000

    feat(argocd-cleanup): use the old way to delete secrets/ssm-parameters

--- infra/scripts/container-scripts/delete-secrets.sh
+++ infra/scripts/container-scripts/delete-secrets.sh
@@ -6,10 +6,4 @@ set -x
 FEATURE_NAME=$1
 echo "Deleting secrets for feature $FEATURE_NAME"
 
-if command -v yarn >/dev/null 2>&1; then
-  # Use the package.json “secrets” script
-  yarn secrets delete -- "/k8s/feature-${FEATURE_NAME}"
-else
-  # Fallback directly to the CLI entrypoint
-  node -r esbuild-register infra/src/secrets.ts delete -- "/k8s/feature-${FEATURE_NAME}"
-fi
+node secrets delete "/k8s/feature-$FEATURE_NAME"
