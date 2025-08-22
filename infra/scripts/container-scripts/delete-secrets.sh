034740c663a7ab903f6d4556be6413c106dd3e0f
Ingvar Sigurðsson <ingvar@apro.is>
2025-08-22 14:15:46 +0000

    feat(argocd-cleanup): check if _read role exists

--- infra/scripts/container-scripts/delete-secrets.sh
+++ infra/scripts/container-scripts/delete-secrets.sh
@@ -6,8 +6,6 @@ set -x
 FEATURE_NAME=$1
 echo "Deleting secrets for feature $FEATURE_NAME"
 
-sleep 10
-
 if command -v yarn >/dev/null 2>&1; then
   # Use the package.json “secrets” script
   yarn secrets delete -- "/k8s/feature-${FEATURE_NAME}"
--- infra/scripts/container-scripts/destroy-dbs.sh
+++ infra/scripts/container-scripts/destroy-dbs.sh
@@ -20,9 +20,19 @@ echo "Dropping role with name ${DB_USER}"
 
 # Determine if $DB_USER is a read-only user and revoke its privileges
 if [[ "$DB_USER" == *"_read" ]]; then
-  psql -c "REVOKE USAGE ON SCHEMA PUBLIC FROM ${DB_USER}"
-  psql -c "REVOKE SELECT ON ALL TABLES IN SCHEMA PUBLIC FROM ${DB_USER}"
-  psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM ${DB_USER}" || true
+
+  # We also need to double check if the role exists before trying to revoke privileges
+  READ_ONLY_USER_EXISTS=$(psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='${DB_USER}'")
+  if [[ "$READ_ONLY_USER_EXISTS" == "1" ]]; then
+    echo "Revoking privileges for read-only user ${DB_USER}"
+    psql -c "REVOKE USAGE ON SCHEMA PUBLIC FROM ${DB_USER}"
+    psql -c "REVOKE SELECT ON ALL TABLES IN SCHEMA PUBLIC FROM ${DB_USER}"
+    psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM ${DB_USER}" || true
+
+  else
+    echo "Read-only user ${DB_USER} does not exist, skipping privilege revocation"
+    exit 0
+  fi
 fi
 
 psql -c "DROP ROLE IF EXISTS ${DB_USER};" || true
