8944e6114f929e5a7e4d6f9f0b67ac042f9a5d8b
Hyosam <hyo-sam.nandkisore@advania.is>
2025-08-25 17:56:40 +0000

    fix(NavComponent): adjust styles and add minWidth for better layout handling

--- libs/portals/admin/form-system/src/components/NavComponent/NavComponent.css.ts
+++ libs/portals/admin/form-system/src/components/NavComponent/NavComponent.css.ts
@@ -6,12 +6,13 @@ const base = style({
   borderRadius: theme.border.radius.standard,
   marginBottom: '2px',
   height: '46px',
-  marginLeft: 'auto',
+  // marginLeft: 'auto',
   lineHeight: '46px',
   ':hover': {
     fontWeight: 'bolder',
   },
   cursor: 'grab',
+  flexShrink: 0,
 })
 
 export const step = style({
