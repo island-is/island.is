ad35f59cd111c7053716d68d01447c12cfa1df62
Gunnar K Vilbergsson <gunnar@norda.is>
2025-08-25 13:07:26 +0000

    Add messages for the termination reasons

--- libs/application/templates/hms/terminate-rental-agreement/src/utils/options.ts
+++ libs/application/templates/hms/terminate-rental-agreement/src/utils/options.ts
@@ -30,23 +30,34 @@ export const rentalContractOptions = (application: Application) => {
   }))
 }
 
-const terminationReasons = [
-  'Leigusali býr í sama húsnæði',
-  'Húsnæði er leigt út með húsgögnum',
-  'Leigusali tekur húsnæðið til eigin nota',
-  'Leigusali ráðstafar húsnæði til skyldmenna',
-  'Leigusali hyggst selja húsnæðið á næstu 6 mánuðum',
-  'Fyrirhugaðar eru verulegar viðgerðir á húsnæði',
-  'Leigjandi var starfsmaður leigusala og hefur látið af störfum',
-  'Leigjandi hefur gerst sekur um vanefndir eða brot sem varða riftun',
-  'Leigjandi hefur á annan hátt vanefnt skyldur sínar',
-  'Sanngjarnt mat á hagsmunum og aðstæðum réttlætir uppsögn',
-  'Leigusali er lögaðili sem er ekki rekinn í hagnaðarskyni og leigjandi uppfyllir ekki lengur skilyrði fyrir leigu',
-]
+const terminationReasons = {
+  'Leigusali býr í sama húsnæði':
+    m.unboundTerminationMessages.reasonOptionsCohabitation,
+  'Húsnæði er leigt út með húsgögnum':
+    m.unboundTerminationMessages.reasonOptionsFurnishedRent,
+  'Leigusali tekur húsnæðið til eigin nota':
+    m.unboundTerminationMessages.reasonOptionsTakingBack,
+  'Leigusali ráðstafar húsnæði til skyldmenna':
+    m.unboundTerminationMessages.reasonOptionsRelatives,
+  'Leigusali hyggst selja húsnæðið á næstu 6 mánuðum':
+    m.unboundTerminationMessages.reasonOptionsSelling,
+  'Fyrirhugaðar eru verulegar viðgerðir á húsnæði':
+    m.unboundTerminationMessages.reasonOptionsSignificantRepairs,
+  'Leigjandi var starfsmaður leigusala og hefur látið af störfum':
+    m.unboundTerminationMessages.reasonOptionsEmployee,
+  'Leigjandi hefur gerst sekur um vanefndir eða brot sem varða riftun':
+    m.unboundTerminationMessages.reasonOptionsNonCompliance,
+  'Leigjandi hefur á annan hátt vanefnt skyldur sínar':
+    m.unboundTerminationMessages.reasonOptionsBehavior,
+  'Sanngjarnt mat á hagsmunum og aðstæðum réttlætir uppsögn':
+    m.unboundTerminationMessages.reasonOptionsBothPartiesInterests,
+  'Leigusali er lögaðili sem er ekki rekinn í hagnaðarskyni og leigjandi uppfyllir ekki lengur skilyrði fyrir leigu':
+    m.unboundTerminationMessages.reasonOptionsNonProfitTenant,
+}
 
-export const terminationReasonOptions = terminationReasons.map(
-  (terminationReason) => ({
-    value: terminationReason,
-    label: terminationReason,
+export const terminationReasonOptions = Object.entries(terminationReasons).map(
+  ([key, value]) => ({
+    value: key,
+    label: value,
   }),
 )
