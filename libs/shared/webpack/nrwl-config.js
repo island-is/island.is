b3f4659944d0a4f5ef774e41d6be91f2ce912f83
Gunnar K Vilbergsson <gunnar@norda.is>
2025-08-26 09:20:26 +0000

    fix(application-system): Add needed polyfills

--- libs/shared/webpack/nrwl-config.js
+++ libs/shared/webpack/nrwl-config.js
@@ -73,6 +73,9 @@ const addNodeModulesPolyfill = (config) => {
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
     vm: require.resolve('vm-browserify'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
   }
 
   config.plugins.push(
