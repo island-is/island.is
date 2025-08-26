09f37a0f331470faad759d035e006232925f0312
Ívar Oddsson <ivaroddsson@gmail.com>
2025-08-26 15:24:49 +0000

    CSS

--- apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.css.ts
+++ apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.css.ts
@@ -2,8 +2,15 @@ import { style } from '@vanilla-extract/css'
 
 import { theme } from '@island.is/island-ui/theme'
 
+export const ul = style({
+  display: 'grid',
+  gap: theme.spacing[2],
+  width: '100%',
+})
+
 export const reorderGroup = style({
   marginTop: theme.spacing[2],
+  gap: theme.spacing[2],
 
   selectors: {
     '&:not(:has([data-reorder-item]))': {
--- apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.tsx
+++ apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.tsx
@@ -1,5 +1,6 @@
 import { FC, useContext, useEffect, useState } from 'react'
 import { Reorder, useDragControls } from 'motion/react'
+import { uuid } from 'uuidv4'
 
 import { Box, Tag } from '@island.is/island-ui/core'
 import {
@@ -8,6 +9,8 @@ import {
   MultipleValueList,
 } from '@island.is/judicial-system-web/src/components'
 import { useCourtDocuments } from '@island.is/judicial-system-web/src/components/CourtDocuments/CourtDocuments'
+import EditableCaseFile from '@island.is/judicial-system-web/src/components/EditableCaseFile/EditableCaseFile'
+import { TUploadFile } from '@island.is/judicial-system-web/src/utils/hooks'
 
 import * as styles from './CourtRecord.css'
 
@@ -15,9 +18,9 @@ const CourtRecord: FC = () => {
   const { workingCase } = useContext(FormContext)
   const { handleAddDocument } = useCourtDocuments()
   const controls = useDragControls()
-  const [reorderableItems, setReorderableItems] = useState<{ name: string }[]>(
-    [],
-  )
+  const [reorderableItems, setReorderableItems] = useState<
+    { id: string; name: string }[]
+  >([])
 
   useEffect(() => {
     if (!workingCase.courtDocuments) {
@@ -26,6 +29,7 @@ const CourtRecord: FC = () => {
 
     setReorderableItems(
       workingCase.courtDocuments.map((doc) => ({
+        id: doc.id,
         name: doc.name,
       })),
     )
@@ -33,7 +37,9 @@ const CourtRecord: FC = () => {
 
   return (
     <MultipleValueList
-      onAddValue={handleAddDocument}
+      onAddValue={(v) =>
+        setReorderableItems((prev) => [...prev, { id: uuid(), name: v }])
+      }
       inputLabel="Heiti dómskjals"
       inputPlaceholder="Skrá inn heiti á skjali hér"
       buttonText="Bæta við skjali"
@@ -50,18 +56,34 @@ const CourtRecord: FC = () => {
           axis="y"
           values={reorderableItems}
           onReorder={setReorderableItems}
+          className={styles.ul}
         >
           {reorderableItems.map((item, index) => {
             return (
-              <Reorder.Item key={item.name} value={item} data-reorder-item>
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="spaceBetween"
-                  key={item.name}
-                >
-                  <p>{item.name}</p>
-                </Box>
+              <Reorder.Item key={item.id} value={item} data-reorder-item>
+                <EditableCaseFile
+                  enableDrag
+                  caseFile={{ id: item.id, displayText: item.name }}
+                  onOpen={function (id: string): void {
+                    throw new Error('Function not implemented.')
+                  }}
+                  onRename={function (
+                    id: string,
+                    name: string,
+                    displayDate: string,
+                  ): void {
+                    throw new Error('Function not implemented.')
+                  }}
+                  onDelete={function (file: TUploadFile): void {
+                    throw new Error('Function not implemented.')
+                  }}
+                  onStartEditing={function (): void {
+                    throw new Error('Function not implemented.')
+                  }}
+                  onStopEditing={function (): void {
+                    throw new Error('Function not implemented.')
+                  }}
+                />
               </Reorder.Item>
             )
           })}
