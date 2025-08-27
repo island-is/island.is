43d32ccda4166c1c851f747f413f9564978211f6
Gunnar K Vilbergsson <gunnar@norda.is>
2025-08-27 13:37:37 +0000

    Revert "Fix TS errors in infra dsl processors"
    This reverts commit e4e9263e30301b0330df37e53ada67434839a8e5.

--- infra/src/dsl/service-to-environment/pre-process-service.ts
+++ infra/src/dsl/service-to-environment/pre-process-service.ts
@@ -73,9 +73,6 @@ export function prepareServiceForEnv(
   const ingress = Object.entries(serviceDef.ingress).reduce(
     (acc, [name, ingress]) => {
       const envType = localFromDev(env.type)
-      if (!ingress) {
-        return acc
-      }
       return {
         ...acc,
         ...(ingress.host[envType] === MissingSetting
