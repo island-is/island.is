cb271eb14cd11f309b073a9656ebc530c18213a0
Ívar Oddsson <ivaroddsson@gmail.com>
2025-08-28 15:24:52 +0000

    Enable using arrow keys in search

--- apps/judicial-system/web/src/components/Modals/SearchModal/SearchModal.css.ts
+++ apps/judicial-system/web/src/components/Modals/SearchModal/SearchModal.css.ts
@@ -4,6 +4,12 @@ import { theme } from '@island.is/island-ui/theme'
 
 export const resultButton = style({
   width: '100%',
+
+  selectors: {
+    '&:hover': {
+      background: theme.color.blue100,
+    },
+  },
 })
 
 export const searchModal = style({
@@ -14,6 +20,11 @@ export const searchModal = style({
 
 export const searchResultsContainer = style({
   overflow: 'scroll',
+})
+
+export const focus = style({
+  background: theme.color.blue100,
+  borderRadius: theme.border.radius.standard,
 })
 
 export const searchResults = style({
