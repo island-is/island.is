1f2cbc3d6bc5bb8bded16cf8eddb2b63d71190c7
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-08-29 10:45:56 +0000

    Add postPruned and postPruneAt db index

---
+++ apps/application-system/api/migrations/20250813095708-application-add-postPruned-and-postPruneAt-col.js
@@ -0,0 +1,55 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await Promise.all([
+        queryInterface.addColumn(
+          'application',
+          'post_pruned',
+          {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+          },
+          { transaction: t },
+        ),
+        queryInterface.addColumn(
+          'application',
+          'post_prune_at',
+          {
+            type: Sequelize.DATE,
+            allowNull: true,
+            defaultValue: null,
+          },
+          { transaction: t },
+        ),
+      ])
+
+      await queryInterface.addIndex(
+        'application',
+        ['post_pruned', 'post_prune_at'],
+        {
+          name: 'application_post_prune_pending_idx',
+          transaction: t,
+        },
+      )
+    })
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.removeIndex(
+        'application',
+        'application_post_prune_pending_idx',
+        { transaction: t },
+      )
+      await queryInterface.removeColumn('application', 'post_pruned', {
+        transaction: t,
+      })
+      await queryInterface.removeColumn('application', 'post_prune_at', {
+        transaction: t,
+      })
+    })
+  },
+}
