9f41a4e99d3101e0eaf769693ad2c26997296784
norda-gunni <gunnar@norda.is>
2025-09-01 12:36:06 +0000

    fix(application-system): Terminate rental agreement: translate termination reasons (#19850)
    * Add messages for the termination reasons
    
    * Remove texts from option values
    
    * Trigger user-profile
    
    * Add polyfills
    
    * Increase webpack build mx memory
    
    * chore: format update dirty file
    
    * Fix TS errors in infra dsl processors
    
    * Revert "Fix TS errors in infra dsl processors"
    
    This reverts commit e4e9263e30301b0330df37e53ada67434839a8e5.
    
    * Revert polyfill changes
    
    * Copy TerminationReason rather than import to avoid headache
    
    * Cleanup
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/application/templates/hms/terminate-rental-agreement/src/lib/messages/unboundTerminationMessage.ts
+++ libs/application/templates/hms/terminate-rental-agreement/src/lib/messages/unboundTerminationMessage.ts
@@ -22,4 +22,62 @@ export const unboundTerminationMessages = defineMessages({
       'Vinsamlegast tilkynnið riftun að lágmarki einum mánuði fyrir riftunardagsetningu',
     description: 'Unbound termination date info',
   },
+  reasonOptionsCohabitation: {
+    id: 'tra.application:unboundTermination.reasonOptionsCohabitation',
+    defaultMessage: 'Leigusali býr í sama húsnæði',
+    description: 'Unbound termination reason options cohabitation',
+  },
+  reasonOptionsFurnishedRent: {
+    id: 'tra.application:unboundTermination.reasonOptionsFurnishedRent',
+    defaultMessage: 'Húsnæði er leigt út með húsgögnum',
+    description: 'Unbound termination reason options furnished rent',
+  },
+  reasonOptionsTakingBack: {
+    id: 'tra.application:unboundTermination.reasonOptionsTakingBack',
+    defaultMessage: 'Leigusali tekur húsnæðið til eigin nota',
+    description: 'Unbound termination reason options taking back',
+  },
+  reasonOptionsRelatives: {
+    id: 'tra.application:unboundTermination.reasonOptionsRelatives',
+    defaultMessage: 'Leigusali ráðstafar húsnæði til skyldmenna',
+    description: 'Unbound termination reason options relatives',
+  },
+  reasonOptionsSelling: {
+    id: 'tra.application:unboundTermination.reasonOptionsSelling',
+    defaultMessage: 'Leigusali hyggst selja húsnæðið á næstu 6 mánuðum',
+    description: 'Unbound termination reason options selling',
+  },
+  reasonOptionsSignificantRepairs: {
+    id: 'tra.application:unboundTermination.reasonOptionsSignificantRepairs',
+    defaultMessage: 'Fyrirhugaðar eru verulegar viðgerðir á húsnæði',
+    description: 'Unbound termination reason options significant repairs',
+  },
+  reasonOptionsEmployee: {
+    id: 'tra.application:unboundTermination.reasonOptionsEmployee',
+    defaultMessage:
+      'Leigjandi var starfsmaður leigusala og hefur látið af störfum',
+    description: 'Unbound termination reason options employee',
+  },
+  reasonOptionsNonCompliance: {
+    id: 'tra.application:unboundTermination.reasonOptionsNonCompliance',
+    defaultMessage:
+      'Leigjandi hefur gerst sekur um vanefndir eða brot sem varða riftun',
+    description: 'Unbound termination reason options non compliance',
+  },
+  reasonOptionsBehavior: {
+    id: 'tra.application:unboundTermination.reasonOptionsBehavior',
+    defaultMessage: 'Leigjandi hefur á annan hátt vanefnt skyldur sínar',
+    description: 'Unbound termination reason options behavior',
+  },
+  reasonOptionsBothPartiesInterests: {
+    id: 'tra.application:unboundTermination.reasonOptionsBothPartiesInterests',
+    defaultMessage: 'Sanngjarnt mat á hagsmunum og aðstæðum réttlætir uppsögn',
+    description: 'Unbound termination reason options both parties interests',
+  },
+  reasonOptionsNonProfitTenant: {
+    id: 'tra.application:unboundTermination.reasonOptionsNonProfitTenant',
+    defaultMessage:
+      'Leigusali er lögaðili sem er ekki rekinn í hagnaðarskyni og leigjandi uppfyllir ekki lengur skilyrði fyrir leigu',
+    description: 'Unbound termination reason options non profit tenant',
+  },
 })
