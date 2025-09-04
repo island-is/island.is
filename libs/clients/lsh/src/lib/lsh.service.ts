5f46427bc221a76dbf100f011eb9812e375995a0
disaerna <disa@hugsmidjan.is>
2025-09-04 21:43:08 +0000

    fix: remove client

--- libs/clients/lsh/src/lib/lsh.service.ts
+++ libs/clients/lsh/src/lib/lsh.service.ts
@@ -3,21 +3,13 @@ import { BloodApi } from '../../gen/fetch'
 import { Auth, AuthMiddleware, User } from '@island.is/auth-nest-tools'
 import { BloodTypeDto, mapBloodTypeDto } from './dtos/bloodTypes.dto'
 import { handle404 } from '@island.is/clients/middlewares'
-import { Form, PatientPropertiesApi } from './questionnaries-temp/gen/fetch'
 
 @Injectable()
 export class LshClientService {
-  constructor(
-    private readonly api: BloodApi,
-    private readonly questionnariesApi: PatientPropertiesApi,
-  ) {}
+  constructor(private readonly api: BloodApi) {}
 
   private bloodTypeWithAuth(auth: Auth) {
     return this.api.withMiddleware(new AuthMiddleware(auth))
-  }
-
-  private questionnariesWithAuth(auth: Auth) {
-    return this.questionnariesApi.withMiddleware(new AuthMiddleware(auth))
   }
 
   getBloodType = async (
@@ -33,14 +25,5 @@ export class LshClientService {
     }
 
     return mapBloodTypeDto(bloodType)
-  }
-
-  getQuestionnaries = async (
-    user: User,
-    locale: string,
-  ): Promise<Form[] | null> => {
-    return this.questionnariesWithAuth(user)
-      .apiV2PatientPropertiesGetFormListGet()
-      .catch(handle404)
   }
 }
