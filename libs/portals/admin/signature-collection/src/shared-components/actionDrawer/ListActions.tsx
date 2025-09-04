b6c797413482a72c4c8ef6f43272cd209cc24997
Albina Guseynova <albina@juni.is>
2025-09-04 10:13:22 +0000

    fix(signature-collection): Signature collection fixes 3/9 (#19957)
    * fix(signature-collection): ui & functionality fixes in admin / stjornbord
    
    * admin - ui and functionality finishups across all types of elections
    
    * test
    
    * chore: format update dirty file
    
    ---------
    
    Co-authored-by: albina <albina@kosmosogkaos.is>
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/portals/admin/signature-collection/src/shared-components/actionDrawer/ListActions.tsx
+++ libs/portals/admin/signature-collection/src/shared-components/actionDrawer/ListActions.tsx
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom'
 import { SignatureCollectionList } from '@island.is/api/schema'
 import ActionExtendDeadline from '../extendDeadline'
 import ActionLockList from '../lockList'
-import ActionReviewComplete from '../completeListReview'
+import CompleteListReview from '../completeListReview'
 import DownloadReports from '../downloadReports'
 import RemoveCandidate from '../removeCandidate'
 import { m } from '../../lib/messages'
@@ -57,12 +57,13 @@ const ListActions = ({ allowedActions }: ListActionsProps = {}) => {
         )}
         {allowedActions?.includes(Actions.LockList) && (
           <ActionLockList
+            isLocked={!list.active}
             listId={list.id}
             collectionType={list.collectionType}
           />
         )}
         {allowedActions?.includes(Actions.ReviewComplete) && (
-          <ActionReviewComplete
+          <CompleteListReview
             listId={list?.id}
             listStatus={listStatus}
             collectionType={list.collectionType}
