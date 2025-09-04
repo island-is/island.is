2db33472fc05008f9d31b6eafb87c81f55ab19f2
norda-gunni <gunnar@norda.is>
2025-09-04 11:45:27 +0000

    fix(application-system): Fix termination reasons (#19960)
    * fix(application-system): Fix termination reasons
    
    * lintfix
    
    * Fix termination reason text display

--- libs/application/template-api-modules/src/lib/modules/templates/hms/terminate-rental-agreement/utils.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/hms/terminate-rental-agreement/utils.ts
@@ -7,28 +7,6 @@ import {
 } from '@island.is/clients/hms-rental-agreement'
 import { AttachmentData } from '../../../shared/services/attachment-s3.service'
 
-const terminationReasonMap: Record<string, TerminationReason> = {
-  'Leigusali býr í sama húsnæði': TerminationReason.OWNERINBUILDING,
-  'Húsnæði er leigt út með húsgögnum': TerminationReason.FURNISHEDRENT,
-  'Leigusali tekur húsnæðið til eigin nota': TerminationReason.OWNERTAKINGBACK,
-  'Leigusali ráðstafar húsnæði til skyldmenna':
-    TerminationReason.OWNERRELATIVES,
-  'Leigusali hyggst selja húsnæðið á næstu 6 mánuðum':
-    TerminationReason.OWNERSELLING,
-  'Fyrirhugaðar eru verulegar viðgerðir á húsnæði':
-    TerminationReason.SIGNIFICANTREPAIRS,
-  'Leigjandi var starfsmaður leigusala og hefur látið af störfum':
-    TerminationReason.TENANTEMPLOYEE,
-  'Leigjandi hefur gerst sekur um vanefndir eða brot sem varða riftun':
-    TerminationReason.TENANTNONCOMPLIANCE,
-  'Leigjandi hefur á annan hátt vanefnt skyldur sínar':
-    TerminationReason.TENANTBEHAVIOR,
-  'Sanngjarnt mat á hagsmunum og aðstæðum réttlætir uppsögn':
-    TerminationReason.BOTHPARTIESINTERESTS,
-  'Leigusali er lögaðili sem er ekki rekinn í hagnaðarskyni og leigjandi uppfyllir ekki lengur skilyrði fyrir leigu':
-    TerminationReason.NONPROFITTENANT,
-}
-
 export const isCancellation = (application: Application) => {
   const terminationType = getValueViaPath<string>(
     application.answers,
@@ -84,10 +62,8 @@ export const parseTerminateContract = (
         'boundTermination.boundTerminationDate',
       ) ?? new Date(),
     reasonUseCode:
-      terminationReason in TerminationReason
-        ? (terminationReason as TerminationReason)
-        : terminationReasonMap[terminationReason] ??
-          TerminationReason.OWNERINBUILDING,
+      (terminationReason as TerminationReason) ??
+      TerminationReason.OWNERINBUILDING,
     document: files[0].fileContent,
     documentMime: files[0].fileName.split('.').pop() ?? 'pdf',
     documentFilename: truncateMiddle(files[0].fileName, 40),
