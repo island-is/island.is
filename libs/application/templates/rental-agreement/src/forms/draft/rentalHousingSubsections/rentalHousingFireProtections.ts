30984caaea1db6062b89f0b64b57b8608e390d3c
Jónas G. Sigurðsson <jonas@norda.is>
2025-09-04 09:33:25 +0000

    chore(hms-ra): make overview shared (#19914)
    * fix: 0 prefix bug
    
    * fix: fire blanket yes/no
    
    * fix: data submission to hms
    
    * chore: remove tenant representative
    
    * Fix: bank guarantee amount should be less than triple rent
    
    * chore: simplify super refines slightly
    
    * feat: add alert if date is more than a year into the future
    
    * feat: refactor so validation for parties works
    
    * fix: pipeline
    
    * chore: remove unneccasery fragment
    
    * chore: start of overview refactor
    
    * chore: address coderabbit comments
    
    * Revert "chore: address coderabbit comments"
    
    This reverts commit 374b3668a8b06a15191d3a17baf962237882b280.
    
    * fix: validation for smoke detectors and fire extingushers
    
    * chore: remove console.logs
    
    * chore: more overview refactoring
    
    * chore: finish refactoring overview and deleteng the custom overview
    
    * chore: remove console.log
    
    * chore: remove email from template-api-module and the template
    
    * chore: remove $ from rendered string
    
    * chore: make message imports uniform across the application
    
    * Update libs/application/templates/rental-agreement/src/utils/overviewUtils.ts
    
    * fix: fix typo from paty to party
    
    * chore: remove 4 added lints
    
    * chore: address code rabbit comments

--- libs/application/templates/rental-agreement/src/forms/draft/rentalHousingSubsections/rentalHousingFireProtections.ts
+++ libs/application/templates/rental-agreement/src/forms/draft/rentalHousingSubsections/rentalHousingFireProtections.ts
@@ -5,30 +5,57 @@ import {
   buildTextField,
   buildHiddenInputWithWatchedValue,
   buildRadioField,
+  buildAlertMessageField,
+  getValueViaPath,
 } from '@island.is/application/core'
 import { Routes } from '../../../utils/enums'
-import { housingFireProtections } from '../../../lib/messages'
-import { getYesNoOptions } from '../../../utils/utils'
+import * as m from '../../../lib/messages'
+import { getRentalPropertySize } from '../../../utils/utils'
+import { shouldShowSmokeDetectorsAlert } from '../../../utils/conditions'
+import { PropertyUnit } from '../../../shared/types'
+import { getYesNoOptions } from '../../../utils/options'
 
 export const RentalHousingFireProtections = buildSubSection({
   id: Routes.FIREPROTECTIONS,
-  title: housingFireProtections.subSectionName,
+  title: m.housingFireProtections.subSectionName,
   children: [
     buildMultiField({
       id: Routes.FIREPROTECTIONS,
-      title: housingFireProtections.pageTitle,
-      description: housingFireProtections.pageDescription,
+      title: m.housingFireProtections.pageTitle,
+      description: m.housingFireProtections.pageDescription,
       children: [
         buildDescriptionField({
           id: 'fireProtections.smokeDetectorsFireExtinguisherRequirements',
           description:
-            housingFireProtections.smokeDetectorsFireExtinguisherRequirements,
+            m.housingFireProtections.smokeDetectorsFireExtinguisherRequirements,
           space: 0,
+        }),
+        buildAlertMessageField({
+          id: 'fireProtections.smokeDetectorsRequirements',
+          condition: shouldShowSmokeDetectorsAlert,
+          title: m.housingFireProtections.smokeDetectorsAlertTitle,
+          message: (application) => {
+            const propertySize = getValueViaPath<Array<PropertyUnit>>(
+              application.answers,
+              'registerProperty.searchresults.units',
+            )
+
+            const size = getRentalPropertySize(propertySize ?? [])
+            const requiredSmokeDetectors = Math.ceil(Number(size) / 80)
+
+            return {
+              ...m.housingFireProtections.smokeDetectorsAlertMessage,
+              values: {
+                propertySize: size,
+                requiredSmokeDetectors,
+              },
+            }
+          },
+          alertType: 'warning',
         }),
         buildTextField({
           id: 'fireProtections.smokeDetectors',
-          title: housingFireProtections.smokeDetectorsLabel,
-          placeholder: '0',
+          title: m.housingFireProtections.smokeDetectorsLabel,
           width: 'half',
           maxLength: 1,
           format: '#',
@@ -37,8 +64,7 @@ export const RentalHousingFireProtections = buildSubSection({
         }),
         buildTextField({
           id: 'fireProtections.fireExtinguisher',
-          title: housingFireProtections.fireExtinguisherLabel,
-          placeholder: '0',
+          title: m.housingFireProtections.fireExtinguisherLabel,
           width: 'half',
           maxLength: 1,
           format: '#',
@@ -47,9 +73,9 @@ export const RentalHousingFireProtections = buildSubSection({
         }),
         buildDescriptionField({
           id: 'fireProtections.fireBlanketRequirements',
-          title: housingFireProtections.fireBlanketLabel,
+          title: m.housingFireProtections.fireBlanketLabel,
           titleVariant: 'h3',
-          description: housingFireProtections.fireBlanketRequirements,
+          description: m.housingFireProtections.fireBlanketRequirements,
           space: 4,
         }),
         buildRadioField({
@@ -60,9 +86,9 @@ export const RentalHousingFireProtections = buildSubSection({
         }),
         buildDescriptionField({
           id: 'fireProtections.exitRequirements',
-          title: housingFireProtections.exitsLabel,
+          title: m.housingFireProtections.exitsLabel,
           titleVariant: 'h3',
-          description: housingFireProtections.exitRequirements,
+          description: m.housingFireProtections.exitRequirements,
           space: 4,
         }),
         buildRadioField({
