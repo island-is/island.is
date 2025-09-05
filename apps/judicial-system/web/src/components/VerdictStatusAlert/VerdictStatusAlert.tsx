bf7394db0792447ea89e4b83478eb8b92918b858
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-09-05 13:02:51 +0000

    chore(j-s): adjust verdict status alert

--- apps/judicial-system/web/src/components/VerdictStatusAlert/VerdictStatusAlert.tsx
+++ apps/judicial-system/web/src/components/VerdictStatusAlert/VerdictStatusAlert.tsx
@@ -1,11 +1,12 @@
 import { useContext, useEffect, useState } from 'react'
 
-import { AlertMessage, Box, Text } from '@island.is/island-ui/core'
+import { AlertMessage, Box, LoadingDots, Text } from '@island.is/island-ui/core'
 import { TIME_FORMAT } from '@island.is/judicial-system/consts'
 import { formatDate } from '@island.is/judicial-system/formatters'
 import { Lawyer } from '@island.is/judicial-system/types'
 
 import { Defendant, Verdict, VerdictServiceStatus } from '../../graphql/schema'
+import useVerdict from '../../utils/hooks/useVerdict'
 import { LawyerRegistryContext } from '../LawyerRegistryProvider/LawyerRegistryProvider'
 
 const mapServiceStatusMessages = (verdict: Verdict, lawyer?: Lawyer) => {
@@ -71,36 +72,18 @@ const mapServiceStatusMessages = (verdict: Verdict, lawyer?: Lawyer) => {
       ]
   }
 }
-
-const VerdictStatusAlert = ({
+const VerdictStatusAlertMessage = ({
+  defendantName,
   verdict,
-  defendant,
+  lawyer,
 }: {
+  defendantName?: string
   verdict: Verdict
-  defendant: Defendant
+  lawyer?: Lawyer
 }) => {
-  const [lawyer, setLawyer] = useState<Lawyer>()
-  const { lawyers } = useContext(LawyerRegistryContext)
-  const messages = mapServiceStatusMessages(verdict, lawyer)
-  const isServed = verdict.serviceDate && verdict.serviceStatus
+  const messages = verdict ? mapServiceStatusMessages(verdict, lawyer) : []
 
-  useEffect(() => {
-    if (
-      !verdict?.defenderNationalId ||
-      verdict?.serviceStatus !== VerdictServiceStatus.DEFENDER ||
-      !lawyers ||
-      lawyers.length === 0
-    ) {
-      return
-    }
-
-    setLawyer(
-      lawyers.find(
-        (lawyer) => lawyer.nationalId === verdict.defenderNationalId,
-      ),
-    )
-  }, [lawyers, verdict?.defenderNationalId, verdict?.serviceStatus])
-
+  const isServed = verdict?.serviceDate && verdict?.serviceStatus
   if (isServed) {
     return (
       <AlertMessage
@@ -123,7 +106,7 @@ const VerdictStatusAlert = ({
     return (
       <AlertMessage
         type="info"
-        title={`Dómur sendur í Lögbirtingarblaðið - ${defendant.name}`}
+        title={`Dómur sendur í Lögbirtingarblaðið - ${defendantName}`}
         message={`Dómur sendur - ${formatDate(
           verdict.legalPaperRequestDate,
         )} kl. ${formatDate(verdict.legalPaperRequestDate, TIME_FORMAT)}`}
@@ -144,6 +127,53 @@ const VerdictStatusAlert = ({
   }
 
   return null
+}
+
+const VerdictStatusAlert = (props: {
+  verdict: Verdict
+  defendant: Defendant
+}) => {
+  const { verdict: currentVerdict, defendant } = props
+  const [lawyer, setLawyer] = useState<Lawyer>()
+  const { lawyers } = useContext(LawyerRegistryContext)
+  const { verdict, verdictLoading } = useVerdict(currentVerdict)
+
+  useEffect(() => {
+    if (
+      !verdict?.defenderNationalId ||
+      verdict?.serviceStatus !== VerdictServiceStatus.DEFENDER ||
+      !lawyers ||
+      lawyers.length === 0
+    ) {
+      return
+    }
+
+    setLawyer(
+      lawyers.find(
+        (lawyer) => lawyer.nationalId === verdict.defenderNationalId,
+      ),
+    )
+  }, [lawyers, verdict?.defenderNationalId, verdict?.serviceStatus])
+
+  return verdictLoading ? (
+    <Box display="flex" justifyContent="center" paddingY={5}>
+      <LoadingDots />
+    </Box>
+  ) : !verdict ? (
+    <Box marginBottom={2}>
+      <AlertMessage
+        type="error"
+        title={'Ekki tókst að sækja stöðu birtingar'}
+        message={'Vinsamlegast reyndu aftur síðar'}
+      />
+    </Box>
+  ) : (
+    <VerdictStatusAlertMessage
+      verdict={verdict}
+      lawyer={lawyer}
+      defendantName={defendant.name ?? ''}
+    />
+  )
 }
 
 export default VerdictStatusAlert
