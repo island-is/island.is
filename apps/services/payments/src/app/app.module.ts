91b1f243163765d211687c3ac811074c5e1384f4
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2025-09-06 10:13:37 +0000

    Refactor app.module.ts: Reorder imports and remove redundant lines for improved clarity

--- apps/services/payments/src/app/app.module.ts
+++ apps/services/payments/src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { Module } from '@nestjs/common'
-import { SequelizeModule } from '@nestjs/sequelize'
-import { ConfigModule } from '@nestjs/config'
-import { FeatureFlagConfig } from '@island.is/nest/feature-flags'
 import { LoggingModule } from '@island.is/logging'
-import { AuditModule } from '@island.is/nest/audit'
+import { FeatureFlagConfig } from '@island.is/nest/feature-flags'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
 
-import { ProblemModule } from '@island.is/nest/problem'
 import { ChargeFjsV2ClientConfig } from '@island.is/clients/charge-fjs-v2'
 import { XRoadConfig } from '@island.is/nest/config'
+import { ProblemModule } from '@island.is/nest/problem'
 
-import { PaymentFlowModule } from './paymentFlow/paymentFlow.module'
+import { SequelizeConfigService } from '../sequelizeConfig.service'
 import { CardPaymentModule } from './cardPayment/cardPayment.module'
 import { InvoicePaymentModule } from './invoicePayment/invoicePayment.module'
-import { SequelizeConfigService } from '../sequelizeConfig.service'
 import { JwksModule } from './jwks/jwks.module'
-import { environment } from '../environments'
+import { PaymentFlowModule } from './paymentFlow/paymentFlow.module'
 
 @Module({
   imports: [
