4114cc01cf2843baa17de978e69da76294f1bc71
Rúnar Vestmann <runar@stefna.is>
2025-09-10 15:29:58 +0000

    Change naming in infra

--- apps/services/payment-flow-update-handler/infra/payment-flow-update-handler.ts
+++ apps/services/payment-flow-update-handler/infra/payment-flow-update-handler.ts
@@ -9,61 +9,62 @@ const namespace = 'services-payment-flow-update-handler'
 const serviceName = namespace
 const imageName = namespace
 
-export const serviceSetup = (): ServiceBuilder<'payment-flow-update-handler'> =>
-  service(serviceName)
-    .image(imageName)
-    .namespace(namespace)
-    .env({
-      PAYMENTS_WEB_URL: {
-        dev: ref(
-          (ctx) =>
-            `https://${
-              ctx.featureDeploymentName ? `${ctx.featureDeploymentName}-` : ''
-            }beta.dev01.devland.is/greida`,
-        ),
-        staging: `https://beta.staging01.devland.is/greida`,
-        prod: `https://island.is/greida`,
-      },
-      PAYMENT_FLOW_UPDATE_HANDLER_API_URL: ref(
-        (ctx) =>
-          `http://payment-flow-update-handler.${
-            ctx.featureDeploymentName
-              ? `${ctx.featureDeploymentName}`
-              : 'payment-flow-update-handler'
-          }.svc.cluster.local`,
-      ),
-      LANDSPITALI_WEB_MEMORIAL_CARD_PAYMENT_CONFIRMATION_EMAIL_SUBJECT: {
-        dev: '[TEST] Minningarkort - Landspítali',
-        staging: '[TEST] Minningarkort - Landspítali',
-        prod: 'Minningarkort - Landspítali',
-      },
-      LANDSPITALI_WEB_DIRECT_GRANT_PAYMENT_CONFIRMATION_EMAIL_SUBJECT: {
-        dev: '[TEST] Beinn styrkur - Landspítali',
-        staging: '[TEST] Beinn styrkur - Landspítali',
-        prod: 'Beinn styrkur - Landspítali',
-      },
-      LANDSPITALI_WEB_PAYMENT_CONFIRMATION_SEND_FROM_EMAIL: {
-        dev: 'development@island.is',
-        staging: 'development@island.is',
-        prod: 'island@island.is',
-      },
-    })
-    .secrets({
-      LANDSPITALI_WEB_PAYMENT_CONFIRMATION_SEND_TO_EMAIL:
-        '/k8s/api/LANDSPITALI_PAYMENT_CONFIRMATION_SEND_TO_EMAIL',
-    })
-    .liveness('/liveness')
-    .readiness('/readiness')
-    .grantNamespaces('services-payments')
-    .codeOwner(CodeOwners.Stefna)
-    .ingress({
-      internal: {
-        host: {
-          dev: 'payment-flow-update-handler',
-          staging: 'payment-flow-update-handler',
-          prod: 'payment-flow-update-handler',
+export const serviceSetup =
+  (): ServiceBuilder<'services-payment-flow-update-handler'> =>
+    service(serviceName)
+      .image(imageName)
+      .namespace(namespace)
+      .env({
+        PAYMENTS_WEB_URL: {
+          dev: ref(
+            (ctx) =>
+              `https://${
+                ctx.featureDeploymentName ? `${ctx.featureDeploymentName}-` : ''
+              }beta.dev01.devland.is/greida`,
+          ),
+          staging: `https://beta.staging01.devland.is/greida`,
+          prod: `https://island.is/greida`,
         },
-        paths: ['/'],
-        public: false,
-      },
-    })
+        PAYMENT_FLOW_UPDATE_HANDLER_API_URL: ref(
+          (ctx) =>
+            `http://services-payment-flow-update-handler.${
+              ctx.featureDeploymentName
+                ? `${ctx.featureDeploymentName}`
+                : 'services-payment-flow-update-handler'
+            }.svc.cluster.local`,
+        ),
+        LANDSPITALI_WEB_MEMORIAL_CARD_PAYMENT_CONFIRMATION_EMAIL_SUBJECT: {
+          dev: '[TEST] Minningarkort - Landspítali',
+          staging: '[TEST] Minningarkort - Landspítali',
+          prod: 'Minningarkort - Landspítali',
+        },
+        LANDSPITALI_WEB_DIRECT_GRANT_PAYMENT_CONFIRMATION_EMAIL_SUBJECT: {
+          dev: '[TEST] Beinn styrkur - Landspítali',
+          staging: '[TEST] Beinn styrkur - Landspítali',
+          prod: 'Beinn styrkur - Landspítali',
+        },
+        LANDSPITALI_WEB_PAYMENT_CONFIRMATION_SEND_FROM_EMAIL: {
+          dev: 'development@island.is',
+          staging: 'development@island.is',
+          prod: 'island@island.is',
+        },
+      })
+      .secrets({
+        LANDSPITALI_WEB_PAYMENT_CONFIRMATION_SEND_TO_EMAIL:
+          '/k8s/api/LANDSPITALI_PAYMENT_CONFIRMATION_SEND_TO_EMAIL',
+      })
+      .liveness('/liveness')
+      .readiness('/readiness')
+      .grantNamespaces('services-payments')
+      .codeOwner(CodeOwners.Stefna)
+      .ingress({
+        internal: {
+          host: {
+            dev: serviceName,
+            staging: serviceName,
+            prod: serviceName,
+          },
+          paths: ['/'],
+          public: false,
+        },
+      })
