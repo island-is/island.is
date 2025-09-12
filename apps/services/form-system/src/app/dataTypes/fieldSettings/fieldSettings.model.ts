2a9b1790ef2d2d39f0fb2e6d6e5f40d182cb8814
OlafurGeorg <olafur.georg.gylfason@advania.is>
2025-09-12 13:55:51 +0000

    fix

--- apps/services/form-system/src/app/dataTypes/fieldSettings/fieldSettings.model.ts
+++ apps/services/form-system/src/app/dataTypes/fieldSettings/fieldSettings.model.ts
@@ -7,6 +7,7 @@ import {
   IsOptional,
   IsString,
   ValidateNested,
+  IsNumberString,
 } from 'class-validator'
 import { Type } from 'class-transformer'
 import { ListTypesEnum } from '@island.is/form-system/shared'
