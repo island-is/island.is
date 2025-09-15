efc86cfa17bc8bb619215226340d5db29ea20f40
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-09-15 15:01:06 +0000

    feat(j-s): generate verdict service certificate (#20032)
    * feat(j-s): generate verdict service certificate
    
    * fix(j-s): verdict testing module
    
    * fix(j-s): conditions for subpoena service certificate pdf

--- apps/judicial-system/backend/src/app/modules/subpoena/subpoena.service.ts
+++ apps/judicial-system/backend/src/app/modules/subpoena/subpoena.service.ts
@@ -423,7 +423,7 @@ export class SubpoenaService {
     user: TUser,
   ): Promise<DeliverResponse> {
     return this.pdfService
-      .getServiceCertificatePdf(theCase, defendant, subpoena)
+      .getSubpoenaServiceCertificatePdf(theCase, defendant, subpoena)
       .then(async (pdf) => {
         const fileName = `Birtingarvottorð - ${defendant.name}`
 
