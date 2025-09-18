bdf4508d6a6af67f343e091a825881229c20546a
albina <albina@kosmosogkaos.is>
2025-09-18 10:01:18 +0000

    linting

--- libs/portals/admin/signature-collection/src/screens-presidential/AllCandidates/index.tsx
+++ libs/portals/admin/signature-collection/src/screens-presidential/AllCandidates/index.tsx
@@ -15,7 +15,7 @@ import { useLocale } from '@island.is/localization'
 import { m } from '../../lib/messages'
 import { IntroHeader, PortalNavigation } from '@island.is/portals/core'
 import { useLoaderData, useNavigate } from 'react-router-dom'
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import {
   CollectionStatus,
   SignatureCollectionCandidate,
