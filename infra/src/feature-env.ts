2557b8320f502fedfe00973edadc4434916e7b6e
lommi <jonorn@gmail.com>
2025-09-18 15:24:44 +0000

    chore: add link to argocd for feature deployment (#20134)
    * fix: ok
    
    * fix: ok
    
    * chore: workflow url
    
    * chore: format update dirty file
    
    * fix: ok
    
    * fix: typing
    
    * fix: comma
    
    ---------
    
    Co-authored-by: Jón Örn <jonorn@apro.is>
    Co-authored-by: islandisis-bot <ci@island.is>

--- infra/src/feature-env.ts
+++ infra/src/feature-env.ts
@@ -99,6 +99,7 @@ const parseArguments = (argv: Arguments) => {
       .filter((x) => !affectedSet.has(x)),
   })
   return {
+    feature,
     habitat,
     affectedServices,
     env,
@@ -129,10 +130,13 @@ const buildComment = (data: Services<HelmService>): string => {
 const deployedComment = (
   data: ServiceBuilder<any>[],
   excluded: string[],
+  feature: string,
 ): string => {
   return `Deployed services: ${data
     .map((d) => d.name())
-    .join(',')}. \n Excluded services: \`${excluded.join(',')}\``
+    .join(',')}. \nExcluded services: \`${excluded.join(',')}\n\n
+
+You can view the progress of your feature deployment in ArgoCD [here](https://argocd.shared.devland.is/applications?labels=feature-name%253D${feature}) \n\n`
 }
 
 yargs(process.argv.slice(2))
@@ -266,7 +270,8 @@ yargs(process.argv.slice(2))
     builder: (yargs) => yargs,
     handler: async (argv) => {
       const typedArgv = argv as unknown as Arguments
-      const { habitat, affectedServices, env } = parseArguments(typedArgv)
+      const { habitat, affectedServices, env, feature } =
+        parseArguments(typedArgv)
       const { included: featureYaml, excluded } =
         await getFeatureAffectedServices(
           habitat,
@@ -278,7 +283,11 @@ yargs(process.argv.slice(2))
         (await renderHelmServices(env, habitat, featureYaml, 'no-mocks'))
           .services,
       )
-      const includedServicesComment = deployedComment(featureYaml, excluded)
+      const includedServicesComment = deployedComment(
+        featureYaml,
+        excluded,
+        feature,
+      )
       await writeToOutput(
         `${ingressComment}\n\n${includedServicesComment}`,
         typedArgv.output,
