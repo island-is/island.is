02cb5ac8a43cd4c0fd818816c37f0ffed2be6734
Rúnar Vestmann <runar@stefna.is>
2025-09-19 15:12:52 +0000

    First draft for UI

--- apps/web/components/Organization/Slice/FeaturedGenericListItemsSlice/FeaturedGenericListItemsSlice.tsx
+++ apps/web/components/Organization/Slice/FeaturedGenericListItemsSlice/FeaturedGenericListItemsSlice.tsx
@@ -1,4 +1,9 @@
-import { FeaturedGenericListItems } from '@island.is/web/graphql/schema'
+import { Stack } from '@island.is/island-ui/core'
+import { ClickableItem, NonClickableItem } from '@island.is/web/components'
+import {
+  FeaturedGenericListItems,
+  GenericListItemType,
+} from '@island.is/web/graphql/schema'
 
 interface FeaturedGenericListItemsSliceProps {
   slice: FeaturedGenericListItems
@@ -7,5 +12,15 @@ interface FeaturedGenericListItemsSliceProps {
 export const FeaturedGenericListItemsSlice = ({
   slice,
 }: FeaturedGenericListItemsSliceProps) => {
-  return <div>FeaturedGenericListItemsSlice</div>
+  return (
+    <Stack space={2}>
+      {slice.items.map((item) => {
+        if (item.genericList?.itemType === GenericListItemType.Clickable) {
+          // TODO: Figure out how we can know the baseUrl for the clickable items
+          return <ClickableItem item={item} key={item.id} />
+        }
+        return <NonClickableItem item={item} key={item.id} />
+      })}
+    </Stack>
+  )
 }
