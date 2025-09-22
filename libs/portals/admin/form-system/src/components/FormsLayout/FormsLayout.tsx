47d69646e1df8a8894f9e58b5fea064362024d09
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2025-09-22 10:04:56 +0000

    feat(form-system): Enhance FormLayout and FormsLayout with Breadcrumbs and improved structure

--- libs/portals/admin/form-system/src/components/FormsLayout/FormsLayout.tsx
+++ libs/portals/admin/form-system/src/components/FormsLayout/FormsLayout.tsx
@@ -1,14 +1,26 @@
+import { Box, Breadcrumbs } from '@island.is/island-ui/core'
 import { useContext } from 'react'
 import { FormsContext } from '../../context/FormsContext'
-import { FormsHeader } from './components/FormsHeader'
-import { Forms } from '../Forms/Forms'
 import { Admin } from '../Admin/Admin'
-import { Applications } from '../Applications/Applications'
+import { Forms } from '../Forms/Forms'
+import { FormsHeader } from './components/FormsHeader'
+import { m } from '@island.is/form-system/ui'
+import { useIntl } from 'react-intl'
 
 export const FormsLayout = () => {
   const { location } = useContext(FormsContext)
+  const { formatMessage } = useIntl()
+
   return (
     <>
+      <Box marginBottom={3}>
+        <Breadcrumbs
+          items={[
+            { title: 'Ísland.is', href: '/stjornbord' },
+            { title: formatMessage(m.rootName) },
+          ]}
+        />
+      </Box>
       <FormsHeader />
       {location === 'forms' ? (
         <Forms />
