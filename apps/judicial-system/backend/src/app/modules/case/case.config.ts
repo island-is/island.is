96ebc03f124a6ea727a0b7d1ec351e6b8b9399f2
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-09-22 22:00:33 +0000

    chore(j-s): Case Archive Repository Service (#20065)
    * Adds a case archive repository service
    
    * Moves encryption to the case archive repository service
    
    * chore: format update dirty file
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- apps/judicial-system/backend/src/app/modules/case/case.config.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from '@island.is/nest/config'
 export const caseModuleConfig = defineConfig({
   name: 'CaseModule',
   load: (env) => ({
-    archiveEncryptionKey: env.required(
-      'ARCHIVE_ENCRYPTION_KEY',
-      'secret-archive-encryption-key',
-    ),
     robotMessageDelay: +(
       (env.optional('ROBOT_MESSAGE_DELAY') ?? '300') // 5 minutes, convert to number with +
     ),
--- apps/judicial-system/backend/src/app/modules/repository/repository.module.ts
+++ apps/judicial-system/backend/src/app/modules/repository/repository.module.ts
@@ -1,21 +1,36 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 
+import { ConfigModule } from '@island.is/nest/config'
+
 import { Case } from './models/case.model'
+import { CaseArchive } from './models/caseArchive.model'
 import { CourtDocument } from './models/courtDocument.model'
 import { CourtSession } from './models/courtSession.model'
+import { CaseArchiveRepositoryService } from './services/caseArchiveRepository.service'
 import { CaseRepositoryService } from './services/caseRepository.service'
 import { CourtDocumentRepositoryService } from './services/courtDocumentRepository.service'
 import { CourtSessionRepositoryService } from './services/courtSessionRepository.service'
+import { repositoryModuleConfig } from './repository.config'
 
 @Module({
-  imports: [SequelizeModule.forFeature([Case, CourtSession, CourtDocument])],
+  imports: [
+    SequelizeModule.forFeature([
+      Case,
+      CaseArchive,
+      CourtSession,
+      CourtDocument,
+    ]),
+    ConfigModule.forFeature(repositoryModuleConfig),
+  ],
   providers: [
+    CaseArchiveRepositoryService,
     CaseRepositoryService,
     CourtSessionRepositoryService,
     CourtDocumentRepositoryService,
   ],
   exports: [
+    CaseArchiveRepositoryService,
     CaseRepositoryService,
     CourtSessionRepositoryService,
     CourtDocumentRepositoryService,
