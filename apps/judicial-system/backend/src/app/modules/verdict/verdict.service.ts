feaaeb4f5b44b9ff4c791c1e4e7284aa348e8bb4
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-09-24 07:13:49 +0000

    chore(j-s): Remove feature flag for get delivery status (#20101)
    * chore(j-s): fix endpoint and remove boolean flag
    
    * fix(j-s): graphql schema
    
    * fix(j-s): avoid hardcoding file type code status
    
    * fix(j-s): remove unused file
    
    * fix(j-s): adjust get status and create document
    
    * fix(j-s): clean-up
    
    * fix(j-s): test

--- apps/judicial-system/backend/src/app/modules/verdict/verdict.service.ts
+++ apps/judicial-system/backend/src/app/modules/verdict/verdict.service.ts
@@ -18,6 +18,7 @@ import { normalizeAndFormatNationalId } from '@island.is/judicial-system/formatt
 import {
   CaseFileCategory,
   isVerdictInfoChanged,
+  PoliceFileTypeCode,
   type User as TUser,
 } from '@island.is/judicial-system/types'
 import { ServiceRequirement } from '@island.is/judicial-system/types'
@@ -33,6 +34,7 @@ import { DeliverResponse } from './models/deliver.response'
 type UpdateVerdict = { serviceDate?: Date | null } & Pick<
   Verdict,
   | 'externalPoliceDocumentId'
+  | 'serviceStatus'
   | 'serviceRequirement'
   | 'servedBy'
   | 'appealDecision'
@@ -97,16 +99,16 @@ export class VerdictService {
     transaction: Transaction,
     rulingDate?: Date,
   ): Promise<UpdateVerdictDto> {
+    if (!update.serviceRequirement) {
+      return update
+    }
+
     // rulingDate should be set, but the case completed guard can not guarantee its presence
     // ensure that ruling date is present to prevent side effects in handle service requirement update
     if (!rulingDate) {
       throw new BadRequestException(
         'Missing rulingDate for service requirement update',
       )
-    }
-
-    if (!update.serviceRequirement) {
-      return update
     }
 
     const currentVerdict = await this.findById(verdictId, transaction)
@@ -203,7 +205,7 @@ export class VerdictService {
       ...(theCase.courtCaseNumber
         ? [
             {
-              code: 'COURT_CASE_NUMBER',
+              code: 'VERDICT_COURT_CASE_NUMBER',
               value: theCase.courtCaseNumber,
             },
           ]
@@ -313,7 +315,7 @@ export class VerdictService {
           ? [{ code: 'RULING_DATE', value: theCase.rulingDate }]
           : []),
       ],
-      fileTypeCode: 'BRTNG_DOMUR',
+      fileTypeCode: PoliceFileTypeCode.VERDICT,
       caseSupplements: this.mapToPoliceSupplementCodes(theCase, defendant),
     })
     if (!createdDocument) {
@@ -326,22 +328,15 @@ export class VerdictService {
   }
 
   async getAndSyncVerdict(verdict: Verdict, user?: TUser) {
-    // RLS: Remove boolean var when the getVerdictDocumentStatus is supported by RLS
-    const isDocumentStatusImplemented = false
-
     // check specifically a verdict that is delivered and service status hasn't been updated
-    if (
-      isDocumentStatusImplemented &&
-      verdict.externalPoliceDocumentId &&
-      !verdict.serviceStatus
-    ) {
+    if (verdict.externalPoliceDocumentId && !verdict.serviceStatus) {
       const verdictInfo = await this.policeService.getVerdictDocumentStatus(
         verdict.externalPoliceDocumentId,
         user,
       )
 
       if (isVerdictInfoChanged(verdictInfo, verdict)) {
-        return this.update(verdict, verdictInfo)
+        return this.updateVerdict(verdict, verdictInfo)
       }
     }
     return verdict
