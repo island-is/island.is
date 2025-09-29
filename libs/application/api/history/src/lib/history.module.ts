7c5d3654251e182db0605ddfae3e908c33780d9f
jonarnarbriem <jon.arnar.briem@origo.is>
2025-09-29 10:58:45 +0000

    adding config imports

--- libs/application/api/history/src/lib/history.module.ts
+++ libs/application/api/history/src/lib/history.module.ts
@@ -5,12 +5,22 @@ import { History } from './history.model'
 import { HistoryBuilder } from './historyBuilder'
 import { CmsTranslationsModule } from '@island.is/cms-translations'
 import { IdentityClientModule } from '@island.is/clients/identity'
+import { NationalRegistryV3ClientConfig } from '@island.is/clients/national-registry-v3'
+import { CompanyRegistryConfig } from '@island.is/clients/rsk/company-registry'
+import { ConfigModule } from '@island.is/nest/config'
 
 @Module({
   imports: [
     SequelizeModule.forFeature([History]),
     CmsTranslationsModule,
     IdentityClientModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        NationalRegistryV3ClientConfig,
+        CompanyRegistryConfig,
+      ],
+    }),
   ],
   providers: [HistoryService, HistoryBuilder],
   exports: [HistoryService, HistoryBuilder],
