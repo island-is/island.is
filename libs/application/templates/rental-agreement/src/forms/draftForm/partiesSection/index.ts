5ad8ba665dde55ea750fed3ae3b34bb79a08b27b
Jónas G. Sigurðsson <jonas@norda.is>
2025-09-29 13:07:35 +0000

    revert(hms-ra): seperate table repeater (#20196)
    * Revert "feat(application-system): Replace representative table with checkbox (#20037)"
    
    This reverts commit 373dbfa8d0675048d3d61d9ea1590450ec2585ed.
    
    * feat: hid representatives behind a checkbox
    
    * feat: make representative into fields behind a checkbox
    
    * fix: codeRabbit comment
    
    * Update libs/application/templates/rental-agreement/src/forms/draftForm/partiesSection/index.ts
    
    * Update libs/application/templates/rental-agreement/src/forms/draftForm/partiesSection/index.ts

--- libs/application/templates/rental-agreement/src/forms/draftForm/partiesSection/index.ts
+++ libs/application/templates/rental-agreement/src/forms/draftForm/partiesSection/index.ts
@@ -1,17 +1,25 @@
 import {
   buildAlertMessageField,
+  buildCheckboxField,
   buildMultiField,
   buildSection,
   buildTableRepeaterField,
+  buildNationalIdWithNameField,
+  YES,
+  buildPhoneField,
+  buildTextField,
+  buildDescriptionField,
 } from '@island.is/application/core'
 import {
   applicantTableConfig,
-  landlordTableConfig,
   applicantTableFields,
   landLordInfoTableFields,
 } from '../../../utils/utils'
 import { Routes } from '../../../utils/enums'
-import { shouldShowLandlordAlert } from '../../../utils/conditions'
+import {
+  shouldShowLandlordAlert,
+  shouldShowRepresentativeTable,
+} from '../../../utils/conditions'
 import * as m from '../../../lib/messages'
 
 export const partiesSection = buildSection({
@@ -37,7 +45,49 @@ export const partiesSection = buildSection({
           marginTop: 1,
           maxRows: 10,
           fields: landLordInfoTableFields,
-          table: landlordTableConfig,
+          table: applicantTableConfig,
+        }),
+        buildCheckboxField({
+          id: 'parties.landlordInfo.shouldShowRepresentativeTable',
+          marginTop: 6,
+          large: false,
+          backgroundColor: 'white',
+          clearOnChange: ['parties.landlordInfo.representativeTable.0'],
+          options: [
+            {
+              label:
+                m.landlordAndTenantDetails.representativeTableCheckboxLabel,
+              value: YES,
+            },
+          ],
+        }),
+        buildDescriptionField({
+          condition: shouldShowRepresentativeTable,
+          id: 'parties.landlordInfo.representativeTableDesctiption',
+          description:
+            m.landlordAndTenantDetails.representativeTableDescription,
+          marginBottom: 2,
+        }),
+        buildNationalIdWithNameField({
+          condition: shouldShowRepresentativeTable,
+          id: 'parties.landlordInfo.representativeTable.0.nationalIdWithName',
+          marginBottom: 2,
+        }),
+        buildPhoneField({
+          condition: shouldShowRepresentativeTable,
+          id: 'parties.landlordInfo.representativeTable.0.phone',
+          enableCountrySelector: true,
+          required: true,
+          title: 'Símanúmer',
+          width: 'half',
+        }),
+        buildTextField({
+          condition: shouldShowRepresentativeTable,
+          id: 'parties.landlordInfo.representativeTable.0.email',
+          variant: 'email',
+          title: 'Netfang',
+          width: 'half',
+          required: true,
         }),
         buildTableRepeaterField({
           id: 'parties.tenantInfo.table',
