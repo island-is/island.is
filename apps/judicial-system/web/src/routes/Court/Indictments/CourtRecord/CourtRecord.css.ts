94b04f5752d459b8eabe412bc75c36e4f1dd315b
Ívar Oddsson <ivaroddsson@gmail.com>
2025-09-30 13:19:10 +0000

    fix(j-s): Court documents count (#20243)
    * Update state on add document
    
    * Show correct nr of court documents filed in each session
    
    * Ensure correct document order
    
    * Fix lint
    
    * Update state when a file if filed
    
    * Refactor
    
    * Refactor
    
    * Simplify state
    
    * Simplify code
    
    * Remove duplicate field from gql file
    
    * Remove duplicate field from gql file
    
    * Remove duplicate field from gql file
    
    * Cleanup

--- apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.css.ts
+++ apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.css.ts
@@ -38,14 +38,3 @@ export const twoColGrid = style({
     },
   },
 })
-
-export const reorderGroup = style({
-  marginTop: theme.spacing[2],
-  gap: theme.spacing[2],
-
-  selectors: {
-    '&:not(:has([data-reorder-item]))': {
-      marginTop: 0,
-    },
-  },
-})
