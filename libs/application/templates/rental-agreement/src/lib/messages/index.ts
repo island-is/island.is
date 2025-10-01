cd4422657437cf8e74788310f0b0360667c2347e
Jonni <jonas@norda.is>
2025-10-01 12:51:04 +0000

    chore: more message organization

--- libs/application/templates/rental-agreement/src/lib/messages/index.ts
+++ libs/application/templates/rental-agreement/src/lib/messages/index.ts
@@ -1,27 +1,28 @@
 // Prerequisites form
 export * from './prerequisitesFormMessages/prerequisitesMessages'
-// Parties section
+
+// Draft form - Parties section
 export * from './draftFormMessages/partiesMessages/personalInformationMessages'
 export * from './draftFormMessages/partiesMessages/assignApplicantPartyMessages'
 export * from './draftFormMessages/partiesMessages/partiesMessages'
-// Rental property section
-export * from './draftFormMessages/rentalPropertyMessages/housingConditionMessages'
-export * from './draftFormMessages/rentalPropertyMessages/fireProtectionsMessages'
+// Draft form - Rental property section
 export * from './draftFormMessages/rentalPropertyMessages/propertySearchMessages'
 export * from './draftFormMessages/rentalPropertyMessages/descriptionAndSpecialProvisionsMessages'
-// Rental period section
+export * from './draftFormMessages/rentalPropertyMessages/housingConditionMessages'
+export * from './draftFormMessages/rentalPropertyMessages/fireProtectionsMessages'
+// Draft form - Rental period section
+export * from './draftFormMessages/rentalPeriodMessages/rentalPeriodMessages'
+// Draft form - Costs section
 export * from './draftFormMessages/costsMessages/costsMessages'
 export * from './draftFormMessages/costsMessages/rentalAmountMessages'
-export * from './draftFormMessages/rentalPeriodMessages/rentalPeriodMessages'
 export * from './draftFormMessages/costsMessages/securityDepositMessages'
-// Costs section
-export * from './draftFormMessages/costsMessages/costsMessages'
 export * from './draftFormMessages/costsMessages/otherFeesMessages'
-// Overview section
+// Draft form - Overview section
 export * from './draftFormMessages/overviewMessages/overviewMessages'
+
 // Other sections and forms
-export * from './completedFormMessages/completedMessages'
 export * from './otherMessages/dataSchemaMessages'
+export * from './otherMessages/miscMessages'
 export * from './inReviewFormMessages/inReviewMessages'
 export * from './signingFormMessages/signingMessages'
-export * from './otherMessages/miscMessages'
+export * from './completedFormMessages/completedMessages'
