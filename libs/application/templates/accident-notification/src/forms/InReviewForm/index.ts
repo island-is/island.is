15e8437c96697324bf8dbbaa4375133c9876cafc
Jonni <jonas@norda.is>
2025-10-02 11:31:31 +0000

    feat: iceland healt logo

--- libs/application/templates/accident-notification/src/forms/InReviewForm/index.ts
+++ libs/application/templates/accident-notification/src/forms/InReviewForm/index.ts
@@ -1,15 +1,15 @@
 import { buildForm } from '@island.is/application/core'
 import { Form } from '@island.is/application/types'
-import Logo from '../../assets/Logo'
 import { inReview } from '../../lib/messages'
 import { addAttachmentsSection } from './addAttachmentsSection'
 import { applicationStatusSection } from './applicationStatusSection'
 import { inReviewOverviewSection } from './inReviewOverviewSection'
+import { IcelandHealthLogo } from '@island.is/application/assets/institution-logos'
 
 export const ApplicantReview: Form = buildForm({
   id: 'applicantReviewForm',
   title: inReview.general.formTitle,
-  logo: Logo,
+  logo: IcelandHealthLogo,
   children: [
     applicationStatusSection(),
     addAttachmentsSection(),
@@ -20,7 +20,7 @@ export const ApplicantReview: Form = buildForm({
 export const AssigneeReview: Form = buildForm({
   id: 'assigneeReviewForm',
   title: inReview.general.formTitle,
-  logo: Logo,
+  logo: IcelandHealthLogo,
   children: [
     applicationStatusSection(true),
     addAttachmentsSection(true),
