6d91f5a160558daec42ed7fd83faec5078f88052
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-10-06 13:42:15 +0000

    fix(j-s): Focus when new court session and added + pdf (#20295)
    * fix(j-s): add the court record button to the court sessions page
    
    * fix(j-s): court officials dropdown
    
    * fix(j-s): temp
    
    * fix(j-s): scroll to top
    
    * fix(j-s): space in court record when there is no confirmation

--- apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.tsx
+++ apps/judicial-system/web/src/routes/Court/Indictments/CourtRecord/CourtRecord.tsx
@@ -56,11 +56,12 @@ const CourtRecord: FC = () => {
   }
 
   useEffect(() => {
-    setExpandedIndex(
-      workingCase.courtSessions?.length
-        ? workingCase.courtSessions.length - 1
-        : 0,
-    )
+    if (
+      workingCase.courtSessions?.length &&
+      workingCase.courtSessions?.length >= 0
+    ) {
+      setExpandedIndex(workingCase.courtSessions.length - 1)
+    }
   }, [workingCase.courtSessions?.length])
 
   return (
@@ -82,9 +83,9 @@ const CourtRecord: FC = () => {
               index={index}
               courtSession={courtSession}
               isExpanded={expandedIndex === index}
-              onToggle={() =>
+              onToggle={() => {
                 setExpandedIndex(index === expandedIndex ? -1 : index)
-              }
+              }}
               onConfirmClick={() =>
                 handleConfirmClick(courtSession.id, courtSession.isConfirmed)
               }
