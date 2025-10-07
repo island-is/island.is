93c01c76790e497a3f530bd5a1af6507618cc0f3
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-10-07 10:10:41 +0000

    fix(j-s): fix space in file (#20317)
    * fix(j-s): space
    
    * fix(j-s): test
    
    * fix(j-s): space

--- apps/judicial-system/backend/src/app/formatters/indictmentCourtRecordPdf.ts
+++ apps/judicial-system/backend/src/app/formatters/indictmentCourtRecordPdf.ts
@@ -48,7 +48,7 @@ export const createIndictmentCourtRecordPdf = (
     addIndictmentCourtRecordConfirmation(doc, confirmation)
   }
 
-  addEmptyLines(doc, confirmation ? 11 : 9, doc.page.margins.left)
+  addEmptyLines(doc, confirmation ? 11 : 6, doc.page.margins.left)
   setLineGap(doc, 2)
   addLargeHeading(doc, theCase.court?.name ?? 'Héraðsdómur', 'Times-Roman')
   addMediumHeading(doc, 'Þingbók')
