54d4142d11eedcc55671e2e480d1a2e2822dc69f
Ívar Oddsson <ivaroddsson@gmail.com>
2025-10-08 12:39:18 +0000

    Fix padding

--- apps/judicial-system/web/src/components/Modals/Modal/Modal.tsx
+++ apps/judicial-system/web/src/components/Modals/Modal/Modal.tsx
@@ -124,7 +124,7 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({
             </Text>
           </Box>
           {text && (
-            <Box marginBottom={6} className={styles.breakSpaces}>
+            <Box marginBottom={4} className={styles.breakSpaces}>
               {
                 // Check if text is a string or Element
                 isValidElement(text) ? text : <Text>{text}</Text>
@@ -138,6 +138,7 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({
             justifyContent="spaceBetween"
             columnGap={2}
             paddingBottom={6}
+            paddingTop={2}
             background="white"
             position="sticky"
             bottom={0}
