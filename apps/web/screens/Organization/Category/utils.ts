ba6be38ccddf6e4fe34f43cd217691755cf6408d
Rúnar Vestmann <runar@stefna.is>
2025-10-08 13:03:22 +0000

    Delete unused file

--- apps/web/screens/Organization/Category/utils.ts
+++
@@ -1,8 +0,0 @@
-import type { OrganizationPage } from '@island.is/web/graphql/schema'
-
-export const getCategoryTitle = (
-  organizationPage: OrganizationPage,
-  categorySlug: string,
-) => {
-  return ''
-}
