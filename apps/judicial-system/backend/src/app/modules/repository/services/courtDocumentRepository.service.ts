49d16868d6c8aec5515e7331b2ab32825b0c9aff
Ívar Oddsson <ivaroddsson@gmail.com>
2025-10-10 10:52:53 +0000

    Remove unused code

--- apps/judicial-system/backend/src/app/modules/repository/services/courtDocumentRepository.service.ts
+++ apps/judicial-system/backend/src/app/modules/repository/services/courtDocumentRepository.service.ts
@@ -475,43 +475,6 @@ export class CourtDocumentRepositoryService {
     return nextOrder
   }
 
-  async deleteDocumentsInSession(
-    caseId: string,
-    courtSessionId: string,
-    transaction?: Transaction,
-  ) {
-    const filedDocumentsInSession = await this.getFiledDocumentsInSession(
-      caseId,
-      courtSessionId,
-      transaction,
-    )
-
-    for (const filedDocument of filedDocumentsInSession) {
-      await this.delete(caseId, courtSessionId, filedDocument.id, {
-        transaction,
-      })
-    }
-  }
-
-  private async getFiledDocumentsInSession(
-    caseId: string,
-    courtSessionId: string,
-    transaction?: Transaction,
-  ) {
-    const filedDocuments = await this.courtDocumentModel.findAll({
-      where: { caseId, courtSessionId },
-      transaction,
-    })
-
-    if (filedDocuments.length === 0) {
-      this.logger.info(
-        `No court documents found in court session ${courtSessionId} of case ${caseId}`,
-      )
-    }
-
-    return filedDocuments
-  }
-
   private async deleteFromDatabase(
     caseId: string,
     courtSessionId: string,
