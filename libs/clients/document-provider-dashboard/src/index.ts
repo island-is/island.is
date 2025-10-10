8fce22613b4245e258e5beac942e0839001aa961
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2025-10-10 13:50:35 +0000

    cleanup

--- libs/clients/document-provider-dashboard/src/index.ts
+++ libs/clients/document-provider-dashboard/src/index.ts
@@ -2,13 +2,3 @@ export * from './lib/documentProviderDashboardClient.module'
 export * from './lib/documentProviderDashboardClient.config'
 
 export { StatisticsApi } from '../gen/fetch'
-
-/*export * from '../gen/fetch/models'
-
-export {
-  ApiV1StatisticsNationalIdCategoriesGetRequest,
-  ApiV1StatisticsNationalIdGetRequest,
-  ApiV1StatisticsNationalIdProvidersGetRequest,
-  ApiV1StatisticsNationalIdProvidersProviderIdBreakdownGetRequest,
-} from '../gen/fetch/apis'
-*/
