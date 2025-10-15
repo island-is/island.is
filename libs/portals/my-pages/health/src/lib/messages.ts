8b15dc945e1cc9f4aad05af09fdec798f4f57d88
disaerna <disa@hugsmidjan.is>
2025-10-15 10:57:04 +0000

    fix: small ui tweaks

--- libs/portals/my-pages/health/src/lib/messages.ts
+++ libs/portals/my-pages/health/src/lib/messages.ts
@@ -1421,6 +1421,10 @@ export const messages = defineMessages({
     defaultMessage: 'Tegund',
     id: 'sp.health:type',
   },
+  therapyType: {
+    defaultMessage: 'Tegund þjálfunar',
+    id: 'sp.health:therapy-type',
+  },
   typeofService: {
     defaultMessage: 'Tegund þjónustu',
     id: 'sp.health:typeof-service',
