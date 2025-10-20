afca2bc8a5fedf482665fddc927676b9669a47ac
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-10-20 14:20:22 +0000

    feat(j-s): Court Record Flow (#20432)
    * Adds court record screen to indictment flow
    
    * Adds service certificate to initial court session
    
    * Fixes start date and end date autofills
    
    * Fixes possible court document duplication
    
    * Fixes guards and date format
    
    * Adds backdoor to activate court record automation
    
    * Adjusts info box size
    
    * Fixes expanded court session condition
    
    * Fixes typo
    
    * Impreves end date initialization
    
    * Updates unit tests
    
    * Removes only
    
    * Removes console log

--- apps/judicial-system/backend/src/app/modules/case/internalCase.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/internalCase.service.ts
@@ -36,7 +36,6 @@ import {
   EventType,
   getIndictmentAppealDeadlineDate,
   hasDatePassed,
-  hasGeneratedCourtRecordPdf,
   isIndictmentCase,
   isProsecutionUser,
   isRequestCase,
@@ -1210,14 +1209,7 @@ export class InternalCaseService {
         }) ?? [],
     )
       .then(async (courtDocuments) => {
-        if (
-          hasGeneratedCourtRecordPdf(
-            theCase.state,
-            theCase.indictmentRulingDecision,
-            theCase.courtSessions,
-            user,
-          )
-        ) {
+        if (theCase.withCourtSessions) {
           const pdf = await this.pdfService.getCourtRecordPdfForIndictmentCase(
             theCase,
             user,
