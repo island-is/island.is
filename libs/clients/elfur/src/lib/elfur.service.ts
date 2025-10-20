0f2a6243acc68f4e61d25bb1bdf78b52f986365d
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-10-20 11:29:19 +0000

    chore: remove lints

--- libs/clients/elfur/src/lib/elfur.service.ts
+++ libs/clients/elfur/src/lib/elfur.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import {
   EmployeeBasicResponseDto,
-  OpenInvoiceDetailPaginatedResponseDto,
   OpenInvoicesApi,
   OrganizationEmployeeApi,
   V1OpeninvoicesInvoicesGetRequest,
