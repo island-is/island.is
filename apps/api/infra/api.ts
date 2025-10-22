0029e0a1d3ddd24ced7c97d9e54949eaf72b6c1c
Rúnar Vestmann <runar@stefna.is>
2025-10-22 08:38:34 +0000

    Change national id and move it to param store

--- apps/api/infra/api.ts
+++ apps/api/infra/api.ts
@@ -453,6 +453,8 @@ export const serviceSetup = (services: {
         '/k8s/application-system-api/HMS_CONTRACTS_AUTH_CLIENT_SECRET',
       LANDSPITALI_PAYMENT_NATIONAL_ID_FALLBACK:
         '/k8s/api/LANDSPITALI_PAYMENT_NATIONAL_ID_FALLBACK',
+      LANDSPITALI_PAYMENT_ORGANISATION_ID:
+        '/k8s/api/LANDSPITALI_PAYMENT_ORGANISATION_ID',
     })
     .xroad(
       AdrAndMachine,
