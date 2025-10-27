1f18f89aa96c1e510db609ae859acdaee630d2b1
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-10-27 10:19:01 +0000

    feat: add mocks

--- libs/api/domains/icelandic-government-institutions/src/lib/icelandicGovernmentInstitutions.module.ts
+++ libs/api/domains/icelandic-government-institutions/src/lib/icelandicGovernmentInstitutions.module.ts
@@ -1,10 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common'
 import { ElfurClientModule } from '@island.is/clients/elfur'
 import { EmployeesResolver } from './resolvers/employees.resolver'
-import { EmployeesService } from './services/employees.service'
+import { EmployeesService } from './services/employees/employees.service'
 import { MockInvoicesService } from './services/invoices/invoices.service.mock'
 import { InvoicesService } from './services/invoices/invoices.service'
 import { InvoicesResolver } from './resolvers/invoices.resolver'
+import { MockEmployeesService } from './services/employees/employees.service.mock'
 
 export interface IcelandicGovernmentInstitutionsModuleConfig {
   useMocks: boolean
@@ -23,7 +24,10 @@ export class IcelandicGovernmentInstitutionsModule {
           provide: 'IInvoicesService',
           useClass: options.useMocks ? MockInvoicesService : InvoicesService,
         },
-        EmployeesService,
+        {
+          provide: 'IEmployeesService',
+          useClass: options.useMocks ? MockEmployeesService : EmployeesService,
+        },
         EmployeesResolver,
         InvoicesResolver,
       ],
---
+++ libs/api/domains/icelandic-government-institutions/src/lib/services/employees/employees.service.mock.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { type IEmployeesService } from './employees.service.interface'
+import { type Logger, LOGGER_PROVIDER } from '@island.is/logging'
+import { MOCK_EMPLOYEES } from '../../mocks/EMPLOYEES'
+import { EmployeeList } from '../../models/employeeList.model'
+
+@Injectable()
+export class MockEmployeesService implements IEmployeesService {
+  constructor(@Inject(LOGGER_PROVIDER) private readonly logger: Logger) {
+    this.logger.info('Using EmployeesServiceMock')
+  }
+  async getEmployees(): Promise<EmployeeList> {
+    return MOCK_EMPLOYEES
+  }
+
+}
