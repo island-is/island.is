8b8437ec1e8ad622f66b08c586799f6947c1bc1a
unakb <una@kolibri.is>
2025-10-28 11:50:37 +0000

    feat(j-s): Added delivery method to update document endpoint (#20513)
    * feat(j-s): Added delivery method to update document endpoint
    
    * Update verdict.ts
    
    * fix(j-s): Use a const for legal paper delivery method
    
    * Update police.service.ts
    
    * fix: Ruling text sent to police
    
    * fix(j-s): fix ruling mapping in police service (#20516)
    
    fix(j-s): ruling
    
    ---------
    
    Co-authored-by: Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>

--- apps/judicial-system/xrd-api/src/app/dto/policeDocument.dto.ts
+++ apps/judicial-system/xrd-api/src/app/dto/policeDocument.dto.ts
@@ -85,6 +85,11 @@ export class UpdatePoliceDocumentDeliveryDto {
   deliveredToLawyer?: boolean
 
   @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ type: String, required: false })
+  deliveryMethod?: string
+
+  @IsOptional()
   @IsArray()
   @ApiPropertyOptional({ type: () => Supplement, isArray: true })
   supplements?: Supplement[]
