412cdaa6cd1e91d78d5c04f700a5627e981b3c23
disaerna <disa@hugsmidjan.is>
2025-11-02 22:23:35 +0000

    fix: test failing

--- libs/island-ui/core/src/lib/Select/Components/ScrollToSelectedMenuList.tsx
+++ libs/island-ui/core/src/lib/Select/Components/ScrollToSelectedMenuList.tsx
@@ -1,12 +1,12 @@
 // ScrollToSelectedMenuList.tsx
 import React, { useEffect, useRef } from 'react'
-import { components } from 'react-select'
+import { components, MenuListProps } from 'react-select'
 
-export const ScrollToSelectedMenuList = (props: any) => {
+export const ScrollToSelectedMenuList = (props: MenuListProps) => {
   const selectedRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (selectedRef.current) {
+    if (selectedRef?.current?.scrollIntoView) {
       selectedRef.current.scrollIntoView({
         block: 'nearest',
         inline: 'center',
@@ -17,12 +17,13 @@ export const ScrollToSelectedMenuList = (props: any) => {
 
   return (
     <components.MenuList {...props}>
+      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
       {React.Children.map(props.children, (child: any) => {
         if (!child || !child.props) return child
 
         const isSelected =
           child.props.isSelected ||
-          child.props.data?.value === props.selectProps?.value?.value
+          child.props.data?.value === props.selectProps?.value
 
         return React.cloneElement(child, {
           innerRef: isSelected ? selectedRef : undefined,
