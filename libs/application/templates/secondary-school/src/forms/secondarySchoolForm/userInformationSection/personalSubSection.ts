f5b26d5e12351c88c7600edb0170017bc6bd62ea
jonarnarbriem <jon.arnar.briem@origo.is>
2025-11-03 15:08:26 +0000

    adding comment

--- libs/application/templates/secondary-school/src/forms/secondarySchoolForm/userInformationSection/personalSubSection.ts
+++ libs/application/templates/secondary-school/src/forms/secondarySchoolForm/userInformationSection/personalSubSection.ts
@@ -93,6 +93,12 @@ export const personalSubSection = buildSubSection({
           ],
         }),
 
+        /* Commenting out the code below as it is in conflict with current data being sent from MMS.
+        * This will break some existing handling of freshman vs general applicant but as there will be no freshmen
+        * applying until the next application period (February) this will work as a fix for now.
+         */
+
+
         // Application type (Fresman vs General)
         // buildDescriptionField({
         //   id: 'applicationTypeInfo.subtitle',
