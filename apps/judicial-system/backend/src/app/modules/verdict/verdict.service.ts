9e9ce6a82a7d8fd6e6007785c808282cef787460
unakb <una@kolibri.is>
2025-11-03 15:54:52 +0000

    fix(j-s): Verdict flags and order by date  (#20558)
    * fix(j-s): Only use verdict info if sent to police
    
    * feat: Add 2 months to order by date
    
    * fix: test
    
    * fix: Remove unused nowfactory

--- apps/judicial-system/backend/src/app/modules/verdict/verdict.service.ts
+++ apps/judicial-system/backend/src/app/modules/verdict/verdict.service.ts
@@ -378,6 +378,10 @@ export class VerdictService {
 
     const documentName = `Dómur í máli ${theCase.courtCaseNumber}`
 
+    const orderByDate = new Date(verdictFile.created)
+    // add two months because we don't want the order by date to be in the past when delivered to the police
+    orderByDate.setMonth(orderByDate.getMonth() + 2)
+
     // deliver the verdict by creating the document at the police
     const createdDocument = await this.policeService.createDocument({
       caseId: theCase.id,
@@ -391,7 +395,7 @@ export class VerdictService {
         },
       ],
       documentDates: [
-        { code: 'ORDER_BY_DATE', value: verdictFile.created },
+        { code: 'ORDER_BY_DATE', value: orderByDate },
         ...(theCase.rulingDate
           ? [{ code: 'RULING_DATE', value: theCase.rulingDate }]
           : []),
