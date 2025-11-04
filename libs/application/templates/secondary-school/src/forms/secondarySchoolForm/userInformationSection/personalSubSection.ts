538cb73444de94f3489a74fbb31d1334ff042a7d
Jón Arnar Briem <jon.arnar.briem@origo.is>
2025-11-04 09:53:16 +0000

    fix(SecondarySchool): Remove isfreshman checkbox (#20560)
    * removed all code related to is freshman checkbox
    
    * fixylinty
    
    * fixylintydos
    
    * chore: format update dirty file
    
    * readded code as comment
    added hidden input to set default value for ApplicationType
    
    * readded code as comment
    added hidden input to set default value for ApplicationType
    
    * chore: format update dirty file
    
    * re-added two hidden fields
    
    * chore: format update dirty file
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/application/templates/secondary-school/src/forms/secondarySchoolForm/userInformationSection/personalSubSection.ts
+++ libs/application/templates/secondary-school/src/forms/secondarySchoolForm/userInformationSection/personalSubSection.ts
@@ -1,25 +1,18 @@
 import {
   buildAlertMessageField,
-  buildCustomField,
   buildDescriptionField,
-  buildHiddenInput,
   buildMultiField,
   buildPhoneField,
-  buildRadioField,
   buildSubSection,
   buildTextField,
   getValueViaPath,
+  buildCustomField,
+  buildHiddenInput,
 } from '@island.is/application/core'
-import { error, userInformation } from '../../../lib/messages'
+import { userInformation } from '../../../lib/messages'
 import { applicantInformationMultiField } from '@island.is/application/ui-forms'
 import { ApplicationType } from '../../../shared'
-import {
-  checkIsActor,
-  checkIsFreshman,
-  getSchoolsData,
-  Routes,
-  Student,
-} from '../../../utils'
+import { checkIsActor, getSchoolsData, Routes } from '../../../utils'
 import { Application, UserProfile } from '@island.is/application/types'
 
 export const personalSubSection = buildSubSection({
@@ -101,66 +94,71 @@ export const personalSubSection = buildSubSection({
         }),
 
         // Application type (Fresman vs General)
-        buildDescriptionField({
-          id: 'applicationTypeInfo.subtitle',
-          condition: (_, externalData) => {
-            const isFreshmanExternalData = getValueViaPath<Student>(
-              externalData,
-              'studentInfo.data',
-            )?.isFreshman
-            return !isFreshmanExternalData
-          },
-          title: userInformation.applicationType.subtitle,
-          titleVariant: 'h5',
-          space: 3,
-        }),
-        buildRadioField({
-          id: 'applicationType.value',
-          condition: (_, externalData) => {
-            const isFreshmanExternalData = getValueViaPath<Student>(
-              externalData,
-              'studentInfo.data',
-            )?.isFreshman
-            return !isFreshmanExternalData
-          },
-          options: [
-            {
-              value: ApplicationType.FRESHMAN,
-              label: userInformation.applicationType.freshmanOptionTitle,
-            },
-            {
-              value: ApplicationType.GENERAL_APPLICATION,
-              label:
-                userInformation.applicationType.generalApplicationOptionTitle,
-            },
-          ],
-          width: 'full',
-        }),
+        // buildDescriptionField({
+        //   id: 'applicationTypeInfo.subtitle',
+        //   condition: (_, externalData) => {
+        //     const isFreshmanExternalData = getValueViaPath<Student>(
+        //       externalData,
+        //       'studentInfo.data',
+        //     )?.isFreshman
+        //     return !isFreshmanExternalData
+        //   },
+        //   title: userInformation.applicationType.subtitle,
+        //   titleVariant: 'h5',
+        //   space: 3,
+        // }),
+        // buildRadioField({
+        //   id: 'applicationType.value',
+        //   condition: (_, externalData) => {
+        //     const isFreshmanExternalData = getValueViaPath<Student>(
+        //       externalData,
+        //       'studentInfo.data',
+        //     )?.isFreshman
+        //     return !isFreshmanExternalData
+        //   },
+        //   options: [
+        //     {
+        //       value: ApplicationType.FRESHMAN,
+        //       label: userInformation.applicationType.freshmanOptionTitle,
+        //     },
+        //     {
+        //       value: ApplicationType.GENERAL_APPLICATION,
+        //       label:
+        //       userInformation.applicationType.generalApplicationOptionTitle,
+        //     },
+        //   ],
+        //   width: 'full',
+        // }),
+
         buildHiddenInput({
           id: 'applicationType.value',
-          condition: (_, externalData) => {
-            const isFreshmanExternalData = getValueViaPath<Student>(
-              externalData,
-              'studentInfo.data',
-            )?.isFreshman
-
-            return !!isFreshmanExternalData
-          },
-          defaultValue: ApplicationType.FRESHMAN,
+          defaultValue: ApplicationType.GENERAL_APPLICATION,
         }),
-        buildAlertMessageField({
-          id: 'applicationTypeValueAlertMessage',
-          alertType: 'warning',
-          message: userInformation.applicationType.alertMessage,
-          condition: (answers, externalData) => {
-            const isFreshmanExternalData = getValueViaPath<Student>(
-              externalData,
-              'studentInfo.data',
-            )?.isFreshman
-            const isFreshmanAnswers = checkIsFreshman(answers)
-            return !isFreshmanExternalData && isFreshmanAnswers
-          },
-        }),
+        // buildHiddenInput({
+        //   id: 'applicationType.value',
+        //   condition: (_, externalData) => {
+        //     const isFreshmanExternalData = getValueViaPath<Student>(
+        //       externalData,
+        //       'studentInfo.data',
+        //     )?.isFreshman
+        //
+        //     return !!isFreshmanExternalData
+        //   },
+        //   defaultValue: ApplicationType.FRESHMAN,
+        // }),
+        // buildAlertMessageField({
+        //   id: 'applicationTypeValueAlertMessage',
+        //   alertType: 'warning',
+        //   message: userInformation.applicationType.alertMessage,
+        //   condition: (answers, externalData) => {
+        //     const isFreshmanExternalData = getValueViaPath<Student>(
+        //       externalData,
+        //       'studentInfo.data',
+        //     )?.isFreshman
+        //     const isFreshmanAnswers = checkIsFreshman(answers)
+        //     return !isFreshmanExternalData && isFreshmanAnswers
+        //   },
+        // }),
 
         // Validation for whether there are any schools open for admission depending on the application type selected above
         buildCustomField({
@@ -187,34 +185,34 @@ export const personalSubSection = buildSubSection({
           },
           defaultValue: true,
         }),
-        buildAlertMessageField({
-          id: 'applicationTypeIsOpenForAdmissionAlertMessage',
-          alertType: 'error',
-          title: error.errorNoSchoolOpenForAdmissionTitle,
-          message: error.errorNoSchoolOpenForAdmissionDescription,
-          condition: (answers) => {
-            const applicationType = getValueViaPath<ApplicationType>(
-              answers,
-              'applicationType.value',
-            )
-            if (!applicationType) return false
-
-            let isOpenForAdmission: boolean | undefined
-            if (checkIsFreshman(answers)) {
-              isOpenForAdmission = getValueViaPath<boolean>(
-                answers,
-                'applicationType.isOpenForAdmissionFreshman',
-              )
-            } else {
-              isOpenForAdmission = getValueViaPath<boolean>(
-                answers,
-                'applicationType.isOpenForAdmissionGeneral',
-              )
-            }
-
-            return !isOpenForAdmission
-          },
-        }),
+        // buildAlertMessageField({
+        //   id: 'applicationTypeIsOpenForAdmissionAlertMessage',
+        //   alertType: 'error',
+        //   title: error.errorNoSchoolOpenForAdmissionTitle,
+        //   message: error.errorNoSchoolOpenForAdmissionDescription,
+        //   condition: (answers) => {
+        //     const applicationType = getValueViaPath<ApplicationType>(
+        //       answers,
+        //       'applicationType.value',
+        //     )
+        //     if (!applicationType) return false
+        //
+        //     let isOpenForAdmission: boolean | undefined
+        //     if (checkIsFreshman(answers)) {
+        //       isOpenForAdmission = getValueViaPath<boolean>(
+        //         answers,
+        //         'applicationType.isOpenForAdmissionFreshman',
+        //       )
+        //     } else {
+        //       isOpenForAdmission = getValueViaPath<boolean>(
+        //         answers,
+        //         'applicationType.isOpenForAdmissionGeneral',
+        //       )
+        //     }
+        //
+        //     return !isOpenForAdmission
+        //   },
+        // }),
       ],
     }),
   ],
