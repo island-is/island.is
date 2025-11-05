21fad2c6020a8d3c55cf0a0837f0a14ac63a4f43
Baldur Óli <ballioli1@gmail.com>
2025-11-05 10:37:02 +0100

    fix(application-system): Hotfix/virknistyrkur bank validation + date type fix (#20572)
    * Graphql fix
    
    * Fix
    
    * lint
    
    * Fixing date error

--- libs/application/templates/activation-allowance/src/fields/PaymentInformationValidation/index.tsx
+++ libs/application/templates/activation-allowance/src/fields/PaymentInformationValidation/index.tsx
@@ -47,7 +47,7 @@ export const PaymentInformationValidation: FC<
       )
     const bankNumberValidity = /^\d{4}$/.test(paymentInfo?.bankNumber || '')
     const ledger = /^\d{2}$/.test(paymentInfo?.ledger || '')
-    const accountNumber = /^\d{4,6}$/.test(paymentInfo?.accountNumber || '')
+    const accountNumber = /^\d{6}$/.test(paymentInfo?.accountNumber || '')
 
     // Set the errors
     if (!bankNumberValidity || !ledger || !accountNumber) {
@@ -93,16 +93,18 @@ export const PaymentInformationValidation: FC<
   return (
     <Box>
       {invalidError && (
-        <AlertMessage
-          title={formatMessage(paymentErrors.invalidValue)}
-          message={formatMessage(paymentErrors.paymentInfoValueErrorsMessage)}
-          type="warning"
-        />
+        <Box marginTop={2}>
+          <AlertMessage
+            title={formatMessage(paymentErrors.invalidValue)}
+            message={formatMessage(paymentErrors.paymentInfoValueErrorsMessage)}
+            type="warning"
+          />
+        </Box>
       )}
       {errors &&
         errors.length > 0 &&
         errors.map((val, index) => (
-          <Box key={index} marginBottom={2}>
+          <Box key={index} marginTop={2}>
             <AlertMessage
               title={formatMessage(paymentErrors.invalidValue)}
               message={formatMessage(val)}
