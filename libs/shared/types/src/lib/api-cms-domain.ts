237dc181ae3c789ae21c5dc51f0ba7efe6c9f76a
Rúnar Vestmann <runar@stefna.is>
2025-11-06 15:19:25 +0000

    feat(contentful-apps): Sitemap category now has short title (#20524)
    * Add short label for category
    
    * Keep status for category on update

--- libs/shared/types/src/lib/api-cms-domain.ts
+++ libs/shared/types/src/lib/api-cms-domain.ts
@@ -49,6 +49,8 @@ export type SitemapTreeNode = SitemapTree &
         type: SitemapTreeNodeType.CATEGORY
         label: string
         labelEN?: string
+        shortLabel?: string
+        shortLabelEN?: string
         slug: string
         slugEN?: string
         description: string
