1124499bab54b64aed9739e30a1cb72be0198795
Thórhildur Thorleiksdóttir <thorhildur@kolibri.is>
2025-11-07 08:46:23 +0000

    feat(j-s): Court record with merged case (P3) - Update court record pdf with merged cases (#20570)
    * feat(j-s): add merged document order field in court session documents
    
    * fix(j-s): temp
    
    * fix(j-s): migration
    
    * fix(j-s): get relevant court session ids
    
    * fix(j-s): temp
    
    * fix(j-s): add mergedFileDocuments to courtSession
    
    * fix(j-s): support updating order for both filed documents and merged documents
    
    * fix(j-s): rename, create filed document, delete filed document
    
    * fix(j-s): remove input box
    
    * fix(j-s): remove console logs
    
    * fix(j-s): prefix on overview when merged files are included in court session which has not
    been approved
    
    * fix(j-s): state and server updates when we have merged court docs from more than 1 case in a single court session
    
    * fix(j-s): component key
    
    * chore: format update dirty file
    
    * chore(j-s): add new court session string table
    
    * fix(j-s): merged documents in a middle court session
    
    * chore(j-s): create court session merged case entries components
    
    * chore(j-s): enable creating and updating text
    
    * fix(j-s): providers in test
    
    * fix(j-s): small fixes
    
    * fix(j-s): add merged case booking on merge
    
    * chore(j-s): initialise merged courtSessionString when a merged case is completed
    
    * fix(j-s): small fixes
    :
    
    * chore(j-s): update indictment court record pdf
    
    * fix(j-s): pass transaction to court record string updates
    
    * fix(j-s): nr of files in prev court session
    
    * fix(j-s): spelling fix
    
    * fix(j-s): small tweaks
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- apps/judicial-system/backend/src/app/formatters/indictmentCourtRecordPdf.ts
+++ apps/judicial-system/backend/src/app/formatters/indictmentCourtRecordPdf.ts
@@ -1,10 +1,14 @@
+import _uniqBy from 'lodash/uniqBy'
 import PDFDocument from 'pdfkit'
 
 import { formatDate } from '@island.is/judicial-system/formatters'
-import { CourtSessionRulingType } from '@island.is/judicial-system/types'
+import {
+  CourtSessionRulingType,
+  CourtSessionStringType,
+} from '@island.is/judicial-system/types'
 
 import { nowFactory } from '../factories'
-import { Case } from '../modules/repository'
+import { Case, CourtDocument } from '../modules/repository'
 import {
   addCoatOfArms,
   addEmptyLines,
@@ -137,6 +141,48 @@ export const createIndictmentCourtRecordPdf = (
       nrOfFiledDocuments =
         courtSession.filedDocuments[courtSession.filedDocuments.length - 1]
           .documentOrder
+    }
+
+    if (
+      courtSession.mergedFiledDocuments &&
+      courtSession.mergedFiledDocuments.length > 0
+    ) {
+      const uniqueCaseIds = _uniqBy(
+        courtSession.mergedFiledDocuments ?? [],
+        (c: CourtDocument) => c.caseId,
+      ).map((doc) => doc.caseId)
+
+      for (const caseId of uniqueCaseIds) {
+        addEmptyLines(doc, 2)
+        addNormalText(
+          doc,
+          courtSession.courtSessionStrings?.find(
+            (courtSessionString) =>
+              courtSessionString.stringType ===
+                CourtSessionStringType.ENTRIES &&
+              courtSessionString.mergedCaseId === caseId,
+          )?.value ?? 'Engar bókanir um sameinað mál voru skráðar.',
+        )
+
+        const mergedCaseFiledDocuments = courtSession.mergedFiledDocuments
+          .filter((document) => document.caseId === caseId)
+          .sort(
+            (a, b) =>
+              (a.mergedDocumentOrder ?? 0) - (b.mergedDocumentOrder ?? 0),
+          )
+        addEmptyLines(doc, 2)
+        addNormalText(doc, 'Lagt er fram:', 'Times-Bold')
+        addNormalText(doc, 'Nr.', 'Times-Roman')
+        addNumberedList(
+          doc,
+          mergedCaseFiledDocuments.map((d) => d.name.normalize()),
+          mergedCaseFiledDocuments[0].mergedDocumentOrder ?? 1,
+        )
+      }
+      nrOfFiledDocuments =
+        courtSession.mergedFiledDocuments[
+          courtSession.mergedFiledDocuments.length - 1
+        ].mergedDocumentOrder ?? nrOfFiledDocuments
     }
 
     addEmptyLines(doc, 2)
