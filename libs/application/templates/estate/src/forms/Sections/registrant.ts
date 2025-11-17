4d506fcbd5bf3673432b9ba7906f68d82d1e8522
Kristján Albert Loftsson <kal@juni.is>
2025-11-17 15:40:06 +0000

    wip

--- libs/application/templates/estate/src/forms/Sections/registrant.ts
+++ libs/application/templates/estate/src/forms/Sections/registrant.ts
@@ -1,3 +1,4 @@
+import { Application } from '@island.is/api/schema'
 import {
   buildMultiField,
   buildPhoneField,
@@ -5,44 +6,74 @@ import {
   buildSelectField,
   buildTextField,
   buildCustomField,
+  getValueViaPath,
 } from '@island.is/application/core'
 import { ApplicantRelation } from '../../lib/constants'
 import { m } from '../../lib/messages'
+import { format as formatNationalId } from 'kennitala'
 
 export const registrant = buildSection({
   id: 'registrant',
   title: m.registrantTitle,
   children: [
     buildMultiField({
-      id: 'applicant',
+      id: 'registrant',
       title: m.registrantTitle,
       description: m.applicantsInfoSubtitle,
       children: [
-        buildCustomField({
-          id: 'applicant',
-          component: 'LookupPerson',
-        }),
+        buildCustomField(
+          {
+            id: 'registrant',
+            component: 'LookupPerson',
+            defaultValue: ({ externalData }: Application) => ({
+              nationalId: formatNationalId(
+                getValueViaPath(
+                  externalData,
+                  'nationalRegistry.data.nationalId',
+                ) ?? '',
+              ),
+              name:
+                getValueViaPath(
+                  externalData,
+                  'nationalRegistry.data.fullName',
+                ) ?? '',
+            }),
+          },
+          {},
+        ),
         buildTextField({
-          id: 'applicant.address',
+          id: 'registrant.address',
           title: m.address,
           width: 'half',
           required: true,
+          defaultValue: ({ externalData }: Application) =>
+            getValueViaPath(
+              externalData,
+              'nationalRegistry.data.address.streetAddress',
+            ) ?? '',
         }),
         buildPhoneField({
-          id: 'applicant.phone',
+          id: 'registrant.phone',
           title: m.phone,
           width: 'half',
           required: true,
           enableCountrySelector: true,
+          defaultValue: ({ externalData }: Application) =>
+            getValueViaPath(
+              externalData,
+              'userProfile.data.mobilePhoneNumber',
+            ) ?? '',
         }),
         buildTextField({
-          id: 'applicant.email',
+          id: 'registrant.email',
           title: m.email,
           width: 'half',
           required: true,
+          defaultValue: ({ externalData }: Application) =>
+            getValueViaPath(externalData, 'userProfile.data.email') ?? '',
         }),
         buildSelectField({
-          id: 'applicant.relation',
+          id: 'registrant.relation',
           title: m.applicantRelation,
           width: 'half',
           required: true,
