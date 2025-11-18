611713aa6e98e2ccf4b007a5eb1fad12f9e4b136
jonarnarbriem <jon.arnar.briem@origo.is>
2025-11-18 15:31:59 +0000

    fix2

--- libs/api/domains/transport-authority/src/index.ts
+++ libs/api/domains/transport-authority/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/graphql/main.resolver'
 export * from './lib/transportAuthority.module'
 export * from './lib/transportAuthority.service'
-export * from './lib/graphql/models/vehicleValidation.model'
