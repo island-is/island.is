b7ab964582bb30f3165a2db84b9c8d4cecc89a44
OlafurGeorg <olafur.georg.gylfason@advania.is>
2025-11-18 10:27:11 +0000

    fix return

--- libs/api/domains/form-system/src/lib/applications/applications.service.ts
+++ libs/api/domains/form-system/src/lib/applications/applications.service.ts
@@ -137,7 +137,7 @@ export class ApplicationsService {
     ).applicationsControllerSaveScreen(
       input as ApplicationsControllerSaveScreenRequest,
     )
-    return response
+    return response as Screen
   }
 
   async submitSection(auth: User, input: SubmitSectionInput): Promise<void> {
