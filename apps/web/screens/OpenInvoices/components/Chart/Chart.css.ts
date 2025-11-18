5b27c011b7220ae2d63ea1e4842595a80b9aeb72
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-11-18 11:45:59 +0000

    fix: merge issue

--- apps/web/screens/OpenInvoices/components/Chart/Chart.css.ts
+++ apps/web/screens/OpenInvoices/components/Chart/Chart.css.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import { style } from "@vanilla-extract/css";
 
 import { theme } from "@island.is/island-ui/theme";
-=======
-import { style } from '@vanilla-extract/css'
->>>>>>> refs/remotes/origin/feat/add-open-invoices
 
 export const xAxisText = style({
   fill: theme.color.black,
