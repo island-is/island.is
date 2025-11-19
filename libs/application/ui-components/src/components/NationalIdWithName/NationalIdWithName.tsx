800493ef33908ff10f914f12783082063b89d552
Baldur Óli Barkarson <ballioli1@gmail.com>
2025-11-19 08:16:32 +0000

    Merge

--- libs/application/ui-components/src/components/NationalIdWithName/NationalIdWithName.tsx
+++ libs/application/ui-components/src/components/NationalIdWithName/NationalIdWithName.tsx
@@ -127,7 +127,11 @@ export const NationalIdWithName: FC<
   }
 
   const getNameFieldErrorMessage = () => {
+<<<<<<< HEAD
     if (!nationalIdField) return
+=======
+    if(!nationalIdInput) return
+>>>>>>> 7c8513165e (Fixing typo)
     if (nationalIdInput.length !== 10) return
 
     const notFoundMessage = formatMessage(
