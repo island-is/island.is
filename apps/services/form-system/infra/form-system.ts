e645c985ea38493f2bace6c728769cfbd9f68b6f
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2025-11-19 13:44:20 +0000

    fix(form-system): fixing failed loading configuration (#20710)
    * fix(aws-s3): remove region and TTL configurations from S3 module
    
    * fix: add S3 region configuration and remove unused AWS S3 module files
    
    * chore: charts update dirty file
    
    * fix(form-system): restore S3 time-to-live configurations
    
    * chore: charts update dirty file
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- apps/services/form-system/infra/form-system.ts
+++ apps/services/form-system/infra/form-system.ts
@@ -19,6 +19,9 @@ export const serviceSetup = (): ServiceBuilder<typeof serviceName> =>
         staging: 'https://identity-server.staging01.devland.is',
         prod: 'https://innskra.island.is',
       },
+      S3_REGION: 'eu-west-1',
+      S3_TIME_TO_LIVE_POST: '15',
+      S3_TIME_TO_LIVE_GET: '5',
       FILE_STORAGE_UPLOAD_BUCKET: {
         dev: 'island-is-dev-upload-api',
         staging: 'island-is-staging-upload-api',
@@ -28,6 +31,15 @@ export const serviceSetup = (): ServiceBuilder<typeof serviceName> =>
         dev: 'island-is-dev-storage-form-system',
         staging: '', // Still need to get buckets created
         prod: '',
+      },
+      REDIS_URL_NODE_01: {
+        dev: JSON.stringify([
+          'localhost:7010',
+          'localhost:7011',
+          'localhost:7012',
+        ]),
+        staging: '/k8s/shared/redis/REDIS_URL_NODE_01',
+        prod: '/k8s/shared/redis/REDIS_URL_NODE_01',
       },
     })
     .secrets({
