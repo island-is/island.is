d93f22bb2781ff1c11b7868facd0ce1909608f4a
krillijuni <kristleifur@juni.is>
2025-11-21 11:02:15 +0000

    Fix environment variables

--- libs/clients/financial-management-authority/src/lib/financialManagementAuthorityClient.config.ts
+++ libs/clients/financial-management-authority/src/lib/financialManagementAuthorityClient.config.ts
@@ -15,16 +15,10 @@ export const FinancialManagementAuthorityClientConfig = defineConfig<
   name: 'FinancialManagementAuthorityClient',
   schema,
   load: (env) => ({
-    basePath: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_BASE_PATH', ''),
-    clientId: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_CLIENT_ID', ''),
-    clientSecret: env.required(
-      'FINANCIAL_MANAGEMENT_AUTHORITY_CLIENT_SECRET',
-      '',
-    ),
-    scope: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_SCOPE', ''),
-    authenticationServer: env.required(
-      'FINANCIAL_MANAGEMENT_AUTHORITY_AUTHENTICATION_SERVER',
-      '',
-    ),
+    basePath: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_BASE_PATH', 'https://fjs-cdn-endpoint-elfur-test-hhesbzhxabbwbqen.a03.azurefd.net'),
+    clientId: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_CLIENT_ID', '@fjs.is/juni-api-elfur'),
+    clientSecret: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_CLIENT_SECRET', ''),
+    scope: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_SCOPE', '@fjs.is/elfur_vacancy_read @fjs.is/elfur_vacancy_write'),
+    authenticationServer: env.required('FINANCIAL_MANAGEMENT_AUTHORITY_AUTHENTICATION_SERVER', 'https://identity-server.staging01.devland.is/'),
   }),
 })
