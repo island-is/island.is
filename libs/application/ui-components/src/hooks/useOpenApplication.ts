bc0d4a29dee99ca7dedafbace69badb3df3c6f1a
Gunnar K Vilbergsson <gunnar@norda.is>
2025-11-24 09:38:32 +0000

    Update local url

--- libs/application/ui-components/src/hooks/useOpenApplication.ts
+++ libs/application/ui-components/src/hooks/useOpenApplication.ts
@@ -8,11 +8,7 @@ export const useOpenApplication = (
 ) => {
   const path = window.location.origin
   const isLocalhost = path.includes('localhost')
-  const localUrl = `${
-    application.applicationPath?.startsWith('umsoknir/')
-      ? 'http://localhost:4242'
-      : 'http://localhost:4201'
-  }/${application.applicationPath}`
+  const localUrl = `http://localhost:4242/${application.applicationPath}`
   const url = isLocalhost ? localUrl : `${path}/${application.applicationPath}`
 
   const openApplication = () => {
