cf53767d92caa1f77ab4c390202be5149f05553f
Þorkell Máni Þorkelsson <wowthorkell@gmail.com>
2025-11-24 09:49:59 +0000

    feat(my-pages-vehicles): Update the access denied bulk mileage string (#20740)
    * feat: init
    
    * fix: update string
    
    * fix: move message
    
    * chore: wrong impot
    
    * chore: format update dirty file
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/portals/my-pages/assets/src/wrappers/BulkMileageWrapper.tsx
+++ libs/portals/my-pages/assets/src/wrappers/BulkMileageWrapper.tsx
@@ -1,6 +1,9 @@
 import { Box } from '@island.is/island-ui/core'
-import { AccessDenied } from '@island.is/portals/core'
+import { useLocale } from '@island.is/localization'
+import { m as coreMessages } from '@island.is/portals/core'
+import { Problem } from '@island.is/react-spa/shared'
 import { useLoaderData } from 'react-router-dom'
+import { vehicleMessage as m } from '../lib/messages'
 
 interface Props {
   children: React.ReactNode
@@ -8,11 +11,19 @@ interface Props {
 
 export const BulkMileageWrapper = ({ children }: Props) => {
   const isAllowedBulkMileageUpload: boolean = useLoaderData() as boolean
+  const { formatMessage } = useLocale()
 
   if (!isAllowedBulkMileageUpload) {
     return (
       <Box paddingY={1}>
-        <AccessDenied />
+        <Problem
+          size="large"
+          noBorder={false}
+          tag={formatMessage(coreMessages.accessDenied)}
+          title={formatMessage(coreMessages.accessDenied)}
+          message={formatMessage(m.notAllowedBulkMileage)}
+          imgSrc="./assets/images/movingTruck.svg"
+        />
       </Box>
     )
   }
