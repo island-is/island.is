f532a19834cf9ec12a10a25549424cd4af12cee2
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-11-27 16:26:47 +0000

    fix(j-s): extra space

--- apps/judicial-system/web/src/components/InfoCard/DefendantInfo/DefendantInfo.strings.ts
+++ apps/judicial-system/web/src/components/InfoCard/DefendantInfo/DefendantInfo.strings.ts
@@ -8,12 +8,12 @@ export const strings = defineMessages({
   },
   appealDateExpired: {
     id: 'judicial.system.core:info_card.defendant_info.appeal_date_expired_v2',
-    defaultMessage: '{deadlineType}  dómfellda var til {appealExpirationDate}',
+    defaultMessage: '{deadlineType} dómfellda var til {appealExpirationDate}',
     description: 'Notað til að láta vita að frestu í ákæru er útrunninn.',
   },
   appealDateNotBegun: {
     id: 'judicial.system.core:info_card.defendant_info.appeal_date_not_begun_v2',
-    defaultMessage: '{deadlineType}  dómfellda er ekki hafinn',
+    defaultMessage: '{deadlineType} dómfellda er ekki hafinn',
     description: 'Notaður til að láta vita að frestu dómfellda er ekki hafinn.',
   },
   defender: {
