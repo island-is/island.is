c61dd33ff60290c3a4689170b62c4833fa99421e
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-11-28 13:37:53 +0000

    chore: rexport types

--- libs/clients/nvs-permits/src/index.ts
+++ libs/clients/nvs-permits/src/index.ts
@@ -1,4 +1,4 @@
 export { NvsPermitsClientService } from './lib/nvs-permits.service'
 export { NvsPermitsClientModule } from './lib/nvs-permits.module'
 export { NvsPermitsClientConfig } from './lib/nvs-permits.config'
-export { PermitHunting, PermitRanger } from '../gen/fetch'
+export type { PermitHunting, PermitRanger } from '../gen/fetch'
