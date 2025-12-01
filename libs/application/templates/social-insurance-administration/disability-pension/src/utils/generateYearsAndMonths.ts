98cfbe982195e0dafcf15dd624dcf5f106dc96f3
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2025-12-01 10:52:07 +0000

    fix: dates

--- libs/application/templates/social-insurance-administration/disability-pension/src/utils/generateYearsAndMonths.ts
+++ libs/application/templates/social-insurance-administration/disability-pension/src/utils/generateYearsAndMonths.ts
@@ -1,8 +1,8 @@
-export const generatePast24Months = (): Record<number, Array<number>> => {
+export const generateYearsAndMonths = (numberOfMonths = 23): Record<number, Array<number>> => {
   const result: Record<number, Array<number>> = {}
   const currentDate = new Date()
 
-  for (let i = 24; i > 0; i--) {
+  for (let i = numberOfMonths; i > 0; i--) {
     const date = new Date(
       currentDate.getFullYear(),
       currentDate.getMonth() - i,
