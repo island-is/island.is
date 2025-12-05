47deee0b5dc880d7966acd88ccc7f8d6129552f1
disaerna <disa@hugsmidjan.is>
2025-12-05 09:31:51 +0000

    fix: width

--- apps/portals/my-pages/src/components/Layout/SidebarLayout.tsx
+++ apps/portals/my-pages/src/components/Layout/SidebarLayout.tsx
@@ -8,7 +8,6 @@ import {
 
 import * as styles from './SidebarLayout.css'
 import cn from 'classnames'
-import { theme } from '@island.is/island-ui/theme'
 import { useWindowSize } from 'react-use'
 
 interface SidebarLayoutProps {
