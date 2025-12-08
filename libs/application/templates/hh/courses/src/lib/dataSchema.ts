9bcc07e210916088b14edbf4b051ce09b284d908
Rúnar Vestmann <runar@stefna.is>
2025-12-08 13:00:14 +0000

    Required fields must not be empty

--- libs/application/templates/hh/courses/src/lib/dataSchema.ts
+++ libs/application/templates/hh/courses/src/lib/dataSchema.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod'
 
 const nationalIdWithNameSchema = z.object({
-  nationalId: z.string(),
-  name: z.string(),
+  nationalId: z.string().length(10),
+  name: z.string().min(1),
 })
 
 export const dataSchema = z.object({
   participantNationalIdAndName: nationalIdWithNameSchema,
-  payerNationalId: z.string(),
-  payerName: z.string(),
+  payerNationalId: z.string().length(10),
+  payerName: z.string().min(1),
 })
 
 export type ApplicationAnswers = z.TypeOf<typeof dataSchema>
