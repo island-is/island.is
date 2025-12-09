8a51909a6629bb72c64a59bdd9928ed31f100ef9
krillijuni <kristleifur@juni.is>
2025-12-09 13:45:48 +0000

    chore(starfatorg): Revert "chore(starfatorg): Revert "feat(starfatorg): Replace Orri API client with new Elfur client (#20730) (#20876)"
    This reverts commit 9344b76a84f1300f4389ec7a9cb3c64a3247203f.

--- libs/api/domains/icelandic-government-institution-vacancies/src/lib/dto/icelandicGovernmentInstitutionVacancyById.input.ts
+++ libs/api/domains/icelandic-government-institution-vacancies/src/lib/dto/icelandicGovernmentInstitutionVacancyById.input.ts
@@ -1,15 +1,11 @@
-import { VacanciesVacancyIdGetLanguageEnum } from '@island.is/clients/icelandic-government-institution-vacancies'
-import { Field, InputType, registerEnumType } from '@nestjs/graphql'
-
-registerEnumType(VacanciesVacancyIdGetLanguageEnum, {
-  name: 'VacanciesVacancyIdGetLanguageEnum',
-})
+import { Field, InputType } from '@nestjs/graphql'
+import { VacancyLanguageEnum } from '../models/enums'
 
 @InputType()
 export class IcelandicGovernmentInstitutionVacancyByIdInput {
   @Field()
   id!: string
 
-  @Field(() => VacanciesVacancyIdGetLanguageEnum, { nullable: true })
-  language?: VacanciesVacancyIdGetLanguageEnum
+  @Field(() => VacancyLanguageEnum, { nullable: true })
+  language?: VacancyLanguageEnum
 }
