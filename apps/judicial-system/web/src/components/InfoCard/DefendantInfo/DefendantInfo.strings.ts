6d374d096a04845b15d071dd755569f2e15a1bc4
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2025-12-09 09:22:25 +0000

    fix(j-s): spelling

--- apps/judicial-system/web/src/components/InfoCard/DefendantInfo/DefendantInfo.strings.ts
+++ apps/judicial-system/web/src/components/InfoCard/DefendantInfo/DefendantInfo.strings.ts
@@ -9,12 +9,12 @@ export const strings = defineMessages({
   appealDateExpired: {
     id: 'judicial.system.core:info_card.defendant_info.appeal_date_expired_v2',
     defaultMessage: '{deadlineType} dómfellda var til {appealExpirationDate}',
-    description: 'Notað til að láta vita að frestu í ákæru er útrunninn.',
+    description: 'Notað til að láta vita að frestur í ákæru er útrunninn.',
   },
   appealDateNotBegun: {
     id: 'judicial.system.core:info_card.defendant_info.appeal_date_not_begun_v2',
     defaultMessage: '{deadlineType} dómfellda er ekki hafinn',
-    description: 'Notaður til að láta vita að frestu dómfellda er ekki hafinn.',
+    description: 'Notaður til að láta vita að frestur dómfellda er ekki hafinn.',
   },
   defender: {
     id: 'judicial.system.core:info_card.defendant_info.defender',
