e120869379a26e1c5592910dfa4dc3434b9bc4c0
Karen Björg Halldórsdóttir <khalldorsdottir@deloitte.is>
2025-12-09 15:49:02 +0000

    feat(new-primary-school): attachment page (#20872)
    * add attachment page for special school and department
    
    * Apply suggestion from @veronikasif
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    
    * Apply suggestion from @veronikasif
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    
    * fix hasAttachments function
    
    * Apply suggestion from @veronikasif
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    
    * Apply suggestion from @veronikasif
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    
    * Apply suggestion from @veronikasif
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    
    * Apply suggestion from @veronikasif
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    
    * chore: format update dirty file
    
    ---------
    
    Co-authored-by: Verónika Sif Sigurðardóttir <vesigurdardottir@deloitte.is>
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/application/templates/new-primary-school/src/utils/constants.ts
+++ libs/application/templates/new-primary-school/src/utils/constants.ts
@@ -2,6 +2,8 @@ import { DefaultEvents } from '@island.is/application/types'
 
 export const FIRST_GRADE_AGE = 6
 export const TENTH_GRADE_AGE = 16
+export const UPLOAD_ACCEPT = '.pdf, .doc, .docx, .rtf, .jpg, .jpeg, .png'
+export const FILE_SIZE_LIMIT = 5000000 // 5MB
 
 export const NU_UNIT_ID = 'G-2236-A'
 
@@ -106,6 +108,12 @@ export enum ApplicationType {
 export enum CaseWorkerInputTypeEnum {
   CaseManager = 'caseManager',
   SupportManager = 'supportManager',
+}
+
+export enum AttachmentOptions {
+  ATTACHMENTS = 'attachments', // only electronic
+  PHYSICAL = 'physical', // only on paper
+  ATTACHMENTS_AND_PHYSICAL = 'attachmentsAndPhysical', // electronic and on paper
 }
 
 export enum OrganizationSubType {
