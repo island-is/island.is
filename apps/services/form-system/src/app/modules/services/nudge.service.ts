cacc53dfab44a2b005f996b57f52ef4a73d0d342
Ólafur Georg Gylfason <olafur.georg.gylfason@advania.is>
2025-12-12 12:30:01 +0000

    feat(form-system): Register submission urls (#20922)
    * slóðasmiður
    
    * new url button
    
    * small fix
    
    * send to zendesk update
    
    * fix lint
    
    * small fix
    
    * rabbit

--- apps/services/form-system/src/app/modules/services/nudge.service.ts
+++ apps/services/form-system/src/app/modules/services/nudge.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common'
 import { ApplicationDto } from '../applications/models/dto/application.dto'
-import { OrganizationUrl } from '../organizationUrls/models/organizationUrl.model'
 import { InjectModel } from '@nestjs/sequelize'
 import { ApplicationEvent } from '../applications/models/applicationEvent.model'
 import { ApplicationEvents } from '@island.is/form-system/shared'
@@ -13,11 +12,11 @@ export class NudgeService {
   ) {}
   async sendNudge(
     applicationDto: ApplicationDto,
-    url: OrganizationUrl,
+    url: string,
   ): Promise<boolean> {
     let success = true
     try {
-      const response = await fetch(url.url, {
+      const response = await fetch(url, {
         method: 'POST',
         body: JSON.stringify({ applicationId: applicationDto.id }),
         headers: {
--- apps/services/form-system/src/app/modules/services/service.manager.ts
+++ apps/services/form-system/src/app/modules/services/service.manager.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@nestjs/common'
-import { InjectModel } from '@nestjs/sequelize'
-import { FormUrl } from '../formUrls/models/formUrl.model'
-import { OrganizationUrl } from '../organizationUrls/models/organizationUrl.model'
 import { ZendeskService } from './zendesk.service'
 import { NudgeService } from './nudge.service'
-import { UrlMethods, UrlTypes } from '@island.is/form-system/shared'
 import { ApplicationDto } from '../applications/models/dto/application.dto'
 import { ScreenValidationResponse } from '../../dataTypes/validationResponse.model'
 import { ValidationService } from './validation.service'
@@ -13,36 +9,22 @@ import { ScreenDto } from '../screens/models/dto/screen.dto'
 @Injectable()
 export class ServiceManager {
   constructor(
-    @InjectModel(FormUrl)
-    private readonly formUrlModel: typeof FormUrl,
-    @InjectModel(OrganizationUrl)
-    private readonly organizationUrlModel: typeof OrganizationUrl,
     private readonly zendeskService: ZendeskService,
     private readonly nugdeService: NudgeService,
     private readonly validationService: ValidationService,
   ) {}
 
   async send(applicationDto: ApplicationDto): Promise<boolean> {
-    const formUrls = await this.formUrlModel.findAll({
-      where: { formId: applicationDto.formId },
-    })
+    const submitUrl = applicationDto.submissionServiceUrl
 
-    if (formUrls.length === 0) {
-      return false
+    if (!submitUrl) {
+      throw new Error('No submission URL configured for this application')
     }
 
-    const submitUrl = await this.organizationUrlModel.findOne({
-      where: {
-        id: formUrls.map((formUrl) => formUrl.organizationUrlId),
-        type: UrlTypes.SUBMIT,
-        isTest: applicationDto.isTest,
-      },
-    })
-
-    if (submitUrl && submitUrl?.method === UrlMethods.SEND_NUDGE) {
+    if (submitUrl === 'zendesk') {
+      return await this.zendeskService.sendToZendesk(applicationDto)
+    } else if (submitUrl !== 'zendesk') {
       return await this.nugdeService.sendNudge(applicationDto, submitUrl)
-    } else if (submitUrl && submitUrl?.method === UrlMethods.SEND_TO_ZENDESK) {
-      return await this.zendeskService.sendToZendesk(applicationDto, submitUrl)
     }
 
     return false
