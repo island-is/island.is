b8a663267d2b27f57ac033b0e7912648d7249d9b
Kristján Albert <kal@juni.is>
2025-12-16 09:57:05 +0000

    feat(estate): prefill some checkboxes and add a new one for knowledge of will (#20776)
    prefill some checkboxes and add a new one for knowledge of will

--- libs/application/templates/estate/src/forms/Sections/testamentInfo.ts
+++ libs/application/templates/estate/src/forms/Sections/testamentInfo.ts
@@ -8,7 +8,11 @@ import {
 } from '@island.is/application/core'
 import { m } from '../../lib/messages'
 import { JA, YES, NEI, NO, EstateTypes } from '../../lib/constants'
-import { getWillsAndAgreementsDescriptionText } from '../../lib/utils'
+import {
+  getWillsAndAgreementsDescriptionText,
+  getEstateDataFromApplication,
+} from '../../lib/utils'
+import { Application } from '@island.is/application/types'
 
 export const testamentInfo = buildSection({
   id: 'testamentInfo',
@@ -25,6 +29,18 @@ export const testamentInfo = buildSection({
           title: m.doesAgreementExist,
           largeButtons: true,
           width: 'half',
+          defaultValue: (application: Application) => {
+            const marriageSettlement =
+              getEstateDataFromApplication(application)?.estate
+                ?.marriageSettlement
+
+            // Only prefill if API has a value (not null/undefined)
+            if (marriageSettlement === true) return YES
+            if (marriageSettlement === false) return NO
+
+            // If marriageSettlement is null/undefined, user must answer
+            return ''
+          },
           options: [
             { value: YES, label: JA },
             { value: NO, label: NEI },
@@ -36,6 +52,41 @@ export const testamentInfo = buildSection({
           largeButtons: true,
           width: 'half',
           space: 2,
+          defaultValue: (application: Application) => {
+            const districtCommissionerHasWill =
+              getEstateDataFromApplication(application)?.estate
+                ?.districtCommissionerHasWill
+
+            // Only prefill if API has a value (not null/undefined)
+            if (districtCommissionerHasWill === true) return YES
+            if (districtCommissionerHasWill === false) return NO
+
+            // If districtCommissionerHasWill is null/undefined, user must answer
+            return ''
+          },
+          options: [
+            { value: YES, label: JA },
+            { value: NO, label: NEI },
+          ],
+        }),
+        buildRadioField({
+          id: 'estate.testament.knowledgeOfOtherWills',
+          title: m.knowledgeOfOtherWills,
+          largeButtons: true,
+          width: 'half',
+          space: 2,
+          defaultValue: (application: Application) => {
+            const knowledgeOfOtherWills =
+              getEstateDataFromApplication(application)?.estate
+                ?.knowledgeOfOtherWills
+
+            // Only prefill if API has a value (not null/undefined)
+            if (knowledgeOfOtherWills === 'Yes') return YES
+            if (knowledgeOfOtherWills === 'No') return NO
+
+            // If knowledgeOfOtherWills is null/undefined, user must answer
+            return ''
+          },
           options: [
             { value: YES, label: JA },
             { value: NO, label: NEI },
