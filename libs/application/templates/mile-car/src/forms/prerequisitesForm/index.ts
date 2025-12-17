8320dc4a17f64995fdc75b3f7c4ea81497683c5a
berglindoma13 <berglindis@gmail.com>
2025-12-17 13:37:03 +0000

    initalizing application

--- libs/application/templates/mile-car/src/forms/prerequisitesForm/index.ts
+++ libs/application/templates/mile-car/src/forms/prerequisitesForm/index.ts
@@ -1,34 +1,56 @@
 import {
+  buildAlertMessageField,
   buildDataProviderItem,
   buildExternalDataProvider,
   buildForm,
   buildSection,
   buildSubmitField,
-  coreMessages,
+  buildMultiField,
 } from '@island.is/application/core'
-import { DefaultEvents } from '@island.is/application/types'
-import { FormModes, UserProfileApi } from '@island.is/application/types'
+import { DefaultEvents, Form, FormModes } from '@island.is/application/types'
+import { externalData } from '../../lib/messages'
 
-export const Prerequisites = buildForm({
-  id: 'PrerequisitesDraft',
+export const Prerequisites: Form = buildForm({
+  id: 'PrerequisitesForm',
   mode: FormModes.NOT_STARTED,
   renderLastScreenButton: true,
+  renderLastScreenBackButton: true,
   children: [
+    // buildSection({
+    //   id: 'preInformation',
+    //   title: externalData.preInformation.sectionTitle,
+
+    //   children: [
+    //     buildMultiField({
+    //       id: 'preInformation.multifield',
+    //       title: externalData.preInformation.sectionTitle,
+    //       description: externalData.preInformation.description,
+    //       children: [
+    //         buildAlertMessageField({
+    //           id: 'preInformation.alertField.hasValidCard',
+    //           alertType: 'info',
+    //           message: externalData.preInformation.hasValidCardAlert,
+    //           marginTop: 0,
+    //         }),
+    //         buildAlertMessageField({
+    //           id: 'preInformation.alertField.lostOldCard',
+    //           alertType: 'info',
+    //           message: externalData.preInformation.lostOldCardAlert,
+    //           marginTop: 0,
+    //         }),
+    //       ],
+    //     }),
+    //   ],
+    // }),
     buildSection({
-      id: 'conditions',
-      tabTitle: 'Forkröfur',
+      id: 'externalData',
+      title: externalData.dataProvider.sectionTitle,
       children: [
         buildExternalDataProvider({
           id: 'approveExternalData',
-          title: 'External data',
-          dataProviders: [
-            buildDataProviderItem({
-              provider: UserProfileApi,
-              title: 'User profile',
-              subTitle: 'User profile',
-            }),
-            // Add more data providers as needed
-          ],
+          title: externalData.dataProvider.pageTitle,
+          subTitle: externalData.dataProvider.subTitle,
+          checkboxLabel: externalData.dataProvider.checkboxLabel,
           submitField: buildSubmitField({
             id: 'submit',
             placement: 'footer',
@@ -36,11 +58,18 @@ export const Prerequisites = buildForm({
             actions: [
               {
                 event: DefaultEvents.SUBMIT,
-                name: coreMessages.buttonNext,
+                name: externalData.dataProvider.submitButton,
                 type: 'primary',
               },
             ],
           }),
+          dataProviders: [
+            // buildDataProviderItem({
+            //   provider: CurrentVehiclesApi,
+            //   title: externalData.transportAuthority.title,
+            //   subTitle: externalData.transportAuthority.subTitle,
+            // }),
+          ],
         }),
       ],
     }),
