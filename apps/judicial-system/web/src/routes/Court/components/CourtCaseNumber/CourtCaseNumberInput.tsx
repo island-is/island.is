b1b03c48e327b5552a5761c987985ee8e92a7f2d
Ívar Oddsson <ivaroddsson@gmail.com>
2025-12-19 09:41:38 +0000

    fix(j-s): Court case number (#20999)
    * Fix court case number
    
    * Fix court case number

--- apps/judicial-system/web/src/routes/Court/components/CourtCaseNumber/CourtCaseNumberInput.tsx
+++ apps/judicial-system/web/src/routes/Court/components/CourtCaseNumber/CourtCaseNumberInput.tsx
@@ -21,7 +21,7 @@ import * as styles from './CourtCaseNumber.css'
 interface Props {
   workingCase: Case
   setWorkingCase?: Dispatch<SetStateAction<Case>>
-  onCreateCourtCase?: () => void
+  onCreateCourtCase?: () => Promise<string>
   onChange?: (courtCaseNumber: string) => void
 }
 
@@ -45,6 +45,7 @@ const CourtCaseNumberInput: FC<Props> = (props) => {
 
     if (courtCaseNumber !== '') {
       setCourtCaseNumberErrorMessage('')
+      setValue(courtCaseNumber)
       setCreateCourtCaseSuccess(true)
     } else {
       setCourtCaseNumberErrorMessage(
@@ -93,10 +94,6 @@ const CourtCaseNumberInput: FC<Props> = (props) => {
 
   useDebounce(
     () => {
-      if (createCourtCaseSuccess) {
-        setCreateCourtCaseSuccess(false)
-      }
-
       updateCourtCaseNumber(workingCase.id, { courtCaseNumber: value })
     },
     500,
@@ -108,11 +105,18 @@ const CourtCaseNumberInput: FC<Props> = (props) => {
       <div className={styles.createCourtCaseButton}>
         <Button
           size="small"
-          onClick={() =>
-            onCreateCourtCase
-              ? onCreateCourtCase()
-              : handleCreateCourtCase(workingCase.id)
-          }
+          onClick={async () => {
+            if (onCreateCourtCase) {
+              const courtCaseNumber = await onCreateCourtCase()
+
+              if (courtCaseNumber !== '') {
+                setCreateCourtCaseSuccess(true)
+                setValue(courtCaseNumber)
+              }
+            } else {
+              handleCreateCourtCase(workingCase.id)
+            }
+          }}
           loading={isCreatingCourtCase}
           disabled={Boolean(
             (workingCase.state !== CaseState.SUBMITTED &&
@@ -147,6 +151,7 @@ const CourtCaseNumberInput: FC<Props> = (props) => {
           hasError={!isCreatingCourtCase && courtCaseNumberErrorMessage !== ''}
           onChange={(evt) => {
             setCourtCaseNumberErrorMessage('')
+            setCreateCourtCaseSuccess(false)
             setValue(evt.target.value)
             onChange?.(evt.target.value)
 
