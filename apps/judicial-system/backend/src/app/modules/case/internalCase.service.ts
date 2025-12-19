99a085f38c9546f6c7700079e3c89ba1cce82fe0
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-12-19 16:23:33 +0000

    Fixes updates to police cases when an rvg case is being corrected

--- apps/judicial-system/backend/src/app/modules/case/internalCase.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/internalCase.service.ts
@@ -1136,7 +1136,9 @@ export class InternalCaseService {
       user,
       originalAncestor.id,
       theCase.type,
-      theCase.state,
+      theCase.state === CaseState.CORRECTING
+        ? CaseState.COMPLETED
+        : theCase.state,
       theCase.policeCaseNumbers.length > 0 ? theCase.policeCaseNumbers[0] : '',
       theCase.courtCaseNumber ?? '',
       defendantNationalIds && defendantNationalIds[0]
