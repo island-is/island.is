cd747685f92c66494fa90073abaf46b5d4444341
berglindoma13 <berglindis@gmail.com>
2025-12-22 15:15:43 +0000

    remove global config

--- libs/api/domains/mile-car/src/lib/mileCar.module.ts
+++ libs/api/domains/mile-car/src/lib/mileCar.module.ts
@@ -1,21 +1,10 @@
-import { ConfigModule } from '@nestjs/config'
 import { Module } from '@nestjs/common'
 import { MainResolver } from './graphql/main.resolver'
 import { MileCarApi } from './mileCar.service'
-import {
-  VehiclesClientModule,
-  VehiclesClientConfig,
-} from '@island.is/clients/vehicles'
+import { VehiclesClientModule } from '@island.is/clients/vehicles'
 
 @Module({
-  imports: [
-    VehiclesClientModule,
-
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [VehiclesClientConfig],
-    }),
-  ],
+  imports: [VehiclesClientModule],
   providers: [MainResolver, MileCarApi],
   exports: [MileCarApi],
 })
