1cbb6e7ffae1ab81e7ba8e1bb938739773fd3b93
Kristján Albert Loftsson <kal@juni.is>
2025-12-30 11:57:20 +0000

    custom applicant personal information subsection

--- libs/application/templates/hms/rental-agreement/src/forms/draftForm/partiesSection/personalInformationSubsection.ts
+++ libs/application/templates/hms/rental-agreement/src/forms/draftForm/partiesSection/personalInformationSubsection.ts
@@ -1,16 +1,136 @@
-import { buildSubSection } from '@island.is/application/core'
+import {
+  buildMultiField,
+  buildSubSection,
+  buildTextField,
+} from '@island.is/application/core'
+import { applicantInformationMessages } from '@island.is/application/ui-forms'
 import { Routes } from '../../../utils/enums'
 import * as m from '../../../lib/messages'
-import { applicantInformationMultiField } from '@island.is/application/ui-forms'
+
+interface ApplicantInformationInterface {
+  externalData?: {
+    nationalRegistry?: {
+      data?: {
+        fullName?: string
+        nationalId?: string
+        address?: {
+          streetAddress?: string
+          postalCode?: string
+          city?: string
+        }
+      }
+    }
+    identity?: {
+      data?: {
+        name?: string
+        nationalId?: string
+        address?: {
+          streetAddress?: string
+          postalCode?: string
+          city?: string
+        }
+      }
+    }
+    userProfile?: {
+      data?: {
+        email?: string
+        mobilePhoneNumber?: string
+      }
+    }
+  }
+}
 
 export const personalInformationSubsection = buildSubSection({
   id: Routes.PERSONALINFORMATION,
   title: m.personalInformation.title,
   children: [
-    applicantInformationMultiField({
-      applicantInformationDescription:
-        m.partiesDetails.personalInformationDescription,
-      phoneEnableCountrySelector: false,
+    buildMultiField({
+      id: 'applicant',
+      title: m.personalInformation.title,
+      description: m.partiesDetails.personalInformationDescription,
+      children: [
+        buildTextField({
+          id: 'applicant.name',
+          title: applicantInformationMessages.labels.name,
+          width: 'full',
+          backgroundColor: 'white',
+          disabled: true,
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.nationalRegistry?.data?.fullName ??
+            application.externalData?.identity?.data?.name ??
+            '',
+        }),
+        buildTextField({
+          id: 'applicant.nationalId',
+          title: applicantInformationMessages.labels.nationalId,
+          format: '######-####',
+          width: 'half',
+          backgroundColor: 'white',
+          disabled: true,
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.nationalRegistry?.data?.nationalId ??
+            application.externalData?.identity?.data?.nationalId ??
+            '',
+        }),
+        buildTextField({
+          id: 'applicant.address',
+          title: applicantInformationMessages.labels.address,
+          width: 'half',
+          backgroundColor: 'white',
+          disabled: true,
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.nationalRegistry?.data?.address
+              ?.streetAddress ??
+            application.externalData?.identity?.data?.address?.streetAddress ??
+            '',
+        }),
+        buildTextField({
+          id: 'applicant.postalCode',
+          title: applicantInformationMessages.labels.postalCode,
+          width: 'half',
+          format: '###',
+          backgroundColor: 'white',
+          disabled: true,
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.nationalRegistry?.data?.address
+              ?.postalCode ??
+            application.externalData?.identity?.data?.address?.postalCode ??
+            '',
+        }),
+        buildTextField({
+          id: 'applicant.city',
+          title: applicantInformationMessages.labels.city,
+          width: 'half',
+          backgroundColor: 'white',
+          disabled: true,
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.nationalRegistry?.data?.address?.city ??
+            application.externalData?.identity?.data?.address?.city ??
+            '',
+        }),
+        buildTextField({
+          id: 'applicant.email',
+          title: applicantInformationMessages.labels.email,
+          width: 'half',
+          variant: 'email',
+          backgroundColor: 'blue',
+          required: true,
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.userProfile?.data?.email ?? '',
+          maxLength: 100,
+        }),
+        buildTextField({
+          id: 'applicant.phoneNumber',
+          title: applicantInformationMessages.labels.tel,
+          placeholder: '000-0000',
+          format: '###-####',
+          width: 'half',
+          backgroundColor: 'blue',
+          defaultValue: (application: ApplicantInformationInterface) =>
+            application.externalData?.userProfile?.data?.mobilePhoneNumber ??
+            '',
+        }),
+      ],
     }),
   ],
 })
