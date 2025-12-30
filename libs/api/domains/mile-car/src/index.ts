9c48bce1d90ea664e362e1105ff42204c3937a13
berglindoma13 <berglindis@gmail.com>
2025-12-30 12:01:33 +0000

    application moved under transport-authority

--- libs/api/domains/mile-car/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/graphql/main.resolver'
-export * from './lib/mileCar.module'
-export * from './lib/mileCar.service'
--- libs/application/templates/mile-car/src/dataProviders/index.ts
+++
@@ -1,6 +0,0 @@
-import { defineTemplateApi } from '@island.is/application/types'
-
-export const CurrentVehiclesApi = defineTemplateApi({
-  action: 'getCurrentVehicles',
-  externalDataId: 'currentVehicleList',
-})
