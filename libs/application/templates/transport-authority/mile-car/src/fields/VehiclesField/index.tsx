12484cb3175d9f4f3bd7a4c25e8ce4aa672edb34
berglindoma13 <berglindis@gmail.com>
2026-01-05 11:21:46 +0000

    message update and path changes

--- libs/application/templates/transport-authority/mile-car/src/fields/VehiclesField/index.tsx
+++ libs/application/templates/transport-authority/mile-car/src/fields/VehiclesField/index.tsx
@@ -4,12 +4,9 @@ import {
   FieldTypes,
 } from '@island.is/application/types'
 import { Box } from '@island.is/island-ui/core'
-import { FC, useCallback, useEffect } from 'react'
-import { useFormContext } from 'react-hook-form'
+import { FC } from 'react'
 import { CurrentVehiclesAndRecords } from '../../shared'
-import { ApolloQueryResult, useMutation } from '@apollo/client'
-import { UPDATE_APPLICATION } from '@island.is/application/graphql'
-import { useLocale } from '@island.is/localization'
+import { ApolloQueryResult } from '@apollo/client'
 import {
   FindVehicleFormField,
   VehicleRadioFormField,
@@ -22,10 +19,7 @@ import { BasicVehicleInformation } from '@island.is/api/schema'
 export const VehiclesField: FC<React.PropsWithChildren<FieldBaseProps>> = (
   props,
 ) => {
-  const { locale } = useLocale()
-  const { setValue } = useFormContext()
   const { application } = props
-  const [updateApplication] = useMutation(UPDATE_APPLICATION)
   const currentVehicleList = application.externalData.currentVehicleList
     .data as CurrentVehiclesAndRecords
 
@@ -43,25 +37,6 @@ export const VehiclesField: FC<React.PropsWithChildren<FieldBaseProps>> = (
       return result.data.vehicleBasicInfoByPermno
     }
   }
-
-  const updateData = useCallback(async () => {
-    await updateApplication({
-      variables: {
-        input: {
-          id: application.id,
-          answers: {
-            sellerCoOwner: [],
-          },
-        },
-        locale,
-      },
-    })
-  }, [application.id, locale, updateApplication])
-
-  useEffect(() => {
-    setValue('sellerCoOwner', [])
-    updateData()
-  }, [setValue, updateData])
 
   return (
     <Box paddingTop={2}>
