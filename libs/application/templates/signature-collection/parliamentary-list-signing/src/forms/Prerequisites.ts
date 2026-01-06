7d9080effa09fcd3a1e26a2e3f9df3be11b031dc
Kristján Albert Loftsson <kal@juni.is>
2026-01-06 17:03:06 +0000

    migrate multiple templates to use national registry v3

--- libs/application/templates/signature-collection/parliamentary-list-signing/src/forms/Prerequisites.ts
+++ libs/application/templates/signature-collection/parliamentary-list-signing/src/forms/Prerequisites.ts
@@ -11,7 +11,7 @@ import {
   DefaultEvents,
   Form,
   FormModes,
-  NationalRegistryUserApi,
+  NationalRegistryV3UserApi,
   UserProfileApi,
 } from '@island.is/application/types'
 import { m } from '../lib/messages'
@@ -72,7 +72,7 @@ export const Prerequisites: Form = buildForm({
               subTitle: m.userProfileProviderSubtitle,
             }),
             buildDataProviderItem({
-              provider: NationalRegistryUserApi,
+              provider: NationalRegistryV3UserApi,
               title: m.nationalRegistryProviderTitle,
               subTitle: m.nationalRegistryProviderSubtitle,
             }),
