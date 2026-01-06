1a3d179d39a7e2608c5fdf648b1bb68d8fa98ccf
Rúnar Vestmann <runar@stefna.is>
2026-01-06 12:04:03 +0000

    feat(application-system): HH courses application (#20868)
    * Skeleton for HH courses
    
    * chore: format update dirty file
    
    * Add static data that user needs to input
    
    * Change application name
    
    * Required fields must not be empty
    
    * Add input validation
    
    * Send an email on submit
    
    * WIP
    
    * Add participant section
    
    * Payment section skeleton
    
    * WIP
    
    * WIP
    
    * chore: charts update dirty file
    
    * Course data fetched from cms
    
    * Remove unused import
    
    * Read initial selection from query params
    
    * Add participant validation
    
    * Validate payment information step
    
    * Remove env variables
    
    * chore: charts update dirty file
    
    * Cleanup
    
    * Remove todo
    
    * Remove todo
    
    * Fix default payment method value
    
    * Add feature flag
    
    * Adjust README.md
    
    * Fix application url
    
    * Change identity provider params
    
    * Change user profile api config
    
    * Add dataProviders to draft api prop
    
    * Remove actor info
    
    * Simplify participant section
    
    * Export from dataProviders file
    
    * Submit zendesk ticket on submission
    
    * chore: charts update dirty file
    
    * Update README.md
    
    * Add startDate to message
    
    * Coderabbit suggestion
    
    * Adjust course select options query
    
    * Coderabbit suggestions
    
    * Coderabbit suggestions
    
    * Remove timestamp
    
    * Use applicant email and phonenumber
    
    * Adjust assignment
    
    * Update api config
    
    * Move graphql queries into its own folder
    
    * Refactor courses.service
    
    * Move function into its own file
    
    * Move loadOptions to its own file
    
    * Rename application type
    
    * Add logo
    
    * Add 'approveExternalData' to schema
    
    * Update dataSchema
    
    * Remove custom field
    
    * Fix typo
    
    * Fix typo
    
    * Adjust types
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

---
+++ libs/application/templates/hh/courses/src/utils/constants.ts
@@ -0,0 +1,25 @@
+import { DefaultEvents } from '@island.is/application/types'
+
+export type Events = {
+  type: DefaultEvents.SUBMIT | DefaultEvents.ABORT
+}
+
+export enum States {
+  PREREQUISITES = 'prerequisites',
+  DRAFT = 'draft',
+  COMPLETED = 'completed',
+  PAYMENT = 'payment',
+}
+
+export enum Roles {
+  APPLICANT = 'applicant',
+}
+
+export enum ApiActions {
+  submitApplication = 'submitApplication',
+}
+
+export enum IndividualOrCompany {
+  individual = 'individual',
+  company = 'company',
+}
