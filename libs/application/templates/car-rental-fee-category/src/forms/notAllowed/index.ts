038f8454c986510d15c1a4cdfa7ba4a0aa78074b
Hjörtur Jóhann Vignisson <hjorturjohann94@gmail.com>
2026-01-12 09:52:00 +0000

    Apply suggestion from @norda-gunni
    Co-authored-by: norda-gunni <gunnar@norda.is>

--- libs/application/templates/car-rental-fee-category/src/forms/notAllowed/index.ts
+++ libs/application/templates/car-rental-fee-category/src/forms/notAllowed/index.ts
@@ -14,7 +14,7 @@ export const notAllowedForm = buildForm({
         buildDescriptionField({
           id: 'notAllowedDescription',
           title:
-            'Aðeins bílaleigu fyrirtæki eða prófkúruhafar þeirra hafa aðgang að þessari umsókn',
+            'Aðeins bílaleigu fyrirtæki eða prókúruhafar þeirra hafa aðgang að þessari umsókn',
           description:
             'Ef þú telur að þú ættir að hafa aðgang að þessari umsókn, vinsamlegast hafðu samband við Skattinn í síma 442 1000',
         }),
