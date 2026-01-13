a9e03984f201cd9fdd324f3cc0baa597bae3b33c
Ívar Oddsson <ivaroddsson@gmail.com>
2026-01-13 14:05:57 +0000

    chore(j-s): Spacing and input refactoring (#21075)
    * Remove CF string
    
    * Remove CF string
    
    * Use grid for spacing
    
    * Fix spacing
    
    * Fix spacing
    
    * Fix radio button tab ordering
    
    * Remove dividers from connected case accordions
    
    * Remove debug logger
    
    * Fix spacing
    
    * Fix lint
    
    * Refactor defendant
    
    * Refactor defendant spacing and other minor things
    
    * Refactor defendant spacing and other minor things
    
    * Refactor spacing
    
    * Refactor spacing
    
    * Refactor overview route
    
    * Refactor overview route
    
    * Refactor spacing and inputs
    
    * Add null check
    
    * Fix lint
    
    * Value should always be string
    
    * Fix hasSent
    
    * Remove console.log

--- apps/judicial-system/web/src/routes/Prosecutor/InvestigationCase/HearingArrangements/HearingArrangements.tsx
+++ apps/judicial-system/web/src/routes/Prosecutor/InvestigationCase/HearingArrangements/HearingArrangements.tsx
@@ -2,7 +2,7 @@ import { useCallback, useContext, useState } from 'react'
 import { useIntl } from 'react-intl'
 import { useRouter } from 'next/router'
 
-import { Box, Input, Text, toast } from '@island.is/island-ui/core'
+import { Box, Input, toast } from '@island.is/island-ui/core'
 import * as constants from '@island.is/judicial-system/consts'
 import {
   errors,
@@ -18,22 +18,20 @@ import {
   PageLayout,
   PageTitle,
   ProsecutorCaseInfo,
-  UserContext,
+  SectionHeading,
 } from '@island.is/judicial-system-web/src/components'
 import {
   CaseState,
   CaseTransition,
   NotificationType,
 } from '@island.is/judicial-system-web/src/graphql/schema'
-import {
-  removeTabsValidateAndSet,
-  stepValidationsType,
-  validateAndSendToServer,
-} from '@island.is/judicial-system-web/src/utils/formHelper'
+import { stepValidationsType } from '@island.is/judicial-system-web/src/utils/formHelper'
 import {
   formatDateForServer,
   useCase,
+  useDebouncedInput,
 } from '@island.is/judicial-system-web/src/utils/hooks'
+import { grid } from '@island.is/judicial-system-web/src/utils/styles/recipes.css'
 import { hasSentNotification } from '@island.is/judicial-system-web/src/utils/utils'
 import { isHearingArrangementsStepValidIC } from '@island.is/judicial-system-web/src/utils/validate'
 
@@ -47,7 +45,7 @@ const HearingArrangements = () => {
   const router = useRouter()
   const { workingCase, setWorkingCase, isLoadingWorkingCase, caseNotFound } =
     useContext(FormContext)
-  const { user } = useContext(UserContext)
+  const translatorInput = useDebouncedInput('translator')
   const { formatMessage } = useIntl()
   const {
     sendNotification,
@@ -55,7 +53,6 @@ const HearingArrangements = () => {
     sendNotificationError,
     transitionCase,
     isTransitioningCase,
-    updateCase,
     setAndSendCaseToServer,
   } = useCase()
 
@@ -112,113 +109,92 @@ const HearingArrangements = () => {
           titles.prosecutor.investigationCases.hearingArrangements,
         )}
       />
-      {user && (
-        <>
-          <FormContentContainer>
-            <PageTitle>{formatMessage(m.heading)}</PageTitle>
-            <Box marginBottom={5}>
-              <ProsecutorCaseInfo workingCase={workingCase} hideCourt />
-            </Box>
-            <ProsecutorSectionHeightenedSecurity />
-            <Box component="section" marginBottom={5}>
-              <SelectCourt />
-            </Box>
-            <Box component="section" marginBottom={5}>
-              <RequestCourtDate
-                workingCase={workingCase}
-                onChange={(date: Date | undefined, valid: boolean) => {
-                  if (date && valid) {
-                    setAndSendCaseToServer(
-                      [
-                        {
-                          requestedCourtDate: formatDateForServer(date),
-                          force: true,
-                        },
-                      ],
-                      workingCase,
-                      setWorkingCase,
-                    )
-                  }
-                }}
-              />
-            </Box>
-            <Box component="section" marginBottom={10}>
-              <Box marginBottom={3}>
-                <Text as="h3" variant="h3">
-                  {formatMessage(m.sections.translator.heading)}
-                </Text>
-              </Box>
-              <Input
-                data-testid="translator"
-                name="translator"
-                autoComplete="off"
-                label={formatMessage(m.sections.translator.label)}
-                placeholder={formatMessage(m.sections.translator.placeholder)}
-                value={workingCase.translator || ''}
-                onChange={(event) =>
-                  removeTabsValidateAndSet(
-                    'translator',
-                    event.target.value,
-                    [],
+      <FormContentContainer>
+        <PageTitle>{formatMessage(m.heading)}</PageTitle>
+        <div className={grid({ gap: 5, marginBottom: 10 })}>
+          <ProsecutorCaseInfo workingCase={workingCase} hideCourt />
+          <ProsecutorSectionHeightenedSecurity />
+          <Box component="section">
+            <SelectCourt />
+          </Box>
+          <Box component="section">
+            <RequestCourtDate
+              workingCase={workingCase}
+              onChange={(date: Date | undefined, valid: boolean) => {
+                if (date && valid) {
+                  setAndSendCaseToServer(
+                    [
+                      {
+                        requestedCourtDate: formatDateForServer(date),
+                        force: true,
+                      },
+                    ],
+                    workingCase,
                     setWorkingCase,
                   )
                 }
-                onBlur={(event) =>
-                  validateAndSendToServer(
-                    'translator',
-                    event.target.value.trim(),
-                    [],
-                    workingCase,
-                    updateCase,
-                  )
-                }
-              />
-            </Box>
-          </FormContentContainer>
-          <FormContentContainer isFooter>
-            <FormFooter
-              nextButtonIcon="arrowForward"
-              previousUrl={`${constants.INVESTIGATION_CASE_DEFENDANT_ROUTE}/${workingCase.id}`}
-              onNextButtonClick={async () =>
-                await handleNavigationTo(
-                  constants.INVESTIGATION_CASE_POLICE_DEMANDS_ROUTE,
-                )
-              }
-              nextIsDisabled={!stepIsValid}
-              nextIsLoading={isLoadingWorkingCase || isTransitioningCase}
+              }}
             />
-          </FormContentContainer>
-          {navigateTo !== undefined && (
-            <Modal
-              title={formatMessage(m.modal.heading)}
-              text={formatMessage(m.modal.text)}
-              primaryButton={{
-                text: formatMessage(m.modal.primaryButtonText),
-                onClick: async () => {
-                  const notificationSent = await sendNotification(
-                    workingCase.id,
-                    NotificationType.HEADS_UP,
-                  )
+          </Box>
+          <Box component="section">
+            <SectionHeading
+              title={formatMessage(m.sections.translator.heading)}
+            />
+            <Input
+              data-testid="translator"
+              name="translator"
+              autoComplete="off"
+              label={formatMessage(m.sections.translator.label)}
+              placeholder={formatMessage(m.sections.translator.placeholder)}
+              value={translatorInput.value}
+              onChange={(evt) => translatorInput.onChange(evt.target.value)}
+              onBlur={(evt) => translatorInput.onBlur(evt.target.value)}
+            />
+          </Box>
+        </div>
+      </FormContentContainer>
+      <FormContentContainer isFooter>
+        <FormFooter
+          nextButtonIcon="arrowForward"
+          previousUrl={`${constants.INVESTIGATION_CASE_DEFENDANT_ROUTE}/${workingCase.id}`}
+          onNextButtonClick={async () =>
+            await handleNavigationTo(
+              constants.INVESTIGATION_CASE_POLICE_DEMANDS_ROUTE,
+            )
+          }
+          nextIsDisabled={!stepIsValid}
+          nextIsLoading={isLoadingWorkingCase || isTransitioningCase}
+        />
+      </FormContentContainer>
+      {navigateTo !== undefined && (
+        <Modal
+          title={formatMessage(m.modal.heading)}
+          text={formatMessage(m.modal.text)}
+          primaryButton={{
+            text: formatMessage(m.modal.primaryButtonText),
+            onClick: async () => {
+              const notificationSent = await sendNotification(
+                workingCase.id,
+                NotificationType.HEADS_UP,
+              )
 
-                  if (notificationSent) {
-                    router.push(`${navigateTo}/${workingCase.id}`)
-                  }
-                },
-                isLoading: isSendingNotification,
-              }}
-              secondaryButton={{
-                text: formatMessage(m.modal.secondaryButtonText),
-                onClick: () => router.push(`${navigateTo}/${workingCase.id}`),
-              }}
-              onClose={() => setNavigateTo(undefined)}
-              errorMessage={
-                sendNotificationError
-                  ? formatMessage(errors.sendNotification)
-                  : undefined
+              if (notificationSent) {
+                router.push(`${navigateTo}/${workingCase.id}`)
               }
-            />
-          )}
-        </>
+            },
+            isLoading: isSendingNotification,
+          }}
+          secondaryButton={{
+            text: formatMessage(m.modal.secondaryButtonText),
+            onClick: () => router.push(`${navigateTo}/${workingCase.id}`),
+          }}
+          onClose={() => setNavigateTo(undefined)}
+          errorMessage={
+            sendNotificationError
+              ? formatMessage(errors.sendNotification)
+              : undefined
+          }
+        />
       )}
     </PageLayout>
   )
