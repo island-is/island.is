d8b0f21965bf254fa44e175c46048ba6065f2c4b
Ívar Oddsson <ivaroddsson@gmail.com>
2026-01-14 13:52:36 +0000

    Remove unused CF string

--- apps/judicial-system/web/src/routes/Court/Indictments/Advocates/Advocates.strings.ts
+++ apps/judicial-system/web/src/routes/Court/Indictments/Advocates/Advocates.strings.ts
@@ -26,13 +26,6 @@ export const strings = defineMessages({
     description:
       'Notaður sem texti fyrir takka til að staðfesta val á talsmanni í dómaraflæði í ákærum.',
   },
-  changeSpokespersonChoice: {
-    id: 'judicial.system.core:court_indictments.advocates.change_spokesperson_choice',
-    defaultMessage:
-      'Breyta {spokespersonIsLawyer, select, true {lögmanni} other {réttargæslumanni}}',
-    description:
-      'Notaður sem texti fyrir takka til að breyta vali á talsmanni í dómaraflæði í ákærum.',
-  },
   civilClaimants: {
     id: 'judicial.system.core:court_indictments.advocates.civil_claimants',
     defaultMessage: 'Kröfuhafar',
