e5f9a5cfd98e1efd57ff18d82f69270aca88a98b
Ívar Oddsson <ivaroddsson@gmail.com>
2026-01-16 10:34:34 +0000

    Create migration

---
+++ apps/judicial-system/backend/migrations/20260115164854-create-communication.js
@@ -0,0 +1,52 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.createTable(
+        'communication',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          created: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
+          modified: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
+          institution_id: {
+            type: Sequelize.UUID,
+            references: {
+              model: 'institution',
+              key: 'id',
+            },
+            allowNull: false,
+          },
+          value: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          type: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        },
+        { transaction: t },
+      ),
+    )
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.sequelize.transaction((t) =>
+      queryInterface.dropTable('communication', { transaction: t }),
+    )
+  },
+}
