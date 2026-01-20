a2faeabee0f7910dbd2f0f791a0e627277a0e2ef
Ívar Oddsson <ivaroddsson@gmail.com>
2026-01-20 10:19:20 +0000

    Fix tests

--- apps/judicial-system/web/src/components/IconButton/IconButton.tsx
+++ apps/judicial-system/web/src/components/IconButton/IconButton.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, MouseEvent } from 'react'
+import { FC, forwardRef, MouseEvent } from 'react'
 import cn from 'classnames'
 import { Button } from '@ariakit/react/button'
 
@@ -14,48 +14,74 @@ interface Props {
   tooltipText?: string
 }
 
+interface RenderButtonProps {
+  icon: IconMapIcon
+  colorScheme: 'blue' | 'red' | 'transparent'
+  onClick?: (evt: MouseEvent) => void
+  disabled?: boolean
+  ref: React.Ref<HTMLButtonElement>
+}
+
+const RenderButton: FC<RenderButtonProps> = ({
+  icon,
+  colorScheme,
+  onClick,
+  disabled,
+  ref,
+}) => (
+  <Box
+    component={Button}
+    ref={ref}
+    className={cn(styles.iconButtonContainer, {
+      [styles.buttonDisabled]: disabled,
+      [styles.transparent]: colorScheme === 'transparent',
+    })}
+    background={
+      colorScheme === 'blue'
+        ? 'blue200'
+        : colorScheme === 'red'
+        ? 'red200'
+        : 'transparent'
+    }
+    onClick={(evt) => onClick && onClick(evt)}
+    disabled={disabled}
+    aria-label="Valmynd"
+  >
+    <Icon
+      icon={icon}
+      color={
+        colorScheme === 'blue' || colorScheme === 'transparent'
+          ? 'blue400'
+          : 'red400'
+      }
+      size="small"
+    />
+  </Box>
+)
+
 const IconButton = forwardRef<HTMLButtonElement, Props>(({ ...props }, ref) => {
   const { icon, colorScheme, onClick, disabled, tooltipText } = props
-
-  const RenderButton = () => (
-    <Box
-      component={Button}
-      ref={ref}
-      className={cn(styles.iconButtonContainer, {
-        [styles.buttonDisabled]: disabled,
-        [styles.transparent]: colorScheme === 'transparent',
-      })}
-      background={
-        colorScheme === 'blue'
-          ? 'blue200'
-          : colorScheme === 'red'
-          ? 'red200'
-          : 'transparent'
-      }
-      onClick={(evt) => onClick && onClick(evt)}
-      disabled={disabled}
-      aria-label="Valmynd"
-    >
-      <Icon
-        icon={icon}
-        color={
-          colorScheme === 'blue' || colorScheme === 'transparent'
-            ? 'blue400'
-            : 'red400'
-        }
-        size="small"
-      />
-    </Box>
-  )
 
   return tooltipText ? (
     <Tooltip placement="top" text={tooltipText}>
       <span>
-        <RenderButton />
+        <RenderButton
+          icon={icon}
+          colorScheme={colorScheme}
+          onClick={onClick}
+          disabled={disabled}
+          ref={ref}
+        />
       </span>
     </Tooltip>
   ) : (
-    <RenderButton />
+    <RenderButton
+      icon={icon}
+      colorScheme={colorScheme}
+      onClick={onClick}
+      disabled={disabled}
+      ref={ref}
+    />
   )
 })
 
