16c545035aac4265801ec91c87f9ad8b655a2a14
Baldur Óli <ballioli1@gmail.com>
2026-01-22 13:59:06 +0000

    fix(application-system): Adding delegation and small fixes (#21263)
    * Small changes and adding delegation
    
    * Fix

--- libs/application/templates/transport-authority/mile-car/src/forms/mainForm/index.ts
+++ libs/application/templates/transport-authority/mile-car/src/forms/mainForm/index.ts
@@ -3,23 +3,28 @@ import { FormModes } from '@island.is/application/types'
 import {
   confirmation as confirmationMessages,
   externalData as externalDataMessages,
+  selectVehicle as selectVehicleMessages,
 } from '../../lib/messages'
 import { InformationSection } from './InformationSection'
+import { TransportAuthorityLogo } from '@island.is/application/assets/institution-logos'
 
 export const MainForm = buildForm({
   id: 'MainForm',
   mode: FormModes.DRAFT,
   renderLastScreenButton: true,
   renderLastScreenBackButton: true,
+  logo: TransportAuthorityLogo,
   children: [
     buildSection({
       id: 'externalData',
+      tabTitle: selectVehicleMessages.general.sectionTitle,
       title: externalDataMessages.dataProvider.sectionTitle,
       children: [],
     }),
     InformationSection,
     buildSection({
       id: 'confirmation',
+      tabTitle: selectVehicleMessages.general.sectionTitle,
       title: confirmationMessages.sectionTitle,
       children: [],
     }),
