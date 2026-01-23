2f349a63b28eb4b08ed8362c924672d9c86272d2
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2026-01-23 16:20:29 +0000

    feat(TableRow): enhance interactivity with clickable styles and animated height

---
+++ libs/portals/admin/form-system/src/components/Forms/components/Table/TableRow.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css'
+
+export const clickable = style({
+  cursor: 'pointer',
+})
+
+export const capitalizeText = style({
+  textTransform: 'capitalize',
+})
