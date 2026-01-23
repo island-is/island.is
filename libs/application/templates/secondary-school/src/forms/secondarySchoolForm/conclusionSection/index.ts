7dbc1a33ef5b99708e1156405df21c6a74f6404d
johannaagma <johannaagma@gmail.com>
2026-01-23 13:51:59 +0000

    Add back freshman radio + get date to display from code instead of updating contentful 3x a year

--- libs/application/templates/secondary-school/src/forms/secondarySchoolForm/conclusionSection/index.ts
+++ libs/application/templates/secondary-school/src/forms/secondarySchoolForm/conclusionSection/index.ts
@@ -1,21 +1,62 @@
+import { getValueViaPath } from '@island.is/application/core'
 import { conclusion } from '../../../lib/messages'
 import { buildFormConclusionSection } from '@island.is/application/ui-forms'
-import { checkIsFreshman } from '../../../utils'
+import {
+  ApplicationPeriod,
+  checkIsFreshman,
+  getDateWordStr,
+} from '../../../utils'
 
 export const conclusionSection = buildFormConclusionSection({
   alertTitle: conclusion.general.alertTitle,
-  alertMessage: (application) => {
+  alertMessage: (application, locale) => {
     const isFreshman = checkIsFreshman(application.answers)
-    return isFreshman
-      ? conclusion.general.alertMessageFreshman
-      : conclusion.general.alertMessageGeneral
+    const applicationPeriodInfo = getValueViaPath<ApplicationPeriod>(
+      application.externalData,
+      'applicationPeriodInfo.data',
+    )
+
+    const message = isFreshman
+      ? conclusion.general.alertMessageWithValuesFreshman
+      : conclusion.general.alertMessageWithValuesGeneral
+
+    const registrationEndDate = isFreshman
+      ? applicationPeriodInfo?.registrationEndDateFreshman
+      : applicationPeriodInfo?.registrationEndDateGeneral
+
+    return {
+      ...message,
+      values: {
+        registrationEndDateStr: getDateWordStr(registrationEndDate, locale),
+      },
+    }
   },
   expandableHeader: conclusion.general.accordionTitle,
   expandableIntro: '',
-  expandableDescription: (application) => {
+  expandableDescription: (application, locale) => {
     const isFreshman = checkIsFreshman(application.answers)
-    return isFreshman
-      ? conclusion.general.accordionTextFreshman
-      : conclusion.general.accordionTextGeneral
+    const applicationPeriodInfo = getValueViaPath<ApplicationPeriod>(
+      application.externalData,
+      'applicationPeriodInfo.data',
+    )
+
+    const message = isFreshman
+      ? conclusion.general.accordionTextWithValuesFreshman
+      : conclusion.general.accordionTextWithValuesGeneral
+
+    const registrationEndDate = isFreshman
+      ? applicationPeriodInfo?.registrationEndDateFreshman
+      : applicationPeriodInfo?.registrationEndDateGeneral
+    const reviewStartDate = isFreshman
+      ? applicationPeriodInfo?.reviewStartDateFreshman
+      : applicationPeriodInfo?.reviewStartDateGeneral
+
+    return {
+      ...message,
+      values: {
+        registrationEndDateStr: getDateWordStr(registrationEndDate, locale),
+        reviewStartDateStr: getDateWordStr(reviewStartDate, locale),
+      },
+    }
   },
 })
