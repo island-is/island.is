3463ad0e4cca6ca1fbb4673d08c0e455eb90bc75
Hjörtur Jóhann Vignisson <hjorturjohann94@gmail.com>
2026-01-23 10:48:08 +0000

    Update libs/application/templates/car-rental-fee-category/src/lib/messages.ts
    Co-authored-by: Jónas G. Sigurðsson <jonas@norda.is>

--- libs/application/templates/car-rental-fee-category/src/lib/messages.ts
+++ libs/application/templates/car-rental-fee-category/src/lib/messages.ts
@@ -193,7 +193,7 @@ export const m = {
     stepsMessage: {
       id: 'rsk.crfc.application:multi.upload.steps#markdown',
       defaultMessage:
-        '1. Sækir sniðmátið \n\n2. Gerir viðeigandi breytingar á skjalinu \n\n3. Hleður upp skjalinu hér að neðan \n\nAth. Einungis ökutæki með gild bílnúmer verða sýndir í sniðmátinu',
+        '1. Sækir sniðmátið \n\n2. Gerir viðeigandi breytingar á skjalinu \n\n3. Hleður upp skjalinu hér að neðan \n\nAth. Einungis ökutæki með gild bílnúmer verða sýnd í sniðmátinu',
       description: 'Bulk upload help steps',
     },
     templateButton: {
