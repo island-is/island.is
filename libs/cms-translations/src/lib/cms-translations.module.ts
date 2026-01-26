df1fcdd0910acaa39c1abfd0a8c6922d602b9af5
norda-gunni <gunnar@norda.is>
2026-01-26 10:08:30 +0000

    feat(cms): Cache translations to redis (#21253)
    * feat(cms): Cache translations to redis
    
    * Removed env var
    
    * add INIT_SCHEMA=true to judicial-system-api codegen
    
    * adding cross-env INIT_SCHEMA=true to api as well

--- libs/cms-translations/src/lib/cms-translations.module.ts
+++ libs/cms-translations/src/lib/cms-translations.module.ts
@@ -4,11 +4,21 @@ import { CmsModule } from '@island.is/cms'
 
 import { CmsTranslationsService } from './cms-translations.service'
 import { CmsTranslationsResolver } from './cms-translations.resolver'
+import { CmsTranslationCacheModule } from './cms-translation.cache'
 import { IntlService } from './intl.service'
+import { CmsTranslationConfig } from './cms-translation.config'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   controllers: [],
-  imports: [CmsModule],
+  imports: [
+    CmsModule,
+    CmsTranslationCacheModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [CmsTranslationConfig],
+    }),
+  ],
   providers: [CmsTranslationsResolver, CmsTranslationsService, IntlService],
   exports: [CmsTranslationsService, IntlService],
 })
