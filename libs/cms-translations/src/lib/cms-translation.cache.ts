4a42ed588f32538eb4e6533a0712216e566af8c1
norda-gunni <gunnar@norda.is>
2026-01-26 10:08:30 +0000

    feat(cms): Cache translations to redis (#21253)
    * feat(cms): Cache translations to redis
    
    * Removed env var
    
    * add INIT_SCHEMA=true to judicial-system-api codegen
    
    * adding cross-env INIT_SCHEMA=true to api as well

---
+++ libs/cms-translations/src/lib/cms-translation.cache.ts
@@ -0,0 +1,31 @@
+import { DynamicModule } from '@nestjs/common'
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager'
+import { redisInsStore } from 'cache-manager-ioredis-yet'
+import { createRedisCluster } from '@island.is/cache'
+import { ConfigType } from '@nestjs/config'
+import { CmsTranslationConfig } from './cms-translation.config'
+
+let CmsTranslationCacheModule: DynamicModule
+
+export const CACHE_MODULE_KEY = 'CmsTranslationModuleCache'
+
+if (process.env.NODE_ENV === 'test' || process.env.INIT_SCHEMA === 'true') {
+  CmsTranslationCacheModule = NestCacheModule.register()
+} else {
+  CmsTranslationCacheModule = NestCacheModule.registerAsync({
+    useFactory: (config: ConfigType<typeof CmsTranslationConfig>) => {
+      return {
+        store: redisInsStore(
+          createRedisCluster({
+            name: 'cmsTranslation',
+            ssl: config.redis.ssl,
+            nodes: config.redis.nodes,
+          }),
+        ),
+      }
+    },
+    inject: [CmsTranslationConfig.KEY],
+  })
+}
+
+export { CmsTranslationCacheModule }
