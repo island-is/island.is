ccdd26ac7a7e6f10d11d7e5c5665c8f7ef605aa5
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2026-01-27 09:16:42 +0000

    fix: update infra

--- apps/services/cms-importer/infra/cms-importer-worker.ts
+++ apps/services/cms-importer/infra/cms-importer-worker.ts
@@ -16,13 +16,7 @@ export const workerSetup = (): ServiceBuilder<'cms-importer-worker'> =>
         '/k8s/contentful-entry-tagger/CONTENTFUL_MANAGEMENT_ACCESS_TOKEN',
     })
     .command('node')
-    .args('main.cjs', 'job:grant-import')
-    .extraAttributes({
-      // Schedule to run daily at 15:00 and midnight, every 3 hours on dev.
-      dev: { schedule: '0 */3 * * *' },
-      staging: { schedule: '0 15,0 * * *' },
-      prod: { schedule: '0 15,0 * * *' },
-    })
+    .args('main.cjs')
 
 export const energyFundImportSetup =
   (): ServiceBuilder<'cms-importer-energy-fund-import'> =>
