56df663d9853758a620fb861ee5bd6bc904ccbc2
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2026-02-03 09:03:18 +0000

    chore: fix jest config

--- libs/clients/ship-registry-v2/jest.config.ts
+++ libs/clients/ship-registry-v2/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'ship-registry',
+  displayName: 'ship-registry-v2',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/clients/ship-registry',
+  coverageDirectory: '../../../coverage/libs/clients/ship-registry-v2',
 }
