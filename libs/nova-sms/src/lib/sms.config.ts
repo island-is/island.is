ba7c19c868b79b5bd59cbc310bb54832f1b4c079
Gunnlaugur Guðmundsson <gunnlaugur16@gmail.com>
2026-02-09 14:17:03 +0000

    update SMS configuration for v1 API

--- libs/nova-sms/src/lib/sms.config.ts
+++ libs/nova-sms/src/lib/sms.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from '@island.is/nest/config'
 export const smsModuleConfig = defineConfig({
   name: 'SmsModule',
   load: (env) => ({
-    url: env.required('NOVA_URL', 'https://smsapi.devnova.is/v1/'),
-    username: env.required('NOVA_USERNAME', 'IslandIs_User_Development'),
-    password: env.required('NOVA_PASSWORD', ''),
+    url: env.required('NOVA_URL_V1', 'https://sms.nova.is/v1/'),
+    username: env.required('NOVA_USERNAME_V1', 'S56572'),
+    password: env.required('NOVA_PASSWORD_V1', ''),
   }),
 })
