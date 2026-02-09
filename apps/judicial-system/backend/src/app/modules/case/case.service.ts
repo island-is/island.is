eeaca5aaac65cc392c9409acd86499b04f9f32a3
Guðjón Guðjónsson <gudjon@kolibri.is>
2026-02-09 13:50:35 +0000

    fix(j-s): Split Old Case (#21410)
    * Force court session support in split cases
    
    * Do not show connected cases that have not been received

--- apps/judicial-system/backend/src/app/modules/case/case.service.ts
+++ apps/judicial-system/backend/src/app/modules/case/case.service.ts
@@ -1871,8 +1871,8 @@ export class CaseService {
         [Op.and]: {
           isArchived: false,
           type: CaseType.INDICTMENT,
+          state: [CaseState.RECEIVED],
           id: { [Op.ne]: theCase.id },
-          state: [CaseState.SUBMITTED, CaseState.RECEIVED],
         },
       },
     })
