1b2fa6cb089c439f7933d5649face9a348941128
gudjonss <gudjon.steinar.sverrisson@kolibri.is>
2026-02-10 09:12:49 +0000

    use verdict id

--- apps/judicial-system/backend/migrations/20260209152933-fix-verdict-service-date.js
+++ apps/judicial-system/backend/migrations/20260209152933-fix-verdict-service-date.js
@@ -6,7 +6,7 @@ module.exports = {
       queryInterface.bulkUpdate(
         'verdict',
         { service_date: '2026-02-01T12:15:00.000Z' },
-        { id: '83c2fdcb-417b-4891-a1e0-4d0f3a731841' },
+        { id: '04253fb0-2596-4453-8082-27844644ed7c' },
         { transaction },
       ),
     )
