e1b388e65709c16365a58702bfb04ca77887b63f
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2026-02-11 17:00:47 +0000

    chore:add scope seeder

---
+++ libs/auth-api-lib/seeders/data/scopes-hms-rental-agreements.ts
@@ -0,0 +1,11 @@
+import { compose, createScope } from './helpers'
+
+export const up = compose(
+  createScope({
+    name: '@hms.is/rental-agreements',
+    displayName: 'Leigusamningar',
+    description:
+      'bakendascope fyrir leigusamninga',
+    addToClients: ['@island.is/clients/api'],
+  }),
+)
