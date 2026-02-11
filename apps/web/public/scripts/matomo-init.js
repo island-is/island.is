a4502590227977b962c519303e874a0acdb02fb2
Geir Garðarsson <geir@programm.is>
2026-02-11 15:02:33 +0000

    format matomo script

--- apps/web/public/scripts/matomo-init.js
+++ apps/web/public/scripts/matomo-init.js
@@ -4,12 +4,12 @@
   var u = script.getAttribute('data-matomo-domain')
   var siteId = script.getAttribute('data-matomo-site-id')
   if (!u || !siteId) {
-    console.warn(
-      'Matomo tracking is not configured properly. Please check the data-matomo-domain and data-matomo-site-id attributes on the script tag.',
-    )
+    console.warn( 'Matomo tracking is not configured properly. Please check the data-matomo-domain and data-matomo-site-id attributes on the script tag.',)
     return
   }
-  if (u.charAt(u.length - 1) !== '/') u += '/'
+  if (u.charAt(u.length - 1) !== '/') {
+    u += '/'
+  }
   var _paq = (window._paq = window._paq || [])
   _paq.push(['trackPageView'])
   _paq.push(['enableLinkTracking'])
