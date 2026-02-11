9b2f2fa9bf384966f1d7bd8076961efad82ace97
Geir Garðarsson <geir@programm.is>
2026-02-11 15:09:04 +0000

    trigger web deploy

--- apps/web/infra/web.ts
+++ apps/web/infra/web.ts
@@ -20,7 +20,7 @@ export const serviceSetup = (services: {
         staging: 'beta.staging01.devland.is',
         prod: 'island.is',
       },
-      MATOMO_DOMAIN: {
+      MATOMO_DOMAIN: { // test
         dev: 'https://matomo-dev.dev01.devland.is',
         staging: 'https://matomo-dev.dev01.devland.is',
         prod: 'https://matomo-dev.dev01.devland.is',
