08dfdafee766f81db6e2ba906ca8b00ec4097df4
norda-gunni <gunnar@norda.is>
2026-02-13 12:35:25 +0000

    fix(as): More descriptive messages for getProperties errors (#21512) (#21516)

--- libs/application/templates/hms/registration-of-new-property-numbers/src/index.ts
+++ libs/application/templates/hms/registration-of-new-property-numbers/src/index.ts
@@ -4,4 +4,6 @@ export * from './lib/dataSchema'
 export const getDataProviders = () => import('./dataProviders')
 export const getFields = () => import('./fields/')
 
+export { prereqMessages } from './lib/messages/prereqMessages'
+
 export default template
