f45e89840f492e7d5ab13778320817b645b15d5f
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2026-02-16 14:02:47 +0000

    refactor(file-module): remove unused BullModule and UploadProcessor

--- apps/services/form-system/src/app/modules/file/file.module.ts
+++ apps/services/form-system/src/app/modules/file/file.module.ts
@@ -1,16 +1,13 @@
-import { createRedisCluster } from '@island.is/cache'
 import { FileStorageConfig } from '@island.is/file-storage'
 import { LoggingModule } from '@island.is/logging'
 import { AwsModule } from '@island.is/nest/aws'
-import { BullModule as NestBullModule } from '@nestjs/bull'
 import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigType } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { Value } from '../applications/models/value.model'
 import { FileConfig } from './file.config'
 import { FileController } from './file.controller'
 import { FileService } from './file.service'
-import { UploadProcessor } from './upload.processor'
 import { FileStorageWrapperModule } from './fileStorageWrapper'
 
 @Module({
@@ -21,24 +18,24 @@ import { FileStorageWrapperModule } from './fileStorageWrapper'
     FileStorageWrapperModule,
     SequelizeModule.forFeature([Value]),
     ConfigModule.forFeature(FileConfig),
-    NestBullModule.registerQueueAsync({
-      name: 'upload',
-      imports: [ConfigModule.forFeature(FileConfig)],
-      useFactory: (config: ConfigType<typeof FileConfig>) => ({
-        prefix: `{${config.bullModuleName ?? 'form_system_api_bull_module'}}`,
-        createClient: () =>
-          createRedisCluster({
-            name: config.bullModuleName ?? 'form_system_api_bull_module',
-            ssl: config.redis.ssl,
-            nodes: config.redis.nodes,
-            noPrefix: true,
-          }),
-      }),
-      inject: [FileConfig.KEY],
-    }),
+    // NestBullModule.registerQueueAsync({
+    //   name: 'upload',
+    //   imports: [ConfigModule.forFeature(FileConfig)],
+    //   useFactory: (config: ConfigType<typeof FileConfig>) => ({
+    //     prefix: `{${config.bullModuleName ?? 'form_system_api_bull_module'}}`,
+    //     createClient: () =>
+    //       createRedisCluster({
+    //         name: config.bullModuleName ?? 'form_system_api_bull_module',
+    //         ssl: config.redis.ssl,
+    //         nodes: config.redis.nodes,
+    //         noPrefix: true,
+    //       }),
+    //   }),
+    //   inject: [FileConfig.KEY],
+    // }),
   ],
   controllers: [FileController],
-  providers: [FileService, UploadProcessor],
+  providers: [FileService],
   exports: [FileService],
 })
 export class FileModule {}
