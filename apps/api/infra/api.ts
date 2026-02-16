66fffd96580e72d7d7ce3efd807b841c69dcac2b
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2026-02-16 09:05:15 +0000

    fix: mrege conflicts

--- apps/api/infra/api.ts
+++ apps/api/infra/api.ts
@@ -313,12 +313,6 @@ export const serviceSetup = (services: {
         staging: 'https://sjodir.rannis.is/statistics/fund_schedule.php',
         prod: 'https://sjodir.rannis.is/statistics/fund_schedule.php',
       },
-      HMS_CONTRACTS_AUTH_TOKEN_ENDPOINT: {
-        dev: 'https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token',
-        staging:
-          'https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token',
-        prod: 'https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token',
-      },
       ELFUR_CLIENT_ID: {
         dev: '@fjs.is/stafraent-island-api-elfur',
         staging: '@fjs.is/stafraent-island-api-elfur',
@@ -334,16 +328,6 @@ export const serviceSetup = (services: {
         dev: 'https://identity-server.staging01.devland.is',
         staging: 'https://identity-server.staging01.devland.is',
         prod: 'https://innskra.island.is',
-      },
-      HMS_CONTRACTS_AUTH_TENANT_ID: {
-        dev: 'c7256472-2622-417e-8955-a54eeb0a110e',
-        staging: 'c7256472-2622-417e-8955-a54eeb0a110e',
-        prod: 'c7256472-2622-417e-8955-a54eeb0a110e',
-      },
-      HMS_CONTRACTS_AUTH_CLIENT_ID: {
-        dev: 'e2411f5c-436a-4c17-aa14-eab9c225bc06',
-        staging: 'e2411f5c-436a-4c17-aa14-eab9c225bc06',
-        prod: '44055958-a462-4ba8-bbd2-5bfedbbd18c0',
       },
       LANDSPITALI_PAYMENT_FLOW_EVENT_CALLBACK_URL: ref(
         (h) => `http://${h.svc(services.paymentFlowUpdateHandlerService)}`,
