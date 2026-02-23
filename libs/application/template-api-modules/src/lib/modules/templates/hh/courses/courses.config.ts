22808cf36f4f697a97b177c1020ab5c1042d5add
Rúnar Vestmann <runar@stefna.is>
2026-02-23 21:29:43 +0000

    Change config

--- libs/application/template-api-modules/src/lib/modules/templates/hh/courses/courses.config.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/hh/courses/courses.config.ts
@@ -6,10 +6,6 @@ const schema = z.object({
   applicationSenderName: z.string(),
   applicationSenderEmail: z.string(),
   applicationEmailSubject: z.string(),
-  zendeskSubdomain: z.string(),
-  zendeskFormEmail: z.string(),
-  zendeskFormToken: z.string(),
-  zendeskFormTokenEmail: z.string(),
 })
 
 export const HHCoursesConfig = defineConfig({
@@ -20,9 +16,5 @@ export const HHCoursesConfig = defineConfig({
     applicationSenderName: env.required('EMAIL_FROM_NAME'),
     applicationSenderEmail: env.required('EMAIL_FROM'),
     applicationEmailSubject: env.required('HH_COURSES_ZENDESK_SUBJECT'),
-    zendeskSubdomain: env.required('HH_ZENDESK_CONTACT_FORM_SUBDOMAIN'),
-    zendeskFormEmail: env.required('HH_ZENDESK_CONTACT_FORM_EMAIL'),
-    zendeskFormToken: env.required('HH_ZENDESK_CONTACT_FORM_TOKEN'),
-    zendeskFormTokenEmail: env.required('HH_ZENDESK_CONTACT_FORM_TOKEN_EMAIL'),
   }),
 })
