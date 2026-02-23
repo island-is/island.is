a9b7e14a15fbab62a65b1a5a3cfb1b6db5a561a2
Rúnar Vestmann <runar@stefna.is>
2026-02-23 14:04:52 +0000

    Update config

--- libs/application/template-api-modules/src/lib/modules/templates/hh/courses/courses.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/hh/courses/courses.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common'
-import {
-  ZendeskService,
-  ZendeskServiceConfig,
-} from '@island.is/clients/zendesk'
-import { ConfigModule, type ConfigType } from '@island.is/nest/config'
+import { ConfigModule } from '@island.is/nest/config'
 import { ApplicationApiCoreModule } from '@island.is/application/api/core'
-import { type Logger, LOGGER_PROVIDER } from '@island.is/logging'
 
 import { SharedTemplateAPIModule } from '../../../shared'
 
@@ -17,29 +12,10 @@ import { HHCoursesConfig } from './courses.config'
     SharedTemplateAPIModule,
     ApplicationApiCoreModule,
     ConfigModule.forRoot({
-      isGlobal: true,
       load: [HHCoursesConfig],
     }),
   ],
-  providers: [
-    CoursesService,
-    {
-      provide: ZendeskService,
-      useFactory: (
-        config: ConfigType<typeof HHCoursesConfig>,
-        logger: Logger,
-      ) =>
-        new ZendeskService(
-          {
-            subdomain: config.zendeskSubdomain,
-            formEmail: config.zendeskFormTokenEmail,
-            formToken: config.zendeskFormToken,
-          } as ConfigType<typeof ZendeskServiceConfig>,
-          logger,
-        ),
-      inject: [HHCoursesConfig.KEY, LOGGER_PROVIDER],
-    },
-  ],
+  providers: [CoursesService],
   exports: [CoursesService],
 })
 export class CoursesModule {}
