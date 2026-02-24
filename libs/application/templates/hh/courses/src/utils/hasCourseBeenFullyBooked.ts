798a8411da31c3c3c5a88bed2945b406e00c65a7
Rúnar Vestmann <runar@stefna.is>
2026-02-24 18:17:26 +0000

    Flip boolean

--- libs/application/templates/hh/courses/src/utils/hasCourseBeenFullyBooked.ts
+++ libs/application/templates/hh/courses/src/utils/hasCourseBeenFullyBooked.ts
@@ -2,9 +2,9 @@ import { getValueViaPath } from '@island.is/application/core'
 import type { Application } from '@island.is/application/types'
 
 export const hasCourseBeenFullyBooked = (application: Application) =>
-  Boolean(
+  !(
     getValueViaPath<boolean>(
       application.externalData,
       'hhCoursesParticipantAvailability.hasAvailability',
-    ) ?? false,
+    ) ?? false
   )
