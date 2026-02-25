d9973b722d3c3d589d90e883d526bdbe03c8ac97
Herdismaria <herdismaria90@gmail.com>
2026-02-25 13:28:16 +0000

    fix: add database config to worker infra setup

--- apps/services/payments/infra/payments.ts
+++ apps/services/payments/infra/payments.ts
@@ -10,33 +10,65 @@ import { Base, Client, ChargeFjsV2 } from '../../../../infra/src/dsl/xroad'
 const namespace = 'services-payments'
 const serviceName = namespace
 const imageName = namespace
+const workerName = `${namespace}-worker`
 
+// common environment variables
+const env = {
+  IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/payments',
+  IDENTITY_SERVER_ISSUER_URL: {
+    dev: 'https://identity-server.dev01.devland.is',
+    staging: 'https://identity-server.staging01.devland.is',
+    prod: 'https://innskra.island.is',
+  },
+  PAYMENTS_WEB_URL: {
+    dev: ref(
+      (ctx) =>
+        `https://${
+          ctx.featureDeploymentName ? `${ctx.featureDeploymentName}-` : ''
+        }beta.dev01.devland.is/greida`,
+    ),
+    staging: `https://beta.staging01.devland.is/greida`,
+    prod: `https://island.is/greida`,
+  },
+  PAYMENTS_JWT_SIGNING_EXPIRES_IN_MINUTES: '5',
+  PAYMENTS_APPLE_PAY_DOMAIN: 'island.is',
+  PAYMENTS_APPLE_PAY_DISPLAY_NAME: 'island.is',
+}
+
+// common database configuration
+const db = {
+  extensions: ['uuid-ossp'],
+}
+
+// common secrets
+const secrets = {
+  IDENTITY_SERVER_CLIENT_SECRET:
+    '/k8s/services-payments/IDENTITY_SERVER_CLIENT_SECRET',
+  PAYMENTS_TOKEN_SIGNING_SECRET:
+    '/k8s/services-payments/PAYMENTS_TOKEN_SIGNING_SECRET',
+  PAYMENTS_TOKEN_SIGNING_ALGORITHM:
+    '/k8s/services-payments/PAYMENTS_TOKEN_SIGNING_ALGORITHM',
+  PAYMENTS_JWT_SIGNING_KEY_ID:
+    '/k8s/services-payments/PAYMENTS_JWT_SIGNING_KEY_ID',
+  PAYMENTS_JWT_SIGNING_PRIVATE_KEY:
+    '/k8s/services-payments/PAYMENTS_JWT_SIGNING_PRIVATE_KEY',
+  PAYMENTS_JWT_SIGNING_PUBLIC_KEY:
+    '/k8s/services-payments/PAYMENTS_JWT_SIGNING_PUBLIC_KEY',
+  PAYMENTS_PREVIOUS_KEY_ID: '/k8s/services-payments/PAYMENTS_PREVIOUS_KEY_ID',
+  PAYMENTS_PREVIOUS_PUBLIC_KEY:
+    '/k8s/services-payments/PAYMENTS_PREVIOUS_PUBLIC_KEY',
+}
+
+// service setup
 export const serviceSetup = (): ServiceBuilder<'services-payments'> =>
   service(serviceName)
     .namespace(namespace)
     .image(imageName)
     .serviceAccount(serviceName)
-    .db({
-      extensions: ['uuid-ossp'],
-    })
+    .db(db)
     .migrations()
     .env({
-      IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/payments',
-      IDENTITY_SERVER_ISSUER_URL: {
-        dev: 'https://identity-server.dev01.devland.is',
-        staging: 'https://identity-server.staging01.devland.is',
-        prod: 'https://innskra.island.is',
-      },
-      PAYMENTS_WEB_URL: {
-        dev: ref(
-          (ctx) =>
-            `https://${
-              ctx.featureDeploymentName ? `${ctx.featureDeploymentName}-` : ''
-            }beta.dev01.devland.is/greida`,
-        ),
-        staging: `https://beta.staging01.devland.is/greida`,
-        prod: `https://island.is/greida`,
-      },
+      ...env,
       REDIS_NODES: {
         dev: json([
           'clustercfg.general-redis-cluster-group.5fzau3.euw1.cache.amazonaws.com:6379',
@@ -48,22 +80,14 @@ export const serviceSetup = (): ServiceBuilder<'services-payments'> =>
           'clustercfg.general-redis-cluster-group.whakos.euw1.cache.amazonaws.com:6379',
         ]),
       },
-      PAYMENTS_JWT_SIGNING_EXPIRES_IN_MINUTES: '5',
       XROAD_FJS_INVOICE_PAYMENT_BASE_CALLBACK_URL: {
         dev: 'XROAD:/IS-DEV/GOV/10000/island-is-protected/payments-v1',
         staging: 'XROAD:/IS-TEST/GOV/10000/island-is-protected/payments-v1',
         prod: 'XROAD:/IS/GOV/5501692829/island-is-protected/payments-v1',
       },
-      PAYMENTS_APPLE_PAY_DOMAIN: 'island.is',
-      PAYMENTS_APPLE_PAY_DISPLAY_NAME: 'island.is',
     })
     .secrets({
-      IDENTITY_SERVER_CLIENT_SECRET:
-        '/k8s/services-payments/IDENTITY_SERVER_CLIENT_SECRET',
-      PAYMENTS_TOKEN_SIGNING_SECRET:
-        '/k8s/services-payments/PAYMENTS_TOKEN_SIGNING_SECRET',
-      PAYMENTS_TOKEN_SIGNING_ALGORITHM:
-        '/k8s/services-payments/PAYMENTS_TOKEN_SIGNING_ALGORITHM',
+      ...secrets,
       PAYMENTS_GATEWAY_API_SECRET:
         '/k8s/services-payments/PAYMENTS_GATEWAY_API_SECRET',
       PAYMENTS_GATEWAY_API_HEADER_KEY:
@@ -74,16 +98,6 @@ export const serviceSetup = (): ServiceBuilder<'services-payments'> =>
         '/k8s/services-payments/PAYMENTS_GATEWAY_API_URL',
       PAYMENTS_GATEWAY_SYSTEM_CALLING:
         '/k8s/services-payments/PAYMENTS_GATEWAY_SYSTEM_CALLING',
-      PAYMENTS_JWT_SIGNING_KEY_ID:
-        '/k8s/services-payments/PAYMENTS_JWT_SIGNING_KEY_ID',
-      PAYMENTS_JWT_SIGNING_PRIVATE_KEY:
-        '/k8s/services-payments/PAYMENTS_JWT_SIGNING_PRIVATE_KEY',
-      PAYMENTS_JWT_SIGNING_PUBLIC_KEY:
-        '/k8s/services-payments/PAYMENTS_JWT_SIGNING_PUBLIC_KEY',
-      PAYMENTS_PREVIOUS_KEY_ID:
-        '/k8s/services-payments/PAYMENTS_PREVIOUS_KEY_ID',
-      PAYMENTS_PREVIOUS_PUBLIC_KEY:
-        '/k8s/services-payments/PAYMENTS_PREVIOUS_PUBLIC_KEY',
       PAYMENTS_INVOICE_TOKEN_SIGNING_SECRET:
         '/k8s/services-payments/PAYMENTS_INVOICE_TOKEN_SIGNING_SECRET',
       PAYMENTS_INVOICE_TOKEN_SIGNING_ALGORITHM:
@@ -128,57 +142,20 @@ export const serviceSetup = (): ServiceBuilder<'services-payments'> =>
     .liveness('/liveness')
     .grantNamespaces('application-system', 'nginx-ingress-internal', 'islandis')
 
-export const serviceSetupForWorker =
-  (): ServiceBuilder<'services-payments-worker'> =>
-    service('services-payments-worker')
-      .namespace(namespace)
-      .image(imageName)
-      .serviceAccount(serviceName)
-      .codeOwner(CodeOwners.Aranja)
-      .env({
-        IDENTITY_SERVER_CLIENT_ID: '@island.is/clients/payments',
-        IDENTITY_SERVER_ISSUER_URL: {
-          dev: 'https://identity-server.dev01.devland.is',
-          staging: 'https://identity-server.staging01.devland.is',
-          prod: 'https://innskra.island.is',
-        },
-        PAYMENTS_WEB_URL: {
-          dev: ref(
-            (ctx) =>
-              `https://${
-                ctx.featureDeploymentName ? `${ctx.featureDeploymentName}-` : ''
-              }beta.dev01.devland.is/greida`,
-          ),
-          staging: `https://beta.staging01.devland.is/greida`,
-          prod: `https://island.is/greida`,
-        },
-        PAYMENTS_JWT_SIGNING_EXPIRES_IN_MINUTES: '5',
-        PAYMENTS_APPLE_PAY_DOMAIN: 'island.is',
-        PAYMENTS_APPLE_PAY_DISPLAY_NAME: 'island.is',
-      })
-      .secrets({
-        IDENTITY_SERVER_CLIENT_SECRET:
-          '/k8s/services-payments/IDENTITY_SERVER_CLIENT_SECRET',
-        PAYMENTS_TOKEN_SIGNING_SECRET:
-          '/k8s/services-payments/PAYMENTS_TOKEN_SIGNING_SECRET',
-        PAYMENTS_TOKEN_SIGNING_ALGORITHM:
-          '/k8s/services-payments/PAYMENTS_TOKEN_SIGNING_ALGORITHM',
-        PAYMENTS_JWT_SIGNING_KEY_ID:
-          '/k8s/services-payments/PAYMENTS_JWT_SIGNING_KEY_ID',
-        PAYMENTS_JWT_SIGNING_PRIVATE_KEY:
-          '/k8s/services-payments/PAYMENTS_JWT_SIGNING_PRIVATE_KEY',
-        PAYMENTS_JWT_SIGNING_PUBLIC_KEY:
-          '/k8s/services-payments/PAYMENTS_JWT_SIGNING_PUBLIC_KEY',
-        PAYMENTS_PREVIOUS_KEY_ID:
-          '/k8s/services-payments/PAYMENTS_PREVIOUS_KEY_ID',
-        PAYMENTS_PREVIOUS_PUBLIC_KEY:
-          '/k8s/services-payments/PAYMENTS_PREVIOUS_PUBLIC_KEY',
-      })
-      .xroad(Base, Client, ChargeFjsV2)
-      .command('node')
-      .args('main.cjs', '--job', 'worker')
-      .extraAttributes({
-        dev: { schedule: '*/5 * * * *' },
-        staging: { schedule: '*/5 * * * *' },
-        prod: { schedule: '*/5 * * * *' },
-      })
+// worker setup
+export const serviceSetupForWorker = (): ServiceBuilder<typeof workerName> =>
+  service(workerName)
+    .namespace(namespace)
+    .image(imageName)
+    .db(db)
+    .codeOwner(CodeOwners.Aranja)
+    .env(env)
+    .secrets(secrets)
+    .xroad(Base, Client, ChargeFjsV2)
+    .command('node')
+    .args('main.cjs', '--job', 'worker')
+    .extraAttributes({
+      dev: { schedule: '*/5 * * * *' },
+      staging: { schedule: '*/5 * * * *' },
+      prod: { schedule: '*/5 * * * *' },
+    })
