4340b8a80e7fac42a4960edadd0ee0ced5b31517
Gunnar K Vilbergsson <gunnar@norda.is>
2026-02-25 09:32:16 +0000

    trigger rebuild

--- libs/application/api/payment/src/lib/payment-callback.controller.ts
+++ libs/application/api/payment/src/lib/payment-callback.controller.ts
@@ -75,7 +75,10 @@ export class PaymentCallbackController {
   async apiClientPaymentCallback(
     @Body() callback: ApiClientCallback,
   ): Promise<void> {
-    console.log('apiClientPaymentCallback', JSON.stringify(callback, null, 2))
+    console.log(
+      'apiClientPaymentCallback body',
+      JSON.stringify(callback, null, 2),
+    )
     if (callback.type === 'success') {
       if (!callback.paymentFlowMetadata.paymentId) {
         throw new BadRequestException('No paymentId found in success callback')
