89e26a30c5c00a0b41be16b8fddf4a7edd2b0c5d
OlafurGeorg <olafur.georg.gylfason@advania.is>
2026-02-27 11:43:05 +0000

    delete form-system apps in mp

--- libs/application/ui-components/src/hooks/useDeleteApplication.ts
+++ libs/application/ui-components/src/hooks/useDeleteApplication.ts
@@ -1,9 +1,13 @@
 import { useMutation } from '@apollo/client'
 import { DELETE_APPLICATION } from '@island.is/application/graphql'
+import { DELETE_APPLICATION as DELETE_FORM_SYSTEM_APPLICATION } from '@island.is/form-system/graphql'
 import { handleServerError } from '../utilities/handleServerError'
 import { useLocale } from '@island.is/localization'
 
-export const useDeleteApplication = (refetch?: (() => void) | undefined) => {
+export const useDeleteApplication = (
+  refetch?: (() => void) | undefined,
+  typeId?: string,
+) => {
   const { formatMessage } = useLocale()
   const [deleteApplicationMutation, { error, loading }] = useMutation(
     DELETE_APPLICATION,
@@ -16,8 +20,26 @@ export const useDeleteApplication = (refetch?: (() => void) | undefined) => {
       },
     },
   )
+  const [
+    deleteFormSystemApplicationMutation,
+    { error: formSystemError, loading: formSystemLoading },
+  ] = useMutation(DELETE_FORM_SYSTEM_APPLICATION, {
+    onCompleted: () => {
+      refetch?.()
+    },
+    onError: (formSystemError) => {
+      handleServerError(formSystemError, formatMessage)
+    },
+  })
 
-  const deleteApplication = (applicationId: string) => {
+  const deleteApplication = (applicationId: string, typeId?: string) => {
+    if (typeId === '') {
+      return deleteFormSystemApplicationMutation({
+        variables: {
+          input: applicationId,
+        },
+      })
+    }
     return deleteApplicationMutation({
       variables: {
         input: {
