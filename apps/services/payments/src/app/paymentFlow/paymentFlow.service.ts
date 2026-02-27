fae848606a30fea80930acf52ea4ece77b4c9cbf
Herdis Maria <herdismaria90@gmail.com>
2026-02-27 11:16:25 +0000

    fix(payments): remove unnecessary Sequelize alias (#21652)
    * remove unnecessary sequelize alias
    
    * remove all aliases

--- apps/services/payments/src/app/paymentFlow/paymentFlow.service.ts
+++ apps/services/payments/src/app/paymentFlow/paymentFlow.service.ts
@@ -941,7 +941,6 @@ export class PaymentFlowService {
       include: [
         {
           model: PaymentFulfillment,
-          as: 'paymentFulfillment',
           required: true,
           where: {
             paymentMethod: 'card',
@@ -950,16 +949,14 @@ export class PaymentFlowService {
             isDeleted: false,
           },
         },
-        { model: PaymentFlowCharge, as: 'charges' },
+        { model: PaymentFlowCharge },
         {
           model: CardPaymentDetails,
-          as: 'cardPaymentDetails',
           required: true,
           where: { isDeleted: false },
         },
         {
           model: PaymentWorkerEvent,
-          as: 'workerEvents',
           required: false,
           where: { taskType: 'create_fjs_charge' },
           order: [['created', 'DESC']],
