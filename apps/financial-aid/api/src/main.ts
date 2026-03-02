8c8461cd2f0aa4a9fe2817b8d82af338bd6c043b
Jón Örn <jonorn@apro.is>
2026-03-02 10:21:49 +0000

    fix: is not a fix

--- apps/financial-aid/api/src/main.ts
+++ apps/financial-aid/api/src/main.ts
@@ -2,11 +2,12 @@ import { bootstrap } from '@island.is/infra-nest-server'
 
 import { AppModule } from './app/app.module'
 
+if (!process.env.THIS_IS_REQUIRED_ENV) {
+  throw new Error('Cannot find env THIS_IS_REQUIRED_ENV')
+}
+
 bootstrap({
   appModule: AppModule,
   name: 'financial-aid-api',
   port: 3339,
 })
-;(() => {
-  await import('skibbidi-muck-mack-mick-moot')
-})()
