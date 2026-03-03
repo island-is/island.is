0bbeb3baad476f71cfdebd6516d5f8c604e1f88c
Jónas G. Sigurðsson <jonas@norda.is>
2026-03-03 16:42:28 +0000

    chore: delete alcohol tax application (#21676)

--- libs/feature-flags/src/lib/features.ts
+++ libs/feature-flags/src/lib/features.ts
@@ -11,7 +11,6 @@ export enum Features {
   passportApplication = 'isPassportApplicationEnabled',
   passportAnnulmentApplication = 'isPassportAnnulmentApplicationEnabled',
   financialStatementInao = 'financialStatementInao',
-  alcoholTaxRedemption = 'isAlcoholTaxRedemptionEnabled',
   consultationPortalApplication = 'isConsultationPortalEnabled',
   childrenResidenceChangeV2 = 'isChildrenResidenceChangeV2Enabled',
   complaintsToAlthingiOmbudsman = 'isComplaintToAlthingiOmbudsmanEnabled',
