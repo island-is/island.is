fd2f7dbb4503716e3332da8e35c376be04a8ca23
Þorkell Máni Þorkelsson <mani@hugsmidjan.is>
2026-03-05 19:40:29 +0000

    feat: add client

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -186,6 +186,7 @@ import { SignatureCollectionModule } from '@island.is/api/domains/signature-coll
 import { RskRelationshipsClientConfig } from '@island.is/clients-rsk-relationships'
 import { RskRentalDayRateClientConfig } from '@island.is/clients-rental-day-rate'
 import { FinanceClientV2Config } from '@island.is/clients/finance-v2'
+import { FinanceClientV3Config } from '@island.is/clients/finance-v3'
 import { MMSClientConfig } from '@island.is/clients/mms'
 import { PCardClientConfig } from '@island.is/clients/p-card'
 import { DistrictCommissionersLicensesClientConfig } from '@island.is/clients/district-commissioners-licenses'
@@ -428,6 +429,7 @@ const environment = getConfig
         HousingBenefitsConfig,
         FinanceClientConfig,
         FinanceClientV2Config,
+        FinanceClientV3Config,
         RegulationsAdminClientConfig,
         RegulationsClientConfig,
         IdsClientConfig,
