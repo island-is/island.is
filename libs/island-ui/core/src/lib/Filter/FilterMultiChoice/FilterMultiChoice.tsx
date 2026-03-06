1fd297b7e7d3158389b703fceb5e27caec644303
Kormákur <korri123@gmail.com>
2026-03-06 09:33:15 +0000

    style: remove blue label

--- libs/island-ui/core/src/lib/Filter/FilterMultiChoice/FilterMultiChoice.tsx
+++ libs/island-ui/core/src/lib/Filter/FilterMultiChoice/FilterMultiChoice.tsx
@@ -152,7 +152,9 @@ export const FilterMultiChoice: FC<
             labelUse={category.labelAs || 'h5'}
             labelVariant="h5"
             labelColor={
-              category.selected.length > 0 ? 'blue400' : 'currentColor'
+              !category.singleOption && category.selected.length > 0
+                ? 'blue400'
+                : 'currentColor'
             }
             iconVariant="small"
           >
