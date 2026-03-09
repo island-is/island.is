8968837324e3e34bb2e99064042cdaeb4d020dd6
Rúnar Vestmann <runar@stefna.is>
2026-03-09 15:51:18 +0000

    Add simple local search to petition list component

--- libs/shared/connected/src/lib/generalPetition/GeneralPetitionLists/GeneralPetitionLists.tsx
+++ libs/shared/connected/src/lib/generalPetition/GeneralPetitionLists/GeneralPetitionLists.tsx
@@ -1,8 +1,8 @@
-import { Box, Text, Stack, ActionCard } from '@island.is/island-ui/core'
+import { Box, Text, Stack, ActionCard, Input } from '@island.is/island-ui/core'
 import { useRouter } from 'next/router'
 import { useGetPetitionLists } from './useGetPetitionLists'
 import format from 'date-fns/format'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { ConnectedComponent, EndorsementList } from '@island.is/api/schema'
 import { useLocalization } from '../../../utils'
 
@@ -18,44 +18,74 @@ const formatDate = (date: string) => {
   }
 }
 
+const normalizeSearchTerm = (value: string) => {
+  return value.replaceAll("'", '').replaceAll('´', '').trim().toLowerCase()
+}
+
 export const GeneralPetitionLists: FC<
   React.PropsWithChildren<GeneralPetitionProps>
 > = ({ slice }) => {
   const router = useRouter()
   const { data: petitionLists, loading } = useGetPetitionLists()
   const t = useLocalization(slice.json)
+  const [queryString, setQueryString] = useState<string>('')
+
+  if (loading || !petitionLists) return null
+
+  const filteredPetitionLists =
+    petitionLists?.filter((petition: EndorsementList) => {
+      return normalizeSearchTerm(petition?.title ?? '').includes(
+        normalizeSearchTerm(queryString),
+      )
+    }) ?? []
 
   return (
-    <Box>
-      {petitionLists?.length > 0 && (
-        <Box marginTop={10} marginBottom={3}>
-          <Text variant="h4">{t('title', 'Virkir listar')}</Text>
-        </Box>
-      )}
+    <Box marginTop={10}>
       <Stack space={3}>
-        {!loading &&
-          petitionLists?.map((petition: EndorsementList) => {
-            return (
-              <ActionCard
-                key={petition.title}
-                backgroundColor="white"
-                heading={petition.title}
-                text={
-                  t('openTil', 'Virkur til:') +
-                  ' ' +
-                  formatDate(petition.closedDate)
-                }
-                cta={{
-                  label: t('viewList', 'Skoða lista'),
-                  variant: 'text',
-                  icon: 'arrowForward',
-                  onClick: () => {
-                    router.push('/undirskriftalistar/' + petition.id)
-                  },
-                }}
-              />
-            )
-          })}
+        <Input
+          name="queryString"
+          value={queryString}
+          onChange={(e) => setQueryString(e.target.value)}
+          label={t('search', 'Leita að undirskrifalista')}
+          size="sm"
+        />
+
+        <Box>
+          {filteredPetitionLists.length === 0 && (
+            <Box marginBottom={3}>
+              <Text>{t('noListsFound', 'Engir listar fundust')}</Text>
+            </Box>
+          )}
+          {filteredPetitionLists.length > 0 && (
+            <Box marginBottom={3}>
+              <Text variant="h4">{t('title', 'Virkir listar')}</Text>
+            </Box>
+          )}
+          <Stack space={3}>
+            {filteredPetitionLists.map((petition: EndorsementList) => {
+              return (
+                <ActionCard
+                  key={petition.title}
+                  backgroundColor="white"
+                  heading={petition.title}
+                  text={
+                    t('openTil', 'Virkur til:') +
+                    ' ' +
+                    formatDate(petition.closedDate)
+                  }
+                  cta={{
+                    label: t('viewList', 'Skoða lista'),
+                    variant: 'text',
+                    icon: 'arrowForward',
+                    onClick: () => {
+                      router.push('/undirskriftalistar/' + petition.id)
+                    },
+                  }}
+                />
+              )
+            })}
+          </Stack>
+        </Box>
       </Stack>
     </Box>
   )
