2b95cbd451e07955ff0c4b34d14dfa5ceda442a9
Gunnlaugur Guðmundsson <34029342+GunnlaugurG@users.noreply.github.com>
2026-03-10 12:04:52 +0000

    Revert "feat(consultation-portal): Remove idToken from session (#21780)"
    This reverts commit 7d1117064f7a158c09a95e8438c42ba0937ac986.

--- apps/consultation-portal/hooks/useLogOut.ts
+++ apps/consultation-portal/hooks/useLogOut.ts
@@ -1,9 +1,11 @@
-import { signOut } from 'next-auth/client'
+import { signOut, useSession } from 'next-auth/client'
 import useUser from './useUser'
+import { AuthSession } from '@island.is/next-ids-auth'
 import { signOutUrl } from '../lib/idsConfig'
 
 export const useLogOut = () => {
   const { setUser, setIsAuthenticated } = useUser()
+  const [session] = useSession() as [AuthSession, boolean]
 
   const logOut = () => {
     setUser && setUser(undefined)
@@ -11,7 +13,7 @@ export const useLogOut = () => {
     sessionStorage.clear()
 
     signOut({
-      callbackUrl: signOutUrl(window),
+      callbackUrl: signOutUrl(window, session?.idToken),
     })
   }
 
