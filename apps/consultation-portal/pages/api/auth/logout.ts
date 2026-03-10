36a846c81f051db04928c9b7e9bb2e69b8f88e75
Gunnlaugur Guðmundsson <34029342+GunnlaugurG@users.noreply.github.com>
2026-03-10 12:19:13 +0000

    feat(consultation-portal): REVERT - Remove idToken from session" (#21797)
    Revert "feat(consultation-portal): Remove idToken from session (#21780)"
    
    This reverts commit 7d1117064f7a158c09a95e8438c42ba0937ac986.

--- apps/consultation-portal/pages/api/auth/logout.ts
+++ apps/consultation-portal/pages/api/auth/logout.ts
@@ -2,16 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import env from '../../../lib/environment'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const idToken =
-    req.cookies['samradsgatt-id-token'] || (req.query.id_token as string)
-
-  // Clear the id-token cookie
-  res.setHeader(
-    'Set-Cookie',
-    'samradsgatt-id-token=; Path=/samradsgatt; HttpOnly; SameSite=Lax; Max-Age=0',
-  )
-
   res.redirect(
-    `https://${env.identityServerDomain}/connect/endsession?id_token_hint=${idToken}&post_logout_redirect_uri=${env.identityServerLogoutURL}`,
+    `https://${env.identityServerDomain}/connect/endsession?id_token_hint=${req.query.id_token}&post_logout_redirect_uri=${env.identityServerLogoutURL}`,
   )
 }
