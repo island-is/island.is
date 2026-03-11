6d94b8975055340146c3c943db5b6642a02a77e2
Þórður Hafliðason <tolleinn@gmail.com>
2026-03-11 10:32:25 +0000

    Remove unused

--- libs/application/templates/official-journal-of-iceland/src/index.ts
+++ libs/application/templates/official-journal-of-iceland/src/index.ts
@@ -5,5 +5,3 @@ export const getFields = () => import('./fields/')
 export type { OJOIApplication } from './lib/types'
 export { getMinistryFromSignature } from './utils/regulationValidations'
 export default OJOIApplicationTemplate
-
-export * from './hooks/useTypes'
