065246b45ec27147970578bb0a510de01072ba24
unakb <unakben@gmail.com>
2026-03-11 12:48:54 +0000

    Filter out info like copyright or footer which come as questions but arent, so dont have answers

--- libs/api/domains/questionnaires/src/lib/questionnaires.service.ts
+++ libs/api/domains/questionnaires/src/lib/questionnaires.service.ts
@@ -350,7 +350,7 @@ export class QuestionnairesService {
                   }
                 }) ?? [],
             )
-            .filter((answer) => answer != null) ?? [],
+            .filter((answer) => answer != null && answer.values.length > 0) ?? [],
       }
 
       return {
