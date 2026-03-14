07f83047e48018cf64496362f93cf02931afa7ab
thorkellmani <wowthorkell@gmail.com>
2026-03-14 10:22:23 +0000

    chore: remove odl resovler, add new

--- libs/api/domains/ship-registry/src/lib/resolvers/shipRegistry.resolver.ts
+++ libs/api/domains/ship-registry/src/lib/resolvers/shipRegistry.resolver.ts
@@ -1,14 +1,15 @@
 import { Args, Query, Resolver } from '@nestjs/graphql'
+import { ShipRegistryClientService } from '@island.is/clients/ship-registry'
 import { ShipSearch } from '../models/ship-search.model'
 import { ShipSearchInput } from '../dto/ship-search.input'
-import { ShipRegistryClientService } from '@island.is/clients/ship-registry'
 
+// Added opid field to ShipSearch model
 @Resolver()
 export class ShipRegistryResolver {
-  constructor(private readonly clientService: ShipRegistryClientService) {}
+  constructor(private readonly service: ShipRegistryClientService) {}
 
   @Query(() => ShipSearch, { name: 'shipRegistryShipSearch' })
   async shipSearch(@Args('input') input: ShipSearchInput): Promise<ShipSearch> {
-    return this.clientService.findShipByNameOrNumber(input)
+    return this.service.findShipByNameOrNumber(input)
   }
 }
