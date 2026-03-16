a74333a6bc238777af2f819c8d4eedbb739a51a8
gudjonss <gudjon.steinar.sverrisson@kolibri.is>
2026-03-16 12:43:25 +0000

    remove unused statistics code

--- apps/judicial-system/web/src/routes/Admin/Statistics/new/StatisticsOverview.tsx
+++ apps/judicial-system/web/src/routes/Admin/Statistics/new/StatisticsOverview.tsx
@@ -12,26 +12,6 @@ import { CaseTableType } from '@island.is/judicial-system-web/src/graphql/schema
 const StatisticsOverview: FC = () => {
   const router = useRouter()
 
-  /*
-  const statistics = {
-    title: 'Tölfræði',
-    pages: [
-      {
-        route: 'rannsoknarmal',
-        title: 'Rannsóknarmál',
-        description: 'Einföld tölfræðigreining úr rannsóknarmálum.',
-        type: CaseTableType.STATISTICS,
-      },
-      {
-        route: 'sakamal',
-        title: 'Sakamál',
-        description: 'Einföld tölfræðigreining úr sakamálum.',
-        type: CaseTableType.STATISTICS,
-      },
-    ],
-  }
-*/
-
   const data = {
     title: 'Gögn',
     pages: [
@@ -64,17 +44,6 @@ const StatisticsOverview: FC = () => {
           />
         ))}
       </CasesDashboardLayout>
-      {/* <CasesDashboardLayout title={statistics.title}>
-        {statistics.pages.map((t, idx) => (
-          <CasesCard
-            title={t.title}
-            description={t.description}
-            href={`${router.asPath}/${t.route}`}
-            key={idx}
-            type={t.type}
-          />
-        ))}
-      </CasesDashboardLayout> */}
     </CasesLayout>
   )
 }
