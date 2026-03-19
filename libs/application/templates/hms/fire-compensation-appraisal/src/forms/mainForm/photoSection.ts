dddd1b78e3df0143e11abc528988d03d85bd2662
Gunnar K Vilbergsson <gunnar@norda.is>
2026-03-19 12:43:12 +0000

    Readjust timeout

--- libs/application/templates/hms/fire-compensation-appraisal/src/forms/mainForm/photoSection.ts
+++ libs/application/templates/hms/fire-compensation-appraisal/src/forms/mainForm/photoSection.ts
@@ -17,8 +17,8 @@ export const photoSection = buildSection({
         buildFileUploadField({
           id: 'photos',
           uploadMultiple: true,
-          maxSize: 10000000,
-          totalMaxSize: 150000000,
+          maxSize: 10 * 1000 * 1000, // 10MB
+          totalMaxSize: 150 * 1000 * 1000, // 150MB
           maxFileCount: 20,
         }),
       ],
