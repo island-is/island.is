8e2e8f18e86bcfcd60592c4621ab08509fd30830
jonarnarbriem <jon.arnar.briem@origo.is>
2026-03-20 16:22:29 +0000

    bugfix and readme update

--- libs/application/templates/secondary-school/src/utils/helperFunctions/checkUseAnswersCopy.ts
+++ libs/application/templates/secondary-school/src/utils/helperFunctions/checkUseAnswersCopy.ts
@@ -4,13 +4,13 @@ import { getValueViaPath } from '@island.is/application/core'
 import { SecondarySchoolAnswers } from '../..'
 
 export const checkUseAnswersCopy = (application: Application): boolean => {
-  // It is only relevant to look at copy answers in states IN_REVIEW and COMPLETED
+  // It is only relevant to look at copy answers in states IN_REVIEW_FROM and COMPLETED
   // (where we got the ping for state change while application was in EDIT state,
   // and resetting answers using copy was not done - action ABORT in EDIT state)
   const state = application.state
-  if (state !== States.IN_REVIEW && state !== States.COMPLETED) return false
+  if (state !== States.IN_REVIEW_FROM_EDIT && state !== States.COMPLETED) return false
 
-  // Picking any field in answers, that should not be empty in IN_REVIEW/COMPLETED state
+  // Picking any field in answers, that should not be empty in IN_REVIEW_FROM_EDIT/COMPLETED state
   const applicant = getValueViaPath<SecondarySchoolAnswers['applicant']>(
     application.answers,
     'copy.applicant',
