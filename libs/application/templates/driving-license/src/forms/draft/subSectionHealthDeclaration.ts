579668cda56e7822440b9a15f48bd06a1cbfc964
Kristján Albert Loftsson <kal@juni.is>
2026-03-24 16:44:57 +0000

    feat(driving-license): add BE-specific health declaration with mandatory upload
    Create separate health declaration multifield for BE flow that shows
    health questions and requires certificate upload when any question is
    answered "yes". Other flows (B-temp, B-full) keep their existing
    behavior unchanged. Backend submission already enforces the upload
    requirement via the fail-fast check.

--- libs/application/templates/driving-license/src/forms/draft/subSectionHealthDeclaration.ts
+++ libs/application/templates/driving-license/src/forms/draft/subSectionHealthDeclaration.ts
@@ -14,7 +14,7 @@ import {
   hasHealthRemarks,
   needsHealthCertificateCondition,
 } from '../../lib/utils/formUtils'
-import { B_FULL_RENEWAL_65 } from '../../lib/constants'
+import { B_FULL_RENEWAL_65, BE } from '../../lib/constants'
 
 const FILE_SIZE_LIMIT = 10000000 // 10MB
 
@@ -26,7 +26,9 @@ export const subSectionHealthDeclaration = buildSubSection({
     buildMultiField({
       id: 'overview',
       title: m.healthDeclarationMultiFieldTitle,
-      condition: (answers) => answers.applicationFor !== B_FULL_RENEWAL_65,
+      condition: (answers) =>
+        answers.applicationFor !== B_FULL_RENEWAL_65 &&
+        answers.applicationFor !== BE,
       space: 2,
       children: [
         buildDescriptionField({
@@ -154,6 +156,152 @@ export const subSectionHealthDeclaration = buildSubSection({
           id: 'healthDeclaration.BE.info',
           message: m.healthCertificateDescription,
           alertType: 'info',
+          condition: (answers, externalData) =>
+            needsHealthCertificateCondition(YES)(answers, externalData),
+        }),
+        buildFileUploadField({
+          id: 'healthCertificate',
+          title: m.healthCertificateTitle,
+          maxSize: FILE_SIZE_LIMIT,
+          uploadAccept: '.pdf, .jpg, .jpeg, .png',
+          uploadHeader: m.healthCertificateUploadHeader,
+          uploadDescription: m.healthCertificateUploadDescription,
+          uploadButtonLabel: m.healthCertificateUploadButtonLabel,
+          condition: (answers, externalData) =>
+            needsHealthCertificateCondition(YES)(answers, externalData),
+        }),
+      ],
+    }),
+    buildMultiField({
+      id: 'healthDeclarationBE',
+      title: m.healthDeclarationMultiFieldTitle,
+      condition: (answers) => answers.applicationFor === BE,
+      space: 2,
+      children: [
+        buildDescriptionField({
+          id: 'healthDeclarationDescriptionBE',
+          description: m.healthDeclarationSubTitle,
+          marginBottom: 2,
+        }),
+        buildCustomField({
+          id: 'remarksBE',
+          component: 'HealthRemarks',
+          condition: (answers, externalData) => hasHealthRemarks(externalData),
+        }),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.usesContactGlasses',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            title: m.healthDeclarationMultiFieldSubTitle,
+            label: m.healthDeclaration1,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.hasReducedPeripheralVision',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration2,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.hasEpilepsy',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration3,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.hasHeartDisease',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration4,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.hasMentalIllness',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration5,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.usesMedicalDrugs',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration6,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.isAlcoholic',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration7,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.hasDiabetes',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration8,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.isDisabled',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration9,
+          },
+        ),
+        buildCustomField(
+          {
+            id: 'healthDeclaration.hasOtherDiseases',
+            title: '',
+            component: 'HealthDeclaration',
+          },
+          {
+            label: m.healthDeclaration10,
+          },
+        ),
+        buildAlertMessageField({
+          id: 'healthDeclaration.contactGlassesMismatchBE',
+          message: m.alertHealthDeclarationGlassesMismatch,
+          alertType: 'warning',
+          condition: (answers) =>
+            getValueViaPath(
+              answers,
+              'healthDeclaration.contactGlassesMismatch',
+            ) === true,
+        }),
+        buildDescriptionField({
+          id: 'healthCertificateRequiredBE',
+          description: m.healthCertificateDescription,
           condition: (answers, externalData) =>
             needsHealthCertificateCondition(YES)(answers, externalData),
         }),
