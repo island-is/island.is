b189cbb61fc40836122d639597e5f9e4995d1b7f
Guðjón Guðjónsson <gudjon@kolibri.is>
2026-03-24 15:15:17 +0000

    fix(j-s): Defendant Level Case Tables (#21927)
    * Updates case table queries
    
    * Consolidates is registered in prison system fields
    
    * Simplifies defendant level case selection for prison admin
    
    * Fixes case list info in search
    
    * Completes defendant filtering
    
    * Resolves linting issues
    
    * Cleans up cell generators
    
    * Removes redundant tests
    
    * Exports tested method
    
    * Fixes code rabbit issues
    
    * Adds a readme
    
    * Adds attribute date tag to ensure correct cell generation
    
    * Updates the readme
    
    * Fixes appeal period expired defendant selection

--- apps/judicial-system/backend/src/app/modules/repository/types/caseRepository.types.ts
+++ apps/judicial-system/backend/src/app/modules/repository/types/caseRepository.types.ts
@@ -490,7 +490,6 @@ export interface UpdateCase
     | 'mergeCaseNumber'
     | 'isCompletedWithoutRuling'
     | 'hasCivilClaims'
-    | 'isRegisteredInPrisonSystem'
     | 'isArchived'
   > {
   type?: CaseType
