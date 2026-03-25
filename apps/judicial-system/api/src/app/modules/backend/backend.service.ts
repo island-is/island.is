b3efa872e923bf75c11b4f338ee4fcc888af4804
Guðjón Steinar Sverrisson <gudjon.steinar.sverrisson@kolibri.is>
2026-03-25 09:49:51 +0000

    feat(j-s): Lazy load case defendants (#21787)
    * feat: create case v2 api
    
    * bulk create defendants
    
    * feat: create case v2 api
    
    * bulk create defendants
    
    * update path to new case create flow
    
    * remove defendant fetch
    
    * fix path in test
    
    * description of test
    
    * add necessary boilerplate
    
    * fetch defendants
    
    * refactor fetch logic into its own hook
    
    * rename old create with deprecated prefix, rename v2 to create
    
    * chore: format update dirty file
    
    * rename old create with deprecated prefix, rename v2 to create
    
    * chore: format update dirty file
    
    * prevent case state from progressing if 0 defendants
    
    * Basic loading state
    
    * chore: lintfix update dirty file
    
    * chore: format update dirty file
    
    * RVAdilarMals api path
    
    * fit zod schemas to api response
    
    * ui reflects invalid defendants state
    
    * Update test to reflect case state change
    
    * create defendants in parallel
    
    * Clean up naming / DRY code
    
    * outdated comment
    
    * remove call to sync defendants from police
    
    * Revert "remove call to sync defendants from police"
    
    This reverts commit b366f86385c9e802029347f224676b86201a68a4.
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- apps/judicial-system/api/src/app/modules/backend/backend.service.ts
+++ apps/judicial-system/api/src/app/modules/backend/backend.service.ts
@@ -55,6 +55,7 @@ import { Institution } from '../institution'
 import {
   PoliceCaseFile,
   PoliceCaseInfo,
+  PoliceDefendant,
   UploadPoliceCaseFileResponse,
 } from '../police'
 import { CaseDataExportInput, CaseStatistics } from '../statistics'
@@ -458,6 +459,10 @@ export class BackendService extends DataSource<{ req: Request }> {
 
   getPoliceCaseFiles(caseId: string): Promise<PoliceCaseFile[]> {
     return this.get(`case/${caseId}/policeFiles`)
+  }
+
+  getPoliceDefendants(caseId: string): Promise<PoliceDefendant[]> {
+    return this.get(`case/${caseId}/policeDefendants`)
   }
 
   getPoliceDigitalCaseFiles(caseId: string): Promise<PoliceDigitalCaseFile[]> {
