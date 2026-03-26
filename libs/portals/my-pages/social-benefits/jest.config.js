b6df94bef2e979d482becab4f74a69e5bfd05830
Baldur Óli Barkarson <ballioli1@gmail.com>
2026-03-26 14:55:11 +0000

    jest string fix

--- libs/portals/my-pages/social-benefits/jest.config.js
+++ libs/portals/my-pages/social-benefits/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory:
-    '<rootDir>/coverage/libs/portals/my-pages/social-insurance-maintenance',
+    '<rootDir>/coverage/libs/portals/my-pages/social-benefits'
 }
