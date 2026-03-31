8c2bab13f461309fe00e414dda9f89b1690df05c
unakb <unakben@gmail.com>
2026-03-31 17:54:19 +0000

    deleted more duplicate code

--- libs/portals/my-pages/social-insurance-maintenance/src/lib/paths.ts
+++
@@ -1,8 +0,0 @@
-export enum SocialInsuranceMaintenancePaths {
-  SocialInsuranceMaintenanceRoot = '/framfaersla',
-  SocialInsuranceMaintenancePaymentPlan = '/framfaersla/greidsluaetlun',
-  SocialInsuranceMaintenancePaymentsReasoning = '/framfaersla/forsendur-greidslna',
-  SocialInsuranceMaintenanceIncomePlan = '/framfaersla/tekjuaaetlun',
-  SocialInsuranceMaintenanceIncomePlanDetail = '/framfaersla/tekjuaaetlun/nuverandi',
-  SocialInsuranceMaintenancePersonalTaxCredit = '/framfaersla/personuafslattur',
-}
