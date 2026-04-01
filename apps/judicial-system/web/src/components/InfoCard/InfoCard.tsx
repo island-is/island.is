f90ed03965a5c048970a6d9c04a50667b04011e2
Thorhildur Thorleiksdottir <thorhildur@kolibri.is>
2026-04-01 13:20:39 +0000

    fix(j-s): info card scaling and value wrap

--- apps/judicial-system/web/src/components/InfoCard/InfoCard.tsx
+++ apps/judicial-system/web/src/components/InfoCard/InfoCard.tsx
@@ -47,9 +47,9 @@ const InfoCard: FC<Props> = (props) => {
               )}
               {item.values.map((value, index) =>
                 typeof value === 'string' ? (
-                  <Text key={`${value}-${index}`}>{value}</Text>
+                  <Text key={`${value}-${index}`} className={styles.breakWord}>{value}</Text>
                 ) : (
-                  <Box key={`${value}-${index}`}>{value}</Box>
+                  <Box key={`${value}-${index}`} className={styles.breakWord}>{value}</Box>
                 ),
               )}
             </Box>
