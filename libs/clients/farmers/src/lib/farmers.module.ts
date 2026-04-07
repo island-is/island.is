dae96140c93d9da00e4f24b0e07df0f3dca9bf7e
thorkellmani <wowthorkell@gmail.com>
2026-04-07 13:19:21 +0000

    fix: add query normalizer

--- libs/clients/farmers/src/lib/farmers.module.ts
+++ libs/clients/farmers/src/lib/farmers.module.ts
@@ -3,8 +3,10 @@ import { FarmersClientService } from './farmers.service'
 import { XRoadConfig, IdsClientConfig } from '@island.is/nest/config'
 import { ConfigType } from '@nestjs/config'
 import { client } from '../../gen/fetch/client.gen'
+import { createQuerySerializer } from '../../gen/fetch/client/utils.gen'
 import { FarmersClientConfig } from './farmers.config'
 import { createEnhancedFetch } from '@island.is/clients/middlewares'
+import { type Logger, LOGGER_PROVIDER } from '@island.is/logging'
 
 @Module({
   providers: [FarmersClientService],
@@ -19,11 +21,20 @@ export class FarmersClientModule {
     @Inject(IdsClientConfig.KEY)
     idsClientConfig: ConfigType<typeof IdsClientConfig>,
   ) {
+    const defaultSerializer = createQuerySerializer()
     client.setConfig({
       baseUrl: `${xroadConfig.xRoadBasePath}/r1/${config.xRoadServicePath}`,
       headers: {
         'X-Road-Client': xroadConfig.xRoadClient,
         Accept: 'application/json',
+      },
+      querySerializer: (query) => {
+        const normalized: Record<string, unknown> = {}
+        for (const [key, value] of Object.entries(query)) {
+          normalized[key] =
+            value instanceof Date ? value.toISOString().split('T')[0] : value
+        }
+        return defaultSerializer(normalized)
       },
       fetch: createEnhancedFetch({
         name: 'clients-farmers',
