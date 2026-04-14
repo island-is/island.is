8ca618e480f9916577d28b93ab0488563b93abf1
Jonni <jonas@norda.is>
2026-04-14 10:44:20 +0000

    chore: remove the module with missing env

--- libs/application/template-api-modules/src/lib/modules/templates/hms/housing-benefits/housing-benefits.module.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/hms/housing-benefits/housing-benefits.module.ts
@@ -6,7 +6,6 @@ import { NationalRegistryV3Module } from '../../../shared/api/national-registry-
 import { HousingBenefitsService } from './housing-benefits.service'
 import { ApplicationsNotificationsModule } from '../../../../notification/notifications.module'
 import { HmsRentalAgreementClientModule } from '@island.is/clients/hms-rental-agreement'
-import { PersonalTaxReturnModule } from '@island.is/clients/rsk/personal-tax-return'
 
 @Module({
   imports: [
@@ -14,7 +13,6 @@ import { PersonalTaxReturnModule } from '@island.is/clients/rsk/personal-tax-ret
     ApplicationsNotificationsModule,
     HmsRentalAgreementClientModule,
     NationalRegistryV3Module,
-    PersonalTaxReturnModule,
   ],
   providers: [HousingBenefitsService],
   exports: [HousingBenefitsService],
