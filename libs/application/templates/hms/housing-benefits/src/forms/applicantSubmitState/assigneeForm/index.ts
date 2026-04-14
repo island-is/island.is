ad6ec9efa0ae75337f636da17bd2439e75e1a6a3
Jonni <jonas@norda.is>
2026-04-14 18:12:06 +0000

    feat: applicant submit assignee form

--- libs/application/templates/hms/housing-benefits/src/forms/applicantSubmitState/assigneeForm/index.ts
+++ libs/application/templates/hms/housing-benefits/src/forms/applicantSubmitState/assigneeForm/index.ts
@@ -1,17 +1,21 @@
 import {
   buildAlertMessageField,
+  buildDescriptionField,
   buildForm,
+  buildImageField,
   buildMultiField,
   buildSection,
+  getValueViaPath,
 } from '@island.is/application/core'
-import { FormModes } from '@island.is/application/types'
+import { Application, FormModes } from '@island.is/application/types'
 import { HmsLogo } from '@island.is/application/assets/institution-logos'
 import { applicantSubmitMessages as m } from '../../../lib/messages/applicantSubmitMessages'
+import { HikingAndWateringPlants } from '@island.is/application/assets/graphics'
 
-/** Read-only conclusion for assignees: applicant must submit the application. */
 export const ApplicantSubmitFormAssigneeVersion = buildForm({
   id: 'ApplicantSubmitAssignee',
   mode: FormModes.IN_PROGRESS,
+  renderLastScreenButton: true,
   logo: HmsLogo,
   children: [
     buildSection({
@@ -24,10 +28,34 @@ export const ApplicantSubmitFormAssigneeVersion = buildForm({
           children: [
             buildAlertMessageField({
               id: 'applicantSubmitAssigneeNextSteps',
-              title: m.assigneeAlertTitle,
-              message: m.assigneeAlertMessage,
-              alertType: 'info',
-              doesNotRequireAnswer: true,
+              title: m.assigneeConsentAlertTitle,
+              message: m.assigneeConsentAlertMessage,
+              alertType: 'success',
+              marginBottom: 4,
+            }),
+            buildDescriptionField({
+              id: 'applicantSubmitAssigneeNextSteps2',
+              description: (application: Application) => {
+                const applicantName =
+                  getValueViaPath<string>(
+                    application.answers,
+                    'applicant.name',
+                  ) ??
+                  getValueViaPath<string>(
+                    application.externalData,
+                    'nationalRegistry.data.fullName',
+                  ) ??
+                  ''
+                return {
+                  ...m.assigneeNextStepsDescription,
+                  values: { applicantName },
+                }
+              },
+              marginBottom: 8,
+            }),
+            buildImageField({
+              id: 'applicantSubmitAssigneeNextSteps3',
+              image: HikingAndWateringPlants,
               marginBottom: 4,
             }),
           ],
--- libs/application/templates/hms/housing-benefits/src/lib/messages/applicantSubmitMessages.ts
+++ libs/application/templates/hms/housing-benefits/src/lib/messages/applicantSubmitMessages.ts
@@ -19,4 +19,24 @@ export const applicantSubmitMessages = defineMessages({
     description:
       'Assignee info: applicant must log in and submit the application',
   },
+  assigneeConsentAlertTitle: {
+    id: 'hb.application:applicantSubmit.assigneeConsentAlertTitle',
+    defaultMessage: 'Heimilismenn hafa veitt samþykki fyrir gagnaöflun',
+    description:
+      'Alert title confirming all household members have given consent',
+  },
+  assigneeConsentAlertMessage: {
+    id: 'hb.application:applicantSubmit.assigneeConsentAlertMessage',
+    defaultMessage:
+      'Allir heimilismenn listaðir á umsókninni hafa veitt samþykki fyrir gagnaöflun.',
+    description:
+      'Alert message confirming all household members have given consent',
+  },
+  assigneeNextStepsDescription: {
+    id: 'hb.application:applicantSubmit.assigneeNextStepsDescription',
+    defaultMessage:
+      'Umsækjandi, {applicantName}, þarf nú að skrá sig inn á Ísland.is, opna og yfirfara umsóknina áður en hún fer inn til vinnslu hjá HMS.',
+    description:
+      'Description telling assignee that the applicant must review and submit',
+  },
 })
