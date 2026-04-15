d6045375d8d068817bab2d4c5db6ae4fad5a26d5
Guðjón Steinar Sverrisson <gudjon.steinar.sverrisson@kolibri.is>
2026-04-15 13:53:09 +0000

    chore(j-s): multiple police case numbers pt 1 - backend/datastructures (#22137)
    * create new table migration
    
    * setup writes to new table
    
    * refactor naming schema of new table
    
    * nonulls in backfill
    
    * update test case
    
    * types in test
    
    * chore: lintfix update dirty file
    
    * avoid unnecessary cast
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- apps/judicial-system/backend/src/app/modules/repository/repository.module.ts
+++ apps/judicial-system/backend/src/app/modules/repository/repository.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@island.is/nest/config'
 import { AppealCase } from './models/appealCase.model'
 import { Case } from './models/case.model'
 import { CaseArchive } from './models/caseArchive.model'
+import { CaseDefendantPoliceCaseNumber } from './models/caseDefendantPoliceCaseNumber.model'
 import { CaseFile } from './models/caseFile.model'
 import { CaseString } from './models/caseString.model'
 import { CourtDocument } from './models/courtDocument.model'
@@ -22,6 +23,7 @@ import { Subpoena } from './models/subpoena.model'
 import { Verdict } from './models/verdict.model'
 import { Victim } from './models/victim.model'
 import { CaseArchiveRepositoryService } from './services/caseArchiveRepository.service'
+import { CaseDefendantPoliceCaseNumberRepositoryService } from './services/caseDefendantPoliceCaseNumber.repository.service'
 import { CaseRepositoryService } from './services/caseRepository.service'
 import { CourtDocumentRepositoryService } from './services/courtDocumentRepository.service'
 import { CourtSessionRepositoryService } from './services/courtSessionRepository.service'
@@ -40,6 +42,7 @@ import { repositoryModuleConfig } from './repository.config'
       Case,
       CaseArchive,
       CaseFile,
+      CaseDefendantPoliceCaseNumber,
       CaseString,
       CourtDocument,
       CourtSession,
@@ -59,6 +62,7 @@ import { repositoryModuleConfig } from './repository.config'
   ],
   providers: [
     CaseArchiveRepositoryService,
+    CaseDefendantPoliceCaseNumberRepositoryService,
     CaseRepositoryService,
     CourtSessionRepositoryService,
     CourtDocumentRepositoryService,
@@ -71,6 +75,7 @@ import { repositoryModuleConfig } from './repository.config'
   ],
   exports: [
     CaseArchiveRepositoryService,
+    CaseDefendantPoliceCaseNumberRepositoryService,
     CaseRepositoryService,
     CourtSessionRepositoryService,
     CourtDocumentRepositoryService,
