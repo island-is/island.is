1f25e3cb092e6492f980b4ea50c427d86c896c33
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2026-04-15 13:02:44 +0000

    fix(form-system): update staging path for XROAD_CHARGE_FJS_V2

--- apps/services/form-system/infra/form-system.ts
+++ apps/services/form-system/infra/form-system.ts
@@ -51,7 +51,7 @@ export const serviceSetup = (services: {
       },
       XROAD_CHARGE_FJS_V2_PATH: {
         dev: 'IS-DEV/GOV/10021/FJS-Public/chargeFJS_v2',
-        staging: 'IS-DEV/GOV/10021/FJS-Public/chargeFJS_v2',
+        staging: 'IS-TEST/GOV/10021/FJS-Public/chargeFJS_v2',
         prod: 'IS/GOV/5402697509/FJS-Public/chargeFJS_v2',
       },
       CLIENT_LOCATION_ORIGIN: {
