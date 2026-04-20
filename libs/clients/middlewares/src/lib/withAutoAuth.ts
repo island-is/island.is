0fd0604868c34a58daedd1f26ff4c23a6f6a3096
Gunnar K Vilbergsson <gunnar@norda.is>
2026-04-20 11:55:22 +0000

    fix(as): Add optional header name for autoauth

--- libs/clients/middlewares/src/lib/withAutoAuth.ts
+++ libs/clients/middlewares/src/lib/withAutoAuth.ts
@@ -51,6 +51,11 @@ export interface AutoAuthOptions {
   scope: string[]
 
   /**
+   * The header name to use for the authorization token.
+   */
+  headerName?: string
+
+  /**
    * Controls how EnhancedFetch should auto-authenticate:
    *
    * - 'token': always request a non-user token. Ignores Auth object that are passed to the fetch function.
@@ -246,7 +251,22 @@ export const withAutoAuth = ({
 
   return async (request) => {
     const authorization = await getAuth(request)
-    request.headers.set('authorization', authorization)
+    const headerName = options.headerName ?? 'authorization'
+    console.log('--------------------------------')
+    console.log('Authorization:')
+    console.dir(authorization, { depth: null })
+    console.log('--------------------------------')
+    console.log('Header name:')
+    console.dir(headerName, { depth: null })
+    console.log('--------------------------------')
+    console.log('Request headers:')
+    console.dir(request.headers, { depth: null })
+    console.log('--------------------------------')
+    request.headers.set(headerName, authorization)
+    console.log('--------------------------------')
+    console.log('Request headers after:')
+    console.dir(request.headers, { depth: null })
+    console.log('--------------------------------')
     return fetch(request)
   }
 }
