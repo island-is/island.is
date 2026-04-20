6713b88efb527571c91e1ca46fb51cea67871ac7
Guðjón Guðjónsson <gudjon@kolibri.is>
2026-04-20 13:11:09 +0000

    Fixes null appeal case id in context menu

--- apps/judicial-system/web/src/routes/Shared/CaseTable/CaseTable.tsx
+++ apps/judicial-system/web/src/routes/Shared/CaseTable/CaseTable.tsx
@@ -165,17 +165,19 @@ const CaseTable: FC = () => {
 
   const getRow = (r: CaseTableRow) => {
     const getContextMenuItems = () => {
-      return r.contextMenuActions.map((a) => {
-        switch (a) {
-          case ContextMenuCaseActionType.DELETE_CASE:
-            return deleteCase(r.caseId)
-          case ContextMenuCaseActionType.WITHDRAW_APPEAL:
-            return r.appealCaseId && withdrawAppeal(r.caseId, r.appealCaseId)
-          case ContextMenuCaseActionType.OPEN_CASE_IN_NEW_TAB:
-          default: // Default to opening the case in a new tab
-            return openCaseInNewTab(r.caseId)
-        }
-      })
+      return r.contextMenuActions
+        .map((a) => {
+          switch (a) {
+            case ContextMenuCaseActionType.DELETE_CASE:
+              return deleteCase(r.caseId)
+            case ContextMenuCaseActionType.WITHDRAW_APPEAL:
+              return r.appealCaseId && withdrawAppeal(r.caseId, r.appealCaseId)
+            case ContextMenuCaseActionType.OPEN_CASE_IN_NEW_TAB:
+            default: // Default to opening the case in a new tab
+              return openCaseInNewTab(r.caseId)
+          }
+        })
+        .filter((i) => i)
     }
 
     const getRowClickAction = () => {
