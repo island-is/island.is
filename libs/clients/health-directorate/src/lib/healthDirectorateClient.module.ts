7f6f03af648a37e981bb1f13be2f25435473f9c1
Una Kristín <una@kolibri.is>
2026-04-21 13:50:56 +0000

    feat(my-pages): Organ donation from health service (#22117)
    * fix(my-pages): Organ donation service update
    
    * chore: format update dirty file
    
    * chore: Remove old organ donation service
    
    * Remove organ donation client
    
    * chore: charts update dirty file
    
    * Update project.json
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/clients/health-directorate/src/lib/healthDirectorateClient.module.ts
+++ libs/clients/health-directorate/src/lib/healthDirectorateClient.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common'
 import { exportedApis } from './clients/occupational-license/healthDirectorateClient.provider'
 import { VaccinationsApiProvider } from './clients/vaccinations/vaccinations.provider'
-import {
-  OrganDonorApiProvider,
-  OrganExceptionsApiProvider,
-} from './clients/organ-donation/organDonation.provider'
 import { HealthDirectorateClientService } from './clients/occupational-license/healthDirectorateClient.service'
 import { HealthDirectorateVaccinationsService } from './clients/vaccinations/vaccinations.service'
-import { HealthDirectorateOrganDonationService } from './clients/organ-donation/organDonation.service'
 import {
   HealthDirectorateHealthModule,
   HealthDirectorateHealthService,
@@ -18,17 +13,13 @@ import {
   providers: [
     HealthDirectorateClientService,
     HealthDirectorateVaccinationsService,
-    HealthDirectorateOrganDonationService,
     HealthDirectorateHealthService,
-    OrganDonorApiProvider,
-    OrganExceptionsApiProvider,
     VaccinationsApiProvider,
     ...exportedApis,
   ],
   exports: [
     HealthDirectorateClientService,
     HealthDirectorateVaccinationsService,
-    HealthDirectorateOrganDonationService,
     HealthDirectorateHealthService,
   ],
 })
