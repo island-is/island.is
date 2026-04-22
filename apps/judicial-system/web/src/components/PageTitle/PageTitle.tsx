d11880304858435559fb74155bd2ceecb5e9eedd
Ívar Oddsson <ivaroddsson@gmail.com>
2026-04-22 12:56:20 +0000

    fix(j-s): simplify PageTitle and TagCaseState components
    Removes the includeTag prop and tag rendering from PageTitle, and
    removes the customMapCaseStateToTag override from TagCaseState.

--- apps/judicial-system/web/src/components/PageTitle/PageTitle.tsx
+++ apps/judicial-system/web/src/components/PageTitle/PageTitle.tsx
@@ -1,26 +1,12 @@
-import { FC, PropsWithChildren, useContext } from 'react'
+import { FC, PropsWithChildren } from 'react'
 
 import { Box, ResponsiveProp, Space, Text } from '@island.is/island-ui/core'
-import {
-  CaseListEntry,
-  CaseState,
-} from '@island.is/judicial-system-web/src/graphql/schema'
-
-import { FormContext } from '../FormProvider/FormProvider'
-import TagCaseState from '../Tags/TagCaseState/TagCaseState'
-import { mapIndictmentRulingDecisionToTagVariant } from '../Tags/TagCaseState/TagCaseState.logic'
-
 interface Props {
   marginBottom?: ResponsiveProp<Space>
-  includeTag?: boolean
 }
 
 const PageTitle: FC<PropsWithChildren<Props>> = (props) => {
-  const { marginBottom, children, includeTag = false } = props
-  const { workingCase } = useContext(FormContext)
-
-  const showRulingDecisionTag =
-    includeTag && workingCase.state !== CaseState.CORRECTING
+  const { marginBottom, children } = props
 
   return (
     <Box
@@ -31,12 +17,6 @@ const PageTitle: FC<PropsWithChildren<Props>> = (props) => {
       <Text as="h1" variant="h1">
         {children}
       </Text>
-      {showRulingDecisionTag && (
-        <TagCaseState
-          theCase={workingCase as CaseListEntry}
-          customMapCaseStateToTag={mapIndictmentRulingDecisionToTagVariant}
-        />
-      )}
     </Box>
   )
 }
