322ffeda44d36a840d5f9ee332c0b1992a22bfe0
HjorturJ <hjortur@norda.is>
2026-04-24 14:15:01 +0000

    Adding new param to backend path

--- libs/api/domains/application/src/lib/applicationV2.service.ts
+++ libs/api/domains/application/src/lib/applicationV2.service.ts
@@ -47,7 +47,7 @@ export class ApplicationV2Service {
             typeId: input?.typeId?.join(','),
             status: input?.status?.join(','),
             scopeCheck: input?.scopeCheck,
-            showPruned: true,
+            showPruned: input?.showPruned,
           },
         ),
         this.formSystemApplicationApiWithAuth(
