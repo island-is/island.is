0e96f90358b71d935bf3506d2973ee43c78d9f13
berglindoma13 <berglindis@gmail.com>
2026-04-28 13:10:46 +0000

    feat(edit-info-unemployment-benefits): edit info unemployment benefits application (#22118)
    * new application
    
    * new application
    
    * new application
    
    * application phase 1 ready
    
    * application done
    
    * chore: format update dirty file
    
    * feat: allow fields in accordion
    
    * chore: move to subsection in example application
    
    * fix: don't render empty box with marginbottom
    
    * fixes from comments phase 1
    
    * Update libs/application/templates/examples/example-inputs/src/forms/mainForm/tablesAndRepeatersSection/accordionSubsection.ts
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix: code rabbit comments
    
    * chore: use coat of arms but not aosh logo
    
    * fix: lint
    
    * chore: format update dirty file
    
    * assets update
    
    * mvoing assets
    
    * moving back to custom component
    
    * final comments
    
    * chore: format update dirty file
    
    * chore: format update dirty file
    
    * moving assets
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>
    Co-authored-by: Jonni <jonas@norda.is>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ libs/application/templates/vmst/edit-unemployment-information/src/lib/dataSchema.ts
@@ -0,0 +1,119 @@
+import { z } from 'zod'
+import { YES, YesOrNoEnum } from '@island.is/application/core'
+import { application as applicationMessages } from './messages'
+
+export const educationSchema = z.object({
+  levelOfStudy: z.string(),
+  degree: z.string(),
+  courseOfStudy: z.string(),
+  endDate: z.string(),
+})
+
+export const licenseSchema = z
+  .object({
+    hasDrivingLicense: z.array(z.string()).optional(),
+    drivingLicenseTypes: z.array(z.string()).optional(),
+    hasHeavyMachineryLicense: z.array(z.string()).optional(),
+    heavyMachineryLicensesTypes: z.array(z.string()).optional().nullable(),
+  })
+  .refine(
+    ({ hasDrivingLicense, drivingLicenseTypes }) => {
+      if (
+        hasDrivingLicense &&
+        hasDrivingLicense.length > 0 &&
+        hasDrivingLicense[0] === YES
+      ) {
+        return (
+          drivingLicenseTypes !== undefined && drivingLicenseTypes.length > 0
+        )
+      }
+
+      return true
+    },
+    { path: ['drivingLicenseTypes'] },
+  )
+  .refine(
+    ({ hasHeavyMachineryLicense, heavyMachineryLicensesTypes }) => {
+      if (
+        hasHeavyMachineryLicense &&
+        hasHeavyMachineryLicense.length > 0 &&
+        hasHeavyMachineryLicense[0] === YES
+      ) {
+        return (
+          heavyMachineryLicensesTypes && heavyMachineryLicensesTypes.length > 0
+        )
+      }
+
+      return true
+    },
+    { path: ['heavyMachineryLicensesTypes'] },
+  )
+
+export const languageSkillsSchema = z.object({
+  language: z.string(),
+  skill: z
+    .string()
+    .optional()
+    .refine((v) => v !== undefined && v !== '', {
+      params: applicationMessages.requiredError,
+    }),
+})
+
+export const bankAccountSchema = z.object({
+  bankNumber: z.string().min(1),
+  ledger: z.string().min(1),
+  accountNumber: z.string().min(1),
+})
+
+export const otherAddressSchema = z
+  .object({
+    otherAddress: z.string().optional(),
+    otherPostcode: z.string().nullish(),
+    currentAddressIsNotDifferent: z.array(z.string()).optional(),
+  })
+  .refine(
+    ({ currentAddressIsNotDifferent, otherPostcode }) => {
+      if (
+        currentAddressIsNotDifferent &&
+        currentAddressIsNotDifferent.length > 0 &&
+        currentAddressIsNotDifferent[0] === YES
+      ) {
+        return true
+      }
+      return !!otherPostcode
+    },
+    {
+      path: ['otherPostcode'],
+    },
+  )
+  .refine(
+    ({ currentAddressIsNotDifferent, otherAddress }) => {
+      if (
+        currentAddressIsNotDifferent &&
+        currentAddressIsNotDifferent.length > 0 &&
+        currentAddressIsNotDifferent[0] === YES
+      ) {
+        return true
+      }
+      return !!otherAddress
+    },
+    {
+      path: ['otherAddress'],
+    },
+  )
+
+export const editUnemploymentInfoDataSchema = z.object({
+  approveExternalData: z.boolean().refine((v) => v),
+  otherAddress: otherAddressSchema,
+  password: z.string().min(4),
+  bankAccount: bankAccountSchema,
+  jobWishes: z.array(z.string()),
+  educationHistory: z.array(educationSchema),
+  languageSkills: z.array(languageSkillsSchema),
+  licenses: licenseSchema,
+  euresAgreement: z
+    .nativeEnum(YesOrNoEnum)
+    .refine((v) => Object.values(YesOrNoEnum).includes(v)),
+})
+
+export type ApplicationAnswers = z.TypeOf<typeof editUnemploymentInfoDataSchema>
