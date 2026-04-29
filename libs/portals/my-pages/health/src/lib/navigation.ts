1e179b216fe4b39c847e81a3c59d40c245cf1dfc
unakb <unakben@gmail.com>
2026-04-29 09:38:47 +0000

    fix(my-pages): Rearrange navigation

--- libs/portals/my-pages/health/src/lib/navigation.ts
+++ libs/portals/my-pages/health/src/lib/navigation.ts
@@ -31,6 +31,79 @@ export const healthNavigation: PortalNavigationItem = {
       path: HealthPaths.HealthBasicOld,
     },
     {
+      name: messages.myMedicine,
+      path: HealthPaths.HealthMedicine,
+      children: [
+        {
+          name: m.medicinePrescriptions,
+          description: m.medicinePrescriptionsIntro,
+          path: HealthPaths.HealthMedicinePrescription,
+        },
+
+        {
+          name: m.medicineDelegation,
+          path: HealthPaths.HealthMedicineDelegation,
+          searchTags: [s.medicineDelegationOther],
+          children: [
+            {
+              name: m.medicineDelegation,
+              path: HealthPaths.HealthMedicineDelegationDetail,
+              navHide: true,
+              breadcrumbHide: true,
+              searchHide: true,
+            },
+            {
+              name: messages.addDelegation,
+              path: HealthPaths.HealthMedicineDelegationAdd,
+              navHide: true,
+              breadcrumbHide: true,
+              searchTags: [
+                s.medicineDelegationOther,
+                s.medicineDelegationOtherNew,
+              ],
+            },
+          ],
+        },
+        {
+          name: m.medicinePaymentParticipation,
+          description: m.medicinePaymentParticipationIntro,
+          path: HealthPaths.HealthMedicinePaymentParticipation,
+          children: [
+            {
+              name: m.medicinePurchaseTitle,
+              path: HealthPaths.HealthMedicinePurchase,
+              activeIfExact: true,
+              navHide: true,
+            },
+            {
+              name: m.medicineCalculatorTitle,
+              path: HealthPaths.HealthMedicineCalculator,
+              activeIfExact: true,
+              navHide: true,
+            },
+          ],
+        },
+        {
+          name: m.medicineLicenseTitle,
+          description: m.medicineLicenseIntro,
+          path: HealthPaths.HealthMedicineCertificates,
+          children: [
+            {
+              name: m.medicineLicenseTitle,
+              path: HealthPaths.HealthMedicineCertificate,
+              navHide: true,
+              breadcrumbHide: true,
+            },
+          ],
+        },
+        {
+          name: m.medicinePrescriptionHistory,
+          description: m.medicinePrescriptionHistoryIntro,
+          path: HealthPaths.HealthMedicinePrescriptionHistory,
+        },
+      ],
+    },
+    {
       name: messages.appointments,
       searchHide: false,
       path: HealthPaths.HealthAppointments,
@@ -105,155 +178,34 @@ export const healthNavigation: PortalNavigationItem = {
       ],
     },
     {
-      name: m.medicine,
-      path: HealthPaths.HealthMedicine,
-      children: [
-        {
-          name: m.medicinePrescriptions,
-          description: m.medicinePrescriptionsIntro,
-          path: HealthPaths.HealthMedicinePrescription,
-        },
-
-        {
-          name: m.medicineDelegation,
-          path: HealthPaths.HealthMedicineDelegation,
-          searchTags: [s.medicineDelegationOther],
-          children: [
-            {
-              name: m.medicineDelegation,
-              path: HealthPaths.HealthMedicineDelegationDetail,
-              navHide: true,
-              breadcrumbHide: true,
-              searchHide: true,
-            },
-            {
-              name: messages.addDelegation,
-              path: HealthPaths.HealthMedicineDelegationAdd,
-              navHide: true,
-              breadcrumbHide: true,
-              searchTags: [
-                s.medicineDelegationOther,
-                s.medicineDelegationOtherNew,
-              ],
-            },
-          ],
-        },
-        {
-          name: m.medicinePaymentParticipation,
-          description: m.medicinePaymentParticipationIntro,
-          path: HealthPaths.HealthMedicinePaymentParticipation,
-          children: [
-            {
-              name: m.medicinePurchaseTitle,
-              path: HealthPaths.HealthMedicinePurchase,
-              activeIfExact: true,
-              navHide: true,
-            },
-            {
-              name: m.medicineCalculatorTitle,
-              path: HealthPaths.HealthMedicineCalculator,
-              activeIfExact: true,
-              navHide: true,
-            },
-          ],
-        },
-        {
-          name: m.medicineLicenseTitle,
-          description: m.medicineLicenseIntro,
-          path: HealthPaths.HealthMedicineCertificates,
-          children: [
-            {
-              name: m.medicineLicenseTitle,
-              path: HealthPaths.HealthMedicineCertificate,
-              navHide: true,
-              breadcrumbHide: true,
-            },
-          ],
-        },
-        {
-          name: m.medicinePrescriptionHistory,
-          description: m.medicinePrescriptionHistoryIntro,
-          path: HealthPaths.HealthMedicinePrescriptionHistory,
-        },
-      ],
-    },
-    {
-      name: m.aidsAndNutrition,
-      description: m.aidsAndNutritionIntro,
-      path: HealthPaths.HealthAidsAndNutrition,
-    },
-    {
-      name: m.therapies,
+      name: messages.therapiesAndAids,
       path: HealthPaths.HealthTherapies,
       children: [
         {
-          name: messages.physicalTherapy,
-          path: HealthPaths.HealthTherapiesPhysical,
-          navHide: true,
-        },
-        {
-          name: messages.speechTherapy,
-          path: HealthPaths.HealthTherapiesSpeech,
-          navHide: true,
-        },
-        {
-          name: messages.occupationalTherapy,
-          path: HealthPaths.HealthTherapiesOccupational,
-          navHide: true,
-        },
-      ],
-    },
-    {
-      name: m.vaccinations,
-      description: m.vaccinationsIntro,
-      path: HealthPaths.HealthVaccinations,
-      children: [
-        {
-          name: messages.generalVaccinations,
-          path: HealthPaths.HealthVaccinationsGeneral,
-          navHide: true,
-        },
-        {
-          name: messages.otherVaccinations,
-          path: HealthPaths.HealthVaccinationsOther,
-          navHide: true,
-        },
-      ],
-    },
-    {
-      name: messages.waitlists,
-      path: HealthPaths.HealthWaitlists,
-      searchTags: [s.healthWaiting],
-      children: [
-        {
-          name: messages.singleWaitlist,
-          path: HealthPaths.HealthWaitlistsDetail,
-          navHide: true,
-        },
-      ],
-    },
-    {
-      name: messages.questionnaires,
-      path: HealthPaths.HealthQuestionnaires,
-      searchTags: [],
-      children: [
-        {
-          name: messages.questionnaire,
-          path: HealthPaths.HealthQuestionnairesDetail,
-          navHide: true,
+          name: m.therapies,
+          path: HealthPaths.HealthTherapies,
           children: [
             {
-              name: messages.questionnaire,
-              path: HealthPaths.HealthQuestionnairesAnswer,
+              name: messages.physicalTherapy,
+              path: HealthPaths.HealthTherapiesPhysical,
               navHide: true,
-              breadcrumbHide: true,
             },
             {
-              name: messages.answers,
-              path: HealthPaths.HealthQuestionnairesAnswered,
+              name: messages.speechTherapy,
+              path: HealthPaths.HealthTherapiesSpeech,
+              navHide: true,
+            },
+            {
+              name: messages.occupationalTherapy,
+              path: HealthPaths.HealthTherapiesOccupational,
               navHide: true,
             },
           ],
+        },
+        {
+          name: m.aidsAndNutrition,
+          description: m.aidsAndNutritionIntro,
+          path: HealthPaths.HealthAidsAndNutrition,
         },
       ],
     },
@@ -262,6 +214,60 @@ export const healthNavigation: PortalNavigationItem = {
       path: HealthPaths.HealthPatientData,
       searchTags: [s.healthPatientData],
       children: [
+        {
+          name: messages.waitlists,
+          path: HealthPaths.HealthWaitlists,
+          searchTags: [s.healthWaiting],
+          children: [
+            {
+              name: messages.singleWaitlist,
+              path: HealthPaths.HealthWaitlistsDetail,
+              navHide: true,
+            },
+          ],
+        },
+        {
+          name: m.vaccinations,
+          description: m.vaccinationsIntro,
+          path: HealthPaths.HealthVaccinations,
+          children: [
+            {
+              name: messages.generalVaccinations,
+              path: HealthPaths.HealthVaccinationsGeneral,
+              navHide: true,
+            },
+            {
+              name: messages.otherVaccinations,
+              path: HealthPaths.HealthVaccinationsOther,
+              navHide: true,
+            },
+          ],
+        },
+        {
+          name: messages.questionnaires,
+          path: HealthPaths.HealthQuestionnaires,
+          searchTags: [],
+          children: [
+            {
+              name: messages.questionnaire,
+              path: HealthPaths.HealthQuestionnairesDetail,
+              navHide: true,
+              children: [
+                {
+                  name: messages.questionnaire,
+                  path: HealthPaths.HealthQuestionnairesAnswer,
+                  navHide: true,
+                  breadcrumbHide: true,
+                },
+                {
+                  name: messages.answers,
+                  path: HealthPaths.HealthQuestionnairesAnswered,
+                  navHide: true,
+                },
+              ],
+            },
+          ],
+        },
         {
           name: messages.patientDataPermit,
           path: HealthPaths.HealthPatientDataPermits,
