5f764d3df8204a54bd1c8b66affbc8731a1dcfdf
Kormákur <korri123@gmail.com>
2026-04-30 11:41:23 +0000

    fix: locale handling

--- apps/skilavottord/web/components/Footer/Footer.tsx
+++ apps/skilavottord/web/components/Footer/Footer.tsx
@@ -4,8 +4,40 @@ import { useI18n } from '@island.is/skilavottord-web/i18n'
 
 export const Footer: FC<React.PropsWithChildren<unknown>> = () => {
   const {
+    activeLocale,
     t: { footer: t },
   } = useI18n()
+
+  const bottomBarLinks =
+    activeLocale === 'en'
+      ? [
+          {
+            title: 'Can we assist?',
+            href: 'https://island.is/en/help/digital-iceland/contact-us',
+          },
+          {
+            title: 'Privacy policy',
+            href: 'https://island.is/en/privacy-policy',
+          },
+          {
+            title: 'Terms of use',
+            href: 'https://island.is/en/terms-of-use',
+          },
+        ]
+      : [
+          {
+            title: 'Getum við aðstoðað?',
+            href: 'https://island.is/s/stafraent-island/hafa-samband',
+          },
+          {
+            title: 'Persónuverndarstefna',
+            href: 'https://island.is/personuverndarstefna-stafraent-islands',
+          },
+          {
+            title: 'Notendaskilmálar',
+            href: 'https://island.is/skilmalar-island-is',
+          },
+        ]
 
   return (
     <IslandUIFooter
@@ -13,6 +45,7 @@ export const Footer: FC<React.PropsWithChildren<unknown>> = () => {
       middleLinksTitle={t.bottomLinksTitle}
       middleLinks={t.bottomLinks}
       showMiddleLinks
+      bottomBarLinks={bottomBarLinks}
     />
   )
 }
