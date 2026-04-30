b7e8eb1baa499792a37a6b0a24b4f98c9993f8ef
Kormákur <korri123@gmail.com>
2026-04-30 14:50:14 +0000

    fix(web): restore footer bottom bar defaults (#22408) (#22414)
    * fix: restore footer bottom bar defaults
    
    * fix: locale handling

--- apps/skilavottord/web/components/Footer/Footer.tsx
+++ apps/skilavottord/web/components/Footer/Footer.tsx
@@ -4,16 +4,48 @@ import { useI18n } from '@island.is/skilavottord-web/i18n'
 
 export const Footer: FC<React.PropsWithChildren<unknown>> = () => {
   const {
+    activeLocale,
     t: { footer: t },
   } = useI18n()
+
+  const bottomBarLinks =
+    activeLocale === 'en'
+      ? [
+          {
+            title: 'Can we assist?',
+            href: 'https://island.is/en/help/digital-iceland/contact-us',
+          },
+          {
+            title: 'Privacy policy',
+            href: 'https://island.is/en/privacy-policy',
+          },
+          {
+            title: 'Terms of use',
+            href: 'https://island.is/en/terms-of-use',
+          },
+        ]
+      : [
+          {
+            title: 'Getum við aðstoðað?',
+            href: 'https://island.is/s/stafraent-island/hafa-samband',
+          },
+          {
+            title: 'Persónuverndarstefna',
+            href: 'https://island.is/personuverndarstefna-stafraent-islands',
+          },
+          {
+            title: 'Notendaskilmálar',
+            href: 'https://island.is/skilmalar-island-is',
+          },
+        ]
 
   return (
     <IslandUIFooter
       topLinks={t.topLinksInfo}
-      topLinksContact={t.topLinksContact}
-      bottomLinksTitle={t.bottomLinksTitle}
-      bottomLinks={t.bottomLinks}
-      hideLanguageSwitch={true}
+      middleLinksTitle={t.bottomLinksTitle}
+      middleLinks={t.bottomLinks}
+      showMiddleLinks
+      bottomBarLinks={bottomBarLinks}
     />
   )
 }
