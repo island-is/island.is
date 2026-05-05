701452fa7b2cc5330560f3e50eb05f4589604cb5
gudjonss <gudjon.steinar.sverrisson@kolibri.is>
2026-05-05 14:05:58 +0000

    add policecasenumbers to limiteAccessCase

--- apps/judicial-system/backend/src/app/modules/case/interceptors/case.interceptor.ts
+++ apps/judicial-system/backend/src/app/modules/case/interceptors/case.interceptor.ts
@@ -314,6 +314,7 @@ export const transformDefendants = ({
 
     return {
       ...defendant.toJSON(),
+      policeCaseNumbers: defendant.policeCaseNumbers,
       ...(verdict
         ? {
             verdict: {
