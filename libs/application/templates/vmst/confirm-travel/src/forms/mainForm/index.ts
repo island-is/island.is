291c05b9f9368416b39adf25e78996bc8d9a7cd9
berglindoma13 <berglindis@gmail.com>
2026-05-11 09:39:31 +0000

    feat(vmst-actions): action for reporting travelling (#22372)
    * new application for travel confirmation when on unemployment benefits
    
    * removed repeater and added featureflag
    
    * application done
    
    * chore: format update dirty file
    
    * final comments
    
    * contentful update
    
    * read me and typo
    
    * small fix
    
    * added 30 day limit
    
    * fixes from comments
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

---
+++ libs/application/templates/vmst/confirm-travel/src/forms/mainForm/index.ts
@@ -0,0 +1,78 @@
+import {
+  buildDateField,
+  buildDescriptionField,
+  buildForm,
+  buildMultiField,
+  buildSubmitField,
+  getValueViaPath,
+} from '@island.is/application/core'
+import { FormModes } from '@island.is/application/types'
+import { DirectorateOfLabourLogo } from '@island.is/application/assets/institution-logos'
+import { mainForm } from '../../lib/messages'
+
+export const MainForm = buildForm({
+  id: 'MainForm',
+  mode: FormModes.DRAFT,
+  renderLastScreenButton: true,
+  title: mainForm.title,
+  logo: DirectorateOfLabourLogo,
+  children: [
+    buildMultiField({
+      id: 'mainFormMultiField',
+      title: mainForm.title,
+      description: mainForm.description,
+      children: [
+        buildDescriptionField({
+          id: 'dateDescription',
+          title: mainForm.dateTitle,
+          titleVariant: 'h5',
+        }),
+        buildDateField({
+          id: 'date.from',
+          title: mainForm.dateFrom,
+          placeholder: mainForm.datePlaceholder,
+          width: 'half',
+          required: true,
+        }),
+        buildDateField({
+          id: 'date.to',
+          title: mainForm.dateTo,
+          placeholder: mainForm.datePlaceholder,
+          width: 'half',
+          required: true,
+          minDate: (application) => {
+            const fromDate = getValueViaPath<string>(
+              application.answers,
+              'date.from',
+            )
+            return fromDate ? new Date(fromDate) : undefined
+          },
+          maxDate: (application) => {
+            const fromDate = getValueViaPath<string>(
+              application.answers,
+              'date.from',
+            )
+            if (fromDate) {
+              const date = new Date(fromDate)
+              date.setDate(date.getDate() + 30)
+              return date
+            }
+            return undefined
+          },
+        }),
+        buildSubmitField({
+          id: 'submit',
+          title: 'Submit',
+          refetchApplicationAfterSubmit: true,
+          actions: [
+            {
+              event: 'SUBMIT',
+              name: mainForm.submitApplication,
+              type: 'primary',
+            },
+          ],
+        }),
+      ],
+    }),
+  ],
+})
