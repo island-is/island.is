4a30a32b59f3a3fb5ac89a4e6d3711414850652b
Gunnar K Vilbergsson <gunnar@norda.is>
2026-05-12 09:37:16 +0000

    fix

--- libs/application/templates/hms/rental-agreement/src/lib/schemas/fireProtectionsSchema.ts
+++ libs/application/templates/hms/rental-agreement/src/lib/schemas/fireProtectionsSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const fireProtectionsSchema = z.object({
   smokeDetectors: z.string().min(1),
-  fireExtinguisher: z.string().min(1),
+  fireExtinguisher: z.string().min(0),
   emergencyExits: z.string().optional(),
   fireBlanket: z.string().optional(),
   propertySize: z
