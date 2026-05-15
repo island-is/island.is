b8c6b98a4ccf570e4df047279fc584c0eeb0228a
thorkellmani <wowthorkell@gmail.com>
2026-05-15 13:04:54 +0000

    chore: update api

--- libs/clients/driving-license/src/index.ts
+++ libs/clients/driving-license/src/index.ts
@@ -3,7 +3,11 @@ export * from './lib/drivingLicenseApi.module'
 export * from './lib/drivingLicenseApi.types'
 
 export { DrivingLicenseApi } from './lib/services/drivingLicenseApi.service'
+<<<<<<< HEAD
 export { PenaltyPointsClientService } from './lib/services/penaltyPoints.service'
+=======
+export { PenaltyPointsService } from './lib/services/penaltyPoints.service'
+>>>>>>> dcd05399b4 (chore: update api)
 export { DrivingLicenseApiConfig } from './lib/drivingLicenseApi.config'
 export {
   DtoV5DriverLicenseDto,
