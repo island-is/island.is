26eba2a4d5396ac3bf90693d6ba6cfcdd342b5d8
thorkellmani <wowthorkell@gmail.com>
2026-05-15 13:04:54 +0000

    chore: update api

--- libs/clients/driving-license/src/lib/drivingLicenseApi.module.ts
+++ libs/clients/driving-license/src/lib/drivingLicenseApi.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common'
 import { DrivingLicenseApi } from './services/drivingLicenseApi.service'
 import { exportedApis } from './apiConfiguration'
+<<<<<<< HEAD
 import { PenaltyPointsClientService } from './services/penaltyPoints.service'
 
 @Module({
   providers: [...exportedApis, DrivingLicenseApi, PenaltyPointsClientService],
   exports: [DrivingLicenseApi, PenaltyPointsClientService],
+=======
+import { PenaltyPointsService } from './services/penaltyPoints.service'
+
+@Module({
+  providers: [...exportedApis, DrivingLicenseApi],
+  exports: [DrivingLicenseApi, PenaltyPointsService],
+>>>>>>> dcd05399b4 (chore: update api)
 })
 export class DrivingLicenseApiModule {}
