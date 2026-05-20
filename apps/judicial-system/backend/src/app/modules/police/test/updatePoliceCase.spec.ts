4108747aa882c21d2af7528ab423eb85dbd64812
gudjonss <gudjon.steinar.sverrisson@kolibri.is>
2026-05-20 09:54:36 +0000

    Use new endpoint when updating cases that were created via the new LOKE window.

--- apps/judicial-system/backend/src/app/modules/police/test/updatePoliceCase.spec.ts
+++ apps/judicial-system/backend/src/app/modules/police/test/updatePoliceCase.spec.ts
@@ -84,14 +84,14 @@ describe('PoliceController - Update Police Case', () => {
     }
   })
 
-  describe('update police case', () => {
+  describe('update police case with defendant national id', () => {
     let then: Then
 
     beforeEach(async () => {
       then = await givenWhenThen()
     })
 
-    it('should update the police case', async () => {
+    it('should update the police case using the v2 endpoint', async () => {
       expect(fetch).toHaveBeenCalledWith(
         `${xRoadPath}/V2/UpdateRVCase/${caseId}`,
         {
@@ -108,6 +108,63 @@ describe('PoliceController - Update Police Case', () => {
             caseNumber: policeCaseNumber,
             courtCaseNumber,
             ssn: defendantNationalId,
+            type: caseType,
+            courtVerdict: caseState,
+            expiringDate: validToDate,
+            courtVerdictString: caseConclusion,
+            courtDocuments,
+          }),
+        },
+      )
+      expect(then.result).toBe(true)
+    })
+  })
+
+  describe('update police case without defendant national id', () => {
+    let then: Then
+
+    beforeEach(async () => {
+      const mockFetch = fetch as unknown as jest.Mock
+      mockFetch.mockResolvedValueOnce({
+        ok: true,
+        json: jest.fn().mockResolvedValueOnce({ ok: true }),
+      })
+
+      const { policeService } = await createTestingPoliceModule()
+
+      then = await policeService
+        .updatePoliceCase(
+          user,
+          caseId,
+          caseType,
+          caseState,
+          policeCaseNumber,
+          courtCaseNumber,
+          '',
+          validToDate,
+          caseConclusion,
+          courtDocuments,
+        )
+        .then((result) => ({ result } as Then))
+        .catch((error) => ({ error } as Then))
+    })
+
+    it('should update the police case using the v4 endpoint without ssn', async () => {
+      expect(fetch).toHaveBeenCalledWith(
+        `${xRoadPath}/V4/case/${caseId}`,
+        {
+          method: 'PUT',
+          headers: {
+            accept: '*/*',
+            'Content-Type': 'application/json',
+            'X-Road-Client': mockConfig.clientId,
+            'X-API-KEY': mockConfig.policeApiKey,
+          },
+          agent: expect.any(Agent),
+          body: JSON.stringify({
+            rvMal_ID: caseId,
+            caseNumber: policeCaseNumber,
+            courtCaseNumber,
             type: caseType,
             courtVerdict: caseState,
             expiringDate: validToDate,
