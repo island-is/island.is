43c421ea07eb4d99bbe2028ebbc8f6ee80e3ab96
unakb <unakben@gmail.com>
2026-05-22 10:32:57 +0000

    Update Table.tsx

--- libs/portals/core/src/components/Table/Table.tsx
+++ libs/portals/core/src/components/Table/Table.tsx
@@ -448,18 +448,11 @@ export const Table = <TData extends object>({
                 <tr aria-hidden={!isExpanded && !isCollapsing}>
                   <T.Data
                     colSpan={columns.length}
-                    style={{
-                      padding: 0,
-                      ...(!isExpanded && !isCollapsing
-                        ? { borderBottom: 'none' }
-                        : {}),
-                    }}
+                    style={{ padding: 0 }}
                     box={{
                       position: 'relative',
                       background:
                         isExpanded || isCollapsing ? 'blue100' : undefined,
-                      borderBottomWidth:
-                        isExpanded || isCollapsing ? 'standard' : undefined,
                     }}
                   >
                     <AnimateHeight
