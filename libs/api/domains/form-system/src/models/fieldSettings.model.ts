7f70982c2ba65f6358e7631e6b8e3d3f2360fabf
Hyo-sam Nandkisore <hyo-sam.nandkisore@advania.is>
2026-05-27 14:44:47 +0000

    feat(form-system): NationalId component now has the option to display an address (#22668)
    * feat(form-system): add support for address display and related fields in National ID components
    
    * feat(form-system): dispatch address reset action on identity query completion
    
    * feat(field-settings): add optional zendeskTicketFieldId property

--- libs/api/domains/form-system/src/models/fieldSettings.model.ts
+++ libs/api/domains/form-system/src/models/fieldSettings.model.ts
@@ -115,6 +115,8 @@ export class FieldSettings {
   @Field(() => String, { nullable: true })
   paymentQuantityId?: string
 
+  @Field(() => Boolean, { nullable: true })
+  showAddress?: boolean
   @Field(() => String, { nullable: true })
   zendeskTicketFieldId?: string
 
