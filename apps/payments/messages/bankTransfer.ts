b168302d8a2f1705f338d45c670a28a2b14bbf1a
Herdismaria <herdismaria90@gmail.com>
2026-05-28 13:25:41 +0000

    feat(payments): add bank transfer button to PaymentSelector

---
+++ apps/payments/messages/bankTransfer.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl'
+
+export const bankTransfer = defineMessages({
+  paymentMethodTitle: {
+    id: 'payments:bankTransfer.title',
+    defaultMessage: 'Tafarlaus millifærsla',
+    description: 'Title for bank transfer payment method',
+  },
+})
