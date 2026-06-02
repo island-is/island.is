978bd351d4de4e628578b71e5355c0cd6547236b
Gunnar K Vilbergsson <gunnar@norda.is>
2026-06-02 12:15:36 +0000

    fix migration

--- apps/application-system/api/migrations/20260528081252-remove-expiresat.js
+++ apps/application-system/api/migrations/20260528081252-remove-expiresat.js
@@ -4,11 +4,4 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.removeColumn('payment', 'expires_at')
   },
-
-  async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn('payment', 'expires_at', {
-      type: Sequelize.DATE,
-      allowNull: false,
-    })
-  },
 }
