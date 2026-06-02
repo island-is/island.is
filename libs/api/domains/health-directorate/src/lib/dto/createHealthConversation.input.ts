c79d2ccc44670aa84883034f47bf498caaeef448
unakb <unakben@gmail.com>
2026-06-02 13:28:17 +0000

    fix validation

--- libs/api/domains/health-directorate/src/lib/dto/createHealthConversation.input.ts
+++ libs/api/domains/health-directorate/src/lib/dto/createHealthConversation.input.ts
@@ -1,4 +1,5 @@
 import { Field, InputType, Int } from '@nestjs/graphql'
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
 @InputType()
 export class HealthDirectorateCreateConversationInput {
@@ -6,20 +7,29 @@ export class HealthDirectorateCreateConversationInput {
     description:
       'Hekla node ID of the recipient. Obtained from the recipient list.',
   })
+  @IsString()
+  @IsNotEmpty()
   nodeId!: string
 
   @Field(() => Int, {
     description:
       'Hekla group ID of the recipient provider. Obtained from the recipient list.',
   })
+  @IsInt()
   groupId!: number
 
   @Field()
+  @IsString()
+  @IsNotEmpty()
   patientInitiatedTypeCode!: string
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   title?: string
 
   @Field()
+  @IsString()
+  @IsNotEmpty()
   messageTextContent!: string
 }
