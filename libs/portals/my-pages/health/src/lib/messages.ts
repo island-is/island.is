d53195d689b6f70710e8e33680366c616c1210df
Una Kristín <una@kolibri.is>
2026-06-08 12:04:35 +0000

    feat(my-pages): Pick prescription renewal target (#22826)
    * feat: pick prescription recipient
    
    * renewals recipient
    
    * chore: format update dirty file
    
    * fix messages
    
    * chore: format update dirty file
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

--- libs/portals/my-pages/health/src/lib/messages.ts
+++ libs/portals/my-pages/health/src/lib/messages.ts
@@ -2513,6 +2513,18 @@ export const messages = defineMessages({
     defaultMessage: 'Endurnýjun ekki í boði',
     id: 'sp.health:prescription-blocked-specialist-only-prescription',
   },
+  prescriptionBlockedNoRenewalTargets: {
+    defaultMessage: 'Endurnýjun ekki í boði',
+    id: 'sp.health:prescription-blocked-no-renewal-targets',
+  },
+  prescriptionBlockedInvalidRenewalTarget: {
+    defaultMessage: 'Endurnýjun ekki í boði',
+    id: 'sp.health:prescription-blocked-invalid-renewal-target',
+  },
+  prescriptionBlockedRecipientExcludesAtc: {
+    defaultMessage: 'Endurnýjun ekki í boði vegna',
+    id: 'sp.health:prescription-blocked-recipient-excludes-atc',
+  },
   prescriptionBlockedOther: {
     defaultMessage: 'Endurnýjun ekki í boði vegna óþekktrar ástæðu',
     id: 'sp.health:prescription-blocked-other',
@@ -2528,6 +2540,15 @@ export const messages = defineMessages({
   renewalStatusRejected: {
     defaultMessage: 'Hafnað',
     id: 'sp.health:renewal-status-rejected',
+  },
+  renewalSendTo: {
+    defaultMessage: 'Sendist til',
+    id: 'sp.health:renewal-send-to',
+  },
+  renewalNoTarget: {
+    defaultMessage:
+      'Ekki tókst að finna stofnun til að senda endurnýjunarbeiðni til. Vinsamlegast hafið samband við heilsugæslu.',
+    id: 'sp.health:renewal-no-target',
   },
   renewalRequestSent: {
     defaultMessage:
