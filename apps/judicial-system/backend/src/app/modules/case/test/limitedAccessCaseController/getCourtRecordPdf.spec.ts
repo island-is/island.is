4163a57b75e7a0ae8b0d980e3f03eaf8a4722c7d
Ívar Oddsson <ivaroddsson@gmail.com>
2026-06-08 12:09:56 +0000

    fix(j-s): update indictmentCourtRecordPdf mock path to generatedPdfs subdirectory

--- apps/judicial-system/backend/src/app/modules/case/test/limitedAccessCaseController/getCourtRecordPdf.spec.ts
+++ apps/judicial-system/backend/src/app/modules/case/test/limitedAccessCaseController/getCourtRecordPdf.spec.ts
@@ -22,7 +22,7 @@ import { AwsS3Service } from '../../../aws-s3'
 import { Case } from '../../../repository'
 
 jest.mock('../../../../formatters/generatedPdfs/courtRecordPdf')
-jest.mock('../../../../formatters/indictmentCourtRecordPdf')
+jest.mock('../../../../formatters/generatedPdfs/indictmentCourtRecordPdf')
 
 interface Then {
   error: Error
