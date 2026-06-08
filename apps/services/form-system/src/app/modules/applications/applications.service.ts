dd3e06a9088b37e31cb934d1f6c2c270a1c92157
OlafurGeorg <olafur.georg.gylfason@advania.is>
2026-06-08 14:31:53 +0000

    fix section info mapping

--- apps/services/form-system/src/app/modules/applications/applications.service.ts
+++ apps/services/form-system/src/app/modules/applications/applications.service.ts
@@ -114,6 +114,8 @@ export class ApplicationsService {
 
     const nationalId = user.actor?.nationalId || user.nationalId
 
+    console.log('Creating application for user with nationalId:', nationalId)
+
     try {
       await this.sequelize.transaction(async (transaction) => {
         const newApplication: Application = await this.applicationModel.create(
@@ -206,7 +208,11 @@ export class ApplicationsService {
       throw error
     }
 
+    console.log('got this far with applicationId:', newApplicationId)
+
     const applicationDto = await this.getApplication(newApplicationId, '', null)
+
+    console.log('getting applicationDto')
 
     return applicationDto
   }
--- libs/api/domains/form-system/src/lib/applications/applications.service.ts
+++ libs/api/domains/form-system/src/lib/applications/applications.service.ts
@@ -58,9 +58,11 @@ export class ApplicationsService {
     auth: User,
     input: CreateApplicationInput,
   ): Promise<ApplicationResponse> {
+    console.log('creating application with slug:', input.slug)
     const response = await this.applicationsApiWithAuth(
       auth,
     ).applicationsControllerCreate(input as ApplicationsControllerCreateRequest)
+    console.log('create application response:', response)
     return response as ApplicationResponse
   }
 
@@ -68,11 +70,18 @@ export class ApplicationsService {
     auth: User,
     input: GetApplicationInput,
   ): Promise<ApplicationResponse> {
+    console.log(
+      'getting application with id:',
+      input.id,
+      'and slug:',
+      input.slug,
+    )
     const response = await this.applicationsApiWithAuth(auth)
       .applicationsControllerGetApplication(
         input as ApplicationsControllerGetApplicationRequest,
       )
       .catch((e) => handle4xx(e, this.handleError, 'failed to get application'))
+    console.log('get application response:', response)
 
     return response as ApplicationResponse
   }
@@ -81,6 +90,7 @@ export class ApplicationsService {
     auth: User,
     input: GetApplicationsInput,
   ): Promise<ApplicationResponse> {
+    console.log('getting all applications with slug:', input.slug)
     const response = await this.applicationsApiWithAuth(auth)
       .applicationsControllerFindAllBySlugAndUser(
         input as ApplicationsControllerFindAllBySlugAndUserRequest,
@@ -88,6 +98,7 @@ export class ApplicationsService {
       .catch((e) =>
         handle4xx(e, this.handleError, 'failed to get applications'),
       )
+    console.log('get all applications response:', response)
     return response as ApplicationResponse
   }
 
