04a74e938a331772d198e851bcca03ef75defe54
Rúnar Vestmann <runar@stefna.is>
2026-06-10 16:17:28 +0000

    Set noindex,nofollow on list pages for Hæstiréttur Íslands

--- apps/web/screens/SupremeCourt/Determinations/DeterminationList.tsx
+++ apps/web/screens/SupremeCourt/Determinations/DeterminationList.tsx
@@ -156,6 +156,7 @@ const Determinations: CustomScreen<DeterminationsProps> = ({
           href: linkResolver('organizationpage', [organizationPage.slug]).href,
         },
       ]}
+      hideFromExternalSearchEngines
     >
       <Stack space={2}>
         <Stack space={2}>
