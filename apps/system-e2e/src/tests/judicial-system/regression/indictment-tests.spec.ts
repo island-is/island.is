dd5337ad17b44e5d04a441105f9c8e951befd98f
Ívar Oddsson <ivaroddsson@gmail.com>
2026-06-11 14:05:41 +0000

    chore(j-s): revert e2e entries feature flag change

--- apps/system-e2e/src/tests/judicial-system/regression/indictment-tests.spec.ts
+++ apps/system-e2e/src/tests/judicial-system/regression/indictment-tests.spec.ts
@@ -245,19 +245,12 @@ test.describe.serial('Indictment tests', () => {
       page.getByRole('button', { name: 'Bæta við þinghaldi' }).click(),
       verifyRequestCompletion(page, '/api/graphql', 'CreateCourtSession'),
     ])
-    const useTinyMCE = await page.request
-      .get('/api/feature/TINY_MCE')
-      .then((r) => r.json())
-
-    if (useTinyMCE) {
-      await page
-        .getByTestId('entries')
-        .frameLocator('iframe')
-        .locator('body')
-        .fill('Afstaða, málflutningur, og bókun')
-    } else {
-      await page.getByTestId('entries').fill('Afstaða, málflutningur, og bókun')
-    }
+    await page.getByTestId('entries').fill('Afstaða, málflutningur, og bókun')
+    // await page
+    //   .getByTestId('entries')
+    //   .frameLocator('iframe')
+    //   .locator('body')
+    //   .fill('Afstaða, málflutningur, og bókun')
 
     await page.locator('label').filter({ hasText: 'Dómur kveðinn upp' }).click()
     await page.getByTestId('ruling').fill('Dómsorð')
