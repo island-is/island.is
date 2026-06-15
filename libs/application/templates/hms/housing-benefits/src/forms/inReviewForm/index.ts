b488a46b490cd1d1a63ddf32858b788c257ae629
Jonni <jonas@norda.is>
2026-06-15 17:48:24 +0000

    chore: messages foem message files

--- libs/application/templates/hms/housing-benefits/src/forms/inReviewForm/index.ts
+++ libs/application/templates/hms/housing-benefits/src/forms/inReviewForm/index.ts
@@ -3,6 +3,7 @@ import { buildFormConclusionSection } from '@island.is/application/ui-forms'
 import { FormModes } from '@island.is/application/types'
 import { HmsLogo } from '@island.is/application/assets/institution-logos'
 import { HikingAndWateringPlants } from '@island.is/application/assets/graphics'
+import * as m from '../../lib/messages'
 
 export const inReviewForm = buildForm({
   id: 'inReviewForm',
@@ -11,10 +12,9 @@ export const inReviewForm = buildForm({
   children: [
     buildFormConclusionSection({
       sectionTitle: '',
-      tabTitle: 'Umsókn í vinnslu ',
-      alertTitle: 'Umsókn hefur verið send inn til HMS',
-      alertMessage:
-        'Umsóknin þín er nú í vinnslu hjá HMS og verður tekin til afgreiðslu sem fyrst.',
+      tabTitle: m.inReviewMessages.tabTitle,
+      alertTitle: m.inReviewMessages.alertTitle,
+      alertMessage: m.inReviewMessages.alertMessage,
       image: buildImageField({
         id: 'inReviewImage',
         image: HikingAndWateringPlants,
