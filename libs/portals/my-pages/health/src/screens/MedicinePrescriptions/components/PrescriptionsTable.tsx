1f090823de433a27e3aa05dbd0af2bdeab3e0343
thorkellmani <wowthorkell@gmail.com>
2026-06-16 14:02:42 +0000

    fix: add new UNKNOWN enum to status renewal dispaly

--- libs/portals/my-pages/health/src/screens/MedicinePrescriptions/components/PrescriptionsTable.tsx
+++ libs/portals/my-pages/health/src/screens/MedicinePrescriptions/components/PrescriptionsTable.tsx
@@ -22,6 +22,17 @@ import RenewPrescriptionModal from './RenewPrescriptionModal/RenewPrescriptionMo
 
 const STRING_MAX_LENGTH = 22
 
+const renewalStatusMessageMap: Record<
+  HealthDirectoratePrescriptionRenewalStatus,
+  keyof typeof messages
+> = {
+  [HealthDirectoratePrescriptionRenewalStatus.Approved]: 'renewalStatusApproved',
+  [HealthDirectoratePrescriptionRenewalStatus.Rejected]: 'renewalStatusRejected',
+  [HealthDirectoratePrescriptionRenewalStatus.Dismissed]: 'renewalStatusDismissed',
+  [HealthDirectoratePrescriptionRenewalStatus.Unknown]: 'renewalStatusUnknown',
+  [HealthDirectoratePrescriptionRenewalStatus.Pending]: 'renewalInProgress',
+}
+
 interface Props {
   data?: HealthDirectoratePrescription[]
   loading?: boolean
@@ -132,19 +143,9 @@ const PrescriptionsTable: React.FC<Props> = ({ data, loading }) => {
               lastNode: item?.renewalStatus
                 ? {
                     type: 'text' as const,
-                    label:
-                      item.renewalStatus ===
-                      HealthDirectoratePrescriptionRenewalStatus.Approved
-                        ? formatMessage(messages.renewalStatusApproved)
-                        : item.renewalStatus ===
-                          HealthDirectoratePrescriptionRenewalStatus.Rejected
-                        ? formatMessage(messages.renewalStatusRejected)
-                        : item.renewalStatus ===
-                          HealthDirectoratePrescriptionRenewalStatus.Dismissed
-                        ? formatMessage(messages.renewalStatusDismissed)
-                        : formatMessage(
-                            messages.medicineIsProcessedCertificate,
-                          ),
+                    label: formatMessage(
+                      messages[renewalStatusMessageMap[item.renewalStatus]],
+                    ),
                     text: item.renewResponseMessage ?? undefined,
                   }
                 : item?.isRenewable
