06036e529700c71aa9409359b2dec2146bd93fa4
OlafurGeorg <olafur.georg.gylfason@advania.is>
2026-06-18 09:59:03 +0000

    guard undefined

--- apps/form-system/web/src/components/Footer/Footer.tsx
+++ apps/form-system/web/src/components/Footer/Footer.tsx
@@ -215,7 +215,8 @@ export const Footer = ({ externalDataAgreement }: Props) => {
             input: {
               applicationId: state.application.id,
               nationalId: '',
-              organizationNationalId: state.application.organizationNationalId,
+              organizationNationalId:
+                state.application.organizationNationalId ?? '',
               slug: state.application.slug,
               isTest: state.application.isTest,
               command: NotificationCommands.VALIDATE,
