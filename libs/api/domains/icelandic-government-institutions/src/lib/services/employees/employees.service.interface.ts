ff97f2bbd8adf18155d6e03db8deae93b6e7bf51
thorkellmani <wowthorkell@gmail.com>
2026-06-19 12:49:17 +0000

    chore: fix import

--- libs/api/domains/icelandic-government-institutions/src/lib/services/employees/employees.service.interface.ts
+++ libs/api/domains/icelandic-government-institutions/src/lib/services/employees/employees.service.interface.ts
@@ -1,5 +1,5 @@
 import { Employees } from '../../models/employees.model'
-import { EmployeesInput } from '@/lib/dtos/getEmployees.input'
+import { EmployeesInput } from '../../dtos/getEmployees.input'
 
 export interface IEmployeesService {
   getEmployees(input: EmployeesInput): Promise<Employees>
