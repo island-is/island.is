adf84baa52ac302462eed77a95ef8925004a29ba
Ólafur Georg Gylfason <olafur.georg.gylfason@advania.is>
2026-06-19 13:36:21 +0000

    feat(form-system): fetch real estate (#22981)
    * fetch real_estate
    
    * block fasteignir from being used
    
    * add to reducer

--- apps/form-system/web/src/reducers/fieldReducer.ts
+++ apps/form-system/web/src/reducers/fieldReducer.ts
@@ -20,12 +20,25 @@ export const fieldReducer = (
       return setMultipleFieldValues(state, id, values, valueIndex)
     }
     case 'SET_ASSET_VALUE': {
-      const { registrationNumber, model, color, id, valueIndex } =
-        action.payload
+      const {
+        registrationNumber,
+        model,
+        color,
+        propertyNumber,
+        address,
+        postalCode,
+        municipality,
+        id,
+        valueIndex,
+      } = action.payload
       const values = {
         registrationNumber,
         model,
         color,
+        propertyNumber,
+        address,
+        postalCode,
+        municipality,
       }
       return setMultipleFieldValues(state, id, values, valueIndex)
     }
