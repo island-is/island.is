5760ff44f7aeb01269a5ad912ae984e898c86b97
Ívar Oddsson <ivaroddsson@gmail.com>
2026-06-22 19:55:21 +0000

    feat(j-s): show "Skjöl héraðsdómsmáls" heading for court of appeals users

--- apps/judicial-system/web/src/components/IndictmentCaseFilesList/IndictmentCaseFilesList.tsx
+++ apps/judicial-system/web/src/components/IndictmentCaseFilesList/IndictmentCaseFilesList.tsx
@@ -406,7 +406,13 @@ const IndictmentCaseFilesList: FC<Props> = ({
   return (
     <>
       {displayHeading && (
-        <SectionHeading title={formatMessage(strings.title)} />
+        <SectionHeading
+          title={
+            isCourtOfAppealsUser(user)
+              ? 'Skjöl héraðsdómsmáls'
+              : formatMessage(strings.title)
+          }
+        />
       )}
       <div className={grid({ gap: 5 })}>
         {displayGeneratedPDFs && (
