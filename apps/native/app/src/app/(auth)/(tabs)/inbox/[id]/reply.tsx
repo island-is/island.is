650ebf757604ed7a3a314d5697ca15d07e58141f
Þórey Jóna <thoreyjona@gmail.com>
2026-06-23 14:02:45 +0000

    fix(native-app): fix 2 way bugs (#22973)
    * fix: make sure text input is 3 lines by default
    
    * fix(native-app): make sure document does not get back to unread state when opening communications

--- apps/native/app/src/app/(auth)/(tabs)/inbox/[id]/reply.tsx
+++ apps/native/app/src/app/(auth)/(tabs)/inbox/[id]/reply.tsx
@@ -217,6 +217,8 @@ export default function DocumentReplyScreen() {
                   })}
                   autoFocus
                   multiline
+                  numberOfLines={3}
+                  inputStyle={{ minHeight: 60 }}
                 />
               </Row>
             </KeyboardAvoidingView>
