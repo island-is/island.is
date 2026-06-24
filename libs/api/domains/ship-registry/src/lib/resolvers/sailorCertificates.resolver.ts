012d7e0a404e995622f73e6be5eed19030737a6c
Þorkell Máni Þorkelsson <wowthorkell@gmail.com>
2026-06-24 15:18:18 +0000

    feat(my-pages-occupational-licenses): add sailors section to occupational licenses (#22513)
    * chore: client update
    
    * feat: add the screens, client and domain
    
    * chore: format update dirty file
    
    * chore: touchups and todos
    
    * chore: fix up ui
    
    * chore: format update dirty file
    
    * chore: update fixes
    
    * chore: extra fixes
    
    * chore: format update dirty file
    
    * chore: readd after error
    
    * chore: integrate new endpoints
    
    * chore: format update dirty file
    
    * chore: update query structure
    
    * chore: format update dirty file
    
    * chore: revert nullable ship search
    
    * chore: remove locale
    
    * chore: update models
    
    * chore: format update dirty file
    
    * chore: integarte frontend data
    
    * chore: format update dirty file
    
    * chore: update client config
    
    * chore: add extra intro line
    
    * chore: format update dirty file
    
    * feat: big update for feature deploy
    
    * chore: format update dirty file
    
    * feat: ui fixups
    
    * chore: format update dirty file
    
    * chore: no initial filter
    
    * chore: string updates
    
    * feat: add max results
    
    * chore: format update dirty file
    
    * fix: final review
    
    * chore: fix messages
    
    * chore: fix exemption query!
    
    * chore: fix query
    
    * chore: format update dirty file
    
    * fix: html eerror
    
    * feat: update mobile
    
    * chore: review fix
    
    * chore: format update dirty file
    
    * chore: revert chagne
    
    ---------
    
    Co-authored-by: islandisis-bot <ci@island.is>

---
+++ libs/api/domains/ship-registry/src/lib/resolvers/sailorCertificates.resolver.ts
@@ -0,0 +1,117 @@
+import { Context, Parent, ResolveField, Resolver } from '@nestjs/graphql'
+import { UseGuards } from '@nestjs/common'
+import {
+  IdsUserGuard,
+  Scopes,
+  ScopesGuard,
+  type User,
+} from '@island.is/auth-nest-tools'
+import { Audit, AuditService } from '@island.is/nest/audit'
+import { ApiScope } from '@island.is/auth/scopes'
+import {
+  FeatureFlag,
+  FeatureFlagGuard,
+  Features,
+} from '@island.is/nest/feature-flags'
+import { CodeOwner } from '@island.is/nest/core'
+import { CodeOwners } from '@island.is/shared/constants'
+import {
+  ShipRegistrySailorCertificates,
+  type ShipRegistrySailorCertificatesBase,
+} from '../models/sailorCertificates.model'
+import { ShipRegistrySailorSchoolCertificate } from '../models/sailorSchoolCertificate.model'
+import { ShipRegistrySailorRightCertificate } from '../models/sailorRightCertificate.model'
+import { ShipRegistrySailorMaritimeBook } from '../models/sailorMaritimeBook.model'
+import { ShipRegistrySailorRegistrationExemption } from '../models/sailorRegistrationExemption.model'
+import { SailorsService } from '../services/sailors.service'
+
+const namespace = '@island.is/api/ship-registry'
+
+@CodeOwner(CodeOwners.Hugsmidjan)
+@UseGuards(IdsUserGuard, ScopesGuard, FeatureFlagGuard)
+@Scopes(ApiScope.ships)
+@FeatureFlag(Features.isServicePortalSailorsPageEnabled)
+@Audit({ namespace })
+@Resolver(() => ShipRegistrySailorCertificates)
+export class SailorCertificatesResolver {
+  constructor(
+    private readonly sailorsService: SailorsService,
+    private readonly auditService: AuditService,
+  ) {}
+
+  @ResolveField(
+    'schoolCertificates',
+    () => [ShipRegistrySailorSchoolCertificate],
+    {
+      nullable: true,
+    },
+  )
+  async resolveSchoolCertificates(
+    @Context('req') { user }: { user: User },
+    @Parent() { locale }: ShipRegistrySailorCertificatesBase,
+  ): Promise<ShipRegistrySailorSchoolCertificate[]> {
+    this.auditService.audit({
+      auth: user,
+      namespace,
+      action: 'resolveSchoolCertificates',
+      resources: user.nationalId,
+    })
+    return this.sailorsService.getSailorSchoolCertificates(user, locale)
+  }
+
+  @ResolveField(
+    'rightCertificates',
+    () => [ShipRegistrySailorRightCertificate],
+    {
+      nullable: true,
+    },
+  )
+  async resolveRightCertificates(
+    @Context('req') { user }: { user: User },
+    @Parent() { locale }: ShipRegistrySailorCertificatesBase,
+  ): Promise<ShipRegistrySailorRightCertificate[]> {
+    this.auditService.audit({
+      auth: user,
+      namespace,
+      action: 'resolveRightCertificates',
+      resources: user.nationalId,
+    })
+    return this.sailorsService.getSailorRightCertificates(user, locale)
+  }
+
+  @ResolveField('seaServiceBooks', () => [ShipRegistrySailorMaritimeBook], {
+    nullable: true,
+  })
+  async resolveSeaServiceBooks(
+    @Context('req') { user }: { user: User },
+    @Parent() { locale }: ShipRegistrySailorCertificatesBase,
+  ): Promise<ShipRegistrySailorMaritimeBook[]> {
+    this.auditService.audit({
+      auth: user,
+      namespace,
+      action: 'resolveSeaServiceBooks',
+      resources: user.nationalId,
+    })
+    return this.sailorsService.getSailorMaritimeBooks(user, locale)
+  }
+
+  @ResolveField(
+    'registrationExemptions',
+    () => [ShipRegistrySailorRegistrationExemption],
+    {
+      nullable: true,
+    },
+  )
+  async resolveRegistrationExemptions(
+    @Context('req') { user }: { user: User },
+    @Parent() { locale }: ShipRegistrySailorCertificatesBase,
+  ): Promise<ShipRegistrySailorRegistrationExemption[]> {
+    this.auditService.audit({
+      auth: user,
+      namespace,
+      action: 'resolveRegistrationExemptions',
+      resources: user.nationalId,
+    })
+    return this.sailorsService.getSailorRegistrationExemptions(user, locale)
+  }
+}
--- libs/portals/my-pages/occupational-licenses/src/lib/paths.ts
+++ libs/portals/my-pages/occupational-licenses/src/lib/paths.ts
@@ -3,4 +3,8 @@ export enum OccupationalLicensesPaths {
   OccupationalLicensesDetail = '/starfsleyfi/:id',
   OccupationalLicensesHealthDirectorateDetail = '/starfsleyfi/landlaeknir/:type/:id',
   OccupationalLicensesEducationDetail = '/starfsleyfi/mms/:type/:id',
+  SailorsRoot = '/starfsleyfi/sjomenn',
+  SailorsSchoolCertificates = '/starfsleyfi/sjomenn/prof-nam-og-frestir',
+  SailorsRightCertificates = '/starfsleyfi/sjomenn/atvinnuskirteini',
+  SailorsCrewRegistrations = '/starfsleyfi/sjomenn/logskraningar',
 }
