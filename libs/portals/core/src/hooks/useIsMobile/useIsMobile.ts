90acaf42cb83abcfd395921dc1a0f5e575f1aa3d
thorkellmani <wowthorkell@gmail.com>
2026-06-25 13:34:21 +0000

    chore: update is mobile hook

--- libs/portals/core/src/hooks/useIsMobile/useIsMobile.ts
+++ libs/portals/core/src/hooks/useIsMobile/useIsMobile.ts
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react'
 import { theme } from '@island.is/island-ui/theme'
 import { useWindowSize } from 'react-use'
 
 export const useIsMobile = () => {
-  const [isClientMobile, setIsClientMobile] = useState(false)
   const { width } = useWindowSize()
-
-  useEffect(() => {
-    if (!!width && width < theme.breakpoints.md) {
-      return setIsClientMobile(true)
-    }
-    return setIsClientMobile(false)
-  }, [width])
-
-  const isMobile = isClientMobile
+  const isMobile = !!width && width < theme.breakpoints.md
 
   return {
     isMobile,
