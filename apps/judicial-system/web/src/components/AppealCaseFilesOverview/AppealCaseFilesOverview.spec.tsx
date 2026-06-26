ed166071f05a845ad7ae71d66635ea77a0a5d117
Ívar Oddsson <ivaroddsson@gmail.com>
2026-06-26 09:03:20 +0000

    test(j-s): scope button queries to context menu in AppealCaseFilesOverview

--- apps/judicial-system/web/src/components/AppealCaseFilesOverview/AppealCaseFilesOverview.spec.tsx
+++ apps/judicial-system/web/src/components/AppealCaseFilesOverview/AppealCaseFilesOverview.spec.tsx
@@ -52,7 +52,9 @@ describe('<AppealCaseFilesOverview />', () => {
       </IntlProvider>,
     )
 
-    expect(await screen.findAllByRole('button')).toHaveLength(2)
+    expect(
+      await screen.findAllByRole('button', { name: 'Valmynd' }),
+    ).toHaveLength(2)
   })
 
   test('should not have an option to delete file if the file is of category APPEAL_RULING', async () => {
@@ -78,7 +80,7 @@ describe('<AppealCaseFilesOverview />', () => {
         </ApolloProvider>
       </IntlProvider>,
     )
-    const button = await screen.findByRole('button')
+    const button = await screen.findByRole('button', { name: 'Valmynd' })
     await userEvent.click(button)
     expect(await screen.findAllByRole('menuitem')).toHaveLength(1)
   })
@@ -110,7 +112,7 @@ describe('<AppealCaseFilesOverview />', () => {
       </IntlProvider>,
     )
 
-    const button = await screen.findByRole('button')
+    const button = await screen.findByRole('button', { name: 'Valmynd' })
     await userEvent.click(button)
     expect(await screen.findAllByRole('menuitem')).toHaveLength(1)
   })
@@ -141,7 +143,7 @@ describe('<AppealCaseFilesOverview />', () => {
       </IntlProvider>,
     )
 
-    const button = await screen.findByRole('button')
+    const button = await screen.findByRole('button', { name: 'Valmynd' })
     await userEvent.click(button)
     expect(await screen.findAllByRole('menuitem')).toHaveLength(1)
   })
@@ -172,7 +174,7 @@ describe('<AppealCaseFilesOverview />', () => {
       </IntlProvider>,
     )
 
-    const button = await screen.findByRole('button')
+    const button = await screen.findByRole('button', { name: 'Valmynd' })
     await userEvent.click(button)
     expect(await screen.findAllByRole('menuitem')).toHaveLength(2)
   })
