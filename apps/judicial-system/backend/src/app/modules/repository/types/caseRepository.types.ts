85032e901103765e5a4b0bfbeb2e950c9114a1c4
gudjonss <gudjon.steinar.sverrisson@kolibri.is>
2026-06-26 11:16:09 +0000

    add sorting in parentcase

--- apps/judicial-system/backend/src/app/modules/repository/types/caseRepository.types.ts
+++ apps/judicial-system/backend/src/app/modules/repository/types/caseRepository.types.ts
@@ -153,6 +153,10 @@ export const caseInclude: Includeable[] = [
         model: CaseFile,
         as: 'caseFiles',
         required: false,
+        order: [
+          ['orderWithinChapter', 'ASC NULLS LAST'],
+          ['created', 'ASC'],
+        ],
         where: { state: { [Op.not]: CaseFileState.DELETED }, category: null },
         separate: true,
       },
