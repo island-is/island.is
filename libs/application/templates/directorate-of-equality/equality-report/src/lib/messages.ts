af603ab6aa98fcb3ce32c5dc9d1ea6d02067d0b4
disaerna <disa@hugsmidjan.is>
2026-07-01 14:32:56 +0000

    feat: boostrap application

---
+++ libs/application/templates/directorate-of-equality/equality-report/src/lib/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from "react-intl";
+
+export const general = defineMessages({
+  applicationName: {
+    id: 'equalityReport.application:general.applicationName',
+    defaultMessage: 'Jafnréttisáætlun',
+  },
+  institution: {
+    id: 'equalityReport.application:general.institution',
+    defaultMessage: 'Dómsmálaráðuneytið',
+  },
+})
