0f1af5874a4be73a3be96d0cd0ab6f322e5d16ca
gudjonss <gudjon.steinar.sverrisson@kolibri.is>
2026-07-01 10:35:23 +0000

    fix test

--- apps/judicial-system/backend/src/app/modules/case/test/internalCaseController/deliverIndictmentCaseToPolice.spec.ts
+++ apps/judicial-system/backend/src/app/modules/case/test/internalCaseController/deliverIndictmentCaseToPolice.spec.ts
@@ -1,4 +1,5 @@
 import { Base64 } from 'js-base64'
+import { Transaction } from 'sequelize'
 import { v4 as uuid } from 'uuid'
 
 import {
@@ -11,9 +12,14 @@ import {
 
 import { createTestingCaseModule } from '../createTestingCaseModule'
 
+import { nowFactory } from '../../../../factories'
+import { randomDate } from '../../../../test'
+import { FileService } from '../../../file'
 import { PoliceDocumentType, PoliceService } from '../../../police'
-import { Case, CaseFile } from '../../../repository'
+import { Case } from '../../../repository'
 import { DeliverResponse } from '../../models/deliver.response'
+
+jest.mock('../../../../factories')
 
 interface Then {
   result: DeliverResponse
@@ -23,18 +29,32 @@ interface Then {
 type GivenWhenThen = (caseId: string, theCase: Case) => Promise<Then>
 
 describe('InternalCaseController - Deliver indictment case to police', () => {
+  const date = randomDate()
   const userId = uuid()
   const user = { id: userId } as User
 
+  let mockFileService: FileService
   let mockPoliceService: PoliceService
+  let transaction: Transaction
   let givenWhenThen: GivenWhenThen
 
   beforeEach(async () => {
-    const { policeService, internalCaseController } =
+    const { sequelize, fileService, policeService, internalCaseController } =
       await createTestingCaseModule()
 
+    mockFileService = fileService
     mockPoliceService = policeService
 
+    const mockTransaction = sequelize.transaction as jest.Mock
+    transaction = {} as Transaction
+    mockTransaction.mockImplementationOnce(
+      (fn: (transaction: Transaction) => unknown) => fn(transaction),
+    )
+
+    const mockToday = nowFactory as jest.Mock
+    mockToday.mockReturnValueOnce(date)
+    const mockGetCaseFileFromS3 = fileService.getCaseFileFromS3 as jest.Mock
+    mockGetCaseFileFromS3.mockRejectedValue(new Error('Some error'))
     const mockUpdatePoliceCase = mockPoliceService.updatePoliceCase as jest.Mock
     mockUpdatePoliceCase.mockRejectedValue(new Error('Some error'))
 
@@ -54,20 +74,20 @@ describe('InternalCaseController - Deliver indictment case to police', () => {
     const caseId = uuid()
     const policeCaseNumber = uuid()
     const courtCaseNumber = uuid()
-    const courtRecordContent = 'test court record'
-
+    const defendantNationalId = '0123456789'
+    const courtRecordPdf = 'test court record'
     const courtRecordFile = {
       id: uuid(),
-      category: CaseFileCategory.COURT_RECORD,
+      key: uuid(),
       isKeyAccessible: true,
-    } as CaseFile
-
+      category: CaseFileCategory.COURT_RECORD,
+    }
     const rulingFile = {
       id: uuid(),
-      category: CaseFileCategory.RULING,
+      key: uuid(),
       isKeyAccessible: true,
-    } as CaseFile
-
+      category: CaseFileCategory.RULING,
+    }
     const theCase = {
       id: caseId,
       origin: CaseOrigin.LOKE,
@@ -76,56 +96,46 @@ describe('InternalCaseController - Deliver indictment case to police', () => {
       policeCaseNumbers: [policeCaseNumber],
       courtCaseNumber,
       defendants: [{ nationalId: uuid() }],
-      conclusion: '',
       caseFiles: [courtRecordFile, rulingFile],
+      policeDefendantNationalId: defendantNationalId,
     } as Case
 
     let then: Then
 
     beforeEach(async () => {
-      const mockGetCaseFile = jest
-        .fn()
-        .mockResolvedValue(Buffer.from(courtRecordContent))
-
-      const { fileService, policeService, internalCaseController } =
-        await createTestingCaseModule()
-
-      mockPoliceService = policeService
-
+      const mockGetCaseFileFromS3 =
+        mockFileService.getCaseFileFromS3 as jest.Mock
+      mockGetCaseFileFromS3.mockResolvedValueOnce(courtRecordPdf)
       const mockUpdatePoliceCase =
         mockPoliceService.updatePoliceCase as jest.Mock
       mockUpdatePoliceCase.mockResolvedValueOnce(true)
-      ;(fileService.getCaseFileFromS3 as jest.Mock) = mockGetCaseFile
-
-      givenWhenThen = async (caseId: string, theCase: Case) => {
-        const then = {} as Then
-
-        await internalCaseController
-          .deliverIndictmentCaseToPolice(caseId, theCase, { user })
-          .then((result) => (then.result = result))
-          .catch((error) => (then.error = error))
-
-        return then
-      }
 
       then = await givenWhenThen(caseId, theCase)
     })
 
     it('should only deliver court record files, not ruling files', () => {
+      expect(mockFileService.getCaseFileFromS3).toHaveBeenCalledWith(
+        theCase,
+        courtRecordFile,
+      )
+      expect(mockFileService.getCaseFileFromS3).not.toHaveBeenCalledWith(
+        theCase,
+        rulingFile,
+      )
       expect(mockPoliceService.updatePoliceCase).toHaveBeenCalledWith(
         user,
-        expect.any(String),
+        caseId,
         CaseType.INDICTMENT,
         CaseState.COMPLETED,
         policeCaseNumber,
         courtCaseNumber,
-        expect.any(String),
-        expect.any(Date),
+        defendantNationalId,
+        date,
         '',
         [
           {
             type: PoliceDocumentType.RVTB,
-            courtDocument: Base64.btoa(courtRecordContent),
+            courtDocument: Base64.btoa(courtRecordPdf),
           },
         ],
       )
