fe3a0d93ce46fe7fcc9198ab02941a55bdffcf41
Jason Guðnason <jasongudnaa@gmail.com>
2026-07-02 11:05:17 +0000

    fix(barnavernd): restrict Þjónustuveitandi and Barn to procuration users only

--- libs/application/templates/national-agency-for-children-and-families/child-protection-notification/src/forms/prerequisitesForm/index.ts
+++ libs/application/templates/national-agency-for-children-and-families/child-protection-notification/src/forms/prerequisitesForm/index.ts
@@ -5,6 +5,20 @@ import { nationalIdLookupSubSection } from '../childSection/nationalIdLookupSubS
 import { externalDataSubSection } from './externalDataSubSection'
 import { serviceProviderSubSection } from './serviceProviderSubSection'
 
+export const PersonalPrerequisites = buildForm({
+  id: 'prerequisites',
+  mode: FormModes.NOT_STARTED,
+  renderLastScreenButton: true,
+  renderLastScreenBackButton: true,
+  children: [
+    buildSection({
+      id: 'prerequisites',
+      title: prerequisitesMessages.shared.sectionTitle,
+      children: [externalDataSubSection],
+    }),
+  ],
+})
+
 export const Prerequisites = buildForm({
   id: 'prerequisites',
   mode: FormModes.NOT_STARTED,
