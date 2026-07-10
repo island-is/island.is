80f4d07df43ad5042100cee8565a565e045979d7
disaerna <disa@hugsmidjan.is>
2026-07-10 13:45:15 +0000

    fix: review comments

--- libs/application/templates/directorate-of-equality/salary-report/src/index.ts
+++ libs/application/templates/directorate-of-equality/salary-report/src/index.ts
@@ -2,6 +2,8 @@ import template from './lib/template'
 
 export const getFields = () => import('./fields/')
 
+export { dataSchema } from './lib/dataSchema'
 export type { ApplicationAnswers } from './lib/dataSchema'
+export { ChiefExecutiveGender } from './utils/types'
 
 export default template
