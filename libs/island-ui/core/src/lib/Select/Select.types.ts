d90c57b6e6ea4cddf1fa7339115872cd3083ef61
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2026-07-15 17:31:43 +0000

    fix(island-ui): keep react-select Props augmentation an interface
    The no-empty-object-type autofix converted the declaration-merging
    interface in Select.types.ts to a type alias, which drops the module
    augmentation and with it the custom props (size, icon, ...) on
    react-select. Restored with a guarding eslint-disable.

--- libs/island-ui/core/src/lib/Select/Select.types.ts
+++ libs/island-ui/core/src/lib/Select/Select.types.ts
@@ -43,11 +43,14 @@ export type PropsBase = {
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore make web strict
 declare module 'react-select/dist/declarations/src/Select' {
-  export type Props<
+  // must stay an interface: declaration merging augments react-select's Props;
+  // a type alias would replace instead of merge and drop our custom props
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Props<
     Option,
     IsMulti extends boolean,
     Group extends GroupBase<Option>,
-  > = PropsBase
+  > extends PropsBase {}
 }
 
 // The typescript declaration customizations above does not allow to change existing props signature.
