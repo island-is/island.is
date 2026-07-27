6e1f6f3c0c60bca9e1e957483590671d9583a041
Baldur Óli Barkarson <ballioli1@gmail.com>
2026-07-27 09:01:59 +0000

    Starting to setup virknistyrkur for minar sidur

---
+++ libs/portals/my-pages/social-benefits/src/lib/messages/activationAllowance.ts
@@ -0,0 +1,232 @@
+import { defineMessages } from 'react-intl'
+
+export const activationAllowanceMessages = defineMessages({
+  activationAllowance: {
+    id: 'sp.social-benefits-activation-allowance:activationAllowance',
+    defaultMessage: 'Virknistyrkur',
+  },
+  title: {
+    id: 'sp.social-benefits-activation-allowance:title',
+    defaultMessage: 'Staðan þín',
+  },
+  intro: {
+    id: 'sp.social-benefits-activation-allowance:intro',
+    defaultMessage: 'Hér er yfirlit yfir þína umsókn um virknistyrk',
+  },
+  tooltip: {
+    id: 'sp.social-benefits-activation-allowance:tooltip',
+    defaultMessage: 'Vinnumálastofnun',
+  },
+  statusContactUs: {
+    id: 'sp.social-benefits-activation-allowance:statusContactUs',
+    defaultMessage: 'Hafa samband',
+  },
+  statusContactUsUrl: {
+    id: 'sp.social-benefits-activation-allowance:statusContactUsUrl',
+    defaultMessage: 'https://vinnumalastofnun.is/is/samskipti/hafdu-samband',
+  },
+  statusSubmitDocuments: {
+    id: 'sp.social-benefits-activation-allowance:statusSubmitDocuments',
+    defaultMessage: 'Skila gögnum',
+  },
+  statusSubmitDocumentsUrl: {
+    id: 'sp.social-benefits-activation-allowance:statusSubmitDocumentsUrl',
+    defaultMessage: 'https://island.is/skila-gognum-vmst',
+  },
+  statusReportIncome: {
+    id: 'sp.social-benefits-activation-allowance:statusReportIncome',
+    defaultMessage: 'Tilkynna tekjur/vinnu',
+  },
+  statusReportIncomeUrl: {
+    id: 'sp.social-benefits-activation-allowance:statusReportIncomeUrl',
+    defaultMessage: 'https://island.is/umsoknir/tilkynna-tekjur',
+  },
+  statusReportTravel: {
+    id: 'sp.social-benefits-activation-allowance:statusReportTravel',
+    defaultMessage: 'Tilkynna utanlandsferð',
+  },
+  statusReportTravelUrl: {
+    id: 'sp.social-benefits-activation-allowance:statusReportTravelUrl',
+    defaultMessage: 'https://island.is/umsoknir/tilkynna-utanlandsferd',
+  },
+  statusUnsubscribe: {
+    id: 'sp.social-benefits-activation-allowance:statusUnsubscribe',
+    defaultMessage: 'Afskrá af bótum',
+  },
+  statusUnsubscribeUrl: {
+    id: 'sp.social-benefits-activation-allowance:statusUnsubscribeUrl',
+    defaultMessage: 'https://island.is/umsoknir/afskraning-atvinnuleysisbota',
+  },
+  statusMoreActions: {
+    id: 'sp.social-benefits-activation-allowance:statusMoreActions',
+    defaultMessage: 'Fleiri aðgerðir',
+  },
+  statusTabApplication: {
+    id: 'sp.social-benefits-activation-allowance:statusTabApplication',
+    defaultMessage: 'Yfirlit yfir stöðu umsóknar',
+  },
+  statusTabApplicant: {
+    id: 'sp.social-benefits-activation-allowance:statusTabApplicant',
+    defaultMessage: 'Upplýsingar um umsækjanda',
+  },
+  statusTabApplicationMobile: {
+    id: 'sp.social-benefits-activation-allowance:statusTabApplicationMobile',
+    defaultMessage: 'Yfirlit',
+  },
+  statusTabApplicantMobile: {
+    id: 'sp.social-benefits-activation-allowance:statusTabApplicantMobile',
+    defaultMessage: 'Upplýsingar',
+  },
+  statusDataLabel: {
+    id: 'sp.social-benefits-activation-allowance:statusDataLabel',
+    defaultMessage: 'Gögn',
+  },
+  statusDataContent: {
+    id: 'sp.social-benefits-activation-allowance:statusDataContent',
+    defaultMessage: 'Nánari upplýsingar má finna undir Mín gögn',
+  },
+  statusDataMissing: {
+    id: 'sp.social-benefits-activation-allowance:statusDataMissing',
+    defaultMessage: 'Vantar gögn',
+  },
+  jobSearchConfirmationHeading: {
+    id: 'sp.social-benefits-activation-allowance:jobSearchConfirmationHeading',
+    defaultMessage: 'Staðfesta atvinnuleit',
+  },
+  jobSearchConfirmationText: {
+    id: 'sp.social-benefits-activation-allowance:jobSearchConfirmationText',
+    defaultMessage:
+      'Mikilvægt er að staðfesta atvinnuleit mánaðarlega til að halda bótarétti. Staðfestingartímabil er frá 20.–25. hvers mánaðar.',
+  },
+  jobSearchConfirmationUrl: {
+    id: 'sp.social-benefits-activation-allowance:jobSearchConfirmationUrl',
+    defaultMessage: 'https://island.is/umsoknir/stadfesta-atvinnuleit',
+  },
+  jobSearchConfirmationCta: {
+    id: 'sp.social-benefits-activation-allowance:jobSearchConfirmationCta',
+    defaultMessage: 'Staðfesta hér',
+  },
+  jobSearchConfirmationNextDate: {
+    id: 'sp.social-benefits-activation-allowance:jobSearchConfirmationNextDate',
+    defaultMessage: 'Næst: {dateRange}',
+    description: 'Label for next job search confirmation date range',
+  },
+  yes: {
+    id: 'sp.social-benefits-activation-allowance:yes',
+    defaultMessage: 'Já',
+  },
+  no: {
+    id: 'sp.social-benefits-activation-allowance:no',
+    defaultMessage: 'Nei',
+  },
+  applicantPassCode: {
+    id: 'sp.social-benefits-activation-allowance:applicantPassCode',
+    defaultMessage: 'Lykilorð',
+  },
+  applicantPreferredJobs: {
+    id: 'sp.social-benefits-activation-allowance:applicantPreferredJobs',
+    defaultMessage: 'Óskastörf',
+  },
+  applicantBankAccount: {
+    id: 'sp.social-benefits-activation-allowance:applicantBankAccount',
+    defaultMessage: 'Innleggsreikningur',
+  },
+  applicantUnion: {
+    id: 'sp.social-benefits-activation-allowance:applicantUnion',
+    defaultMessage: 'Stéttarfélag',
+  },
+  applicantPensionFund: {
+    id: 'sp.social-benefits-activation-allowance:applicantPensionFund',
+    defaultMessage: 'Lífeyrissjóður',
+  },
+  applicantUsedPersonalTaxCredit: {
+    id: 'sp.social-benefits-activation-allowance:applicantUsedPersonalTaxCredit',
+    defaultMessage: 'Nýting persónuafsláttar',
+  },
+  applicantNumberOfChildren: {
+    id: 'sp.social-benefits-activation-allowance:applicantNumberOfChildren',
+    defaultMessage: 'Börn á framfæri',
+  },
+  applicantEmploymentHistory: {
+    id: 'sp.social-benefits-activation-allowance:applicantEmploymentHistory',
+    defaultMessage: 'Atvinnuferill',
+  },
+  applicantEducationHistory: {
+    id: 'sp.social-benefits-activation-allowance:applicantEducationHistory',
+    defaultMessage: 'Menntun',
+  },
+  applicantDrivingLicenses: {
+    id: 'sp.social-benefits-activation-allowance:applicantDrivingLicenses',
+    defaultMessage: 'Ökuréttindi',
+  },
+  applicantLanguageAbilities: {
+    id: 'sp.social-benefits-activation-allowance:applicantLanguageAbilities',
+    defaultMessage: 'Tungumál',
+  },
+  applicantServiceArea: {
+    id: 'sp.social-benefits-activation-allowance:applicantServiceArea',
+    defaultMessage: 'Þjónustuskrifstofa',
+  },
+  applicantCurrentAddressDifferent: {
+    id: 'sp.social-benefits-activation-allowance:applicantCurrentAddressDifferent',
+    defaultMessage: 'Dvalarstaður annar en lögheimili',
+  },
+  applicantSavedToEures: {
+    id: 'sp.social-benefits-activation-allowance:applicantSavedToEures',
+    defaultMessage: 'Skráning á EURES svæðinu',
+  },
+  applicantEditInfo: {
+    id: 'sp.social-benefits-activation-allowance:applicantEditInfo',
+    defaultMessage: 'Breyta upplýsingum',
+  },
+  applicantEditInfoUrl: {
+    id: 'sp.social-benefits-activation-allowance:applicantEditInfoUrl',
+    defaultMessage: 'https://island.is/umsoknir/breyta-upplysingum',
+  },
+  myDataTitle: {
+    id: 'sp.social-benefits-activation-allowance:myDataTitle',
+    defaultMessage: 'Mín gögn',
+  },
+  myDataIntro: {
+    id: 'sp.social-benefits-activation-allowance:myDataIntro',
+    defaultMessage:
+      'Hér eru skjöl og gögn sem þú hefur lagt fram til Vinnumálastofnunar, ásamt yfirliti yfir það sem enn vantar.',
+  },
+  myDataMissingAttachmentsHeading: {
+    id: 'sp.social-benefits-activation-allowance:myDataMissingAttachmentsHeading',
+    defaultMessage: 'Óskað er eftir að eftirfarandi gögn verði lögð fram',
+  },
+  myDataSubmittedAttachmentsHeading: {
+    id: 'sp.social-benefits-activation-allowance:myDataSubmittedAttachmentsHeading',
+    defaultMessage: 'Gögn sem þú hefur skilað',
+  },
+  myDataLettersHeading: {
+    id: 'sp.social-benefits-activation-allowance:myDataLettersHeading',
+    defaultMessage: 'Bréf frá Vinnumálastofnun',
+  },
+  myDataMissingTag: {
+    id: 'sp.social-benefits-activation-allowance:myDataMissingTag',
+    defaultMessage: 'Vantar gögn',
+  },
+  myDataSubmitDocuments: {
+    id: 'sp.social-benefits-activation-allowance:myDataSubmitDocuments',
+    defaultMessage: 'Skila gögnum',
+  },
+  myDataSubmitDocumentsUrl: {
+    id: 'sp.social-benefits-activation-allowance:myDataSubmitDocumentsUrl',
+    defaultMessage:
+      'https://island.is/umsoknir/breyta-upplysingum-atvinnuleysisbaetur',
+  },
+  myDataViewDocument: {
+    id: 'sp.social-benefits-activation-allowance:myDataViewDocument',
+    defaultMessage: 'Skoða',
+  },
+  myDataDownloadDocument: {
+    id: 'sp.social-benefits-activation-allowance:myDataDownloadDocument',
+    defaultMessage: 'Sækja',
+  },
+  myDataAttachmentError: {
+    id: 'sp.social-benefits-activation-allowance:myDataAttachmentError',
+    defaultMessage: 'Ekki tókst að sækja skjal',
+  },
+})
---
+++ libs/portals/my-pages/social-benefits/src/lib/routes/activationAllowanceRoutes.ts
@@ -0,0 +1,30 @@
+import { PortalRoute } from '@island.is/portals/core'
+import { BffUser } from '@island.is/shared/types'
+import { m, sharedMessages } from '../messages'
+import { ActivationAllowancePaths } from '../paths'
+
+export const activationAllowanceRoutes = (userInfo: BffUser): PortalRoute[] => [
+  {
+    name: m.maintenance,
+    key: 'ActivationAllowance',
+    path: ActivationAllowancePaths.Root,
+    enabled: true,
+    element: null,
+  },
+  {
+    name: sharedMessages.myStatus,
+    key: 'ActivationAllowance',
+    path: ActivationAllowancePaths.Status,
+    enabled: true,
+    dynamic: true,
+    element: null,
+  },
+  {
+    name: sharedMessages.myData,
+    key: 'ActivationAllowance',
+    path: ActivationAllowancePaths.MyData,
+    enabled: true,
+    dynamic: true,
+    element: null,
+  },
+]
