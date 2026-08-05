3d0c07f1d6592aa2b0d82c4298c356af66b49871
Magnea Rún Vignisdóttir <magneavignisdottir@gmail.com>
2026-08-05 11:04:00 +0000

    fix: remove leftover buildCsp import after CSP revert

--- apps/web/server.ts
+++ apps/web/server.ts
@@ -1,7 +1,6 @@
 import { bootstrap } from '@island.is/infra-next-server'
 
 import { getServerRuntimeEnv } from './environments/runtimeEnvironment'
-import { buildCsp } from './csp'
 import proxyConfig from './proxy.config.json'
 
 bootstrap({
