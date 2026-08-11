18e9b99b21fec5ab412722685f20debc302a9ccf
unakb <unakben@gmail.com>
2026-08-11 17:37:41 +0000

    Added filter count

--- libs/portals/my-pages/health/src/screens/HealthConversations/HealthConversations.tsx
+++ libs/portals/my-pages/health/src/screens/HealthConversations/HealthConversations.tsx
@@ -111,6 +111,9 @@ const HealthConversations = () => {
     onError: onMutationError,
   })
 
+  const filterCount =
+    (filterValues.starred ? 1 : 0) + (filterValues.archived ? 1 : 0)
+
   const filteredConversations = useMemo(() => {
     if (!healthConversations) {
       return []
@@ -159,6 +162,7 @@ const HealthConversations = () => {
             variant="popover"
             align="left"
             mobileWrap={false}
+            filterCount={filterCount}
             filterInput={
               <Input
                 name="messageSearch"
