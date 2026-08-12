ff2e84356f34b8a05e51279256f026af32a90b07
Veronika Sif <vesigurdardottir@deloitte.is>
2026-08-12 13:38:04 +0000

    Fix lint error: Update Storybook import to use react-webpack5

--- libs/island-ui/core/src/lib/Scale/Scale.stories.tsx
+++ libs/island-ui/core/src/lib/Scale/Scale.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-webpack5'
 import { withFigma } from '../../utils/withFigma'
 import { Scale, type ScaleProps } from './Scale'
 
