106dbab0e7606d7282415804df832bf6443ece7c
Veronika Sif <vesigurdardottir@deloitte.is>
2026-08-17 14:33:52 +0000

    Updated Child safety urgency AlertMessage

--- libs/application/templates/national-agency-for-children-and-families/child-protection-notification/src/forms/shared/childSafetySection.ts
+++ libs/application/templates/national-agency-for-children-and-families/child-protection-notification/src/forms/shared/childSafetySection.ts
@@ -29,37 +29,6 @@ export const childSafetySection = buildSection({
           doesNotRequireAnswer: true,
           space: 2,
         }),
-        buildAlertMessageField({
-          id: 'childSafety.urgencyDescription',
-          alertType: 'info',
-          doesNotRequireAnswer: true,
-          marginBottom: 0,
-          condition: (answers) => {
-            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
-            return (
-              childSafetyUrgencyLevel !== null &&
-              childSafetyUrgencyLevel !== undefined
-            )
-          },
-          title: ({ answers, externalData }) => {
-            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
-            const { childSafetyLevels } =
-              getApplicationExternalData(externalData)
-            return (
-              childSafetyLevels.find((a) => a.value === childSafetyUrgencyLevel)
-                ?.label ?? ''
-            )
-          },
-          message: ({ answers, externalData }) => {
-            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
-            const { childSafetyLevels } =
-              getApplicationExternalData(externalData)
-            return (
-              childSafetyLevels.find((a) => a.value === childSafetyUrgencyLevel)
-                ?.description ?? ''
-            )
-          },
-        }),
         buildScaleField({
           id: 'childSafetyUrgencyLevel',
           min: 0,
@@ -84,6 +53,51 @@ export const childSafetySection = buildSection({
           required: true,
           marginTop: 2,
           marginBottom: 2,
+        }),
+        buildAlertMessageField({
+          id: 'childSafety.urgencyDescription',
+          alertType: ({ answers }) => {
+            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
+
+            const urgencyLevel = Number(childSafetyUrgencyLevel)
+
+            if (urgencyLevel <= 3) {
+              return 'error'
+            }
+
+            if (urgencyLevel <= 6) {
+              return 'warning'
+            }
+
+            return 'info'
+          },
+          doesNotRequireAnswer: true,
+          marginTop: 0,
+          title: ({ answers, externalData }) => {
+            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
+            const { childSafetyLevels } =
+              getApplicationExternalData(externalData)
+            return (
+              childSafetyLevels.find((a) => a.value === childSafetyUrgencyLevel)
+                ?.label ?? ''
+            )
+          },
+          message: ({ answers, externalData }) => {
+            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
+            const { childSafetyLevels } =
+              getApplicationExternalData(externalData)
+            return (
+              childSafetyLevels.find((a) => a.value === childSafetyUrgencyLevel)
+                ?.description ?? ''
+            )
+          },
+          condition: (answers) => {
+            const { childSafetyUrgencyLevel } = getApplicationAnswers(answers)
+            return (
+              childSafetyUrgencyLevel !== null &&
+              childSafetyUrgencyLevel !== undefined
+            )
+          },
         }),
         buildAlertMessageField({
           id: 'childSafety.emergencyWarning',
