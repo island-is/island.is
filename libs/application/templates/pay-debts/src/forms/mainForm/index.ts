ce454c5c0762678187ecb34a4c5578fdef28d60a
helgahjartar <helga.hjartardottir@origo.is>
2026-08-25 12:21:25 +0000

    Add getSelectedDebts and some UI updates

--- libs/application/templates/pay-debts/src/forms/mainForm/index.ts
+++ libs/application/templates/pay-debts/src/forms/mainForm/index.ts
@@ -2,11 +2,10 @@ import { buildForm } from '@island.is/application/core'
 import { FormModes } from '@island.is/application/types'
 import { debtsSection } from './debtsSection'
 import { paymentSection } from './paymentSection'
-import { overviewSection } from './overview'
 
 export const MainForm = buildForm({
   id: 'MainForm',
   mode: FormModes.DRAFT,
   renderLastScreenButton: true,
-  children: [debtsSection, paymentSection, overviewSection],
+  children: [debtsSection, paymentSection],
 })
