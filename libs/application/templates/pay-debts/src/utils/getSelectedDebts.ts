0d67930f46a812549f31349efc8e99d8fe3314f1
helgahjartar <helga.hjartardottir@origo.is>
2026-08-26 21:43:01 +0000

    Send selected debts over to payment overview

--- libs/application/templates/pay-debts/src/utils/getSelectedDebts.ts
+++ libs/application/templates/pay-debts/src/utils/getSelectedDebts.ts
@@ -1,13 +1,28 @@
 import { getValueViaPath } from '@island.is/application/core'
 import { Application } from '@island.is/application/types'
-import { CustomerDebt } from './types'
+import { SelectedDebt } from './types'
 import { getDebts } from './getDebts'
 
-export const getSelectedDebts = (application: Application): CustomerDebt[] => {
+export const getSelectedDebts = (application: Application): SelectedDebt[] => {
   const debts = getDebts(application)
 
   const selectedFlags =
     getValueViaPath<boolean[]>(application.answers, 'selectedDebts') ?? []
+  const amountsToPay =
+    getValueViaPath<string[]>(application.answers, 'debtsToPay') ?? []
 
-  return debts.filter((_, index) => !!selectedFlags[index])
+  return debts.reduce<SelectedDebt[]>((selected, debt, index) => {
+    if (!selectedFlags[index]) {
+      return selected
+    }
+
+    const amountToPay = parseInt(amountsToPay[index], 10)
+
+    selected.push({
+      ...debt,
+      amountToPay: Number.isNaN(amountToPay) ? debt.debts : amountToPay,
+    })
+
+    return selected
+  }, [])
 }
