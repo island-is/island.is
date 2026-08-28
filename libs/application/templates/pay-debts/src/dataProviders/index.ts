7a5d81f30e22c6bfb804853fadedf85aba3c3d88
helgahjartar <helga.hjartardottir@origo.is>
2026-08-28 14:26:26 +0000

    Setup structure for payment state, enable mock payments for testing

--- libs/application/templates/pay-debts/src/dataProviders/index.ts
+++ libs/application/templates/pay-debts/src/dataProviders/index.ts
@@ -1,7 +1,19 @@
-import { defineTemplateApi } from '@island.is/application/types'
+import {
+  defineTemplateApi,
+  InstitutionNationalIds,
+  MockablePaymentCatalogApi,
+} from '@island.is/application/types'
 
 export const GetDebtsApi = defineTemplateApi({
   action: 'getCustomerDebts',
   externalDataId: 'customerDebts',
   namespace: 'PayDebts',
+})
+
+export const MockPaymentCatalog = MockablePaymentCatalogApi.configure({
+  params: {
+    organizationId: InstitutionNationalIds.FJARSYSLA_RIKISINS,
+    enableMockPayment: true,
+  },
+  externalDataId: 'payment',
 })
