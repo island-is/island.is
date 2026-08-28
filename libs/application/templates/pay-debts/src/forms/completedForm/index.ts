5468e054c4348679b20a3a224a3c6b4b0e2b6914
helgahjartar <helga.hjartardottir@origo.is>
2026-08-28 11:35:01 +0000

    Update message setup for overview screen

--- libs/application/templates/pay-debts/src/forms/completedForm/index.ts
+++ libs/application/templates/pay-debts/src/forms/completedForm/index.ts
@@ -1,14 +1,15 @@
 import { buildForm } from '@island.is/application/core'
 import { buildFormConclusionSection } from '@island.is/application/ui-forms'
 import { FormModes } from '@island.is/application/types'
+import { completedForm as messages } from '../../lib/messages'
 
 export const completedForm = buildForm({
   id: 'completedForm',
   mode: FormModes.COMPLETED,
+  title: messages.alertTitle,
   children: [
     buildFormConclusionSection({
-      alertTitle: 'Staðfesting',
-      alertMessage: 'Greiðsla til Fjársýslunnar hefur verið móttekin!',
+      alertMessage: messages.alertMessage,
     }),
   ],
 })
---
+++ libs/application/templates/pay-debts/src/lib/messages/completedForm.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from 'react-intl'
+
+export const completedForm = defineMessages({
+  alertTitle: {
+    id: 'pd.application:completedForm.alertTitle',
+    defaultMessage: 'Staðfesting',
+    description: 'Title of the alert message in the completed form',
+  },
+  alertMessage: {
+    id: 'pd.application:completedForm.alertMessage',
+    defaultMessage: 'Greiðsla til Fjársýslunnar hefur verið móttekin!',
+    description: 'Message of the alert message in the completed form',
+  },
+})
