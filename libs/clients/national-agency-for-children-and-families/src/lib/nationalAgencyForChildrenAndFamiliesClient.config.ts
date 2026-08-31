3985d93d114e8f2ad241f67f242e6570ded5e03a
Jason Guðnason <jasongudnaa@gmail.com>
2026-08-31 09:53:24 +0000

    feat: change barnavernd api connection to xroad

--- libs/clients/national-agency-for-children-and-families/src/lib/nationalAgencyForChildrenAndFamiliesClient.config.ts
+++ libs/clients/national-agency-for-children-and-families/src/lib/nationalAgencyForChildrenAndFamiliesClient.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from '@island.is/nest/config'
 import * as z from 'zod'
 
 const schema = z.object({
-  baseUrl: z.string(),
+  xroadApiPath: z.string(),
+  xroadAuthPath: z.string(),
   clientId: z.string(),
   clientSecret: z.string(),
   scope: z.array(z.string()),
@@ -13,7 +14,14 @@ export const NationalAgencyForChildrenAndFamiliesClientConfig = defineConfig({
   schema,
   load(env) {
     return {
-      baseUrl: env.required('BARNAVERND_API_URL'),
+      xroadApiPath: env.required(
+        'XROAD_BARNAVERND_API_PATH',
+        'IS-DEV/GOV/10077/BOFS-Protected/bvg-digital-iceland',
+      ),
+      xroadAuthPath: env.required(
+        'XROAD_BARNAVERND_AUTH_PATH',
+        'IS-DEV/GOV/10077/BOFS-Protected/bvg-auth',
+      ),
       clientId: env.required('BARNAVERND_API_CLIENT_ID'),
       clientSecret: env.required('BARNAVERND_API_CLIENT_SECRET'),
       scope: env.optionalJSON('BARNAVERND_API_SCOPE') ?? [],
