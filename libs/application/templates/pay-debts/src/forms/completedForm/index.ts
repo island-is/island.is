f879ede5f12437dab79a39dcca84480e1cfa7357
helgahjartar <helga.hjartardottir@origo.is>
2026-08-31 09:23:36 +0000

    Include Fjársýsla ríkisins logo on forms

--- libs/application/templates/pay-debts/src/forms/completedForm/index.ts
+++ libs/application/templates/pay-debts/src/forms/completedForm/index.ts
@@ -1,6 +1,7 @@
 import { buildForm, buildSection } from '@island.is/application/core'
 import { buildFormConclusionSection } from '@island.is/application/ui-forms'
 import { FormModes } from '@island.is/application/types'
+import { FjarsyslaRikisinsLogo } from '@island.is/application/assets/institution-logos'
 import {
   completedForm as messages,
   debts as debtsMessages,
@@ -10,6 +11,7 @@ import {
 export const completedForm = buildForm({
   id: 'completedForm',
   mode: FormModes.COMPLETED,
+  logo: FjarsyslaRikisinsLogo,
   children: [
     buildSection({
       id: 'debtsSection',
