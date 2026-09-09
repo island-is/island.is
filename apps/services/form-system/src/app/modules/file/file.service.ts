48bc5a9156dcc0b96a56659d12bf226b06860fcb
OlafurGeorg <olafur.georg.gylfason@advania.is>
2026-09-09 11:27:51 +0000

    add isTest to file upload

--- apps/services/form-system/src/app/modules/file/file.service.ts
+++ apps/services/form-system/src/app/modules/file/file.service.ts
@@ -8,6 +8,7 @@ import { Value } from '../applications/models/value.model'
 import { FileConfig } from './file.config'
 import { Sequelize } from 'sequelize-typescript'
 import { Transaction } from 'sequelize'
+import { Application } from '../applications/models/application.model'
 
 @Injectable()
 export class FileService {
@@ -19,6 +20,8 @@ export class FileService {
     private readonly fileStorageService: FileStorageService,
     @InjectModel(Value)
     private readonly valueModel: typeof Value,
+    @InjectModel(Application)
+    private readonly applicationModel: typeof Application,
     private readonly sequelize: Sequelize,
   ) {}
 
@@ -45,6 +48,7 @@ export class FileService {
       if (exists) {
         let applicationId = ''
         let key = ''
+        let isTest: boolean | undefined
         try {
           await this.sequelize.transaction(async (transaction) => {
             const value = await this.valueModel.findByPk(valueId, {
@@ -83,11 +87,22 @@ export class FileService {
             await value.save({ transaction })
           })
 
+          const application = await this.applicationModel.findByPk(
+            applicationId,
+          )
+          if (!application) {
+            throw new NotFoundException(
+              `Application with id '${applicationId}' not found`,
+            )
+          }
+          isTest = application.isTest
+
           this.logger.info('form system file uploaded', {
             applicationId,
             key,
             fieldId,
             valueId,
+            isTest,
             datadogEvent: 'form_system_file_uploaded',
           })
 
@@ -98,6 +113,7 @@ export class FileService {
             key,
             fieldId,
             valueId,
+            isTest,
             datadogEvent: 'form_system_file_upload_failed',
             error,
           })
