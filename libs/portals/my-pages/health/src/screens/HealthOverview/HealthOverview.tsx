a0de95294fbd4bd0bec3a9bbf033ba0f662f2624
Una Kristín <una@kolibri.is>
2026-09-11 09:59:57 +0000

    Update libs/portals/my-pages/health/src/screens/HealthOverview/HealthOverview.tsx
    Co-authored-by: Þorkell Máni Þorkelsson <wowthorkell@gmail.com>

--- libs/portals/my-pages/health/src/screens/HealthOverview/HealthOverview.tsx
+++ libs/portals/my-pages/health/src/screens/HealthOverview/HealthOverview.tsx
@@ -220,7 +220,7 @@ export const HealthOverview = () => {
                       callback={() =>
                         healthOverviewQuickLinkClick({
                           ...formatPlausiblePathToParams(pathname),
-                          label: link.label,
+                          label: link.href,
                         })
                       }
                     >
