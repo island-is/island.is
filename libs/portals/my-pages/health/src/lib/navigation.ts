0f825b6e32e014c36420edceecb6dcb871096257
unakb <unakben@gmail.com>
2026-09-18 10:09:38 +0000

    Update navigation.ts

--- libs/portals/my-pages/health/src/lib/navigation.ts
+++ libs/portals/my-pages/health/src/lib/navigation.ts
@@ -100,7 +100,6 @@ export const healthNavigation: PortalNavigationItem = {
     {
       name: messages.appointments,
       searchHide: false,
-      breadcrumbHide: true,
       path: HealthPaths.HealthAppointments,
       searchTags: [s.appointment, s.appointmentBook, s.appointmentDoctor],
       children: [
