6c0ab1a021c2fafebe52d764b1cb466d7d2d0d3d
Baldur Óli <ballioli1@gmail.com>
2026-09-24 08:20:51 +0000

    fix(application-system): Fixing action card and in review last screen (#23861)
    Changing action card and completion screen

--- libs/application/templates/hms/housing-benefits/src/lib/messages/inReviewMessages.ts
+++ libs/application/templates/hms/housing-benefits/src/lib/messages/inReviewMessages.ts
@@ -17,4 +17,32 @@ export const inReviewMessages = defineMessages({
       'Umsóknin þín er nú í vinnslu hjá HMS og verður tekin til afgreiðslu sem fyrst.',
     description: 'In review conclusion alert message',
   },
+  descriptionFieldTitle: {
+    id: 'hb.application:inReview.descriptionFieldTitle',
+    defaultMessage: 'Staða umsóknar',
+    description: 'In review conclusion description field title',
+  },
+  descriptionFieldDescription: {
+    id: 'hb.application:inReview.descriptionFieldDescription#markdown',
+    defaultMessage:
+      'Hægt er að sjá stöðu umsóknar og nánari upplýsingar á mínum síðum HMS https://minarsidur.hms.is',
+    description: 'In review conclusion description field description',
+  },
+  expandableDescription: {
+    id: 'hb.application:inReview.expandableDescription#markdown',
+    defaultMessage:
+      '- Yfirferð umsóknar - Starfsfólk HMS fer yfir umsóknina og öll fylgigögn ef á við',
+    description: 'In review conclusion description field description',
+  },
+  bottomButtonMessage: {
+    id: 'hb.application:inReview.bottomButtonMessage',
+    defaultMessage:
+      'Á mínum síðum og í appi eru nú margvíslegar upplýsingar s.s. stafrænt pósthólf, þínar upplýsingar, fjármál, umsóknir, menntun, ökutæki, skírteini, starfsleyfi o.fl.',
+    description: 'In review conclusion bottom button message',
+  },
+  bottomButtonLabel: {
+    id: 'hb.application:inReview.bottomButtonLabel',
+    defaultMessage: 'Áfram',
+    description: 'In review conclusion bottom button label',
+  },
 })
