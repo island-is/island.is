d45d1bb1502de9f63fd4ebd152f6bab050557fee
sigurthor-origo <sigurthor.sumarlidason@origo.is>
2026-09-24 12:43:16 +0000

    Reorder action buttons when on mobile so wrap looks clean

--- libs/portals/my-pages/social-benefits/src/screens/unemployment-benefits/components/ActionButtons.tsx
+++ libs/portals/my-pages/social-benefits/src/screens/unemployment-benefits/components/ActionButtons.tsx
@@ -2,7 +2,7 @@ import { Box } from '@island.is/island-ui/core'
 import { useLocale, useNamespaces } from '@island.is/localization'
 import { unemploymentBenefitsMessages as um } from '../../../lib/messages/unemployment'
 import { VmstApplicationsAvailableActions } from '@island.is/portals/my-pages/graphql'
-import { LinkButton } from '@island.is/portals/my-pages/core'
+import { LinkButton, useIsMobile } from '@island.is/portals/my-pages/core'
 
 interface ActionButtonsProps {
   availableActions?: VmstApplicationsAvailableActions
@@ -15,6 +15,7 @@ export const ActionButtons = ({
 }: ActionButtonsProps) => {
   const { formatMessage } = useLocale()
   useNamespaces('sp.social-benefits-unemployment')
+  const { isMobile } = useIsMobile()
   const showContactButton = availableActions?.canContact === true
   const showSubmitDocumentsButton =
     availableActions?.canSubmitDocuments === true
@@ -36,6 +37,73 @@ export const ActionButtons = ({
     return null
   }
 
+  const contactButton = showContactButton && (
+    <LinkButton
+      key="contact"
+      to={formatMessage(um.statusContactUsUrl)}
+      text={formatMessage(um.statusContactUs)}
+      icon="open"
+      variant="utility"
+      size="small"
+    />
+  )
+  const submitDocumentsButton = showSubmitDocumentsButton && (
+    <LinkButton
+      key="submitDocuments"
+      to={formatMessage(um.statusSubmitDocumentsUrl)}
+      text={formatMessage(um.statusSubmitDocuments)}
+      icon="documents"
+      variant="utility"
+      size="small"
+    />
+  )
+  const reportIncomeButton = showReportIncomeButton && (
+    <LinkButton
+      key="reportIncome"
+      to={formatMessage(um.statusReportIncomeUrl)}
+      text={formatMessage(um.statusReportIncome)}
+      icon="wallet"
+      variant="utility"
+      size="small"
+    />
+  )
+  const reportTravelButton = showReportTravelButton && (
+    <LinkButton
+      key="reportTravel"
+      to={formatMessage(um.statusReportTravelUrl)}
+      text={formatMessage(um.statusReportTravel)}
+      icon="airplane"
+      variant="utility"
+      size="small"
+    />
+  )
+  const unsubscribeButton = showUnsubscribeButton && (
+    <LinkButton
+      key="unsubscribe"
+      to={formatMessage(um.statusUnsubscribeUrl)}
+      text={formatMessage(um.statusUnsubscribe)}
+      icon="logOut"
+      variant="utility"
+      size="small"
+    />
+  )
+
+  const orderedButtons = isMobile
+    ? [
+        contactButton,
+        submitDocumentsButton,
+        reportIncomeButton,
+        unsubscribeButton,
+        reportTravelButton,
+      ]
+    : [
+        contactButton,
+        submitDocumentsButton,
+        reportIncomeButton,
+        reportTravelButton,
+        unsubscribeButton,
+      ]
+
   return (
     <Box
       display="flex"
@@ -45,51 +113,7 @@ export const ActionButtons = ({
       alignItems="center"
       marginBottom={4}
     >
-      {showContactButton && (
-        <LinkButton
-          to={formatMessage(um.statusContactUsUrl)}
-          text={formatMessage(um.statusContactUs)}
-          icon="open"
-          variant="utility"
-          size="small"
-        />
-      )}
-      {showSubmitDocumentsButton && (
-        <LinkButton
-          to={formatMessage(um.statusSubmitDocumentsUrl)}
-          text={formatMessage(um.statusSubmitDocuments)}
-          icon="documents"
-          variant="utility"
-          size="small"
-        />
-      )}
-      {showReportIncomeButton && (
-        <LinkButton
-          to={formatMessage(um.statusReportIncomeUrl)}
-          text={formatMessage(um.statusReportIncome)}
-          icon="wallet"
-          variant="utility"
-          size="small"
-        />
-      )}
-      {showReportTravelButton && (
-        <LinkButton
-          to={formatMessage(um.statusReportTravelUrl)}
-          text={formatMessage(um.statusReportTravel)}
-          icon="airplane"
-          variant="utility"
-          size="small"
-        />
-      )}
-      {showUnsubscribeButton && (
-        <LinkButton
-          to={formatMessage(um.statusUnsubscribeUrl)}
-          text={formatMessage(um.statusUnsubscribe)}
-          icon="logOut"
-          variant="utility"
-          size="small"
-        />
-      )}
+      {orderedButtons}
     </Box>
   )
 }
